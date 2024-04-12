2c9a4b042ed99e5219ea02eedd98448bc91f2958
oswian <oswian123@gmail.com>
2024-04-12 11:53:03 +1000

    Add features-grid story for newsletter flow

--- packages/plans-grid-next/src/storybook-mocks/grid-plans.tsx
+++ packages/plans-grid-next/src/storybook-mocks/grid-plans.tsx
@@ -3,7 +3,8 @@ import { GridPlan } from '../types';
 import { jetpackFeatures, wpcomFeatures } from './features';
 
 type BillingPeriod = 365 | -1 | 31 | 730 | 1095 | undefined;
-const freePlan: GridPlan = {
+
+export const free: GridPlan = {
 	planSlug: 'free_plan' as PlanSlug,
 	isVisible: true,
 	tagline: 'Get a taste of the world’s most popular CMS & blogging software.',
@@ -41,7 +42,7 @@ const freePlan: GridPlan = {
 	},
 };
 
-const personalBundle: GridPlan = {
+export const personal: GridPlan = {
 	planSlug: 'personal-bundle' as PlanSlug,
 	isVisible: true,
 	tagline: 'Create your home on the web with a custom domain name.',
@@ -80,7 +81,8 @@ const personalBundle: GridPlan = {
 		],
 	},
 };
-const valueBundle: GridPlan = {
+
+export const value: GridPlan = {
 	planSlug: 'value_bundle' as PlanSlug,
 	isVisible: true,
 	tagline: 'Build a unique website with powerful design tools.',
@@ -120,7 +122,7 @@ const valueBundle: GridPlan = {
 	},
 };
 
-const businessBundle: GridPlan = {
+export const business: GridPlan = {
 	planSlug: 'business-bundle' as PlanSlug,
 	isVisible: true,
 	tagline: 'Unlock the power of WordPress with plugins and cloud tools.',
@@ -209,7 +211,7 @@ const businessBundle: GridPlan = {
 	},
 };
 
-const ecommerce: GridPlan = {
+export const ecommerce: GridPlan = {
 	planSlug: 'ecommerce-bundle' as PlanSlug,
 	isVisible: true,
 	tagline: 'Create a powerful online store with built-in premium extensions.',
@@ -257,7 +259,7 @@ const ecommerce: GridPlan = {
 	},
 };
 
-const enterprise = {
+export const enterprise = {
 	planSlug: 'plan-enterprise-grid-wpcom' as PlanSlug,
 	isVisible: true,
 	tagline:
@@ -287,11 +289,91 @@ const enterprise = {
 		storageOptions: [],
 	},
 };
-export const gridPlans: GridPlan[] = [
-	freePlan,
-	personalBundle,
-	valueBundle,
-	businessBundle,
-	ecommerce,
-	enterprise,
-];
+
+export const spotlight = {
+	planSlug: 'business-bundle',
+	isVisible: true,
+	tagline: 'Unlock the power of WordPress with plugins and cloud tools.',
+	availableForPurchase: false,
+	productNameShort: 'Creator',
+	planTitle: 'Creator',
+	billingTimeframe: 'per month, billed annually',
+	current: true,
+	isMonthlyPlan: false,
+	cartItemForPlan: {
+		product_slug: 'business-bundle',
+	},
+	highlightLabel: 'Your plan',
+	pricing: {
+		originalPrice: {
+			monthly: 3800,
+			full: 45600,
+		},
+		discountedPrice: {
+			monthly: null,
+			full: null,
+		},
+		billingPeriod: 365,
+		currencyCode: 'AUD',
+		expiry: '2025-01-17T00:00:00+00:00',
+		introOffer: null,
+	},
+	storageAddOnsForPlan: [
+		{
+			productSlug: 'wordpress_com_1gb_space_addon_yearly',
+			featureSlugs: [ '50gb-storage-add-on' ],
+			icon: null,
+			quantity: 50,
+			name: '50 GB Storage',
+			displayCost: 'A$74.58/month, billed yearly',
+			prices: {
+				monthlyPrice: 7458.333333333333,
+				yearlyPrice: 89500,
+				formattedMonthlyPrice: 'A$74.58',
+				formattedYearlyPrice: 'A$895',
+			},
+			description: 'Make more space for high-quality photos, videos, and other media. ',
+			featured: false,
+			purchased: false,
+			checkoutLink: '/checkout/wordpress_com_1gb_space_addon_yearly',
+			exceedsSiteStorageLimits: true,
+		},
+		{
+			productSlug: 'wordpress_com_1gb_space_addon_yearly',
+			featureSlugs: [ '100gb-storage-add-on' ],
+			quantity: 100,
+			name: '100 GB Storage',
+			displayCost: 'A$124.25/month, billed yearly',
+			prices: {
+				monthlyPrice: 12425,
+				yearlyPrice: 149100,
+				formattedMonthlyPrice: 'A$124.25',
+				formattedYearlyPrice: 'A$1,491',
+			},
+			description:
+				'Take your site to the next level. Store all your media in one place without worrying about running out of space.',
+			featured: false,
+			purchased: false,
+			checkoutLink: '/checkout/wordpress_com_1gb_space_addon_yearly',
+			exceedsSiteStorageLimits: true,
+		},
+	],
+	features: {
+		wpcomFeatures,
+		jetpackFeatures,
+		storageOptions: [
+			{
+				slug: '50gb-storage',
+				isAddOn: false,
+			},
+			{
+				slug: '50gb-storage-add-on',
+				isAddOn: true,
+			},
+			{
+				slug: '100gb-storage-add-on',
+				isAddOn: true,
+			},
+		],
+	},
+} as unknown as GridPlan;
