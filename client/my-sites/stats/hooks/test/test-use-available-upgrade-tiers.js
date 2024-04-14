66605a4ab0c7c6eef513c6d1837b4fb5ee7f7675
Jasper Kang <kangzjnet@gmail.com>
2024-04-15 10:04:36 +1200

    add test to cover base tier is included

--- client/my-sites/stats/hooks/test/test-use-available-upgrade-tiers.js
+++ client/my-sites/stats/hooks/test/test-use-available-upgrade-tiers.js
@@ -15,7 +15,7 @@ describe( 'getAvailableUpgradeTiers', () => {
 		expect( extendedTiers[ 4 ].minimum_price ).toEqual( tiers[ 4 ].minimum_price );
 		expect( extendedTiers[ 5 ].views ).toEqual( 2_000_000 );
 	} );
-	it( 'should return 250k~4m tiers if purchased and with little monthly views', () => {
+	it( 'should return 250k~4m tiers if purchased with little monthly views', () => {
 		const usageData = { views_limit: 100_000, billableMonthlyViews: 0 };
 		const extendedTiers = getAvailableUpgradeTiers( stateFixture, usageData, true );
 
@@ -24,7 +24,7 @@ describe( 'getAvailableUpgradeTiers', () => {
 		expect( extendedTiers[ 0 ].minimum_price ).toEqual( tiers[ 2 ].minimum_price );
 		expect( extendedTiers[ 5 ].views ).toEqual( 4_000_000 );
 	} );
-	it( 'should return 100k~3m tiers if not purchased and higer monthly views', () => {
+	it( 'should return 100k~3m tiers if not purchased with higer monthly views', () => {
 		const usageData = { views_limit: null, billableMonthlyViews: 10_000 };
 		const extendedTiers = getAvailableUpgradeTiers( stateFixture, usageData, true );
 
@@ -33,7 +33,7 @@ describe( 'getAvailableUpgradeTiers', () => {
 		expect( extendedTiers[ 0 ].minimum_price ).toEqual( tiers[ 1 ].minimum_price );
 		expect( extendedTiers[ 5 ].views ).toEqual( 3_000_000 );
 	} );
-	it( 'should return 250k~4m tiers if purchased and higer monthly views', () => {
+	it( 'should return 250k~4m tiers if purchased with higer monthly views', () => {
 		const usageData = { views_limit: 100_00, billableMonthlyViews: 100_001 };
 		const extendedTiers = getAvailableUpgradeTiers( stateFixture, usageData, true );
 
@@ -42,7 +42,7 @@ describe( 'getAvailableUpgradeTiers', () => {
 		expect( extendedTiers[ 0 ].minimum_price ).toEqual( tiers[ 2 ].minimum_price );
 		expect( extendedTiers[ 5 ].views ).toEqual( 4_000_000 );
 	} );
-	it( 'should return 2m~7m tiers if not purchased and 1m+ monthly views', () => {
+	it( 'should return 2~7m tiers if not purchased with 1m+ monthly views', () => {
 		const usageData = { views_limit: null, billableMonthlyViews: 1_000_001 };
 		const extendedTiers = getAvailableUpgradeTiers( stateFixture, usageData, true );
 
@@ -52,5 +52,14 @@ describe( 'getAvailableUpgradeTiers', () => {
 		// the price for 7m views - 95000 + 25000 * 5
 		expect( extendedTiers[ 5 ].minimum_price ).toEqual( 220_000 );
 		expect( extendedTiers[ 5 ].views ).toEqual( 7_000_000 );
+	} );
+	it( 'should return 3~8m tiers if purchased 1m and with 2m monthly views', () => {
+		const usageData = { views_limit: 1_000_000, billableMonthlyViews: 2_000_001 };
+		const extendedTiers = getAvailableUpgradeTiers( stateFixture, usageData, true );
+
+		expect( extendedTiers.length ).toBe( MAX_TIERS_NUMBER );
+		expect( extendedTiers[ 0 ].views ).toEqual( 3_000_000 );
+		expect( extendedTiers[ 0 ].minimum_price ).toEqual( 120000 );
+		expect( extendedTiers[ 5 ].views ).toEqual( 8_000_000 );
 	} );
 } );
