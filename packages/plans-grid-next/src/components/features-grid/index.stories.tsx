f3a77d6972904ee7ec03cc1528abad40a434ff14
Adam <oswian123@gmail.com>
2024-04-15 18:17:38 +1000

    Plans Next: Improve isolation of plan grid hooks (#87478)
    * Improve isolation of plan grid hooks
    
    * Include siteId param in hook call
    
    * Fix the Story book cases of the comparison grid and the feature grid by
    allowing an optional overriding `useGridPlans` to be passed in as an
    argument.
    
    * selectedSiteId -> siteId
    
    ---------
    
    Co-authored-by: James Tien <southp0105@gmail.com>

--- packages/plans-grid-next/src/components/features-grid/index.stories.tsx
+++ packages/plans-grid-next/src/components/features-grid/index.stories.tsx
@@ -1,13 +1,31 @@
+import { getFeaturesList } from '@automattic/calypso-products';
 import { Meta } from '@storybook/react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { FeaturesGrid } from '../..';
+import { FeaturesGrid, useGridPlansForFeaturesGrid } from '../..';
 import { defaultArgs } from '../../storybook-mocks';
 
 const queryClient = new QueryClient();
 
+const RenderFeaturesGrid = ( props: any ) => {
+	const useGridPlans = () => defaultArgs.gridPlans;
+
+	const gridPlansForFeaturesGrid = useGridPlansForFeaturesGrid(
+		{
+			allFeaturesList: getFeaturesList(),
+			selectedFeature: props.selectedFeature,
+			showLegacyStorageFeature: props.showLegacyStorageFeature,
+			useCheckPlanAvailabilityForPurchase: () => ( { value_bundle: true } ),
+			storageAddOns: [],
+		},
+		useGridPlans
+	);
+
+	return <FeaturesGrid { ...props } gridPlans={ gridPlansForFeaturesGrid } />;
+};
+
 export default {
 	title: 'plans-grid-next',
-	component: FeaturesGrid,
+	component: RenderFeaturesGrid,
 	decorators: [
 		( Story ) => (
 			<QueryClientProvider client={ queryClient }>
