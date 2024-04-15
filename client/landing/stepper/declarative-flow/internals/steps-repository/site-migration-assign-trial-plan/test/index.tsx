dd0a64031890010cd9f7c46e68f0136901eab509
Caroline Moore <calobee@gmail.com>
2024-04-15 11:25:51 -0400

    add test for positive result

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-assign-trial-plan/test/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-assign-trial-plan/test/index.tsx
@@ -5,9 +5,20 @@ import { PLAN_MIGRATION_TRIAL_MONTHLY } from '@automattic/calypso-products';
 import { waitFor } from '@testing-library/react';
 import nock from 'nock';
 import React from 'react';
+import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import SiteMigrationAssignTrialPlanStep from '..';
 import { StepProps } from '../../../types';
 import { RenderStepOptions, mockStepProps, renderStep } from '../../test/helpers';
+
+jest.mock( 'calypso/landing/stepper/hooks/use-site' );
+
+const siteId = 123456789;
+const planSlug = PLAN_MIGRATION_TRIAL_MONTHLY;
+
+( useSite as jest.Mock ).mockReturnValue( {
+	ID: siteId,
+	URL: 'https://site-url.wordpress.com',
+} );
 
 const mockApi = () => nock( 'https://public-api.wordpress.com:443' );
 
@@ -16,18 +27,36 @@ const render = ( props?: Partial< StepProps >, renderOptions?: RenderStepOptions
 	return renderStep( <SiteMigrationAssignTrialPlanStep { ...combinedProps } />, renderOptions );
 };
 
-const planSlug = PLAN_MIGRATION_TRIAL_MONTHLY;
-
 describe( 'SiteMigrationAssignTrialPlanStep', () => {
 	beforeAll( () => nock.disableNetConnect() );
 
-	it( 'shows the error step when the api returns an error', async () => {
+	it( 'submits the step when the api returns a response', async () => {
 		const submit = jest.fn();
 		render( { navigation: { submit } } );
 
 		mockApi()
-			.post( `/wpcom/v2/sites/123456789/hosting/trial/add/${ planSlug }` )
-			.reply( 500, new Error( 'Internal Server Error' ) );
+			.post( `/wpcom/v2/sites/${ siteId }/hosting/trial/add/${ planSlug }`, {
+				hosting_intent: 'migrate',
+			} )
+			.reply( 200 );
+
+		await waitFor( () => expect( submit ).toHaveBeenCalled() );
+	} );
+
+	it( 'submits the step with an error object when the api returns an error', async () => {
+		const submit = jest.fn();
+		render( { navigation: { submit } } );
+
+		mockApi()
+			.post( `/wpcom/v2/sites/${ siteId }/hosting/trial/add/${ planSlug }`, {
+				hosting_intent: 'migrate',
+			} )
+			.reply(
+				400,
+				new Error(
+					'You cannot add WordPress.com Migration Trial when you already have paid upgrades'
+				)
+			);
 
 		await waitFor( () =>
 			expect( submit ).toHaveBeenCalledWith( expect.objectContaining( { error: true } ) )
