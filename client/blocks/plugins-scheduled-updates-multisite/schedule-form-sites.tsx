5afdc893ffb13dcade62dbcfd7ea2425c0fec0e5
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-04-16 10:22:26 +0200

    Create site selector component

---
+++ client/blocks/plugins-scheduled-updates-multisite/schedule-form-sites.tsx
@@ -0,0 +1,54 @@
+import { CheckboxControl, SearchControl } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+import { Fragment, useCallback, useState } from 'react';
+import type { SiteExcerptData } from '@automattic/sites';
+
+interface Props {
+	sites: SiteExcerptData[];
+	initSites?: number[];
+}
+export const ScheduleFormSites = ( props: Props ) => {
+	const { sites, initSites = [] } = props;
+	const translate = useTranslate();
+	const [ searchTerm, setSearchTerm ] = useState( '' );
+	const [ selectedSites, setSelectedSites ] = useState< number[] >( initSites );
+
+	const onSiteSelectionChange = useCallback( ( site: SiteExcerptData, isChecked: boolean ) => {
+		if ( isChecked ) {
+			const _sites: number[] = [ ...selectedSites ];
+			_sites.push( site.ID );
+			setSelectedSites( _sites );
+		} else {
+			setSelectedSites( selectedSites.filter( ( id ) => id !== site.ID ) );
+		}
+	}, [] );
+
+	return (
+		<div className="form-field">
+			<label htmlFor="sites">{ translate( 'Select sites' ) }</label>
+			<div className="form-control-container">
+				<SearchControl
+					id="sites"
+					onChange={ ( s ) => setSearchTerm( s.trim() ) }
+					placeholder={ translate( 'Search site' ) }
+				/>
+				<div className="checkbox-options-container">
+					{ sites.map( ( site ) => (
+						<Fragment key={ site.ID }>
+							{ site?.name && site.name.toLowerCase().includes( searchTerm.toLowerCase() ) && (
+								<CheckboxControl
+									key={ site.ID }
+									label={ site.name }
+									onChange={ ( isChecked ) => {
+										onSiteSelectionChange( site, isChecked );
+									} }
+									checked={ selectedSites.includes( site.ID ) }
+								/>
+							) }
+						</Fragment>
+					) ) }
+				</div>
+			</div>
+		</div>
+	);
+};
--- client/blocks/plugins-scheduled-updates-multisite/schedule-form.tsx
+++ client/blocks/plugins-scheduled-updates-multisite/schedule-form.tsx
@@ -1,13 +1,20 @@
 import { Flex, FlexItem } from '@wordpress/components';
+import { useSiteExcerptsSorted } from 'calypso/data/sites/use-site-excerpts-sorted';
 import { ScheduleFormFrequency } from '../plugins-scheduled-updates/schedule-form-frequency';
 import { ScheduleFormPlugins } from '../plugins-scheduled-updates/schedule-form-plugins';
+import { ScheduleFormSites } from './schedule-form-sites';
 
 export const ScheduleForm = () => {
+	const sites = useSiteExcerptsSorted();
+	const atomicSites = sites.filter( ( site ) => site.is_wpcom_atomic );
+
 	return (
 		<div className="schedule-form">
 			<div className="form-control-container">
 				<Flex direction={ [ 'column', 'row' ] } expanded={ true } align="start">
-					<FlexItem>Site selection</FlexItem>
+					<FlexItem>
+						<ScheduleFormSites sites={ atomicSites } />
+					</FlexItem>
 					<FlexItem>
 						<ScheduleFormPlugins
 							plugins={ [] }
