848463c9e730590a262e389bcb1d04840287ea08
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-04-16 13:04:54 +0200

    Implement site component validation

--- client/blocks/plugins-scheduled-updates-multisite/schedule-form-sites.tsx
+++ client/blocks/plugins-scheduled-updates-multisite/schedule-form-sites.tsx
@@ -1,21 +1,35 @@
-import { CheckboxControl, SearchControl } from '@wordpress/components';
+import { __experimentalText as Text, CheckboxControl, SearchControl } from '@wordpress/components';
+import { Icon, info } from '@wordpress/icons';
 import classnames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
-import { Fragment, useCallback, useEffect, useState } from 'react';
+import { useEffect, Fragment, useCallback, useState } from 'react';
 import type { SiteExcerptData } from '@automattic/sites';
 
 interface Props {
 	sites: SiteExcerptData[];
 	selectedSites?: number[];
+	error?: string;
+	showError?: boolean;
 	onChange?: ( value: number[] ) => void;
+	onTouch?: ( touched: boolean ) => void;
 	borderWrapper?: boolean;
 }
 export const ScheduleFormSites = ( props: Props ) => {
-	const { sites, selectedSites: initSites = [], onChange, borderWrapper = true } = props;
+	const {
+		sites,
+		selectedSites: initSites = [],
+		error,
+		showError,
+		onChange,
+		onTouch,
+		borderWrapper = true,
+	} = props;
 	const translate = useTranslate();
 	const [ searchTerm, setSearchTerm ] = useState( '' );
 	const [ selectedSites, setSelectedSites ] = useState< number[] >( initSites );
+	const [ fieldTouched, setFieldTouched ] = useState( false );
 
+	useEffect( () => onTouch?.( fieldTouched ), [ fieldTouched ] );
 	useEffect( () => onChange?.( selectedSites ), [ selectedSites ] );
 
 	const onSiteSelectionChange = useCallback(
@@ -35,6 +49,12 @@ export const ScheduleFormSites = ( props: Props ) => {
 		<div className="form-field">
 			<label htmlFor="sites">{ translate( 'Select sites' ) }</label>
 			<div className={ classnames( { 'form-control-container': borderWrapper } ) }>
+				{ ( ( showError && error ) || ( fieldTouched && error ) ) && (
+					<Text className="validation-msg">
+						<Icon className="icon-info" icon={ info } size={ 16 } />
+						{ error }
+					</Text>
+				) }
 				<SearchControl
 					id="sites"
 					onChange={ ( s ) => setSearchTerm( s.trim() ) }
@@ -49,6 +69,7 @@ export const ScheduleFormSites = ( props: Props ) => {
 									label={ site.name }
 									onChange={ ( isChecked ) => {
 										onSiteSelectionChange( site, isChecked );
+										setFieldTouched( true );
 									} }
 									checked={ selectedSites.includes( site.ID ) }
 								/>
