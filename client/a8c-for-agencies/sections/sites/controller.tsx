c9fbf35d8e3dc889342f146a43549c8c8303b62c
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-04-16 15:21:35 +0800

    A4A: Make the Site listing always visible in Preview mode. (#89554)

--- client/a8c-for-agencies/sections/sites/controller.tsx
+++ client/a8c-for-agencies/sections/sites/controller.tsx
@@ -14,7 +14,6 @@ function configureSitesContext( context: Context ) {
 	const category = context.params.category || A4A_SITES_DASHBOARD_DEFAULT_CATEGORY;
 	const siteUrl = context.params.siteUrl;
 	const siteFeature = context.params.feature || A4A_SITES_DASHBOARD_DEFAULT_FEATURE;
-	const hideListingInitialState = !! siteUrl;
 
 	const {
 		s: search,
@@ -36,7 +35,6 @@ function configureSitesContext( context: Context ) {
 			categoryInitialState={ category }
 			siteUrlInitialState={ siteUrl }
 			siteFeatureInitialState={ siteFeature }
-			hideListingInitialState={ hideListingInitialState }
 			showOnlyFavoritesInitialState={
 				is_favorite === '' || is_favorite === '1' || is_favorite === 'true'
 			}
