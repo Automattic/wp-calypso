5cd460c1de93228a418d6cafdca4b8c74fc5762b
cleacos <javacodia@gmail.com>
2024-04-17 11:27:09 +0100

    Update items-dataviews/interfaces.ts

--- client/a8c-for-agencies/components/items-dashboard/items-dataviews/interfaces.ts
+++ client/a8c-for-agencies/components/items-dashboard/items-dataviews/interfaces.ts
@@ -1,5 +1,8 @@
+import { ReactNode } from 'react';
+// todo: Site and SiteDate should not be here
 import { Site, SiteData } from 'calypso/a8c-for-agencies/sections/sites/types';
 
+// todo: extract
 export interface SitesDataResponse {
 	sites: Array< Site >;
 	total: number;
@@ -13,33 +16,61 @@ export interface SitesDataViewsProps {
 	forceTourExampleSite?: boolean;
 	isLargeScreen: boolean;
 	isLoading: boolean;
-	onSitesViewChange: ( view: SitesViewState ) => void;
-	sitesViewState: SitesViewState;
+	fields: DataViewsColumn[];
+	actions: string[];
+	onSitesViewChange: ( view: DataViewsState ) => void;
+	sitesViewState: DataViewsState;
 }
 
-export interface Sort {
+export interface DataViewsColumn {
+	id: string;
+	enableHiding?: boolean;
+	enableSorting?: boolean;
+	elements?: {
+		value: number;
+		label: string;
+	}[];
+	filterBy?: {
+		operators: string[];
+		isPrimary?: boolean;
+	};
+	header: ReactNode;
+	getValue: () => null;
+	render?: () => ReactNode;
+}
+
+export interface DataViewsPaginationInfo {
+	totalItems: number;
+	totalPages: number;
+}
+
+export interface DataViewsSort {
 	field: string;
 	direction: 'asc' | 'desc' | '';
 }
 
-export interface Filter {
+export interface DataViewsFilter {
 	field: string;
 	operator: string;
 	value: number;
 }
 
-export interface SitesViewState {
+export interface DataViewsState {
 	type: 'table' | 'list' | 'grid';
+	search: string;
+	filters: DataViewsFilter[];
 	perPage: number;
 	page: number;
-	sort: Sort;
-	search: string;
-	filters: Filter[];
+	sort: DataViewsSort;
 	hiddenFields: string[];
 	layout: object;
+	// todo: This is not part of the DataViews. We could extends the DataViewsState interface into a new one adding this.
 	selectedSite?: Site | undefined;
 }
 
+// todo: extract?
 export interface SiteInfo extends SiteData {
 	id: number;
 }
+
+// todo: Add the DataViewsAction interface: https://github.com/WordPress/gutenberg/blob/trunk/packages/dataviews/README.md#actions-object
