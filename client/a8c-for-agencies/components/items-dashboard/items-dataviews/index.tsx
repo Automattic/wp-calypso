43122046a980a130c97b7d46ef4863315c31746a
cleacos <javacodia@gmail.com>
2024-04-17 12:31:16 +0100

    Update the ItemDataViews. Clean it up, todo-extract comments, commenting to extract code.

--- client/a8c-for-agencies/components/items-dashboard/items-dataviews/index.tsx
+++ client/a8c-for-agencies/components/items-dashboard/items-dataviews/index.tsx
@@ -1,51 +1,99 @@
-import { Button, Gridicon, Spinner } from '@automattic/components';
+import { Spinner } from '@automattic/components';
 import { DataViews } from '@wordpress/dataviews';
-import { Icon, starFilled } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
-import { useCallback, useContext, useMemo, useState } from 'react';
+import { ReactNode } from 'react';
 import ReactDOM from 'react-dom';
 // todo: extract
-import { GuidedTourStep } from 'calypso/a8c-for-agencies/components/guided-tour-step';
+//import { GuidedTourStep } from 'calypso/a8c-for-agencies/components/guided-tour-step';
 // todo: extract
-import SiteSetFavorite from 'calypso/a8c-for-agencies/sections/sites/site-set-favorite';
+//import SiteSetFavorite from 'calypso/a8c-for-agencies/sections/sites/site-set-favorite';
 // todo: extract?? We could keep a common context through the ItemsDashboard environment.
-import SitesDashboardContext from 'calypso/a8c-for-agencies/sections/sites/sites-dashboard-context';
+//import SitesDashboardContext from 'calypso/a8c-for-agencies/sections/sites/sites-dashboard-context';
 // todo: extract. Customizable via columns
-import SiteDataField from 'calypso/a8c-for-agencies/sections/sites/sites-dataviews/site-data-field';
+//import SiteDataField from 'calypso/a8c-for-agencies/sections/sites/sites-dataviews/site-data-field';
 // todo: extract/refactor
-import { AllowedTypes, Site } from 'calypso/a8c-for-agencies/sections/sites/types';
-import SiteActions from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-actions';
+//import { AllowedTypes, Site } from 'calypso/a8c-for-agencies/sections/sites/types';
+//import SiteActions from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-actions';
 // todo: extract??
-import useFormattedSites from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-content/hooks/use-formatted-sites';
+//import useFormattedSites from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-content/hooks/use-formatted-sites';
 // todo: extract
-import SiteStatusContent from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content';
+//import SiteStatusContent from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content';
 // todo: extract
-import { JETPACK_MANAGE_ONBOARDING_TOURS_EXAMPLE_SITE } from 'calypso/jetpack-cloud/sections/onboarding-tours/constants';
+//import { JETPACK_MANAGE_ONBOARDING_TOURS_EXAMPLE_SITE } from 'calypso/jetpack-cloud/sections/onboarding-tours/constants';
 // todo: extract
-import TextPlaceholder from 'calypso/jetpack-cloud/sections/partner-portal/text-placeholder';
-// todo: extract
-import { SitesDataViewsProps, SiteInfo } from './interfaces';
-import SiteSort from './site-sort';
+//import TextPlaceholder from 'calypso/jetpack-cloud/sections/partner-portal/text-placeholder';
+import { ItemsDataViewsProps, DataViewsColumn } from './interfaces';
+//import SiteSort from './site-sort';
 // todo: extract partially. Extract common styles
 import './style.scss';
 
+const getIdByPath = ( item: object, path: string ) => {
+	const fields = path.split( '.' );
+	let result: any = item;
+	for ( const field of fields ) {
+		if ( result[ field ] === undefined ) {
+			return undefined;
+		}
+		result = result[ field ];
+	}
+	return result;
+};
+
+/**
+ * Create an item column for the DataViews component
+ * @param id
+ * @param header
+ * @param displayField
+ * @param getValue
+ * @param isSortable
+ * @param canHide
+ */
+export const createItemColumn = (
+	id: string,
+	header: ReactNode,
+	displayField: () => ReactNode,
+	getValue: () => null,
+	isSortable: boolean = false,
+	canHide: boolean = false
+): DataViewsColumn => {
+	return {
+		id,
+		enableSorting: isSortable,
+		enableHiding: canHide,
+		getValue,
+		header,
+		render: displayField,
+	};
+};
+
 const ItemsDataViews = ( {
-	data,
+	items,
+	pagination,
 	isLoading,
-	isLargeScreen,
 	onSitesViewChange,
 	sitesViewState,
-	forceTourExampleSite = false,
+	fields,
+	actions = [],
+	searchLabel,
+	getItemId,
+	itemFieldId,
+	// todo: extract
+	//forceTourExampleSite = false,
 	className,
-}: SitesDataViewsProps ) => {
+}: ItemsDataViewsProps ) => {
 	const translate = useTranslate();
-	const { showOnlyFavorites } = useContext( SitesDashboardContext );
-	const totalSites = showOnlyFavorites ? data?.totalFavorites || 0 : data?.total || 0;
-	const sitesPerPage = sitesViewState.perPage > 0 ? sitesViewState.perPage : 20;
-	const totalPages = Math.ceil( totalSites / sitesPerPage );
-	const sites = useFormattedSites( data?.sites ?? [] );
 
-	const openSitePreviewPane = useCallback(
+	// todo: update/extract. We have to receive info about the total pages via props
+	//const { showOnlyFavorites } = useContext( SitesDashboardContext );
+	//const totalSites = showOnlyFavorites ? data?.totalFavorites || 0 : data?.total || 0;
+	//const sitesPerPage = sitesViewState.perPage > 0 ? sitesViewState.perPage : 20;
+	//const totalPages = Math.ceil( totalSites / sitesPerPage );
+
+	// todo: extract
+	//const sites = useFormattedSites( data?.sites ?? [] );
+
+	// todo: extract
+	/*const openSitePreviewPane = useCallback(
 		( site: Site ) => {
 			onSitesViewChange( {
 				...sitesViewState,
@@ -54,9 +102,10 @@ const ItemsDataViews = ( {
 			} );
 		},
 		[ onSitesViewChange, sitesViewState ]
-	);
+	);*/
 
-	const renderField = useCallback(
+	// todo: extract
+	/*const renderField = useCallback(
 		( column: AllowedTypes, item: SiteInfo ) => {
 			if ( isLoading ) {
 				return <TextPlaceholder />;
@@ -75,9 +124,11 @@ const ItemsDataViews = ( {
 			}
 		},
 		[ isLoading, isLargeScreen ]
-	);
+	);*/
 
+	// todo: extract
 	// Legacy refs for guided tour popovers
+	/*
 	const [ introRef, setIntroRef ] = useState< HTMLElement | null >();
 	const [ statsRef, setStatsRef ] = useState< HTMLElement | null >();
 	const [ boostRef, setBoostRef ] = useState< HTMLElement | null >();
@@ -86,333 +137,7 @@ const ItemsDataViews = ( {
 	const [ scanRef, setScanRef ] = useState< HTMLElement | null >();
 	const [ pluginsRef, setPluginsRef ] = useState< HTMLElement | null >();
 	const [ actionsRef, setActionsRef ] = useState< HTMLElement | null >();
-
-	// todo - refactor: extract fields, along actions, to the upper component
-	const fields = useMemo(
-		() => [
-			{
-				id: 'status',
-				header: translate( 'Status' ),
-				getValue: ( { item }: { item: SiteInfo } ) =>
-					item.site.error || item.scan.status === 'critical',
-				render: () => {},
-				type: 'enumeration',
-				elements: [
-					{ value: 1, label: translate( 'Needs Attention' ) },
-					{ value: 2, label: translate( 'Backup Failed' ) },
-					{ value: 3, label: translate( 'Backup Warning' ) },
-					{ value: 4, label: translate( 'Threat Found' ) },
-					{ value: 5, label: translate( 'Site Disconnected' ) },
-					{ value: 6, label: translate( 'Site Down' ) },
-					{ value: 7, label: translate( 'Plugins Needing Updates' ) },
-				],
-				filterBy: {
-					operators: [ 'in' ],
-				},
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'site',
-				header: (
-					<>
-						<SiteSort isSortable={ true } columnKey="site">
-							<span
-								className="sites-dataview__site-header sites-dataview__site-header--sort"
-								ref={ ( ref ) => setIntroRef( ref as HTMLElement | null ) }
-							>
-								{ translate( 'Site' ).toUpperCase() }
-							</span>
-						</SiteSort>
-						<GuidedTourStep
-							id="sites-walkthrough-intro"
-							tourId="sitesWalkthrough"
-							context={ introRef }
-						/>
-					</>
-				),
-				getValue: ( { item }: { item: SiteInfo } ) => item.site.value.url,
-				render: ( { item }: { item: SiteInfo } ) => {
-					if ( isLoading ) {
-						return <TextPlaceholder />;
-					}
-					const site = item.site.value;
-					return (
-						<SiteDataField
-							site={ site }
-							isLoading={ isLoading }
-							onSiteTitleClick={ openSitePreviewPane }
-						/>
-					);
-				},
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'stats',
-				header: (
-					<div>
-						<span
-							className="sites-dataview__stats-header"
-							ref={ ( ref ) => setStatsRef( ref as HTMLElement | null ) }
-						>
-							STATS
-						</span>
-						<GuidedTourStep
-							id="sites-walkthrough-stats"
-							tourId="sitesWalkthrough"
-							context={ statsRef }
-						/>
-					</div>
-				),
-				getValue: () => '-',
-				render: ( { item }: { item: SiteInfo } ) => renderField( 'stats', item ),
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'boost',
-				header: (
-					<>
-						<span
-							className="sites-dataview__boost-header"
-							ref={ ( ref ) => setBoostRef( ref as HTMLElement | null ) }
-						>
-							BOOST
-						</span>
-						<GuidedTourStep
-							id="sites-walkthrough-boost"
-							tourId="sitesWalkthrough"
-							context={ boostRef }
-						/>
-					</>
-				),
-				getValue: ( { item }: { item: SiteInfo } ) => item.boost.status,
-				render: ( { item }: { item: SiteInfo } ) => renderField( 'boost', item ),
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'backup',
-				header: (
-					<>
-						<span
-							className="sites-dataview__backup-header"
-							ref={ ( ref ) => setBackupRef( ref as HTMLElement | null ) }
-						>
-							BACKUP
-						</span>
-						<GuidedTourStep
-							id="sites-walkthrough-backup"
-							tourId="sitesWalkthrough"
-							context={ backupRef }
-						/>
-					</>
-				),
-				getValue: () => '-',
-				render: ( { item }: { item: SiteInfo } ) => renderField( 'backup', item ),
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'monitor',
-				header: (
-					<>
-						<span
-							className="sites-dataview__monitor-header"
-							ref={ ( ref ) => setMonitorRef( ref as HTMLElement | null ) }
-						>
-							MONITOR
-						</span>
-						<GuidedTourStep
-							id="sites-walkthrough-monitor"
-							tourId="sitesWalkthrough"
-							context={ monitorRef }
-						/>
-					</>
-				),
-				getValue: () => '-',
-				render: ( { item }: { item: SiteInfo } ) => renderField( 'monitor', item ),
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'scan',
-				header: (
-					<>
-						<span
-							className="sites-dataview__scan-header"
-							ref={ ( ref ) => setScanRef( ref as HTMLElement | null ) }
-						>
-							SCAN
-						</span>
-						<GuidedTourStep
-							id="sites-walkthrough-scan"
-							tourId="sitesWalkthrough"
-							context={ scanRef }
-						/>
-					</>
-				),
-				getValue: () => '-',
-				render: ( { item }: { item: SiteInfo } ) => renderField( 'scan', item ),
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'plugins',
-				header: (
-					<>
-						<span
-							className="sites-dataview__plugins-header"
-							ref={ ( ref ) => setPluginsRef( ref as HTMLElement | null ) }
-						>
-							PLUGINS
-						</span>
-						<GuidedTourStep
-							id="sites-walkthrough-plugins"
-							tourId="sitesWalkthrough"
-							context={ pluginsRef }
-						/>
-					</>
-				),
-				getValue: () => '-',
-				render: ( { item }: { item: SiteInfo } ) => renderField( 'plugin', item ),
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'favorite',
-				header: (
-					<Icon
-						className="site-table__favorite-icon sites-dataview__favorites-header"
-						size={ 24 }
-						icon={ starFilled }
-					/>
-				),
-				getValue: ( { item }: { item: SiteInfo } ) => item.isFavorite,
-				render: ( { item }: { item: SiteInfo } ) => {
-					if ( isLoading ) {
-						return <TextPlaceholder />;
-					}
-					return (
-						<span className="sites-dataviews__favorite-btn-wrapper">
-							<SiteSetFavorite
-								isFavorite={ item.isFavorite || false }
-								siteId={ item.site.value.blog_id }
-								siteUrl={ item.site.value.url }
-							/>
-						</span>
-					);
-				},
-				enableHiding: false,
-				enableSorting: false,
-			},
-			{
-				id: 'actions',
-				getValue: ( { item }: { item: SiteInfo } ) => item.isFavorite,
-				render: ( { item }: { item: SiteInfo } ) => {
-					if ( isLoading ) {
-						return <TextPlaceholder />;
-					}
-					return (
-						<div className="sites-dataviews__actions">
-							<SiteActions
-								isLargeScreen={ isLargeScreen }
-								site={ item.site }
-								siteError={ item.site.error }
-							/>
-							<Button
-								onClick={ () => openSitePreviewPane( item.site.value ) }
-								className="site-preview__open"
-								borderless
-								ref={ ( ref ) => setActionsRef( ( current ) => current || ref ) }
-							>
-								<Gridicon icon="chevron-right" />
-							</Button>
-						</div>
-					);
-				},
-				header: (
-					<GuidedTourStep
-						id="sites-walkthrough-site-preview"
-						tourId="sitesWalkthrough"
-						context={ actionsRef }
-					/>
-				),
-				enableHiding: false,
-				enableSorting: false,
-			},
-		],
-		[
-			translate,
-			introRef,
-			statsRef,
-			boostRef,
-			backupRef,
-			monitorRef,
-			scanRef,
-			pluginsRef,
-			actionsRef,
-			isLoading,
-			openSitePreviewPane,
-			renderField,
-			isLargeScreen,
-		]
-	);
-
-	// Actions: Pause Monitor, Resume Monitor, Custom Notification, Reset Notification
-	// todo - refactor: extract actions, along fields, to the upper component
-	// Currently not in use until bulk selections are properly implemented.
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const actions = useMemo(
-		() => [
-			{
-				id: 'pause-monitor',
-				label: translate( 'Pause Monitor' ),
-				supportsBulk: true,
-				isEligible( site: SiteInfo ) {
-					return site.monitor.status === 'active';
-				},
-				callback() {
-					// todo: pause monitor. Param: sites: SiteInfo[]
-				},
-			},
-			{
-				id: 'resume-monitor',
-				label: translate( 'Resume Monitor' ),
-				supportsBulk: true,
-				isEligible( site: SiteInfo ) {
-					return site.monitor.status === 'inactive';
-				},
-				callback() {
-					// todo: resume monitor. Param: sites: SiteInfo[]
-				},
-			},
-			{
-				id: 'custom-notification',
-				label: translate( 'Custom Notification' ),
-				supportsBulk: true,
-				isEligible( site: SiteInfo ) {
-					return site.monitor.status === 'active';
-				},
-				callback() {
-					// todo: custom notification. Param: sites: SiteInfo[]
-				},
-			},
-			{
-				id: 'reset-notification',
-				label: translate( 'Reset Notification' ),
-				supportsBulk: true,
-				isEligible( site: SiteInfo ) {
-					return site.monitor.status === 'active';
-				},
-				callback() {
-					// todo: reset notification. Param: sites: SiteInfo[]
-				},
-			},
-		],
-		[ translate ]
-	);
+  */
 
 	// Until the DataViews package is updated to support the spinner, we need to manually add the (loading) spinner to the table wrapper for now.
 	const SpinnerWrapper = () => {
@@ -435,30 +160,35 @@ const ItemsDataViews = ( {
 		spinnerWrapper.classList.add( 'spinner-wrapper' );
 		// Render the SpinnerWrapper component inside the spinner wrapper
 		ReactDOM.hydrate( <SpinnerWrapper />, spinnerWrapper );
-		//}
 	}
 
-	const urlParams = new URLSearchParams( window.location.search );
-	const isOnboardingTourActive = urlParams.get( 'tour' ) !== null;
-	const useExampleDataForTour =
-		forceTourExampleSite || ( isOnboardingTourActive && ( ! sites || sites.length === 0 ) );
+	// todo: extract
+	//const urlParams = new URLSearchParams( window.location.search );
+	//const isOnboardingTourActive = urlParams.get( 'tour' ) !== null;
+	//const useExampleDataForTour = forceTourExampleSite || ( isOnboardingTourActive && ( ! sites || sites.length === 0 ) );
 
 	return (
 		<div className={ className }>
 			<DataViews
-				data={ ! useExampleDataForTour ? sites : JETPACK_MANAGE_ONBOARDING_TOURS_EXAMPLE_SITE }
-				paginationInfo={ { totalItems: totalSites, totalPages: totalPages } }
+				// todo: extract => data={ ! useExampleDataForTour ? sites : JETPACK_MANAGE_ONBOARDING_TOURS_EXAMPLE_SITE }
+				data={ items }
+				// todo: extract
+				//paginationInfo={ { totalItems: totalSites, totalPages: totalPages } }
+				paginationInfo={ pagination }
 				fields={ fields }
 				view={ sitesViewState }
 				search={ true }
-				searchLabel={ translate( 'Search for sites' ) }
-				getItemId={ ( item: SiteInfo ) => {
-					item.id = item.site.value.blog_id; // setting the id because of a issue with the DataViews component
-					return item.id;
-				} }
+				searchLabel={ searchLabel ?? translate( 'Search' ) }
+				// todo: update/extract this. DataViews should take the id info, from the item, if it exists.
+				getItemId={
+					getItemId ??
+					( ( item: object ) => {
+						return itemFieldId && getIdByPath( item, itemFieldId );
+					} )
+				}
 				onChangeView={ onSitesViewChange }
 				supportedLayouts={ [ 'table' ] }
-				actions={ [] } // Replace with actions when bulk selections are implemented.
+				actions={ actions }
 				isLoading={ isLoading }
 			/>
 		</div>
