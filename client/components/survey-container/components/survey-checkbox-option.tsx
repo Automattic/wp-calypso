70840e312286c0b7a8d6d1a071125fec2523c291
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2024-04-18 14:02:09 +0200

    Replace `CheckboxControl` with `input`

--- client/components/survey-container/components/survey-checkbox-option.tsx
+++ client/components/survey-container/components/survey-checkbox-option.tsx
@@ -1,4 +1,3 @@
-import { CheckboxControl } from '@wordpress/components';
 import { Question, Option } from '../types';
 
 // todo: consider renaming and moving the type into ../types
@@ -16,18 +15,40 @@ const SurveyCheckboxOption = ( {
 	value,
 }: SurveyCheckboxOptionType ) => {
 	const isSelected = value.includes( option.value );
-	return (
-		<div className={ `question-options__option-control ${ isSelected ? 'checked' : '' }` }>
-			<CheckboxControl
-				label={ option.label }
-				checked={ isSelected }
-				onChange={ ( checked ) => {
-					const newValue = checked
-						? [ ...value, option.value ]
-						: value.filter( ( v ) => v !== option.value );
 
-					onChange( question.key, newValue );
-				} }
+	const handleToggle = () => {
+		const newValue = isSelected
+			? value.filter( ( v ) => v !== option.value )
+			: [ ...value, option.value ];
+
+		onChange( question.key, newValue );
+	};
+
+	const handleKeyDown = ( event: React.KeyboardEvent< HTMLDivElement > ) => {
+		if ( event.key === 'Enter' || event.key === ' ' ) {
+			handleToggle();
+		}
+	};
+
+	return (
+		<div
+			className={ `question-options__option-control ${ isSelected ? 'checked' : '' }` }
+			onClick={ handleToggle }
+			onKeyDown={ handleKeyDown }
+			role="checkbox"
+			tabIndex={ 0 }
+			aria-checked={ isSelected ? 'true' : 'false' }
+		>
+			<input
+				type="checkbox"
+				id={ `option-${ option.value }` }
+				name={ question.key }
+				value={ option.value }
+				onChange={ handleToggle }
+				checked={ isSelected }
+				className="form-checkbox"
+				tabIndex={ -1 }
+				aria-hidden="true"
 			/>
 		</div>
 	);
