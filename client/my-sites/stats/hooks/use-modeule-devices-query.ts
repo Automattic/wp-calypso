f559f755264663f8992611649c447d8be44e7c4b
Dognose <dognose24@gmail.com>
2024-04-19 13:05:09 +0800

    Process Stats query parameters by selected periods

--- client/my-sites/stats/hooks/use-modeule-devices-query.ts
+++ client/my-sites/stats/hooks/use-modeule-devices-query.ts
@@ -6,6 +6,8 @@ export interface QueryStatsDevicesParams {
 	date?: string;
 	days?: number;
 	max?: number;
+	num?: number;
+	period?: string;
 }
 
 export interface StatsDevicesData {
@@ -13,6 +15,51 @@ export interface StatsDevicesData {
 	label: string;
 	value: number;
 }
+
+const getDaysOfMonthFromDate = ( date: string ): number => {
+	const dateObj = new Date( date );
+	const year = dateObj.getFullYear();
+	const month = dateObj.getMonth() + 1;
+
+	return new Date( year, month, 0 ).getDate();
+};
+
+const daysInYearFromDate = ( date: string ) => {
+	const dateObj = new Date( date );
+	const year = dateObj.getFullYear();
+
+	return ( year % 4 === 0 && year % 100 > 0 ) || year % 400 === 0 ? 366 : 365;
+};
+
+const processQueryParams = ( query: QueryStatsDevicesParams ) => {
+	// `num` is only for the period `day`.
+	const num = query.num || 1;
+	// `max` is probably set to 0 to fetch all results.
+	const max = query.max ?? 10;
+	const date = query.date || new Date().toISOString().split( 'T' )[ 0 ];
+
+	// Calculate the number of days to query based on the period.
+	let days = num;
+	switch ( query.period ) {
+		case 'week':
+			days = 7;
+			break;
+		case 'month':
+			days = getDaysOfMonthFromDate( date );
+			break;
+		case 'year':
+			days = daysInYearFromDate( date );
+			break;
+	}
+
+	return {
+		...query,
+		num,
+		max,
+		date,
+		days,
+	};
+};
 
 function queryStatsDevices( siteId: number, deviceParam: string, query: QueryStatsDevicesParams ) {
 	return wpcom.req.get( `/sites/${ siteId }/stats/devices/${ deviceParam }`, query );
@@ -55,7 +102,7 @@ const useModuleDevicesQuery = (
 	return useQuery( {
 		...getDefaultQueryParams(),
 		queryKey: [ 'stats', 'devices', siteId, deviceParam, query ],
-		queryFn: () => queryStatsDevices( siteId, deviceParam, query ),
+		queryFn: () => queryStatsDevices( siteId, deviceParam, processQueryParams( query ) ),
 		select: ( data ) => parseDevicesData( data as { top_values: { [ key: string ]: number } } ),
 		staleTime: 1000 * 60 * 5,
 	} );
