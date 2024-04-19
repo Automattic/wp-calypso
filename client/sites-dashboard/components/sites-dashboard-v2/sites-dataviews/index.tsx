2306772ccc57a58888e944d8d91123cb29d47464
cleacos <javacodia@gmail.com>
2024-04-19 16:50:54 +0100

    Update Dotcom SitesDataViews to render columns and fields

--- client/sites-dashboard/components/sites-dashboard-v2/sites-dataviews/index.tsx
+++ client/sites-dashboard/components/sites-dashboard-v2/sites-dataviews/index.tsx
@@ -1,7 +1,6 @@
-import { Button } from '@automattic/components';
 import { useSitesListSorting } from '@automattic/sites';
 import { useI18n } from '@wordpress/react-i18n';
-import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { initialDataViewsState } from 'calypso/a8c-for-agencies/components/items-dashboard/constants';
 import ItemsDataViews from 'calypso/a8c-for-agencies/components/items-dashboard/items-dataviews';
 import {
@@ -9,12 +8,15 @@ import {
 	DataViewsState,
 	ItemsDataViewsType,
 } from 'calypso/a8c-for-agencies/components/items-dashboard/items-dataviews/interfaces';
+import JetpackLogo from 'calypso/components/jetpack-logo';
 import TimeSince from 'calypso/components/time-since';
 import { useDispatch, useSelector } from 'calypso/state';
 import { getCurrentUserId } from 'calypso/state/current-user/selectors';
 import { setSelectedSiteId } from 'calypso/state/ui/actions';
 import { SitePlan } from '../../sites-site-plan';
-import { SiteActions } from './sites-site-actions';
+import ActionsField from './dataviews-fields/actions-field';
+import SiteField from './dataviews-fields/site-field';
+import { SiteInfo } from './interfaces';
 import { SiteStats } from './sites-site-stats';
 import { SiteStatus } from './sites-site-status';
 import { getSitesPagination, mapFieldIdToSortKey } from './utils';
@@ -22,10 +24,6 @@ import type { SiteExcerptData } from '@automattic/sites';
 import type { SitesSortOptions } from '@automattic/sites/src/use-sites-list-sorting';
 
 import './style.scss';
-
-interface SitesDataViewsSite {
-	item: SiteExcerptData;
-}
 
 type Props = {
 	sites: SiteExcerptData[];
@@ -35,11 +33,15 @@ type Props = {
 const DotcomSitesDataViews = ( { sites, isLoading }: Props ) => {
 	const { __ } = useI18n();
 	const userId = useSelector( getCurrentUserId );
+	const dispatch = useDispatch();
 
 	const [ dataViewsState, setDataViewsState ] = useState< DataViewsState >( initialDataViewsState );
 
 	const openSitePreviewPane = useCallback(
 		( site: SiteExcerptData ) => {
+			// Dispatch Site selection
+			dispatch( setSelectedSiteId( site.ID || null ) );
+
 			setDataViewsState( ( prevState: DataViewsState ) => ( {
 				...prevState,
 				selectedItem: site,
@@ -51,76 +53,71 @@ const DotcomSitesDataViews = ( { sites, isLoading }: Props ) => {
 
 	const { page: sitesViewPage, perPage: sitesViewPerPage, sort: sitesViewSort } = dataViewsState;
 
+	// Sort sites:
 	const sortedSites = useSitesListSorting( sites, {
 		sortKey: mapFieldIdToSortKey( sitesViewSort.field ),
 		sortOrder: sitesViewSort.direction,
 	} as SitesSortOptions ) as SiteExcerptData[];
 
+	// Paginate sites:
 	const { paginatedSites, totalItems, totalPages } = getSitesPagination(
 		sortedSites,
 		sitesViewPage,
 		sitesViewPerPage
 	);
 
-	/*const dispatch = useDispatch();
-	useEffect( () => {
-		dispatch( setSelectedSiteId( selectedSiteId || null ) );
-	}, [ dispatch, selectedSiteId ] );*/
-
+	// Generate DataViews table field-columns
 	const fields = useMemo< DataViewsColumn[] >(
 		() => [
 			{
 				id: 'site',
 				header: __( 'Site' ),
-				getValue: ( { item }: SitesDataViewsSite ) => item.URL,
-				render: ( { item }: SitesDataViewsSite ) => {
-					return (
-						<Button
-							onClick={ () => {
-								openSitePreviewPane( item );
-							} }
-						>
-							<div className="sites-dataviews__site-name">{ item.title }</div>
-						</Button>
-					);
+				getValue: ( { item }: { item: SiteInfo } ) => item.URL,
+				render: ( { item }: { item: SiteInfo } ) => {
+					return <SiteField site={ item } />;
 				},
 				enableHiding: false,
-				enableSorting: true,
+				enableSorting: false,
 			},
 			{
 				id: 'plan',
 				header: __( 'Plan' ),
-				render: ( { item }: SitesDataViewsSite ) => <SitePlan site={ item } userId={ userId } />,
+				render: ( { item }: { item: SiteInfo } ) => <SitePlan site={ item } userId={ userId } />,
 				enableHiding: false,
 				enableSorting: false,
 			},
 			{
 				id: 'status',
 				header: __( 'Status' ),
-				render: ( { item }: SitesDataViewsSite ) => <SiteStatus site={ item } />,
+				render: ( { item }: { item: SiteInfo } ) => <SiteStatus site={ item } />,
 				enableHiding: false,
 				enableSorting: false,
 			},
 			{
 				id: 'last-publish',
 				header: __( 'Last Publish' ),
-				render: ( { item }: SitesDataViewsSite ) =>
+				render: ( { item }: { item: SiteInfo } ) =>
 					item.options?.updated_at ? <TimeSince date={ item.options.updated_at } /> : '',
 				enableHiding: false,
-				enableSorting: true,
+				enableSorting: false,
 			},
 			{
 				id: 'stats',
-				// todo: Use the Stats header component
-				header: __( 'Stats' ),
-				render: ( { item }: SitesDataViewsSite ) => <SiteStats site={ item } />,
+				header: (
+					<>
+						<JetpackLogo size={ 16 } /> <span>{ __( 'Stats' ) }</span>
+					</>
+				),
+				render: ( { item }: { item: SiteInfo } ) => <SiteStats site={ item } />,
 				enableHiding: false,
 				enableSorting: false,
 			},
 			{
 				id: 'actions',
 				header: __( 'Actions' ),
-				render: ( { item }: SitesDataViewsSite ) => <SiteActions site={ item } />,
+				render: ( { item }: { item: SiteInfo } ) => (
+					<ActionsField site={ item } openSitePreviewPane={ openSitePreviewPane } />
+				),
 				enableHiding: false,
 				enableSorting: false,
 			},
@@ -128,18 +125,39 @@ const DotcomSitesDataViews = ( { sites, isLoading }: Props ) => {
 		[ __, openSitePreviewPane, userId ]
 	);
 
-	const dotcomSitesDataViews: ItemsDataViewsType< SiteExcerptData > = {
+	// Create the itemData packet
+	const [ itemsData, setItemsData ] = useState< ItemsDataViewsType< SiteExcerptData > >( {
 		items: paginatedSites,
 		pagination: {
 			totalItems,
 			totalPages,
 		},
+		itemFieldId: 'ID',
+		searchLabel: __( 'Search for sites' ),
 		fields,
-		setDataViewsState,
-		dataViewsState,
-	};
+		actions: [],
+		setDataViewsState: setDataViewsState,
+		dataViewsState: dataViewsState,
+	} );
 
-	return <ItemsDataViews data={ dotcomSitesDataViews } isLoading={ isLoading } />;
+	// Update the itemData packet
+	useEffect( () => {
+		setItemsData( ( prevState: ItemsDataViewsType< SiteExcerptData > ) => ( {
+			...prevState,
+			items: sites,
+			fields,
+			//actions: actions,
+			pagination: {
+				totalItems,
+				totalPages,
+			},
+			setDataViewsState,
+			dataViewsState,
+			selectedItem: dataViewsState.selectedItem,
+		} ) );
+	}, [ fields, dataViewsState, setDataViewsState, sites, totalItems, totalPages ] ); // add actions when implemented
+
+	return <ItemsDataViews data={ itemsData } isLoading={ isLoading } />;
 };
 
 export default DotcomSitesDataViews;
