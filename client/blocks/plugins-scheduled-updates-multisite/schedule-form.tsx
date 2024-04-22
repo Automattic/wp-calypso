f30498a251586aa471d8b98a3d7686aed21415ff
Tim Broddin <tim@wannabes.be>
2024-04-22 14:38:31 +0200

    Add batch mutations

--- client/blocks/plugins-scheduled-updates-multisite/schedule-form.tsx
+++ client/blocks/plugins-scheduled-updates-multisite/schedule-form.tsx
@@ -1,20 +1,27 @@
-import { __experimentalText as Text } from '@wordpress/components';
+import { __experimentalText as Text, Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { useCoreSitesPluginsQuery } from 'calypso/data/plugins/use-core-sites-plugins-query';
+import { useBatchCreateUpdateScheduleMutation } from 'calypso/data/plugins/use-update-schedules-mutation';
 import { useSiteExcerptsQuery } from 'calypso/data/sites/use-site-excerpts-query';
 import { ScheduleFormFrequency } from '../plugins-scheduled-updates/schedule-form-frequency';
 import { ScheduleFormPlugins } from '../plugins-scheduled-updates/schedule-form-plugins';
 import { validateSites, validatePlugins } from '../plugins-scheduled-updates/schedule-form.helper';
 import { ScheduleFormSites } from './schedule-form-sites';
 
-export const ScheduleForm = () => {
-	const translate = useTranslate();
+type Props = {
+	onNavBack?: () => void;
+};
 
+export const ScheduleForm = ( { onNavBack }: Props ) => {
 	const [ selectedSites, setSelectedSites ] = useState< number[] >( [] );
 	const [ selectedPlugins, setSelectedPlugins ] = useState< string[] >( [] );
 	const [ validationErrors, setValidationErrors ] = useState< Record< string, string > >( {} );
 	const [ fieldTouched, setFieldTouched ] = useState< Record< string, boolean > >( {} );
+	const [ frequency, setFrequency ] = useState< 'daily' | 'weekly' >( 'daily' );
+	const [ timestamp, setTimestamp ] = useState< number >( Date.now() );
+
+	const translate = useTranslate();
 
 	const { data: sites } = useSiteExcerptsQuery( [ 'atomic' ] );
 	const {
@@ -60,31 +67,82 @@ export const ScheduleForm = () => {
 		}
 	}, [ plugins ] );
 
+	const selectedSiteSlugs = sites
+		? sites.filter( ( site ) => selectedSites.includes( site.ID ) ).map( ( site ) => site.slug )
+		: [];
+
+	const { mutateAsync: createUpdateScheduleAsync, isPending: createUpdateSchedulePending } =
+		useBatchCreateUpdateScheduleMutation( selectedSiteSlugs );
+
+	const submitForm = async ( event: React.FormEvent ) => {
+		event.preventDefault();
+
+		const params = {
+			plugins: selectedPlugins,
+			schedule: {
+				timestamp,
+				interval: frequency,
+			},
+		};
+
+		try {
+			await createUpdateScheduleAsync( params );
+			// Handle successful case
+			onNavBack && onNavBack();
+		} catch ( error ) {
+			// TODO: store errors in context?
+			onNavBack && onNavBack();
+		}
+	};
+
 	return (
-		<div className="schedule-form">
-			<Text>{ translate( 'Step 1' ) }</Text>
-			<ScheduleFormSites
-				sites={ sites }
-				onChange={ setSelectedSites }
-				onTouch={ ( touched ) => setFieldTouched( { ...fieldTouched, sites: touched } ) }
-				error={ validationErrors?.sites }
-				showError={ fieldTouched?.sites }
-			/>
+		<form id="schedule" onSubmit={ submitForm }>
+			<div className="schedule-form">
+				<Text>{ translate( 'Step 1' ) }</Text>
+				<ScheduleFormSites
+					sites={ sites }
+					onChange={ setSelectedSites }
+					onTouch={ ( touched ) => setFieldTouched( { ...fieldTouched, sites: touched } ) }
+					error={ validationErrors?.sites }
+					showError={ fieldTouched?.sites }
+				/>
 
-			<Text>{ translate( 'Step 2' ) }</Text>
-			<ScheduleFormPlugins
-				plugins={ getPlugins() }
-				selectedPlugins={ selectedPlugins }
-				isPluginsFetching={ isPluginsFetching }
-				isPluginsFetched={ isPluginsFetched }
-				onChange={ setSelectedPlugins }
-				onTouch={ ( touched ) => setFieldTouched( { ...fieldTouched, plugins: touched } ) }
-				error={ validationErrors?.plugins }
-				showError={ fieldTouched?.plugins }
-			/>
+				<Text>{ translate( 'Step 2' ) }</Text>
+				<ScheduleFormPlugins
+					plugins={ getPlugins() }
+					selectedPlugins={ selectedPlugins }
+					isPluginsFetching={ isPluginsFetching }
+					isPluginsFetched={ isPluginsFetched }
+					onChange={ setSelectedPlugins }
+					onTouch={ ( touched ) => setFieldTouched( { ...fieldTouched, plugins: touched } ) }
+					error={ validationErrors?.plugins }
+					showError={ fieldTouched?.plugins }
+				/>
 
-			<Text>{ translate( 'Step 3' ) }</Text>
-			<ScheduleFormFrequency initFrequency="daily" />
-		</div>
+				<Text>{ translate( 'Step 3' ) }</Text>
+				<ScheduleFormFrequency
+					initFrequency="daily"
+					onChange={ ( frequency, timestamp ) => {
+						setTimestamp( timestamp );
+						setFrequency( frequency );
+					} }
+					onTouch={ ( touched ) => {
+						setFieldTouched( { ...fieldTouched, timestamp: touched } );
+					} }
+				/>
+			</div>
+
+			<Button
+				form="schedule"
+				type="submit"
+				variant="primary"
+				isBusy={ createUpdateSchedulePending }
+				disabled={
+					! selectedSites.length || ! selectedPlugins.length || createUpdateSchedulePending
+				}
+			>
+				{ translate( 'Create' ) }
+			</Button>
+		</form>
 	);
 };
