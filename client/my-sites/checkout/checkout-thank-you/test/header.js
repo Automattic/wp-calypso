61a413779ec2d2f3b7bc95a6553f2a9821bf1f1e
chad1008 <13856531+chad1008@users.noreply.github.com>
2024-04-22 16:39:17 -0400

    remove unused mock

--- client/my-sites/checkout/checkout-thank-you/test/header.js
+++ client/my-sites/checkout/checkout-thank-you/test/header.js
@@ -13,7 +13,6 @@ jest.mock( 'calypso/lib/analytics/tracks', () => ( {
 	recordTracksEvent: () => null,
 } ) );
 jest.mock( '../google-apps-details', () => 'component--google-apps-details' );
-jest.mock( '../jetpack-plan-details', () => 'component--jetpack-plan-details' );
 jest.mock( '../atomic-store-thank-you-card', () => 'component--AtomicStoreThankYouCard' );
 jest.mock( 'calypso/lib/analytics/page-view-tracker', () => 'PageViewTracker' );
 jest.mock( 'calypso/components/happiness-support', () => 'HappinessSupport' );
