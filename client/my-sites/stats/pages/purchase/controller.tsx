4a9cfc71ca5b3ff3635b07a7464213ee8407369c
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2024-04-22 15:45:21 +0300

    Use AsyncLoad for stats purchase

--- client/my-sites/stats/pages/purchase/controller.tsx
+++ client/my-sites/stats/pages/purchase/controller.tsx
@@ -1,10 +1,15 @@
-import StatsPurchasePage from 'calypso/my-sites/stats/pages/purchase';
+import AsyncLoad from 'calypso/components/async-load';
+import PageLoading from '../shared/page-loading';
 import type { Context } from '@automattic/calypso-router';
 
 function purchase( context: Context, next: () => void ) {
 	context.primary = (
 		// DO NOT WRAP WITH <LoadStatsPage /> or you will get an infinite loop
-		<StatsPurchasePage query={ context.query } />
+		<AsyncLoad
+			require="calypso/my-sites/stats/pages/purchase"
+			placeholder={ PageLoading }
+			query={ context.query }
+		/>
 	);
 	next();
 }
