264f5e1842210967520fe1bd4235cd58b10fa281
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2024-04-23 16:29:03 +0200

    Fix animation issue discovered in #89661

--- client/my-sites/patterns/components/pattern-type-switcher/index.tsx
+++ client/my-sites/patterns/components/pattern-type-switcher/index.tsx
@@ -1,7 +1,6 @@
 import { Tooltip } from '@wordpress/components';
-import { useInstanceId } from '@wordpress/compose';
 import classNames from 'classnames';
-import { LayoutGroup, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useTranslate } from 'i18n-calypso';
 import { LocalizedLink } from 'calypso/my-sites/patterns/components/localized-link';
 import { usePatternsContext } from 'calypso/my-sites/patterns/context';
@@ -56,42 +55,34 @@ type PatternTypeSwitcherProps = {
 
 export function PatternTypeSwitcher( { onChange }: PatternTypeSwitcherProps ) {
 	const translate = useTranslate();
-	const baseId = useInstanceId( PatternTypeSwitcher, 'pattern-type-switcher' );
 
 	return (
-		<LayoutGroup id={ baseId }>
-			<motion.div
-				className="pattern-library__toggle pattern-library__toggle--type"
-				layout
-				layoutRoot
+		<motion.div className="pattern-library__toggle pattern-library__toggle--type" layout layoutRoot>
+			<PatternTypeSwitcherOption
+				onChange={ onChange }
+				toolTipText={ translate( 'A collection of blocks that make up one section of a page', {
+					comment: 'Tooltip text for displaying regular patterns within a Pattern Library category',
+					textOnly: true,
+				} ) }
+				value={ PatternTypeFilter.REGULAR }
 			>
-				<PatternTypeSwitcherOption
-					onChange={ onChange }
-					toolTipText={ translate( 'A collection of blocks that make up one section of a page', {
-						comment:
-							'Tooltip text for displaying regular patterns within a Pattern Library category',
-						textOnly: true,
-					} ) }
-					value={ PatternTypeFilter.REGULAR }
-				>
-					{ translate( 'Patterns', {
-						comment: 'Refers to block patterns',
-					} ) }
-				</PatternTypeSwitcherOption>
+				{ translate( 'Patterns', {
+					comment: 'Refers to block patterns',
+				} ) }
+			</PatternTypeSwitcherOption>
 
-				<PatternTypeSwitcherOption
-					onChange={ onChange }
-					toolTipText={ translate( 'A collection of patterns that form an entire page', {
-						comment: 'Tooltip text for displaying page patterns within a Pattern Library category',
-						textOnly: true,
-					} ) }
-					value={ PatternTypeFilter.PAGES }
-				>
-					{ translate( 'Page Layouts', {
-						comment: 'Refers to block patterns that contain entire page layouts',
-					} ) }
-				</PatternTypeSwitcherOption>
-			</motion.div>
-		</LayoutGroup>
+			<PatternTypeSwitcherOption
+				onChange={ onChange }
+				toolTipText={ translate( 'A collection of patterns that form an entire page', {
+					comment: 'Tooltip text for displaying page patterns within a Pattern Library category',
+					textOnly: true,
+				} ) }
+				value={ PatternTypeFilter.PAGES }
+			>
+				{ translate( 'Page Layouts', {
+					comment: 'Refers to block patterns that contain entire page layouts',
+				} ) }
+			</PatternTypeSwitcherOption>
+		</motion.div>
 	);
 }
