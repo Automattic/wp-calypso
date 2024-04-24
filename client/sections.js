b789ffd7063d8612d9f8f0c20b5f6d4ae229764e
Eoin Gallagher <eoin@automattic.com>
2024-04-24 06:12:34 +0100

    Add hosting config page (#89735)
    * Add hosting config page
    
    * refactor to be more generic and add hosting config
    
    * update logic to show global sidebar
    
    * revert rename on directory
    
    * revert change
    
    * update logic around showing sidebars
    
    * update logic again!
    
    * remove debug
    
    * Simplify GSV condition logic
    
    ---------
    
    Co-authored-by: Ashar Fuadi <ashar.fuadi@automattic.com>

--- client/sections.js
+++ client/sections.js
@@ -219,7 +219,7 @@ const sections = [
 		name: 'hosting-overview',
 		paths: [ '/hosting-overview' ],
 		module: 'calypso/hosting-overview',
-		group: 'sites-dashboard',
+		group: 'sites',
 	},
 	{
 		name: 'jetpack-connect',
@@ -548,7 +548,7 @@ const sections = [
 	{
 		name: 'hosting',
 		paths: [ '/hosting-config' ],
-		module: 'calypso/my-sites/hosting',
+		module: 'calypso/hosting-overview',
 		group: 'sites',
 	},
 	{
