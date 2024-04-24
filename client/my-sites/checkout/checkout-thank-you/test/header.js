a3c4ac5ef3f6228fc8810b5bd08e07c715b4f1bd
chad1008 <13856531+chad1008@users.noreply.github.com>
2024-04-24 15:15:41 -0400

    remove` siteUnlaunchedBeforeUpgrade test

--- client/my-sites/checkout/checkout-thank-you/test/header.js
+++ client/my-sites/checkout/checkout-thank-you/test/header.js
@@ -34,28 +34,5 @@ describe( 'CheckoutThankYouHeader', () => {
 			render( <CheckoutThankYouHeader isDataLoaded={ false } { ...defaultProps } /> );
 			expect( screen.getByRole( 'heading', { level: 1 } ) ).toHaveTextContent( 'Loading…' );
 		} );
-		test( 'Should display a list of success messages when siteUnlaunchedBeforeUpgrade=true', () => {
-			render(
-				<CheckoutThankYouHeader
-					isDataLoaded={ true }
-					siteUnlaunchedBeforeUpgrade={ true }
-					{ ...defaultProps }
-				/>
-			);
-			expect( screen.getByRole( 'heading', { level: 1 } ) ).toHaveTextContent(
-				'Get the best out of your site'
-			);
-			expect( screen.queryByRole( 'heading', { level: 2 } ) ).not.toBeInTheDocument();
-
-			const messages = screen.queryAllByRole( 'listitem' );
-
-			expect( messages ).toHaveLength( 2 );
-			expect( messages[ 0 ] ).toHaveTextContent(
-				'All set! Start exploring the features included with your {{strong}}%(productName)s{{/strong}} plan'
-			);
-			expect( messages[ 1 ] ).toHaveTextContent(
-				"Your site has been launched. You can share it with the world whenever you're ready."
-			);
-		} );
 	} );
 } );
