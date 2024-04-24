d8971c7ced7c506990a86f1c5fc53ba93e7abb8e
Payton Swick <payton@foolord.com>
2024-04-24 15:09:20 -0400

    Convert CartFreeUserPlanUpsell to function component
    This is incomplete and missing the plan price data.

--- client/my-sites/checkout/cart/cart-free-user-plan-upsell.tsx
+++ client/my-sites/checkout/cart/cart-free-user-plan-upsell.tsx
@@ -6,9 +6,14 @@ import {
 } from '@automattic/calypso-products';
 import { Button } from '@automattic/components';
 import formatCurrency from '@automattic/format-currency';
-import { localize, useTranslate } from 'i18n-calypso';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import {
+	type ResponseCart,
+	type MinimalRequestCartProduct,
+	type ResponseCartProduct,
+	useShoppingCart,
+} from '@automattic/shopping-cart';
+import { useTranslate } from 'i18n-calypso';
+import { useDispatch } from 'react-redux';
 import SectionHeader from 'calypso/components/section-header';
 import TrackComponentView from 'calypso/lib/analytics/track-component-view';
 import {
@@ -18,96 +23,57 @@ import {
 	planItem,
 } from 'calypso/lib/cart-values/cart-items';
 import { siteHasPaidPlan } from 'calypso/signup/steps/site-picker/site-picker-submit';
+import { useSelector } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import { isRequestingPlans } from 'calypso/state/plans/selectors';
-import { getPlanPrice } from 'calypso/state/products-list/selectors';
-import { isRequestingSitePlans } from 'calypso/state/sites/plans/selectors';
-import { CalypsoDispatch } from 'calypso/state/types';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
-import type { SiteDetails } from '@automattic/data-stores';
-import type {
-	ResponseCart,
-	MinimalRequestCartProduct,
-	ResponseCartProduct,
-} from '@automattic/shopping-cart';
-import type { AppState } from 'calypso/types';
+import useCartKey from '../use-cart-key';
 
 export interface CartFreeUserPlanUpsellProps {
-	cart: Pick< ResponseCart, 'products' >;
-	isCartPendingUpdate?: boolean;
 	addItemToCart: ( item: MinimalRequestCartProduct ) => void;
-	upsellPlan?: ReturnType< typeof getPlan >;
 }
 
-interface CartFreeUserPlanUpsellHocProps {
-	selectedSite?: SiteDetails | null;
-	hasPaidPlan?: boolean;
-	hasPlanInCart?: boolean;
-	isPlansListFetching?: boolean;
-	isRegisteringOrTransferringDomain?: boolean;
-	isSitePlansListFetching?: boolean;
-	planPrice: number | false | null | undefined;
-	showPlanUpsell?: boolean;
-	translate: ReturnType< typeof useTranslate >;
-	clickUpsellAddToCart: () => void;
-}
+const isRegistrationOrTransfer = ( item: ResponseCartProduct ) => {
+	return isDomainRegistration( item ) || isDomainTransfer( item );
+};
 
-class CartFreeUserPlanUpsell extends Component<
-	CartFreeUserPlanUpsellProps & CartFreeUserPlanUpsellHocProps
-> {
-	isLoading() {
-		const { isCartPendingUpdate: isLoadingCart } = this.props;
-		const isLoadingPlans = this.props.isPlansListFetching;
-		const isLoadingSitePlans = this.props.isSitePlansListFetching;
-		return isLoadingCart || isLoadingPlans || isLoadingSitePlans;
-	}
+function UpgradeText( {
+	cart,
+	planPrice,
+	upsellPlan,
+}: {
+	cart: ResponseCart;
+	planPrice: number;
+	upsellPlan: ReturnType< typeof getPlan >;
+} ) {
+	const translate = useTranslate();
+	const firstDomain = cart.products.find( isRegistrationOrTransfer );
+	const planName = upsellPlan?.getTitle() ?? '';
 
-	isRegistrationOrTransfer = ( item: ResponseCartProduct ) => {
-		return isDomainRegistration( item ) || isDomainTransfer( item );
-	};
-
-	getUpgradeText() {
-		const { cart, planPrice, translate, upsellPlan } = this.props;
-		const firstDomain = cart.products.find( this.isRegistrationOrTransfer );
-		const planName = upsellPlan?.getTitle() ?? '';
-
-		if ( planPrice && firstDomain && planPrice > firstDomain.cost ) {
-			const extraToPay = planPrice - firstDomain.cost;
-			return translate(
-				'Pay an {{strong}}extra %(extraToPay)s{{/strong}} for our %(planName)s plan, and get access to all its ' +
-					'features, plus the first year of your domain for free.',
-				{
-					args: {
-						extraToPay: formatCurrency( extraToPay, firstDomain.currency ),
-						planName,
-					},
-					components: {
-						strong: <strong />,
-					},
-				}
-			);
-		} else if ( planPrice && firstDomain && planPrice < firstDomain.cost ) {
-			const savings = firstDomain.cost - planPrice;
-			return translate(
-				'{{strong}}Save %(savings)s{{/strong}} when you purchase a WordPress.com %(planName)s plan ' +
-					'instead — your domain comes free for a year.',
-				{
-					args: {
-						planName,
-						savings: formatCurrency( savings, firstDomain.currency ),
-					},
-					components: {
-						strong: <strong />,
-					},
-				}
-			);
-		}
-
+	if ( planPrice && firstDomain && planPrice > firstDomain.cost ) {
+		const extraToPay = planPrice - firstDomain.cost;
 		return translate(
-			'Purchase our %(planName)s plan at {{strong}}no extra cost{{/strong}}, and get access to all its ' +
+			'Pay an {{strong}}extra %(extraToPay)s{{/strong}} for our %(planName)s plan, and get access to all its ' +
 				'features, plus the first year of your domain for free.',
 			{
-				args: { planName },
+				args: {
+					extraToPay: formatCurrency( extraToPay, firstDomain.currency ),
+					planName,
+				},
+				components: {
+					strong: <strong />,
+				},
+			}
+		);
+	} else if ( planPrice && firstDomain && planPrice < firstDomain.cost ) {
+		const savings = firstDomain.cost - planPrice;
+		return translate(
+			'{{strong}}Save %(savings)s{{/strong}} when you purchase a WordPress.com %(planName)s plan ' +
+				'instead — your domain comes free for a year.',
+			{
+				args: {
+					planName,
+					savings: formatCurrency( savings, firstDomain.currency ),
+				},
 				components: {
 					strong: <strong />,
 				},
@@ -115,18 +81,43 @@ class CartFreeUserPlanUpsell extends Component<
 		);
 	}
 
-	shouldRender() {
-		if ( this.isLoading() ) {
+	return translate(
+		'Purchase our %(planName)s plan at {{strong}}no extra cost{{/strong}}, and get access to all its ' +
+			'features, plus the first year of your domain for free.',
+		{
+			args: { planName },
+			components: {
+				strong: <strong />,
+			},
+		}
+	);
+}
+
+export default function CartFreeUserPlanUpsell( { addItemToCart }: CartFreeUserPlanUpsellProps ) {
+	const cartKey = useCartKey();
+	const {
+		responseCart,
+		isLoading: isLoadingCart,
+		isPendingUpdate: isCartPendingUpdate,
+	} = useShoppingCart( cartKey );
+	const selectedSite = useSelector( getSelectedSite );
+	const isRegisteringOrTransferringDomain =
+		hasDomainRegistration( responseCart ) || hasTransferProduct( responseCart );
+	const showPlanUpsell = !! selectedSite?.ID;
+	const hasPaidPlan = siteHasPaidPlan( selectedSite );
+	const hasPlanInCart = hasPlan( responseCart );
+	const dispatch = useDispatch();
+	const upsellPlan = getPlan( PLAN_PERSONAL );
+
+	const translate = useTranslate();
+
+	// FIXME
+	const planPrice = 0;
+
+	const shouldRender = ( () => {
+		if ( isCartPendingUpdate || isLoadingCart ) {
 			return false;
 		}
-
-		const {
-			hasPaidPlan,
-			hasPlanInCart,
-			isRegisteringOrTransferringDomain,
-			selectedSite,
-			showPlanUpsell,
-		} = this.props;
 
 		return (
 			isRegisteringOrTransferringDomain &&
@@ -135,68 +126,34 @@ class CartFreeUserPlanUpsell extends Component<
 			! hasPaidPlan &&
 			! hasPlanInCart
 		);
-	}
+	} )();
 
-	addPlanToCart = () => {
+	const addPlanToCart = () => {
 		const planCartItem = planItem( PLAN_PERSONAL );
 
 		if ( planCartItem ) {
-			this.props.addItemToCart( planCartItem );
-			this.props.clickUpsellAddToCart();
+			addItemToCart( planCartItem );
+			dispatch( recordTracksEvent( 'calypso_non_dwpo_checkout_plan_upsell_add_to_cart', {} ) );
 		}
 	};
 
-	render() {
-		if ( ! this.shouldRender() ) {
-			return null;
-		}
-
-		const { translate } = this.props;
-
-		return (
-			<div className="cart__upsell-wrapper">
-				<SectionHeader
-					className="cart__header cart__upsell-header"
-					label={ translate( 'Upgrade and save' ) }
-				/>
-				<div className="cart__upsell-body">
-					<p>{ this.getUpgradeText() }</p>
-					<Button onClick={ this.addPlanToCart }>{ translate( 'Add to Cart' ) }</Button>
-				</div>
-				<TrackComponentView eventName="calypso_non_dwpo_checkout_plan_upsell_impression" />
-			</div>
-		);
+	if ( ! shouldRender ) {
+		return null;
 	}
+
+	return (
+		<div className="cart__upsell-wrapper">
+			<SectionHeader
+				className="cart__header cart__upsell-header"
+				label={ translate( 'Upgrade and save' ) }
+			/>
+			<div className="cart__upsell-body">
+				<p>
+					<UpgradeText cart={ responseCart } planPrice={ planPrice } upsellPlan={ upsellPlan } />
+				</p>
+				<Button onClick={ addPlanToCart }>{ translate( 'Add to Cart' ) }</Button>
+			</div>
+			<TrackComponentView eventName="calypso_non_dwpo_checkout_plan_upsell_impression" />
+		</div>
+	);
 }
-
-const mapStateToProps = ( state: AppState, { cart }: CartFreeUserPlanUpsellProps ) => {
-	const selectedSite = getSelectedSite( state );
-	const selectedSiteId = selectedSite ? selectedSite.ID : null;
-	const isPlansListFetching = isRequestingPlans( state );
-	const upsellPlan = getPlan( PLAN_PERSONAL );
-
-	return {
-		hasPaidPlan: siteHasPaidPlan( selectedSite ),
-		hasPlanInCart: hasPlan( cart ),
-		isPlansListFetching,
-		isRegisteringOrTransferringDomain: hasDomainRegistration( cart ) || hasTransferProduct( cart ),
-		isSitePlansListFetching: isRequestingSitePlans( state ),
-		planPrice:
-			! isPlansListFetching &&
-			selectedSiteId &&
-			upsellPlan &&
-			getPlanPrice( state, selectedSiteId, upsellPlan, false ),
-		selectedSite,
-		showPlanUpsell: !! selectedSiteId,
-		upsellPlan,
-	};
-};
-
-const mapDispatchToProps = ( dispatch: CalypsoDispatch ) => {
-	return {
-		clickUpsellAddToCart: () =>
-			dispatch( recordTracksEvent( 'calypso_non_dwpo_checkout_plan_upsell_add_to_cart', {} ) ),
-	};
-};
-
-export default connect( mapStateToProps, mapDispatchToProps )( localize( CartFreeUserPlanUpsell ) );
