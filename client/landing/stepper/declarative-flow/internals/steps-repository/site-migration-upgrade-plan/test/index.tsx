2db38ee92ae08518b8235bd2fb6f83abb6cc2ab6
Mark Biek <mark.biek@automattic.com>
2024-04-24 09:34:55 -0400

    Update site-migration-upgrade-plan tests
    This updates the tests to mock the check-eligibility API call instead of
    the `useCheckEligibilityMigrationTrialPlan` hook.

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-upgrade-plan/test/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-upgrade-plan/test/index.tsx
@@ -1,19 +1,21 @@
 /**
  * @jest-environment jsdom
  */
-import { screen } from '@testing-library/react';
+import { PLAN_MIGRATION_TRIAL_MONTHLY } from '@automattic/calypso-products';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import nock from 'nock';
 import React from 'react';
 import useAddHostingTrialMutation from 'calypso/data/hosting/use-add-hosting-trial-mutation';
-import useCheckEligibilityMigrationTrialPlan from 'calypso/data/plans/use-check-eligibility-migration-trial-plan';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import plansReducer from 'calypso/state/plans/reducer';
 import SiteMigrationUpgradePlan from '../';
 import { StepProps } from '../../../types';
 import { mockStepProps, renderStep } from '../../test/helpers';
 
+const planSlug = PLAN_MIGRATION_TRIAL_MONTHLY;
+
 jest.mock( 'calypso/landing/stepper/hooks/use-site' );
-jest.mock( 'calypso/data/plans/use-check-eligibility-migration-trial-plan' );
 jest.mock( 'calypso/data/hosting/use-add-hosting-trial-mutation' );
 
 ( useSite as jest.Mock ).mockReturnValue( {
@@ -21,13 +23,27 @@ jest.mock( 'calypso/data/hosting/use-add-hosting-trial-mutation' );
 	URL: 'https://site-url.wordpress.com',
 } );
 
-( useCheckEligibilityMigrationTrialPlan as jest.Mock ).mockReturnValue( {
-	data: { eligible: true },
-} );
-
 ( useAddHostingTrialMutation as jest.Mock ).mockImplementation( ( { onSuccess } ) => ( {
 	addHostingTrial: () => onSuccess(),
 } ) );
+
+const mockApi = () => nock( 'https://public-api.wordpress.com:443' );
+
+const API_RESPONSE_EMAIL_VERIFIED = {
+	eligible: true,
+	error_code: 'email-verified',
+};
+
+const API_RESPONSE_EMAIL_UNVERIFIED = {
+	eligible: false,
+	error_code: 'email-unverified',
+};
+
+const mockTrialEligibilityAPI = ( payload: any ) => {
+	mockApi()
+		.get( `/wpcom/v2/sites/site-id/hosting/trial/check-eligibility/${ planSlug }` )
+		.reply( 200, payload );
+};
 
 describe( 'SiteMigrationUpgradePlan', () => {
 	const render = ( props?: Partial< StepProps > ) => {
@@ -38,6 +54,11 @@ describe( 'SiteMigrationUpgradePlan', () => {
 			},
 		} );
 	};
+
+	beforeAll( () => {
+		nock.disableNetConnect();
+		mockTrialEligibilityAPI( API_RESPONSE_EMAIL_VERIFIED );
+	} );
 
 	it( 'selects annual plan as default', async () => {
 		const navigation = { submit: jest.fn() };
@@ -78,14 +99,40 @@ describe( 'SiteMigrationUpgradePlan', () => {
 	} );
 
 	it( 'selects free trial', async () => {
+		mockTrialEligibilityAPI( API_RESPONSE_EMAIL_VERIFIED );
 		const navigation = { submit: jest.fn() };
 		render( { navigation } );
 
-		await userEvent.click( screen.getByRole( 'button', { name: /Try 7 days for free/ } ) );
-		await userEvent.click( screen.getByRole( 'button', { name: /Upgrade and migrate/ } ) );
+		await waitFor( async () => {
+			await userEvent.click( screen.getByRole( 'button', { name: /Try 7 days for free/ } ) );
 
-		expect( navigation.submit ).toHaveBeenCalledWith( {
-			freeTrialSelected: true,
+			expect( navigation.submit ).toHaveBeenCalledWith( {
+				freeTrialSelected: true,
+			} );
+		} );
+	} );
+
+	it( 'show the trial plan for verified users', async () => {
+		nock.cleanAll();
+		mockTrialEligibilityAPI( API_RESPONSE_EMAIL_VERIFIED );
+
+		render( { data: { hideFreeMigrationTrialForNonVerifiedEmail: true } } );
+
+		await waitFor( () => {
+			expect( screen.queryByRole( 'button', { name: /Try 7 days for free/ } ) ).toBeInTheDocument();
+		} );
+	} );
+
+	it( 'hides the trial plan for unverified users', async () => {
+		nock.cleanAll();
+		mockTrialEligibilityAPI( API_RESPONSE_EMAIL_UNVERIFIED );
+
+		render( { data: { hideFreeMigrationTrialForNonVerifiedEmail: true } } );
+
+		await waitFor( () => {
+			expect(
+				screen.queryByRole( 'button', { name: /Try 7 days for free/ } )
+			).not.toBeInTheDocument();
 		} );
 	} );
 } );
