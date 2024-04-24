26e68724cdd6dc0d93548bf46f0726328e7d1247
Dale du Preez <dale@automattic.com>
2024-04-24 13:31:20 +0200

    Add a test case for non-en logins

--- client/landing/stepper/declarative-flow/test/migration-signup-flow.tsx
+++ client/landing/stepper/declarative-flow/test/migration-signup-flow.tsx
@@ -27,6 +27,7 @@ describe( 'Migration Signup Flow', () => {
 
 	beforeEach( () => {
 		( window.location.assign as jest.Mock ).mockClear();
+		window.location.search = '';
 		( isCurrentUserLoggedIn as jest.Mock ).mockReturnValue( true );
 		( useIsSiteOwner as jest.Mock ).mockReturnValue( {
 			isOwner: true,
@@ -36,14 +37,33 @@ describe( 'Migration Signup Flow', () => {
 	describe( 'useAssertConditions', () => {
 		it( 'redirects the user to the login page when they are not logged in', () => {
 			( isCurrentUserLoggedIn as jest.Mock ).mockReturnValue( false );
+			const searchValue = '?ref=logged-out-homepage&aff=aff123&vid=vid321';
+			window.location.search = searchValue;
 
 			const { runUseAssertionCondition } = renderFlow( migrationSignupFlow );
 			runUseAssertionCondition( {
 				currentStep: STEPS.SITE_MIGRATION_IDENTIFY.slug,
+				currentURL: `/setup/${ STEPS.SITE_MIGRATION_IDENTIFY.slug }${ searchValue }`,
 			} );
 
 			expect( window.location.assign ).toHaveBeenCalledWith(
-				`/start/account/user-social?variationName=migration-signup&toStepper=true&redirect_to=/setup/migration-signup`
+				`/start/account/user-social?variationName=migration-signup&toStepper=true&redirect_to=/setup/migration-signup&vid=vid321&aff=aff123&ref=logged-out-homepage`
+			);
+		} );
+
+		it( 'redirects the user to the login page with the correct locale when they are not logged in', () => {
+			( isCurrentUserLoggedIn as jest.Mock ).mockReturnValue( false );
+			const searchValue = '?ref=logged-out-homepage&aff=aff123&vid=vid321&locale=fr';
+			window.location.search = searchValue;
+
+			const { runUseAssertionCondition } = renderFlow( migrationSignupFlow );
+			runUseAssertionCondition( {
+				currentStep: STEPS.SITE_MIGRATION_IDENTIFY.slug,
+				currentURL: `/setup/${ STEPS.SITE_MIGRATION_IDENTIFY.slug }${ searchValue }`,
+			} );
+
+			expect( window.location.assign ).toHaveBeenCalledWith(
+				`/start/account/user-social/fr?variationName=migration-signup&toStepper=true&redirect_to=/setup/migration-signup%3Flocale%3Dfr&vid=vid321&aff=aff123&ref=logged-out-homepage`
 			);
 		} );
 	} );
