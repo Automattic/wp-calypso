4c4939dc8ec2cbc72e67c478c002bdaf3d28b434
Eoin Gallagher <eoin@automattic.com>
2024-04-25 11:57:37 +0100

    Add panels to global sites dashboard (#89862)
    * Add panels
    
    * update styles
    
    * add overflow to hosting config

--- client/site-monitoring/components/site-monitoring-overview.tsx
+++ client/site-monitoring/components/site-monitoring-overview.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { MetricsTab } from 'calypso/my-sites/site-monitoring/metrics-tab';
 
+import '../style.scss';
+
 const SiteMonitoringOverview: FC = () => {
 	return (
 		<div className="site-monitoring-overview">
