4015ef70317a5bf51a12ea6b15a7090482be5df7
LuPuS2k <gabrieldemichelis@gmail.com>
2024-04-25 22:00:58 -0300

    Added hosting-overview + monitoring

--- client/hosting-overview/controller.tsx
+++ client/hosting-overview/controller.tsx
@@ -5,38 +5,43 @@ import HostingActivate from 'calypso/my-sites/hosting/hosting-activate';
 import Hosting from 'calypso/my-sites/hosting/main';
 import MySitesNavigation from 'calypso/my-sites/navigation';
 import SitesDashboardV2 from 'calypso/sites-dashboard-v2';
-import { DOTCOM_HOSTING_CONFIG } from 'calypso/sites-dashboard-v2/site-preview-pane/constants';
+import {
+	DOTCOM_HOSTING_CONFIG,
+	DOTCOM_OVERVIEW,
+} from 'calypso/sites-dashboard-v2/site-preview-pane/constants';
 import { isGlobalSiteViewEnabled } from 'calypso/state/sites/selectors';
 import { getSelectedSite, getSelectedSiteId } from 'calypso/state/ui/selectors';
 
-export function hostingOverview( context: PageJSContext, next: () => void ) {
-	context.secondary = <MySitesNavigation path={ context.path } />;
-	context.primary = <HostingOverview />;
-	next();
-}
-
-export function hostingConfiguration( context: PageJSContext, next: () => void ) {
-	context.secondary = <MySitesNavigation path={ context.path } />;
+function shouldShowInSitesFlyoutPanel( context: PageJSContext, feature: string ) {
 	const state = context.store.getState();
 	const selectedSiteId = getSelectedSiteId( state );
+
 	// Show the hosting configuration page in the preview pane from the sites dashboard.
 	if (
 		isEnabled( 'layout/dotcom-nav-redesign-v2' ) &&
 		isGlobalSiteViewEnabled( state, selectedSiteId )
 	) {
 		const selectedSite = getSelectedSite( state );
-		context.primary = (
+		return (
 			// Sites Dashboard V2 - Dotcom Nav Redesign V2
 			<SitesDashboardV2
 				queryParams={ {} }
 				selectedSite={ selectedSite }
-				selectedFeature={ DOTCOM_HOSTING_CONFIG }
+				selectedFeature={ feature }
 			/>
 		);
-		next();
 	}
+}
 
-	context.primary = (
+export function hostingOverview( context: PageJSContext, next: () => void ) {
+	context.secondary = <MySitesNavigation path={ context.path } />;
+	context.primary = shouldShowInSitesFlyoutPanel( context, DOTCOM_OVERVIEW ) ?? <HostingOverview />;
+	next();
+}
+
+export function hostingConfiguration( context: PageJSContext, next: () => void ) {
+	context.secondary = <MySitesNavigation path={ context.path } />;
+	context.primary = shouldShowInSitesFlyoutPanel( context, DOTCOM_HOSTING_CONFIG ) ?? (
 		<div className="hosting-configuration">
 			<Hosting />
 		</div>
@@ -46,7 +51,7 @@ export function hostingConfiguration( context: PageJSContext, next: () => void )
 
 export function hostingActivate( context: PageJSContext, next: () => void ) {
 	context.secondary = <MySitesNavigation path={ context.path } />;
-	context.primary = (
+	context.primary = shouldShowInSitesFlyoutPanel( context, DOTCOM_HOSTING_CONFIG ) ?? (
 		<div className="hosting-configuration">
 			<HostingActivate />
 		</div>
--- client/site-monitoring/controller.tsx
+++ client/site-monitoring/controller.tsx
@@ -1,13 +1,45 @@
+import { isEnabled } from '@automattic/calypso-config';
 import MySitesNavigation from 'calypso/my-sites/navigation';
+import SitesDashboardV2 from 'calypso/sites-dashboard-v2';
+import {
+	DOTCOM_MONITORING,
+	DOTCOM_PHP_LOGS,
+	DOTCOM_SERVER_LOGS,
+} from 'calypso/sites-dashboard-v2/site-preview-pane/constants';
+import { isGlobalSiteViewEnabled } from 'calypso/state/sites/selectors';
+import { getSelectedSite, getSelectedSiteId } from 'calypso/state/ui/selectors';
 import SiteMonitoringPhpLogs from './components/php-logs';
 import SiteMonitoringServerLogs from './components/server-logs';
 import SiteMonitoringOverview from './components/site-monitoring-overview';
 import type { Context as PageJSContext } from '@automattic/calypso-router';
 
+function shouldShowInSitesFlyoutPanel( context: PageJSContext, feature: string ) {
+	const state = context.store.getState();
+	const selectedSiteId = getSelectedSiteId( state );
+
+	// Show the hosting configuration page in the preview pane from the sites dashboard.
+	if (
+		isEnabled( 'layout/dotcom-nav-redesign-v2' ) &&
+		isGlobalSiteViewEnabled( state, selectedSiteId )
+	) {
+		const selectedSite = getSelectedSite( state );
+		return (
+			// Sites Dashboard V2 - Dotcom Nav Redesign V2
+			<SitesDashboardV2
+				queryParams={ {} }
+				selectedSite={ selectedSite }
+				selectedFeature={ feature }
+			/>
+		);
+	}
+}
+
 export function siteMonitoringOverview( context: PageJSContext, next: () => void ) {
 	context.secondary = <MySitesNavigation path={ context.path } />;
 
-	context.primary = <SiteMonitoringOverview />;
+	context.primary = shouldShowInSitesFlyoutPanel( context, DOTCOM_MONITORING ) ?? (
+		<SiteMonitoringOverview />
+	);
 
 	next();
 }
@@ -15,7 +47,9 @@ export function siteMonitoringOverview( context: PageJSContext, next: () => void
 export function siteMonitoringPhpLogs( context: PageJSContext, next: () => void ) {
 	context.secondary = <MySitesNavigation path={ context.path } />;
 
-	context.primary = <SiteMonitoringPhpLogs />;
+	context.primary = shouldShowInSitesFlyoutPanel( context, DOTCOM_PHP_LOGS ) ?? (
+		<SiteMonitoringPhpLogs />
+	);
 
 	next();
 }
@@ -23,7 +57,9 @@ export function siteMonitoringPhpLogs( context: PageJSContext, next: () => void 
 export function siteMonitoringServerLogs( context: PageJSContext, next: () => void ) {
 	context.secondary = <MySitesNavigation path={ context.path } />;
 
-	context.primary = <SiteMonitoringServerLogs />;
+	context.primary = shouldShowInSitesFlyoutPanel( context, DOTCOM_SERVER_LOGS ) ?? (
+		<SiteMonitoringServerLogs />
+	);
 
 	next();
 }
