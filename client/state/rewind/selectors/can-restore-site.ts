4df9b83c4df2175cbcaa7a3d5f518152745c2432
Rafael Agostini <rafael.agostini@automattic.com>
2024-04-26 22:43:27 -0500

    Add Jetpack site connection validation to canRestoreSite

--- client/state/rewind/selectors/can-restore-site.ts
+++ client/state/rewind/selectors/can-restore-site.ts
@@ -1,10 +1,16 @@
 import getDoesRewindNeedCredentials from 'calypso/state/selectors/get-does-rewind-need-credentials';
 import getIsRestoreInProgress from 'calypso/state/selectors/get-is-restore-in-progress';
+import isJetpackSiteConnected from 'calypso/state/selectors/is-jetpack-site-connected';
 import { AppState } from 'calypso/types';
 
 const canRestoreSite = ( state: AppState, siteId: number ): boolean => {
 	const doesRewindNeedCredentials = getDoesRewindNeedCredentials( state, siteId ) as boolean;
 	const isRestoreInProgress = getIsRestoreInProgress( state, siteId );
+	const isSiteConnected = isJetpackSiteConnected( state, siteId );
+
+	if ( ! isSiteConnected ) {
+		return false;
+	}
 
 	return ! ( doesRewindNeedCredentials || isRestoreInProgress );
 };
