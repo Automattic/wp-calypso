3cf62dddd7cee353b0987e73e48b9ff1a25e95b1
Renan <renansscarvalho@gmail.com>
2024-04-29 18:03:09 +0200

    Use stepNavigator to redirect to checkout

--- client/landing/stepper/declarative-flow/internals/steps-repository/importer-wordpress/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer-wordpress/index.tsx
@@ -1,8 +1,10 @@
 import WordpressImporter from 'calypso/blocks/importer/wordpress';
+import { WPImportOption } from 'calypso/blocks/importer/wordpress/types';
 import { MigrationAssistanceModal } from 'calypso/landing/stepper/declarative-flow/internals/components/migration-assistance-modal';
+import { useStepNavigator } from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/importer/hooks/use-step-navigator';
 import { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
 import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
-import { getMigrationAssistanceCheckoutUrl } from 'calypso/landing/stepper/utils/migration-assistance-checkout';
+import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { withImporterWrapper } from '../importer';
 import './style.scss';
 
@@ -10,25 +12,23 @@ const Importer = withImporterWrapper( WordpressImporter );
 
 const ImporterWordpress: Step = function ( props ) {
 	const queryParams = useQuery();
+	const site = useSite();
 	const migrateFrom = queryParams.get( 'from' );
 	const showMigrationModal = queryParams.get( 'showModal' );
 	const siteSlug = queryParams.get( 'siteSlug' );
-
-	function navigateToCheckoutPage() {
-		const preparedCheckoutUrl = getMigrationAssistanceCheckoutUrl(
-			siteSlug || '',
-			props.flow,
-			props.stepName,
-			queryParams
-		);
-		props.navigation?.submit?.( { action: 'checkout', checkoutUrl: preparedCheckoutUrl } );
-	}
+	const stepNavigator = useStepNavigator(
+		props.flow,
+		props.navigation,
+		site?.ID,
+		siteSlug,
+		migrateFrom
+	);
 
 	return (
 		<>
 			{ showMigrationModal && (
 				<MigrationAssistanceModal
-					onConfirm={ navigateToCheckoutPage }
+					onConfirm={ () => stepNavigator?.goToCheckoutPage?.( WPImportOption.EVERYTHING ) }
 					migrateFrom={ migrateFrom }
 					navigateBack={ props.navigation.goBack }
 				/>
