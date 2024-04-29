553997073a2eac3ad890253331266f85a49e1c51
Nate Weller <hello@nateweller.com>
2024-04-29 17:31:01 -0600

    Redesign hosting page

--- client/a8c-for-agencies/sections/marketplace/hosting-overview/hosting-card/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/hosting-overview/hosting-card/index.tsx
@@ -1,8 +1,22 @@
 import { isEnabled } from '@automattic/calypso-config';
-import { Badge, Button } from '@automattic/components';
+import {
+	Badge,
+	BloombergLogo,
+	Button,
+	CNNLogo,
+	CondenastLogo,
+	DisneyLogo,
+	FacebookLogo,
+	Gridicon,
+	SalesforceLogo,
+	SlackLogo,
+	TimeLogo,
+} from '@automattic/components';
 import { formatCurrency } from '@automattic/format-currency';
 import { Icon, external } from '@wordpress/icons';
+import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
+import { useCallback, useMemo } from 'react';
 import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { APIProductFamilyProduct } from 'calypso/state/partner-portal/types';
@@ -15,9 +29,10 @@ import './style.scss';
 type Props = {
 	plan: APIProductFamilyProduct;
 	pressableOwnership?: boolean;
+	className?: string;
 };
 
-export default function HostingCard( { plan, pressableOwnership }: Props ) {
+export default function HostingCard( { plan, pressableOwnership, className }: Props ) {
 	const translate = useTranslate();
 	const dispatch = useDispatch();
 	//FIXME: We want to unify and refactor this logic, once we decide
@@ -26,67 +41,131 @@ export default function HostingCard( { plan, pressableOwnership }: Props ) {
 
 	const { name, description, features } = useHostingDescription( plan.family_slug );
 
-	const onExploreClick = () => {
+	const onExploreClick = useCallback( () => {
 		dispatch(
 			recordTracksEvent( 'calypso_marketplace_hosting_overview_explore_plan_click', {
 				hosting: plan.family_slug,
 			} )
 		);
-	};
+	}, [ dispatch, plan.family_slug ] );
+
+	const onVipDemoClick = useCallback( () => {
+		dispatch( recordTracksEvent( 'calypso_marketplace_hosting_overview_request_vip_demo_click' ) );
+	}, [ dispatch ] );
+
+	const priceStartingAt = useMemo< string >( () => {
+		// If the amount is not a number, we assume it's a string that should be displayed as is.
+		if ( plan.amount && isNaN( Number( plan.amount ) ) ) {
+			return plan.amount;
+		}
+
+		return formatCurrency( Number( plan.amount ), plan.currency );
+	}, [ plan.amount, plan.currency ] );
+
+	const priceIntervalDescription = useMemo< string >( () => {
+		if ( plan.price_interval === 'day' ) {
+			return translate( 'USD per plan per day' );
+		}
+
+		if ( plan.price_interval === 'month' ) {
+			return translate( 'USD per plan per month' );
+		}
+
+		return translate( 'USD' );
+	}, [ plan.price_interval, translate ] );
+
+	const exploreButton = useMemo( () => {
+		if ( pressableOwnership ) {
+			return (
+				<Button
+					className="hosting-card__pressable-dashboard-button"
+					target="_blank"
+					rel="norefferer nooppener"
+					href={ pressableUrl }
+				>
+					{ translate( 'Go to Pressable Dashboard' ) }
+					<Icon icon={ external } size={ 18 } />
+				</Button>
+			);
+		}
+
+		if ( plan.family_slug === 'vip' ) {
+			return (
+				<Button
+					href={ getHostingPageUrl( plan.family_slug ) }
+					target="_blank"
+					onClick={ onVipDemoClick }
+					primary
+				>
+					<span className="hosting-card__explore-button-content">
+						{ translate( 'Request a Demo' ) }
+						<Gridicon icon="external" />
+					</span>
+				</Button>
+			);
+		}
+
+		return (
+			<Button
+				className="hosting-card__explore-button"
+				href={ getHostingPageUrl( plan.family_slug ) }
+				onClick={ onExploreClick }
+				primary
+			>
+				{ translate( 'Explore %(hosting)s plans', {
+					args: {
+						hosting: name,
+					},
+					comment: '%(hosting)s is the name of the hosting provider.',
+				} ) }
+			</Button>
+		);
+	}, [ name, onExploreClick, onVipDemoClick, plan.family_slug, pressableOwnership, translate ] );
 
 	return (
-		<div className="hosting-card">
+		<div className={ classNames( 'hosting-card', className ) }>
 			<div className="hosting-card__section">
 				<div className="hosting-card__header">
 					{ getHostingLogo( plan.family_slug ) }
 					{ pressableOwnership && <Badge type="success">{ translate( 'You own this' ) }</Badge> }
 				</div>
 
-				<p className="hosting-card__description">{ description }</p>
-
 				<div className="hosting-card__price">
 					<b className="hosting-card__price-value">
-						{ translate( 'Starting at %(price)s', {
-							args: { price: formatCurrency( Number( plan.amount ), plan.currency ) },
+						{ translate( 'Starting at %(priceStartingAt)s', {
+							args: { priceStartingAt },
 						} ) }
 					</b>
-					<div className="hosting-card__price-interval">
-						{ plan.price_interval === 'day' && translate( 'USD per plan per day' ) }
-						{ plan.price_interval === 'month' && translate( 'USD per plan per month' ) }
-					</div>
+					<div className="hosting-card__price-interval">{ priceIntervalDescription }</div>
 				</div>
 
-				{ ! pressableOwnership ? (
-					<Button
-						className="hosting-card__explore-button"
-						href={ getHostingPageUrl( plan.family_slug ) }
-						onClick={ onExploreClick }
-						primary
-					>
-						{ translate( 'Explore %(hosting)s plans', {
-							args: {
-								hosting: name,
-							},
-							comment: '%(hosting)s is the name of the hosting provider.',
-						} ) }
-					</Button>
-				) : (
-					<Button
-						className="hosting-card__pressable-dashboard-button"
-						target="_blank"
-						rel="norefferer nooppener"
-						href={ pressableUrl }
-					>
-						{ translate( 'Go to Pressable Dashboard' ) }
-						<Icon icon={ external } size={ 18 } />
-					</Button>
-				) }
+				<p className="hosting-card__description">{ description }</p>
+
+				{ exploreButton }
 			</div>
 
 			{ isEnabled( 'a8c-for-agencies/wpcom-creator-plan-purchase-flow' ) && (
-				<div className="hosting-card__section">
-					<SimpleList items={ features } />
-				</div>
+				<>
+					{ features.length > 0 && (
+						<div className="hosting-card__section">
+							<SimpleList items={ features } />
+						</div>
+					) }
+					{ plan.family_slug === 'vip' && (
+						<div className="hosting-card__section">
+							<div className="hosting-card__logos">
+								<TimeLogo />
+								<SlackLogo />
+								<DisneyLogo />
+								<CNNLogo />
+								<SalesforceLogo />
+								<FacebookLogo />
+								<CondenastLogo />
+								<BloombergLogo />
+							</div>
+						</div>
+					) }
+				</>
 			) }
 		</div>
 	);
