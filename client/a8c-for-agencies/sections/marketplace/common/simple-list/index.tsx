daef621eee9037b6099e7b6c3c0267f23f7fa619
Nate Weller <hello@nateweller.com>
2024-04-29 14:12:26 -0600

    Add support to SimpleList for className prop

--- client/a8c-for-agencies/sections/marketplace/common/simple-list/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/common/simple-list/index.tsx
@@ -1,11 +1,17 @@
 import { Icon, check } from '@wordpress/icons';
+import classNames from 'classnames';
 import { ReactNode } from 'react';
-
 import './style.scss';
 
-export default function SimpleList( { items }: { items: ReactNode[] } ) {
+export default function SimpleList( {
+	items,
+	className,
+}: {
+	items: ReactNode[];
+	className?: string;
+} ) {
 	return (
-		<ul className="simple-list">
+		<ul className={ classNames( 'simple-list', className ) }>
 			{ items.map( ( item, index ) => (
 				<li key={ `item-${ index }` }>
 					<Icon className="simple-list-icon" icon={ check } size={ 24 } />
