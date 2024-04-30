3f1c0cfc06680536d531cc3b2963067d6024ca86
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-04-30 14:37:32 +0700

    Nav redesign: delete duplicated controllers (#90038)

--- client/site-monitoring/controller.tsx
+++ client/site-monitoring/controller.tsx
@@ -1,22 +1,37 @@
-import SiteMonitoringPhpLogs from './components/php-logs';
-import SiteMonitoringServerLogs from './components/server-logs';
-import SiteMonitoringOverview from './components/site-monitoring-overview';
+import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { LogsTab } from 'calypso/my-sites/site-monitoring/logs-tab';
+import { MetricsTab } from 'calypso/my-sites/site-monitoring/metrics-tab';
 import type { Context as PageJSContext } from '@automattic/calypso-router';
 
 export function siteMonitoringOverview( context: PageJSContext, next: () => void ) {
-	context.primary = <SiteMonitoringOverview />;
+	context.primary = (
+		<div className="site-monitoring-overview">
+			<PageViewTracker path="/site-monitoring/:site" title="Site Monitoring" />
+			<MetricsTab />
+		</div>
+	);
 
 	next();
 }
 
 export function siteMonitoringPhpLogs( context: PageJSContext, next: () => void ) {
-	context.primary = <SiteMonitoringPhpLogs />;
+	context.primary = (
+		<div className="site-monitoring-php-logs">
+			<PageViewTracker path="/site-monitoring/:site/php" title="Site Monitoring" />
+			<LogsTab logType="php" />
+		</div>
+	);
 
 	next();
 }
 
 export function siteMonitoringServerLogs( context: PageJSContext, next: () => void ) {
-	context.primary = <SiteMonitoringServerLogs />;
+	context.primary = (
+		<div className="site-monitoring-server-logs">
+			<PageViewTracker path="/site-monitoring/:site/web" title="Site Monitoring" />
+			<LogsTab logType="web" />
+		</div>
+	);
 
 	next();
 }
