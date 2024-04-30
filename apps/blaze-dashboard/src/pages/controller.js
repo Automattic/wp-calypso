c64e03adecce72231589bf19bdc62f3a90bbfc86
Sebastián Barbosa <sebabarbosa@gmail.com>
2024-04-30 15:05:02 -0300

    Adds the posibility to define different setup pages for Blaze Dashboard (#89636)

--- apps/blaze-dashboard/src/pages/controller.js
+++ apps/blaze-dashboard/src/pages/controller.js
@@ -1,6 +1,7 @@
+import config from '@automattic/calypso-config';
 import BlazeSetup from './setup/main';
 
 export const setup = ( context, next ) => {
-	context.primary = <BlazeSetup />;
+	context.primary = <BlazeSetup setupInfo={ config( 'need_setup' ) } />;
 	next();
 };
