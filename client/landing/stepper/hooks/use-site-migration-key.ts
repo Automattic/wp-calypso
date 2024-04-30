398b039c3d70fa3084d45539a5807b4a0f3c550a
Gabriel Caires <gabriel.caires@automattic.com>
2024-04-30 16:04:48 +0100

    Site Migration: Enable to manage when the site migration key will be loaded (#90086)
    * Add prop to manage when the request will happens
    
    * Fix site id type
    
    * Fix typo on the migration file name
    
    * Fix file name

--- client/landing/stepper/hooks/use-site-migration-key.ts
+++ client/landing/stepper/hooks/use-site-migration-key.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import wpcom from 'calypso/lib/wp';
 
 interface ApiResponse {
@@ -10,12 +10,14 @@ const getMigrationKey = async ( siteId: number ): Promise< ApiResponse > =>
 		apiNamespace: 'wpcom/v2',
 	} );
 
-export const useSiteMigrationKey = ( siteId?: number ) => {
+type Options = Pick< UseQueryOptions, 'enabled' >;
+
+export const useSiteMigrationKey = ( siteId?: number, options?: Options ) => {
 	return useQuery( {
 		queryKey: [ 'site-migration-key', siteId ],
 		queryFn: () => getMigrationKey( siteId! ),
 		retry: false,
-		enabled: !! siteId,
+		enabled: !! siteId && ( options?.enabled ?? true ),
 		select: ( data ) => ( { migrationKey: data?.migration_key } ),
 		refetchOnWindowFocus: false,
 	} );
