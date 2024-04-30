5469ef5cc3d933e9ae290b23ba4d86f6b27a44e2
Eoin Gallagher <eoin@automattic.com>
2024-04-30 15:42:21 +0100

    update styles in proper place

--- client/sites-dashboard/components/sites-search.ts
+++ client/sites-dashboard/components/sites-search.ts
@@ -2,15 +2,12 @@ import Search from '@automattic/search';
 import styled from '@emotion/styled';
 import { MEDIA_QUERIES } from '../utils';
 
-//The font should be 13px and --studio-gray-40. The icons should use --studio-black.
 export const SitesSearch = styled( Search )( {
 	'--color-surface': 'var( --studio-white )',
 
 	maxHeight: '42px',
 	overflow: 'hidden',
 	border: '1px solid #c3c4c7',
-	color: 'var( --studio-gray-40 )',
-	fontSize: '13px',
 
 	[ MEDIA_QUERIES.mediumOrLarger ]: {
 		flex: '0 1 390px !important',
