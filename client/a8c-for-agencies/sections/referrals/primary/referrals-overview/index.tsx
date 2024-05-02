f2a2a4680baf9c283940a882ef7ca8d4d6307e2d
Andrii <lysenkoa.work@gmail.com>
2024-05-02 13:55:28 -0700

    A4A: highlight migration offer incentive

--- client/a8c-for-agencies/sections/referrals/primary/referrals-overview/index.tsx
+++ client/a8c-for-agencies/sections/referrals/primary/referrals-overview/index.tsx
@@ -1,6 +1,7 @@
 import { plugins, payment, percent } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import { useCallback } from 'react';
+import MigrationOffer from 'calypso/a8c-for-agencies/components/a4a-migration-offer';
 import Layout from 'calypso/a8c-for-agencies/components/layout';
 import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
 import LayoutHeader, {
@@ -118,6 +119,7 @@ export default function ReferralsOverview() {
 								heading={ translate( 'Earn commissions from your referrals' ) }
 								stepCount={ 2 }
 							>
+								<MigrationOffer />
 								<StepSectionItem
 									icon={ payment }
 									heading={ translate( 'Encourage your clients to purchase Automattic products' ) }
