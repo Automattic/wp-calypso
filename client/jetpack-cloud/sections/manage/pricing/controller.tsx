cc2935deed158ab7fd5539067f1c619081a398c0
Bryan Elliott <bryan@elliottprogrammer.com>
2024-05-02 12:04:39 -0400

    Change "Explore bulk pricing" link to "Jetpack for Agencies".

--- client/jetpack-cloud/sections/manage/pricing/controller.tsx
+++ client/jetpack-cloud/sections/manage/pricing/controller.tsx
@@ -1,6 +1,8 @@
 import { type Callback } from '@automattic/calypso-router';
 import ManagePricingPage from 'calypso/jetpack-cloud/sections/manage/pricing/primary';
 
+export const JETPACK_COM_A4A_LANDING_PAGE = 'https://jetpack.com/for-agencies/';
+
 export const jetpackManagePricingContext: Callback = ( context, next ) => {
 	context.primary = <ManagePricingPage />;
 
