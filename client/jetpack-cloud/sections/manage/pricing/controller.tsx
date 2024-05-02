39854cd19a031a5b6d1fe1a0ea04c3f2076233f4
Bryan Elliott <bryan@elliottprogrammer.com>
2024-05-02 14:07:42 -0400

    Minor clean up & add comments.

--- client/jetpack-cloud/sections/manage/pricing/controller.tsx
+++ client/jetpack-cloud/sections/manage/pricing/controller.tsx
@@ -2,7 +2,7 @@ import { type Callback } from '@automattic/calypso-router';
 import { getQueryArgs, addQueryArgs } from '@wordpress/url';
 import ManagePricingPage from 'calypso/jetpack-cloud/sections/manage/pricing/primary';
 
-const JETPACK_COM_A4A_LANDING_PAGE = 'https://jetpack.com/for-agencies/';
+export const JETPACK_COM_A4A_LANDING_PAGE = 'https://jetpack.com/for-agencies/';
 
 export const jetpackManagePricingContext: Callback = ( context, next ) => {
 	context.primary = <ManagePricingPage />;
@@ -12,8 +12,11 @@ export const jetpackManagePricingContext: Callback = ( context, next ) => {
 
 export const redirectToJetpackComA4aLanding: Callback = () => {
 	const args = getQueryArgs( window.location.href );
+	// The JETPACK_COM_A4A_LANDING_PAGE is only available in English at this time, so we
+	// won't worry about localizing the link for now. Although we may want to localize it
+	// in the future when/if the page gets translated & posted to other languages/locales.
 	const jetpackA4aLandingPage = addQueryArgs( JETPACK_COM_A4A_LANDING_PAGE, args );
 
-	window.location.href = jetpackA4aLandingPage;
+	window.location.assign( jetpackA4aLandingPage );
 	return;
 };
