53d75b161992d982d4d11c04489739ea6568f0de
Maciej Grabowski <littlesoldiergraba@gmail.com>
2024-05-02 10:39:28 +0200

    Segmentation Survey - Cache anon id for cases when it's cleared from the cookie after the user logs in but we still need it to link his answers

--- client/landing/stepper/declarative-flow/entrepreneur-flow.ts
+++ client/landing/stepper/declarative-flow/entrepreneur-flow.ts
@@ -1,5 +1,10 @@
+import { getTracksAnonymousUserId } from '@automattic/calypso-analytics';
 import { ENTREPRENEUR_FLOW } from '@automattic/onboarding';
 import { useSelect, useDispatch } from '@wordpress/data';
+import { useEffect } from 'react';
+import { anonIdCache } from 'calypso/data/segmentaton-survey';
+import { useSelector } from 'calypso/state';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { useFlowLocale } from '../hooks/use-flow-locale';
 import { USER_STORE, ONBOARD_STORE } from '../stores';
 import { getLoginUrl } from '../utils/path';
@@ -132,6 +137,23 @@ const entrepreneurFlow: Flow = {
 		}
 
 		return { submit };
+	},
+
+	useSideEffect() {
+		const isLoggedIn = useSelector( isUserLoggedIn );
+
+		useEffect( () => {
+			if ( isLoggedIn ) {
+				// Proactively remove the anonymous user ID from localStorage if the user is logged in.
+				anonIdCache.clear();
+				return;
+			}
+
+			// We need to store the anonymous user ID in localStorage because
+			// we need to pass it to the server on site creation, i.e. after the user signs up or logs in.
+			const anonymousUserId = getTracksAnonymousUserId();
+			anonIdCache.store( anonymousUserId );
+		}, [ isLoggedIn ] );
 	},
 };
 
