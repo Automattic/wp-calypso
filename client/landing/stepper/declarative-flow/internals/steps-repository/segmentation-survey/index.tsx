e1c0e3702644ea9d05338da8f123d8331d666d47
Paulo Cruz <paulo.cruz@automattic.com>
2024-05-03 13:55:13 -0300

    Handle custom navigation based on answers

--- client/landing/stepper/declarative-flow/internals/steps-repository/segmentation-survey/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/segmentation-survey/index.tsx
@@ -1,17 +1,53 @@
 import Main from 'calypso/components/main';
 import SegmentationSurvey from 'calypso/components/segmentation-survey';
-import type { Step } from '../../types';
+import { useCachedAnswers } from 'calypso/data/segmentaton-survey';
+import type { ProvidedDependencies, Step } from '../../types';
 import './style.scss';
 
 const SURVEY_KEY = 'entrepreneur-trial';
 
+type NavigationDecision = {
+	proceedWithNavigation: boolean;
+	providedDependencies: ProvidedDependencies;
+};
+
+const shouldNavigate = (
+	questionKey: string,
+	answerKeys: string[],
+	isLastQuestion?: boolean
+): NavigationDecision => {
+	return {
+		proceedWithNavigation: !! isLastQuestion,
+		providedDependencies: {},
+	};
+};
+
 const SegmentationSurveyStep: Step = ( { navigation } ) => {
+	const { clearAnswers } = useCachedAnswers( SURVEY_KEY );
+
+	const handleNext = ( questionKey: string, answerKeys: string[], isLastQuestion?: boolean ) => {
+		const { proceedWithNavigation, providedDependencies } = shouldNavigate(
+			questionKey,
+			answerKeys,
+			isLastQuestion
+		);
+
+		if ( proceedWithNavigation ) {
+			// For custom navigation, we need to clear the answers before the last question
+			if ( ! isLastQuestion ) {
+				clearAnswers();
+			}
+
+			navigation.submit?.( providedDependencies );
+		}
+	};
+
 	return (
 		<Main className="segmentation-survey-step">
 			<SegmentationSurvey
 				surveyKey={ SURVEY_KEY }
 				onBack={ navigation.goBack }
-				onComplete={ navigation.submit }
+				onNext={ handleNext }
 			/>
 		</Main>
 	);
