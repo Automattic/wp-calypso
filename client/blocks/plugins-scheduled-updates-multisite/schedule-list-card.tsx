2c9f61fa3e4be36af1674290f21b7c3192a65efa
Tim Broddin <tim@wannabes.be>
2024-05-03 14:21:59 +0200

    Mobile view

---
+++ client/blocks/plugins-scheduled-updates-multisite/schedule-list-card.tsx
@@ -0,0 +1,139 @@
+import { Button, Tooltip } from '@wordpress/components';
+import { chevronDown, chevronUp, Icon, info } from '@wordpress/icons';
+import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
+import { useDateTimeFormat } from 'calypso/blocks/plugin-scheduled-updates-common/hooks/use-date-time-format';
+import { usePrepareMultisitePluginsTooltipInfo } from 'calypso/blocks/plugin-scheduled-updates-common/hooks/use-prepare-plugins-tooltip-info';
+import { usePrepareScheduleName } from 'calypso/blocks/plugin-scheduled-updates-common/hooks/use-prepare-schedule-name';
+import { usePrepareSitesTooltipInfo } from 'calypso/blocks/plugins-scheduled-updates-multisite/hooks/use-prepare-sites-tooltip-info';
+import { ScheduleListLastRunStatus } from 'calypso/blocks/plugins-scheduled-updates-multisite/schedule-list-last-run-status';
+import { ScheduleListTableRowMenu } from 'calypso/blocks/plugins-scheduled-updates-multisite/schedule-list-table-row-menu';
+import { SiteSlug } from 'calypso/types';
+import type {
+	MultisiteSchedulesUpdates,
+	ScheduleUpdates,
+} from 'calypso/data/plugins/use-update-schedules-query';
+
+type Props = {
+	schedule: MultisiteSchedulesUpdates;
+	onEditClick: ( id: string ) => void;
+	onRemoveClick: ( id: string ) => void;
+	onLogsClick: ( id: string, siteSlug: SiteSlug ) => void;
+};
+
+export const ScheduleListCard = ( props: Props ) => {
+	const { schedule, onEditClick, onRemoveClick, onLogsClick } = props;
+
+	const { prepareSitesTooltipInfo } = usePrepareSitesTooltipInfo();
+	const { prepareScheduleName } = usePrepareScheduleName();
+	const { prepareDateTime } = useDateTimeFormat();
+	const { preparePluginsTooltipInfo, countInstalledPlugins } =
+		usePrepareMultisitePluginsTooltipInfo( schedule.sites.map( ( site ) => site.ID ) );
+	const translate = useTranslate();
+	const [ isExpanded, setIsExpanded ] = useState( false );
+
+	return (
+		<div className="plugins-update-manager-multisite-card">
+			<ScheduleListTableRowMenu
+				schedule={ schedule }
+				onEditClick={ onEditClick }
+				onRemoveClick={ onRemoveClick }
+				onLogsClick={ onLogsClick }
+				className="plugins-update-manager-multisite-card__actions"
+			/>
+
+			<div className="plugins-update-manager-multisite-card__label">
+				<strong id="name">
+					<Button
+						className="schedule-name"
+						variant="link"
+						onClick={ () => onEditClick && onEditClick( schedule.id ) }
+					>
+						{ prepareScheduleName( schedule as unknown as ScheduleUpdates ) }
+					</Button>
+				</strong>
+			</div>
+
+			<div className="plugins-update-manager-multisite-card__label">
+				<label htmlFor="sites">{ translate( 'Sites' ) }</label>
+				<strong id="sites">
+					{ schedule.sites.length }{ ' ' }
+					<Tooltip
+						text={ prepareSitesTooltipInfo( schedule.sites ) as unknown as string }
+						position="middle right"
+						delay={ 0 }
+						hideOnClick={ false }
+					>
+						<Icon className="icon-info" icon={ info } size={ 16 } />
+					</Tooltip>
+				</strong>
+			</div>
+
+			<div className="plugins-update-manager-multisite-card__label plugins-update-manager-multisite-card__last-update-label">
+				<label htmlFor="last-update">
+					{ translate( 'Last update' ) }
+					<Button variant="link" onClick={ () => setIsExpanded( ! isExpanded ) } size="small">
+						<Icon icon={ isExpanded ? chevronUp : chevronDown } />
+					</Button>
+				</label>
+				<button onClick={ () => setIsExpanded( ! isExpanded ) }>
+					<ScheduleListLastRunStatus schedule={ schedule } />
+				</button>
+			</div>
+
+			{ isExpanded && (
+				<div className="plugins-update-manager-multisite-card__sites">
+					{ schedule.sites.map( ( site ) => (
+						<div
+							key={ `${ schedule.schedule_id }.${ site.ID }` }
+							className="plugins-update-manager-multisite-card__sites-site"
+						>
+							<div className="plugins-update-manager-multisite-card__label">
+								<label htmlFor="name">{ translate( 'Name' ) }</label>
+								<strong id="name">{ site.title }</strong>
+							</div>
+							<div className="plugins-update-manager-multisite-card__label plugins-update-manager-multisite-card__last-update-label">
+								<label htmlFor="last-update">{ translate( 'Last update' ) }</label>
+								<div>
+									<ScheduleListLastRunStatus schedule={ schedule } site={ site } />
+								</div>
+							</div>
+						</div>
+					) ) }
+				</div>
+			) }
+
+			<div className="plugins-update-manager-multisite-card__label">
+				<label htmlFor="next-update">{ translate( 'Next update' ) }</label>
+				<span id="next-update">{ prepareDateTime( schedule.timestamp ) }</span>
+			</div>
+
+			<div className="plugins-update-manager-multisite-card__label">
+				<label htmlFor="frequency">{ translate( 'Frequency' ) }</label>
+				<span id="frequency">
+					{
+						{
+							daily: translate( 'Daily' ),
+							weekly: translate( 'Weekly' ),
+						}[ schedule.schedule ]
+					}
+				</span>
+			</div>
+
+			<div className="plugins-update-manager-multisite-card__label">
+				<label htmlFor="plugins">{ translate( 'Plugins' ) }</label>
+				<span id="plugins">
+					{ countInstalledPlugins( schedule.args ) }
+					<Tooltip
+						text={ preparePluginsTooltipInfo( schedule.args ) as unknown as string }
+						position="middle right"
+						delay={ 0 }
+						hideOnClick={ false }
+					>
+						<Icon className="icon-info" icon={ info } size={ 16 } />
+					</Tooltip>
+				</span>
+			</div>
+		</div>
+	);
+};
---
+++ client/blocks/plugins-scheduled-updates-multisite/schedule-list-table-row-menu.tsx
@@ -0,0 +1,55 @@
+import { DropdownMenu } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+import { ellipsis } from 'calypso/blocks/plugins-scheduled-updates/icons';
+import { SiteSlug } from 'calypso/types';
+import type {
+	MultisiteSchedulesUpdates,
+	MultisiteSiteDetails,
+} from 'calypso/data/plugins/use-update-schedules-query';
+
+type Props = {
+	schedule: MultisiteSchedulesUpdates;
+	onEditClick: ( id: string ) => void;
+	onRemoveClick: ( id: string ) => void;
+	onLogsClick: ( id: string, siteSlug: SiteSlug ) => void;
+};
+
+export const ScheduleListTableRowMenu = ( {
+	schedule,
+	site,
+	onEditClick,
+	onRemoveClick,
+	onLogsClick,
+	className,
+}: Props & { site?: MultisiteSiteDetails; className?: string } ) => {
+	const translate = useTranslate();
+
+	const items = [
+		{
+			title: translate( 'Edit' ),
+			onClick: () => onEditClick( schedule.id ),
+		},
+	];
+
+	if ( site ) {
+		items.push( {
+			title: translate( 'Logs' ),
+			onClick: () => onLogsClick( schedule.schedule_id, site.slug ),
+		} );
+	}
+
+	items.push( {
+		title: translate( 'Remove' ),
+		onClick: () => onRemoveClick( schedule.schedule_id ),
+	} );
+
+	return (
+		<DropdownMenu
+			className={ className }
+			popoverProps={ { position: 'bottom left' } }
+			controls={ items }
+			icon={ ellipsis }
+			label={ translate( 'More' ) }
+		/>
+	);
+};
