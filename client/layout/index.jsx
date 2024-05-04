d775943e18eea2a757024f0daed0b479e7c3041c
Eoin Gallagher <eoin@automattic.com>
2024-05-04 12:41:20 +0100

    Revert "Nav Redesign - fix color scheme switch when viewing GSV (#90291)"
    This reverts commit ec352294c58bb7fcec357554a1841d71e99edecc.

--- client/layout/index.jsx
+++ client/layout/index.jsx
@@ -222,20 +222,10 @@ class Layout extends Component {
 		this.refreshColorScheme( undefined, this.props.colorScheme );
 	}
 
-	/**
-	 * Refresh the color scheme if
-	 * - the color scheme has changed
-	 * - the global sidebar is visible and the color scheme is not `modern`
-	 * - the global sidebar was visible and is now hidden and the color scheme is not `modern`
-	 * @param prevProps object
-	 */
 	componentDidUpdate( prevProps ) {
 		if (
 			prevProps.colorScheme !== this.props.colorScheme ||
-			( this.props.isGlobalSidebarVisible && this.props.colorScheme !== 'modern' ) ||
-			( prevProps.isGlobalSidebarVisible &&
-				! this.props.isGlobalSidebarVisible &&
-				this.props.colorScheme !== 'modern' )
+			( this.props.isGlobalSidebarVisible && this.props.colorScheme !== 'modern' )
 		) {
 			this.refreshColorScheme( prevProps.colorScheme, this.props.colorScheme );
 		}
