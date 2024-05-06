2ece04dff71b056a3f820650932901352915f13f
Tim Broddin <tim@wannabes.be>
2024-05-06 10:10:34 +0200

    Add close button to edit/create

--- client/blocks/plugins-scheduled-updates-multisite/schedule-create.tsx
+++ client/blocks/plugins-scheduled-updates-multisite/schedule-create.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@wordpress/components';
+import { close, Icon } from '@wordpress/icons';
+import { useTranslate } from 'i18n-calypso';
 import { ScheduleForm } from './schedule-form';
 
 type Props = {
@@ -5,9 +8,15 @@ type Props = {
 };
 
 export const ScheduleCreate = ( { onNavBack }: Props ) => {
+	const translate = useTranslate();
 	return (
 		<div className="plugins-update-manager plugins-update-manager-multisite">
-			<h1 className="wp-brand-font">New schedule</h1>
+			<div className="plugins-update-manager-multisite__header no-border">
+				<h1 className="wp-brand-font">{ translate( 'New schedule' ) }</h1>
+				<Button onClick={ onNavBack }>
+					<Icon icon={ close } />
+				</Button>
+			</div>
 			<ScheduleForm onNavBack={ onNavBack } />
 		</div>
 	);
