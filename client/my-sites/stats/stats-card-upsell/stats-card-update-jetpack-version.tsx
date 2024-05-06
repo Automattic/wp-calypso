dbe54de81aa44b4a6c066dd83e61fb4b3a23401d
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2024-05-06 15:07:57 +1000

    Stats: Updating upgrade link.

--- client/my-sites/stats/stats-card-upsell/stats-card-update-jetpack-version.tsx
+++ client/my-sites/stats/stats-card-upsell/stats-card-update-jetpack-version.tsx
@@ -1,18 +1,18 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import config from '@automattic/calypso-config';
-import page from '@automattic/calypso-router';
 import { Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import React from 'react';
-import InlineSupportLink from 'calypso/components/inline-support-link';
 import { useSelector } from 'calypso/state';
 import getIsSiteWPCOM from 'calypso/state/selectors/is-site-wpcom';
+import { getSiteAdminUrl } from 'calypso/state/sites/selectors';
 import StatsCardUpsellOverlay from './stats-card-upsell-overlay';
 import { Props } from './';
 
 const StatsCardUpdateJetpackVersion: React.FC< Props > = ( { className, siteId, statType } ) => {
 	const translate = useTranslate();
 	const isOdysseyStats = config.isEnabled( 'is_running_in_jetpack_site' );
+	const siteAdminUrl = useSelector( ( state ) => getSiteAdminUrl( state, siteId ) );
 
 	const isWPCOMSite = useSelector( ( state ) => siteId && getIsSiteWPCOM( state, siteId ) );
 	const tracksEvent = 'update_jetpack_feature_card_clicked';
@@ -30,21 +30,14 @@ const StatsCardUpdateJetpackVersion: React.FC< Props > = ( { className, siteId, 
 		} );
 
 		// redirect to the Plugins page
-		setTimeout( () => page( `/wp-admin/plugins.php` ), 250 );
+		setTimeout( () => ( window.location.href = `${ siteAdminUrl }plugins.php` ), 250 );
 	};
 
 	return (
 		<StatsCardUpsellOverlay
 			className={ className }
 			onClick={ onClick }
-			copyText={ translate(
-				'Update your Jetpack plugin to see this feature. {{learnMoreLink}}Learn more{{/learnMoreLink}}',
-				{
-					components: {
-						learnMoreLink: <InlineSupportLink supportContext="stats" showIcon={ false } />,
-					},
-				}
-			) }
+			copyText={ translate( 'Update your Jetpack plugin to see this feature.' ) }
 			buttonComponent={
 				<Button className="jetpack-emerald-button" onClick={ onClick } primary>
 					{ translate( 'Update Jetpack plugin' ) }
