8bc24fb1bd2cd2038f137cc9000f2ce7d385a423
Matt Wiebe <wiebe@automattic.com>
2024-05-06 14:44:27 -0500

    translate new strings

--- client/my-sites/marketing/connections/services/fediverse.jsx
+++ client/my-sites/marketing/connections/services/fediverse.jsx
@@ -1,18 +1,20 @@
 import { Badge, FoldableCard } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
 import SocialLogo from 'calypso/components/social-logo';
 import { FediverseServiceSection } from 'calypso/my-sites/site-settings/fediverse-settings';
 
 function FediverseHeader() {
+	const translate = useTranslate();
 	return (
 		<div>
 			<SocialLogo icon="fediverse" size={ 48 } className="sharing-service__logo" />
 			<div class="sharing-service__name">
 				<h2>
-					Fediverse
-					<Badge className="service__new-badge">New</Badge>
+					{ translate( 'Fediverse' ) }
+					<Badge className="service__new-badge">{ translate( 'New' ) }</Badge>
 				</h2>
 				<p class="sharing-service__description">
-					Mastodon today, Threads tomorrow. Enter the Fediverse with ActivityPub.
+					{ translate( 'Mastodon today, Threads tomorrow. Enter the Fediverse with ActivityPub.' ) }
 				</p>
 			</div>
 		</div>
