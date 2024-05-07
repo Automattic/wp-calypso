9a3478c45b343397e746eb9c3aa989ff26da6366
Jeremy Yip <jeremy.yip@automattic.com>
2024-05-07 13:04:45 -0700

    Refactor useCanUserUpgradePlans hook

--- client/my-sites/plans-features-main/hooks/use-can-user-upgrade-plans.ts
+++ client/my-sites/plans-features-main/hooks/use-can-user-upgrade-plans.ts
@@ -2,26 +2,12 @@ import { useSelector } from 'react-redux';
 import { isCurrentUserCurrentPlanOwner } from 'calypso/state/sites/plans/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
-/**
- * TODO: Update docs
- * Returns a dictionary of plan slugs and whether or not they are available for purchase.
- * Note that:
- * - If there is not current-plan or selected-site defined, then plan is marked as purchasable.
- * This would be the equivalent of an Onboarding/Signup context, where no other conditions currently apply.
- * - If a selected site exists, then we check if the plan is available for purchase on that site.
- */
 const useCanUserUpgradePlans = () => {
 	const selectedSiteId = useSelector( getSelectedSiteId );
 
-	return useSelector( ( state ) => {
-		// TODO: Update this comment
-		// 1. No selected site: plan is in a context-free state and we assume the user is
-		if ( ! selectedSiteId ) {
-			return false;
-		}
-
-		return isCurrentUserCurrentPlanOwner( state, selectedSiteId );
-	} );
+	return useSelector( ( state ) =>
+		selectedSiteId ? isCurrentUserCurrentPlanOwner( state, selectedSiteId ) : false
+	);
 };
 
 export default useCanUserUpgradePlans;
