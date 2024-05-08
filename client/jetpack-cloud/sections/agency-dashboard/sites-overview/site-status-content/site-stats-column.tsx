cca9306048de6f23a50e5fcc13210bbac37cf2ca
Nate Weller <hello@nateweller.com>
2024-05-08 16:26:52 -0600

    Convert the stats dataview item to a clickable button that opens the preview pane

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/site-stats-column.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/site-stats-column.tsx
@@ -1,9 +1,16 @@
-import { ShortenedNumber } from '@automattic/components';
+import formatNumber from '@automattic/components/src/number-formatters/lib/format-number';
 import { Icon, arrowUp, arrowDown } from '@wordpress/icons';
 import classNames from 'classnames';
+import { translate } from 'i18n-calypso';
+import { useCallback, useContext } from 'react';
+import { DATAVIEWS_LIST } from 'calypso/a8c-for-agencies/components/items-dashboard/constants';
+import { DataViewsState } from 'calypso/a8c-for-agencies/components/items-dashboard/items-dataviews/interfaces';
+import SitesDashboardContext from 'calypso/a8c-for-agencies/sections/sites/sites-dashboard-context';
+import { type Site } from '../../sites-overview/types';
 import { SiteStats } from '../types';
 
 type Props = {
+	site: Site;
 	stats: SiteStats;
 };
 
@@ -18,12 +25,31 @@ const getTrendIcon = ( viewsTrend: 'up' | 'down' | 'same' ) => {
 	}
 };
 
-export default function SiteStatsColumn( { stats }: Props ) {
+export default function SiteStatsColumn( { site, stats }: Props ) {
+	const { setSelectedSiteFeature, setDataViewsState } = useContext( SitesDashboardContext );
 	const { total: totalViews, trend: viewsTrend } = stats.views;
 	const trendIcon = getTrendIcon( viewsTrend );
+
+	// Open the site's preview pane with the stats tab displayed.
+	const openStats = useCallback( () => {
+		setDataViewsState( ( prevState: DataViewsState ) => ( {
+			...prevState,
+			selectedItem: site,
+			type: DATAVIEWS_LIST,
+		} ) );
+		setSelectedSiteFeature( 'jetpack-stats' );
+	}, [ site, setSelectedSiteFeature, setDataViewsState ] );
+
 	return (
-		<span
+		<button
+			type="button"
+			title={
+				translate( '%(totalViews)s views in the last 7 days', { args: { totalViews } } ) as string
+			}
+			onClick={ openStats }
 			className={ classNames(
+				'button',
+				'is-borderless',
 				'sites-overview__stats-trend',
 				`sites-overview__stats-trend__${ viewsTrend }`
 			) }
@@ -31,8 +57,8 @@ export default function SiteStatsColumn( { stats }: Props ) {
 			{ trendIcon }
 
 			<div className="sites-overview__stats">
-				<ShortenedNumber value={ totalViews } />
+				<span className="shortened-number">{ formatNumber( totalViews ) }</span>
 			</div>
-		</span>
+		</button>
 	);
 }
