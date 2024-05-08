4288c7688a5ce0fe86a5cadce4e6280e5bc67a10
Wesley Berrêdo <wberredo@gmail.com>
2024-05-08 11:25:21 -0400

    use the offset calculation on the visibility hook

--- client/site-profiler/hooks/use-is-menu-section-visible.ts
+++ client/site-profiler/hooks/use-is-menu-section-visible.ts
@@ -1,23 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'calypso/state';
-import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { calculateMetricsSectionScrollOffset } from '../utils/calculate-metrics-section-scroll-offset';
 
 export function useIsMenuSectionVisible( ref: React.RefObject< HTMLObjectElement > | undefined ) {
 	const [ isIntersecting, setIntersecting ] = useState( false );
-	const isLoggedIn = useSelector( isUserLoggedIn );
-
-	// the additional pixel is used to automattically trigger the change when
-	// the top of the element is at the top of the viewport
-	const topbarHeight = 32;
-	const menuHeight = 64;
-	const totalTopMargin = menuHeight + ( isLoggedIn ? topbarHeight : 0 ) + 1;
 
 	useEffect( () => {
 		const observer = new IntersectionObserver(
 			( [ entry ] ) => {
 				setIntersecting( entry.isIntersecting );
 			},
-			{ rootMargin: `-${ totalTopMargin }px` }
+			{ rootMargin: `-${ calculateMetricsSectionScrollOffset() + 1 }px` } // the additional pixel triggers the tab change
 		);
 
 		ref?.current && observer.observe( ref.current );
@@ -25,7 +17,7 @@ export function useIsMenuSectionVisible( ref: React.RefObject< HTMLObjectElement
 		return () => {
 			observer.disconnect();
 		};
-	}, [ ref, totalTopMargin ] );
+	}, [ ref ] );
 
 	return isIntersecting;
 }
