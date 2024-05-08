60c9f64428898a820f44b0ce5f564265c9443cb5
Wesley Berrêdo <wberredo@gmail.com>
2024-05-08 11:53:46 -0400

    Fix type issue

--- client/site-profiler/components/basic-metrics.tsx
+++ client/site-profiler/components/basic-metrics.tsx
@@ -3,8 +3,6 @@ import { translate } from 'i18n-calypso';
 import { ForwardedRef, forwardRef } from 'react';
 import { BASIC_METRICS_UNITS } from 'calypso/data/site-profiler/metrics-dictionaries';
 import { calculateMetricsSectionScrollOffset } from 'calypso/site-profiler/utils/calculate-metrics-section-scroll-offset';
-import { useSelector } from 'calypso/state';
-import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import type { BasicMetrics as BasicMetricsType } from 'calypso/data/site-profiler/types';
 
 const Container = styled.div`
@@ -16,10 +14,8 @@ export const BasicMetrics = forwardRef(
 		{ basicMetrics }: { basicMetrics: BasicMetricsType },
 		ref: ForwardedRef< HTMLObjectElement >
 	) => {
-		const isLoggedIn = useSelector( isUserLoggedIn );
-
 		return (
-			<Container className="basic-metrics" ref={ ref } isLoggedIn={ isLoggedIn }>
+			<Container className="basic-metrics" ref={ ref }>
 				<h3>{ translate( 'Basic Performance Metrics' ) }</h3>
 				<ul className="basic-metric-details result-list">
 					{ Object.entries( basicMetrics ).map( ( [ key, value ] ) => {
