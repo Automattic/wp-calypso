9e5859c5d705a08931bfdaa69a044570c620149f
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-05-08 10:21:56 -0600

    Separate multi option components to their own files

--- client/my-sites/plans/jetpack-plans/product-lightbox/index.tsx
+++ client/my-sites/plans/jetpack-plans/product-lightbox/index.tsx
@@ -1,32 +1,23 @@
-import {
-	isJetpackPlanSlug,
-	JETPACK_RELATED_PRODUCTS_MAP,
-	isJetpackAISlug,
-} from '@automattic/calypso-products';
-import { Button, SelectDropdown } from '@automattic/components';
+import { isJetpackPlanSlug } from '@automattic/calypso-products';
+import { Button } from '@automattic/components';
 import { useBreakpoint } from '@automattic/viewport-react';
-import { useCallback, useEffect, useMemo } from 'react';
-import FormFieldset from 'calypso/components/forms/form-fieldset';
-import FormLegend from 'calypso/components/forms/form-legend';
+import { useCallback, useEffect, type FC } from 'react';
 import JetpackLightbox, {
 	JetpackLightboxAside,
 	JetpackLightboxMain,
 } from 'calypso/components/jetpack/jetpack-lightbox';
 import useMobileSidebar from 'calypso/components/jetpack/jetpack-lightbox/hooks/use-mobile-sidebar';
 import JetpackProductInfo from 'calypso/components/jetpack/jetpack-product-info';
-import MultipleChoiceQuestion from 'calypso/components/multiple-choice-question';
-import { getProductPartsFromAlias } from 'calypso/my-sites/checkout/src/hooks/use-prepare-products-for-cart';
 import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions/record';
 import { useStoreItemInfoContext } from '../product-store/context/store-item-info-context';
 import { PricingBreakdown } from '../product-store/pricing-breakdown';
 import { ProductStoreBaseProps } from '../product-store/types';
-import slugToSelectorProduct from '../slug-to-selector-product';
 import { Duration, SelectorProduct } from '../types';
-import useItemPrice from '../use-item-price';
-import { PRODUCT_OPTIONS, PRODUCT_OPTIONS_HEADER, PRODUCT_TIER_OPTIONS } from './constants';
 import PaymentPlan from './payment-plan';
 import './style.scss';
+import ProductSelect from './product-select';
+import QuantityDropdown from './quantity-dropdown';
 
 type Props = ProductStoreBaseProps & {
 	product: SelectorProduct;
@@ -37,47 +28,15 @@ type Props = ProductStoreBaseProps & {
 	siteId: number | null;
 };
 
-const ProductLightbox: React.FC< Props > = ( {
+const ProductLightbox: FC< Props > = ( {
 	product,
 	isVisible,
 	onClose,
 	onChangeProduct,
 	siteId,
 } ) => {
-	const listPrices = useItemPrice( siteId, product, product?.monthlyProductSlug || '' );
 	const close = useCallback( () => onClose?.(), [ onClose ] );
 	const dispatch = useDispatch();
-
-	const onChangeOption = useCallback(
-		( productSlug: string ) => {
-			onChangeProduct( slugToSelectorProduct( productSlug ) );
-
-			// Tracking when variant selected inside the lightbox
-			dispatch(
-				recordTracksEvent( 'calypso_product_lightbox_variant_select', {
-					site_id: siteId,
-					product_slug: productSlug,
-				} )
-			);
-		},
-		[ onChangeProduct, dispatch, siteId ]
-	);
-
-	const onDropdownTierSelect = useCallback(
-		( { value: slug }: { value: string } ) => {
-			onChangeProduct( slugToSelectorProduct( slug ) );
-			const { slug: productSlug, quantity } = getProductPartsFromAlias( slug );
-
-			dispatch(
-				recordTracksEvent( 'calypso_product_lightbox_dropdown_tier_select', {
-					site_id: siteId,
-					product_slug: productSlug,
-					quantity,
-				} )
-			);
-		},
-		[ onChangeProduct, dispatch, siteId ]
-	);
 
 	const {
 		getCheckoutURL,
@@ -110,37 +69,7 @@ const ProductLightbox: React.FC< Props > = ( {
 		);
 	}, [] ); // eslint-disable-line react-hooks/exhaustive-deps
 
-	const tierOptions = useMemo( () => {
-		if ( ! isJetpackAISlug( product.productSlug ) ) {
-			return [];
-		}
-
-		const tiers = listPrices.priceTierList || [];
-
-		return tiers.map( ( tier ) => {
-			const id = `${ product.productSlug }:-q-${ tier.maximum_units }`;
-
-			return {
-				value: id,
-				label: PRODUCT_TIER_OPTIONS[ id ].toString(),
-			};
-		} );
-	}, [ listPrices.priceTierList, product.productSlug ] );
-
-	const variantOptions = useMemo( () => {
-		const variants = JETPACK_RELATED_PRODUCTS_MAP[ product.productSlug ] || [];
-		return variants.map( ( itemSlug ) => ( {
-			id: itemSlug,
-			answerText: PRODUCT_OPTIONS[ itemSlug ].toString(),
-		} ) );
-	}, [ product.productSlug ] );
-
 	const { sidebarRef, mainRef, initMobileSidebar } = useMobileSidebar();
-
-	const shouldShowOptions = variantOptions.length > 1;
-
-	const shouldShowDropdown =
-		isJetpackAISlug( product.productSlug ) && tierOptions.length > 1 && ! shouldShowOptions;
 
 	const isMultiSiteIncompatible = isMultisite && ! getIsMultisiteCompatible( product );
 
@@ -174,33 +103,16 @@ const ProductLightbox: React.FC< Props > = ( {
 			<JetpackLightboxAside ref={ sidebarRef }>
 				<div className="product-lightbox__variants">
 					<div className="product-lightbox__variants-content">
-						{ shouldShowOptions && (
-							<div>
-								<div className="product-lightbox__variants-options">
-									<MultipleChoiceQuestion
-										name="product-variants"
-										question={ PRODUCT_OPTIONS_HEADER[ product?.productSlug ] }
-										answers={ variantOptions }
-										selectedAnswerId={ product?.productSlug }
-										onAnswerChange={ onChangeOption }
-										shouldShuffleAnswers={ false }
-									/>
-								</div>
-							</div>
-						) }
-						{ shouldShowDropdown && (
-							<div className="product-lightbox__variants-dropdown">
-								<FormFieldset>
-									<FormLegend>{ PRODUCT_OPTIONS_HEADER[ product?.productSlug ] }</FormLegend>
-									<SelectDropdown
-										className="product-lightbox__tiers-dropdown"
-										options={ tierOptions }
-										onSelect={ onDropdownTierSelect }
-										initialSelected={ tierOptions[ 0 ].value }
-									/>
-								</FormFieldset>
-							</div>
-						) }
+						<ProductSelect
+							product={ product }
+							siteId={ siteId }
+							onChangeProduct={ onChangeProduct }
+						/>
+						<QuantityDropdown
+							product={ product }
+							siteId={ siteId }
+							onChangeProduct={ onChangeProduct }
+						/>
 						{ ! isOwned && (
 							<PaymentPlan
 								isMultiSiteIncompatible={ isMultiSiteIncompatible }
---
+++ client/my-sites/plans/jetpack-plans/product-lightbox/quantity-dropdown.tsx
@@ -0,0 +1,77 @@
+import { isJetpackAISlug } from '@automattic/calypso-products';
+import { SelectDropdown } from '@automattic/components';
+import { useMemo, useCallback, type FC } from 'react';
+import FormFieldset from 'calypso/components/forms/form-fieldset';
+import FormLegend from 'calypso/components/forms/form-legend';
+import { getProductPartsFromAlias } from 'calypso/my-sites/checkout/src/hooks/use-prepare-products-for-cart';
+import { useDispatch } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions/record';
+import slugToSelectorProduct from '../slug-to-selector-product';
+import useItemPrice from '../use-item-price';
+import { PRODUCT_OPTIONS_HEADER, PRODUCT_TIER_OPTIONS } from './constants';
+import type { SelectorProduct } from '../types';
+
+interface QuantityDropdownProps {
+	product: SelectorProduct;
+	siteId: number | null;
+	onChangeProduct: ( product: SelectorProduct | null ) => void;
+}
+
+const QuantityDropdown: FC< QuantityDropdownProps > = ( { product, siteId, onChangeProduct } ) => {
+	const dispatch = useDispatch();
+	const listPrices = useItemPrice( siteId, product, product?.monthlyProductSlug || '' );
+	const isProductWithTierList = isJetpackAISlug( product.productSlug );
+
+	const tierOptions = useMemo( () => {
+		if ( ! isJetpackAISlug( product.productSlug ) ) {
+			return [];
+		}
+
+		const tiers = listPrices.priceTierList || [];
+
+		return tiers.map( ( tier ) => {
+			const id = `${ product.productSlug }:-q-${ tier.maximum_units }`;
+
+			return {
+				value: id,
+				label: PRODUCT_TIER_OPTIONS[ id ].toString(),
+			};
+		} );
+	}, [ listPrices.priceTierList, product.productSlug ] );
+
+	const onDropdownTierSelect = useCallback(
+		( { value: slug }: { value: string } ) => {
+			onChangeProduct( slugToSelectorProduct( slug ) );
+			const { slug: productSlug, quantity } = getProductPartsFromAlias( slug );
+
+			dispatch(
+				recordTracksEvent( 'calypso_product_lightbox_dropdown_tier_select', {
+					site_id: siteId,
+					product_slug: productSlug,
+					quantity,
+				} )
+			);
+		},
+		[ onChangeProduct, dispatch, siteId ]
+	);
+
+	if ( ! isProductWithTierList || tierOptions.length < 1 ) {
+		return <></>;
+	}
+
+	return (
+		<div className="product-lightbox__variants-dropdown">
+			<FormFieldset>
+				<FormLegend>{ PRODUCT_OPTIONS_HEADER[ product?.productSlug ] }</FormLegend>
+				<SelectDropdown
+					className="product-lightbox__tiers-dropdown"
+					options={ tierOptions }
+					onSelect={ onDropdownTierSelect }
+					initialSelected={ tierOptions[ 0 ].value }
+				/>
+			</FormFieldset>
+		</div>
+	);
+};
+
+export default QuantityDropdown;
