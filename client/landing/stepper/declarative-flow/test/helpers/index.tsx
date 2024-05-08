57d9fffafbb99484a00342230d176b767ab94eb2
Dale du Preez <dale@automattic.com>
2024-05-08 19:28:35 +0200

    Include calypso_signup_start tests; clean up

--- client/landing/stepper/declarative-flow/test/helpers/index.tsx
+++ client/landing/stepper/declarative-flow/test/helpers/index.tsx
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { act, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React, { useEffect } from 'react';
 import { MemoryRouter, useNavigate, useLocation } from 'react-router';
 import themeReducer from 'calypso/state/themes/reducer';
@@ -12,12 +12,25 @@ import type { Flow, ProvidedDependencies } from '../../internals/types';
 export const getFlowLocation = () => {
 	return {
 		path: screen.getByTestId( 'pathname' ).textContent,
+		search: screen.getByTestId( 'search' ).textContent,
 		state: JSON.parse( screen.getByTestId( 'state' ).textContent || '{}' ),
 	};
 };
 
 export const getAssertionConditionResult = () => {
 	return JSON.parse( screen.getByTestId( 'assertionConditionResult' ).textContent || '{}' );
+};
+
+const DebugLocation = () => {
+	const location = useLocation();
+
+	return (
+		<>
+			<p data-testid="pathname">{ `${ location.pathname }${ location.search }` }</p>
+			<p data-testid="search">{ location.search }</p>
+			<p data-testid="state">{ JSON.stringify( location.state ) }</p>
+		</>
+	);
 };
 
 interface RenderFlowParams {
@@ -31,7 +44,6 @@ interface RenderFlowParams {
 export const renderFlow = ( flow: Flow ) => {
 	const FakeStepRender = ( { currentStep, dependencies, method } ) => {
 		const navigate = useNavigate();
-		const location = useLocation();
 		const fakeNavigate = ( pathname, state ) => navigate( pathname, { state } );
 		const flowSteps = flow.useSteps();
 		const effectiveStep = currentStep ?? flowSteps[ 0 ].slug;
@@ -53,9 +65,7 @@ export const renderFlow = ( flow: Flow ) => {
 
 		return (
 			<>
-				<p data-testid="pathname">{ `${ location.pathname }${ location.search }` }</p>
-				<p data-testid="search">{ location.search }</p>
-				<p data-testid="state">{ JSON.stringify( location.state ) }</p>
+				<DebugLocation />
 				{ assertionConditionResult && (
 					<p data-testid="assertionConditionResult">
 						{ JSON.stringify( assertionConditionResult ) }
@@ -107,12 +117,49 @@ export const renderFlow = ( flow: Flow ) => {
 	};
 };
 
+type FlowWrapperProps = {
+	currentURL: string;
+	flow: Flow;
+	preferredStepIndex?: number;
+};
+
+const FlowWrapper = ( { currentURL, flow, preferredStepIndex }: FlowWrapperProps ) => {
+	let resolvedURL = currentURL;
+
+	if ( preferredStepIndex ) {
+		const steps = flow.useSteps();
+
+		const targetStep = steps[ preferredStepIndex ] ?? steps[ preferredStepIndex - 1 ];
+		if ( targetStep ) {
+			const url = new URL( currentURL, 'https://example.com' );
+			resolvedURL = `${ url.pathname }/${ targetStep.slug }${ url.search }`;
+		}
+	}
+
+	return (
+		<MemoryRouter basename="setup" initialEntries={ [ resolvedURL ] }>
+			<FlowRenderer flow={ flow } />
+			<DebugLocation />
+		</MemoryRouter>
+	);
+};
+
+type RenderRootProps = {
+	currentURL: string;
+	preferredStepIndex?: number;
+};
+
 export const renderFlowRoot = ( flow: Flow ) => {
-	const render = ( { currentURL = '/some-path?siteSlug=example.wordpress.com' } ) => {
+	const render = ( {
+		currentURL = '/some-path?siteSlug=example.wordpress.com',
+		preferredStepIndex,
+	}: RenderRootProps ) => {
 		renderWithProvider(
-			<MemoryRouter initialEntries={ [ currentURL ] }>
-				<FlowRenderer flow={ flow } />
-			</MemoryRouter>,
+			<FlowWrapper
+				currentURL={ currentURL }
+				flow={ flow }
+				preferredStepIndex={ preferredStepIndex }
+			/>,
 			{
 				initialState: { themes: { queries: [] }, currentUser: { id: 'some-id' } },
 				reducers: {
@@ -122,14 +169,7 @@ export const renderFlowRoot = ( flow: Flow ) => {
 		);
 	};
 
-	const renderWithAct = ( { currentURL = '/some-path?siteSlug=example.wordpress.com' } ) => {
-		act( () => {
-			render( { currentURL } );
-		} );
-	};
-
 	return {
 		render,
-		renderWithAct,
 	};
 };
--- client/landing/stepper/declarative-flow/test/signup-tracks-events.tsx
+++ client/landing/stepper/declarative-flow/test/signup-tracks-events.tsx
@@ -3,9 +3,9 @@
  */
 import { isEnabled } from '@automattic/calypso-config';
 import { isCurrentUserLoggedIn } from '@automattic/data-stores/src/user/selectors';
+import { waitFor } from '@testing-library/react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Modal from 'react-modal';
-import { useLocation } from 'react-router';
 import { recordSignupStart } from 'calypso/lib/analytics/signup';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { triggerGuidesForStep } from 'calypso/lib/guides/trigger-guides-for-step';
@@ -13,6 +13,8 @@ import { triggerGuidesForStep } from 'calypso/lib/guides/trigger-guides-for-step
 import { logToLogstash } from 'calypso/lib/logstash';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { getDocumentHeadFormattedTitle } from 'calypso/state/document-head/selectors/get-document-head-formatted-title';
+import { getAvailableProductsList } from 'calypso/state/products-list/selectors';
+import { getSelectedSite } from 'calypso/state/ui/selectors';
 import availableFlows from '../registered-flows';
 import { renderFlow, renderFlowRoot } from './helpers';
 import './helpers/to-match-path-and-search-params';
@@ -24,13 +26,6 @@ const originalScrollTo = window.scrollTo;
 
 jest.mock( '@automattic/data-stores/src/user/selectors' );
 jest.mock( 'react-modal' );
-jest.mock( 'react-router', () => {
-	const originalModule = jest.requireActual( 'react-router' );
-	return {
-		...originalModule,
-		useLocation: jest.fn(),
-	};
-} );
 jest.mock( 'calypso/lib/analytics/signup' );
 jest.mock( 'calypso/lib/guides/trigger-guides-for-step' );
 jest.mock( 'calypso/lib/logstash' );
@@ -42,20 +37,45 @@ jest.mock( 'calypso/state/current-user/selectors', () => {
 	};
 } );
 jest.mock( 'calypso/state/document-head/selectors/get-document-head-formatted-title' );
+jest.mock( 'calypso/state/imports/url-analyzer/selectors' );
+jest.mock( 'calypso/state/products-list/selectors' );
+jest.mock( 'calypso/state/ui/selectors' );
 
 const expectedLoginPath = `/start/account/${
 	isEnabled( 'signup/social-first' ) ? 'user-social' : 'user'
 }`;
 
+/**
+ * Flows that have a specific step that needs to be loaded before we redirect to a login.
+ * This is most common for flows with an intro step.
+ */
 const FLOWS_WITH_SPECIFIC_STEPS_FOR_LOGIN: Record< string, string > = {
 	newsletter: 'newsletterSetup',
 	videopress: 'videomakerSetup',
 };
 
+/**
+ * Flows that require a feature flag to be enabled for the flow to work.
+ */
 const FLOWS_BEHIND_FEATURE_FLAG: Record< string, boolean > = {
 	'ai-assembler': isEnabled( 'calypso/ai-assembler' ),
 };
 
+/**
+ * Flows that perform redirects to the user/login flow via a mechanism that we can't test yet.
+ */
+const FLOWS_WITH_ALT_LOGIN_REDIRECTS = [
+	'entrepreneur', // TODO: redirect happens from submission
+	'import-focused', // TODO: no login redirects
+	'link-in-bio', // TODO: redirect happens from submission
+	'link-in-bio-domain', // TODO: redirect happens from submission
+	'link-in-bio-tld', // TODO: redirect happens from submission
+	'videopress', // TODO: redirect happens from submission
+];
+
+/**
+ * Flows that redirect to an alternative user/login flow.
+ */
 const FLOWS_WITH_ALTERNATE_USER_PATH: Record< string, string > = {
 	videopress: '/start/videopress-account/user',
 };
@@ -66,6 +86,14 @@ type AlternateLoggedOutDestination = {
 	expectSearchParams?: boolean;
 };
 
+/**
+ * Helper function to identify a non-standard target path for logged-out users.
+ * In the long term, we want to minimise exceptions of this kind.
+ *
+ * @param flowKey The flow name/variation.
+ * @param locale The current locale.
+ * @returns The alternate path for logged-out users, or null if the default path should be used.
+ */
 const getAlternateLoggedOutDestination = ( flowKey: string, locale?: string ): string | null => {
 	const FLOWS_WITH_ALTERNATE_LOGGED_OUT_DESTINATIONS: Record<
 		string,
@@ -109,6 +137,10 @@ const getAlternateLoggedOutDestination = ( flowKey: string, locale?: string ): s
 	return expect.stringMatching( new RegExp( `^${ path }/${ locale }[^/]*` ) );
 };
 
+/* ===================================
+   Identify and load all Stepper flows
+   ===================================
+ */
 const flowKeys = Object.keys( availableFlows ).filter(
 	( flowKey ) => FLOWS_BEHIND_FEATURE_FLAG[ flowKey ] ?? true
 );
@@ -117,6 +149,29 @@ async function loadAllFlows() {
 	return Promise.all( flowKeys.map( ( flowKey ) => availableFlows[ flowKey ]() ) );
 }
 
+/**
+ * Initialise any flow-specific mocks. The goal is to make it easy to know which flows require
+ * specific mocks/functions, rather than having them all polluting a central function.
+ *
+ * @param flow
+ */
+function initMocksForRootRender( flow: Flow ) {
+	switch ( flow.name ) {
+		case 'link-in-bio':
+			( getSelectedSite as jest.Mock ).mockReturnValue( null );
+			( getAvailableProductsList as jest.Mock ).mockReturnValue( {} );
+			break;
+	}
+}
+
+/**
+ * The purpose of these tests is to dynamically ensure that all Stepper flows
+ * that are flagged with isSignupFlow: true obey some basic requirements of signup flows,
+ * including the following:
+ *  - When redirecting to the login page, ensure that toStepper=true is included as a URL parameter
+ *  - When a flow is loaded using an alternative locale, ensure that the login redirect includes that locale
+ *
+ */
 describe( 'Stepper signup flows', () => {
 	const signupFlows: Record< string, Flow > = {};
 
@@ -141,7 +196,6 @@ describe( 'Stepper signup flows', () => {
 
 	afterAll( () => {
 		Object.defineProperty( window, 'location', originalLocation );
-		( useLocation as jest.Mock ).mockClear();
 		( window.scrollTo as jest.Mock ).mockClear();
 
 		Object.defineProperty( window, 'scrollTo', originalScrollTo );
@@ -150,28 +204,26 @@ describe( 'Stepper signup flows', () => {
 	beforeEach( () => {
 		( window.location.assign as jest.Mock ).mockClear();
 		window.location.search = '';
-		( useLocation as jest.Mock ).mockReturnValue( window.location );
 		( getDocumentHeadFormattedTitle as jest.Mock ).mockReturnValue( 'Test Title' );
+		( recordSignupStart as jest.Mock ).mockClear();
 	} );
 
+	/*
+	 * Loop over known flows and create tests.
+	 * Note that for now we skip non-signup flows within the tests because
+	 * we only load them all in beforeAll() - jest requires the initial test setup
+	 * to be defined synchronosusly.
+	 */
 	flowKeys.forEach( ( flowKey ) => {
-		const FLOWS_TO_SKIP = [
-			'entrepreneur', // TODO: redirect happens from submission
-			'import-focused', // TODO: no login redirects
-			'link-in-bio', // TODO: redirect happens from submission
-			'link-in-bio-domain', // TODO: redirect happens from submission
-			'link-in-bio-tld', // TODO: redirect happens from submission
-			'videopress', // TODO: redirect happens from submission
-		];
-		if ( FLOWS_TO_SKIP.includes( flowKey ) ) {
-			return;
-		}
-
 		describe( `flow: ${ flowKey }`, () => {
 			const flowPath = flowKey;
 
 			it( 'redirects the user to the login page when they are not logged in', () => {
 				if ( ! signupFlows[ flowKey ] ) {
+					return;
+				}
+				// We can't test flows with alternate redirect mechanisms yet.
+				if ( FLOWS_WITH_ALT_LOGIN_REDIRECTS.includes( flowKey ) ) {
 					return;
 				}
 
@@ -211,11 +263,6 @@ describe( 'Stepper signup flows', () => {
 							variationName: flowPath,
 							redirect_to: expect.stringContaining( `/setup/${ flowPath }` + flowSubPath ),
 							toStepper: 'true',
-							/*
-							vid: 'vid321',
-							aff: 'aff123',
-							ref: 'logged-out-homepage',
-							*/
 						}
 					)
 				);
@@ -223,6 +270,10 @@ describe( 'Stepper signup flows', () => {
 
 			it( 'redirects the user to the login page with the correct locale when they are not logged in', () => {
 				if ( ! signupFlows[ flowKey ] ) {
+					return;
+				}
+				// We can't test flows with alternate redirect mechanisms yet.
+				if ( FLOWS_WITH_ALT_LOGIN_REDIRECTS.includes( flowKey ) ) {
 					return;
 				}
 
@@ -268,18 +319,12 @@ describe( 'Stepper signup flows', () => {
 								)
 							),
 							toStepper: 'true',
-							/*
-							vid: 'vid321',
-							aff: 'aff123',
-							ref: 'logged-out-homepage',
-							*/
 						}
 					)
 				);
 			} );
 
-			// This test is not working yet for logged-in or logged-out users.
-			it.skip( 'triggers a signup start event when loading the flow for a logged-in user', () => {
+			it( 'triggers a calypso_signup_start event when loading a new flow for a logged-in user', () => {
 				if ( ! signupFlows[ flowKey ] ) {
 					return;
 				}
@@ -288,6 +333,7 @@ describe( 'Stepper signup flows', () => {
 
 				( isCurrentUserLoggedIn as jest.Mock ).mockReturnValue( true );
 				( isUserLoggedIn as jest.Mock ).mockReturnValue( true );
+				initMocksForRootRender( flow );
 
 				const searchValue = '?ref=logged-out-homepage&aff=aff123&vid=vid321&locale=fr';
 				window.location.search = searchValue;
@@ -297,14 +343,89 @@ describe( 'Stepper signup flows', () => {
 				window.location.href = currentURL;
 				window.location.pathname = `/setup/${ flowPath }`;
 
-				const { renderWithAct } = renderFlowRoot( flow );
-				renderWithAct( { currentURL } );
+				const { render } = renderFlowRoot( flow );
+				render( { currentURL } );
 
-				expect( recordSignupStart ).toHaveBeenCalledWith(
-					flowPath,
-					'logged-out-homepage',
-					expect.any( Object )
+				waitFor( () =>
+					expect( recordSignupStart ).toHaveBeenCalledWith(
+						flow.name, // Note that we ignore the variant name, which is used in the path
+						'logged-out-homepage',
+						expect.any( Object )
+					)
 				);
+			} );
+
+			it( 'triggers a calypso_signup_start event when loading a new flow for a logged-out user', () => {
+				if ( ! signupFlows[ flowKey ] ) {
+					return;
+				}
+
+				const flow = signupFlows[ flowKey ];
+
+				( isCurrentUserLoggedIn as jest.Mock ).mockReturnValue( false );
+				( isUserLoggedIn as jest.Mock ).mockReturnValue( false );
+				initMocksForRootRender( flow );
+
+				const searchValue = '?ref=logged-out-homepage&aff=aff123&vid=vid321&locale=fr';
+				window.location.search = searchValue;
+
+				const currentURL = `/setup/${ flowPath }${ searchValue }`;
+
+				window.location.href = currentURL;
+				window.location.pathname = `/setup/${ flowPath }`;
+
+				const { render } = renderFlowRoot( flow );
+				render( { currentURL } );
+
+				waitFor( () =>
+					expect( recordSignupStart ).toHaveBeenCalledWith(
+						flow.name,
+						'logged-out-homepage',
+						expect.any( Object )
+					)
+				);
+			} );
+
+			it( 'does not trigger a calypso_signup_start event when loading a later step in a flow', () => {
+				if ( ! signupFlows[ flowKey ] ) {
+					return;
+				}
+
+				// The components we use to render the flow root need to get the steps
+				// before we set up a Router component, so we're not allowed to call useLocation()
+				// from that context.
+				const FLOWS_WITH_LOCATION_HOOKS_IN_USE_STEPS = [ 'newsletter' ];
+
+				if ( FLOWS_WITH_LOCATION_HOOKS_IN_USE_STEPS.includes( flowKey ) ) {
+					return;
+				}
+
+				const flow = signupFlows[ flowKey ];
+
+				( isCurrentUserLoggedIn as jest.Mock ).mockReturnValue( true );
+				( isUserLoggedIn as jest.Mock ).mockReturnValue( true );
+				initMocksForRootRender( flow );
+
+				const searchValue = '?ref=logged-out-homepage&aff=aff123&vid=vid321&locale=fr';
+				window.location.search = searchValue;
+
+				const currentURL = `/setup/${ flowPath }${ searchValue }`;
+
+				window.location.href = currentURL;
+				window.location.pathname = `/setup/${ flowPath }`;
+
+				// Some flows have internal navigation logic that results in step 2
+				// redirecting to the initial step, so we want a way to override the default of 2.
+				const PREFERRED_STEP_INDEX_FOR_FLOWS = {
+					'import-focused': 1,
+				};
+
+				const preferredStepIndex = PREFERRED_STEP_INDEX_FOR_FLOWS[ flow.name ] ?? 2;
+
+				const { render } = renderFlowRoot( flow );
+				render( { currentURL, preferredStepIndex } );
+
+				waitFor( () => expect( recordSignupStart ).not.toHaveBeenCalled() );
 			} );
 		} );
 	} );
