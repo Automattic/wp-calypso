6cd758498e07ecbd14fca670ffdfe2cff4b4b1e2
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-05-09 15:08:25 +0200

    Improve condition for applying proper global navigation styles

--- client/state/global-sidebar/selectors.ts
+++ client/state/global-sidebar/selectors.ts
@@ -64,7 +64,7 @@ export const getShouldShowCollapsedGlobalSidebar = (
 		isAllowedRegion &&
 		( siteSelected ||
 			siteLoaded ||
-			( ! siteSelected && pluginsScheduledUpdatesEditMode ) ||
+			( ! siteId && pluginsScheduledUpdatesEditMode ) ||
 			isWithinBreakpoint( '<782px' ) )
 	);
 };
