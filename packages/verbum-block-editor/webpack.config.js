85ee8e1c593478a097d1cf3c93ae3432df551cde
Omar Alshaker <omar@omaralshaker.com>
2024-05-09 17:31:19 +0200

    Simplify the fix

--- packages/verbum-block-editor/webpack.config.js
+++ packages/verbum-block-editor/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require( 'path' );
 const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config.js' );
+const GenerateChunksMapPlugin = require( '../../build-tools/webpack/generate-chunks-map-plugin' );
 
 /* Arguments to this function replicate webpack's so this config can be used on the command line,
  * with individual options overridden by command line args.
@@ -22,6 +23,11 @@ function getWebpackConfig( env = { source: '' }, argv = {} ) {
 		entry: {
 			'block-editor': path.join( __dirname, 'src', 'index.ts' ),
 		},
+		optimization: {
+			...webpackConfig.optimization,
+			// disable module concatenation so that instances of `__()` are not renamed
+			concatenateModules: false,
+		},
 		output: {
 			...webpackConfig.output,
 			path: outputPath,
@@ -34,6 +40,12 @@ function getWebpackConfig( env = { source: '' }, argv = {} ) {
 			filename: '[name].min.js', // dynamic filename
 			library: 'verbumBlockEditor',
 		},
+		plugins: [
+			...webpackConfig.plugins,
+			new GenerateChunksMapPlugin( {
+				output: path.resolve( __dirname, 'dist/chunks-map.json' ),
+			} ),
+		],
 	};
 }
 
