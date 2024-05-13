5ba9386f56dd8a6ac2c310b65c08721a732f5610
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2024-05-13 17:35:25 +0300

    Translate BadgeNew label

--- packages/components/src/horizontal-bar-list/sideElements/badge-new/index.tsx
+++ packages/components/src/horizontal-bar-list/sideElements/badge-new/index.tsx
@@ -1,11 +1,16 @@
+import { useTranslate } from 'i18n-calypso';
 import { Badge } from '../../../.';
 
 import './style.scss';
 
-const BadgeNew = () => (
-	<Badge type="success" className="stats-card__badge--success">
-		New
-	</Badge>
-);
+const BadgeNew = () => {
+	const translate = useTranslate();
+
+	return (
+		<Badge type="success" className="stats-card__badge--success">
+			{ translate( 'New' ) }
+		</Badge>
+	);
+};
 
 export default BadgeNew;
