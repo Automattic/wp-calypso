20104cda764db14a9a76c2f7a9ada0f601008e77
Candy Tsai <candy02058912@gmail.com>
2024-05-13 20:52:59 +0800

    Update Headers for site management panel

--- client/hosting-overview/components/hosting-overview.tsx
+++ client/hosting-overview/components/hosting-overview.tsx
@@ -1,12 +1,20 @@
+import { translate } from 'i18n-calypso';
 import { FC } from 'react';
+import NavigationHeader from 'calypso/components/navigation-header';
 import ActiveDomainsCard from 'calypso/hosting-overview/components/active-domains-card';
 import PlanCard from 'calypso/hosting-overview/components/plan-card';
 import QuickActionsCard from 'calypso/hosting-overview/components/quick-actions-card';
+
 import './style.scss';
 
 const HostingOverview: FC = () => {
 	return (
 		<div className="hosting-overview">
+			<NavigationHeader
+				className="hosting-overview__navigation-header"
+				title={ translate( 'Overview' ) }
+				subtitle={ translate( 'Get a quick glance at your plans, storage, and domains.' ) }
+			/>
 			<PlanCard />
 			<QuickActionsCard />
 			<ActiveDomainsCard />
