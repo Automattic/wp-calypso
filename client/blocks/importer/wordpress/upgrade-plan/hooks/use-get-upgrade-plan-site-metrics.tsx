f24a862b254761aaaa141a971f56739512a5712f
Anthony Grullon <agrullon95@gmail.com>
2024-05-13 11:20:31 -0400

    DIsplay speed changes for english locale

--- client/blocks/importer/wordpress/upgrade-plan/hooks/use-get-upgrade-plan-site-metrics.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/hooks/use-get-upgrade-plan-site-metrics.tsx
@@ -1,14 +1,17 @@
+import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { getQueryArg } from '@wordpress/url';
 import { useUrlBasicMetricsQuery } from 'calypso/data/site-profiler/use-url-basic-metrics-query';
 import { upgradePlanSiteMetricsLcpThreshold } from '../constants';
 
 export const useGetUpgradePlanSiteMetrics = () => {
+	const isEnglishLocale = useIsEnglishLocale();
 	const importSiteQueryParam = getQueryArg( window.location.href, 'from' )?.toString() || '';
 	const { data: siteMetricData } = useUrlBasicMetricsQuery( importSiteQueryParam );
 	let showUpdatedSpeedMetrics = false;
 	let lcpPercentageDifference = 0;
 
 	if (
+		isEnglishLocale &&
 		siteMetricData?.basic?.lcp &&
 		siteMetricData?.basic?.lcp > upgradePlanSiteMetricsLcpThreshold
 	) {
