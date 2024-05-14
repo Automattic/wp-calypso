50aa0ac9e7d9afa15b23372a5610b1fff141b9b2
Marin Atanasov <tyxla@abv.bg>
2024-05-14 16:34:40 +0300

    Try introducing an ESLint error

--- client/me/account/close-link.jsx
+++ client/me/account/close-link.jsx
@@ -1,5 +1,6 @@
 import { CompactCard } from '@automattic/components';
 import { localize } from 'i18n-calypso';
+import { translate } from 'i18n-calypso';
 import { Component } from 'react';
 
 import './close-link.scss';
