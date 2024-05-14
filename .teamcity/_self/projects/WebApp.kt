3162489b5da9ddae99f1c617b75115ccbc366bb4
Marin Atanasov <tyxla@abv.bg>
2024-05-14 16:42:20 +0300

    Revert "Try without failOnMetricChange"
    This reverts commit 057bde26143bc61e409526ff0501dd609e9c7eed.

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -614,6 +614,14 @@ object CheckCodeStyleBranch : BuildType({
 	failureConditions {
 		executionTimeoutMin = 20
 		nonZeroExitCode = false
+		failOnMetricChange {
+			metric = BuildFailureOnMetric.MetricType.INSPECTION_ERROR_COUNT
+			units = BuildFailureOnMetric.MetricUnit.DEFAULT_UNIT
+			comparison = BuildFailureOnMetric.MetricComparison.MORE
+			compareTo = build {
+				buildRule = lastSuccessful(branch = "trunk")
+			}
+		}
 	}
 
 	features {
