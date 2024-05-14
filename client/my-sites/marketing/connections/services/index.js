977e6ba52b9105bb8d834cc9b943883aee726803
Matt Wiebe <wiebe@automattic.com>
2024-05-14 15:52:51 -0500

    Fediverse: add to Connections page (#90339)
    Artificially adds a Fediverse section to the Connections page, just above Twitter

--- client/my-sites/marketing/connections/services/index.js
+++ client/my-sites/marketing/connections/services/index.js
@@ -3,6 +3,7 @@ export { default as google_photos } from './google-photos';
 export { default as google_drive } from './google-drive';
 export { default as google_my_business } from './google-my-business';
 export { default as facebook } from './facebook';
+export { default as fediverse } from './fediverse';
 export { default as instagram_business } from './instagram-business';
 export { default as mastodon } from './mastodon';
 export { default as mailchimp } from './mailchimp';
@@ -13,6 +14,7 @@ export { default as nextdoor } from './nextdoor';
 const services = new Set( [
 	'p2_github',
 	'p2_slack',
+	'fediverse',
 	'facebook',
 	'instagram_business',
 	'mastodon',
