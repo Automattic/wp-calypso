1b7b6e26cf96f89a1e72efc70ea2a8eda4621f33
Renatho De Carli Rosa <renatho@gmail.com>
2024-05-14 16:44:10 -0300

    Add FID percentage difference to the site metrics

--- client/blocks/importer/wordpress/upgrade-plan/hooks/use-get-upgrade-plan-site-metrics.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/hooks/use-get-upgrade-plan-site-metrics.tsx
@@ -1,33 +1,42 @@
 import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { getQueryArg } from '@wordpress/url';
 import { useUrlBasicMetricsQuery } from 'calypso/data/site-profiler/use-url-basic-metrics-query';
-import { upgradePlanSiteMetricsLcpThreshold } from '../constants';
+import {
+	upgradePlanSiteMetricsLcpThreshold,
+	upgradePlanSiteMetricsFidThreshold,
+} from '../constants';
+
+const calcPercentageDifferenceFromThreshold = ( value: number | undefined, threshold: number ) => {
+	if ( ! value || value <= threshold ) {
+		return 0;
+	}
+
+	return Math.round( 100 * Math.abs( ( value - threshold ) / ( ( value + threshold ) / 2 ) ) );
+};
 
 export const useGetUpgradePlanSiteMetrics = () => {
 	const isEnglishLocale = useIsEnglishLocale();
 	const importSiteQueryParam = getQueryArg( window.location.href, 'from' )?.toString() || '';
 	const { data: siteMetricData } = useUrlBasicMetricsQuery( importSiteQueryParam );
-	let lcpPercentageDifference = 0;
 
-	if (
-		isEnglishLocale &&
-		siteMetricData?.basic?.lcp &&
-		siteMetricData?.basic?.lcp > upgradePlanSiteMetricsLcpThreshold
-	) {
-		lcpPercentageDifference =
-			( siteMetricData?.basic?.lcp &&
-				Math.round(
-					100 *
-						Math.abs(
-							( siteMetricData?.basic?.lcp - upgradePlanSiteMetricsLcpThreshold ) /
-								( ( siteMetricData?.basic?.lcp + upgradePlanSiteMetricsLcpThreshold ) / 2 )
-						)
-				) ) ||
-			0;
+	let lcpPercentageDifference = 0;
+	let fidPercentageDifference = 0;
+
+	if ( isEnglishLocale ) {
+		lcpPercentageDifference = calcPercentageDifferenceFromThreshold(
+			siteMetricData?.basic?.lcp,
+			upgradePlanSiteMetricsLcpThreshold
+		);
+
+		fidPercentageDifference = calcPercentageDifferenceFromThreshold(
+			siteMetricData?.basic?.fid,
+			upgradePlanSiteMetricsFidThreshold
+		);
 	}
 
 	return {
 		siteMetricData,
 		lcpPercentageDifference,
+		fidPercentageDifference,
 	};
 };
