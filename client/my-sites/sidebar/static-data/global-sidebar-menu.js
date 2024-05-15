670399071d9ba2706cf73f89d952823b3116ddb8
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-05-15 12:34:21 +0200

    Update calendar icon

--- client/my-sites/sidebar/static-data/global-sidebar-menu.js
+++ client/my-sites/sidebar/static-data/global-sidebar-menu.js
@@ -36,7 +36,6 @@ export default function globalSidebarMenu() {
 						fillRule="evenodd"
 						clipRule="evenodd"
 						d="M10.5 4L10.5 8H13.5V4H15V8H16.5C17.0523 8 17.5 8.44772 17.5 9V13L14.5 17V19C14.5 19.5523 14.0523 20 13.5 20H10.5C9.94772 20 9.5 19.5523 9.5 19V17L6.5 13V9C6.5 8.44772 6.94772 8 7.5 8H9L9 4H10.5ZM11 16.5V18.5H13V16.5L16 12.5V9.5H8V12.5L11 16.5Z"
-						fill="#2C3338"
 					/>
 				</svg>
 			),
