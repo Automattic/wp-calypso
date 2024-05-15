626dc729517ebe549c767ef5f4ed8bc77298ba1d
Roberto Aranda <roberto.aranda@automattic.com>
2024-05-15 09:33:52 +0200

    Site Profiler: Use `final_url` to get user report (#90689)
    Co-authored-by: Wesley Berrêdo <wberredo@gmail.com>

--- client/site-profiler/components/site-profiler.tsx
+++ client/site-profiler/components/site-profiler.tsx
@@ -205,7 +205,7 @@ export default function SiteProfiler( props: Props ) {
 				</LayoutBlock>
 			) }
 			<GetReportForm
-				url={ url }
+				url={ basicMetrics?.final_url }
 				token={ basicMetrics?.token }
 				isOpen={ showGetReportForm }
 				onClose={ () => setIsGetReportFormOpen( false ) }
