d986c1a654fb23518a083357f5ab6e4d1e48143f
Heyde Moura <moura.heyde@gmail.com>
2024-05-15 18:00:21 -0300

    Isolate atomic site selector component

---
+++ client/a8c-for-agencies/sections/sites/add-sites/atomic-sites-selector.tsx
@@ -0,0 +1,30 @@
+import { SiteDetails } from '@automattic/data-stores';
+import { A4A_SITES_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
+import BaseSiteSelector from 'calypso/components/site-selector';
+
+interface AtomicSitesSelectorProps {
+	onSiteSelect?: ( blogId: number ) => void;
+	isPlaceholder?: boolean;
+}
+
+const AtomicSitesSelector = ( { onSiteSelect, isPlaceholder }: AtomicSitesSelectorProps ) => {
+	const atomicSitesFilter = ( site: SiteDetails ) => site?.is_wpcom_atomic;
+
+	return (
+		<div className="atomic-sites-selector">
+			<BaseSiteSelector
+				clasName="add-sites-from-wpcom_site-selector"
+				indicator
+				allSitesPath={ A4A_SITES_LINK }
+				sitesBasePath={ A4A_SITES_LINK }
+				onSiteSelect={ onSiteSelect }
+				filter={ atomicSitesFilter }
+				showHiddenSites={ false }
+				showListBottomAdornment={ false }
+				isPlaceholder={ isPlaceholder }
+			/>
+		</div>
+	);
+};
+
+export default AtomicSitesSelector;
