578e9d6e7655db9348af8fc3f33f3672368ed182
cleacos <javacodia@gmail.com>
2024-05-15 13:31:51 +0100

    Update the sidebar/utils.js to detect when /setting section is active

--- client/my-sites/sidebar/utils.js
+++ client/my-sites/sidebar/utils.js
@@ -41,7 +41,7 @@ export const itemLinkMatches = ( path, currentPath ) => {
 
 	if ( pathIncludes( currentPath, 'settings', 1 ) ) {
 		// Jetpack Cloud uses a simpler /settings/:site pattern for the settings page.
-		if ( isJetpackCloud() ) {
+		if ( isJetpackCloud() || isA8CForAgencies() ) {
 			return fragmentIsEqual( path, currentPath, 1 );
 		}
 
