457c0c46a131fd6da28ec989cd5e805916962631
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2024-05-16 08:48:27 -0600

    Add/highlight on includes limits for ai and stats (#90528)
    * Add highlight on includes line with limits for ai and stats
    
    * Use wrapper function for highlightable includes
    
    * Remove outdated comment

--- client/my-sites/plans/jetpack-plans/product-lightbox/quantity-dropdown.tsx
+++ client/my-sites/plans/jetpack-plans/product-lightbox/quantity-dropdown.tsx
@@ -50,8 +50,27 @@ const QuantityDropdown: FC< QuantityDropdownProps > = ( { product, siteId, onCha
 		} );
 	}, [ listPrices.priceTierList, product.productSlug ] );
 
+	const triggerHighlightedIncludes = useCallback( () => {
+		const highlightedIncludes = document.getElementsByClassName( 'highlight-on-render' );
+
+		if ( highlightedIncludes.length > 0 ) {
+			for ( const element of highlightedIncludes ) {
+				element.classList.add( 'trigger-highlight' );
+			}
+		}
+
+		setTimeout( () => {
+			if ( highlightedIncludes.length > 0 ) {
+				for ( const element of highlightedIncludes ) {
+					element.classList.remove( 'trigger-highlight' );
+				}
+			}
+		}, 500 );
+	}, [] );
+
 	const onDropdownTierSelect = useCallback(
 		( { value: slug }: { value: string } ) => {
+			triggerHighlightedIncludes();
 			onChangeProduct( slugToSelectorProduct( slug ) );
 			const { slug: productSlug, quantity } = getProductPartsFromAlias( slug );
 
@@ -63,7 +82,7 @@ const QuantityDropdown: FC< QuantityDropdownProps > = ( { product, siteId, onCha
 				} )
 			);
 		},
-		[ onChangeProduct, dispatch, siteId ]
+		[ onChangeProduct, dispatch, siteId, triggerHighlightedIncludes ]
 	);
 
 	if ( ! isJetpackTieredProduct( product.productSlug ) || tierOptions.length < 1 ) {
