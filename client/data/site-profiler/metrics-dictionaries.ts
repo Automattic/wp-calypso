c4e8f71e39768bd3bc1d8570288ec388dfe183bc
Roberto Aranda <roberto.aranda@automattic.com>
2024-05-16 13:12:41 +0200

    Document the sources where the values have been taken from

--- client/data/site-profiler/metrics-dictionaries.ts
+++ client/data/site-profiler/metrics-dictionaries.ts
@@ -16,14 +16,21 @@ export const BASIC_METRICS_UNITS: Record< Metrics, string > = {
 };
 
 export const BASIC_METRICS_SCORES: Record< Metrics, [ number, number ] > = {
-	cls: [ 0.1, 0.25 ],
-	fid: [ 100, 300 ],
-	lcp: [ 2500, 4000 ],
-	fcp: [ 1800, 3000 ],
-	ttfb: [ 800, 1800 ],
-	inp: [ 200, 500 ],
+	cls: [ 0.1, 0.25 ], // https://web.dev/articles/cls
+	fid: [ 100, 300 ], // https://web.dev/articles/fid
+	lcp: [ 2500, 4000 ], // https://web.dev/articles/lcp
+	fcp: [ 1800, 3000 ], // https://web.dev/articles/fcp
+	ttfb: [ 800, 1800 ], // https://web.dev/articles/ttfb
+	inp: [ 200, 500 ], // https://web.dev/articles/inp
 };
 
+/**
+ * Get the score of a metric based on its value according to the Google
+ * documentation that has been represented in the BASIC_METRICS_SCORES constant.
+ * @param metric The metric in the Metrics type
+ * @param value The value of the metric
+ * @returns A score based on the value of the metric
+ */
 export function getScore( metric: Metrics, value: number ): Scores {
 	const [ good, poor ] = BASIC_METRICS_SCORES[ metric ];
 	if ( value <= good ) {
