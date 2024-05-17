8ea68547a4c857771cd4a5accc3e61648a3a56ea
Christos <chriskmnds@gmail.com>
2024-05-17 15:44:48 +0300

    revert - preload experiment on entering the flow

--- client/signup/flow-actions.ts
+++ client/signup/flow-actions.ts
@@ -4,6 +4,5 @@ export const onEnterOnboarding = ( flowName: string ) => {
 	// just to be extra safe since `loadExperimentAssignment` doesn't have the same eligibility check like `useExperiment` does
 	if ( flowName === 'onboarding' ) {
 		loadExperimentAssignment( 'calypso_signup_onboarding_deemphasize_free_plan' );
-		loadExperimentAssignment( 'calypso_signup_onboarding_emphasize_14d_refund' );
 	}
 };
