696d5646cd6ab93ddf32468a8af7449332f01c17
DustyReagan <dusty@dustyreagan.com>
2024-05-17 17:10:35 -0500

    Try fix flakey test

--- test/e2e/specs/onboarding/lifecycle__signup-onboarding-launch-cancel.ts
+++ test/e2e/specs/onboarding/lifecycle__signup-onboarding-launch-cancel.ts
@@ -84,7 +84,16 @@ describe( 'Lifecyle: Signup, onboard, launch and cancel subscription', function 
 
 		it( 'See secure payment', async function () {
 			cartCheckoutPage = new CartCheckoutPage( page );
-			await cartCheckoutPage.validateCartItem( `WordPress.com ${ newPlanName }` );
+			try {
+				await Promise.any( [
+					cartCheckoutPage.validateCartItem( 'WordPress.com Beginner' ),
+					cartCheckoutPage.validateCartItem( `WordPress.com ${ newPlanName }` ),
+				] );
+			} catch ( error ) {
+				// If both promises are rejected, handle the error (optional)
+				console.log( `Neither "Beginner" or "Starter" were found on the page.` );
+				throw error; // Re-throw the error to fail the test
+			}
 		} );
 
 		it( 'Prices are shown in GBP', async function () {
@@ -231,11 +240,22 @@ describe( 'Lifecyle: Signup, onboard, launch and cancel subscription', function 
 		it( 'View details of purchased plan', async function () {
 			purchasesPage = new PurchasesPage( page );
 
-			await purchasesPage.clickOnPurchase(
-				`WordPress.com ${ newPlanName }`,
-				newSiteDetails.blog_details.site_slug
-			);
-			await purchasesPage.purchaseAction( 'Cancel plan' );
+			try {
+				await Promise.any( [
+					purchasesPage.clickOnPurchase(
+						'WordPress.com Beginner',
+						newSiteDetails.blog_details.site_slug
+					),
+					purchasesPage.clickOnPurchase(
+						`WordPress.com ${ newPlanName }`,
+						newSiteDetails.blog_details.site_slug
+					),
+				] );
+				await purchasesPage.purchaseAction( 'Cancel plan' );
+			} catch ( error ) {
+				console.log( `Neither "Beginner" nor "Starter" plan was found.` );
+				throw error;
+			}
 		} );
 
 		it( 'Cancel plan renewal', async function () {
