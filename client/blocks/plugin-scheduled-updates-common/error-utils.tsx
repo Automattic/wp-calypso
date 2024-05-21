1620824da107ad59776b210fdbadccd9393dde0e
Wayne Wang <wayne.wang@automattic.com>
2024-05-21 14:44:12 +0800

    Change the condition to use switch case

--- client/blocks/plugin-scheduled-updates-common/error-utils.tsx
+++ client/blocks/plugin-scheduled-updates-common/error-utils.tsx
@@ -3,25 +3,26 @@ import { translate } from 'i18n-calypso';
 import type { APIError } from '@automattic/data-stores';
 
 export const handleErrorMessage = ( error: APIError ): string => {
-	let errorMsg = error.message;
-	if ( error.status === 500 ) {
-		// If error code is 500, we want to output a more useful error message and point them to support doc
-		errorMsg = translate(
-			'Your website is experiencing technical issues. Please refer to our {{a}}support documentation{{/a}} for assistance in resolving this error.',
-			{
-				components: {
-					a: (
-						<a
-							href={ localizeUrl(
-								'https://wordpress.com/support/resolve-jetpack-errors/#critical-error-on-the-site'
-							) }
-							target="_blank"
-							rel="noreferrer"
-						/>
-					 ) as JSX.Element,
-				},
-			}
-		) as string;
+	switch ( error.status ) {
+		case 500:
+			return translate(
+				'Your website is experiencing technical issues. Please refer to our {{a}}support documentation{{/a}} for assistance in resolving this error.',
+				{
+					components: {
+						a: (
+							<a
+								href={ localizeUrl(
+									'https://wordpress.com/support/resolve-jetpack-errors/#critical-error-on-the-site'
+								) }
+								target="_blank"
+								rel="noreferrer"
+							/>
+						 ) as JSX.Element,
+					},
+				}
+			) as string;
+		default:
+			// Return the default error message for other error codes
+			return error.message;
 	}
-	return errorMsg;
 };
