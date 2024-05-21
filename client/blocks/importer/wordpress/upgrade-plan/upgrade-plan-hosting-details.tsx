60ddb4a12b28fb175a15165c410ce8cb88cf8120
Renatho De Carli Rosa <renatho@gmail.com>
2024-05-21 17:18:59 -0300

    Update with number from April 2024

--- client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
@@ -78,7 +78,7 @@ export const UpgradePlanHostingDetails = () => {
 					<p className="import__upgrade-plan-hosting-details-header-main">{ headerMainText }</p>
 					<p className="import__upgrade-plan-hosting-details-header-subtext">
 						{ translate(
-							'We offer a proven 25% boost* in Core Web Vitals—endorsed by data from Google.'
+							'We offer a proven 24% boost* in Core Web Vitals—endorsed by data from Google.'
 						) }
 					</p>
 				</div>
