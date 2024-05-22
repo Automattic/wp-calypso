8318307f7fff7d48626a1a068013a5848169ec8e
Richard Ortiz <rcrd.ortiz@gmail.com>
2024-05-22 13:40:30 +0200

    Changed the command palette icon to the one provided by design

--- client/layout/global-sidebar/menu-items/search/index.jsx
+++ client/layout/global-sidebar/menu-items/search/index.jsx
@@ -1,8 +1,8 @@
-import { Icon, search } from '@wordpress/icons';
 import classnames from 'classnames';
 import { useDispatch } from 'calypso/state';
 import { openCommandPalette } from 'calypso/state/command-palette/actions';
 import SidebarMenuItem from '../menu-item';
+import { PromptIcon } from './icon';
 
 export const SidebarSearch = ( { tooltip, onClick } ) => {
 	const dispatch = useDispatch();
@@ -19,7 +19,7 @@ export const SidebarSearch = ( { tooltip, onClick } ) => {
 				} ) }
 				tooltip={ tooltip }
 				tooltipPlacement="top"
-				icon={ <Icon icon={ search } size={ 28 } /> }
+				icon={ <PromptIcon /> }
 			/>
 		</>
 	);
