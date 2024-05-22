fec8ff2463a9aa1f22569104b2118a282bd5ab4c
Renatho De Carli Rosa <renatho@gmail.com>
2024-05-22 14:29:16 -0300

    Change the spacing approach to a margin

--- client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
@@ -3,6 +3,7 @@ import { useState } from '@wordpress/element';
 import { hasTranslation } from '@wordpress/i18n';
 import { Icon } from '@wordpress/icons';
 import { getQueryArg } from '@wordpress/url';
+import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import { useAnalyzeUrlQuery } from 'calypso/data/site-profiler/use-analyze-url-query';
 import { useHostingProviderQuery } from 'calypso/data/site-profiler/use-hosting-provider-query';
@@ -42,7 +43,12 @@ export const UpgradePlanHostingDetails = () => {
 
 	return (
 		<div className="import__upgrade-plan-hosting-details">
-			<div className="import__upgrade-plan-hosting-details-card-container">
+			<div
+				className={ classNames( 'import__upgrade-plan-hosting-details-card-container', {
+					'import__upgrade-plan-hosting-details-card-container--without-identified-host':
+						! shouldDisplayHostIdentificationMessage,
+				} ) }
+			>
 				<div className="import__upgrade-plan-hosting-details-header">
 					<p className="import__upgrade-plan-hosting-details-header-main">{ headerMainText }</p>
 					<p className="import__upgrade-plan-hosting-details-header-subtext">
@@ -96,12 +102,13 @@ export const UpgradePlanHostingDetails = () => {
 					</div>
 				) }
 			</div>
-			<div className="import__upgrade-plan-hosting-details-identified-host">
-				{ shouldDisplayHostIdentificationMessage &&
-					translate( "We've identified %(hostingProviderName)s as your host.", {
+			{ shouldDisplayHostIdentificationMessage && (
+				<div className="import__upgrade-plan-hosting-details-identified-host">
+					{ translate( "We've identified %(hostingProviderName)s as your host.", {
 						args: { hostingProviderName },
 					} ) }
-			</div>
+				</div>
+			) }
 		</div>
 	);
 };
