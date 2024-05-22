23614ab723b176737ae1d73b743c1d6f2cf27247
Heyde Moura <moura.heyde@gmail.com>
2024-05-22 19:53:27 -0300

    Disable dataviews spinner

--- client/a8c-for-agencies/components/items-dashboard/items-dataviews/index.tsx
+++ client/a8c-for-agencies/components/items-dashboard/items-dataviews/index.tsx
@@ -1,7 +1,5 @@
-import { Spinner } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { ReactNode } from 'react';
-import ReactDOM from 'react-dom';
 import { DataViews } from 'calypso/components/dataviews';
 import { ItemsDataViewsType, DataViewsColumn } from './interfaces';
 
@@ -57,6 +55,11 @@ const ItemsDataViews = ( { data, isLoading = false, className }: ItemsDataViewsP
 
 	// Until the DataViews package is updated to support the spinner, we need to manually add the (loading) spinner to the table wrapper for now.
 	// todo: The DataViews v0.9 has the spinner support. Remove this once we upgrade the package.
+	// Disabling this for now, as manipulating the DOM on the React tree completely breaks the point of using React.
+	// This generates mismatches and React has to re-render the whole application to recover.
+	// When the mismatch happens, the page might break on a unrecoverable state.
+
+	/*
 	const SpinnerWrapper = () => {
 		return (
 			<div className="spinner-wrapper">
@@ -64,20 +67,20 @@ const ItemsDataViews = ( { data, isLoading = false, className }: ItemsDataViewsP
 			</div>
 		);
 	};
-	const dataviewsWrapper = document.getElementsByClassName( 'dataviews-wrapper' )[ 0 ];
-	if ( dataviewsWrapper ) {
-		// Remove any existing spinner if present
-		const existingSpinner = dataviewsWrapper.querySelector( '.spinner-wrapper' );
-		if ( existingSpinner ) {
-			existingSpinner.remove();
-		}
+  const dataviewsWrapper = document.getElementsByClassName( 'dataviews-wrapper' )[ 0 ];
+    if ( dataviewsWrapper ) {
+        // Remove any existing spinner if present
+        const existingSpinner = dataviewsWrapper.querySelector( '.spinner-wrapper' );
+        if ( existingSpinner ) {
+            existingSpinner.remove();
+        }
 
-		const spinnerWrapper = dataviewsWrapper.appendChild( document.createElement( 'div' ) );
-		spinnerWrapper.classList.add( 'spinner-wrapper' );
-		// Render the SpinnerWrapper component inside the spinner wrapper
-		ReactDOM.hydrate( <SpinnerWrapper />, spinnerWrapper );
-	}
-
+        const spinnerWrapper = dataviewsWrapper.appendChild( document.createElement( 'div' ) );
+        spinnerWrapper.classList.add( 'spinner-wrapper' );
+        // Render the SpinnerWrapper component inside the spinner wrapper
+        ReactDOM.hydrate( <SpinnerWrapper />, spinnerWrapper );
+  }
+ */
 	return (
 		<div className={ className }>
 			<DataViews
