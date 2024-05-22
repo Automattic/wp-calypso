5e81726607fc455a5979502d82381578316aee02
Yashwin Poojary <yashwinpoojary@gmail.com>
2024-05-22 09:23:49 +0530

    A4A > Referrals: Initial implementation of automated referrals (#90941)

--- client/a8c-for-agencies/sections/referrals/index.ts
+++ client/a8c-for-agencies/sections/referrals/index.ts
@@ -1,14 +1,21 @@
+import config from '@automattic/calypso-config';
 import page from '@automattic/calypso-router';
 import {
 	A4A_REFERRALS_LINK,
 	A4A_REFERRALS_BANK_DETAILS_LINK,
 	A4A_REFERRALS_COMMISSIONS_LINK,
+	A4A_REFERRALS_DASHBOARD,
+	A4A_REFERRALS_PAYMENT_SETTINGS,
+	A4A_REFERRALS_FAQ,
 } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import { requireAccessContext } from 'calypso/a8c-for-agencies/controller';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import * as controller from './controller';
 
+const isAutomatedReferralsEnabled = config.isEnabled( 'a4a-automated-referrals' );
+
 export default function () {
+	// Remove the following when the automated referrals feature flag is removed
 	page(
 		A4A_REFERRALS_LINK,
 		requireAccessContext,
@@ -30,4 +37,28 @@ export default function () {
 		makeLayout,
 		clientRender
 	);
+	if ( isAutomatedReferralsEnabled ) {
+		page(
+			A4A_REFERRALS_DASHBOARD,
+			requireAccessContext,
+			controller.referralsDashboardContext,
+			makeLayout,
+			clientRender
+		);
+		page(
+			A4A_REFERRALS_PAYMENT_SETTINGS,
+			requireAccessContext,
+			controller.referralsPaymentSettingsContext,
+			makeLayout,
+			clientRender
+		);
+		page(
+			A4A_REFERRALS_FAQ,
+			requireAccessContext,
+			controller.referralsFAQContext,
+			makeLayout,
+			clientRender
+		);
+		page( A4A_REFERRALS_LINK, () => page.redirect( A4A_REFERRALS_DASHBOARD ) );
+	}
 }
