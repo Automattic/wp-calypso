cb1bac7de94330ca07580007b20f0e8ecce6e604
Anthony Grullon <agrullon95@gmail.com>
2024-05-22 13:25:14 -0400

    Add hook to retrieve segment

---
+++ client/signup/hooks/use-get-segment.tsx
@@ -0,0 +1,39 @@
+const useGetSegment = ( intent: string, goals: string[] ): string => {
+	// Handle the case when no goals are provided (n/a)
+	if ( intent === 'migrate-or-import-site' && goals.length === 0 ) {
+		return 'Migration';
+	}
+
+	if ( intent === 'client' && goals.length === 0 ) {
+		return 'Developer / Agency';
+	}
+
+	// Handle different cases when intent is 'Create for self'
+	if ( intent === 'myself-business-or-friend' ) {
+		if ( goals.length === 0 ) {
+			return 'Unknown';
+		}
+		if ( goals.includes( 'difm' ) ) {
+			return 'DIFM'; // Do it for me
+		}
+		if ( goals.includes( 'sell' ) && ! goals.includes( 'difm' ) ) {
+			return 'Merchant';
+		}
+		if ( goals.includes( 'blog' ) ) {
+			return 'Blogger';
+		}
+		if ( goals.includes( 'educational-or-nonprofit' ) ) {
+			return 'Nonprofit';
+		}
+		if ( goals.includes( 'newsletter' ) ) {
+			return 'Newsletter';
+		}
+		// Catch-all case for when none of the specific goals are met
+		return 'Consumer / Business';
+	}
+
+	// Default return if no conditions are met
+	return 'Unrecognized';
+};
+
+export default useGetSegment;
