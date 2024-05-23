f646b06784af0c7942b5262fcd76d69f81317a52
Anthony Grullon <agrullon95@gmail.com>
2024-05-22 20:16:39 -0400

    Update plan type imports

--- packages/plans-grid-next/src/hooks/data-store/use-segment-based-plans.ts
+++ packages/plans-grid-next/src/hooks/data-store/use-segment-based-plans.ts
@@ -1,3 +1,12 @@
+import {
+	TYPE_BUSINESS,
+	TYPE_ECOMMERCE,
+	TYPE_ENTERPRISE_GRID_WPCOM,
+	TYPE_FREE,
+	TYPE_PERSONAL,
+	TYPE_PREMIUM,
+} from '@automattic/calypso-products';
+
 export function determineSegment( intent: string, goals: string[] ) {
 	// Handle the case when no goals are provided (n/a)
 	if ( intent === 'Migrate or import' && goals.length === 0 ) {
@@ -37,31 +46,31 @@ export function useSegmentedPlans( segment: string ) {
 	switch ( segment ) {
 		case 'Unknown':
 			return [
-				'TYPE_FREE',
-				'TYPE_STARTER',
-				'TYPE_EXPLORER',
-				'TYPE_CREATOR',
-				'TYPE_ENTREPRENEUR',
-				'TYPE_ENTERPRISE',
+				TYPE_FREE,
+				TYPE_PERSONAL,
+				TYPE_PREMIUM,
+				TYPE_BUSINESS,
+				TYPE_ECOMMERCE,
+				TYPE_ENTERPRISE_GRID_WPCOM,
 			];
 		case 'Blogger':
-			return [ 'TYPE_FREE', 'TYPE_STARTER', 'TYPE_EXPLORER', 'TYPE_CREATOR' ];
+			return [ TYPE_FREE, TYPE_PERSONAL, TYPE_PREMIUM, TYPE_BUSINESS ];
 		case 'Consumer':
 		case 'Business':
-			return [ 'TYPE_STARTER', 'TYPE_EXPLORER', 'TYPE_CREATOR' ];
+			return [ TYPE_PERSONAL, TYPE_PREMIUM, TYPE_BUSINESS ];
 		case 'Merchant':
-			return [ 'TYPE_CREATOR', 'TYPE_ENTREPRENEUR', 'TYPE_ENTERPRISE' ];
+			return [ TYPE_BUSINESS, TYPE_ECOMMERCE, TYPE_ENTERPRISE_GRID_WPCOM ];
 		case 'Developer':
 		case 'Agency':
-			return [ 'TYPE_CREATOR', 'TYPE_ENTREPRENEUR', 'TYPE_ENTERPRISE' ];
+			return [ TYPE_BUSINESS, TYPE_ECOMMERCE, TYPE_ENTERPRISE_GRID_WPCOM ];
 		default:
 			return [
-				'TYPE_FREE',
-				'TYPE_STARTER',
-				'TYPE_EXPLORER',
-				'TYPE_CREATOR',
-				'TYPE_ENTREPRENEUR',
-				'TYPE_ENTERPRISE',
+				TYPE_FREE,
+				TYPE_PERSONAL,
+				TYPE_PREMIUM,
+				TYPE_BUSINESS,
+				TYPE_ECOMMERCE,
+				TYPE_ENTERPRISE_GRID_WPCOM,
 			];
 	}
 }
