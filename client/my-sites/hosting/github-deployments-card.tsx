3c6a8180c7ea0c52ba40b97db7579ee549dfdb56
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-05-23 16:15:13 +0200

    `GitHubDeploymentsCard`: Show only when site has no repos connected

--- client/my-sites/hosting/github-deployments-card.tsx
+++ client/my-sites/hosting/github-deployments-card.tsx
@@ -1,8 +1,9 @@
 import { useI18n } from '@wordpress/react-i18n';
 import { useDispatch } from 'react-redux';
+import { useCodeDeploymentsQuery } from 'calypso/my-sites/github-deployments/deployments/use-code-deployments-query';
 import { useSelector } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import Banner from '../../components/banner';
 import SocialLogo from '../../components/social-logo';
 import { indexPage } from '../github-deployments/routes';
@@ -11,21 +12,31 @@ export const GitHubDeploymentsCard = () => {
 	const { __ } = useI18n();
 	const dispatch = useDispatch();
 	const siteSlug = useSelector( getSelectedSiteSlug );
+	const siteId = useSelector( getSelectedSiteId );
+	const { data: deployments, isLoading } = useCodeDeploymentsQuery( siteId );
+
+	if ( isLoading ) {
+		return null;
+	}
+
+	const hasNoDeployments = ! deployments?.length;
 
 	return (
-		<Banner
-			className="github-deployments-card"
-			icon={ <SocialLogo icon="github" size={ 32 } /> }
-			disableCircle
-			callToAction={ __( 'Get started' ) }
-			href={ indexPage( siteSlug! ) }
-			title={ __( 'GitHub Deployments' ) }
-			onClick={ () => {
-				dispatch( recordTracksEvent( 'calypso_hosting_github_nudge_clicked' ) );
-			} }
-			description={ __(
-				'Effortlessly deploy themes, plugins, or your entire site directly from GitHub!'
-			) }
-		/>
+		hasNoDeployments && (
+			<Banner
+				className="github-deployments-card"
+				icon={ <SocialLogo icon="github" size={ 32 } /> }
+				disableCircle
+				callToAction={ __( 'Get started' ) }
+				href={ indexPage( siteSlug! ) }
+				title={ __( 'GitHub Deployments' ) }
+				onClick={ () => {
+					dispatch( recordTracksEvent( 'calypso_hosting_github_nudge_clicked' ) );
+				} }
+				description={ __(
+					'Effortlessly deploy themes, plugins, or your entire site directly from GitHub!'
+				) }
+			/>
+		)
 	);
 };
