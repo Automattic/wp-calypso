38a34f5398f0cacbc9eb30e210d2ae0074baec4c
dereksmart <smart@automattic.com>
2024-05-23 16:28:42 -0700

    loading state only

--- client/landing/stepper/declarative-flow/internals/steps-repository/ai-site-prompt/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/ai-site-prompt/index.tsx
@@ -1,58 +1,26 @@
 import { Onboard } from '@automattic/data-stores';
-import { StepContainer, NextButton } from '@automattic/onboarding';
-import styled from '@emotion/styled';
-import { TextareaControl } from '@wordpress/components';
-import { resolveSelect, useDispatch, useSelect } from '@wordpress/data';
-import { useState } from '@wordpress/element';
+import { resolveSelect, useDispatch } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
-import { FormEvent } from 'react';
+import { useEffect, FormEvent } from 'react';
 import wpcomRequest from 'wpcom-proxy-request';
-import FormattedHeader from 'calypso/components/formatted-header';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
-import { ONBOARD_STORE, SITE_STORE } from 'calypso/landing/stepper/stores';
-import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { SITE_STORE } from 'calypso/landing/stepper/stores';
 import { useSiteData } from '../../../../hooks/use-site-data';
 import useAIAssembler from '../pattern-assembler/hooks/use-ai-assembler';
 import type { Step } from '../../types';
-import type { OnboardSelect } from '@automattic/data-stores';
 
 import './style.scss';
 
 const SiteIntent = Onboard.SiteIntent;
 
-const ActionSection = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: baseline;
-	flex-wrap: wrap;
-	margin-top: 40px;
-
-	@media ( max-width: 320px ) {
-		align-items: center;
-	}
-`;
-
-const StyledNextButton = styled( NextButton )`
-	@media ( max-width: 320px ) {
-		width: 100%;
-		margin-bottom: 20px;
-	}
-`;
-
 const AISitePrompt: Step = function ( props ) {
 	const { goNext, goBack, submit } = props.navigation; // eslint-disable-line @typescript-eslint/no-unused-vars
-	const [ isLaunchingBigSky, setIsLaunchingBigSky ] = useState( false );
 
 	const { __ } = useI18n();
-	const intent = useSelect(
-		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getIntent(),
-		[]
-	);
 
 	const [ callAIAssembler, setPrompt, prompt, loading ] = useAIAssembler(); // eslint-disable-line @typescript-eslint/no-unused-vars
 
 	const { siteSlug, siteId } = useSiteData();
-	// const { setPendingAction } = useDispatch( ONBOARD_STORE );
 	const { saveSiteSettings, setDesignOnSite, setStaticHomepageOnSite, setIntentOnSite } =
 		useDispatch( SITE_STORE );
 
@@ -61,8 +29,6 @@ const AISitePrompt: Step = function ( props ) {
 		if ( ! selectedSiteId || ! selectedSiteSlug ) {
 			return;
 		}
-
-		setIsLaunchingBigSky( true );
 
 		saveSiteSettings( selectedSiteId, {
 			wpcom_ai_site_prompt: prompt,
@@ -115,6 +81,16 @@ const AISitePrompt: Step = function ( props ) {
 		exitFlow( siteId.toString(), siteSlug );
 	};
 
+	useEffect( () => {
+		const syntheticEvent = {
+			preventDefault: () => {},
+			target: {
+				elements: {},
+			},
+		} as unknown as FormEvent;
+		onSubmit( syntheticEvent );
+	}, [] );
+
 	function LaunchingBigSky() {
 		return (
 			<div className="processing-step__container">
@@ -126,58 +102,10 @@ const AISitePrompt: Step = function ( props ) {
 		);
 	}
 
-	function getContent() {
-		return (
-			<>
-				<div className="site-prompt__instructions-container">
-					<form onSubmit={ onSubmit }>
-						<TextareaControl
-							help={ __( 'Sharing more detail here will help AI understand your intent better.' ) }
-							value={ prompt }
-							onChange={ ( value ) => setPrompt( value ) }
-							disabled={ loading }
-						/>
-
-						<ActionSection>
-							{ loading && <LoadingEllipsis /> }
-							{ ! loading && (
-								<StyledNextButton type="submit" disabled={ loading || prompt.length < 16 }>
-									{ __( 'Continue' ) }
-								</StyledNextButton>
-							) }
-						</ActionSection>
-					</form>
-				</div>
-			</>
-		);
-	}
-
 	return (
 		<div className="site-prompt__signup is-woocommerce-install">
 			<div className="site-prompt__is-store-address">
-				{ isLaunchingBigSky && <LaunchingBigSky /> }
-				{ ! isLaunchingBigSky && (
-					<StepContainer
-						stepName="site-prompt"
-						className={ `is-step-${ intent }` }
-						skipButtonAlign="top"
-						hideBack
-						goNext={ goNext }
-						isHorizontalLayout
-						formattedHeader={
-							<FormattedHeader
-								id="site-prompt-header"
-								headerText={ __( 'Tell us a bit about your web site or business.' ) }
-								subHeaderText={ __(
-									'We will create a home page template for you based on best practices for sites like yours.'
-								) }
-								align="left"
-							/>
-						}
-						stepContent={ getContent() }
-						recordTracksEvent={ recordTracksEvent }
-					/>
-				) }
+				<LaunchingBigSky />
 			</div>
 		</div>
 	);
