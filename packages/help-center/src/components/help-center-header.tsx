444a1de648e5e3e8f9aaa568ab41c25a377eb289
Tony Arcangelini <tony@arcangelini.com>
2024-05-24 14:45:13 +0200

    Change text

--- packages/help-center/src/components/help-center-header.tsx
+++ packages/help-center/src/components/help-center-header.tsx
@@ -34,7 +34,7 @@ const SupportModeTitle = () => {
 			return (
 				<>
 					<Icon icon={ commentContent } />
-					{ __( 'Connect to our Happiness Engineers', __i18n_text_domain__ ) }
+					{ __( 'Contact WordPress.com Support', __i18n_text_domain__ ) }
 				</>
 			);
 		case 'EMAIL': {
