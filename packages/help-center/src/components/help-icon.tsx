636fe4c584c6423ec04b9f628faffeace446117f
Addison-Stavlo <stavz01@gmail.com>
2024-05-24 15:10:55 -0400

    update svgs per figma

--- packages/help-center/src/components/help-icon.tsx
+++ packages/help-center/src/components/help-icon.tsx
@@ -29,6 +29,7 @@ const HelpIcon = forwardRef< SVGSVGElement >( ( _, ref ) => {
 		<>
 			{ newItems || unreadCount > 0 ? (
 				<svg
+					className="help-center__icon-has-new-items"
 					ref={ ref }
 					width="24"
 					height="24"
@@ -38,16 +39,16 @@ const HelpIcon = forwardRef< SVGSVGElement >( ( _, ref ) => {
 				>
 					<path d="M12 4.75a7.25 7.25 0 100 14.5 7.25 7.25 0 000-14.5zM3.25 12a8.75 8.75 0 1117.5 0 8.75 8.75 0 01-17.5 0zM12 8.75a1.5 1.5 0 01.167 2.99c-.465.052-.917.44-.917 1.01V14h1.5v-.845A3 3 0 109 10.25h1.5a1.5 1.5 0 011.5-1.5zM11.25 15v1.5h1.5V15h-1.5z" />
 					<circle
-						cx="17"
-						cy="6"
+						cx="18.5"
+						cy="6.5"
 						r="3"
 						fill="var( --color-masterbar-unread-dot-background )"
-						stroke="#101517"
-						strokeWidth="2"
+						stroke="white"
 					/>
 				</svg>
 			) : (
 				<svg
+					className="help-center__icon"
 					ref={ ref }
 					width="24"
 					height="24"
