7dfaabc33c6920b129392fdc095489fe86199f80
Igor Zinovyev <zinigor@gmail.com>
2024-05-24 19:02:20 +0300

    Changed the test to look for a specific role of element.

--- test/e2e/specs/editor/editor__page-basic-flow.ts
+++ test/e2e/specs/editor/editor__page-basic-flow.ts
@@ -74,7 +74,7 @@ describe( DataHelper.createSuiteTitle( 'Editor: Basic Post Flow' ), function () 
 
 	it( 'Template content loads into editor', async function () {
 		const editorCanvas = await editorPage.getEditorCanvas();
-		await editorCanvas.locator( `h1:text-is('${ pageTemplateToSelect }')` ).waitFor();
+		await editorCanvas.getByRole( 'button', { name: `${ pageTemplateToSelect }` } ).waitFor();
 	} );
 
 	it( 'Open setting sidebar', async function () {
