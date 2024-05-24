cd541ec880f9ffaea57de36295b61bc085a365a3
Roberto Aranda <roberto.aranda@automattic.com>
2024-05-24 13:41:53 +0200

    Site Profiler: Change header background and title based on Score

--- client/site-profiler/components/site-profiler-v2.tsx
+++ client/site-profiler/components/site-profiler-v2.tsx
@@ -4,6 +4,7 @@ import debugFactory from 'debug';
 import { translate } from 'i18n-calypso';
 import { useRef, useState } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
+import { getOveralScore, isScoreGood } from 'calypso/data/site-profiler/metrics-dictionaries';
 import { useAnalyzeUrlQuery } from 'calypso/data/site-profiler/use-analyze-url-query';
 import { useDomainAnalyzerQuery } from 'calypso/data/site-profiler/use-domain-analyzer-query';
 import { useHostingProviderQuery } from 'calypso/data/site-profiler/use-hosting-provider-query';
@@ -95,9 +96,7 @@ export default function SiteProfilerV2( props: Props ) {
 
 	const showGetReportForm = !! showBasicMetrics && !! url && isGetReportFormOpen;
 
-	// TODO: Get overall score from the API
-	// https://github.com/Automattic/dotcom-forge/issues/7298
-	const overallScore = false;
+	const overallScore = getOveralScore( basicMetrics?.basic );
 
 	const updateDomainRouteParam = ( value: string ) => {
 		// Update the domain param;
@@ -125,15 +124,15 @@ export default function SiteProfilerV2( props: Props ) {
 					<LayoutBlock
 						className={ classnames(
 							'results-header-block',
-							{ poor: ! overallScore },
-							{ good: overallScore }
+							{ poor: ! isScoreGood( overallScore ) },
+							{ good: isScoreGood( overallScore ) }
 						) }
 						width="medium"
 					>
 						{ showBasicMetrics && (
 							<ResultsHeader
 								domain={ domain }
-								basicMetrics={ basicMetrics }
+								overallScore={ overallScore }
 								onGetReport={ () => setIsGetReportFormOpen( true ) }
 							/>
 						) }
