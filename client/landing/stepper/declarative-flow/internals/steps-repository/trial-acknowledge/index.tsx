6a4b443bbaad5b9ce6db3370af0d1b769a53f5de
Yan Sern <yansern@yan.sr>
2024-05-24 21:57:01 +0800

    Step last step during trial acknowledgement.

--- client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/index.tsx
@@ -3,6 +3,7 @@ import {
 	isNewHostedSiteCreationFlow,
 	isEntrepreneurSignupFlow,
 } from '@automattic/onboarding';
+import { useSaveEntrepreneurFlowPathStep } from 'calypso/landing/stepper/hooks/use-save-entrepreneur-flow-path-step';
 import { useSaveHostingFlowPathStep } from 'calypso/landing/stepper/hooks/use-save-hosting-flow-path-step';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { EntrepreneurTrialAcknowledge } from './entrepreneur-trial-acknowledge';
@@ -14,7 +15,15 @@ import './style.scss';
 const TrialAcknowledge: Step = function TrialAcknowledge( { navigation, flow, stepName } ) {
 	const { goBack } = navigation;
 
+	// TODO: Refactor component & hook to have better support for different flows.
+	// This won't execute for Entrepreneur flow has internally it checks for specifically hosting flow.
 	useSaveHostingFlowPathStep( flow, `/setup/${ flow }/${ stepName }` );
+
+	// This will execute for Entrepreneur flow
+	useSaveEntrepreneurFlowPathStep(
+		flow,
+		`/setup/${ flow }/${ stepName }${ window.location.search }`
+	);
 
 	const getStepContent = () => {
 		if ( isEntrepreneurSignupFlow( flow ) ) {
