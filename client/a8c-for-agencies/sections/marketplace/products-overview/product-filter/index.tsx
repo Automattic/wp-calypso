0265707ae64ff6bb026be4b745e4c1d84f180c24
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-05-24 15:58:40 +0800

    Fix overlapping issues.

--- client/a8c-for-agencies/sections/marketplace/products-overview/product-filter/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/products-overview/product-filter/index.tsx
@@ -2,12 +2,14 @@ import { SubmenuPopover, useSubmenuPopoverProps } from '@automattic/components';
 import { DropdownMenu, MenuGroup, MenuItem } from '@wordpress/components';
 import { Icon, chevronRight, funnel, check } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
+import { useEffect, useRef, useState } from 'react';
 import {
 	PRODUCT_FILTER_KEY_CATEGORIES,
 	PRODUCT_FILTER_KEY_PRICES,
 	PRODUCT_FILTER_KEY_TYPES,
 } from '../../constants';
 import { SelectedFilters } from '../../lib/product-filter';
+import useOnScreen from './hooks/use-on-screen';
 import useProductFilterOptions from './hooks/use-product-filter-options';
 
 import './style.scss';
@@ -57,6 +59,11 @@ type Props = {
 export default function ProductFilter( { selectedFilters, setSelectedFilters }: Props ) {
 	const translate = useTranslate();
 
+	const [ openDropdown, setOpenDropdown ] = useState< boolean >( false );
+
+	const ref = useRef< HTMLDivElement >( null );
+	const isVisible = useOnScreen( ref );
+
 	const {
 		[ PRODUCT_FILTER_KEY_CATEGORIES ]: categories,
 		[ PRODUCT_FILTER_KEY_TYPES ]: types,
@@ -73,35 +80,46 @@ export default function ProductFilter( { selectedFilters, setSelectedFilters }: 
 		} );
 	};
 
+	useEffect( () => {
+		// Dropdown doesn't play well with our layout when scrolling. We need to close it when the toggle is not visible to avoid overlapping issues.
+		if ( openDropdown && ! isVisible ) {
+			setOpenDropdown( false );
+		}
+	}, [ isVisible, openDropdown ] );
+
 	return (
-		<DropdownMenu
-			className="product-filter"
-			label={ translate( 'Filter' ) }
-			icon={ funnel }
-			variant="product-filter"
-		>
-			{ () => (
-				<MenuGroup className="product-filter__group">
-					<ProductFilterItem
-						label={ translate( 'Category' ) }
-						options={ categories }
-						selectedOptions={ selectedFilters[ PRODUCT_FILTER_KEY_CATEGORIES ] }
-						onOptionClick={ ( option ) => updateFilter( PRODUCT_FILTER_KEY_CATEGORIES, option ) }
-					/>
-					<ProductFilterItem
-						label={ translate( 'Type' ) }
-						options={ types }
-						selectedOptions={ selectedFilters[ PRODUCT_FILTER_KEY_TYPES ] }
-						onOptionClick={ ( option ) => updateFilter( PRODUCT_FILTER_KEY_TYPES, option ) }
-					/>
-					<ProductFilterItem
-						label={ translate( 'Price' ) }
-						options={ prices }
-						selectedOptions={ selectedFilters[ PRODUCT_FILTER_KEY_PRICES ] }
-						onOptionClick={ ( option ) => updateFilter( PRODUCT_FILTER_KEY_PRICES, option ) }
-					/>
-				</MenuGroup>
-			) }
-		</DropdownMenu>
+		<div ref={ ref }>
+			<DropdownMenu
+				className="product-filter"
+				label={ translate( 'Filter' ) }
+				icon={ funnel }
+				variant="product-filter"
+				open={ openDropdown }
+				onToggle={ () => setOpenDropdown( ! openDropdown ) }
+			>
+				{ () => (
+					<MenuGroup className="product-filter__group">
+						<ProductFilterItem
+							label={ translate( 'Category' ) }
+							options={ categories }
+							selectedOptions={ selectedFilters[ PRODUCT_FILTER_KEY_CATEGORIES ] }
+							onOptionClick={ ( option ) => updateFilter( PRODUCT_FILTER_KEY_CATEGORIES, option ) }
+						/>
+						<ProductFilterItem
+							label={ translate( 'Type' ) }
+							options={ types }
+							selectedOptions={ selectedFilters[ PRODUCT_FILTER_KEY_TYPES ] }
+							onOptionClick={ ( option ) => updateFilter( PRODUCT_FILTER_KEY_TYPES, option ) }
+						/>
+						<ProductFilterItem
+							label={ translate( 'Price' ) }
+							options={ prices }
+							selectedOptions={ selectedFilters[ PRODUCT_FILTER_KEY_PRICES ] }
+							onOptionClick={ ( option ) => updateFilter( PRODUCT_FILTER_KEY_PRICES, option ) }
+						/>
+					</MenuGroup>
+				) }
+			</DropdownMenu>
+		</div>
 	);
 }
