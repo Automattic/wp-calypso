b69300a67f517ff78decee7d244581b5d03e3c22
Omar Alshaker <omar@omaralshaker.com>
2024-05-28 15:34:50 +0200

    Simplify the hook

--- client/my-sites/plans/hooks/use-segmented-intent.tsx
+++ client/my-sites/plans/hooks/use-segmented-intent.tsx
@@ -18,27 +18,29 @@ export function useSegmentedIntent(
 	} );
 
 	const goals = data?.[ 'what-are-your-goals' ];
-	const intent = data?.[ 'what-brings-you-to-wordpress' ]?.[ 0 ];
+	const surveyedIntent = data?.[ 'what-brings-you-to-wordpress' ]?.[ 0 ];
 
-	if ( ! enabled || ! intent || ! goals ) {
+	if ( ! enabled || ! surveyedIntent || ! goals ) {
 		return fallback;
 	}
 
 	// Return default wpcom plans for migration flow.
-	if ( intent === 'migrate-or-import-site' && goals.includes( 'skip' ) ) {
+	if ( surveyedIntent === 'migrate-or-import-site' && goals.includes( 'skip' ) ) {
 		return fallback;
 	}
 
-	if ( intent === 'client' && goals.includes( 'skip' ) ) {
+	if ( surveyedIntent === 'client' && goals.includes( 'skip' ) ) {
 		return 'plans-segment-developer-or-agency';
 	}
 
 	// Handle different cases when intent is 'Create for self'
-	if ( intent === 'myself-business-or-friend' ) {
-		if ( goals.length === 0 ) {
-			return fallback;
-		}
-		if ( goals.includes( 'difm' ) ) {
+	if ( surveyedIntent === 'myself-business-or-friend' ) {
+		if (
+			goals.length === 0 ||
+			goals.includes( 'skip' ) ||
+			goals.includes( 'newsletter' ) ||
+			goals.includes( 'difm' )
+		) {
 			return fallback;
 		}
 		if ( goals.includes( 'sell' ) && ! goals.includes( 'difm' ) ) {
@@ -49,9 +51,6 @@ export function useSegmentedIntent(
 		}
 		if ( goals.includes( 'educational-or-nonprofit' ) ) {
 			return 'plans-segment-nonprofit';
-		}
-		if ( goals.includes( 'newsletter' ) ) {
-			return fallback;
 		}
 		// Catch-all case for when none of the specific goals are met
 		// This will also account for "( ! DIFM && ! Sell ) = Consumer / Business" condition
