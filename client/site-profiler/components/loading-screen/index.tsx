001538bd75dfecc78f3a287c8a66b71200942721
Gergely Csécsey <gcsecsey@gmail.com>
2024-05-30 12:27:04 +0100

    animate progress bar while loading

--- client/site-profiler/components/loading-screen/index.tsx
+++ client/site-profiler/components/loading-screen/index.tsx
@@ -1,12 +1,11 @@
 import { ProgressBar } from '@automattic/components';
 import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
+import { useEffect, useState } from 'react';
 import { LayoutBlock } from 'calypso/site-profiler/components/layout';
 import './styles.scss';
 
 const Progress = styled( ProgressBar )`
-	background-color: #349f4b;
-
 	div {
 		background: linear-gradient( 90deg, #3858e9 0%, #349f4b 100% );
 	}
@@ -15,11 +14,23 @@ const Progress = styled( ProgressBar )`
 export const LoadingScreen = () => {
 	const translate = useTranslate();
 
+	const [ progress, setProgress ] = useState( 0 );
+
+	const interval = setInterval( () => {
+		setProgress( progress + 20 );
+	}, 1000 );
+
+	useEffect( () => {
+		if ( progress >= 100 ) {
+			clearInterval( interval );
+		}
+	}, [ progress, interval ] );
+
 	return (
 		<LayoutBlock className="landing-page-header-block" width="medium">
 			<div className="landing-page-loading-screen">
 				<h2>{ translate( "Crunching your site's numbers…" ) }</h2>
-				<Progress value={ 20 } total={ 100 } />
+				<Progress value={ progress } total={ 100 } />
 			</div>
 		</LayoutBlock>
 	);
