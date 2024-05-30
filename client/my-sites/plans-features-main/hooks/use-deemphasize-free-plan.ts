daa2d2101202e6933e26c5abc2fe72189a228bc2
James Tien <southp0105@gmail.com>
2024-05-30 13:59:35 +0800

    Add the inline comment to explain further context.

--- client/my-sites/plans-features-main/hooks/use-deemphasize-free-plan.ts
+++ client/my-sites/plans-features-main/hooks/use-deemphasize-free-plan.ts
@@ -1,4 +1,5 @@
 function useDeemphasizeFreePlan( flowName?: string | null, paidDomainName?: string ): boolean {
+	// De-emphasize the Free plan as a CTA link on the main onboarding flow when a paid domain is picked. More context can be found in p2-p5uIfZ-f5p
 	return flowName === 'onboarding' && paidDomainName != null;
 }
 
