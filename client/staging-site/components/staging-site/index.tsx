e13e48efb8a01cf0d1f79e8089e985f27fd312cc
arthur <arthur.chu@automattic.com>
2024-05-30 19:04:48 +0900

    Update styles

--- client/staging-site/components/staging-site/index.tsx
+++ client/staging-site/components/staging-site/index.tsx
@@ -3,6 +3,7 @@ import StagingSiteProductionCard from 'calypso/my-sites/hosting/staging-site-car
 import { useSelector } from 'calypso/state';
 import isSiteWpcomStaging from 'calypso/state/selectors/is-site-wpcom-staging';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import './style.scss';
 
 const StagingSite = () => {
 	const selectedSiteId = useSelector( ( state ) => getSelectedSiteId( state ) ) ?? 0;
