09e9dd121bb95a5df3367669afc530cb7372be16
Roberto Aranda <roberto.aranda@automattic.com>
2024-05-31 09:39:04 +0200

    Add the rest of the metrics copies objects and update fid

--- client/site-profiler/components/basic-metrics/copies.tsx
+++ client/site-profiler/components/basic-metrics/copies.tsx
@@ -91,13 +91,293 @@ export function getCopies(
 		},
 	};
 
+	const fid: CopiesProps = {
+		title: translate( 'First Input Delay (FID)' ),
+		nonWpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s FID is %(value)fms, offering quick response times. Excellent job maintaining fast interactions!',
+					{ args: { value: basicMetrics?.fid?.value } }
+				),
+				solution: translate(
+					'Migrate to WordPress.com to sustain this performance and further improve response times.'
+				),
+				cta: translate( 'Boost FID performance' ),
+				url: migrateUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s FID is %(value)fms, slower than most. Aim for less than 100ms for better responsiveness.',
+					{
+						args: {
+							value: basicMetrics?.fid?.value,
+						},
+					}
+				),
+				solution: translate(
+					'Migrate to WordPress.com for improved FID and faster user interactions.'
+				),
+				cta: translate( 'Experience Better FID' ),
+				url: migrateUrl,
+			},
+		},
+		wpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s FID is %(value)fms, offering quick response times. Excellent job on maintaining swift interactions!',
+					{ args: { value: basicMetrics?.fid?.value } }
+				),
+				solution: translate(
+					'Maintain this top-notch responsiveness by regularly optimizing your site for consistent performance.'
+				),
+				cta: translate( 'Keep layout stable' ),
+				url: supportUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s FID is %(value)fms, slower than most. Aim for less than 100ms for better responsiveness.',
+					{ args: { value: basicMetrics?.fid?.value } }
+				),
+				solution: translate(
+					'Connect with a Happiness Engineer to optimize FID and improve your site’s responsiveness'
+				),
+				cta: translate( 'Enhance responsiveness' ),
+				url: supportUrl,
+			},
+		},
+	};
+
+	const fcp: CopiesProps = {
+		title: translate( 'Cumulative Layout Shift (CLS)' ),
+		nonWpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Migrate to WordPress.com for tools to reduce CLS and stabilize your layout.”'
+				),
+				cta: translate( 'Migrate for stability' ),
+				url: migrateUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Excellent job maintaining low shifts!',
+					{
+						args: {
+							value: basicMetrics?.cls?.value,
+						},
+					}
+				),
+				solution: translate(
+					'Migrate to WordPress.com to maintain this stability and further enhance your site’s layout performance.'
+				),
+				cta: translate( 'Enhance stability' ),
+				url: migrateUrl,
+			},
+		},
+		wpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Great job maintaining low shifts.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Keep up the good work! Continue using best practices to maintain your site’s stability.'
+				),
+				cta: translate( 'Explore Advanced Tips' ),
+				url: supportUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Connect with a Happiness Engineer to reduce CLS and ensure a more stable layout for your users.'
+				),
+				cta: translate( 'Improve layout stability' ),
+				url: supportUrl,
+			},
+		},
+	};
+
+	const lcp: CopiesProps = {
+		title: translate( 'Cumulative Layout Shift (CLS)' ),
+		nonWpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Migrate to WordPress.com for tools to reduce CLS and stabilize your layout.”'
+				),
+				cta: translate( 'Migrate for stability' ),
+				url: migrateUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Excellent job maintaining low shifts!',
+					{
+						args: {
+							value: basicMetrics?.cls?.value,
+						},
+					}
+				),
+				solution: translate(
+					'Migrate to WordPress.com to maintain this stability and further enhance your site’s layout performance.'
+				),
+				cta: translate( 'Enhance stability' ),
+				url: migrateUrl,
+			},
+		},
+		wpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Great job maintaining low shifts.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Keep up the good work! Continue using best practices to maintain your site’s stability.'
+				),
+				cta: translate( 'Explore Advanced Tips' ),
+				url: supportUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Connect with a Happiness Engineer to reduce CLS and ensure a more stable layout for your users.'
+				),
+				cta: translate( 'Improve layout stability' ),
+				url: supportUrl,
+			},
+		},
+	};
+
+	const ttfb: CopiesProps = {
+		title: translate( 'Cumulative Layout Shift (CLS)' ),
+		nonWpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Migrate to WordPress.com for tools to reduce CLS and stabilize your layout.”'
+				),
+				cta: translate( 'Migrate for stability' ),
+				url: migrateUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Excellent job maintaining low shifts!',
+					{
+						args: {
+							value: basicMetrics?.cls?.value,
+						},
+					}
+				),
+				solution: translate(
+					'Migrate to WordPress.com to maintain this stability and further enhance your site’s layout performance.'
+				),
+				cta: translate( 'Enhance stability' ),
+				url: migrateUrl,
+			},
+		},
+		wpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Great job maintaining low shifts.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Keep up the good work! Continue using best practices to maintain your site’s stability.'
+				),
+				cta: translate( 'Explore Advanced Tips' ),
+				url: supportUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Connect with a Happiness Engineer to reduce CLS and ensure a more stable layout for your users.'
+				),
+				cta: translate( 'Improve layout stability' ),
+				url: supportUrl,
+			},
+		},
+	};
+
+	const inp: CopiesProps = {
+		title: translate( 'Cumulative Layout Shift (CLS)' ),
+		nonWpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Migrate to WordPress.com for tools to reduce CLS and stabilize your layout.”'
+				),
+				cta: translate( 'Migrate for stability' ),
+				url: migrateUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Excellent job maintaining low shifts!',
+					{
+						args: {
+							value: basicMetrics?.cls?.value,
+						},
+					}
+				),
+				solution: translate(
+					'Migrate to WordPress.com to maintain this stability and further enhance your site’s layout performance.'
+				),
+				cta: translate( 'Enhance stability' ),
+				url: migrateUrl,
+			},
+		},
+		wpcom: {
+			good: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, ensuring a stable layout. Great job maintaining low shifts.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Keep up the good work! Continue using best practices to maintain your site’s stability.'
+				),
+				cta: translate( 'Explore Advanced Tips' ),
+				url: supportUrl,
+			},
+			poor: {
+				diagnostic: translate(
+					'Your site’s CLS is %(value)f, higher than average, causing noticeable shifts. Aim for 0.1 for smoother layout.',
+					{ args: { value: basicMetrics?.cls?.value } }
+				),
+				solution: translate(
+					'Connect with a Happiness Engineer to reduce CLS and ensure a more stable layout for your users.'
+				),
+				cta: translate( 'Improve layout stability' ),
+				url: supportUrl,
+			},
+		},
+	};
+
 	// TODO: Add the rest of the metrics
 	return {
-		ttfb: cls,
-		fcp: cls,
-		lcp: cls,
-		inp: cls,
+		ttfb,
+		fcp,
+		lcp,
+		inp,
 		cls,
-		fid: cls,
+		fid,
 	};
 }
