c302d14e6d0495627c89ef5217d17b858c69ef3e
Yashwin <yashwinpoojary@gmail.com>
2024-05-31 09:28:27 +0530

    Implement Client UI

--- client/state/a8c-for-agencies/reducer.js
+++ client/state/a8c-for-agencies/reducer.js
@@ -1,9 +1,11 @@
 import { withStorageKey } from '@automattic/state-utils';
 import agencies from 'calypso/state/a8c-for-agencies/agency/reducer';
+import client from 'calypso/state/a8c-for-agencies/client/reducer';
 import { combineReducers } from 'calypso/state/utils';
 
 const combinedReducer = combineReducers( {
 	agencies,
+	client,
 } );
 
 export default withStorageKey( 'a8cForAgencies', combinedReducer );
