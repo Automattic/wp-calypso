95badb96d4b94cee0bec97f9c95fb8515402a3ae
cpapazoglou <hrrsppzgl@gmail.com>
2024-05-31 18:56:50 +0300

    Site Profiler: progress bar styles.

--- client/site-profiler/components/loading-screen/index.tsx
+++ client/site-profiler/components/loading-screen/index.tsx
@@ -3,11 +3,18 @@ import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect, useState } from 'react';
 import { LayoutBlock } from 'calypso/site-profiler/components/layout';
-import './styles.scss';
 
 const Progress = styled( ProgressBar )`
 	div {
 		background: linear-gradient( 90deg, #3858e9 0%, #349f4b 100% );
+	}
+`;
+
+const StyledLoadingScreen = styled.div`
+	padding: 100px 40px;
+
+	h2 {
+		text-align: center;
 	}
 `;
 
@@ -28,17 +35,17 @@ export const LoadingScreen = () => {
 			if ( progress >= 100 ) {
 				setProgress( 0 );
 			} else {
-				setProgress( progress + 10 );
+				setProgress( progress + 3 );
 			}
 		}, 1000 );
 	}, [ progress ] );
 
 	return (
 		<LayoutBlock className="landing-page-header-block" width="medium">
-			<div className="landing-page-loading-screen">
+			<StyledLoadingScreen>
 				<h2>{ progressHeadings[ Math.floor( ( progress / 101 ) * progressHeadings.length ) ] }</h2>
 				<Progress value={ progress } total={ 100 } canGoBackwards />
-			</div>
+			</StyledLoadingScreen>
 		</LayoutBlock>
 	);
 };
