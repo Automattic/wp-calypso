ea38282342d30c74c16c51c00ce344239d7ab581
Anthony Grullon <agrullon95@gmail.com>
2024-06-03 13:00:51 -0400

    Remove email support variation mode

--- packages/help-center/src/support-variations.ts
+++ packages/help-center/src/support-variations.ts
@@ -1,15 +1,12 @@
 export const SUPPORT_FORUM = 'SUPPORT_FORUM';
-export const SUPPORT_TICKET = 'SUPPORT_TICKET';
 export const SUPPORT_UPWORK_TICKET = 'SUPPORT_UPWORK_TICKET';
 
 /**
  * @param {string} mode Help Center contact form mode
  * @returns {string} One of the exported support variation constants listed above
  */
-export const getSupportVariationFromMode = ( mode: 'EMAIL' | 'FORUM' | 'UPWORK' ) => {
+export const getSupportVariationFromMode = ( mode: 'FORUM' | 'UPWORK' ) => {
 	switch ( mode ) {
-		case 'EMAIL':
-			return SUPPORT_TICKET;
 		case 'FORUM':
 			return SUPPORT_FORUM;
 		case 'UPWORK':
