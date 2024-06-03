0fe2515c544487e197ad0a8bfc5f7bb0d504ba83
Anthony Grullon <agrullon95@gmail.com>
2024-06-03 12:44:40 -0400

    Remove SUPPORT_CHAT const and form mode

--- packages/help-center/src/support-variations.ts
+++ packages/help-center/src/support-variations.ts
@@ -1,6 +1,4 @@
-export const SUPPORT_CHAT_OVERFLOW = 'SUPPORT_CHAT_OVERFLOW';
 export const SUPPORT_FORUM = 'SUPPORT_FORUM';
-export const SUPPORT_CHAT = 'SUPPORT_CHAT';
 export const SUPPORT_TICKET = 'SUPPORT_TICKET';
 export const SUPPORT_UPWORK_TICKET = 'SUPPORT_UPWORK_TICKET';
 
@@ -8,10 +6,8 @@ export const SUPPORT_UPWORK_TICKET = 'SUPPORT_UPWORK_TICKET';
  * @param {string} mode Help Center contact form mode
  * @returns {string} One of the exported support variation constants listed above
  */
-export const getSupportVariationFromMode = ( mode: 'CHAT' | 'EMAIL' | 'FORUM' | 'UPWORK' ) => {
+export const getSupportVariationFromMode = ( mode: 'EMAIL' | 'FORUM' | 'UPWORK' ) => {
 	switch ( mode ) {
-		case 'CHAT':
-			return SUPPORT_CHAT;
 		case 'EMAIL':
 			return SUPPORT_TICKET;
 		case 'FORUM':
