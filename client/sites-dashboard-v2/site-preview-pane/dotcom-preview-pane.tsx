b8c6d1c6defe1840fd1cf10c789b1e4100615611
Griffith Chen <griffith.chen@automattic.com>
2024-06-04 09:33:53 +0800

    Update Site Management Panel tab names

--- client/sites-dashboard-v2/site-preview-pane/dotcom-preview-pane.tsx
+++ client/sites-dashboard-v2/site-preview-pane/dotcom-preview-pane.tsx
@@ -1,3 +1,4 @@
+import { useHasEnTranslation } from '@automattic/i18n-utils';
 import { SiteExcerptData } from '@automattic/sites';
 import { useI18n } from '@wordpress/react-i18n';
 import React, { useMemo, useEffect } from 'react';
@@ -39,6 +40,7 @@ const DotcomPreviewPane = ( {
 	closeSitePreviewPane,
 }: Props ) => {
 	const { __ } = useI18n();
+	const hasEnTranslation = useHasEnTranslation();
 
 	const isAtomicSite = !! site.is_wpcom_atomic || !! site.is_wpcom_staging_site;
 	const isSimpleSite = ! site.jetpack;
@@ -57,7 +59,8 @@ const DotcomPreviewPane = ( {
 			createFeaturePreview(
 				DOTCOM_DEVELOPER_TOOLS,
 				<span>
-					{ __( 'Dev Tools' ) } <DevToolsIcon />
+					{ hasEnTranslation ? __( 'Hosting Settings' ) : __( 'Dev Tools' ) }
+					<DevToolsIcon />
 				</span>,
 				isSimpleSite || isPlanExpired,
 				selectedSiteFeature,
@@ -98,7 +101,7 @@ const DotcomPreviewPane = ( {
 			),
 			createFeaturePreview(
 				DOTCOM_HOSTING_CONFIG,
-				__( 'Server Config' ),
+				hasEnTranslation ? __( 'Server Settings' ) : __( 'Server Config' ),
 				isAtomicSite && ! isPlanExpired,
 				selectedSiteFeature,
 				setSelectedSiteFeature,
