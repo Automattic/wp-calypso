60da75ae7d8d6653e89b708cb99edb1d6ee2d949
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-06-04 12:46:34 +0200

    Add missing description for the Form section.

--- client/a8c-for-agencies/components/form/section/index.tsx
+++ client/a8c-for-agencies/components/form/section/index.tsx
@@ -5,14 +5,16 @@ import './style.scss';
 
 type Props = {
 	title?: string;
+	description?: string;
 	children: ReactNode;
 };
 
-export default function FormSection( { title, children }: Props ) {
+export default function FormSection( { title, description, children }: Props ) {
 	return (
 		<Card className="a4a-form__section" isRounded={ false }>
 			<div className="a4a-form__section-heading">
 				<h2 className="a4a-form__section-heading-title">{ title }</h2>
+				{ description && <p className="a4a-form__section-heading-description">{ description }</p> }
 			</div>
 
 			<div className="a4a-form__section-body">{ children }</div>
