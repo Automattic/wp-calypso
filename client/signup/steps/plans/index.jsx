352cc85a2d052c782546c35a74bd3ab2d88cf3a0
Omar Alshaker <omar@omaralshaker.com>
2024-06-04 17:39:11 +0200

    Guided Onboarding: deemphasize free plan for paid domains (#91444)
    Co-authored-by: escapemanuele <escapemanuele@gmail.com>

--- client/signup/steps/plans/index.jsx
+++ client/signup/steps/plans/index.jsx
@@ -129,10 +129,11 @@ export class PlansStep extends Component {
 			freeWPComSubdomain = siteUrl;
 		}
 
-		// De-emphasize the Free plan as a CTA link on the main onboarding flow when a paid domain is picked.
+		// De-emphasize the Free plan as a CTA link on the main onboarding flow, and the guided flow, when a paid domain is picked.
 		// More context can be found in p2-p5uIfZ-f5p
 		const deemphasizeFreePlan =
-			( flowName === 'onboarding' && paidDomainName != null ) || deemphasizeFreePlanFromProps;
+			( [ 'onboarding', 'guided' ].includes( flowName ) && paidDomainName != null ) ||
+			deemphasizeFreePlanFromProps;
 
 		return (
 			<div>
