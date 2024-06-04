4160afa909b0ea9dc1aded719e25f5aa5184d742
Omar Alshaker <omar@omaralshaker.com>
2024-06-04 12:42:03 +0200

    Update code comment

--- client/signup/steps/plans/index.jsx
+++ client/signup/steps/plans/index.jsx
@@ -129,7 +129,7 @@ export class PlansStep extends Component {
 			freeWPComSubdomain = siteUrl;
 		}
 
-		// De-emphasize the Free plan as a CTA link on the main onboarding flow when a paid domain is picked.
+		// De-emphasize the Free plan as a CTA link on the main onboarding flow, and the guided flow, when a paid domain is picked.
 		// More context can be found in p2-p5uIfZ-f5p
 		const deemphasizeFreePlan =
 			( [ 'onboarding', 'guided' ].includes( flowName ) && paidDomainName != null ) ||
