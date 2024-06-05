9ed970132631f09c9fc862be232c4f2e4c529930
cleacos <javacodia@gmail.com>
2024-06-05 12:01:33 +0200

    Common selector components: Industry, Languages, Products, Services

---
+++ client/a8c-for-agencies/sections/partner-directory/components/products-selector.tsx
@@ -0,0 +1,29 @@
+import { FormTokenField } from '@wordpress/components';
+import { TokenItem } from '@wordpress/components/build-types/form-token-field/types';
+
+type Props = {
+	setProducts: ( tokens: ( string | TokenItem )[] ) => void;
+	selectedProducts: string[] | undefined;
+};
+
+const ProductsSelector = ( { setProducts, selectedProducts }: Props ) => {
+	const availableProducts: string[] = [
+		'WordPress',
+		'WooCommerce',
+		'Jetpack',
+		'WordPress VIP',
+		'Pressable',
+	];
+
+	return (
+		<FormTokenField
+			__experimentalAutoSelectFirstMatch
+			__experimentalExpandOnFocus
+			onChange={ setProducts }
+			suggestions={ availableProducts }
+			value={ selectedProducts }
+		/>
+	);
+};
+
+export default ProductsSelector;
