beaa489db0fa5a5aeeb8e146e644b3956ad13964
Eoin Gallagher <eoin@automattic.com>
2024-06-06 17:12:47 +0100

    Add A4A plan description to plan card

--- client/hosting-overview/components/plan-card.tsx
+++ client/hosting-overview/components/plan-card.tsx
@@ -256,6 +256,17 @@ const PlanCard: FC = () => {
 						</PlanStorage>
 					</>
 				) }
+				{ isAgencyPurchase && (
+					<div className="hosting-overview__plan-agency-purchase">
+						{ translate( 'This site is managed through {{a}}Automattic for Agencies{{/a}}.', {
+							components: {
+								a: (
+									<a href={ `https://agencies.automattic.com/sites/overview/${ site?.slug }` }></a>
+								),
+							},
+						} ) }
+					</div>
+				) }
 			</HostingCard>
 		</>
 	);
