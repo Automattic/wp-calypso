78ee55cd478eb7195f151f0563ba2fc6d6ab6f28
heavyweight <kpapazov@gmail.com>
2024-06-06 16:45:39 +0200

    Add different comparison plans for guided onboarding

--- packages/plans-grid-next/src/hooks/data-store/use-grid-plans-for-comparison-grid.ts
+++ packages/plans-grid-next/src/hooks/data-store/use-grid-plans-for-comparison-grid.ts
@@ -6,9 +6,25 @@ import { useMemo } from '@wordpress/element';
 import useGridPlans from './use-grid-plans';
 import useRestructuredPlanFeaturesForComparisonGrid from './use-restructured-plan-features-for-comparison-grid';
 import type { UseGridPlansParams } from './types';
-import type { GridPlan } from '../../types';
+import type { GridPlan, PlansIntent } from '../../types';
 
 const HIDDEN_PLANS = [ PLAN_HOSTING_TRIAL_MONTHLY, PLAN_ENTERPRISE_GRID_WPCOM ];
+
+const getIntentForComparisonTable = ( intent: PlansIntent ): PlansIntent => {
+	if (
+		[
+			'plans-guided-segment-merchant',
+			'plans-guided-segment-blogger',
+			'plans-guided-segment-nonprofit',
+			'plans-guided-segment-consumer-or-business',
+			'plans-guided-segment-developer-or-agency',
+		].includes( intent ?? '' )
+	) {
+		return 'plans-default-wpcom';
+	}
+
+	return intent;
+};
 
 const useGridPlansForComparisonGrid = ( {
 	allFeaturesList,
@@ -28,12 +44,13 @@ const useGridPlansForComparisonGrid = ( {
 	useCheckPlanAvailabilityForPurchase,
 	useFreeTrialPlanSlugs,
 }: UseGridPlansParams ): GridPlan[] | null => {
+	const comparisonTableIntent = getIntentForComparisonTable( intent ?? 'default' );
 	const gridPlans = useGridPlans( {
 		allFeaturesList,
 		coupon,
 		eligibleForFreeHostingTrial,
 		hiddenPlans,
-		intent,
+		intent: comparisonTableIntent,
 		isDisplayingPlansNeededForFeature,
 		isSubdomainNotGenerated,
 		selectedFeature,
