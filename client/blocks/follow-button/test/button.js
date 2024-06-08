dcedd003ad652b28e89d0b6552522c2cb6e60053
Enej Bajgoric <enej.bajgoric@automattic.com>
2024-06-07 17:11:39 -0700

    Remove test

--- client/blocks/follow-button/test/button.js
+++ client/blocks/follow-button/test/button.js
@@ -21,19 +21,4 @@ describe( 'FollowButton', () => {
 
 		expect( screen.getByText( 'Follow Tag' ) ).toBeVisible();
 	} );
-
-	test( 'should NOT apply a custom follow label', () => {
-		const initialState = createReduxStore( {
-			currentUser: {
-				email_verified: false,
-			},
-		} );
-		render(
-			<Provider store={ initialState }>
-				<FollowButton followLabel="Follow Tag" />
-			</Provider>
-		);
-
-		expect( screen.getByText( 'Follow Tag' ) ).toBeFalsy();
-	} );
 } );
