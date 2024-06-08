5d47f61177bc63e047dcd35423348f3b0993c081
Omar Alshaker <omar@omaralshaker.com>
2024-06-08 15:21:49 +0200

    Revert "Simplify OAuth handling"
    This reverts commit bf9d5b5fb64ebd0d178d85ce8c5a6b9d0b42e77d.

--- packages/data-stores/src/user/actions.ts
+++ packages/data-stores/src/user/actions.ts
@@ -1,9 +1,14 @@
-import type { CurrentUser } from './types';
+import type { AuthRedirectParams, CurrentUser } from './types';
 
 export function createActions() {
 	const receiveCurrentUser = ( currentUser: CurrentUser ) => ( {
 		type: 'RECEIVE_CURRENT_USER' as const,
 		currentUser,
+	} );
+
+	const receiveAuthRedirectParams = ( authRedirectParams: AuthRedirectParams ) => ( {
+		type: 'RECEIVE_AUTH_REDIRECT' as const,
+		authRedirectParams,
 	} );
 
 	const receiveCurrentUserFailed = () => ( {
@@ -13,6 +18,7 @@ export function createActions() {
 	return {
 		receiveCurrentUser,
 		receiveCurrentUserFailed,
+		receiveAuthRedirectParams,
 	};
 }
 
@@ -20,7 +26,9 @@ export type ActionCreators = ReturnType< typeof createActions >;
 
 export type Action =
 	| ReturnType<
-			ActionCreators[ 'receiveCurrentUser' ] | ActionCreators[ 'receiveCurrentUserFailed' ]
+			| ActionCreators[ 'receiveCurrentUser' ]
+			| ActionCreators[ 'receiveCurrentUserFailed' ]
+			| ActionCreators[ 'receiveAuthRedirectParams' ]
 	  >
 	// Type added so we can dispatch actions in tests, but has no runtime cost
 	| { type: 'TEST_ACTION' };
--- packages/data-stores/src/user/reducer.ts
+++ packages/data-stores/src/user/reducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from '@wordpress/data';
 import type { Action } from './actions';
-import type { CurrentUser } from './types';
+import type { AuthRedirectParams, CurrentUser } from './types';
 import type { Reducer } from 'redux';
 
 export const currentUser: Reducer< CurrentUser | null | undefined, Action > = ( state, action ) => {
@@ -13,7 +13,18 @@ export const currentUser: Reducer< CurrentUser | null | undefined, Action > = ( 
 	return state;
 };
 
-const reducer = combineReducers( { currentUser } );
+export const authRedirectParams: Reducer< AuthRedirectParams | null | undefined, Action > = (
+	state,
+	action
+) => {
+	switch ( action.type ) {
+		case 'RECEIVE_AUTH_REDIRECT':
+			return action.authRedirectParams;
+	}
+	return state;
+};
+
+const reducer = combineReducers( { currentUser, authRedirectParams } );
 export type State = ReturnType< typeof reducer >;
 
 export default reducer;
