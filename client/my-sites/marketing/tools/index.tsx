626f3fc9d9975e3a2f8e77ae765e0c2b6fa63360
Renatho De Carli Rosa <renatho@gmail.com>
2024-06-10 11:53:07 -0300

    Add Mailchimp feature as a marketing tool

--- client/my-sites/marketing/tools/index.tsx
+++ client/my-sites/marketing/tools/index.tsx
@@ -49,6 +49,10 @@ export const MarketingTools: FunctionComponent = () => {
 		recordTracksEvent( 'calypso_marketing_tools_create_a_logo_button_click' );
 	};
 
+	const handleMailchimpClick = () => {
+		// recordTracksEvent( 'calypso_marketing_tools_mailchimp_button_click' );
+	};
+
 	const handleVerblioClick = () => {
 		recordTracksEvent( 'calypso_marketing_tools_verblio_button_click' );
 	};
@@ -107,6 +111,17 @@ export const MarketingTools: FunctionComponent = () => {
 					>
 						{ translate( 'Create a logo' ) }
 					</Button>
+				</MarketingToolsFeature>
+
+				<MarketingToolsFeature
+					title={ translate( 'Grow your business with Mailchimp' ) }
+					description={ translate(
+						'Create newsletters, automate your email and SMS marketing campaigns, track your subscriber activity, and target your customers with personalized messages. Enhance your site and build long-term relationships with your audience.'
+					) }
+					imagePath="/calypso/images/marketing/mailchimp-logo.svg"
+					imageAlt={ translate( 'Mailchimp logo' ) }
+				>
+					<Button onClick={ handleMailchimpClick }>{ translate( 'Get started today' ) }</Button>
 				</MarketingToolsFeature>
 
 				<MarketingToolsFeature
