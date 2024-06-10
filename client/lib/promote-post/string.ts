ce3e80ddcd3e1dc0378a706f2ff51427b4c368d0
halldorth <halldor.thorhallsson@automattic.com>
2024-06-10 11:01:59 -0700

    Make string have 3 dots instead of 4

--- client/lib/promote-post/string.ts
+++ client/lib/promote-post/string.ts
@@ -9,9 +9,13 @@ const BlazePressStrings = () => {
 	translate( 'The campaign cannot be created. Please {{a}}contact our support team{{/a}}.' );
 	translate( 'Go to my campaigns' );
 	translate( 'All set!' );
-	translate( 'The ad has been submitted for approval. We’ll send you a confirmation email once it’s approved and running.' );
+	translate(
+		'The ad has been submitted for approval. We’ll send you a confirmation email once it’s approved and running.'
+	);
 	translate( 'Skip and go to my campaigns next time.' );
-	translate( 'Learn more about the {{linkAdvertisingPolicy}}Advertising Policy{{/linkAdvertisingPolicy}}.' );
+	translate(
+		'Learn more about the {{linkAdvertisingPolicy}}Advertising Policy{{/linkAdvertisingPolicy}}.'
+	);
 	translate( 'Creating campaign…' );
 	translate( 'Make the most of your Blaze campaign' );
 	translate( 'Choose an eye-catching image for your ad' );
@@ -19,7 +23,7 @@ const BlazePressStrings = () => {
 	translate( 'Pick the right audience, budget and duration' );
 	translate( 'Get started' );
 	translate( 'Learn more' );
-	translate( 'Don\'t show me this step again.' );
+	translate( "Don't show me this step again." );
 	translate( 'Uploading images...' );
 	translate( 'Checking payment information…' );
 	translate( 'Fetching pages…' );
@@ -40,7 +44,9 @@ const BlazePressStrings = () => {
 	translate( 'Visitors' );
 	translate( 'Likes' );
 	translate( 'Comments' );
-	translate( 'Cannot create subscription. Please {{supportLink}}contact support{{/supportLink}} or try again later.' );
+	translate(
+		'Cannot create subscription. Please {{supportLink}}contact support{{/supportLink}} or try again later.'
+	);
 	translate( 'Advanced Settings' );
 	translate( 'Targeted Devices' );
 	translate( 'Destination URL' );
@@ -51,16 +57,22 @@ const BlazePressStrings = () => {
 	translate( 'URL parameters' );
 	translate( 'key1=value1&key2=value2&key3=value3' );
 	translate( 'URL parameters are invalid' );
-	translate( '%(charactersLeft)s character remaining', '%(charactersLeft)s characters remaining', { count: 1 } );
+	translate( '%(charactersLeft)s character remaining', '%(charactersLeft)s characters remaining', {
+		count: 1,
+	} );
 	translate( 'Appearance' );
 	translate( 'Ad creative' );
-	translate( 'Use post\'s media' );
+	translate( "Use post's media" );
 	translate( 'Site title' );
 	translate( 'Loading page title...' );
 	translate( 'Page title' );
 	translate( 'Snippet' );
-	translate( '%(snippetCharactersLeft)s character remaining', '%(snippetCharactersLeft)s characters remaining', { count: 1 } );
-	translate( 'Loading ad text....' );
+	translate(
+		'%(snippetCharactersLeft)s character remaining',
+		'%(snippetCharactersLeft)s characters remaining',
+		{ count: 1 }
+	);
+	translate( 'Loading ad text…' );
 	translate( 'Ad text' );
 	translate( 'Use + / - or simply drag the image to adjust it' );
 	translate( 'Apply' );
@@ -75,7 +87,11 @@ const BlazePressStrings = () => {
 	translate( 'Drop images to upload' );
 	translate( 'Use selected' );
 	translate( 'Load More' );
-	translate( 'Showing %(found)s of %(found)s media item', 'Showing %(found)s of %(found)s media items', { count: 1 } );
+	translate(
+		'Showing %(found)s of %(found)s media item',
+		'Showing %(found)s of %(found)s media items',
+		{ count: 1 }
+	);
 	translate( 'Search images' );
 	translate( 'Search' );
 	translate( 'Select' );
@@ -85,11 +101,15 @@ const BlazePressStrings = () => {
 	translate( 'Click or drag an image here to upload.' );
 	translate( 'Upload an image file, or pick one from your media library.' );
 	translate( 'Audience' );
-	translate( 'Not enough reach to create a campaign with the current audience configuration. Please, expand your reach by changing or removing some of the audience settings' );
+	translate(
+		'Not enough reach to create a campaign with the current audience configuration. Please, expand your reach by changing or removing some of the audience settings'
+	);
 	translate( 'Language' );
 	translate( 'Location' );
 	translate( 'All languages' );
-	translate( 'Based on the language of your site we suggest targeting %(lang)s speaking users to ensure the ad is seen by the right audience and to increase its effectiveness.' );
+	translate(
+		'Based on the language of your site we suggest targeting %(lang)s speaking users to ensure the ad is seen by the right audience and to increase its effectiveness.'
+	);
 	translate( 'All topics' );
 	translate( 'Interests' );
 	translate( 'Budget and duration' );
@@ -101,13 +121,19 @@ const BlazePressStrings = () => {
 	translate( 'Duration (days)' );
 	translate( 'Weekly Total' );
 	translate( 'Total' );
-	translate( 'Daily spend for %(durationDays)s-day duration', 'Daily spend for %(durationDays)s day duration', { count: 1 } );
+	translate(
+		'Daily spend for %(durationDays)s-day duration',
+		'Daily spend for %(durationDays)s day duration',
+		{ count: 1 }
+	);
 	translate( 'Estimated people reached per day' );
 	translate( 'days' );
 	translate( 'Credits will be automatically applied to your order when available.' );
 	translate( 'Credits: %(creditsUsed)s (%(remainingCredit)s remain)' );
 	translate( 'Review your campaign' );
-	translate( 'We created this campaign to deliver the most valuable traffic, yet you can still make changes before submitting it.' );
+	translate(
+		'We created this campaign to deliver the most valuable traffic, yet you can still make changes before submitting it.'
+	);
 	translate( 'Campaign Objective' );
 	translate( 'Change' );
 	translate( 'Start Date' );
@@ -146,9 +172,15 @@ const BlazePressStrings = () => {
 	translate( 'Error setting up payment. Please try again later' );
 	translate( 'Error submitting payment. Please check payment information.' );
 	translate( 'Error fetching payment method. Please try again later.' );
-	translate( 'There was an error with the address. Please verify that all the required data is valid' );
-	translate( 'There was an error with the address. The province, state or region should be filled' );
-	translate( 'There was an error with the address. Please, check that the Zip code exists, is valid for the country, and corresponds for the given address' );
+	translate(
+		'There was an error with the address. Please verify that all the required data is valid'
+	);
+	translate(
+		'There was an error with the address. The province, state or region should be filled'
+	);
+	translate(
+		'There was an error with the address. Please, check that the Zip code exists, is valid for the country, and corresponds for the given address'
+	);
 	translate( 'Use saved card' );
 	translate( 'First Name' );
 	translate( 'Last Name' );
@@ -168,7 +200,9 @@ const BlazePressStrings = () => {
 	translate( 'Untitled' );
 	translate( 'View' );
 	translate( 'Promote' );
-	translate( 'By clicking \"Submit campaign\" you agree to the {{linkTos}}Terms of Service{{externalLinkIcon/}}{{/linkTos}} and {{linkAdvertisingPolicy}}Advertising Policy{{externalLinkIcon/}}{{/linkAdvertisingPolicy}}, and authorize your payment method to be charged for the budget and duration you chose. {{linkMoreAboutAds}}Learn more{{externalLinkIcon/}}{{/linkMoreAboutAds}} about how budgets and payments for Promoted Posts work.' );
+	translate(
+		'By clicking "Submit campaign" you agree to the {{linkTos}}Terms of Service{{externalLinkIcon/}}{{/linkTos}} and {{linkAdvertisingPolicy}}Advertising Policy{{externalLinkIcon/}}{{/linkAdvertisingPolicy}}, and authorize your payment method to be charged for the budget and duration you chose. {{linkMoreAboutAds}}Learn more{{externalLinkIcon/}}{{/linkMoreAboutAds}} about how budgets and payments for Promoted Posts work.'
+	);
 	translate( 'Creating campaign' );
 	translate( 'Submit campaign' );
 	translate( 'Make changes' );
@@ -192,7 +226,9 @@ const BlazePressStrings = () => {
 	translate( 'Oops, something went wrong' );
 	translate( 'Loading site…' );
 	translate( 'Fetching subscriptions…' );
-	translate( 'There was a problem getting the necessary information to create your campaign. Please try again soon or {{a}}contact our support team{{/a}} for help.' );
+	translate(
+		'There was a problem getting the necessary information to create your campaign. Please try again soon or {{a}}contact our support team{{/a}} for help.'
+	);
 	translate( 'Preparing the wizard…' );
 	translate( 'Drop image here' );
 	translate( 'Click or Drag an image here' );
