326eccb5fddc43963ba38aece1194f299df0fec3
cleacos <javacodia@gmail.com>
2024-06-11 00:07:08 +0100

    Update IndustrySelector removing disabled: true

--- client/a8c-for-agencies/sections/partner-directory/components/industry-selector.tsx
+++ client/a8c-for-agencies/sections/partner-directory/components/industry-selector.tsx
@@ -11,52 +11,42 @@ const IndustrySelector = ( { setIndustry, industry }: Props ) => {
 
 	const industries = [
 		{
-			disabled: true,
 			label: translate( 'Agricultural services' ),
 			value: 'agricultural_services',
 		},
 		{
-			disabled: true,
 			label: translate( 'Contracted services' ),
 			value: 'contracted_services',
 		},
 		{
-			disabled: true,
 			label: translate( 'Transportation services' ),
 			value: 'transportation_services',
 		},
 		{
-			disabled: true,
 			label: translate( 'Utility services' ),
 			value: 'utility_services',
 		},
 		{
-			disabled: true,
 			label: translate( 'Retail outlet services' ),
 			value: 'retail_outlet_services',
 		},
 		{
-			disabled: true,
 			label: translate( 'Clothing shops' ),
 			value: 'clothing_shops',
 		},
 		{
-			disabled: true,
 			label: translate( 'Miscellaneous shops' ),
 			value: 'miscellaneous_shops',
 		},
 		{
-			disabled: true,
 			label: translate( 'Business services' ),
 			value: 'business_services',
 		},
 		{
-			disabled: true,
 			label: translate( 'Professional services and membership organisations' ),
 			value: 'professional_services_and_membership_organisations',
 		},
 		{
-			disabled: true,
 			label: translate( 'Government services' ),
 			value: 'government_services',
 		},
