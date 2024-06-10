bf2b7a923b25ba3a031c4aaaae8b6b8e585675a2
Imran Hossain <imranh920@gmail.com>
2024-06-10 21:11:35 +0600

    Update tests

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-identify/test/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-identify/test/index.tsx
@@ -163,7 +163,7 @@ describe( 'SiteMigrationIdentify', () => {
 		expect( screen.getByText( /Unmatched Reliability and Uptime/ ) ).toBeVisible();
 		expect(
 			screen.getByText(
-				/Our rock-solid infrastructure ensures 99.999% uptime, keeping your site always accessible to your users no matter what!/
+				/Our infrastructure ensures 99.999% uptime and automatic WordPress updates, your site is always accessible and updated!/
 			)
 		).toBeVisible();
 	} );
