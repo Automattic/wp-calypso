9d5730e85e7af041a33d73ea234d6ae1f6764307
Dean Sas <dean@deansas.org>
2024-06-11 13:26:56 +0100

    make prettier

--- packages/command-palette/src/tracks.ts
+++ packages/command-palette/src/tracks.ts
@@ -1,6 +1,9 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 
-export const recordCommandPaletteOpen = ( currentRoute: string, openType: 'keyboard' | 'sidebar_click' ) => {
+export const recordCommandPaletteOpen = (
+	currentRoute: string,
+	openType: 'keyboard' | 'sidebar_click'
+) => {
 	recordTracksEvent( 'calypso_hosting_command_palette_open', {
 		current_route: currentRoute,
 		opened_with: openType,
