9fcb889b3969bb266055a9babef2920c7824398c
Dean Sas <dean.sas@automattic.com>
2024-06-11 13:20:42 +0100

    Update packages/command-palette/src/tracks.ts
    Co-authored-by: Xavier Lozano Carreras <8511199+xavier-lc@users.noreply.github.com>

--- packages/command-palette/src/tracks.ts
+++ packages/command-palette/src/tracks.ts
@@ -1,6 +1,6 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 
-export const recordCommandPaletteOpen = ( currentRoute: string, openType: string ) => {
+export const recordCommandPaletteOpen = ( currentRoute: string, openType: 'keyboard' | 'sidebar_click' ) => {
 	recordTracksEvent( 'calypso_hosting_command_palette_open', {
 		current_route: currentRoute,
 		opened_with: openType,
