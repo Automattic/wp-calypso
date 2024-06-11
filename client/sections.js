15c42c913a6dc7b9f1e882ca3c9f5032a94d64a3
Yashwin <yashwinpoojary@gmail.com>
2024-06-11 14:38:18 +0530

    Address PR comment

--- client/sections.js
+++ client/sections.js
@@ -834,12 +834,7 @@ const sections = [
 	},
 	{
 		name: 'a8c-for-agencies-client',
-		paths: [
-			'/client/landing',
-			'/client/subscriptions',
-			'/client/payment-methods',
-			'/purchases/payment-methods/add',
-		],
+		paths: [ '/client/landing', '/client/subscriptions', '/client/payment-methods' ],
 		module: 'calypso/a8c-for-agencies/sections/client',
 		group: 'a8c-for-agencies',
 	},
