b42a48be2f3040ed41fb5f434fc617b193ad717a
Candy Tsai <candy02058912@gmail.com>
2024-06-12 11:09:46 +0800

    Capitalize first letter for fallback category name

--- client/my-sites/plugins/plugins-browser/index.jsx
+++ client/my-sites/plugins/plugins-browser/index.jsx
@@ -96,7 +96,8 @@ const PluginsBrowser = ( { trackPageViews = true, category, search, hideHeader }
 	const locale = useLocale();
 
 	const categories = useCategories();
-	const categoryName = categories[ category ]?.menu || category;
+	const fallbackCategoryName = category.charAt( 0 ).toUpperCase() + category.slice( 1 );
+	const categoryName = categories[ category ]?.menu || fallbackCategoryName;
 
 	// this is a temporary hack until we merge Phase 4 of the refactor
 	const renderList = () => {
