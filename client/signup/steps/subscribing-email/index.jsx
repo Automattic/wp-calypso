9f6fb14684c62ca2e5e9d632765698ae481d62d2
Jeremy Yip <jeremy.yip@automattic.com>
2024-06-13 13:30:27 -0700

    Add email subscription stepper flow
    Create signup flow with traditional framework
    
    Update flow description
    
    Add dummy subscribe email step
    
    Rename step to subscribing email
    
    Update styling classnames
    
    Add feature flag
    
    Update loading screen logic
    
    Fix signup form proptype errors
    
    Support basic url redirect

--- client/signup/steps/subscribing-email/index.jsx
+++ client/signup/steps/subscribing-email/index.jsx
@@ -1,40 +1,50 @@
-// import { getTracksAnonymousUserId } from '@automattic/calypso-analytics';
-// import config from '@automattic/calypso-config';
+import { getTracksAnonymousUserId } from '@automattic/calypso-analytics';
+import config from '@automattic/calypso-config';
+import page from '@automattic/calypso-router';
 import { useEffect, useState } from '@wordpress/element';
+import { addQueryArgs } from '@wordpress/url';
 import emailValidator from 'email-validator';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
-// import { getLocaleSlug } from 'calypso/lib/i18n-utils';
+import { getLocaleSlug } from 'calypso/lib/i18n-utils';
+import { isExternal } from 'calypso/lib/url';
 import StepWrapper from 'calypso/signup/step-wrapper';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { submitSignupStep } from 'calypso/state/signup/progress/actions';
 import SubscribingEmailStepContent from './content';
 
-// const createNewAccount = async ( props, setIsLoading, setDisplayUserExistsError ) => {
-// 	const email = props.queryParams.email;
-// 	try {
-// 		// eslint-disable-next-line no-undef
-// 		await wpcom.req.post( '/users/new', {
-// 			email: typeof email === 'string' ? email.trim() : '',
-// 			is_passwordless: true,
-// 			signup_flow_name: props.flowName,
-// 			validate: false,
-// 			locale: getLocaleSlug(),
-// 			client_id: config( 'wpcom_signup_id' ),
-// 			client_secret: config( 'wpcom_signup_key' ),
-// 			anon_id: getTracksAnonymousUserId(),
-// 		} );
-// 		// Do stuff with response and redirect
-// 		props.recordTracksEvent( 'calypso_signup_reader_landing_cta' );
-// 		props.submitSignupStep( { stepName: props.stepName } );
-// 		props.goToNextStep();
-// 	} catch ( error ) {
-// 		if ( ! [ 'already_taken', 'already_active', 'email_exists' ].includes( error.error ) ) {
-// 			// Subscribe existing user to guides emails through API endpoint
-// 		}
-// 		setIsLoading( false );
-// 	}
-// };
+const createNewAccount = async ( props, setIsLoading ) => {
+	const email = props.queryParams.email;
+	const redirectUrl = props.queryParams.redirect_to;
+
+	try {
+		// eslint-disable-next-line no-undef
+		await wpcom.req.post( '/users/new', {
+			email: typeof email === 'string' ? email.trim() : '',
+			is_passwordless: true,
+			signup_flow_name: props.flowName,
+			validate: false,
+			locale: getLocaleSlug(),
+			client_id: config( 'wpcom_signup_id' ),
+			client_secret: config( 'wpcom_signup_key' ),
+			anon_id: getTracksAnonymousUserId(),
+		} );
+		// // Do stuff with response and redirect
+		// props.recordTracksEvent( 'calypso_signup_reader_landing_cta' );
+		// props.submitSignupStep( { stepName: props.stepName } );
+		// props.goToNextStep();
+		if ( isExternal( redirectUrl ) ) {
+			window.location.replace( addQueryArgs( redirectUrl, { subscribed: true } ) );
+		}
+		page.redirect( redirectUrl );
+		throw new TypeError( { error: 'already_taken' } );
+	} catch ( error ) {
+		if ( [ 'already_taken', 'already_active', 'email_exists' ].includes( error.error ) ) {
+			// Subscribe existing user to guides emails through API endpoint https://github.com/Automattic/martech/issues/3090
+		}
+		setIsLoading( false );
+	}
+};
 
 function SubscribingEmailStep( props ) {
 	const handleButtonClick = async () => {};
@@ -46,9 +56,10 @@ function SubscribingEmailStep( props ) {
 		const email = queryParams.email;
 
 		if ( emailValidator.validate( email ) ) {
-			// TODO: Fetch request to create new account will replace this placeholder timeout
-			setTimeout( () => setIsLoading( false ), 5000 );
+			createNewAccount( props, setIsLoading );
 		}
+
+		setIsLoading( false );
 	}, [] );
 
 	return (
