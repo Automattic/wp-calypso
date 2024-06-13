a25d1404d2177ff00d0f930340d0a4f303dd52ee
cleacos <javacodia@gmail.com>
2024-06-14 00:22:24 +0100

    Add the mapAgencyDetailsFormData to map the endpoint response data.

--- client/a8c-for-agencies/sections/partner-directory/utils/map-application-form-data.ts
+++ client/a8c-for-agencies/sections/partner-directory/utils/map-application-form-data.ts
@@ -1,5 +1,5 @@
 import { Agency } from 'calypso/state/a8c-for-agencies/types';
-import { AgencyDirectoryApplication } from '../types';
+import { AgencyDetails, AgencyDirectoryApplication } from '../types';
 
 export function mapApplicationFormData( agency: Agency | null ): AgencyDirectoryApplication | null {
 	if ( ! agency?.profile?.partner_directory_application ) {
@@ -20,5 +20,27 @@ export function mapApplicationFormData( agency: Agency | null ): AgencyDirectory
 			} )
 		),
 		feedbackUrl: agency.profile.partner_directory_application.feedback_url,
+	};
+}
+
+export function mapAgencyDetailsFormData( agency: Agency | null ): AgencyDetails | null {
+	if ( ! agency?.profile ) {
+		return null;
+	}
+
+	return {
+		name: agency.profile.company_details.name,
+		email: agency.profile.company_details.email,
+		website: agency.profile.company_details.website,
+		bioDescription: agency.profile.company_details.bio_description,
+		logoUrl: agency.profile.company_details.logo_url,
+		landingPageUrl: agency.profile.company_details.landing_page_url,
+		country: agency.profile.company_details.country,
+		isAvailable: agency.profile.listing_details.is_available,
+		industry: agency.profile.listing_details.industry,
+		services: agency.profile.listing_details.services,
+		products: agency.profile.listing_details.products,
+		languagesSpoken: agency.profile.listing_details.languages_spoken,
+		budgetLowerRange: agency.profile.budget_details.budget_lower_range,
 	};
 }
