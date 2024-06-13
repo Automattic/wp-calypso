e8a27a615338a3f49d0b75458cda8b75b2186a54
Imran Hossain <imranh920@gmail.com>
2024-06-13 11:20:43 +0600

    Show dynamic validation message for URL

--- client/blocks/import/capture/capture-input.tsx
+++ client/blocks/import/capture/capture-input.tsx
@@ -1,5 +1,6 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { Button, FormLabel } from '@automattic/components';
+import { useHasEnTranslation } from '@automattic/i18n-utils';
 import { NextButton } from '@automattic/onboarding';
 import { createElement, createInterpolateElement } from '@wordpress/element';
 import { Icon, info } from '@wordpress/icons';
@@ -41,9 +42,11 @@ const CaptureInput: FunctionComponent< Props > = ( props ) => {
 	} = props;
 
 	const translate = useTranslate();
+	const hasEnTranslation = useHasEnTranslation();
 	const [ urlValue, setUrlValue ] = useState( '' );
 	const [ isValid, setIsValid ] = useState( false );
 	const [ submitted, setSubmitted ] = useState( false );
+	const [ validationMessage, setValidationMessage ] = useState( '' );
 	const lastInvalidValue = useRef< string | undefined >();
 	const showValidationMsg = hasError || ( submitted && ! isValid );
 	const { search } = useLocation();
@@ -72,6 +75,78 @@ const CaptureInput: FunctionComponent< Props > = ( props ) => {
 	function validateUrl( url: string ) {
 		const isValid = CAPTURE_URL_RGX.test( url );
 		setIsValid( isValid );
+		const tempValidationMessage = isValid ? '' : getValidationMessage( url );
+		setValidationMessage( tempValidationMessage );
+	}
+
+	// TODO: Just return the translated string directly from getValidationMessage once we have translations for all messages.
+	const errorMessages = {
+		'no-tld': {
+			message: translate(
+				'Your URL is missing a top-level domain (e.g., .com, .net, etc.). Example URL: example.com'
+			),
+			messageString:
+				'Your URL is missing a top-level domain (e.g., .com, .net, etc.). Example URL: example.com',
+		},
+		email: {
+			message: translate(
+				'It looks like you’ve entered an email address. Please enter a valid URL instead (e.g., example.com).'
+			),
+			messageString:
+				'It looks like you’ve entered an email address. Please enter a valid URL instead (e.g., example.com).',
+		},
+		'invalid-chars': {
+			message: translate(
+				'URL contains invalid characters. Please remove special characters and enter a valid URL (e.g., example.com)'
+			),
+			messageString:
+				'URL contains invalid characters. Please remove special characters and enter a valid URL (e.g., example.com)',
+		},
+		'invalid-protocol': {
+			message: translate(
+				'URLs with protocols can only start with http:// or https:// (e.g., https://example.com)'
+			),
+			messageString:
+				'URLs with protocols can only start with http:// or https:// (e.g., https://example.com)',
+		},
+		default: {
+			message: translate(
+				'Please enter a valid website address (e.g., example.com). You can copy and paste.'
+			),
+			messageString:
+				'Please enter a valid website address (e.g., example.com). You can copy and paste.',
+		},
+	};
+
+	function getValidationMessage( url ) {
+		const hasEnTranslationForAllMessages = Object.values( errorMessages ).every( ( message ) =>
+			hasEnTranslation( message.messageString )
+		);
+
+		if ( ! hasEnTranslationForAllMessages ) {
+			return translate( 'Please enter a valid website address. You can copy and paste.' );
+		}
+
+		const missingTLDRegex = /^(?:https?:\/\/)?(?!.*\.[a-z]{2,})([a-zA-Z0-9-_]+)$/;
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		const invalidURLProtocolRegex = /^(?!https?:\/\/)\S+:\/\//;
+		const invalidCharsRegex = /[^a-z0-9\-._~!$&'()*+,;:@/?=%[\]]/i;
+
+		const removedInitialDots = url.replace( 'www.', '' );
+
+		let errorMessage = errorMessages[ 'default' ].message;
+
+		if ( emailRegex.test( url ) ) {
+			errorMessage = errorMessages[ 'email' ].message;
+		} else if ( invalidCharsRegex.test( url ) ) {
+			errorMessage = errorMessages[ 'invalid-chars' ].message;
+		} else if ( missingTLDRegex.test( removedInitialDots ) ) {
+			errorMessage = errorMessages[ 'no-tld' ].message;
+		} else if ( invalidURLProtocolRegex.test( url ) ) {
+			errorMessage = errorMessages[ 'invalid-protocol' ].message;
+		}
+
+		return errorMessage;
 	}
 
 	function onChange( e: ChangeEvent< HTMLInputElement > ) {
@@ -118,8 +193,7 @@ const CaptureInput: FunctionComponent< Props > = ( props ) => {
 					<span className={ clsx( { 'is-error': showValidationMsg } ) }>
 						{ showValidationMsg && (
 							<>
-								<Icon icon={ info } size={ 20 } />{ ' ' }
-								{ translate( 'Please enter a valid website address. You can copy and paste.' ) }
+								<Icon icon={ info } size={ 20 } /> { validationMessage }
 							</>
 						) }
 					</span>
