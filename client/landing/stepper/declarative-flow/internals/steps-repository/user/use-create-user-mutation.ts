2a463dcaa6a6aa832336200fc3d1262e4e21803b
Omar Alshaker <omar@omaralshaker.com>
2024-06-14 17:48:41 +0200

    Move `createAccount` to its own file to avoid pulling a lot of stuff into Stepper

--- client/landing/stepper/declarative-flow/internals/steps-repository/user/use-create-user-mutation.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/user/use-create-user-mutation.ts
@@ -1,7 +1,7 @@
 // import config from '@automattic/calypso-config';
 // import { initGoogleRecaptcha, recordGoogleRecaptchaAction } from 'calypso/lib/analytics/recaptcha';
 import { useMutation } from '@tanstack/react-query';
-import { createAccount } from 'calypso/lib/signup/step-actions';
+import { createAccount } from 'calypso/lib/signup/step-actions/create-account';
 import type {
 	CreateAccountParams,
 	AccountCreateError,
