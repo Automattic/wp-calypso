cd24a805731d32200bd49fcc9bee8a6472d2df90
Wesley Berrêdo <wberredo@gmail.com>
2024-06-14 10:40:00 -0300

    Site Profiler: Show the values on basic metrics as seconds when too big

---
+++ client/site-profiler/utils/format-ms-value.ts
@@ -0,0 +1,18 @@
+import { I18N } from 'i18n-calypso';
+
+export const formatMsValue = (
+	msValue: number,
+	translate: I18N[ 'translate' ]
+): React.ReactElement | string | number => {
+	if ( msValue < 1000 ) {
+		return translate( '%(value)sms', {
+			comment: 'value to be displayed in milliseconds',
+			args: { value: Math.floor( msValue ) },
+		} );
+	}
+
+	return translate( '%(value)ss', {
+		comment: 'value to be displayed in seconds',
+		args: { value: ( msValue / 1000 ).toFixed( 2 ) },
+	} );
+};
