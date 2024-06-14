e5e3de2f9716274341a945ca4ab3e80a8156b569
cleacos <javacodia@gmail.com>
2024-06-14 08:08:27 +0100

    Fix the slug<=>label issue on the ServicesSelector

--- client/a8c-for-agencies/sections/partner-directory/components/services-selector.tsx
+++ client/a8c-for-agencies/sections/partner-directory/components/services-selector.tsx
@@ -1,6 +1,7 @@
 import { FormTokenField } from '@wordpress/components';
 import { TokenItem } from '@wordpress/components/build-types/form-token-field/types';
-import { useFormSelectors } from './hooks/use-form-selectors';
+import { useMemo } from 'react';
+import { reverseMap, useFormSelectors } from './hooks/use-form-selectors';
 
 type Props = {
 	setServices: ( services: ( string | TokenItem )[] ) => void;
@@ -10,6 +11,28 @@ type Props = {
 const ServicesSelector = ( { setServices, selectedServices }: Props ) => {
 	const { availableServices } = useFormSelectors();
 
+	// Get the reverse map of available services
+	const availableServicesByLabel = useMemo(
+		() => reverseMap( availableServices ),
+		[ availableServices ]
+	);
+
+	// Get the selected services by label
+	const selectedServicesByLabel = selectedServices.map( ( slug ) => {
+		const key = slug as string;
+		return availableServices[ key ];
+	} );
+
+	// Set the selected services by slug
+	const onServiceLabelsSelected = ( selectedServiceLabels: ( string | TokenItem )[] ) => {
+		const selectedServicesBySlug = selectedServiceLabels.map( ( label ) => {
+			const key = label as string;
+			return availableServicesByLabel[ key ];
+		} );
+
+		setServices( selectedServicesBySlug );
+	};
+
 	return (
 		<FormTokenField
 			__experimentalAutoSelectFirstMatch
@@ -17,9 +40,9 @@ const ServicesSelector = ( { setServices, selectedServices }: Props ) => {
 			__experimentalShowHowTo={ false }
 			__nextHasNoMarginBottom
 			label=""
-			onChange={ setServices }
+			onChange={ onServiceLabelsSelected }
 			suggestions={ Object.values( availableServices ) }
-			value={ selectedServices }
+			value={ selectedServicesByLabel }
 		/>
 	);
 };
