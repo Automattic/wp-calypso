94b2f4771ee4c04455b070d1bb96b0c39670fb39
Andrii <lysenkoa.work@gmail.com>
2024-06-14 13:58:33 -0700

    remove code duplications

--- client/a8c-for-agencies/sections/referrals/referral-details/mobile/purchases-mobile.tsx
+++ client/a8c-for-agencies/sections/referrals/referral-details/mobile/purchases-mobile.tsx
@@ -1,17 +1,16 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import page from '@automattic/calypso-router';
-import { Button } from '@automattic/components';
 import { useDispatch } from '@wordpress/data';
 import { useTranslate } from 'i18n-calypso';
 import { useCallback } from 'react';
-import { A4A_SITES_LINK_NEEDS_SETUP } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
-import TextPlaceholder from 'calypso/a8c-for-agencies/components/text-placeholder';
 import useProductsQuery from 'calypso/a8c-for-agencies/data/marketplace/use-products-query';
-import { addQueryArgs, urlToSlug } from 'calypso/lib/url';
 import { APIProductFamilyProduct } from 'calypso/state/partner-portal/types';
-import StatusBadge from '../../common/step-section-item/status-badge';
 import { ReferralPurchase } from '../../types';
 import './style.scss';
+import AssignedTo from '../components/assigned-to';
+import DateAssigned from '../components/date';
+import ProductDetails from '../components/product-details';
+import TotalAmount from '../components/total-amount';
 
 type PurchaseItemProps = {
 	purchase: ReferralPurchase;
@@ -22,14 +21,6 @@ type PurchaseItemProps = {
 const PurchaseItem = ( { purchase, data, isFetching }: PurchaseItemProps ) => {
 	const translate = useTranslate();
 	const dispatch = useDispatch();
-	const product = data?.find( ( product ) => product.product_id === purchase.product_id );
-	const isWPCOMLicense = product?.family_slug === 'wpcom-hosting';
-	const redirectUrl = isWPCOMLicense
-		? A4A_SITES_LINK_NEEDS_SETUP
-		: purchase.license_key &&
-		  addQueryArgs( { key: purchase.license_key }, '/marketplace/assign-license' );
-
-	const isDisabled = purchase.status !== 'active' || isFetching || ! product || ! redirectUrl;
 
 	const handleAssignToSite = useCallback(
 		( url: string ) => {
@@ -40,55 +31,35 @@ const PurchaseItem = ( { purchase, data, isFetching }: PurchaseItemProps ) => {
 	);
 
 	return (
-		<div className="testmenow">
-			<div className="referral-purchases-mobile">
-				<div className="referral-purchases-mobile__content">
-					<div className="referral-purchases-mobile__header">
-						<h3>{ translate( 'Product Details' ).toUpperCase() }</h3>
-					</div>
-					<p className="referral-purchases-mobile__product-name">
-						{ isFetching ? <TextPlaceholder /> : product?.name }
-					</p>
+		<div className="referral-purchases-mobile">
+			<div className="referral-purchases-mobile__content">
+				<div className="referral-purchases-mobile__header">
+					<h3>{ translate( 'Product Details' ).toUpperCase() }</h3>
 				</div>
-				<div className="referral-purchases-mobile__content">
-					<h3>{ translate( 'Date' ).toUpperCase() }</h3>
-					<p>
-						{ purchase.date_assigned
-							? new Date( purchase.date_assigned ).toLocaleDateString()
-							: '-' }
-					</p>
-				</div>
-				<div className="referral-purchases-mobile__content">
-					<h3>{ translate( 'Assigned to' ).toUpperCase() }</h3>
-					{ purchase.site_assigned ? (
-						<Button
-							className="referrals-purchases__assign-button"
-							borderless
-							href={ `/sites/overview/${ urlToSlug( purchase.site_assigned ) }` }
-						>
-							{ urlToSlug( purchase.site_assigned ) }
-						</Button>
-					) : (
-						<>
-							<StatusBadge
-								statusProps={ { children: translate( 'Unassigned' ), type: 'warning' } }
-							/>
-
-							<Button
-								disabled={ isDisabled }
-								className="referrals-purchases__assign-button"
-								borderless
-								onClick={ () => handleAssignToSite( redirectUrl ) }
-							>
-								{ isWPCOMLicense ? translate( 'Create site' ) : translate( 'Assign to site' ) }
-							</Button>
-						</>
-					) }
-				</div>
-				<div className="referral-purchases-mobile__content">
-					<h3>{ translate( 'Total' ).toUpperCase() }</h3>
-					<p>{ isFetching ? <TextPlaceholder /> : `$${ product?.amount }` }</p>
-				</div>
+				<p className="referral-purchases-mobile__product-name">
+					<ProductDetails purchase={ purchase } isFetching={ isFetching } data={ data } />
+				</p>
+			</div>
+			<div className="referral-purchases-mobile__content">
+				<h3>{ translate( 'Date' ).toUpperCase() }</h3>
+				<p>
+					<DateAssigned purchase={ purchase } />
+				</p>
+			</div>
+			<div className="referral-purchases-mobile__content">
+				<h3>{ translate( 'Assigned to' ).toUpperCase() }</h3>
+				<AssignedTo
+					purchase={ purchase }
+					data={ data }
+					handleAssignToSite={ handleAssignToSite }
+					isFetching={ isFetching }
+				/>
+			</div>
+			<div className="referral-purchases-mobile__content">
+				<h3>{ translate( 'Total' ).toUpperCase() }</h3>
+				<p>
+					<TotalAmount isFetching={ isFetching } data={ data } purchase={ purchase } />
+				</p>
 			</div>
 		</div>
 	);
