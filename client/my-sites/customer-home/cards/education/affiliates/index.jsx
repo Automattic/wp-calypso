8a8cda3e6837ac497cf980c59a8d5c58f8701658
Claudiu Filip <claudiu.filip@automattic.com>
2024-06-14 06:21:31 +0300

    Add affiliates cards to the My Home screen (#91551)
    * Add affiliates cards
    
    * Show affiliates cards if they're translated
    
    * Use correct translation checks
    
    * Remove unnecessary assets
    
    * Fix typo in variable name
    
    * Improve card links
    
    * Alphabetically order the affiliates constant
    
    * Update card signup links

---
+++ client/my-sites/customer-home/cards/education/affiliates/index.jsx
@@ -0,0 +1,44 @@
+import { useHasEnTranslation } from '@automattic/i18n-utils';
+import { useTranslate } from 'i18n-calypso';
+import affiliatesIllustration from 'calypso/assets/images/customer-home/illustration--affiliates-icons-small.png';
+import EducationalContent from '../educational-content';
+export const EDUCATION_EARN = 'home-education-earn';
+
+const EducationAffiliates = () => {
+	const translate = useTranslate();
+	const hasTranslation = useHasEnTranslation();
+
+	const title = translate( 'Earn with the Automattic Affiliate Program' );
+	const hasTitleTranslation = hasTranslation( 'Earn with the Automattic Affiliate Program' );
+
+	const description = translate(
+		'Join the Automattic Affiliate Program and get up to 100% payouts by promoting one or more of Automattic’s trusted, profitable products.'
+	);
+	const hasDescriptionTranslation = hasTranslation(
+		'Join the Automattic Affiliate Program and get up to 100% payouts by promoting one or more of Automattic’s trusted, profitable products.'
+	);
+
+	if ( ! hasTitleTranslation || ! hasDescriptionTranslation ) {
+		EducationAffiliates.isDisabled = true;
+		return null;
+	}
+	return (
+		<EducationalContent
+			title={ title }
+			description={ description }
+			links={ [
+				{
+					externalLink: true,
+					url: `https://automattic.com/affiliates/?ref=wordpressdotcom_education_home_card`,
+					text: translate( 'Sign up' ),
+				},
+			] }
+			illustration={ affiliatesIllustration }
+			cardName={ EDUCATION_EARN }
+			width="201"
+			height="114"
+		/>
+	);
+};
+
+export default EducationAffiliates;
