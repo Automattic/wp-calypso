f0eda605c5b782303e774b35df9b92fbce125b14
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2024-06-14 19:30:05 -0300

    Show copy about free Gravatar domain in checkout

---
+++ client/my-sites/checkout/src/components/domain-registration-free-gravatar-domain.tsx
@@ -0,0 +1,27 @@
+import { useTranslate } from 'i18n-calypso';
+import { getDomainRegistrations } from 'calypso/lib/cart-values/cart-items';
+import CheckoutTermsItem from 'calypso/my-sites/checkout/src/components/checkout-terms-item';
+import type { ResponseCart } from '@automattic/shopping-cart';
+
+interface DomainRegistrationFreeGravatarDomainProps {
+	cart: ResponseCart;
+}
+
+export function DomainRegistrationFreeGravatarDomain(
+	props: DomainRegistrationFreeGravatarDomainProps
+) {
+	const translate = useTranslate();
+	const domains = getDomainRegistrations( props.cart );
+
+	if ( ! domains.some( ( domain ) => domain.extra?.is_gravatar_domain ) ) {
+		return null;
+	}
+
+	return (
+		<CheckoutTermsItem>
+			{ translate(
+				'This is a free Gravatar domain. You can only get one free domain to use with Gravatar'
+			) }
+		</CheckoutTermsItem>
+	);
+}
