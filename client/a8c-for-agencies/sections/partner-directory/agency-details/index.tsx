9ac6517a751618e105e84cd47894c5415d21e930
cleacos <javacodia@gmail.com>
2024-06-17 17:54:58 +0100

    Use the LogoUploadButton

--- client/a8c-for-agencies/sections/partner-directory/agency-details/index.tsx
+++ client/a8c-for-agencies/sections/partner-directory/agency-details/index.tsx
@@ -11,7 +11,9 @@ import { A4A_PARTNER_DIRECTORY_DASHBOARD_LINK } from 'calypso/a8c-for-agencies/c
 import BudgetSelector from 'calypso/a8c-for-agencies/sections/partner-directory/components/budget-selector';
 import { AgencyDetails } from 'calypso/a8c-for-agencies/sections/partner-directory/types';
 import { reduxDispatch } from 'calypso/lib/redux-bridge';
+import { useSelector } from 'calypso/state';
 import { setActiveAgency } from 'calypso/state/a8c-for-agencies/agency/actions';
+import { getActiveAgency } from 'calypso/state/a8c-for-agencies/agency/selectors';
 import { Agency } from 'calypso/state/a8c-for-agencies/types';
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import IndustrySelector from '../components/industry-selector';
@@ -22,6 +24,7 @@ import { PARTNER_DIRECTORY_AGENCY_EXPERTISE_SLUG } from '../constants';
 import { useCountryList } from './hooks/use-country-list';
 import useDetailsForm from './hooks/use-details-form';
 import useSubmitForm from './hooks/use-submit-form';
+import LogoUploadButton from './logo-upload-button';
 
 import './style.scss';
 
@@ -31,6 +34,8 @@ type Props = {
 
 const AgencyDetailsForm = ( { initialFormData }: Props ) => {
 	const translate = useTranslate();
+
+	const agency = useSelector( getActiveAgency );
 
 	const onSubmitSuccess = useCallback(
 		( response: Agency ) => {
@@ -69,6 +74,14 @@ const AgencyDetailsForm = ( { initialFormData }: Props ) => {
 				...fields,
 			};
 		} );
+	};
+
+	// We have to simulate sa Site object to use addMedia
+	const site = {
+		ID: agency?.id,
+		options: {
+			allowed_file_types: [],
+		},
 	};
 
 	return (
@@ -143,10 +156,7 @@ const AgencyDetailsForm = ( { initialFormData }: Props ) => {
 					) }
 					isRequired
 				>
-					<TextControl
-						value={ formData.logoUrl }
-						onChange={ ( value ) => setFormFields( { logoUrl: value } ) }
-					/>
+					<LogoUploadButton site={ site } />
 				</FormField>
 			</FormSection>
 
