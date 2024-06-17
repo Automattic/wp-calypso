bf12b0cfb107166c3990cfc44817f828494b5731
Brian Willis <briowill@gmail.com>
2024-06-17 14:54:36 -0500

    Code clean up, remove old google analyitcs references

--- client/lib/analytics/ad-tracking/index.js
+++ client/lib/analytics/ad-tracking/index.js
@@ -1,12 +1,6 @@
 export { adTrackRegistration } from './ad-track-registration';
 export { adTrackSignupComplete } from './ad-track-signup-complete';
 export { adTrackSignupStart } from './ad-track-signup-start';
-export {
-	fireGoogleAnalyticsEvent,
-	fireGoogleAnalyticsPageView,
-	getGoogleAnalyticsDefaultConfig,
-	setupGoogleAnalyticsGtag,
-} from './google-analytics';
 export { recordAddToCart } from './record-add-to-cart';
 export { recordAliasInFloodlight } from './record-alias-in-floodlight';
 export { recordOrder } from './record-order';
