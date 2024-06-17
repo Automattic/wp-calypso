223c0b012c3d13ee5063fda414b5902b1e437575
Renatho De Carli Rosa <renatho@gmail.com>
2024-06-17 14:43:56 -0300

    Add initial structure for migration instructions

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/index.tsx
@@ -1,8 +1,27 @@
+import { StepContainer } from '@automattic/onboarding';
 import React from 'react';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { Questions } from './questions';
 import type { Step } from '../../types';
+import './style.scss';
 
 const SiteMigrationInstructions: Step = function () {
-	return <div>New migration instructions screen</div>;
+	const stepContent = <div>New migration instructions!</div>;
+
+	const questions = <Questions />;
+
+	return (
+		<StepContainer
+			stepName="site-migration-instructions"
+			shouldHideNavButtons={ false }
+			className="is-step-site-migration-instructions site-migration-instructions"
+			hideSkip
+			hideBack
+			stepContent={ stepContent }
+			recordTracksEvent={ recordTracksEvent }
+			customizedActionButtons={ questions }
+		/>
+	);
 };
 
 export default SiteMigrationInstructions;
---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/questions.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const Questions: FC = () => (
+	<div className="site-migration-instructions__questions-wrapper">Questions?</div>
+);
