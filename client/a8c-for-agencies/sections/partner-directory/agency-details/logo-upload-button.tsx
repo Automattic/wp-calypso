e574405cfd09fe34d9d5c503d4382e579ddfd631
cleacos <javacodia@gmail.com>
2024-06-17 17:39:43 +0100

    Add LogoUploadButton component

---
+++ client/a8c-for-agencies/sections/partner-directory/agency-details/logo-upload-button.tsx
@@ -0,0 +1,38 @@
+import { ChangeEvent, useRef } from 'react';
+import { useAddMedia } from 'calypso/data/media/use-add-media';
+
+type Props = {
+	site: {
+		ID?: number;
+	};
+	onLogoUpload?: ( url: string ) => void;
+};
+
+const LogoUploadButton = ( { site, onLogoUpload }: Props ) => {
+	const addMedia = useAddMedia();
+	const fileInputRef = useRef( null );
+
+	if ( ! site.ID ) {
+		return null;
+	}
+
+	const uploadFiles = ( event: ChangeEvent< HTMLInputElement > ) => {
+		if ( event.target.files ) {
+			addMedia( event.target.files, site ).then( ( media ) => {
+				onLogoUpload && onLogoUpload( media[ 0 ].URL );
+			} );
+		}
+	};
+
+	return (
+		<input
+			style={ { display: 'none' } }
+			ref={ fileInputRef }
+			type="file"
+			accept="image/*"
+			onChange={ uploadFiles }
+		/>
+	);
+};
+
+export default LogoUploadButton;
