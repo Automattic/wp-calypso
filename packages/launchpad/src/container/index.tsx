7d77ecdc60bb838f77ad8021d1c486998053e684
Renatho De Carli Rosa <renatho@gmail.com>
2024-06-17 18:19:52 -0300

    Create container component to extract logic

---
+++ packages/launchpad/src/container/index.tsx
@@ -0,0 +1,39 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import './style.scss';
+
+export interface ContainerProps {
+	className?: string;
+	headerClassName?: string;
+	sidebarClassName?: string;
+	header?: React.ReactNode;
+	sidebar: React.ReactNode;
+	children: React.ReactNode;
+}
+
+const Container: React.FunctionComponent< ContainerProps > = ( {
+	className,
+	headerClassName,
+	sidebarClassName,
+	header,
+	sidebar,
+	children,
+} ) => {
+	return (
+		<main className={ clsx( 'launchpad-container', className ) }>
+			{ header && (
+				<div className={ clsx( 'launchpad-container__header', headerClassName ) }>{ header }</div>
+			) }
+
+			<div className="launchpad-container__content">
+				<div className={ clsx( 'launchpad-container__sidebar', sidebarClassName ) }>
+					<div className="launchpad-container__sidebar-content-container">{ sidebar }</div>
+				</div>
+				<div className="launchpad-container__main-content">{ children }</div>
+			</div>
+		</main>
+	);
+};
+
+export default Container;
