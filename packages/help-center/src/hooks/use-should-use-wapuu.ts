4f24f8995e1382ce8fda0d6b7246b1332a1426a8
Tony Arcangelini <tony@arcangelini.com>
2024-06-18 11:56:18 +0200

    Put the flagInURL check back

--- packages/help-center/src/hooks/use-should-use-wapuu.ts
+++ packages/help-center/src/hooks/use-should-use-wapuu.ts
@@ -2,14 +2,23 @@
 import config from '@automattic/calypso-config';
 import { useSupportAvailability } from '../data/use-support-availability';
 
+function isWapuuFlagSetInURL(): boolean {
+	const currentUrl = window.location.href;
+	const urlParams = new URLSearchParams( new URL( currentUrl ).search );
+	return urlParams.get( 'flags' ) === 'wapuu';
+}
+
 export const useShouldUseWapuu = () => {
 	const { data: supportAvailability } = useSupportAvailability();
 
 	// All users eligible for support should see the Wapuu assistant.
 	const isEligibleForSupport = Boolean( supportAvailability?.is_user_eligible );
 
-	// Wapuu can be enabled via config/flag
+	// Force Wapuu via URL flag (config is not available in wp-admin)
+	const isFlagSetInURL = isWapuuFlagSetInURL();
+
+	// Wapuu can be enabled via config
 	const isConfigEnabled = config.isEnabled( 'wapuu' );
 
-	return isEligibleForSupport || isConfigEnabled;
+	return isEligibleForSupport || isFlagSetInURL || isConfigEnabled;
 };
