a12a3907e19005cd79683e59c9509d5ca43edefe
Tony Arcangelini <tony@arcangelini.com>
2024-06-18 11:43:03 +0200

    Update config check

--- packages/help-center/src/hooks/use-should-use-wapuu.ts
+++ packages/help-center/src/hooks/use-should-use-wapuu.ts
@@ -2,23 +2,14 @@
 import config from '@automattic/calypso-config';
 import { useSupportAvailability } from '../data/use-support-availability';
 
-function isWapuuFlagSetInURL(): boolean {
-	const currentUrl = window.location.href;
-	const urlParams = new URLSearchParams( new URL( currentUrl ).search );
-	return urlParams.get( 'flags' ) === 'wapuu';
-}
-
 export const useShouldUseWapuu = () => {
 	const { data: supportAvailability } = useSupportAvailability();
 
 	// All users eligible for support should see the Wapuu assistant.
 	const isEligibleForSupport = Boolean( supportAvailability?.is_user_eligible );
 
-	// Force Wapuu via URL flag
-	const isFlagSetInURL = isWapuuFlagSetInURL();
-
-	// Wapuu can be enabled via config
+	// Wapuu can be enabled via config/flag
 	const isConfigEnabled = config.isEnabled( 'wapuu' );
 
-	return isEligibleForSupport || isFlagSetInURL || isConfigEnabled;
+	return isEligibleForSupport || isConfigEnabled;
 };
