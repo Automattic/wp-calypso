56ec9ba79bba02083cd2d90226df5667cc3872ed
Renatho De Carli Rosa <renatho@gmail.com>
2024-06-18 18:23:37 -0300

    Add questions link and styles

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/questions.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/questions.tsx
@@ -1,5 +1,25 @@
+import { useTranslate } from 'i18n-calypso';
 import React, { FC } from 'react';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 
-export const Questions: FC = () => (
-	<div className="site-migration-instructions__questions-wrapper">Questions?</div>
-);
+export const Questions: FC = () => {
+	const translate = useTranslate();
+
+	return (
+		<div className="site-migration-instructions__questions-wrapper">
+			{ translate( 'Questions?' ) }{ ' ' }
+			<a
+				href="https://wordpress.com/help/contact/"
+				target="_blank"
+				rel="noopener noreferrer"
+				onClick={ () => {
+					recordTracksEvent(
+						'calypso_onboarding_site_migration_instructions_questions_happiness_engineer'
+					);
+				} }
+			>
+				{ translate( 'Ask a Happiness Engineer' ) }
+			</a>
+		</div>
+	);
+};
