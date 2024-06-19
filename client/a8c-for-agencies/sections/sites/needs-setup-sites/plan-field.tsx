5a255c5020f18e9ee17a520f01a80d414daa2f04
Heyde Moura <moura.heyde@gmail.com>
2024-06-19 16:19:32 -0300

    Agencies: Add temporary migration procedure (#91955)
    * Add temporary migration procedure
    
    * Fix missing dependency array

--- client/a8c-for-agencies/sections/sites/needs-setup-sites/plan-field.tsx
+++ client/a8c-for-agencies/sections/sites/needs-setup-sites/plan-field.tsx
@@ -1,5 +1,7 @@
+import config from '@automattic/calypso-config';
 import wpcomIcon from 'calypso/assets/images/icons/wordpress-logo.svg';
 import CreateSiteButton from './create-site-button';
+import MigrateSiteButton from './migrate-site-button';
 
 export type AvailablePlans = {
 	name: string;
@@ -10,9 +12,17 @@ export type AvailablePlans = {
 type Props = AvailablePlans & {
 	provisioning?: boolean;
 	onCreateSite: ( id: number ) => void;
+	onMigrateSite: ( id: number ) => void;
 };
 
-export default function PlanField( { name, subTitle, ids, provisioning, onCreateSite }: Props ) {
+export default function PlanField( {
+	name,
+	subTitle,
+	ids,
+	provisioning,
+	onCreateSite,
+	onMigrateSite,
+}: Props ) {
 	return (
 		<div className="plan-field">
 			<div className="plan-field__icon">
@@ -28,6 +38,9 @@ export default function PlanField( { name, subTitle, ids, provisioning, onCreate
 				provisioning={ provisioning }
 				onActivate={ () => onCreateSite( ids[ 0 ] ) }
 			/>
+			{ config.isEnabled( 'a4a/site-migration' ) && (
+				<MigrateSiteButton onClick={ () => onMigrateSite( ids[ 0 ] ) } />
+			) }
 		</div>
 	);
 }
