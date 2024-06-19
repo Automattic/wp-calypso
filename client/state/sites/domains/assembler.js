78be37a130573de3dfed863d35c6f4452e123a21
rafaelgallani <galani.rafael@gmail.com>
2024-06-19 11:24:22 -0300

    Fix unit tests

--- client/state/sites/domains/assembler.js
+++ client/state/sites/domains/assembler.js
@@ -141,7 +141,7 @@ export const createSiteDomainObject = ( domain ) => {
 		pendingRegistrationTime: String( domain.pending_registration_time ),
 		pendingTransfer: domain.pending_transfer,
 		pointsToWpcom: Boolean( domain.points_to_wpcom ),
-		productSlug: String( domain.product_slug ),
+		productSlug: ! domain.product_slug ? null : String( domain.product_slug ),
 		privateDomain: domain.private_domain,
 		privacyAvailable: Boolean( domain.privacy_available ),
 		registeredViaTrustee: Boolean( domain.registered_via_trustee ),
