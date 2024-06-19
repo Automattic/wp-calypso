9a2dd9b32552450a7237012ed604d783eac73829
Renatho De Carli Rosa <renatho@gmail.com>
2024-06-19 18:47:21 -0300

    Update getting the from url to a hook

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/site-preview/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/site-preview/index.tsx
@@ -1,19 +1,18 @@
-import { getQueryArg } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
 import React, { FC } from 'react';
+import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
 import './style.scss';
-
-const importSiteFrom = getQueryArg( window.location.href, 'from' )?.toString() || '';
 
 export const SitePreview: FC = () => {
 	const translate = useTranslate();
+	const fromUrl = useQuery().get( 'from' ) || '';
 
 	return (
 		<div className="migration-instructions-from-preview">
 			<iframe
 				className="migration-instructions-from-preview__iframe"
 				title={ translate( 'Preview of the site being imported' ) }
-				src={ importSiteFrom }
+				src={ fromUrl }
 			/>
 		</div>
 	);
