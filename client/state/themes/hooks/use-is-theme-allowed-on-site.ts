fdcb6e769f3f2096a3628313194d092c59d20c0a
Richard Ortiz <rcrd.ortiz@gmail.com>
2024-06-20 13:48:33 +0200

    Fixed type error

--- client/state/themes/hooks/use-is-theme-allowed-on-site.ts
+++ client/state/themes/hooks/use-is-theme-allowed-on-site.ts
@@ -9,7 +9,8 @@ export function useIsThemeAllowedOnSite( siteId: number | null, themeId: string 
 		const features = themeTier?.featureList ?? [ themeTier?.feature ] ?? [ null ];
 
 		return features.some(
-			( feature: string | null ) => ! feature || siteHasFeature( state, siteId, feature )
+			( feature: string | null | undefined ) =>
+				! feature || siteHasFeature( state, siteId, feature )
 		);
 	} );
 
@@ -21,7 +22,8 @@ export function useIsThemeAllowedOnSite( siteId: number | null, themeId: string 
 			] ?? [ null ];
 
 		return retainedFeatures.some(
-			( feature: string | null ) => ! feature || siteHasFeature( state, siteId, feature )
+			( feature: string | null | undefined ) =>
+				! feature || siteHasFeature( state, siteId, feature )
 		);
 	} );
 
