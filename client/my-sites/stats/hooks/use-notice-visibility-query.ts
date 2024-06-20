27f531e368e7e2e4082e86b60c4626d8a78be9a9
Dognose <dognose24@users.noreply.github.com>
2024-06-20 10:57:24 +0800

    Stats: Add GDPR cookie consent notice support (#91762)
    * Add GDPRCookieConsentNotice with the id gdpr_cookie_consent
    
    * Set the highest priority for id gdpr_cookie_consent
    
    * Use adminUrl for plugin settings link
    
    * Only show the GDPRCookieConsentNotice for Odyssey Stats
    
    * Postpone GDPRCookieConsentNotice for 2 months to avoid bothering users too often
    
    * Update notice copy with level

--- client/my-sites/stats/hooks/use-notice-visibility-query.ts
+++ client/my-sites/stats/hooks/use-notice-visibility-query.ts
@@ -11,6 +11,7 @@ const DEFAULT_SERVER_NOTICES_VISIBILITY = {
 	focus_jetpack_purchase: false,
 	// TODO: Check if the site needs to be upgraded to a higher tier on the back end.
 	tier_upgrade: true,
+	gdpr_cookie_consent: false,
 };
 const DEFAULT_CLIENT_NOTICES_VISIBILITY = {
 	client_paid_plan_purchase_success: true,
@@ -28,10 +29,13 @@ export type NoticeIdType = keyof Notices;
 const CONFLICT_NOTICE_ID_GROUPS: Record< string, Array< NoticeIdType > > = {
 	settings_tool_tips: [ 'traffic_page_settings', 'traffic_page_highlights_module_settings' ],
 	dashboard_notices: [
+		// Set the highest priority to prevent blocking Stats under any circumstances.
+		'gdpr_cookie_consent',
 		'client_paid_plan_purchase_success',
 		'client_free_plan_purchase_success',
 		'do_you_love_jetpack_stats',
 		'commercial_site_upgrade',
+		// TODO: Check if the current usage is over the tier limit inside the isVisibleFunc.
 		'tier_upgrade',
 	],
 };
