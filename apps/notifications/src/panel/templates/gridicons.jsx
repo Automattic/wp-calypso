ed321eb4a83c5374615d2c0fd345fd6ff8bc3e05
Eoin Gallagher <eoin@automattic.com>
2024-06-20 16:31:27 +0100

    Notifications: add keyboard shortcut info

--- apps/notifications/src/panel/templates/gridicons.jsx
+++ apps/notifications/src/panel/templates/gridicons.jsx
@@ -127,6 +127,16 @@ export default class extends Component {
 					</svg>
 				);
 
+			case 'gridicons-info-outline':
+				return (
+					<svg { ...sharedProps } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<title>Info</title>
+						<g>
+							<path d="M13 9h-2V7h2v2zm0 2h-2v6h2v-6zm-1-7c-4.411 0-8 3.589-8 8s3.589 8 8 8 8-3.589 8-8-3.589-8-8-8m0-2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2z" />
+						</g>
+					</svg>
+				);
+
 			case 'gridicons-lock':
 				return (
 					<svg { ...sharedProps } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
--- apps/notifications/src/panel/templates/list-header.jsx
+++ apps/notifications/src/panel/templates/list-header.jsx
@@ -1,4 +1,5 @@
 import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
 import { connect } from 'react-redux';
 import actions from '../state/actions';
 import Gridicon from './gridicons';
@@ -6,26 +7,67 @@ import Gridicon from './gridicons';
 export const ListHeader = ( { isFirst, title, viewSettings } ) => {
 	const translate = useTranslate();
 
+	// Use state to determine if the keyboard shortcuts are visible
+	const [ shortcutsVisible, setShortcutsVisible ] = useState( false );
+
+	// This function toggles the visibility of the keyboard shortcuts
+	const viewShortcuts = () => {
+		setShortcutsVisible( ! shortcutsVisible );
+	};
+
+	// This function renders a list of keyboard shortcuts
+	const renderShortcutsPopover = () => {
+		// Render the keyboard shortcuts list
+		return (
+			<div className="wpnc__keyboard-shortcuts-popover">
+				<h2>{ translate( 'Keyboard Shortcuts' ) }</h2>
+				<ul>
+					<li>
+						<span className="shortcut">?</span>
+						<span>{ translate( 'Show this help' ) }</span>
+					</li>
+				</ul>
+			</div>
+		);
+	};
+
 	return (
 		<li className="wpnc__time-group-wrap">
 			<div className="wpnc__time-group-title">
 				<Gridicon icon="time" size={ 18 } />
 				{ title }
 				{ isFirst && (
-					<span
-						className="wpnc__settings"
-						onClick={ viewSettings }
-						onKeyDown={ ( e ) => {
-							if ( e.key === 'Enter' ) {
-								viewSettings();
-							}
-						} }
-						aria-label={ translate( 'Open notification settings' ) }
-						role="button"
-						tabIndex="0"
-					>
-						<Gridicon icon="cog" size={ 18 } />
-					</span>
+					<>
+						<span
+							className="wpnc__settings"
+							onClick={ viewSettings }
+							onKeyDown={ ( e ) => {
+								if ( e.key === 'Enter' ) {
+									viewSettings();
+								}
+							} }
+							aria-label={ translate( 'Open notification settings' ) }
+							role="button"
+							tabIndex="0"
+						>
+							<Gridicon icon="cog" size={ 18 } />
+						</span>
+						<span
+							className="wpnc__keyboard-shortcuts-button"
+							onClick={ viewShortcuts }
+							onKeyDown={ ( e ) => {
+								if ( e.key === 'i' ) {
+									viewShortcuts();
+								}
+							} }
+							aria-label={ translate( 'Open Keyboard Shortcuts' ) }
+							role="button"
+							tabIndex="0"
+						>
+							<Gridicon icon="info-outline" size={ 18 } />
+						</span>
+						{ shortcutsVisible && renderShortcutsPopover() }
+					</>
 				) }
 			</div>
 		</li>
