048601de37704974688b87a98eeaa914d489c32d
Jarda Snajdr <jsnajdr@gmail.com>
2024-06-20 09:45:04 +0200

    LoggedOutForm: modernize class components (#91894)

--- client/components/logged-out-form/index.jsx
+++ client/components/logged-out-form/index.jsx
@@ -1,22 +1,18 @@
 import { Card } from '@automattic/components';
 import clsx from 'clsx';
-import { omit } from 'lodash';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 
 import './style.scss';
 
-export default class LoggedOutForm extends Component {
-	static propTypes = {
-		children: PropTypes.node.isRequired,
-		className: PropTypes.string,
-	};
-
-	render() {
-		return (
-			<Card className={ clsx( 'logged-out-form', this.props.className ) }>
-				<form { ...omit( this.props, 'className' ) }>{ this.props.children }</form>
-			</Card>
-		);
-	}
+export default function LoggedOutForm( { className, ...props } ) {
+	return (
+		<Card className={ clsx( 'logged-out-form', className ) }>
+			<form { ...props } />
+		</Card>
+	);
 }
+
+LoggedOutForm.propTypes = {
+	children: PropTypes.node.isRequired,
+	className: PropTypes.string,
+};
