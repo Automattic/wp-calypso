a9b0bcc03ce1e797acef858656cf7f17748c5d70
Wesley Berrêdo <Wberredo@gmail.com>
2024-06-21 13:22:49 -0300

    Square Payments: redirect all start-with to square-payments (#92026)
    * Square Payments: redirect all start-with to square-payments
    
    * Show a not found page for non existing routes

--- client/start-with/index.web.ts
+++ client/start-with/index.web.ts
@@ -1,11 +1,8 @@
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller/index.web';
-import {
-	redirectToSquarePayments,
-	startWithSquarePaymentsContext,
-} from 'calypso/start-with/controller';
+import { notFound, startWithSquarePaymentsContext } from 'calypso/start-with/controller';
 
 export default function () {
-	page( '/start-with', redirectToSquarePayments );
 	page( '/start-with/square-payments', startWithSquarePaymentsContext, makeLayout, clientRender );
+	page( '/start-with*', notFound, makeLayout, clientRender );
 }
