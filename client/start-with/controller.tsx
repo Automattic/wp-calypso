3a11252a2090b9f5113aaf84475becb450361495
Wesley Berrêdo <Wberredo@gmail.com>
2024-06-21 08:33:14 -0300

    Square Payments: Create landing page (#91993)
    * Add the content for Square Payments landing page
    
    * Add the footer section
    
    * Add CTA click event
    
    * Add page view event
    
    * Add review suggestions

--- client/start-with/controller.tsx
+++ client/start-with/controller.tsx
@@ -10,7 +10,7 @@ export function startWithSquarePaymentsContext( context: Context, next: () => vo
 	}
 
 	context.primary = (
-		<Main wideLayout>
+		<Main fullWidthLayout>
 			<StartWithSquarePayments />
 		</Main>
 	);
