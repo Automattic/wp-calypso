b63b7c66bb48dd5cd83a7349afcb2ff4a149e1bd
Omar Alshaker <omar@omaralshaker.com>
2024-06-22 01:56:43 +0200

    Dedupe `PANEL_MAPPINGS`

--- packages/help-center/src/constants.ts
+++ packages/help-center/src/constants.ts
@@ -1,1 +1,15 @@
 export const SUPPORT_BLOG_ID = 9619154;
+
+/**
+ * Customizer: Mapping from Calypso panel slug to tuple of focus key and value.
+ */
+export const PANEL_MAPPINGS: Record< string, [ string, string ] > = {
+	widgets: [ 'panel', 'widgets' ],
+	fonts: [ 'section', 'jetpack_fonts' ],
+	identity: [ 'section', 'title_tagline' ],
+	'custom-css': [ 'section', 'jetpack_custom_css' ],
+	amp: [ 'section', 'amp_design' ],
+	menus: [ 'panel', 'nav_menus' ],
+	homepage: [ 'section', 'static_front_page' ],
+	jetpack_search: [ 'section', 'jetpack_search' ],
+};
--- packages/help-center/src/hooks/use-customizer-url.ts
+++ packages/help-center/src/hooks/use-customizer-url.ts
@@ -1,20 +1,7 @@
 import { addQueryArgs } from '@wordpress/url';
+import { PANEL_MAPPINGS } from '../constants';
 import { useHelpCenterContext } from '../contexts/HelpCenterContext';
 import { useSiteSlug } from './use-site-slug';
-
-/**
- * Mapping from Calypso panel slug to tuple of focus key and value.
- */
-export const PANEL_MAPPINGS: Record< string, [ string, string ] > = {
-	widgets: [ 'panel', 'widgets' ],
-	fonts: [ 'section', 'jetpack_fonts' ],
-	identity: [ 'section', 'title_tagline' ],
-	'custom-css': [ 'section', 'jetpack_custom_css' ],
-	amp: [ 'section', 'amp_design' ],
-	menus: [ 'panel', 'nav_menus' ],
-	homepage: [ 'section', 'static_front_page' ],
-	jetpack_search: [ 'section', 'jetpack_search' ],
-};
 
 /**
  * Given the name of a Calypso customizer panel, returns an object containing
