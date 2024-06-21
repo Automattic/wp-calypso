09ecf1278ffd9813ff916ab4e1d7d7ca818486e4
Dehan <dehandecroos@gmail.com>
2024-06-21 07:19:23 -0400

    Force opening contact form

--- packages/help-center/src/hooks/use-still-need-help-url.tsx
+++ packages/help-center/src/hooks/use-still-need-help-url.tsx
@@ -3,14 +3,17 @@
 import { useSupportStatus } from '../data/use-support-status';
 import { useShouldUseWapuu } from './use-should-use-wapuu';
 
-export function useStillNeedHelpURL() {
+export function useStillNeedHelpURL( forceContactForm = false ) {
 	const { data: supportStatus, isLoading } = useSupportStatus();
 	const shouldUseWapuu = useShouldUseWapuu();
 	const isEligibleForSupport = Boolean( supportStatus?.eligibility?.is_user_eligible );
 
+	if ( isEligibleForSupport && shouldUseWapuu && ! forceContactForm ) {
+		return { url: '/odie', isLoading: false };
+	}
+
 	if ( isEligibleForSupport ) {
-		const url = shouldUseWapuu ? '/odie' : '/contact-options';
-		return { url, isLoading: false };
+		return { url: '/contact-options', isLoading: false };
 	}
 
 	return { url: '/contact-form?mode=FORUM', isLoading };
