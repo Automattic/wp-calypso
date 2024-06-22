72fd36dd8336d0d94c1c64b488bb9742d8f8cbc7
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2024-06-22 17:40:29 +0900

    Add logic confirming PWYW-only state.

--- client/state/sites/selectors/has-site-product-jetpack-stats-pwyw-only.ts
+++ client/state/sites/selectors/has-site-product-jetpack-stats-pwyw-only.ts
@@ -1,3 +1,5 @@
+import { camelOrSnakeSlug } from '@automattic/calypso-products';
+import { productHasStats } from 'calypso/blocks/jetpack-benefits/feature-checks';
 import { getSitePurchases } from 'calypso/state/purchases/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { AppState } from 'calypso/types';
@@ -7,9 +9,33 @@ function hasSiteProductJetpackStatsPWYWOnly(
 	siteId = getSelectedSiteId( state )
 ): boolean {
 	const sitePurchases = getSitePurchases( state, siteId );
-	console.log( 'sitePurchases: ', sitePurchases.length );
 
-	return true;
+	// Get listing of all plans that support stats in some way and qualify as "paid" plans.
+	const plansSupportingStats = sitePurchases?.filter(
+		( product ) =>
+			productHasStats( camelOrSnakeSlug( product ), true ) && product.expiryStatus !== 'expired'
+	);
+
+	if ( plansSupportingStats.length === 0 ) {
+		return false;
+	}
+
+	// Check for one or more PWYW plans.
+	const plansPWYWStats = plansSupportingStats?.filter( ( product ) =>
+		product.productSlug.includes( 'pwyw' )
+	);
+
+	if ( plansPWYWStats.length === 0 ) {
+		return false;
+	}
+
+	// PWYW plan confirmed. Check for additional paid plans.
+	const plansExcludingPWYWStats = plansSupportingStats?.filter(
+		( product ) => ! product.productSlug.includes( 'pwyw' )
+	);
+
+	// If we have any plans remaining, the site has paid stats.
+	return plansPWYWStats.length > 0 && plansExcludingPWYWStats.length === 0;
 }
 
 export default hasSiteProductJetpackStatsPWYWOnly;
