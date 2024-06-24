e165b68aee23bb40c05079d918e9fdf82a47ca49
heavyweight <kpapazov@gmail.com>
2024-06-24 12:03:08 +0200

    Remove feature flag check for redirect destination

--- client/signup/config/flows.js
+++ client/signup/config/flows.js
@@ -246,10 +246,6 @@ function getEntrepreneurFlowDestination( { redirect_to } ) {
 }
 
 function getGuidedOnboardingFlowDestination( dependencies ) {
-	if ( ! config.isEnabled( 'onboarding/guided' ) ) {
-		return getSignupDestination( dependencies );
-	}
-
 	const { onboardingSegment, siteSlug, siteId, domainItem, cartItems, refParameter } = dependencies;
 
 	if ( 'no-site' === siteSlug ) {
