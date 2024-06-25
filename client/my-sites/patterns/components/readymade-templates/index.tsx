72b3a9b1e382e1f0bf99fbafe60f43d16dd16446
mmtr <1233880+mmtr@users.noreply.github.com>
2024-06-25 18:31:22 +0200

    Fix missing SCSS var

--- client/my-sites/patterns/components/readymade-templates/index.tsx
+++ client/my-sites/patterns/components/readymade-templates/index.tsx
@@ -13,30 +13,28 @@ export const ReadymadeTemplates = () => {
 	}
 
 	return (
-		<div id="site-layouts">
-			<PatternsSection
-				bodyFullWidth
-				description={ translate(
-					'Explore a collection of beautiful site layouts made with our patterns.'
-				) }
-				theme="dark"
-				title={ translate( 'Ready-to-use site layouts' ) }
-			>
-				<div className="readymade-templates">
-					{ readymadeTemplates.map( ( readymadeTemplate ) => (
-						<a
-							href={ `/patterns/site-layouts/${ readymadeTemplate.template_id } ` }
-							className="readymade-template"
-							key={ readymadeTemplate.template_id }
-						>
-							<div className="readymade-template__content">
-								<img src={ readymadeTemplate.screenshot } alt="" />
-							</div>
-							<div className="readymade-template__title">{ readymadeTemplate.title }</div>
-						</a>
-					) ) }
-				</div>
-			</PatternsSection>
-		</div>
+		<PatternsSection
+			bodyFullWidth
+			description={ translate(
+				'Explore a collection of beautiful site layouts made with our patterns.'
+			) }
+			theme="dark"
+			title={ translate( 'Ready-to-use site layouts' ) }
+		>
+			<div className="readymade-templates">
+				{ readymadeTemplates.map( ( readymadeTemplate ) => (
+					<a
+						href={ `/patterns/site-layouts/${ readymadeTemplate.template_id } ` }
+						className="readymade-template"
+						key={ readymadeTemplate.template_id }
+					>
+						<div className="readymade-template__content">
+							<img src={ readymadeTemplate.screenshot } alt="" />
+						</div>
+						<div className="readymade-template__title">{ readymadeTemplate.title }</div>
+					</a>
+				) ) }
+			</div>
+		</PatternsSection>
 	);
 };
