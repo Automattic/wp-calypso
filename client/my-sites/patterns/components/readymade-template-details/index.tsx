c0084fb72c6cc76a9ea22594966861a72a0ea359
mmtr <1233880+mmtr@users.noreply.github.com>
2024-06-26 09:47:59 +0200

    Implement actions

--- client/my-sites/patterns/components/readymade-template-details/index.tsx
+++ client/my-sites/patterns/components/readymade-template-details/index.tsx
@@ -1,8 +1,8 @@
 import { addLocaleToPathLocaleInFront } from '@automattic/i18n-utils';
 import { Button } from '@wordpress/components';
-import { Icon, arrowLeft } from '@wordpress/icons';
+import { Icon, arrowLeft, check, copy } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { PatternsGetStarted } from 'calypso/my-sites/patterns/components/get-started';
 import { useReadymadeTemplates } from 'calypso/my-sites/patterns/hooks/use-readymade-templates';
 import { ReadymadeTemplateDetailsFC } from 'calypso/my-sites/patterns/types';
@@ -10,11 +10,12 @@ import { ReadymadeTemplateDetailsFC } from 'calypso/my-sites/patterns/types';
 import './style.scss';
 
 export const ReadymadeTemplateDetails: ReadymadeTemplateDetailsFC = ( { id } ) => {
+	const translate = useTranslate();
+	const [ isCopied, setIsCopied ] = useState( false );
+
 	useEffect( () => {
 		window.scroll( 0, 0 );
 	}, [] );
-
-	const translate = useTranslate();
 
 	const { data: readymadeTemplates = [] } = useReadymadeTemplates();
 	if ( ! readymadeTemplates.length ) {
@@ -25,6 +26,11 @@ export const ReadymadeTemplateDetails: ReadymadeTemplateDetailsFC = ( { id } ) =
 	if ( ! readymadeTemplate ) {
 		return null;
 	}
+
+	const copyLayout = () => {
+		navigator.clipboard.writeText( readymadeTemplate.content );
+		setIsCopied( true );
+	};
 
 	return (
 		<>
@@ -41,8 +47,19 @@ export const ReadymadeTemplateDetails: ReadymadeTemplateDetailsFC = ( { id } ) =
 							<div className="readymade-template-details-header">
 								<h1 className="readymade-template-details-title">{ readymadeTemplate.title }</h1>
 								<div className="readymade-template-details-actions">
-									<Button variant="secondary">{ translate( 'Copy layout' ) }</Button>
-									<Button variant="primary">{ translate( 'Pick this layout' ) }</Button>
+									<Button
+										variant="secondary"
+										icon={ isCopied ? check : copy }
+										onClick={ copyLayout }
+									>
+										{ isCopied ? translate( 'Copied' ) : translate( 'Copy layout' ) }
+									</Button>
+									<Button
+										variant="primary"
+										href={ `/setup/readymade-template?readymadeTemplateId=${ readymadeTemplate.template_id }` }
+									>
+										{ translate( 'Pick this layout' ) }
+									</Button>
 								</div>
 							</div>
 							<div
