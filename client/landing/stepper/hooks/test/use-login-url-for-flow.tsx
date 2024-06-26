3cba79e27aed8460b5fdde813d856f6712b7bcdc
Gabriel Caires <gabriel.caires@automattic.com>
2024-06-26 13:28:05 +0100

    Use same lib to manage query args
    The implemention and tests are using different libraries to add query params, it may cause incosistencies

--- client/landing/stepper/hooks/test/use-login-url-for-flow.tsx
+++ client/landing/stepper/hooks/test/use-login-url-for-flow.tsx
@@ -1,10 +1,10 @@
 /**
  * @jest-environment jsdom
  */
+import { addQueryArgs } from '@wordpress/url';
 import React from 'react';
 import { MemoryRouter } from 'react-router';
 import { renderHookWithProvider } from 'calypso/test-helpers/testing-library';
-import { addQueryArgs } from '../../../../lib/route';
 import { type Flow } from '../../declarative-flow/internals/types';
 import { useLoginUrlForFlow } from '../use-login-url-for-flow';
 
@@ -28,15 +28,12 @@ describe( 'useLoginUrlForFlow', () => {
 		} );
 
 		expect( result.current ).toEqual(
-			addQueryArgs(
-				{
-					variationName: 'some-flow',
-					redirect_to: '/setup/site-migration-flow',
-					pageTitle: 'some-title',
-					toStepper: true,
-				},
-				'/start/account/user-social'
-			)
+			addQueryArgs( '/start/account/user-social', {
+				variationName: 'some-flow',
+				redirect_to: '/setup/site-migration-flow',
+				pageTitle: 'some-title',
+				toStepper: true,
+			} )
 		);
 	} );
 
@@ -75,15 +72,12 @@ describe( 'useLoginUrlForFlow', () => {
 		} );
 
 		expect( result.current ).toEqual(
-			addQueryArgs(
-				{
-					variationName: 'some-flow',
-					redirect_to: '/crazy-setup/site-migration-flow',
-					pageTitle: 'some-title',
-					toStepper: true,
-				},
-				'/start/account/user-social'
-			)
+			addQueryArgs( '/start/account/user-social', {
+				variationName: 'some-flow',
+				redirect_to: '/crazy-setup/site-migration-flow',
+				pageTitle: 'some-title',
+				toStepper: true,
+			} )
 		);
 	} );
 } );
