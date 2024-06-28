e5ef2287501d4e32df6698509c3efc66fd8cebbb
Anna McPhee <anna.mcphee@automattic.com>
2024-06-28 12:14:56 +1000

    update selector

--- client/my-sites/stats/features/modules/stats-countries/stats-countries.tsx
+++ client/my-sites/stats/features/modules/stats-countries/stats-countries.tsx
@@ -3,7 +3,7 @@ import { localizeUrl } from '@automattic/i18n-utils';
 import { mapMarker } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector } from 'calypso/state';
 import {
 	isRequestingSiteStatsForQuery,
 	getSiteStatsNormalizedData,
@@ -37,7 +37,7 @@ const StatCountries: React.FC< StatCountriesProps > = ( {
 	const siteId = useSelector( getSelectedSiteId ) as number;
 	const statType = 'statsCountryViews';
 
-	const requesting = useSelector( ( state: any ) =>
+	const requesting = useSelector( ( state ) =>
 		isRequestingSiteStatsForQuery( state, siteId, statType, query )
 	);
 	const data = useSelector( ( state ) =>
