4dcc3ecea6ec1e81cf1bc0f51a4589294b8f38ff
Miroslav Mitev <m1r0@users.noreply.github.com>
2024-07-01 12:40:30 +0300

    Improve tests

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/test/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/test/index.tsx
@@ -14,54 +14,25 @@ const render = ( props?: Partial< StepProps > ) => {
 jest.mock( 'calypso/data/site-profiler/use-hosting-provider-url-details' );
 
 describe( 'SiteMigrationInstructions', () => {
-	it( 'shows the hosting badge when the host is known and not a8c', async () => {
-		useHostingProviderUrlDetails.mockReturnValue( {
-			data: {
-				name: 'WP Engine',
-				is_unknown: false,
-				is_a8c: false,
-			},
-		} );
+	it.each( [
+		{ hostingName: 'WP Engine', isUnknown: false, isA8c: false, expected: true },
+		{ hostingName: 'WordPress.com', isUnknown: false, isA8c: true, expected: false },
+		{ hostingName: 'Unknown', isUnknown: true, isA8c: false, expected: false },
+	] )(
+		'renders the hosting badge only when the hosting is known and not A8C',
+		async ( { hostingName, isUnknown, isA8c, expected } ) => {
+			useHostingProviderUrlDetails.mockReturnValue( {
+				data: {
+					name: hostingName,
+					is_unknown: isUnknown,
+					is_a8c: isA8c,
+				},
+			} );
 
-		const { container, queryByText } = render();
+			const { queryByText } = render();
 
-		expect( container.querySelectorAll( '.migration-instructions-hosting-badge' ) ).toHaveLength(
-			1
-		);
-		expect( queryByText( /WP Engine/ ) ).toBeInTheDocument();
-	} );
-
-	it( "doesn't show the hosting badge when the host is a8c", async () => {
-		useHostingProviderUrlDetails.mockReturnValue( {
-			data: {
-				name: 'WordPress.com',
-				is_unknown: false,
-				is_a8c: true,
-			},
-		} );
-
-		const { container, queryByText } = render();
-
-		expect( container.querySelectorAll( '.migration-instructions-hosting-badge' ) ).toHaveLength(
-			0
-		);
-		expect( queryByText( /WordPress.com/ ) ).not.toBeInTheDocument();
-	} );
-
-	it( "doesn't show the hosting badge when the host is unknown", async () => {
-		useHostingProviderUrlDetails.mockReturnValue( {
-			data: {
-				name: 'unknown',
-				is_unknown: true,
-				is_a8c: false,
-			},
-		} );
-
-		const { container, queryByText } = render();
-
-		expect( container.querySelectorAll( '.migration-instructions-hosting-badge' ) ).toHaveLength(
-			0
-		);
-		expect( queryByText( /unknown/ ) ).not.toBeInTheDocument();
-	} );
+			const maybeExpect = ( arg, exp ) => ( exp ? expect( arg ) : expect( arg ).not );
+			maybeExpect( queryByText( `Hosted with ${ hostingName }` ), expected ).toBeInTheDocument();
+		}
+	);
 } );
