b54d66cb62a67d70a88054e9d888aed4e6f98906
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-07-02 01:53:36 +0800

    Implement AddSiteViaJetpackConnectionModal component.

--- client/a8c-for-agencies/components/a4a-themed-modal/index.tsx
+++ client/a8c-for-agencies/components/a4a-themed-modal/index.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@automattic/components';
 import { Modal } from '@wordpress/components';
 import { Icon, close } from '@wordpress/icons';
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 import './style.scss';
 
 type Props = {
+	className?: string;
 	children: ReactNode;
 	onClose?: () => void;
 	sidebarImage?: string;
@@ -13,13 +15,18 @@ type Props = {
 };
 
 export default function A4AThemedModal( {
+	className,
 	children,
 	onClose = () => {},
 	sidebarImage,
 	dismissable,
 }: Props ) {
 	return (
-		<Modal className="a4a-themed-modal" onRequestClose={ onClose } __experimentalHideHeader>
+		<Modal
+			className={ clsx( 'a4a-themed-modal', className ) }
+			onRequestClose={ onClose }
+			__experimentalHideHeader
+		>
 			<div className="a4a-themed-modal__wrapper">
 				<img className="a4a-themed-modal__sidebar-image" src={ sidebarImage } alt="" />
 				<div className="a4a-themed-modal__content">
