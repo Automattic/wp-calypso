cd11c534e0c50bf0c3adc5e3154d18a878050928
Christos <chriskmnds@gmail.com>
2024-07-01 12:16:17 +0300

    TODOs

--- client/state/purchases/selectors/get-downgrade-plan-from-purchase.js
+++ client/state/purchases/selectors/get-downgrade-plan-from-purchase.js
@@ -1,8 +1,11 @@
 import { getPlan, findPlansKeys, TYPE_PERSONAL } from '@automattic/calypso-products';
 
+// TODO clk: is this needed? confirm
 import 'calypso/state/purchases/init';
 
+// TODO: clk: rename to getDowngradePlan and pass planSlug
 export const getDowngradePlanFromPurchase = ( purchase ) => {
+	// TODO clk: purchase.productSlug is the planSlug
 	const plan = getPlan( purchase.productSlug );
 	if ( ! plan ) {
 		return null;
