73bc6a8b1fe43472421ff140d28bde9fedb59b44
Addison-Stavlo <stavz01@gmail.com>
2024-07-01 13:47:04 -0400

    remove sticky position restriction from chromium

--- apps/notifications/src/panel/templates/shortcuts-popover.jsx
+++ apps/notifications/src/panel/templates/shortcuts-popover.jsx
@@ -127,9 +127,7 @@ export const ShortcutsPopover = ( {
 						} }
 						ref={ spanRef }
 					>
-						{ /* Attach the popover to this anchor instead of the button, so we can have retain position with scrolling. */ }
-						<div className="wpnc__keyboard-shortcuts-popover-anchor" ref={ popoverAnchorRef } />
-						<Gridicon icon="info-outline" size={ 18 } />
+						<Gridicon icon="info-outline" size={ 18 } ref={ popoverAnchorRef } />
 					</button>
 				</HotkeyContainer>
 			) }
