1c3b27212ebbb0bc6e229d010ea773aaff8e891d
Welly Shen <hivoid19@gmail.com>
2024-07-01 15:54:24 +0800

    Gravatar: Implement magic code signup / login (#91724)
    * Gravatar: Implement magic code signup / login
    
    * Use `gravatar_from` instead of `gravatar_auth_source`
    
    * Adjust mobile layout for the verification code view
    
    * Add secondary email options view
    
    * Adjust font size and fix icon cutted issue
    
    * Update switch email event name
    
    * Integrate gravatar-info API
    
    * Implement authenticated with code
    
    * Code refactor
    
    * Use real data & Add tracking events
    
    * Code refactor
    
    * Handle code validation UX and errors
    
    * Address secondary email case
    
    * Code refactor
    
    * Update page title
    
    * Fix: display the correct email & resend email bug
    
    * Adjust mobile and footer layout
    
    * Use `rebootAfterLogin`
    
    * Correct title
    
    * Handle 2FA redirection
    
    * Remove todos
    
    * Refine tracking event names
    
    * Update comment
    
    * Update comment
    
    * Ensure the code validated logic only be triggered once
    
    * Use Calypso form components
    
    * Address API fetching UX
    
    * Align the styles of the password page with other pages
    
    * Use calypso/lib/wp lib & Correct gravatar-info API error logic
    
    * Use gravatar-info API v1
    
    * Rename `username` to `mainUsername`
    
    * Track the event of backing to the email form from the secondary email options view
    
    * Fix: Use error.status for the `error_code` event property
    
    * Code refactor
    
    * Code refactor
    
    * Remove un-necessary code
    
    * Trigger `onErrorDismiss` event when it is set
    
    * Revert `onErrorDismiss` event logic
    
    * Refine styles
    
    * Refine styles
    
    * Fix: Email code verification text logic
    
    * Fix: Email code verification text logic
    
    * Adjust code verification layout
    
    * Fix: Email form title font size for WPJM
    
    * Fix: The footer line-height of WPJM
    
    * Fix: The footer line-height of WPJM
    
    * Fix: Switch email function for WPJM
    
    * Fix: Password login page styles for WPJM
    
    * Fix: Update footer wording for Gravatar magic code
    
    * Keep both Gravatar magic link and magic code
    
    * Address feedback & Fix: Use the right state to disable the "Resend again" button"
    
    * Add "Switch email" option on the magic code verification view
    
    * Fix: Email input error styles
    
    * Fix: prop type
    
    * Fix: Use hashed email instead of `main_email`
    
    * Check the `window.crypto` API before using it
    
    * Revert un-related file
    
    * Fix: `is3rdPartyApp` only works for Gravatar

--- client/lib/paths/login/index.js
+++ client/lib/paths/login/index.js
@@ -40,6 +40,8 @@ export function login( {
 	action = undefined,
 	lostpasswordFlow = undefined,
 	usernameOnly = undefined,
+	gravatarMagicCode = undefined,
+	gravatarFrom = undefined,
 } = {} ) {
 	let url = '/log-in';
 
@@ -109,6 +111,14 @@ export function login( {
 
 	if ( usernameOnly ) {
 		url = addQueryArgs( { username_only: true }, url );
+	}
+
+	if ( gravatarMagicCode ) {
+		url = addQueryArgs( { gravatar_magic_code: true }, url );
+	}
+
+	if ( gravatarFrom ) {
+		url = addQueryArgs( { gravatar_from: gravatarFrom }, url );
 	}
 
 	return url;
