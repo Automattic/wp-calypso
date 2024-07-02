d7a392d57977206b737e46d9b05ca184a3253497
Addison-Stavlo <stavz01@gmail.com>
2024-07-02 13:45:45 -0400

    remove i shortkey info from popover

--- apps/notifications/src/panel/templates/shortcuts-popover.jsx
+++ apps/notifications/src/panel/templates/shortcuts-popover.jsx
@@ -94,10 +94,6 @@ export const ShortcutsPopover = ( {
 							<span className="description">{ translate( 'View Likes' ) }</span>
 							<span className="shortcut letter">l</span>
 						</li>
-						<li>
-							<span className="description">{ translate( 'Toggle Shortcuts Menu' ) }</span>
-							<span className="shortcut letter">i</span>
-						</li>
 					</ul>
 				</div>
 			</Popover>
