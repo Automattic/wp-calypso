2a5c4740e2fbc83dedadcf8be074cd7a63237449
Omar Alshaker <omar@omaralshaker.com>
2024-07-03 20:28:56 +0200

    Add instructions to welcome.js

--- bin/mock-wordpressdotcom.sh
+++ bin/mock-wordpressdotcom.sh
@@ -1,8 +1,6 @@
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
-echo "To mock WordPress.com locally:"
-
-echo "  1. Add ${GREEN}127.0.0.1 wordpress.com${NC} to your hosts file."
-echo "  2. Restart Calypso with ${GREEN}yarn start-mock-wordpress-com${NC}."
-echo "  3. Type ${GREEN}thisisunsafe${NC} in Chrome when you visit wordpress.com."
+echo "  - Add ${GREEN}127.0.0.1 wordpress.com${NC} to your hosts file."
+echo "  - Restart Calypso with ${GREEN}yarn start-mock-wordpress-com${NC}."
+echo "  - Type ${GREEN}thisisunsafe${NC} in Chrome when you visit wordpress.com."
