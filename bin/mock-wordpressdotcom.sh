b250564b9761ee75061b21eec161936436accc30
Omar Alshaker <omar@omaralshaker.com>
2024-07-03 20:16:21 +0200

    Keep instructions

--- bin/mock-wordpressdotcom.sh
+++ bin/mock-wordpressdotcom.sh
@@ -1,4 +1,8 @@
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
-echo "Please use ${GREEN}yarn start-mock-wordpress-com${NC}"
+echo "To mock WordPress.com locally:"
+
+echo "  1. Add ${GREEN}127.0.0.1 wordpress.com${NC} to your hosts file."
+echo "  2. Restart Calypso with ${GREEN}yarn start-mock-wordpress-com${NC}."
+echo "  3. Type ${GREEN}thisisunsafe${NC} in Chrome when you visit wordpress.com."
