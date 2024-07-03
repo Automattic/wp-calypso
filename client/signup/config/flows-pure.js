087d333a77735818abd27ec55a915b25d72a5a2c
Andrés Blanco <email@gmail.com>
2024-07-03 14:45:46 -0300

    redirect to /setup/migration-signup

--- client/signup/config/flows-pure.js
+++ client/signup/config/flows-pure.js
@@ -249,9 +249,8 @@ export function generateFlows( {
 		},
 		{
 			name: 'import',
-			steps: [ userSocialStep, 'domains', 'plans-import' ],
-			destination: ( dependencies ) =>
-				`/setup/import-focused/import?siteSlug=${ dependencies.siteSlug }`,
+			steps: [ userSocialStep ],
+			destination: () => `/setup/migration-signup`,
 			description: 'Beginning of the flow to import content',
 			lastModified: '2023-10-11',
 			showRecaptcha: true,
