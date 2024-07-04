39c152a72ed53e5ed72088590e269f10837e80ca
Tony Arcangelini <tony@arcangelini.com>
2024-07-04 12:14:21 +0200

    Use forward ref

--- packages/help-center/src/components/help-center-container.tsx
+++ packages/help-center/src/components/help-center-container.tsx
@@ -7,7 +7,7 @@ import { Card } from '@wordpress/components';
 import { useFocusReturn, useMergeRefs } from '@wordpress/compose';
 import { useSelect, useDispatch } from '@wordpress/data';
 import clsx from 'clsx';
-import { useState, useRef, useEffect, useCallback, FC } from 'react';
+import { useState, useRef, useEffect, useCallback, FC, forwardRef } from 'react';
 import Draggable, { DraggableProps } from 'react-draggable';
 import { MemoryRouter } from 'react-router-dom';
 /**
@@ -34,118 +34,112 @@ const OptionalDraggable: FC< OptionalDraggableProps > = ( { draggable, ...props 
 	return <Draggable { ...props } />;
 };
 
-const HelpCenterContainer: React.FC< Container > = ( {
-	handleClose,
-	hidden,
-	currentRoute,
-	lastClickEvent,
-} ) => {
-	const { show, isMinimized, initialRoute } = useSelect( ( select ) => {
-		const store = select( HELP_CENTER_STORE ) as HelpCenterSelect;
-		return {
-			show: store.isHelpCenterShown(),
-			isMinimized: store.getIsMinimized(),
-			initialRoute: store.getInitialRoute(),
-		};
-	}, [] );
+const HelpCenterContainer: React.FC< Container > = forwardRef(
+	( { handleClose, hidden, currentRoute, lastClickEvent }, nodeRef ) => {
+		const { show, isMinimized, initialRoute } = useSelect( ( select ) => {
+			const store = select( HELP_CENTER_STORE ) as HelpCenterSelect;
+			return {
+				show: store.isHelpCenterShown(),
+				isMinimized: store.getIsMinimized(),
+				initialRoute: store.getInitialRoute(),
+			};
+		}, [] );
 
-	const { setIsMinimized } = useDispatch( HELP_CENTER_STORE );
+		const { setIsMinimized } = useDispatch( HELP_CENTER_STORE );
 
-	const [ clickedOpenPosition, setClickedOpenPosition ] = useState( { left: 0, top: 0 } );
-	const [ isVisible, setIsVisible ] = useState( true );
-	const isMobile = useMobileBreakpoint();
-	const classNames = clsx( 'help-center__container', isMobile ? 'is-mobile' : 'is-desktop', {
-		'is-minimized': isMinimized,
-	} );
+		const [ clickedOpenPosition, setClickedOpenPosition ] = useState( { left: 0, top: 0 } );
+		const [ isVisible, setIsVisible ] = useState( true );
+		const isMobile = useMobileBreakpoint();
+		const classNames = clsx( 'help-center__container', isMobile ? 'is-mobile' : 'is-desktop', {
+			'is-minimized': isMinimized,
+		} );
 
-	const onDismiss = useCallback( () => {
-		setIsVisible( false );
-		recordTracksEvent( `calypso_inlinehelp_close` );
-	}, [ setIsVisible, recordTracksEvent ] );
+		const onDismiss = useCallback( () => {
+			setIsVisible( false );
+			recordTracksEvent( `calypso_inlinehelp_close` );
+		}, [ setIsVisible, recordTracksEvent ] );
 
-	const toggleVisible = () => {
-		if ( ! isVisible ) {
-			handleClose();
-			// after calling handleClose, reset the visibility state to default
-			setIsVisible( true );
-		}
-	};
-
-	const animationProps = {
-		style: {
-			animation: `${ isVisible ? 'fadeIn' : 'fadeOut' } .5s`,
-			top: `${ clickedOpenPosition.top }px`,
-			left: `${ clickedOpenPosition.left }px`,
-		},
-		onAnimationEnd: toggleVisible,
-	};
-	// This is a workaround for an issue with Draggable in StrictMode
-	// https://github.com/react-grid-layout/react-draggable/blob/781ef77c86be9486400da9837f43b96186166e38/README.md
-	const nodeRef = useRef( null );
-
-	const focusReturnRef = useFocusReturn();
-
-	const cardMergeRefs = useMergeRefs( [ nodeRef, focusReturnRef ] );
-
-	const shouldCloseOnEscapeRef = useRef( false );
-
-	shouldCloseOnEscapeRef.current = !! show && ! hidden && ! isMinimized;
-
-	// Set Help Center initial position relative to the last click
-	useEffect( () => {
-		if ( show && nodeRef.current ) {
-			const { left, top } = calculateOpeningPosition(
-				lastClickEvent as MouseEvent,
-				nodeRef.current
-			);
-
-			setClickedOpenPosition( {
-				left: left,
-				top: top,
-			} );
-		}
-	}, [ show ] );
-
-	useEffect( () => {
-		const handleKeydown = ( e: KeyboardEvent ) => {
-			if ( e.key === 'Escape' && shouldCloseOnEscapeRef.current ) {
-				onDismiss();
+		const toggleVisible = () => {
+			if ( ! isVisible ) {
+				handleClose();
+				// after calling handleClose, reset the visibility state to default
+				setIsVisible( true );
 			}
 		};
 
-		document.addEventListener( 'keydown', handleKeydown );
-		return () => {
-			document.removeEventListener( 'keydown', handleKeydown );
+		const animationProps = {
+			style: {
+				animation: `${ isVisible ? 'fadeIn' : 'fadeOut' } .5s`,
+				top: `${ clickedOpenPosition.top }px`,
+				left: `${ clickedOpenPosition.left }px`,
+			},
+			onAnimationEnd: toggleVisible,
 		};
-	}, [ shouldCloseOnEscapeRef, onDismiss ] );
 
-	if ( ! show || hidden ) {
-		return null;
+		const focusReturnRef = useFocusReturn();
+
+		const cardMergeRefs = useMergeRefs( [ nodeRef, focusReturnRef ] );
+
+		const shouldCloseOnEscapeRef = useRef( false );
+
+		shouldCloseOnEscapeRef.current = !! show && ! hidden && ! isMinimized;
+
+		// Set Help Center initial position relative to the last click
+		useEffect( () => {
+			if ( show && nodeRef.current ) {
+				const { left, top } = calculateOpeningPosition(
+					lastClickEvent as MouseEvent,
+					nodeRef.current
+				);
+
+				setClickedOpenPosition( {
+					left: left,
+					top: top,
+				} );
+			}
+		}, [ show ] );
+
+		useEffect( () => {
+			const handleKeydown = ( e: KeyboardEvent ) => {
+				if ( e.key === 'Escape' && shouldCloseOnEscapeRef.current ) {
+					onDismiss();
+				}
+			};
+
+			document.addEventListener( 'keydown', handleKeydown );
+			return () => {
+				document.removeEventListener( 'keydown', handleKeydown );
+			};
+		}, [ shouldCloseOnEscapeRef, onDismiss ] );
+
+		if ( ! show || hidden ) {
+			return null;
+		}
+
+		return (
+			<MemoryRouter initialEntries={ initialRoute ? [ initialRoute ] : undefined }>
+				<FeatureFlagProvider>
+					<OptionalDraggable
+						draggable={ ! isMobile && ! isMinimized }
+						nodeRef={ nodeRef }
+						handle=".help-center__container-header"
+						bounds="body"
+					>
+						<Card className={ classNames } { ...animationProps } ref={ cardMergeRefs }>
+							<HelpCenterHeader
+								isMinimized={ isMinimized }
+								onMinimize={ () => setIsMinimized( true ) }
+								onMaximize={ () => setIsMinimized( false ) }
+								onDismiss={ onDismiss }
+							/>
+							<HelpCenterContent currentRoute={ currentRoute } />
+							{ ! isMinimized && <HelpCenterFooter /> }
+						</Card>
+					</OptionalDraggable>
+				</FeatureFlagProvider>
+			</MemoryRouter>
+		);
 	}
-
-	return (
-		<MemoryRouter initialEntries={ initialRoute ? [ initialRoute ] : undefined }>
-			<FeatureFlagProvider>
-				<OptionalDraggable
-					draggable={ ! isMobile && ! isMinimized }
-					nodeRef={ nodeRef }
-					handle=".help-center__container-header"
-					bounds="body"
-				>
-					<Card className={ classNames } { ...animationProps } ref={ cardMergeRefs }>
-						<HelpCenterHeader
-							isMinimized={ isMinimized }
-							onMinimize={ () => setIsMinimized( true ) }
-							onMaximize={ () => setIsMinimized( false ) }
-							onDismiss={ onDismiss }
-						/>
-						<HelpCenterContent currentRoute={ currentRoute } />
-						{ ! isMinimized && <HelpCenterFooter /> }
-					</Card>
-				</OptionalDraggable>
-			</FeatureFlagProvider>
-		</MemoryRouter>
-	);
-};
+);
 
 export default HelpCenterContainer;
