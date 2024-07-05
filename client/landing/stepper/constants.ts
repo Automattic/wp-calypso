120a7d0c0f18b163ff6e68ab85416fd4c2329a88
Dean Sas <dean.sas@automattic.com>
2024-07-05 10:27:40 +0100

    Simplify the Design object (#92324)
    Remove the `is_premium` prop from the Design (and related) types. It's
    unnecessary as it can be calculated from the theme's `design_type`
    property, and using that will force people to be more intentional about
    whether they mean free, paid, premium tier etc.

--- client/landing/stepper/constants.ts
+++ client/landing/stepper/constants.ts
@@ -3,7 +3,6 @@ import type { Design } from '@automattic/design-picker';
 export const WRITE_INTENT_DEFAULT_DESIGN: Design = {
 	slug: 'hey',
 	title: 'Hey',
-	is_premium: false,
 	categories: [],
 	theme: 'hey',
 	design_tier: null,
