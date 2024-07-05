04ad634bcae2c27a8137491411fc5df9350067e8
valterlorran <valterlorran@hotmail.com>
2024-07-05 12:31:02 -0300

    Revert "Fix deprecated react render warning (#92218)" (#92440)
    This reverts commit 3928f016aab92408a15355b99d654fb758b15764.

--- client/landing/stepper/index.tsx
+++ client/landing/stepper/index.tsx
@@ -8,7 +8,7 @@ import { IMPORT_HOSTED_SITE_FLOW } from '@automattic/onboarding';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { useDispatch } from '@wordpress/data';
 import defaultCalypsoI18n from 'i18n-calypso';
-import { createRoot } from 'react-dom/client';
+import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, matchPath } from 'react-router-dom';
 import { requestAllBlogsAccess } from 'wpcom-proxy-request';
@@ -118,8 +118,7 @@ window.AppBoot = async () => {
 	const flowLoader = determineFlow();
 	const { default: flow } = await flowLoader();
 
-	const root = createRoot( document.getElementById( 'wpcom' )! );
-	root.render(
+	ReactDom.render(
 		<CalypsoI18nProvider i18n={ defaultCalypsoI18n }>
 			<Provider store={ reduxStore }>
 				<QueryClientProvider client={ queryClient }>
@@ -141,6 +140,7 @@ window.AppBoot = async () => {
 					) }
 				</QueryClientProvider>
 			</Provider>
-		</CalypsoI18nProvider>
+		</CalypsoI18nProvider>,
+		document.getElementById( 'wpcom' )
 	);
 };
