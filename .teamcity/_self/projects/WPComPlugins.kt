7abf0dfac5ec03788fa4092f86726f39b1491d37
arthur791004 <arthur.chu@automattic.com>
2024-07-08 20:04:23 +0900

    Whats New: Add the app to Team City build (#92482)
    * WPComPlugins: Add whats-new app
    
    * Fix description

--- .teamcity/_self/projects/WPComPlugins.kt
+++ .teamcity/_self/projects/WPComPlugins.kt
@@ -44,6 +44,7 @@ object WPComPlugins : Project({
 					"happy-blocks-release-build",
 					"command-palette-wp-admin-release-build",
 					"help-center-release-build",
+					"whats-new-release-build",
 				)
 			}
 			dataToKeep = everything()
@@ -127,6 +128,7 @@ object CalypsoApps: BuildType({
 		apps/editing-toolkit/editing-toolkit-plugin => editing-toolkit.zip
 		apps/command-palette-wp-admin/dist => command-palette-wp-admin.zip
 		apps/help-center/dist => help-center.zip
+		apps/whats-new/dist => whats-new.zip
 	""".trimIndent()
 
 	steps {
