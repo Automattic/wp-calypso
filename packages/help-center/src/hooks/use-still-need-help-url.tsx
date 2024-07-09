d8d0085407322b863a1d8131486325df2d257bfa
Aneesh Devasthale <aneesh.devasthale@automattic.com>
2024-07-09 18:37:19 +0530

    Open chat on downgrade click.

--- packages/help-center/src/hooks/use-still-need-help-url.tsx
+++ packages/help-center/src/hooks/use-still-need-help-url.tsx
@@ -3,17 +3,14 @@
 import { useSupportStatus } from '../data/use-support-status';
 import { useShouldUseWapuu } from './use-should-use-wapuu';
 
-export function useStillNeedHelpURL( forceContactForm = false ) {
+export function useStillNeedHelpURL() {
 	const { data: supportStatus, isLoading } = useSupportStatus();
 	const shouldUseWapuu = useShouldUseWapuu();
 	const isEligibleForSupport = Boolean( supportStatus?.eligibility?.is_user_eligible );
 
-	if ( isEligibleForSupport && shouldUseWapuu && ! forceContactForm ) {
-		return { url: '/odie', isLoading: false };
-	}
-
 	if ( isEligibleForSupport ) {
-		return { url: '/contact-options', isLoading: false };
+		const url = shouldUseWapuu ? '/odie' : '/contact-options';
+		return { url, isLoading: false };
 	}
 
 	return { url: '/contact-form?mode=FORUM', isLoading };
