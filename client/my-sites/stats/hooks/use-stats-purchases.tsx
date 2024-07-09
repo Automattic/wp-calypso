bd0b31cbb7bd85c471a7e97556d52b597ff2e7a6
Jasper Kang <kangzjnet@gmail.com>
2024-07-09 13:46:10 +1200

    fix typo

--- client/my-sites/stats/hooks/use-stats-purchases.tsx
+++ client/my-sites/stats/hooks/use-stats-purchases.tsx
@@ -123,7 +123,7 @@ export default function useStatsPurchases( siteId: number | null ) {
 		isLegacyCommercialLicense,
 		hasLoadedSitePurchases,
 		hasAnyPlan: isFreeOwned || isCommercialOwned || isPWYWOwned || supportCommercialUse,
-		hasAnyStatsPlan: isCommercialOwned || isPWYWOwned || isPWYWOwned,
+		hasAnyStatsPlan: isCommercialOwned || isPWYWOwned || isFreeOwned,
 		isLoading: ! hasLoadedSitePurchases || isRequestingSitePurchases,
 	};
 }
