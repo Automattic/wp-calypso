fc3790577d02237adbbf15d20cb4bef59aef45ae
sejas <antonio@sejas.es>
2024-07-09 10:45:37 +0100

    Undo unlock string for global-styles package

--- packages/global-styles/src/gutenberg-bridge/index.tsx
+++ packages/global-styles/src/gutenberg-bridge/index.tsx
@@ -12,7 +12,7 @@ import { isPlainObject } from 'is-plain-object';
 import type { GlobalStylesObject, GlobalStylesContextObject } from '../types';
 
 const { unlock } = __dangerousOptInToUnstableAPIsOnlyForCoreModules(
-	'I acknowledge private features are not for use in themes or plugins and doing so will break in the next version of WordPress.',
+	'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.',
 	'@wordpress/block-editor'
 );
 
