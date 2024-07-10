24ea311ac2028032f169f973a75bf813312bbc63
Dognose <dognose24@gmail.com>
2024-07-10 23:08:25 +0800

    Patch upsell copies for the Insights page

--- client/my-sites/stats/stats-card-upsell/stats-card-upsell-jetpack.tsx
+++ client/my-sites/stats/stats-card-upsell/stats-card-upsell-jetpack.tsx
@@ -6,7 +6,7 @@ import { localizeUrl } from '@automattic/i18n-utils';
 import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
 import React from 'react';
-import { SUPPORT_URL } from 'calypso/my-sites/stats/const';
+import { SUPPORT_URL, INSIGHTS_SUPPORT_URL } from 'calypso/my-sites/stats/const';
 import { useSelector } from 'calypso/state';
 import getIsSiteWPCOM from 'calypso/state/selectors/is-site-wpcom';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
@@ -22,15 +22,31 @@ import {
 	STAT_TYPE_EMAILS_SUMMARY,
 	STAT_TYPE_SEARCH_TERMS,
 	STAT_TYPE_VIDEO_PLAYS,
+	STAT_TYPE_INSIGHTS_ALL_TIME_STATS,
+	STAT_TYPE_INSIGHTS_MOST_POPULAR_TIME,
+	STAT_TYPE_INSIGHTS_MOST_POPULAR_DAY,
+	STAT_TYPE_INSIGHTS_ALL_TIME_INSIGHTS,
+	STAT_TYPE_TAGS,
+	STAT_TYPE_COMMENTS,
 } from '../constants';
 import { trackStatsAnalyticsEvent } from '../utils';
 import StatsCardUpsellOverlay from './stats-card-upsell-overlay';
 import { Props } from './';
 
-const supportLinkWithAnchor = ( anchor: string ) => {
+const trafficSupportLinkWithAnchor = ( anchor: string ) => {
 	return (
 		<a
 			href={ localizeUrl( `${ SUPPORT_URL }#${ anchor }` ) }
+			target="_blank"
+			rel="noopenner noreferrer"
+		/>
+	);
+};
+
+const insightsSupportLinkWithAnchor = ( anchor: string ) => {
+	return (
+		<a
+			href={ localizeUrl( `${ INSIGHTS_SUPPORT_URL }#${ anchor }` ) }
 			target="_blank"
 			rel="noopenner noreferrer"
 		/>
@@ -49,7 +65,7 @@ const useUpsellCopy = ( statType: string ) => {
 		case STAT_TYPE_TOP_POSTS:
 			return translate( 'Track which {{link}}Posts and Pages{{/link}} receive the most views.', {
 				components: {
-					link: supportLinkWithAnchor( 'posts-amp-pages' ),
+					link: trafficSupportLinkWithAnchor( 'posts-amp-pages' ),
 				},
 			} );
 		case STAT_TYPE_REFERRERS:
@@ -57,26 +73,26 @@ const useUpsellCopy = ( statType: string ) => {
 				'Discover which websites are {{link}}referring visitors{{/link}} to your site.',
 				{
 					components: {
-						link: supportLinkWithAnchor( 'referrers' ),
+						link: trafficSupportLinkWithAnchor( 'referrers' ),
 					},
 				}
 			);
 		case STAT_TYPE_COUNTRY_VIEWS:
 			return translate( 'Upgrade to track visitors by {{link}}location{{/link}}.', {
 				components: {
-					link: supportLinkWithAnchor( 'countries' ),
+					link: trafficSupportLinkWithAnchor( 'countries' ),
 				},
 			} );
 		case STAT_TYPE_CLICKS:
 			return translate( 'View the most clicked {{link}}external links{{/link}} on your site.', {
 				components: {
-					link: supportLinkWithAnchor( 'clicks' ),
+					link: trafficSupportLinkWithAnchor( 'clicks' ),
 				},
 			} );
 		case STAT_TYPE_TOP_AUTHORS:
 			return translate( 'See how much traffic each {{link}}author{{/link}} has generated.', {
 				components: {
-					link: supportLinkWithAnchor( 'authors' ),
+					link: trafficSupportLinkWithAnchor( 'authors' ),
 				},
 			} );
 		case STAT_TYPE_EMAILS_SUMMARY:
@@ -84,22 +100,42 @@ const useUpsellCopy = ( statType: string ) => {
 				'View information about {{link}}emails{{/link}} sent to your subscribers.',
 				{
 					components: {
-						link: supportLinkWithAnchor( 'emails' ),
+						link: trafficSupportLinkWithAnchor( 'emails' ),
 					},
 				}
 			);
 		case STAT_TYPE_SEARCH_TERMS:
 			return translate( 'See {{link}}terms that visitors search{{/link}} to find your site.', {
 				components: {
-					link: supportLinkWithAnchor( 'search-terms' ),
+					link: trafficSupportLinkWithAnchor( 'search-terms' ),
 				},
 			} );
 		case STAT_TYPE_VIDEO_PLAYS:
 			return translate( 'Engagement information on your most viewed {{link}}videos{{/link}}.', {
 				components: {
-					link: supportLinkWithAnchor( 'videos' ),
+					link: trafficSupportLinkWithAnchor( 'videos' ),
 				},
 			} );
+		case STAT_TYPE_INSIGHTS_ALL_TIME_STATS:
+			return translate( 'All-time {{link}}website insights{{/link}}.', {
+				components: {
+					link: insightsSupportLinkWithAnchor( 'all-time-highlights' ),
+				},
+			} );
+		case STAT_TYPE_INSIGHTS_MOST_POPULAR_TIME:
+			return translate( 'Best day and hour to post.' );
+		case STAT_TYPE_INSIGHTS_MOST_POPULAR_DAY:
+			return translate( 'Most popular day this year.' );
+		case STAT_TYPE_INSIGHTS_ALL_TIME_INSIGHTS:
+			return translate( 'All-time views.' );
+		case STAT_TYPE_TAGS:
+			return translate( 'Track the most viewed {{link}}tags & categories{{/link}}.', {
+				components: {
+					link: insightsSupportLinkWithAnchor( ':~:text=Tags%20,%20Categories' ),
+				},
+			} );
+		case STAT_TYPE_COMMENTS:
+			return translate( 'Top comments overview.' );
 		default:
 			return translate( 'Upgrade to unlock the feature' );
 	}
