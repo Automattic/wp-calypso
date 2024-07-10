8d849a5792740da20c6e8b0e8c330232ade871f0
Dognose <dognose24@gmail.com>
2024-07-10 15:54:52 +0800

    Dispatch plan usage to Redux for check with the state

---
+++ client/state/stats/plan-usage/selectors.ts
@@ -0,0 +1,14 @@
+import { get } from 'lodash';
+import { PlanUsage } from 'calypso/my-sites/stats/hooks/use-plan-usage-query';
+
+import 'calypso/state/stats/init';
+
+export function getPlanUsageBillableMonthlyViews( state: object, siteId: number ) {
+	const data = get( state, [ 'stats', 'planUsage', 'data', siteId ], null ) as PlanUsage;
+	const recentViews =
+		data?.recent_usages
+			?.map( ( usage ) => usage?.views_count ?? 0 )
+			.filter( ( views ) => views > 0 ) ?? [];
+
+	return recentViews.length > 0 ? Math.min( ...recentViews ) : 0;
+}
