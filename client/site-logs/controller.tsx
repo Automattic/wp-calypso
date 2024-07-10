057ca5abd864da3ce66df5fc259019b33f6875c2
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-07-10 08:06:46 +0700

    Rename outermost div for consistency

--- client/site-logs/controller.tsx
+++ client/site-logs/controller.tsx
@@ -5,7 +5,7 @@ import type { Context as PageJSContext } from '@automattic/calypso-router';
 
 export function phpErrorLogs( context: PageJSContext, next: () => void ) {
 	context.primary = (
-		<div className="site-logs-overview">
+		<div className="site-logs">
 			<PageViewTracker path="/site-logs/:site/php" title="PHP Error Logs" />
 			<LogsHeader logType="php" />
 			<LogsTab logType="php" />
@@ -17,7 +17,7 @@ export function phpErrorLogs( context: PageJSContext, next: () => void ) {
 
 export function httpRequestLogs( context: PageJSContext, next: () => void ) {
 	context.primary = (
-		<div className="site-logs-overview">
+		<div className="site-logs">
 			<PageViewTracker path="/site-logs/:site/web" title="Web Server Logs" />
 			<LogsHeader logType="web" />
 			<LogsTab logType="web" />
