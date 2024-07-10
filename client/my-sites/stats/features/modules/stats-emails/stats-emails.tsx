2ee5962b30c25422c0f00cfa46ebd7da071ea293
Anna McPhee <anna.mcphee@automattic.com>
2024-07-10 11:37:47 +1000

    fresh start :)

---
+++ client/my-sites/stats/features/modules/stats-emails/stats-emails.tsx
@@ -0,0 +1,147 @@
+import { StatsCard } from '@automattic/components';
+import { mail } from '@automattic/components/src/icons';
+import { localizeUrl } from '@automattic/i18n-utils';
+import clsx from 'clsx';
+import { useTranslate } from 'i18n-calypso';
+import React from 'react';
+import QuerySiteStats from 'calypso/components/data/query-site-stats';
+import { useSelector } from 'calypso/state';
+import {
+	isRequestingSiteStatsForQuery,
+	getSiteStatsNormalizedData,
+} from 'calypso/state/stats/lists/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import EmptyModuleCard from '../../../components/empty-module-card/empty-module-card';
+import { SUPPORT_URL } from '../../../const';
+import StatsModule from '../../../stats-module';
+import StatsModulePlaceholder from '../../../stats-module/placeholder';
+import statsStrings from '../../../stats-strings';
+import StatsEmptyActionEmail from '../shared/stats-empty-action-email';
+import type { StatsDefaultModuleProps, StatsStateProps } from '../types';
+
+const StatEmails: React.FC< StatsDefaultModuleProps > = ( {
+	period,
+	query,
+	moduleStrings,
+	className,
+}: StatsDefaultModuleProps ) => {
+	const translate = useTranslate();
+	const siteId = useSelector( getSelectedSiteId ) as number;
+	const statType = 'statsEmailsSummary';
+
+	const requesting = useSelector( ( state: StatsStateProps ) =>
+		isRequestingSiteStatsForQuery( state, siteId, statType, query )
+	);
+	const data = useSelector( ( state ) =>
+		getSiteStatsNormalizedData( state, siteId, statType, query )
+	) as [ id: number, label: string ];
+
+	return (
+		<>
+			{ siteId && statType && (
+				<QuerySiteStats statType={ statType } siteId={ siteId } query={ query } />
+			) }
+			{ requesting && <StatsModulePlaceholder isLoading={ requesting } /> }
+			{ ! data?.length ? (
+				<StatsCard
+					className={ clsx( 'stats-card--empty-variant', className ) }
+					title={ translate( 'Emails' ) }
+					isEmpty
+					emptyMessage={
+						<EmptyModuleCard
+							icon={ mail }
+							description={ translate(
+								'Learn about your {{link}}latest emails sent{{/link}} to better understand how they performed. Start sending!',
+								{
+									comment: '{{link}} links to support documentation.',
+									components: {
+										link: <a href={ localizeUrl( `${ SUPPORT_URL }#emails` ) } />,
+									},
+									context: 'Stats: Info box label when the Emails module is empty',
+								}
+							) }
+							cards={ <StatsEmptyActionEmail from="module_emails" /> }
+						/>
+					}
+				/>
+			) : (
+				<StatsModule
+					additionalColumns={ {
+						header: (
+							<>
+								<span>{ translate( 'Opens' ) }</span>
+							</>
+						),
+						body: ( item: { opens: number } ) => (
+							<>
+								<span>{ item.opens }</span>
+							</>
+						),
+					} }
+					path="emails"
+					moduleStrings={ moduleStrings }
+					period={ period }
+					query={ query }
+					statType="statsEmailsSummary"
+					mainItemLabel={ translate( 'Latest Emails' ) }
+					metricLabel={ translate( 'Clicks' ) }
+					showSummaryLink
+					className={ className }
+					hasNoBackground
+				/>
+			) }
+		</>
+	);
+};
+
+// TODO: remove this and use exclusively class above (StatEmail) once stats/empty-module-traffic feature flag is removed.
+
+type StatEmailsOldProps = {
+	period: string;
+	query: string;
+	className?: string;
+};
+
+const StatEmailsOld = ( { period, query, className }: StatEmailsOldProps ) => {
+	const translate = useTranslate();
+	const moduleStrings = statsStrings();
+
+	return (
+		<StatsModule
+			additionalColumns={ {
+				header: (
+					<>
+						<span>{ translate( 'Opens' ) }</span>
+					</>
+				),
+				body: ( item: any ) => (
+					<>
+						<span>{ item.opens }</span>
+					</>
+				),
+			} }
+			path="emails"
+			moduleStrings={ moduleStrings.emails }
+			period={ period }
+			query={ query }
+			statType="statsEmailsSummary"
+			mainItemLabel={ translate( 'Latest Emails' ) }
+			metricLabel={ translate( 'Clicks' ) }
+			showSummaryLink
+			className={ className }
+			hasNoBackground
+		/>
+	);
+};
+
+// Feature flag control component. TODO: remove this along with JSX above once stats/empty-module-traffic feature flag is removed.
+const EmailStatsComponent: React.FC< any > = ( props ) => {
+	const isNewStateEnabled = config.isEnabled( 'stats/empty-module-traffic' );
+
+	if ( isNewStateEnabled ) {
+		return <StatEmails { ...props } />;
+	}
+	return <StatEmailsOld { ...props } />;
+};
+
+export default EmailStatsComponent;
