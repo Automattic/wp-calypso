63b5968c590c3f51276fd5a2ca1b4761c7473eeb
André Gardi <andre.gardi@automattic.com>
2024-07-11 11:24:06 -0300

    Add one more log.

--- client/a8c-for-agencies/sections/sites/needs-setup-sites/site-configurations-modal/use-random-site-name.tsx
+++ client/a8c-for-agencies/sections/sites/needs-setup-sites/site-configurations-modal/use-random-site-name.tsx
@@ -4,26 +4,31 @@ import wpcom from 'calypso/lib/wp';
 export const getRandomSiteBaseUrl = async ( title: string ) => {
 	const siteName = '';
 	try {
-		const { body: urlSuggestions } = await wpcom.req.get( {
+		// const { body: urlSuggestions } = await wpcom.req.get( {
+		const wpcomRequest = await wpcom.req.get( {
 			apiNamespace: 'rest/v1.1',
 			path: `/domains/suggestions?http_envelope=1&query=${ title }&quantity=1&include_wordpressdotcom=true&include_dotblogsubdomain=false&vendor=dot`,
 		} );
 		// eslint-disable-next-line no-console
-		console.log( 'debuglog urlSuggestions line 12', urlSuggestions );
+		console.log( 'debuglog wpcomRequest line 12', wpcomRequest );
+
+		const urlSuggestions = wpcomRequest.body;
+		// eslint-disable-next-line no-console
+		console.log( 'debuglog urlSuggestions line 17', urlSuggestions );
 		const validUrlWpComUrl = urlSuggestions.find( ( suggestion: { domain_name: string } ) =>
 			suggestion.domain_name.includes( 'wordpress.com' )
 		);
 		// eslint-disable-next-line no-console
-		console.log( 'debuglog validUrlWpComUrl line 17', validUrlWpComUrl );
+		console.log( 'debuglog validUrlWpComUrl line 22', validUrlWpComUrl );
 		if ( validUrlWpComUrl ) {
 			return validUrlWpComUrl.domain_name.split( '.' )[ 0 ];
 		}
 	} catch ( error ) {
 		// eslint-disable-next-line no-console
-		console.log( 'debuglog error line 23', error );
+		console.log( 'debuglog error line 28', error );
 	}
 	// eslint-disable-next-line no-console
-	console.log( 'debuglog siteName line 26', siteName );
+	console.log( 'debuglog siteName line 31', siteName );
 	return siteName;
 };
 
