57ddf1d0d476a0aa0a46a70f258e19a345d3c014
Imran Hossain <imranh920@gmail.com>
2024-07-15 22:51:59 +0600

    Add hook to apply migration sticker on checkout page

---
+++ client/data/site-migration/use-checkout-migration-introductory-offer-sticker.ts
@@ -0,0 +1,51 @@
+import { useEffect, useMemo } from 'react';
+import { useMigrationStickerMutation } from './use-migration-sticker';
+
+const REF_PATHS_WITH_INTRODUCTORY_OFFER = [ 'move-lp' ];
+
+export const useCheckoutMigrationIntroductoryOfferSticker = ( siteId, onSuccessCallback ) => {
+	const urlQueryParams = new URLSearchParams( window.location.search );
+	const refPath = urlQueryParams?.get( 'ref' ) ?? '';
+	const checkoutBackPath = urlQueryParams?.get( 'checkoutBackUrl' ) ?? '';
+
+	const {
+		addMigrationSticker,
+		addMutationRest: {
+			isIdle: isMigrationStickerSettingIdle,
+			isPending: isMigrationStickerSettingPending,
+			isSuccess: isMigrationStickerSettingSuccess,
+		},
+	} = useMigrationStickerMutation();
+
+	const shouldSetMigrationSticker = useMemo( () => {
+		if ( ! siteId ) {
+			return false;
+		}
+
+		let tempRefPath = refPath;
+
+		if ( ! refPath && checkoutBackPath ) {
+			const backUrl = new URL( checkoutBackPath );
+			tempRefPath = backUrl.searchParams.get( 'ref' ) ?? '';
+		}
+
+		return REF_PATHS_WITH_INTRODUCTORY_OFFER.includes( tempRefPath );
+	}, [ refPath, checkoutBackPath, siteId ] );
+
+	useEffect( () => {
+		if ( shouldSetMigrationSticker ) {
+			addMigrationSticker( siteId! );
+		}
+	}, [ siteId, addMigrationSticker, shouldSetMigrationSticker ] );
+
+	useEffect( () => {
+		if ( shouldSetMigrationSticker && isMigrationStickerSettingSuccess && onSuccessCallback ) {
+			onSuccessCallback();
+		}
+	}, [ isMigrationStickerSettingSuccess, onSuccessCallback, shouldSetMigrationSticker ] );
+
+	return {
+		shouldSetMigrationSticker,
+		isLoading: isMigrationStickerSettingPending || isMigrationStickerSettingIdle,
+	};
+};
