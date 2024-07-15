fea1350b4eb006c841bb01aee33976e642f1503f
Imran Hossain <imranh920@gmail.com>
2024-07-15 23:56:02 +0600

    Add a 50ms delay to make sure

--- client/data/site-migration/use-checkout-migration-introductory-offer-sticker.ts
+++ client/data/site-migration/use-checkout-migration-introductory-offer-sticker.ts
@@ -43,7 +43,7 @@ export const useCheckoutMigrationIntroductoryOfferSticker = (
 
 	useEffect( () => {
 		if ( shouldSetMigrationSticker && isMigrationStickerSettingSuccess && onSuccessCallback ) {
-			setTimeout( onSuccessCallback, 0 );
+			setTimeout( onSuccessCallback, 50 );
 		}
 	}, [ isMigrationStickerSettingSuccess, onSuccessCallback, shouldSetMigrationSticker ] );
 
