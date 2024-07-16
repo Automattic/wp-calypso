41dfba96ad439817d9948abbd6a35ab593fbc27a
Renatho De Carli Rosa <renatho@gmail.com>
2024-07-16 14:28:49 -0300

    Add migration started header

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-started/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-started/index.tsx
@@ -1,16 +1,32 @@
 import { StepContainer } from '@automattic/onboarding';
-import React from 'react';
+import { useTranslate } from 'i18n-calypso';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import type { Step } from '../../types';
 
 const SiteMigrationStarted: Step = function () {
+	const translate = useTranslate();
+
 	const stepContent = <div>Migration started</div>;
 
 	return (
 		<StepContainer
 			stepName="site-migration-started"
 			isFullLayout
-			hideFormattedHeader
+			formattedHeader={
+				<FormattedHeader
+					headerText={ translate( 'Migration started' ) }
+					subHeaderText={
+						<>
+							{ translate( 'Your migration process has started.' ) }
+							<br />
+							{ translate( ' Migrate Guru will email you when the process is finished.' ) }
+						</>
+					}
+					align="center"
+					subHeaderAlign="center"
+				/>
+			}
 			hideSkip
 			hideBack
 			stepContent={ stepContent }
