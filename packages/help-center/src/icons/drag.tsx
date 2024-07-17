ebaa4e0aa82f45e48f52aa4679665d14ccfdde6d
Tony Arcangelini <tony@arcangelini.com>
2024-07-17 16:05:15 +0200

    Adjust size and color

--- packages/help-center/src/icons/drag.tsx
+++ packages/help-center/src/icons/drag.tsx
@@ -2,18 +2,18 @@ const DragIcon = () => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="10"
-			height="20"
-			viewBox="0 0 15 25"
+			width="8"
+			height="12"
+			viewBox="0 0 10 24"
 			className="help-center__drag-icon"
 		>
-			<g fill="var(--studio-gray-50)">
-				<circle cx="3" cy="3" r="3" />
-				<circle cx="3" cy="12" r="3" />
-				<circle cx="3" cy="21" r="3" />
-				<circle cx="12" cy="3" r="3" />
-				<circle cx="12" cy="12" r="3" />
-				<circle cx="12" cy="21" r="3" />
+			<g fill="var(--studio-gray-20)">
+				<circle cx="0" cy="3" r="3" />
+				<circle cx="0" cy="12" r="3" />
+				<circle cx="0" cy="21" r="3" />
+				<circle cx="10" cy="3" r="3" />
+				<circle cx="10" cy="12" r="3" />
+				<circle cx="10" cy="21" r="3" />
 			</g>
 		</svg>
 	);
