53cd7e891c4587934c2570e97d7eda4b71afc31b
Anthony Grullon <agrullon95@gmail.com>
2024-07-18 18:06:14 +0200

    Testing anchor links

--- packages/help-center/src/components/help-center-article-content.tsx
+++ packages/help-center/src/components/help-center-article-content.tsx
@@ -17,6 +17,54 @@ const ArticleContent = ( {
 	articleUrl,
 }: ArticleContentProps ) => {
 	const post = { title, link };
+
+	const embedResultHeaderHeight = document.querySelector( '.help-center-embed-result__header' )
+		?.clientHeight;
+	const embedResultContainer = document.querySelector( '.help-center-embed-result' );
+
+	if ( content ) {
+		// console.log( { content } );
+		// parse content and find the first anchor link with a hash
+		// cast firstAnchor to HTMLAnchorElement
+		// if it exists, find the id with that hash and add scrollMarginTop: 69px to the first anchor link
+		const parser = new DOMParser();
+		const doc = parser.parseFromString( content, 'text/html' );
+		const firstAnchor = doc.querySelector< HTMLElement >( 'a[href^="#"]' );
+		if ( firstAnchor ) {
+			const anchorId = firstAnchor.getAttribute( 'href' ) || '';
+			// console.log( { anchorId } );
+			const element = doc.querySelector< HTMLElement >( anchorId );
+			console.log({element});
+			if ( element ) {
+				element.style.scrollMarginTop = `${ embedResultHeaderHeight }px`;
+			}
+		}
+		content = doc.body.innerHTML;
+	}
+
+	const handleOnClick = ( event: React.MouseEvent ) => {
+		console.log( { event } );
+		// event.preventDefault();
+		// If event target hash includes an anchor, let's scroll this into view!
+		if ( event.target instanceof HTMLAnchorElement ) {
+			const anchorId = event.target.hash.slice( 1 );
+			const element = document.getElementById( anchorId );
+			console.log( { anchorId, element } );
+			if ( element && embedResultHeaderHeight ) {
+				const elementPosition = element.getBoundingClientRect().top;
+				const offsetPosition = elementPosition + window.scrollY - embedResultHeaderHeight;
+
+				console.log( { elementPosition, offsetPosition } );
+				event.preventDefault();
+				setTimeout( () => {
+					element.scrollIntoView();
+				}, 0 );
+				// window.scrollTo( 0, elementPosition );
+				// window.scrollTo( { top: offsetPosition } );
+			}
+		}
+	};
+
 	return (
 		<article className="help-center-article-content__story">
 			{ isLoading || ! post ? (
@@ -29,6 +77,7 @@ const ArticleContent = ( {
 							className="help-center-article-content__story-content"
 							// eslint-disable-next-line react/no-danger
 							dangerouslySetInnerHTML={ { __html: content } }
+							onClick={ handleOnClick }
 						/>
 						<HelpCenterFeedbackForm
 							postId={ postId }
