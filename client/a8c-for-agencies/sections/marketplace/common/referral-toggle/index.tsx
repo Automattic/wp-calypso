6b24b88915bdb1063d82224aba3034eb99bde69f
Yashwin Poojary <yashwinpoojary@gmail.com>
2024-07-22 09:09:46 +0530

    A4A > Referrals: Disable referral toggle when no Tipalti account (#92546)
    * A4A: remove referrals mode when no tipalty account is set
    
    * update hook to use default marketplace type when no payable account
    
    * Update hook to support stale data and use isPayable
    
    * Disable buttons instead of hiding
    
    * Fix button hover color
    
    ---------
    
    Co-authored-by: Andrii <lysenkoa.work@gmail.com>

--- client/a8c-for-agencies/sections/marketplace/common/referral-toggle/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/common/referral-toggle/index.tsx
@@ -1,9 +1,11 @@
 import { isEnabled } from '@automattic/calypso-config';
-import { Gridicon } from '@automattic/components';
+import { Button, Gridicon } from '@automattic/components';
 import { ToggleControl } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState, useRef } from 'react';
+import A4APopover from 'calypso/a8c-for-agencies/components/a4a-popover';
 import useReferralsGuide from 'calypso/a8c-for-agencies/components/guide-modal/guides/useReferralsGuide';
+import useGetTipaltiPayee from 'calypso/a8c-for-agencies/sections/referrals/hooks/use-get-tipalti-payee';
 import { useDispatch, useSelector } from 'calypso/state';
 import { savePreference } from 'calypso/state/preferences/actions';
 import { getPreference } from 'calypso/state/preferences/selectors';
@@ -15,6 +17,13 @@ const PREFERENCE_NAME = 'a4a-marketplace-referral-guide-seen';
 
 const ReferralToggle = () => {
 	const isAutomatedReferrals = isEnabled( 'a4a-automated-referrals' );
+	const { data: tipaltiData } = useGetTipaltiPayee( true );
+	const isPayable = tipaltiData?.IsPayable;
+
+	const [ showPopover, setShowPopover ] = useState( false );
+
+	const wrapperRef = useRef< HTMLSpanElement | null >( null );
+
 	const translate = useTranslate();
 	const dispatch = useDispatch();
 	const { marketplaceType, toggleMarketplaceType } = useContext( MarketplaceTypeContext );
@@ -35,12 +44,38 @@ const ReferralToggle = () => {
 	return (
 		<div className="a4a-marketplace__toggle-marketplace-type">
 			{ guideModal }
-			<ToggleControl
-				onChange={ toggleMarketplaceType }
-				checked={ marketplaceType === 'referral' }
-				id="a4a-marketplace__toggle-marketplace-type"
-				label={ translate( 'Refer products' ) }
-			/>
+			<span
+				onMouseEnter={ () => ! isPayable && setShowPopover( true ) }
+				role="button"
+				tabIndex={ 0 }
+				ref={ wrapperRef }
+			>
+				<ToggleControl
+					disabled={ ! isPayable }
+					onChange={ toggleMarketplaceType }
+					checked={ marketplaceType === 'referral' }
+					id="a4a-marketplace__toggle-marketplace-type"
+					label={ translate( 'Refer products' ) }
+				/>
+				{ showPopover && (
+					<A4APopover
+						className="referral-toggle__notice"
+						title={ translate( 'Your payment settings require action' ) }
+						offset={ 12 }
+						wrapperRef={ wrapperRef }
+						onFocusOutside={ () => setShowPopover( false ) }
+					>
+						<div className="referral-toggle__notice-description">
+							{ translate(
+								'Please confirm your details before referring products to your clients.'
+							) }
+						</div>
+						<Button href="/referrals/payment-settings">
+							{ translate( 'Go to payment settings' ) }
+						</Button>
+					</A4APopover>
+				) }
+			</span>
 			<Gridicon icon="info-outline" size={ 16 } onClick={ openGuide } />
 		</div>
 	);
