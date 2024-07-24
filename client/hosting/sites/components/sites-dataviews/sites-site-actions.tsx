bfcfcaa835a3dfe9fcb82ee32bf2278b41e33a6f
Addison Stavlo <Stavz01@gmail.com>
2024-07-24 15:50:37 -0400

    Sites dashboard - remove click from area around site actions ellipsis. (#92940)
    * undo changes from 92881
    
    * resize and position button, handle pointer events

--- client/hosting/sites/components/sites-dataviews/sites-site-actions.tsx
+++ client/hosting/sites/components/sites-dataviews/sites-site-actions.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled';
 import { useInView } from 'react-intersection-observer';
 import { SitesEllipsisMenu } from 'calypso/sites-dashboard/components/sites-ellipsis-menu';
 import type { SiteExcerptData } from '@automattic/sites';
@@ -7,18 +6,11 @@ interface SiteActionsProps {
 	site: SiteExcerptData;
 }
 
-const SiteActionsDiv = styled.div`
-	flex: 1;
-	justify-content: 'end';
-`;
-
 export const SiteActions = ( { site }: SiteActionsProps ) => {
 	const { ref, inView } = useInView( { triggerOnce: true } );
 	if ( site.is_deleted ) {
 		return false;
 	}
 
-	return (
-		<SiteActionsDiv ref={ ref }>{ inView && <SitesEllipsisMenu site={ site } /> }</SiteActionsDiv>
-	);
+	return <div ref={ ref }>{ inView && <SitesEllipsisMenu site={ site } /> }</div>;
 };
