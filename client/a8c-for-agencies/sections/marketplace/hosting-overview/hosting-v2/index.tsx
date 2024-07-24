38a8b05a49948c7e25eea91961a8337ec6e3e6d0
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-07-25 00:28:35 +0800

    A4A: Implement migration offer banner v2 based on the new design. (#92929)
    * Implement migration offer banner v2 based on the new design.
    
    * Fix incorrect spacing when not expanded.

--- client/a8c-for-agencies/sections/marketplace/hosting-overview/hosting-v2/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/hosting-overview/hosting-v2/index.tsx
@@ -1,4 +1,5 @@
 import { useTranslate } from 'i18n-calypso';
+import MigrationOffer from 'calypso/a8c-for-agencies/components/a4a-migration-offer-v2';
 
 export default function HostingV2() {
 	const translate = useTranslate();
@@ -7,6 +8,7 @@ export default function HostingV2() {
 		<>
 			<span>{ translate( "Choose the hosting tailored for your client's needs." ) }</span>
 			Append the URL with `?flags=-a4a-hosting-page-redesign` to see the old design.
+			<MigrationOffer />
 		</>
 	);
 }
