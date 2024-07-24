4f9f1c865d08771305cb15a573370d99cc9cbb7b
Jess Boctor <jessboctor@automattic.com>
2024-07-24 10:29:34 -0500

    Begin replacing the Source API version of iDeal payments

--- client/my-sites/checkout/src/lib/translate-payment-method-names.ts
+++ client/my-sites/checkout/src/lib/translate-payment-method-names.ts
@@ -32,6 +32,7 @@ export function translateWpcomPaymentMethodToCheckoutPaymentMethod(
 		case 'WPCOM_Billing_Stripe_Source_Eps':
 			return 'eps';
 		case 'WPCOM_Billing_Stripe_Source_Ideal':
+		case 'WPCOM_Billing_Stripe_IDeal':
 			return 'ideal';
 		case 'WPCOM_Billing_Stripe_Source_P24':
 		case 'WPCOM_Billing_Stripe_P24':
@@ -84,7 +85,7 @@ export function translateCheckoutPaymentMethodToWpcomPaymentMethod(
 		case 'eps':
 			return 'WPCOM_Billing_Stripe_Source_Eps';
 		case 'ideal':
-			return 'WPCOM_Billing_Stripe_Source_Ideal';
+			return 'WPCOM_Billing_Stripe_IDeal';
 		case 'p24':
 			if ( isP24RedirectEnabled ) {
 				return 'WPCOM_Billing_Stripe_P24';
@@ -122,6 +123,7 @@ export function readWPCOMPaymentMethodClass( slug: string ): WPCOMPaymentMethod 
 		case 'WPCOM_Billing_Stripe_Source_Bancontact':
 		case 'WPCOM_Billing_Stripe_Source_Eps':
 		case 'WPCOM_Billing_Stripe_Source_Ideal':
+		case 'WPCOM_Billing_Stripe_IDeal':
 		case 'WPCOM_Billing_Stripe_Source_P24':
 		case 'WPCOM_Billing_Stripe_Source_Sofort':
 		case 'WPCOM_Billing_Stripe_Source_Three_D_Secure':
