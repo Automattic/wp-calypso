01cbe1b51c92984ee2ad452de566eac8ff0fba15
niranjan-uma-shankar <niranjan.u@gmail.com>
2024-07-25 15:32:29 +0530

    Remove query param

--- packages/data-stores/src/plans/queries/use-plans.ts
+++ packages/data-stores/src/plans/queries/use-plans.ts
@@ -26,7 +26,6 @@ function usePlans( {
 	const params = new URLSearchParams();
 	coupon && params.append( 'coupon_code', coupon );
 	params.append( 'locale', locale );
-	params.append( 'eligible_request_for_experiment', 'true' );
 
 	return useQuery( {
 		queryKey: queryKeys.plans( coupon ),
