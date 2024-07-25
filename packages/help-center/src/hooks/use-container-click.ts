d4b2441b8ebd3421b36ae7c4f75a6d272b90213b
Tony Arcangelini <tony@arcangelini.com>
2024-07-25 19:13:08 +0200

    Fix a few bugs - everything is working as expected now

--- packages/help-center/src/hooks/use-container-click.ts
+++ packages/help-center/src/hooks/use-container-click.ts
@@ -1,57 +1,69 @@
-import { useEffect, useMemo } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect } from '@wordpress/element';
+import { useNavigate, useSearchParams, useLocation } from 'react-router-dom';
+
+const isThisASupportArticleLink = ( href: string ) =>
+	/wordpress\.com(\/\w\w)?(?=\/support\/)|support\.wordpress\.com/.test( href );
 
 export const useContainerClick = ( node: HTMLDivElement | null ) => {
 	const navigate = useNavigate();
+	const { pathname } = useLocation();
 	const [ searchParams ] = useSearchParams();
 	const link = searchParams.get( 'link' ) || '';
 
-	const filters = useMemo(
-		() => [
-			/**
-			 * Make support article links open within Help Center.
-			 */
-			{
-				pattern: '.toc-parent-list a',
-				action: ( element ) => {
-					const href = element.getAttribute( 'href' );
-					if ( href.startsWith( '#' ) ) {
-						element.onclick = ( event ) => {
-							event.preventDefault();
-							const target = node?.querySelector( href );
-							if ( target ) {
-								target.scrollIntoView();
-							}
-						};
-					}
-				},
-			},
-			{
-				pattern: 'a[href^="/"]',
-				action: ( element ) => {
-					const href = element.getAttribute( 'href' );
-					const fixedUrl = new URL( href, link ).href;
-
-					element.href = fixedUrl;
-					element.onclick = ( event ) => {
+	const filters = [
+		/**
+		 * Fix table of content jump-to links.
+		 */
+		{
+			pattern: '.toc-parent-list a',
+			action: ( element: HTMLAnchorElement ) => {
+				const href = element.getAttribute( 'href' );
+				if ( href?.startsWith( '#' ) ) {
+					element.onclick = ( event: Event ) => {
 						event.preventDefault();
-						navigate( `/post?link=${ fixedUrl }` );
+						const target = node?.querySelector( href );
+						if ( target ) {
+							target.scrollIntoView();
+						}
 					};
-				},
+				}
 			},
-		],
-		[ node, link ]
-	);
+		},
+
+		/**
+		 * Make support article links open within Help Center.
+		 */
+		{
+			pattern: 'a[href*="wordpress.com"], a[href^="/"]',
+			action: ( element: HTMLAnchorElement ) => {
+				if ( ! element.href.startsWith( '/' ) && ! isThisASupportArticleLink( element.href ) ) {
+					return;
+				}
+
+				if ( element.href.startsWith( '/' ) ) {
+					element.href = new URL( element.href, link ).href;
+				}
+
+				element.onclick = ( event: Event ) => {
+					event.preventDefault();
+					navigate( `/post?link=${ element.href }` );
+				};
+			},
+		},
+	];
 
 	useEffect( () => {
-		if ( node ) {
-			const timeout = setTimeout( () => {
-				filters.forEach( ( { pattern, action } ) => {
-					node.querySelectorAll( pattern ).forEach( action );
-				} );
-			}, 0 );
-
-			return () => clearTimeout( timeout );
+		// Currently only apply this to article pages.
+		// Later we can apply to Wapuu.
+		if ( pathname === '/post/' && node ) {
+			filters.forEach( ( { pattern, action } ) => {
+				( node.querySelectorAll( pattern ) as NodeListOf< HTMLAnchorElement > ).forEach(
+					( element ) => {
+						action( element );
+					}
+				);
+			} );
 		}
-	}, [ node, filters ] );
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ node, pathname ] );
 };
