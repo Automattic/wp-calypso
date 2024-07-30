bb6a71ba0d2703a2e0cc3c1e6144ca1fb4a9e1be
Damien Alleyne <31164683+d-alleyne@users.noreply.github.com>
2024-07-30 09:47:12 -0400

    Fix issues with the TranslatorInvite component
    - Icon color changed to match design
    - Margin between description and link updated.
    - Font weight, and link color, and link color underline offset match
    - Added padding to mobile view

--- client/my-sites/customer-home/cards/features/app-promo/index.tsx
+++ client/my-sites/customer-home/cards/features/app-promo/index.tsx
@@ -4,16 +4,12 @@ import AppPromo from 'calypso/blocks/app-promo';
 const AppPromoHome = () => {
 	const translate = useTranslate();
 	return (
-		<>
-			<AppPromo
-				title={ translate( 'Get our mobile app' ) }
-				className="customer-home__card"
-				subheader={ translate(
-					'Everything you need to build and grow your site from any device.'
-				) }
-				campaign="calypso-customer-home"
-			/>
-		</>
+		<AppPromo
+			title={ translate( 'Get our mobile app' ) }
+			className="customer-home__card"
+			subheader={ translate( 'Everything you need to build and grow your site from any device.' ) }
+			campaign="calypso-customer-home"
+		/>
 	);
 };
 export default AppPromoHome;
