50ab3ccab75900317140a8c97786c076e55ca98f
Christos <chriskmnds@gmail.com>
2024-07-30 12:33:15 +0300

    Stepper: Update `newsletter` flow to use framework tracking for step-submit event

--- client/landing/stepper/declarative-flow/internals/hooks/use-step-navigation-with-tracking/index.ts
+++ client/landing/stepper/declarative-flow/internals/hooks/use-step-navigation-with-tracking/index.ts
@@ -1,3 +1,6 @@
+import { OnboardSelect } from '@automattic/data-stores';
+import { useSelect } from '@wordpress/data';
+import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordSubmitStep } from '../../analytics/record-submit-step';
 import type { Flow, Navigate, ProvidedDependencies, StepperStep } from '../../types';
 
@@ -19,6 +22,8 @@ export const useStepNavigationWithTracking = ( {
 		navigate,
 		steps.map( ( step ) => step.slug )
 	);
+	const intent =
+		useSelect( ( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getIntent(), [] ) ?? '';
 
 	const shouldTrackSubmit = flow.use__Temporary__ShouldTrackEvent?.( 'submit' ) ?? false;
 
@@ -26,7 +31,7 @@ export const useStepNavigationWithTracking = ( {
 		...stepNavigation,
 		submit: ( providedDependencies: ProvidedDependencies = {}, ...params: string[] ) => {
 			shouldTrackSubmit &&
-				recordSubmitStep( providedDependencies, '', flow.name, currentStepRoute );
+				recordSubmitStep( providedDependencies, intent, flow.name, currentStepRoute );
 			stepNavigation.submit?.( providedDependencies, ...params );
 		},
 	};
