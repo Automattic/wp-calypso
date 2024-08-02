24336f0585f03116545da4c790e9d1f80415f858
Anthony Grullon <agrullon95@gmail.com>
2024-08-02 12:17:44 -0400

    Open "Custom code" and "Free SSL Certificate" articles in Help Center

--- client/me/developer/features/index.tsx
+++ client/me/developer/features/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@automattic/components';
+import { useOpenArticleInHelpCenter } from '@automattic/help-center/src/hooks';
 import { useTranslate } from 'i18n-calypso';
 import { GitHubDeploymentCard } from './github-deployment-card';
 import { StudioCard } from './studio-card';
@@ -10,7 +11,26 @@ import './style.scss';
 export const DeveloperFeatures = () => {
 	const translate = useTranslate();
 	const features = useFeaturesList();
-	const handleClickLink = useHandleClickLink();
+	const handleClickLinkTracks = useHandleClickLink();
+	const { openArticleInHelpCenter } = useOpenArticleInHelpCenter();
+
+	const handleFeatureClickLink = ( event: React.MouseEvent< HTMLAnchorElement > ) => {
+		handleClickLinkTracks( event );
+		const featureArticleLink = event?.currentTarget?.href;
+		const featureItemId = event?.currentTarget?.id;
+		// Opens the following feature articles in Help Center:
+		// "Custom code" and "Free SSL certificate"
+		const featureArticlesToOpenInHelpCenter = [ 'code', 'https-ssl' ];
+
+		if (
+			openArticleInHelpCenter &&
+			featureArticleLink &&
+			featureArticlesToOpenInHelpCenter.includes( featureItemId )
+		) {
+			event.preventDefault();
+			openArticleInHelpCenter( featureArticleLink );
+		}
+	};
 
 	return (
 		<>
@@ -32,7 +52,7 @@ export const DeveloperFeatures = () => {
 									href={ linkLearnMore }
 									target="_blank"
 									rel="noopener noreferrer"
-									onClick={ handleClickLink }
+									onClick={ handleFeatureClickLink }
 								>
 									{ translate( 'Learn more' ) }
 								</a>
