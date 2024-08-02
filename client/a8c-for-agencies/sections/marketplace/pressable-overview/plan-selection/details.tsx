3b609df76265d3cd24f2df7f49b05eacb02b2e49
Yashwin Poojary <yashwinpoojary@gmail.com>
2024-08-02 14:06:47 +0530

    A4A > Marketplace: Add hosting page track events (#93203)

--- client/a8c-for-agencies/sections/marketplace/pressable-overview/plan-selection/details.tsx
+++ client/a8c-for-agencies/sections/marketplace/pressable-overview/plan-selection/details.tsx
@@ -41,6 +41,12 @@ export default function PlanSelectionDetails( { selectedPlan, onSelectPlan, isLo
 		dispatch( recordTracksEvent( 'calypso_a4a_marketplace_hosting_pressable_contact_us_click' ) );
 	}, [ dispatch ] );
 
+	const onScheduleDemo = useCallback( () => {
+		dispatch(
+			recordTracksEvent( 'calypso_a4a_marketplace_hosting_pressable_schedule_demo_click' )
+		);
+	}, [ dispatch ] );
+
 	const PRESSABLE_CONTACT_LINK = 'https://pressable.com/request-demo';
 
 	if ( isLoading ) {
@@ -167,7 +173,7 @@ export default function PlanSelectionDetails( { selectedPlan, onSelectPlan, isLo
 					/>
 					<Button
 						className="pressable-overview-plan-selection__details-card-cta-button"
-						onClick={ onContactUs }
+						onClick={ onScheduleDemo }
 						href={ PRESSABLE_CONTACT_LINK }
 						target="_blank"
 					>
