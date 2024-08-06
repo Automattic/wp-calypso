c428fed8a0527b5803073e26d392fb2a79e5a40b
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2024-08-06 17:15:43 +0300

    Scroll the page to the RMT section when clicking back in details and update copy (#92794)
    * Scroll the page to the RMT section when clicking back in details.
    
    Change the copy for some parts of RMT section.
    
    * Use interpolate for the /patterns link
    
    * Remove copy.
    
    * Fix scrolling on back button.
    
    * Remove unneeded change
    
    * Add additional check and remove dep for useEffect.
    
    * Rename const
    
    * Re-add the check removed by mistake
    
    * Fix linting
    
    * Fix the ref and the typing issues.

--- client/my-sites/patterns/components/pattern-library/index.tsx
+++ client/my-sites/patterns/components/pattern-library/index.tsx
@@ -73,6 +73,15 @@ function scrollToPatternView( stickyFiltersElement: HTMLDivElement, onlyIfBelowT
 	} );
 }
 
+function scrollToSection( element: HTMLDivElement, scrollBehavior: ScrollBehavior = 'smooth' ) {
+	requestAnimationFrame( () => {
+		element.scrollIntoView( {
+			behavior: scrollBehavior,
+			block: 'center',
+		} );
+	} );
+}
+
 type PatternLibraryProps = {
 	categoryGallery: CategoryGalleryFC;
 	patternGallery: PatternGalleryFC;
@@ -88,9 +97,10 @@ export const PatternLibrary = ( {
 	const translate = useTranslate();
 	const hasTranslation = useHasEnTranslation();
 	const navRef = useRef< HTMLDivElement >( null );
+	const readymadeTemplateSectionRef = useRef< HTMLDivElement >( null );
 
 	const { recordPatternsEvent } = useRecordPatternsEvent();
-	const { category, searchTerm, isGridView, patternTypeFilter, patternPermalinkId } =
+	const { category, searchTerm, section, isGridView, patternTypeFilter, patternPermalinkId } =
 		usePatternsContext();
 
 	const { data: categories = [] } = usePatternCategories( locale );
@@ -167,6 +177,16 @@ export const PatternLibrary = ( {
 			window.removeEventListener( 'scroll', handleScroll );
 		};
 	}, [] );
+
+	useEffect( () => {
+		if (
+			'readymade-templates-section' === section &&
+			readymadeTemplateSectionRef.current &&
+			! searchTerm
+		) {
+			scrollToSection( readymadeTemplateSectionRef.current, 'instant' );
+		}
+	}, [ searchTerm, section ] );
 
 	// `calypso-router` has trouble with the onboarding URL we use. This code prevents click
 	// events on onboarding links from propagating to the `calypso-router` event listener,
@@ -380,7 +400,10 @@ export const PatternLibrary = ( {
 							patternTypeFilter={ PatternTypeFilter.PAGES }
 						/>
 						{ isEnabled( 'readymade-templates/showcase' ) && (
-							<ReadymadeTemplates readymadeTemplates={ readymadeTemplates } />
+							<ReadymadeTemplates
+								readymadeTemplates={ readymadeTemplates }
+								forwardRef={ readymadeTemplateSectionRef }
+							/>
 						) }
 					</>
 				) }
--- client/my-sites/patterns/components/readymade-template-details/index.tsx
+++ client/my-sites/patterns/components/readymade-template-details/index.tsx
@@ -1,13 +1,18 @@
 import { addLocaleToPathLocaleInFront } from '@automattic/i18n-utils';
 import { Button } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
 import { Icon, arrowLeft, check, copy } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
-import { useEffect, useState } from 'react';
+import { createElement, useEffect, useState } from 'react';
 import { PatternsGetStarted } from 'calypso/my-sites/patterns/components/get-started';
 import { useReadymadeTemplates } from 'calypso/my-sites/patterns/hooks/use-readymade-templates';
 import { ReadymadeTemplateDetailsFC } from 'calypso/my-sites/patterns/types';
 
 import './style.scss';
+
+const PatternLibraryLink = ( { children }: { children: React.ReactNode } ) => (
+	<a href={ addLocaleToPathLocaleInFront( '/patterns' ) }>{ children }</a>
+);
 
 export const ReadymadeTemplateDetails: ReadymadeTemplateDetailsFC = ( { id, renderPreview } ) => {
 	const translate = useTranslate();
@@ -39,7 +44,7 @@ export const ReadymadeTemplateDetails: ReadymadeTemplateDetailsFC = ( { id, rend
 			<section className="readymade-template-details-wrapper">
 				<div className="readymade-template-details-section">
 					<a
-						href={ addLocaleToPathLocaleInFront( '/patterns' ) }
+						href={ addLocaleToPathLocaleInFront( '/patterns' ) + '#readymade-templates-section' }
 						className="readymade-template-details-back"
 					>
 						<Icon icon={ arrowLeft } size={ 24 } /> { translate( 'Back' ) }
@@ -72,6 +77,33 @@ export const ReadymadeTemplateDetails: ReadymadeTemplateDetailsFC = ( { id, rend
 								// eslint-disable-next-line react/no-danger
 								dangerouslySetInnerHTML={ { __html: readymadeTemplate.description } }
 							/>
+
+							<div className="readymade-template-details-info">
+								<div className="readymade-template-details-subheading">
+									{ translate( 'Customize it to your heart’s content' ) }
+								</div>
+
+								<p>
+									{ translate(
+										'Begin with this layout and transform it using our powerful site editing tools.'
+									) }
+								</p>
+								<p>
+									{ createInterpolateElement(
+										translate(
+											'Modify the layout, colors, typography, and content to fit your unique style and needs. Plus, use any pattern from our <Link>pattern library</Link> to enhance and tailor your site ensuring it truly stands out.'
+										),
+										{
+											Link: createElement( PatternLibraryLink ),
+										}
+									) }
+								</p>
+								<p>
+									<a href="/support/site-editor">
+										{ translate( 'Learn more about how the site editor works.' ) }
+									</a>
+								</p>
+							</div>
 						</div>
 						<div className="readymade-template-details-preview">
 							{ renderPreview?.( readymadeTemplate ) }
