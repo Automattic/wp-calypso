2e266407c4fbe3e7f3a2cbd6611dcf4903f5d2fb
Gabriel Caires <gabriel.caires@automattic.com>
2024-08-06 11:06:35 +0100

    Add new migration-flow

---
+++ client/landing/stepper/declarative-flow/site-migration-v2/index.tsx
@@ -0,0 +1,21 @@
+import { translate } from 'i18n-calypso';
+import { type Flow } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import { stepsWithRequiredLogin } from '../../utils/steps-with-required-login';
+import { STEPS } from '../internals/steps';
+
+export default {
+	name: 'site-migration-v2',
+	get title() {
+		return translate( 'Site Migration' );
+	},
+	isSignupFlow: false,
+	useSteps() {
+		return stepsWithRequiredLogin( [ STEPS.PLATFORM_SELECTION ] );
+	},
+
+	useStepNavigation( currentStep, navigate ) {
+		return {
+			submit: () => navigate( STEPS.SITE_LAUNCH.slug ),
+		};
+	},
+} satisfies Flow;
