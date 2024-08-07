852dcd8b1fa8d66ac3de073678afe5ecc2678040
Richard Ortiz <rcrd.ortiz@gmail.com>
2024-08-07 16:08:27 +0200

    Added the continue button and refactored the logic for setting and persisting the generated content

--- client/landing/stepper/declarative-flow/internals/steps-repository/readymade-template-generate-content/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/readymade-template-generate-content/index.tsx
@@ -3,6 +3,7 @@ import { OnboardSelect, updateLaunchpadSettings } from '@automattic/data-stores'
 import { StepContainer } from '@automattic/onboarding';
 import { Button } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
+import clsx from 'clsx';
 import { translate } from 'i18n-calypso';
 import React, { useState } from 'react';
 import useUrlQueryParam from 'calypso/a8c-for-agencies/hooks/use-url-query-param';
@@ -20,20 +21,30 @@ import TextProgressBar from './components/text-progress-bar';
 import './style.scss';
 
 type ReadymadeTemplateGenerateContentProps = {
-	readymadeTemplate: ReadymadeTemplate;
+	selectedReadymadeTemplate: ReadymadeTemplate;
 	siteId: number;
 	siteSlug: string;
+	next?: () => void;
 };
 
 const ReadymadeTemplateGenerateContent: React.FC< ReadymadeTemplateGenerateContentProps > = ( {
-	readymadeTemplate,
+	selectedReadymadeTemplate,
 	siteId,
 	siteSlug,
+	next = () => {},
 } ) => {
 	const [ aiContext, setAiContext ] = useState( '' );
 	const [ numberOfGenerations, setNumberOfGenerations ] = useState( 0 );
 	const [ isGeneratingContent, setIsGeneratingContent ] = useState( false );
+	const [ isSaving, setIsSaving ] = useState( false );
+	const [ aiGeneratedRT, setAiGeneratedRT ] = useState( selectedReadymadeTemplate );
 	const { assembleSite } = useDispatch( SITE_STORE );
+	const { setSelectedReadymadeTemplate } = useDispatch( ONBOARD_STORE );
+
+	const markContentGenerationTaskComplete = () =>
+		updateLaunchpadSettings( siteSlug, {
+			checklist_statuses: { generate_content: true },
+		} );
 
 	const isPromptEmpty = ! aiContext.trim();
 
@@ -41,28 +52,42 @@ const ReadymadeTemplateGenerateContent: React.FC< ReadymadeTemplateGenerateConte
 		setAiContext( event.target.value );
 	};
 
+	const updateSiteContents = ( rt: ReadymadeTemplate ) =>
+		assembleSite( siteId, 'pub/assembler', {
+			homeHtml: rt.home.content,
+			headerHtml: rt.home.header,
+			footerHtml: rt.home.footer,
+			pages: [],
+			globalStyles: rt.globalStyles,
+			canReplaceContent: true,
+			siteSetupOption: 'readymade-template',
+		} );
+
+	const restoreSiteContents = () => updateSiteContents( selectedReadymadeTemplate );
+
+	const applyContent = () => {
+		setIsSaving( true );
+		setSelectedReadymadeTemplate( aiGeneratedRT );
+		updateSiteContents( aiGeneratedRT ).then( markContentGenerationTaskComplete ).then( next );
+	};
+
 	const generateContent = () => {
 		setIsGeneratingContent( true );
-		setNumberOfGenerations( numberOfGenerations + 1 );
-		generateAIContentForTemplate( readymadeTemplate, aiContext )
-			.then( ( rt: ReadymadeTemplate ) =>
-				assembleSite( siteId, 'pub/assembler', {
-					homeHtml: rt.home.content,
-					headerHtml: rt.home.header,
-					footerHtml: rt.home.footer,
-					pages: [],
-					globalStyles: rt.globalStyles,
-					canReplaceContent: true,
-					siteSetupOption: 'readymade-template',
-				} )
-			)
-			.then( () =>
-				updateLaunchpadSettings( siteSlug, {
-					checklist_statuses: { generate_content: true },
-				} )
-			)
-			.then( () => setIsGeneratingContent( false ) );
+		generateAIContentForTemplate( selectedReadymadeTemplate, aiContext )
+			.then( ( aiGeneratedRt: ReadymadeTemplate ) => {
+				setAiGeneratedRT( aiGeneratedRt );
+				return updateSiteContents( aiGeneratedRt );
+			} )
+			.then( () => {
+				setIsGeneratingContent( false );
+				restoreSiteContents();
+				setNumberOfGenerations( numberOfGenerations + 1 );
+			} );
 	};
+
+	const primaryButtonClasses = clsx( 'checklist-item__checklist-primary-button', {
+		hidden: numberOfGenerations === 0,
+	} );
 
 	return (
 		<>
@@ -89,11 +114,20 @@ const ReadymadeTemplateGenerateContent: React.FC< ReadymadeTemplateGenerateConte
 					</FormFieldset>
 					<div className="generate-content__buttons">
 						<Button
-							className="checklist-item__checklist-primary-button"
+							className="checklist-item__checklist-secondary-button"
 							onClick={ generateContent }
 							disabled={ isPromptEmpty || numberOfGenerations >= 5 || isGeneratingContent }
 						>
-							{ translate( 'Generate content' ) }
+							{ isGeneratingContent
+								? translate( 'Generating content' )
+								: translate( 'Generate content' ) }
+						</Button>
+						<Button
+							className={ primaryButtonClasses }
+							onClick={ applyContent }
+							disabled={ isSaving || numberOfGenerations === 0 || isGeneratingContent }
+						>
+							{ isSaving ? translate( 'Saving, please wait' ) : translate( 'Continue' ) }
 						</Button>
 					</div>
 				</form>
@@ -119,9 +153,10 @@ const ReadymadeTemplateGenerateContentStep: Step = function ReadymadeTemplateGen
 	if ( readymadeTemplate && siteId && siteSlug ) {
 		content = (
 			<ReadymadeTemplateGenerateContent
-				readymadeTemplate={ readymadeTemplate }
+				selectedReadymadeTemplate={ readymadeTemplate }
 				siteId={ siteId }
 				siteSlug={ siteSlug }
+				next={ navigation.goBack }
 			/>
 		);
 	}
