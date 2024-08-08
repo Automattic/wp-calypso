815f402698a1e94dedf2418c99a90325b555f4d8
Enej Bajgoric <enej.bajgoric@automattic.com>
2024-08-08 05:14:34 -0700

    Update: subscription managemnt  (#93231)
    Co-authored-by: pkuliga <pkuliga@gmail.com>

--- client/landing/subscriptions/components/settings/site-settings/notify-me-of-new-posts-toggle.tsx
+++ client/landing/subscriptions/components/settings/site-settings/notify-me-of-new-posts-toggle.tsx
@@ -1,3 +1,4 @@
+import { JetpackLogo } from '@automattic/components';
 import { ToggleControl } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 
@@ -15,16 +16,34 @@ const NotifyMeOfNewPostsToggle = ( {
 	const translate = useTranslate();
 
 	return (
-		<div className="setting-item">
+		<div className="setting-item setting-item__last">
 			<ToggleControl
-				label={ translate( 'Notify me of new posts' ) }
+				label={ translate( 'Receive web and mobile notifications' ) }
 				onChange={ () => onChange( ! value ) }
 				checked={ value }
 				disabled={ isUpdating }
 			/>
-			<p className="setting-item__hint">
-				{ translate( 'Receive web and mobile notifications for new posts from this site.' ) }
-			</p>
+			{ value && (
+				<div className="setting-item__app-hint">
+					<JetpackLogo size={ 20 } />
+					<p>
+						{ translate(
+							'Take your subscriptions on the go with the {{a}}Jetpack mobile app{{/a}}.',
+							{
+								components: {
+									a: (
+										<a
+											href="https://wp.com/app/?campaign=calypso-subscription-link"
+											target="_blank"
+											rel="noopener noreferrer"
+										/>
+									),
+								},
+							}
+						) }
+					</p>
+				</div>
+			) }
 		</div>
 	);
 };
