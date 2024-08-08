ef9483e871329b0a4d37731f56e23d0dfe0295d0
Addison-Stavlo <stavz01@gmail.com>
2024-08-08 11:39:25 -0400

    hide pseudo element via props

--- client/my-sites/plugins/education-footer/index.tsx
+++ client/my-sites/plugins/education-footer/index.tsx
@@ -12,7 +12,8 @@ import PluginsResultsHeader from 'calypso/my-sites/plugins/plugins-results-heade
 import { useDispatch, useSelector } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions/record';
 import { isUserLoggedIn, getCurrentUserSiteCount } from 'calypso/state/current-user/selectors';
-import { getSectionName } from 'calypso/state/ui/selectors';
+import { getShouldShowGlobalSidebar } from 'calypso/state/global-sidebar/selectors';
+import { getSectionGroup, getSectionName, getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 const ThreeColumnContainer = styled.div`
 	@media ( max-width: 660px ) {
@@ -89,7 +90,12 @@ export const MarketplaceFooter = () => {
 	const { __ } = useI18n();
 	const isLoggedIn = useSelector( isUserLoggedIn );
 	const currentUserSiteCount = useSelector( getCurrentUserSiteCount );
-	const sectionName = useSelector( getSectionName );
+	const sectionName = useSelector( getSectionName ) || '';
+	const sectionGroup = useSelector( getSectionGroup ) || '';
+	const siteId = useSelector( getSelectedSiteId );
+	const isGlobalSidebarVisible = useSelector( ( state ) =>
+		getShouldShowGlobalSidebar( state, siteId, sectionGroup, sectionName )
+	);
 
 	const startUrl = addQueryArgs(
 		{
@@ -102,6 +108,7 @@ export const MarketplaceFooter = () => {
 		<MarketplaceContainer isloggedIn={ isLoggedIn }>
 			<Section
 				header={ preventWidows( __( 'You pick the plugin. We’ll take care of the rest.' ) ) }
+				hidePseudo={ isGlobalSidebarVisible }
 			>
 				{ ( ! isLoggedIn || currentUserSiteCount === 0 ) && (
 					<Button className="is-primary marketplace-cta" href={ startUrl }>
