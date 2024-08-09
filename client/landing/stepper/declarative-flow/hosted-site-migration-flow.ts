cb36219727165b8e6450153117594775602b5488
Renatho De Carli Rosa <renatho@gmail.com>
2024-08-09 11:57:34 -0300

    Replace hosted migration flow if feature flag is enabled

--- client/landing/stepper/declarative-flow/hosted-site-migration-flow.ts
+++ client/landing/stepper/declarative-flow/hosted-site-migration-flow.ts
@@ -1,9 +1,13 @@
+import { isEnabled } from '@automattic/calypso-config';
 import { HOSTED_SITE_MIGRATION_FLOW } from '@automattic/onboarding';
 import { type Flow } from './internals/types';
+import migration from './migration';
 import siteMigration from './site-migration-flow';
 
+const baseFlow = isEnabled( 'migration-flow/revamp' ) ? migration : siteMigration;
+
 const hostedSiteMigrationFlow: Flow = {
-	...siteMigration,
+	...baseFlow,
 	variantSlug: HOSTED_SITE_MIGRATION_FLOW,
 	isSignupFlow: true,
 };
