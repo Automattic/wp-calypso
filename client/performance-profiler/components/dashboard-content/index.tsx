f014ccf4fb44b95875263159bd6a6a5f39cb8c37
Wesley Berrêdo <wberredo@gmail.com>
2024-08-12 19:13:29 -0300

    Performance Profiler: Add Performance Score

--- client/performance-profiler/components/dashboard-content/index.tsx
+++ client/performance-profiler/components/dashboard-content/index.tsx
@@ -1,4 +1,5 @@
 import { TabType } from 'calypso/performance-profiler/components/header';
+import { PerformanceScore } from 'calypso/performance-profiler/components/performance-score';
 import './style.scss';
 
 type PerformanceProfilerDashboardContentProps = {
@@ -8,11 +9,14 @@ type PerformanceProfilerDashboardContentProps = {
 export const PerformanceProfilerDashboardContent = (
 	props: PerformanceProfilerDashboardContentProps
 ) => {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { activeTab } = props;
 
 	return (
 		<div className="performance-profiler-content">
-			<div className="l-block-wrapper">Dashboard content { activeTab }</div>
+			<div className="l-block-wrapper">
+				<PerformanceScore value={ 70 } />
+			</div>
 		</div>
 	);
 };
