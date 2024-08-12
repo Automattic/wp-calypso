f74018cb184af1935ed7cfa5d7976be4d170b5ac
Valter Lorran <valterlorran@hotmail.com>
2024-08-12 17:34:49 -0300

    Apply the updated design

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-credentials/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-credentials/index.tsx
@@ -7,6 +7,7 @@ import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
 import FormRadio from 'calypso/components/forms/form-radio';
 import FormTextInput from 'calypso/components/forms/form-text-input';
+import FormTextArea from 'calypso/components/forms/form-textarea';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import type { Step } from '../../types';
 
@@ -70,32 +71,46 @@ export const CredentialsForm: FC< CredentialsFormProps > = ( { onSubmit } ) => {
 									type="text"
 									id="site-address"
 									value={ siteAddress }
+									placeholder={ translate( 'Enter your WordPress site address.' ) }
 									onChange={ ( e: ChangeEvent< HTMLInputElement > ) =>
 										setSiteAddress( e.target.value )
 									}
 								/>
+								<div className="site-migration-credentials__form-error">
+									{ translate( 'Enter the URL of your source site.' ) }
+								</div>
 							</div>
-							<div className="site-migration-credentials__form-field">
-								<FormLabel htmlFor="username">{ translate( 'Username' ) }</FormLabel>
-								<FormTextInput
-									type="text"
-									id="username"
-									value={ username }
-									onChange={ ( e: ChangeEvent< HTMLInputElement > ) =>
-										setUsername( e.target.value )
-									}
-								/>
+
+							<div className="site-migration-credentials__form-fields-row">
+								<div className="site-migration-credentials__form-field">
+									<FormLabel htmlFor="username">
+										{ translate( 'WordPress admin username' ) }
+									</FormLabel>
+									<FormTextInput
+										type="text"
+										id="username"
+										value={ username }
+										placeholder={ translate( 'Username' ) }
+										onChange={ ( e: ChangeEvent< HTMLInputElement > ) =>
+											setUsername( e.target.value )
+										}
+									/>
+								</div>
+								<div className="site-migration-credentials__form-field">
+									<FormLabel htmlFor="password">{ translate( 'Password' ) }</FormLabel>
+									<FormTextInput
+										type="password"
+										id="password"
+										value={ password }
+										placeholder={ translate( 'Password' ) }
+										onChange={ ( e: ChangeEvent< HTMLInputElement > ) =>
+											setPassword( e.target.value )
+										}
+									/>
+								</div>
 							</div>
-							<div className="site-migration-credentials__form-field">
-								<FormLabel htmlFor="password">{ translate( 'Password' ) }</FormLabel>
-								<FormTextInput
-									type="password"
-									id="password"
-									value={ password }
-									onChange={ ( e: ChangeEvent< HTMLInputElement > ) =>
-										setPassword( e.target.value )
-									}
-								/>
+							<div className="site-migration-credentials__form-error">
+								{ translate( 'Enter your WordPress admin username and password.' ) }
 							</div>
 						</div>
 					</div>
@@ -115,18 +130,36 @@ export const CredentialsForm: FC< CredentialsFormProps > = ( { onSubmit } ) => {
 									placeholder={ translate( 'Enter your backup file location' ) }
 								/>
 							</div>
-							<div>
+							<div className="site-migration-credentials__form-note">
 								{ translate(
-									'Share a link to your backup file, hosted on a service like Dropbox or Google Drive, ensuring that anyone with the link has access to the file.'
+									"Upload your file to a service like Dropbox or Google Drive to get a link. Don't forget to make sure that anyone with the link can access it."
 								) }
 							</div>
 						</div>
 					</div>
 				) }
+
+				<div className="site-migration-credentials__form-field">
+					<FormLabel htmlFor="site-address">{ translate( 'Notes (optional)' ) }</FormLabel>
+					<FormTextArea
+						type="text"
+						id="site-address"
+						placeholder={ translate(
+							'Share any other details that will help us access your site for the migration.'
+						) }
+						value={ siteAddress }
+						onChange={ ( e: ChangeEvent< HTMLInputElement > ) => setSiteAddress( e.target.value ) }
+					/>
+				</div>
 				<div>
 					<NextButton type="submit">{ translate( 'Continue' ) }</NextButton>
 				</div>
 			</Card>
+			<div className="site-migration-credentials__skip">
+				<button className="button navigation-link step-container__navigation-link has-underline is-borderless">
+					{ translate( 'Skip, I need help providing access' ) }
+				</button>
+			</div>
 		</form>
 	);
 };
@@ -146,6 +179,7 @@ const SiteMigrationCredentials: Step = function ( { navigation } ) {
 				flowName="site-migration"
 				goBack={ navigation?.goBack }
 				goNext={ navigation?.submit }
+				hideSkip
 				isFullLayout
 				formattedHeader={
 					<FormattedHeader
