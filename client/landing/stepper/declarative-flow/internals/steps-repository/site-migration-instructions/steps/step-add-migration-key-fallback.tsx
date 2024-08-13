6fbd99df68c0e70243e8f740b3af258a7da27695
Donna Peplinskie <donnapep@gmail.com>
2024-08-13 13:20:16 -0400

    Add new CTAs for migration instruction steps

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/step-add-migration-key-fallback.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/step-add-migration-key-fallback.tsx
@@ -1,38 +1,56 @@
-import { ExternalLink } from '@automattic/components';
+import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { recordMigrationInstructionsLinkClick } from '../tracking';
 import { getMigrateGuruPageURL } from './utils';
 import type { FC } from 'react';
 
-export const StepAddMigrationKeyFallback: FC = () => {
+interface Props {
+	onDoneClick: () => void;
+}
+
+export const StepAddMigrationKeyFallback: FC< Props > = ( { onDoneClick } ) => {
 	const translate = useTranslate();
 	const site = useSite();
 	const siteUrl = site?.URL ?? '';
 
+	const onGetKeyClick = () => {
+		window.open( getMigrateGuruPageURL( siteUrl ), '_blank' );
+		recordMigrationInstructionsLinkClick( 'copy-key-fallback' );
+	};
+
 	return (
-		<p>
-			{ translate(
-				'Go to the {{a}}Migrate Guru page on the new WordPress.com site{{/a}} and copy the migration key. Then paste it on the {{strong}}%(migrationKeyLabel)s{{/strong}} field of your existing site and click {{strong}}%(migrateLabel)s{{/strong}}.',
-				{
-					components: {
-						a: (
-							<ExternalLink
-								href={ getMigrateGuruPageURL( siteUrl ) }
-								icon
-								iconSize={ 14 }
-								target="_blank"
-								onClick={ () => recordMigrationInstructionsLinkClick( 'copy-key-fallback' ) }
-							/>
-						),
-						strong: <strong />,
-					},
-					args: {
-						migrationKeyLabel: 'Migrate Guru Migration Key',
-						migrateLabel: 'Migrate',
-					},
-				}
-			) }
-		</p>
+		<>
+			<p>
+				{ translate(
+					'Go to the Migrate Guru page on the new WordPress.com site and copy the migration key. Then paste it on the {{strong}}%(migrationKeyLabel)s{{/strong}} field of your existing site and click {{strong}}%(migrateLabel)s{{/strong}}.',
+					{
+						components: {
+							strong: <strong />,
+						},
+						args: {
+							migrationKeyLabel: 'Migrate Guru Migration Key',
+							migrateLabel: 'Migrate',
+						},
+					}
+				) }
+			</p>
+			<div className="checklist-item__checklist-expanded-ctas">
+				<Button
+					className="checklist-item__checklist-expanded-cta"
+					variant="primary"
+					onClick={ onGetKeyClick }
+				>
+					{ translate( 'Get the key' ) }
+				</Button>
+				<Button
+					className="checklist-item__checklist-expanded-cta"
+					variant="secondary"
+					onClick={ onDoneClick }
+				>
+					{ translate( 'Done' ) }
+				</Button>
+			</div>
+		</>
 	);
 };
