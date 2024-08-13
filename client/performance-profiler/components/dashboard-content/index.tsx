23f3c5d750759a0a9da80abf848a2cd3e18dd78c
Gergely Csécsey <gcsecsey@gmail.com>
2024-08-13 15:29:24 +0100

    add back the core web vitals display

--- client/performance-profiler/components/dashboard-content/index.tsx
+++ client/performance-profiler/components/dashboard-content/index.tsx
@@ -1,6 +1,7 @@
 import { TabType } from 'calypso/performance-profiler/components/header';
 import { PerformanceScore } from 'calypso/performance-profiler/components/performance-score';
 import './style.scss';
+import { CoreWebVitalsDisplay } from '../core-web-vitals-display';
 
 type PerformanceProfilerDashboardContentProps = {
 	activeTab: TabType;
@@ -16,6 +17,7 @@ export const PerformanceProfilerDashboardContent = (
 		<div className="performance-profiler-content">
 			<div className="l-block-wrapper">
 				<PerformanceScore value={ 70 } />
+				<CoreWebVitalsDisplay lcp={ 1966 } cls={ 0.01 } fcp={ 1794 } ttfb={ 916 } inp={ 391 } />
 			</div>
 		</div>
 	);
