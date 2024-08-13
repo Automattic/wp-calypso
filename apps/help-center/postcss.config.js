9ec2ab23eeed30700dad00c8fe8020b7b0288be4
Tony Arcangelini <tony@arcangelini.com>
2024-08-13 12:31:41 +0200

    Help Center: add prefix to .card selector

--- apps/help-center/postcss.config.js
+++ apps/help-center/postcss.config.js
@@ -7,6 +7,12 @@ module.exports = {
 				if ( path.includes( 'search/style.scss' ) ) {
 					return selector === '.search' ? prefixedSelector : selector;
 				}
+
+				// The search component has very generic class that causes many bugs.
+				if ( path.includes( 'card/style.scss' ) ) {
+					return selector === '.card' ? prefixedSelector : selector;
+				}
+
 				return selector;
 			},
 		} ),
