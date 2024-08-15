2b18eb079157ed97e10e1952408d12984e88fa12
Gabriel Caires <gabriel.caires@automattic.com>
2024-08-15 14:08:14 +0100

    Fix wrong type name

--- client/landing/stepper/declarative-flow/internals/steps-repository/migration-upgrade-plan/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/migration-upgrade-plan/index.tsx
@@ -4,7 +4,7 @@ import { useTranslate } from 'i18n-calypso';
 import { FC } from 'react';
 import { Step } from '../../types';
 import SiteMigrationUpgradePlan from '../site-migration-upgrade-plan';
-import { MigrationUpgradePlanAction as Actions } from './actions';
+import { MigrationUpgradePlanActions as Actions } from './actions';
 
 interface Props {
 	onClick: () => void;
@@ -36,7 +36,7 @@ const MigrationUpgradePlan: Step = ( props ) => {
 					label={ translate( 'I want to import my content only' ) }
 					onClick={ () =>
 						navigation?.submit?.( {
-							action: Actions.IMPORT_CONTENT_ONLY_ACTION,
+							action: Actions.IMPORT_CONTENT_ONLY,
 						} )
 					}
 				/>
