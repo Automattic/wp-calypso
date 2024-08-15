6d53013e7ac7b1f0e3bdc33d20ed8bdcca3d3de3
Anna McPhee <anna.mcphee@automattic.com>
2024-08-15 11:51:19 +1000

    test text input

--- client/components/stats-date-control/stats-date-control-date-input.tsx
+++ client/components/stats-date-control/stats-date-control-date-input.tsx
@@ -18,7 +18,20 @@ const DateInput: React.FC< Props > = ( {
 		onChange && onChange( event.target.value );
 	};
 
-	return <input id={ id } type="date" value={ value } onChange={ handleChange } max={ max } />;
+	return (
+		<input
+			id={ id }
+			type="text"
+			value={ value }
+			onChange={ handleChange }
+			placeholder="YYYY-MM-DD" // Guide users on the expected format
+			pattern="\d{4}-\d{2}-\d{2}" // Use a pattern to enforce the format
+			title="Date format: YYYY-MM-DD" // Provides a tooltip on hover to guide users
+			max={ max }
+		/>
+	);
+
+	// <input id={ id } type="date" value={ value } onChange={ handleChange } max={ max } />;
 };
 
 export default DateInput;
