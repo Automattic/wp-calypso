fb805108dc60547d3ac8ecae4e5557ed462d281d
Addison Stavlo <Stavz01@gmail.com>
2024-08-15 15:45:29 -0400

    add listName to lastVisitedPlugin state (#93564)

--- client/state/plugins/last-visited/reducer.js
+++ client/state/plugins/last-visited/reducer.js
@@ -1,6 +1,6 @@
 import { PLUGINS_SET_LAST_VISITED } from 'calypso/state/action-types';
 
-export function lastVisited( state = '', action ) {
+export function lastVisited( state = { slug: '', listName: '' }, action ) {
 	if ( action.type === PLUGINS_SET_LAST_VISITED ) {
 		return action.payload;
 	}
