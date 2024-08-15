0164fd70a7216fe8e5c58dc366132b38f6c5c1fc
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-08-15 14:47:31 +0700

    Use site-level user locale for upsell notices

--- client/components/data/query-jitm/index.jsx
+++ client/components/data/query-jitm/index.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCurrentUserLocale } from 'calypso/state/current-user/selectors';
 import { fetchJITM } from 'calypso/state/jitm/actions';
+import { getLocale } from 'calypso/state/user-profile/selectors';
 
 export default function QueryJITM( { siteId, messagePath, searchQuery } ) {
-	const locale = useSelector( getCurrentUserLocale );
+	const locale = useSelector( ( state ) => getLocale( state, siteId ) );
 	const dispatch = useDispatch();
 	useEffect( () => {
 		dispatch( fetchJITM( siteId, messagePath, searchQuery, locale ) );
