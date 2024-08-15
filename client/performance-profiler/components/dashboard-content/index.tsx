721e8e937558d4ec4cb85a2287a0a5d568df55b4
Gergely Csécsey <gcsecsey@gmail.com>
2024-08-15 16:48:22 +0100

    update timeline component

--- client/performance-profiler/components/dashboard-content/index.tsx
+++ client/performance-profiler/components/dashboard-content/index.tsx
@@ -1,10 +1,10 @@
 import { translate } from 'i18n-calypso';
-import { PerformanceReport, ScreenShotsTimeLine } from 'calypso/data/site-profiler/types';
+import { PerformanceReport } from 'calypso/data/site-profiler/types';
 import { CoreWebVitalsDisplay } from 'calypso/performance-profiler/components/core-web-vitals-display';
-import Image from 'calypso/performance-profiler/components/image';
 import { InsightsSection } from 'calypso/performance-profiler/components/insights-section';
 import { PerformanceScore } from 'calypso/performance-profiler/components/performance-score';
 import { ScreenshotThumbnail } from '../screenshot-thumbnail';
+import { ScreenshotTimeline } from '../screenshot-timeline';
 import './style.scss';
 
 type PerformanceProfilerDashboardContentProps = {
@@ -16,23 +16,6 @@ export const PerformanceProfilerDashboardContent = ( {
 }: PerformanceProfilerDashboardContentProps ) => {
 	const { overall_score, fcp, lcp, cls, inp, ttfb, audits, history, screenshots } =
 		performanceReport;
-
-	const renderScreenShotsTimeLine = ( screenshots: ScreenShotsTimeLine[] | undefined ) => {
-		if ( ! screenshots || ! screenshots.length ) {
-			return null;
-		}
-
-		return (
-			<div className="timeline">
-				{ screenshots.map( ( screenshot, index ) => (
-					<div key={ index }>
-						<Image className="thumbnail" src={ screenshot.data } />
-						<p>{ ( screenshot.timing / 1000 ).toFixed( 1 ) }s</p>
-					</div>
-				) ) }
-			</div>
-		);
-	};
 
 	return (
 		<div className="performance-profiler-content">
@@ -52,11 +35,7 @@ export const PerformanceProfilerDashboardContent = ( {
 					ttfb={ ttfb }
 					history={ history }
 				/>
-				<div className="timeline-container">
-					<h1>Timeline</h1>
-					<p>Screenshots of your site loading taken while loading the page. </p>
-					{ renderScreenShotsTimeLine( performanceReport?.screenshots ) }
-				</div>
+				<ScreenshotTimeline screenshots={ screenshots ?? [] } />
 				{ audits && <InsightsSection audits={ audits } /> }
 			</div>
 		</div>
