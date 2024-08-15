05eb86a8dc3ca073f80238179bced4b7edc2cdd4
Anna McPhee <anna.mcphee@automattic.com>
2024-08-15 21:58:15 +1000

    add onBlur validation, feature flag handling

--- client/components/stats-date-control/stats-date-control-date-input.tsx
+++ client/components/stats-date-control/stats-date-control-date-input.tsx
@@ -18,20 +18,44 @@ const DateInput: React.FC< Props > = ( {
 		onChange && onChange( event.target.value );
 	};
 
-	return (
-		<input
-			id={ id }
-			type="text"
-			value={ value }
-			onChange={ handleChange }
-			placeholder="YYYY-MM-DD" // Guide users on the expected format
-			pattern="\d{4}-\d{2}-\d{2}" // Use a pattern to enforce the format
-			title="Date format: YYYY-MM-DD" // Provides a tooltip on hover to guide users
-			max={ max }
-		/>
-	);
+	const featureFlagEnabled = false;
 
-	// <input id={ id } type="date" value={ value } onChange={ handleChange } max={ max } />;
+	const toDateString = ( date: moment.MomentInput ) => {
+		if ( moment.isMoment( date ) || moment( date ).isValid() ) {
+			return moment( date ).format( 'YYYY-MM-DD' );
+		}
+		return moment().format( 'YYYY-MM-DD' );
+	};
+
+	const handleBlur = () => {
+		if ( featureFlagEnabled ) {
+			const isValid = moment( value, 'YYYY-MM-DD', true ).isValid();
+			if ( ! isValid ) {
+				onChange && onChange( moment().format( 'YYYY-MM-DD' ) );
+			} else {
+				toDateString( value );
+			}
+		}
+	};
+
+	return (
+		<>
+			{ featureFlagEnabled ? (
+				<input
+					id={ id }
+					type="text"
+					value={ value }
+					onChange={ handleChange }
+					onBlur={ handleBlur } // Add onBlur event for validation
+					placeholder="YYYY-MM-DD" // Placeholder text in input field
+					title="Date format: YYYY-MM-DD" // tooltip for format
+					max={ max }
+				/>
+			) : (
+				<input id={ id } type="date" value={ value } onChange={ handleChange } max={ max } /> // original date type input pre-feature flag
+			) }
+		</>
+	);
 };
 
 export default DateInput;
