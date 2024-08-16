d54be4cb294a8a06e76b1a8e6db0450165341091
Wesley Berrêdo <Wberredo@gmail.com>
2024-08-16 08:01:36 -0300

    Performance Profiler: Use LLM to clarify the performance insights (#93577)

--- client/performance-profiler/components/metrics-insight/insight-content.tsx
+++ client/performance-profiler/components/metrics-insight/insight-content.tsx
@@ -1,30 +1,39 @@
+import { useTranslate } from 'i18n-calypso';
 import Markdown from 'react-markdown';
 import { PerformanceMetricsItemQueryResponse } from 'calypso/data/site-profiler/types';
 import { InsightDetailedContent } from './insight-detailed-content';
 
 interface InsightContentProps {
 	data: PerformanceMetricsItemQueryResponse;
+	isLoading?: boolean;
 }
 
 export const InsightContent: React.FC< InsightContentProps > = ( props ) => {
-	const { data } = props;
+	const translate = useTranslate();
+	const { data, isLoading } = props;
 	const { description = '' } = data ?? {};
 
 	return (
 		<div className="metrics-insight-content">
-			<Markdown
-				components={ {
-					a( props ) {
-						return <a target="_blank" { ...props } />;
-					},
-				} }
-			>
-				{ description }
-			</Markdown>
-			{ data.details?.type && (
-				<div className="metrics-insight-detailed-content">
-					<InsightDetailedContent data={ data.details } />
-				</div>
+			{ isLoading ? (
+				translate( 'Looking for the best solution…' )
+			) : (
+				<>
+					<Markdown
+						components={ {
+							a( props ) {
+								return <a target="_blank" { ...props } />;
+							},
+						} }
+					>
+						{ description }
+					</Markdown>
+					{ data.details?.type && (
+						<div className="metrics-insight-detailed-content">
+							<InsightDetailedContent data={ data.details } />
+						</div>
+					) }
+				</>
 			) }
 		</div>
 	);
