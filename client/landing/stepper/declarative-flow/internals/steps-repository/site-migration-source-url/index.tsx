698fef6be02d6c60d12f359996454c3e5e684a65
Gabriel Caires <gabriel.caires@automattic.com>
2024-08-19 09:58:49 +0100

    Use flag to manage if the back button should be rendered

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-source-url/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-source-url/index.tsx
@@ -74,6 +74,7 @@ const saveSiteSettings = async ( siteSlug: string, settings: Record< string, unk
 interface SourceURLProps extends StepProps {
 	headerText?: string;
 	subHeaderText?: string;
+	showBack?: boolean;
 }
 
 const SiteMigrationSourceUrl: FC< SourceURLProps > = function ( {
@@ -82,6 +83,7 @@ const SiteMigrationSourceUrl: FC< SourceURLProps > = function ( {
 	subHeaderText,
 	stepName,
 	flow,
+	showBack = true,
 } ) {
 	const siteSlug = useSiteSlug();
 	const translate = useTranslate();
@@ -105,7 +107,7 @@ const SiteMigrationSourceUrl: FC< SourceURLProps > = function ( {
 				stepName={ stepName }
 				flowName={ flow }
 				className="import__onboarding-page"
-				hideBack={ navigation?.goBack === undefined }
+				hideBack={ ! showBack }
 				hideSkip
 				goBack={ navigation?.goBack }
 				hideFormattedHeader
