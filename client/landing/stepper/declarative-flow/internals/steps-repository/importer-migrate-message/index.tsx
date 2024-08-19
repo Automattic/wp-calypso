a25ea009087eb46347511bfc9942c8863d78a816
Valter Lorran <valterlorran@hotmail.com>
2024-08-19 15:01:45 -0300

    Add the Automated Migration texts on the instructions step

--- client/landing/stepper/declarative-flow/internals/steps-repository/importer-migrate-message/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer-migrate-message/index.tsx
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import { useLocale, useHasEnTranslation } from '@automattic/i18n-utils';
 import { StepContainer } from '@automattic/onboarding';
 import { createInterpolateElement } from '@wordpress/element';
@@ -17,6 +18,17 @@ import { getCurrentUser } from 'calypso/state/current-user/selectors';
 import FlowCard from '../components/flow-card';
 import { redirect } from '../import/util';
 import { useSubmitMigrationTicket } from './hooks/use-submit-migration-ticket';
+
+interface ActionsProps {
+	title: string;
+	text: string;
+	onClick: () => void;
+}
+
+interface WhatToExpectProps {
+	icon: JSX.Element;
+	text: string;
+}
 
 const ImporterMigrateMessage: Step = () => {
 	const locale = useLocale();
@@ -38,6 +50,71 @@ const ImporterMigrateMessage: Step = () => {
 		} );
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
+	let whatToExpect: WhatToExpectProps[] = [];
+	let actions: ActionsProps[] = [];
+
+	if ( config.isEnabled( 'automated-migration/collect-credentials' ) ) {
+		whatToExpect = [
+			{
+				icon: group,
+				text: __(
+					`We'll bring over a copy of your site, without affecting the current live version.`
+				),
+			},
+			{
+				icon: backup,
+				text: __(
+					`You'll get an update on the progress of your migration within 2-3 business days.`
+				),
+			},
+		];
+		actions = [
+			{
+				title: __( 'Explore features' ),
+				text: __( 'Discover the features available on WordPress.com' ),
+				onClick: () => redirect( `/home/${ siteSlug }` ),
+			},
+			{
+				title: __( 'Learn about WordPress.com' ),
+				text: __( 'Access guides and tutorials to better understand how to use WordPress.com.' ),
+				onClick: () => redirect( '/support' ),
+			},
+		];
+	} else {
+		whatToExpect = [
+			{
+				icon: shield,
+				text: __( `We'll explain how to securely share your site credentials with us.` ),
+			},
+			{
+				icon: backup,
+				text: __(
+					`We'll update you on the progress of the migration, which usually takes 2-3 business days.`
+				),
+			},
+			{
+				icon: group,
+				text: __( `We'll create a copy of your live site, allowing you to compare the two.` ),
+			},
+		];
+		actions = [
+			{
+				title: __( 'Let me explore' ),
+				text: __( 'Discover more features and options available on WordPress.com on your own.' ),
+				onClick: () => redirect( `/home/${ siteSlug }` ),
+			},
+			{
+				title: __( 'Help me learn' ),
+				text: __( 'Access guides and tutorials to better understand how to use WordPress.com.' ),
+				onClick: () => redirect( '/support' ),
+			},
+		];
+	}
+
+	whatToExpect.push( {
+		icon: globe,
+		text: __( `We'll help you switch your domain over after the migration is complete.` ),
+	} );
 
 	const title = hasEnTranslation( "We'll take it from here!" )
 		? __( "We'll take it from here!" )
@@ -72,47 +149,18 @@ const ImporterMigrateMessage: Step = () => {
 						</div>
 					) }
 					<h3>{ __( 'What to expect' ) }</h3>
-					<div className="feature">
-						<span className="icon">
-							<Icon icon={ shield } />
-						</span>
-						{ __( `We'll explain how to securely share your site credentials with us.` ) }
-					</div>
-					<div className="feature">
-						<span className="icon">
-							<Icon icon={ backup } />
-						</span>
-						{ __(
-							`We'll update you on the progress of the migration, which usually takes 2-3 business days.`
-						) }
-					</div>
-					<div className="feature">
-						<span className="icon">
-							<Icon icon={ group } />
-						</span>
-						{ __( `We’ll create a copy of your live site, allowing you to compare the two.` ) }
-					</div>
-					<div className="feature">
-						<span className="icon">
-							<Icon icon={ globe } />
-						</span>
-						{ __( `We'll help you with the domain changes after the migration is completed.` ) }
-					</div>
+					{ whatToExpect.map( ( { icon, text }, index ) => (
+						<div key={ index } className="feature">
+							<span className="icon">
+								<Icon icon={ icon } />
+							</span>
+							{ text }
+						</div>
+					) ) }
 					<div className="migration-message__actions">
-						<FlowCard
-							title={ __( 'Let me explore' ) }
-							text={ __(
-								'Discover more features and options available on WordPress.com on your own.'
-							) }
-							onClick={ () => redirect( `/home/${ siteSlug }` ) }
-						/>
-						<FlowCard
-							title={ __( 'Help me learn' ) }
-							text={ __(
-								'Access guides and tutorials to better understand how to use WordPress.com.'
-							) }
-							onClick={ () => redirect( '/support' ) }
-						/>
+						{ actions.map( ( { title, text, onClick }, index ) => (
+							<FlowCard key={ index } title={ title } text={ text } onClick={ onClick } />
+						) ) }
 					</div>
 				</>
 			}
