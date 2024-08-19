c1254a1c0ecdec0e12aacfeccfaf4fda463d559d
Valter Lorran <valterlorran@hotmail.com>
2024-08-19 10:02:06 -0300

    Adapt the API body format

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-credentials/hooks/use-request-automated-migration.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-credentials/hooks/use-request-automated-migration.ts
@@ -21,6 +21,15 @@ interface AutomatedMigrationRequest {
 	howToAccessSite: 'credentials' | 'backup';
 }
 
+interface AutomatedMigrationBody {
+	migration_type: 'credentials' | 'backup';
+	from_url?: string;
+	username?: string;
+	password?: string;
+	backup_file_location?: string;
+	notes?: string;
+}
+
 export const useRequestAutomatedMigration = <
 	TData = AutomatedMigrationAPIResponse | APIError,
 	TError = APIError,
@@ -36,21 +45,35 @@ export const useRequestAutomatedMigration = <
 			backupFileLocation,
 			notes,
 			howToAccessSite,
-		} ) =>
-			wpcomRequest( {
+		} ) => {
+			let body: AutomatedMigrationBody = {
+				migration_type: howToAccessSite,
+				notes,
+			};
+
+			if ( howToAccessSite === 'credentials' ) {
+				body = {
+					...body,
+					from_url: siteAddress,
+					username,
+					password,
+				};
+			} else {
+				// In case of backup, we need to send the backup file location
+				body = {
+					...body,
+					from_url: backupFileLocation,
+				};
+			}
+
+			return wpcomRequest( {
 				path: 'help/automated-migration',
 				apiNamespace: 'wpcom/v2/',
 				apiVersion: '2',
 				method: 'POST',
-				body: {
-					migration_type: howToAccessSite,
-					from_url: siteAddress,
-					username,
-					password,
-					backup_file_location: backupFileLocation,
-					notes,
-				},
-			} ),
+				body,
+			} );
+		},
 		...options,
 	} );
 
