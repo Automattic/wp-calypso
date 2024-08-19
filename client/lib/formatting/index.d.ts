ace500306ab8245a559e80c0acb04849fb9f72ed
Omar Alshaker <omar@omaralshaker.com>
2024-08-19 22:54:01 +0200

    Type decodeEntites

--- client/lib/formatting/index.d.ts
+++ client/lib/formatting/index.d.ts
@@ -1,3 +1,4 @@
 import type { ReactNode } from 'react';
 
 export declare function preventWidows( text: string | ReactNode, wordsToKeep?: number = 2 ): string;
+export declare function decodeEntities( text: string ): string;
