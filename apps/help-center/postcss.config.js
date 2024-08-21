f3d4c8da0c78541debefcc93f4dd624c567153b5
Tony Arcangelini <tony@arcangelini.com>
2024-08-21 08:45:09 +0200

    Undo postcss changes

--- apps/help-center/postcss.config.js
+++ apps/help-center/postcss.config.js
@@ -1,7 +1,20 @@
 module.exports = {
 	plugins: [
-		require( 'postcss-prefixwrap' )( '.help-center', {
-			ignoredSelectors: [ '.help-center', /^\.popover(.+)?$/ ],
+		require( 'postcss-prefix-selector' )( {
+			prefix: '.help-center',
+			transform: function ( prefix, selector, prefixedSelector, path ) {
+				// The search component has very generic class that causes many bugs.
+				if ( path.includes( 'search/style.scss' ) ) {
+					return selector === '.search' ? prefixedSelector : selector;
+				}
+
+				// The search component has very generic class that causes many bugs.
+				if ( path.includes( 'card/style.scss' ) ) {
+					return selector === '.card' ? prefixedSelector : selector;
+				}
+
+				return selector;
+			},
 		} ),
 	],
 };
