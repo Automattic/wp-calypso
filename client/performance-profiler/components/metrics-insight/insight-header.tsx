d7f96c651f5064359ad705997ff9b1f2e5fb208e
Gergely Csécsey <gergely.csecsey@automattic.com>
2024-08-21 16:17:43 +0200

    Performance Profiler: update color styles for insights and metrics (#93777)
    * update colors on insights
    
    * Update color styles for insights and metrics

--- client/performance-profiler/components/metrics-insight/insight-header.tsx
+++ client/performance-profiler/components/metrics-insight/insight-header.tsx
@@ -1,4 +1,5 @@
 import { isMobile } from '@automattic/viewport';
+import clsx from 'clsx';
 import Markdown from 'react-markdown';
 import { PerformanceMetricsItemQueryResponse } from 'calypso/data/site-profiler/types';
 
@@ -11,10 +12,11 @@ export const InsightHeader: React.FC< InsightHeaderProps > = ( props ) => {
 	const { data, index } = props;
 	const title = data.title ?? '';
 	const value = data.displayValue ?? '';
+	const { type } = data;
 
 	return (
 		<>
-			<span className="counter">{ index + 1 }</span>
+			<span className={ clsx( 'counter', { [ type ]: true } ) }>{ index + 1 }</span>
 			<Markdown
 				components={ {
 					p( props ) {
@@ -27,10 +29,13 @@ export const InsightHeader: React.FC< InsightHeaderProps > = ( props ) => {
 			>
 				{ title }
 			</Markdown>
-			{ value && isMobile() && <span className="value is-mobile"> { value }</span> }
+			{ value && isMobile() && (
+				<span className={ clsx( 'value is-mobile', { [ type ]: true } ) }> { value }</span>
+			) }
 			{ value && ! isMobile() && (
 				<span>
-					&nbsp;&minus;&nbsp;<span className="value"> { value }</span>
+					&nbsp;&minus;&nbsp;
+					<span className={ clsx( 'value', { [ type ]: true } ) }> { value }</span>
 				</span>
 			) }
 		</>
