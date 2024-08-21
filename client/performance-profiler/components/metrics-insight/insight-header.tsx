292e3b372e06218919b1cb0af957253a8a9a358b
Gergely Csécsey <gcsecsey@gmail.com>
2024-08-21 14:17:14 +0100

    update colors on insights

--- client/performance-profiler/components/metrics-insight/insight-header.tsx
+++ client/performance-profiler/components/metrics-insight/insight-header.tsx
@@ -14,14 +14,23 @@ export const InsightHeader: React.FC< InsightHeaderProps > = ( props ) => {
 
 	return (
 		<>
-			<span className="counter">{ index + 1 }</span>
+			<span className="counter" style={ { color: data.type === 'fail' ? 'red' : 'orange' } }>
+				{ index + 1 }
+			</span>
 			<Markdown
 				components={ {
 					p( props ) {
 						return <p className="title-description">{ props.children }</p>;
 					},
 					code( props ) {
-						return <span className="value">{ props.children }</span>;
+						return (
+							<span
+								className="value"
+								style={ { color: props.data.type === 'fail' ? 'red' : 'orange' } }
+							>
+								{ props.children }
+							</span>
+						);
 					},
 				} }
 			>
@@ -30,7 +39,11 @@ export const InsightHeader: React.FC< InsightHeaderProps > = ( props ) => {
 			{ value && isMobile() && <span className="value is-mobile"> { value }</span> }
 			{ value && ! isMobile() && (
 				<span>
-					&nbsp;&minus;&nbsp;<span className="value"> { value }</span>
+					&nbsp;&minus;&nbsp;
+					<span className="value" style={ { color: data.type === 'fail' ? 'red' : 'orange' } }>
+						{ ' ' }
+						{ value }
+					</span>
 				</span>
 			) }
 		</>
