e0e0d78b28283e088c24f37f401850f6adcd9a53
Renatho De Carli Rosa <renatho@gmail.com>
2024-08-21 17:22:08 -0300

    Revert "Replace hosted migration flow if feature flag is enabled"
    This reverts commit cb36219727165b8e6450153117594775602b5488.

--- client/landing/stepper/declarative-flow/hosted-site-migration-flow.ts
+++ client/landing/stepper/declarative-flow/hosted-site-migration-flow.ts
@@ -1,13 +1,9 @@
-import { isEnabled } from '@automattic/calypso-config';
 import { HOSTED_SITE_MIGRATION_FLOW } from '@automattic/onboarding';
 import { type Flow } from './internals/types';
-import migration from './migration';
 import siteMigration from './site-migration-flow';
 
-const baseFlow = isEnabled( 'migration-flow/revamp' ) ? migration : siteMigration;
-
 const hostedSiteMigrationFlow: Flow = {
-	...baseFlow,
+	...siteMigration,
 	variantSlug: HOSTED_SITE_MIGRATION_FLOW,
 	isSignupFlow: true,
 };
