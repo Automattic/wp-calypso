a4c5c3b400b7603c7c80719614f4aadac5f555c0
Anthony Grullon <anthony.grullon@automattic.com>
2024-08-21 11:57:26 -0400

    Help Center: Close help center when notifications panel is opened (#93228)
    * Add changes on Calypso to close help center when notifications panel is opened.
    
    * Add change for WP-admin

--- client/layout/masterbar/masterbar-help-center.jsx
+++ client/layout/masterbar/masterbar-help-center.jsx
@@ -1,12 +1,15 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { HelpCenter } from '@automattic/data-stores';
+import { usePrevious } from '@wordpress/compose';
 import {
 	useDispatch as useDataStoreDispatch,
 	useSelect as useDateStoreSelect,
 } from '@wordpress/data';
 import { helpFilled } from '@wordpress/icons';
 import clsx from 'clsx';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import getIsNotificationsOpen from 'calypso/state/selectors/is-notifications-open';
 import { getSectionName } from 'calypso/state/ui/selectors';
 import Item from './item';
 
@@ -14,6 +17,8 @@ const HELP_CENTER_STORE = HelpCenter.register();
 
 const MasterbarHelpCenter = ( { tooltip } ) => {
 	const sectionName = useSelector( getSectionName );
+	const isNotificationsOpen = useSelector( ( state ) => getIsNotificationsOpen( state ) );
+	const prevIsNotificationsOpen = usePrevious( isNotificationsOpen );
 
 	const helpCenterVisible = useDateStoreSelect( ( select ) =>
 		select( HELP_CENTER_STORE ).isHelpCenterShown()
@@ -29,6 +34,13 @@ const MasterbarHelpCenter = ( { tooltip } ) => {
 
 		setShowHelpCenter( ! helpCenterVisible );
 	};
+
+	// Close the help center when notifications are opened
+	useEffect( () => {
+		if ( ! prevIsNotificationsOpen && isNotificationsOpen && helpCenterVisible ) {
+			setShowHelpCenter( false );
+		}
+	}, [ helpCenterVisible, isNotificationsOpen, prevIsNotificationsOpen, setShowHelpCenter ] );
 
 	return (
 		<>
