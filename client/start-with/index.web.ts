7d1dadcb0416e121ff64d38a5d6f0524621880bf
Wesley Berrêdo <wberredo@gmail.com>
2024-08-22 16:29:10 -0300

    Start With: Removal of resources from Calypso

--- client/start-with/index.web.ts
+++
@@ -1,8 +0,0 @@
-import page from '@automattic/calypso-router';
-import { makeLayout, render as clientRender } from 'calypso/controller/index.web';
-import { notFound, startWithSquarePaymentsContext } from 'calypso/start-with/controller';
-
-export default function () {
-	page( '/start-with/square-payments', startWithSquarePaymentsContext, makeLayout, clientRender );
-	page( '/start-with*', notFound, makeLayout, clientRender );
-}
