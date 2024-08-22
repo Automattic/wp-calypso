11dd46640629153fe82b2d588d06c194e5529e0e
chad1008 <13856531+chad1008@users.noreply.github.com>
2024-08-22 07:59:47 -0400

    convert test file to typescript

--- client/signup/steps/emails/test/index.tsx
+++ client/signup/steps/emails/test/index.tsx
@@ -3,6 +3,7 @@
  */
 
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
