ce9b9b1e3c1154a6b6dad55c7228f839735f62c7
Donna Peplinskie <donnapep@gmail.com>
2024-08-23 15:51:02 -0400

    Add buttons in useSteps instead of in each step component

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/step-add-migration-key.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/step-add-migration-key.tsx
@@ -1,24 +1,14 @@
-import { Button } from '@wordpress/components';
 import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
 import { MigrationKeyInput } from '../migration-key-input';
-import { recordMigrationInstructionsLinkClick } from '../tracking';
-import { getMigrateGuruPageURL } from './utils';
 import type { FC } from 'react';
 
 interface Props {
-	fromUrl: string;
 	migrationKey: string;
-	onDoneClick: () => void;
 	preparationError: Error | null;
 }
 
-export const StepAddMigrationKey: FC< Props > = ( {
-	fromUrl,
-	migrationKey,
-	onDoneClick,
-	preparationError,
-} ) => {
+export const StepAddMigrationKey: FC< Props > = ( { migrationKey, preparationError } ) => {
 	const translate = useTranslate();
 	const migrationKeyLabel = 'Migration Key';
 	const migrateLabel = 'Start migration';
@@ -36,11 +26,6 @@ export const StepAddMigrationKey: FC< Props > = ( {
 		);
 	}
 
-	const onEnterKeyClick = () => {
-		window.open( getMigrateGuruPageURL( fromUrl ), '_blank' );
-		recordMigrationInstructionsLinkClick( 'enter-key' );
-	};
-
 	return (
 		<>
 			<p>
@@ -55,22 +40,6 @@ export const StepAddMigrationKey: FC< Props > = ( {
 				) }
 			</p>
 			<MigrationKeyInput value={ migrationKey } />
-			<div className="checklist-item__checklist-expanded-ctas">
-				<Button
-					className="checklist-item__checklist-expanded-cta"
-					variant="primary"
-					onClick={ onEnterKeyClick }
-				>
-					{ translate( 'Enter key' ) }
-				</Button>
-				<Button
-					className="checklist-item__checklist-expanded-cta"
-					variant="secondary"
-					onClick={ onDoneClick }
-				>
-					{ translate( 'Done' ) }
-				</Button>
-			</div>
 		</>
 	);
 };
