99333d0be4f6a89ff78127d821ede65dde4096e5
Omar Alshaker <omar@omaralshaker.com>
2024-08-26 12:45:43 +0200

    Onboarding Package: add `usePersistedState` hook (#93878)

---
+++ packages/onboarding/src/hooks/test/index.ts
@@ -0,0 +1,51 @@
+import { act, renderHook } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { usePersistedState } from '../use-persisted-state';
+
+describe( 'usePersistedState', () => {
+	test( 'Should return default value and update it on request', () => {
+		const { result, rerender } = renderHook( () => usePersistedState( 'some-random-state' ), {
+			wrapper: MemoryRouter,
+		} );
+
+		const [ state, setState ] = result.current;
+
+		expect( state ).toEqual( 'some-random-state' );
+
+		act( () => setState( 'new-state' ) );
+
+		rerender();
+
+		expect( result.current[ 0 ] ).toEqual( 'new-state' );
+	} );
+
+	test( 'Instances should not collide', () => {
+		const { result: resultOne, rerender: rerenderOne } = renderHook(
+			() => usePersistedState( 'some-random-state' ),
+			{
+				wrapper: MemoryRouter,
+			}
+		);
+		const { result: resultTwo, rerender: rerenderTwo } = renderHook(
+			() => usePersistedState( 'some-random-state-2' ),
+			{
+				wrapper: MemoryRouter,
+			}
+		);
+
+		const [ state1, setState1 ] = resultOne.current;
+		const [ state2, setState2 ] = resultTwo.current;
+
+		expect( state1 ).toEqual( 'some-random-state' );
+		expect( state2 ).toEqual( 'some-random-state-2' );
+
+		act( () => setState1( 'new-state-one' ) );
+		act( () => setState2( 'new-state-two' ) );
+
+		rerenderOne();
+		rerenderTwo();
+
+		expect( resultOne.current[ 0 ] ).toEqual( 'new-state-one' );
+		expect( resultTwo.current[ 0 ] ).toEqual( 'new-state-two' );
+	} );
+} );
