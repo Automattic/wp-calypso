58b3d1f47285b58d976bfa5e6f9bfcbd513dcaf5
Paulo Cruz <paulo.cruz@automattic.com>
2024-08-27 10:31:49 -0300

    Include useGetTipaltiPayee as a loading dependency

--- client/a8c-for-agencies/sections/marketplace/hooks/use-referral-dev-site.ts
+++ client/a8c-for-agencies/sections/marketplace/hooks/use-referral-dev-site.ts
@@ -1,6 +1,7 @@
 import { useCallback, useMemo } from 'react';
 import useProductsQuery from 'calypso/a8c-for-agencies/data/marketplace/use-products-query';
 import useFetchDevLicense from 'calypso/a8c-for-agencies/data/purchases/use-fetch-dev-license';
+import useGetTipaltiPayee from 'calypso/a8c-for-agencies/sections/referrals/hooks/use-get-tipalti-payee';
 import { ShoppingCartItem } from '../types';
 
 export default function useReferralDevSite(
@@ -10,6 +11,7 @@ export default function useReferralDevSite(
 ) {
 	const { data: allProducts, isLoading: isLoadingProducts } = useProductsQuery();
 	const { data: license, isLoading: isLoadingLicense } = useFetchDevLicense( referralBlogId );
+	const { isLoading: isLoadingTipalti } = useGetTipaltiPayee();
 
 	const product = useMemo(
 		() => allProducts?.find( ( p ) => p.product_id === license?.productId ),
@@ -34,5 +36,8 @@ export default function useReferralDevSite(
 		}
 	}, [ license, product, referralPlanAdded, setSelectedCartItems ] );
 
-	return { addReferralPlanToCart, isLoading: isLoadingProducts || isLoadingLicense };
+	return {
+		addReferralPlanToCart,
+		isLoading: isLoadingProducts || isLoadingLicense || isLoadingTipalti,
+	};
 }
