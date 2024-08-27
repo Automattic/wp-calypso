13b360b0029b4a4e32979f5731cfe30fb7be2ec5
André <583546+oandregal@users.noreply.github.com>
2024-08-27 15:29:08 +0200

    A4A Referrals details: add pagination

--- client/a8c-for-agencies/sections/referrals/common/referral-details-table/index.tsx
+++ client/a8c-for-agencies/sections/referrals/common/referral-details-table/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { filterSortAndPaginate } from '@wordpress/dataviews';
+import { useState, useMemo } from 'react';
 import { initialDataViewsState } from 'calypso/a8c-for-agencies/components/items-dashboard/constants';
 import ItemsDataViews from 'calypso/a8c-for-agencies/components/items-dashboard/items-dataviews';
 import type { ReferralPurchase } from '../../types';
@@ -14,16 +15,17 @@ interface Props {
 export default function ReferralDetailsTable( { items, fields }: Props ) {
 	const [ dataViewsState, setDataViewsState ] = useState( initialDataViewsState );
 
+	const { data, paginationInfo } = useMemo( () => {
+		return filterSortAndPaginate( items, dataViewsState, fields );
+	}, [ items, dataViewsState, fields ] );
+
 	return (
 		<div className="referrals-details-table__container redesigned-a8c-table">
 			<ItemsDataViews
 				data={ {
-					items,
+					items: data,
 					fields,
-					pagination: {
-						totalItems: 1,
-						totalPages: 1,
-					},
+					pagination: paginationInfo,
 					enableSearch: false,
 					actions: [],
 					dataViewsState: dataViewsState,
