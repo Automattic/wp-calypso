453b8b2e3cdf43e2a763725a835b0e6ea7aeced0
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2024-08-27 16:02:41 +0300

    Server: Add noindex meta tags for /start/[sub-page] and /setup/* (#93943)

--- client/signup/index.node.js
+++ client/signup/index.node.js
@@ -1,4 +1,6 @@
 import { getLanguage, getLanguageRouteParam } from '@automattic/i18n-utils';
+import { setDocumentHeadMeta } from 'calypso/state/document-head/actions';
+import { getDocumentHeadMeta } from 'calypso/state/document-head/selectors';
 
 export default function ( router ) {
 	const lang = getLanguageRouteParam();
@@ -10,7 +12,8 @@ export default function ( router ) {
 			`/start/:flowName/:stepName/${ lang }`,
 			`/start/:flowName/:stepName/:stepSectionName/${ lang }`,
 		],
-		setUpLocale
+		setUpLocale,
+		setupMetaTags
 	);
 }
 
@@ -19,6 +22,20 @@ function setUpLocale( context, next ) {
 	const language = getLanguage( context.params.lang );
 	if ( language ) {
 		context.lang = context.params.lang;
+	}
+
+	next();
+}
+
+// Set up meta tags.
+function setupMetaTags( context, next ) {
+	// All `/start/*` sub-pages should be noindex. See 3065-gh-Automattic/martech.
+	if ( ! /^\/start\/?$/.test( context.pathname ) ) {
+		const meta = getDocumentHeadMeta( context.store.getState() ).concat( {
+			name: 'robots',
+			content: 'noindex',
+		} );
+		context.store.dispatch( setDocumentHeadMeta( meta ) );
 	}
 
 	next();
