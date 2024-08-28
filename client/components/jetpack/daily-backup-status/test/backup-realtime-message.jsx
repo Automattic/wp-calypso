e7e3989bc456078751b461216dc678cf4ac7bae5
Rafael Agostini <rafael.agostini@automattic.com>
2024-08-27 22:12:57 -0500

    Refactor BackupRealtimeMessage unit tests to consider latest changes

--- client/components/jetpack/daily-backup-status/test/backup-realtime-message.jsx
+++ client/components/jetpack/daily-backup-status/test/backup-realtime-message.jsx
@@ -3,23 +3,19 @@
  */
 import { render } from '@testing-library/react';
 import { useTranslate } from 'i18n-calypso';
-import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import moment from 'moment';
 import { BackupRealtimeMessage } from '../status-card/backup-realtime-message';
 
 // Mock the useTranslate function
 jest.mock( 'i18n-calypso' );
 
-// Mock the useLocalizedMoment hook
-jest.mock( 'calypso/components/localized-moment' );
-
 describe( 'BackupRealtimeMessage', () => {
-	const fixedDate = '2024-08-26T12:00:00Z'; // Fixed date to use as "today"
-
-	const renderMessage = ( backupDate, eventsCount ) => {
+	const renderMessage = ( baseBackupDate, eventsCount, selectedDate ) => {
 		return render(
 			<BackupRealtimeMessage
-				backupDate={ new Date( backupDate ).getTime() }
+				baseBackupDate={ baseBackupDate }
 				eventsCount={ eventsCount }
+				selectedBackupDate={ selectedDate }
 			/>
 		);
 	};
@@ -27,7 +23,7 @@ describe( 'BackupRealtimeMessage', () => {
 	const translateMock = jest.fn( ( singular, plural, { count, args } ) => {
 		const translatedText = count === 1 ? singular : plural;
 		return translatedText
-			.replace( '%(backupDate)s', args.backupDate )
+			.replace( '%(baseBackupDate)s', args.baseBackupDate )
 			.replace( '%(eventsCount)d', args.eventsCount )
 			.replace( '%(daysAgo)d', args.daysAgo );
 	} );
@@ -35,48 +31,50 @@ describe( 'BackupRealtimeMessage', () => {
 	beforeEach( () => {
 		jest.clearAllMocks();
 		useTranslate.mockImplementation( () => translateMock );
-
-		// Mock useLocalizedMoment to return a fixed moment instance
-		useLocalizedMoment.mockImplementation( () => {
-			const moment = jest.requireActual( 'moment' );
-			return ( input ) => {
-				return input ? moment( input ) : moment( fixedDate );
-			};
-		} );
 	} );
 
 	test( "renders correct message for today's backup", () => {
-		const { container } = renderMessage( '2024-08-26T12:00:00Z', 3 );
+		const selectedDate = moment( '2024-08-26T12:00:00Z' );
+		const baseBackupDate = selectedDate; // same day
+		const { container } = renderMessage( baseBackupDate, 3, selectedDate );
 		expect( container.textContent ).toBe(
-			`We are using the full backup from today (2024-08-26 12:00 PM) with 3 changes you have made since then until now.`
+			`We are using a full backup from this day (2024-08-26 12:00 PM) with 3 changes you have made since then until now.`
 		);
 	} );
 
 	test( "renders correct message for yesterday's backup", () => {
-		const { container } = renderMessage( '2024-08-25T12:00:00Z', 25 );
+		const selectedDate = moment( '2024-08-26T12:00:00Z' );
+		const baseBackupDate = selectedDate.clone().subtract( 1, 'day' ); // previous day
+		const { container } = renderMessage( baseBackupDate, 25, selectedDate );
 		expect( container.textContent ).toBe(
-			`We are using the full backup from yesterday (2024-08-25 12:00 PM) with 25 changes you have made since then until now.`
+			`We are using a full backup from the previous day (2024-08-25 12:00 PM) with 25 changes you have made since then until now.`
 		);
 	} );
 
 	test( 'renders correct message for a backup less than 7 days old', () => {
-		const { container } = renderMessage( '2024-08-23T12:00:00Z', 5 );
+		const selectedDate = moment( '2024-08-26T12:00:00Z' );
+		const baseBackupDate = moment( '2024-08-23T12:00:00Z' );
+		const { container } = renderMessage( baseBackupDate, 5, selectedDate );
 		expect( container.textContent ).toBe(
 			`We are using a 3-day old full backup (2024-08-23 12:00 PM) with 5 changes you have made since then until now.`
 		);
 	} );
 
 	test( 'renders correct singular message', () => {
-		const { container } = renderMessage( '2024-08-26T12:00:00Z', 1 );
+		const selectedDate = moment( '2024-08-26T12:00:00Z' );
+		const baseBackupDate = moment( '2024-08-26T12:00:00Z' );
+		const { container } = renderMessage( baseBackupDate, 1, selectedDate );
 		expect( container.textContent ).toBe(
-			`We are using the full backup from today (2024-08-26 12:00 PM) with 1 change you have made since then until now.`
+			`We are using a full backup from this day (2024-08-26 12:00 PM) with 1 change you have made since then until now.`
 		);
 	} );
 
 	test( 'renders correct plural message', () => {
-		const { container } = renderMessage( '2024-08-26T12:00:00Z', 2 );
+		const selectedDate = moment( '2024-08-26T12:00:00Z' );
+		const baseBackupDate = moment( '2024-08-26T12:00:00Z' );
+		const { container } = renderMessage( baseBackupDate, 2, selectedDate );
 		expect( container.textContent ).toBe(
-			`We are using the full backup from today (2024-08-26 12:00 PM) with 2 changes you have made since then until now.`
+			`We are using a full backup from this day (2024-08-26 12:00 PM) with 2 changes you have made since then until now.`
 		);
 	} );
 } );
