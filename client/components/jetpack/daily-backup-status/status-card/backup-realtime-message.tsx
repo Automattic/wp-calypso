6e5c8e3e2bbc9464d94ed037a471f1bcf46df181
Rafael Agostini <rafael.agostini@automattic.com>
2024-08-27 21:29:37 -0500

    feat: Update BackupRealtimeMessage component to use baseBackupDate instead of backupDate
    The BackupRealtimeMessage component has been updated to use the baseBackupDate prop instead of the backupDate prop. This change ensures that the correct backup date is displayed in the message. Also we are adding `selectedBackupDate` to use instead of `today` date.

--- client/components/jetpack/daily-backup-status/status-card/backup-realtime-message.tsx
+++ client/components/jetpack/daily-backup-status/status-card/backup-realtime-message.tsx
@@ -1,80 +1,83 @@
 import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import type { Moment } from 'moment';
 
 type Props = {
-	backupDate: number; // timestamp
+	baseBackupDate: number; // timestamp
 	eventsCount: number;
+	selectedBackupDate: Moment;
 };
 
 export const BackupRealtimeMessage: FunctionComponent< Props > = ( {
-	backupDate,
+	baseBackupDate,
 	eventsCount,
+	selectedBackupDate,
 } ) => {
 	const translate = useTranslate();
 	const moment = useLocalizedMoment();
-	const backupDateMoment = moment( backupDate );
-	const today = moment();
-	const daysAgo = today.diff( backupDateMoment, 'days' );
+	const baseBackupDateMoment = moment( baseBackupDate );
+	const today = selectedBackupDate;
+	const daysAgo = selectedBackupDate.diff( baseBackupDate, 'days' );
 
 	let message: string | React.ReactNode;
 
-	if ( backupDateMoment.isSame( today, 'day' ) ) {
+	if ( baseBackupDateMoment.isSame( today, 'day' ) ) {
 		message = translate(
-			'We are using the full backup from today (%(backupDate)s) with %(eventsCount)d change you have made since then until now.',
-			'We are using the full backup from today (%(backupDate)s) with %(eventsCount)d changes you have made since then until now.',
+			'We are using a full backup from this day (%(baseBackupDate)s) with %(eventsCount)d change you have made since then until now.',
+			'We are using a full backup from this day (%(baseBackupDate)s) with %(eventsCount)d changes you have made since then until now.',
 			{
 				count: eventsCount,
 				args: {
-					backupDate: backupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
+					baseBackupDate: baseBackupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
 					eventsCount: eventsCount,
 				},
 				comment:
-					'%(backupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
+					'%(baseBackupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
 			}
 		);
-	} else if ( backupDateMoment.isSame( today.clone().subtract( 1, 'days' ), 'day' ) ) {
+	} else if ( baseBackupDateMoment.isSame( today.clone().subtract( 1, 'days' ), 'day' ) ) {
 		message = translate(
-			'We are using the full backup from yesterday (%(backupDate)s) with %(eventsCount)d change you have made since then until now.',
-			'We are using the full backup from yesterday (%(backupDate)s) with %(eventsCount)d changes you have made since then until now.',
+			'We are using a full backup from the previous day (%(baseBackupDate)s) with %(eventsCount)d change you have made since then until now.',
+			'We are using a full backup from the previous day (%(baseBackupDate)s) with %(eventsCount)d changes you have made since then until now.',
 			{
 				count: eventsCount,
 				args: {
-					backupDate: backupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
+					baseBackupDate: baseBackupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
 					eventsCount: eventsCount,
 				},
 				comment:
-					'%(backupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
+					'%(baseBackupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
 			}
 		);
 	} else if ( daysAgo < 7 ) {
 		message = translate(
-			'We are using a %(daysAgo)d-day old full backup (%(backupDate)s) with %(eventsCount)d change you have made since then until now.',
-			'We are using a %(daysAgo)d-day old full backup (%(backupDate)s) with %(eventsCount)d changes you have made since then until now.',
+			'We are using a %(daysAgo)d-day old full backup (%(baseBackupDate)s) with %(eventsCount)d change you have made since then until now.',
+			'We are using a %(daysAgo)d-day old full backup (%(baseBackupDate)s) with %(eventsCount)d changes you have made since then until now.',
 			{
 				count: eventsCount,
 				args: {
 					daysAgo: daysAgo,
-					backupDate: backupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
+					baseBackupDate: baseBackupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
 					eventsCount: eventsCount,
 				},
 				comment:
-					'%(daysAgo)d is the number of days since the backup, %(backupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
+					'%(daysAgo)d is the number of days since the backup, %(baseBackupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
 			}
 		);
 	} else {
 		message = translate(
-			'We are using a %(daysAgo)d-day old full backup (%(backupDate)s) with %(eventsCount)d change you have made since then until now.',
-			'We are using a %(daysAgo)d-day old full backup (%(backupDate)s) with %(eventsCount)d changes you have made since then until now.',
+			'We are using a %(daysAgo)d-day old full backup (%(baseBackupDate)s) with %(eventsCount)d change you have made since then until now.',
+			'We are using a %(daysAgo)d-day old full backup (%(baseBackupDate)s) with %(eventsCount)d changes you have made since then until now.',
 			{
 				count: eventsCount,
 				args: {
 					daysAgo: daysAgo,
-					backupDate: backupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
+					baseBackupDate: baseBackupDateMoment.format( 'YYYY-MM-DD hh:mm A' ),
 					eventsCount: eventsCount,
 				},
 				comment:
-					'%(daysAgo)d is the number of days since the backup, %(backupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
+					'%(daysAgo)d is the number of days since the backup, %(baseBackupDate)s is the date and time of the backup, and %(eventsCount)d is the number of changes made since the backup.',
 			}
 		);
 	}
