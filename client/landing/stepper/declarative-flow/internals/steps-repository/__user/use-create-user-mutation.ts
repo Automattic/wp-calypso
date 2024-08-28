164c0e66a31b7e307a622dbbb70c7df5d79fbab6
Omar Alshaker <omar@omaralshaker.com>
2024-08-28 18:11:48 +0200

    Disable signup form during loading

--- client/landing/stepper/declarative-flow/internals/steps-repository/__user/use-create-user-mutation.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/__user/use-create-user-mutation.ts
@@ -2,10 +2,24 @@
 // import { initGoogleRecaptcha, recordGoogleRecaptchaAction } from 'calypso/lib/analytics/recaptcha';
 import { useMutation } from '@tanstack/react-query';
 import { createAccount } from 'calypso/lib/signup/api/account';
+import { useDispatch } from 'calypso/state';
+import { LOGIN_AUTH_ACCOUNT_TYPE_REQUESTING } from 'calypso/state/action-types';
+import { receiveSuccess } from 'calypso/state/data-layer/wpcom/users/auth-options';
 
 export function useCreateAccountMutation() {
+	const dispatch = useDispatch();
+
 	return useMutation( {
 		mutationKey: [ 'create' ],
 		mutationFn: createAccount,
+		onMutate: ( variables ) => {
+			dispatch( {
+				type: LOGIN_AUTH_ACCOUNT_TYPE_REQUESTING,
+				usernameOrEmail: variables.userData?.email,
+			} );
+		},
+		onSuccess: ( data ) => {
+			dispatch( receiveSuccess( null, data ) );
+		},
 	} );
 }
