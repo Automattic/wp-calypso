ff51734299711450d91a2c22e85da9c069b8c5e1
Allison Levine <allison.levine@automattic.com>
2024-08-28 08:57:51 -0400

    Hide the popover in the test environment.

--- client/layout/masterbar/logged-in.jsx
+++ client/layout/masterbar/logged-in.jsx
@@ -530,6 +530,7 @@ class MasterbarLoggedIn extends Component {
 	renderReader() {
 		const { translate, sectionGroup, isFetchingPrefs, hasDismissedReaderPopover } = this.props;
 		const { readerBtnRef } = this.state;
+		const showReaderPopover = ! config.isEnabled( 'reader/hide-tooltip' );
 		return (
 			<>
 				<Item
@@ -555,7 +556,7 @@ class MasterbarLoggedIn extends Component {
 					ref={ ( ref ) => ref !== readerBtnRef && this.setState( { readerBtnRef: ref } ) }
 					hasGlobalBorderStyle
 				/>
-				{ readerBtnRef && (
+				{ showReaderPopover && readerBtnRef && (
 					<Popover
 						className="masterbar__reader-popover"
 						isVisible={
