ae4e29c0ab2689391ae8a7a861675fe3e49e0cc9
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2024-08-29 20:23:41 +0300

    Fix imports vs type imports

--- packages/fingerprintjs/src/index.ts
+++ packages/fingerprintjs/src/index.ts
@@ -1,33 +1,24 @@
-import {
-	load,
-	Agent,
-	LoadOptions,
-	GetOptions,
-	GetResult,
-	hashComponents,
-	componentsToDebugString,
-} from './agent';
+import { load, hashComponents, componentsToDebugString } from './agent';
 import { getVisitorId } from './runtime';
-import { BuiltinComponents } from './sources';
-import { Confidence } from './confidence';
-import { Component, UnknownComponents } from './utils/entropy_source';
 import { x64hash128 } from './utils/hashing';
+import type { Agent, GetOptions, GetResult, LoadOptions } from './agent';
+import type { BuiltinComponents } from './sources';
+import type { Component, UnknownComponents } from './utils/entropy_source';
+import type { Confidence } from './confidence';
 
 // Exports that are under Semantic versioning
-export {
-	getVisitorId,
-	load,
+export { getVisitorId, load, hashComponents, componentsToDebugString };
+export type {
 	Agent,
-	LoadOptions,
+	BuiltinComponents,
+	Component,
+	Confidence,
 	GetOptions,
 	GetResult,
-	hashComponents,
-	componentsToDebugString,
-	Component,
+	LoadOptions,
 	UnknownComponents,
-	BuiltinComponents,
-	Confidence,
 };
+
 // The default export is a syntax sugar (`import * as FP from '...' → import FP from '...'`).
 // It should contain all the public exported values.
 export default { load, hashComponents, componentsToDebugString };
@@ -50,9 +41,7 @@ export {
 } from './utils/browser';
 export {
 	loadSources,
-	Source,
-	SourcesToComponents,
 	transformSource, // Not used here but adds only 222 uncompressed (60 compressed) bytes of code
-	UnknownSources,
 } from './utils/entropy_source';
+export type { Source, SourcesToComponents, UnknownSources } from './utils/entropy_source';
 export { withIframe } from './utils/dom';
