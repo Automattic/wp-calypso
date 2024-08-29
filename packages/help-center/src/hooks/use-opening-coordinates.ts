b15a2ee5a8eb245ab6a1676705cef5c019e46b7d
Renan <renansscarvalho@gmail.com>
2024-08-29 17:13:17 +0200

    Align close button position

--- packages/help-center/src/hooks/use-opening-coordinates.ts
+++ packages/help-center/src/hooks/use-opening-coordinates.ts
@@ -2,6 +2,19 @@ import { useMobileBreakpoint } from '@automattic/viewport-react';
 import { useState, useEffect } from 'react';
 
 const AESTHETIC_OFFSET = 20;
+const HELP_CENTER_POSITION_MASTERBAR = 11;
+const HELP_CENTER_POSITION_EDITOR = 15;
+
+const getPosition = ( element: HTMLElement ) => {
+	if ( element.classList.contains( 'masterbar__item' ) ) {
+		return HELP_CENTER_POSITION_MASTERBAR;
+	}
+	if ( element.classList.contains( 'entry-point-button' ) ) {
+		return HELP_CENTER_POSITION_EDITOR;
+	}
+	// opening it from another location, example `my home`
+	return 0;
+};
 
 /**
  * This function calculates the position of the Help Center based on the last click event.
@@ -31,8 +44,17 @@ export const calculateOpeningPosition = ( element: HTMLElement ) => {
 
 	const { x, y, width, height } = element.getBoundingClientRect();
 
-	const buttonLeftEdge = x;
-	const buttonRightEdge = x + width;
+	const position = getPosition( element );
+
+	// handle RTL languages
+	const buttonLeftEdge = x - position;
+
+	const buttonRightEdge =
+		x +
+		width +
+		( element.classList.contains( 'masterbar__item' )
+			? HELP_CENTER_POSITION_MASTERBAR
+			: HELP_CENTER_POSITION_EDITOR );
 
 	const buttonTopEdge = y;
 	const buttonBottomEdge = y + height;
