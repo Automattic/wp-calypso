577b887f56422a4fae2c30cb52e14c8d0350acaa
Cris Busquets <cris.busquets@automattic.com>
2024-08-31 17:35:18 +0200

    Update ellipsis icon

--- client/components/ellipsis-menu/index.jsx
+++ client/components/ellipsis-menu/index.jsx
@@ -1,4 +1,4 @@
-import { Button, Gridicon } from '@automattic/components';
+import { Button } from '@automattic/components';
 import clsx from 'clsx';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
