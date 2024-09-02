4786b4ca36ae9b4b0852abcdb255bee2fb06a1d8
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-09-02 17:56:56 +0800

    Add handler for 'Remove member' action.

--- client/a8c-for-agencies/sections/team/hooks/use-handle-member-action.ts
+++ client/a8c-for-agencies/sections/team/hooks/use-handle-member-action.ts
@@ -1,6 +1,7 @@
 import { useTranslate } from 'i18n-calypso';
 import { useCallback } from 'react';
 import useCancelMemberInviteMutation from 'calypso/a8c-for-agencies/data/team/use-cancel-member-invite';
+import useRemoveMemberMutation from 'calypso/a8c-for-agencies/data/team/use-remove-member';
 import { useDispatch } from 'calypso/state';
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import { TeamMember } from '../types';
@@ -14,6 +15,8 @@ export default function useHandleMemberAction( { onRefetchList }: Props ) {
 	const dispatch = useDispatch();
 
 	const { mutate: cancelMemberInvite } = useCancelMemberInviteMutation();
+
+	const { mutate: removeMember } = useRemoveMemberMutation();
 
 	return useCallback(
 		( action: string, item: TeamMember ) => {
@@ -37,7 +40,28 @@ export default function useHandleMemberAction( { onRefetchList }: Props ) {
 					}
 				);
 			}
+
+			if ( action === 'delete-user' ) {
+				removeMember(
+					{ id: item.id },
+					{
+						onSuccess: () => {
+							dispatch(
+								successNotice( translate( 'The user has been successfully removed.' ), {
+									id: 'remove-user-success',
+									duration: 5000,
+								} )
+							);
+							onRefetchList?.();
+						},
+
+						onError: ( error ) => {
+							dispatch( errorNotice( error.message ) );
+						},
+					}
+				);
+			}
 		},
-		[ cancelMemberInvite, dispatch, onRefetchList, translate ]
+		[ cancelMemberInvite, dispatch, onRefetchList, removeMember, translate ]
 	);
 }
