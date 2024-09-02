3a13b7b6abbf17524eb535f3237510e27ebcd22e
Gergely Csécsey <gcsecsey@gmail.com>
2024-09-02 12:51:35 +0100

    add loading and error states

--- client/performance-profiler/components/message-display/index.tsx
+++ client/performance-profiler/components/message-display/index.tsx
@@ -1,17 +1,20 @@
+import { Gridicon } from '@automattic/components';
 import { Button, IconType } from '@wordpress/components';
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 import { Badge } from 'calypso/performance-profiler/components/badge';
 
 import './style.scss';
 
 type Props = {
-	title: string;
+	title?: string;
 	message: string | ReactNode;
-	ctaText: string;
-	ctaHref: string;
+	ctaText?: string;
+	ctaHref?: string;
 	secondaryMessage?: string;
 	displayBadge?: boolean;
 	ctaIcon?: string;
+	isErrorMessage?: boolean;
 };
 
 export const MessageDisplay = ( {
@@ -22,22 +25,28 @@ export const MessageDisplay = ( {
 	ctaHref,
 	secondaryMessage,
 	ctaIcon = '',
+	isErrorMessage = false,
 }: Props ) => {
 	return (
 		<div className="message-display">
 			<div className="l-block-wrapper">
 				<div className="message-wrapper">
 					{ displayBadge && <Badge /> }
-					<h1 className="title">{ title }</h1>
-					<p className="message">{ message }</p>
-					<Button
-						variant="primary"
-						icon={ ctaIcon as IconType }
-						className="cta-button"
-						href={ ctaHref }
-					>
-						{ ctaText }
-					</Button>
+					<div className={ clsx( 'main-message', { error: isErrorMessage } ) }>
+						{ isErrorMessage && <Gridicon icon="notice-outline" /> }
+						{ title && <h1 className="title">{ title }</h1> }
+						<p className="message">{ message }</p>
+						{ ctaText && ctaHref && (
+							<Button
+								variant="primary"
+								icon={ ctaIcon as IconType }
+								className="cta-button"
+								href={ ctaHref }
+							>
+								{ ctaText }
+							</Button>
+						) }
+					</div>
 					{ secondaryMessage && <p className="secondary-message">{ secondaryMessage }</p> }
 				</div>
 			</div>
--- client/performance-profiler/pages/weekly-report/index.tsx
+++ client/performance-profiler/pages/weekly-report/index.tsx
@@ -1,7 +1,8 @@
+import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
-import wp from 'calypso/lib/wp';
+import { useLeadquery } from 'calypso/data/site-profiler/use-lead-query';
 import { MessageDisplay } from 'calypso/performance-profiler/components/message-display';
 
 type WeeklyReportProps = {
@@ -9,45 +10,132 @@ type WeeklyReportProps = {
 	hash: string;
 };
 
+const LoaderText = styled.span`
+	display: flex;
+	align-items: center;
+	font-size: 16px;
+	font-weight: 400;
+	line-height: 24px;
+	position: relative;
+
+	&:before {
+		content: '';
+		display: inline-block;
+		border-radius: 50%;
+		margin-right: 10px;
+		content: '';
+		width: 16px;
+		height: 16px;
+		border: solid 2px #074ee8;
+		border-radius: 50%;
+		border-bottom-color: transparent;
+		-webkit-transition: all 0.5s ease-in;
+		-webkit-animation-name: rotate;
+		-webkit-animation-duration: 1s;
+		-webkit-animation-iteration-count: infinite;
+		-webkit-animation-timing-function: linear;
+
+		transition: all 0.5s ease-in;
+		animation-name: rotate;
+		animation-duration: 1s;
+		animation-iteration-count: infinite;
+		animation-timing-function: linear;
+	}
+
+	@keyframes rotate {
+		from {
+			transform: rotate( 0deg );
+		}
+		to {
+			transform: rotate( 360deg );
+		}
+	}
+
+	@-webkit-keyframes rotate {
+		from {
+			-webkit-transform: rotate( 0deg );
+		}
+		to {
+			-webkit-transform: rotate( 360deg );
+		}
+	}
+`;
+
+const ErrorSecondLine = styled.span`
+	color: var( --studio-red-5 );
+	font-weight: 400;
+	line-height: 20px;
+`;
+
 export const WeeklyReport = ( props: WeeklyReportProps ) => {
 	const translate = useTranslate();
 	const { url, hash } = props;
 
 	const siteUrl = new URL( url );
 
-	useEffect( () => {
-		const postLead = async () => {
-			await wp.req.post(
-				{
-					path: '/site-profiler/lead',
-					apiNamespace: 'wpcom/v2',
-				},
-				{
-					url,
-					hash,
-				}
-			);
-		};
+	const { mutate, isLoading, isError, isSuccess } = useLeadquery( url, hash );
 
-		postLead();
-	}, [ hash, url ] );
+	useEffect( () => {
+		mutate();
+	}, [ mutate ] );
+
+	const secondaryMessage = translate(
+		'You can stop receiving performance reports at any time by clicking the Unsubscribe link in the email footer.'
+	);
 
 	return (
 		<div className="peformance-profiler-weekly-report-container">
 			<DocumentHead title={ translate( 'Speed Test weekly reports' ) } />
-			<MessageDisplay
-				title={ translate( 'You’re all set!' ) }
-				message={ translate(
-					"We'll send you a weekly performance report for {{strong}}%s{{/strong}} so you can keep an eye on your site's speed. The first email is on it's way now.",
-					{ args: [ siteUrl.host ], components: { strong: <strong /> } }
-				) }
-				ctaText={ translate( '← Back to speed test' ) }
-				ctaHref="/speed-test"
-				ctaIcon="arrow-left"
-				secondaryMessage={ translate(
-					'You can stop receiving performance reports at any time by clicking the Unsubscribe link in the email footer.'
-				) }
-			/>
+
+			{ isLoading && (
+				<MessageDisplay
+					displayBadge
+					message={
+						<LoaderText>
+							{ translate( 'Enabling email reports for %s', {
+								args: [ siteUrl.host ],
+							} ) }
+						</LoaderText>
+					}
+					secondaryMessage={ secondaryMessage }
+				/>
+			) }
+			{ isError && (
+				<MessageDisplay
+					isErrorMessage
+					displayBadge
+					message={
+						<>
+							{ translate( 'Email reports could not be enabled for %s', {
+								args: [ siteUrl.host ],
+							} ) }
+							<br />
+							<ErrorSecondLine>
+								{ translate(
+									'Please try again or contact support if you continue to experience problems.'
+								) }
+							</ErrorSecondLine>
+						</>
+					}
+					ctaText={ translate( 'Enable email reports' ) }
+					ctaHref="/speed-test"
+					secondaryMessage={ secondaryMessage }
+				/>
+			) }
+			{ isSuccess && (
+				<MessageDisplay
+					displayBadge
+					title={ translate( 'You’re all set!' ) }
+					message={ translate(
+						"We'll send you a weekly performance report for {{strong}}%s{{/strong}} so you can keep an eye on your site's speed. The first email is on it's way now.",
+						{ args: [ siteUrl.host ], components: { strong: <strong /> } }
+					) }
+					ctaText={ translate( '← Back to speed test' ) }
+					ctaHref="/speed-test"
+					ctaIcon="arrow-left"
+					secondaryMessage={ secondaryMessage }
+				/>
+			) }
 		</div>
 	);
 };
