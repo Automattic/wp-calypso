bdd843c6d5e600c721284048e027562d7cb9de93
Gergely Csécsey <gcsecsey@gmail.com>
2024-09-03 16:19:35 +0100

    update email banner styling

--- client/performance-profiler/components/newsletter-banner.tsx
+++ client/performance-profiler/components/newsletter-banner.tsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
 import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'calypso/state';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 
 const Container = styled.div`
-	background-color: #f7f8fe;
+	background-color: var( --studio-gray-100, #101517 );
+	border-radius: 6px;
 	display: flex;
 	flex-direction: row;
 	align-items: center;
@@ -14,16 +17,26 @@ const Container = styled.div`
 	& > * {
 		margin: 24px;
 	}
+
+	@media ( max-width: 600px ) {
+		flex-direction: column;
+		gap: 6px;
+
+		& > button {
+			margin-top: 0;
+		}
+	}
 `;
 
 const Heading = styled.div`
 	font-weight: 500;
 	line-height: 24px;
 	text-align: left;
+	color: var( --studio-white );
 `;
 
 const Body = styled.div`
-	color: var( --studio-gray-70 );
+	color: var( --studio-gray-20 );
 `;
 
 const BlueberryButton = styled( Button )`
@@ -31,25 +44,40 @@ const BlueberryButton = styled( Button )`
 	&& {
 		background: #3858e9;
 		border-color: #3858e9;
+
+		&:hover:not( :disabled ),
+		&:active:not( :disabled ),
+		&:focus:not( :disabled ) {
+			background-color: darken( #3858e9, 10% );
+			border-color: darken( #3858e9, 10% );
+			box-shadow: none;
+		}
 	}
 `;
 
 export const NewsletterBanner = ( { link }: { link: string } ) => {
 	const translate = useTranslate();
+	const isLoggedIn = useSelector( isUserLoggedIn );
 
 	return (
 		<Container>
 			<div>
-				<Heading>{ translate( 'Sign up for weekly performance reports—it’s free!' ) }</Heading>
+				<Heading>
+					{ translate( 'Get notified about changes to your site’s performance—it’s free!' ) }
+				</Heading>
 				<Body>
 					{ translate(
-						'Monitor your site’s key performance metrics with a free report delivered to your inbox each week.'
+						"Monitor your site's key performance metrics with a free report delivered to your inbox each week."
 					) }
 				</Body>
-				<Body>{ translate( 'All you need is a free WordPress.com account to get started.' ) }</Body>
+				{ ! isLoggedIn && (
+					<Body>
+						{ translate( 'All you need is a free WordPress.com account to get started.' ) }
+					</Body>
+				) }
 			</div>
 			<BlueberryButton variant="primary" href={ link }>
-				{ translate( 'Sign up for email reports' ) }
+				{ translate( 'Enable email alerts' ) }
 			</BlueberryButton>
 		</Container>
 	);
