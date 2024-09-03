b786252170c099abb5054e48db1b0029f97cb212
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2024-09-03 14:53:12 +0900

    Extract content UI to new component

--- client/my-sites/stats/feedback/index.tsx
+++ client/my-sites/stats/feedback/index.tsx
@@ -6,33 +6,16 @@ import FeedbackModal from './modal';
 import './style.scss';
 
 function StatsFeedbackCard() {
-	const translate = useTranslate();
 	const [ isOpen, setIsOpen ] = useState( false );
 
-	const ctaText = translate( 'How do you rate your overall experience with Jetpack Stats?' );
-	const primaryButtonText = translate( 'Love it? Leave a review' );
-	const secondaryButtonText = translate( 'Not a fan? Help us improve' );
-
-	const handleClickWriteReview = () => {};
-
-	const handleClickSendFeedback = () => {
-		setIsOpen( true );
-	};
+	// const handleClickSendFeedback = () => {
+	// 	setIsOpen( true );
+	// };
 
 	return (
 		<div className="stats-feedback-container">
 			<div className="stats-feedback-card">
-				<div className="stats-feedback-card__cta">{ ctaText }</div>
-				<div className="stats-feedback-card__actions">
-					<Button variant="secondary" onClick={ handleClickWriteReview }>
-						<span className="stats-feedback-card__emoji">😍</span>
-						{ primaryButtonText }
-					</Button>
-					<Button variant="secondary" onClick={ handleClickSendFeedback }>
-						<span className="stats-feedback-card__emoji">😠</span>
-						{ secondaryButtonText }
-					</Button>
-				</div>
+				<FeedbackContent />
 				<FeedbackModal isOpen={ isOpen } onClose={ () => setIsOpen( false ) } />
 			</div>
 			<FeedbackPanel />
@@ -45,6 +28,34 @@ function FeedbackPanel() {
 		<div className="stats-feedback-panel">
 			<p>floating panel content here</p>
 			<p>buttons here</p>
+		</div>
+	);
+}
+
+function FeedbackContent() {
+	const translate = useTranslate();
+
+	const ctaText = translate( 'How do you rate your overall experience with Jetpack Stats?' );
+	const primaryButtonText = translate( 'Love it? Leave a review' );
+	const secondaryButtonText = translate( 'Not a fan? Help us improve' );
+
+	const handleButtonClick = () => {
+		console.log( 'button clicked' );
+	};
+
+	return (
+		<div className="stats-feedback-card__content">
+			<div className="stats-feedback-card__cta">{ ctaText }</div>
+			<div className="stats-feedback-card__actions">
+				<Button variant="secondary" onClick={ handleButtonClick }>
+					<span className="stats-feedback-card__emoji">😍</span>
+					{ primaryButtonText }
+				</Button>
+				<Button variant="secondary" onClick={ handleButtonClick }>
+					<span className="stats-feedback-card__emoji">😠</span>
+					{ secondaryButtonText }
+				</Button>
+			</div>
 		</div>
 	);
 }
