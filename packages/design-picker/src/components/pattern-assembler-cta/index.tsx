b1bab3ddaae59612eaf27b4eb0cf1b15ef575073
Eoin Gallagher <eoin@automattic.com>
2024-09-03 21:06:04 +0100

    Site setup: Update image in mobile design choice step (#94149)
    * Site setup: Update image in mobile design choice step
    
    * fix test

--- packages/design-picker/src/components/pattern-assembler-cta/index.tsx
+++ packages/design-picker/src/components/pattern-assembler-cta/index.tsx
@@ -2,7 +2,7 @@ import { Button } from '@automattic/components';
 import { useViewportMatch } from '@wordpress/compose';
 import { useTranslate } from 'i18n-calypso';
 import { ReactNode } from 'react';
-import assemblerIllustrationImage from '../assets/images/assembler-illustration.png';
+import assemblerIllustrationV2Image from '../assets/images/assembler-illustration-v2.png';
 import './style.scss';
 
 type PatternAssemblerCtaData = {
@@ -49,7 +49,7 @@ const PatternAssemblerCta = ( { onButtonClick }: PatternAssemblerCtaProps ) => {
 			<div className="pattern-assembler-cta__image-wrapper">
 				<img
 					className="pattern-assembler-cta__image"
-					src={ assemblerIllustrationImage }
+					src={ assemblerIllustrationV2Image }
 					alt="Pattern Assembler"
 				/>
 			</div>
