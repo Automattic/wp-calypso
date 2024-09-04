13b36dde31e4879030f57cd7e27f44bcd6131f19
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-09-04 13:11:17 +0200

    Extend useExitFlow hook

--- client/landing/stepper/hooks/use-exit-flow.ts
+++ client/landing/stepper/hooks/use-exit-flow.ts
@@ -1,11 +1,17 @@
 import { useDispatch } from '@wordpress/data';
 import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 
+type UseExitFlowParams =
+	| { processing: true; navigate: ( to: string ) => void }
+	| { processing?: false; navigate?: ( to: string ) => void }
+	| undefined;
+
 /**
  * The useExitFlow hook provides a function to handle exiting a flow
- * by setting a pending action that redirects the browser to a specified URL.
+ * by setting a pending action that redirects the browser to a specified URL,
+ * with an optional navigation step if processing is required.
  */
-export const useExitFlow = () => {
+export const useExitFlow = ( params?: UseExitFlowParams ) => {
 	const { setPendingAction } = useDispatch( ONBOARD_STORE );
 
 	const exitFlow = ( to: string ) => {
@@ -14,6 +20,10 @@ export const useExitFlow = () => {
 				window.location.assign( to );
 			} );
 		} );
+
+		if ( params?.processing && params?.navigate ) {
+			return params.navigate( 'processing' );
+		}
 	};
 
 	return {
