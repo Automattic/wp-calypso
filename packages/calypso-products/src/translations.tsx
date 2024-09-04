0d5df5f80bf8223f3d9f947e585cf976b2574f53
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2024-09-04 10:38:28 -0300

    Update request limit description

--- packages/calypso-products/src/translations.tsx
+++ packages/calypso-products/src/translations.tsx
@@ -1544,7 +1544,10 @@ export const getJetpackProductsWhatIsIncluded = (): Record< string, Array< Trans
 	];
 
 	return {
-		[ PRODUCT_JETPACK_AI_MONTHLY ]: aiAssistantIncludesInfo,
+		[ PRODUCT_JETPACK_AI_MONTHLY ]: [
+			translate( 'High request capacity' ),
+			...aiAssistantIncludesInfo,
+		],
 		[ PRODUCT_JETPACK_AI_MONTHLY_100 ]: [
 			translate( '100 monthly requests (upgradeable)' ),
 			...aiAssistantIncludesInfo,
@@ -1565,7 +1568,10 @@ export const getJetpackProductsWhatIsIncluded = (): Record< string, Array< Trans
 			translate( '1000 monthly requests (upgradeable)' ),
 			...aiAssistantIncludesInfo,
 		],
-		[ PRODUCT_JETPACK_AI_YEARLY ]: aiAssistantIncludesInfo,
+		[ PRODUCT_JETPACK_AI_YEARLY ]: [
+			translate( 'High request capacity' ),
+			...aiAssistantIncludesInfo,
+		],
 		[ PRODUCT_JETPACK_AI_YEARLY_100 ]: [
 			translate( '100 monthly requests (upgradeable)' ),
 			...aiAssistantIncludesInfo,
@@ -1586,7 +1592,10 @@ export const getJetpackProductsWhatIsIncluded = (): Record< string, Array< Trans
 			translate( '1000 monthly requests (upgradeable)' ),
 			...aiAssistantIncludesInfo,
 		],
-		[ PRODUCT_JETPACK_AI_BI_YEARLY ]: aiAssistantIncludesInfo,
+		[ PRODUCT_JETPACK_AI_BI_YEARLY ]: [
+			translate( 'High request capacity' ),
+			...aiAssistantIncludesInfo,
+		],
 		[ PRODUCT_JETPACK_AI_BI_YEARLY_100 ]: [
 			translate( '100 monthly requests (upgradeable)' ),
 			...aiAssistantIncludesInfo,
