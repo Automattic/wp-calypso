9dbae6c9495ff9c54f2e08af905dedeca32e10a8
Enej Bajgoric <enej.bajgoric@automattic.com>
2024-09-03 17:28:51 -0700

    Paid Newsletter: Improve UI consistancy
    This PR fixes a number of issues with the current UI.

--- client/my-sites/importer/newsletter/content-upload/import-summary-modal.tsx
+++ client/my-sites/importer/newsletter/content-upload/import-summary-modal.tsx
@@ -8,6 +8,68 @@ interface ImportSummaryModalProps {
 	authorsNumber?: number;
 }
 
+function getContent( postsNumber: number, pagesNumber: number, attachmentsNumber: number ) {
+	if ( postsNumber > 0 && pagesNumber > 0 && attachmentsNumber > 0 ) {
+		return (
+			<p>
+				We’ve found <strong>{ postsNumber } posts</strong>, <strong>{ pagesNumber } pages</strong>
+				and <strong>{ attachmentsNumber } media</strong> to import.
+			</p>
+		);
+	}
+
+	if ( postsNumber > 0 && pagesNumber > 0 ) {
+		return (
+			<p>
+				We’ve found <strong>{ postsNumber } posts</strong> and{ ' ' }
+				<strong>{ pagesNumber } pages</strong> to import.
+			</p>
+		);
+	}
+
+	if ( postsNumber > 0 && attachmentsNumber > 0 ) {
+		return (
+			<p>
+				We’ve found <strong>{ postsNumber } posts</strong> and{ ' ' }
+				<strong>{ attachmentsNumber } media</strong> to import.
+			</p>
+		);
+	}
+
+	if ( pagesNumber > 0 && attachmentsNumber > 0 ) {
+		return (
+			<p>
+				We’ve found <strong>{ pagesNumber } pages</strong> and{ ' ' }
+				<strong>{ attachmentsNumber } media</strong> to import.
+			</p>
+		);
+	}
+
+	if ( postsNumber > 0 ) {
+		return (
+			<p>
+				We’ve found <strong>{ postsNumber } posts</strong> to import.
+			</p>
+		);
+	}
+
+	if ( pagesNumber > 0 ) {
+		return (
+			<p>
+				We’ve found <strong>{ pagesNumber } pages</strong> to import.
+			</p>
+		);
+	}
+
+	if ( attachmentsNumber > 0 ) {
+		return (
+			<p>
+				We’ve found <strong>{ attachmentsNumber } media</strong> to import.
+			</p>
+		);
+	}
+}
+
 export default function ImportSummaryModal( {
 	onRequestClose,
 	postsNumber,
@@ -17,9 +79,8 @@ export default function ImportSummaryModal( {
 }: ImportSummaryModalProps ) {
 	return (
 		<Modal title="All set!" isDismissible={ false } onRequestClose={ onRequestClose } size="medium">
+			{ getContent( postsNumber, pagesNumber, attachmentsNumber ) }
 			<p>
-				We’ve found <strong>{ postsNumber } posts</strong>, <strong>{ pagesNumber } pages</strong>{ ' ' }
-				and <strong>{ attachmentsNumber } media</strong> to import.
 				{ authorsNumber && (
 					<>
 						<br />
