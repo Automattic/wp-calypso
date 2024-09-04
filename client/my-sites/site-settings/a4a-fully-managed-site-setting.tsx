780dad772903f7f3fa2e1e0a2e7d0f63fcd4fed6
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2024-09-04 09:04:46 +0200

    Remove `isDevSite` check

--- client/my-sites/site-settings/a4a-fully-managed-site-setting.tsx
+++ client/my-sites/site-settings/a4a-fully-managed-site-setting.tsx
@@ -8,12 +8,11 @@ type Props = {
 
 export function A4AFullyManagedSiteSetting( { site }: Props ) {
 	const devSitesEnabled = config.isEnabled( 'a4a-dev-sites' );
-	const isDevSite = site.is_a4a_dev_site;
 	const isAtomicSite = site.is_wpcom_atomic;
 
 	const { data: agencySite } = useFetchAgencyFromBlog( site?.ID, { enabled: !! site?.ID } );
 
-	const shouldShowToggle = devSitesEnabled && agencySite && isAtomicSite && ! isDevSite;
+	const shouldShowToggle = devSitesEnabled && agencySite && isAtomicSite;
 
 	if ( ! shouldShowToggle ) {
 		return null;
