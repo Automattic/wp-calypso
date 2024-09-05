7602800ccf7f09fc67897f69a79bbfad7d4e30cc
Addison-Stavlo <stavz01@gmail.com>
2024-09-05 10:23:56 -0400

    fix return value for no translation

--- packages/components/src/highlight-cards/highlight-cards-heading.tsx
+++ packages/components/src/highlight-cards/highlight-cards-heading.tsx
@@ -4,15 +4,15 @@ import { useTranslate } from 'i18n-calypso';
 export default function HighlightCardsHeading( { children }: { children: React.ReactNode } ) {
 	const translate = useTranslate();
 	const hasEnTranslation = useHasEnTranslation();
-	if ( ! hasEnTranslation( 'Updates every 30 minutes' ) ) {
-		return null;
-	}
+	const hasTranslation = hasEnTranslation( 'Updates every 30 minutes' );
 	return (
 		<div className="highlight-cards-heading__wrapper">
 			<h3 className="highlight-cards-heading">{ children }</h3>
-			<div className="highlight-cards-heading__update-frequency">
-				<span>{ translate( 'Updates every 30 minutes' ) }</span>
-			</div>
+			{ hasTranslation && (
+				<div className="highlight-cards-heading__update-frequency">
+					<span>{ translate( 'Updates every 30 minutes' ) }</span>
+				</div>
+			) }
 		</div>
 	);
 }
