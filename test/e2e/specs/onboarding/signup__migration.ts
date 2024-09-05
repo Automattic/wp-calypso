0d124784243c5d14c1266bde3567e02f6c848313
Gabriel Caires <gabriel.caires@automattic.com>
2024-09-05 15:37:49 +0100

    WIP

---
+++ test/e2e/specs/onboarding/signup__migration.ts
@@ -0,0 +1,124 @@
+/**
+ * @group calypso-release
+ */
+import {
+	DataHelper,
+	ElementHelper,
+	UserSignupPage,
+	BrowserManager,
+	NewUserResponse,
+	RestAPIClient,
+	DomainSearchComponent,
+	EditorPage,
+	CartCheckoutPage,
+} from '@automattic/calypso-e2e';
+import { Page, Browser } from 'playwright';
+import { apiCloseAccount } from '../shared';
+
+declare const browser: Browser;
+
+describe( 'Signup: Tailored Start Writing Flow', () => {
+	const testUser = DataHelper.getNewTestUser( {
+		usernamePrefix: 'start_writing',
+	} );
+	let page: Page;
+	let newUserDetails: NewUserResponse;
+
+	beforeAll( async () => {
+		page = await browser.newPage();
+		await BrowserManager.setStoreCookie( page, { currency: 'EUR' } );
+	} );
+
+	it( 'Navigate to /setup/start-writing', async function () {
+		await page.goto( DataHelper.getCalypsoURL( '/setup/migration' ) );
+	} );
+
+	it( 'Sign up with email', async function () {
+		const userSignupPage = new UserSignupPage( page );
+		newUserDetails = await userSignupPage.signupSocialFirstWithEmail( testUser.email );
+	} );
+
+	it( 'Publish first post', async function () {
+		await page.getByRole( 'button', { name: 'WordPress' } ).click();
+		await page.getByRole( 'button', { name: 'Get the plan and migrate' } ).click();
+
+		const cartCheckoutPage = new CartCheckoutPage( page );
+		const paymentDetails = DataHelper.getTestPaymentDetails();
+		await cartCheckoutPage.enterBillingDetails( paymentDetails );
+		await cartCheckoutPage.enterPaymentDetails( paymentDetails );
+
+		await cartCheckoutPage.purchase( { timeout: 90 * 1000 } );
+
+		await page.pause();
+
+		const editorPage = new EditorPage( page );
+		await editorPage.waitUntilLoaded();
+		await page.getByLabel( 'Close', { exact: true } ).click();
+		await editorPage.enterTitle( 'my first post title' );
+		await editorPage.publish();
+		await page.getByText( "Your blog's almost ready!" ).waitFor();
+	} );
+
+	it( 'Add blog name and description', async function () {
+		await page.getByText( 'Name your blog' ).click();
+
+		await page.locator( 'input[name="setup-form-input-name"]' ).fill( 'The Land of Foo' );
+		await page
+			.locator( 'textarea[name="setup-form-input-description"]' )
+			.fill( 'A blog about Foo' );
+
+		await page.locator( 'button.setup-form__submit' ).click();
+	} );
+
+	it( 'Ensure domain search is working', async function () {
+		await page.getByText( 'Choose a domain' ).click();
+		const domainSearchComponent = new DomainSearchComponent( page );
+		await domainSearchComponent.search( 'test' );
+		await page
+			.locator( '.domain-search-results' )
+			.getByRole( 'button', { name: 'Select' } )
+			.first()
+			.waitFor();
+	} );
+
+	it( 'Skip the domain selection step', async function () {
+		await page.getByText( 'Decide later' ).click();
+	} );
+
+	it( 'Select WordPress.com Free plan', async function () {
+		await page.getByText( 'Choose a plan' ).click();
+		// See https://github.com/Automattic/wp-calypso/pull/84468
+		await ElementHelper.reloadAndRetry( page, async function () {
+			await page.getByRole( 'button', { name: 'Start with Free' } ).click();
+		} );
+	} );
+
+	it( 'Launch the blog', async function () {
+		await page.getByRole( 'button', { name: 'Launch your blog' } ).click();
+	} );
+
+	it( 'Ensure "Connect to social" navigates to Marketing page', async function () {
+		await page.getByRole( 'button', { name: 'Connect to social' } ).click();
+		await page.getByText( 'Marketing and Integrations' ).waitFor();
+	} );
+
+	afterAll( async function () {
+		if ( ! newUserDetails ) {
+			return;
+		}
+
+		const restAPIClient = new RestAPIClient(
+			{
+				username: testUser.username,
+				password: testUser.password,
+			},
+			newUserDetails.body.bearer_token
+		);
+
+		await apiCloseAccount( restAPIClient, {
+			userID: newUserDetails.body.user_id,
+			username: newUserDetails.body.username,
+			email: testUser.email,
+		} );
+	} );
+} );
