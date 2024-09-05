afb7d79adbb0c1c2ecdbba0183dbc70f3b6d1215
Addison-Stavlo <stavz01@gmail.com>
2024-09-05 10:14:32 -0400

    add translation check

--- packages/components/src/highlight-cards/highlight-cards-heading.tsx
+++ packages/components/src/highlight-cards/highlight-cards-heading.tsx
@@ -1,7 +1,12 @@
+import { useHasEnTranslation } from '@automattic/i18n-utils';
 import { useTranslate } from 'i18n-calypso';
 
 export default function HighlightCardsHeading( { children }: { children: React.ReactNode } ) {
 	const translate = useTranslate();
+	const hasEnTranslation = useHasEnTranslation();
+	if ( ! hasEnTranslation( 'Updates every 30 minutes' ) ) {
+		return null;
+	}
 	return (
 		<div className="highlight-cards-heading__wrapper">
 			<h3 className="highlight-cards-heading">{ children }</h3>
