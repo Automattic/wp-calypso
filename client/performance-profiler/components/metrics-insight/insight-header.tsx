f2e125b587b1bab75ff5f43a08ca4a2f0bf79433
Wesley Berrêdo <Wberredo@gmail.com>
2024-09-06 11:55:32 -0300

    Performance Profiler: Update Recommendations responsiveness (#94263)
    * Make the section not overflow horizontally
    
    * Update wrap of the recommendation tip
    
    * add horiontal scroll to tables
    
    * fix impact badge responsiveness
    
    ---------
    
    Co-authored-by: Gergely Csécsey <gcsecsey@gmail.com>

--- client/performance-profiler/components/metrics-insight/insight-header.tsx
+++ client/performance-profiler/components/metrics-insight/insight-header.tsx
@@ -1,4 +1,4 @@
-import { isMobile } from '@automattic/viewport';
+import { useDesktopBreakpoint } from '@automattic/viewport-react';
 import clsx from 'clsx';
 import Markdown from 'react-markdown';
 import { PerformanceMetricsItemQueryResponse } from 'calypso/data/site-profiler/types';
@@ -7,8 +7,8 @@ interface InsightHeaderProps {
 	data: PerformanceMetricsItemQueryResponse;
 	index: number;
 }
-
 export const InsightHeader: React.FC< InsightHeaderProps > = ( props ) => {
+	const isMobile = ! useDesktopBreakpoint();
 	const { data, index } = props;
 	const title = data.title ?? '';
 	const value = data.displayValue ?? '';
@@ -22,36 +22,36 @@ export const InsightHeader: React.FC< InsightHeaderProps > = ( props ) => {
 			return null;
 		}
 
-		return (
-			<span className={ clsx( 'impact fail', { 'is-mobile': isMobile() } ) }>High Impact</span>
-		);
+		return <span className={ clsx( 'impact fail', { 'is-mobile': isMobile } ) }>High Impact</span>;
 	};
 
 	return (
-		<>
+		<div className="insight-header-container">
 			<span className={ clsx( 'counter', { [ type ]: true } ) }>{ index + 1 }</span>
-			<Markdown
-				components={ {
-					p( props ) {
-						return <p className="title-description">{ props.children }</p>;
-					},
-					code( props ) {
-						return <span className="header-code">{ props.children }</span>;
-					},
-				} }
-			>
-				{ title }
-			</Markdown>
-			{ value && isMobile() && (
-				<span className={ clsx( 'value is-mobile', { [ type ]: true } ) }> { value }</span>
-			) }
-			{ value && ! isMobile() && (
-				<span>
-					&nbsp;&minus;&nbsp;
-					<span className={ clsx( 'value', { [ type ]: true } ) }> { value }</span>
-				</span>
-			) }
-			{ renderBadge() }
-		</>
+			<div>
+				<Markdown
+					components={ {
+						p( props ) {
+							return <p className="title-description">{ props.children }</p>;
+						},
+						code( props ) {
+							return <span className="value">{ props.children }</span>;
+						},
+					} }
+				>
+					{ title }
+				</Markdown>
+				{ value && isMobile && (
+					<span className={ clsx( 'value is-mobile', { [ type ]: true } ) }> { value }</span>
+				) }
+				{ value && ! isMobile && (
+					<span>
+						&nbsp;&minus;&nbsp;
+						<span className={ clsx( 'value', { [ type ]: true } ) }> { value }</span>
+					</span>
+				) }
+				{ renderBadge() }
+			</div>
+		</div>
 	);
 };
