662e1a6569eb06aafa20dbdf1a698ec052ad1dbf
Paulo Cruz <paulo.cruz@automattic.com>
2024-09-06 12:59:46 +0200

    Add notice when missig tipalti referral data

---
+++ client/a8c-for-agencies/sections/referrals/common/missing-payment-settings-notice/index.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+import LayoutBanner from 'calypso/a8c-for-agencies/components/layout/banner';
+
+type Props = {
+	onClose: () => void;
+};
+
+export const MissingPaymentSettingsNotice = ( { onClose }: Props ) => {
+	const translate = useTranslate();
+	return (
+		<LayoutBanner
+			level="warning"
+			title={ translate( 'Your payment settings require action' ) }
+			onClose={ onClose }
+		>
+			<div>
+				{ translate( 'Please confirm your details before referring products to your clients.' ) }
+			</div>
+			<Button
+				className="referrals-overview__notice-button is-dark"
+				href="/referrals/payment-settings"
+			>
+				{ translate( 'Go to payment settings' ) }
+			</Button>
+		</LayoutBanner>
+	);
+};
+
+export default MissingPaymentSettingsNotice;
