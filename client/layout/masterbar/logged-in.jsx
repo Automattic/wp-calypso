8891bed55f43eff4333f90d05cd982ab3b7f0719
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-09-09 11:57:34 +0700

    Site Level User Profile: clean up feature flags

--- client/layout/masterbar/logged-in.jsx
+++ client/layout/masterbar/logged-in.jsx
@@ -500,11 +500,8 @@ class MasterbarLoggedIn extends Component {
 	}
 
 	renderProfileMenu() {
-		const { translate, user, siteUrl, isClassicView } = this.props;
-		const editProfileLink =
-			config.isEnabled( 'layout/site-level-user-profile' ) || isClassicView
-				? siteUrl + '/wp-admin/profile.php'
-				: '/me';
+		const { translate, user, siteAdminUrl } = this.props;
+		const editProfileLink = `${ siteAdminUrl }profile.php`;
 		const profileActions = [
 			{
 				label: (
