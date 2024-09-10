15f63a8992b896098895b126a2863c7d50097122
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-09-10 19:23:03 +0800

    Update Send team member invite mutation hook to supply username instead of email.

--- client/a8c-for-agencies/data/team/use-send-team-member-invite.ts
+++ client/a8c-for-agencies/data/team/use-send-team-member-invite.ts
@@ -30,7 +30,7 @@ function mutationSendTeamMemberInvite(
 		path: `/agency/${ agencyId }/user-invites`,
 		method: 'POST',
 		body: {
-			email: inviteDetails.username,
+			username: inviteDetails.username,
 			message: inviteDetails.message,
 		},
 	} );
