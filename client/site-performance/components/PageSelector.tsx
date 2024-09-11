0bf72bc535a9caca1062010bf61e6128b5dc72af
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-09-11 16:24:53 -0300

    Add PageSelector to site performance page

---
+++ client/site-performance/components/PageSelector.tsx
@@ -0,0 +1,80 @@
+import page from '@automattic/calypso-router';
+import { SearchableDropdown } from '@automattic/components';
+import { useDebouncedInput } from '@wordpress/compose';
+import QueryPosts from 'calypso/components/data/query-posts';
+import { useSelector } from 'calypso/state';
+import { getPostsForQueryIgnoringPage } from 'calypso/state/posts/selectors';
+import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
+import { getSelectedSite } from 'calypso/state/ui/selectors';
+
+interface Page {
+	slug: string;
+	URL: string;
+	title: string;
+}
+
+const PER_PAGE = 10;
+
+export const PageSelector = () => {
+	const queryParams = useSelector( getCurrentQueryArguments );
+	const site = useSelector( getSelectedSite );
+
+	const [ , setPageQuery, pageQuery ] = useDebouncedInput();
+	const query = { type: 'page', number: PER_PAGE, search: pageQuery, page: 1 };
+	const pages = useSelector( ( state ): Page[] | null =>
+		getPostsForQueryIgnoringPage( state, site?.ID, query )
+	);
+
+	const options =
+		pages?.map( ( page ) => {
+			let url = page.URL.replace( site?.URL ?? '', '' );
+			url = url.length > 1 ? url.replace( /\/$/, '' ) : url;
+
+			return {
+				URL: url,
+				label: page.title,
+				value: page.slug,
+			};
+		} ) ?? [];
+
+	return (
+		<>
+			<QueryPosts postId={ null } siteId={ site?.ID } query={ query } />
+			<SearchableDropdown
+				onFilterValueChange={ setPageQuery }
+				options={ options }
+				value={ queryParams?.page_slug?.toString() ?? null }
+				onChange={ ( slug ) => {
+					const url = new URL( window.location.href );
+
+					if ( slug ) {
+						url.searchParams.set( 'page_slug', slug );
+					} else {
+						url.searchParams.delete( 'page_slug' );
+					}
+
+					page.replace( url.pathname + url.search );
+				} }
+				css={ {
+					maxWidth: '240px',
+					'.components-form-token-field__suggestions-list': { maxHeight: 'auto !important' },
+					'.components-form-token-field__suggestions-list li': { padding: '0 !important' },
+				} }
+				__experimentalRenderItem={ ( { item } ) => (
+					<div
+						aria-label={ item.label }
+						css={ {
+							display: 'flex',
+							flexDirection: 'column',
+							paddingInline: '16px',
+							paddingBlock: '8px',
+						} }
+					>
+						<span>{ item.label }</span>
+						<span>{ item.URL }</span>
+					</div>
+				) }
+			/>
+		</>
+	);
+};
