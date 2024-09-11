2b8b163193d1a4a8df329b9a3ac5b1921784bad7
Marcelo Vega <marcelo.vega@automattic.com>
2024-09-10 21:10:02 -0300

    Change Gridicon by Icon. Refactor, move translate and onClick to a constant

--- client/components/jetpack/daily-backup-status/status-card/backup-last-failed.tsx
+++ client/components/jetpack/daily-backup-status/status-card/backup-last-failed.tsx
@@ -1,7 +1,9 @@
-import { Gridicon } from '@automattic/components';
 import { ExternalLink } from '@wordpress/components';
+import { useCallback } from '@wordpress/element';
+import { Icon, info } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent } from 'react';
+import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions/record';
 import type { Moment } from 'moment';
 
@@ -14,31 +16,31 @@ type Props = {
 
 export const BackupLastFailed: FunctionComponent< Props > = () => {
 	const translate = useTranslate();
+	const dispatch = useDispatch();
 	const learnMoreLink = 'https://jetpack.com/support/backup/troubleshooting-jetpack-backup/';
+
+	const onLearnMoreClick = useCallback( () => {
+		dispatch( recordTracksEvent( 'calypso_jetpack_backup_last_failed_learn_more_click' ) );
+	}, [ dispatch ] );
+
+	const translatedText = translate(
+		"We encountered some issues with the latest backup, but don't worry! " +
+			"We're using the most recent backup available. " +
+			'{{ExternalLink}}Learn more.{{/ExternalLink}}',
+		{
+			components: {
+				ExternalLink: (
+					<ExternalLink href={ learnMoreLink } children={ null } onClick={ onLearnMoreClick } />
+				),
+			},
+		}
+	);
 
 	return (
 		<div className="backup-last-failed__wrapper">
 			<span className="backup-last-failed__info">
-				<Gridicon icon="notice-outline" />
-				{ translate(
-					"We encountered some issues with the latest backup, but don't worry! " +
-						"We're using the most recent backup available. " +
-						'{{ExternalLink}}Learn more.{{/ExternalLink}}',
-					{
-						components: {
-							ExternalLink: (
-								<ExternalLink
-									href={ learnMoreLink }
-									children={ null }
-									rel="noopener noreferrer"
-									onClick={ () =>
-										recordTracksEvent( 'calypso_jetpack_backup_last_faileds_learn_more_click' )
-									}
-								/>
-							),
-						},
-					}
-				) }
+				<Icon icon={ info } size={ 26 } className="icon" />
+				{ translatedText }
 			</span>
 		</div>
 	);
--- client/components/jetpack/daily-backup-status/status-card/backup-successful.jsx
+++ client/components/jetpack/daily-backup-status/status-card/backup-successful.jsx
@@ -45,8 +45,6 @@ const BackupSuccessful = ( {
 	const getDisplayDate = useGetDisplayDate();
 	const displayDate = getDisplayDate( backup.activityTs );
 	const displayDateNoLatest = getDisplayDate( backup.activityTs, false );
-	// TODO To be use in the future
-	//const displayDateBaseRewind = backup.baseRewindId ? getDisplayDate( backup.baseRewindId * 1000, false ) : null;
 
 	const today = applySiteOffset( moment(), {
 		timezone: timezone,
