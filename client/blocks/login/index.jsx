1e459caf3f91f605d386fae4d482156f4e1b4aa1
moon <moon.kyong@automattic.com>
2024-09-11 10:22:18 -0700

    Fix syntax issue from merge

--- client/blocks/login/index.jsx
+++ client/blocks/login/index.jsx
@@ -631,22 +631,22 @@ class Login extends Component {
 					headerText = <h3>{ translate( 'Authenticate your login' ) }</h3>;
 					break;
 				default:
-
 					headerText = (
 						<h3>
 							{ config.isEnabled( 'woocommerce/core-profiler-passwordless-auth' )
 								? translate( 'Log in to your account' )
 								: translate( 'One last step' ) }
 						</h3>
-
+					);
 					if ( config.isEnabled( 'woocommerce/core-profiler-passwordless-auth' ) ) {
 						subtitle = translate(
-							"In order to take advantage of the benefits offered by Jetpack and WooPayments, please log in to your WordPress.com account below. {{br}}{{/br}}Don't have an account? {{signupLink}}Sign up{{/signupLink}}",
+							"In order to take advantage of the benefits offered by %(pluginName)s, please log in to your WordPress.com account below. {{br}}{{/br}}Don't have an account? {{signupLink}}Sign up{{/signupLink}}",
 							{
 								components: {
 									signupLink,
 									br: <br />,
 								},
+								args: { pluginName },
 							}
 						);
 					} else {
