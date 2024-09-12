b3113870d8b6c29ceb95a6b8c9b1e524b4782e91
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-09-11 21:06:28 -0300

    Fetch pages using wpv2 endpoints

--- client/site-performance/components/PageSelector.tsx
+++ client/site-performance/components/PageSelector.tsx
@@ -1,68 +1,28 @@
 import page from '@automattic/calypso-router';
 import { SearchableDropdown } from '@automattic/components';
 import { useDebouncedInput } from '@wordpress/compose';
-import { useI18n } from '@wordpress/react-i18n';
-import { useMemo } from 'react';
-import QueryPosts from 'calypso/components/data/query-posts';
 import { useSelector } from 'calypso/state';
-import { getPostsForQueryIgnoringPage } from 'calypso/state/posts/selectors';
 import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
-import { getSelectedSite } from 'calypso/state/ui/selectors';
-
-interface Page {
-	slug: string;
-	URL: string;
-	title: string;
-}
-
-const PER_PAGE = 10;
+import { useSitePages } from '../hooks/useSitePages';
 
 export const PageSelector = () => {
-	const { __ } = useI18n();
-
 	const queryParams = useSelector( getCurrentQueryArguments );
-	const site = useSelector( getSelectedSite );
-
-	const [ , setPageQuery, pageQuery ] = useDebouncedInput();
-	const query = { type: 'page', number: PER_PAGE, search: pageQuery, page: 1 };
-	const pages = useSelector( ( state ): Page[] | null =>
-		getPostsForQueryIgnoringPage( state, site?.ID, query )
-	);
-
-	const options = useMemo( () => {
-		const mappedPages =
-			pages?.map( ( page ) => {
-				let url = page.URL.replace( site?.URL ?? '', '' );
-				url = url.length > 1 ? url.replace( /\/$/, '' ) : url;
-
-				return {
-					URL: url,
-					label: page.title,
-					value: url,
-				};
-			} ) ?? [];
-
-		if ( pageQuery.length > 0 ) {
-			return mappedPages;
-		}
-
-		return [ { URL: '/', label: __( 'Home' ), value: '/' }, ...mappedPages ];
-	}, [ pageQuery.length, pages, site?.URL, __ ] );
+	const [ , setQuery, query ] = useDebouncedInput();
+	const pages = useSitePages( { query } );
 
 	return (
 		<>
-			<QueryPosts postId={ null } siteId={ site?.ID } query={ query } />
 			<SearchableDropdown
-				onFilterValueChange={ setPageQuery }
-				options={ options }
-				value={ queryParams?.page_slug?.toString() ?? '/' }
-				onChange={ ( slug ) => {
+				onFilterValueChange={ setQuery }
+				options={ pages }
+				value={ queryParams?.page_id?.toString() }
+				onChange={ ( page_id ) => {
 					const url = new URL( window.location.href );
 
-					if ( slug ) {
-						url.searchParams.set( 'page_slug', slug );
+					if ( page_id ) {
+						url.searchParams.set( 'page_id', page_id );
 					} else {
-						url.searchParams.delete( 'page_slug' );
+						url.searchParams.delete( 'page_id' );
 					}
 
 					page.replace( url.pathname + url.search );
@@ -83,7 +43,7 @@ export const PageSelector = () => {
 						} }
 					>
 						<span>{ item.label }</span>
-						<span>{ item.URL }</span>
+						<span>{ item.url }</span>
 					</div>
 				) }
 			/>
