75fb633648210447963c1a73519d161c845c4b88
Anna McPhee <anna.mcphee@automattic.com>
2024-09-13 16:25:33 +1000

    move buttons to bottom

--- client/components/date-range/index.js
+++ client/components/date-range/index.js
@@ -497,7 +497,6 @@ export class DateRange extends Component {
 						{ this.renderDatePicker() }
 						{ this.props.renderHeader( headerProps ) }
 					</div>
-					<div className="date-range__controls">{ this.props.renderHeader( headerProps ) }</div>
 					{ /* Render shortcuts to the right of the calendar */ }
 					{ this.props.displayShortcuts && (
 						<div className="date-range-picker-shortcuts">
