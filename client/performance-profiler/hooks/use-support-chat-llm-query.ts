fbcc2a3bfdb245e63a6831c75f62b05207d9c9ab
cpap <papazoglou.charalampos@gmail.com>
2024-09-16 18:51:03 +0300

    Differentiates between wpcom and self hosted.

--- client/performance-profiler/hooks/use-support-chat-llm-query.ts
+++ client/performance-profiler/hooks/use-support-chat-llm-query.ts
@@ -17,8 +17,13 @@ export const useSupportChatLLMQuery = (
 		'Answer me in two topics in bold: "Why is this important?" and "How to fix this?".';
 	const contextToInclude = `Use each of the following context: ${ context }. Visit any links provided for more information.`;
 	const suggestionsToInclude =
-		'Add if the issue can be fixed automatically with a WordPress setting, an editor block setting, or a plugin. Prefer Automattic owned plugins. Add relevant links.';
-	const message = `${ question } ${ howToAnswer } ${ contextToInclude } ${ suggestionsToInclude }`;
+		'Add if the issue can be fixed automatically with a WordPress setting, an editor block setting, or a plugin. Prefer Automattic owned plugins. Add relevant links. Provide code snippets as a last resort.';
+	const wpcomOrSelfHostedSuggestions = ! is_wpcom
+		? 'Do not show suggestions that work only in WordPress.com. Do not show any references to documentation or support forums related to WordPress.com.'
+		: 'Provide suggestions that work only in WordPress.com. Use WordPress.com documentation if availabble.';
+	const otherDirectives =
+		'Do not suggest doing additional tests to identify the issue. Do not suggest any other tool/website/service for doing another test except WordPress Speed Test. Do not mention Lighthouse reports or any other tool. Rarely suggest subscribing to weekly emails feature of the Performance Profiler tool to monitor changes. Always provide links to the relevant plugin/theme or services. Do not ask follow-up questions or offer further assistance or ask for any feedback.';
+	const message = `${ question } ${ howToAnswer } ${ contextToInclude } ${ suggestionsToInclude } ${ wpcomOrSelfHostedSuggestions } ${ otherDirectives }`;
 
 	return useQuery( {
 		// eslint-disable-next-line @tanstack/query/exhaustive-deps
