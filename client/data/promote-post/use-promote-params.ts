fa54783a2abc86e6f25f5b590892e001480dc773
Vassilis Dimitrakis <vassilis.dimitrakis@automattic.com>
2024-09-16 19:24:40 +0300

    added a getWidgetParams util method to reuse the treatment of the url partial

--- client/data/promote-post/use-promote-params.ts
+++ client/data/promote-post/use-promote-params.ts
@@ -1,4 +1,5 @@
 import { useRouteModal } from 'calypso/lib/route-modal';
+import { getWidgetParams } from 'calypso/my-sites/promote-post-i2/utils';
 import { useSelector } from 'calypso/state';
 import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
@@ -18,13 +19,9 @@ const usePromoteParams = (): UsePromoteParams => {
 	const selectedSiteId = selectedSite?.ID || 0;
 	const currentQuery = useSelector( getCurrentQueryArguments );
 	const keyValue = ( currentQuery && ( currentQuery[ blazePressWidgetKey ] as string ) ) || '';
-
-	const postPartial = keyValue?.split( '_' )[ 0 ];
-	const campaignPartial = keyValue?.split( '_' )[ 1 ];
-
-	const selectedPostId = postPartial?.split( '-' )[ 1 ];
-	const selectedCampaignId = campaignPartial?.split( '-' )[ 1 ];
-
+	const widgetParams = getWidgetParams( keyValue );
+	const selectedPostId = widgetParams.selectedPostId;
+	const selectedCampaignId = widgetParams.selectedCampaignId;
 	const { isModalOpen } = useRouteModal( blazePressWidgetKey, keyValue );
 
 	return {
