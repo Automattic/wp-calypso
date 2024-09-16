2351baaaed56776c55bbac549e60fadb6f0a04eb
Caroline Moore <calobee@gmail.com>
2024-09-16 09:13:54 -0400

    Update links on plugin setup (#94453)

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/utils.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/utils.ts
@@ -1,14 +1,32 @@
+import config from '@automattic/calypso-config';
+
 const removeDuplicatedSlashes = ( url: string ) => url.replace( /(https?:\/\/)|(\/)+/g, '$1$2' );
+const isWhiteLabeledPluginEnabled = config.isEnabled(
+	'migration-flow/enable-white-labeled-plugin'
+);
 
 export const getPluginInstallationPage = ( fromUrl: string ) => {
 	if ( fromUrl !== '' ) {
+		if ( isWhiteLabeledPluginEnabled ) {
+			return removeDuplicatedSlashes(
+				`${ fromUrl }/wp-admin/plugin-install.php?s=%2522wpcom%2520migration%2522&tab=search&type=term`
+			);
+		}
+
 		return removeDuplicatedSlashes(
 			`${ fromUrl }/wp-admin/plugin-install.php?s=%2522migrate%2520guru%2522&tab=search&type=term`
 		);
 	}
 
-	return 'https://wordpress.org/plugins/migrate-guru/';
+	return isWhiteLabeledPluginEnabled
+		? 'https://wordpress.org/plugins/wpcom-migration/'
+		: 'https://wordpress.org/plugins/migrate-guru/';
 };
 
-export const getMigrateGuruPageURL = ( siteURL: string ) =>
-	removeDuplicatedSlashes( `${ siteURL }/wp-admin/admin.php?page=migrateguru` );
+export const getMigrateGuruPageURL = ( siteURL: string ) => {
+	if ( isWhiteLabeledPluginEnabled ) {
+		return removeDuplicatedSlashes( `${ siteURL }/wp-admin/admin.php?page=wpcom-migration` );
+	}
+
+	return removeDuplicatedSlashes( `${ siteURL }/wp-admin/admin.php?page=migrateguru` );
+};
