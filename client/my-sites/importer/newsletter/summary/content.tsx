b077e2e7a5e6e360909340acdb1119bf394be8c8
pkuliga <pkuliga@gmail.com>
2024-09-17 18:01:34 +0200

    copy fixes

--- client/my-sites/importer/newsletter/summary/content.tsx
+++ client/my-sites/importer/newsletter/summary/content.tsx
@@ -2,31 +2,64 @@ import { Icon, post } from '@wordpress/icons';
 
 function getSummaryCopy( postsNumber: number, pagesNumber: number, attachmentsNumber: number ) {
 	if ( postsNumber > 0 && pagesNumber > 0 && attachmentsNumber > 0 ) {
-		return `We imported ${ postsNumber } posts, ${ pagesNumber } pages and ${ attachmentsNumber } media.`;
+		return (
+			<>
+				We imported <strong>{ postsNumber } posts</strong>, <strong>{ pagesNumber } pages</strong>{ ' ' }
+				and
+				<strong>{ attachmentsNumber } media</strong>.
+			</>
+		);
 	}
 
 	if ( postsNumber > 0 && pagesNumber > 0 ) {
-		return `We imported ${ postsNumber } posts and ${ pagesNumber } pages`;
+		return (
+			<>
+				We imported <strong>{ postsNumber } posts</strong> and{ ' ' }
+				<strong>{ pagesNumber } pages</strong>.
+			</>
+		);
 	}
 
 	if ( postsNumber > 0 && attachmentsNumber > 0 ) {
-		return `We imported ${ postsNumber } posts and ${ attachmentsNumber } media`;
+		return (
+			<>
+				We imported <strong>{ postsNumber } posts</strong> and{ ' ' }
+				<strong>{ attachmentsNumber } media</strong>.
+			</>
+		);
 	}
 
 	if ( pagesNumber > 0 && attachmentsNumber > 0 ) {
-		return `We imported ${ postsNumber } pages and ${ attachmentsNumber } media`;
+		return (
+			<>
+				We imported <strong>{ postsNumber }</strong> pages and{ ' ' }
+				<strong>{ attachmentsNumber } media</strong>.
+			</>
+		);
 	}
 
 	if ( postsNumber > 0 ) {
-		return `We imported ${ postsNumber } posts.`;
+		return (
+			<>
+				We imported <strong>{ postsNumber } posts</strong>.
+			</>
+		);
 	}
 
 	if ( pagesNumber > 0 ) {
-		return `We imported ${ postsNumber } pages.`;
+		return (
+			<>
+				We imported <strong>{ postsNumber } pages</strong>.
+			</>
+		);
 	}
 
 	if ( attachmentsNumber > 0 ) {
-		return `We imported ${ postsNumber } media.`;
+		return (
+			<>
+				We imported <strong>{ postsNumber } media</strong>.
+			</>
+		);
 	}
 }
 
