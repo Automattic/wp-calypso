db1775055f231e7261745be323bfc9213736c9a3
Przemek Kuliga <pkuliga@gmail.com>
2024-09-17 17:40:20 +0200

    Paid newsletter importer summary step UI fixes (#94624)
    * copy
    
    * copy again
    
    * fix
    
    * cleanup uploading pane
    
    * more cleanup
    
    * summary copy

--- client/my-sites/importer/newsletter/summary/content.tsx
+++ client/my-sites/importer/newsletter/summary/content.tsx
@@ -1,4 +1,34 @@
-import { Icon, post, media, comment, page } from '@wordpress/icons';
+import { Icon, post } from '@wordpress/icons';
+
+function getSummaryCopy( postsNumber: number, pagesNumber: number, attachmentsNumber: number ) {
+	if ( postsNumber > 0 && pagesNumber > 0 && attachmentsNumber > 0 ) {
+		return `We imported ${ postsNumber } posts, ${ pagesNumber } pages and ${ attachmentsNumber } media.`;
+	}
+
+	if ( postsNumber > 0 && pagesNumber > 0 ) {
+		return `We imported ${ postsNumber } posts and ${ pagesNumber } pages`;
+	}
+
+	if ( postsNumber > 0 && attachmentsNumber > 0 ) {
+		return `We imported ${ postsNumber } posts and ${ attachmentsNumber } media`;
+	}
+
+	if ( pagesNumber > 0 && attachmentsNumber > 0 ) {
+		return `We imported ${ postsNumber } pages and ${ attachmentsNumber } media`;
+	}
+
+	if ( postsNumber > 0 ) {
+		return `We imported ${ postsNumber } posts.`;
+	}
+
+	if ( pagesNumber > 0 ) {
+		return `We imported ${ postsNumber } pages.`;
+	}
+
+	if ( attachmentsNumber > 0 ) {
+		return `We imported ${ postsNumber } media.`;
+	}
+}
 
 type Props = {
 	cardData: any;
@@ -10,7 +40,20 @@ export default function ContentSummary( { status, cardData }: Props ) {
 		return (
 			<div className="summary__content">
 				<p>
-					<Icon icon={ post } /> Content importing was <strong>skipped</strong>
+					<Icon icon={ post } /> You <strong>skipped</strong> content importing.
+				</p>
+			</div>
+		);
+	}
+
+	if ( status === 'importing' || status === 'processing' ) {
+		return (
+			<div className="summary__content">
+				<p>
+					<Icon icon={ post } /> <strong>We're importing your content.</strong>
+					<br />
+					This may take a few minutes. Feel free to leave this window – we'll let you know when it's
+					done.
 				</p>
 			</div>
 		);
@@ -21,44 +64,13 @@ export default function ContentSummary( { status, cardData }: Props ) {
 
 		return (
 			<div className="summary__content">
-				<p>We imported:</p>
-
-				{ progress.post.completed !== 0 && (
-					<p>
-						<Icon icon={ post } />
-						<strong>{ progress.post.completed }</strong> posts
-					</p>
-				) }
-
-				{ progress.page.completed !== 0 && (
-					<p>
-						<Icon icon={ page } />
-						<strong>{ progress.page.completed }</strong> pages
-					</p>
-				) }
-
-				{ progress.attachment.completed !== 0 && (
-					<p>
-						<Icon icon={ media } />
-						<strong>{ progress.attachment.completed }</strong> media
-					</p>
-				) }
-
-				{ progress.comment.completed !== 0 && (
-					<p>
-						<Icon icon={ comment } />
-						<strong>{ progress.comment.completed }</strong> comments
-					</p>
-				) }
-			</div>
-		);
-	}
-
-	if ( status === 'importing' || status === 'processing' ) {
-		return (
-			<div className="summary__content">
 				<p>
-					<Icon icon={ post } /> Content is importing...
+					<Icon icon={ post } />
+					{ getSummaryCopy(
+						progress.post.completed,
+						progress.page.completed,
+						progress.attachment.completed
+					) }
 				</p>
 			</div>
 		);
