7be9cd62054a606998f6a5b788be1b820e275266
Jarda Snajdr <jsnajdr@gmail.com>
2024-09-17 20:42:03 +0200

    Fix the flipped condition

--- client/components/action-panel/figure.jsx
+++ client/components/action-panel/figure.jsx
@@ -1,11 +1,12 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
-const ActionPanelFigure = ( { inlineBodyText = false, align = 'left', children } ) => {
-	const figureClasses = clsx( 'action-panel__figure', {
-		[ `align-${ 'left' === align ? 'left' : 'right' }` ]: true,
-		'is-inline-body-text': inlineBodyText,
-	} );
+const ActionPanelFigure = ( { inlineBodyText = false, align = 'right', children } ) => {
+	const figureClasses = clsx(
+		'action-panel__figure',
+		'left' === align ? 'align-left' : 'align-right',
+		{ 'is-inline-body-text': inlineBodyText }
+	);
 
 	return <div className={ figureClasses }>{ children }</div>;
 };
