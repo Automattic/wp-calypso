4bd833cc0b0f4eec4ecc00b49be8696cac752f20
Enej Bajgoric <enej.bajgoric@automattic.com>
2024-09-17 13:03:28 -0700

    Also accept undefined.

--- client/my-sites/importer/newsletter/utils.tsx
+++ client/my-sites/importer/newsletter/utils.tsx
@@ -25,7 +25,7 @@ export function getSetpProgressSteps(
 	engine: string,
 	selectedSiteSlug: string,
 	fromSite: string,
-	paidNewsletterData: PaidNewsletterData
+	paidNewsletterData?: PaidNewsletterData
 ) {
 	const result: ClickHandler[] = [
 		{
