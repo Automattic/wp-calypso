7b6b5efaa8d45ec48136e1c960a70308576f43fd
Gergely Csécsey <gergely.csecsey@automattic.com>
2024-09-17 16:40:11 +0200

    Performance Profiler: add image previews to insights list (#94582)
    * add image previews to insights list
    
    * add overlay when clicking thumbnail
    
    * update types
    
    * hide element markers outside screenshot area
    
    * update types
    
    * update marker styles

--- client/performance-profiler/components/metrics-insight/insight-detailed-content.tsx
+++ client/performance-profiler/components/metrics-insight/insight-detailed-content.tsx
@@ -1,13 +1,17 @@
-import { PerformanceMetricsDetailsQueryResponse } from 'calypso/data/site-profiler/types';
+import {
+	FullPageScreenshot,
+	PerformanceMetricsDetailsQueryResponse,
+} from 'calypso/data/site-profiler/types';
 import { InsightTable } from './insight-table';
 import { InsightTree } from './insight-tree';
 
 export interface InsightDetailedContentProps {
 	data: PerformanceMetricsDetailsQueryResponse;
+	fullPageScreenshot: FullPageScreenshot;
 }
 
 export const InsightDetailedContent: React.FC< InsightDetailedContentProps > = ( props ) => {
-	const { data } = props;
+	const { data, fullPageScreenshot } = props;
 
 	if ( data.type === 'table' || data.type === 'opportunity' ) {
 		return <InsightTable { ...props } />;
@@ -16,7 +20,9 @@ export const InsightDetailedContent: React.FC< InsightDetailedContentProps > = (
 	if ( data.type === 'list' ) {
 		const tables = data.items ?? [];
 
-		return tables.map( ( item, index ) => <InsightTable key={ index } data={ item as any } /> );
+		return tables.map( ( item, index ) => (
+			<InsightTable key={ index } fullPageScreenshot={ fullPageScreenshot } data={ item as any } />
+		) );
 	}
 
 	if ( data.type === 'criticalrequestchain' ) {
