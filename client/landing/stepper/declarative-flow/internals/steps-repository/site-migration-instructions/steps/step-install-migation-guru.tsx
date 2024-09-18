bd6c719df3e50e638419f1eaa43f8bdedbafbb20
Caroline Moore <calobee@gmail.com>
2024-09-18 12:13:09 -0400

    White-labeled plugin: Update text on site instructions based on feature flag (#94631)
    * Begin updating text based on white-labeled flag
    
    * Update title
    
    * force type to string

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/step-install-migation-guru.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/steps/step-install-migation-guru.tsx
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import { ExternalLink } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { recordMigrationInstructionsLinkClick } from '../tracking';
@@ -10,11 +11,15 @@ interface Props {
 
 export const StepInstallMigrationGuru: FC< Props > = ( { fromUrl } ) => {
 	const translate = useTranslate();
+	const isWhiteLabeledPluginEnabled = config.isEnabled(
+		'migration-flow/enable-white-labeled-plugin'
+	);
+	const pluginName = isWhiteLabeledPluginEnabled ? 'Migrate to WordPress.com' : 'Migrate Guru';
 
 	return (
 		<p>
 			{ translate(
-				"First you'll need to install and activate the {{a}}Migrate Guru plugin{{/a}} on the site you want to migrate. Click {{strong}}Next{{/strong}} when you're ready.",
+				"First you'll need to install and activate the {{a}}%(pluginName)s plugin{{/a}} on the site you want to migrate. Click {{strong}}Next{{/strong}} when you're ready.",
 				{
 					components: {
 						strong: <strong />,
@@ -28,6 +33,7 @@ export const StepInstallMigrationGuru: FC< Props > = ( { fromUrl } ) => {
 							/>
 						),
 					},
+					args: { pluginName },
 				}
 			) }
 		</p>
