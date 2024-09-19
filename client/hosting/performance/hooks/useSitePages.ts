8ba3dc28df5028812174cb6c185cd2c589928661
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-09-19 11:28:11 -0300

    Logged-in Performance Profiler: Trigger test re-run

--- client/hosting/performance/hooks/useSitePages.ts
+++ client/hosting/performance/hooks/useSitePages.ts
@@ -1,10 +1,11 @@
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import { useI18n } from '@wordpress/react-i18n';
 import { addQueryArgs } from '@wordpress/url';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import wpcomRequest from 'wpcom-proxy-request';
 import { useSiteSettings } from 'calypso/blocks/plugins-scheduled-updates/hooks/use-site-settings';
-import { useSelector } from 'calypso/state';
+import { useDispatch, useSelector } from 'calypso/state';
+import { saveSiteSettings } from 'calypso/state/site-settings/actions';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 interface SitePage {
@@ -12,11 +13,9 @@ interface SitePage {
 	link: string;
 	title: { rendered: string };
 	wpcom_performance_report_url: string;
-	wpcom_performance_url?: {
-		url: string;
-		hash: string;
-	};
 }
+
+const FIELDS_TO_RETRIEVE = [ 'id', 'link', 'title', 'wpcom_performance_report_url' ];
 
 const getPages = ( siteId: number, query = '' ) => {
 	return wpcomRequest< SitePage[] >( {
@@ -25,29 +24,61 @@ const getPages = ( siteId: number, query = '' ) => {
 			search: query,
 			page: 1,
 			status: 'publish',
-			_fields: [ 'id', 'link', 'title', 'wpcom_performance_report_url' ],
+			_fields: FIELDS_TO_RETRIEVE,
 		} ),
 		method: 'GET',
 		apiNamespace: 'wp/v2',
 	} );
 };
 
-export const getSitePagesQueryKey = ( {
-	siteId,
-	query,
-}: {
-	siteId?: number | null;
-	query: string;
-} ) => [ 'useSitePages', siteId, query ];
+const savePageMeta = ( siteId: number, pageId: number, url: string ) => {
+	return wpcomRequest< SitePage >( {
+		path: addQueryArgs( `/sites/${ siteId }/pages/${ pageId }`, {
+			_fields: FIELDS_TO_RETRIEVE,
+		} ),
+		method: 'POST',
+		apiNamespace: 'wp/v2',
+		body: {
+			wpcom_performance_report_url: url,
+		},
+	} );
+};
 
-export const useSitePages = ( { query = '' } ) => {
+interface PerformanceReportUrl {
+	url: string;
+	hash: string;
+}
+
+const toPerformanceReportParts = (
+	pageUrl: string,
+	performanceReportUrl: string
+): PerformanceReportUrl => {
+	const [ url, hash ] = performanceReportUrl.split( '&hash=' );
+
+	if ( ! url || ! hash ) {
+		return { url: pageUrl, hash: '' };
+	}
+
+	return {
+		url,
+		hash,
+	};
+};
+
+const toPerformanceReportUrl = ( { url, hash }: PerformanceReportUrl ) => {
+	return `${ url }&hash=${ hash }`;
+};
+
+const HOME_PAGE_ID = '0';
+
+export const useSitePages = ( { query = '' } = {} ) => {
 	const { __ } = useI18n();
 
 	const site = useSelector( getSelectedSite );
 	const siteId = site?.ID;
 
 	const { data } = useQuery( {
-		queryKey: getSitePagesQueryKey( { siteId, query } ),
+		queryKey: [ 'useSitePages', siteId, query ],
 		queryFn: () => getPages( siteId!, query ),
 		refetchOnWindowFocus: false,
 		enabled: !! siteId,
@@ -56,14 +87,16 @@ export const useSitePages = ( { query = '' } ) => {
 			return data.map( ( page ) => {
 				let path = page.link.replace( site?.URL ?? '', '' );
 				path = path.length > 1 ? path.replace( /\/$/, '' ) : path;
-				const [ url, hash ] = page.wpcom_performance_report_url?.split( '&hash=' ) ?? [];
 
 				return {
 					url: page.link,
 					path,
 					label: page.title.rendered,
 					value: page.id.toString(),
-					wpcom_performance_url: { url, hash },
+					wpcom_performance_report_url: toPerformanceReportParts(
+						page.link,
+						page.wpcom_performance_report_url
+					),
 				};
 			} );
 		},
@@ -73,27 +106,54 @@ export const useSitePages = ( { query = '' } ) => {
 	} );
 
 	const { getSiteSetting } = useSiteSettings( site?.slug );
-	const homePagePerformanceUrl: SitePage[ 'wpcom_performance_report_url' ] =
-		getSiteSetting( 'wpcom_performance_report_url' ) || undefined;
-
-	const [ url, hash ] = homePagePerformanceUrl?.split( '&hash=' ) ?? [];
+	const homePagePerformanceUrl: SitePage[ 'wpcom_performance_report_url' ] = getSiteSetting(
+		'wpcom_performance_report_url'
+	);
 
 	const pages = useMemo( () => {
+		if ( ! site?.URL ) {
+			return [];
+		}
+
 		if ( ! query ) {
 			return [
 				{
 					url: site?.URL,
 					path: '/',
 					label: __( 'Home' ),
-					value: '0',
-					wpcom_performance_url: { url, hash },
+					value: HOME_PAGE_ID,
+					wpcom_performance_report_url: toPerformanceReportParts(
+						site.URL,
+						homePagePerformanceUrl
+					),
 				},
 				...( data ?? [] ),
 			];
 		}
 
 		return data ?? [];
-	}, [ query, data, site?.URL, __, url, hash ] );
+	}, [ query, data, site?.URL, __, homePagePerformanceUrl ] );
 
-	return pages;
+	const dispatch = useDispatch();
+
+	const savePerformanceReportUrl = useCallback(
+		async ( pageId: string, performanceReport: PerformanceReportUrl ) => {
+			if ( ! siteId ) {
+				return;
+			}
+
+			const performanceReportUrl = toPerformanceReportUrl( performanceReport );
+
+			if ( pageId === HOME_PAGE_ID ) {
+				dispatch(
+					saveSiteSettings( siteId, { wpcom_performance_report_url: performanceReportUrl } )
+				);
+			} else {
+				savePageMeta( siteId, parseInt( pageId, 10 ), performanceReportUrl );
+			}
+		},
+		[ siteId, dispatch ]
+	);
+
+	return { pages, savePerformanceReportUrl };
 };
