01957d8b71c5c5237fb41836bb8f42fc8227a963
pkuliga <pkuliga@gmail.com>
2024-09-19 10:39:07 +0200

    show subscribers import button

--- client/my-sites/importer/newsletter/subscribers/start-import-button.tsx
+++ client/my-sites/importer/newsletter/subscribers/start-import-button.tsx
@@ -1,7 +1,3 @@
-import { FormLabel } from '@automattic/components';
-import { useState } from '@wordpress/element';
-import { ChangeEvent } from 'react';
-import FormCheckbox from 'calypso/components/forms/form-checkbox';
 import { useSubscriberImportMutation } from 'calypso/data/paid-newsletter/use-subscriber-import-mutation';
 import ImporterActionButton from '../../importer-action-buttons/action-button';
 
@@ -9,19 +5,17 @@ type Props = {
 	step: string;
 	engine: string;
 	siteId: number;
-	hasPaidSubscribers: boolean;
 	navigate?: () => void;
+	disabled?: boolean;
 };
 
 export default function StartImportButton( {
 	siteId,
-	hasPaidSubscribers,
 	step,
 	engine,
 	navigate = () => {},
+	disabled,
 }: Props ) {
-	const [ isDisabled, setIsDisabled ] = useState( ! hasPaidSubscribers );
-
 	const { enqueueSubscriberImport } = useSubscriberImportMutation();
 
 	const importSubscribers = () => {
@@ -29,30 +23,9 @@ export default function StartImportButton( {
 		navigate();
 	};
 
-	const onChange = ( { target: { checked } }: ChangeEvent< HTMLInputElement > ) =>
-		setIsDisabled( checked );
-
 	return (
-		<>
-			{ hasPaidSubscribers && (
-				<>
-					<p>
-						<strong>To prevent any unexpected actions by your old provider</strong>, go to your
-						Stripe dashboard and click “Revoke access” for any service previously associated with
-						this subscription.
-					</p>
-
-					<p>
-						<FormLabel>
-							<FormCheckbox checked={ isDisabled } onChange={ onChange } />
-							I’ve disconnected other providers from the Stripe account
-						</FormLabel>
-					</p>
-				</>
-			) }
-			<ImporterActionButton primary disabled={ ! isDisabled } onClick={ importSubscribers }>
-				Import subscribers
-			</ImporterActionButton>
-		</>
+		<ImporterActionButton primary disabled={ disabled } onClick={ importSubscribers }>
+			Continue
+		</ImporterActionButton>
 	);
 }
