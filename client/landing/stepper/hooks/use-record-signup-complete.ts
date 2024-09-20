525322f53a5a10ddf980cc0ff313681f27b05a9d
Renan Carvalho <renan.carvalho@automattic.com>
2024-09-20 11:13:50 +0200

    Stepper: calypso_signup_complete as in Start (#94706)
    * Use same tracking values as in start
    
    * Pass destinationState to signup completion tracking
    
    * Add newUser call
    
    * Register in the store when it is a new account
    
    * Better check
    
    ---------
    
    Co-authored-by: escapemanuele <escapemanuele@gmail.com>

--- client/landing/stepper/hooks/use-record-signup-complete.ts
+++ client/landing/stepper/hooks/use-record-signup-complete.ts
@@ -3,6 +3,8 @@ import { useSelect } from '@wordpress/data';
 import { useCallback } from 'react';
 import { USER_STORE, ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { SIGNUP_DOMAIN_ORIGIN, recordSignupComplete } from 'calypso/lib/analytics/signup';
+import { useSelector } from 'calypso/state';
+import isUserRegistrationDaysWithinRange from 'calypso/state/selectors/is-user-registration-days-within-range';
 import { useSite } from './use-site';
 import type { UserSelect, OnboardSelect } from '@automattic/data-stores';
 
@@ -10,61 +12,70 @@ export const useRecordSignupComplete = ( flow: string | null ) => {
 	const site = useSite();
 	const siteId = site?.ID || null;
 	const theme = site?.options?.theme_slug || '';
-	const { domainCartItem, planCartItem, siteCount, selectedDomain } = useSelect( ( select ) => {
-		return {
-			siteCount: ( select( USER_STORE ) as UserSelect ).getCurrentUser()?.site_count,
-			domainCartItem: ( select( ONBOARD_STORE ) as OnboardSelect ).getDomainCartItem(),
-			planCartItem: ( select( ONBOARD_STORE ) as OnboardSelect ).getPlanCartItem(),
-			selectedDomain: ( select( ONBOARD_STORE ) as OnboardSelect ).getSelectedDomain(),
-		};
-	}, [] );
+	const { domainCartItem, planCartItem, siteCount, selectedDomain, isNewUser } = useSelect(
+		( select ) => {
+			return {
+				siteCount: ( select( USER_STORE ) as UserSelect ).getCurrentUser()?.site_count,
+				domainCartItem: ( select( ONBOARD_STORE ) as OnboardSelect ).getDomainCartItem(),
+				planCartItem: ( select( ONBOARD_STORE ) as OnboardSelect ).getPlanCartItem(),
+				selectedDomain: ( select( ONBOARD_STORE ) as OnboardSelect ).getSelectedDomain(),
+				isNewUser: ( select( USER_STORE ) as UserSelect ).isNewUser(),
+			};
+		},
+		[]
+	);
 
-	return useCallback( () => {
-		// FIXME: once moving to the Stepper version of User step,
-		// wire the value of `isNewUser()` from the user store.
-		const isNewUser = ! siteCount;
+	const isNewishUser = useSelector( ( state ) =>
+		isUserRegistrationDaysWithinRange( state, null, 0, 7 )
+	);
 
-		// FIXME:
-		// currently it's impossible to derive this data since it requires
-		// the length of registration, so I use isNewUser here as an approximation
-		const isNew7DUserSite = isNewUser;
+	return useCallback(
+		( signupCompletionState: Record< string, unknown > ) => {
+			const siteSlug = site?.slug ?? signupCompletionState?.siteSlug;
 
-		// Domain product slugs can be a domain purchases like dotcom_domain or dotblog_domain or a mapping like domain_mapping
-		// When purchasing free subdomains the product_slugs is empty (since there is no actual produce being purchased)
-		// so we avoid capturing the product slug in these instances.
-		const domainProductSlug = domainCartItem?.product_slug ?? undefined;
+			const isNew7DUserSite = !! (
+				isNewUser ||
+				( isNewishUser && siteSlug && siteCount && siteCount <= 1 )
+			);
 
-		// Domain cart items can sometimes be included when free. So the selected domain is explicitly checked to see if it's free.
-		// For mappings and transfers this attribute should be empty but it needs to be checked.
-		const hasCartItems = !! ( domainProductSlug || planCartItem ); // see the function `dependenciesContainCartItem()
+			// Domain product slugs can be a domain purchases like dotcom_domain or dotblog_domain or a mapping like domain_mapping
+			// When purchasing free subdomains the product_slugs is empty (since there is no actual produce being purchased)
+			// so we avoid capturing the product slug in these instances.
+			const domainProductSlug = domainCartItem?.product_slug ?? undefined;
 
-		// When there is no plan put in the cart, `planCartItem` is `null` instead of `undefined` like domainCartItem.
-		// It worths a investigation of whether the both should behave the same.
-		const planProductSlug = planCartItem?.product_slug ?? undefined;
-		// To have a paid domain item it has to either be a paid domain or a different domain product like mapping or transfer.
-		const hasPaidDomainItem =
-			( selectedDomain && ! selectedDomain.is_free ) || !! domainProductSlug;
+			// Domain cart items can sometimes be included when free. So the selected domain is explicitly checked to see if it's free.
+			// For mappings and transfers this attribute should be empty but it needs to be checked.
+			const hasCartItems = !! ( domainProductSlug || planCartItem ); // see the function `dependenciesContainCartItem()
 
-		recordSignupComplete(
-			{
-				flow,
-				siteId,
-				isNewUser,
-				hasCartItems,
-				isNew7DUserSite,
-				theme,
-				intent: flow,
-				startingPoint: flow,
-				isBlankCanvas: theme?.includes( 'blank-canvas' ),
-				planProductSlug,
-				domainProductSlug,
-				isMapping:
-					hasPaidDomainItem && domainCartItem ? isDomainMapping( domainCartItem ) : undefined,
-				isTransfer:
-					hasPaidDomainItem && domainCartItem ? isDomainTransfer( domainCartItem ) : undefined,
-				signupDomainOrigin: SIGNUP_DOMAIN_ORIGIN.NOT_SET,
-			},
-			true
-		);
-	}, [ domainCartItem, flow, planCartItem, selectedDomain, siteCount, siteId, theme ] );
+			// When there is no plan put in the cart, `planCartItem` is `null` instead of `undefined` like domainCartItem.
+			// It worths a investigation of whether the both should behave the same.
+			const planProductSlug = planCartItem?.product_slug ?? undefined;
+			// To have a paid domain item it has to either be a paid domain or a different domain product like mapping or transfer.
+			const hasPaidDomainItem =
+				( selectedDomain && ! selectedDomain.is_free ) || !! domainProductSlug;
+
+			recordSignupComplete(
+				{
+					flow,
+					siteId: siteId ?? signupCompletionState?.siteId,
+					isNewUser,
+					hasCartItems,
+					isNew7DUserSite,
+					theme,
+					intent: flow,
+					startingPoint: flow,
+					isBlankCanvas: theme?.includes( 'blank-canvas' ),
+					planProductSlug,
+					domainProductSlug,
+					isMapping:
+						hasPaidDomainItem && domainCartItem ? isDomainMapping( domainCartItem ) : undefined,
+					isTransfer:
+						hasPaidDomainItem && domainCartItem ? isDomainTransfer( domainCartItem ) : undefined,
+					signupDomainOrigin: SIGNUP_DOMAIN_ORIGIN.NOT_SET,
+				},
+				true
+			);
+		},
+		[ domainCartItem, flow, planCartItem, selectedDomain, siteCount, siteId, theme ]
+	);
 };
