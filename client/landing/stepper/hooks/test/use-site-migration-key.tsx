fd3a40e20c78b9d85334f09033f4ba0a78e0fc8e
Caroline Moore <calobee@gmail.com>
2024-09-23 10:00:24 +0100

    Update test

--- client/landing/stepper/hooks/test/use-site-migration-key.tsx
+++ client/landing/stepper/hooks/test/use-site-migration-key.tsx
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+import config from '@automattic/calypso-config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
 import nock from 'nock';
@@ -10,15 +11,26 @@ import { useSiteMigrationKey } from '../use-site-migration-key';
 describe( 'useSiteMigrationKey', () => {
 	it( 'returns the site migration key', async () => {
 		const queryClient = new QueryClient();
+		const isWhiteLabeledPluginEnabled = config.isEnabled(
+			'migration-flow/enable-white-labeled-plugin'
+		);
 		const wrapper = ( { children } ) => (
 			<QueryClientProvider client={ queryClient }>{ children }</QueryClientProvider>
 		);
 
-		nock( 'https://public-api.wordpress.com' )
-			.get( '/wpcom/v2/sites/123/atomic-migration-status/migrate-guru-key' )
-			.query( { http_envelope: 1 } )
-			.once()
-			.reply( 200, { migration_key: 'some-migration-key' } );
+		if ( isWhiteLabeledPluginEnabled ) {
+			nock( 'https://public-api.wordpress.com' )
+				.get( '/wpcom/v2/sites/123/atomic-migration-status/wpcom-migration-key' )
+				.query( { http_envelope: 1 } )
+				.once()
+				.reply( 200, { migration_key: 'some-migration-key' } );
+		} else {
+			nock( 'https://public-api.wordpress.com' )
+				.get( '/wpcom/v2/sites/123/atomic-migration-status/migrate-guru-key' )
+				.query( { http_envelope: 1 } )
+				.once()
+				.reply( 200, { migration_key: 'some-migration-key' } );
+		}
 
 		const { result } = renderHook( () => useSiteMigrationKey( 123 ), { wrapper } );
 
