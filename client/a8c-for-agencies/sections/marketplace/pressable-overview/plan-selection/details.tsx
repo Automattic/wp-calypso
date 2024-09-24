dc03ab24fc73381bd831ab95d37a34d09d6522a3
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-09-24 17:14:07 +0800

    Add overage pricing text on Pressable section.

--- client/a8c-for-agencies/sections/marketplace/pressable-overview/plan-selection/details.tsx
+++ client/a8c-for-agencies/sections/marketplace/pressable-overview/plan-selection/details.tsx
@@ -77,43 +77,174 @@ export default function PlanSelectionDetails( {
 	const isRegularOwnership = pressableOwnership === 'regular';
 
 	return (
-		<section className="pressable-overview-plan-selection__details">
-			<div className="pressable-overview-plan-selection__details-card">
-				<div className="pressable-overview-plan-selection__details-card-header">
-					<h3 className="pressable-overview-plan-selection__details-card-header-title plan-name">
-						{ isNewHostingPage
-							? translate( 'Pressable' )
-							: translate( '%(planName)s plan', {
-									args: {
-										planName: selectedPlan
-											? getPressableShortName( selectedPlan.name )
-											: customString,
-									},
-									comment: '%(planName)s is the name of the selected plan.',
-							  } ) }
-					</h3>
+		<>
+			<section className="pressable-overview-plan-selection__details">
+				<div className="pressable-overview-plan-selection__details-card">
+					<div className="pressable-overview-plan-selection__details-card-header">
+						<h3 className="pressable-overview-plan-selection__details-card-header-title plan-name">
+							{ isNewHostingPage
+								? translate( 'Pressable' )
+								: translate( '%(planName)s plan', {
+										args: {
+											planName: selectedPlan
+												? getPressableShortName( selectedPlan.name )
+												: customString,
+										},
+										comment: '%(planName)s is the name of the selected plan.',
+								  } ) }
+						</h3>
 
-					{ ! isReferMode && selectedPlan && (
-						<div className="pressable-overview-plan-selection__details-card-header-price">
-							<strong className="pressable-overview-plan-selection__details-card-header-price-value">
-								{ formatCurrency( discountedCost, selectedPlan.currency ) }
-							</strong>
-							<span className="pressable-overview-plan-selection__details-card-header-price-interval">
-								{ selectedPlan.price_interval === 'day' && translate( 'per plan per day' ) }
-								{ selectedPlan.price_interval === 'month' && translate( 'per plan per month' ) }
-							</span>
-						</div>
+						{ ! isReferMode && selectedPlan && (
+							<div className="pressable-overview-plan-selection__details-card-header-price">
+								<strong className="pressable-overview-plan-selection__details-card-header-price-value">
+									{ formatCurrency( discountedCost, selectedPlan.currency ) }
+								</strong>
+								<span className="pressable-overview-plan-selection__details-card-header-price-interval">
+									{ selectedPlan.price_interval === 'day' && translate( 'per plan per day' ) }
+									{ selectedPlan.price_interval === 'month' && translate( 'per plan per month' ) }
+								</span>
+							</div>
+						) }
+
+						{ isRegularOwnership && ! isReferMode && (
+							<div className="pressable-overview-plan-selection__details-card-header-subtitle is-regular-ownership">
+								{ translate(
+									'{{b}}You own this plan.{{/b}} Manage your hosting seamlessly by accessing the Pressable dashboard',
+									{
+										components: { b: <b /> },
+									}
+								) }
+							</div>
+						) }
+					</div>
+
+					{ ! isRegularOwnership && ! isReferMode && (
+						<SimpleList
+							items={ [
+								info?.install
+									? translate(
+											'{{b}}%(count)d{{/b}} WordPress install',
+											'{{b}}%(count)d{{/b}} WordPress installs',
+											{
+												args: {
+													count: info.install,
+												},
+												count: info.install,
+												components: { b: <b /> },
+												comment: '%(count)s is the number of WordPress installs.',
+											}
+									  )
+									: translate( 'Custom WordPress installs' ),
+								translate( '{{b}}%(count)s{{/b}} visits per month', {
+									args: {
+										count: info ? formatNumber( info.visits ) : customString,
+									},
+									components: { b: <b /> },
+									comment: '%(count)s is the number of visits per month.',
+								} ),
+								translate( '{{b}}%(size)s{{/b}} storage per month', {
+									args: {
+										size: info ? `${ info.storage }GB` : customString,
+									},
+									components: { b: <b /> },
+									comment: '%(size)s is the amount of storage in gigabytes.',
+								} ),
+								...( isNewHostingPage
+									? [
+											translate( '{{b}}Unmetered{{/b}} bandwidth', {
+												components: { b: <b /> },
+											} ),
+									  ]
+									: [] ),
+							] }
+						/>
 					) }
 
-					{ isRegularOwnership && ! isReferMode && (
-						<div className="pressable-overview-plan-selection__details-card-header-subtitle is-regular-ownership">
-							{ translate(
-								'{{b}}You own this plan.{{/b}} Manage your hosting seamlessly by accessing the Pressable dashboard',
-								{
-									components: { b: <b /> },
-								}
-							) }
+					{ isReferMode ? (
+						<div>
+							<div className="pressable-overview-plan-selection__details-card-header-subtitle is-refer-mode">
+								{ translate(
+									'Pressable hosting will be included in the referral program in the future.'
+								) }
+							</div>
+							<Button
+								className="pressable-overview-plan-selection__details-card-cta-button"
+								href={ CONTACT_URL_HASH_FRAGMENT }
+								primary
+							>
+								{ translate( 'Contact support' ) } <Icon icon={ external } size={ 16 } />
+							</Button>
 						</div>
+					) : (
+						<>
+							{ selectedPlan && (
+								<>
+									{ isRegularOwnership ? (
+										<div
+											className="pressable-overview-plan-selection__manage-account-button-container"
+											ref={ managedButtonRef }
+											role="button"
+											tabIndex={ 0 }
+											onMouseEnter={ () => setShowManagedTooltip( true ) }
+											onMouseLeave={ () => setShowManagedTooltip( false ) }
+											onMouseDown={ () => setShowManagedTooltip( false ) }
+										>
+											<Button
+												target="_blank"
+												rel="norefferer nooppener"
+												href="https://my.pressable.com/agency/auth"
+												disabled={ ! isOwner }
+											>
+												{ isOwner
+													? translate( 'Manage in Pressable' )
+													: translate( 'Managed by agency owner' ) }
+												{ isOwner && <Icon icon={ external } size={ 18 } /> }
+											</Button>
+										</div>
+									) : (
+										<Button
+											className="pressable-overview-plan-selection__details-card-cta-button"
+											onClick={ onSelectPlan }
+											primary
+										>
+											{ isNewHostingPage
+												? translate( 'Select this plan' )
+												: translate( 'Select %(planName)s plan', {
+														args: {
+															planName: selectedPlan
+																? getPressableShortName( selectedPlan.name )
+																: customString,
+														},
+														comment: '%(planName)s is the name of the selected plan.',
+												  } ) }
+										</Button>
+									) }
+
+									<Tooltip
+										context={ managedButtonRef.current }
+										isVisible={ ! isOwner && showManagedTooltip }
+										position="bottom"
+										className="pressable-overview-plan-selection__tooltip"
+									>
+										{ translate(
+											"This Pressable account is managed by the Agency Owner user on your account. If you'd like access to this Pressable account, request that they add you as a user in Pressable."
+										) }
+									</Tooltip>
+								</>
+							) }
+
+							{ ! selectedPlan && (
+								<Button
+									className="pressable-overview-plan-selection__details-card-cta-button"
+									onClick={ onContactUs }
+									href={ PRESSABLE_CONTACT_LINK }
+									target="_blank"
+									primary
+								>
+									{ translate( 'Contact us' ) } <Icon icon={ external } size={ 16 } />
+								</Button>
+							) }
+						</>
 					) }
 					{ isReferMode && (
 						<div className="pressable-overview-plan-selection__details-card-header-price">
@@ -124,180 +255,62 @@ export default function PlanSelectionDetails( {
 					) }
 				</div>
 
-				{ ! isRegularOwnership && ! isReferMode && (
-					<SimpleList
-						items={ [
-							info?.install
-								? translate(
-										'{{b}}%(count)d{{/b}} WordPress install',
-										'{{b}}%(count)d{{/b}} WordPress installs',
-										{
-											args: {
-												count: info.install,
-											},
-											count: info.install,
-											components: { b: <b /> },
-											comment: '%(count)s is the number of WordPress installs.',
-										}
-								  )
-								: translate( 'Custom WordPress installs' ),
-							translate( '{{b}}%(count)s{{/b}} visits per month', {
-								args: {
-									count: info ? formatNumber( info.visits ) : customString,
-								},
-								components: { b: <b /> },
-								comment: '%(count)s is the number of visits per month.',
-							} ),
-							translate( '{{b}}%(size)s{{/b}} storage per month', {
-								args: {
-									size: info ? `${ info.storage }GB` : customString,
-								},
-								components: { b: <b /> },
-								comment: '%(size)s is the amount of storage in gigabytes.',
-							} ),
-							...( isNewHostingPage
-								? [
-										translate( '{{b}}Unmetered{{/b}} bandwidth', {
-											components: { b: <b /> },
-										} ),
-								  ]
-								: [] ),
-						] }
-					/>
-				) }
-
-				{ isReferMode ? (
-					<div>
-						<div className="pressable-overview-plan-selection__details-card-header-subtitle is-refer-mode">
+				{ isNewHostingPage ? (
+					<div className="pressable-overview-plan-selection__details-card is-aside">
+						<h3 className="pressable-overview-plan-selection__details-card-header-title">
+							{ translate( 'Schedule a demo and personal consultation' ) }
+						</h3>
+						<div className="pressable-overview-plan-selection__details-card-header-subtitle">
 							{ translate(
-								'Pressable hosting will be included in the referral program in the future.'
+								'One of our friendly experts would be happy to give you a one-on-one tour of our platform and discuss:'
 							) }
 						</div>
+
+						<SimpleList
+							items={ [
+								translate( 'Our support, service, and pricing flexibility' ),
+								translate( 'The best hosting plan for your needs' ),
+								translate( 'How to launch and manage WordPress sites' ),
+								translate( 'The free perks that come with Pressable' ),
+							] }
+						/>
 						<Button
 							className="pressable-overview-plan-selection__details-card-cta-button"
-							href={ CONTACT_URL_HASH_FRAGMENT }
-							primary
+							onClick={ onScheduleDemo }
+							href={ PRESSABLE_CONTACT_LINK }
+							target="_blank"
 						>
-							{ translate( 'Contact support' ) } <Icon icon={ external } size={ 16 } />
+							{ translate( 'Schedule a Demo' ) } <Icon icon={ external } size={ 18 } />
 						</Button>
 					</div>
 				) : (
-					<>
-						{ selectedPlan && (
-							<>
-								{ isRegularOwnership ? (
-									<div
-										className="pressable-overview-plan-selection__manage-account-button-container"
-										ref={ managedButtonRef }
-										role="button"
-										tabIndex={ 0 }
-										onMouseEnter={ () => setShowManagedTooltip( true ) }
-										onMouseLeave={ () => setShowManagedTooltip( false ) }
-										onMouseDown={ () => setShowManagedTooltip( false ) }
-									>
-										<Button
-											target="_blank"
-											rel="norefferer nooppener"
-											href="https://my.pressable.com/agency/auth"
-											disabled={ ! isOwner }
-										>
-											{ isOwner
-												? translate( 'Manage in Pressable' )
-												: translate( 'Managed by agency owner' ) }
-											{ isOwner && <Icon icon={ external } size={ 18 } /> }
-										</Button>
-									</div>
-								) : (
-									<Button
-										className="pressable-overview-plan-selection__details-card-cta-button"
-										onClick={ onSelectPlan }
-										primary
-									>
-										{ isNewHostingPage
-											? translate( 'Select this plan' )
-											: translate( 'Select %(planName)s plan', {
-													args: {
-														planName: selectedPlan
-															? getPressableShortName( selectedPlan.name )
-															: customString,
-													},
-													comment: '%(planName)s is the name of the selected plan.',
-											  } ) }
-									</Button>
-								) }
+					<div className="pressable-overview-plan-selection__details-card is-aside">
+						<h3 className="pressable-overview-plan-selection__details-card-header-title">
+							{ translate( 'All plans include:' ) }
+						</h3>
 
-								<Tooltip
-									context={ managedButtonRef.current }
-									isVisible={ ! isOwner && showManagedTooltip }
-									position="bottom"
-									className="pressable-overview-plan-selection__tooltip"
-								>
-									{ translate(
-										"This Pressable account is managed by the Agency Owner user on your account. If you'd like access to this Pressable account, request that they add you as a user in Pressable."
-									) }
-								</Tooltip>
-							</>
-						) }
+						<SimpleList
+							items={ [
+								translate( '24/7 WordPress hosting support' ),
+								translate( 'WP Cloud platform' ),
+								translate( 'Jetpack Security (optional)' ),
+								translate( 'Free site migrations' ),
+							] }
+						/>
+					</div>
+				) }
+			</section>
 
-						{ ! selectedPlan && (
-							<Button
-								className="pressable-overview-plan-selection__details-card-cta-button"
-								onClick={ onContactUs }
-								href={ PRESSABLE_CONTACT_LINK }
-								target="_blank"
-								primary
-							>
-								{ translate( 'Contact us' ) } <Icon icon={ external } size={ 16 } />
-							</Button>
-						) }
-					</>
+			<div className="pressable-overview-plan-selection__hint">
+				{ translate(
+					'*We charge {{b}}$0.5{{/b}} per month per GB over their Storage limit, and {{b}}$8{{/b}} per 10k Visits per month over their Traffic limit.',
+					{
+						components: {
+							b: <b />,
+						},
+					}
 				) }
 			</div>
-
-			{ isNewHostingPage ? (
-				<div className="pressable-overview-plan-selection__details-card is-aside">
-					<h3 className="pressable-overview-plan-selection__details-card-header-title">
-						{ translate( 'Schedule a demo and personal consultation' ) }
-					</h3>
-					<div className="pressable-overview-plan-selection__details-card-header-subtitle">
-						{ translate(
-							'One of our friendly experts would be happy to give you a one-on-one tour of our platform and discuss:'
-						) }
-					</div>
-
-					<SimpleList
-						items={ [
-							translate( 'Our support, service, and pricing flexibility' ),
-							translate( 'The best hosting plan for your needs' ),
-							translate( 'How to launch and manage WordPress sites' ),
-							translate( 'The free perks that come with Pressable' ),
-						] }
-					/>
-					<Button
-						className="pressable-overview-plan-selection__details-card-cta-button"
-						onClick={ onScheduleDemo }
-						href={ PRESSABLE_CONTACT_LINK }
-						target="_blank"
-					>
-						{ translate( 'Schedule a Demo' ) } <Icon icon={ external } size={ 18 } />
-					</Button>
-				</div>
-			) : (
-				<div className="pressable-overview-plan-selection__details-card is-aside">
-					<h3 className="pressable-overview-plan-selection__details-card-header-title">
-						{ translate( 'All plans include:' ) }
-					</h3>
-
-					<SimpleList
-						items={ [
-							translate( '24/7 WordPress hosting support' ),
-							translate( 'WP Cloud platform' ),
-							translate( 'Jetpack Security (optional)' ),
-							translate( 'Free site migrations' ),
-						] }
-					/>
-				</div>
-			) }
-		</section>
+		</>
 	);
 }
