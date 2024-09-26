35fd2e4a2fd8fc37ed038b579acc2e434dd48d86
vykes-mac <knrymcleish@gmail.com>
2024-09-25 19:33:27 -0500

    update page selector to include search icon and match design more closely

--- client/hosting/performance/components/PageSelector.tsx
+++ client/hosting/performance/components/PageSelector.tsx
@@ -1,4 +1,4 @@
-import { SearchableDropdown } from '@automattic/components';
+import { Gridicon, SearchableDropdown } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { ComponentProps } from 'react';
 
@@ -10,24 +10,45 @@ export const PageSelector = ( props: ComponentProps< typeof SearchableDropdown >
 			<div css={ { alignSelf: 'stretch', display: 'flex', alignItems: 'center' } }>
 				{ translate( 'Page' ) }
 			</div>
-			<SearchableDropdown
-				{ ...props }
-				className="site-performance__page-selector-drowdown"
-				__experimentalRenderItem={ ( { item } ) => (
-					<div
-						aria-label={ item.label }
-						css={ {
-							display: 'flex',
-							flexDirection: 'column',
-							paddingInline: '16px',
-							paddingBlock: '8px',
-						} }
-					>
-						<span>{ item.label }</span>
-						<span>{ item.path }</span>
-					</div>
-				) }
-			/>
+			<div className="site-performance__page-selector-container">
+				<SearchableDropdown
+					{ ...props }
+					className="site-performance__page-selector-drowdown"
+					__experimentalRenderItem={ ( { item } ) => (
+						<div
+							aria-label={ item.label }
+							css={ {
+								display: 'flex',
+								flexDirection: 'column',
+								paddingInline: '16px',
+								paddingBlock: '8px',
+								fontSize: '0.875rem',
+								gap: '4px',
+							} }
+						>
+							<span>{ item.label }</span>
+							<span>{ item.path }</span>
+						</div>
+					) }
+				/>
+				<div
+					css={ {
+						position: 'absolute',
+						right: '16px',
+						top: '50%',
+						transform: 'translateY(-50%)',
+						display: 'flex',
+						alignSelf: 'start',
+						zIndex: 2,
+						height: '100%',
+						marginTop: '10px',
+						pointerEvents: 'none',
+						color: 'var(--color-neutral-50)',
+					} }
+				>
+					<Gridicon icon="search" size={ 16 } css={ { transform: 'scaleX(-1)' } } />
+				</div>
+			</div>
 		</div>
 	);
 };
