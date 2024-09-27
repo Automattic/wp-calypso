6bb0f91c12492c1956aca0eee89d8f4ea6a1ce9b
Matt West <matthew.west@automattic.com>
2024-09-27 17:25:53 +0100

    Updated wording on QR code login screen.

--- client/login/qr-code-login-page/index.jsx
+++ client/login/qr-code-login-page/index.jsx
@@ -17,7 +17,9 @@ function QrCodeLoginPage( { locale, redirectTo } ) {
 	return (
 		<Main className="qr-code-login-page">
 			<div className="qr-code-login-page__form">
-				<h1 className="qr-code-login-page__heading">{ translate( 'Login via the mobile app' ) }</h1>
+				<h1 className="qr-code-login-page__heading">
+					{ translate( 'Log in with the Jetpack app' ) }
+				</h1>
 				<AsyncLoad
 					require="calypso/blocks/qr-code-login"
 					placeholder={ <QrCodeLoginPlaceholder /> }
