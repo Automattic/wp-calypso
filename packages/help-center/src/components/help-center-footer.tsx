286f2e6397a5ad5cdea17f3b0f3f822b00a3b76a
Tony Arcangelini <tony@arcangelini.com>
2024-09-27 13:39:56 +0200

    Cleanup

--- packages/help-center/src/components/help-center-footer.tsx
+++ packages/help-center/src/components/help-center-footer.tsx
@@ -1,6 +1,7 @@
 import { CardFooter } from '@wordpress/components';
 import { Route, Routes } from 'react-router-dom';
 import { HelpCenterContactButton } from './help-center-contact-page';
+
 import './help-center-footer.scss';
 
 const HelpCenterFooter: React.FC = () => {
