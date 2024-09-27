5fbe9a54a8397959625574d3f55161282ebb308a
Wesley Berrêdo <wberredo@gmail.com>
2024-09-27 14:12:43 -0300

    Performance Profiler: Handle the lang parameter in the URL

--- client/performance-profiler/index.web.ts
+++ client/performance-profiler/index.web.ts
@@ -1,4 +1,5 @@
 import page from '@automattic/calypso-router';
+import { getAnyLanguageRouteParam } from '@automattic/i18n-utils/';
 import { makeLayout, render as clientRender } from 'calypso/controller/index.web';
 import {
 	PerformanceProfilerDashboardContext,
@@ -8,6 +9,23 @@ import {
 } from './controller';
 
 export default function () {
+	const lang = getAnyLanguageRouteParam();
+
+	page(
+		`/${ lang }/speed-test-tool/`,
+		PerformanceProfilerDashboardContext,
+		makeLayout,
+		clientRender
+	);
+	page( `/${ lang }/speed-test-tool/weekly-report`, WeeklyReportContext, makeLayout, clientRender );
+	page(
+		`/${ lang }/speed-test-tool/weekly-report/unsubscribe`,
+		WeeklyReportUnsubscribeContext,
+		makeLayout,
+		clientRender
+	);
+	page( `/${ lang }/speed-test-tool*`, notFound, makeLayout, clientRender );
+
 	page( '/speed-test-tool/', PerformanceProfilerDashboardContext, makeLayout, clientRender );
 	page( '/speed-test-tool/weekly-report', WeeklyReportContext, makeLayout, clientRender );
 	page(
