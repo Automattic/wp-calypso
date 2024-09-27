f494bd05295a3407ee92a2a18b0aa33d39b4f3e5
Tony Arcangelini <tony@arcangelini.com>
2024-09-27 17:32:04 +0200

    Help Center: use Zendesk staging when proxied

--- apps/help-center/config.js
+++ apps/help-center/config.js
@@ -1,6 +1,9 @@
+/* global helpCenterData */
 window.configData = {
-	env_id: 'production',
-	zendesk_support_chat_key: 'cec07bc9-4da6-4dd2-afa7-c7e01ae73584',
+	env_id: helpCenterData?.isProxied ? 'staging' : 'production',
+	zendesk_support_chat_key: helpCenterData?.isProxied
+		? '715f17a8-4a28-4a7f-8447-0ef8f06c70d7'
+		: 'cec07bc9-4da6-4dd2-afa7-c7e01ae73584',
 	features: {
 		'help/gpt-response': true,
 	},
