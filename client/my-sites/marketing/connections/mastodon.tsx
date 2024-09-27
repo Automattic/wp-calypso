63a924358e72a50123434c4200831a1b0a95bf8e
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-09-27 18:03:37 +0530

    Extract common types from Mastodon

--- client/my-sites/marketing/connections/mastodon.tsx
+++ client/my-sites/marketing/connections/mastodon.tsx
@@ -4,6 +4,7 @@ import { useTranslate } from 'i18n-calypso';
 import { useState, FormEvent, ChangeEvent, useEffect } from 'react';
 import FormsButton from 'calypso/components/forms/form-button';
 import FormTextInput from 'calypso/components/forms/form-text-input';
+import { Connection, Service } from './types';
 
 interface Props {
 	service: Service;
@@ -11,45 +12,6 @@ interface Props {
 	connectAnother: () => void;
 	connections: Connection[];
 	isConnecting: boolean;
-}
-
-interface Service {
-	ID: string;
-	connect_URL: string;
-	description: string;
-	external_users_only: boolean;
-	genericon: {
-		class: string;
-		unicode: string;
-	};
-	icon: string;
-	jetpack_module_required: string;
-	jetpack_support: boolean;
-	label: string;
-	multiple_external_user_ID_support: boolean;
-	type: string;
-}
-
-interface Connection {
-	ID: number;
-	site_ID: number;
-	user_ID: number;
-	keyring_connection_ID: number;
-	keyring_connection_user_ID: number;
-	shared: boolean;
-	service: string;
-	label: string;
-	issued: string;
-	expires: string;
-	external_ID: string | null;
-	external_name: string | null;
-	external_display: string | null;
-	external_profile_picture: string | null;
-	external_profile_URL: string | null;
-	external_follower_count: number | null;
-	status: string;
-	refresh_URL: string;
-	meta: object;
 }
 
 const InstanceContainer = styled.div( {
