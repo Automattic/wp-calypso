5a08791d3adbae58dc46d6a89775515200087885
Jorn Postma <75777864+jornp@users.noreply.github.com>
2024-09-27 12:36:33 +0200

    Improve index.node.js
    redirectInvalidLanguage is a no-op on the server. And simply match
    anyLangParam to match the section definition.

--- client/reader/discover/index.node.js
+++ client/reader/discover/index.node.js
@@ -1,11 +1,9 @@
-import { getLanguageRouteParam, getAnyLanguageRouteParam } from '@automattic/i18n-utils';
-import { makeLayout, redirectInvalidLanguage, ssrSetupLocale } from 'calypso/controller';
+import { getAnyLanguageRouteParam } from '@automattic/i18n-utils';
+import { makeLayout, ssrSetupLocale } from 'calypso/controller';
 import { discoverSsr } from './controller';
 
 export default function ( router ) {
-	const langParam = getLanguageRouteParam();
 	const anyLangParam = getAnyLanguageRouteParam();
-	router( `/${ anyLangParam }/discover`, redirectInvalidLanguage );
 
-	router( [ '/discover', `/${ langParam }/discover` ], ssrSetupLocale, discoverSsr, makeLayout );
+	router( [ '/discover', `/${ anyLangParam }/discover` ], ssrSetupLocale, discoverSsr, makeLayout );
 }
