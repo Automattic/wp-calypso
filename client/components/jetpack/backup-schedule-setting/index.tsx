50bd8de901a00bf7be7634dff36f58aa77b5877d
Rafael Agostini <rafael.agostini@automattic.com>
2024-09-27 22:49:39 -0500

    Import styles

--- client/components/jetpack/backup-schedule-setting/index.tsx
+++ client/components/jetpack/backup-schedule-setting/index.tsx
@@ -2,6 +2,7 @@ import { Card } from '@automattic/components';
 import { SelectControl } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import type { FunctionComponent } from 'react';
+import './style.scss';
 
 // Helper function to generate all time slots
 const generateTimeSlots = (): { label: string; value: string }[] => {
@@ -22,14 +23,14 @@ const BackupScheduleSetting: FunctionComponent = () => {
 	const options = generateTimeSlots();
 
 	return (
-		<div className="backup-schedule-setting">
+		<div id="backup-schedule" className="backup-schedule-setting">
 			<Card compact className="setting-title">
 				<h3>{ translate( 'Backup schedule' ) }</h3>
 			</Card>
 			<Card className="setting-content">
 				<p>
 					{ translate(
-						'Pick a timeframe for your backup to run. Some site owners prefer scheduling backups at specific times for better control'
+						'Pick a timeframe for your backup to run. Some site owners prefer scheduling backups at specific times for better control.'
 					) }
 				</p>
 				<SelectControl options={ options } help={ translate( 'Default time' ) } />
