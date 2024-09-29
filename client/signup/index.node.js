983e0f2cd1ec71b64108afba5eeeec5793cb8548
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2024-09-29 19:33:00 +0300

    SSR: Add meta description tag on the Signup section

--- client/signup/index.node.js
+++ client/signup/index.node.js
@@ -1,4 +1,6 @@
 import { getLanguage, getLanguageRouteParam } from '@automattic/i18n-utils';
+import defaultI18n from 'i18n-calypso';
+import { ssrSetupLocale } from 'calypso/controller';
 import { setDocumentHeadMeta } from 'calypso/state/document-head/actions';
 import { getDocumentHeadMeta } from 'calypso/state/document-head/selectors';
 
@@ -13,6 +15,7 @@ export default function ( router ) {
 			`/start/:flowName/:stepName/:stepSectionName/${ lang }`,
 		],
 		setUpLocale,
+		ssrSetupLocale,
 		setupMetaTags
 	);
 }
@@ -29,14 +32,25 @@ function setUpLocale( context, next ) {
 
 // Set up meta tags.
 function setupMetaTags( context, next ) {
+	const i18n = context.i18n || defaultI18n;
+	const translate = i18n.translate.bind( i18n );
+
+	const meta = getDocumentHeadMeta( context.store.getState() );
+	meta.push( {
+		name: 'description',
+		content: translate(
+			'Sign up for a free WordPress.com account to start building your new website. Get access to powerful tools and customizable designs to bring your ideas to life.'
+		),
+	} );
+
 	// All `/start/*` sub-pages should be noindex. See 3065-gh-Automattic/martech.
 	if ( ! /^\/start\/?$/.test( context.pathname ) ) {
-		const meta = getDocumentHeadMeta( context.store.getState() ).concat( {
+		meta.push( {
 			name: 'robots',
 			content: 'noindex',
 		} );
-		context.store.dispatch( setDocumentHeadMeta( meta ) );
 	}
 
+	context.store.dispatch( setDocumentHeadMeta( meta ) );
 	next();
 }
