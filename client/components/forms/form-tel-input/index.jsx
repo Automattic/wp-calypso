9f6b626a0db493b26cdfd750cca4648d4c6ff7a4
Jarda Snajdr <jsnajdr@gmail.com>
2024-09-30 10:11:54 +0200

    FormTelInput: remove functional defaultProps

--- client/components/forms/form-tel-input/index.jsx
+++ client/components/forms/form-tel-input/index.jsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 
-export default function FormTelInput( { className, isError, isValid, ...props } ) {
+export default function FormTelInput( { className, isError = false, isValid = false, ...props } ) {
 	const classes = clsx( 'form-tel-input', className, {
 		'is-error': isError,
 		'is-valid': isValid,
@@ -15,9 +15,4 @@ FormTelInput.propTypes = {
 	className: PropTypes.string,
 	isError: PropTypes.bool,
 	isValid: PropTypes.bool,
-};
-
-FormTelInput.defaultProps = {
-	isError: false,
-	isValid: false,
 };
