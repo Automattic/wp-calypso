670295b8a3fc0d206c2333dd5af03d34eda2230c
Anthony Grullon <agrullon95@gmail.com>
2024-09-30 15:31:08 -0400

    Refactor hosting overview controller to handle redirect query parameter

--- client/hosting/overview/controller.tsx
+++ client/hosting/overview/controller.tsx
@@ -2,8 +2,13 @@ import { Context as PageJSContext } from '@automattic/calypso-router';
 import HostingOverview from 'calypso/hosting/overview/components/hosting-overview';
 import HostingActivate from 'calypso/hosting/server-settings/hosting-activate';
 import Hosting from 'calypso/hosting/server-settings/main';
+import { getQueryArg } from '@wordpress/url';
 
 export function hostingOverview( context: PageJSContext, next: () => void ) {
+	const redirectToQueryParam = String( getQueryArg( context.path, 'redirect_to' ) );
+	if ( redirectToQueryParam ) {
+		return window.location.assign( redirectToQueryParam );
+	}
 	context.primary = <HostingOverview />;
 	next();
 }
