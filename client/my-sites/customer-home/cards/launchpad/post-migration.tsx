9dc6fdbcb11d3bff69b72ed20ad39011fbef5e52
valterlorran <valterlorran@hotmail.com>
2024-10-01 14:36:29 -0300

    Update client/my-sites/customer-home/cards/launchpad/post-migration.tsx
    Co-authored-by: Gabriel Caires <gabriel.caires@automattic.com>

--- client/my-sites/customer-home/cards/launchpad/post-migration.tsx
+++ client/my-sites/customer-home/cards/launchpad/post-migration.tsx
@@ -3,7 +3,7 @@ import LaunchpadPreLaunch from './pre-launch';
 export const LaunchpadPostMigration = (): JSX.Element => {
 	const checklistSlug = 'post-migration';
 
-	return (
+	return <LaunchpadPreLaunch checklistSlug={ checklistSlug } />
 		<>
 			<LaunchpadPreLaunch checklistSlug={ checklistSlug } />
 		</>
