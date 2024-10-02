426076d3e7b526efe0092f9382d8da2bd4b40b3a
Griffith Chen <griffith.chen@automattic.com>
2024-10-02 15:34:04 +0800

    Disable Assembler (#95083)

--- client/data/site-assembler/index.ts
+++ client/data/site-assembler/index.ts
@@ -1,32 +1,3 @@
-import { useExperiment } from 'calypso/lib/explat';
-import { useSelector } from 'calypso/state';
-import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
-
-export const useIsSiteAssemblerEnabledExp = (
-	source: 'design-choices' | 'design-picker' | 'theme-showcase'
-) => {
-	const isLoggedIn = useSelector( ( state ) => isUserLoggedIn( state ) );
-	const [ isLoading, assignment ] = useExperiment( 'calypso_disable_site_assembler' );
-	const variationName = assignment?.variationName;
-
-	if ( isLoading ) {
-		return false;
-	}
-
-	if ( variationName === 'treatment_disable_onboarding' ) {
-		switch ( source ) {
-			case 'theme-showcase':
-				return isLoggedIn;
-			case 'design-choices':
-			case 'design-picker':
-			default:
-				return false;
-		}
-	}
-
-	if ( variationName === 'treatment_disable_all' ) {
-		return false;
-	}
-
-	return isLoggedIn;
+export const useIsSiteAssemblerEnabled = () => {
+	return false;
 };
