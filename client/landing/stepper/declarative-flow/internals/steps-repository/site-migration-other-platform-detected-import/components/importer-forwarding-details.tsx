41892705f99020b383542c0882f0df6a2944d776
Gabriel Caires <gabriel.caires@automattic.com>
2024-10-03 18:47:55 +0100

    Implement platform-detected step

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-other-platform-detected-import/components/importer-forwarding-details.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-other-platform-detected-import/components/importer-forwarding-details.tsx
@@ -1,8 +1,6 @@
 import { NextButton } from '@automattic/onboarding';
 import { useTranslate } from 'i18n-calypso';
 import { FC } from 'react';
-import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-
 interface Props {
 	onSubmit: () => void;
 	onSkip: () => void;
@@ -11,31 +9,27 @@ interface Props {
 export const ImportPlatformForwarder: FC< Props > = ( { onSubmit, onSkip } ) => {
 	const translate = useTranslate();
 
-	const handleSubmit = () => {
-		recordTracksEvent( 'calypso_site_migration_automated_other_platform_import_btn_clicked' );
+	const handleImport = () => {
 		onSubmit();
 	};
 
 	const handleSkip = () => {
-		recordTracksEvent( 'calypso_site_migration_automated_other_platform_import_skip_btn_clicked' );
 		onSkip();
 	};
 
 	return (
-		<form className="site-migration-other-platform__form" onSubmit={ handleSubmit }>
-			<div className="site-migration-other-platform__submit">
-				<NextButton type="submit">{ translate( 'Import Your Content' ) }</NextButton>
-			</div>
+		<div className="site-migration-other-platform__actions">
+			<NextButton type="submit" onClick={ handleImport }>
+				{ translate( 'Import Your Content' ) }
+			</NextButton>
 
-			<div className="site-migration-credentials__skip">
-				<button
-					className="button navigation-link step-container__navigation-link has-underline is-borderless"
-					onClick={ handleSkip }
-					type="button"
-				>
-					{ translate( 'I need help, please guide me' ) }
-				</button>
-			</div>
-		</form>
+			<button
+				className="button navigation-link step-container__navigation-link has-underline is-borderless"
+				onClick={ handleSkip }
+				type="button"
+			>
+				{ translate( 'I need help, please guide me' ) }
+			</button>
+		</div>
 	);
 };
