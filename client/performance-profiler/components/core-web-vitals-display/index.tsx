3ceec7362bd6c2e3aa2253e0872482f2a0045e3b
Philip Jackson <p-jackson@users.noreply.github.com>
2024-10-07 10:19:18 +1300

    Performance Profiler: logged-out profiler uses the same components as the logged-in profiler (#95108)
    * Fix missing dep in useCallback hook
    
    * Ensure "High Impact" badges are translated
    
    * Use V2 components in logged out site performance view
    
    The `showV2` prop has been replaced by `useLoggedInCopy` and
    `overallScoreIsTab` props. Which I think are more semantic, rather than
    a V2 prop to distinguish between the logged in and logged out view.
    
    Separate props are still needed because the components still differ
    between logged in and logged out views. The copy is slightly different
    ("your page" vs. "your site") and the overall score appears above the
    tabs in the logged out view, instead of just another one of the existing
    tabs.
    
    I've also changed how translations are calculated, since it's not best
    practice to inject segment fragments into other sentences.
    
    The old V1 components are still in the repo for now, but I'll remove
    those soon too.
    
    * Fix centering of thumbnail in logged out view
    
    Previously the site preview in the logged-out view was only centered for
    screens smaller than 480px. But the mobile layout kicks in before then.
    
    Removing the breakpoint is safe because the justify-content rule is
    inert in the desktop layout.
    
    * Logged-in/out views use the same insights heading
    
    * Consistently refer to "page" as the thing being tested
    
    It used to be "page" or "site" depending on whether you were logged in
    or not.

--- client/performance-profiler/components/core-web-vitals-display/index.tsx
+++ client/performance-profiler/components/core-web-vitals-display/index.tsx
@@ -1,16 +1,12 @@
 import { useDesktopBreakpoint } from '@automattic/viewport-react';
-import clsx from 'clsx';
 import { useState } from 'react';
 import {
 	Metrics,
 	PerformanceMetricsHistory,
 	PerformanceMetricsItemQueryResponse,
 } from 'calypso/data/site-profiler/types';
-import { CoreWebVitalsAccordion } from '../core-web-vitals-accordion';
 import { CoreWebVitalsAccordionV2 } from '../core-web-vitals-accordion/core-web-vitals-accordion-v2';
-import { MetricTabBar } from '../metric-tab-bar';
 import MetricTabBarV2 from '../metric-tab-bar/metric-tab-bar-v2';
-import { CoreWebVitalsDetails } from './core-web-vitals-details';
 import { CoreWebVitalsDetailsV2 } from './core-web-vitals-details_v2';
 import './style.scss';
 
@@ -18,43 +14,39 @@ type CoreWebVitalsDisplayProps = Record< Metrics, number > & {
 	history: PerformanceMetricsHistory;
 	audits: Record< string, PerformanceMetricsItemQueryResponse >;
 	recommendationsRef: React.RefObject< HTMLDivElement > | null;
-	showV2?: boolean;
+	overallScoreIsTab?: boolean;
 	onRecommendationsFilterChange?: ( filter: string ) => void;
 };
 
 export const CoreWebVitalsDisplay = ( props: CoreWebVitalsDisplayProps ) => {
-	const defaultTab = props.showV2 ? 'overall' : 'fcp';
+	const defaultTab = props.overallScoreIsTab ? 'overall' : 'fcp';
 	const [ activeTab, setActiveTab ] = useState< Metrics | null >( defaultTab );
 	const isDesktop = useDesktopBreakpoint();
 
-	const MetricBar = props.showV2 ? MetricTabBarV2 : MetricTabBar;
-	const CoreWebVitalsDetail = props.showV2 ? CoreWebVitalsDetailsV2 : CoreWebVitalsDetails;
-
-	const Accordion = props.showV2 ? CoreWebVitalsAccordionV2 : CoreWebVitalsAccordion;
+	if ( isDesktop ) {
+		return (
+			<div className="core-web-vitals-display core-web-vitals-display-v2">
+				<MetricTabBarV2
+					activeTab={ activeTab ?? defaultTab }
+					setActiveTab={ setActiveTab }
+					showOverall={ props.overallScoreIsTab }
+					{ ...props }
+				/>
+				<CoreWebVitalsDetailsV2 activeTab={ activeTab } { ...props } />
+			</div>
+		);
+	}
 
 	return (
-		<>
-			{ isDesktop && (
-				<div
-					className={ clsx( 'core-web-vitals-display', {
-						[ 'core-web-vitals-display-v2' ]: props.showV2,
-					} ) }
-				>
-					<MetricBar
-						activeTab={ activeTab ?? defaultTab }
-						setActiveTab={ setActiveTab }
-						{ ...props }
-					/>
-					<CoreWebVitalsDetail activeTab={ activeTab } { ...props } />
-				</div>
-			) }
-			{ ! isDesktop && (
-				<div className="core-web-vitals-display">
-					<Accordion activeTab={ activeTab } setActiveTab={ setActiveTab } { ...props }>
-						<CoreWebVitalsDetail activeTab={ activeTab } { ...props } />
-					</Accordion>
-				</div>
-			) }
-		</>
+		<div className="core-web-vitals-display">
+			<CoreWebVitalsAccordionV2
+				activeTab={ activeTab }
+				setActiveTab={ setActiveTab }
+				showOverall={ props.overallScoreIsTab }
+				{ ...props }
+			>
+				<CoreWebVitalsDetailsV2 activeTab={ activeTab } { ...props } />
+			</CoreWebVitalsAccordionV2>
+		</div>
 	);
 };
