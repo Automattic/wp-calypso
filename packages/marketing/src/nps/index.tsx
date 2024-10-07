75f6bb7efe632af4f8c43c4e8018773b69c729a5
James Tien <southp0105@gmail.com>
2024-10-07 09:49:05 +0800

    Draft all the sections & components in using the standard HTML elements.

--- packages/marketing/src/nps/index.tsx
+++ packages/marketing/src/nps/index.tsx
@@ -1,5 +1,32 @@
-const Nps = () => {
-	return null;
-};
+function NpsScore( score: number ) {
+	return (
+		<label>
+			<input type="radio" name="nps-score" value={ score } />
+			{ score }
+		</label>
+	);
+}
+
+export function Nps() {
+	const scores = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+	return (
+		<form>
+			<h1>How are we doing so far?</h1>
+			<fieldset>
+				<legend>
+					On a scale from 0–10, how likely are you to recommend WordPress.com to a friend or
+					colleague?
+				</legend>
+				<div className="nps-scale">
+					<span className="label-left">Not likely</span>
+					<div className="score-buttons">{ scores.map( ( score ) => NpsScore( score ) ) }</div>
+					<span className="label-right">Definitely</span>
+				</div>
+			</fieldset>
+			<button type="submit">Submit</button>
+		</form>
+	);
+}
 
 export default Nps;
