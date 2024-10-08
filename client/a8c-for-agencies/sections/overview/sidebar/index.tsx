36d1d7e0ae03fd3d36855bac59b366cb08fd4a80
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-10-08 18:58:24 +0800

    Implement and add the 'Growth Accelerator card.

--- client/a8c-for-agencies/sections/overview/sidebar/index.tsx
+++ client/a8c-for-agencies/sections/overview/sidebar/index.tsx
@@ -1,10 +1,12 @@
 import OverviewSidebarContactSupport from './contact-support';
+import OverviewSidebarGrowthAccelerator from './growth-accelator';
 import OverviewSidebarQuickLinks from './quick-links';
 
 const OverviewSidebar = () => {
 	return (
 		<div className="overview-sidebar">
 			<OverviewSidebarQuickLinks />
+			<OverviewSidebarGrowthAccelerator />
 			<OverviewSidebarContactSupport />
 		</div>
 	);
