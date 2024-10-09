9ce5a55c51b751b6332b6b903e3e124cc03f57c2
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-10-09 15:57:37 +0200

    Customize modal close button to match the design

--- client/landing/stepper/declarative-flow/internals/steps-repository/hundred-year-plan-diy-or-difm/schedule-appointment-modal.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/hundred-year-plan-diy-or-difm/schedule-appointment-modal.tsx
@@ -1,8 +1,8 @@
 import config from '@automattic/calypso-config';
 import { UserSelect } from '@automattic/data-stores';
-import { Modal } from '@wordpress/components';
+import { Button, Modal } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
-import { Icon, captureVideo, scheduled } from '@wordpress/icons';
+import { Icon, captureVideo, scheduled, close } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import { USER_STORE } from 'calypso/landing/stepper/stores';
 import CalendlyWidget from '../components/calendy-widget';
@@ -22,68 +22,78 @@ const ScheduleAppointmentModal = ( props: Props ) => {
 	);
 
 	return (
-		<Modal
-			size="large"
-			className="hundred-year-plan-schedule-appointment-modal"
-			shouldCloseOnClickOutside={ false }
-			__experimentalHideHeader
-			onRequestClose={ () => onClose?.() }
-		>
-			<div className="calendly-container">
-				<div className="calendly-details">
-					<div className="calendly-details__header">
-						<HunderYearPlanLogo />
-					</div>
-					<div className="calendly-details__content">
-						<h3>{ translate( 'WordPress.com 100-Year plan' ) }</h3>
-						<h2>
-							{ translate( '%(minutes)s Minute Meeting', {
-								args: {
-									minutes: 30,
-								},
-							} ) }
-						</h2>
-						<div className="calendly-details__item">
-							<span className="icon">
-								<Icon icon={ scheduled } size={ 28 } />
-							</span>
-							<strong>
-								{ translate( '%(minutes)s min', {
+		<>
+			<Button
+				className="hundred-year-plan-schedule-appointment-modal-close"
+				onClick={ () => onClose?.() }
+			>
+				<Icon icon={ close } />
+			</Button>
+			<Modal
+				size="large"
+				className="hundred-year-plan-schedule-appointment-modal"
+				shouldCloseOnClickOutside
+				__experimentalHideHeader
+				onRequestClose={ () => onClose?.() }
+			>
+				<div className="calendly-container">
+					<div className="calendly-details">
+						<div className="calendly-details__header">
+							<HunderYearPlanLogo />
+						</div>
+						<div className="calendly-details__content">
+							<h3>{ translate( 'WordPress.com 100-Year plan' ) }</h3>
+							<h2>
+								{ translate( '%(minutes)s Minute Meeting', {
 									args: {
 										minutes: 30,
 									},
 								} ) }
-							</strong>
+							</h2>
+							<div className="calendly-details__item">
+								<span className="icon">
+									<Icon icon={ scheduled } size={ 28 } />
+								</span>
+								<strong>
+									{ translate( '%(minutes)s min', {
+										args: {
+											minutes: 30,
+										},
+									} ) }
+								</strong>
+							</div>
+							<div className="calendly-details__item">
+								<span className="icon">
+									<Icon icon={ captureVideo } size={ 28 } />
+								</span>
+								<strong>
+									{ translate( 'Web conferencing details provided upon confirmation.' ) }
+								</strong>
+							</div>
+							<p>
+								{ translate(
+									'Join us for an exclusive strategy session where we’ll chart a visionary course for your digital evolution.'
+								) }
+							</p>
 						</div>
-						<div className="calendly-details__item">
-							<span className="icon">
-								<Icon icon={ captureVideo } size={ 28 } />
-							</span>
-							<strong>
-								{ translate( 'Web conferencing details provided upon confirmation.' ) }
-							</strong>
-						</div>
-						<p>
-							{ translate(
-								'Join us for an exclusive strategy session where we’ll chart a visionary course for your digital evolution.'
-							) }
-						</p>
 					</div>
+					<CalendlyWidget
+						url={ config( '100_year_plan_calendly_id' ) }
+						prefill={
+							currentUser
+								? { name: currentUser?.display_name, email: currentUser?.email }
+								: undefined
+						}
+						hideLandingPageDetails
+						hideEventTypeDetails
+						hideGdprBanner
+						onSchedule={ () => {
+							// TODO: submit
+						} }
+					/>
 				</div>
-				<CalendlyWidget
-					url={ config( '100_year_plan_calendly_id' ) }
-					prefill={
-						currentUser ? { name: currentUser?.display_name, email: currentUser?.email } : undefined
-					}
-					hideLandingPageDetails
-					hideEventTypeDetails
-					hideGdprBanner
-					onSchedule={ () => {
-						// TODO: submit
-					} }
-				/>
-			</div>
-		</Modal>
+			</Modal>
+		</>
 	);
 };
 
