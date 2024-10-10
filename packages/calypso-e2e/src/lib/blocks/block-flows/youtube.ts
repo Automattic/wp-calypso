91c90dae3ae7b79b5ed4a322be600bb000a273de
Imran Hossain <imranh920@gmail.com>
2024-10-10 21:12:49 +0600

    Fix issues on blocks test for Gutenberg

--- packages/calypso-e2e/src/lib/blocks/block-flows/youtube.ts
+++ packages/calypso-e2e/src/lib/blocks/block-flows/youtube.ts
@@ -29,7 +29,6 @@ export class YouTubeBlockFlow implements BlockFlow {
 	}
 
 	blockSidebarName = 'YouTube Embed';
-	blockTestFallBackName = 'YouTube';
 	blockEditorSelector = blockParentSelector;
 
 	/**
