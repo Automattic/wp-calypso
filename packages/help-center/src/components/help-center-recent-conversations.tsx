a13225c5b202ec87fce2c1355f04422619f88af7
heavyweight <kpapazov@gmail.com>
2024-10-11 15:34:16 +0200

    Calculate unread conversations and messages

--- packages/help-center/src/components/help-center-recent-conversations.tsx
+++ packages/help-center/src/components/help-center-recent-conversations.tsx
@@ -16,16 +16,45 @@ const GetSectionName = ( unreadCount: number ) => {
 	return __( 'Recent Conversation', __i18n_text_domain__ );
 };
 
+const calculateUnread = ( conversations ) => {
+	let unreadConversations = 0;
+	let unreadMessages = 0;
+
+	conversations.forEach( ( conversation ) => {
+		let currentUnreadMessages = 0;
+		conversation.messages.forEach( ( message ) => {
+			if ( message.received > conversation.businessLastRead ) {
+				currentUnreadMessages++;
+			}
+		} );
+
+		unreadMessages += currentUnreadMessages;
+
+		if ( currentUnreadMessages > 0 ) {
+			unreadConversations++;
+		}
+	} );
+
+	return { unreadConversations, unreadMessages };
+};
+
 const HelpCenterRecentConversations: React.FC = () => {
 	const { __ } = useI18n();
 	const { init, getConversations } = useSmooch();
 	const [ conversations, setConversations ] = useState( [] );
+	const [ unreadConversationsCount, setUnreadConversationsCount ] = useState( 0 );
+	const [ unreadMessagesCount, setUnreadMessagesCount ] = useState( 0 );
 
 	useEffect( () => {
 		if ( init ) {
-			setConversations( getConversations() );
+			const conversations = getConversations();
+			const { unreadConversations, unreadMessages } = calculateUnread( conversations );
+
+			setUnreadConversationsCount( unreadConversations );
+			setUnreadMessagesCount( unreadMessages );
+			setConversations( conversations );
 		}
-	}, [ getConversations, init ] );
+	}, [ init, getConversations ] );
 
 	if ( ! conversations ) {
 		return [];
@@ -34,18 +63,14 @@ const HelpCenterRecentConversations: React.FC = () => {
 	const lastConversation = conversations[ conversations.length - 1 ];
 	const lastMessage = lastConversation?.messages[ lastConversation?.messages.length - 1 ];
 
-	// Testing values here
-	const unreadConversations = 0;
-	const unreadMessages = 0;
-
 	const multipleUnreadMessages = {
 		text: 'Multiple Unread Messages',
-		received: 1631616000,
+		received: lastMessage?.received,
 		displayName: sprintf(
 			/* translators: %1$s is total number of unread messages, %2$s is the total number of chats with unread messages */
 			__( '%1$s messages from %2$s chats', __i18n_text_domain__ ),
-			unreadMessages,
-			unreadConversations
+			unreadMessagesCount,
+			unreadConversationsCount
 		),
 		avatarUrl: 'https://secure.gravatar.com/avatar/0e5d5a8e3d1c0d8d3d1c0d8d3d1c0d8d',
 		id: uuid(),
@@ -54,20 +79,20 @@ const HelpCenterRecentConversations: React.FC = () => {
 	// const chatMessageKey = lastMessage.id;
 	let chatMessage = lastMessage;
 
-	if ( unreadConversations > 1 ) {
+	if ( unreadConversationsCount > 1 ) {
 		chatMessage = multipleUnreadMessages;
 	}
 
 	return (
 		<div className="help-center-homepage-conversations">
 			<h3 className="help-center-search-results__title help-center__section-title">
-				{ GetSectionName( unreadConversations ) }
+				{ GetSectionName( unreadConversationsCount ) }
 			</h3>
 			{ lastMessage ? (
 				<HelpCenterSupportChatMessage
 					key={ lastConversation.id }
 					message={ chatMessage }
-					isUnread={ unreadMessages > 0 }
+					isUnread={ unreadMessagesCount > 0 }
 					navigateTo="odie"
 				/>
 			) : null }
