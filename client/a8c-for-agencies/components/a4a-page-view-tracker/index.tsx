c2dc5cc797fa59d1f412007acf4d539b29cff076
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-10-14 17:05:23 +0800

    Update page tracker to extract query parameters and pass as properties and path to only include base path.

--- client/a8c-for-agencies/components/a4a-page-view-tracker/index.tsx
+++ client/a8c-for-agencies/components/a4a-page-view-tracker/index.tsx
@@ -8,11 +8,20 @@ type Props = {
 };
 
 export default function A4APageViewTracker( { title, path, properties }: Props ) {
+	// Split the path into its components
+	const [ basePath, queryString ] = path.split( /[?]/ );
+
+	// Parse query string into an object
+	const queryParams = queryString ? Object.fromEntries( new URLSearchParams( queryString ) ) : {};
+
 	return (
 		<PageViewTracker
 			title={ title }
-			path={ path }
-			properties={ properties }
+			path={ basePath }
+			properties={ {
+				...queryParams,
+				...properties,
+			} }
 			options={ { useA8CForAgenciesGoogleAnalytics: true } }
 		/>
 	);
