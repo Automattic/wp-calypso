f4a051f265caa93e443091720bea0bc5d1e02d60
Dusty Reagan <dusty@dustyreagan.com>
2024-10-15 08:40:38 -0500

    Reader Onboarding: List suggested sites in "step 2" modal (#95301)
    * Lists suggested sites in modal
    
    * Add site_recs_per_card though it seems inert
    
    * Remove sort
    
    * Use data in recommended_blogs response field
    
    * Overlay currated suggestions over the API suggestions
    
    * Use multiple files for curated blogs

---
+++ client/reader/onboarding/curated-blogs/society.tsx
@@ -0,0 +1,31 @@
+import { CuratedBlogsList } from './index';
+
+export const societyBlogs: CuratedBlogsList = {
+	education: [
+		// Add more education blogs...
+	],
+	nature: [
+		// Add nature blogs...
+	],
+	future: [
+		// Add future-focused blogs...
+	],
+	politics: [
+		// Add politics blogs...
+	],
+	'climate-change': [
+		// Add climate change blogs...
+	],
+	history: [
+		// Add history blogs...
+	],
+	society: [
+		// Add society blogs...
+	],
+	culture: [
+		// Add culture blogs...
+	],
+	philosophy: [
+		// Add philosophy blogs...
+	],
+};
