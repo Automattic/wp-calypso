9bdc21e93c8ea5f73244a261a57dba3b6c7a2ce8
escapemanuele <escapemanuele@gmail.com>
2024-10-15 10:49:57 +0200

    Simplify function

--- packages/help-center/src/hooks/use-site-editor-urls.ts
+++ packages/help-center/src/hooks/use-site-editor-urls.ts
@@ -5,25 +5,13 @@ const panels = [ 'root', 'wp_global_styles' ] as const;
 
 export function useSiteEditorUrls() {
 	const { site } = useHelpCenterContext();
-	const returnUrl = window.location.href;
 
 	const siteEditorUrl = `${ site?.options.admin_url }site-editor.php`;
 
-	return panels.reduce(
-		( acc, panel ) => {
-			let url = siteEditorUrl;
-			if ( panel !== 'root' ) {
-				url = addQueryArgs( siteEditorUrl, {
-					path: `/${ panel }`,
-				} );
-			}
-
-			acc[ panel ] = addQueryArgs( url, {
-				return: returnUrl,
-			} );
-
-			return acc;
-		},
-		{} as Record< ( typeof panels )[ number ], string >
-	);
+	return Object.fromEntries(
+		panels.map( ( panel ) => [
+			panel,
+			panel === 'root' ? siteEditorUrl : addQueryArgs( siteEditorUrl, { path: `/${ panel }` } ),
+		] )
+	) as Record< ( typeof panels )[ number ], string >;
 }
