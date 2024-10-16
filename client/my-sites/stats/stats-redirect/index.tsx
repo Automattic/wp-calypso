1bf5895a4b2d12e906bfa20e01cbe00a1630efc3
Dognose <dognose24@gmail.com>
2024-10-17 01:08:22 +0800

    Remove notice id focus_jetpack_purchase for force redirection

--- client/my-sites/stats/stats-redirect/index.tsx
+++ client/my-sites/stats/stats-redirect/index.tsx
@@ -1,14 +1,7 @@
-import { ReactNode, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNoticeVisibilityQuery } from 'calypso/my-sites/stats/hooks/use-notice-visibility-query';
+import { ReactNode } from 'react';
 import { useSelector } from 'calypso/state';
 import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
-import {
-	receiveStatNoticeSettings,
-	requestStatNoticeSettings,
-} from 'calypso/state/stats/notices/actions';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import StatsLoader from './stats-loader';
 
 interface StatsRedirectFlowProps {
 	children: ReactNode;
@@ -27,39 +20,10 @@ const StatsRedirectFlow: React.FC< StatsRedirectFlowProps > = ( { children } ) =
 		canCurrentUser( state, siteId, 'view_stats' )
 	);
 
-	const { isLoading: isLoadingNotices, data: purchaseNotPostponed } = useNoticeVisibilityQuery(
-		siteId,
-		'focus_jetpack_purchase',
-		canUserManageOptions
-	);
-
-	// TODO: If notices are not used by class components, we don't have any reasons to launch any of those actions anymore. If we do need them, we should consider refactoring them to another component.
-	const dispatch = useDispatch();
-	useEffect( () => {
-		if ( isLoadingNotices ) {
-			// when react-query is fetching data
-			dispatch( requestStatNoticeSettings( siteId ) );
-		} else {
-			dispatch(
-				receiveStatNoticeSettings( siteId, {
-					focus_jetpack_purchase: purchaseNotPostponed,
-				} )
-			);
-		}
-	}, [ dispatch, siteId, isLoadingNotices, purchaseNotPostponed ] );
-
-	const shouldRenderContent = ! isLoadingNotices && ( canUserViewStats || canUserManageOptions );
-
-	// Handle possible render conditions.
-	// Based on render conditions, loading state takes priority.
-	if ( isLoadingNotices ) {
-		return <StatsLoader />;
-	}
-
 	// Default is to show the user some stats.
 	// There are permissions considerations though, in which case we fall
 	// through and show nothing. Feels broken.
-	if ( shouldRenderContent ) {
+	if ( canUserViewStats || canUserManageOptions ) {
 		return <>{ children }</>;
 	}
 
