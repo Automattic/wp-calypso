78a61cc702bff12b2f5da609b41047521e87d8f5
Paulo Cruz <paulo.cruz@automattic.com>
2024-10-16 14:16:03 -0300

    Refresh admin menu when updating comment status through notification

--- apps/notifications/src/panel/state/notes/thunks/spam-note.js
+++ apps/notifications/src/panel/state/notes/thunks/spam-note.js
@@ -1,8 +1,9 @@
+import { requestAdminMenu } from 'calypso/state/admin-menu/actions';
 import { recordTracksEvent } from '../../../helpers/stats';
 import { spamNote as spamNoteAction } from '../actions';
 import bumpStat from '../utils/bump-stat';
 
-const spamNote = ( note, restClient ) => ( dispatch ) => {
+const spamNote = ( note, siteId, restClient ) => ( dispatch ) => {
 	bumpStat( 'spam-comment' );
 	recordTracksEvent( 'calypso_notification_note_spam', {
 		note_type: note.type,
@@ -10,6 +11,9 @@ const spamNote = ( note, restClient ) => ( dispatch ) => {
 
 	dispatch( spamNoteAction( note.id ) );
 	restClient.global.updateUndoBar( 'spam', note );
+
+	// Refresh the admin menu on update of status to ensure count shown is not stale
+	dispatch( requestAdminMenu( siteId ) );
 };
 
 export default spamNote;
