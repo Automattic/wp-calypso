126d5da107ad3409b88846d9d411654a94286b1b
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-10-16 14:48:16 +0200

    Handle the "Single Posts" command so it's properly redirected to wp admin

--- apps/wpcom-block-editor/src/calypso/features/iframe-bridge-server.js
+++ apps/wpcom-block-editor/src/calypso/features/iframe-bridge-server.js
@@ -1087,6 +1087,59 @@ function handleAddPost( calypsoPort ) {
 	handleWpAdminRedirect( { calypsoPort, path: 'post-new.php', title: __( 'Add new post' ) } );
 }
 
+/**
+ * @external HTMLDivElement
+ */
+
+/**
+ * Extracts the post ID from the data-value attribute of the "Single Posts" command item,
+ * and encodes it for use in a URL.
+ * Example: data-value="Single Posts pub/fewer//single" -> "pub/fewer//single"
+ * Example: data-value="Single Posts pub/twentytwentyfour//single" -> "pub/twentytwentyfour//single"
+ * @param {HTMLDivElement} commandItem The command item element.
+ * @returns string
+ */
+function extractPostIdFromDataValueAttribute( commandItem ) {
+	const value = commandItem.getAttribute( 'data-value' );
+	const theme = value.replace( __( 'Single Posts' ), '' ).trim();
+
+	return encodeURIComponent( theme );
+}
+
+function handleSinglePosts( calypsoPort ) {
+	const selector = `[data-value^="${ __( 'Single Posts' ) }"]`;
+
+	const postMessage = ( commandItem ) => {
+		const postId = extractPostIdFromDataValueAttribute( commandItem );
+
+		calypsoPort.postMessage( {
+			action: 'wpAdminRedirect',
+			payload: {
+				destinationUrl: `/wp-admin/site-editor.php?postType=wp_template&postId=${ postId }&canvas=edit`,
+				unsavedChanges: select( 'core/editor' ).isEditedPostDirty(),
+			},
+		} );
+	};
+
+	const clickCallback = ( e ) => {
+		e.preventDefault();
+
+		const commandItem = e.target.closest( '[cmdk-item]' );
+		postMessage( commandItem );
+	};
+
+	addEditorListener( selector, clickCallback );
+
+	const enterCallback = ( e ) => {
+		e.preventDefault();
+
+		const commandItem = document.querySelector( '[data-selected=true]' );
+		postMessage( commandItem );
+	};
+
+	addCommandsInputListener( selector, enterCallback );
+}
+
 function initPort( message ) {
 	if ( 'initPort' !== message.data.action ) {
 		return;
@@ -1193,6 +1246,8 @@ function initPort( message ) {
 		handleAddPage( calypsoPort );
 
 		handleAddPost( calypsoPort );
+
+		handleSinglePosts( calypsoPort );
 	}
 
 	window.removeEventListener( 'message', initPort, false );
