4434b9ba44bf60cc3109e715f529b7d8e5d86f1e
Chris Holder <chris.holder@automattic.com>
2024-10-18 15:45:05 -0500

    Add health blogs

--- client/reader/onboarding/curated-blogs/lifestyle.tsx
+++ client/reader/onboarding/curated-blogs/lifestyle.tsx
@@ -2,7 +2,42 @@ import { CuratedBlogsList } from './index';
 
 export const lifestyleBlogs: CuratedBlogsList = {
 	health: [
-		// Add more health blogs...
+		{
+			feed_ID: 27232798,
+			site_ID: 62003347,
+			site_URL: 'https://phoebemd.com/',
+			site_name: 'HEALTH + INSPIRATION',
+		},
+		{
+			feed_ID: 99853252,
+			site_ID: 166484180,
+			site_URL: 'https://healthtipsnow.net/',
+			site_name: 'Health Tips Now',
+		},
+		{
+			feed_ID: 71988509,
+			site_ID: 133681874,
+			site_URL: 'https://newcancerinfo.com/',
+			site_name: "Dr. Michael Hunter's Wellness",
+		},
+		{
+			feed_ID: 158840488,
+			site_ID: 233388913,
+			site_URL: 'http://medhelp-ai.com/',
+			site_name: 'MedHelp',
+		},
+		{
+			feed_ID: 9031944,
+			site_ID: 0,
+			site_URL: 'https://chriskresser.com/',
+			site_name: 'Chris Kresser',
+		},
+		{
+			feed_ID: 122109,
+			site_ID: 0,
+			site_URL: 'https://www.kevinmd.com/',
+			site_name: 'KevinMD.com',
+		},
 	],
 	'personal-finance': [
 		// Add more personal finance blogs...
