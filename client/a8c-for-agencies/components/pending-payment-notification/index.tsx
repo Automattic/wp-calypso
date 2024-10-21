c27426f6ae9d170c567bf10b5825f97d211e1ea1
Andrii Lysenko <60262784+andrii-lysenko@users.noreply.github.com>
2024-10-21 15:51:16 -0700

    A4A: update unpaid invoice banner (#95503)

--- client/a8c-for-agencies/components/pending-payment-notification/index.tsx
+++ client/a8c-for-agencies/components/pending-payment-notification/index.tsx
@@ -1,54 +1,89 @@
 import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
+import { useMemo } from 'react';
 import LayoutBanner from 'calypso/a8c-for-agencies/components/layout/banner';
-import { useDispatch, useSelector } from 'calypso/state';
-import { getActiveAgency } from 'calypso/state/a8c-for-agencies/agency/selectors';
-import { setPreference } from 'calypso/state/preferences/actions';
-import { getPreference } from 'calypso/state/preferences/selectors';
+import useFetchInvoices from 'calypso/a8c-for-agencies/data/purchases/use-fetch-invoices';
 import { CONTACT_URL_HASH_FRAGMENT } from '../a4a-contact-support-widget';
 import { A4A_INVOICES_LINK } from '../sidebar-menu/lib/constants';
 
 import './style.scss';
 
-const PENDING_PAYMENT_NOTIFICATION_DISMISS_PREFERENCE = 'pending-payment-notification-dismissed';
+// TODO: Uncomment this if we would want to dismiss the notification.
+// const PENDING_PAYMENT_NOTIFICATION_DISMISS_PREFERENCE = 'pending-payment-notification-dismissed';
 
 export default function PendingPaymentNotification() {
-	const dispatch = useDispatch();
-	const agency = useSelector( getActiveAgency );
-
-	const isDismissed = useSelector( ( state ) =>
-		getPreference( state, PENDING_PAYMENT_NOTIFICATION_DISMISS_PREFERENCE )
-	);
-
-	const canIssueLicenses = agency?.can_issue_licenses ?? true;
+	const invoices = useFetchInvoices( { starting_after: '', ending_before: '' }, undefined, 'open' );
 
 	const translate = useTranslate();
 
-	// If the agency can issue licenses or the notification has been dismissed, do not show the notification.
-	if ( canIssueLicenses || isDismissed ) {
+	const daysDue = useMemo( () => {
+		const oldestUnpaidInvoice =
+			invoices?.data?.items.reduce( ( oldest, current ) => {
+				return oldest < current.created ? oldest : current.created;
+			}, Infinity ) || 0;
+		const nowInSeconds = Math.floor( Date.now() / 1000 ); // Get current time in seconds
+		const differenceInSeconds = nowInSeconds - oldestUnpaidInvoice;
+		return Math.floor( differenceInSeconds / ( 60 * 60 * 24 ) ); // Convert seconds to days
+	}, [ invoices ] );
+	const daysLeft = 28 - daysDue;
+
+	if ( ! invoices?.isSuccess || ! invoices?.data?.items?.length || daysDue < 7 ) {
 		return null;
+	}
+
+	const title =
+		daysDue < 21 ? translate( 'Overdue invoice' ) : translate( 'Urgent: Overdue invoice' );
+
+	const level = daysDue < 21 ? 'warning' : 'error';
+
+	let description;
+
+	if ( daysDue < 14 ) {
+		description = translate(
+			'Your invoice is %(daysDue)d days overdue. Please pay as soon as possible to avoid service disruption.',
+			{
+				args: { daysDue },
+				comment: '%(daysDue)d is the number of days invoice is overdue.',
+			}
+		);
+	} else if ( daysDue < 21 ) {
+		description = translate(
+			'Your invoice is %(daysDue)d days overdue. You can’t issue new licenses until it’s paid.',
+			{
+				args: { daysDue },
+				comment: '%(daysDue)d is the number of days invoice is overdue.',
+			}
+		);
+	} else if ( daysDue < 27 ) {
+		description = translate(
+			'Your invoice is %(daysDue)d days overdue. We’ll revoke your active licenses if we don’t receive payment in %(daysLeft)d days.',
+			{
+				args: { daysDue, daysLeft },
+				comment: '%(daysDue)d is the number of days invoice is overdue.',
+			}
+		);
+	} else if ( daysDue < 28 ) {
+		description = translate(
+			'Your invoice is %(daysDue)d days overdue. We’ll revoke your active licenses tomorrow if we don’t receive payment.',
+			{
+				args: { daysDue },
+				comment: '%(daysDue)d is the number of days invoice is overdue.',
+			}
+		);
+	} else {
+		description = translate(
+			'Your product licenses have now been revoked. If you want to continue using the plaform, please pay your outstanding invoices.'
+		);
 	}
 
 	return (
 		<LayoutBanner
 			className="pending-payment-notification"
-			level="warning"
-			title={ translate( 'Payment reminder' ) }
-			onClose={ () =>
-				// Dismiss the notification temporarily so that it shows again on the next page load.
-				dispatch( setPreference( PENDING_PAYMENT_NOTIFICATION_DISMISS_PREFERENCE, true ) )
-			}
+			level={ level }
+			title={ title }
+			hideCloseButton
 		>
-			<div>
-				{ translate(
-					'Your payment for the latest invoice is now overdue. To continue purchasing products and maintain uninterrupted services,{{br/}}please make your payment as soon as possible.',
-					{
-						components: {
-							br: <br />,
-						},
-					}
-				) }
-			</div>
+			<div>{ description }</div>
 			<Button className="is-dark" href={ A4A_INVOICES_LINK }>
 				{ translate( 'Pay invoice' ) }
 			</Button>
