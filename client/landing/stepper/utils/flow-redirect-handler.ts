1295789cafe1131abffe3423a85b1da8eccb4033
Richard Ortiz <rcrd.ortiz@gmail.com>
2024-10-22 16:24:43 +0200

    Removed subflow matches since we do not need them at the moment

--- client/landing/stepper/utils/flow-redirect-handler.ts
+++ client/landing/stepper/utils/flow-redirect-handler.ts
@@ -2,66 +2,46 @@ import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 
 // Flows to redirect
 const redirectRoutes = [
-	{ flow: '/setup/blog/', to: '/start:lang?' },
-	{ flow: '/setup/free/', to: '/start/free:lang?' },
-	{ flow: '/setup/link-in-bio/', to: '/start:lang?' },
-	{ flow: '/setup/videopress/', to: '/start:lang?' },
-	{ flow: '/setup/sensei/', to: ':lang?/plugins/sensei-pro/' },
+	{ flow: '/setup/blog', to: '/start:lang?' },
+	{ flow: '/setup/free', to: '/start/free:lang?' },
+	{ flow: '/setup/link-in-bio', to: '/start:lang?' },
+	{ flow: '/setup/videopress', to: '/start:lang?' },
+	{ flow: '/setup/sensei', to: ':lang?/plugins/sensei-pro/' },
 ];
-
-// Sub-flow parts to match
-const subFlows = [
-	'freeSetup',
-	'processing',
-	'create-site',
-	'launchpad',
-	'designSetup',
-	'pattern-assembler',
-	'error',
-];
-
-// Build the regex pattern to match
-const routePaths = redirectRoutes.map( ( route ) => route.flow.replace( /\/$/, '' ) ); // Removing trailing slash
-
-// Join the subFlows with '|' to create an "OR" regex pattern, making it optional
-const flowsPattern = `(?:/(${ subFlows.join( '|' ) }))?`;
 
 // Regex pattern for the optional language code in the format xx or xx-yy
-const langPattern = '(?:/([a-z]{2}(?:-[a-z]{2})?))?';
-
-// Build the complete regex to match the routes
-const regex = new RegExp( `^(${ routePaths.join( '|' ) })${ flowsPattern }${ langPattern }/?$` );
+const langPattern = '(?:/([a-z]{2}(?:-[a-z]{2})?))?/?$';
 
 // Test against a location pathname and build a redirect URL
 const redirectPathIfNecessary = ( pathname: string, search: string ) => {
 	// Find the matching redirect route
-	const match = pathname.match( regex );
-	if ( match ) {
-		const [ , baseRoute, , lang ] = match;
+	const route = redirectRoutes.find( ( redirect ) => pathname.startsWith( redirect.flow ) );
 
-		// Find the corresponding redirect rule
-		const route = redirectRoutes.find( ( route ) => route.flow === `${ baseRoute }/` );
-		if ( route ) {
-			// Replace the ":lang?" placeholder in the "to" field with the matched language or empty string if not present
-			const redirectUrl = route.to.replace( ':lang?', lang ? `/${ lang }` : '' );
-
-			// Construct the final URL with search parameters if present
-			const finalUrl = `${ redirectUrl }${ search }`;
-
-			// Track the redirect event
-			recordTracksEvent( 'calypso_tailored_flows_redirect', {
-				redirectFromUrl: location.pathname + location.search,
-				redirectTo: finalUrl,
-				referrer: document.referrer,
-			} );
-
-			// Perform the actual redirection
-			window.location.href = finalUrl;
-
-			return true;
-		}
+	// If no route is found we don't redirect and return false
+	if ( ! route ) {
+		return false;
 	}
-	return false;
+
+	// Find the language code in the pathname if present
+	const [ , lang ] = pathname.match( langPattern ) ?? [];
+
+	// Replace the ":lang?" placeholder in the "to" field with the matched language or empty string if not present
+	const redirectUrl = route.to.replace( ':lang?', lang ? `/${ lang }` : '' );
+
+	// Construct the final URL with search parameters if present
+	const finalUrl = `${ redirectUrl }${ search }`;
+
+	// Track the redirect event
+	recordTracksEvent( 'calypso_tailored_flows_redirect', {
+		redirectFromUrl: location.pathname + location.search,
+		redirectTo: finalUrl,
+		referrer: document.referrer,
+	} );
+
+	// Perform the actual redirection
+	window.location.href = finalUrl;
+
+	return true;
 };
 
 export default redirectPathIfNecessary;
