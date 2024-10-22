2f6493283a199613af3ea9bcbb7a4d08545feced
Yashwin <yashwinpoojary@gmail.com>
2024-10-22 11:54:06 +0530

    Implement agency tier celebration modal responsiveness

--- client/a8c-for-agencies/sections/overview/sidebar/agency-tier/celebration-modal.tsx
+++ client/a8c-for-agencies/sections/overview/sidebar/agency-tier/celebration-modal.tsx
@@ -1,6 +1,8 @@
+import { useBreakpoint } from '@automattic/viewport-react';
 import { Button } from '@wordpress/components';
 import { clsx } from 'clsx';
 import { useTranslate } from 'i18n-calypso';
+import { useEffect, useRef, useState } from 'react';
 import A4AThemedModal from 'calypso/a8c-for-agencies/components/a4a-themed-modal';
 import { A4A_AGENCY_TIER_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import { useDispatch, useSelector } from 'calypso/state';
@@ -21,10 +23,38 @@ export default function AgencyTierCelebrationModal( {
 } ) {
 	const dispatch = useDispatch();
 	const translate = useTranslate();
+	const isNarrowView = useBreakpoint( '<660px' );
 
 	const celebrationModalShowForCurrentType = useSelector( ( state ) =>
 		getPreference( state, PREFERENCE_NAME )
 	);
+
+	const benefitslistRef = useRef< HTMLDivElement >( null );
+	const [ isOverflowing, setIsOverflowing ] = useState( false );
+
+	useEffect( () => {
+		const currentRef = benefitslistRef?.current;
+
+		const checkVerticalOverflow = () => {
+			if ( currentRef ) {
+				const { scrollTop, scrollHeight, clientHeight } = currentRef;
+				// Determine if the user is at the bottom of the list (allow some leeway with a small threshold)
+				if ( scrollHeight > clientHeight && scrollTop < scrollHeight - clientHeight - 1 ) {
+					setIsOverflowing( true );
+				} else {
+					setIsOverflowing( false );
+				}
+			}
+		};
+
+		checkVerticalOverflow(); // Initial check on component mount
+
+		currentRef?.addEventListener( 'scroll', checkVerticalOverflow );
+
+		return () => {
+			currentRef?.removeEventListener( 'scroll', checkVerticalOverflow );
+		};
+	}, [] );
 
 	if ( ! agencyTierInfo || celebrationModalShowForCurrentType === currentAgencyTier ) {
 		return null;
@@ -34,28 +64,43 @@ export default function AgencyTierCelebrationModal( {
 		dispatch( savePreference( PREFERENCE_NAME, currentAgencyTier ?? '' ) );
 	};
 
-	const { title, description, extraDescription, benefits, video } = agencyTierInfo.celebrationModal;
+	const { title, description, extraDescription, benefits, video, image } =
+		agencyTierInfo.celebrationModal;
 
 	return (
 		<A4AThemedModal
-			className={ clsx( 'agency-tier-celebration-modal', currentAgencyTier ) }
-			modalVideo={ <video src={ video } preload="auto" width={ 470 } loop muted autoPlay /> }
+			className={ clsx( 'agency-tier-celebration-modal', currentAgencyTier, {
+				'is-narrow-view': isNarrowView,
+			} ) }
+			modalVideo={
+				! isNarrowView ? (
+					<video src={ video } preload="auto" width={ 470 } loop muted autoPlay />
+				) : undefined
+			}
+			modalImage={ isNarrowView ? image : undefined }
 			onClose={ handleOnClose }
 			dismissable
 		>
-			<div className="agency-tier-celebration-modal__content">
-				<div className="agency-tier-celebration-modal__title">{ title }</div>
-				<div className="agency-tier-celebration-modal__description">{ description }</div>
-				{ extraDescription && (
-					<div className="agency-tier-celebration-modal__extra-description">
-						{ extraDescription }
-					</div>
-				) }
-				<ul className="agency-tier-celebration-modal__benefits">
-					{ benefits.map( ( benefit ) => (
-						<li key={ benefit }>{ benefit }</li>
-					) ) }
-				</ul>
+			<div className="agency-tier-celebration-modal__content-wrapper">
+				<div className="agency-tier-celebration-modal__content" ref={ benefitslistRef }>
+					<div className="agency-tier-celebration-modal__title">{ title }</div>
+					<div className="agency-tier-celebration-modal__description">{ description }</div>
+					{ extraDescription && (
+						<div className="agency-tier-celebration-modal__extra-description">
+							{ extraDescription }
+						</div>
+					) }
+					<ul className="agency-tier-celebration-modal__benefits">
+						{ benefits.map( ( benefit ) => (
+							<li key={ benefit }>{ benefit }</li>
+						) ) }
+					</ul>
+				</div>
+				<div
+					className={ clsx( 'agency-tier-celebration-modal__scroll-indicator', {
+						'is-overflowing': isOverflowing,
+					} ) }
+				></div>
 			</div>
 			<div className="agency-tier-celebration-modal__footer">
 				<Button variant="primary" onClick={ handleOnClose } href={ A4A_AGENCY_TIER_LINK }>
