83c5929387af5042aacb716b87afbaec927bfd3f
escapemanuele <escapemanuele@gmail.com>
2024-10-23 16:40:13 +0200

    Fix new wapuu when no zendesk

--- packages/odie-client/src/context/use-load-previous-chat.ts
+++ packages/odie-client/src/context/use-load-previous-chat.ts
@@ -32,6 +32,11 @@ export const useLoadPreviousChat = ( {
 			const initialMessage = getOdieInitialMessage( botNameSlug, odieInitialPromptText );
 			const messages = [ initialMessage, ...( existingChat as Chat ).messages ];
 
+			setChat( {
+				...existingChat,
+				messages,
+			} );
+
 			getZendeskConversation( {
 				chatId: existingChat?.chat_id,
 				conversationId: selectedConversationId,
