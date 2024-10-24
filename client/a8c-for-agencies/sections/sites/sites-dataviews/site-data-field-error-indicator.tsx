4148e93725515b1083f0abd3e358535ba3bc6ba3
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-10-24 18:33:48 +0800

    Show popover when hovering the indicator.

--- client/a8c-for-agencies/sections/sites/sites-dataviews/site-data-field-error-indicator.tsx
+++ client/a8c-for-agencies/sections/sites/sites-dataviews/site-data-field-error-indicator.tsx
@@ -1,5 +1,6 @@
-import { Gridicon } from '@automattic/components';
+import { Gridicon, Popover } from '@automattic/components';
 import clsx from 'clsx';
+import { useRef, useState } from 'react';
 import { SiteError } from '../types';
 
 type Props = {
@@ -7,6 +8,9 @@ type Props = {
 };
 
 export default function SiteDataFieldErrorIndicator( { errors }: Props ) {
+	const popoverContext = useRef( null );
+	const [ isPopoverVisible, setIsPopoverVisible ] = useState( false );
+
 	const highestSeverity =
 		errors?.reduce( ( prev, current ) => {
 			const severityOrder: Record< string, number > = { high: 3, medium: 2, low: 1 };
@@ -14,8 +18,39 @@ export default function SiteDataFieldErrorIndicator( { errors }: Props ) {
 		}, 'low' ) ?? 'low';
 
 	return (
-		<div className={ clsx( 'sites-dataviews__site-error-indicator', `is-${ highestSeverity }` ) }>
+		<div
+			className={ clsx( 'sites-dataviews__site-error-indicator', `is-${ highestSeverity }` ) }
+			ref={ popoverContext }
+			onMouseEnter={ () => setIsPopoverVisible( true ) }
+			onMouseLeave={ () => setIsPopoverVisible( false ) }
+			onMouseDown={ () => setIsPopoverVisible( true ) }
+			onMouseUp={ () => setIsPopoverVisible( false ) }
+			onTouchStart={ () => setIsPopoverVisible( true ) }
+			onTouchEnd={ () => setIsPopoverVisible( false ) }
+			role="button"
+			tabIndex={ 0 }
+		>
 			{ !! errors?.length && <Gridicon size={ 18 } icon="notice-outline" /> }
+			{ !! errors?.length && (
+				<Popover
+					className="sites-dataviews__site-error-popover"
+					position="right"
+					noArrow={ false }
+					context={ popoverContext.current }
+					isVisible={ isPopoverVisible }
+				>
+					<ul className="sites-dataviews__site-error-popover-list">
+						{ errors.map( ( error, index ) => (
+							<li
+								key={ index }
+								className={ `sites-dataviews__site-error-item is-${ error.severity }` }
+							>
+								{ error.message }
+							</li>
+						) ) }
+					</ul>
+				</Popover>
+			) }
 		</div>
 	);
 }
