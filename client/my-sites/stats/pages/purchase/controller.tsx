c0da961fdadbd066237bddf9b160005c5ebd32bf
Dognose <dognose24@users.noreply.github.com>
2024-10-24 13:13:59 +0800

    Stats: Fix purchase page for site-less flow (#95646)
    * Revert "Use StatsPageLoader to wrap purchase page"
    
    This reverts commit 7a982901896b926b155abcd28653661e2e9cebe2.
    
    * Update annotations for purchase page controller

--- client/my-sites/stats/pages/purchase/controller.tsx
+++ client/my-sites/stats/pages/purchase/controller.tsx
@@ -1,17 +1,16 @@
 import AsyncLoad from 'calypso/components/async-load';
-import StatsPageLoader from '../../stats-page-loader';
 import PageLoading from '../shared/page-loading';
 import type { Context } from '@automattic/calypso-router';
 
 function purchase( context: Context, next: () => void ) {
 	context.primary = (
-		<StatsPageLoader>
-			<AsyncLoad
-				require="calypso/my-sites/stats/pages/purchase"
-				placeholder={ PageLoading }
-				query={ context.query }
-			/>
-		</StatsPageLoader>
+		// DO NOT WRAP WITH <StatsPageLoader />
+		// Or the site-less purchase flow will not work because there is no permission to access Stats.
+		<AsyncLoad
+			require="calypso/my-sites/stats/pages/purchase"
+			placeholder={ PageLoading }
+			query={ context.query }
+		/>
 	);
 	next();
 }
