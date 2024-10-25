37306bf5712c76bbef7a0eb583a584ce30bf86ac
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-10-25 15:31:25 +0800

    Add Scan and Plugin errors.

--- client/a8c-for-agencies/sections/sites/sites-dataviews/hooks/use-get-site-errors.ts
+++ client/a8c-for-agencies/sections/sites/sites-dataviews/hooks/use-get-site-errors.ts
@@ -1,18 +1,48 @@
 import { useTranslate } from 'i18n-calypso';
 import { useCallback } from 'react';
-import { ErrorNode, SiteError } from '../../types';
+import { SiteData, SiteError } from '../../types';
 
 export default function useGetSiteErrors() {
 	const translate = useTranslate();
 
 	return useCallback(
-		( error?: ErrorNode ): SiteError[] => {
-			if ( error?.status === 'failed' ) {
-				// FIXME: For the time being we only have one error type and that is network issue.
-				return [ { severity: 'high', message: translate( 'Connectivity issue' ) } ];
+		( site?: SiteData ): SiteError[] => {
+			const errors: SiteError[] = [];
+			if ( site?.error?.status === 'failed' ) {
+				errors.push( { severity: 'high', message: translate( 'Connectivity issue' ) } );
 			}
 
-			return [];
+			if ( site?.scan?.status === 'failed' ) {
+				errors.push( {
+					severity: 'medium',
+					message: translate( '%(count)s threat found', '%(count)s threats found', {
+						count: site.scan.threats,
+						args: {
+							count: site.scan.threats,
+						},
+						comment: '%(count) here is the number of threats found',
+					} ),
+				} );
+			}
+
+			if ( site?.plugin?.status === 'warning' ) {
+				errors.push( {
+					severity: 'medium',
+					message: translate(
+						'%(count)s plugin requires update',
+						'%(count)s plugins require updates',
+						{
+							count: site.plugin.updates,
+							args: {
+								count: site.plugin.updates,
+							},
+							comment: '%(count) here is the number of plugins that require updates',
+						}
+					),
+				} );
+			}
+
+			return errors;
 		},
 		[ translate ]
 	);
