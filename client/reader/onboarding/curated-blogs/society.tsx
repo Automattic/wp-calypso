6b424ed8baad1f84170d2823432102d09be38b7e
DustyReagan <dusty@dustyreagan.com>
2024-10-25 17:44:32 -0500

    Two thirds done

--- client/reader/onboarding/curated-blogs/society.tsx
+++ client/reader/onboarding/curated-blogs/society.tsx
@@ -20,12 +20,112 @@ export const societyBlogs: CuratedBlogsList = {
 			site_URL: 'http://www.shakeuplearning.com/',
 			site_name: 'Educators Technology',
 		},
+		{
+			feed_ID: 69814417,
+			site_ID: 39960713,
+			site_URL: 'https://www.teachertoolkit.co.uk/',
+			site_name: 'TeacherToolkit',
+		},
+		{
+			feed_ID: 19068123,
+			site_ID: 64463434,
+			site_URL: 'https://jenniefitzkee.com/',
+			site_name: "A Teacher's Reflections",
+		},
+		{
+			feed_ID: 22002764,
+			site_ID: 60068710,
+			site_URL: 'https://www.coolcatteacher.com/',
+			site_name: 'Cool Cat Teacher Blog',
+		},
 	],
 	nature: [
-		// Add nature blogs...
+		{
+			feed_ID: 93708735,
+			site_ID: 158320507,
+			site_URL: 'https://scubahanknyc.com/',
+			site_name: 'Scuba Hank NYC',
+		},
+		{
+			feed_ID: 47864083,
+			site_ID: 43646855,
+			site_URL: 'https://stevecreek.com/',
+			site_name: 'Steve Creek Wildlife Photography',
+		},
+		{
+			feed_ID: 136146583,
+			site_ID: 159711569,
+			site_URL: 'https://elpasozoo.home.blog/',
+			site_name: 'Conservation Education',
+		},
+		{
+			feed_ID: 79041338,
+			site_ID: 142325617,
+			site_URL: 'https://nationalparkswitht.com/',
+			site_name: 'National Parks With T',
+		},
+		{
+			feed_ID: 17239565,
+			site_ID: 62426488,
+			site_URL: 'https://backyardbirdnerd.com/',
+			site_name: 'Backyard Bird Nerd',
+		},
+		{
+			feed_ID: 155668618,
+			site_ID: 48819432,
+			site_URL: 'https://scotlandsnature.wordpress.com/',
+			site_name: "Scotland's Nature",
+		},
 	],
 	future: [
-		// Add future-focused blogs...
+		{
+			feed_ID: 66308872,
+			site_ID: 101450901,
+			site_URL: 'https://futurism.com/',
+			site_name: 'Futurism',
+		},
+		{
+			feed_ID: 16857803,
+			site_ID: 4183809,
+			site_URL: 'https://singularityhub.com/',
+			site_name: 'Singularity Hub',
+		},
+		{
+			feed_ID: 64152258,
+			site_ID: 127277856,
+			site_URL: 'http://www.nextbigfuture.com/',
+			site_name: 'NextBigFuture.com',
+		},
+		{
+			feed_ID: 77891657,
+			site_ID: 136066274,
+			site_URL: 'https://www.fanaticalfuturist.com/',
+			site_name: 'Matthew Griffin | Keynote Speaker & Master Futurist',
+		},
+		{
+			feed_ID: 77936565,
+			site_ID: 141327587,
+			site_URL: 'https://bryanalexander.org/',
+			site_name: 'Bryan Alexander',
+		},
+		{
+			feed_ID: 388033,
+			site_ID: 1011537,
+			site_URL: 'https://thenextwavefutures.wordpress.com/',
+			site_name: 'the next wave',
+		},
+		{
+			feed_ID: 46138681,
+			site_ID: 16638040,
+			site_URL: 'https://frankdiana.net/',
+			site_name: 'Reimagining the Future',
+		},
+		{
+			feed_ID: 1430889,
+			site_ID: 8522080,
+			site_URL: 'https://lucept.com/',
+			site_name: 'Lucept',
+		},
 	],
 	politics: [
 		// Add politics blogs...
