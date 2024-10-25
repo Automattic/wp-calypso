7b9a760bcf2085d3cd6db8f89d23acede194e9e5
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-10-25 14:50:14 +0200

    Remove from available flows

--- client/landing/stepper/declarative-flow/registered-flows.ts
+++ client/landing/stepper/declarative-flow/registered-flows.ts
@@ -8,8 +8,6 @@ import {
 	TRANSFERRING_HOSTED_SITE_FLOW,
 	IMPORT_HOSTED_SITE_FLOW,
 	DOMAIN_TRANSFER,
-	VIDEOPRESS_TV_FLOW,
-	VIDEOPRESS_TV_PURCHASE_FLOW,
 	GOOGLE_TRANSFER,
 	REBLOGGING_FLOW,
 	MIGRATION_FLOW,
@@ -38,9 +36,6 @@ const availableFlows: Record< string, () => Promise< { default: Flow } > > = {
 
 	[ IMPORT_FOCUSED_FLOW ]: () =>
 		import( /* webpackChunkName: "import-flow" */ '../declarative-flow/import-flow' ),
-
-	videopress: () =>
-		import( /* webpackChunkName: "videopress-flow" */ '../declarative-flow/videopress' ),
 
 	'link-in-bio': () =>
 		import( /* webpackChunkName: "link-in-bio-flow" */ '../declarative-flow/link-in-bio' ),
@@ -155,21 +150,6 @@ const availableFlows: Record< string, () => Promise< { default: Flow } > > = {
 		import( /* webpackChunkName: "migration-flow" */ '../declarative-flow/migration' ),
 };
 
-const videoPressTvFlows: Record< string, () => Promise< { default: Flow } > > = config.isEnabled(
-	'videopress-tv'
-)
-	? {
-			[ VIDEOPRESS_TV_FLOW ]: () =>
-				import( /* webpackChunkName: "videopress-tv-flow" */ `../declarative-flow/videopress-tv` ),
-
-			[ VIDEOPRESS_TV_PURCHASE_FLOW ]: () =>
-				import(
-					/* webpackChunkName: "videopress-tv-flow" */
-					`../declarative-flow/videopress-tv-purchase`
-				),
-	  }
-	: {};
-
 const hostedSiteMigrationFlow: Record< string, () => Promise< { default: Flow } > > = {
 	[ HOSTED_SITE_MIGRATION_FLOW ]: () =>
 		import(
@@ -187,7 +167,6 @@ const hundredYearDomainFlow: Record< string, () => Promise< { default: Flow } > 
 
 export default {
 	...availableFlows,
-	...videoPressTvFlows,
 	...hostedSiteMigrationFlow,
 	...hundredYearDomainFlow,
 };
