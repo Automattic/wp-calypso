95ea98ccc6d83088b52f37c96bf55a4caafb4fc4
Jeremy Yip <jeremy.yip@automattic.com>
2024-10-24 17:15:15 -0700

    Pass coupon through from calypso admin plans page

--- packages/data-stores/src/plans/queries/lib/use-query-keys-factory.ts
+++ packages/data-stores/src/plans/queries/lib/use-query-keys-factory.ts
@@ -1,5 +1,10 @@
 const useQueryKeysFactory = () => ( {
-	sitePlans: ( siteId?: string | number | null ) => [ 'site-plans', siteId ],
+	// TODO: Verify that this is the correct convention for query keys
+	sitePlans: ( coupon?: string, siteId?: string | number | null ) => [
+		'site-plans',
+		siteId,
+		coupon,
+	],
 	plans: ( coupon?: string ) => [ 'plans', coupon ],
 } );
 
