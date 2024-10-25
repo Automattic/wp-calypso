2ea304d2ef4c1750427ee92a0073d9a5703d5899
Allison Levine <allison.levine@automattic.com>
2024-10-25 08:58:24 -0400

    Add DataViews to Reader.

--- client/reader/following/main.jsx
+++ client/reader/following/main.jsx
@@ -1,3 +1,5 @@
+import config from '@automattic/calypso-config';
+import { DataViews } from '@wordpress/dataviews';
 import clsx from 'clsx';
 import { translate } from 'i18n-calypso';
 import AsyncLoad from 'calypso/components/async-load';
@@ -12,24 +14,69 @@ import ReaderListFollowedSites from 'calypso/reader/stream/reader-list-followed-
 import './style.scss';
 
 function FollowingStream( { ...props } ) {
+<<<<<<< HEAD
+=======
+	const [ readerOnboardingIsRendered, setReaderOnboardingIsRendered ] = useState( false );
+
+	const [ view, setView ] = useState( {
+		type: 'list',
+		fields: [ 'title', 'blog' ],
+	} );
+	const fields = [
+		{
+			id: 'title',
+			label: translate( 'Title' ),
+			enableHiding: false,
+		},
+		{
+			id: 'blog',
+			label: translate( 'Blog' ),
+			enableHiding: false,
+		},
+	];
+	const data = [
+		{
+			id: 1,
+			title: 'Title',
+			blog: 'Blog',
+		},
+	];
+	/* eslint-disable wpcalypso/jsx-classname-namespace */
+>>>>>>> 3eb6d0536b (Add DataViews to Reader.)
 	return (
 		<>
-			<Stream
-				{ ...props }
-				className="following"
-				streamSidebar={ () => <ReaderListFollowedSites path={ window.location.pathname } /> }
-			>
-				<BloganuaryHeader />
-				<NavigationHeader
-					title={ translate( 'Recent' ) }
-					subtitle={ translate( "Stay current with the blogs you've subscribed to." ) }
-					className={ clsx( 'following-stream-header', {
-						'reader-dual-column': props.width > WIDE_DISPLAY_CUTOFF,
-					} ) }
+			{ config.isEnabled( 'reader/recent-feed-overhaul' ) ? (
+				<DataViews
+					getItemId={ ( item ) => item.id.toString() }
+					data={ data }
+					view={ view }
+					fields={ fields }
+					onChangeView={ setView }
 				/>
+<<<<<<< HEAD
 
 				<ReaderOnboarding />
 			</Stream>
+=======
+			) : (
+				<Stream
+					{ ...props }
+					className="following"
+					streamSidebar={ () => <ReaderListFollowedSites path={ window.location.pathname } /> }
+				>
+					<BloganuaryHeader />
+					<NavigationHeader
+						title={ translate( 'Recent' ) }
+						subtitle={ translate( "Stay current with the blogs you've subscribed to." ) }
+						className={ clsx( 'following-stream-header', {
+							'reader-dual-column': props.width > WIDE_DISPLAY_CUTOFF,
+						} ) }
+					/>
+					<ReaderOnboarding onRender={ setReaderOnboardingIsRendered } />
+					{ ! readerOnboardingIsRendered && <FollowingIntro /> }
+				</Stream>
+			) }
+>>>>>>> 3eb6d0536b (Add DataViews to Reader.)
 			<AsyncLoad require="calypso/lib/analytics/track-resurrections" placeholder={ null } />
 		</>
 	);
