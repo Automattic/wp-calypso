8778c14480e6c2b93c6ca0be4388d65508de4ec5
DustyReagan <dusty@dustyreagan.com>
2024-10-25 10:09:04 -0500

    Start adding blogs

--- client/reader/onboarding/curated-blogs/society.tsx
+++ client/reader/onboarding/curated-blogs/society.tsx
@@ -2,7 +2,24 @@ import { CuratedBlogsList } from './index';
 
 export const societyBlogs: CuratedBlogsList = {
 	education: [
-		// Add more education blogs...
+		{
+			feed_ID: 155447430,
+			site_ID: 202092965,
+			site_URL: 'https://world-education-blog.org',
+			site_name: 'World Education Blog',
+		},
+		{
+			feed_ID: 161689814,
+			site_ID: 223822458,
+			site_URL: 'https://www.educatorstechnology.com/',
+			site_name: 'Educators Technology',
+		},
+		{
+			feed_ID: 78449659,
+			site_ID: 102727187,
+			site_URL: 'http://www.shakeuplearning.com/',
+			site_name: 'Educators Technology',
+		},
 	],
 	nature: [
 		// Add nature blogs...
