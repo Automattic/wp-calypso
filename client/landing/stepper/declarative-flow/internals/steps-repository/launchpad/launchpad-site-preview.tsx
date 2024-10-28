0f438e1fc4de53b82ee57724c5959257be0c69a4
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-10-28 17:14:38 +0100

    Rm unused code

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/launchpad-site-preview.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/launchpad-site-preview.tsx
@@ -1,4 +1,3 @@
-import { FEATURE_VIDEO_UPLOADS, planHasFeature } from '@automattic/calypso-products';
 import { DEVICE_TYPES } from '@automattic/components';
 import {
 	FREE_FLOW,
@@ -7,14 +6,10 @@ import {
 	WRITE_FLOW,
 	START_WRITING_FLOW,
 	DESIGN_FIRST_FLOW,
-	VIDEOPRESS_FLOW,
-	VIDEOPRESS_ACCOUNT,
 	ASSEMBLER_FIRST_FLOW,
 	READYMADE_TEMPLATE_FLOW,
 	AI_ASSEMBLER_FLOW,
-	isVideoPressFlow,
 } from '@automattic/onboarding';
-import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import SitePreview from '../../components/site-preview';
 
 interface Props {
@@ -23,9 +18,6 @@ interface Props {
 }
 
 const LaunchpadSitePreview = ( { siteSlug, flow }: Props ) => {
-	const site = useSite();
-	const isInVideoPressFlow = isVideoPressFlow( flow );
-
 	const getSitePreviewDefaultDevice = ( flow: string | null ) => {
 		switch ( flow ) {
 			case NEWSLETTER_FLOW:
@@ -38,9 +30,6 @@ const LaunchpadSitePreview = ( { siteSlug, flow }: Props ) => {
 			case READYMADE_TEMPLATE_FLOW:
 			case AI_ASSEMBLER_FLOW:
 				return DEVICE_TYPES.COMPUTER;
-			case VIDEOPRESS_FLOW:
-			case VIDEOPRESS_ACCOUNT:
-				return window.innerWidth >= 1000 ? DEVICE_TYPES.COMPUTER : DEVICE_TYPES.PHONE;
 			default:
 				return DEVICE_TYPES.PHONE;
 		}
@@ -49,13 +38,10 @@ const LaunchpadSitePreview = ( { siteSlug, flow }: Props ) => {
 	return (
 		<SitePreview
 			siteSlug={ siteSlug }
-			isUnsupportedPlan={
-				isInVideoPressFlow &&
-				! planHasFeature( site?.plan?.product_slug as string, FEATURE_VIDEO_UPLOADS )
-			}
+			isUnsupportedPlan={ false }
 			defaultDevice={ getSitePreviewDefaultDevice( flow ) }
 			showDeviceSwitcher
-			enableInteractions={ isInVideoPressFlow }
+			enableInteractions={ false }
 		/>
 	);
 };
--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/translations.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/translations.tsx
@@ -1,7 +1,6 @@
 import {
 	LINK_IN_BIO_TLD_FLOW,
 	NEWSLETTER_FLOW,
-	VIDEOPRESS_FLOW,
 	FREE_FLOW,
 	WRITE_FLOW,
 	BUILD_FLOW,
@@ -40,11 +39,6 @@ export function getLaunchpadTranslations(
 			translatedStrings.title = translate( "Let's get ready to launch!" );
 			translatedStrings.launchTitle = translate( "Let's get ready to launch!" );
 			translatedStrings.subtitle = translate( "Here's what to do next." );
-			break;
-		case VIDEOPRESS_FLOW:
-			translatedStrings.flowName = translate( 'Video' );
-			translatedStrings.title = translate( 'Your site is almost ready!' );
-			translatedStrings.launchTitle = translate( 'Your site is almost ready!' );
 			break;
 		case START_WRITING_FLOW:
 		case DESIGN_FIRST_FLOW:
