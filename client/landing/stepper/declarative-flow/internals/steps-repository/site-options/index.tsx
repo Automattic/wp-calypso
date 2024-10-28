0fbbec8a39697c916e87048006a7d1ae081cf4e3
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-10-28 11:38:28 +0100

    Rm Videopress TV flow from site options router

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-options/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-options/index.tsx
@@ -1,11 +1,11 @@
-import { VIDEOPRESS_FLOW, VIDEOPRESS_TV_FLOW } from '@automattic/onboarding';
+import { VIDEOPRESS_FLOW } from '@automattic/onboarding';
 import { SiteOptions } from './site-options';
 import { VideoPressSiteOptions } from './videopress-site-options';
 import type { Step } from '../../types';
 import './style.scss';
 
 const SiteOptionsStepRouter: Step = function SiteOptionsStepRouter( { navigation, flow } ) {
-	if ( flow === VIDEOPRESS_FLOW || flow === VIDEOPRESS_TV_FLOW ) {
+	if ( flow === VIDEOPRESS_FLOW ) {
 		return <VideoPressSiteOptions navigation={ navigation } />;
 	}
 
