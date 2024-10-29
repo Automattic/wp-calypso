7b0875e662ace346292f09dd1e09072cbf9e5d5a
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-10-29 19:57:14 +0800

    Implement the 'Client relationship' section on the new Migrations page.

--- client/a8c-for-agencies/sections/migrations/migrations-overview-v2/sections/migrations-client-relationship.tsx
+++ client/a8c-for-agencies/sections/migrations/migrations-overview-v2/sections/migrations-client-relationship.tsx
@@ -1,14 +1,41 @@
 import { useTranslate } from 'i18n-calypso';
-import PageSection from 'calypso/a8c-for-agencies/components/page-section';
+import HostingBenefitsSection from 'calypso/a8c-for-agencies/sections/marketplace/common/hosting-benefits-section';
 
 export default function MigrationsClientRelationship() {
 	const translate = useTranslate();
 	return (
-		<PageSection
+		<HostingBenefitsSection
 			heading={ translate( 'Improve your client relationships with our hosting' ) }
 			description={ translate( 'Our hosting platform is built exclusively for WordPress.' ) }
-		>
-			CLIENT RELATIONSHIP HERE
-		</PageSection>
+			items={ [
+				{
+					title: translate( 'Create trust' ),
+					description: translate(
+						"With over 15 years of experience running hundreds of millions of sites on WordPress.com, including the highest-trafficked sites globally, we've developed a platform we confidently put up against any cloud service."
+					),
+					benefits: [
+						translate( '99.999% Uptime' ),
+						translate( 'High availability with automated scaling' ),
+					],
+				},
+				{
+					title: translate( 'Minimize risk' ),
+					description: translate(
+						'Automattic hosting plans offer exceptional security from day one, with the option to include or sell additional client-facing security features like real-time backups, anti-spam, and malware scanning.'
+					),
+					benefits: [ translate( 'Web Application Firewall' ), translate( 'DDoS protection' ) ],
+				},
+				{
+					title: translate( 'Increase speed' ),
+					description: translate(
+						"We're the only cloud platform team fully dedicated to optimizing WordPress. Your customers will feel the difference."
+					),
+					benefits: [
+						translate( 'Incredibly low page speed index' ),
+						'Automated WordPress edge caching',
+					],
+				},
+			] }
+		/>
 	);
 }
