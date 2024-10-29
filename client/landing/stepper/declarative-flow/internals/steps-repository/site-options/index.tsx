0523bce96cc39bc7e033d0d0f5dfaebc9cc15567
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-10-29 16:11:08 +0100

    Rm `SiteOptionsStepRouter` component

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-options/index.tsx
+++
@@ -1,9 +0,0 @@
-import { SiteOptions } from './site-options';
-import type { Step } from '../../types';
-import './style.scss';
-
-const SiteOptionsStepRouter: Step = function SiteOptionsStepRouter( { navigation } ) {
-	return <SiteOptions navigation={ navigation } />;
-};
-
-export default SiteOptionsStepRouter;
