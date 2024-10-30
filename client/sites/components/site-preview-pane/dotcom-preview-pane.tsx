25e4ed3b0d33acaff9f610f52f1486dc816b4766
Griffith Chen <griffith.chen@automattic.com>
2024-10-30 14:09:31 +0800

    Revert unneeded file changes

--- client/sites/components/site-preview-pane/dotcom-preview-pane.tsx
+++ client/sites/components/site-preview-pane/dotcom-preview-pane.tsx
@@ -22,6 +22,7 @@ import {
 	DOTCOM_HOSTING_FEATURES,
 	DOTCOM_STAGING_SITE,
 	SITE_MARKETING_TOOLS,
+	SITE_MARKETING_BUSINESS_TOOLS,
 } from './constants';
 import PreviewPaneHeaderButtons from './preview-pane-header-buttons';
 import SiteEnvironmentSwitcher from './site-environment-switcher';
@@ -109,7 +110,7 @@ const DotcomPreviewPane = ( {
 			{
 				label: __( 'Marketing' ),
 				enabled: config.isEnabled( 'untangling/hosting-menu' ),
-				featureIds: [ SITE_MARKETING_TOOLS ],
+				featureIds: [ SITE_MARKETING_TOOLS, SITE_MARKETING_BUSINESS_TOOLS ],
 			},
 			{
 				label: hasEnTranslation( 'Server Settings' )
