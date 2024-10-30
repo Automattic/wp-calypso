1b3fce0676aa860e5cabae76b433dd06e809e25d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-10-30 15:07:51 +0100

    Update HOC providing more props

--- client/data/media/with-google-photos-picker-session.js
+++ client/data/media/with-google-photos-picker-session.js
@@ -1,18 +1,26 @@
+import config from '@automattic/calypso-config';
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { useSelector } from 'react-redux';
 import getGooglePhotosPickerSession from 'calypso/state/selectors/get-google-photos-picker-session';
-import { useCreateGooglePhotosPickerSessionMutation } from './use-google-photos-picker-session-mutation';
+import {
+	useCreateGooglePhotosPickerSessionMutation,
+	useDeleteGooglePhotosPickerSessionMutation,
+} from './use-google-photos-picker-session-mutation';
 
 export const withGooglePhotosPickerSession = createHigherOrderComponent( ( Wrapped ) => {
 	return ( props ) => {
+		const photosPickerApiEnabled = config.isEnabled( 'google-photos-picker' );
 		const photoPickerSession = useSelector( ( state ) => getGooglePhotosPickerSession( state ) );
 
 		const { mutate: createPickerSession } = useCreateGooglePhotosPickerSessionMutation();
+		const { mutate: deletePickerSession } = useDeleteGooglePhotosPickerSessionMutation();
 
 		return (
 			<Wrapped
 				{ ...props }
+				photosPickerApiEnabled={ photosPickerApiEnabled }
 				photoPickerSession={ photoPickerSession }
+				deletePickerSession={ deletePickerSession }
 				createPhotoPickerSession={ createPickerSession }
 			/>
 		);
