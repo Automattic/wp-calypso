eaf338182818a9cdcdff67621aeca32e6770a73a
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-10-30 15:08:22 +0100

    Fix wrong typing

--- client/state/selectors/get-google-photos-picker-session.ts
+++ client/state/selectors/get-google-photos-picker-session.ts
@@ -1,9 +1,10 @@
 import { get } from 'lodash';
+import { SessionData } from 'calypso/data/media/use-google-photos-picker-session-query';
 import { AppState } from 'calypso/types';
 
 /**
  * Get the Google Photos Picker session ID
  */
-export default function getGooglePhotosPickerSession( state: AppState ): string | undefined {
+export default function getGooglePhotosPickerSession( state: AppState ): SessionData | undefined {
 	return get( state, [ 'media', 'googlePhotosPicker', 'session' ], undefined );
 }
