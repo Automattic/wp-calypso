6880626a0d30c95e5c6d1bc9a7ab49fd53191409
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-10-30 15:21:43 +0100

    `Theme`: Use intersection observer to render a placeholder when not visible

--- client/components/theme/index.jsx
+++ client/components/theme/index.jsx
@@ -11,6 +11,7 @@ import { isEmpty, isEqual } from 'lodash';
 import photon from 'photon';
 import PropTypes from 'prop-types';
 import { Component, createRef } from 'react';
+import { InView } from 'react-intersection-observer';
 import { connect } from 'react-redux';
 import ThemeTierBadge from 'calypso/components/theme-tier/theme-tier-badge';
 import { decodeEntities } from 'calypso/lib/formatting';
@@ -23,6 +24,8 @@ import { setThemesBookmark } from 'calypso/state/themes/themes-ui/actions';
 import ThemeMoreButton from './more-button';
 
 import './style.scss';
+
+const THEME_CARD_HEIGHT = 420;
 
 const noop = () => {};
 
@@ -347,27 +350,37 @@ export class Theme extends Component {
 		}
 
 		return (
-			<ThemeCard
-				ref={ this.props.bookmarkRef }
-				name={ name }
-				description={ themeDescription }
-				image={ this.renderScreenshot() }
-				imageClickUrl={ this.props.screenshotClickUrl }
-				imageActionLabel={ this.props.actionLabel }
-				banner={ this.renderUpdateAlert() }
-				badge={ this.renderBadge() }
-				styleVariations={ style_variations }
-				selectedStyleVariation={ selectedStyleVariation }
-				optionsMenu={ this.renderMoreButton() }
-				isActive={ this.props.active }
-				isLoading={ this.props.loading }
-				isSoftLaunched={ this.props.softLaunched }
-				isShowDescriptionOnImageHover={ ! isCustomGeneratedTheme }
-				onClick={ this.setBookmark }
-				onImageClick={ this.onScreenshotClick }
-				onStyleVariationClick={ this.onStyleVariationClick }
-				onStyleVariationMoreClick={ this.onStyleVariationClick }
-			/>
+			<InView triggerOnce rootMargin={ `${ THEME_CARD_HEIGHT }px` }>
+				{ ( { inView, ref } ) => (
+					<div ref={ ref }>
+						{ inView ? (
+							<ThemeCard
+								ref={ this.props.bookmarkRef }
+								name={ name }
+								description={ themeDescription }
+								image={ this.renderScreenshot() }
+								imageClickUrl={ this.props.screenshotClickUrl }
+								imageActionLabel={ this.props.actionLabel }
+								banner={ this.renderUpdateAlert() }
+								badge={ this.renderBadge() }
+								styleVariations={ style_variations }
+								selectedStyleVariation={ selectedStyleVariation }
+								optionsMenu={ this.renderMoreButton() }
+								isActive={ this.props.active }
+								isLoading={ this.props.loading }
+								isSoftLaunched={ this.props.softLaunched }
+								isShowDescriptionOnImageHover={ ! isCustomGeneratedTheme }
+								onClick={ this.setBookmark }
+								onImageClick={ this.onScreenshotClick }
+								onStyleVariationClick={ this.onStyleVariationClick }
+								onStyleVariationMoreClick={ this.onStyleVariationClick }
+							/>
+						) : (
+							this.renderPlaceholder()
+						) }
+					</div>
+				) }
+			</InView>
 		);
 	}
 }
