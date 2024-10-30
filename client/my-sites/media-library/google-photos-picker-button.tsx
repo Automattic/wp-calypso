176757a6dda4b1789c9c103db87882650f0f96c5
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-10-30 17:39:51 +0100

    Adjust google photos picker screen and add translation

--- client/my-sites/media-library/google-photos-picker-button.tsx
+++ client/my-sites/media-library/google-photos-picker-button.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@wordpress/components';
 import './google-photos-picker-button.scss';
+import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
 import {
 	useCreateGooglePhotosPickerSessionMutation,
@@ -12,6 +13,7 @@ import getGooglePhotosPickerSession from 'calypso/state/selectors/get-google-pho
 
 const GooglePhotosPickerButton = () => {
 	const dispatch = useDispatch();
+	const translate = useTranslate();
 	const session: PickerSession = useSelector( getGooglePhotosPickerSession );
 	const { mutate: createSession, isPending } = useCreateGooglePhotosPickerSessionMutation();
 	const { data: sessionData, refetch } = useGooglePhotosPickerSessionQuery(
@@ -37,15 +39,24 @@ const GooglePhotosPickerButton = () => {
 	}, [ sessionData ] );
 
 	return (
-		<div className="google-photos-picker--container">
+		<div className="google-photos-picker--container media-library__connect-message">
 			<p>
-				Select photos directly from your Google Photos library. Click the button below to open
-				Google’s secure photo picker in a new tab. After choosing your photos, they will be
-				available for use here.
+				{ translate(
+					'Select photos directly from your Google Photos library. Click the button below to open Google’s secure photo picker in a new tab.'
+				) }
 			</p>
 			<Button variant="primary" isBusy={ isPending } onClick={ openPickerTab }>
-				Open Google Photos Picker
+				{ translate( 'Open Google Photos Picker' ) }
 			</Button>
+			<p>
+				<small>
+					(
+					{ translate(
+						"this page will automatically update when you've completed your selection"
+					) }
+					)
+				</small>
+			</p>
 		</div>
 	);
 };
