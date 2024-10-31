999fbdab826a7f13a9ab91cec010d738abf4fc4c
Gabriel Caires <gabriel.caires@automattic.com>
2024-10-31 18:10:53 +0000

    Split into small components

--- client/hosting/overview/components/migration-overview/test/index.tsx
+++ client/hosting/overview/components/migration-overview/test/index.tsx
@@ -1,115 +1,219 @@
 /**
  * @jest-environment jsdom
  */
-import { canInstallPlugins } from '@automattic/sites';
-import { render, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { getMigrationStatus, getMigrationType } from 'calypso/sites-dashboard/utils';
-import MigrationOverview from '../';
+import MigrationOverview from '..';
 import type { SiteDetails } from '@automattic/data-stores';
 
-jest.mock( '@automattic/sites' );
-jest.mock( 'calypso/sites-dashboard/utils' );
+function textContentMatcher( textMatch: string | RegExp ) {
+	const hasText =
+		typeof textMatch === 'string'
+			? ( node: Element ) => node.textContent === textMatch
+			: ( node: Element ) => textMatch.test( node.textContent ?? '' );
 
-const baseSite = {
-	ID: 123,
-	slug: 'example.com',
-} as SiteDetails;
+	return ( _content: string, node: Element ) => {
+		if ( ! hasText( node ) ) {
+			return false;
+		}
+
+		const childrenDontHaveText = Array.from( node?.children || [] ).every(
+			( child ) => ! hasText( child )
+		);
+
+		return childrenDontHaveText;
+	};
+}
+
+const buildMigrationSite = ( {
+	status,
+	how,
+	canInstallPlugins = false,
+}: {
+	status: string;
+	how?: string;
+	canInstallPlugins?: boolean;
+} ) =>
+	( {
+		ID: 123,
+		slug: 'example.com',
+		site_migration: {
+			migration_status: [ 'migration', status, how ].filter( Boolean ).join( '-' ),
+		},
+		name: 'Bold Apps',
+		plan: {
+			features: {
+				active: canInstallPlugins ? [ 'install-plugins' ] : [],
+			},
+		},
+	} ) as SiteDetails;
 
 describe( 'MigrationOverview', () => {
-	it.each( [
-		[
-			'diy',
-			false,
-			'/setup/site-migration/site-migration-upgrade-plan?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview&destination=upgrade&how=myself',
-		],
-		[
-			'difm',
-			false,
-			'/setup/site-migration/site-migration-upgrade-plan?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview&destination=upgrade&how=difm',
-		],
-		[
-			undefined,
-			false,
-			'/setup/site-migration/site-migration-how-to-migrate?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview',
-		],
-		[
-			'diy',
-			true,
-			'/setup/migration/site-migration-instructions?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview',
-		],
-		[
-			'difm',
-			true,
-			'/setup/migration/site-migration-credentials?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview',
-		],
-		[
-			undefined,
-			true,
-			'/setup/migration/migration-how-to-migrate?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview',
-		],
-	] )(
-		'should set continueMigrationUrl correctly for migrationType: %s and isFreePlan: %s',
-		( migrationType, siteCanInstallPlugins, expectedUrl ) => {
-			( getMigrationType as jest.Mock ).mockReturnValue( migrationType );
-			( getMigrationStatus as jest.Mock ).mockReturnValue( 'pending' );
-			( canInstallPlugins as jest.Mock ).mockReturnValue( siteCanInstallPlugins );
+	const getStartMigrationLink = () => {
+		return screen.queryByRole( 'link', { name: 'Start your migration' } );
+	};
 
-			const { getByRole } = render( <MigrationOverview site={ baseSite } /> );
-			const button = getByRole( 'link', { name: 'Start your migration' } );
+	describe( 'DIY pending migration', () => {
+		it( 'shows the migrating pending instructions', () => {
+			const site = buildMigrationSite( { status: 'pending', how: 'diy' } );
 
-			expect( button ).toHaveAttribute( 'href', expectedUrl );
-		}
-	);
+			const { getByText } = render( <MigrationOverview site={ site } /> );
 
-	it( 'should not render the button if it is not in the pending status', () => {
-		( getMigrationType as jest.Mock ).mockReturnValue( 'diy' );
-		( getMigrationStatus as jest.Mock ).mockReturnValue( 'started' );
-		( canInstallPlugins as jest.Mock ).mockReturnValue( true );
+			expect(
+				getByText( /Start your migration today and get ready for unmatched WordPress hosting./ )
+			).toBeVisible();
+		} );
 
-		const { queryByRole } = render( <MigrationOverview site={ baseSite } /> );
-		expect( queryByRole( 'link', { name: 'Start your migration' } ) ).not.toBeInTheDocument();
+		it( 'shows a link to the upgrade plan page when the site cannot install plugins', () => {
+			const site = buildMigrationSite( {
+				status: 'pending',
+				how: 'diy',
+				canInstallPlugins: false,
+			} );
+
+			render( <MigrationOverview site={ site } /> );
+
+			const link = getStartMigrationLink();
+
+			expect( link ).toHaveAttribute(
+				'href',
+				'/setup/site-migration/site-migration-upgrade-plan?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview&destination=upgrade&how=myself'
+			);
+		} );
+
+		it( 'shows a link to the instructions page when the site is able to install plugins', () => {
+			const site = buildMigrationSite( {
+				status: 'pending',
+				how: 'diy',
+				canInstallPlugins: true,
+			} );
+
+			render( <MigrationOverview site={ site } /> );
+
+			const link = getStartMigrationLink();
+
+			expect( link ).toHaveAttribute(
+				'href',
+				'/setup/migration/site-migration-instructions?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview'
+			);
+		} );
 	} );
 
-	it.each( [
-		[
-			'pending',
-			'Your WordPress site is ready to be migrated',
-			'Start your migration today and get ready for unmatched WordPress hosting.',
-		],
-		[
-			'started',
-			'Your migration is underway',
-			/transfers to its new home. Get ready for unmatched WordPress hosting./,
-		],
-	] )(
-		'should render the correct title and paragraph for migration status: %s',
-		( migrationStatus, expectedTitle, expectedParagraph ) => {
-			( getMigrationType as jest.Mock ).mockReturnValue( 'diy' );
-			( getMigrationStatus as jest.Mock ).mockReturnValue( migrationStatus );
+	describe( 'DIFM pending migration', () => {
+		it( 'shows the migrating pending instructions', () => {
+			const site = buildMigrationSite( { status: 'pending', how: 'difm' } );
 
-			const { getByText } = render( <MigrationOverview site={ baseSite } /> );
-			expect( getByText( expectedTitle ) ).toBeInTheDocument();
-			expect( getByText( expectedParagraph ) ).toBeInTheDocument();
-		}
-	);
+			const { getByText } = render( <MigrationOverview site={ site } /> );
 
-	it( 'should render the correct paragraph for a site without a name', () => {
-		( getMigrationType as jest.Mock ).mockReturnValue( 'diy' );
-		( getMigrationStatus as jest.Mock ).mockReturnValue( 'started' );
+			expect(
+				getByText( /Start your migration today and get ready for unmatched WordPress hosting./ )
+			).toBeVisible();
+		} );
 
-		const { container } = render( <MigrationOverview site={ baseSite } /> );
-		const hostingHero = container.querySelector( '.hosting-hero' ) as HTMLElement;
-		expect( within( hostingHero ).getByText( /your site/i ) ).toBeInTheDocument();
+		it( 'shows a link to the upgrade plan page when the site cannot install plugins', () => {
+			const site = buildMigrationSite( {
+				status: 'pending',
+				how: 'difm',
+				canInstallPlugins: false,
+			} );
+
+			render( <MigrationOverview site={ site } /> );
+
+			const link = getStartMigrationLink();
+
+			expect( link ).toHaveAttribute(
+				'href',
+				'/setup/site-migration/site-migration-upgrade-plan?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview&destination=upgrade&how=difm'
+			);
+		} );
+
+		it( 'shows a link to the instructions page when the site is able to install plugins', () => {
+			const site = buildMigrationSite( {
+				status: 'pending',
+				how: 'difm',
+				canInstallPlugins: true,
+			} );
+
+			render( <MigrationOverview site={ site } /> );
+
+			const link = getStartMigrationLink();
+
+			expect( link ).toHaveAttribute(
+				'href',
+				'/setup/migration/site-migration-credentials?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview'
+			);
+		} );
 	} );
 
-	it( 'should render the correct paragraph for a site with a name', () => {
-		( getMigrationType as jest.Mock ).mockReturnValue( 'diy' );
-		( getMigrationStatus as jest.Mock ).mockReturnValue( 'started' );
+	describe( 'DIY started migration', () => {
+		it( 'shows the migrating started instructions', () => {
+			const site = buildMigrationSite( { status: 'started', how: 'diy' } );
 
-		const site = { ...baseSite, name: 'My Site' };
+			render( <MigrationOverview site={ site } /> );
 
-		const { getByText } = render( <MigrationOverview site={ site } /> );
-		expect( getByText( /My Site/i ) ).toBeInTheDocument();
+			expect( screen.queryByText( /Your migration is underway/ ) ).toBeVisible();
+			expect(
+				screen.queryByText(
+					textContentMatcher(
+						/Sit back as Bold Apps transfers to its new home. Get ready for unmatched WordPress hosting./
+					)
+				)
+			).toBeVisible();
+		} );
+
+		it( 'does not show the continue migration link', () => {
+			const site = buildMigrationSite( { status: 'started', how: 'diy' } );
+
+			render( <MigrationOverview site={ site } /> );
+
+			expect( getStartMigrationLink() ).not.toBeInTheDocument();
+		} );
+	} );
+
+	describe( 'DIFM started migration', () => {
+		it( 'shows the migrating started instructions', () => {
+			const site = buildMigrationSite( { status: 'started', how: 'difm' } );
+
+			render( <MigrationOverview site={ site } /> );
+
+			expect( screen.queryByText( /Your migration is underway/ ) ).toBeVisible();
+			expect(
+				screen.queryByText(
+					textContentMatcher(
+						/Sit back as Bold Apps transfers to its new home. Here’s what you can expect./
+					)
+				)
+			).toBeVisible();
+		} );
+
+		it( 'does not show the continue migration link', () => {
+			const site = buildMigrationSite( { status: 'started', how: 'difm' } );
+
+			render( <MigrationOverview site={ site } /> );
+
+			expect( getStartMigrationLink() ).not.toBeInTheDocument();
+		} );
+	} );
+
+	describe( 'no migration strategy was selected', () => {
+		it( 'shows the how to migrate page', () => {
+			const site = buildMigrationSite( { status: 'pending', how: undefined } );
+
+			render( <MigrationOverview site={ site } /> );
+
+			expect( screen.queryByText( /Your WordPress site is ready to be migrated?/ ) ).toBeVisible();
+			expect(
+				screen.queryByText(
+					/Start your migration today and get ready for unmatched WordPress hosting./
+				)
+			).toBeVisible();
+
+			const link = getStartMigrationLink();
+
+			expect( link ).toHaveAttribute(
+				'href',
+				'/setup/site-migration/site-migration-how-to-migrate?siteId=123&siteSlug=example.com&start=true&ref=hosting-migration-overview'
+			);
+		} );
 	} );
 } );
