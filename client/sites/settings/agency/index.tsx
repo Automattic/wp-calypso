e90b7853c707fea3f7687fa8318d0118216cd922
Louis Laugesen <louis.laugesen@gmail.com>
2024-10-31 17:10:08 +1100

    Empty state

--- client/sites/settings/agency/index.tsx
+++ client/sites/settings/agency/index.tsx
@@ -1,4 +1,5 @@
 import { __ } from '@wordpress/i18n';
+import useFetchAgencyFromBlog from 'calypso/a8c-for-agencies/data/agencies/use-fetch-agency-from-blog';
 import NavigationHeader from 'calypso/components/navigation-header';
 import { A4AFullyManagedSiteSetting } from 'calypso/my-sites/site-settings/a4a-fully-managed-site-setting';
 import wrapSettingsForm from 'calypso/my-sites/site-settings/wrap-settings-form';
@@ -13,18 +14,31 @@ const AgencySettings = ( {
 	isSavingSettings,
 } ) => {
 	const site = useSelector( getSelectedSite );
+	const isAtomicSite = site.is_wpcom_atomic;
+
+	const { data: agencySite } = useFetchAgencyFromBlog( site?.ID, { enabled: !! site?.ID } );
+
+	const shouldShowToggle = agencySite && isAtomicSite;
+
 	return (
 		<div className="agency-settings">
 			<NavigationHeader title={ __( 'Agency' ) } />
-			<A4AFullyManagedSiteSetting
-				title={ __( 'Client Access' ) }
-				site={ site }
-				isFullyManagedAgencySite={ fields.is_fully_managed_agency_site }
-				onChange={ handleToggle( 'is_fully_managed_agency_site' ) }
-				isSaving={ isSavingSettings }
-				onSaveSetting={ handleSubmitForm }
-				disabled={ isRequestingSettings || isSavingSettings }
-			/>
+			{ shouldShowToggle && (
+				<A4AFullyManagedSiteSetting
+					title={ __( 'Client Access' ) }
+					site={ site }
+					isFullyManagedAgencySite={ fields.is_fully_managed_agency_site }
+					onChange={ handleToggle( 'is_fully_managed_agency_site' ) }
+					isSaving={ isSavingSettings }
+					onSaveSetting={ handleSubmitForm }
+					disabled={ isRequestingSettings || isSavingSettings }
+				/>
+			) }
+			{ ! shouldShowToggle && (
+				<div>
+					<p>{ __( 'Agency settings are not available for this site.' ) }</p>
+				</div>
+			) }
 		</div>
 	);
 };
