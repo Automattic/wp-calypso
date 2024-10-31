5fea6ff0650538eb8cf4555641cb0b5f74d81df5
Jasper Kang <jasper.kang@automattic.com>
2024-10-31 14:40:06 +1300

    devices

--- client/my-sites/stats/hooks/utils.ts
+++ client/my-sites/stats/hooks/utils.ts
@@ -1,9 +1,11 @@
 export interface QueryStatsParams {
 	date?: string;
+	start_date?: string;
 	days?: number;
 	max?: number;
 	num?: number;
-	period?: string;
+	period?: 'day' | 'week' | 'month' | 'year';
+	summarize?: number;
 }
 
 const getDaysOfMonthFromDate = ( date: string ): number => {
@@ -27,8 +29,10 @@ export const processQueryParams = ( query: QueryStatsParams ) => {
 	// `max` is probably set to 0 to fetch all results.
 	const max = query.max ?? 10;
 	const date = query.date || new Date().toISOString().split( 'T' )[ 0 ];
+	// Explicitly test start_date?
 
 	// Calculate the number of days to query based on the period.
+	// What is days for?? 🧐
 	let days = num;
 	switch ( query.period ) {
 		case 'week':
