4dac800ab5d0079968d9da64e64d37633791876d
Jasper Kang <jasper.kang@automattic.com>
2024-11-01 14:22:11 +1300

    Apply suggestions from code review

--- client/my-sites/stats/hooks/utils.ts
+++ client/my-sites/stats/hooks/utils.ts
@@ -29,10 +29,8 @@ export const processQueryParams = ( query: QueryStatsParams ) => {
 	// `max` is probably set to 0 to fetch all results.
 	const max = query.max ?? 10;
 	const date = query.date || new Date().toISOString().split( 'T' )[ 0 ];
-	// Explicitly test start_date?
 
 	// Calculate the number of days to query based on the period.
-	// What is days for?? 🧐
 	let days = num;
 	switch ( query.period ) {
 		case 'week':
