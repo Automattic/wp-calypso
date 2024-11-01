d7d97d3e91ee99053d43c8ffd3b7e64b0da5261a
Louis Laugesen <louis.laugesen@gmail.com>
2024-11-01 15:19:25 +1100

    Fix types

--- client/sites/settings/site/index.tsx
+++ client/sites/settings/site/index.tsx
@@ -1,3 +1,4 @@
+import { SiteSettings as SiteSettingsType } from '@automattic/data-stores';
 import { __ } from '@wordpress/i18n';
 import NavigationHeader from 'calypso/components/navigation-header';
 import SiteSettingPrivacy from 'calypso/my-sites/site-settings/site-setting-privacy';
@@ -9,13 +10,28 @@ import getIsUnlaunchedSite from 'calypso/state/selectors/is-unlaunched-site';
 import { useSelectedSiteSelector } from 'calypso/state/sites/hooks';
 import { getSiteOption } from 'calypso/state/sites/selectors';
 
+interface Fields {
+	blog_public: number;
+	wpcom_coming_soon: number;
+	wpcom_public_coming_soon: number;
+	wpcom_data_sharing_opt_out: boolean;
+}
+
+interface SiteSettingsProps {
+	fields: Fields;
+	handleSubmitForm: ( event?: React.FormEvent< HTMLFormElement > ) => void;
+	updateFields: ( fields: Fields ) => void;
+	isRequestingSettings: boolean;
+	isSavingSettings: boolean;
+}
+
 const SiteSettings = ( {
 	fields,
 	handleSubmitForm,
 	updateFields,
 	isRequestingSettings,
 	isSavingSettings,
-} ) => {
+}: SiteSettingsProps ) => {
 	const isUnlaunchedSite = useSelectedSiteSelector( getIsUnlaunchedSite );
 	const editingToolkitIsActive = useSelectedSiteSelector(
 		getSiteOption,
@@ -43,13 +59,14 @@ const SiteSettings = ( {
 	);
 };
 
-const getFormSettings = ( settings ) => {
+const getFormSettings = ( settings: SiteSettingsType ): Fields | unknown => {
 	if ( ! settings ) {
 		return {};
 	}
 
-	const { blog_public, wpcom_coming_soon, wpcom_public_coming_soon } = settings;
-	return { blog_public, wpcom_coming_soon, wpcom_public_coming_soon };
+	const { blog_public, wpcom_coming_soon, wpcom_public_coming_soon, wpcom_data_sharing_opt_out } =
+		settings;
+	return { blog_public, wpcom_coming_soon, wpcom_public_coming_soon, wpcom_data_sharing_opt_out };
 };
 
 export default wrapSettingsForm( getFormSettings )( SiteSettings );
