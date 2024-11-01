98f8de9a146070c9ac9d521146ad9f23ff55cfd8
arthur <arthur.chu@automattic.com>
2024-11-01 14:09:36 +0800

    wpcom-block-editor: Simplify the logic of useCanvasMode

--- apps/wpcom-block-editor/src/wpcom/hooks/use-canvas-mode.ts
+++ apps/wpcom-block-editor/src/wpcom/hooks/use-canvas-mode.ts
@@ -1,21 +1,14 @@
 import { useSelect } from '@wordpress/data';
-import { getUnlock } from '../utils';
 import useLocation from './use-location';
-
-const unlock = getUnlock();
 
 const useCanvasMode = () => {
 	const location = useLocation();
 
 	return useSelect(
 		( select ) => {
-			const getCanvasMode = ( unlock &&
-				select( 'core/edit-site' ) &&
-				unlock( select( 'core/edit-site' ) ).getCanvasMode ) as ( () => string ) | undefined;
-
-			// The selector is deprecated after GB 19.6. See https://github.com/WordPress/gutenberg/pull/66213.
-			if ( getCanvasMode ) {
-				return getCanvasMode();
+			// The canvas mode is limited to the site editor.
+			if ( ! select( 'core/edit-site' ) ) {
+				return null;
 			}
 
 			return new URLSearchParams( location?.search ).get( 'canvas' ) || 'view';
