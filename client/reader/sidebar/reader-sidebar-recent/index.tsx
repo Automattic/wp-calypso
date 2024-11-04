a8e377359820a7b73de12aa4e3f6484841ae2b7b
Chris Holder <chris.holder@automattic.com>
2024-11-04 15:08:55 -0600

    Get sites rendering

--- client/reader/sidebar/reader-sidebar-recent/index.tsx
+++ client/reader/sidebar/reader-sidebar-recent/index.tsx
@@ -1,16 +1,40 @@
 import { localize } from 'i18n-calypso';
 import { Component } from 'react';
+import { connect } from 'react-redux';
 import ExpandableSidebarMenu from 'calypso/layout/sidebar/expandable';
 import ReaderFollowingIcon from 'calypso/reader/components/icons/following-icon';
+import getReaderFollowedSites from 'calypso/state/reader/follows/selectors/get-reader-followed-sites';
+
+// TODO: Find the right home for this, or the existing definition
+type Site = {
+	ID: number;
+	URL: string;
+	feed_URL: string;
+	feed_ID: number;
+	date_subscribed: number;
+	last_updated: number;
+	delivery_methods: {
+		notification: {
+			send_posts: boolean;
+		};
+	};
+	is_owner: boolean;
+	organization_id: number;
+	name: string;
+	unseen_count: number;
+	site_icon: string | null;
+	is_following: boolean;
+};
 
 export class ReaderSidebarRecent extends Component< {
 	isOpen: boolean;
 	onClick: () => void;
 	className: string;
 	translate: ( key: string ) => string;
+	sites: Site[];
 } > {
 	render() {
-		const { translate, isOpen, onClick, className } = this.props;
+		const { translate, isOpen, onClick, className, sites } = this.props;
 		return (
 			<li>
 				<ExpandableSidebarMenu
@@ -25,11 +49,17 @@ export class ReaderSidebarRecent extends Component< {
 					materialIcon={ null }
 					materialIconStyle={ null }
 				>
-					<li>Sites list here</li>
+					{ sites.map( ( site ) => (
+						<li key={ site.ID }>{ site.name }</li>
+					) ) }
 				</ExpandableSidebarMenu>
 			</li>
 		);
 	}
 }
 
-export default localize( ReaderSidebarRecent );
+export default connect( ( state ) => {
+	return {
+		sites: getReaderFollowedSites( state ),
+	};
+} )( localize( ReaderSidebarRecent ) );
