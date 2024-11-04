a096d047cd75d0fb160f1f577cf74650f5b21193
DustyReagan <dusty@dustyreagan.com>
2024-11-04 17:15:58 -0600

    Use the FullPostView component

--- client/reader/recent/index.tsx
+++ client/reader/recent/index.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect, useCallback } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { AnyAction } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
+import { FullPostView } from 'calypso/blocks/reader-full-post';
 import { getPostByKey } from 'calypso/state/reader/posts/selectors';
 import { requestPage } from 'calypso/state/reader/streams/actions';
 import { viewStream } from 'calypso/state/reader-ui/actions';
@@ -36,9 +37,14 @@ const Recent = () => {
 						postId: +selectedItem.postId,
 				  } )
 				: null,
+			state,
 		} ),
 		shallowEqual
 	);
+
+	const handleClose = () => {
+		setSelectedItem( null );
+	};
 
 	const fields = [
 		{
@@ -97,7 +103,16 @@ const Recent = () => {
 					defaultLayouts={ defaultLayouts as SupportedLayouts }
 				/>
 			</div>
-			<div className="recent-feed__post-column">{ selectedItem && post && post.title }</div>
+			<div className="recent-feed__post-column">
+				{ selectedItem && post && (
+					<FullPostView
+						post={ post }
+						onClose={ handleClose }
+						referralStream={ window.location.pathname }
+						notificationsOpen
+					/>
+				) }
+			</div>
 		</div>
 	);
 };
