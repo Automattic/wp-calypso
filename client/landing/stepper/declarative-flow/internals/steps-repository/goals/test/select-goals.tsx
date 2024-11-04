7edb270d366f0d196948aa2ed9b6c0f1e031d3bc
Philip Jackson <p-jackson@live.com>
2024-11-05 09:45:00 +1300

    Fix SelectGoal tests to be robust in the face of randomness
    There was a 1% chance the "randomizes goals between sessions" test would
    fail because `shuffleArray` randomly used the same order.

--- client/landing/stepper/declarative-flow/internals/steps-repository/goals/test/select-goals.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/goals/test/select-goals.tsx
@@ -6,6 +6,14 @@ import React from 'react';
 import { seedManager } from 'calypso/landing/stepper/utils/shuffle-array';
 import { SelectGoals } from '../select-goals';
 
+beforeEach( () => {
+	jest.spyOn( global.Math, 'random' ).mockReturnValue( 0.12345 );
+} );
+
+afterEach( () => {
+	jest.spyOn( global.Math, 'random' ).mockRestore();
+} );
+
 describe( 'SelectGoals', () => {
 	it( 'preserves goals order on page refresh', () => {
 		const { rerender } = render( <SelectGoals onChange={ jest.fn() } selectedGoals={ [] } /> );
@@ -23,6 +31,9 @@ describe( 'SelectGoals', () => {
 		const firstRenderGoals = screen.getAllByTestId( 'goal-title' ).map( ( e ) => e.textContent );
 
 		seedManager.clearSeed();
+
+		// Guarantee goals will have a different order (for testing purposes).
+		jest.spyOn( global.Math, 'random' ).mockReturnValue( 0.98765 );
 
 		// I'm simulating a page load by rerendering the component with a different key.
 		rerender( <SelectGoals key="second-instance" onChange={ jest.fn() } selectedGoals={ [] } /> );
