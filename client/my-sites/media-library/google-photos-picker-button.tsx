76b0cf3592e1916566af54029dc8ce634d347651
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-11-04 23:44:29 +0100

    Adjust Google Photos empty state

--- client/my-sites/media-library/google-photos-picker-button.tsx
+++ client/my-sites/media-library/google-photos-picker-button.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@wordpress/components';
+import { Icon, external } from '@wordpress/icons';
 import './google-photos-picker-button.scss';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
+import mediaImage from 'calypso/assets/images/illustrations/media.svg';
 import { useCreateGooglePhotosPickerSessionMutation } from 'calypso/data/media/use-google-photos-picker-session-mutation';
 import useGooglePhotosPickerSessionQuery from 'calypso/data/media/use-google-photos-picker-session-query';
 import { useDispatch, useSelector } from 'calypso/state';
@@ -34,23 +36,18 @@ const GooglePhotosPickerButton = () => {
 
 	return (
 		<div className="google-photos-picker--container media-library__connect-message">
-			<p>
-				{ translate(
-					'Select photos directly from your Google Photos library. Click the button below to open Google’s secure photo picker in a new tab.'
-				) }
-			</p>
-			<Button variant="primary" isBusy={ isPending } onClick={ openPickerTab }>
-				{ translate( 'Open Google Photos Picker' ) }
-			</Button>
-			<p>
-				<small>
-					(
-					{ translate(
-						"this page will automatically update when you've completed your selection"
-					) }
-					)
-				</small>
-			</p>
+			<div className="empty-content">
+				<img src={ mediaImage } width="150" alt={ translate( 'Google Photos' ) } />
+				<h2 className="empty-content__title">{ translate( 'Google Photos' ) }</h2>
+				<h3 className="empty-content__line">
+					{ translate( 'Select photos directly from your Google Photos library.' ) }
+				</h3>
+				<Button variant="primary" isBusy={ isPending } onClick={ openPickerTab }>
+					{ translate( 'Open Google Photos Picker' ) }
+					&nbsp;
+					<Icon icon={ external } size={ 18 } />
+				</Button>
+			</div>
 		</div>
 	);
 };
