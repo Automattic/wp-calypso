738adf74138aa6ceaf2a670bbdf4f44f8d96500f
Allison Levine <allison.levine@automattic.com>
2024-11-05 10:28:04 -0500

    Add featured image and formatting.

--- client/reader/recent/recent-post-field.tsx
+++ client/reader/recent/recent-post-field.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@wordpress/components';
 import { useSelector } from 'react-redux';
+import ReaderFeaturedImage from 'calypso/blocks/reader-featured-image';
 import { getPostByKey } from 'calypso/state/reader/posts/selectors';
 import { AppState } from 'calypso/types';
 import type { ReaderPost } from './types';
-
 interface RecentPostFieldProps {
 	item: ReaderPost;
 	setSelectedItem: ( post: ReaderPost | null ) => void;
@@ -18,9 +18,19 @@ const RecentPostField: React.FC< RecentPostFieldProps > = ( { item, setSelectedI
 	);
 
 	return (
-		<Button onClick={ () => setSelectedItem( item ) }>
-			{ post?.title }
-			{ item.site_name }
+		<Button className="recent-post-field" onClick={ () => setSelectedItem( item ) }>
+			<div className="recent-post-field__title">
+				<div className="recent-post-field__title-text">{ post?.title }</div>
+				<div className="recent-post-field__site-name">{ item.site_name }</div>
+			</div>
+			<div className="recent-post-field__featured-image">
+				<ReaderFeaturedImage
+					imageUrl={ post?.featured_image }
+					imageWidth={ 38 }
+					imageHeight={ 38 }
+					isCompactPost
+				/>
+			</div>
 		</Button>
 	);
 };
