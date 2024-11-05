488f4e19817d0183db29fc6a9d1d89b72fb43d05
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2024-11-05 18:04:32 -0300

    SHow notice on the email forwarding page for Gravatar domains

--- client/my-sites/email/email-forwards-add/index.tsx
+++ client/my-sites/email/email-forwards-add/index.tsx
@@ -7,7 +7,9 @@ import QueryProductsList from 'calypso/components/data/query-products-list';
 import QuerySiteDomains from 'calypso/components/data/query-site-domains';
 import HeaderCake from 'calypso/components/header-cake';
 import Main from 'calypso/components/main';
+import Notice from 'calypso/components/notice';
 import SectionHeader from 'calypso/components/section-header';
+import { getCurrentUserCannotAddEmailReason, getSelectedDomain } from 'calypso/lib/domains';
 import EmailForwardingAddNewCompactList from 'calypso/my-sites/email/email-forwarding/email-forwarding-add-new-compact-list';
 import EmailHeader from 'calypso/my-sites/email/email-header';
 import {
@@ -17,6 +19,7 @@ import {
 import { useSelector } from 'calypso/state';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import {
+	getDomainsBySiteId,
 	hasLoadedSiteDomains,
 	isRequestingSiteDomains,
 } from 'calypso/state/sites/domains/selectors';
@@ -46,6 +49,11 @@ const EmailForwardsAdd = ( { selectedDomainName, source }: EmailForwardsAddProps
 
 	const translate = useTranslate();
 
+	const domains = useSelector( ( state ) => getDomainsBySiteId( state, selectedSite?.ID ) );
+	const selectedDomain = getSelectedDomain( { domains, selectedDomainName } );
+	const cannotAddEmailWarningReason = getCurrentUserCannotAddEmailReason( selectedDomain );
+	const isGravatarDomain = cannotAddEmailWarningReason?.code === 'domain-gravatar-domain';
+
 	const goToEmail = useCallback( (): void => {
 		if ( ! selectedSite ) {
 			return;
@@ -67,6 +75,30 @@ const EmailForwardsAdd = ( { selectedDomainName, source }: EmailForwardsAddProps
 		page( getEmailManagementPath( selectedSite.slug, selectedDomainName, currentRoute ) );
 	}, [ currentRoute, selectedDomainName, selectedSite ] );
 
+	const content = isGravatarDomain ? (
+		<Notice showDismiss={ false } className="email-forwards-add__notice">
+			{ translate( 'This is a Gravatar domain and cannot have email services purchased for it.' ) }
+		</Notice>
+	) : (
+		<Card>
+			{ areDomainsLoading && (
+				<div className="email-forwards-add__placeholder">
+					<p />
+					<p />
+					<Button disabled />
+				</div>
+			) }
+
+			{ ! areDomainsLoading && (
+				<EmailForwardingAddNewCompactList
+					onAddedEmailForwards={ onAddedEmailForwards }
+					onBeforeAddEmailForwards={ noop }
+					selectedDomainName={ selectedDomainName }
+				/>
+			) }
+		</Card>
+	);
+
 	return (
 		<div className="email-forwards-add">
 			<QueryProductsList />
@@ -84,23 +116,7 @@ const EmailForwardsAdd = ( { selectedDomainName, source }: EmailForwardsAddProps
 
 				<SectionHeader label={ translate( 'Add New Email Forwards' ) } />
 
-				<Card>
-					{ areDomainsLoading && (
-						<div className="email-forwards-add__placeholder">
-							<p />
-							<p />
-							<Button disabled />
-						</div>
-					) }
-
-					{ ! areDomainsLoading && (
-						<EmailForwardingAddNewCompactList
-							onAddedEmailForwards={ onAddedEmailForwards }
-							onBeforeAddEmailForwards={ noop }
-							selectedDomainName={ selectedDomainName }
-						/>
-					) }
-				</Card>
+				{ content }
 			</Main>
 		</div>
 	);
