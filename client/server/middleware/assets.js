503863b8a8f497a0a645acc44ff50e8cc399022e
arthur <arthur.chu@automattic.com>
2024-11-05 15:53:47 +0800

    Get rid of fallback

--- client/server/middleware/assets.js
+++ client/server/middleware/assets.js
@@ -58,12 +58,14 @@ export default () => {
 				return EMPTY_ASSETS;
 			}
 
-			const allTheFiles = chunk.chunks
-				? flatten( chunk.chunks.map( ( chunkId ) => getChunkById( assets, chunkId ).files ) )
-				: chunk.files.concat(
-						flatten( chunk.siblings.map( ( sibling ) => getChunkById( assets, sibling ).files ) )
-				  );
+			if ( ! chunk.chunks ) {
+				console.warn( 'cannot find the chunks of the chunk ' + chunkName );
+				return EMPTY_ASSETS;
+			}
 
+			const allTheFiles = flatten(
+				chunk.chunks.map( ( chunkId ) => getChunkById( assets, chunkId ).files )
+			);
 			return groupAssetsByType( allTheFiles );
 		};
 
