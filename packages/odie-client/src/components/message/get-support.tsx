9f1c0527cc69f3909e8a4a4242984e3717174f8c
Tony Arcangelini <tony@arcangelini.com>
2024-11-08 10:49:20 -0600

    Odie Client: hide contact button after clicking

--- packages/odie-client/src/components/message/get-support.tsx
+++ packages/odie-client/src/components/message/get-support.tsx
@@ -6,7 +6,8 @@ import './get-support.scss';
 
 export const GetSupport = () => {
 	const newConversation = useCreateZendeskConversation();
-	const { shouldUseHelpCenterExperience } = useOdieAssistantContext();
+	const { shouldUseHelpCenterExperience, chat } = useOdieAssistantContext();
+
 	const handleOnClick = async ( event: React.MouseEvent< HTMLButtonElement > ) => {
 		event.preventDefault();
 
@@ -18,6 +19,12 @@ export const GetSupport = () => {
 			? __( 'Get instant support', __i18n_text_domain__ )
 			: __( 'Get support', __i18n_text_domain__ );
 	};
+
+	// We don't want the user to see this button if they are already talking to a human.
+	if ( chat.provider !== 'odie' ) {
+		return null;
+	}
+
 	return (
 		<div className="odie__transfer-to-human">
 			<button onClick={ handleOnClick }>{ getButtonText() }</button>
