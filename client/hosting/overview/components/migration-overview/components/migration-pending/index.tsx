ea60114f6aeef8fd333e1e2acf06204cc818e43a
Gabriel Caires <gabriel.caires@automattic.com>
2024-11-11 12:27:51 +0000

    Reapply "Force page reload after cancel migration"
    This reverts commit e54fa9c40bd4290f0587f77fc4cada762b92434a.

--- client/hosting/overview/components/migration-overview/components/migration-pending/index.tsx
+++ client/hosting/overview/components/migration-overview/components/migration-pending/index.tsx
@@ -1,13 +1,15 @@
 import { LoadingPlaceholder } from '@automattic/components';
+import { setSelectedSite } from '@automattic/data-stores/src/onboard/actions';
 import { Button } from '@wordpress/components';
 import { translate } from 'i18n-calypso';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { HostingHeroButton } from 'calypso/components/hosting-hero';
 import { useMigrationCancellation } from 'calypso/data/site-migration/landing/use-migration-cancellation';
 import { addQueryArgs } from 'calypso/lib/url';
 import { getMigrationType } from 'calypso/sites-dashboard/utils';
-import { useDispatch } from 'calypso/state';
+import { useDispatch, useSelector } from 'calypso/state';
 import { requestSite } from 'calypso/state/sites/actions';
+import { isRequestingSite } from 'calypso/state/sites/selectors';
 import Cards from '../cards';
 import { Container, Header } from '../layout';
 import type { SiteDetails } from '@automattic/data-stores';
@@ -31,35 +33,48 @@ const getContinueMigrationUrl = ( site: SiteDetails ): string | null => {
 	return addQueryArgs( baseQueryArgs, '/setup/hosted-site-migration/site-migration-credentials' );
 };
 
-export const MigrationPending = ( { site }: { site: SiteDetails } ) => {
-	const continueMigrationUrl = getContinueMigrationUrl( site );
+// Hook to force a site reload after migration cancellation
+// This is used to clear the migration status from the site state
+const useForceSiteRefresh = ( siteId: number ) => {
+	const [ isRefreshing, setIsRefreshing ] = useState( false );
+	const isSiteRefreshing = useSelector( ( state ) => isRequestingSite( state, siteId ) );
+	const isRefreshCompleted = isSiteRefreshing === false && isRefreshing;
+	const dispatch = useDispatch();
 
-	const title = translate( 'Your WordPress site is ready to be migrated' );
-	const subTitle = translate(
-		'Start your migration today and get ready for unmatched WordPress hosting.'
-	);
+	useEffect( () => {
+		if ( isRefreshCompleted ) {
+			window.location.reload();
+		}
+	}, [ isRefreshCompleted ] );
+
+	const forceSiteReload = () => {
+		dispatch( setSelectedSite( siteId ) );
+		dispatch( requestSite( siteId ) );
+		setIsRefreshing( true );
+	};
+
+	return { forceSiteReload, isRefreshing };
+};
+
+export const MigrationPending = ( { site }: { site: SiteDetails } ) => {
 	const {
 		mutate: cancelMigration,
 		isSuccess: isCancellationSuccess,
 		isPending: isCancelling,
 	} = useMigrationCancellation( site.ID );
-	const dispatch = useDispatch();
-
-	const reloadSite = useCallback( () => {
-		dispatch( requestSite( site.ID ) );
-	}, [ dispatch, site.ID ] );
+	const { forceSiteReload, isRefreshing } = useForceSiteRefresh( site.ID );
 
 	useEffect( () => {
 		if ( isCancellationSuccess ) {
-			reloadSite();
+			forceSiteReload();
 		}
-	}, [ isCancellationSuccess, reloadSite ] );
+	}, [ isCancellationSuccess, forceSiteReload ] );
 
 	const handleCancelButtonClick = useCallback( () => {
 		cancelMigration();
 	}, [ cancelMigration ] );
 
-	if ( isCancelling || isCancellationSuccess ) {
+	if ( isCancelling || isRefreshing ) {
 		return (
 			<LoadingPlaceholder
 				aria-busy
@@ -69,9 +84,16 @@ export const MigrationPending = ( { site }: { site: SiteDetails } ) => {
 		);
 	}
 
+	const continueMigrationUrl = getContinueMigrationUrl( site );
+
 	return (
 		<Container>
-			<Header title={ title } subTitle={ subTitle }>
+			<Header
+				title={ translate( 'Your WordPress site is ready to be migrated' ) }
+				subTitle={ translate(
+					'Start your migration today and get ready for unmatched WordPress hosting.'
+				) }
+			>
 				{ continueMigrationUrl && (
 					<div className="migration-pending__buttons">
 						<HostingHeroButton href={ continueMigrationUrl }>
