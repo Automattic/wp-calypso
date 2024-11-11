fbed8c0b6f4dbb1ac7dfb25e60a5dc6748342779
Yashwin <yashwinpoojary@gmail.com>
2024-11-11 19:01:19 +0530

    improve the logic to get the current quarter

--- client/a8c-for-agencies/sections/migrations/consolidated-commissions/index.tsx
+++ client/a8c-for-agencies/sections/migrations/consolidated-commissions/index.tsx
@@ -3,25 +3,14 @@ import { useTranslate } from 'i18n-calypso';
 
 import './style.scss';
 
+const getCurrentQuarter = () => {
+	const currentMonth = new Date().getMonth();
+	const quarter = Math.ceil( ( currentMonth + 1 ) / 3 );
+	return `Q${ quarter }`;
+};
+
 export default function MigrationsConsolidatedCommissions() {
 	const translate = useTranslate();
-
-	const getCurrentQuarter = () => {
-		const date = new Date();
-		const quarter = Math.floor( ( date.getMonth() + 3 ) / 3 );
-		switch ( quarter ) {
-			case 1:
-				return 'Q1';
-			case 2:
-				return 'Q2';
-			case 3:
-				return 'Q3';
-			case 4:
-				return 'Q4';
-			default:
-				return '';
-		}
-	};
 
 	const data = {
 		migrationCommissions: 300,
