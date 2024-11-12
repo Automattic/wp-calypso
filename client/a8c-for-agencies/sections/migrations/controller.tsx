e1b7031d84ac407069194ca36051ce09ff060e34
Yashwin <yashwinpoojary@gmail.com>
2024-11-12 15:41:45 +0530

    Update the Migrations Payment Settings page

--- client/a8c-for-agencies/sections/migrations/controller.tsx
+++ client/a8c-for-agencies/sections/migrations/controller.tsx
@@ -45,7 +45,7 @@ export const migrationsPaymentSettingsContext: Callback = ( context, next ) => {
 	context.primary = (
 		<>
 			<PageViewTracker title="Migrations > Payment Settings" path={ context.path } />
-			<ReferralsBankDetails isAutomatedReferral />
+			<ReferralsBankDetails isAutomatedReferral isMigrations />
 		</>
 	);
 	context.secondary = <MigrationsSidebar path={ context.path } />;
