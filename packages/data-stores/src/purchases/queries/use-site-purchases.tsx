dd68e71edf1d57cf8f3416e518cbe985c1d3a40f
Payton Swick <payton@foolord.com>
2024-11-13 16:39:13 -0500

    Add withSitePurchases HOC

--- packages/data-stores/src/purchases/queries/use-site-purchases.tsx
+++ packages/data-stores/src/purchases/queries/use-site-purchases.tsx
@@ -1,15 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
 import wpcomRequest from 'wpcom-proxy-request';
 import { createPurchaseObject } from '../lib/assembler';
 import useQueryKeysFactory from './lib/use-query-keys-factory';
 import type { RawPurchase, Purchase } from '../types';
+import type { ComponentType } from 'react';
 
 export interface PurchasesIndex {
 	[ purchaseId: number ]: Purchase;
 }
 
-interface Props {
+export interface GetUseSitePurchasesOptionsProps {
 	siteId?: string | number | null;
+}
+
+export type SitePurchasesState = UseQueryResult< Purchase[], Error >;
+
+export interface WithSitePurchasesProps {
+	sitePurchasesState: SitePurchasesState;
 }
 
 async function fetchSitePurchases( siteId: string | number ): Promise< RawPurchase[] > {
@@ -32,7 +39,7 @@ async function fetchAndTransformSitePurchases(
 }
 
 export function getUseSitePurchasesOptions(
-	{ siteId }: Props,
+	{ siteId }: GetUseSitePurchasesOptionsProps,
 	queryKey: ( string | number | null | undefined )[]
 ) {
 	return {
@@ -52,10 +59,20 @@ export function getUseSitePurchasesOptions(
 /**
  * Fetches all purchases for a given site, transformed into a map of purchaseId => Purchase
  */
-export default function useSitePurchases( { siteId }: Props ) {
+export default function useSitePurchases( { siteId }: GetUseSitePurchasesOptionsProps ) {
 	const queryKeys = useQueryKeysFactory();
 
 	return useQuery< PurchasesIndex, Error, PurchasesIndex >(
 		getUseSitePurchasesOptions( { siteId }, queryKeys.sitePurchases( siteId ) )
 	);
+}
+
+export function withSitePurchases< P >(
+	Component: ComponentType< P >,
+	siteId: GetUseSitePurchasesOptionsProps[ 'siteId' ]
+) {
+	return function UserPurchasesWrapper( props: Omit< P, keyof WithSitePurchasesProps > ) {
+		const state = useSitePurchases( { siteId } );
+		return <Component { ...( props as P ) } sitePurchasesState={ state } />;
+	};
 }
