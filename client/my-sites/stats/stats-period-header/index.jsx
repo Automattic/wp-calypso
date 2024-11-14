8ce33fe0c53328afed83bcd96dcf4a9a67048b59
Anna McPhee <anna.mcphee@automattic.com>
2024-11-14 13:15:07 +1100

    fix up classname passing

--- client/my-sites/stats/stats-period-header/index.jsx
+++ client/my-sites/stats/stats-period-header/index.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
 import './style.scss';
 
-const StatsPeriodHeader = ( { children, className } ) => {
-	return <div className={ `stats__period-header ${ className || '' }` }>{ children }</div>;
+const StatsPeriodHeader = ( { children, className = '' } ) => {
+	return <div className={ `stats__period-header ${ className }`.trim() }>{ children }</div>;
+};
+
+StatsPeriodHeader.propTypes = {
+	children: PropTypes.node.isRequired,
+	className: PropTypes.string,
 };
 
 export default StatsPeriodHeader;
