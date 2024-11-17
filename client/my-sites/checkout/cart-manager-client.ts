45bf9c81bac7319b52fe01ac02cc23c3e8213e30
Payton Swick <payton@foolord.com>
2024-11-17 17:46:01 -0500

    Update calypso to use default getCart/setCart

--- client/my-sites/checkout/cart-manager-client.ts
+++ client/my-sites/checkout/cart-manager-client.ts
@@ -1,12 +1,3 @@
 import { createShoppingCartManagerClient } from '@automattic/shopping-cart';
-import wp from 'calypso/lib/wp';
-import type { RequestCart, CartKey } from '@automattic/shopping-cart';
 
-const wpcomGetCart = ( cartKey: CartKey ) => wp.req.get( `/me/shopping-cart/${ cartKey }` );
-const wpcomSetCart = ( cartKey: CartKey, cartData: RequestCart ) =>
-	wp.req.post( `/me/shopping-cart/${ cartKey }`, cartData );
-
-export const cartManagerClient = createShoppingCartManagerClient( {
-	getCart: wpcomGetCart,
-	setCart: wpcomSetCart,
-} );
+export const cartManagerClient = createShoppingCartManagerClient();
