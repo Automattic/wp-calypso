bd24b6c8733d37a6c01d58e0199f37773fd7cfbd
Payton Swick <payton@foolord.com>
2024-11-17 17:58:46 -0500

    Update onboarding package to use default getCart/setCart

--- packages/onboarding/src/cart/create-cart-manager-client.ts
+++ packages/onboarding/src/cart/create-cart-manager-client.ts
@@ -1,34 +1,3 @@
 import { createShoppingCartManagerClient } from '@automattic/shopping-cart';
-import wpcomRequest from 'wpcom-proxy-request';
-import type {
-	RequestCart,
-	CartKey,
-	ResponseCart,
-	ResponseCartProduct,
-} from '@automattic/shopping-cart';
 
-const wpcomGetCart = ( cartKey: CartKey ) => {
-	let source;
-	try {
-		source = window?.location?.pathname;
-	} catch {
-		// Ignore failures here if window is not present.
-	}
-	return wpcomRequest< ResponseCart< ResponseCartProduct > >( {
-		path: `/me/shopping-cart/${ cartKey }?source=${ source ?? 'unknown' }`,
-		method: 'GET',
-		apiVersion: '1.1',
-	} );
-};
-const wpcomSetCart = ( cartKey: CartKey, cartData: RequestCart ) =>
-	wpcomRequest< ResponseCart< ResponseCartProduct > >( {
-		path: `/me/shopping-cart/${ cartKey }`,
-		apiVersion: '1.1',
-		method: 'POST',
-		body: cartData,
-	} );
-
-export default createShoppingCartManagerClient( {
-	getCart: wpcomGetCart,
-	setCart: wpcomSetCart,
-} );
+export default createShoppingCartManagerClient();
