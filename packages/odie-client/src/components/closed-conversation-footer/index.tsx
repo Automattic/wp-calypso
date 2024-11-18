778392d990832e170672fa4915fa62ef106ba2ad
heavyweight <kpapazov@gmail.com>
2024-11-18 10:32:10 +0100

    Hide closed conversation footer when loading

--- packages/odie-client/src/components/closed-conversation-footer/index.tsx
+++ packages/odie-client/src/components/closed-conversation-footer/index.tsx
@@ -2,20 +2,27 @@ import { Button } from '@wordpress/components';
 import { comment, Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import { v4 as uuidv4 } from 'uuid';
+import { useOdieAssistantContext } from '../../context';
 import { useManageSupportInteraction } from '../../data';
 import './style.scss';
 
 export const ClosedConversationFooter = () => {
 	const { __ } = useI18n();
+	const { trackEvent, chat, shouldUseHelpCenterExperience } = useOdieAssistantContext();
 
 	const { startNewInteraction } = useManageSupportInteraction();
 
 	const handleOnClick = async () => {
+		trackEvent( 'chat_new_from_closed_conversation' );
 		await startNewInteraction( {
 			event_source: 'help-center',
 			event_external_id: uuidv4(),
 		} );
 	};
+
+	if ( ! shouldUseHelpCenterExperience || chat.status !== 'closed' ) {
+		return null;
+	}
 
 	return (
 		<div className="odie-closed-conversation-footer">
