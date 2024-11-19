0be003b22e038c6b72aacb6a175f6b80341e50c9
Roberto Aranda <roberto.aranda@automattic.com>
2024-11-19 15:45:40 +0100

    Business Tools: Refactor to extract component logic to a data component

---
+++ client/sites/marketing/tools/types.ts
@@ -0,0 +1,11 @@
+export interface MarketingToolsFeatureData {
+	title: string;
+	description: string;
+	imagePath: string;
+	imageAlt?: string;
+	buttonText: string;
+	buttonHref?: string;
+	buttonTarget?: '_blank' | '_self';
+	onClick?: () => void;
+	showOnlyInEnglish?: boolean;
+}
