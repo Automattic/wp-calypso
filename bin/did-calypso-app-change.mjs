711d76e606d4663db703786494f5764cacc723f1
Philip Jackson <p-jackson@live.com>
2024-11-20 11:51:26 +1300

    Bump max buffer size

--- bin/did-calypso-app-change.mjs
+++ bin/did-calypso-app-change.mjs
@@ -34,7 +34,7 @@ export default async function didCalypsoAppChange( { slug, dir, artifactDir } ) 
 	try {
 		await exec(
 			`diff -r --exclude="*.js.map" --exclude="*.asset.php" --exclude="build_meta.json" --exclude="README.md" ${ artifactDir } ${ prevReleaseDir }`,
-			{ cwd: dir, maxBuffer: 1024 * 1024 * 16 } // 16MB buffer for large diffs.
+			{ cwd: dir, maxBuffer: 1024 * 1024 * 32 } // 32MB buffer for large diffs.
 		);
 		return false;
 	} catch ( { code, stdout, stderr } ) {
