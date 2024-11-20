137f0d4d052f5989e25063db34ec0ed374eceb83
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-11-20 12:46:19 +0100

    Updated tittle

--- client/sites/marketing/connections/google-photos-migration.jsx
+++ client/sites/marketing/connections/google-photos-migration.jsx
@@ -3,7 +3,7 @@ import { Fragment } from 'react';
 
 const GooglePhotosMigration = ( { translate } ) => (
 	<Fragment>
-		<h3>{ translate( 'Your Google Photos connection is being upgraded!' ) }</h3>
+		<h3>{ translate( 'Your Google Photos connection is being updated!' ) }</h3>
 		<p>
 			{ translate(
 				"We've updated our Google Photos service. You will need to disconnect and reconnect to continue accessing your photos."
