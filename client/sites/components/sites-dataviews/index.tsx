0e9dc9ef27a99f92860a4ec558130295eb9a3cc1
Louis Laugesen <louis.laugesen@gmail.com>
2024-11-20 12:27:29 +1100

    Remove all dataview css overrides from /sites

--- client/sites/components/sites-dataviews/index.tsx
+++ client/sites/components/sites-dataviews/index.tsx
@@ -15,7 +15,6 @@ import type { SiteExcerptData } from '@automattic/sites';
 import type { View } from '@wordpress/dataviews';
 
 import './style.scss';
-import './dataview-style.scss';
 
 type Props = {
 	sites: SiteExcerptData[];
