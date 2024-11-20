fb72658cbf9b917ad81e3570a775101381e8adf3
Jeremy Yip <jeremy.yip@automattic.com>
2024-11-20 14:57:20 -0800

    Remove completed TODO around react query key generation

--- packages/data-stores/src/plans/queries/lib/use-query-keys-factory.ts
+++ packages/data-stores/src/plans/queries/lib/use-query-keys-factory.ts
@@ -1,5 +1,4 @@
 const useQueryKeysFactory = () => ( {
-	// TODO: Verify that this is the correct convention for query keys
 	sitePlans: ( coupon?: string, siteId?: string | number | null ) => [
 		'site-plans',
 		siteId,
