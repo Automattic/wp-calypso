98b98a91743f1867fb847ca7822647587f733a17
Claudiu Filip <claudiu.filip@automattic.com>
2024-11-21 11:32:16 +0200

    Add Black Friday card with illustration

---
+++ client/my-sites/customer-home/cards/tasks/black-friday-2024/index.jsx
@@ -0,0 +1,30 @@
+import { localizeUrl } from '@automattic/i18n-utils';
+import { useTranslate } from 'i18n-calypso';
+import blackFriday2024Illustration from 'calypso/assets/images/customer-home/illustration--black-friday-2024.png';
+import { TASK_BLACK_FRIDAY_2024 } from 'calypso/my-sites/customer-home/cards/constants';
+import Task from 'calypso/my-sites/customer-home/cards/tasks/task';
+
+import './style.scss';
+
+const Affiliates = () => {
+	const translate = useTranslate();
+
+	const title = translate( 'The Biggest Sale of the Year' );
+	const description = translate( 'For a limited time, we’re offering an unbeatable deal.' );
+
+	return (
+		<Task
+			customClass="task__black-friday-2024"
+			title={ title }
+			description={ description }
+			actionText={ translate( 'Get the Sale' ) }
+			actionUrl={ localizeUrl( 'https://wordpress.com/pricing/black-friday-2024' ) }
+			actionTarget="_blank"
+			completeOnStart={ false }
+			illustration={ blackFriday2024Illustration }
+			taskId={ TASK_BLACK_FRIDAY_2024 }
+		/>
+	);
+};
+
+export default Affiliates;
