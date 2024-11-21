339f8d9bd2cac70c2d828f0650f2a3dc720428eb
Christos <chriskmnds@gmail.com>
2024-11-21 10:41:45 +0200

    Revert "Revert "skip term-saving: inside the hook""
    This reverts commit 0a76d1c89a9c0a1e152ca8b8c70a752836d85455.

--- packages/plans-grid-next/src/hooks/data-store/types.ts
+++ packages/plans-grid-next/src/hooks/data-store/types.ts
@@ -20,6 +20,7 @@ export interface UseGridPlansParams {
 	siteId?: number | null;
 	storageAddOns: ( AddOnMeta | null )[];
 	term?: ( typeof TERMS_LIST )[ number ]; // defaults to monthly
+	terms?: ( typeof TERMS_LIST )[];
 	useCheckPlanAvailabilityForPurchase: Plans.UseCheckPlanAvailabilityForPurchase;
 	useFreeTrialPlanSlugs?: UseFreeTrialPlanSlugs;
 	/**
