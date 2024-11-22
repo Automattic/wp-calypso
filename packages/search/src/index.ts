240e7f2f8c649f1c81c2a704558b234d911dcb12
Roberto Aranda <roberto.aranda@automattic.com>
2024-11-22 13:06:37 +0100

    Remove use-terms-suggestions from index

--- packages/search/src/index.ts
+++ packages/search/src/index.ts
@@ -1,5 +1,4 @@
 export { default } from './search';
 export type { ImperativeHandle } from './search';
 export { SearchIcon } from './search-icon';
-export { useTermsSuggestions } from './use-terms-suggestions';
 export { useFuzzySearch } from './use-fuzzy-search';
