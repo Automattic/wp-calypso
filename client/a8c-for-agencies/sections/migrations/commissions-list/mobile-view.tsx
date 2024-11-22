4ad92e9e2b94588d1745cbab71c550683267875e
Andrii Lysenko <60262784+andrii-lysenko@users.noreply.github.com>
2024-11-22 14:49:43 -0800

    Revert "Revert "A4A > Migrations: Implement Get & Save tagged sites for commi…"
    This reverts commit 0aee66e774172d3a45568d329ea8d002add088cc.

--- client/a8c-for-agencies/sections/migrations/commissions-list/mobile-view.tsx
+++ client/a8c-for-agencies/sections/migrations/commissions-list/mobile-view.tsx
@@ -5,14 +5,14 @@ import {
 	ListItemCardContent,
 } from 'calypso/a8c-for-agencies/components/list-item-cards';
 import { MigratedOnColumn, ReviewStatusColumn, SiteColumn } from './commission-columns';
-import type { MigrationCommissionItem } from '../types';
+import type { TaggedSite } from '../types';
 
 import './style.scss';
 
 export default function MigrationsCommissionsListMobileView( {
 	commissions,
 }: {
-	commissions: MigrationCommissionItem[];
+	commissions: TaggedSite[];
 } ) {
 	const translate = useTranslate();
 
@@ -23,16 +23,16 @@ export default function MigrationsCommissionsListMobileView( {
 					<ListItemCard key={ commission.id }>
 						<ListItemCardContent title={ translate( 'Site' ) }>
 							<div className="migrations-commissions-list-mobile-view__column">
-								<SiteColumn site={ commission.siteUrl } />
+								<SiteColumn site={ commission.url } />
 							</div>
 						</ListItemCardContent>
 						<ListItemCardContent title={ translate( 'Migrated on' ) }>
 							<div className="migrations-commissions-list-mobile-view__column">
-								<MigratedOnColumn migratedOn={ commission.migratedOn } />
+								<MigratedOnColumn migratedOn={ commission.created_at } />
 							</div>
 						</ListItemCardContent>
 						<ListItemCardContent title={ translate( 'Review status' ) }>
-							<ReviewStatusColumn reviewStatus={ commission.reviewStatus } />
+							<ReviewStatusColumn reviewStatus={ commission.state } />
 						</ListItemCardContent>
 					</ListItemCard>
 				) ) }
