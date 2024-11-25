8f6640f6da48d05c00acc3b39ec55711c1ed317b
Jasper Kang <jasper.kang@automattic.com>
2024-11-25 14:26:15 +1300

    prevent indefinite loop

--- client/my-sites/stats/site.jsx
+++ client/my-sites/stats/site.jsx
@@ -351,14 +351,14 @@ class StatsSite extends Component {
 				.format( 'YYYY-MM-DD' );
 		}
 
+		customChartRange.daysInRange = daysInRange;
+
 		// TODO: all the date logic should be done in controllers, otherwise it affects the performance.
 		// If it's single day period, redirect to hourly stats.
 		if ( period === 'day' && daysInRange === 1 ) {
-			page( '/stats/hour/' + slug + window.location.search );
+			page.redirect( `/stats/hour/${ slug } ${ window.location.search }` );
 			return;
 		}
-
-		customChartRange.daysInRange = daysInRange;
 
 		// Calculate diff between requested start and end in `priod` units.
 		// Move end point (most recent) to the end of period to account for partial periods
