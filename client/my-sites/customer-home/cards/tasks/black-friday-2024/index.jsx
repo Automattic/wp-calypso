69a72245f87faecae08ae5d603c42cd1636bb57c
Claudiu Filip <claudiu.filip@automattic.com>
2024-11-25 10:26:58 +0200

    Set CTA link to go to plans page

--- client/my-sites/customer-home/cards/tasks/black-friday-2024/index.jsx
+++ client/my-sites/customer-home/cards/tasks/black-friday-2024/index.jsx
@@ -1,13 +1,15 @@
-import { localizeUrl } from '@automattic/i18n-utils';
 import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
 import blackFriday2024Illustration from 'calypso/assets/images/customer-home/illustration--black-friday-2024.jpg';
 import { TASK_BLACK_FRIDAY_2024 } from 'calypso/my-sites/customer-home/cards/constants';
 import Task from 'calypso/my-sites/customer-home/cards/tasks/task';
+import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 import './style.scss';
 
 const Affiliates = () => {
 	const translate = useTranslate();
+	const siteSlug = useSelector( getSelectedSiteSlug );
 
 	const title = translate( 'The Biggest Sale of the Year' );
 	const description = translate( 'For a limited time, we’re offering an unbeatable deal.' );
@@ -18,8 +20,7 @@ const Affiliates = () => {
 			title={ title }
 			description={ description }
 			actionText={ translate( 'Get the Sale' ) }
-			actionUrl={ localizeUrl( 'https://wordpress.com/pricing/black-friday-2024' ) }
-			actionTarget="_blank"
+			actionUrl={ `/plans/${ siteSlug }?coupon=` }
 			completeOnStart={ false }
 			illustration={ blackFriday2024Illustration }
 			taskId={ TASK_BLACK_FRIDAY_2024 }
