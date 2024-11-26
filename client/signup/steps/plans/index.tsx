78e08fc279f651699997fd50364766f5dc9855fa
Christos <chriskmnds@gmail.com>
2024-11-26 19:02:06 +0200

    Plans Step: TS and function form

--- client/signup/steps/plans/index.tsx
+++ client/signup/steps/plans/index.tsx
@@ -7,12 +7,13 @@ import {
 	isOnboardingGuidedFlow,
 	ONBOARDING_GUIDED_FLOW,
 } from '@automattic/onboarding';
-import { isDesktop, subscribeIsDesktop } from '@automattic/viewport';
+import { MinimalRequestCartProduct } from '@automattic/shopping-cart';
+import { isDesktop as isDesktopViewport, subscribeIsDesktop } from '@automattic/viewport';
+import { useEffect, useState } from '@wordpress/element';
 import clsx from 'clsx';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { parse as parseQs } from 'qs';
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AsyncLoad from 'calypso/components/async-load';
 import FormattedHeader from 'calypso/components/formatted-header';
@@ -37,64 +38,127 @@ import { getSiteBySlug } from 'calypso/state/sites/selectors';
 import { getIntervalType, shouldBasePlansOnSegment } from './util';
 import './style.scss';
 
-export class PlansStep extends Component {
-	state = {
-		isDesktop: isDesktop(),
+interface Props {
+	saveSignupStep: ( step: { stepName: string } ) => void;
+	flowName: string;
+	stepName: string;
+	/**
+	 * TODO: Stepper pass something?
+	 */
+	customerType?: string;
+	submitSignupStep: ( stepInfo: object, domainInfo: object ) => void;
+	isCustomDomainAllowedOnFreePlan: boolean;
+	displayedIntervals: any; // Define proper type
+	onPlanIntervalUpdate: () => void;
+	hidePersonalPlan: boolean;
+	hidePremiumPlan: boolean;
+	hideEnterprisePlan: boolean;
+	recordTracksEvent: ( event: string ) => void;
+	translate: ( text: string, options?: object ) => string;
+	headerText?: string;
+	fallbackHeaderText?: string;
+	useEmailOnboardingSubheader: boolean;
+	deemphasizeFreePlan: boolean;
+	hasInitializedSitesBackUrl: string | null;
+	steps: string[];
+	wrapperProps: object;
+	useStepperWrapper: boolean;
+	queryParams: object;
+	progress?: Record< string, any >; // Define proper type
+	positionInFlow: number;
+	shouldHideNavButtons: boolean;
+	signupDependencies: {
+		siteUrl?: string;
+		domainItem?: { meta?: string };
+		siteTitle?: string;
+		username?: string;
+		coupon?: string;
+		segmentationSurveyAnswers?: any; // Define proper type
+	};
+	selectedSite: {
+		URL?: string;
+		ID?: number;
+	};
+	intent?: string;
+	/**
+	 * TODO: Stepper pass something?
+	 */
+	disableBloggerPlanWithNonBlogDomain: boolean;
+	deemphasizeFreePlanFromProps: boolean;
+	hideFreePlan: boolean;
+	isLaunchPage: boolean;
+	intervalType?: string;
+	longerPlanTermDefaultExperiment?: { term?: string };
+	isDomainOnlySite: boolean;
+	initialContext?: {
+		trailMapExperimentVariant?: null | 'treatment_guided' | 'treatment_survey_only';
+	};
+	/**
+	 * TODO: Stepper pass something?
+	 */
+	path: string;
+	step?: {
+		status?: string;
+		errors?: { message: string };
+	};
+}
+
+export function PlansStep( props: Props ) {
+	const [ isDesktop, setIsDesktop ] = useState< boolean | undefined >( isDesktopViewport() );
+
+	useEffect( () => {
+		const unsubscribe = subscribeIsDesktop( ( matchesDesktop ) => setIsDesktop( matchesDesktop ) );
+		props.saveSignupStep( { stepName: props.stepName } );
+
+		if ( isTailoredSignupFlow( props.flowName ) ) {
+			triggerGuidesForStep( props.flowName, 'plans' );
+		}
+
+		return () => {
+			unsubscribe();
+		};
+	}, [] );
+
+	const onSelectPlan = ( cartItems?: MinimalRequestCartProduct[] | null ) => {
+		buildUpgradeFunction( props, cartItems );
 	};
 
-	componentDidMount() {
-		this.unsubscribe = subscribeIsDesktop( ( matchesDesktop ) =>
-			this.setState( { isDesktop: matchesDesktop } )
-		);
-		this.props.saveSignupStep( { stepName: this.props.stepName } );
-
-		if ( isTailoredSignupFlow( this.props.flowName ) ) {
-			triggerGuidesForStep( this.props.flowName, 'plans' );
-		}
-	}
-
-	componentWillUnmount() {
-		this.unsubscribe();
-	}
-
-	onSelectPlan( cartItems ) {
-		buildUpgradeFunction( this.props, cartItems );
-	}
-
-	getCustomerType() {
-		if ( this.props.customerType ) {
-			return this.props.customerType;
+	const getCustomerType = () => {
+		if ( props.customerType ) {
+			return props.customerType;
 		}
 
 		return 'personal';
-	}
+	};
 
-	removePaidDomain = () => {
+	const removePaidDomain = () => {
 		const domainItem = undefined;
 
-		this.props.submitSignupStep(
+		props.submitSignupStep(
 			{
 				stepName: 'domains',
 				domainItem,
 				isPurchasingItem: false,
 				stepSectionName: undefined,
 			},
-			// Since we're removing the paid domain, it means that the user chose to continue
-			// with a free domain. Because signupDomainOrigin should reflect the last domain
-			// selection status before they land on the checkout page, we switch the value
-			// to "free".
-			{ domainItem, signupDomainOrigin: SIGNUP_DOMAIN_ORIGIN.FREE }
+			{
+				domainItem,
+				signupDomainOrigin: SIGNUP_DOMAIN_ORIGIN.FREE,
+			}
 		);
 	};
 
-	setSiteUrlAsFreeDomainSuggestion = ( freeDomainSuggestion ) => {
+	const setSiteUrlAsFreeDomainSuggestion = ( freeDomainSuggestion: {
+		domain_name: string;
+		product_slug?: string;
+	} ) => {
 		if ( freeDomainSuggestion?.product_slug ) {
 			return;
 		}
 
 		const siteUrl = freeDomainSuggestion.domain_name.replace( '.wordpress.com', '' );
 
-		this.props.submitSignupStep(
+		props.submitSignupStep(
 			{
 				stepName: 'domains',
 				siteUrl,
@@ -103,7 +167,7 @@ export class PlansStep extends Component {
 		);
 	};
 
-	plansFeaturesList() {
+	const plansFeaturesList = () => {
 		const {
 			disableBloggerPlanWithNonBlogDomain,
 			deemphasizeFreePlan: deemphasizeFreePlanFromProps,
@@ -116,12 +180,12 @@ export class PlansStep extends Component {
 			intervalType,
 			isDomainOnlySite,
 			longerPlanTermDefaultExperiment,
-		} = this.props;
+		} = props;
 
 		const intervalTypeValue =
 			intervalType ||
 			getIntervalType(
-				this.props.path,
+				props.path,
 				flowName === 'onboarding' && longerPlanTermDefaultExperiment?.term
 					? longerPlanTermDefaultExperiment.term
 					: undefined
@@ -129,17 +193,17 @@ export class PlansStep extends Component {
 
 		let errorDisplay;
 
-		if ( 'invalid' === this.props.step?.status ) {
+		if ( 'invalid' === props.step?.status ) {
 			errorDisplay = (
 				<div>
 					<Notice status="is-error" showDismiss={ false }>
-						{ this.props.step.errors.message }
+						{ props.step?.errors?.message }
 					</Notice>
 				</div>
 			);
 		}
 
-		const { signupDependencies } = this.props;
+		const { signupDependencies } = props;
 		const { siteUrl, domainItem, siteTitle, username, coupon, segmentationSurveyAnswers } =
 			signupDependencies;
 
@@ -154,11 +218,6 @@ export class PlansStep extends Component {
 
 		let paidDomainName = domainItem?.meta;
 
-		/**
-		 * Domain-only sites have a paid domain but are on the free plan.
-		 * For flows showing plans for domain-only sites, we need to set
-		 * `paidDomainName` to the value of the domain-only site's domain.
-		 */
 		if ( ! paidDomainName && isDomainOnlySite && selectedSite.URL ) {
 			paidDomainName = getDomainFromUrl( selectedSite.URL );
 		}
@@ -168,8 +227,6 @@ export class PlansStep extends Component {
 			freeWPComSubdomain = siteUrl;
 		}
 
-		// De-emphasize the Free plan as a CTA link on the main onboarding flow, and the guided flow, when a paid domain is picked.
-		// More context can be found in p2-p5uIfZ-f5p
 		const deemphasizeFreePlan =
 			( [ 'onboarding', ONBOARDING_GUIDED_FLOW ].includes( flowName ) && paidDomainName != null ) ||
 			deemphasizeFreePlanFromProps;
@@ -183,59 +240,59 @@ export class PlansStep extends Component {
 					siteTitle={ siteTitle }
 					signupFlowUserName={ username }
 					siteId={ selectedSite?.ID }
-					isCustomDomainAllowedOnFreePlan={ this.props.isCustomDomainAllowedOnFreePlan }
+					isCustomDomainAllowedOnFreePlan={ props.isCustomDomainAllowedOnFreePlan }
 					isInSignup
 					isLaunchPage={ isLaunchPage }
 					intervalType={ intervalTypeValue }
-					displayedIntervals={ this.props.displayedIntervals }
-					onUpgradeClick={ ( cartItems ) => this.onSelectPlan( cartItems ) }
-					customerType={ this.getCustomerType() }
-					disableBloggerPlanWithNonBlogDomain={ disableBloggerPlanWithNonBlogDomain } // TODO clk investigate
+					displayedIntervals={ props.displayedIntervals }
+					onUpgradeClick={ ( cartItems ) => onSelectPlan( cartItems ) }
+					customerType={ getCustomerType() }
+					disableBloggerPlanWithNonBlogDomain={ disableBloggerPlanWithNonBlogDomain }
 					deemphasizeFreePlan={ deemphasizeFreePlan }
-					plansWithScroll={ this.state.isDesktop }
+					plansWithScroll={ isDesktop }
 					intent={ intent || surveyedIntent }
 					flowName={ flowName }
 					hideFreePlan={ hideFreePlan }
-					hidePersonalPlan={ this.props.hidePersonalPlan }
-					hidePremiumPlan={ this.props.hidePremiumPlan }
-					hideEcommercePlan={ this.shouldHideEcommercePlan() }
-					hideEnterprisePlan={ this.props.hideEnterprisePlan }
-					removePaidDomain={ this.removePaidDomain }
-					setSiteUrlAsFreeDomainSuggestion={ this.setSiteUrlAsFreeDomainSuggestion }
+					hidePersonalPlan={ props.hidePersonalPlan }
+					hidePremiumPlan={ props.hidePremiumPlan }
+					hideEcommercePlan={ shouldHideEcommercePlan() }
+					hideEnterprisePlan={ props.hideEnterprisePlan }
+					removePaidDomain={ removePaidDomain }
+					setSiteUrlAsFreeDomainSuggestion={ setSiteUrlAsFreeDomainSuggestion }
 					coupon={ coupon }
 					showPlanTypeSelectorDropdown={ config.isEnabled( 'onboarding/interval-dropdown' ) }
-					onPlanIntervalUpdate={ this.props.onPlanIntervalUpdate }
+					onPlanIntervalUpdate={ props.onPlanIntervalUpdate }
 				/>
 			</div>
 		);
-	}
+	};
 
-	renderLoading() {
+	const renderLoading = () => {
 		return (
 			<div className="plans__loading">
 				<LoadingEllipsis active />
 			</div>
 		);
-	}
+	};
 
-	getHeaderText() {
-		const { headerText, translate } = this.props;
+	const getHeaderText = () => {
+		const { headerText, translate } = props;
 
 		if ( headerText ) {
 			return headerText;
 		}
 
 		return translate( 'Choose your flavor of WordPress' );
-	}
+	};
 
-	getSubHeaderText() {
+	const getSubHeaderText = () => {
 		const {
 			translate,
 			useEmailOnboardingSubheader,
 			signupDependencies,
 			flowName,
 			deemphasizeFreePlan,
-		} = this.props;
+		} = props;
 
 		const { segmentationSurveyAnswers } = signupDependencies;
 		const { segmentSlug } = getSegmentedIntent( segmentationSurveyAnswers );
@@ -249,9 +306,7 @@ export class PlansStep extends Component {
 					href={ localizeUrl( 'https://wordpress.com/for-agencies?ref=onboarding' ) }
 					target="_blank"
 					rel="noopener noreferrer"
-					onClick={ () =>
-						this.props.recordTracksEvent( 'calypso_guided_onboarding_agency_link_click' )
-					}
+					onClick={ () => props.recordTracksEvent( 'calypso_guided_onboarding_agency_link_click' ) }
 					borderless
 				/>
 			);
@@ -263,7 +318,7 @@ export class PlansStep extends Component {
 		}
 
 		const freePlanButton = (
-			<Button onClick={ () => buildUpgradeFunction( this.props, null ) } borderless />
+			<Button onClick={ () => buildUpgradeFunction( props, null ) } borderless />
 		);
 
 		if ( useEmailOnboardingSubheader ) {
@@ -273,22 +328,16 @@ export class PlansStep extends Component {
 			);
 		}
 
-		/**
-		 * If deemphasizeFreePlan is shown, we already show a subheader.
-		 * Returning null here hides the default subheader and prevents two subheaders from being shown.
-		 */
 		if ( deemphasizeFreePlan ) {
 			return null;
 		}
-	}
+	};
 
-	shouldHideEcommercePlan() {
-		// The flow with the Site Assembler step doesn't support atomic site, so we have to hide the plan.
-		// We also hide the plan if the flag is set - currently it's only set for the `site-selected` flow.
-		return isSiteAssemblerFlow( this.props.flowName ) || this.props.hideEcommercePlan;
-	}
+	const shouldHideEcommercePlan = () => {
+		return isSiteAssemblerFlow( props.flowName ) || props.hideEcommercePlan;
+	};
 
-	plansFeaturesSelection() {
+	const plansFeaturesSelection = () => {
 		const {
 			flowName,
 			stepName,
@@ -298,12 +347,12 @@ export class PlansStep extends Component {
 			steps,
 			wrapperProps,
 			useStepperWrapper,
-		} = this.props;
+		} = props;
 
-		const headerText = this.getHeaderText();
-		const fallbackHeaderText = this.props.fallbackHeaderText || headerText;
-		const subHeaderText = this.getSubHeaderText();
-		const fallbackSubHeaderText = this.props.fallbackSubHeaderText || subHeaderText;
+		const headerText = getHeaderText();
+		const fallbackHeaderText = props.fallbackHeaderText || headerText;
+		const subHeaderText = getSubHeaderText();
+		const fallbackSubHeaderText = props.fallbackSubHeaderText || subHeaderText;
 
 		let backUrl;
 		let backLabelText;
@@ -315,22 +364,18 @@ export class PlansStep extends Component {
 
 		let queryParams;
 		if ( ! isNaN( Number( positionInFlow ) ) && 0 !== positionInFlow ) {
-			const previousStepName = steps[ this.props.positionInFlow - 1 ];
-			const previousStep = this.props.progress?.[ previousStepName ];
+			const previousStepName = steps[ props.positionInFlow - 1 ];
+			const previousStep = props.progress?.[ previousStepName ];
 
 			const isComingFromUseYourDomainStep = 'use-your-domain' === previousStep?.stepSectionName;
 
 			if ( isComingFromUseYourDomainStep ) {
 				queryParams = {
-					...this.props.queryParams,
+					...props.queryParams,
 					step: 'transfer-or-connect',
 					initialQuery: previousStep?.siteUrl,
 				};
 
-				// During onboarding, if the user chooses to use their own domain, but that domain needs to have
-				// its ownership verified, they can skip the domain selection step and the `domainItem` dependency
-				// is not provided. In that case, the "Back" button in the plan selection step needs to go back to
-				// the initial domain selection step and not to the "transfer or connect" step.
 				if (
 					( 'onboarding' === flowName || 'onboarding-pm' === flowName ) &&
 					undefined === previousStep?.providedDependencies?.domainItem
@@ -357,7 +402,7 @@ export class PlansStep extends Component {
 					}
 					isWideLayout={ false }
 					isExtraWideLayout
-					stepContent={ this.plansFeaturesList() }
+					stepContent={ plansFeaturesList() }
 					backLabelText={ backLabelText }
 					{ ...wrapperProps }
 				/>
@@ -371,35 +416,33 @@ export class PlansStep extends Component {
 				stepName={ stepName }
 				positionInFlow={ positionInFlow }
 				headerText={ headerText }
-				shouldHideNavButtons={ this.props.shouldHideNavButtons }
+				shouldHideNavButtons={ props.shouldHideNavButtons }
 				fallbackHeaderText={ fallbackHeaderText }
 				subHeaderText={ subHeaderText }
 				fallbackSubHeaderText={ fallbackSubHeaderText }
 				isWideLayout={ false }
 				isExtraWideLayout
-				stepContent={ this.plansFeaturesList() }
+				stepContent={ plansFeaturesList() }
 				allowBackFirstStep={ !! hasInitializedSitesBackUrl }
 				backUrl={ backUrl }
 				backLabelText={ backLabelText }
 				queryParams={ queryParams }
 			/>
 		);
-	}
+	};
 
-	render() {
-		const classes = clsx( 'plans plans-step', {
-			'has-no-sidebar': true,
-			'is-wide-layout': false,
-			'is-extra-wide-layout': true,
-		} );
+	const classes = clsx( 'plans plans-step', {
+		'has-no-sidebar': true,
+		'is-wide-layout': false,
+		'is-extra-wide-layout': true,
+	} );
 
-		return (
-			<>
-				<MarketingMessage path="signup/plans" />
-				<div className={ classes }>{ this.plansFeaturesSelection() }</div>
-			</>
-		);
-	}
+	return (
+		<>
+			<MarketingMessage path="signup/plans" />
+			<div className={ classes }>{ plansFeaturesSelection() }</div>
+		</>
+	);
 }
 
 PlansStep.propTypes = {
