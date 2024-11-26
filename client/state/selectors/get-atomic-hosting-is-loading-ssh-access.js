a8a15118d28b3f788eb822d5c8ac6432703b74c6
Candy Tsai <candy02058912@gmail.com>
2024-11-26 20:47:46 +0800

    Separate logic for ssh and sftp loading check

--- client/state/selectors/get-atomic-hosting-is-loading-ssh-access.js
+++ client/state/selectors/get-atomic-hosting-is-loading-ssh-access.js
@@ -3,7 +3,7 @@ import 'calypso/state/hosting/init';
 /**
  * Returns if the SSH access data loaded for given site.
  * @param  {Object}  state   Global state tree
- * @param  {number}  siteId The ID of the site we're querying
+ * @param  {number|null}  siteId The ID of the site we're querying
  * @returns {boolean} If the SSH access data has finished the first request
  */
 export function getAtomicHostingIsLoadingSshAccess( state, siteId ) {
