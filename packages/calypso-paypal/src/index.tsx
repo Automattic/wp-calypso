c78b2fd269f8682e43a32b6db61739cb6c84e73a
Payton Swick <payton@foolord.com>
2024-11-27 13:23:19 -0500

    Add calypso-paypal package

---
+++ packages/calypso-paypal/src/index.tsx
@@ -0,0 +1,101 @@
+import { PayPalScriptProvider, ReactPayPalScriptOptions } from '@paypal/react-paypal-js';
+import { useEffect, useState, createContext, PropsWithChildren, useContext } from 'react';
+import wpcomRequest from 'wpcom-proxy-request';
+
+interface PayPalConfigurationApiResponse {
+	client_id: string | undefined;
+}
+
+export interface PayPalConfiguration {
+	clientId: string | undefined;
+}
+
+export interface UsePayPalConfiguration {
+	payPalConfiguration: PayPalConfiguration | undefined;
+}
+
+async function fetchPayPalConfiguration(): Promise< PayPalConfigurationApiResponse > {
+	return await wpcomRequest( {
+		path: `/me/paypal-configuration`,
+		method: 'GET',
+	} );
+}
+
+const PayPalContext = createContext< PayPalConfiguration | undefined >( undefined );
+
+const defaultConfiguration: PayPalConfiguration = {
+	clientId: undefined,
+};
+
+function usePayPalConfigurationInternalOnly(): {
+	payPalConfiguration: PayPalConfiguration | undefined;
+	error: undefined | Error;
+} {
+	const [ configurationError, setConfigurationError ] = useState< undefined | Error >();
+	const [ payPalConfiguration, setConfiguration ] = useState< undefined | PayPalConfiguration >(
+		defaultConfiguration
+	);
+
+	useEffect( () => {
+		let isSubscribed = true;
+		fetchPayPalConfiguration()
+			.then( ( configuration ) => {
+				if ( ! isSubscribed ) {
+					return;
+				}
+				if ( ! configuration.client_id ) {
+					throw new Error(
+						'Error loading PayPal configuration. Received invalid data from the server.'
+					);
+				}
+				setConfiguration( { clientId: configuration.client_id } );
+			} )
+			.catch( ( error ) => {
+				setConfigurationError( error );
+			} );
+		return () => {
+			isSubscribed = false;
+		};
+	}, [] );
+
+	return { payPalConfiguration, error: configurationError };
+}
+
+export function usePayPalConfiguration(): UsePayPalConfiguration {
+	const payPalConfiguration = useContext( PayPalContext );
+	if ( ! payPalConfiguration ) {
+		throw new Error( 'usePayPalConfiguration can only be used inside a PayPalProvider' );
+	}
+	return { payPalConfiguration };
+}
+
+export function PayPalProvider( {
+	children,
+	currency,
+}: PropsWithChildren< { currency: string } > ) {
+	const { payPalConfiguration, error } = usePayPalConfigurationInternalOnly();
+
+	if ( error ) {
+		throw error;
+	}
+
+	const payPalScriptOptions: ReactPayPalScriptOptions = {
+		clientId: payPalConfiguration?.clientId ?? 'loading-client-id',
+		components: 'buttons',
+		currency,
+		commit: true,
+		intent: 'capture',
+		vault: true,
+	};
+
+	const isConfigurationLoaded = payPalConfiguration?.clientId ? true : false;
+
+	return (
+		<PayPalScriptProvider
+			options={ isConfigurationLoaded ? payPalScriptOptions : undefined }
+			deferLoading={ isConfigurationLoaded ? false : true }
+		>
+			<PayPalContext.Provider value={ payPalConfiguration }>{ children }</PayPalContext.Provider>
+		</PayPalScriptProvider>
+	);
+}
