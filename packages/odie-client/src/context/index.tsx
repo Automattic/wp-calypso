112aeab7ae4406aa194072193cd308d666e1ab7e
escapemanuele <escapemanuele@gmail.com>
2024-11-27 10:33:36 +0100

    Missing check on zendesk

--- packages/odie-client/src/context/index.tsx
+++ packages/odie-client/src/context/index.tsx
@@ -89,7 +89,10 @@ export const OdieAssistantProvider: React.FC< OdieAssistantProviderProps > = ( {
 	 * The main chat thread.
 	 * This is where we manage the state of the chat.
 	 */
-	const { mainChatState, setMainChatState } = useGetCombinedChat( shouldUseHelpCenterExperience );
+	const { mainChatState, setMainChatState } = useGetCombinedChat(
+		canConnectToZendesk,
+		shouldUseHelpCenterExperience
+	);
 
 	/**
 	 * Tracking event.
