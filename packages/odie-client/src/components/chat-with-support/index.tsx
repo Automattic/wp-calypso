f5eafb4bc85db9a0ffbc63c60b008528bfcf22d2
Anthony Grullon <agrullon95@gmail.com>
2024-11-27 12:15:53 -0500

    Localize support chat message using i18n

--- packages/odie-client/src/components/chat-with-support/index.tsx
+++ packages/odie-client/src/components/chat-with-support/index.tsx
@@ -6,7 +6,9 @@ const ChatWithSupportLabel = () => {
 	return (
 		<div className="chat-with-support-wrapper">
 			<div className="chat-with-support__line"></div>
-			<div className="chat-with-support__message">{ __( 'Chatting with support now' ) }</div>
+			<div className="chat-with-support__message">
+				{ __( 'Chatting with support now', __i18n_text_domain__ ) }
+			</div>
 			<div className="chat-with-support__line"></div>
 		</div>
 	);
