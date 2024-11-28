c97dbe122a7d4bdfcba9f88b1bbf13908c69db3e
vykes-mac <knrymcleish@gmail.com>
2024-11-27 21:08:34 -0500

    ensure the css is applied to the correct body element

--- client/my-sites/domains/domain-management/list/bulk-all-domains.tsx
+++ client/my-sites/domains/domain-management/list/bulk-all-domains.tsx
@@ -37,7 +37,7 @@ export default function BulkAllDomains( props: BulkAllDomainsProps ) {
 		html {
 			overflow-y: auto;
 		}
-		body.is-section-domains {
+		body.is-bulk-all-domains-page {
 			background: var( --studio-gray-0 );
 
 			&.rtl .layout__content {
