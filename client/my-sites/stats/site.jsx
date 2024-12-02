9bd309fbab9d66f5bb6028c5d6ef071ba94006c9
Dognose <dognose24@gmail.com>
2024-12-02 22:44:52 +0800

    Fix daysInRange of enforced  7 days range by paywall gate

--- client/my-sites/stats/site.jsx
+++ client/my-sites/stats/site.jsx
@@ -393,7 +393,7 @@ class StatsSite extends Component {
 			customChartRange.chartEnd = momentSiteZone.format( 'YYYY-MM-DD' );
 			customChartRange.chartStart = momentSiteZone
 				.clone()
-				.subtract( 7, 'days' )
+				.subtract( customChartRange.daysInRange - 1, 'days' )
 				.format( 'YYYY-MM-DD' );
 		}
 
