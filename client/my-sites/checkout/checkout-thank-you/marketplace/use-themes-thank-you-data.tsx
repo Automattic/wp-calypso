5f0a7c992b9d229792e5193c6bd0885f26ff7967
bogdanungureanu <bogdanungureanu21@gmail.com>
2024-12-02 15:50:17 +0200

    Revert "Marketplace Theme Install: Fix stuck loading screen (#96889)"
    This reverts commit 292a2078d55a115895d10125b91c3e4031ad6018.

--- client/my-sites/checkout/checkout-thank-you/marketplace/use-themes-thank-you-data.tsx
+++ client/my-sites/checkout/checkout-thank-you/marketplace/use-themes-thank-you-data.tsx
@@ -2,16 +2,11 @@ import page from '@automattic/calypso-router';
 import { addQueryArgs } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect, useMemo } from 'react';
-import { useQuerySitePurchases } from 'calypso/components/data/query-site-purchases';
 import { useQueryThemes } from 'calypso/components/data/query-theme';
 import { useDispatch, useSelector } from 'calypso/state';
-import { isFetchingSitePurchases } from 'calypso/state/purchases/selectors';
 import { isJetpackSite, getSiteAdminUrl, getSiteOption } from 'calypso/state/sites/selectors';
 import { clearActivated } from 'calypso/state/themes/actions';
-import {
-	getThemes,
-	isMarketplaceThemeSubscribed as getIsMarketplaceThemeSubscribed,
-} from 'calypso/state/themes/selectors';
+import { getThemes } from 'calypso/state/themes/selectors';
 import { hasExternallyManagedThemes as getHasExternallyManagedThemes } from 'calypso/state/themes/selectors/is-externally-managed-theme';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import { Theme } from 'calypso/types';
@@ -49,9 +44,6 @@ export function useThemesThankYouData(
 	const subtitle = translate(
 		"Your new theme is a reflection of your unique style and personality, and we're thrilled to see it come to life."
 	);
-
-	useQuerySitePurchases( siteId );
-	const isRequestingSitePurchases = useSelector( isFetchingSitePurchases );
 
 	const dotComThemes = useSelector( ( state ) => getThemes( state, 'wpcom', themeSlugs ) );
 	const dotOrgThemes = useSelector( ( state ) => getThemes( state, 'wporg', themeSlugs ) );
@@ -125,27 +117,10 @@ export function useThemesThankYouData(
 				( dotComTheme: { id: string } | undefined ) => dotComTheme?.id === theme.id
 			)
 	);
-
-	const themesSubscribed = useSelector( ( state ) =>
-		themeSlugs.filter( ( themeId ) => getIsMarketplaceThemeSubscribed( state, themeId, siteId ) )
-	);
-
-	const hasExternallyManagedThemesSubscribed = themesSubscribed.length > 0;
-
 	const hasExternallyManagedThemes = useSelector( ( state ) =>
 		getHasExternallyManagedThemes( state, themeSlugs )
 	);
-
-	useEffect( () => {
-		if ( ! hasExternallyManagedThemesSubscribed && hasExternallyManagedThemes ) {
-			page( `/home/${ siteSlug }` );
-		}
-	}, [ hasExternallyManagedThemes, hasExternallyManagedThemesSubscribed, siteSlug ] );
-
-	const isAtomicNeeded =
-		hasDotOrgThemes ||
-		( hasExternallyManagedThemes &&
-			( isRequestingSitePurchases || hasExternallyManagedThemesSubscribed ) );
+	const isAtomicNeeded = hasDotOrgThemes || hasExternallyManagedThemes;
 
 	// Clear completed activated theme request state to avoid displaying the Thanks modal
 	useEffect( () => {
@@ -188,7 +163,6 @@ export function useThemesThankYouData(
 		// Always display the loading screen for the following situations:
 		// - Redirect to the plugin-bundle flow after the theme is activated for Woo themes.
 		// - Redirect to the Theme Details page after the atomic transfer if it's required.
-		// - Redirect to the /home page if the user removed the externally managed theme from checkout.
-		! ( continueWithPluginBundle || isAtomicNeeded || ( ! isAtomicNeeded && allThemesFetched ) ),
+		! ( continueWithPluginBundle || isAtomicNeeded ),
 	];
 }
