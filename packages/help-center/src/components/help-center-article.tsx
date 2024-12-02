95916162ad7377b7a0013d6e3d45248f6cba9bfe
Anthony Grullon <agrullon95@gmail.com>
2024-12-02 13:43:32 -0500

    Show WP Support 3 tab component and enhance tab functionality

--- packages/help-center/src/components/help-center-article.tsx
+++ packages/help-center/src/components/help-center-article.tsx
@@ -1,7 +1,7 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { useEffect, createInterpolateElement } from '@wordpress/element';
+import { useEffect, createInterpolateElement, useCallback, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { useSearchParams } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { useHelpCenterContext } from '../contexts/HelpCenterContext';
 import { usePostByUrl } from '../hooks';
 import { BackToTopButton } from './back-to-top-button';
@@ -12,7 +12,9 @@ import './help-center-article.scss';
 export const HelpCenterArticle = () => {
 	const [ searchParams ] = useSearchParams();
 	const { sectionName } = useHelpCenterContext();
-
+	const location = useLocation();
+	const navigate = useNavigate();
+	const [ tabHash, setTabHash ] = useState( '' );
 	const postUrl = searchParams.get( 'link' ) || '';
 	const query = searchParams.get( 'query' );
 
@@ -52,6 +54,66 @@ export const HelpCenterArticle = () => {
 				: recordTracksEvent( 'calypso_helpcenter_article_viewed', tracksData );
 		}
 	}, [ post, query, sectionName ] );
+
+	const toggleTab = ( element: Element, show: boolean ) => {
+		( element as HTMLElement ).style.display = show ? 'block' : 'none';
+		element.setAttribute( 'aria-hidden', show ? 'false' : 'true' );
+	};
+
+	const toggleTabTitle = ( element: Element, show: boolean ) => {
+		element.setAttribute( 'aria-selected', show ? 'true' : 'false' );
+	};
+
+	const activateTab = useCallback( () => {
+		const hash = tabHash;
+
+		const tabs = Array.from( document.querySelectorAll( '.wp-block-wpsupport3-tabs' ) );
+
+		tabs.forEach( ( tab ) => {
+			const titles = Array.from( tab.querySelectorAll( '.wpsupport3-tab__title' ) );
+			const bodies = Array.from(
+				tab.querySelectorAll( '.wp-block-wpsupport3-tab:not(.invisible_tabpanel)' )
+			);
+
+			const match = titles.findIndex( ( titles ) => titles.id === hash?.substring( 1 ) );
+
+			// Reset selection
+			titles.forEach( ( title ) => toggleTabTitle( title, false ) );
+			bodies.forEach( ( body ) => toggleTab( body, false ) );
+
+			if ( hash && match !== -1 ) {
+				toggleTabTitle( titles[ match ], true );
+				toggleTab( bodies[ match ], true );
+			} else {
+				// If the first tab is invisible from the editor, we set the first tab as active.
+				toggleTabTitle( titles[ 0 ], true );
+				toggleTab( bodies[ 0 ], true );
+			}
+		} );
+	}, [ tabHash ] );
+
+	useEffect( () => {
+		if ( tabHash || post ) {
+			activateTab();
+		}
+	}, [ activateTab, tabHash, post ] );
+
+	useEffect( () => {
+		if ( post ) {
+			const titles = Array.from(
+				document.querySelectorAll( '.wp-block-wpsupport3-tabs .wpsupport3-tab__title' )
+			);
+			titles.forEach( ( title ) => {
+				title.addEventListener( 'click', ( e ) => {
+					e.preventDefault();
+					setTabHash( `#${ title?.id }` );
+					setTimeout( () => {
+						window.scroll( 0, document.documentElement.scrollTop );
+					} );
+				} );
+			} );
+		}
+	}, [ location.pathname, location.search, navigate, post ] );
 
 	return (
 		<div className="help-center-article">
