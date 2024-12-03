e083727faf31f47142cccb6fc611219db4809443
Caroline Moore <calobee@gmail.com>
2024-12-03 10:32:57 -0500

    Update icon type

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-identify/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-identify/index.tsx
@@ -18,7 +18,7 @@ import type { UrlData } from 'calypso/blocks/import/types';
 
 import './style.scss';
 interface HostingDetailsProps {
-	items: { title: string; description: string; icon: typeof Icon }[];
+	items: { title: string; description: string; icon: JSX.Element }[];
 }
 
 const HostingDetails: FC< HostingDetailsProps > = ( { items } ) => {
