dcad6799d1403daaa4621df6f1f6cf0af6fcddcd
escapemanuele <escapemanuele@gmail.com>
2024-12-03 14:04:26 +0100

    Add editor type

--- apps/help-center/utils.js
+++ apps/help-center/utils.js
@@ -21,3 +21,23 @@ export const getUnlock = () => {
 		return undefined;
 	}
 };
+
+export const getEditorType = () => {
+	if ( document.querySelector( '#editor .edit-post-layout' ) ) {
+		return 'post';
+	}
+
+	if ( document.querySelector( '#site-editor' ) ) {
+		return 'site';
+	}
+
+	if ( document.querySelector( '#widgets-editor' ) ) {
+		return 'widgets';
+	}
+
+	if ( document.querySelector( '#customize-controls .customize-widgets__sidebar-section.open' ) ) {
+		return 'customize-widgets';
+	}
+
+	return undefined;
+};
