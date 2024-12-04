862cdd8a406a93ff7759f28822903a74b39c3ebb
Caroline Moore <calobee@gmail.com>
2024-12-04 11:23:46 -0500

    Simplify loader

--- client/blocks/importer/wordpress/upgrade-plan/skeleton/index.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/skeleton/index.tsx
@@ -2,39 +2,40 @@ import type { FC } from 'react';
 import './style.scss';
 
 export const Skeleton: FC< { showVariants?: boolean } > = ( { showVariants = false } ) => {
+	const variantSkeleton = (
+		<div className="import__upgrade-plan-features-container">
+			<div
+				className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-gray"
+				style={ { width: '173px', height: '32px', marginBottom: '20px' } }
+			/>
+			<div className="import-upgrade-plan-skeleton" style={ { width: '277px' } } />
+			<div
+				className="import-upgrade-plan-skeleton"
+				style={ { width: '195px', marginBottom: '40px' } }
+			/>
+			<div
+				className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-gray"
+				style={ { width: '173px', height: '32px' } }
+			/>
+			<div
+				className="import-upgrade-plan-skeleton"
+				style={ { width: '103px', marginBottom: '40px' } }
+			/>
+			<div
+				className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-highlight"
+				style={ { width: '305px', height: '32px', borderRadius: '4px' } }
+			/>
+			<div
+				className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-highlight"
+				style={ { width: '103px', margin: '0 auto' } }
+			/>
+		</div>
+	);
 	return (
 		<div className="import__upgrade-plan">
 			<div className="import__upgrade-plan-details import__upgrade-plan-details--loading">
 				<div className="import__upgrade-plan-container">
-					{ showVariants && (
-						<div className="import__upgrade-plan-features-container">
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-gray"
-								style={ { width: '173px', height: '32px', marginBottom: '20px' } }
-							/>
-							<div className="import-upgrade-plan-skeleton" style={ { width: '277px' } } />
-							<div
-								className="import-upgrade-plan-skeleton"
-								style={ { width: '195px', marginBottom: '40px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-gray"
-								style={ { width: '173px', height: '32px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton"
-								style={ { width: '103px', marginBottom: '40px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-highlight"
-								style={ { width: '305px', height: '32px', borderRadius: '4px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-highlight"
-								style={ { width: '103px', margin: '0 auto' } }
-							/>
-						</div>
-					) }
+					{ showVariants && variantSkeleton }
 					<div className="import__upgrade-plan-features-container">
 						<div
 							className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-gray"
@@ -116,35 +117,7 @@ export const Skeleton: FC< { showVariants?: boolean } > = ( { showVariants = fal
 							</div>
 						</div>
 					) }
-					{ showVariants && (
-						<div className="import__upgrade-plan-features-container">
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-gray"
-								style={ { width: '173px', height: '32px', marginBottom: '20px' } }
-							/>
-							<div className="import-upgrade-plan-skeleton" style={ { width: '277px' } } />
-							<div
-								className="import-upgrade-plan-skeleton"
-								style={ { width: '195px', marginBottom: '40px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-gray"
-								style={ { width: '173px', height: '32px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton"
-								style={ { width: '103px', marginBottom: '40px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-highlight"
-								style={ { width: '305px', height: '32px', borderRadius: '4px' } }
-							/>
-							<div
-								className="import-upgrade-plan-skeleton import-upgrade-plan-skeleton--dark-highlight"
-								style={ { width: '103px', margin: '0 auto' } }
-							/>
-						</div>
-					) }
+					{ showVariants && variantSkeleton }
 				</div>
 			</div>
 		</div>
