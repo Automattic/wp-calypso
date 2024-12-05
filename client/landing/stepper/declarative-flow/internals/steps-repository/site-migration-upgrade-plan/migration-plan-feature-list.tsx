1f1bd56c1c054498a3b9b47fab6b5d84d564b6b7
Caroline Moore <calobee@gmail.com>
2024-12-05 15:50:25 -0500

    Move so we can take full advantage of hooks in translate, add bold text

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-upgrade-plan/migration-plan-feature-list.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-upgrade-plan/migration-plan-feature-list.tsx
@@ -1,19 +1,98 @@
+import {
+	PLAN_BUSINESS,
+	PLAN_BUSINESS_MONTHLY,
+	PLAN_BUSINESS_2_YEARS,
+} from '@automattic/calypso-products';
 import { JetpackLogo } from '@automattic/components';
-import { migrationPlanFeatures } from './migration-plan-features';
+import { useTranslate } from 'i18n-calypso';
 import type { PlanSlug } from '@automattic/calypso-products';
+import type { ReactNode } from 'react';
+
 export const MigrationPlanFeatureList = ( { planSlug }: { planSlug: PlanSlug } ) => {
+	const translate = useTranslate();
+	const jetpackFeatures = [
+		translate( 'In-depth site analytics dashboard' ),
+		translate( 'SEO, marketing, and analytics tools' ),
+		translate( 'Plugin auto-updates' ),
+		translate( 'Real-time backups and one-click restores' ),
+	];
+
+	const businessFeatures = [
+		translate( 'Unrestricted bandwidth and visitors' ),
+		translate( 'Install plugins and themes' ),
+		translate( 'SFTP/SSH, WP-CLI, Git tools' ),
+	];
+
+	const migrationPlanFeatures: {
+		wpcomFeatures: Partial< Record< PlanSlug, ReactNode[] > >;
+		jetpackFeatures: ReactNode[];
+	} = {
+		wpcomFeatures: {
+			[ PLAN_BUSINESS ]: [
+				translate( '{{strong}}50% off{{/strong}} your first year', {
+					components: { strong: <strong /> },
+				} ),
+				translate( '{{strong}}39%{{/strong}} annual savings', {
+					components: { strong: <strong /> },
+				} ),
+				translate( '{{strong}}Free{{/strong}} domain for a year', {
+					components: { strong: <strong /> },
+				} ),
+				translate( '{{strong}}Free{{/strong}} migration service', {
+					components: { strong: <strong /> },
+				} ),
+				translate( 'Refundable within {{strong}}14 days{{/strong}}', {
+					components: { strong: <strong /> },
+				} ),
+				...businessFeatures,
+			],
+			[ PLAN_BUSINESS_MONTHLY ]: [
+				translate( 'No first year discount' ),
+				translate( 'No annual savings' ),
+				translate( 'No free domain' ),
+				translate( '{{strong}}Free{{/strong}} migration service', {
+					components: { strong: <strong /> },
+				} ),
+				translate( 'Refundable within {{strong}}7 days{{/strong}}', {
+					components: { strong: <strong /> },
+				} ),
+				...businessFeatures,
+			],
+			[ PLAN_BUSINESS_2_YEARS ]: [
+				translate( '{{strong}}50% off{{/strong}} your first two years', {
+					components: { strong: <strong /> },
+				} ),
+				translate( '{{strong}}52%{{/strong}} annual savings', {
+					components: { strong: <strong /> },
+				} ),
+				translate( '{{strong}}Free{{/strong}} domain for a year', {
+					components: { strong: <strong /> },
+				} ),
+				translate( '{{strong}}Free{{/strong}} migration service', {
+					components: { strong: <strong /> },
+				} ),
+				translate( 'Refundable within {{strong}}14 days{{/strong}}', {
+					components: { strong: <strong /> },
+				} ),
+				...businessFeatures,
+			],
+		},
+		jetpackFeatures: [ ...jetpackFeatures ],
+	};
 	return (
 		<ul className="import__details-list">
-			{ migrationPlanFeatures[ 'wpcomFeatures' ][ planSlug ]?.map( ( feature: string ) => (
-				<li className="import__upgrade-plan-feature" key={ feature }>
-					{ feature }
-				</li>
-			) ) }
+			{ migrationPlanFeatures[ 'wpcomFeatures' ][ planSlug ]?.map(
+				( feature: ReactNode, index: number ) => (
+					<li key={ index } className="import__upgrade-plan-feature">
+						{ feature }
+					</li>
+				)
+			) }
 			<li className="import__upgrade-plan-feature logo">
 				<JetpackLogo size={ 24 } />
 			</li>
-			{ migrationPlanFeatures[ 'jetpackFeatures' ].map( ( feature: string ) => (
-				<li className="import__upgrade-plan-feature" key={ feature }>
+			{ migrationPlanFeatures[ 'jetpackFeatures' ].map( ( feature: ReactNode, index: number ) => (
+				<li key={ index } className="import__upgrade-plan-feature">
 					{ feature }
 				</li>
 			) ) }
