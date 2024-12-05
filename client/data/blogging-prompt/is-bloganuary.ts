b632c5be1bc88f17fa1d39c4ce0b6d7445b0ca73
Allison Levine <allison.levine@automattic.com>
2024-12-05 14:39:53 -0500

    Disable bloganuary.

--- client/data/blogging-prompt/is-bloganuary.ts
+++ client/data/blogging-prompt/is-bloganuary.ts
@@ -4,5 +4,10 @@ import moment from 'moment';
  * @returns true if bloganuary mode is active
  */
 export default function isBloganuary() {
+	// Disable for January 2025 (see https://wp.me/p5uIfZ-gxX).
+	const BLOGANUARY_ENABLED = false;
+	if ( ! BLOGANUARY_ENABLED ) {
+		return false;
+	}
 	return moment().format( 'MM' ) === '01' || isEnabled( 'bloganuary' );
 }
