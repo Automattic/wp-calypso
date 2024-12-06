b12bebf33841b286356159e91f40b07fcaf072aa
Allison Levine <allison.levine@automattic.com>
2024-12-06 14:20:28 -0500

    Add comment clarifying the redundant logic.

--- client/data/blogging-prompt/is-bloganuary.ts
+++ client/data/blogging-prompt/is-bloganuary.ts
@@ -4,7 +4,9 @@ import moment from 'moment';
  * @returns true if bloganuary mode is active
  */
 export default function isBloganuary() {
-	// Disable for January 2025 (see https://wp.me/p5uIfZ-gxX).
+	// Disable for January 2025 and beyond (see https://wp.me/p5uIfZ-gxX).
+	// Intentionally redundant to make it easier to spot the disable condition,
+	// and avoid removing code we may re-enable in the future.
 	const BLOGANUARY_ENABLED = false;
 	if ( ! BLOGANUARY_ENABLED ) {
 		return false;
