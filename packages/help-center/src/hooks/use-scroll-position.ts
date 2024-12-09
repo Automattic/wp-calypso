b4dbff2264108e4da90ebf2ff6849476c90d5d4c
Omar Alshaker <omar@omaralshaker.com>
2024-12-10 00:56:47 +0100

    Code comments

--- packages/help-center/src/hooks/use-scroll-position.ts
+++ packages/help-center/src/hooks/use-scroll-position.ts
@@ -6,9 +6,9 @@ import { useEffect, useState } from '@wordpress/element';
 let cachedScrollPositions: Record< string, number > = {};
 
 /**
- * Persists the scroll position of an element in memory and returns it.
+ * Persists the scroll position the scrollable container in memory and returns it. Keyed by article URL to persist multiple scroll positions for every open article.
  * @param ref the HTML element to track the scroll position of.
- * @param url the pathname of the current route.
+ * @param url the pathname+search of the current route.
  * @returns the current or last recorded scroll position.
  */
 export function useArticleScrollPosition( ref: React.RefObject< HTMLElement >, url: string ) {
@@ -26,6 +26,7 @@ export function useArticleScrollPosition( ref: React.RefObject< HTMLElement >, u
 		};
 		window.addEventListener( 'scroll', handleScroll, true );
 
+		// When the Help Center is closed and reopened, the scroll position should be reset.
 		if ( ! url.startsWith( '/post' ) ) {
 			setScrollPositions( ( cachedScrollPositions = {} ) );
 		}
