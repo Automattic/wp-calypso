ff78d9fc76ff9eb5126435c9f6f094368256b466
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-12-11 16:11:24 +0100

    Refactor MailboxesList component and load standalone components

--- client/my-sites/email/email-management/home/email-plan-mailboxes-list.jsx
+++ client/my-sites/email/email-management/home/email-plan-mailboxes-list.jsx
@@ -1,88 +1,14 @@
-import { Badge, CompactCard, Gridicon, MaterialIcon } from '@automattic/components';
-import { Button } from '@wordpress/components';
-import clsx from 'clsx';
+import { Badge, Gridicon, MaterialIcon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import PropTypes from 'prop-types';
-import ExternalLink from 'calypso/components/external-link';
-import SectionHeader from 'calypso/components/section-header';
 import { isRecentlyRegistered } from 'calypso/lib/domains/utils';
-import {
-	getEmailAddress,
-	getEmailForwardAddress,
-	isEmailForward,
-	isEmailForwardAccount,
-	isEmailUserAdmin,
-	isTitanMailAccount,
-} from 'calypso/lib/emails';
-import { EMAIL_ACCOUNT_TYPE_FORWARD } from 'calypso/lib/emails/email-provider-constants';
-import { getGSuiteSubscriptionStatus, getGmailUrl } from 'calypso/lib/gsuite';
-import { getTitanEmailUrl, useTitanAppsUrlPrefix } from 'calypso/lib/titan';
+import { getEmailForwardAddress, isEmailForward, isEmailUserAdmin } from 'calypso/lib/emails';
+import { getGSuiteSubscriptionStatus } from 'calypso/lib/gsuite';
 import EmailMailboxActionMenu from 'calypso/my-sites/email/email-management/home/email-mailbox-action-menu';
 import EmailMailboxWarnings from 'calypso/my-sites/email/email-management/home/email-mailbox-warnings';
-import EmailTypeIcon from 'calypso/my-sites/email/email-management/home/email-type-icon';
-import { recordEmailAppLaunchEvent } from './utils';
-
-const getListHeaderTextForAccountType = ( accountType, translate ) => {
-	if ( accountType === EMAIL_ACCOUNT_TYPE_FORWARD ) {
-		return translate( 'Email forwards', {
-			comment:
-				'This is a header for a list of email addresses that forward all emails to another email account',
-		} );
-	}
-	return translate( 'Mailboxes', {
-		comment: 'This is a header for a list of email addresses the user owns',
-	} );
-};
-
-const MailboxListHeader = ( {
-	accountType = null,
-	children,
-	isPlaceholder = false,
-	addMailboxPath,
-	domain,
-	showIcon,
-} ) => {
-	const translate = useTranslate();
-
-	const HeaderIcon = () => (
-		<div className="email-plan-mailboxes-list__mailbox-header-icon">
-			<EmailTypeIcon domain={ domain } />
-		</div>
-	);
-
-	return (
-		<div className="email-plan-mailboxes-list__mailbox-list">
-			<SectionHeader
-				isPlaceholder={ isPlaceholder }
-				label={
-					<>
-						{ !! showIcon && domain && <HeaderIcon /> }
-						{ getListHeaderTextForAccountType( accountType, translate ) }
-					</>
-				}
-			>
-				{ addMailboxPath && (
-					<Button isLink="link" href={ addMailboxPath }>
-						{ translate( 'Add mailbox' ) }
-					</Button>
-				) }
-			</SectionHeader>
-			{ children }
-		</div>
-	);
-};
-
-const MailboxListItem = ( { children, isError = false, isPlaceholder, hasNoEmails } ) => {
-	const className = clsx( 'email-plan-mailboxes-list__mailbox-list-item', {
-		'is-placeholder': isPlaceholder,
-		'no-emails': hasNoEmails,
-	} );
-	return (
-		<CompactCard className={ className } highlight={ isError ? 'error' : null }>
-			{ children }
-		</CompactCard>
-	);
-};
+import MailboxListHeader from './email-plan-mailboxes/list-header';
+import MailboxListItem from './email-plan-mailboxes/list-item';
+import MailboxLink from './email-plan-mailboxes/list-item-link';
 
 function EmailForwardSecondaryDetails( { mailbox } ) {
 	if ( isEmailForward( mailbox ) ) {
@@ -94,43 +20,6 @@ function EmailForwardSecondaryDetails( { mailbox } ) {
 		);
 	}
 	return null;
-}
-
-function MailboxLink( { account, mailbox } ) {
-	const titanAppsUrlPrefix = useTitanAppsUrlPrefix();
-	const emailAddress = getEmailAddress( mailbox );
-
-	if ( isEmailForwardAccount( account ) ) {
-		return (
-			<div className="email-plan-mailboxes-list__mailbox-list-link">
-				<span>{ emailAddress }</span>
-			</div>
-		);
-	}
-
-	const isTitan = isTitanMailAccount( account );
-	const primaryHref = isTitan
-		? getTitanEmailUrl( titanAppsUrlPrefix, emailAddress, false, window.location.href )
-		: getGmailUrl( emailAddress );
-
-	return (
-		<ExternalLink
-			className="email-plan-mailboxes-list__mailbox-list-link"
-			href={ primaryHref }
-			onClick={ () => {
-				recordEmailAppLaunchEvent( {
-					app: isTitan ? titanAppsUrlPrefix : 'webmail',
-					context: 'email-management-list',
-					provider: isTitan ? 'titan' : 'google',
-				} );
-			} }
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			<span>{ emailAddress }</span>
-			<Gridicon icon="external" size={ 18 } />
-		</ExternalLink>
-	);
 }
 
 function EmailPlanMailboxesList( { account, domain, isLoadingEmails, mailboxes, addMailboxPath } ) {
