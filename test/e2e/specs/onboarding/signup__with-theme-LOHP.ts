b0b8b1ef99c7f1666dd1cd0dd117faeead1b905b
Andrija Vučinić <andrija.vucinic@gmail.com>
2024-12-11 17:54:48 +0100

    E2E: Makes the test get to the redirect every time
    was flaky, as the scrolling thing doesn't seem to work great
    with new Playwright...

--- test/e2e/specs/onboarding/signup__with-theme-LOHP.ts
+++ test/e2e/specs/onboarding/signup__with-theme-LOHP.ts
@@ -84,9 +84,8 @@ describe( 'Lifecyle: Logged Out Home Page, signup, onboard, launch and cancel su
 			// Get theme slug
 			const pageMatch = new URL( themeButtonUrl.href ).search.match( 'theme=([a-z]*)?&' );
 			themeSlug = pageMatch?.[ 1 ] || null;
-
-			// Hover, otherwise the element isn't considered stable, and is out of the viewport.
-			await themeCard.hover();
+			// Silly, but this wait makes it constantly work...
+			await page.waitForTimeout( 2000 );
 			await themeCard.getByText( 'Start with this theme' ).click();
 		} );
 
