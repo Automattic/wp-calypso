6d12ca4d7147512382eecca99c7e40a596375405
arthur <arthur.chu@automattic.com>
2024-12-11 14:30:04 +0800

    Design Picker: Dedupe the themes

--- packages/design-picker/src/hooks/use-filtered-designs.ts
+++ packages/design-picker/src/hooks/use-filtered-designs.ts
@@ -28,21 +28,33 @@ export const getFilteredDesignsByCategory = (
 	}
 
 	// Get designs by the selected category.
+	// Note that we don't want to show a theme in multiple sections.
+	// See https://github.com/Automattic/dotcom-forge/issues/10110.
 	const categorySlugsSet = new Set( categorySlugs );
 	for ( let i = 0; i < filteredDesigns.length; i++ ) {
 		const design = filteredDesigns[ i ];
-		let count = 0;
+		let lastMatchedCategory = null;
+		let matchedCount = 0;
 		for ( let j = 0; j < design.categories.length; j++ ) {
 			const category = design.categories[ j ];
 			if ( categorySlugsSet.has( category.slug ) ) {
-				filteredDesignsByCategory[ category.slug ].push( design );
-				count++;
+				// We show the designs for the last selected category on top first
+				// so it would be better to put the design into the last matched category
+				// if it doesn't match all selected categories.
+				lastMatchedCategory = category.slug;
+				matchedCount++;
 			}
 		}
 
 		// For designs that match all selected categories.
-		if ( count === categorySlugs.length ) {
+		// Limit the best matches to at least 2 selected categories.
+		if ( categorySlugs.length > 1 && matchedCount === categorySlugs.length ) {
 			filteredDesignsByCategory.best.push( design );
+			continue;
+		}
+
+		if ( lastMatchedCategory ) {
+			filteredDesignsByCategory[ lastMatchedCategory ].push( design );
 		}
 	}
 
