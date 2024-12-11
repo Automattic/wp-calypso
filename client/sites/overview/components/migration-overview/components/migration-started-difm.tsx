1d748763b099b67c25ea2fa25ee64c557e46ad0c
Caroline Moore <calobee@gmail.com>
2024-12-11 11:44:56 -0500

    Make sure we have a value before running replace

--- client/sites/overview/components/migration-overview/components/migration-started-difm.tsx
+++ client/sites/overview/components/migration-overview/components/migration-started-difm.tsx
@@ -5,10 +5,10 @@ import type { SiteDetails } from '@automattic/data-stores';
 
 export const MigrationStartedDIFM = ( { site }: { site?: SiteDetails } ) => {
 	const translate = useTranslate();
-	const migrationSourceSiteDomain = site?.options?.migration_source_site_domain?.replace(
-		/^https?:\/\/|\/+$/g,
-		''
-	);
+	const migrationSourceSiteDomain = site?.options?.migration_source_site_domain
+		? site?.options?.migration_source_site_domain?.replace( /^https?:\/\/|\/+$/g, '' )
+		: translate( 'your site' );
+
 	const title = translate( 'Your migration is underway' );
 	const subTitle = translate(
 		"Sit back as {{strong}}%(siteUrl)s{{/strong}} transfers to its new home. Here's what you can expect.",
