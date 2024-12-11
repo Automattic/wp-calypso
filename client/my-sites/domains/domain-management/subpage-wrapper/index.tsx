e0847ee3fb39417977140150c6397763a0101eec
Paulo Cruz <paulo.cruz@automattic.com>
2024-12-11 18:42:51 -0300

    Create a SubPageWrapper component

---
+++ client/my-sites/domains/domain-management/subpage-wrapper/index.tsx
@@ -0,0 +1,27 @@
+import NavigationHeader from 'calypso/components/navigation-header';
+import { getSubPageParams } from './subpage-params';
+import './style.scss';
+
+type SubPageWrapperProps = {
+	children: React.ReactNode;
+	subPageKey: string;
+};
+
+const SubPageWrapper = ( { children, subPageKey }: SubPageWrapperProps ) => {
+	const subPageParams = getSubPageParams( subPageKey );
+
+	return subPageParams ? (
+		<>
+			<NavigationHeader
+				navigationItems={ [] }
+				title={ subPageParams.title }
+				subtitle={ subPageParams.subtitle }
+			/>
+			<div className="subpage-wrapper">{ children }</div>
+		</>
+	) : (
+		<>{ children }</>
+	);
+};
+
+export default SubPageWrapper;
