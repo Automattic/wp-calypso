fc79f3d75f803a60f2a0a314bc2def571d1cbd89
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-12-12 17:02:33 +0100

    Integrate existing EmailPlanWarnings component

--- client/my-sites/email/email-management/home/email-plan-mailboxes-list.tsx
+++ client/my-sites/email/email-management/home/email-plan-mailboxes-list.tsx
@@ -1,12 +1,12 @@
 import { Badge, MaterialIcon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import Notice from 'calypso/components/notice';
-import NoticeAction from 'calypso/components/notice/notice-action';
 import { isRecentlyRegistered } from 'calypso/lib/domains/utils';
 import { isEmailUserAdmin } from 'calypso/lib/emails';
 import { getGSuiteSubscriptionStatus } from 'calypso/lib/gsuite';
 import EmailMailboxActionMenu from 'calypso/my-sites/email/email-management/home/email-mailbox-action-menu';
 import EmailMailboxWarnings from 'calypso/my-sites/email/email-management/home/email-mailbox-warnings';
+import EmailPlanWarnings from 'calypso/my-sites/email/email-management/home/email-plan-warnings';
 import EmailForwardSecondaryDetails from './email-plan-mailboxes/email-forward-secondary-details';
 import MailboxListHeader from './email-plan-mailboxes/list-header';
 import MailboxListItem from './email-plan-mailboxes/list-item';
@@ -33,6 +33,7 @@ function EmailPlanMailboxesList( {
 	const translate = useTranslate();
 	const accountType = account?.account_type;
 
+	const isNoMailboxes = ! mailboxes || mailboxes.length < 1;
 	const isGoogleConfiguring =
 		isRecentlyRegistered( domain.registrationDate, 45 ) &&
 		getGSuiteSubscriptionStatus( domain ) === 'unknown';
@@ -101,18 +102,20 @@ function EmailPlanMailboxesList( {
 
 	return (
 		<>
-			{ isGoogleConfiguring && configuringStateMode === 'notice' && (
+			{ configuringStateMode === 'notice' && account?.warnings.length && (
 				<Notice
 					className="email-plan-mailboxes-list__notice"
 					status="is-warning"
 					showDismiss={ false }
-					text={ translate(
-						'We are configuring your mailboxes. You will receive an email shortly when they are ready to use.'
-					) }
 				>
-					<NoticeAction>{ translate( 'Finish setup' ) }</NoticeAction>
+					<EmailPlanWarnings
+						domain={ domain }
+						emailAccount={ account }
+						ctaBtnProps={ { primary: false, plain: true } }
+					/>
 				</Notice>
 			) }
+
 			<MailboxListHeader
 				accountType={ accountType }
 				addMailboxPath={ addMailboxPath }
