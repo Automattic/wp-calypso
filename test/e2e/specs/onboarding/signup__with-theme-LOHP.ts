36b9e271ed4f9a5821269510546c3b6fc8a72266
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2024-12-12 13:42:32 +0200

    Revert "Move goto out of route call; force user actions"
    This reverts commit 0d6f5e915ec61ac0b7056aa4d7945c2b86becdcf.

--- test/e2e/specs/onboarding/signup__with-theme-LOHP.ts
+++ test/e2e/specs/onboarding/signup__with-theme-LOHP.ts
@@ -65,7 +65,7 @@ describe( 'Lifecyle: Logged Out Home Page, signup, onboard, launch and cancel su
 
 			// Hovering over the theme card is necessary to make the "Start with this theme" button visible.
 			const themeCard = themeContainer.locator( '.lp-image-top-row' ).last();
-			await themeCard.hover( { force: true } );
+			await themeCard.hover();
 
 			const themeButton = themeCard.getByText( 'Start with this theme' );
 			const calypsoUrl = new URL( DataHelper.getCalypsoURL() );
@@ -74,17 +74,20 @@ describe( 'Lifecyle: Logged Out Home Page, signup, onboard, launch and cancel su
 			if ( calypsoUrl.hostname !== 'wordpress.com' ) {
 				// Reroute the click to the current Calypso URL.
 				await page.route( themeButtonUrl.href, async ( route ) => {
-					await route.abort();
-				} );
+					themeButtonUrl.host = calypsoUrl.host;
+					themeButtonUrl.protocol = calypsoUrl.protocol;
 
-				themeButtonUrl.host = calypsoUrl.host;
-				themeButtonUrl.protocol = calypsoUrl.protocol;
-				await page.goto( themeButtonUrl.href );
+					await route.abort();
+					await page.waitForTimeout( 2000 );
+
+					await page.goto( themeButtonUrl.href );
+				} );
 			}
 			// Get theme slug
 			const pageMatch = new URL( themeButtonUrl.href ).search.match( 'theme=([a-z]*)?&' );
 			themeSlug = pageMatch?.[ 1 ] || null;
-			await themeCard.hover( { force: true } );
+			// Silly, but this wait makes it constantly work...
+			await page.waitForTimeout( 2000 );
 			await themeCard.getByText( 'Start with this theme' ).click();
 		} );
 
