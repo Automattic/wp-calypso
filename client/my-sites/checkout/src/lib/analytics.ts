a98322c46d9ece70a1153ef650b94c4838725c25
Payton Swick <payton@foolord.com>
2024-12-13 11:24:11 -0500

    Recursively serialize error causes when logging checkout errors

--- client/my-sites/checkout/src/lib/analytics.ts
+++ client/my-sites/checkout/src/lib/analytics.ts
@@ -9,11 +9,44 @@ import {
 import type { CheckoutPaymentMethodSlug } from '@automattic/wpcom-checkout';
 import type { CalypsoDispatch } from 'calypso/state/types';
 
-export function convertErrorToString( error: Error ): string {
-	if ( error.cause ) {
-		return `${ error }; Cause: ${ error.cause }; Stack: ${ error.stack }`;
+function serializeCaughtError(
+	// This may come from Error.cause which I'm pretty sure has no defined
+	// type. It can be used to keep another Error but it could also be anything
+	// else so let's not make any assumptions. Also things other than Error
+	// objects can be thrown so let's not even assume this is an Error.
+	error: unknown
+): string {
+	const messages = [];
+	messages.push( getErrorMessageFromError( error ) );
+	const errorObject = error as Error;
+	if ( 'cause' in errorObject && errorObject.cause ) {
+		const cause = serializeCaughtError( errorObject.cause );
+		messages.push( `(Cause: ${ cause })` );
 	}
-	return `${ error }; Stack: ${ error.stack }`;
+	if ( 'stack' in errorObject && errorObject.stack ) {
+		messages.push( `(Stack: ${ errorObject.stack })` );
+	}
+	return messages.join( '; ' );
+}
+
+function getErrorMessageFromError( error: unknown ): string {
+	const errorObject = error as Error;
+	if ( 'message' in errorObject && errorObject.message ) {
+		return `Message: ${ errorObject.message }`;
+	}
+	return `Non-Error: ${ error }`;
+}
+
+/**
+ * Convert a thrown error to a string for logging.
+ *
+ * I've typed this function as requiring an Error because that's the intended
+ * behavior and I'd like TypeScript to enforce that as best as it can. However,
+ * other things can be thrown besides Error objects and so this will actually
+ * handle other things like strings just fine.
+ */
+export function convertErrorToString( error: Error ): string {
+	return serializeCaughtError( error );
 }
 
 export function logStashLoadErrorEvent(
