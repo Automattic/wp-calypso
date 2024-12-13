41b5dd14ac59b9c7cdb6452b951997e8498f4f27
Paulo Cruz <paulo.cruz@automattic.com>
2024-12-13 16:11:07 -0300

    Fix unit tests

--- client/my-sites/email/test/paths.ts
+++ client/my-sites/email/test/paths.ts
@@ -133,10 +133,6 @@ describe( 'path helper functions', () => {
 		expect( getEmailManagementPath( ':site' ) ).toEqual( '/email/:site' );
 		expect( getEmailManagementPath( siteName, null ) ).toEqual( `/email/${ siteName }` );
 		expect( getEmailManagementPath( null, null ) ).toEqual( '/email' );
-
-		expect( getEmailManagementPath( siteName, domainName, domainsManagementPrefix ) ).toEqual(
-			`/domains/manage/all/email/${ domainName }/${ siteName }`
-		);
 	} );
 
 	it( 'getForwardingPath', () => {
