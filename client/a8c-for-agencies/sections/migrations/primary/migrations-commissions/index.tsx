66719f0e5e9a4330383181cbf9ed28594714a35c
Yashwin <yashwinpoojary@gmail.com>
2024-12-13 15:28:38 +0530

    Implement payment delayed banner

--- client/a8c-for-agencies/sections/migrations/primary/migrations-commissions/index.tsx
+++ client/a8c-for-agencies/sections/migrations/primary/migrations-commissions/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@wordpress/components';
 import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
 import { useCallback, useMemo, useState } from 'react';
+import A4APaymentDelayedNotice from 'calypso/a8c-for-agencies/components/a4a-payment-delayed-notice';
 import Layout from 'calypso/a8c-for-agencies/components/layout';
 import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
 import LayoutHeader, {
@@ -12,6 +13,7 @@ import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
 import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
 import { A4A_MIGRATIONS_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import TextPlaceholder from 'calypso/a8c-for-agencies/components/text-placeholder';
+import useGetTipaltiPayee from 'calypso/a8c-for-agencies/sections/referrals/hooks/use-get-tipalti-payee';
 import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import MigrationsCommissionsList from '../../commissions-list';
@@ -25,6 +27,10 @@ import './style.scss';
 export default function MigrationsCommissions() {
 	const translate = useTranslate();
 	const dispatch = useDispatch();
+
+	const { data: tipaltiData } = useGetTipaltiPayee();
+
+	const isPayable = !! tipaltiData?.IsPayable;
 
 	const [ showAddSitesModal, setShowAddSitesModal ] = useState( false );
 
@@ -75,6 +81,7 @@ export default function MigrationsCommissions() {
 			wide
 		>
 			<LayoutTop>
+				{ isPayable && ! showEmptyState && <A4APaymentDelayedNotice /> }
 				<LayoutHeader>
 					<Breadcrumb
 						hideOnMobile
