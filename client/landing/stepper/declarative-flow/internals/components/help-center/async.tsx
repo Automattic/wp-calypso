206c44ade7d2d7f5a9ef0c716043b938f01133c2
Omar Alshaker <omar@omaralshaker.com>
2024-12-16 14:30:17 +0100

    Better "query" param

--- client/landing/stepper/declarative-flow/internals/components/help-center/async.tsx
+++ client/landing/stepper/declarative-flow/internals/components/help-center/async.tsx
@@ -13,14 +13,14 @@ const AsyncHelpCenter = () => {
 	}, [ setShowHelpCenter ] );
 
 	/**
-	 * The ?v2 query parameter ensures Webpack treats this Help Center as separate from the one in the main client app.
+	 * The stepper query parameter ensures Webpack treats this Help Center as separate from the one in the main client app.
 	 * Without it, Webpack would create one shared chunk, loaded in both apps. Since Stepper is smaller, more CSS would
 	 * need be bundled into that shared chunk. This is great for Stepper, but it duplicates the CSS in the main client app.
 	 * See: #97480
 	 */
 	return (
 		<AsyncLoad
-			require="@automattic/help-center?v2"
+			require="@automattic/help-center?stepper"
 			placeholder={ null }
 			handleClose={ handleClose }
 		/>
