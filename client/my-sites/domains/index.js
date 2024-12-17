89ec045527374c34468235ded59dc0b43081af8b
Miroslav Mitev <m1r0@users.noreply.github.com>
2024-12-17 18:23:16 +0200

    Use the existing route

--- client/my-sites/domains/index.js
+++ client/my-sites/domains/index.js
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import { recordSiftScienceUser } from 'calypso/lib/siftscience';
@@ -66,6 +67,8 @@ function getCommonHandlers( {
 }
 
 export default function () {
+	const isAllDomainManagementEnabled = config.isEnabled( 'calypso/all-domain-management' );
+
 	page( '/domains*', recordSiftScienceUser );
 
 	// These redirects are work-around in response to an issue where navigating back after a
@@ -100,10 +103,24 @@ export default function () {
 		domainManagementController.domainManagementRedirectSettings
 	);
 
-	registerStandardDomainManagementPages(
-		paths.domainManagementEditContactInfo,
-		domainManagementController.domainManagementEditContactInfo
-	);
+	if ( isAllDomainManagementEnabled ) {
+		page(
+			paths.domainManagementAllRoot() + '/:domain/edit-contact-info/:site',
+			siteSelection,
+			navigation,
+			domainManagementController.domainManagementSubpageParams( EDIT_CONTACT_INFO ),
+			domainManagementController.domainManagementEditContactInfo,
+			domainManagementController.domainManagementSubpageView,
+			domainManagementController.domainDashboardLayout,
+			makeLayout,
+			clientRender
+		);
+	} else {
+		registerStandardDomainManagementPages(
+			paths.domainManagementEditContactInfo,
+			domainManagementController.domainManagementEditContactInfo
+		);
+	}
 
 	registerStandardDomainManagementPages(
 		paths.domainManagementManageConsent,
@@ -415,18 +432,6 @@ export default function () {
 		navigation,
 		domainManagementController.domainManagementSubpageParams( ADD_FOWARDING_EMAIL ),
 		emailController.emailManagementAddEmailForwards,
-		domainManagementController.domainManagementSubpageView,
-		domainManagementController.domainDashboardLayout,
-		makeLayout,
-		clientRender
-	);
-
-	page(
-		paths.domainManagementAllRoot() + '/:domain/contact-info/edit/:site',
-		siteSelection,
-		navigation,
-		domainManagementController.domainManagementSubpageParams( EDIT_CONTACT_INFO ),
-		domainManagementController.domainManagementEditContactInfo,
 		domainManagementController.domainManagementSubpageView,
 		domainManagementController.domainDashboardLayout,
 		makeLayout,
