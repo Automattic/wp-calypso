8e38862a7c1b559fe29c99fe9c9c139fb9dd8e51
Ignacio Dobronich <ignacio.dobronich@automattic.com>
2024-12-18 12:46:05 -0300

    Removed unnecessary type cast

--- client/me/purchases/manage-purchase/payment-method-selector/assignment-processor-functions.ts
+++ client/me/purchases/manage-purchase/payment-method-selector/assignment-processor-functions.ts
@@ -235,7 +235,7 @@ export async function assignNewCardProcessor(
 			city,
 			organization,
 			address,
-			setupKey: String( setupKey ),
+			setupKey,
 		} );
 
 		return makeSuccessResponse( result );
