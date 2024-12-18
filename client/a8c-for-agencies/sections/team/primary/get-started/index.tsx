44fa87dd74457562f3ed4b1d0215b9ac8cd77f6d
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-12-19 01:58:49 +0800

    Add missing Mobile navigation on the Team get started page.

--- client/a8c-for-agencies/sections/team/primary/get-started/index.tsx
+++ client/a8c-for-agencies/sections/team/primary/get-started/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@wordpress/components';
 import { Icon, external } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
+import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
 import { A4A_TEAM_INVITE_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import StepSection from 'calypso/a8c-for-agencies/components/step-section';
 import StepSectionItem from 'calypso/a8c-for-agencies/components/step-section-item';
@@ -36,6 +37,7 @@ export default function GetStarted() {
 				<LayoutHeader>
 					<Title>{ title }</Title>
 					<Actions>
+						<MobileSidebarNavigation />
 						<Button variant="primary" onClick={ onInviteClick } href={ A4A_TEAM_INVITE_LINK }>
 							{ translate( 'Invite a team member' ) }
 						</Button>
