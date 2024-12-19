bf387ad060df13a7c49af37943b82c85a1b253e5
Yashwin <yashwinpoojary@gmail.com>
2024-12-19 12:25:51 +0530

    initial implementation of 'Add site' flow for WPCOM

--- client/components/add-new-site/menu-items/index.tsx
+++ client/components/add-new-site/menu-items/index.tsx
@@ -1,10 +1,15 @@
 import isA8CForAgencies from 'calypso/lib/a8c-for-agencies/is-a8c-for-agencies';
+import isWPCOMEnvironment from 'calypso/lib/wpcom/is-wpcom-environment';
 import AddNewSiteA4AMenuItems from './a4a';
+import AddNewSiteWPCOMMenuItems from './wpcom';
 import type { AddNewSiteMenuItemsProps } from '../types';
 
 const AddNewSiteMenuItems = ( props: AddNewSiteMenuItemsProps ) => {
 	if ( isA8CForAgencies() ) {
 		return <AddNewSiteA4AMenuItems { ...props } />;
+	}
+	if ( isWPCOMEnvironment() ) {
+		return <AddNewSiteWPCOMMenuItems { ...props } />;
 	}
 	return null;
 };
