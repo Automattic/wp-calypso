b4de86705d928518adbebe8f3998eabfa7e5a023
Gergely Csécsey <gcsecsey@gmail.com>
2024-12-19 16:28:54 +0000

    use Modal component for plugin action dialogs

--- client/my-sites/plugins/hooks/use-show-plugin-action-dialog.tsx
+++ client/my-sites/plugins/hooks/use-show-plugin-action-dialog.tsx
@@ -1,42 +1,81 @@
-import React, { useCallback } from 'react';
-import acceptDialog from 'calypso/lib/accept';
+import { Modal, Button } from '@wordpress/components';
+import React, { useCallback, useState } from 'react';
 import { PluginActions } from './types';
 import useGetDialogText from './use-get-dialog-text';
 import type { Site, Plugin } from './types';
 import type { TranslateResult } from 'i18n-calypso';
+import './styles.scss';
 
 type DialogMessageProps = {
 	message: TranslateResult;
+	onConfirm: () => void;
+	onCancel: () => void;
+	heading?: string;
+	confirmText?: string;
+	cancelText?: string;
+	isScary?: boolean;
 };
-const DialogMessage: React.FC< DialogMessageProps > = ( { message } ) => <p>{ message }</p>;
 
-type DialogCallback = ( accepted: boolean ) => void;
+const PluginActionDialog: React.FC< DialogMessageProps > = ( {
+	message,
+	onConfirm,
+	onCancel,
+	heading,
+	confirmText,
+	cancelText,
+	isScary,
+} ) => (
+	<Modal title={ heading } onRequestClose={ onCancel } className="plugins__confirmation-modal">
+		<div className="plugin-action-dialog__content">
+			<p>{ message }</p>
+			<div className="plugin-action-dialog__buttons">
+				<Button variant="secondary" onClick={ onCancel }>
+					{ cancelText }
+				</Button>
+				<Button isDestructive={ isScary } variant="primary" onClick={ onConfirm }>
+					{ confirmText }
+				</Button>
+			</div>
+		</div>
+	</Modal>
+);
+
 const useShowPluginActionDialog = () => {
 	const getDialogText = useGetDialogText();
+	const [ dialogProps, setDialogProps ] = useState< DialogMessageProps | null >( null );
 
-	return useCallback(
-		( action: string, plugins: Plugin[], sites: Site[], callback: DialogCallback ) => {
+	const showDialog = useCallback(
+		(
+			action: string,
+			plugins: Plugin[],
+			sites: Site[],
+			callback: ( accepted: boolean ) => void
+		) => {
 			const { heading, message, cta } = getDialogText( action, plugins, sites );
 
-			const dialogOptions = {
-				additionalClassNames: 'plugins__confirmation-modal',
-				...( action === PluginActions.REMOVE && { isScary: true } ),
-				useModal: true,
-				modalOptions: {
-					title: heading,
+			setDialogProps( {
+				message,
+				heading: heading as string,
+				confirmText: cta?.confirm,
+				cancelText: cta?.cancel,
+				isScary: action === PluginActions.REMOVE,
+				onConfirm: () => {
+					callback( true );
+					setDialogProps( null );
 				},
-			};
-
-			acceptDialog(
-				<DialogMessage message={ message } />,
-				callback,
-				cta?.confirm,
-				cta?.cancel,
-				dialogOptions
-			);
+				onCancel: () => {
+					callback( false );
+					setDialogProps( null );
+				},
+			} );
 		},
 		[ getDialogText ]
 	);
+
+	return {
+		showDialog,
+		DialogComponent: dialogProps ? <PluginActionDialog { ...dialogProps } /> : null,
+	};
 };
 
 // For use in situations where hooks aren't supported :-(
@@ -44,8 +83,13 @@ export function withShowPluginActionDialog< ComponentProps >(
 	Component: React.ComponentType< ComponentProps >
 ) {
 	return ( props: ComponentProps ) => {
-		const showPluginActionDialog = useShowPluginActionDialog();
-		return <Component showPluginActionDialog={ showPluginActionDialog } { ...props } />;
+		const { showDialog, DialogComponent } = useShowPluginActionDialog();
+		return (
+			<>
+				<Component showPluginActionDialog={ showDialog } { ...props } />
+				{ DialogComponent }
+			</>
+		);
 	};
 }
 
