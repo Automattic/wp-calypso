5cd7998e20cbe00ccf048dcd165b8324c0811e80
Dognose <dognose24@users.noreply.github.com>
2024-12-19 11:39:36 +0800

    Stats: Unify Jetpack Stats upsell copies (#97544)
    * Extract shared getUpsellCopy
    
    * Expand shared upsell copies for Jetpack sites
    
    * Update unified upsell copies according to the design
    
    * Update upsell action button for self-hosted sites

--- client/my-sites/stats/stats-card-upsell/stats-card-upsell-jetpack.tsx
+++ client/my-sites/stats/stats-card-upsell/stats-card-upsell-jetpack.tsx
@@ -2,166 +2,21 @@ import { recordTracksEvent } from '@automattic/calypso-analytics';
 import config from '@automattic/calypso-config';
 import page from '@automattic/calypso-router';
 import { Button } from '@automattic/components';
-import { localizeUrl } from '@automattic/i18n-utils';
 import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
 import React from 'react';
-import { SUPPORT_URL, INSIGHTS_SUPPORT_URL } from 'calypso/my-sites/stats/const';
 import { useSelector } from 'calypso/state';
 import getIsSiteWPCOM from 'calypso/state/selectors/is-site-wpcom';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
-import {
-	STATS_FEATURE_DATE_CONTROL,
-	STATS_FEATURE_UTM_STATS,
-	STATS_TYPE_DEVICE_STATS,
-	STAT_TYPE_TOP_POSTS,
-	STAT_TYPE_REFERRERS,
-	STAT_TYPE_COUNTRY_VIEWS,
-	STAT_TYPE_CLICKS,
-	STAT_TYPE_TOP_AUTHORS,
-	STAT_TYPE_EMAILS_SUMMARY,
-	STAT_TYPE_SEARCH_TERMS,
-	STAT_TYPE_VIDEO_PLAYS,
-	STAT_TYPE_INSIGHTS_ALL_TIME_STATS,
-	STAT_TYPE_INSIGHTS_MOST_POPULAR_TIME,
-	STAT_TYPE_INSIGHTS_MOST_POPULAR_DAY,
-	STAT_TYPE_INSIGHTS_ALL_TIME_INSIGHTS,
-	STAT_TYPE_TAGS,
-	STAT_TYPE_COMMENTS,
-} from '../constants';
 import { trackStatsAnalyticsEvent } from '../utils';
 import StatsCardUpsellOverlay from './stats-card-upsell-overlay';
+import getUpsellCopy from './stats-upsell-copy';
 import { Props } from './';
-
-const trafficSupportLinkWithAnchor = ( anchor: string ) => {
-	return (
-		<a
-			href={ localizeUrl( `${ SUPPORT_URL }#${ anchor }` ) }
-			target="_blank"
-			rel="noopenner noreferrer"
-		/>
-	);
-};
-
-const insightsSupportLinkWithAnchor = ( anchor: string ) => {
-	return (
-		<a
-			href={ localizeUrl( `${ INSIGHTS_SUPPORT_URL }#${ anchor }` ) }
-			target="_blank"
-			rel="noopenner noreferrer"
-		/>
-	);
-};
-
-const utmLearnMoreLink = () => {
-	return (
-		<a
-			href="https://jetpack.com/redirect/?source=jetpack-stats-learn-more-about-utm-url-builder"
-			target="_blank"
-			rel="noopenner noreferrer"
-		/>
-	);
-};
-
-const useUpsellCopy = ( statType: string ) => {
-	const translate = useTranslate();
-	switch ( statType ) {
-		case STATS_FEATURE_DATE_CONTROL:
-			return translate( 'Compare different time periods to analyze your site’s growth.' );
-		case STATS_FEATURE_UTM_STATS:
-			return translate(
-				'Generate UTM parameters and track your campaign performance data. {{link}}Learn more{{/link}}',
-				{
-					components: {
-						link: utmLearnMoreLink(),
-					},
-				}
-			);
-		case STATS_TYPE_DEVICE_STATS:
-			return translate( 'See which devices your visitors are using.' );
-		case STAT_TYPE_TOP_POSTS:
-			return translate( 'Track which {{link}}Posts and Pages{{/link}} receive the most views.', {
-				components: {
-					link: trafficSupportLinkWithAnchor( 'posts-amp-pages' ),
-				},
-			} );
-		case STAT_TYPE_REFERRERS:
-			return translate(
-				'Discover which websites are {{link}}referring visitors{{/link}} to your site.',
-				{
-					components: {
-						link: trafficSupportLinkWithAnchor( 'referrers' ),
-					},
-				}
-			);
-		case STAT_TYPE_COUNTRY_VIEWS:
-			return translate( 'Upgrade to track visitors by {{link}}location{{/link}}.', {
-				components: {
-					link: trafficSupportLinkWithAnchor( 'countries' ),
-				},
-			} );
-		case STAT_TYPE_CLICKS:
-			return translate( 'View the most clicked {{link}}external links{{/link}} on your site.', {
-				components: {
-					link: trafficSupportLinkWithAnchor( 'clicks' ),
-				},
-			} );
-		case STAT_TYPE_TOP_AUTHORS:
-			return translate( 'See how much traffic each {{link}}author{{/link}} has generated.', {
-				components: {
-					link: trafficSupportLinkWithAnchor( 'authors' ),
-				},
-			} );
-		case STAT_TYPE_EMAILS_SUMMARY:
-			return translate(
-				'View information about {{link}}emails{{/link}} sent to your subscribers.',
-				{
-					components: {
-						link: trafficSupportLinkWithAnchor( 'emails' ),
-					},
-				}
-			);
-		case STAT_TYPE_SEARCH_TERMS:
-			return translate( 'See {{link}}terms that visitors search{{/link}} to find your site.', {
-				components: {
-					link: trafficSupportLinkWithAnchor( 'search-terms' ),
-				},
-			} );
-		case STAT_TYPE_VIDEO_PLAYS:
-			return translate( 'Engagement information on your most viewed {{link}}videos{{/link}}.', {
-				components: {
-					link: trafficSupportLinkWithAnchor( 'videos' ),
-				},
-			} );
-		case STAT_TYPE_INSIGHTS_ALL_TIME_STATS:
-			return translate( 'All-time {{link}}website insights{{/link}}.', {
-				components: {
-					link: insightsSupportLinkWithAnchor( 'all-time-highlights' ),
-				},
-			} );
-		case STAT_TYPE_INSIGHTS_MOST_POPULAR_TIME:
-			return translate( 'Best day and hour to post.' );
-		case STAT_TYPE_INSIGHTS_MOST_POPULAR_DAY:
-			return translate( 'Most popular day this year.' );
-		case STAT_TYPE_INSIGHTS_ALL_TIME_INSIGHTS:
-			return translate( 'All-time views.' );
-		case STAT_TYPE_TAGS:
-			return translate( 'Track the most viewed {{link}}tags & categories{{/link}}.', {
-				components: {
-					link: insightsSupportLinkWithAnchor( ':~:text=Tags%20,%20Categories' ),
-				},
-			} );
-		case STAT_TYPE_COMMENTS:
-			return translate( 'Top comments overview.' );
-		default:
-			return translate( 'Upgrade to unlock the feature' );
-	}
-};
 
 const StatsCardUpsellJetpack: React.FC< Props > = ( { className, siteId, statType } ) => {
 	const translate = useTranslate();
 	const isOdysseyStats = config.isEnabled( 'is_running_in_jetpack_site' );
-	const copyText = useUpsellCopy( statType );
+	const copyText = getUpsellCopy( statType );
 
 	const siteSlug = useSelector( ( state ) => getSiteSlug( state, siteId ) );
 	const isWPCOMSite = useSelector( ( state ) => siteId && getIsSiteWPCOM( state, siteId ) );
@@ -203,7 +58,7 @@ const StatsCardUpsellJetpack: React.FC< Props > = ( { className, siteId, statTyp
 					onClick={ onClick }
 					primary
 				>
-					{ translate( 'Upgrade plan' ) }
+					{ translate( 'Upgrade to Commercial' ) }
 				</Button>
 			}
 		/>
---
+++ client/my-sites/stats/stats-card-upsell/stats-upsell-copy.tsx
@@ -0,0 +1,197 @@
+import { localizeUrl } from '@automattic/i18n-utils';
+import { translate } from 'i18n-calypso';
+import {
+	SUPPORT_URL,
+	INSIGHTS_SUPPORT_URL,
+	JETPACK_SUPPORT_URL_TRAFFIC,
+	JETPACK_SUPPORT_VIDEOPRESS_URL_STATS,
+} from 'calypso/my-sites/stats/const';
+import {
+	STATS_FEATURE_DATE_CONTROL,
+	STATS_FEATURE_UTM_STATS,
+	STATS_TYPE_DEVICE_STATS,
+	STAT_TYPE_CLICKS,
+	STAT_TYPE_COUNTRY_VIEWS,
+	STAT_TYPE_FILE_DOWNLOADS,
+	STAT_TYPE_REFERRERS,
+	STAT_TYPE_SEARCH_TERMS,
+	STAT_TYPE_TOP_AUTHORS,
+	STAT_TYPE_TOP_POSTS,
+	STAT_TYPE_VIDEO_PLAYS,
+	STAT_TYPE_EMAILS_SUMMARY,
+	STAT_TYPE_INSIGHTS_ALL_TIME_STATS,
+	STAT_TYPE_INSIGHTS_MOST_POPULAR_TIME,
+	STAT_TYPE_INSIGHTS_MOST_POPULAR_DAY,
+	STAT_TYPE_INSIGHTS_ALL_TIME_INSIGHTS,
+	STAT_TYPE_TAGS,
+	STAT_TYPE_COMMENTS,
+} from '../constants';
+
+const jetpackTrafficSupportLinkWithAnchor = ( anchor: string ) => {
+	return (
+		<a
+			href={ localizeUrl( `${ JETPACK_SUPPORT_URL_TRAFFIC }#${ anchor }` ) }
+			target="_blank"
+			rel="noopenner noreferrer"
+		/>
+	);
+};
+
+const trafficSupportLinkWithAnchor = ( anchor: string ) => {
+	return (
+		<a
+			href={ localizeUrl( `${ SUPPORT_URL }#${ anchor }` ) }
+			target="_blank"
+			rel="noopenner noreferrer"
+		/>
+	);
+};
+
+const insightsSupportLinkWithAnchor = ( anchor: string ) => {
+	return (
+		<a
+			href={ localizeUrl( `${ INSIGHTS_SUPPORT_URL }#${ anchor }` ) }
+			target="_blank"
+			rel="noopenner noreferrer"
+		/>
+	);
+};
+
+const getUpsellCopy = ( statType: string ) => {
+	switch ( statType ) {
+		case STAT_TYPE_CLICKS:
+			return translate(
+				'Learn what {{link}}external links{{/link}} your visitors click on your site to reveal their areas of interest.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'clicks' ),
+					},
+				}
+			);
+		case STAT_TYPE_COUNTRY_VIEWS:
+			return translate(
+				'Discover where your {{link}}visitors are located{{/link}} and identify where your traffic is coming from.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'countries' ),
+					},
+				}
+			);
+		case STAT_TYPE_FILE_DOWNLOADS:
+			return translate( 'Discover the most {{link}}downloaded files{{/link}} by your visitors.', {
+				components: {
+					link: trafficSupportLinkWithAnchor( 'file-downloads' ),
+				},
+			} );
+		case STAT_TYPE_REFERRERS:
+			return translate(
+				'Find out where your {{link}}visitors come from{{/link}} to optimize your content strategy.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'referrers' ),
+					},
+				}
+			);
+		case STAT_TYPE_SEARCH_TERMS:
+			return translate(
+				'Discover the {{link}}terms and phrases{{/link}} your visitors use to find your site.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'search-terms' ),
+					},
+				}
+			);
+		case STAT_TYPE_TOP_AUTHORS:
+			return translate(
+				'Identify your audience’s {{link}}favorite writers{{/link}} and perspectives.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'authors' ),
+					},
+				}
+			);
+		case STAT_TYPE_TOP_POSTS:
+			return translate(
+				'Discover your {{link}}post and pages{{/link}} traffic in detail and learn what content resonates the most.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'analyzing-popular-posts-and-pages' ),
+					},
+				}
+			);
+		case STAT_TYPE_VIDEO_PLAYS:
+			return translate(
+				'Discover your {{link}}most popular videos{{/link}} and find out how they performed.',
+				{
+					components: {
+						link: (
+							<a
+								href={ localizeUrl( JETPACK_SUPPORT_VIDEOPRESS_URL_STATS ) }
+								target="_blank"
+								rel="noopenner noreferrer"
+							/>
+						),
+					},
+				}
+			);
+		case STATS_FEATURE_DATE_CONTROL:
+			return translate(
+				'Compare different {{link}}time periods{{/link}} to analyze your site’s growth.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'using-date-filters' ),
+					},
+				}
+			);
+		case STATS_FEATURE_UTM_STATS:
+			return translate(
+				'Generate UTM parameters and track your {{link}}campaign performance data{{/link}}.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor(
+							'harnessing-utm-stats-for-precision-tracking'
+						),
+					},
+				}
+			);
+		case STATS_TYPE_DEVICE_STATS:
+			return translate(
+				'See which {{link}}devices, browsers and OS{{/link}} your visitors are using.',
+				{
+					components: {
+						link: jetpackTrafficSupportLinkWithAnchor( 'devices-stats' ),
+					},
+				}
+			);
+		case STAT_TYPE_EMAILS_SUMMARY:
+			return translate( 'Learn about the performance of the emails you sent to your subscribers.', {
+				components: {
+					link: trafficSupportLinkWithAnchor( 'emails' ),
+				},
+			} );
+		case STAT_TYPE_INSIGHTS_ALL_TIME_STATS:
+			return translate( 'All-time {{link}}website insights{{/link}}.', {
+				components: {
+					link: insightsSupportLinkWithAnchor( 'all-time-highlights' ),
+				},
+			} );
+		case STAT_TYPE_INSIGHTS_MOST_POPULAR_TIME:
+			return translate( 'Best day and hour to post.' );
+		case STAT_TYPE_INSIGHTS_MOST_POPULAR_DAY:
+			return translate( 'Most popular day this year.' );
+		case STAT_TYPE_INSIGHTS_ALL_TIME_INSIGHTS:
+			return translate( 'All-time views.' );
+		case STAT_TYPE_TAGS:
+			return translate( 'Track the most viewed {{link}}tags & categories{{/link}}.', {
+				components: {
+					link: insightsSupportLinkWithAnchor( ':~:text=Tags%20,%20Categories' ),
+				},
+			} );
+		case STAT_TYPE_COMMENTS:
+			return translate( 'Top comments overview.' );
+		default:
+			return translate( 'Upgrade your plan to unlock Jetpack Stats.' );
+	}
+};
+
+export default getUpsellCopy;
