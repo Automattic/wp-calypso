ac2f91299f3bc0bb2f7c1c4e7c18eb26a36e2c56
James Tien <southp0105@gmail.com>
2024-12-20 16:27:54 +0800

    Move the add-on meta list data to `addons-list`

--- packages/data-stores/src/add-ons/hooks/use-add-ons.ts
+++ packages/data-stores/src/add-ons/hooks/use-add-ons.ts
@@ -1,66 +1,12 @@
-import {
-	PRODUCT_WPCOM_CUSTOM_DESIGN,
-	PRODUCT_WPCOM_UNLIMITED_THEMES,
-	PRODUCT_1GB_SPACE,
-	WPCOM_FEATURES_PREMIUM_THEMES_UNLIMITED,
-	WPCOM_FEATURES_CUSTOM_DESIGN,
-} from '@automattic/calypso-products';
+import { PRODUCT_1GB_SPACE } from '@automattic/calypso-products';
 import { useMemo } from '@wordpress/element';
-import i18n, { useTranslate } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 import * as ProductsList from '../../products-list';
 import * as Site from '../../site';
-import {
-	ADD_ON_100GB_STORAGE,
-	ADD_ON_50GB_STORAGE,
-	ADD_ON_CUSTOM_DESIGN,
-	ADD_ON_UNLIMITED_THEMES,
-} from '../constants';
-import customDesignIcon from '../icons/custom-design';
-import spaceUpgradeIcon from '../icons/space-upgrade';
-import unlimitedThemesIcon from '../icons/unlimited-themes';
+import { getAddOnsList } from '../addons-list';
 import useAddOnCheckoutLink from './use-add-on-checkout-link';
 import { createAddOnPriceKey, useAddOnPrices } from './use-add-on-prices';
 import type { AddOnMeta } from '../types';
-
-const getActiveAddOns = (): AddOnMeta[] => {
-	return [
-		{
-			addOnSlug: ADD_ON_UNLIMITED_THEMES,
-			productSlug: PRODUCT_WPCOM_UNLIMITED_THEMES,
-			featureSlugs: [ WPCOM_FEATURES_PREMIUM_THEMES_UNLIMITED ] as string[],
-			icon: unlimitedThemesIcon,
-			featured: true,
-		},
-		{
-			addOnSlug: ADD_ON_CUSTOM_DESIGN,
-			productSlug: PRODUCT_WPCOM_CUSTOM_DESIGN,
-			featureSlugs: [ WPCOM_FEATURES_CUSTOM_DESIGN ] as string[],
-			icon: customDesignIcon,
-		},
-		{
-			addOnSlug: ADD_ON_50GB_STORAGE,
-			productSlug: PRODUCT_1GB_SPACE,
-			featureSlugs: null,
-			icon: spaceUpgradeIcon,
-			quantity: 50,
-			name: i18n.translate( '50 GB Storage' ),
-			description: i18n.translate(
-				'Make more space for high-quality photos, videos, and other media. '
-			),
-		},
-		{
-			addOnSlug: ADD_ON_100GB_STORAGE,
-			productSlug: PRODUCT_1GB_SPACE,
-			featureSlugs: null,
-			icon: spaceUpgradeIcon,
-			quantity: 100,
-			name: i18n.translate( '100 GB Storage' ),
-			description: i18n.translate(
-				'Take your site to the next level. Store all your media in one place without worrying about running out of space.'
-			),
-		},
-	];
-};
 
 interface Props {
 	selectedSiteId?: number | null | undefined;
@@ -69,15 +15,15 @@ interface Props {
 const useAddOns = ( { selectedSiteId }: Props = {} ): ( AddOnMeta | null )[] => {
 	const translate = useTranslate();
 	const checkoutLink = useAddOnCheckoutLink();
-	const activeAddOns = getActiveAddOns();
-	const addOnPrices = useAddOnPrices( activeAddOns );
-	const productSlugs = activeAddOns.map( ( item ) => item.productSlug );
+	const addOns = getAddOnsList();
+	const addOnPrices = useAddOnPrices( addOns );
+	const productSlugs = addOns.map( ( item ) => item.productSlug );
 	const productsList = ProductsList.useProducts( productSlugs );
 	const mediaStorage = Site.useSiteMediaStorage( { siteIdOrSlug: selectedSiteId } );
 
 	return useMemo(
 		() =>
-			activeAddOns.map( ( addOnMeta ) => {
+			addOns.map( ( addOnMeta ) => {
 				const { productSlug, quantity } = addOnMeta;
 				const key = createAddOnPriceKey( addOnMeta );
 
