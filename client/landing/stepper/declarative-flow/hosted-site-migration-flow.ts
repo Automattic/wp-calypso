cc30370cf45545c44cc1ff4903a569e5d9a4e7e6
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-12-20 18:17:31 -0300

    Accept back_url query parameter in setup/hosted-site-migration

--- client/landing/stepper/declarative-flow/hosted-site-migration-flow.ts
+++ client/landing/stepper/declarative-flow/hosted-site-migration-flow.ts
@@ -1,4 +1,5 @@
 import { HOSTED_SITE_MIGRATION_FLOW } from '@automattic/onboarding';
+import { useSearchParams } from 'react-router-dom';
 import { type Flow } from './internals/types';
 import siteMigration from './site-migration-flow';
 
@@ -6,6 +7,16 @@ const hostedSiteMigrationFlow: Flow = {
 	...siteMigration,
 	variantSlug: HOSTED_SITE_MIGRATION_FLOW,
 	isSignupFlow: true,
+	useLoginParams() {
+		const [ searchParams ] = useSearchParams();
+		const backUrl = searchParams.get( 'back_url' );
+
+		return {
+			extraQueryParams: {
+				...( backUrl ? { back_url: backUrl } : {} ),
+			},
+		};
+	},
 };
 
 export default hostedSiteMigrationFlow;
