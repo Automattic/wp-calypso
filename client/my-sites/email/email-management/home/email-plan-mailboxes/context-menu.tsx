45b074c3dc644402dc67b11273adb3251b65257c
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-12-20 18:22:55 +0100

    Domain management: Email: Create context menu component

---
+++ client/my-sites/email/email-management/home/email-plan-mailboxes/context-menu.tsx
@@ -0,0 +1,52 @@
+import { Icon, desktop, mobile, cloudUpload, payment, settings, login } from '@wordpress/icons';
+import { useTranslate } from 'i18n-calypso';
+import EllipsisMenu from 'calypso/components/ellipsis-menu';
+import PopoverMenuItem from 'calypso/components/popover-menu/item';
+import './context-menu.scss';
+
+type Props = {
+	className?: string;
+};
+export default function ContextMenu( { className }: Props ) {
+	const translate = useTranslate();
+
+	const options = [
+		{
+			icon: <Icon icon={ desktop } />,
+			label: translate( 'Configure desktop app' ),
+		},
+		{
+			icon: <Icon icon={ mobile } />,
+			label: translate( 'Get mobile app' ),
+		},
+		{
+			icon: <Icon icon={ cloudUpload } />,
+			label: translate( 'Import email data' ),
+		},
+		{
+			icon: <Icon icon={ settings } />,
+			label: translate( 'Configure catch-all email' ),
+		},
+		{
+			icon: <Icon icon={ login } />,
+			label: translate( 'Set up internal forwarding' ),
+		},
+		{
+			icon: <Icon icon={ payment } />,
+			label: translate( 'View billing and payments' ),
+		},
+	];
+
+	return (
+		<EllipsisMenu
+			className={ className }
+			popoverClassName="email-plan-mailboxes-list__mailbox-context-menu-popover"
+		>
+			{ options.map( ( option, key ) => (
+				<PopoverMenuItem key={ key } { ...option }>
+					{ option.label }
+				</PopoverMenuItem>
+			) ) }
+		</EllipsisMenu>
+	);
+}
