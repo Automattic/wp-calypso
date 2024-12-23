c8dc694e3acab063c026c6c7c6bb3a66b26dd42d
Allison Levine <allison.levine@automattic.com>
2024-12-23 10:35:39 -0500

    Add placeholder text to FormTokenField.

--- packages/subscriber/src/components/add-form/categories-section.tsx
+++ packages/subscriber/src/components/add-form/categories-section.tsx
@@ -125,6 +125,7 @@ export const CategoriesSection: React.FC< Props > = ( {
 					suggestions={ newsletterCategories.map( ( cat ) => cat.name ) }
 					onChange={ handleCategoryChange }
 					label=""
+					placeholder={ __( 'Add categories' ) }
 				/>
 			) }
 		</div>
