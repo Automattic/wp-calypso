13da92b1cefcfd445f2fb8a1ce9bf5c8b6d7d209
Candy Tsai <candy02058912@gmail.com>
2024-12-24 14:41:27 +0800

    Use a more flexible locale check

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/hooks/use-is-updated-badge-design.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/hooks/use-is-updated-badge-design.tsx
@@ -1,16 +1,11 @@
-import { useLocale } from '@automattic/i18n-utils';
+import { useHasEnTranslation } from '@automattic/i18n-utils';
 import { useMemo } from 'react';
-import { useGoalsFirstExperiment } from 'calypso/landing/stepper/declarative-flow/helpers/use-goals-first-experiment';
 import { getStepFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url';
 
 const useIsUpdatedBadgeDesign = () => {
-	// TODO: Remove the locale and isGoalsAtFrontExperiment check after translations are complete.
-	const [ , isGoalsAtFrontExperiment ] = useGoalsFirstExperiment();
-	const locale = useLocale();
-	const isEligible = isGoalsAtFrontExperiment || locale === 'en';
-
 	const step = useMemo( () => getStepFromURL(), [] );
-
+	const hasEnTranslation = useHasEnTranslation();
+	const isEligible = hasEnTranslation( '%(planName)s + %(price)s/mo' );
 	return isEligible ? step?.toLowerCase() === 'designsetup' : false;
 };
 
