179021251a9884cb1f4276c933a3c0b1042c780d
Candy Tsai <candy02058912@gmail.com>
2024-12-24 16:37:23 +0800

    Add TODO comment for translations

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/hooks/use-is-updated-badge-design.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/hooks/use-is-updated-badge-design.tsx
@@ -4,6 +4,7 @@ import { getStepFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url'
 
 const useIsUpdatedBadgeDesign = () => {
 	const step = useMemo( () => getStepFromURL(), [] );
+	// TODO: Remove this after translations are completed
 	const hasEnTranslation = useHasEnTranslation();
 	const isEligible = hasEnTranslation( '%(planName)s + %(price)s/mo' );
 	return isEligible ? step?.toLowerCase() === 'designsetup' : false;
