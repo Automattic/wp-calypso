d6a81840d2c3276c634c25da0ddf491dea666d69
arthur <arthur.chu@automattic.com>
2024-12-24 17:05:15 +0800

    Fix types

--- client/landing/stepper/declarative-flow/internals/steps-repository/difm-starting-point/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/difm-starting-point/index.tsx
@@ -7,14 +7,15 @@ import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import DIFMLanding from 'calypso/my-sites/marketing/do-it-for-me/difm-landing';
 import { getCurrentUserSiteCount } from 'calypso/state/current-user/selectors';
 import type { Step } from '../../types';
+import type { AppState } from 'calypso/types';
 
 const STEP_NAME = 'difmStartingPoint';
 const DIFMStartingPoint: Step = function ( { navigation } ) {
 	const { goNext, goBack, submit } = navigation;
 	const translate = useTranslate();
-	const existingSiteCount = useSelector( ( state ) => getCurrentUserSiteCount( state ) );
+	const existingSiteCount = useSelector( ( state: AppState ) => getCurrentUserSiteCount( state ) );
 	const siteId = useSite()?.ID;
-	const showNewOrExistingSiteChoice = ! siteId && existingSiteCount > 0;
+	const showNewOrExistingSiteChoice = ! siteId && !! existingSiteCount && existingSiteCount > 0;
 
 	const onSubmit = ( value: string ) => {
 		submit?.( {
