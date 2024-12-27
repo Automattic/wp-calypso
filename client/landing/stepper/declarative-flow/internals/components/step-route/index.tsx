8d49f4992b6d1281bb54d0bbbe963a1142ea6d70
arthur791004 <arthur.chu@automattic.com>
2024-12-27 17:02:11 +0800

    Stepper: Fix the previous step of the user step may be wrong (#97775)
    * Stepper: Fix the previous step of the user step may be wrong
    
    * Fix types
    
    * Avoid showing the back button if the first step requires auth

--- client/landing/stepper/declarative-flow/internals/components/step-route/index.tsx
+++ client/landing/stepper/declarative-flow/internals/components/step-route/index.tsx
@@ -1,14 +1,18 @@
+import { useSelect } from '@wordpress/data';
 import clsx from 'clsx';
 import { useEffect } from 'react';
 import { useLoginUrlForFlow } from 'calypso/landing/stepper/hooks/use-login-url-for-flow';
+import { STEPPER_INTERNAL_STORE } from 'calypso/landing/stepper/stores';
 import kebabCase from 'calypso/landing/stepper/utils/kebabCase';
 import { StepperPerformanceTrackerStop } from 'calypso/landing/stepper/utils/performance-tracking';
 import SignupHeader from 'calypso/signup/signup-header';
 import { useSelector } from 'calypso/state';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { STEPS } from '../../steps';
 import SurveyManager from '../survery-manager';
 import { useStepRouteTracking } from './hooks/use-step-route-tracking';
 import type { Flow, Navigate, StepperStep } from '../../types';
+import type { StepperInternalSelect } from '@automattic/data-stores';
 
 type StepRouteProps = {
 	step: StepperStep;
@@ -22,6 +26,11 @@ type StepRouteProps = {
 const StepRoute = ( { step, flow, showWooLogo, renderStep, navigate }: StepRouteProps ) => {
 	const userIsLoggedIn = useSelector( isUserLoggedIn );
 	const stepContent = renderStep( step );
+	const stepData = useSelect(
+		( select ) => ( select( STEPPER_INTERNAL_STORE ) as StepperInternalSelect ).getStepData(),
+		[]
+	);
+
 	const loginUrl = useLoginUrlForFlow( { flow } );
 	const shouldAuthUser = step.requiresLoggedInUser && ! userIsLoggedIn;
 	const shouldSkipRender = shouldAuthUser || ! stepContent;
@@ -41,7 +50,13 @@ const StepRoute = ( { step, flow, showWooLogo, renderStep, navigate }: StepRoute
 	}, [ loginUrl, shouldAuthUser, useBuiltItInAuth ] );
 
 	if ( useBuiltItInAuth && shouldAuthUser && ! userIsLoggedIn ) {
-		navigate( 'user', undefined, true );
+		// If the current step requires the auth, it should become a next step after the auth.
+		const extraData = {
+			previousStep: stepData?.previousStep,
+			nextStep: step.slug,
+		};
+
+		navigate( STEPS.USER.slug, extraData, true );
 		return null;
 	}
 
--- client/landing/stepper/declarative-flow/internals/index.tsx
+++ client/landing/stepper/declarative-flow/internals/index.tsx
@@ -23,6 +23,7 @@ import { useFlowAnalytics } from './hooks/use-flow-analytics';
 import { useFlowNavigation } from './hooks/use-flow-navigation';
 import { useSignUpStartTracking } from './hooks/use-sign-up-start-tracking';
 import { useStepNavigationWithTracking } from './hooks/use-step-navigation-with-tracking';
+import { STEPS } from './steps';
 import { AssertConditionState, type Flow, type StepperStep, type StepProps } from './types';
 import type { StepperInternalSelect } from '@automattic/data-stores';
 import './global.scss';
@@ -160,34 +161,32 @@ export const FlowRenderer: React.FC< { flow: Flow } > = ( { flow } ) => {
 			return null;
 		}
 
-		const firstAuthWalledStep = flowSteps.find( ( step ) => step.requiresLoggedInUser );
-
-		if ( step.slug === 'user' && firstAuthWalledStep ) {
+		// The `nextStep` is available only when logged-out users go to the step that requires auth
+		// and are redirected to the user step.
+		const nextStepSlug = stepData?.nextStep ?? '';
+		if ( step.slug === STEPS.USER.slug && nextStepSlug ) {
+			const previousStepSlug = stepData?.previousStep;
 			const postAuthStepPath = generatePath( '/setup/:flow/:step/:lang?', {
 				flow: flow.name,
-				step: firstAuthWalledStep.slug,
+				step: nextStepSlug,
 				lang: lang === 'en' || isLoggedIn ? null : lang,
 			} );
+
 			const signupUrl = generatePath( '/setup/:flow/:step/:lang?', {
 				flow: flow.name,
 				step: 'user',
 				lang: lang === 'en' || isLoggedIn ? null : lang,
 			} );
 
-			const lastPreAuthWalledStepIndex =
-				flowSteps.findIndex( ( step ) => step.slug === 'user' ) - 1;
-			const lastPreAuthWalledStep =
-				lastPreAuthWalledStepIndex < 0 ? null : flowSteps[ lastPreAuthWalledStepIndex ];
-
 			return (
 				<StepComponent
 					navigation={ {
 						submit() {
-							navigate( firstAuthWalledStep.slug, undefined, true );
+							navigate( nextStepSlug, undefined, true );
 						},
-						...( lastPreAuthWalledStep && {
+						...( previousStepSlug && {
 							goBack() {
-								navigate( lastPreAuthWalledStep.slug, undefined, true );
+								navigate( previousStepSlug, undefined, true );
 							},
 						} ),
 					} }
