91ee5907d64ade31b8a80be3ecd91ce59facebde
Allison Levine <allison.levine@automattic.com>
2024-12-27 10:26:41 -0500

    Undo another svg path change.

--- apps/notifications/src/panel/templates/gridicons.tsx
+++ apps/notifications/src/panel/templates/gridicons.tsx
@@ -235,7 +235,7 @@ const Gridicons = forwardRef< SVGSVGElement, GridiconsProps >(
 					<svg { ...sharedProps } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<title>{ translate( 'Arrow Left' ) }</title>
 						<g>
-							<path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L13 16.17V4h-2z" />
+							<path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
 						</g>
 					</svg>
 				);
