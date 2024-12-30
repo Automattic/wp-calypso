a0761a4f7b5afa59d9acc88c9f55f8c8d5168fb8
Ben Zhu <ben.zhu@automattic.com>
2024-12-30 11:47:36 +1000

    Add OpenAI health check to Big Sky eligibility hook

--- client/landing/stepper/hooks/use-is-site-big-sky-eligible.ts
+++ client/landing/stepper/hooks/use-is-site-big-sky-eligible.ts
@@ -2,6 +2,7 @@ import config from '@automattic/calypso-config';
 import { isBusinessPlan, isPremiumPlan } from '@automattic/calypso-products';
 import { Onboard } from '@automattic/data-stores';
 import { useSelect } from '@wordpress/data';
+import { useEffect, useState } from 'react';
 import userAgent from 'calypso/lib/user-agent';
 import { useIsSiteOwner } from '../hooks/use-is-site-owner';
 import { ONBOARD_STORE } from '../stores';
@@ -21,6 +22,31 @@ const invalidGoals = [
 	SiteGoal.SellDigital,
 ];
 
+function useOpenAIHealth() {
+	const [ isOpenAIDown, setIsOpenAIDown ] = useState( false );
+	const [ isLoading, setIsLoading ] = useState( true );
+
+	useEffect( () => {
+		const checkHealth = async () => {
+			try {
+				const response = await fetch(
+					'https://public-api.wordpress.com/wpcom/v2/openai-proxy/v1/healthcheck'
+				);
+				const data = await response.json();
+				setIsOpenAIDown( data.status === 'KO' );
+			} catch ( error ) {
+				setIsOpenAIDown( false );
+			} finally {
+				setIsLoading( false );
+			}
+		};
+
+		checkHealth();
+	}, [] );
+
+	return { isOpenAIDown, isLoading };
+}
+
 export function useIsBigSkyEligible() {
 	const { isOwner } = useIsSiteOwner();
 	const site = useSite();
@@ -30,6 +56,7 @@ export function useIsBigSkyEligible() {
 		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getGoals(),
 		[ site ]
 	);
+	const { isOpenAIDown, isLoading: isOpenAIStatusLoading } = useOpenAIHealth();
 
 	const isEligibleGoals = isGoalsBigSkyEligible( goals );
 	const isEligiblePlan = isPremiumPlan( product_slug ) || isBusinessPlan( product_slug );
@@ -38,7 +65,7 @@ export function useIsBigSkyEligible() {
 		( featureFlagEnabled && isOwner && isEligiblePlan && isEligibleGoals && onSupportedDevice ) ||
 		false;
 
-	return { isLoading: false, isEligible: eligibilityResult };
+	return { isLoading: isOpenAIStatusLoading, isEligible: eligibilityResult, isOpenAIDown };
 }
 
 export function isGoalsBigSkyEligible( goals: Onboard.SiteGoal[] ) {
