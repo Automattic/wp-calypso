73f2c3f7e66798e17eaf0307123269102c75787f
Manzoor Wani <manzoorwani.jk@gmail.com>
2024-12-30 21:35:11 -0800

    Fix Social E2E tests for Atomic sites (#97859)

--- packages/calypso-e2e/src/lib/utils/social-connections-manager.ts
+++ packages/calypso-e2e/src/lib/utils/social-connections-manager.ts
@@ -20,13 +20,13 @@ export class SocialConnectionsManager {
 	 */
 	get patterns() {
 		return {
-			CONNECTION_TESTS: new RegExp(
-				// The request that deals with connection test results
+			CONNECTION_TESTS_ON_SIMPLE: new RegExp(
+				// The request that deals with connection test results on Simple sites
 				`wpcom/v2/sites/${ this.siteId }/publicize/connection-test-results`
 			),
-			JP_CONNECTION_TESTS: new RegExp(
-				// The request that deals with connection test results via Jetpack
-				`jetpack/v4/publicize/connections\\?test_connections=1`
+			CONNECTION_TESTS_ON_ATOMIC: new RegExp(
+				// The request that deals with connection test results on Atomic sites
+				'wpcom/v2/publicize/connection-test-results'
 			),
 			GET_POST: new RegExp(
 				// The request that gets the post data in the editor
@@ -61,14 +61,18 @@ export class SocialConnectionsManager {
 	 * Whether the URL is the one we want to intercept
 	 */
 	isTargetUrl( url: URL ) {
-		const { GET_POST, POST_AUTOSAVES, CONNECTION_TESTS, JP_CONNECTION_TESTS } = this.patterns;
+		const { GET_POST, POST_AUTOSAVES, CONNECTION_TESTS_ON_SIMPLE, CONNECTION_TESTS_ON_ATOMIC } =
+			this.patterns;
 
 		// We don't want to intercept autosave requests.
 		if ( GET_POST.test( url.pathname ) && ! POST_AUTOSAVES.test( url.pathname ) ) {
 			return true;
 		}
 
-		return CONNECTION_TESTS.test( url.pathname ) || JP_CONNECTION_TESTS.test( url.toString() );
+		return (
+			CONNECTION_TESTS_ON_SIMPLE.test( url.pathname ) ||
+			CONNECTION_TESTS_ON_ATOMIC.test( url.toString() )
+		);
 	}
 
 	/**
@@ -79,44 +83,21 @@ export class SocialConnectionsManager {
 			// Fetch original response.
 			const response = await route.fetch();
 
-			let result = await response.json();
+			const result = await response.json();
 
 			const url = route.request().url();
 
 			if ( this.patterns.GET_POST.test( url ) ) {
 				// For posts, add a test connection to post attributes.
 				result.body.jetpack_publicize_connections.push( ...this.testConnections );
-			} else if ( this.patterns.CONNECTION_TESTS.test( url ) ) {
+			} else if ( this.patterns.CONNECTION_TESTS_ON_SIMPLE.test( url ) ) {
 				// For connection tests, add test connections to the body.
+				// Because the response is an object {"body":[],"status":200,"headers":{}}
 				result.body.push( ...this.testConnections );
-			} else if ( this.patterns.JP_CONNECTION_TESTS.test( url ) ) {
-				// For JP connection tests, add test connections to the result.
-				if ( Array.isArray( result ) ) {
-					result.push( ...this.testConnections );
-				} else {
-					/**
-					 * It's possible that there are other connections added while these tests are running.
-					 * So we need to merge them.
-					 *
-					 * Since useAdminUiV1 flag is not activated on Atomic sites,
-					 * the result is still in weird format, thus we need to convert to
-					 *	{
-					 *		"tumblr": {
-					 *			"25481710": {
-					 *				"display_name": "Untitled",
-					 *				...
-					 *			}
-					 *		}
-					 *	}
-					 */
-					result = this.testConnections.reduce( ( acc, cnxn ) => {
-						acc[ cnxn.service_name ] = acc[ cnxn.service_name ] || {};
-
-						acc[ cnxn.service_name ][ cnxn.connection_id ] = cnxn;
-
-						return acc;
-					}, result );
-				}
+			} else if ( this.patterns.CONNECTION_TESTS_ON_ATOMIC.test( url ) ) {
+				// For Atomic connection tests, add test connections directly to the result.
+				// because the response is already an array.
+				result.push( ...this.testConnections );
 			}
 
 			await route.fulfill( {
@@ -139,8 +120,8 @@ export class SocialConnectionsManager {
 	async waitForConnectionTests() {
 		await this.page.waitForResponse( ( response ) => {
 			return (
-				this.patterns.CONNECTION_TESTS.test( response.url() ) ||
-				this.patterns.JP_CONNECTION_TESTS.test( response.url() )
+				this.patterns.CONNECTION_TESTS_ON_SIMPLE.test( response.url() ) ||
+				this.patterns.CONNECTION_TESTS_ON_ATOMIC.test( response.url() )
 			);
 		} );
 	}
