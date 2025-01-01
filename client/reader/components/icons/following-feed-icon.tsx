129819d1c2d1e55f8b804885dee339afeaf105c4
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-01-01 15:04:31 +0500

    💫 UPDATE: ReaderFollowingFeedIcon

---
+++ client/reader/components/icons/following-feed-icon.tsx
@@ -0,0 +1,21 @@
+import { Icon, published } from '@wordpress/icons';
+
+interface ReaderFollowingFeedIconProps {
+	iconSize: number;
+}
+
+export default function ReaderFollowingFeedIcon(
+	props: ReaderFollowingFeedIconProps
+): JSX.Element {
+	const { iconSize } = props;
+
+	return (
+		<Icon
+			key="following"
+			className="reader-following-feed"
+			icon={ published }
+			size={ iconSize }
+			stroke="#00c92f"
+		/>
+	);
+}
