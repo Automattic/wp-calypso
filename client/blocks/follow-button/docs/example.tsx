d858561416e5320751d8241520aae235b24a3c99
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-01-03 13:03:44 +0500

    REFACTOR: Update FollowButtonExample to use TypeScript

---
+++ client/blocks/follow-button/docs/example.tsx
@@ -0,0 +1,22 @@
+import { CompactCard as Card } from '@automattic/components';
+import FollowButton from 'calypso/blocks/follow-button/button';
+
+export default function FollowButtonExample(): JSX.Element {
+	return (
+		<div>
+			<Card compact>
+				<FollowButton following={ false } />
+			</Card>
+			<Card compact>
+				<FollowButton following />
+			</Card>
+			<Card compact>
+				<FollowButton disabled />
+			</Card>
+			<Card compact>
+				<h3>With custom label</h3>
+				<FollowButton followLabel="Follow Tag" />
+			</Card>
+		</div>
+	);
+}
