5c24c134ccce82e974b1a6afbabb29381232cb80
Anthony Grullon <agrullon95@gmail.com>
2025-01-06 13:32:26 +0700

    Enhance BackButton accessibility: add keyboard support and improve ARIA attributes

--- packages/help-center/src/components/back-button.tsx
+++ packages/help-center/src/components/back-button.tsx
@@ -1,4 +1,6 @@
 import { Icon, chevronLeft } from '@wordpress/icons';
+import { useI18n } from '@wordpress/react-i18n';
+import { useEffect, useRef } from 'react';
 import { useNavigate, useSearchParams, useLocation } from 'react-router-dom';
 
 import './back-button.scss';
@@ -7,6 +9,8 @@ export const BackButton = () => {
 	const navigate = useNavigate();
 	const [ searchParams ] = useSearchParams();
 	const { pathname } = useLocation();
+	const backButtonRef = useRef< HTMLElement >( null );
+	const { __ } = useI18n();
 
 	function handleClick() {
 		if ( pathname === '/success' ) {
@@ -18,9 +22,32 @@ export const BackButton = () => {
 		}
 	}
 
+	useEffect( () => {
+		const nodeRef = backButtonRef;
+		const onBackButtonKeyDown = ( e: KeyboardEvent ) => {
+			if ( e.key === 'Enter' ) {
+				handleClick();
+			}
+		};
+
+		if ( nodeRef.current ) {
+			nodeRef.current?.addEventListener( 'keydown', onBackButtonKeyDown );
+			// Fixes accessibility for back button
+			nodeRef.current?.removeAttribute( 'aria-hidden' );
+		}
+		return () => {
+			nodeRef.current?.removeEventListener( 'keydown', onBackButtonKeyDown );
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [] );
+
 	return (
 		<span className="back-button__help-center">
 			<Icon
+				aria-label={ __( 'Go Back', __i18n_text_domain__ ) }
+				role="button"
+				ref={ backButtonRef }
+				tabIndex={ 0 }
 				data-testid="back-button-icon"
 				onClick={ handleClick }
 				icon={ chevronLeft }
