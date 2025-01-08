0322ca2b0e0a3a730ae9904fd7903fa2d35e1090
Vassilis Dimitrakis <vassilis.dimitrakis@automattic.com>
2025-01-08 17:52:58 +0200

    fixes the outperformed percentages when in evergreen

--- client/my-sites/promote-post-i2/components/campaign-item-details/index.tsx
+++ client/my-sites/promote-post-i2/components/campaign-item-details/index.tsx
@@ -189,6 +189,16 @@ export default function CampaignItemDetails( props: Props ) {
 			const [ minValue, maxValue ] = tempValues.map( Number );
 			median = ( minValue + maxValue ) / 2;
 		}
+		if ( is_evergreen ) {
+			// Calculate the duration in weeks from the start date.
+			// Add 1 to ensure the minimum value is 1 week (avoiding division by zero or values less than 1).
+			// This ensures clicks are divided by the total whole number of weeks that have elapsed since the start date.
+			const durationInWeeksFromStartDate =
+				moment.utc().diff( moment.utc( start_date ), 'weeks' ) + 1;
+			// The total is divided by the number of weeks,
+			// as the estimated values are calculated on a weekly basis.
+			total = Math.round( total / durationInWeeksFromStartDate );
+		}
 		if ( total > median && median > 0 ) {
 			return Math.round( ( ( total - median ) / median ) * 100 );
 		}
