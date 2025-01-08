e43d48c443b70cda2bec43d43f47af0f6acb4c3b
Omar Alshaker <omar@omaralshaker.com>
2025-01-08 13:57:49 +0100

    Fix navigating to a gated step

--- client/landing/stepper/declarative-flow/internals/hooks/use-flow-navigation/index.tsx
+++ client/landing/stepper/declarative-flow/internals/hooks/use-flow-navigation/index.tsx
@@ -4,7 +4,10 @@ import { useCallback } from 'react';
 import { generatePath, useMatch, useNavigate } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
 import { ONBOARD_STORE, STEPPER_INTERNAL_STORE } from 'calypso/landing/stepper/stores';
-import type { Navigate, StepperStep } from '../../types';
+import { useSelector } from 'calypso/state';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { PRIVATE_STEPS } from '../../steps';
+import type { Flow, Navigate, StepperStep } from '../../types';
 
 const useOnboardingIntent = () => {
 	const intent = useSelect(
@@ -33,16 +36,37 @@ interface FlowNavigation {
 /**
  *  Hook to manage the navigation between steps in the flow
  */
-export const useFlowNavigation = (): FlowNavigation => {
+export const useFlowNavigation = ( flow: Flow ): FlowNavigation => {
 	const intent = useOnboardingIntent();
 	const { setStepData } = useDispatch( STEPPER_INTERNAL_STORE );
 	const navigate = useNavigate();
 	const match = useMatch( '/:flow/:step?/:lang?' );
-	const { flow = null, step: currentStepSlug = null, lang = null } = match?.params || {};
+	const { step: currentStepSlug = null, lang = null } = match?.params || {};
 	const [ currentSearchParams ] = useSearchParams();
+	const steps = flow.useSteps();
+	const isLoggedIn = useSelector( isUserLoggedIn );
+	const stepsSlugs = steps.map( ( step ) => step.slug );
 
 	const customNavigate = useCallback< Navigate< StepperStep[] > >(
 		( nextStep: string, extraData = {}, replace = false ) => {
+			// If the user is not logged in, and the next step requires a logged in user, redirect to the login step.
+			if (
+				! isLoggedIn &&
+				steps.find( ( step ) => step.slug === nextStep )?.requiresLoggedInUser
+			) {
+				setStepData( {
+					intent: intent,
+					previousStep: currentStepSlug,
+					nextStep,
+				} );
+				const signInPath = generatePath( `/:flow/:step/:lang?`, {
+					flow: flow.name,
+					lang,
+					step: PRIVATE_STEPS.USER.slug,
+				} );
+
+				return navigate( signInPath, { replace: true } );
+			}
 			const hasQueryParams = nextStep.includes( '?' );
 
 			// Get the latest search params from the current location
@@ -56,20 +80,21 @@ export const useFlowNavigation = (): FlowNavigation => {
 			} );
 
 			const newPath = generatePath( `/:flow/:step/:lang?`, {
-				flow,
+				flow: flow.name,
 				lang,
 				step: nextStep,
 			} );
 
 			navigate( addQueryParams( newPath, queryParams ), { replace } );
 		},
-		[ flow, intent, lang, navigate, setStepData, currentStepSlug ]
+		// eslint-disable-next-line react-hooks/exhaustive-deps -- steps array is recreated on every render, use stepsSlugs instead.
+		[ isLoggedIn, stepsSlugs, flow, intent, lang, navigate, setStepData, currentStepSlug ]
 	);
 
 	return {
 		navigate: customNavigate,
 		params: {
-			flow,
+			flow: flow.name,
 			step: currentStepSlug,
 		},
 		search: currentSearchParams,
