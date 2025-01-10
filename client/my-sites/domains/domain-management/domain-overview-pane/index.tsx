b5fc922ef8d66b4398d1221ac3f47474494f9c74
Imran Hossain <imranh920@gmail.com>
2025-01-10 19:04:50 +0600

    Fix rebase error

--- client/my-sites/domains/domain-management/domain-overview-pane/index.tsx
+++ client/my-sites/domains/domain-management/domain-overview-pane/index.tsx
@@ -167,18 +167,19 @@ const DomainOverviewPane = ( {
 				</div>
 			) }
 
-		<ItemView
-			itemData={ itemData }
-			closeItemView={ () => {
-				inSiteContext ? page.show( '/sites' ) : page.show( paths.domainManagementRoot() );
-			} }
-			features={ features }
-			enforceTabsView
-			itemViewHeaderExtraProps={ {
-				headerButtons: PreviewPaneHeaderButtons,
-			} }
-			hideHeader={ inSiteContext }
-		/>
+			<ItemView
+				itemData={ itemData }
+				closeItemView={ () => {
+					inSiteContext ? page.show( '/sites' ) : page.show( paths.domainManagementRoot() );
+				} }
+				features={ features }
+				enforceTabsView
+				itemViewHeaderExtraProps={ {
+					headerButtons: PreviewPaneHeaderButtons,
+				} }
+				hideHeader={ inSiteContext }
+			/>
+		</>
 	);
 };
 
