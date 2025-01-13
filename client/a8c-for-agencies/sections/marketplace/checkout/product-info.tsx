57705fe5e20efc849f37fb05c50f3b053aff8514
Yashwin Poojary <yashwinpoojary@gmail.com>
2025-01-13 22:13:44 +0530

    A4A > Checkout: Show Pressable limit warning (#98011)
    * Show Pressable limit warning on Checkout
    
    * show the message for client & agency non-refer mode

--- client/a8c-for-agencies/sections/marketplace/checkout/product-info.tsx
+++ client/a8c-for-agencies/sections/marketplace/checkout/product-info.tsx
@@ -7,7 +7,13 @@ import getProductIcon from 'calypso/my-sites/plans/jetpack-plans/product-store/u
 import getPressablePlan from '../pressable-overview/lib/get-pressable-plan';
 import type { ShoppingCartItem } from '../types';
 
-export default function ProductInfo( { product }: { product: ShoppingCartItem } ) {
+export default function ProductInfo( {
+	product,
+	isAutomatedReferrals,
+}: {
+	product: ShoppingCartItem;
+	isAutomatedReferrals?: boolean;
+} ) {
 	const translate = useTranslate();
 
 	const { title, product: productInfo } = useLicenseLightboxData( product );
@@ -109,6 +115,16 @@ export default function ProductInfo( { product }: { product: ShoppingCartItem } 
 					<span className="product-info__count">{ countInfo }</span>
 				</div>
 				<p className="product-info__description">{ productDescription }</p>
+				{
+					// Show pressable limit warning if the product is a Pressable plan and it's not a referral
+					product.family_slug === 'pressable-hosting' && ! isAutomatedReferrals && (
+						<div className="product-info__pressable-limit-warning">
+							{ translate(
+								"*If you exceed your plan's storage or traffic limits, you will be charged $0.50 per GB and $8 per 10K visits per month."
+							) }
+						</div>
+					)
+				}
 				{ product.licenseId && siteUrls && <p className="product-info__site-url">{ siteUrls }</p> }
 			</div>
 		</div>
