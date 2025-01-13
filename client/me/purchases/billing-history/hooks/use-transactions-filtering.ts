35885578862ee6c24fbfbef57dabad002cfa1f12
Bill Robbins <bill.robbins@automattic.com>
2025-01-13 16:32:56 -0600

    Refactor useTransactionsFiltering() to use top-level functions

--- client/me/purchases/billing-history/hooks/use-transactions-filtering.ts
+++ client/me/purchases/billing-history/hooks/use-transactions-filtering.ts
@@ -4,6 +4,46 @@ import { BillingTransaction } from 'calypso/state/billing-transactions/types';
 import { groupDomainProducts, formatDisplayDate, formatMonthYear } from '../utils';
 import type { ViewState, Filter } from '../data-views-types';
 
+function matchesSearch(
+	transaction: BillingTransaction,
+	searchTerm: string,
+	translate: ReturnType< typeof useTranslate >
+) {
+	const [ transactionItem ] = groupDomainProducts( transaction.items, translate );
+	const searchableFields = [
+		transaction.service,
+		transactionItem.product,
+		transactionItem.variation,
+		transactionItem.domain,
+		formatDisplayDate( new Date( transaction.date ) ),
+	];
+
+	return searchableFields.some(
+		( field ) => field && field.toString().toLowerCase().includes( searchTerm.toLowerCase() )
+	);
+}
+
+function matchesFilter(
+	transaction: BillingTransaction,
+	filter: Filter,
+	translate: ReturnType< typeof useTranslate >
+) {
+	if ( filter.field === 'service' && filter.value ) {
+		return transaction.service === filter.value;
+	}
+	if ( filter.field === 'type' && filter.value ) {
+		const [ firstItem ] = groupDomainProducts( transaction.items, translate );
+		return firstItem.type === filter.value;
+	}
+	if ( filter.field === 'date' && filter.value && typeof filter.value === 'string' ) {
+		const [ year, month ] = filter.value.split( '-' ).map( Number );
+		const filterDate = new Date( year, month - 1 );
+		const transactionDate = new Date( transaction.date );
+		return formatMonthYear( transactionDate ) === formatMonthYear( filterDate );
+	}
+	return true;
+}
+
 export function useTransactionsFiltering(
 	transactions: BillingTransaction[] | null,
 	view: ViewState
@@ -12,47 +52,15 @@ export function useTransactionsFiltering(
 
 	return useMemo( () => {
 		return ( transactions ?? [] ).filter( ( transaction ) => {
-			if ( view.search ) {
-				const searchTerm = view.search.toLowerCase();
-				const [ transactionItem ] = groupDomainProducts( transaction.items, translate );
-
-				const searchableFields = [
-					transaction.service,
-					transactionItem.product,
-					transactionItem.variation,
-					transactionItem.domain,
-					formatDisplayDate( new Date( transaction.date ) ),
-				];
-
-				if (
-					! searchableFields.some(
-						( field ) => field && field.toString().toLowerCase().includes( searchTerm )
-					)
-				) {
-					return false;
-				}
+			if ( view.search && ! matchesSearch( transaction, view.search, translate ) ) {
+				return false;
 			}
 
 			if ( view.filters.length === 0 ) {
 				return true;
 			}
 
-			return view.filters.every( ( filter: Filter ) => {
-				if ( filter.field === 'service' && filter.value ) {
-					return transaction.service === filter.value;
-				}
-				if ( filter.field === 'type' && filter.value ) {
-					const [ firstItem ] = groupDomainProducts( transaction.items, translate );
-					return firstItem.type === filter.value;
-				}
-				if ( filter.field === 'date' && filter.value && typeof filter.value === 'string' ) {
-					const [ year, month ] = filter.value.split( '-' ).map( Number );
-					const filterDate = new Date( year, month - 1 );
-					const transactionDate = new Date( transaction.date );
-					return formatMonthYear( transactionDate ) === formatMonthYear( filterDate );
-				}
-				return true;
-			} );
+			return view.filters.every( ( filter ) => matchesFilter( transaction, filter, translate ) );
 		} );
 	}, [ transactions, view.search, view.filters, translate ] );
 }
