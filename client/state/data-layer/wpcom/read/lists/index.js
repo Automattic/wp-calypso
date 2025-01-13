9e75aee0489cfe1e32096b43b324cf2b252f30c7
Ricardo Artemio Morales <artemio.morales@automattic.com>
2025-01-13 15:53:24 -0500

    Add clarifying comments

--- client/state/data-layer/wpcom/read/lists/index.js
+++ client/state/data-layer/wpcom/read/lists/index.js
@@ -138,6 +138,7 @@ registerHandlers( 'state/data-layer/wpcom/read/lists/index.js', {
 			],
 		} ),
 	],
+	// Request pulic and private lists for the current user
 	[ READER_LISTS_REQUEST ]: [
 		dispatchRequest( {
 			fetch: ( action ) =>
@@ -153,6 +154,7 @@ registerHandlers( 'state/data-layer/wpcom/read/lists/index.js', {
 			onError: () => noop,
 		} ),
 	],
+	// Request only public lists for a specific user
 	[ READER_USER_LISTS_REQUEST ]: [
 		dispatchRequest( {
 			fetch: ( action ) =>
