d969ac61792ff044ae2d10d2b82da06c12c85287
Emanuele Buccelli <escapemanuele@gmail.com>
2025-01-13 22:35:17 +0100

    Remove unnecessary spread operator
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- packages/help-center/src/components/help-center.tsx
+++ packages/help-center/src/components/help-center.tsx
@@ -90,7 +90,7 @@ export default function ContextualizedHelpCenter(
 	props: Container & HelpCenterRequiredInformation
 ) {
 	return (
-		<HelpCenterRequiredContextProvider value={ { ...props } }>
+		<HelpCenterRequiredContextProvider value={ props }>
 			<HelpCenter { ...props } />
 		</HelpCenterRequiredContextProvider>
 	);
