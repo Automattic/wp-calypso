60987aa972ceb2c32dc7a261c42043cecfde36eb
Paulo Cruz <paulo.cruz@automattic.com>
2025-01-14 15:57:19 -0300

    Domains Management Revamp: Show the Add Forward Email subpage in site context (#98294)
    * Include Add Email Forwarding subpage to the site context
    
    * Update paths
    
    * Update tests
    
    * Fix SubpageWrapper tests
    
    * Update EmailPathUtilityFunction
    
    * Fix typo
    
    * Fix site and domain order on breadcrumb path

--- client/my-sites/domains/domain-management/subpage-wrapper/test/index.test.tsx
+++ client/my-sites/domains/domain-management/subpage-wrapper/test/index.test.tsx
@@ -3,14 +3,42 @@
  */
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import SubpageWrapper from '../index';
 import { ADD_FORWARDING_EMAIL, ADD_DNS_RECORD, EDIT_DNS_RECORD } from '../subpages';
 
 jest.mock( 'component-file-picker', () => () => <div>File Picker</div> );
 
+const initialState = {
+	sites: {
+		items: {
+			1: {
+				ID: 1,
+				URL: 'example.wordpress.com',
+				plan: {
+					product_slug: 'free_plan',
+				},
+				options: {
+					is_wpforteams_site: false,
+				},
+			},
+		},
+	},
+	ui: {
+		selectedSiteId: 1,
+	},
+};
+
+const mockStore = configureStore();
+const store = mockStore( initialState );
+
+const wrapper = ( { children } ) => <Provider store={ store }>{ children }</Provider>;
+const renderWithWrapper = ( component ) => render( component, { wrapper } );
+
 describe( 'SubpageWrapper', () => {
 	it( 'should render the children', () => {
-		render(
+		renderWithWrapper(
 			<SubpageWrapper
 				subpageKey={ ADD_FORWARDING_EMAIL }
 				siteName="site.com"
@@ -24,7 +52,7 @@ describe( 'SubpageWrapper', () => {
 	} );
 
 	it( 'should render the children with the subpage header', () => {
-		render(
+		renderWithWrapper(
 			<SubpageWrapper
 				subpageKey={ ADD_FORWARDING_EMAIL }
 				siteName="site.com"
@@ -43,7 +71,7 @@ describe( 'SubpageWrapper', () => {
 	} );
 
 	it( 'should render the children without the subpage header', () => {
-		render(
+		renderWithWrapper(
 			<SubpageWrapper subpageKey="non-existent" siteName="site.com" domainName="domain.com">
 				<span>Hello</span>
 			</SubpageWrapper>
@@ -53,7 +81,7 @@ describe( 'SubpageWrapper', () => {
 	} );
 
 	it( 'should render Add DNS subpage breadcrumbs', () => {
-		const { container } = render(
+		const { container } = renderWithWrapper(
 			<SubpageWrapper subpageKey={ ADD_DNS_RECORD } siteName="site.com" domainName="domain.com">
 				<span>Hello</span>
 			</SubpageWrapper>
@@ -68,7 +96,7 @@ describe( 'SubpageWrapper', () => {
 	} );
 
 	it( 'should render Edit DNS subpage breadcrumbs', () => {
-		const { container } = render(
+		const { container } = renderWithWrapper(
 			<SubpageWrapper subpageKey={ EDIT_DNS_RECORD } siteName="site.com" domainName="domain.com">
 				<span>Hello</span>
 			</SubpageWrapper>
