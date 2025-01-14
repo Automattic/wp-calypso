b34e43272df98ea265edcfdfe5994c9c22d64a28
Bill Robbins <bill.robbins@automattic.com>
2025-01-14 13:15:57 -0600

    Convert arrow functions in utils.tsx

--- client/me/purchases/billing-history/utils.tsx
+++ client/me/purchases/billing-history/utils.tsx
@@ -343,23 +343,23 @@ export function getTransactionTermLabel(
 	}
 }
 
-export const formatDisplayDate = ( date: Date ): string => {
+export function formatDisplayDate( date: Date ): string {
 	return date.toLocaleDateString( undefined, {
 		year: 'numeric',
 		month: 'short',
 		day: 'numeric',
 	} );
-};
+}
 
-export const formatMonthYearLabel = ( date: Date ): string => {
+export function formatMonthYearLabel( date: Date ): string {
 	return date.toLocaleDateString( undefined, {
 		year: 'numeric',
 		month: 'long',
 	} );
-};
+}
 
-export const formatMonthYear = ( date: Date ): string => {
+export function formatMonthYear( date: Date ): string {
 	const year = date.getFullYear();
 	const month = String( date.getMonth() + 1 ).padStart( 2, '0' );
 	return `${ year }-${ month }`;
-};
+}
