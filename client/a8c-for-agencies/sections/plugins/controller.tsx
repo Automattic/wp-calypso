57f79c7c5dd1414f3d1a665ffa393c8f5d876a69
Tiago Noronha <hello@tiagonoronha.com>
2025-01-15 12:02:32 +0000

    Add plugins management to A4A dashboard

--- client/a8c-for-agencies/sections/plugins/controller.tsx
+++ client/a8c-for-agencies/sections/plugins/controller.tsx
@@ -1,29 +1,34 @@
+import page from '@automattic/calypso-router';
 import { type Callback } from '@automattic/calypso-router';
-import { LayoutWithGuidedTour as Layout } from 'calypso/a8c-for-agencies/components/layout/layout-with-guided-tour';
-import LayoutTop from 'calypso/a8c-for-agencies/components/layout/layout-with-payment-notification';
-import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
-import LayoutBody from 'calypso/layout/hosting-dashboard/body';
-import LayoutHeader, {
-	LayoutHeaderSubtitle as Subtitle,
-	LayoutHeaderTitle as Title,
-} from 'calypso/layout/hosting-dashboard/header';
+import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { A4A_PLUGINS_LINK } from '../../components/sidebar-menu/lib/constants';
 import MainSidebar from '../../components/sidebar-menu/main';
 
-export const pluginsContext: Callback = ( context, next ) => {
+export const pluginsContext: Callback = ( context ) => {
+	const { slug } = context.params;
+	const redirectPath = slug
+		? `${ A4A_PLUGINS_LINK }/manage/sites/${ slug }`
+		: `${ A4A_PLUGINS_LINK }/manage/sites`;
+
+	page.redirect( redirectPath );
+};
+
+export const pluginManagementContext: Callback = ( context, next ) => {
 	context.secondary = <MainSidebar path={ context.path } />;
 	context.primary = (
-		<Layout title="Plugins" wide sidebarNavigation={ <MobileSidebarNavigation /> }>
-			<LayoutTop>
-				<LayoutHeader>
-					<Title>Plugins</Title>
-					<Subtitle>plugins of your agency</Subtitle>
-				</LayoutHeader>
-			</LayoutTop>
-			<LayoutBody>
-				<div>test</div>
-			</LayoutBody>
-		</Layout>
+		<>
+			<PageViewTracker title="Plugins" path={ context.path } />
+		</>
 	);
+	next();
+};
 
+export const pluginDetailsContext: Callback = ( context, next ) => {
+	context.secondary = <MainSidebar path={ context.path } />;
+	context.primary = (
+		<>
+			<PageViewTracker title="Plugins" path={ context.path } />
+		</>
+	);
 	next();
 };
