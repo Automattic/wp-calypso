cb32f8d88534e491853c4bd05fda0d1a236a4b8a
vykes-mac <knrymcleish@gmail.com>
2025-01-15 19:08:09 -0500

    capture list of goals and is_goals_first flag in the build flow for launchpad step

--- client/landing/stepper/declarative-flow/build.ts
+++ client/landing/stepper/declarative-flow/build.ts
@@ -1,10 +1,16 @@
+import { OnboardSelect } from '@automattic/data-stores';
 import { BUILD_FLOW } from '@automattic/onboarding';
+import { useSelect } from '@wordpress/data';
 import { addQueryArgs } from '@wordpress/url';
+import { useMemo, useRef } from 'react';
 import { skipLaunchpad } from 'calypso/landing/stepper/utils/skip-launchpad';
 import { triggerGuidesForStep } from 'calypso/lib/guides/trigger-guides-for-step';
+import { STEPPER_TRACKS_EVENT_SIGNUP_STEP_START } from '../constants';
 import { useExitFlow } from '../hooks/use-exit-flow';
 import { useSiteIdParam } from '../hooks/use-site-id-param';
 import { useSiteSlug } from '../hooks/use-site-slug';
+import { ONBOARD_STORE } from '../stores';
+import { useGoalsFirstExperiment } from './helpers/use-goals-first-experiment';
 import { useLaunchpadDecider } from './internals/hooks/use-launchpad-decider';
 import LaunchPad from './internals/steps-repository/launchpad';
 import Processing from './internals/steps-repository/processing-step';
@@ -21,6 +27,33 @@ const build: Flow = {
 			{ slug: 'launchpad', component: LaunchPad },
 			{ slug: 'processing', component: Processing },
 		];
+	},
+	useTracksEventProps() {
+		const [ isLoading, isGoalsAtFrontExperiment ] = useGoalsFirstExperiment();
+		const goals = useSelect(
+			( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getGoals(),
+			[]
+		);
+
+		// we are only interested in the initial values and not when they values change
+		const initialGoals = useRef( goals );
+
+		return useMemo(
+			() => ( {
+				isLoading,
+				eventsProperties: {
+					[ STEPPER_TRACKS_EVENT_SIGNUP_STEP_START ]: {
+						...( isGoalsAtFrontExperiment && {
+							is_goals_first: 'true',
+						} ),
+						...( initialGoals.current.length && {
+							goals: initialGoals.current.join( ',' ),
+						} ),
+					},
+				},
+			} ),
+			[ isGoalsAtFrontExperiment, initialGoals, isLoading ]
+		);
 	},
 
 	useStepNavigation( _currentStep, navigate ) {
