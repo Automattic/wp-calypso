b14d3d71709919f01c7c1fa2b36ef035fcd512af
vykes-mac <knrymcleish@gmail.com>
2025-01-15 19:09:24 -0500

    skip clearing goals from onboarding store since we need to capture them in launchpad step

--- client/landing/stepper/declarative-flow/site-setup-flow.ts
+++ client/landing/stepper/declarative-flow/site-setup-flow.ts
@@ -247,7 +247,7 @@ const siteSetupFlow: Flow = {
 			navigate( 'processing' );
 
 			// Clean-up the store so that if onboard for new site will be launched it will be launched with no preselected values
-			resetOnboardStoreWithSkipFlags( [ 'skipPendingAction', 'skipIntent' ] );
+			resetOnboardStoreWithSkipFlags( [ 'skipPendingAction', 'skipIntent', 'skipGoals' ] );
 		};
 
 		const { getPostFlowUrl, initializeLaunchpadState } = useLaunchpadDecider( {
