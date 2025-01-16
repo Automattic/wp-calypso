feb07dcf721751b4a7be5e03a5c2e9a1564fac9e
Jarda Snajdr <jsnajdr@gmail.com>
2025-01-16 16:47:26 +0100

    Reader: remove unused code from reader/following (#98475)
    * Reader: remove unused code from reader/following
    
    * Remove more assets and styles

--- client/reader/follow-sources.ts
+++ client/reader/follow-sources.ts
@@ -3,6 +3,4 @@ export const IN_STREAM_RECOMMENDATION = 'in-stream-recommendation';
 export const SEARCH_RESULTS_SITES = 'search-results-sites';
 export const READER_POST_OPTIONS_MENU = 'reader-post-options-menu';
 export const READER_SUGGESTED_FOLLOWS_DIALOG = 'reader-suggested-follows-dialog';
-export const READER_RECENT_SIDEBAR_POPULAR_SITES = 'reader-recent-sidebar-popular-sites';
 export const READER_SEARCH_POPULAR_SITES = 'reader-search-popular-sites';
-export const READER_DISCOVER_POPULAR_SITES = 'reader-discover-popular-sites';
--- client/reader/following/index.js
+++ client/reader/following/index.js
@@ -1,10 +1,10 @@
 import page from '@automattic/calypso-router';
-import { makeLayout, render as clientRender } from 'calypso/controller';
-import { updateLastRoute, sidebar } from 'calypso/reader/controller';
-import { followingManage } from './controller';
 
 export default function () {
-	page( '/following/manage', updateLastRoute, sidebar, followingManage, makeLayout, clientRender );
+	page( '/following/manage', ( { querystring } ) => {
+		page.redirect( '/read/subscriptions' + ( querystring ? '?' + querystring : '' ) );
+	} );
+
 	page( '/following/edit*', '/following/manage' );
 
 	// Send /following to Reader root
