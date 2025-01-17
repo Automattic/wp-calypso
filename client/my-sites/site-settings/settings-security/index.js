f5e5cd9f221a22e2be2a29ebeb9e9ec04d5ea057
Louis Laugesen <louis.laugesen@gmail.com>
2025-01-17 12:24:42 +1100

    Redirect /settings/security/:site to Jetpack security settings

--- client/my-sites/site-settings/settings-security/index.js
+++ client/my-sites/site-settings/settings-security/index.js
@@ -1,13 +1,22 @@
 import page from '@automattic/calypso-router';
-import { makeLayout, render as clientRender } from 'calypso/controller';
+import {
+	makeLayout,
+	render as clientRender,
+	redirectIfDuplicatedView as _redirectIfDuplicatedView,
+} from 'calypso/controller';
 import { navigation, siteSelection } from 'calypso/my-sites/controller';
 import { setScroll, siteSettings } from 'calypso/my-sites/site-settings/settings-controller';
 import { security } from './controller';
+
+const redirectIfDuplicatedView = ( context, next ) => {
+	_redirectIfDuplicatedView( 'admin.php?page=jetpack#security' )( context, next );
+};
 
 export default function () {
 	page(
 		'/settings/security/:site_id',
 		siteSelection,
+		redirectIfDuplicatedView,
 		navigation,
 		setScroll,
 		siteSettings,
