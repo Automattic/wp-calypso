0560ad6276e213e4cfca5c34e1799898ca6ee0a1
Candy Tsai <candy02058912@gmail.com>
2025-01-17 20:22:17 +0800

    Create new launchpad only home structure (#98476)
    * Create new launchpad only home structure
    
    * Rename feature flag
    
    * Allow dismissing the full-screen Launchpad in My Home
    
    ---------
    
    Co-authored-by: Luis Felipe Zaguini <luisfelipezaguini@gmail.com>

--- client/my-sites/customer-home/index.js
+++ client/my-sites/customer-home/index.js
@@ -1,10 +1,18 @@
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import { navigation, siteSelection, sites } from 'calypso/my-sites/controller';
-import home, { maybeRedirect } from './controller';
+import renderHome, { maybeRedirect } from './controller';
 
 export default function () {
 	page( '/home', siteSelection, sites, makeLayout, clientRender );
 
-	page( '/home/:siteId', siteSelection, maybeRedirect, navigation, home, makeLayout, clientRender );
+	page(
+		'/home/:siteId',
+		siteSelection,
+		maybeRedirect,
+		navigation,
+		renderHome,
+		makeLayout,
+		clientRender
+	);
 }
