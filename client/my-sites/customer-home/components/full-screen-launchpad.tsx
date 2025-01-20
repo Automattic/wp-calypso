1c992f2f5425d2dffc3a833e83347ec5704ac5aa
Paulo Trentin <paulo@paulotrentin.com.br>
2025-01-20 19:21:05 -0300

    Created custom styles for the new full screen launchpad

--- client/my-sites/customer-home/components/full-screen-launchpad.tsx
+++ client/my-sites/customer-home/components/full-screen-launchpad.tsx
@@ -2,13 +2,15 @@ import { Button } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
 import LaunchpadPreLaunch from '../cards/launchpad/pre-launch';
 
+import './full-screen-launchpad.scss';
+
 export const FullScreenLaunchpad = ( { onClose }: { onClose: () => void } ) => {
 	const { __ } = useI18n();
 
 	return (
 		<div css={ { display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '24px' } }>
-			<div css={ { width: '100%' } }>
-				<LaunchpadPreLaunch />
+			<div className="is-launchpad-first" css={ { width: '100%' } }>
+				<LaunchpadPreLaunch highlightNextAction />
 			</div>
 			<Button onClick={ onClose }>{ __( 'Hide onboarding setup' ) }</Button>
 		</div>
