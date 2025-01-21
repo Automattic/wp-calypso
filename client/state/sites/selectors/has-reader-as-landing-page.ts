782c7aa1f5a51f7e36d5321772ad49c6685897d3
Eoin Gallagher <eoin@automattic.com>
2025-01-21 23:06:29 +0000

    tidy up some code

--- client/state/sites/selectors/has-reader-as-landing-page.ts
+++ client/state/sites/selectors/has-reader-as-landing-page.ts
@@ -4,9 +4,7 @@ import type { AppState } from 'calypso/types';
 export const READER_AS_LANDING_PAGE_PREFERENCE = 'reader-landing-page';
 
 export const hasReadersAsLandingPage = ( state: AppState ): boolean => {
-	const { useReaderAsLandingPage = false } = getPreference(
-		state,
-		READER_AS_LANDING_PAGE_PREFERENCE
-	);
+	const preference = getPreference( state, READER_AS_LANDING_PAGE_PREFERENCE ) || {};
+	const { useReaderAsLandingPage = false } = preference;
 	return !! useReaderAsLandingPage;
 };
--- client/state/sites/selectors/has-sites-as-landing-page.ts
+++ client/state/sites/selectors/has-sites-as-landing-page.ts
@@ -4,9 +4,7 @@ import type { AppState } from 'calypso/types';
 export const SITES_AS_LANDING_PAGE_PREFERENCE = 'sites-landing-page';
 
 export const hasSitesAsLandingPage = ( state: AppState ): boolean => {
-	const { useSitesAsLandingPage = false } = getPreference(
-		state,
-		SITES_AS_LANDING_PAGE_PREFERENCE
-	);
+	const preference = getPreference( state, SITES_AS_LANDING_PAGE_PREFERENCE ) || {};
+	const { useSitesAsLandingPage = false } = preference;
 	return !! useSitesAsLandingPage;
 };
