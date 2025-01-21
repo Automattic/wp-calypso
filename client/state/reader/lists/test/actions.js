cbf953a6b4ed4a4ff7f04a82c1f3cea89c78a727
Ricardo Artemio Morales <artemio.morales@automattic.com>
2025-01-21 13:44:30 -0500

    Update tests

--- client/state/reader/lists/test/actions.js
+++ client/state/reader/lists/test/actions.js
@@ -1,51 +1,51 @@
 import {
 	READER_LIST__DELETE,
-	READER_LIST__REQUEST,
-	READER_LISTS__RECEIVE,
-	READER_LISTS__REQUEST,
+	READER_LIST__REQUEST_TARGET_LIST,
+	READER_LIST__RECEIVE_CURRENT_USER_SUBSCRIBED_LISTS,
+	READER_LIST__REQUEST_CURRENT_USER_SUBSCRIBED_LISTS,
 	READER_LIST__FOLLOW,
 	READER_LIST__UNFOLLOW,
 } from 'calypso/state/reader/action-types';
 import {
 	deleteReaderList,
-	receiveLists,
-	requestList,
-	requestSubscribedLists,
+	receiveCurrentUserSubscribedLists,
+	requestTargetList,
+	requestCurrentUserSubscribedLists,
 	followList,
 	unfollowList,
 } from '../actions';
 
 describe( 'actions', () => {
-	describe( '#receiveLists()', () => {
+	describe( '#receiveCurrentUserSubscribedLists()', () => {
 		test( 'should return an action object', () => {
 			const lists = [ { ID: 841, title: 'Hello World', slug: 'hello-world' } ];
-			const action = receiveLists( lists );
+			const action = receiveCurrentUserSubscribedLists( lists );
 
 			expect( action ).toEqual( {
-				type: READER_LISTS__RECEIVE,
+				type: READER_LIST__RECEIVE_CURRENT_USER_SUBSCRIBED_LISTS,
 				lists,
 			} );
 		} );
 	} );
 
-	describe( '#requestList()', () => {
+	describe( '#requestTargetList()', () => {
 		test( 'should return an action object', () => {
-			const action = requestList( 'pob', 'things-i-like' );
+			const action = requestTargetList( 'pob', 'things-i-like' );
 
 			expect( action ).toEqual( {
-				type: READER_LIST__REQUEST,
+				type: READER_LIST__REQUEST_TARGET_LIST,
 				listOwner: 'pob',
 				listSlug: 'things-i-like',
 			} );
 		} );
 	} );
 
-	describe( '#requestSubscribedLists()', () => {
+	describe( '#requestCurrentUserSubscribedLists()', () => {
 		test( 'should return an action object', () => {
-			const action = requestSubscribedLists();
+			const action = requestCurrentUserSubscribedLists();
 
 			expect( action ).toEqual( {
-				type: READER_LISTS__REQUEST,
+				type: READER_LIST__REQUEST_CURRENT_USER_SUBSCRIBED_LISTS,
 			} );
 		} );
 	} );
