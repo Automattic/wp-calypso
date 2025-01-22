883de8dfa0472c3997da9f23f31cd730f6061e62
Caroline Moore <calobee@gmail.com>
2025-01-22 14:14:00 -0500

    Remove plugin files, fix retry on migration key endpoint

--- client/landing/stepper/hooks/use-prepare-site-for-migration.ts
+++ client/landing/stepper/hooks/use-prepare-site-for-migration.ts
@@ -92,7 +92,7 @@ export const usePrepareSiteForMigration = ( siteId?: number ) => {
 		fetchStatus: migrationKeyFetchStatus,
 	} = useSiteMigrationKey( siteId, {
 		enabled: Boolean( siteTransferState.completed ),
-		retry: false,
+		retry: true,
 	} );
 
 	const completed = siteTransferState.completed;
