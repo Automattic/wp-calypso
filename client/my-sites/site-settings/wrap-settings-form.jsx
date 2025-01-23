7bb636f169580feb7f4230c0c2e915cc53dc6bc9
Allison Levine <allison.levine@automattic.com>
2025-01-23 16:34:42 -0500

    Add back useEffect and change empty Jetpack welcome message logic.

--- client/my-sites/site-settings/wrap-settings-form.jsx
+++ client/my-sites/site-settings/wrap-settings-form.jsx
@@ -42,19 +42,6 @@ const FIELDS_TO_LAUNCHPAD_TASKS = {
 const debug = debugFactory( 'calypso:site-settings' );
 
 const wrapSettingsForm = ( getFormSettings ) => ( SettingsForm ) => {
-	const mergeSubscriptionOptions = ( baseOptions = {}, jetpackOptions = {} ) => {
-		if ( ! jetpackOptions ) {
-			return baseOptions;
-		}
-		return {
-			...baseOptions,
-			// Only include non-empty Jetpack values
-			...Object.fromEntries(
-				Object.entries( jetpackOptions ).filter( ( [ , value ] ) => value != null && value !== '' )
-			),
-		};
-	};
-
 	class WrappedSettingsForm extends Component {
 		state = {
 			uniqueEvents: {},
@@ -420,11 +407,9 @@ const wrapSettingsForm = ( getFormSettings ) => ( SettingsForm ) => {
 				const jetpackSettings = getJetpackSettings( state, siteId );
 				settings = {
 					...settings,
-					...jetpackSettings,
-					subscription_options: mergeSubscriptionOptions(
-						settings?.subscription_options,
-						jetpackSettings?.subscription_options
-					),
+					...( jetpackSettings?.subscription_options?.welcome === undefined
+						? omit( jetpackSettings, 'subscription_options' )
+						: jetpackSettings ),
 				};
 				settingsFields.jetpack = keys( jetpackSettings );
 				const fieldsToUpdate = /^error_/.test( fields.lang_id )
