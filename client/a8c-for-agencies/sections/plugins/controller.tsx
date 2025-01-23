b143a7d8b71641cc32de92206fff62b99758b036
Yashwin Poojary <yashwinpoojary@gmail.com>
2025-01-23 17:14:13 +0530

    Reset selected site when plugin management page loads (#98779)

--- client/a8c-for-agencies/sections/plugins/controller.tsx
+++ client/a8c-for-agencies/sections/plugins/controller.tsx
@@ -1,8 +1,20 @@
-import page from '@automattic/calypso-router';
-import { type Callback } from '@automattic/calypso-router';
+import page, { type Callback, type Context } from '@automattic/calypso-router';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { setSelectedSiteId } from 'calypso/state/ui/actions';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { A4A_PLUGINS_LINK } from '../../components/sidebar-menu/lib/constants';
 import MainSidebar from '../../components/sidebar-menu/main';
+
+// Reset selected site id for multi-site view since it is never reset
+// and the plugins component behaves differently when there
+// is a selected site which is incorrect for multi-site view
+const resetSite = ( context: Context ) => {
+	const state = context.store.getState();
+	const siteId = getSelectedSiteId( state );
+	if ( siteId ) {
+		context.store.dispatch( setSelectedSiteId( null ) );
+	}
+};
 
 export const pluginsContext: Callback = ( context ) => {
 	const { slug } = context.params;
@@ -14,6 +26,7 @@ export const pluginsContext: Callback = ( context ) => {
 };
 
 export const pluginManagementContext: Callback = ( context, next ) => {
+	resetSite( context );
 	context.secondary = <MainSidebar path={ context.path } />;
 	context.primary = (
 		<>
@@ -24,6 +37,7 @@ export const pluginManagementContext: Callback = ( context, next ) => {
 };
 
 export const pluginDetailsContext: Callback = ( context, next ) => {
+	resetSite( context );
 	context.secondary = <MainSidebar path={ context.path } />;
 	context.primary = (
 		<>
