f93c6e079815ff82d8d95d184ddb13d9f913861f
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-01-23 08:50:38 +0200

    A4A: Cleanup some unused code (#98731)

--- client/a8c-for-agencies/sections/marketplace/lib/wpcom-bulk-values-utils.ts
+++ client/a8c-for-agencies/sections/marketplace/lib/wpcom-bulk-values-utils.ts
@@ -4,51 +4,6 @@ type Option = {
 	sub?: string;
 };
 
-type SliderOption = {
-	minAbsolute: number;
-	maxAbsolute: number;
-	minValue: number;
-	maxValue: number;
-};
-
-export const mapOptionsToSliderOptions = ( options: Option[], total: number ) => {
-	return options.map( ( _, index ) => {
-		const minAbsolute = ( total * index ) / ( options.length - 1 );
-		const maxAbsolute = ( total * ( index + 1 ) ) / ( options.length - 1 );
-		const minValue = options[ index ].value as number;
-		const maxValue = (
-			options[ index + 1 ] ? options[ index + 1 ].value : options[ index ].value
-		) as number;
-
-		return { minAbsolute, maxAbsolute, minValue, maxValue };
-	} );
-};
-
-export const sliderPosToValue = ( sliderPos: number, options: SliderOption[] ) => {
-	const foundOption = options.find(
-		( option ) => sliderPos >= option.minAbsolute && sliderPos <= option.maxAbsolute
-	);
-	if ( ! foundOption ) {
-		return 1;
-	}
-	const { minAbsolute, maxAbsolute, minValue, maxValue } = foundOption;
-	const normalizedOption = ( sliderPos - minAbsolute ) / ( maxAbsolute - minAbsolute );
-	return Math.floor( minValue + normalizedOption * ( maxValue - minValue ) );
-};
-
-export const valueToSliderPos = ( value: number, options: SliderOption[] ) => {
-	const foundOption = options.find(
-		( option ) => value >= option.minValue && value <= option.maxValue
-	);
-	if ( ! foundOption ) {
-		return value === 0 ? 1 : options[ options.length - 1 ].maxAbsolute;
-	}
-
-	const { minAbsolute, maxAbsolute, minValue, maxValue } = foundOption;
-	const normalizedValue = ( value - minValue ) / ( maxValue - minValue );
-	return Math.floor( minAbsolute + normalizedValue * ( maxAbsolute - minAbsolute ) );
-};
-
 export type DiscountTier = Option & {
 	discount: number;
 };
--- client/a8c-for-agencies/sections/marketplace/pressable-overview/lib/get-pressable-plan.ts
+++ client/a8c-for-agencies/sections/marketplace/pressable-overview/lib/get-pressable-plan.ts
@@ -186,7 +186,3 @@ const PLAN_DATA: Record< string, PressablePlan > = {
 export default function getPressablePlan( slug: string ) {
 	return PLAN_DATA[ slug ];
 }
-
-export function getAllPressablePlans() {
-	return Object.keys( PLAN_DATA );
-}
