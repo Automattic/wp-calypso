1a3f51661e24eba3e502213943669efd911b0b61
Chris Holder <chris.holder@automattic.com>
2025-01-23 19:32:03 -0600

    Only show the poll to a user once

--- client/reader/components/crowdsignal-poll/index.tsx
+++ client/reader/components/crowdsignal-poll/index.tsx
@@ -1,14 +1,31 @@
-import { useSelector } from 'calypso/state';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'calypso/state';
+import { savePreference } from 'calypso/state/preferences/actions';
+import { getPreference } from 'calypso/state/preferences/selectors';
 import { isA8cTeamMember } from 'calypso/state/teams/selectors';
 import ErrorBoundary from './error-boundary';
 import CrowdsignalPollComponent from './main';
 
 import './style.scss';
 
-const CrowdsignalPoll = () => {
-	const isAutomattician = useSelector( isA8cTeamMember );
+const READER_CROWDSIGNAL_POLL_VIEWED_PREFERENCE = 'reader-crowdsignal-poll-viewed';
 
-	if ( isAutomattician ) {
+const CrowdsignalPoll = () => {
+	const dispatch = useDispatch();
+
+	const isAutomattician = useSelector( isA8cTeamMember );
+	const hasViewedPollPref = useSelector( ( state ): boolean | undefined | null =>
+		getPreference( state, READER_CROWDSIGNAL_POLL_VIEWED_PREFERENCE )
+	);
+	const hasViewedPoll = useRef( hasViewedPollPref ); // Show the poll when the component first mounts, but not subsequently
+
+	useEffect( () => {
+		if ( ! hasViewedPoll.current ) {
+			dispatch( savePreference( READER_CROWDSIGNAL_POLL_VIEWED_PREFERENCE, true ) );
+		}
+	}, [ dispatch ] );
+
+	if ( hasViewedPoll.current || isAutomattician ) {
 		return null;
 	}
 
