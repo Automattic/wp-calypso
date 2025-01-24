1a2a70887a634dcf8ce99ab60c9e117ad4fa77be
Chris Holder <chris.holder@automattic.com>
2025-01-23 18:09:39 -0600

    Don't show to A12s

--- client/reader/components/crowdsignal-poll/index.tsx
+++ client/reader/components/crowdsignal-poll/index.tsx
@@ -1,12 +1,22 @@
+import { useSelector } from 'calypso/state';
+import { isA8cTeamMember } from 'calypso/state/teams/selectors';
 import ErrorBoundary from './error-boundary';
 import CrowdsignalPollComponent from './main';
 
 import './style.scss';
 
-const CrowdsignalPoll = () => (
-	<ErrorBoundary>
-		<CrowdsignalPollComponent />
-	</ErrorBoundary>
-);
+const CrowdsignalPoll = () => {
+	const isAutomattician = useSelector( isA8cTeamMember );
+
+	if ( isAutomattician ) {
+		return null;
+	}
+
+	return (
+		<ErrorBoundary>
+			<CrowdsignalPollComponent />
+		</ErrorBoundary>
+	);
+};
 
 export default CrowdsignalPoll;
