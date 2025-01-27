865561592a09e26c619b8dd080cdfdd344463319
Anthony Grullon <agrullon95@gmail.com>
2025-01-27 10:38:05 +0700

    Remove unused matchSupportInteractionId function from utils

--- packages/help-center/src/components/utils.tsx
+++ packages/help-center/src/components/utils.tsx
@@ -106,24 +106,6 @@ export const getConversationsFromSupportInteractions = (
 	);
 };
 
-export const matchSupportInteractionId = (
-	getConversations: () => ZendeskConversation[],
-	isChatLoaded: boolean,
-	currentSupportInteraction: SupportInteraction | undefined
-) => {
-	if ( currentSupportInteraction && isChatLoaded && getConversations ) {
-		const conversations = getConversations();
-		const getCurrentSupportInteractionId = currentSupportInteraction?.events.find(
-			( event ) => event.event_source === 'zendesk'
-		)?.event_external_id;
-		const foundMatch = conversations.find( ( conversation ) => {
-			return conversation.id === getCurrentSupportInteractionId;
-		} );
-
-		return foundMatch;
-	}
-};
-
 export const sortConversationByMostRecent = ( conversations: ZendeskConversation[] ) => {
 	if ( ! Array.isArray( conversations ) ) {
 		return conversations;
