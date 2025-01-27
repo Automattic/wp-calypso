f7a2993754ffda99bc8f62e8026eadc0f4bbcb48
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-01-27 17:01:02 +0200

    Marketplace: Remove MarketplaceDomainUpsell and DomainPicker

--- packages/domain-picker/src/hooks/use-domain-availabilities.ts
+++
@@ -1,8 +0,0 @@
-import { DomainSuggestions } from '@automattic/data-stores';
-import { useSelect } from '@wordpress/data';
-
-export function useDomainAvailabilities() {
-	return useSelect( ( select ) => {
-		return select( DomainSuggestions.store ).getDomainAvailabilities();
-	}, [] );
-}
