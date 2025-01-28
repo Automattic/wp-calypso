9dc749675c05b696ee9fb6dc88779965b1487cf1
Omar Alshaker <omar@omaralshaker.com>
2025-01-28 18:58:27 +0100

    Code comment

--- client/lib/domains/email-forwarding/has-too-many-email-forwards.js
+++ client/lib/domains/email-forwarding/has-too-many-email-forwards.js
@@ -2,7 +2,8 @@ const MAX_FORWARD_DESTINATIONS = 5;
 
 /**
  * @param newEmailForward a string representing a new email forward
- * @returns { boolean } If the email forward is has more than the maximum number of destinations.
+ * @param existingEmailForwards an array of strings representing existing email forwards
+ * @returns { boolean } true if the email forward has more than the maximum number of destinations.
  */
 export function hasTooManyEmailForwardsForMailbox( newEmailForward, existingEmailForwards ) {
 	return (
