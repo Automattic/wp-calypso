97f83f35ffdbd5e644a37a2eb7fdabe24b16a26b
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-01-28 22:15:46 +0800

    A4A: Add missing track events in the new Product marketplace. (#98967)

--- client/a8c-for-agencies/sections/marketplace/products-overview-v2/product-listing/section.tsx
+++ client/a8c-for-agencies/sections/marketplace/products-overview-v2/product-listing/section.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 
 type Props = {
 	icon?: ReactNode;
@@ -15,8 +17,42 @@ export default function ProductListingSection( {
 	children,
 	extraContent,
 }: Props ) {
+	const sectionRef = useRef< HTMLDivElement >( null );
+	const [ isVisibleOnce, setIsVisibleOnce ] = useState( false );
+	const dispatch = useDispatch();
+	useEffect( () => {
+		const observer = new IntersectionObserver(
+			( [ entry ] ) => {
+				if ( ! isVisibleOnce ) {
+					setIsVisibleOnce( entry.isIntersecting );
+				}
+			},
+			{
+				threshold: 0.1, // Trigger when at least 10% of the element is visible
+			}
+		);
+
+		if ( sectionRef.current ) {
+			observer.observe( sectionRef.current );
+		}
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [ isVisibleOnce ] );
+
+	useEffect( () => {
+		if ( isVisibleOnce ) {
+			dispatch(
+				recordTracksEvent( 'calypso_a4a_marketplace_products_overview_section_visible', {
+					section: title,
+				} )
+			);
+		}
+	}, [ dispatch, isVisibleOnce, title ] );
+
 	return (
-		<div className="product-listing-section">
+		<div className="product-listing-section" ref={ sectionRef }>
 			<div className="product-listing-section__header-wrapper">
 				<div className="product-listing-section__header">
 					{ icon }
