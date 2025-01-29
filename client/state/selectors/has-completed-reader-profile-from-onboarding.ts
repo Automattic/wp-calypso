e43e40bdf0da2ac5d3465c9418b441a0abe0b980
Chris Holder <chris.holder@automattic.com>
2025-01-28 19:36:58 -0600

    Guard against undefined path

--- client/state/selectors/has-completed-reader-profile-from-onboarding.ts
+++ client/state/selectors/has-completed-reader-profile-from-onboarding.ts
@@ -5,7 +5,7 @@ import { AppState } from 'calypso/types';
 export default ( state: AppState ): boolean => {
 	const hasCompletedProfile = hasCompletedReaderProfile( state );
 	const fromReaderOnboarding =
-		state.route.path.previous.startsWith( '/read' ) &&
+		state?.route?.path.previous.startsWith( '/read' ) &&
 		getCurrentQueryArguments( state )?.ref === 'reader-onboarding';
 
 	return hasCompletedProfile && fromReaderOnboarding;
