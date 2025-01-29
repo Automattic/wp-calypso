5c53cbc0e69ba7d522ccf4af2b0b883332390afc
vykes-mac <knrymcleish@gmail.com>
2025-01-28 22:15:12 -0500

    show next step snackbar when ssh keys are setup

--- client/hosting/constants.ts
+++ client/hosting/constants.ts
@@ -1,5 +1,4 @@
 //hosting flow task hashes
 export const HOSTING_THEME_SELCETED_HASH = '#theme-selected';
 export const HOSTING_INSTALL_PLUGIN_HASH = '#install-plugin';
-export const HOSTING_SET_UP_SSH_HASH = '#sftp-credentials';
 export const HOSTING_SITE_MONITORING_HASH = '#site-monitoring';
