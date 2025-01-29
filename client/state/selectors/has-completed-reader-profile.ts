4e898e88f1c059e161c841fe763700b459eeccf0
Chris Holder <chris.holder@automattic.com>
2025-01-28 19:20:32 -0600

    Guard against settings being undefined

--- client/state/selectors/has-completed-reader-profile.ts
+++ client/state/selectors/has-completed-reader-profile.ts
@@ -1,7 +1,7 @@
 import { AppState } from 'calypso/types';
 
 export default ( state: AppState ): boolean =>
-	state.userSettings.settings?.first_name &&
-	state.userSettings.settings?.last_name &&
-	state.userSettings.settings?.has_gravatar &&
-	state.userSettings.settings?.description;
+	state.userSettings?.settings?.first_name &&
+	state.userSettings?.settings?.last_name &&
+	state.userSettings?.settings?.has_gravatar &&
+	state.userSettings?.settings?.description;
