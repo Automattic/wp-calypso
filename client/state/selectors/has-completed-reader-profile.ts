be641b5ba4f6b0bdc1a4d380c502dcc694a05ef7
Chris Holder <chris.holder@automattic.com>
2025-01-29 14:06:12 -0600

    Optimistically complete onboarding if gravatar image is in state

--- client/state/selectors/has-completed-reader-profile.ts
+++ client/state/selectors/has-completed-reader-profile.ts
@@ -3,5 +3,5 @@ import { AppState } from 'calypso/types';
 export default ( state: AppState ): boolean =>
 	state.userSettings?.settings?.first_name &&
 	state.userSettings?.settings?.last_name &&
-	state.userSettings?.settings?.has_gravatar &&
+	( state.userSettings?.settings?.has_gravatar || state.gravatarStatus?.tempImage ) &&
 	state.userSettings?.settings?.description;
