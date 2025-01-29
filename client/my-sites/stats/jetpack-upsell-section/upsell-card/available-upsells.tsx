3652a8466b5b7c7951e2ccf70ddaf8118de4145f
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2025-01-30 00:16:16 +0800

    Extract configuration into new file

---
+++ client/my-sites/stats/jetpack-upsell-section/upsell-card/available-upsells.tsx
@@ -0,0 +1,99 @@
+import { translate } from 'i18n-calypso';
+import SecurityIcon from './icons/jetpack-icon-lock.svg';
+import BackupIcon from './icons/jetpack-product-icon-backup.svg';
+import BoostIcon from './icons/jetpack-product-icon-boost.svg';
+import SearchIcon from './icons/jetpack-product-icon-search.svg';
+import SocialIcon from './icons/jetpack-product-icon-social.svg';
+import VideoPressIcon from './icons/jetpack-product-icon-videopress.svg';
+
+export type Product = {
+	description: string;
+	href: string;
+	iconUrl: string;
+	isFree: boolean;
+	slug: string;
+	title: string;
+	features: string[];
+};
+
+export function getAvailableUpsells() {
+	return [
+		{
+			description: translate(
+				'Protect your site from hackers and spam with automated backups, malware scanning, and spam filtering.'
+			),
+			href: 'https://jetpack.com/features/security/',
+			iconUrl: SecurityIcon,
+			isFree: false,
+			slug: 'security',
+			title: translate( 'Security', { context: 'Jetpack product name' } ),
+			features: [ 'scan' ],
+		},
+		{
+			description: translate(
+				'Save every single change and get back online quickly with one-click restores.'
+			),
+			href: 'https://jetpack.com/upgrade/backup/',
+			iconUrl: BackupIcon,
+			isFree: false,
+			slug: 'backup',
+			title: translate( 'Backup' ),
+			features: [ 'backups', 'restore' ],
+		},
+		{
+			description: translate(
+				"Help your site visitors instantly find what they're looking for so they read and buy more."
+			),
+			href: 'https://jetpack.com/upgrade/search/',
+			iconUrl: SearchIcon,
+			isFree: false,
+			slug: 'search',
+			title: translate( 'Search' ),
+			features: [ 'search', 'instant-search' ],
+		},
+		{
+			description: translate(
+				'Engage your visitors with high-quality, ad-free videos build specifically for WordPress.'
+			),
+			href: 'https://jetpack.com/videopress/',
+			iconUrl: VideoPressIcon,
+			isFree: false,
+			slug: 'video',
+			title: translate( 'VideoPress' ),
+			features: [ 'videopress', 'videopress-1tb-storage' ],
+		},
+		{
+			description: translate(
+				"Improve your site's performance and SEO in a few clicks with the free Jetpack Boost plugin."
+			),
+			href: 'https://jetpack.com/boost/',
+			iconUrl: BoostIcon,
+			isFree: true,
+			slug: 'boost',
+			title: translate( 'Boost' ),
+			features: [
+				'cloud-critical-css',
+				'cornerstone-10-pages',
+				'image-cdn-liar',
+				'image-cdn-quality',
+				'image-size-analysis',
+				'performance-history',
+			],
+		},
+		{
+			description: translate(
+				'Save time by auto-posting your content to social networks like Facebook, LinkedIn, and more.'
+			),
+			href: 'https://jetpack.com/social/',
+			iconUrl: SocialIcon,
+			isFree: true,
+			slug: 'social',
+			title: translate( 'Social' ),
+			features: [
+				'social-enhanced-publishing',
+				'social-image-generator',
+				'subscriber-unlimited-imports',
+			],
+		},
+	] as Product[];
+}
