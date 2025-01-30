1913df895f5a6a64c30e128e10f4c8fca8c56b56
Jasper Kang <jasper.kang@automattic.com>
2025-01-30 13:27:13 +1300

    use subscribers_by_user_type for email/wpcom subs

--- client/my-sites/stats/hooks/use-subscribers-totals-query.tsx
+++ client/my-sites/stats/hooks/use-subscribers-totals-query.tsx
@@ -2,18 +2,25 @@ import { useQueries } from '@tanstack/react-query';
 import wpcom from 'calypso/lib/wp';
 import getDefaultQueryParams from './default-query-params';
 
-const querySubscribersTotals = ( siteId: number | null, filterAdmin?: boolean ): Promise< any > => {
-	return wpcom.req.get(
-		{
-			path: `/sites/${ siteId }/stats/followers`,
-		},
-		{
-			type: 'all',
-			filter_admin: filterAdmin ? true : false,
-			// Only one-page results adjust visible subscribers with deleted accounts to align with the subscriber list.
-			max: 10,
-		}
-	);
+const querySubscribersTotals = (
+	siteId: number | null,
+	user_type: 'email' | 'wpcom',
+	filterAdmin?: boolean
+): Promise< any > => {
+	return wpcom.req
+		.get(
+			{
+				apiNamespace: 'wpcom/v2',
+				path: `/sites/${ siteId }/subscribers_by_user_type`,
+			},
+			{
+				per_page: 10,
+				page: 1,
+				user_type,
+				filterAdmin,
+			}
+		)
+		.catch( () => ( {} ) );
 };
 
 const queryMore = ( siteId: number | null ): Promise< any > => {
@@ -61,8 +68,8 @@ function useSubscribersTotalsQueries( siteId: number | null, filterAdmin?: boole
 		queries: [
 			{
 				...getDefaultQueryParams(),
-				queryKey: [ 'stats', 'totals', 'subscribers', siteId, filterAdmin ],
-				queryFn: () => querySubscribersTotals( siteId, filterAdmin ),
+				queryKey: [ 'stats', 'totals', 'subscribers', 'email', siteId, filterAdmin ],
+				queryFn: () => querySubscribersTotals( siteId, 'email', filterAdmin ),
 				select: selectSubscribers,
 				staleTime: 1000 * 60 * 5, // 5 minutes
 			},
@@ -73,14 +80,21 @@ function useSubscribersTotalsQueries( siteId: number | null, filterAdmin?: boole
 				select: selectPaidSubscribers,
 				staleTime: 1000 * 60 * 5, // 5 minutes
 			},
+			{
+				...getDefaultQueryParams(),
+				queryKey: [ 'stats', 'totals', 'subscribers', 'wpcom', siteId, filterAdmin ],
+				queryFn: () => querySubscribersTotals( siteId, 'wpcom', filterAdmin ),
+				select: selectSubscribers,
+				staleTime: 1000 * 60 * 5, // 5 minutes
+			},
 		],
 	} );
 
 	return {
 		data: {
-			total_email: queries[ 0 ]?.data?.total_email,
-			total_wpcom: queries[ 0 ]?.data?.total_wpcom,
-			total: queries[ 0 ]?.data?.total,
+			total_email: queries[ 0 ]?.data?.total,
+			total_wpcom: queries[ 2 ]?.data?.total,
+			total: queries[ 1 ].data?.email_subscribers,
 			paid_subscribers: queries[ 1 ]?.data?.paid_subscribers,
 			free_subscribers:
 				queries[ 1 ]?.data?.email_subscribers !== undefined &&
@@ -88,7 +102,7 @@ function useSubscribersTotalsQueries( siteId: number | null, filterAdmin?: boole
 					? queries[ 1 ].data.email_subscribers - queries[ 1 ].data.paid_subscribers
 					: null,
 			social_followers: queries[ 1 ]?.data?.social_followers,
-			is_owner_subscribing: queries[ 0 ]?.data?.is_owner_subscribing,
+			is_owner_subscribing: false,
 		},
 		isLoading: queries.some( ( result ) => result.isLoading ),
 		isError: queries.some( ( result ) => result.isError ),
