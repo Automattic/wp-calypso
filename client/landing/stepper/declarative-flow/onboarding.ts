4b0c2c6c45209ed89f8077ea6df0b0e7463d82c7
Philip Jackson <p-jackson@live.com>
2025-01-30 19:28:10 +1300

    Fix back navigation from domains step when big-sky-before-plans onboarding

--- client/landing/stepper/declarative-flow/onboarding.ts
+++ client/landing/stepper/declarative-flow/onboarding.ts
@@ -395,6 +395,9 @@ const onboarding: Flow = {
 					return navigate( 'domains' );
 				case 'domains':
 					if ( isGoalsAtFrontExperiment ) {
+						if ( config.isEnabled( 'onboarding/big-sky-before-plans' ) && createWithBigSky ) {
+							return navigate( 'design-choices' );
+						}
 						return navigate( 'designSetup' );
 					}
 				case 'designSetup':
