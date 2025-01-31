22e4a7553ae8e7d2cff7d57400cba53e886bae8f
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-01-31 17:47:36 +0200

    Components: Remove unused UplotBarChart (#99163)

--- client/components/chart-uplot/stories/index.stories.tsx
+++ client/components/chart-uplot/stories/index.stories.tsx
@@ -2,7 +2,6 @@
 
 import { useRef } from 'react';
 import UplotChart from '../';
-import UplotBarChart from '../bar';
 
 export default { title: 'client/components/Chart (uPlot)' };
 
@@ -40,46 +39,5 @@ export const ExternalLegendMount = () => {
 			</div>
 			<Variant legendContainer={ legendRef } />
 		</>
-	);
-};
-
-// Bar chart
-
-const barChartData: [ string[], ...number[][] ] = [
-	[ 'HTTP 200', 'HTTP 401', 'HTTP 400', 'HTTP 404', 'HTTP 500' ],
-	[ 2000, 1000, 800, 1400, 1200, 2100, 1800 ], // 200
-	[ 0, 0, 0, 0, 0, 0, 0 ], // 401
-	[ 0, 0, 0, 0, 0, 0, 0 ], // 400
-	[ 138, 112, 98, 84, 70, 56, 42 ], // 404
-	[ 0, 0, 0, 0, 70, 0, 0 ], // 500
-];
-
-const barChartLabels = [ '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00' ];
-const legendData = [
-	{
-		fillColor: '#68B3E8',
-		tooltip: <span>Tooltip content for HTTP 200</span>,
-	},
-	{
-		fillColor: '#A7AAAD',
-		tooltip: <span>Tooltip content for HTTP 401</span>,
-	},
-	{
-		fillColor: '#F2D76B',
-		tooltip: <span>Tooltip content for HTTP 400</span>,
-	},
-	{
-		fillColor: '#09B585',
-		tooltip: <span>Tooltip content for HTTP 404</span>,
-	},
-	{
-		fillColor: '#F283AA',
-		tooltip: <span>Tooltip content for HTTP 500</span>,
-	},
-];
-
-export const BarChartUplot = () => {
-	return (
-		<UplotBarChart data={ barChartData } labels={ barChartLabels } legendData={ legendData } />
 	);
 };
