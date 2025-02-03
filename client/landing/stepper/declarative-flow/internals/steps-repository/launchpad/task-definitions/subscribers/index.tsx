6041d1afcdfe73dabeb63be55c80b6ae01249134
Addison Stavlo <Addison.Stavlo@Automattic.com>
2025-02-03 10:26:41 -0500

    handle add_first_subscribers task in fullscreen launchpad as well

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/subscribers/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/subscribers/index.tsx
@@ -1,5 +1,5 @@
+import { updateLaunchpadSettings } from '@automattic/data-stores';
 import { isNewsletterFlow } from '@automattic/onboarding';
-import { type TaskAction } from '../../types';
 
 const getSubscribersTask: TaskAction = ( task, flow, context ) => {
 	const { goToStep, isEmailVerified } = context;
@@ -16,6 +16,23 @@ const getSubscribersTask: TaskAction = ( task, flow, context ) => {
 	};
 };
 
+const addFirstSubscribersTask: TaskAction = ( task, flow, context ) => {
+	const { siteSlug, isEmailVerified } = context;
+
+	const mustVerifyEmailBeforePosting = ! isEmailVerified;
+	return {
+		...task,
+		disabled: mustVerifyEmailBeforePosting || false,
+		useCalypsoPath: true,
+		actionDispatch: () => {
+			updateLaunchpadSettings( siteSlug, {
+				checklist_statuses: { add_first_subscribers: true },
+			} );
+		},
+	};
+};
+
 export const actions = {
 	subscribers_added: getSubscribersTask,
+	add_first_subscribers: addFirstSubscribersTask,
 };
