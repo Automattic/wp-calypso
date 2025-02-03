6b5e0db59ebabaa77006ffdf6d514dc1af420ecc
Yashwin <yashwinpoojary@gmail.com>
2025-02-03 09:04:31 +0530

    remove the product icon check

--- client/my-sites/plans/jetpack-plans/product-store/utils/get-product-icon.ts
+++ client/my-sites/plans/jetpack-plans/product-store/utils/get-product-icon.ts
@@ -44,9 +44,7 @@ import JetpackProductIconStatsLight from 'calypso/assets/images/jetpack/jetpack-
 import JetpackProductIconStats from 'calypso/assets/images/jetpack/jetpack-product-icon-stats.svg';
 import JetpackProductIconVideopressLight from 'calypso/assets/images/jetpack/jetpack-product-icon-videopress-light.svg';
 import JetpackProductIconVideopress from 'calypso/assets/images/jetpack/jetpack-product-icon-videopress.svg';
-import WooCommerceProductIcon from 'calypso/assets/images/jetpack/woo-icon.svg';
 import WooCommerceLogo from 'calypso/assets/images/woocommerce/product-icon.svg';
-import isA8CForAgencies from 'calypso/lib/a8c-for-agencies/is-a8c-for-agencies';
 import { productIconProps } from '../types';
 
 const setProductsIcon = ( slugs: ReadonlyArray< string >, resource: IconResource ) =>
@@ -126,18 +124,10 @@ const PRODUCT_ICON_MAP: Record< string, IconResource > = {
 		regular: JetpackProductIconCreator,
 		light: '',
 	} ),
-	...setProductsIcon(
-		WOOCOMMERCE_PRODUCTS,
-		isA8CForAgencies()
-			? {
-					regular: WooCommerceLogo,
-					light: WooCommerceLogo,
-			  }
-			: {
-					regular: WooCommerceProductIcon,
-					light: WooCommerceProductIcon,
-			  }
-	),
+	...setProductsIcon( WOOCOMMERCE_PRODUCTS, {
+		regular: WooCommerceLogo,
+		light: WooCommerceLogo,
+	} ),
 };
 
 const getProductIcon = ( { productSlug, light }: productIconProps ): string => {
