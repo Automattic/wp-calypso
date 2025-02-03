fda2c476359e82e532d4d325cb5a6f2b34617055
rafaelgallani <galani.rafael@gmail.com>
2025-02-03 16:00:42 -0300

    Prefer relative import

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/domain-code-pair.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/domain-code-pair.tsx
@@ -12,11 +12,11 @@ import { useSelector } from 'react-redux';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormInput from 'calypso/components/forms/form-text-input';
 import InfoPopover from 'calypso/components/info-popover';
-import { usePriceOverride } from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/use-price-override';
 import { domainAvailability } from 'calypso/lib/domains/constants';
 import { getLocaleSlug } from 'calypso/lib/i18n-utils';
 import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
 import GoogleDomainsModal from '../../components/google-domains-transfer-instructions';
+import { usePriceOverride } from './use-price-override';
 import { useValidationMessage } from './use-validation-message';
 
 type Props = {
