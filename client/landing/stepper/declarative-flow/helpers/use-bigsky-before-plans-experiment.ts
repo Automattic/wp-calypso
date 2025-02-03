c0a2bdf3ece845ab73e1025a0f2b26e93636bd98
Philip Jackson <p-jackson@live.com>
2025-02-03 19:41:14 +1300

    A quick patch to disable the Big Sky onboarding experience

--- client/landing/stepper/declarative-flow/helpers/use-bigsky-before-plans-experiment.ts
+++ client/landing/stepper/declarative-flow/helpers/use-bigsky-before-plans-experiment.ts
@@ -1,10 +1,10 @@
 import { isEnabled } from '@automattic/calypso-config';
 import { setPlansListExperiment } from '@automattic/calypso-products';
-import { ONBOARDING_FLOW } from '@automattic/onboarding';
-import { useMemo } from 'react';
-import { useExperiment } from 'calypso/lib/explat';
-import { getFlowFromURL } from '../../utils/get-flow-from-url';
-import { useGoalsFirstExperiment } from './use-goals-first-experiment';
+//import { ONBOARDING_FLOW } from '@automattic/onboarding';
+//import { useMemo } from 'react';
+//import { useExperiment } from 'calypso/lib/explat';
+//import { getFlowFromURL } from '../../utils/get-flow-from-url';
+//import { useGoalsFirstExperiment } from './use-goals-first-experiment';
 
 export const EXPERIMENT_NAME = 'calypso_signup_onboarding_goals_first_bigsky_202501_v1';
 
@@ -14,29 +14,36 @@ export const EXPERIMENT_NAME = 'calypso_signup_onboarding_goals_first_bigsky_202
  * Returns [ isLoading, isBigSkyBeforePlans ]
  */
 export function useBigSkyBeforePlans(): [ boolean, boolean ] {
-	const flow = useMemo( () => getFlowFromURL(), [] );
-	const [ isLoadingGoalsFirst, isGoalsFirstExperiment ] = useGoalsFirstExperiment();
-
-	const [ isLoading, experimentAssignment ] = useExperiment( EXPERIMENT_NAME, {
-		isEligible:
-			! isLoadingGoalsFirst &&
-			isGoalsFirstExperiment &&
-			flow === ONBOARDING_FLOW &&
-			! isEnabled( 'onboarding/force-big-sky-before-plan' ),
-	} );
-
 	if ( isEnabled( 'onboarding/force-big-sky-before-plan' ) ) {
 		setPlansListExperiment( EXPERIMENT_NAME, 'treatment' );
 		return [ false, true ];
 	}
 
-	/**
-	 * This fallback is necessary because experimentAssignment returns null when the user
-	 * is not eligible, and we're using this hook within steps that are used by other flows.
-	 */
-	const variationName = experimentAssignment?.variationName ?? 'control';
+	return [ false, false ];
 
-	setPlansListExperiment( EXPERIMENT_NAME, variationName );
-
-	return [ isLoading, variationName === 'treatment' ];
+	//const flow = useMemo( () => getFlowFromURL(), [] );
+	//const [ isLoadingGoalsFirst, isGoalsFirstExperiment ] = useGoalsFirstExperiment();
+	//
+	//const [ isLoading, experimentAssignment ] = useExperiment( EXPERIMENT_NAME, {
+	//	isEligible:
+	//		! isLoadingGoalsFirst &&
+	//		isGoalsFirstExperiment &&
+	//		flow === ONBOARDING_FLOW &&
+	//		! isEnabled( 'onboarding/force-big-sky-before-plan' ),
+	//} );
+	//
+	//if ( isEnabled( 'onboarding/force-big-sky-before-plan' ) ) {
+	//	setPlansListExperiment( EXPERIMENT_NAME, 'treatment' );
+	//	return [ false, true ];
+	//}
+	//
+	///**
+	// * This fallback is necessary because experimentAssignment returns null when the user
+	// * is not eligible, and we're using this hook within steps that are used by other flows.
+	// */
+	//const variationName = experimentAssignment?.variationName ?? 'control';
+	//
+	//setPlansListExperiment( EXPERIMENT_NAME, variationName );
+	//
+	//return [ isLoading, variationName === 'treatment' ];
 }
