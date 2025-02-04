230eba755a897cf23e331d2299b3ebfebbea45b1
Marco Ciampini <marco.ciampo@gmail.com>
2025-02-04 22:24:45 +0100

    Revert "Login form: use `TextControl` from `@wordpress/components` instead of…"
    This reverts commit 60dc707acc7aae1b8d57241726801943fdc246b4.

--- client/blocks/login/login-form.jsx
+++ client/blocks/login/login-form.jsx
@@ -4,7 +4,7 @@ import { Button, Card, FormInputValidation, FormLabel, Gridicon } from '@automat
 import { alert } from '@automattic/components/src/icons';
 import { localizeUrl } from '@automattic/i18n-utils';
 import { suggestEmailCorrection } from '@automattic/onboarding';
-import { Spinner, TextControl } from '@wordpress/components';
+import { Spinner } from '@wordpress/components';
 import { Icon } from '@wordpress/icons';
 import clsx from 'clsx';
 import cookie from 'cookie';
@@ -22,6 +22,7 @@ import FormPasswordInput from 'calypso/components/forms/form-password-input';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 import Notice from 'calypso/components/notice';
 import { LastUsedSocialButton } from 'calypso/components/social-buttons';
+import TextControl from 'calypso/components/text-control';
 import wooDnaConfig from 'calypso/jetpack-connect/woo-dna-config';
 import {
 	getSignupUrl,
@@ -490,8 +491,6 @@ export class LoginForm extends Component {
 									usernameOrEmail: value,
 								} );
 							} }
-							__next40pxDefaultSize
-							__nextHasNoMarginBottom
 						/>
 
 						{ requestError && requestError.field === 'usernameOrEmail' && (
@@ -516,8 +515,6 @@ export class LoginForm extends Component {
 										password: value,
 									} );
 								} }
-								__next40pxDefaultSize
-								__nextHasNoMarginBottom
 							/>
 
 							{ requestError && requestError.field === 'password' && (
