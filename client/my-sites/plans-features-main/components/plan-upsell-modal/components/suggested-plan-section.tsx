a34437da978438641f918b8dd27ded311fd0bba7
Paulo Marcos Trentin <paulo@paulotrentin.com.br>
2025-02-04 14:08:17 -0300

    Big Sky: Updated free site modal copy, size and console error (#99218)
    * Updated modal's copy and size to accomodate the new information without breaking line
    
    * Fixed the key console error
    
    * Code improvement by avoiding multiple hook calls
    
    * Conditioning new copies to the treatment_cumulative group
    
    * Improve code readability

--- client/my-sites/plans-features-main/components/plan-upsell-modal/components/suggested-plan-section.tsx
+++ client/my-sites/plans-features-main/components/plan-upsell-modal/components/suggested-plan-section.tsx
@@ -5,7 +5,9 @@ import {
 	PLAN_BUSINESS,
 	PLAN_ECOMMERCE,
 } from '@automattic/calypso-products';
+import { useHasEnTranslation } from '@automattic/i18n-utils';
 import { useTranslate } from 'i18n-calypso';
+import { useGoalsFirstExperiment } from 'calypso/landing/stepper/declarative-flow/helpers/use-goals-first-experiment';
 import PlanItem from './plan-item';
 import { RowWithBorder } from '.';
 
@@ -26,24 +28,43 @@ export default function SuggestedPlanSection( {
 	onPlanSelected,
 }: Props ) {
 	const translate = useTranslate();
+	const hasEnTranslation = useHasEnTranslation();
+	const [ , , variationName ] = useGoalsFirstExperiment();
+	const shouldUseNewCopy = variationName === 'treatment_cumulative';
+
 	const suggestedPlans = [
 		{
 			planSlug: PLAN_PERSONAL,
-			description: translate( 'Domain credit, some premium themes' ),
+			description:
+				hasEnTranslation( 'Free one-year domain and some premium themes' ) && shouldUseNewCopy
+					? translate( 'Free one-year domain and some premium themes' )
+					: translate( 'Domain credit, some premium themes' ),
 			disabled: hidePersonalPlan,
 		},
 		{
 			planSlug: PLAN_PREMIUM,
-			description: translate( 'Domain credit, all premium themes' ),
+			description:
+				hasEnTranslation( 'Free one-year domain and all premium themes' ) && shouldUseNewCopy
+					? translate( 'Free one-year domain and all premium themes' )
+					: translate( 'Domain credit, all premium themes' ),
 			disabled: hidePremiumPlan,
 		},
 		{
 			planSlug: PLAN_BUSINESS,
-			description: translate( 'Domain credit, plugins, all premium themes' ),
+			description:
+				hasEnTranslation( 'Free one-year domain, plugins, and all premium themes' ) &&
+				shouldUseNewCopy
+					? translate( 'Free one-year domain, plugins, and all premium themes' )
+					: translate( 'Domain credit, plugins, all premium themes' ),
 		},
 		{
 			planSlug: PLAN_ECOMMERCE,
-			description: translate( 'Domain credit, plugins, all premium and store themes, WooCommerce' ),
+			description:
+				hasEnTranslation(
+					'Free one-year domain, plugins, all premium and store themes, WooCommerce'
+				) && shouldUseNewCopy
+					? translate( 'Free one-year domain, plugins, all premium and store themes, WooCommerce' )
+					: translate( 'Domain credit, plugins, all premium and store themes, WooCommerce' ),
 		},
 	];
 
@@ -53,9 +74,8 @@ export default function SuggestedPlanSection( {
 				.filter( ( plan ) => ! plan.disabled )
 				.slice( 0, 2 )
 				.map( ( { planSlug, description } ) => (
-					<RowWithBorder>
+					<RowWithBorder key={ planSlug }>
 						<PlanItem
-							key={ planSlug }
 							planSlug={ planSlug as PlanSlug }
 							description={ description }
 							isBusy={ isBusy }
