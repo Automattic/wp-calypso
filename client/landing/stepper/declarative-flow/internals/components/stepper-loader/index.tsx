1bd918841e77ce877670c30de71ff707761db1fe
escapemanuele <escapemanuele@gmail.com>
2025-02-05 14:01:44 +0100

    Remove unused classname

--- client/landing/stepper/declarative-flow/internals/components/stepper-loader/index.tsx
+++ client/landing/stepper/declarative-flow/internals/components/stepper-loader/index.tsx
@@ -15,19 +15,13 @@ const StepperLoader: React.FC< StepperLoaderProps > = ( {
 	progress = -1,
 	className,
 } ) => {
-	const renderProgressComponent = () => {
-		return (
-			<ProgressBar
-				value={ progress >= 0 ? progress * 100 : undefined }
-				className="stepper-loader__progress-bar processing-step__progress-bar"
-			/>
-		);
-	};
-
 	return (
 		<div className={ clsx( 'stepper-loader', className ) }>
-			<h1 className="stepper-loader__title processing-step__progress-step">{ title }</h1>
-			{ renderProgressComponent() }
+			<h1 className="stepper-loader__title">{ title }</h1>
+			<ProgressBar
+				value={ progress >= 0 ? progress * 100 : undefined }
+				className="stepper-loader__progress-bar"
+			/>
 			{ subtitle && <p className="stepper-loader__subtitle">{ subtitle }</p> }
 		</div>
 	);
