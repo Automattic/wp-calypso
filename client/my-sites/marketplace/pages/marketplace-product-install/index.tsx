0d0e2afa48294b2867261a0ecdcb99ece5db0754
Roberto Aranda <roberto.aranda@automattic.com>
2025-02-06 18:37:37 +0100

    Fix linting error and remove redundant useMemo

--- client/my-sites/marketplace/pages/marketplace-product-install/index.tsx
+++ client/my-sites/marketplace/pages/marketplace-product-install/index.tsx
@@ -172,15 +172,11 @@ const MarketplaceProductInstall = ( {
 
 	const { primaryDomain } = useSelector( getPurchaseFlowState );
 
-	const shouldShowNoDirectAccessError = useMemo( () => {
-		return (
-			// Show error if:
-			// 1. This is a plugin upload flow (via zip file) and we don't have a primary domain set
-			( isPluginUploadFlow && ! primaryDomain ) ||
-			// 2. This is a marketplace plugin installation but the installation process hasn't started
-			( ! isPluginUploadFlow && ! marketplaceInstallationInProgress )
-		);
-	}, [ isPluginUploadFlow, marketplaceInstallationInProgress ] );
+	const shouldShowNoDirectAccessError =
+		// 1. This is a plugin upload flow (via zip file) and we don't have a primary domain set
+		( isPluginUploadFlow && ! primaryDomain ) ||
+		// 2. This is a marketplace plugin installation but the installation process hasn't started
+		( ! isPluginUploadFlow && ! marketplaceInstallationInProgress );
 
 	// Check that the site URL and the plugin slug are the same which were selected on the plugin page
 	useEffect( () => {
@@ -189,7 +185,7 @@ const MarketplaceProductInstall = ( {
 				shouldShowNoDirectAccessError && setNoDirectAccessError( true );
 			} );
 		}
-	}, [ marketplaceInstallationInProgress, isPluginUploadFlow ] );
+	}, [ shouldShowNoDirectAccessError ] );
 
 	// Upload flow startup
 	useEffect( () => {
