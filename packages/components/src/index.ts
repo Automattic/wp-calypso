3326838c02edd0d477f32ed7c3b19ca218fe448c
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-02-07 16:19:36 +0200

    Components: Move AppPromoCard to client

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -46,7 +46,6 @@ export { default as CountComparisonCard } from './highlight-cards/count-comparis
 export { default as AnnualHighlightCards } from './highlight-cards/annual-highlight-cards';
 export { default as NoticeBanner } from './notice-banner';
 export { default as MobileHighlightCardListing } from './highlight-cards/mobile-highlight-cards';
-export { default as AppPromoCard } from './app-promo-card';
 export { default as ShortenedNumber } from './number-formatters';
 export { default as formattedNumber } from './number-formatters/formatted-number';
 export { default as PostStatsCard } from './post-stats-card';
