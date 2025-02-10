f6b004ec63fbdc100bb2ef6483c1563c2eb95d16
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-02-10 22:57:22 +0800

    Make the card dismissable.

--- client/a8c-for-agencies/sections/overview/sidebar/featured-woopayments/index.tsx
+++ client/a8c-for-agencies/sections/overview/sidebar/featured-woopayments/index.tsx
@@ -1,15 +1,42 @@
 import { Badge, Card } from '@automattic/components';
 import { Button } from '@wordpress/components';
+import { close } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
+import { useCallback } from 'react';
 import WooPaymentsLogo from 'calypso/assets/images/a8c-for-agencies/product-logos/woopayments.svg';
+import { useDispatch, useSelector } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { savePreference } from 'calypso/state/preferences/actions';
+import { getPreference } from 'calypso/state/preferences/selectors';
 
 import './style.scss';
 
+const DISMISSED_PREFERENCE = 'a4a_woopayments_featured_overview_card_dismissed';
+
 export default function OverviewSidebarFeaturedWooPayments() {
 	const translate = useTranslate();
+	const dispatch = useDispatch();
+
+	const onDismiss = useCallback( () => {
+		dispatch( recordTracksEvent( 'calypso_a4a_overview_featured_woopayments_dismiss_click' ) );
+		dispatch( savePreference( DISMISSED_PREFERENCE, true ) );
+	}, [ dispatch ] );
+
+	const isDismissed = useSelector( ( state ) => getPreference( state, DISMISSED_PREFERENCE ) );
+
+	if ( isDismissed ) {
+		return null;
+	}
 
 	return (
 		<Card className="overview__featured-woopayments">
+			<Button
+				className="overview__featured-dismiss-button"
+				variant="tertiary"
+				icon={ close }
+				onClick={ onDismiss }
+			/>
+
 			<Badge className="overview__featured-woopayments-badge">{ translate( 'Featured' ) }</Badge>
 
 			<img
