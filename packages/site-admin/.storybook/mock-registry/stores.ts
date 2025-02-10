8a7018f66445c969330862723de23eb8e777593c
Damián Suárez <rdsuarez@gmail.com>
2025-02-10 14:51:14 +0000

    add regular and jetpack testing sites

--- packages/site-admin/.storybook/mock-registry/stores.ts
+++ packages/site-admin/.storybook/mock-registry/stores.ts
@@ -6,7 +6,17 @@ import { store as coreDataStore } from '@wordpress/core-data';
 const stores = {
 	'woocommerce-analytics': {
 		[ coreDataStore.name ]: {
-			'root/__unstableBase': { site_icon_url: './Woo_logo_color.png' },
+			'root/__unstableBase': { site_icon_url: './woo-logo.png' },
+		},
+	},
+	'regular-site': {
+		[ coreDataStore.name ]: {
+			'root/__unstableBase': { site_icon_url: undefined },
+		},
+	},
+	'jetpack-site': {
+		[ coreDataStore.name ]: {
+			'root/__unstableBase': { site_icon_url: './jetpack-favicon-2018.png' },
 		},
 	},
 };
