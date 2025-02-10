7edf11560ef9f5dcd3485e3668abc7dd3b6f8939
Tiago Noronha <hello@tiagonoronha.com>
2025-02-10 11:00:52 +0000

    Update Jetpack banner message

--- client/a8c-for-agencies/sections/plugins/plugins-jetpack-banner.tsx
+++ client/a8c-for-agencies/sections/plugins/plugins-jetpack-banner.tsx
@@ -10,7 +10,7 @@ export default function A4APluginsJetpackBanner() {
 			<Banner
 				title={ translate( 'Jetpack Required for Plugin Management' ) }
 				description={ translate(
-					'To update plugins, Jetpack must be activated on each site. Your Pressable plan includes Jetpack Complete for free. Activate it to access plugin management in this dashboard.'
+					'To manage plugins, Jetpack must be activated on each site. Your Pressable plan includes Jetpack Complete for free. Activate it to access plugin management in this dashboard.'
 				) }
 				icon={ <JetpackLogo size={ 16 } /> }
 				className="plugins__jetpack-banner"
