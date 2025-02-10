f66825b9bc41abc8edc2595b0a3bac65828506ae
Paulo Trentin <paulo@paulotrentin.com.br>
2025-02-10 17:34:47 -0300

    Improved the logic to handle the CTA for lauching the site: - made it a dedicated button so we can handle the loading state - always hide the last button from the launchpad list - improved the way we deduct the last button from counters

--- client/my-sites/customer-home/components/full-screen-launchpad.tsx
+++ client/my-sites/customer-home/components/full-screen-launchpad.tsx
@@ -1,6 +1,7 @@
 import { CircularProgressBar, ConfettiAnimation } from '@automattic/components';
-import { updateLaunchpadSettings } from '@automattic/data-stores';
-import { Launchpad } from '@automattic/launchpad';
+import { updateLaunchpadSettings, useSortedLaunchpadTasks } from '@automattic/data-stores';
+import { wpcomRequest } from '@automattic/data-stores/src/wpcom-request-controls';
+import { Launchpad, Task } from '@automattic/launchpad';
 import { Button } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
 import { useSelector } from 'react-redux';
@@ -38,6 +39,14 @@ export const FullScreenLaunchpad = ( { onClose }: { onClose: () => void } ): JSX
 	} );
 
 	const onSiteLaunched = async () => {
+		onClose();
+
+		await wpcomRequest( {
+			path: `/sites/${ siteSlug }/launch`,
+			apiVersion: '1.1',
+			method: 'post',
+		} );
+
 		await updateLaunchpadSettings( siteId, {
 			checklist_statuses: { site_launched: true },
 		} );
@@ -59,44 +68,57 @@ export const FullScreenLaunchpad = ( { onClose }: { onClose: () => void } ): JSX
 		dispatch( requestSite( siteId ) );
 	};
 
+	const {
+		data: { checklist },
+	} = useSortedLaunchpadTasks( siteSlug, checklistSlug, launchpadContext );
+
 	if ( isDismissed ) {
 		return null;
 	}
 
+	const launchSiteTask = checklist?.find( ( task: Task ) =>
+		[ 'site_launched', 'blog_launched', 'link_in_bio_launched' ].includes( task.id )
+	);
+
 	const isAllTasksCompleted =
-		hasChecklist && numberOfSteps > 0 && completedSteps >= numberOfSteps - 1; // -1 because the last task is the "Launch your site" button
+		hasChecklist &&
+		numberOfSteps > 0 &&
+		completedSteps >= numberOfSteps - ( launchSiteTask ? 1 : 0 );
 
 	return (
-		<div css={ { display: 'flex', flexDirection: 'column', alignItems: 'center' } }>
-			<div className="is-launchpad-first" css={ { width: '100%' } }>
-				<div
-					className={ `customer-home-launchpad customer-home__card is-small-hero ${
-						isAllTasksCompleted ? 'all-tasks-completed' : ''
-					}` }
-				>
-					<div className="customer-home__launchpad-header">
-						<CircularProgressBar
-							size={ 40 }
-							enableDesktopScaling
-							numberOfSteps={ numberOfSteps }
-							currentStep={ completedSteps }
-						/>
-						<h2>
-							{ ! isAllTasksCompleted ? __( "Let's get started!" ) : __( "You're all set!" ) }
-						</h2>
-						<span>{ ! isAllTasksCompleted && launchpadTitle }</span>
-					</div>
-					{ isAllTasksCompleted && <ConfettiAnimation /> }
-					<Launchpad
-						siteSlug={ siteSlug }
-						checklistSlug={ checklistSlug }
-						launchpadContext={ launchpadContext }
-						onSiteLaunched={ onSiteLaunched }
-						highlightNextAction
+		<div className="is-launchpad-first" css={ { width: '100%' } }>
+			<div
+				className={ `customer-home-launchpad customer-home__card is-small-hero ${
+					isAllTasksCompleted ? 'all-tasks-completed' : ''
+				}` }
+			>
+				<div className="customer-home__launchpad-header">
+					<CircularProgressBar
+						size={ 40 }
+						enableDesktopScaling
+						numberOfSteps={ numberOfSteps - ( launchSiteTask ? 1 : 0 ) }
+						currentStep={ completedSteps }
 					/>
+					<h2>{ ! isAllTasksCompleted ? __( "Let's get started!" ) : __( "You're all set!" ) }</h2>
+					<span>{ ! isAllTasksCompleted && launchpadTitle }</span>
+				</div>
+				{ isAllTasksCompleted && <ConfettiAnimation /> }
+				<Launchpad
+					siteSlug={ siteSlug }
+					checklistSlug={ checklistSlug }
+					launchpadContext={ launchpadContext }
+					onSiteLaunched={ onSiteLaunched }
+					highlightNextAction
+				/>
+				<div className="launchpad-actions">
+					{ launchSiteTask && (
+						<Button onClick={ onSkipLaunchpad } className="launchpad-site-launch" variant="primary">
+							{ launchSiteTask?.title }
+						</Button>
+					) }
+					<Button onClick={ onSkipLaunchpad }>{ __( 'Skip to dashboard' ) }</Button>
 				</div>
 			</div>
-			<Button onClick={ onSkipLaunchpad }>{ __( 'Skip to dashboard' ) }</Button>
 		</div>
 	);
 };
