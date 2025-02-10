8887ff8e6f99ca27a2025a95a78ce5c64facc823
Philip Jackson <p-jackson@live.com>
2025-02-11 11:17:21 +1300

    Use clsx to concat classnames

--- client/my-sites/customer-home/components/full-screen-launchpad.tsx
+++ client/my-sites/customer-home/components/full-screen-launchpad.tsx
@@ -3,6 +3,7 @@ import { updateLaunchpadSettings, useSortedLaunchpadTasks } from '@automattic/da
 import { Launchpad, Task } from '@automattic/launchpad';
 import { Button } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
+import clsx from 'clsx';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import useHomeLayoutQuery from 'calypso/data/home/use-home-layout-query';
@@ -91,9 +92,9 @@ export const FullScreenLaunchpad = ( { onClose }: { onClose: () => void } ): JSX
 	return (
 		<div className="is-launchpad-first" css={ { width: '100%' } }>
 			<div
-				className={ `customer-home-launchpad customer-home__card is-small-hero ${
-					isAllTasksCompleted ? 'all-tasks-completed' : ''
-				}` }
+				className={ clsx( `customer-home-launchpad customer-home__card is-small-hero`, {
+					'all-tasks-completed': isAllTasksCompleted,
+				} ) }
 			>
 				<div className="customer-home__launchpad-header">
 					<CircularProgressBar
