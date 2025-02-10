29874e3d9258f80e4c8dab894978f48c3b9625c1
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-02-10 14:11:14 +0200

    Components: Move AppPromoCard to client (#99483)

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -45,7 +45,6 @@ export { default as CountComparisonCard } from './highlight-cards/count-comparis
 export { default as AnnualHighlightCards } from './highlight-cards/annual-highlight-cards';
 export { default as NoticeBanner } from './notice-banner';
 export { default as MobileHighlightCardListing } from './highlight-cards/mobile-highlight-cards';
-export { default as AppPromoCard } from './app-promo-card';
 export { default as PostStatsCard } from './post-stats-card';
 export { default as ConfettiAnimation } from './confetti';
 export { default as ComponentSwapper } from './component-swapper';
