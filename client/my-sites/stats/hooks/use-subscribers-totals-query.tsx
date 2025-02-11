e4a7e445e9bb941e16ef7a7f3c057a6c9b8049e2
Jasper Kang <jasper.kang@automattic.com>
2025-02-12 11:56:50 +1300

    Stats: use subscribers_by_user_type for email/wpcom subs (#99573)
    * use subscribers_by_user_type for email/wpcom subs
    
    * switch follower module to use subscribers api
    
    * add in comment
    
    * add back is_owner_subscribing
    
    * owner should exist in wpcom sub list
    
    * being compatible with Odyssey Stats
    
    * fix subscribers empty issue
    
    * revert unnecessary changes
    
    * Update client/my-sites/stats/hooks/use-subscribers-totals-query.tsx
    
    * Update client/my-sites/stats/hooks/use-subscribers-totals-query.tsx
    
    * do not swallow the errors
    
    * skip stats/followers query for en.blog.wp.com

--- client/my-sites/stats/hooks/use-subscribers-totals-query.tsx
+++ client/my-sites/stats/hooks/use-subscribers-totals-query.tsx
@@ -1,8 +1,20 @@
+import config from '@automattic/calypso-config';
 import { useQueries } from '@tanstack/react-query';
 import wpcom from 'calypso/lib/wp';
+import { parseAvatar } from 'calypso/state/stats/lists/utils';
 import getDefaultQueryParams from './default-query-params';
 
+const MAX_SUBSCRIBERS_TO_RETURN = 10;
+const isJetpackApi = config.isEnabled( 'is_running_in_jetpack_site' );
+const sortByDateDesc = ( a: { date_subscribed: string }, b: { date_subscribed: string } ) => {
+	return new Date( b.date_subscribed ).getTime() - new Date( a.date_subscribed ).getTime();
+};
+
 const querySubscribersTotals = ( siteId: number | null, filterAdmin?: boolean ): Promise< any > => {
+	// Skip the query for en.blog.wordpress.com as it's blocked.
+	if ( siteId === 3584907 ) {
+		return {} as any;
+	}
 	return wpcom.req.get(
 		{
 			path: `/sites/${ siteId }/stats/followers`,
@@ -11,7 +23,31 @@ const querySubscribersTotals = ( siteId: number | null, filterAdmin?: boolean ):
 			type: 'all',
 			filter_admin: filterAdmin ? true : false,
 			// Only one-page results adjust visible subscribers with deleted accounts to align with the subscriber list.
-			max: 10,
+			max: MAX_SUBSCRIBERS_TO_RETURN,
+		}
+	);
+};
+
+const querySubscribersTotalByType = (
+	siteId: number | null,
+	user_type: 'email' | 'wpcom',
+	filterAdmin?: boolean
+): Promise< any > => {
+	// Return early to avoid 404.
+	if ( isJetpackApi ) {
+		return {} as any;
+	}
+	return wpcom.req.get(
+		{
+			apiNamespace: 'wpcom/v2',
+			path: `/sites/${ siteId }/subscribers_by_user_type`,
+		},
+		{
+			per_page: MAX_SUBSCRIBERS_TO_RETURN,
+			page: 1,
+			user_type,
+			filter_admin: filterAdmin, //not used.
+			sort: 'date_subscribed',
 		}
 	);
 };
@@ -28,12 +64,39 @@ const selectSubscribers = ( payload: {
 	total_email: number;
 	total_wpcom: number;
 	is_owner_subscribing: boolean;
+	subscribers: {
+		label?: string;
+		date_subscribed: string;
+		display_name: string;
+		avatar: string;
+		url: string;
+		follow_data: { params: object }; //Empty object atm
+	}[];
 } ) => {
 	return {
 		total: payload.total,
 		total_email: payload.total_email,
 		total_wpcom: payload.total_wpcom,
 		is_owner_subscribing: payload.is_owner_subscribing,
+		subscribers: payload.subscribers?.map( ( item ) => {
+			return {
+				label: item.label ?? item.display_name,
+				iconClassName: 'avatar-user',
+				icon: parseAvatar( item.avatar ),
+				link: item.url,
+				value: {
+					type: 'relative-date',
+					value: item.date_subscribed,
+				},
+				actions: [
+					{
+						type: 'follow',
+						data: item.follow_data ? item.follow_data.params : false,
+					},
+				],
+				date_subscribed: item.date_subscribed,
+			};
+		} ),
 	};
 };
 
@@ -57,11 +120,11 @@ export function useSubscribersTotalsWithoutAdminQueries( siteId: number | null )
 }
 
 function useSubscribersTotalsQueries( siteId: number | null, filterAdmin?: boolean ) {
-	const queries = useQueries( {
+	const results = useQueries( {
 		queries: [
 			{
 				...getDefaultQueryParams(),
-				queryKey: [ 'stats', 'totals', 'subscribers', siteId, filterAdmin ],
+				queryKey: [ 'stats', 'totals', 'subscribers', 'all', siteId, filterAdmin ],
 				queryFn: () => querySubscribersTotals( siteId, filterAdmin ),
 				select: selectSubscribers,
 				staleTime: 1000 * 60 * 5, // 5 minutes
@@ -73,25 +136,69 @@ function useSubscribersTotalsQueries( siteId: number | null, filterAdmin?: boole
 				select: selectPaidSubscribers,
 				staleTime: 1000 * 60 * 5, // 5 minutes
 			},
+			{
+				...getDefaultQueryParams(),
+				queryKey: [ 'stats', 'totals', 'subscribers', 'wpcom', siteId, filterAdmin ],
+				queryFn: () => querySubscribersTotalByType( siteId, 'wpcom', filterAdmin ),
+				select: selectSubscribers,
+				staleTime: 1000 * 60 * 5, // 5 minutes
+			},
+			{
+				...getDefaultQueryParams(),
+				queryKey: [ 'stats', 'totals', 'subscribers', 'email', siteId, filterAdmin ],
+				queryFn: () => querySubscribersTotalByType( siteId, 'email', filterAdmin ),
+				select: selectSubscribers,
+				staleTime: 1000 * 60 * 5, // 5 minutes
+			},
 		],
 	} );
 
+	if ( isJetpackApi ) {
+		// `subscribers_by_user_type` endpoint is not available for Odyssey Stats yet.
+		return {
+			data: {
+				total_email: results[ 0 ]?.data?.total_email,
+				total_wpcom: results[ 0 ]?.data?.total_wpcom,
+				total: results[ 0 ]?.data?.total,
+				paid_subscribers: results[ 1 ]?.data?.paid_subscribers,
+				free_subscribers:
+					results[ 1 ]?.data?.email_subscribers !== undefined &&
+					results[ 1 ]?.data?.paid_subscribers !== undefined
+						? results[ 1 ].data.email_subscribers - results[ 1 ].data.paid_subscribers
+						: null,
+				social_followers: results[ 1 ]?.data?.social_followers,
+				is_owner_subscribing: results[ 0 ]?.data?.is_owner_subscribing,
+				subscribers: ( results[ 0 ]?.data?.subscribers ?? [] ).sort( sortByDateDesc ),
+			},
+			isLoading: results.some( ( result ) => result.isPending ),
+			isError: results.some( ( result ) => result.isError ),
+		};
+	}
+
+	// Use `subscribers_by_user_type` endpoint in Calypso Stats.
 	return {
 		data: {
-			total_email: queries[ 0 ]?.data?.total_email,
-			total_wpcom: queries[ 0 ]?.data?.total_wpcom,
-			total: queries[ 0 ]?.data?.total,
-			paid_subscribers: queries[ 1 ]?.data?.paid_subscribers,
+			total_email: results[ 3 ]?.data?.total,
+			total_wpcom: results[ 2 ]?.data?.total,
+			total: results[ 1 ].data?.email_subscribers,
+			paid_subscribers: results[ 1 ]?.data?.paid_subscribers,
 			free_subscribers:
-				queries[ 1 ]?.data?.email_subscribers !== undefined &&
-				queries[ 1 ]?.data?.paid_subscribers !== undefined
-					? queries[ 1 ].data.email_subscribers - queries[ 1 ].data.paid_subscribers
+				results[ 1 ]?.data?.email_subscribers !== undefined &&
+				results[ 1 ]?.data?.paid_subscribers !== undefined
+					? results[ 1 ].data.email_subscribers - results[ 1 ].data.paid_subscribers
 					: null,
-			social_followers: queries[ 1 ]?.data?.social_followers,
-			is_owner_subscribing: queries[ 0 ]?.data?.is_owner_subscribing,
+			social_followers: results[ 1 ]?.data?.social_followers,
+			is_owner_subscribing: results[ 2 ]?.data?.is_owner_subscribing,
+			// Merge email and wpcom subscribers and sort by date_subscribed, and only shows the most recent 10 subscribers.
+			subscribers: [
+				...( results[ 3 ]?.data?.subscribers ?? [] ),
+				...( results[ 2 ]?.data?.subscribers ?? [] ),
+			]
+				.sort( sortByDateDesc )
+				.slice( 0, MAX_SUBSCRIBERS_TO_RETURN ),
 		},
-		isLoading: queries.some( ( result ) => result.isLoading ),
-		isError: queries.some( ( result ) => result.isError ),
+		isLoading: results.some( ( result ) => result.isLoading ),
+		isError: results.some( ( result ) => result.isError ),
 	};
 }
 
--- client/my-sites/stats/stats-followers/index.jsx
+++ client/my-sites/stats/stats-followers/index.jsx
@@ -2,26 +2,18 @@ import { isEnabled } from '@automattic/calypso-config';
 import { localizeUrl } from '@automattic/i18n-utils';
 import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
-import { get } from 'lodash';
 import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import QuerySiteStats from 'calypso/components/data/query-site-stats';
 import isAtomicSite from 'calypso/state/selectors/is-site-wpcom-atomic';
 import { getSiteSlug, isAdminInterfaceWPAdmin, isJetpackSite } from 'calypso/state/sites/selectors';
-import {
-	isRequestingSiteStatsForQuery,
-	getSiteStatsNormalizedData,
-	hasSiteStatsQueryFailed,
-} from 'calypso/state/stats/lists/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { SUBSCRIBERS_SUPPORT_URL } from '../const';
+import useSubscribersTotalsQueries from '../hooks/use-subscribers-totals-query';
 import ErrorPanel from '../stats-error';
 import StatsListCard from '../stats-list/stats-list-card';
 import StatsModulePlaceholder from '../stats-module/placeholder';
 
 import './style.scss';
-
-const MAX_FOLLOWERS_TO_SHOW = 10;
 
 const StatModuleFollowers = ( { className } ) => {
 	const translate = useTranslate();
@@ -33,29 +25,7 @@ const StatModuleFollowers = ( { className } ) => {
 	const isJetpack = useSelector( ( state ) => isJetpackSite( state, siteId ) );
 	const isAdminInterface = useSelector( ( state ) => isAdminInterfaceWPAdmin( state, siteId ) );
 
-	// Query objects
-	const emailQuery = { type: 'email', max: 10 };
-	const wpcomQuery = { type: 'wpcom', max: 10 };
-
-	// Stats data and loading states
-	const requestingEmailFollowers = useSelector( ( state ) =>
-		isRequestingSiteStatsForQuery( state, siteId, 'statsFollowers', emailQuery )
-	);
-	const emailData = useSelector( ( state ) =>
-		getSiteStatsNormalizedData( state, siteId, 'statsFollowers', emailQuery )
-	);
-	const hasEmailQueryFailed = useSelector( ( state ) =>
-		hasSiteStatsQueryFailed( state, siteId, 'statsFollowers', emailQuery )
-	);
-	const requestingWpcomFollowers = useSelector( ( state ) =>
-		isRequestingSiteStatsForQuery( state, siteId, 'statsFollowers', wpcomQuery )
-	);
-	const wpcomData = useSelector( ( state ) =>
-		getSiteStatsNormalizedData( state, siteId, 'statsFollowers', wpcomQuery )
-	);
-	const hasWpcomQueryFailed = useSelector( ( state ) =>
-		hasSiteStatsQueryFailed( state, siteId, 'statsFollowers', wpcomQuery )
-	);
+	const { data: subTotals, isLoading, isError: hasError } = useSubscribersTotalsQueries( siteId );
 
 	const calculateOffset = useCallback(
 		( pastValue ) => {
@@ -64,7 +34,7 @@ const StatModuleFollowers = ( { className } ) => {
 			const difference = now.getTime() - value.getTime();
 
 			const seconds = Math.floor( difference / 1000 );
-			const minutes = Math.floor( seconds / 60 );
+			const minutes = Math.ceil( seconds / 60 );
 			const hours = Math.floor( minutes / 60 );
 			const days = Math.floor( hours / 24 );
 
@@ -83,12 +53,7 @@ const StatModuleFollowers = ( { className } ) => {
 		[ translate ]
 	);
 
-	const isLoading = requestingWpcomFollowers || requestingEmailFollowers;
-	const hasEmailFollowers = !! get( emailData, 'subscribers', [] ).length;
-	const hasWpcomFollowers = !! get( wpcomData, 'subscribers', [] ).length;
-	const noData = ! hasWpcomFollowers && ! hasEmailFollowers;
-	const hasError = hasEmailQueryFailed || hasWpcomQueryFailed;
-
+	const noData = ! subTotals.subscribers.length;
 	const summaryPageSlug = siteSlug || '';
 	const useJetpackCloudLinks =
 		isAtomic || isJetpack || ( isEnabled( 'jetpack/manage-simple-sites' ) && isAdminInterface );
@@ -96,65 +61,50 @@ const StatModuleFollowers = ( { className } ) => {
 		? `https://cloud.jetpack.com/subscribers/${ summaryPageSlug }`
 		: `https://wordpress.com/subscribers/${ summaryPageSlug }`;
 
-	// Combine data sets, sort by recency, and limit to 10
-	const data = [ ...( wpcomData?.subscribers ?? [] ), ...( emailData?.subscribers ?? [] ) ]
-		.sort( ( a, b ) => {
-			return new Date( b.value?.value || 0 ) - new Date( a.value?.value || 0 );
-		} )
-		.slice( 0, MAX_FOLLOWERS_TO_SHOW );
-
 	return (
-		<>
-			{ siteId && (
-				<QuerySiteStats statType="statsFollowers" siteId={ siteId } query={ wpcomQuery } />
-			) }
-			{ siteId && (
-				<QuerySiteStats statType="statsFollowers" siteId={ siteId } query={ emailQuery } />
-			) }
-			<StatsListCard
-				moduleType="followers"
-				data={ data.map( ( dataPoint ) => ( {
-					...dataPoint,
-					value: calculateOffset( dataPoint.value?.value ),
-				} ) ) }
-				usePlainCard
-				hasNoBackground
-				title={ translate( 'Subscribers' ) }
-				emptyMessage={ translate(
-					'Once you get a few, {{link}}your subscribers{{/link}} will appear here.',
-					{
-						comment: '{{link}} links to support documentation.',
-						components: {
-							link: (
-								<a
-									target="_blank"
-									rel="noreferrer"
-									href={ localizeUrl( `${ SUBSCRIBERS_SUPPORT_URL }#subscriber-stats` ) }
-								/>
-							),
-						},
-						context: 'Stats: Info box label when the Subscribers module is empty',
-					}
-				) }
-				mainItemLabel={ translate( 'Subscriber' ) }
-				metricLabel={ translate( 'Since' ) }
-				splitHeader
-				showMore={ {
-					url: subscriberManagementUrl,
-					label: translate( 'Manage subscribers' ),
-				} }
-				error={
-					noData &&
-					! hasError &&
-					! isLoading && (
-						<ErrorPanel className="is-empty-message" message={ translate( 'No subscribers' ) } />
-					)
+		<StatsListCard
+			moduleType="followers"
+			data={ subTotals.subscribers.map( ( dataPoint ) => ( {
+				...dataPoint,
+				value: calculateOffset( dataPoint.value?.value ),
+			} ) ) }
+			usePlainCard
+			hasNoBackground
+			title={ translate( 'Subscribers' ) }
+			emptyMessage={ translate(
+				'Once you get a few, {{link}}your subscribers{{/link}} will appear here.',
+				{
+					comment: '{{link}} links to support documentation.',
+					components: {
+						link: (
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href={ localizeUrl( `${ SUBSCRIBERS_SUPPORT_URL }#subscriber-stats` ) }
+							/>
+						),
+					},
+					context: 'Stats: Info box label when the Subscribers module is empty',
 				}
-				loader={ isLoading && <StatsModulePlaceholder isLoading={ isLoading } /> }
-				className={ clsx( 'stats__modernised-followers', className ) }
-				showLeftIcon
-			/>
-		</>
+			) }
+			mainItemLabel={ translate( 'Subscriber' ) }
+			metricLabel={ translate( 'Since' ) }
+			splitHeader
+			showMore={ {
+				url: subscriberManagementUrl,
+				label: translate( 'Manage subscribers' ),
+			} }
+			error={
+				noData &&
+				! hasError &&
+				! isLoading && (
+					<ErrorPanel className="is-empty-message" message={ translate( 'No subscribers' ) } />
+				)
+			}
+			loader={ isLoading && <StatsModulePlaceholder isLoading={ isLoading } /> }
+			className={ clsx( 'stats__modernised-followers', className ) }
+			showLeftIcon
+		/>
 	);
 };
 
