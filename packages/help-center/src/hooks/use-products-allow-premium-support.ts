d2cea4ba8bb5c5e4a024b80a766aca9d4047c7e7
Emanuele Buccelli <escapemanuele@gmail.com>
2025-02-11 17:35:12 +0100

    Help Center:  DIFM users can access premium support (#99591)
    * Add DIFM to premium for free users
    
    * Use constant

--- packages/help-center/src/hooks/use-products-allow-premium-support.ts
+++ packages/help-center/src/hooks/use-products-allow-premium-support.ts
@@ -1,10 +1,11 @@
-import { PLAN_100_YEARS } from '@automattic/calypso-products';
+import { isDIFMProduct, PLAN_100_YEARS } from '@automattic/calypso-products';
 import { ResponseCartProduct } from '@automattic/shopping-cart';
 
 export function useProductsAllowPremiumSupport( products: ResponseCartProduct[] ) {
 	const productHasPremiumSupport = ( product: ResponseCartProduct ) => {
 		switch ( true ) {
-			case product?.product_slug === PLAN_100_YEARS:
+			case isDIFMProduct( product ):
+			case PLAN_100_YEARS === product?.product_slug:
 				return true;
 			case product?.extra?.is_hundred_year_domain:
 				return true;
