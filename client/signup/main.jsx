173712dc641353a8add7096e98e40e081bab1a3d
Dusty Reagan <dusty@dustyreagan.com>
2025-02-11 11:04:33 -0600

    When reader and user-social flow is complete set Reader as users landing page (#99565)

--- client/signup/main.jsx
+++ client/signup/main.jsx
@@ -384,7 +384,11 @@ class Signup extends Component {
 
 		if ( flow.postCompleteCallback ) {
 			const siteId = dependencies && dependencies.siteId;
-			await flow.postCompleteCallback( { siteId, flowName: this.props.flowName } );
+			await flow.postCompleteCallback( {
+				siteId,
+				flowName: this.props.flowName,
+				dispatch: this.props.dispatch,
+			} );
 		}
 	};
 
@@ -620,7 +624,7 @@ class Signup extends Component {
 			dependencies.oauth2_client_id && ! progress?.[ 'oauth2-user' ]?.service; // service is set for social signup (e.g. Google, Apple)
 		// If the user is not logged in, we need to log them in first.
 		// And if it's regular oauth client signup, we perform the oauth login because the WPCC user creation code automatically logs the user in.
-		// There’s no need to turn the bearer token into a cookie. If we log user in again, it will cause an activation error.
+		// There's no need to turn the bearer token into a cookie. If we log user in again, it will cause an activation error.
 		// However, we need to skip this to perform a regular login for social sign in.
 		if ( ! userIsLoggedIn && ( config.isEnabled( 'oauth' ) || isRegularOauth2ClientSignup ) ) {
 			debug( `Handling oauth login` );
@@ -990,9 +994,10 @@ export default connect(
 			hostingFlow,
 		};
 	},
-	{
+	( dispatch ) => ( {
+		dispatch,
 		submitSignupStep,
 		removeStep,
 		addStep,
-	}
+	} )
 )( Signup );
