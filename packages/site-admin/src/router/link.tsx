7fde41d5315cac415e8123d832c5b3a506e51b72
Damián Suárez <rdsuarez@gmail.com>
2025-02-11 16:51:49 +0000

    Introduce router libraries This first implementation is based (copied) from the core implementation

---
+++ packages/site-admin/src/router/link.tsx
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import { useContext, useMemo } from '@wordpress/element';
+import { getQueryArgs, getPath, buildQueryString } from '@wordpress/url';
+/**
+ * Internal dependencies
+ */
+import { ConfigContext, useHistory, type NavigationOptions } from './router';
+
+export function useLink( to: string, options: NavigationOptions = {} ) {
+	const history = useHistory();
+	const { pathArg, beforeNavigate } = useContext( ConfigContext );
+	function onClick( event: React.SyntheticEvent< HTMLAnchorElement > ) {
+		event?.preventDefault();
+		history.navigate( to, options );
+	}
+	const query = getQueryArgs( to );
+
+	// @Todo: @unstable: TS fix -> path should be string
+	const path = getPath( 'http://domain.com/' + to ) || '';
+	const link = useMemo( () => {
+		return beforeNavigate ? beforeNavigate( { path, query } ) : { path, query };
+	}, [ path, query, beforeNavigate ] );
+
+	const [ before ] = window.location.href.split( '?' );
+
+	return {
+		href: `${ before }?${ buildQueryString( {
+			[ pathArg ]: link.path,
+			...link.query,
+		} ) }`,
+		onClick,
+	};
+}
