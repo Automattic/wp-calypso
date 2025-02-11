2e7413ef0c14e23d93dca161275877544c7631e9
Christos <chriskmnds@gmail.com>
2025-02-11 18:33:31 +0200

    cleanup. translate is a declared const not type

--- client/lib/promote-post/index.ts
+++ client/lib/promote-post/index.ts
@@ -3,6 +3,7 @@ import { initSentry, captureException } from '@automattic/calypso-sentry';
 import { loadScript } from '@automattic/load-script';
 import { __ } from '@wordpress/i18n';
 import debugFactory from 'debug';
+import { translate } from 'i18n-calypso';
 import { Dispatch } from 'redux';
 import { getHotjarSiteSettings, mayWeLoadHotJarScript } from 'calypso/lib/analytics/hotjar';
 import { getMobileDeviceInfo, isWcMobileApp, isWpMobileApp } from 'calypso/lib/mobile-app';
@@ -18,7 +19,6 @@ import {
 	isJetpackMinimumVersion,
 } from 'calypso/state/sites/selectors';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
-import type { translate } from 'i18n-calypso';
 
 const debug = debugFactory( 'calypso:promote-post' );
 
@@ -52,7 +52,7 @@ declare global {
 				originProps?: DSPOriginProps;
 				onLoaded?: () => void;
 				onClose?: () => void;
-				translateFn?: translate;
+				translateFn?: typeof translate;
 				localizeUrlFn?: ( fullUrl: string ) => string;
 				locale?: string;
 				showDialog?: boolean;
