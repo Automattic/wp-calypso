53cac8e39cd3024f6e2586fab31c8583fb3f6b9a
AllTerrainDeveloper <daniel.lopez@automattic.com>
2025-02-12 13:13:07 +0100

    Refetch Interval for query receiging the Zendesk status

--- packages/zendesk-client/src/use-can-connect-to-zendesk-messaging.ts
+++ packages/zendesk-client/src/use-can-connect-to-zendesk-messaging.ts
@@ -1,18 +1,30 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { useQuery } from '@tanstack/react-query';
+
+async function fetchZendeskConfig(): Promise< boolean > {
+	const config = await fetch( 'https://wpcom.zendesk.com/embeddable/config' );
+	const validResponse = config.ok && config.status === 200;
+
+	if ( ! validResponse ) {
+		recordTracksEvent( 'calypso_helpcenter_zendesk_config_error', {
+			status: config.status,
+			statusText: config.statusText,
+		} );
+	}
+
+	return validResponse;
+}
 
 export function useCanConnectToZendeskMessaging( enabled = true ) {
 	return useQuery( {
 		queryKey: [ 'canConnectToZendesk' ],
-		queryFn: async () => {
-			const config = await fetch( 'https://wpcom.zendesk.com/embeddable/config' );
-
-			return config.ok;
-		},
+		queryFn: fetchZendeskConfig,
 		staleTime: Infinity,
 		retry: false,
 		refetchOnMount: false,
 		retryOnMount: false,
 		refetchOnWindowFocus: false,
+		refetchInterval: 180000,
 		meta: {
 			persist: false,
 		},
