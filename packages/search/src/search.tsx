c50b7fbd21a1dc4ea14086491c046f1773ff968e
Dehan <dehandecroos@gmail.com>
2025-02-11 19:18:34 -0500

    Fix bugs

--- packages/search/src/search.tsx
+++ packages/search/src/search.tsx
@@ -146,6 +146,8 @@ const InnerSearch = (
 		maxLength,
 		hideClose = false,
 		searchMode = 'when-typing',
+		searchIcon,
+		submitOnOpenIconClick = false,
 	}: Props,
 	forwardedRef: Ref< ImperativeHandle >
 ) => {
@@ -220,6 +222,15 @@ const InnerSearch = (
 		onSearchChange?.( keyword );
 	}, [ keyword ] );
 
+	const openSearch = ( event: KeyboardOrMouseEvent ) => {
+		event.preventDefault();
+
+		setKeyword( '' );
+		setIsOpen( true );
+
+		recordEvent?.( 'Clicked Open Search' );
+	};
+
 	const closeSearch = ( event: KeyboardOrMouseEvent ) => {
 		event.preventDefault();
 
@@ -266,6 +277,7 @@ const InnerSearch = (
 	}, [ isOpen ] );
 
 	const closeListener = keyListener( closeSearch );
+	const openListener = keyListener( openSearch );
 
 	const scrollOverlay = () => {
 		window.requestAnimationFrame( () => {
@@ -381,24 +393,61 @@ const InnerSearch = (
 	};
 
 	const renderOpenIcon = () => {
-		const searchIcon = (
-			<svg viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path
-					fillRule="evenodd"
-					clipRule="evenodd"
-					d="M13.8269 19.8984L8.49362 24.5651L7.50586 23.4362L12.8392 18.7695L13.8269 19.8984Z"
-				/>
-				<path
-					fillRule="evenodd"
-					clipRule="evenodd"
-					d="M17.9994 21.1667C21.221 21.1667 23.8327 18.555 23.8327 15.3333C23.8327 12.1117 21.221 9.5 17.9994 9.5C14.7777 9.5 12.166 12.1117 12.166 15.3333C12.166 18.555 14.7777 21.1667 17.9994 21.1667ZM17.9994 22.6667C22.0494 22.6667 25.3327 19.3834 25.3327 15.3333C25.3327 11.2832 22.0494 8 17.9994 8C13.9493 8 10.666 11.2832 10.666 15.3333C10.666 19.3834 13.9493 22.6667 17.9994 22.6667Z"
-				/>
-			</svg>
+		const enableOpenIcon = pinned && ! isOpen;
+
+		const defaultSearchIcon = (
+			<div className="search-component__left-icon-container">
+				<svg
+					viewBox="0 0 32 32"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					className="search-component__icon-search"
+				>
+					<path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="M13.8269 19.8984L8.49362 24.5651L7.50586 23.4362L12.8392 18.7695L13.8269 19.8984Z"
+					/>
+					<path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="M17.9994 21.1667C21.221 21.1667 23.8327 18.555 23.8327 15.3333C23.8327 12.1117 21.221 9.5 17.9994 9.5C14.7777 9.5 12.166 12.1117 12.166 15.3333C12.166 18.555 14.7777 21.1667 17.9994 21.1667ZM17.9994 22.6667C22.0494 22.6667 25.3327 19.3834 25.3327 15.3333C25.3327 11.2832 22.0494 8 17.9994 8C13.9493 8 10.666 11.2832 10.666 15.3333C10.666 19.3834 13.9493 22.6667 17.9994 22.6667Z"
+					/>
+				</svg>
+			</div>
 		);
+
 		if ( searchIcon ) {
 			return searchIcon;
 		}
-		return <Icon icon={ searchIcon } size={ 32 } className="search-component__icon-search" />;
+
+		const onClick = ( props: React.MouseEvent< HTMLButtonElement > ) => {
+			if ( submitOnOpenIconClick ) {
+				handleSubmit();
+			}
+
+			if ( enableOpenIcon ) {
+				return openSearch( props );
+			}
+
+			return () => searchInput.current?.focus();
+		};
+
+		return (
+			<Button
+				className="search-component__icon-navigation"
+				ref={ openIcon }
+				onClick={ onClick }
+				tabIndex={ enableOpenIcon ? 0 : undefined }
+				onKeyDown={ enableOpenIcon ? openListener : undefined }
+				aria-controls={ 'search-component-' + instanceId }
+				aria-label={ __( 'Open Search', __i18n_text_domain__ ) }
+			>
+				{ ! hideOpenIcon && (
+					<Icon icon={ defaultSearchIcon } className="search-component__open-icon" />
+				) }
+			</Button>
+		);
 	};
 
 	const renderCloseButton = () => {
