4c4be74508f634ac3bd80263e7ca99d7b4ab620f
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-02-12 12:45:17 +0100

    Clear `LINK_IN_BIO_POST_SETUP_FLOW`  usage

--- packages/onboarding/src/utils/flows.ts
+++ packages/onboarding/src/utils/flows.ts
@@ -5,7 +5,6 @@ export const HOSTING_LP_FLOW = 'hosting-start';
 export const NEW_HOSTED_SITE_FLOW = 'new-hosted-site';
 export const NEW_HOSTED_SITE_FLOW_USER_INCLUDED = 'new-hosted-site-user-included';
 export const TRANSFERRING_HOSTED_SITE_FLOW = 'transferring-hosted-site';
-export const LINK_IN_BIO_POST_SETUP_FLOW = 'link-in-bio-post-setup';
 export const CONNECT_DOMAIN_FLOW = 'connect-domain';
 export const IMPORT_FOCUSED_FLOW = 'import-focused';
 export const IMPORT_HOSTED_SITE_FLOW = 'import-hosted-site';
@@ -54,10 +53,7 @@ export const isFreeFlow = ( flowName: string | null ) => {
 
 export const isNewsletterOrLinkInBioFlow = ( flowName: string | null ) => {
 	return Boolean(
-		flowName &&
-			[ NEWSLETTER_FLOW, NEWSLETTER_POST_SETUP_FLOW, LINK_IN_BIO_POST_SETUP_FLOW ].includes(
-				flowName
-			)
+		flowName && [ NEWSLETTER_FLOW, NEWSLETTER_POST_SETUP_FLOW ].includes( flowName )
 	);
 };
 
