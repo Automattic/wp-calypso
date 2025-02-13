9fa94bda2e328b2f9effc20c9b3a21b4657b586c
Jarda Snajdr <jsnajdr@gmail.com>
2025-02-13 12:05:40 +0100

    Notice: convert to functional component (#99637)
    * Notice: convert to functional component
    
    * Start dismiss timeout only when duration > 0
    
    * Convert GlobalNotices to functional, too
    
    * Remove noop default for onDismissClick
    
    * Fix Notice unit tests
    
    * Fix GlobalNotices tests

--- client/components/global-notices/test/index.js
+++ client/components/global-notices/test/index.js
@@ -3,32 +3,32 @@
  */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import deepFreeze from 'deep-freeze';
-import { GlobalNotices } from '..';
+import { useSelector, useDispatch } from 'react-redux';
+import GlobalNotices from '..';
 
-const baseProps = deepFreeze( {
-	storeNotices: [],
-	removeNotice: jest.fn(),
-} );
+jest.mock( 'react-redux', () => ( {
+	useSelector: jest.fn( () => [] ),
+	useDispatch: jest.fn(),
+} ) );
 
 beforeEach( jest.clearAllMocks );
 
 describe( '<GlobalNotices />', () => {
 	test( 'should not render without notices', () => {
-		const { container } = render( <GlobalNotices { ...baseProps } /> );
+		const { container } = render( <GlobalNotices /> );
 		expect( container ).toBeEmptyDOMElement();
 	} );
 
 	test( 'should render notices with the expected structure', () => {
-		const notices = [
+		jest.mocked( useSelector ).mockReturnValue( [
 			{
 				noticeId: 'testing-notice',
 				showDismiss: true,
 				status: 'is-success',
 				text: 'A test notice',
 			},
-		];
-		const { container } = render( <GlobalNotices { ...baseProps } storeNotices={ notices } /> );
+		] );
+		const { container } = render( <GlobalNotices /> );
 		expect( container.firstChild ).toHaveClass( 'global-notices' );
 		expect( container.firstChild ).toHaveAttribute( 'id', 'overlay-notices' );
 		expect( screen.queryAllByRole( 'status' ) ).toHaveLength( 1 );
@@ -36,17 +36,15 @@ describe( '<GlobalNotices />', () => {
 	} );
 
 	test( 'should use provided id', () => {
-		const notices = [
+		jest.mocked( useSelector ).mockReturnValue( [
 			{
 				noticeId: 'testing-notice',
 				showDismiss: true,
 				status: 'is-success',
 				text: 'A test notice',
 			},
-		];
-		const { container } = render(
-			<GlobalNotices { ...baseProps } storeNotices={ notices } id="test-id" />
-		);
+		] );
+		const { container } = render( <GlobalNotices id="test-id" /> );
 		expect( container.firstChild ).toHaveAttribute( 'id', 'test-id' );
 	} );
 
@@ -60,13 +58,20 @@ describe( '<GlobalNotices />', () => {
 				text: 'A test notice',
 			},
 		];
+		jest.mocked( useSelector ).mockReturnValue( notices );
 
-		render( <GlobalNotices { ...baseProps } storeNotices={ notices } id="test-id" /> );
+		const dispatch = jest.fn();
+		jest.mocked( useDispatch ).mockReturnValue( dispatch );
+
+		render( <GlobalNotices id="test-id" /> );
 
 		await userEvent.click( screen.getByRole( 'button' ) );
 
 		expect( notices[ 0 ].onDismissClick ).toHaveBeenCalledTimes( 1 );
-		expect( baseProps.removeNotice ).toHaveBeenCalledTimes( 1 );
-		expect( baseProps.removeNotice ).toHaveBeenCalledWith( 'testing-notice' );
+		expect( dispatch ).toHaveBeenCalledTimes( 1 );
+		expect( dispatch ).toHaveBeenCalledWith( {
+			type: 'NOTICE_REMOVE',
+			noticeId: 'testing-notice',
+		} );
 	} );
 } );
--- client/components/notice/test/index.js
+++ client/components/notice/test/index.js
@@ -2,58 +2,56 @@
  * @jest-environment jsdom
  */
 import { render, screen } from '@testing-library/react';
-import { Notice } from '../index';
+import Notice from '../index';
 
 describe( 'Notice', () => {
-	const translate = ( string ) => string;
-
 	test( 'should output the component', () => {
-		render( <Notice translate={ translate } /> );
+		render( <Notice /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'notice' );
 	} );
 
 	test( 'should have dismiss button when showDismiss passed as true', () => {
-		render( <Notice showDismiss translate={ translate } /> );
+		render( <Notice showDismiss /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-dismissable' );
 	} );
 
 	test( 'should have dismiss button by default if isCompact is false', () => {
-		render( <Notice isCompact={ false } translate={ translate } /> );
+		render( <Notice isCompact={ false } /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-dismissable' );
 	} );
 
 	test( 'should have compact look when isCompact passed as true', () => {
-		render( <Notice isCompact translate={ translate } /> );
+		render( <Notice isCompact /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-compact' );
 	} );
 
 	test( 'should not have dismiss button by default if isCompact is true', () => {
-		render( <Notice isCompact translate={ translate } /> );
+		render( <Notice isCompact /> );
 		expect( screen.queryByRole( 'status' ) ).not.toHaveClass( 'is-dismissable' );
 	} );
 
 	test( 'should have dismiss button when showDismiss is true and isCompact is true', () => {
-		render( <Notice isCompact showDismiss translate={ translate } /> );
+		render( <Notice isCompact showDismiss /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-dismissable' );
 	} );
 
 	test( 'should have proper class for is-info status parameter', () => {
-		render( <Notice status="is-info" translate={ translate } /> );
+		render( <Notice status="is-info" /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-info' );
 	} );
 
 	test( 'should have proper class for is-success status parameter', () => {
-		render( <Notice status="is-success" translate={ translate } /> );
+		render( <Notice status="is-success" /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-success' );
 	} );
 
 	test( 'should have proper class for is-error status parameter', () => {
-		render( <Notice status="is-error" translate={ translate } /> );
+		render( <Notice status="is-error" /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-error' );
 	} );
 
 	test( 'should have proper class for is-warning status parameter', () => {
-		render( <Notice status="is-warning" translate={ translate } /> );
+		render( <Notice status="is-warning" /> );
 		expect( screen.queryByRole( 'status' ) ).toHaveClass( 'is-warning' );
 	} );
 } );
