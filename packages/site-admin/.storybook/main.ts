0cc73e4094de22b5631dcd68119ef14756484d15
Damián Suárez <rdsuarez@gmail.com>
2025-02-13 09:30:10 +0000

    add client assets images into the story public

--- packages/site-admin/.storybook/main.ts
+++ packages/site-admin/.storybook/main.ts
@@ -1,4 +1,4 @@
 const storybookDefaultConfig = require( '@automattic/calypso-storybook' );
-const config = { ...storybookDefaultConfig(), staticDirs: [ './public' ] };
+const config = { ...storybookDefaultConfig(), staticDirs: [ '../../../client/assets/images' ] };
 
 module.exports = config;
