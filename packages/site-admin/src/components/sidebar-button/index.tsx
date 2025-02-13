d28fc342fab025847ef75cd8a2007d2d6f4813ef
Damián Suárez <rdsuarez@gmail.com>
2025-02-13 12:53:13 +0000

    introduce SidebarButton component

---
+++ packages/site-admin/src/components/sidebar-button/index.tsx
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import { Button } from '@wordpress/components';
+import clsx from 'clsx';
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+/**
+ * Types
+ */
+import type { ButtonProps } from '@wordpress/components/src/button/types';
+
+export function SidebarButton( props: ButtonProps ) {
+	return (
+		<Button
+			size="compact"
+			{ ...props }
+			className={ clsx( 'site-admin-sidebar-button', props.className ) }
+		/>
+	);
+}
