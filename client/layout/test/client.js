00e4b8cc37cb2d102c5a653c4f6ff1a097099ec2
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2025-02-14 17:14:06 +1300

    Split test file into separate ssr and client contexts

--- client/layout/test/client.js
+++ client/layout/test/client.js
@@ -1,10 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import accessibleFocus from '@automattic/accessible-focus';
 import { render } from '@testing-library/react';
-import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import LayoutLoggedOut from '../logged-out';
 
-jest.mock( 'calypso/lib/signup/step-actions', () => ( {} ) );
 jest.mock( '@automattic/accessible-focus', () => jest.fn() );
 
 const store = {
@@ -19,24 +21,12 @@ const store = {
 	subscribe: () => {},
 };
 
-describe( 'index', () => {
+describe( 'LayoutLoggedOut', () => {
 	beforeEach( () => {
 		jest.clearAllMocks();
 	} );
 
-	describe( 'when trying to renderToString() LayoutLoggedOut', () => {
-		test( "doesn't throw an exception", () => {
-			expect( () => {
-				renderToString(
-					<Provider store={ store }>
-						<LayoutLoggedOut />
-					</Provider>
-				);
-			} ).not.toThrow();
-		} );
-	} );
-
-	describe( 'when mounting LayoutLoggedOut', () => {
+	describe( 'when mounting', () => {
 		test( 'calls accessibleFocus', () => {
 			render(
 				<Provider store={ store }>
