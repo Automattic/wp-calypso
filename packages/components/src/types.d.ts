e66987e33978932e05524c5ecb454f662a40321d
Lena Morita <lena@jaguchi.com>
2025-02-14 21:47:44 +0900

    Fix build

--- packages/components/src/types.d.ts
+++ packages/components/src/types.d.ts
@@ -4,11 +4,3 @@ declare module '*.svg' {
 }
 
 declare const __i18n_text_domain__: string;
-
-// See: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/f6d4d15522356eba4a0267142834e3abc6b603fc/types/react/index.d.ts#L2580-L2587
-declare module 'csstype' {
-	interface Properties {
-		// Allow any CSS Custom Properties
-		[ index: `--${ string }` ]: unknown;
-	}
-}
