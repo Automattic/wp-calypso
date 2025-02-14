883ebd28166a5578b32e41b8ade3526307748f6f
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2025-02-14 16:55:45 +1300

    Move test

--- client/layout/test/index.js
+++ client/layout/test/index.js
@@ -1,31 +1,50 @@
+import accessibleFocus from '@automattic/accessible-focus';
+import { render } from '@testing-library/react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import LayoutLoggedOut from '../logged-out';
 
 jest.mock( 'calypso/lib/signup/step-actions', () => ( {} ) );
+jest.mock( '@automattic/accessible-focus', () => jest.fn() );
+
+const store = {
+	dispatch: () => {},
+	getState: () => ( {
+		ui: {},
+		notices: {
+			items: {},
+			lastTimeShown: {},
+		},
+	} ),
+	subscribe: () => {},
+};
 
 describe( 'index', () => {
+	beforeEach( () => {
+		jest.clearAllMocks();
+	} );
+
 	describe( 'when trying to renderToString() LayoutLoggedOut', () => {
 		test( "doesn't throw an exception", () => {
 			expect( () => {
 				renderToString(
-					<Provider
-						store={ {
-							dispatch: () => {},
-							getState: () => ( {
-								ui: {},
-								notices: {
-									items: {},
-									lastTimeShown: {},
-								},
-							} ),
-							subscribe: () => {},
-						} }
-					>
+					<Provider store={ store }>
 						<LayoutLoggedOut />
 					</Provider>
 				);
 			} ).not.toThrow();
+		} );
+	} );
+
+	describe( 'when mounting LayoutLoggedOut', () => {
+		test( 'calls accessibleFocus', () => {
+			render(
+				<Provider store={ store }>
+					<LayoutLoggedOut />
+				</Provider>
+			);
+
+			expect( accessibleFocus ).toHaveBeenCalledTimes( 1 );
 		} );
 	} );
 } );
