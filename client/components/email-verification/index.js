27a020e5d994602c88afd7d649585f4f1289bf8f
Kateryna Kodonenko <kateryna@automattic.com>
2025-02-14 11:41:50 +0100

    Ensure that events are tracked

--- client/components/email-verification/index.js
+++ client/components/email-verification/index.js
@@ -1,6 +1,7 @@
 import { removeQueryArgs } from '@wordpress/url';
 import i18n from 'i18n-calypso';
 import { sendVerificationSignal } from 'calypso/lib/user/verification-checker';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { successNotice } from 'calypso/state/notices/actions';
 
 /**
@@ -29,6 +30,14 @@ export default function emailVerification( context, next ) {
 				duration: 10000,
 				button: i18n.translate( 'Update information' ),
 				href: '/domains/manage?site=all&action=edit-contact-email',
+				onClick: () => {
+					context.store.dispatch(
+						recordTracksEvent( 'calypso_domain_contact_email_update_notice_click', {
+							link_text: 'Update information',
+							domain: null,
+						} )
+					);
+				},
 			} );
 			context.store.dispatch( notice );
 		}, 500 ); // A delay is needed here, because the notice state seems to be cleared upon page load
