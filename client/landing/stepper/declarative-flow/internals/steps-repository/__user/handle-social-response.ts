de5d5e2de6dc05abbeb7c8c1d2cef24d6c4e56b7
Philip Jackson <p-jackson@live.com>
2025-02-17 10:52:39 +1300

    Fix inifite calls to `users/social/new` when error occurs

--- client/landing/stepper/declarative-flow/internals/steps-repository/__user/handle-social-response.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/__user/handle-social-response.ts
@@ -35,6 +35,10 @@ export const useHandleSocialResponse = ( flowName: string ) => {
 			id_token: string | null = null,
 			userData: PreSignUpUserData | null
 		) => {
+			if ( ! id_token ) {
+				return;
+			}
+
 			const storedRedirectTo = window.sessionStorage.getItem( 'signup_redirect_to' );
 
 			if ( storedRedirectTo ) {
