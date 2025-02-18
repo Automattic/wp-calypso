5a5acec86e075cfeb40f3899967677001da3288e
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-02-18 18:43:35 +0800

    Fix failing lint

--- client/a8c-for-agencies/sections/marketplace/controller.tsx
+++ client/a8c-for-agencies/sections/marketplace/controller.tsx
@@ -1,4 +1,3 @@
-import { isEnabled } from '@automattic/calypso-config';
 import { type Callback } from '@automattic/calypso-router';
 import page from '@automattic/calypso-router';
 import PageViewTracker from 'calypso/a8c-for-agencies/components/a4a-page-view-tracker';
