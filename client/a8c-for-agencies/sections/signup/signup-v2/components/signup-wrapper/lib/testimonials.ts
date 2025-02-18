2a7f9995e50fc7c76f85be5c1e9d145a9ec636bc
Andrii <lysenkoa.work@gmail.com>
2025-02-18 09:00:34 -0800

    replace image with smaller size

--- client/a8c-for-agencies/sections/signup/signup-v2/components/signup-wrapper/lib/testimonials.ts
+++ client/a8c-for-agencies/sections/signup/signup-v2/components/signup-wrapper/lib/testimonials.ts
@@ -1,5 +1,5 @@
 import { translate } from 'i18n-calypso';
-import TestimonialAvatar1 from 'calypso/assets/images/a8c-for-agencies/signup/testimonial-1.png';
+import TestimonialAvatar1 from 'calypso/assets/images/a8c-for-agencies/signup/testimonial-1.jpg';
 
 export const TESTIMONIALS = [
 	{
