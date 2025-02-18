e62e366fbb625cd9db68b2eec8dedfb7cc67df86
Christos <chriskmnds@gmail.com>
2025-02-18 20:07:56 +0200

    lots of cleanup

--- packages/format-currency/src/index.ts
+++ packages/format-currency/src/index.ts
@@ -13,32 +13,6 @@ const fallbackLocale = 'en';
 const fallbackCurrency = 'USD';
 const geolocationEndpointUrl = 'https://public-api.wordpress.com/geo/';
 
-/**
- * `numberingSystem` is an option to `Intl.NumberFormat` and is available
- * in all major browsers according to
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#options
- * but is not part of the TypeScript types in `es2020`:
- *
- * https://github.com/microsoft/TypeScript/blob/cfd472f7aa5a2010a3115263bf457b30c5b489f3/src/lib/es2020.intl.d.ts#L272
- *
- * However, it is part of the TypeScript types in `es5`:
- *
- * https://github.com/microsoft/TypeScript/blob/cfd472f7aa5a2010a3115263bf457b30c5b489f3/src/lib/es5.d.ts#L4310
- *
- * Apparently calypso uses `es2020` so we cannot use that option here right
- * now. Instead, we will use the unicode extension to the locale, documented
- * here:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/numberingSystem#adding_a_numbering_system_via_the_locale_string
- */
-function addNumberingSystemToLocale( locale: string ): string {
-	// TODO clk numberFormatCurrency this can all go. it's just a static string added to locale
-	const numberingSystem = 'latn';
-	const numberingSystemUnicodeLocaleExtension = `-u-nu-${ numberingSystem }`;
-	const localeWithNumberingSystem = `${ locale }${ numberingSystemUnicodeLocaleExtension }`;
-	return localeWithNumberingSystem;
-}
-
 // TODO clk numberFormatCurrency exported only for tests
 export function createFormatter(): CurrencyFormatter {
 	let defaultLocale: string | undefined = undefined;
@@ -85,7 +59,7 @@ export function createFormatter(): CurrencyFormatter {
 		};
 
 		return getCachedFormatter( {
-			locale: addNumberingSystemToLocale( getLocaleToUse( options ) ),
+			locale: `${ getLocaleToUse( options ) }-u-nu-latn`,
 			options: numberFormatOptions,
 		} );
 	}
