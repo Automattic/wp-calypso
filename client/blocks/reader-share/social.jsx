c49f1c904734ecfe014e6c4d041c09f9d03f8805
Artemio Morales <artemio.morales@a8c.com>
2025-02-18 17:51:35 +0100

    Reader: Update share menu Twitter icon to X icon (#99871)
    * Replace Twitter icon with X icon
    
    * Remove Twitter icon
    
    * Remove hard-coded fill color
    
    * Add comment regarding base URL
    
    * Add logic for centering popup
    
    * Center icons vertically
    
    ---------
    
    Co-authored-by: Ricardo Artemio Morales <artemio.morales@automattic.com>

--- client/blocks/reader-share/social.jsx
+++ client/blocks/reader-share/social.jsx
@@ -3,18 +3,29 @@ import { useTranslate } from 'i18n-calypso';
 import PopoverMenuItem from 'calypso/components/popover-menu/item';
 import PopoverMenuItemClipboard from 'calypso/components/popover-menu/item-clipboard';
 import ReaderFacebookIcon from 'calypso/reader/components/icons/facebook-icon';
-import ReaderTwitterIcon from 'calypso/reader/components/icons/twitter-icon';
+import ReaderXIcon from 'calypso/reader/components/icons/x-icon';
 import ReaderPopoverMenu from 'calypso/reader/components/reader-popover/menu';
 import * as stats from 'calypso/reader/stats';
 import { useDispatch } from 'calypso/state';
 import { infoNotice } from 'calypso/state/notices/actions';
 import { recordReaderTracksEvent } from 'calypso/state/reader/analytics/actions';
 
+const getWindowCenterPosition = ( targetWidth, targetHeight ) => {
+	return {
+		left: window.screenX + ( window.innerWidth / 2 - targetWidth / 2 ),
+		top: window.screenY + ( window.innerHeight / 2 - targetHeight / 2 ),
+	};
+};
+
 /**
  * Local variables
  */
 const actionMap = {
-	twitter( post ) {
+	x( post ) {
+		// Note: Changing the base URL to x.com breaks the sharing of the featured image
+		// when sharing the post; it seems the Twitter API may have been updated in the move
+		// to X. This works well as is, so let's just leave it for now. In the future, we can
+		// consider updating the base URL if this endpoint becomes deprecated or stops working.
 		const baseUrl = new URL( 'https://twitter.com/intent/tweet' );
 		const params = new URLSearchParams( {
 			text: post.title,
@@ -22,9 +33,16 @@ const actionMap = {
 		} );
 		baseUrl.search = params.toString();
 
-		const twitterUrl = baseUrl.href;
+		const xUrl = baseUrl.href;
+		const width = 550;
+		const height = 420;
+		const { left, top } = getWindowCenterPosition( width, height );
 
-		window.open( twitterUrl, 'twitter', 'width=550,height=420,resizeable,scrollbars' );
+		window.open(
+			xUrl,
+			'x',
+			`width=${ width },height=${ height },left=${ left },top=${ top },resizable,scrollbars`
+		);
 	},
 	facebook( post ) {
 		const baseUrl = new URL( 'https://www.facebook.com/sharer.php' );
@@ -36,7 +54,15 @@ const actionMap = {
 
 		const facebookUrl = baseUrl.href;
 
-		window.open( facebookUrl, 'facebook', 'width=626,height=436,resizeable,scrollbars' );
+		const width = 626;
+		const height = 436;
+		const { left, top } = getWindowCenterPosition( width, height );
+
+		window.open(
+			facebookUrl,
+			'facebook',
+			`width=${ width },height=${ height },left=${ left },top=${ top },resizable,scrollbars`
+		);
 	},
 	copy_link() {},
 };
@@ -84,13 +110,13 @@ const ReaderSocialShareSelection = ( props ) => {
 				<span>Facebook</span>
 			</PopoverMenuItem>
 			<PopoverMenuItem
-				action="twitter"
+				action="x"
 				className="reader-share__popover-item"
-				title={ translate( 'Share on Twitter' ) }
+				title={ translate( 'Share on X' ) }
 				focusOnHover={ false }
 			>
-				<ReaderTwitterIcon iconSize={ 20 } />
-				<span>Twitter</span>
+				<ReaderXIcon iconSize={ 20 } />
+				<span>X</span>
 			</PopoverMenuItem>
 			<PopoverMenuItemClipboard
 				action="copy_link"
