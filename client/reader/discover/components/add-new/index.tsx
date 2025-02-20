0452d2bee343891a67131e1141311352f74a2a5e
Ricardo Artemio Morales <artemio.morales@automattic.com>
2025-02-20 14:28:05 +0100

    Remove erroneous class

--- client/reader/discover/components/add-new/index.tsx
+++ client/reader/discover/components/add-new/index.tsx
@@ -18,7 +18,7 @@ const DiscoverAddNew = () => {
 	const needsEmailVerification = isLoggedIn && ! isEmailVerified;
 
 	return (
-		<div className="discover-add-new add-sites-tab">
+		<div className="discover-add-new">
 			<SubscriptionManagerContextProvider portal={ SubscriptionsPortal.Reader }>
 				{ needsEmailVerification && (
 					<Notice
