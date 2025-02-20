3534b474b9af397930b38954baef3b01ba892978
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-02-21 03:09:27 +0500

    🐛 FIX: duplication

--- client/reader/site-subscriptions-manager/not-found-site-subscriptions.tsx
+++ client/reader/site-subscriptions-manager/not-found-site-subscriptions.tsx
@@ -9,19 +9,16 @@ const NotFoundSiteSubscriptions = () => {
 	return (
 		<div className="not-found-site-subscriptions">
 			{ searchTerm && searchTerm.length
-				? translate(
-						"You're not subscribed to any matching sites. Here are some other sites related to your search.",
-						{
-							comment:
-								"When users type something into the search field of their site subscriptions manager in Reader, they'll see this message if their search doesn't find any of the websites they're currently subscribed to.",
-						}
-				  )
+				? translate( "You're not subscribed to any matching sites.", {
+						comment:
+							"When users type something into the search field of their site subscriptions manager in Reader, they'll see this message if their search doesn't find any of the websites they're currently subscribed to.",
+				  } )
 				: translate( 'No results found.', {
 						comment:
 							"When users type something into the search field of their site subscriptions manager in Reader, they'll see this message if their search doesn't find any of the websites they're currently subscribed to.",
 				  } ) }{ ' ' }
 			{ ( readFeedSearch?.feedItems.length ?? 0 ) > 0 &&
-				translate( 'Here are some other sites that match your search.' ) }
+				translate( 'Here are some other sites related to your search.' ) }
 		</div>
 	);
 };
