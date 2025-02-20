80acb2414938169130fca0317571dfcaa14942cc
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-02-20 18:14:12 +0500

    Reader: Makes navigation header styling consistent across all screens (#99959)
    Makes reader navigation header styling consitent. This involves removing stylings from multiple places and keeping them in a common place so that we can apply it consistently.

--- client/reader/list-stream/header.jsx
+++ client/reader/list-stream/header.jsx
@@ -1,5 +1,4 @@
-import { Card, Gridicon, Button } from '@automattic/components';
-import clsx from 'clsx';
+import { Gridicon, Button } from '@automattic/components';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import FollowButton from 'calypso/blocks/follow-button/button';
@@ -9,7 +8,6 @@ import ReaderFollowFeedIcon from 'calypso/reader/components/icons/follow-feed-ic
 import ReaderFollowingFeedIcon from 'calypso/reader/components/icons/following-feed-icon';
 
 const ListStreamHeader = ( {
-	isPlaceholder,
 	isPublic,
 	title,
 	description,
@@ -20,47 +18,38 @@ const ListStreamHeader = ( {
 	onFollowToggle,
 	translate,
 } ) => {
-	const classes = clsx( {
-		'list-stream__header': true,
-		'is-placeholder': isPlaceholder,
-		'has-description': !! description,
-	} );
-
 	return (
-		<Card className={ classes }>
-			<NavigationHeader title={ title } subtitle={ description }>
-				{ ! isPublic && (
-					<div className="list-stream__header-title-privacy">
-						<Gridicon icon="lock" size={ 24 } title={ translate( 'Private list' ) } />
-					</div>
-				) }
+		<NavigationHeader title={ title } subtitle={ description }>
+			{ ! isPublic && (
+				<div className="list-stream__header-title-privacy">
+					<Gridicon icon="lock" size={ 24 } title={ translate( 'Private list' ) } />
+				</div>
+			) }
 
-				{ showFollow && (
-					<div className="list-stream__header-follow">
-						<FollowButton
-							iconSize={ 24 }
-							following={ following }
-							onFollowToggle={ onFollowToggle }
-							followIcon={ ReaderFollowFeedIcon( { iconSize: 24 } ) }
-							followingIcon={ ReaderFollowingFeedIcon( { iconSize: 24 } ) }
-						/>
-					</div>
-				) }
+			{ showFollow && (
+				<div className="list-stream__header-follow">
+					<FollowButton
+						iconSize={ 24 }
+						following={ following }
+						onFollowToggle={ onFollowToggle }
+						followIcon={ ReaderFollowFeedIcon( { iconSize: 24 } ) }
+						followingIcon={ ReaderFollowingFeedIcon( { iconSize: 24 } ) }
+					/>
+				</div>
+			) }
 
-				{ showEdit && editUrl && (
-					<div className="list-stream__header-edit">
-						<Button rel={ isExternal( editUrl ) ? 'external' : '' } href={ editUrl }>
-							{ translate( 'Edit' ) }
-						</Button>
-					</div>
-				) }
-			</NavigationHeader>
-		</Card>
+			{ showEdit && editUrl && (
+				<div className="list-stream__header-edit">
+					<Button rel={ isExternal( editUrl ) ? 'external' : '' } href={ editUrl }>
+						{ translate( 'Edit' ) }
+					</Button>
+				</div>
+			) }
+		</NavigationHeader>
 	);
 };
 
 ListStreamHeader.propTypes = {
-	isPlaceholder: PropTypes.bool,
 	isPublic: PropTypes.bool,
 	title: PropTypes.string,
 	description: PropTypes.string,
