d02e8bd37b560bf091a3d81b29b73c23f581be36
DustyReagan <dusty@dustyreagan.com>
2025-02-20 10:04:27 -0600

    Use dispatch instead of connect

--- client/signup/steps/set-reader-landing/index.tsx
+++ client/signup/steps/set-reader-landing/index.tsx
@@ -1,21 +1,23 @@
 import { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'calypso/state';
 import { savePreference } from 'calypso/state/preferences/actions';
 import { READER_AS_LANDING_PAGE_PREFERENCE } from 'calypso/state/sites/selectors/has-reader-as-landing-page';
 
 interface Props {
 	submitSignupStep: ( args: { stepName: string } ) => void;
-	saveReaderPreference: () => void;
 	goToNextStep: () => void;
 }
 
-const SetReaderLanding = ( {
-	submitSignupStep,
-	saveReaderPreference,
-	goToNextStep,
-}: Props ): null => {
+const SetReaderLanding = ( { submitSignupStep, goToNextStep }: Props ): null => {
+	const dispatch = useDispatch();
+
 	useEffect( () => {
-		saveReaderPreference();
+		dispatch(
+			savePreference( READER_AS_LANDING_PAGE_PREFERENCE, {
+				useReaderAsLandingPage: true,
+				updatedAt: Date.now(),
+			} )
+		);
 		submitSignupStep( { stepName: 'set-reader-landing' } );
 		goToNextStep();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
@@ -24,10 +26,4 @@ const SetReaderLanding = ( {
 	return null; // Non-interactive step.
 };
 
-export default connect( null, {
-	saveReaderPreference: () =>
-		savePreference( READER_AS_LANDING_PAGE_PREFERENCE, {
-			useReaderAsLandingPage: true,
-			updatedAt: Date.now(),
-		} ),
-} )( SetReaderLanding );
+export default SetReaderLanding;
