824159e0eeafda47925ee2dd9c65a4ee3adda495
Marco Ciampini <marco.ciampo@gmail.com>
2025-02-21 17:04:46 +0100

    Stepper: remove site setup wg flow

--- client/landing/stepper/declarative-flow/registered-flows.ts
+++ client/landing/stepper/declarative-flow/registered-flows.ts
@@ -24,8 +24,6 @@ import type { Flow } from '../declarative-flow/internals/types';
 const availableFlows: Record< string, () => Promise< { default: Flow } > > = {
 	'site-setup': () =>
 		import( /* webpackChunkName: "site-setup-flow" */ '../declarative-flow/site-setup-flow' ),
-	'site-setup-wg': () =>
-		import( /* webpackChunkName: "site-setup-wg" */ '../declarative-flow/site-setup-wg-flow' ),
 
 	'copy-site': () =>
 		import( /* webpackChunkName: "copy-site-flow" */ '../declarative-flow/copy-site' ),
