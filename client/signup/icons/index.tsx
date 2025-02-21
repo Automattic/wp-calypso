2bd1b192dab09f2c423d9b80992cb2921b4ab5fe
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-02-21 16:15:40 +0200

    Device switchers: Use `@wordpress/icons` instead of custom ones (#100057)
    * Stepper: Use @wordpress/icons for devices
    
    * Components: Use @wordpress/icons for devices
    
    * Signup: Cleanup unused device icons
    
    * Components: Remove unused device icons
    
    * Components: Remove unused zoom controls from DeviceSwitcher
    
    * Icons: Resize from 36px to 24px
    
    * Components: Remove unused fixed viewport functionality
    
    * Also set text color just in case

--- client/signup/icons/index.tsx
+++ client/signup/icons/index.tsx
@@ -79,27 +79,6 @@ export const design = (
 	</SVG>
 );
 
-export const computer = (
-	<SVG width="36" height="36" viewBox="0 0 36 36">
-		<Rect x="6" y="9" width="24" height="18" rx="1.25" stroke="currentColor" strokeWidth="1.5" />
-		<Rect x="3" y="26.5" width="30" height="1.5" fill="currentColor" />
-	</SVG>
-);
-
-export const tablet = (
-	<SVG width="24" height="24" viewBox="0 0 24 24">
-		<Rect x="3" y="2" width="18" height="20" rx="1.25" stroke="currentColor" strokeWidth="1.5" />
-		<Rect x="10" y="17" width="4" height="1.5" fill="currentColor" />
-	</SVG>
-);
-
-export const phone = (
-	<SVG width="24" height="24" viewBox="0 0 24 24">
-		<Rect x="6" y="3" width="12" height="18" rx="1.25" stroke="currentColor" strokeWidth="1.5" />
-		<Rect x="11" y="17" width="2" height="1.5" fill="currentColor" />
-	</SVG>
-);
-
 export const tip = (
 	<SVG
 		width="20"
--- packages/components/src/device-switcher/toolbar.tsx
+++ packages/components/src/device-switcher/toolbar.tsx
@@ -1,32 +1,22 @@
 import { Button } from '@wordpress/components';
-import { Icon } from '@wordpress/icons';
+import { Icon, desktop, mobile, tablet } from '@wordpress/icons';
 import clsx from 'clsx';
 import { translate } from 'i18n-calypso';
 import { useRef } from 'react';
 import { DEVICES_SUPPORTED, DEVICE_TYPES } from './constants';
-import { computer, tablet, phone, zoomIn, zoomOut } from './icons';
 import type { Device } from './types';
 import './toolbar.scss';
 
 interface ToolbarProps {
 	device: Device;
-	isZoomable: boolean;
-	isZoomActive: boolean;
 	onDeviceClick: ( device: Device ) => void;
-	onZoomClick: () => void;
 }
 
-const DeviceSwitcherToolbar = ( {
-	device: currentDevice,
-	isZoomable,
-	isZoomActive,
-	onDeviceClick,
-	onZoomClick,
-}: ToolbarProps ) => {
+const DeviceSwitcherToolbar = ( { device: currentDevice, onDeviceClick }: ToolbarProps ) => {
 	const devices = useRef( {
-		[ DEVICE_TYPES.COMPUTER ]: { title: translate( 'Desktop' ), icon: computer, iconSize: 36 },
+		[ DEVICE_TYPES.COMPUTER ]: { title: translate( 'Desktop' ), icon: desktop, iconSize: 24 },
 		[ DEVICE_TYPES.TABLET ]: { title: translate( 'Tablet' ), icon: tablet, iconSize: 24 },
-		[ DEVICE_TYPES.PHONE ]: { title: translate( 'Phone' ), icon: phone, iconSize: 24 },
+		[ DEVICE_TYPES.PHONE ]: { title: translate( 'Phone' ), icon: mobile, iconSize: 24 },
 	} );
 
 	return (
@@ -48,15 +38,6 @@ const DeviceSwitcherToolbar = ( {
 						/>
 					</Button>
 				) ) }
-				{ isZoomable && (
-					<Button
-						aria-label={ translate( 'Zoom out 50%' ) }
-						className={ clsx( { zoom: true, 'is-selected': isZoomActive } ) }
-						onClick={ () => onZoomClick() }
-					>
-						<Icon icon={ isZoomActive ? zoomIn : zoomOut } size={ 24 } />
-					</Button>
-				) }
 			</div>
 		</div>
 	);
