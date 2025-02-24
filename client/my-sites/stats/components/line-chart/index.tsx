9cc678e9b8a83958d7c8f6cee0484ff24be653ef
Jasper Kang <jasper.kang@automattic.com>
2025-02-24 14:35:22 +1300

    sort data from distant to near by date

--- client/my-sites/stats/components/line-chart/index.tsx
+++ client/my-sites/stats/components/line-chart/index.tsx
@@ -53,6 +53,16 @@ function StatsLineChart( {
 		[ chartData ]
 	);
 
+	// TODO: we should have this in charts lib.
+	const chartDataSorted = useMemo(
+		() =>
+			chartData.map( ( series ) => ( {
+				...series,
+				data: series.data.sort( ( a, b ) => a.date.getTime() - b.date.getTime() ),
+			} ) ),
+		[ chartData ]
+	);
+
 	return (
 		<div className={ clsx( 'stats-line-chart', className ) }>
 			{ isEmpty && (
@@ -64,7 +74,7 @@ function StatsLineChart( {
 			{ ! isEmpty && (
 				<ThemeProvider theme={ jetpackTheme }>
 					<LineChart
-						data={ chartData }
+						data={ chartDataSorted }
 						withTooltips
 						withGradientFill
 						height={ height }
