4d86d71814689b4b9b35e7ca1d3ae51c27cfbfb4
Dave Martin <davemart-in@users.noreply.github.com>
2025-02-25 14:46:38 -0500

    Revert "Update Substack import subscribers screen (#100281)" (#100423)
    This reverts commit 64c54b7d7b61e117695dbca7f3e07feed47b7c19.

--- client/my-sites/importer/newsletter/subscribers/step-initial.tsx
+++ client/my-sites/importer/newsletter/subscribers/step-initial.tsx
@@ -1,15 +1,12 @@
 import { Card } from '@automattic/components';
 import { Subscriber } from '@automattic/data-stores';
-import { localizeUrl } from '@automattic/i18n-utils';
 import { Button } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { createInterpolateElement } from '@wordpress/element';
 import { external } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
-import i18n from 'i18n-calypso';
 import { useEffect, useRef } from 'react';
 import exportSubstackSubscribersImg from 'calypso/assets/images/importer/export-substack-subscribers.png';
-import InlineSupportLink from 'calypso/components/inline-support-link';
 import { SubscribersStepProps } from '../types';
 import { normalizeFromSite } from '../utils';
 import SubscriberUploadForm from './upload-form';
@@ -44,28 +41,11 @@ export default function StepInitial( {
 			<h2>{ __( 'Step 1: Export your subscribers from Substack' ) }</h2>
 			<p>
 				{ createInterpolateElement(
-					// @ts-expect-error - fixMe method is not typed in the package. Once types are added upstream, remove this.
-					i18n.fixMe( {
-						text: `Generate a CSV of your Substack subscribers. In Substack, go to Subscribers, click Export under "All subscribers," then upload the CSV in the next step. On the free plan, you can import up to 100 subscribers.`,
-						newCopy: __(
-							`Generate a CSV of your Substack subscribers. In Substack, go to <strong>Subscribers</strong>, click <strong>Export</strong> under "All subscribers," then upload the CSV in the next step. On the free plan, <supportLink>you can import up to 100 subscribers.</supportLink>`
-						),
-						oldCopy: __(
-							`Generate a CSV file of all your Substack subscribers. On Substack, go to the <strong>Subscribers</strong> tab and click the <strong>Export</strong> button you’ll find on top of the table. Then, upload the downloaded CSV in the next step.`
-						),
-					} ),
+					__(
+						'Generate a CSV file of all your Substack subscribers. On Substack, go to the <strong>Subscribers</strong> tab and click the <strong>Export</strong> button you’ll find on top of the table. Then, upload the downloaded CSV in the next step.'
+					),
 					{
 						strong: <strong />,
-						supportLink: (
-							<InlineSupportLink
-								noWrap={ false }
-								showIcon={ false }
-								supportLink={ localizeUrl(
-									'https://wordpress.com/support/import-subscribers-to-a-newsletter/#troubleshooting-subscriber-imports'
-								) }
-								supportPostId={ 220199 }
-							/>
-						),
 					}
 				) }
 			</p>
