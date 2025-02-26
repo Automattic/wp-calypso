2e9c32c19067d90ece6d084f4ec0109b25084dfe
Griffith Chen <griffith.chen@automattic.com>
2025-02-26 15:45:15 +0800

    Type check

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/hooks/use-is-updated-badge-design.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/hooks/use-is-updated-badge-design.tsx
@@ -3,7 +3,7 @@ import { getStepFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url'
 
 const useIsUpdatedBadgeDesign = () => {
 	const step = useMemo( () => getStepFromURL(), [] );
-	return [ 'designsetup', 'design-setup' ].includes( step?.toLowerCase() );
+	return [ 'designsetup', 'design-setup' ].includes( step?.toLowerCase() ?? '' );
 };
 
 export default useIsUpdatedBadgeDesign;
