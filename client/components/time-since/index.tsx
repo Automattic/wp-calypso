fe10c09a8ce17fc79fe0e11516853d7fbbf12910
Omar Alshaker <omar@omaralshaker.com>
2025-02-26 20:39:49 +0100

    Discard changes to client/components/time-since/index.tsx

--- client/components/time-since/index.tsx
+++ client/components/time-since/index.tsx
@@ -1,53 +1,16 @@
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import { useHumanDate } from 'calypso/lib/human-date';
+
 interface TimeSinceProps {
 	date: string;
+	dateFormat?: string;
 	className?: string;
 }
 
-const rtf = new Intl.RelativeTimeFormat( 'default', {
-	numeric: 'auto',
-	style: 'narrow',
-} );
-
-const intervals = {
-	week: 604800,
-	day: 86400,
-	hour: 3600,
-	minute: 60,
-} as const;
-
-function getHumanTimeSince( date: string ): string {
-	const timestamp = new Date( date ).getTime();
-	const diffInSeconds = ( timestamp - Date.now() ) / 1000;
-
-	// If older than a month, return formatted date
-	if ( Math.abs( diffInSeconds ) > 2592000 ) {
-		return new Intl.DateTimeFormat( 'default', {
-			day: 'numeric',
-			month: 'short',
-			year: 'numeric',
-		} ).format( timestamp );
-	}
-
-	for ( const [ unit, secondsInUnit ] of Object.entries( intervals ) ) {
-		const value = Math.round( diffInSeconds / secondsInUnit );
-		if ( Math.abs( value ) >= 1 ) {
-			return rtf.format( value, unit as Intl.RelativeTimeFormatUnit );
-		}
-	}
-
-	return rtf.format( 0, 'second' );
-}
-
-function TimeSince( { className, date }: TimeSinceProps ) {
-	const fullDate = new Intl.DateTimeFormat( 'default', {
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-		hour: 'numeric',
-		minute: 'numeric',
-	} ).format( new Date( date ) );
-
-	const humanDate = getHumanTimeSince( date );
+function TimeSince( { className, date, dateFormat = 'll' }: TimeSinceProps ) {
+	const moment = useLocalizedMoment();
+	const fullDate = moment( date ).format( 'llll' );
+	const humanDate = useHumanDate( date, dateFormat );
 
 	return (
 		<time className={ className } dateTime={ date } title={ fullDate }>
