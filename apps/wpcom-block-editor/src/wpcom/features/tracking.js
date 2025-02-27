68655c67b251b94b1f407f9f38dd14f4a6238ce9
okmttdhr <okmttdhr@users.noreply.github.com>
2025-02-27 16:02:33 +0900

    Tracking: Ignore async blocks in inner blocks replacement

--- apps/wpcom-block-editor/src/wpcom/features/tracking.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking.js
@@ -522,7 +522,11 @@ const trackInnerBlocksReplacement = ( rootClientId, blocks ) => {
 			// Reusable Block
 			name === 'core/block' ||
 			// Post Content
-			name === 'core/post-content'
+			name === 'core/post-content' ||
+			// Page List
+			name === 'core/page-list' ||
+			// Page List Item
+			name === 'core/page-list-item'
 		) {
 			return;
 		}
