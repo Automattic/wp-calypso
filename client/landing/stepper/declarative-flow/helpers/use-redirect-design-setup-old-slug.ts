32e9cb75a656a12fded314bbf82d12bcd50b8592
Griffith Chen <griffith.chen@automattic.com>
2025-02-27 09:32:33 +0800

    Refactor logic

--- client/landing/stepper/declarative-flow/helpers/use-redirect-design-setup-old-slug.ts
+++ client/landing/stepper/declarative-flow/helpers/use-redirect-design-setup-old-slug.ts
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
+import { getStepOldSlug } from './helpers/get-step-old-slug';
 import type { StepperStep, Navigate } from '../internals/types';
+
+const DESIGN_SETUP_SLUG = 'design-setup';
 
 export function useRedirectDesignSetupOldSlug(
 	currentStep: string,
 	navigate: Navigate< StepperStep[] >
 ) {
+	const oldSlug = getStepOldSlug( DESIGN_SETUP_SLUG );
 	useEffect( () => {
-		if ( currentStep === 'designSetup' ) {
-			navigate( 'design-setup' );
+		if ( currentStep === oldSlug ) {
+			navigate( DESIGN_SETUP_SLUG );
 		}
-	}, [ currentStep, navigate ] );
+	}, [ currentStep, navigate, oldSlug ] );
 }
