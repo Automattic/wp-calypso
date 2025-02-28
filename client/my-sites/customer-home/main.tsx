62a3a1c836bd03c318fe445d17d2e58fd7a631b7
Paulo Marcos Trentin <paulo@paulotrentin.com.br>
2025-02-28 18:17:06 -0300

    Revert "Enable Focused Launchpad for all onboarding users (#100526)"
    This reverts commit 55b5404df092e15ecd43dbcaa19fc976b2510f32.

--- client/my-sites/customer-home/main.tsx
+++ client/my-sites/customer-home/main.tsx
@@ -5,13 +5,16 @@ import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
 import { useGetDomainsQuery } from 'calypso/data/domains/use-get-domains-query';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
-import { shouldShowLaunchpadFirst } from 'calypso/state/selectors/should-show-launchpad-first';
+import { useShouldShowLaunchpadFirst } from 'calypso/state/selectors/should-show-launchpad-first';
 import CelebrateLaunchModal from './components/celebrate-launch-modal';
 import { FullScreenLaunchpad } from './components/full-screen-launchpad';
 import HomeContent from './components/home-content';
 import type { SiteDetails } from '@automattic/data-stores';
 
 export default function CustomerHome( { site }: { site: SiteDetails } ) {
+	const [ isLoadingShouldShowLaunchpadFirst, shouldShowLaunchpadFirst ] =
+		useShouldShowLaunchpadFirst( site );
+
 	const isSiteLaunched = site?.launch_status === 'launched' || false;
 
 	const [ isFullLaunchpadDismissed, setIsFullLaunchpadDismissed ] = useState(
@@ -32,9 +35,9 @@ export default function CustomerHome( { site }: { site: SiteDetails } ) {
 		<Main wideLayout>
 			<PageViewTracker path="/home/:site" title={ translate( 'My Home' ) } />
 			<DocumentHead title={ translate( 'My Home' ) } />
-			{ site.options && (
+			{ ! isLoadingShouldShowLaunchpadFirst && (
 				<>
-					{ shouldShowLaunchpadFirst( site ) && ! isFullLaunchpadDismissed ? (
+					{ shouldShowLaunchpadFirst && ! isFullLaunchpadDismissed ? (
 						<FullScreenLaunchpad
 							onClose={ () => setIsFullLaunchpadDismissed( true ) }
 							onSiteLaunch={ () => {
--- client/state/selectors/test/should-show-launchpad-first.ts
+++ client/state/selectors/test/should-show-launchpad-first.ts
@@ -1,8 +1,12 @@
 /**
  * @jest-environment jsdom
  */
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { loadExperimentAssignment } from 'calypso/lib/explat';
-import { shouldShowLaunchpadFirst } from '../should-show-launchpad-first';
+import {
+	shouldShowLaunchpadFirst,
+	useShouldShowLaunchpadFirst,
+} from '../should-show-launchpad-first';
 import type { SiteDetails } from '@automattic/data-stores';
 
 jest.mock( 'calypso/lib/explat', () => ( {
@@ -21,10 +25,25 @@ describe( 'shouldShowLaunchpadFirst', () => {
 		const site = {
 			options: {
 				site_creation_flow: 'onboarding',
+				created_at: '2025-02-18T00:00:00+00:00',
 			},
 		} as SiteDetails;
 
 		expect( await shouldShowLaunchpadFirst( site ) ).toBe( true );
+	} );
+
+	it( 'should return false when site was created via onboarding flow but assigned to control', async () => {
+		( loadExperimentAssignment as jest.Mock ).mockResolvedValue( {
+			variationName: 'control',
+		} );
+		const site = {
+			options: {
+				site_creation_flow: 'onboarding',
+				created_at: '2025-02-18T00:00:00+00:00',
+			},
+		} as SiteDetails;
+
+		expect( await shouldShowLaunchpadFirst( site ) ).toBe( false );
 	} );
 
 	it( 'should return false when site was not created via onboarding flow', async () => {
@@ -34,6 +53,7 @@ describe( 'shouldShowLaunchpadFirst', () => {
 		const site = {
 			options: {
 				site_creation_flow: 'other',
+				created_at: '2025-02-18T00:00:00+00:00',
 			},
 		} as SiteDetails;
 
@@ -45,9 +65,107 @@ describe( 'shouldShowLaunchpadFirst', () => {
 			variationName: 'treatment_cumulative',
 		} );
 		const site = {
-			options: {},
+			options: {
+				created_at: '2025-02-18T00:00:00+00:00',
+			},
 		} as SiteDetails;
 
 		expect( await shouldShowLaunchpadFirst( site ) ).toBe( false );
 	} );
 } );
+
+describe( 'useShouldShowLaunchpadFirst', () => {
+	it( 'returns loading state until promise resolves', async () => {
+		const { promise, resolve } = promiseWithResolvers();
+		( loadExperimentAssignment as jest.Mock ).mockReturnValue( promise );
+		const site = {
+			options: {
+				site_creation_flow: 'onboarding',
+				created_at: '2025-02-18T00:00:00+00:00',
+			},
+		} as SiteDetails;
+
+		const { result } = renderHook( () => useShouldShowLaunchpadFirst( site ) );
+
+		expect( result.current[ 0 ] ).toBe( true );
+
+		await act( () => resolve( { variationName: 'treatment_cumulative' } ) );
+
+		expect( result.current ).toEqual( [ false, true ] );
+	} );
+
+	it( 'should return true when site was created via onboarding flow and assigned to experiment', async () => {
+		( loadExperimentAssignment as jest.Mock ).mockResolvedValue( {
+			variationName: 'treatment_cumulative',
+		} );
+		const site = {
+			options: {
+				site_creation_flow: 'onboarding',
+				created_at: '2025-02-18T00:00:00+00:00',
+			},
+		} as SiteDetails;
+
+		const { result } = renderHook( () => useShouldShowLaunchpadFirst( site ) );
+
+		await waitFor( () => expect( result.current ).toEqual( [ false, true ] ) );
+	} );
+
+	it( 'should return false when site was created via onboarding flow but assigned to control', async () => {
+		( loadExperimentAssignment as jest.Mock ).mockResolvedValue( {
+			variationName: 'control',
+		} );
+		const site = {
+			options: {
+				site_creation_flow: 'onboarding',
+				created_at: '2025-02-18T00:00:00+00:00',
+			},
+		} as SiteDetails;
+
+		const { result } = renderHook( () => useShouldShowLaunchpadFirst( site ) );
+
+		await waitFor( () => expect( result.current ).toEqual( [ false, false ] ) );
+	} );
+
+	it( 'should return false when site was not created via onboarding flow', async () => {
+		( loadExperimentAssignment as jest.Mock ).mockResolvedValue( {
+			variationName: 'treatment_cumulative',
+		} );
+		const site = {
+			options: {
+				site_creation_flow: 'other',
+				created_at: '2025-02-18T00:00:00+00:00',
+			},
+		} as SiteDetails;
+
+		const { result } = renderHook( () => useShouldShowLaunchpadFirst( site ) );
+
+		await waitFor( () => expect( result.current ).toEqual( [ false, false ] ) );
+	} );
+
+	it( 'should return false when site has no creation flow information', async () => {
+		( loadExperimentAssignment as jest.Mock ).mockResolvedValue( {
+			variationName: 'treatment_cumulative',
+		} );
+		const site = {
+			options: {
+				created_at: '2025-02-18T00:00:00+00:00',
+			},
+		} as SiteDetails;
+
+		const { result } = renderHook( () => useShouldShowLaunchpadFirst( site ) );
+
+		await waitFor( () => expect( result.current ).toEqual( [ false, false ] ) );
+	} );
+} );
+
+// Our TS Config doesn't know the standard Promise.withResolvers is available
+// in our version of Node.
+function promiseWithResolvers() {
+	let resolve;
+	let reject;
+	const promise = new Promise( ( res, rej ) => {
+		resolve = res;
+		reject = rej;
+	} );
+	return { promise, resolve, reject };
+}
