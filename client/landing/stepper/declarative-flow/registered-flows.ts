8c2c4c98fc611a9198f2c4d0f8201cda52276820
Francesco Bigiarini <francesco.bigiarini@automattic.com>
2025-03-03 17:21:38 +0100

    Add new Playground step

--- client/landing/stepper/declarative-flow/registered-flows.ts
+++ client/landing/stepper/declarative-flow/registered-flows.ts
@@ -16,6 +16,7 @@ import {
 	ONBOARDING_FLOW,
 	HUNDRED_YEAR_DOMAIN_FLOW,
 	EXAMPLE_FLOW,
+	PLAYGROUND_FLOW,
 } from '@automattic/onboarding';
 import type { Flow } from '../declarative-flow/internals/types';
 
@@ -97,6 +98,9 @@ const availableFlows: Record< string, () => Promise< { default: Flow } > > = {
 		),
 	[ EXAMPLE_FLOW ]: () =>
 		import( /* webpackChunkName: "example-flow" */ '../declarative-flow/example' ),
+
+	[ PLAYGROUND_FLOW ]: () =>
+		import( /* webpackChunkName: "playground-flow" */ '../declarative-flow/playground' ),
 };
 
 const hostedSiteMigrationFlow: Record< string, () => Promise< { default: Flow } > > = {
--- packages/onboarding/src/utils/flows.ts
+++ packages/onboarding/src/utils/flows.ts
@@ -35,6 +35,7 @@ export const EXAMPLE_FLOW = 'example';
 export const DIFM_FLOW = 'do-it-for-me';
 export const DIFM_FLOW_STORE = 'do-it-for-me-store';
 export const WEBSITE_DESIGN_SERVICES = 'website-design-services';
+export const PLAYGROUND_FLOW = 'playground';
 
 export const isNewsletterFlow = ( flowName: string | null ) => {
 	return Boolean( flowName && NEWSLETTER_FLOW === flowName );
@@ -153,4 +154,8 @@ export const isDIFMFlow = ( flowName: string | null ) => {
 	return Boolean(
 		flowName && [ DIFM_FLOW, DIFM_FLOW_STORE, WEBSITE_DESIGN_SERVICES ].includes( flowName )
 	);
+};
+
+export const isPlaygroundFlow = ( flowName: string | null ) => {
+	return Boolean( flowName && [ PLAYGROUND_FLOW ].includes( flowName ) );
 };
