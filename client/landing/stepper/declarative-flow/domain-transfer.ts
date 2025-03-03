ead8e8181ea92c035205fbfd7691b26bae095c47
Omar Alshaker <omar@omaralshaker.com>
2025-03-03 12:04:24 +0100

    Stepper: migrate domain-transfer flow to use built in authentication

--- client/landing/stepper/declarative-flow/domain-transfer.ts
+++ client/landing/stepper/declarative-flow/domain-transfer.ts
@@ -1,29 +1,17 @@
 import { DOMAIN_TRANSFER } from '@automattic/onboarding';
-import { useSelect } from '@wordpress/data';
 import { translate } from 'i18n-calypso';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import {
 	clearSignupDestinationCookie,
 	setSignupCompleteSlug,
 	setSignupCompleteFlowName,
 } from 'calypso/signup/storageUtils';
-import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
-import { USER_STORE } from '../stores';
-import { useLoginUrl } from '../utils/path';
+import { stepsWithRequiredLogin } from '../utils/steps-with-required-login';
 import { STEPS } from './internals/steps';
-import {
-	ProvidedDependencies,
-	AssertConditionResult,
-	AssertConditionState,
-	FlowV1,
-} from './internals/types';
-import type { UserSelect } from '@automattic/data-stores';
+import { ProvidedDependencies, FlowV1 } from './internals/types';
 
 const DOMAIN_TRANSFER_STEPS = [
 	STEPS.DOMAIN_TRANSFER_INTRO,
-	STEPS.DOMAIN_TRANSFER_DOMAINS,
-	STEPS.PROCESSING,
+	...stepsWithRequiredLogin( [ STEPS.DOMAIN_TRANSFER_DOMAINS, STEPS.PROCESSING ] ),
 ];
 
 const domainTransfer: FlowV1 = {
@@ -31,49 +19,20 @@ const domainTransfer: FlowV1 = {
 	get title() {
 		return translate( 'Bulk domain transfer' );
 	},
+	__experimentalUseBuiltinAuth: true,
 	isSignupFlow: false,
 	useSteps() {
 		return DOMAIN_TRANSFER_STEPS;
 	},
 
-	useAssertConditions( navigate ): AssertConditionResult {
-		const isLoggedIn = useSelector( isUserLoggedIn );
-
-		useEffect( () => {
-			if ( ! isLoggedIn && navigate ) {
-				navigate( 'intro' );
-			}
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-		}, [ isLoggedIn ] );
-
-		return { state: AssertConditionState.SUCCESS };
-	},
-
 	useStepNavigation( _currentStepSlug, navigate ) {
 		const flowName = this.name;
-		const userIsLoggedIn = useSelect(
-			( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
-			[]
-		);
-
-		const logInUrl = useLoginUrl( {
-			variationName: flowName,
-			redirectTo: `/setup/${ flowName }/domains`,
-			pageTitle: 'Bulk Transfer',
-		} );
-
 		const submit = ( providedDependencies: ProvidedDependencies = {} ) => {
 			switch ( _currentStepSlug ) {
 				case 'intro':
 					clearSignupDestinationCookie();
-
-					if ( userIsLoggedIn ) {
-						return navigate( 'domains' );
-					}
-					return window.location.assign( logInUrl );
+					return navigate( 'domains' );
 				case 'domains': {
-					// go to processing step without pushing it to history
-					// so the back button would go back to domains step
 					return navigate( 'processing', undefined );
 				}
 				case 'processing': {
