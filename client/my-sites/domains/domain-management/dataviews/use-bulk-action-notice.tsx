453c1d245b1f6a3ff3fa67cbcd04a7e91cd6fe02
Dmitry Merkushin <merkushin@gmail.com>
2025-03-03 11:52:07 -0600

    Optimize translate function usage

--- client/my-sites/domains/domain-management/dataviews/use-bulk-action-notice.tsx
+++ client/my-sites/domains/domain-management/dataviews/use-bulk-action-notice.tsx
@@ -1,48 +1,65 @@
 import { JobStatus } from '@automattic/data-stores';
 import { StatusPopover } from '@automattic/domains-table/src/status-popover';
-import { useTranslate } from 'i18n-calypso';
+import { TranslateOptionsPlural, useTranslate } from 'i18n-calypso';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import wpcomRequest from 'wpcom-proxy-request';
 import { successNotice, errorNotice } from 'calypso/state/notices/actions';
 import { useDomainsDataViewsContext } from './use-context';
 
-const getSuccessMessage = ( job: JobStatus, translate: ( original: string ) => string ) => {
+type ExistingReactNode = React.ReactElement | string | number;
+type TranslateFunction = (
+	original: string,
+	plural: string,
+	options: TranslateOptionsPlural
+) => ExistingReactNode;
+
+const getSuccessMessage = ( job: JobStatus, translate: TranslateFunction ) => {
 	if ( job.action !== 'set_auto_renew' ) {
-		return job.success.length > 1
-			? translate( 'Your domains have been updated.' )
-			: translate( 'Your domain has been updated.' );
+		return translate( 'Your domain has been updated.', 'Your domains have been updated.', {
+			count: job.success.length,
+		} );
 	}
 
 	// If the user tried to enable auto-renew:
 	if ( job.params.auto_renew ) {
-		return job.success.length > 1
-			? translate( 'Automatic renewal has been enabled for your domains.' )
-			: translate( 'Automatic renewal has been enabled for your domain.' );
+		return translate(
+			'Automatic renewal has been enabled for your domain.',
+			'Automatic renewal has been enabled for your domains.',
+			{ count: job.success.length }
+		);
 	}
 
 	// If the user tired to disable auto-renew:
-	return job.success.length > 1
-		? translate( 'Automatic renewal has been disabled for your domains.' )
-		: translate( 'Automatic renewal has been disabled for your domain.' );
+	return translate(
+		'Automatic renewal has been disabled for your domain.',
+		'Automatic renewal has been disabled for your domains.',
+		{ count: job.success.length }
+	);
 };
 
-const getFailureMessage = ( job: JobStatus, translate: ( original: string ) => string ) => {
+const getFailureMessage = ( job: JobStatus, translate: TranslateFunction ) => {
 	if ( job.action !== 'set_auto_renew' ) {
-		return job.failed.length > 1
-			? translate( 'Some domain updates were not successful.' )
-			: translate( 'Your domain update has failed.' );
+		return translate(
+			'Your domain update has failed.',
+			'Some domain updates were not successful.',
+			{ count: job.failed.length }
+		);
 	}
 
 	if ( job.params.auto_renew ) {
-		return job.failed.length > 1
-			? translate( 'Enabling automatic renewal has failed for your domains.' )
-			: translate( 'Enabling automatic renewal has failed for your domain.' );
+		return translate(
+			'Enabling automatic renewal has failed for your domain.',
+			'Enabling automatic renewal has failed for your domains.',
+			{ count: job.failed.length }
+		);
 	}
 
-	return job.failed.length > 1
-		? translate( 'Disabling automatic renewal has failed for your domains.' )
-		: translate( 'Disabling automatic renewal has failed for your domain.' );
+	return translate(
+		'Disabling automatic renewal has failed for your domain.',
+		'Disabling automatic renewal has failed for your domains.',
+		{ count: job.failed.length }
+	);
 };
 
 export default function useBulkActionNotice() {
