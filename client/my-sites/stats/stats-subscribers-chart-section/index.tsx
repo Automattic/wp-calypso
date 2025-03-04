76185e8097fd07273708d352aa6f3461fc89b32e
Jasper Kang <jasper.kang@automattic.com>
2025-03-04 20:06:28 +1300

    use same loading state so that the page does not jump

--- client/my-sites/stats/stats-subscribers-chart-section/index.tsx
+++ client/my-sites/stats/stats-subscribers-chart-section/index.tsx
@@ -9,7 +9,6 @@ import UplotChart from 'calypso/components/chart-uplot';
 import useSubscribersQuery from 'calypso/my-sites/stats/hooks/use-subscribers-query';
 import { useSelector } from 'calypso/state';
 import useCssVariable from '../hooks/use-css-variable';
-import PageLoading from '../pages/shared/page-loading';
 import StatsModulePlaceholder from '../stats-module/placeholder';
 import StatsPeriodHeader from '../stats-period-header';
 import { parseLocalDate } from '../utils';
@@ -275,7 +274,7 @@ export default function SubscribersChartSection( {
 							EmptyState={ () => null }
 							zeroBaseline={ lineChartData.length > 1 }
 							formatTimeTick={ formatTimeTick }
-							placeholder={ PageLoading }
+							placeholder={ <StatsModulePlaceholder className="is-chart" isLoading /> }
 						/>
 					) : (
 						<UplotChart
