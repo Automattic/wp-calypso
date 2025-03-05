39fc4b7902d933c0954ff34c510439f484d6a7b3
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-03-05 14:26:03 +0500

    🐛 FIX: types

--- client/landing/stepper/hooks/use-send-email-verification.ts
+++ client/landing/stepper/hooks/use-send-email-verification.ts
@@ -1,10 +1,10 @@
 import wpcom from 'calypso/lib/wp';
 
 export interface ResendEmailVerificationBody {
-	from: string; // Helps decide the url of the page after confirmation.
+	from?: string; // Helps decide the url of the page after confirmation.
 }
 
-export function useSendEmailVerification( body: ResendEmailVerificationBody ) {
+export function useSendEmailVerification( body: ResendEmailVerificationBody = {} ) {
 	return async () => {
 		return wpcom.req.post( '/me/send-verification-email', {
 			apiVersion: '1.1',
