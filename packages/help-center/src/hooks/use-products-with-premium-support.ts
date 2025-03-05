a9136ee2b95fd6b60831cc237a04acc72fab213e
escapemanuele <escapemanuele@gmail.com>
2025-03-05 11:29:53 +0100

    Update both hooks to use constants

--- packages/help-center/src/hooks/use-products-with-premium-support.ts
+++ packages/help-center/src/hooks/use-products-with-premium-support.ts
@@ -1,27 +1,48 @@
 import { isDIFMProduct, PLAN_100_YEARS } from '@automattic/calypso-products';
+import {
+	DIFM_FLOW,
+	HUNDRED_YEAR_DOMAIN_FLOW,
+	HUNDRED_YEAR_PLAN_FLOW,
+} from '@automattic/onboarding';
 import { ResponseCartProduct } from '@automattic/shopping-cart';
+import { FLOWS_ZENDESK_INITIAL_MESSAGES, FLOWS_ZENDESK_FLOWNAME } from '../constants';
+
+const getUserFieldMessage = ( flowName: string, url?: string ) => {
+	return `${
+		FLOWS_ZENDESK_INITIAL_MESSAGES[ flowName as keyof typeof FLOWS_ZENDESK_INITIAL_MESSAGES ]
+	}. URL: ${ url }`;
+};
 
 export function useProductsWithPremiumSupport( products: ResponseCartProduct[], url?: string ) {
-	let hasDIFMProduct = false;
-	let has100YPlan = false;
-
 	for ( const product of products ) {
 		if ( isDIFMProduct( product ) ) {
-			hasDIFMProduct = true;
+			return {
+				userFieldMessage: getUserFieldMessage( DIFM_FLOW, url ),
+				userFieldFlowName:
+					FLOWS_ZENDESK_FLOWNAME[ DIFM_FLOW as keyof typeof FLOWS_ZENDESK_FLOWNAME ],
+				hasPremiumSupport: true,
+			};
 		}
-		if ( product?.product_slug === PLAN_100_YEARS || product?.extra?.is_hundred_year_domain ) {
-			has100YPlan = true;
+		if ( product?.product_slug === PLAN_100_YEARS ) {
+			return {
+				userFieldMessage: getUserFieldMessage( HUNDRED_YEAR_PLAN_FLOW, url ),
+				userFieldFlowName:
+					FLOWS_ZENDESK_FLOWNAME[ HUNDRED_YEAR_PLAN_FLOW as keyof typeof FLOWS_ZENDESK_FLOWNAME ],
+				hasPremiumSupport: true,
+			};
+		}
+		if ( product?.extra?.is_hundred_year_domain ) {
+			return {
+				userFieldMessage: getUserFieldMessage( HUNDRED_YEAR_DOMAIN_FLOW, url ),
+				userFieldFlowName:
+					FLOWS_ZENDESK_FLOWNAME[ HUNDRED_YEAR_DOMAIN_FLOW as keyof typeof FLOWS_ZENDESK_FLOWNAME ],
+				hasPremiumSupport: true,
+			};
 		}
 	}
 
-	const initialMessage = hasDIFMProduct
-		? `User is purchasing DIFM plan.${ url ? ` URL: ${ url }` : '' }`
-		: `User is purchasing 100 year plan.`;
-
-	const hasPremiumSupport = hasDIFMProduct || has100YPlan;
-
 	return {
-		initialMessage,
-		hasPremiumSupport,
+		hasPremiumSupport: false,
+		userFieldMessage: null,
 	};
 }
