17c60cce2e083509eade8c171cee6c1fb48204ec
Allison Levine <allison.levine@automattic.com>
2025-03-05 10:26:41 -0500

    Update copy with editorial improvements.

--- client/my-sites/importer/newsletter/select-newsletter-form.tsx
+++ client/my-sites/importer/newsletter/select-newsletter-form.tsx
@@ -62,9 +62,9 @@ export default function SelectNewsletterForm( {
 			{ ! hasError && (
 				<p className="select-newsletter-form__help">
 					{ i18n.fixMe( {
-						text: 'Enter your Substack URL and we will direct you to the page where you can download your newsletter content.',
+						text: "Enter your Substack URL. We'll create a link where you can download your newsletter content.",
 						newCopy: i18n.translate(
-							'Enter your Substack URL and we will direct you to the page where you can download your newsletter content.'
+							"Enter your Substack URL. We'll create a link where you can download your newsletter content."
 						),
 						oldCopy: i18n.translate(
 							'Enter the URL of the Substack newsletter that you wish to import.'
