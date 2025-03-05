641a22fb790b50f64d4f2be51e99e0052ebdb07b
Allison Levine <allison.levine@automattic.com>
2025-03-05 11:28:13 -0500

    Remove comment since where prop shows possible events.

--- client/my-sites/subscribers/tracks/use-record-subscriber-clicked.ts
+++ client/my-sites/subscribers/tracks/use-record-subscriber-clicked.ts
@@ -7,7 +7,6 @@ const useRecordSubscriberClicked = () => {
 		where: 'title' | 'icon' | 'row' | 'list',
 		tracksProps: { site_id?: number | null; subscription_id?: number; user_id?: number }
 	) => {
-		// Allows for calypso_subscribers_subscriber_title_clicked, calypso_subscribers_subscriber_icon_clicked, calypso_subscribers_subscriber_row_clicked,
 		recordSubscribersTracksEvent(
 			`calypso_subscribers_subscriber_${ where }_clicked`,
 			tracksProps
