79974297b12b70ae2d31decf009aaccba6abb4e8
Caroline Moore <calobee@gmail.com>
2025-03-05 11:51:39 -0500

    Update typing for clarity, pass an object with options

--- client/sites/hooks/use-transfer-with-software-start-mutation.ts
+++ client/sites/hooks/use-transfer-with-software-start-mutation.ts
@@ -5,17 +5,20 @@ type transferWithSoftwareResponse = {
 	transferId: number;
 };
 
+type transferOptions = {
+	siteId: number;
+	from?: string;
+	plugins?: Record< string, 'install' | 'activate' >;
+	themes?: Record< string, 'install' | 'activate' >;
+};
+
+type SoftwareSlug = string;
+type SoftwareStatus = 'install' | 'activate';
+type Software = Record< SoftwareSlug, SoftwareStatus >;
+
 const requestTransferWithSoftware: (
-	siteId: number,
-	from?: string,
-	plugins?: Record< string, 'install' | 'activate' >,
-	themes?: Record< string, 'install' | 'activate' >
-) => Promise< transferWithSoftwareResponse > = async (
-	siteId: number,
-	from?: string,
-	plugins?: Record< string, 'install' | 'activate' >,
-	themes?: Record< string, 'install' | 'activate' >
-) => {
+	options: transferOptions
+) => Promise< transferWithSoftwareResponse > = async ( { siteId, from, plugins, themes } ) => {
 	const response = await wpcom.req.post(
 		{
 			path: `/sites/${ siteId }/atomic/transfer-with-software?http_envelope=1`,
@@ -36,13 +39,18 @@ const requestTransferWithSoftware: (
 export const useRequestTransferWithSoftware = (
 	siteId?: number,
 	from?: string,
-	plugins?: Record< string, 'install' | 'activate' >,
-	themes?: Record< string, 'install' | 'activate' >,
+	plugins?: Software,
+	themes?: Software,
 	options?: { retry?: number }
 ): UseMutationResult< transferWithSoftwareResponse, Error, void > => {
 	return useMutation( {
 		mutationKey: [ 'transfer-with-software', siteId, from, plugins, themes ],
-		mutationFn: async () => requestTransferWithSoftware( siteId!, from!, plugins!, themes! ),
+		mutationFn: async () => {
+			if ( ! siteId ) {
+				throw new Error( 'Site ID is required' );
+			}
+			return requestTransferWithSoftware( { siteId, from, plugins, themes } );
+		},
 		retry: options?.retry ?? 3, // Default retry 3 times
 	} );
 };
