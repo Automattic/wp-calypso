1cab54cd5c5515efe78a00e7661b024a8f8c8abc
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-03-05 16:02:35 +0500

    🐛 FIX: allow users to answer daily prompts more than once

--- client/components/blogging-prompt-card/prompts-navigation.jsx
+++ client/components/blogging-prompt-card/prompts-navigation.jsx
@@ -74,10 +74,6 @@ const PromptsNavigation = ( { siteId, prompts, tracksPrefix, index, menu } ) => 
 		// Prevent navigating away so we have time to record the click.
 		e.preventDefault();
 
-		if ( getPrompt()?.answered ) {
-			return;
-		}
-
 		dispatch(
 			recordTracksEvent( tracksPrefix + 'answer_prompt', {
 				site_id: siteId,
@@ -234,19 +230,11 @@ const PromptsNavigation = ( { siteId, prompts, tracksPrefix, index, menu } ) => 
 					href={ getNewPostLink() }
 					onClick={ handleBloggingPromptClick }
 					className="blogging-prompt__new-post-link"
-					disabled={ getPrompt()?.answered }
 				>
-					{ getPrompt()?.answered ? (
-						<>
-							<Gridicon icon="checkmark" size={ 18 } />
-							{ translate( 'Answered' ) }
-						</>
-					) : (
-						translate( 'Post Answer', {
-							comment:
-								'"Post" here is a verb meaning "to publish", as in "post an answer to this writing prompt"',
-						} )
-					) }
+					{ translate( 'Post Answer', {
+						comment:
+							'"Post" here is a verb meaning "to publish", as in "post an answer to this writing prompt"',
+					} ) }
 				</Button>
 			</div>
 		);
