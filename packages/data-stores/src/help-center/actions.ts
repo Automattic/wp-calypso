2c943e4279fc01a0af9f3ca4b76986a93f3c3ee2
Emanuele Buccelli <escapemanuele@gmail.com>
2025-03-06 18:28:26 +0100

    Update packages/data-stores/src/help-center/actions.ts
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- packages/data-stores/src/help-center/actions.ts
+++ packages/data-stores/src/help-center/actions.ts
@@ -208,7 +208,7 @@ export const setNewMessagingChat = function* ( {
 	siteId?: string;
 } ) {
 	yield setNavigateToRoute(
-		`/odie?provider=zendesk&userFieldMessage=${ initialMessage ?? '' }&section=${
+		`/odie?provider=zendesk&userFieldMessage=${ initialMessage }&section=${
 			section ?? ''
 		}&siteUrl=${ siteUrl ?? '' }&siteId=${ siteId ?? '' }`
 	);
