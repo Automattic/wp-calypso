acd55c88ec0d6870db828edce05e576c3d641732
Caroline Moore <calobee@gmail.com>
2025-03-06 13:06:36 -0500

    Update hook

--- client/sites/hooks/use-transfer-with-software-start-mutation.ts
+++ client/sites/hooks/use-transfer-with-software-start-mutation.ts
@@ -2,7 +2,7 @@ import { useMutation, UseMutationResult } from '@tanstack/react-query';
 import wpcom from 'calypso/lib/wp';
 
 type TransferWithSoftwareResponse = {
-	transferId: number;
+	transfer_with_software_id: number;
 };
 
 type SoftwareSlug = string;
