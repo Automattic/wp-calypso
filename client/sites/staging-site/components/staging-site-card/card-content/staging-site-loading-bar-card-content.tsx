4e2d06a606f229e22aeace744dc45f290eee84e3
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-03-06 18:05:50 +0100

    Fix formatting

--- client/sites/staging-site/components/staging-site-card/card-content/staging-site-loading-bar-card-content.tsx
+++ client/sites/staging-site/components/staging-site-card/card-content/staging-site-loading-bar-card-content.tsx
@@ -8,35 +8,35 @@ type CardContentProps = {
 	progress: number;
 };
 
-export const StagingSiteLoadingBarCardContent = ({
+export const StagingSiteLoadingBarCardContent = ( {
 	isReverting,
 	progress,
 	isOwner,
-}: CardContentProps) => {
+}: CardContentProps ) => {
 	{
 		const translate = useTranslate();
-		if (isReverting) {
+		if ( isReverting ) {
 			return (
 				<>
 					<ProgressBar
 						key="delete-loading-bar"
-						value={progress}
+						value={ progress }
 						className="staging-site-card__loading-bar"
 					/>
-					<p>{translate('We are deleting your staging site.')}</p>
+					<p>{ translate( 'We are deleting your staging site.' ) }</p>
 				</>
 			);
 		}
 
 		const message = isOwner
-			? translate('We are setting up your staging site. We’ll email you once it is ready.')
+			? translate( 'We are setting up your staging site. We’ll email you once it is ready.' )
 			: translate(
 					'We are setting up the staging site. We’ll email the site owner once it is ready.'
-				);
+			  );
 		return (
 			<div data-testid="transferring-staging-content">
-				<ProgressBar value={progress} className="staging-site-card__loading-bar" />
-				<p>{message}</p>
+				<ProgressBar value={ progress } className="staging-site-card__loading-bar" />
+				<p>{ message }</p>
 			</div>
 		);
 	}
