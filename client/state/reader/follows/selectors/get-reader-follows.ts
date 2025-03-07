305ac516394fd58c5efd699060a6124d6ad6f07e
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-03-08 00:49:19 +0500

    🐛 FIX: lint errors

--- client/state/reader/follows/selectors/get-reader-follows.ts
+++ client/state/reader/follows/selectors/get-reader-follows.ts
@@ -31,7 +31,8 @@ const getReaderFollows = createSelector(
 			( item ) =>
 				( item.site && item.site.is_error && item.site.error?.statusCode === 410 ) ||
 				( item.feed && item.feed.is_error )
-		);
+		) as typeof withSiteAndFeed;
+
 		return withoutErrors;
 	},
 	( state ) => [
