d42bc8620edd847aa565ac8f026d15121b0926f0
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-03-07 16:58:34 +0100

    Remove unnecessary initial syncing progress

--- client/state/sync/reducer.js
+++ client/state/sync/reducer.js
@@ -127,10 +127,6 @@ export const progress = withPersistence( ( state = 0, action ) => {
 			if ( action.isSyncingInProgress === false ) {
 				return 0;
 			}
-			// Set initial progress to 5% when sync is initiated
-			if ( action.isSyncingInProgress === true && state === 0 ) {
-				return 0.05;
-			}
 		case REQUEST_STATUS:
 			return state;
 		case SET_STATUS: {
