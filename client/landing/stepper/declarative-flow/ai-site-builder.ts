5565bc2ad198fd156f5c376ac94bde135e6a294c
Omar Alshaker <omar@omaralshaker.com>
2025-03-07 14:57:11 +0100

    Update ai-site-builder.ts (#100953)
    * Update ai-site-builder.ts
    
    * Delete client/landing/stepper/declarative-flow/internals/steps-repository/ai-site-builder-processing/index.tsx
    
    You can add you custom messages in `client/landing/stepper/declarative-flow/internals/steps-repository/processing-step/hooks/use-processing-loading-messages.tsx`
    
    * Discard changes to client/landing/stepper/declarative-flow/internals/steps.tsx

--- client/landing/stepper/declarative-flow/ai-site-builder.ts
+++ client/landing/stepper/declarative-flow/ai-site-builder.ts
@@ -1,92 +1,42 @@
-import { dispatch, useSelect } from '@wordpress/data';
-import { useEffect } from 'react';
-import { STEPS, PRIVATE_STEPS } from './internals/steps';
-import {
-	AssertConditionResult,
-	AssertConditionState,
-	Flow,
-	ProvidedDependencies,
-} from './internals/types';
-import { USER_STORE } from '../stores';
-import type { UserSelect } from '@automattic/data-stores';
-import wpcom from 'calypso/lib/wp';
-import { AccountCreateReturn } from 'calypso/lib/signup/api/type';
-import {
-	reloadProxy,
-	requestAllBlogsAccess,
-	wpcomRequest,
-} from '@automattic/data-stores/src/wpcom-request-controls';
-import config from '@automattic/calypso-config';
-import { createSite } from '../hooks/use-create-site-hook';
-import { fetchCurrentUser } from 'calypso/state/current-user/actions';
-import { remoteLoginUser } from 'calypso/state/login/actions/remote-login-user';
+import { stepsWithRequiredLogin } from '../utils/steps-with-required-login';
+import { STEPS } from './internals/steps';
+import { Flow, ProvidedDependencies } from './internals/types';
+
 export const AI_SITE_BUILDER_FLOW = 'ai-site-builder';
 
 const aiSiteBuilder: Flow = {
 	name: AI_SITE_BUILDER_FLOW,
+	/**
+	 * Should it fire calypso_signup_start event?
+	 */
 	isSignupFlow: true,
 	__experimentalUseBuiltinAuth: true,
-
-	useSteps() {
-		const userIsLoggedIn = useSelect(
-			( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
-			[]
-		);
-
-		return userIsLoggedIn ? [ STEPS.PROCESSING ] : [ PRIVATE_STEPS.USER, STEPS.PROCESSING ];
+	initialize() {
+		// stepsWithRequiredLogin will take care of redirecting to the login step if the user is not logged in.
+		return stepsWithRequiredLogin( [ STEPS.SITE_CREATION_STEP, STEPS.PROCESSING ] );
 	},
-
 	useStepNavigation( currentStep, navigate ) {
-		const userIsLoggedIn = useSelect(
-			( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
-			[]
-		);
-
-		async function submit( providedDependencies: ProvidedDependencies = {} ) {
+		function submit( providedDependencies: ProvidedDependencies = {} ) {
 			switch ( currentStep ) {
-				case 'user':
-					// The user step will handle loading the token in its useEffect
-					// We just need to navigate to the next step
-					return navigate( 'processing' );
-
-				case 'processing':
-					// Wait for the user to be logged in and token to be loaded
-					await new Promise( ( resolve ) => {
-						const checkAuth = () => {
-							if ( wpcom.token || userIsLoggedIn ) {
-								resolve( true );
-							} else {
-								setTimeout( checkAuth, 100 );
-							}
-						};
-						checkAuth();
-					} );
-
-					// Get the user
-					const user = await wpcom.req.get( '/me' );
-
-					// Create a new site using the user's username
-					const newSite = await wpcom.req.post( '/sites/new', {
-						blog_name: user.username,
-						blog_title: 'My New Website',
-						lang_id: 1,
-						client_id: config( 'wpcom_signup_id' ),
-						client_secret: config( 'wpcom_signup_key' ),
-					} );
-
-				// No idea how to log in a user if they use passwordless signup
-				// Not sure how to attach a site to a user
-				// I just want to redirect to the new site's admin dashboard with them logged in
+				// The create-site step will start creating a site and will add the promise of that operation to pendingAction field in the store.
+				case 'create-site': {
+					// Go to the processing step and pass `true` to remove it from history. So clicking back will not go back to the create-site step.
+					return navigate( 'processing', undefined, true );
+				}
+				// The processing step will wait the aforementioned promise to be resolved and then will submit to you whatever the promise resolves to.
+				// Which will be the created site { "siteId": "242341575", "siteSlug": "something.wordpress.com", "goToCheckout": false, "siteCreated": true }
+				case 'processing': {
+					const { siteSlug } = providedDependencies;
+					// You can redirect anywhere you want.
+					// Make sure to redirect using window.location.replace, so the user cannot go back to the processing step.
+					window.location.replace( `/home/${ siteSlug }` );
+				}
 				default:
 					return;
 			}
 		}
 
 		return { submit };
-	},
-
-	useAssertConditions(): AssertConditionResult {
-		return { state: AssertConditionState.SUCCESS };
 	},
 };
 
