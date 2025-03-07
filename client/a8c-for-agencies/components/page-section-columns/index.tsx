0cb4f2d054f07453e462b53c33a1d0f6ef791d11
Yashwin <yashwinpoojary@gmail.com>
2025-03-07 13:13:46 +0530

    Implement PageSectionColumns component

---
+++ client/a8c-for-agencies/components/page-section-columns/index.tsx
@@ -0,0 +1,56 @@
+import { useBreakpoint } from '@automattic/viewport-react';
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+import './style.scss';
+
+type SectionBackground = {
+	image?: string;
+	color?: string;
+	isDarkBackground?: boolean;
+};
+
+interface ColumnProps {
+	children?: ReactNode;
+	alignCenter?: boolean;
+}
+
+const Column = ( { children, alignCenter }: ColumnProps ) => (
+	<div className={ clsx( 'page-section-column', { 'is-align-center': alignCenter } ) }>
+		<div className="page-section-column__content">{ children }</div>
+	</div>
+);
+
+interface PageSectionColumnsProps {
+	heading?: ReactNode;
+	children: ReactNode;
+	background?: SectionBackground;
+}
+
+const PageSectionColumns = ( { heading, children, background }: PageSectionColumnsProps ) => {
+	const isNarrowView = useBreakpoint( '<960px' );
+
+	const backgroundImageUrl =
+		! isNarrowView && background?.image ? `url(${ background.image })` : undefined;
+
+	return (
+		<div
+			className={ clsx( 'page-section-columns', {
+				'is-dark-background': background?.isDarkBackground,
+				'is-narrow-view': isNarrowView,
+			} ) }
+			style={ {
+				backgroundColor: background?.color,
+				backgroundImage: backgroundImageUrl,
+				backgroundSize: 'auto 100%',
+			} }
+		>
+			{ heading && <div className="page-section-columns__heading">{ heading }</div> }
+			<div className="page-section-columns__content">{ children }</div>
+		</div>
+	);
+};
+
+PageSectionColumns.Column = Column;
+
+export default PageSectionColumns;
