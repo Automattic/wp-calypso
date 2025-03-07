ec17c5c61d28c45cd812813197961f8be41ef22c
heavyweight <kpapazov@gmail.com>
2025-03-07 15:30:40 +0100

    Refactor to functional component

--- client/me/account-close/main.jsx
+++ client/me/account-close/main.jsx
@@ -2,11 +2,11 @@ import page from '@automattic/calypso-router';
 import { Button as LegacyButton, Gridicon } from '@automattic/components';
 import { HelpCenter } from '@automattic/data-stores';
 import { Button } from '@wordpress/components';
-import { dispatch as dataStoreDispatch } from '@wordpress/data';
+import { dispatch as useDataStoreDispatch } from '@wordpress/data';
 import clsx from 'clsx';
 import { localize } from 'i18n-calypso';
 import { map } from 'lodash';
-import { Component, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import ActionPanel from 'calypso/components/action-panel';
 import ActionPanelBody from 'calypso/components/action-panel/body';
@@ -33,235 +33,215 @@ import './style.scss';
 
 const HELP_CENTER_STORE = HelpCenter.register();
 
-class AccountSettingsClose extends Component {
-	state = {
-		showConfirmDialog: false,
-		showSiteDropdown: true,
-	};
+const AccountSettingsClose = ( {
+	translate,
+	hasAtomicSites,
+	hasCancelablePurchases,
+	isLoading,
+	purchasedPremiumThemes,
+	sitesToBeDeleted,
+	isAccountAlreadyClosed,
+	handleRedirectToLogout,
+} ) => {
+	const [ showConfirmDialog, setShowConfirmDialog ] = useState( false );
+	const [ showSiteDropdown, setShowSiteDropdown ] = useState( true );
+	const { setNewMessagingChat } = useDataStoreDispatch( HELP_CENTER_STORE );
 
-	componentDidUpdate() {
-		// If the account is closed, logout
-		if ( this.props.isAccountClosed === true ) {
-			this.props.redirectToLogout();
+	useEffect( () => {
+		if ( isAccountAlreadyClosed ) {
+			handleRedirectToLogout();
 		}
-	}
+	}, [ isAccountAlreadyClosed, handleRedirectToLogout ] );
 
-	goBack = () => {
-		page( '/me/account' );
-	};
-
-	handleDeleteClick = ( event ) => {
-		event.preventDefault();
-
-		// Check if purchases and sites have loaded
-		if ( this.props.isLoading ) {
-			return false;
-		}
-
-		this.setState( { showConfirmDialog: true } );
-	};
-
-	closeConfirmDialog = () => {
-		this.setState( { showConfirmDialog: false } );
-	};
-
-	handleSiteDropdown = () => {
-		this.setState( ( state ) => ( {
-			showSiteDropdown: ! state.showSiteDropdown,
-		} ) );
-	};
-
-	render() {
-		const { translate, hasAtomicSites, hasCancelablePurchases, isLoading, purchasedPremiumThemes } =
-			this.props;
-		const isDeletePossible = ! isLoading && ! hasAtomicSites && ! hasCancelablePurchases;
-		const containerClasses = clsx( 'account-close', 'main', 'is-wide-layout', {
-			'is-loading': isLoading,
-			'is-hiding-other-sites': this.state.showSiteDropdown,
+	const handleContactSupport = () => {
+		setNewMessagingChat( {
+			initialMessage: 'User is contacting us requesting information about account deletion',
+			section: 'account-deletion',
 		} );
+	};
+	const goBack = () => page( '/me/account' );
+	const handleDeleteClick = ( event ) => {
+		event.preventDefault();
+		if ( ! isLoading ) {
+			setShowConfirmDialog( true );
+		}
+	};
+	const closeConfirmDialog = () => setShowConfirmDialog( false );
+	const toggleSiteDropdown = () => setShowSiteDropdown( ( prev ) => ! prev );
 
-		return (
-			<div className={ containerClasses } role="main">
-				<QueryUserPurchases />
-				<NavigationHeader navigationItems={ [] } title={ translate( 'Account Settings' ) } />
+	const isDeletePossible = ! isLoading && ! hasAtomicSites && ! hasCancelablePurchases;
+	const containerClasses = clsx( 'account-close', 'main', 'is-wide-layout', {
+		'is-loading': isLoading,
+		'is-hiding-other-sites': showSiteDropdown,
+	} );
 
-				<HeaderCake onClick={ this.goBack }>
-					<h1>{ translate( 'Delete account' ) }</h1>
-				</HeaderCake>
-				<ActionPanel>
-					<ActionPanelBody>
-						{ isDeletePossible && (
-							<ActionPanelFigure>
-								<ActionPanelFigureHeader>
-									{ translate( 'These items will be deleted' ) }
-								</ActionPanelFigureHeader>
-								<ActionPanelFigureList>
-									<ActionPanelFigureListItem>
-										{ translate( 'Personal details' ) }
-									</ActionPanelFigureListItem>
-									{ this.props.sitesToBeDeleted.length > 0 && (
-										<Fragment>
-											<ActionPanelFigureListItem className="account-close__sites-item">
-												{ translate( 'Sites' ) }
-												<Gridicon
-													size={ 18 }
-													onClick={ this.handleSiteDropdown }
-													icon="chevron-down"
-												/>
-												{ this.state.showSiteDropdown && (
-													<ul className="account-close__sites-list">
-														{ this.props.sitesToBeDeleted.map( ( sitesToBeDeleted ) => (
-															<li key={ sitesToBeDeleted.slug }>
-																{ [ sitesToBeDeleted.name ] }
-																<span>{ [ sitesToBeDeleted.slug ] }</span>
-															</li>
-														) ) }
-													</ul>
-												) }
-											</ActionPanelFigureListItem>
-											<ActionPanelFigureListItem>
-												{ translate( 'Posts' ) }
-											</ActionPanelFigureListItem>
-											<ActionPanelFigureListItem>
-												{ translate( 'Pages' ) }
-											</ActionPanelFigureListItem>
-											<ActionPanelFigureListItem>
-												{ translate( 'Media' ) }
-											</ActionPanelFigureListItem>
-											<ActionPanelFigureListItem>
-												{ translate( 'Domains' ) }
-											</ActionPanelFigureListItem>
-										</Fragment>
-									) }
-									{ purchasedPremiumThemes && purchasedPremiumThemes.length > 0 && (
-										<ActionPanelFigureListItem>
-											{ translate( 'Premium themes' ) }
-										</ActionPanelFigureListItem>
-									) }
-									<ActionPanelFigureListItem>Gravatar</ActionPanelFigureListItem>
-								</ActionPanelFigureList>
-							</ActionPanelFigure>
-						) }
-						{ ! isLoading && hasCancelablePurchases && (
-							<Fragment>
-								<p className="account-close__body-copy">
-									{ translate( 'You still have active purchases on your account.' ) }
-								</p>
-								<p className="account-close__body-copy">
-									{ translate(
-										"To delete your account, you'll need to cancel any active purchases " +
-											'in {{a}}Manage Purchases{{/a}} before proceeding.',
-										{
-											components: {
-												a: <ActionPanelLink href="/me/purchases" />,
-											},
-										}
-									) }
-								</p>
-							</Fragment>
-						) }
-						{ ! isLoading && hasAtomicSites && ! hasCancelablePurchases && (
-							<Fragment>
-								<p className="account-close__body-copy">
-									{ translate(
-										'We are still in the process of removing one or more of your sites. This process normally takes 15-20 minutes. Once removal is completed, you should be able to close your account from this page.'
-									) }
-								</p>
-								<p className="account-close__body-copy">
-									{ translate( 'To close this account now, {{a}}contact our support team{{/a}}.', {
-										components: {
-											a: <Button onClick={ this.props.handleContactSupport } variant="link" />,
-										},
-									} ) }
-								</p>
-							</Fragment>
-						) }
+	return (
+		<div className={ containerClasses } role="main">
+			<QueryUserPurchases />
+			<NavigationHeader navigationItems={ [] } title={ translate( 'Account Settings' ) } />
 
-						{ ( isLoading || isDeletePossible ) && (
-							<Fragment>
-								<p className="account-close__body-copy">
-									<strong>
-										{ translate(
-											'Deleting your account will also delete all your sites and their content.'
-										) }
-									</strong>
-								</p>
-								{ purchasedPremiumThemes && purchasedPremiumThemes.length > 0 && (
+			<HeaderCake onClick={ goBack }>
+				<h1>{ translate( 'Delete account' ) }</h1>
+			</HeaderCake>
+			<ActionPanel>
+				<ActionPanelBody>
+					{ isDeletePossible && (
+						<ActionPanelFigure>
+							<ActionPanelFigureHeader>
+								{ translate( 'These items will be deleted' ) }
+							</ActionPanelFigureHeader>
+							<ActionPanelFigureList>
+								<ActionPanelFigureListItem>
+									{ translate( 'Personal details' ) }
+								</ActionPanelFigureListItem>
+								{ sitesToBeDeleted.length > 0 && (
 									<Fragment>
-										{ translate(
-											'You will also lose access to the following premium themes you have purchased:'
-										) }
-										<ul className="account-close__theme-list">
-											{ map( purchasedPremiumThemes, ( purchasedPremiumTheme ) => {
-												return (
-													<li key={ purchasedPremiumTheme.id }>
-														{ purchasedPremiumTheme.productName }
-													</li>
-												);
-											} ) }
-										</ul>
+										<ActionPanelFigureListItem className="account-close__sites-item">
+											{ translate( 'Sites' ) }
+											<Gridicon size={ 18 } onClick={ toggleSiteDropdown } icon="chevron-down" />
+											{ showSiteDropdown && (
+												<ul className="account-close__sites-list">
+													{ sitesToBeDeleted.map( ( siteToBeDeleted ) => (
+														<li key={ siteToBeDeleted.slug }>
+															{ [ siteToBeDeleted.name ] }
+															<span>{ [ siteToBeDeleted.slug ] }</span>
+														</li>
+													) ) }
+												</ul>
+											) }
+										</ActionPanelFigureListItem>
+										<ActionPanelFigureListItem>{ translate( 'Posts' ) }</ActionPanelFigureListItem>
+										<ActionPanelFigureListItem>{ translate( 'Pages' ) }</ActionPanelFigureListItem>
+										<ActionPanelFigureListItem>{ translate( 'Media' ) }</ActionPanelFigureListItem>
+										<ActionPanelFigureListItem>
+											{ translate( 'Domains' ) }
+										</ActionPanelFigureListItem>
 									</Fragment>
 								) }
-								<p className="account-close__body-copy">
+								{ purchasedPremiumThemes && purchasedPremiumThemes.length > 0 && (
+									<ActionPanelFigureListItem>
+										{ translate( 'Premium themes' ) }
+									</ActionPanelFigureListItem>
+								) }
+								<ActionPanelFigureListItem>Gravatar</ActionPanelFigureListItem>
+							</ActionPanelFigureList>
+						</ActionPanelFigure>
+					) }
+					{ ! isLoading && hasCancelablePurchases && (
+						<Fragment>
+							<p className="account-close__body-copy">
+								{ translate( 'You still have active purchases on your account.' ) }
+							</p>
+							<p className="account-close__body-copy">
+								{ translate(
+									"To delete your account, you'll need to cancel any active purchases " +
+										'in {{a}}Manage Purchases{{/a}} before proceeding.',
+									{
+										components: {
+											a: <ActionPanelLink href="/me/purchases" />,
+										},
+									}
+								) }
+							</p>
+						</Fragment>
+					) }
+					{ ! isLoading && hasAtomicSites && ! hasCancelablePurchases && (
+						<Fragment>
+							<p className="account-close__body-copy">
+								{ translate(
+									'We are still in the process of removing one or more of your sites. This process normally takes 15-20 minutes. Once removal is completed, you should be able to close your account from this page.'
+								) }
+							</p>
+							<p className="account-close__body-copy">
+								{ translate( 'To close this account now, {{a}}contact our support team{{/a}}.', {
+									components: {
+										a: <Button onClick={ handleContactSupport } variant="link" />,
+									},
+								} ) }
+							</p>
+						</Fragment>
+					) }
+
+					{ ( isLoading || isDeletePossible ) && (
+						<Fragment>
+							<p className="account-close__body-copy">
+								<strong>
 									{ translate(
-										'You will not be able to open a new WordPress.com account using the same email address for 30 days.'
+										'Deleting your account will also delete all your sites and their content.'
 									) }
-								</p>
-								<p className="account-close__body-copy">
+								</strong>
+							</p>
+							{ purchasedPremiumThemes && purchasedPremiumThemes.length > 0 && (
+								<Fragment>
 									{ translate(
-										'You will not be able to log in to any other Automattic Services that use your WordPress.com account as a login. This includes WooCommerce.com, Crowdsignal.com, IntenseDebate.com, and Gravatar.com. Once your WordPress.com account is deleted, these services will also be deleted and you will lose access to any orders or support history you may have.'
+										'You will also lose access to the following premium themes you have purchased:'
 									) }
-								</p>
-								<p className="account-close__body-copy">
-									{ translate(
-										'If you have any questions at all about what happens when you delete an account, ' +
-											'please {{a}}contact someone from our support team{{/a}} first. ' +
-											"They'll explain the ramifications and help you explore alternatives. ",
-										{
-											components: {
-												a: <Button onClick={ this.props.handleContactSupport } variant="link" />,
-											},
-										}
-									) }
-								</p>
-							</Fragment>
-						) }
-					</ActionPanelBody>
-					<ActionPanelFooter>
-						{ ( isLoading || isDeletePossible ) && (
-							<LegacyButton
-								scary
-								onClick={ this.handleDeleteClick }
-								data-testid="close-account-button"
-							>
-								<Gridicon icon="trash" />
-								{ translate( 'Delete account', { context: 'button label' } ) }
-							</LegacyButton>
-						) }
-						{ hasAtomicSites && ! hasCancelablePurchases && (
-							<LegacyButton
-								primary
-								onClick={ this.props.handleContactSupport }
-								data-testid="contact-support-button"
-							>
-								{ translate( 'Contact support' ) }
-							</LegacyButton>
-						) }
-						{ hasCancelablePurchases && (
-							<LegacyButton primary href="/me/purchases" data-testid="manage-purchases-button">
-								{ translate( 'Manage purchases', { context: 'button label' } ) }
-							</LegacyButton>
-						) }
-					</ActionPanelFooter>
-					<AccountCloseConfirmDialog
-						isVisible={ this.state.showConfirmDialog }
-						closeConfirmDialog={ this.closeConfirmDialog }
-					/>
-				</ActionPanel>
-			</div>
-		);
-	}
-}
+									<ul className="account-close__theme-list">
+										{ map( purchasedPremiumThemes, ( purchasedPremiumTheme ) => {
+											return (
+												<li key={ purchasedPremiumTheme.id }>
+													{ purchasedPremiumTheme.productName }
+												</li>
+											);
+										} ) }
+									</ul>
+								</Fragment>
+							) }
+							<p className="account-close__body-copy">
+								{ translate(
+									'You will not be able to open a new WordPress.com account using the same email address for 30 days.'
+								) }
+							</p>
+							<p className="account-close__body-copy">
+								{ translate(
+									'You will not be able to log in to any other Automattic Services that use your WordPress.com account as a login. This includes WooCommerce.com, Crowdsignal.com, IntenseDebate.com, and Gravatar.com. Once your WordPress.com account is deleted, these services will also be deleted and you will lose access to any orders or support history you may have.'
+								) }
+							</p>
+							<p className="account-close__body-copy">
+								{ translate(
+									'If you have any questions at all about what happens when you delete an account, ' +
+										'please {{a}}contact someone from our support team{{/a}} first. ' +
+										"They'll explain the ramifications and help you explore alternatives. ",
+									{
+										components: {
+											a: <Button onClick={ handleContactSupport } variant="link" />,
+										},
+									}
+								) }
+							</p>
+						</Fragment>
+					) }
+				</ActionPanelBody>
+				<ActionPanelFooter>
+					{ ( isLoading || isDeletePossible ) && (
+						<LegacyButton scary onClick={ handleDeleteClick } data-testid="close-account-button">
+							<Gridicon icon="trash" />
+							{ translate( 'Delete account', { context: 'button label' } ) }
+						</LegacyButton>
+					) }
+					{ hasAtomicSites && ! hasCancelablePurchases && (
+						<LegacyButton
+							primary
+							onClick={ handleContactSupport }
+							data-testid="contact-support-button"
+						>
+							{ translate( 'Contact support' ) }
+						</LegacyButton>
+					) }
+					{ hasCancelablePurchases && (
+						<LegacyButton primary href="/me/purchases" data-testid="manage-purchases-button">
+							{ translate( 'Manage purchases', { context: 'button label' } ) }
+						</LegacyButton>
+					) }
+				</ActionPanelFooter>
+				<AccountCloseConfirmDialog
+					isVisible={ showConfirmDialog }
+					closeConfirmDialog={ closeConfirmDialog }
+				/>
+			</ActionPanel>
+		</div>
+	);
+};
 
 export default connect(
 	( state ) => {
@@ -276,17 +256,11 @@ export default connect(
 			hasCancelablePurchases: hasCancelableUserPurchases( state ),
 			purchasedPremiumThemes,
 			hasAtomicSites: userHasAnyAtomicSites( state ),
-			isAccountClosed: isAccountClosed( state ),
+			isAccountAlreadyClosed: isAccountClosed( state ),
 			sitesToBeDeleted: getAccountClosureSites( state ),
 		};
 	},
 	{
-		redirectToLogout,
-		handleContactSupport: () => {
-			dataStoreDispatch( HELP_CENTER_STORE ).setNewMessagingChat( {
-				initialMessage: 'User is contacting us requesting information about account deletion',
-				section: 'account-deletion',
-			} );
-		},
+		handleRedirectToLogout: redirectToLogout,
 	}
 )( localize( AccountSettingsClose ) );
