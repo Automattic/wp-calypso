d5291cd4b153973a19a4af2e9166bc4f03147c57
Allison Levine <allison.levine@automattic.com>
2025-03-10 16:18:21 -0400

    Move Jetpack subscribers empty state to separate component.

--- client/my-sites/subscribers/components/empty-list-view/empty-list-view.tsx
+++ client/my-sites/subscribers/components/empty-list-view/empty-list-view.tsx
@@ -1,12 +1,10 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import page from '@automattic/calypso-router';
 import { localizeUrl } from '@automattic/i18n-utils';
 import { Card, CardBody, Icon } from '@wordpress/components';
-import { copy, upload, reusableBlock, chevronRight } from '@wordpress/icons';
-import { useTranslate, default as i18n } from 'i18n-calypso';
+import { chartBar, chevronRight, people, trendingUp } from '@wordpress/icons';
+import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import getIsSiteWPCOM from 'calypso/state/selectors/is-site-wpcom';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
@@ -15,12 +13,25 @@ import './style.scss';
 type EmptyListCTALinkProps = {
 	icon: JSX.Element;
 	text: string;
-	onClick: () => void;
+	url: string;
+	eventName: string;
 };
 
-const EmptyListCTALink = ( { icon, text, onClick }: EmptyListCTALinkProps ) => {
+const EmptyListCTALink = ( { icon, text, url, eventName }: EmptyListCTALinkProps ) => {
+	const handleClick = () => {
+		recordTracksEvent( eventName );
+	};
+
 	return (
-		<Card className="empty-list-view__cta-link" size="small" as="button" onClick={ onClick }>
+		<Card
+			className="empty-list-view__cta-link"
+			size="small"
+			as="a"
+			href={ url }
+			rel="noreferrer"
+			target="_blank"
+			onClick={ handleClick }
+		>
 			<CardBody className="empty-list-view__card-body">
 				<Icon className="empty-list-view__cta-link-icon" icon={ icon } size={ 20 } />
 				<span className="empty-list-view__cta-link-text">{ text }</span>
@@ -32,82 +43,52 @@ const EmptyListCTALink = ( { icon, text, onClick }: EmptyListCTALinkProps ) => {
 
 const EmptyListView = () => {
 	const translate = useTranslate();
-	const selectedSite = useSelector( getSelectedSite );
-	const isWPCOMSite = useSelector( ( state ) => getIsSiteWPCOM( state, selectedSite?.ID ) );
 
 	// Record an event when the empty view is rendered
 	useEffect( () => {
 		recordTracksEvent( 'calypso_subscribers_empty_view_displayed' );
 	}, [] );
 
+	const selectedSite = useSelector( getSelectedSite );
+	const siteId = selectedSite?.ID || null;
+	const isWPCOMSite = useSelector( ( state ) => getIsSiteWPCOM( state, siteId ) );
+
 	const subscribeBlockUrl = ! isWPCOMSite
 		? 'https://jetpack.com/support/jetpack-blocks/subscription-form-block/'
 		: 'https://wordpress.com/support/wordpress-editor/blocks/subscribe-block/';
 
-	const handleMethodSelect = ( method: string ) => {
-		recordTracksEvent( 'calypso_subscribers_empty_view_add_method_clicked', {
-			method,
-		} );
-		if ( method === 'substack' ) {
-			if ( isJetpackCloud() ) {
-				window.location.href = `https://wordpress.com/import/newsletter/substack/${
-					selectedSite?.slug || selectedSite?.ID || ''
-				}`;
-			} else {
-				page( `/import/newsletter/substack/${ selectedSite?.slug || selectedSite?.ID || '' }` );
-			}
-			return;
-		}
-		// Update URL hash with selected method
-		window.location.hash = `#add-subscribers?method=${ method }`;
-	};
+	const importSubscribersUrl = ! isWPCOMSite
+		? 'https://jetpack.com/support/newsletter/import-subscribers/'
+		: 'https://wordpress.com/support/launch-a-newsletter/import-subscribers-to-a-newsletter/';
 
 	return (
 		<div className="empty-list-view">
-			<h2 className="empty-list-view__title">
-				{ translate( 'Add subscribers to %s', {
-					args: [ selectedSite?.title || '' ],
-					comment: "%s is the site's title",
-				} ) }
-			</h2>
+			<h2 className="empty-list-view__title">{ translate( 'Grow your subscribers' ) }</h2>
 			<p className="empty-list-view__description">
-				{ i18n.fixMe( {
-					text: translate( 'No subscribers yet? Turn your site visitors into subscribers.' ),
-					newCopy: translate(
-						'No subscribers yet? {{howToTurnVisitorsLink}}Turn your site visitors into subscribers.{{/howToTurnVisitorsLink}}',
-						{
-							components: {
-								howToTurnVisitorsLink: (
-									<a
-										href={ localizeUrl( subscribeBlockUrl ) }
-										target="_blank"
-										rel="noopener noreferrer"
-										onClick={ () =>
-											recordTracksEvent( 'calypso_subscribers_empty_view_subscribe_block_clicked' )
-										}
-									/>
-								),
-							},
-						}
-					),
-					oldCopy: '',
-				} ) }
+				{ translate(
+					'Publishing & sharing content can help bring traffic to your site. Let’s help you get started.'
+				) }
 			</p>
 			<EmptyListCTALink
-				icon={ copy }
-				text={ translate( 'Add subscribers manually' ) }
-				onClick={ () => handleMethodSelect( 'manually' ) }
+				icon={ chartBar }
+				text={ translate( 'Turn your visitors into subscribers' ) }
+				url={ localizeUrl( subscribeBlockUrl ) }
+				eventName="calypso_subscribers_empty_view_subscribe_block_clicked"
 			/>
 			<EmptyListCTALink
-				icon={ upload }
-				text={ translate( 'Use a CSV file' ) }
-				onClick={ () => handleMethodSelect( 'upload' ) }
+				icon={ people }
+				text={ translate( 'Import existing subscribers' ) }
+				url={ localizeUrl( importSubscribersUrl ) }
+				eventName="calypso_subscribers_empty_view_import_subscribers_clicked"
 			/>
-			<EmptyListCTALink
-				icon={ reusableBlock }
-				text={ translate( 'Import from Substack' ) }
-				onClick={ () => handleMethodSelect( 'substack' ) }
-			/>
+			{ isWPCOMSite && (
+				<EmptyListCTALink
+					icon={ trendingUp }
+					text={ translate( 'Grow your audience' ) }
+					url={ localizeUrl( 'https://wordpress.com/support/category/grow-your-audience/' ) }
+					eventName="calypso_subscribers_empty_view_grow_your_audience_clicked"
+				/>
+			) }
 		</div>
 	);
 };
