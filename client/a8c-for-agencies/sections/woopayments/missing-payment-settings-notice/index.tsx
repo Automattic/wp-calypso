81f7b47cf9e056484dabc51f7102a6262d00cc51
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-03-10 18:16:21 +0800

    Use proper constant variable for the settings link.

--- client/a8c-for-agencies/sections/woopayments/missing-payment-settings-notice/index.tsx
+++ client/a8c-for-agencies/sections/woopayments/missing-payment-settings-notice/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import LayoutBanner from 'calypso/a8c-for-agencies/components/layout/banner';
+import { A4A_WOOPAYMENTS_PAYMENT_SETTINGS_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import useGetTipaltiPayee from '../../referrals/hooks/use-get-tipalti-payee';
 
 import './style.scss';
@@ -25,7 +26,7 @@ export const MissingPaymentSettingsNotice = () => {
 			<div>{ translate( 'To receive your revenue share, add your payment information.' ) }</div>
 			<Button
 				className="missing-payment-settings-notice__button is-dark"
-				href="/woopayments/payment-settings"
+				href={ A4A_WOOPAYMENTS_PAYMENT_SETTINGS_LINK }
 			>
 				{ translate( 'Add your payment information' ) }
 			</Button>
