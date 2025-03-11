eca089112efb5925ebe390e9965cf022de961af5
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-03-12 00:45:39 +0500

    ➕ ADDS: ReaderFeedItem in preview and fixes padding

--- client/landing/subscriptions/components/feed-preview/feed-preview.tsx
+++ client/landing/subscriptions/components/feed-preview/feed-preview.tsx
@@ -1,58 +1,72 @@
+import './feed-preview.styles.scss';
+import { Reader } from '@automattic/data-stores';
 import { useTranslate } from 'i18n-calypso';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useDebounce } from 'use-debounce';
+import ReaderFeedItem from 'calypso/blocks/reader-feed-item';
 import wpcom from 'calypso/lib/wp';
 import Stream from 'calypso/reader/stream';
 
 interface GetFeedResponse {
-	feeds: { feed_ID: number }[];
+	feeds: {
+		feed_ID: string;
+		subscribe_URL: string;
+		meta: object;
+	}[];
 }
 
 interface FeedPreviewProps {
 	url: string;
+	source: string;
 }
 
 export default function FeedPreview( props: FeedPreviewProps ): JSX.Element | null {
-	const { url } = props;
+	const { url, source } = props;
 	const translate = useTranslate();
 	const dispatch = useDispatch();
 	const [ debouncedUrl ] = useDebounce( url, 700 );
-	const [ feedId, setFeedId ] = useState< number >( 0 );
+	const [ feed, setFeed ] = useState< Reader.FeedItem >();
 
 	/**
-	 * Fetch the feed ID for the given URL.
+	 * Fetch the feed for the given URL.
 	 */
 	useEffect( (): void => {
-		setFeedId( 0 ); // Reset feed ID before each request.
+		setFeed( undefined ); // Reset feed before each request.
 
 		wpcom.req
 			.get( '/read/feed', { url: debouncedUrl } )
 			.then( ( response: GetFeedResponse ): void => {
-				const feedId: number | undefined = response?.feeds?.[ 0 ]?.feed_ID;
-				if ( ! feedId ) {
+				const feed = response?.feeds?.[ 0 ];
+				if ( ! feed ) {
 					return;
 				}
 
-				setFeedId( feedId );
+				setFeed( feed );
 			} );
 	}, [ dispatch, debouncedUrl, translate ] );
 
 	const FeedPreviewContent = (): JSX.Element | null => {
-		if ( ! feedId ) {
+		if ( ! feed ) {
 			return null;
 		}
 
 		return (
-			<div className="feed-preview__stream">
-				<Stream
-					className="is-site-stream"
-					streamKey={ `feed:${ feedId }` }
-					useCompactCards
-					showFollowButton={ false }
-					suppressSiteNameLink
-				/>
-			</div>
+			<>
+				<ul className="feed-preview__site">
+					<ReaderFeedItem feed={ feed } source={ source } />
+				</ul>
+
+				<div className="feed-preview__stream">
+					<Stream
+						className="no-padding"
+						streamKey={ `feed:${ feed.feed_ID }` }
+						useCompactCards
+						showFollowButton={ false }
+						suppressSiteNameLink
+					/>
+				</div>
+			</>
 		);
 	};
 
