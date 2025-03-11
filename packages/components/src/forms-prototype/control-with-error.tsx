e79360a3329dbe24fd739fc1a192f706f91e05a1
Lena Morita <lena@jaguchi.com>
2025-03-12 01:35:11 +0900

    Refactor

---
+++ packages/components/src/forms-prototype/control-with-error.tsx
@@ -0,0 +1,76 @@
+import { Icon } from '@wordpress/components';
+import { caution } from '@wordpress/icons';
+import { cloneElement, useRef, useState } from 'react';
+
+export function ControlWithError< C extends React.ReactElement >( {
+	onReportCustomValidity,
+	render,
+	...props
+}: {
+	onReportCustomValidity?: ( value: string ) => string | void;
+	render: C;
+} ) {
+	const [ errorMessage, setErrorMessage ] = useState< string | undefined >();
+	const ref = useRef< HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement >( null );
+
+	const validate = () => {
+		if ( ! ref.current ) {
+			return;
+		}
+
+		const message = onReportCustomValidity?.( ref.current.value );
+		ref.current.setCustomValidity?.( message ?? '' );
+
+		setErrorMessage( ref.current.validationMessage );
+	};
+
+	const onBlur = ( ...args ) => {
+		validate();
+
+		// Workaround for setCustomValidity() forcing an immediate re-render,
+		// which can reset the field value in uncontrolled mode.
+		const previousValue = ref.current?.value;
+		setTimeout( () => {
+			if ( ref.current ) {
+				ref.current.value = previousValue ?? '';
+			}
+		}, 0 );
+
+		render.props.onBlur?.( ...args );
+	};
+
+	const onChange = ( ...args ) => {
+		// Only validate incrementally if the value is already marked as invalid.
+		if ( ! ref.current?.validity?.valid ) {
+			validate();
+		}
+
+		render.props.onChange?.( ...args );
+	};
+
+	const label = render.props.required ? (
+		<>
+			{ render.props.label } <span aria-hidden="true">(Required)</span>
+		</>
+	) : (
+		render.props.label
+	);
+
+	return (
+		<div className="a8c-use-validation">
+			{ cloneElement( render, {
+				...props,
+				label,
+				onBlur,
+				onChange,
+				ref,
+			} ) }
+			{ errorMessage && (
+				<p className="a8c-use-validation__error">
+					<Icon icon={ caution } size={ 16 } fill="currentColor" />
+					{ errorMessage }
+				</p>
+			) }
+		</div>
+	);
+}
--- packages/components/src/forms-prototype/forms.stories.tsx
+++ packages/components/src/forms-prototype/forms.stories.tsx
@@ -1,7 +1,6 @@
 import {
 	Button,
 	CheckboxControl,
-	Icon,
 	/* eslint-disable wpcalypso/no-unsafe-wp-apis */
 	__experimentalInputControl as InputControl,
 	__experimentalNumberControl as NumberControl,
@@ -17,8 +16,8 @@ import {
 	RadioControl,
 	RangeControl,
 } from '@wordpress/components';
-import { caution } from '@wordpress/icons';
-import { cloneElement, useRef, useState } from 'react';
+import { useState } from 'react';
+import { ControlWithError } from './control-with-error';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import './style.scss';
@@ -27,79 +26,6 @@ const meta: Meta = {
 	title: 'Prototypes/Forms',
 };
 export default meta;
-
-function ControlWithError< C extends React.ReactElement >( {
-	onReportCustomValidity,
-	render,
-	...props
-}: {
-	onReportCustomValidity?: ( value: string ) => string | void;
-	render: C;
-} ) {
-	const [ errorMessage, setErrorMessage ] = useState< string | undefined >();
-	const ref = useRef< HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement >( null );
-
-	const validate = () => {
-		if ( ! ref.current ) {
-			return;
-		}
-
-		const message = onReportCustomValidity?.( ref.current.value );
-		ref.current.setCustomValidity?.( message ?? '' );
-
-		setErrorMessage( ref.current.validationMessage );
-	};
-
-	const onBlur = ( ...args ) => {
-		validate();
-
-		// Workaround for setCustomValidity() forcing an immediate re-render,
-		// which can reset the field value in uncontrolled mode.
-		const previousValue = ref.current?.value;
-		setTimeout( () => {
-			if ( ref.current ) {
-				ref.current.value = previousValue ?? '';
-			}
-		}, 0 );
-
-		render.props.onBlur?.( ...args );
-	};
-
-	const onChange = ( ...args ) => {
-		// Only validate incrementally if the value is already marked as invalid.
-		if ( ! ref.current?.validity?.valid ) {
-			validate();
-		}
-
-		render.props.onChange?.( ...args );
-	};
-
-	const label = render.props.required ? (
-		<>
-			{ render.props.label } <span aria-hidden="true">(Required)</span>
-		</>
-	) : (
-		render.props.label
-	);
-
-	return (
-		<div className="a8c-use-validation">
-			{ cloneElement( render, {
-				...props,
-				label,
-				onBlur,
-				onChange,
-				ref,
-			} ) }
-			{ errorMessage && (
-				<p className="a8c-use-validation__error">
-					<Icon icon={ caution } size={ 16 } fill="currentColor" />
-					{ errorMessage }
-				</p>
-			) }
-		</div>
-	);
-}
 
 export const Default: StoryObj = {
 	render: function Template() {
