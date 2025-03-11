2bd0eb5c0eecbf2d8f04ff2e8c722f90fd2fb7c4
Lena Morita <lena@jaguchi.com>
2025-03-12 02:06:10 +0900

    Split ToggleGroup story

--- packages/components/src/forms-prototype/forms.stories.tsx
+++ packages/components/src/forms-prototype/forms.stories.tsx
@@ -24,18 +24,11 @@ import './style.scss';
 
 const meta: Meta = {
 	title: 'Prototypes/Forms',
-};
-export default meta;
-
-export const Default: StoryObj = {
-	render: function Template() {
-		const [ toggleControlChecked, setToggleControlChecked ] = useState( false );
-		const [ checkboxControlChecked, setCheckboxControlChecked ] = useState( false );
-		const [ radioControlChecked, setRadioControlChecked ] = useState< string | undefined >();
-
-		return (
+	decorators: [
+		( Story ) => (
 			<form
 				style={ {
+					fontFamily: 'sans-serif',
 					display: 'flex',
 					flexDirection: 'column',
 					alignItems: 'flex-start',
@@ -51,224 +44,217 @@ export const Default: StoryObj = {
 						width: 300,
 					} }
 				>
-					<ControlWithError
-						render={
-							<InputControl
-								__next40pxDefaultSize
-								required
-								label="Input"
-								help="The word 'error' will trigger an error."
-							/>
-						}
-						onReportCustomValidity={ ( value ) => {
-							if ( value.toLowerCase() === 'error' ) {
-								return 'The word "error" is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<NumberControl
-								__next40pxDefaultSize
-								label="Number"
-								help="Odd numbers are not allowed."
-							/>
-						}
-						onReportCustomValidity={ ( value ) => {
-							if ( value && parseInt( value, 10 ) % 2 !== 0 ) {
-								return 'Choose an even number.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							// TODO: Use of `required` renders an invalid label in HTML.
-							<ToggleGroupControl
-								__nextHasNoMarginBottom
-								label="Toggle Group"
-								isBlock
-								__next40pxDefaultSize
-								required
-								value="1"
-							>
-								<ToggleGroupControlOption value="1" label="Option 1" />
-								<ToggleGroupControlOption value="2" label="Option 2" />
-							</ToggleGroupControl>
-						}
-						// TODO: Needs custom handling
-						onReportCustomValidity={ ( value ) => {
-							if ( value === '1' ) {
-								return 'Option 1 is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							// TODO: Rest props are not passed down.
-							<ToggleControl
-								required
-								__nextHasNoMarginBottom
-								label="Toggle"
-								checked={ toggleControlChecked }
-								onChange={ setToggleControlChecked }
-							/>
-						}
-						onReportCustomValidity={ ( value ) => {
-							if ( value ) {
-								return 'Checkbox is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<CheckboxControl
-								__nextHasNoMarginBottom
-								required
-								label="Checkbox"
-								checked={ checkboxControlChecked }
-								onChange={ setCheckboxControlChecked }
-							/>
-						}
-						// TODO: Ref is not forwarded.
-						onReportCustomValidity={ ( value ) => {
-							if ( value ) {
-								return 'Checkbox is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<SelectControl
-								required
-								__nextHasNoMarginBottom
-								__next40pxDefaultSize
-								label="Select"
-								help="Option 1 is not allowed."
-								options={ [
-									{ value: '', label: 'Select an option' },
-									{ value: '1', label: 'Option 1 (not allowed)' },
-									{ value: '2', label: 'Option 2' },
-								] }
-							/>
-						}
-						onReportCustomValidity={ ( value ) => {
-							if ( value === '1' ) {
-								return 'Option 1 is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<CustomSelectControl
-								// TODO: Required isn't passed down correctly.
-								required
-								__next40pxDefaultSize
-								label="Custom Select"
-								options={ [
-									{ key: '', name: 'Select an option' },
-									{ key: 'a', name: 'Option A (not allowed)' },
-									{ key: 'b', name: 'Option B' },
-								] }
-							/>
-						}
-						// TODO: Ref is not forwarded.
-						onReportCustomValidity={ ( value ) => {
-							if ( value === 'a' ) {
-								return 'Option A is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<ComboboxControl
-								__nextHasNoMarginBottom
-								__next40pxDefaultSize
-								// TODO: Rest props are not passed down.
-								required
-								label="Combobox"
-								help="Option A is not allowed."
-								options={ [
-									{ value: 'a', label: 'Option A (not allowed)' },
-									{ value: 'b', label: 'Option B' },
-								] }
-							/>
-						}
-						// TODO: onBlur is not passed down.
-						onReportCustomValidity={ ( value ) => {
-							if ( value === 'a' ) {
-								return 'Option A is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<RadioControl
-								label="Radio"
-								required
-								help="Option A is not allowed"
-								selected={ radioControlChecked }
-								onChange={ setRadioControlChecked }
-								options={ [
-									{ label: 'Option A', value: 'a' },
-									{ label: 'Option B (not allowed)', value: 'b' },
-								] }
-							/>
-						}
-						// TODO: Ref is not forwarded.
-						onReportCustomValidity={ ( value ) => {
-							if ( value === 'b' ) {
-								return 'Option B is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<RangeControl
-								// TODO: Use of `required` renders an invalid label in HTML.
-								required
-								label="Range"
-								help="Odd numbers are not allowed."
-								min={ 0 }
-								max={ 20 }
-							/>
-						}
-						onReportCustomValidity={ ( value ) => {
-							if ( value && parseInt( value, 10 ) % 2 !== 0 ) {
-								return 'Choose an even number.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<TextControl __next40pxDefaultSize __nextHasNoMarginBottom label="Text" required />
-						}
-						onReportCustomValidity={ ( value ) => {
-							if ( value.toLowerCase() === 'error' ) {
-								return 'The word "error" is not allowed.';
-							}
-						} }
-					/>
-					<ControlWithError
-						render={
-							<TextareaControl
-								__nextHasNoMarginBottom
-								label="Textarea"
-								required
-								help="The word 'error' will trigger an error."
-							/>
-						}
-						onReportCustomValidity={ ( value ) => {
-							if ( value.toLowerCase() === 'error' ) {
-								return 'The word "error" is not allowed.';
-							}
-						} }
-					/>
+					<Story />
 				</div>
 
 				<Button variant="primary" type="submit" __next40pxDefaultSize>
 					Submit
 				</Button>
 			</form>
+		),
+	],
+};
+export default meta;
+
+export const Default: StoryObj = {
+	render: function Template() {
+		const [ toggleControlChecked, setToggleControlChecked ] = useState( false );
+		const [ checkboxControlChecked, setCheckboxControlChecked ] = useState( false );
+		const [ radioControlChecked, setRadioControlChecked ] = useState< string | undefined >();
+
+		return (
+			<>
+				<ControlWithError
+					render={
+						<InputControl
+							__next40pxDefaultSize
+							required
+							label="Input"
+							help="The word 'error' will trigger an error."
+						/>
+					}
+					onReportCustomValidity={ ( value ) => {
+						if ( value.toLowerCase() === 'error' ) {
+							return 'The word "error" is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<NumberControl
+							__next40pxDefaultSize
+							label="Number"
+							help="Odd numbers are not allowed."
+						/>
+					}
+					onReportCustomValidity={ ( value ) => {
+						if ( value && parseInt( value, 10 ) % 2 !== 0 ) {
+							return 'Choose an even number.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						// TODO: Rest props are not passed down.
+						<ToggleControl
+							required
+							__nextHasNoMarginBottom
+							label="Toggle"
+							checked={ toggleControlChecked }
+							onChange={ setToggleControlChecked }
+						/>
+					}
+					onReportCustomValidity={ ( value ) => {
+						if ( value ) {
+							return 'Checkbox is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<CheckboxControl
+							__nextHasNoMarginBottom
+							required
+							label="Checkbox"
+							checked={ checkboxControlChecked }
+							onChange={ setCheckboxControlChecked }
+						/>
+					}
+					// TODO: Ref is not forwarded.
+					onReportCustomValidity={ ( value ) => {
+						if ( value ) {
+							return 'Checkbox is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<SelectControl
+							required
+							__nextHasNoMarginBottom
+							__next40pxDefaultSize
+							label="Select"
+							help="Option 1 is not allowed."
+							options={ [
+								{ value: '', label: 'Select an option' },
+								{ value: '1', label: 'Option 1 (not allowed)' },
+								{ value: '2', label: 'Option 2' },
+							] }
+						/>
+					}
+					onReportCustomValidity={ ( value ) => {
+						if ( value === '1' ) {
+							return 'Option 1 is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<CustomSelectControl
+							// TODO: Required isn't passed down correctly.
+							required
+							__next40pxDefaultSize
+							label="Custom Select"
+							options={ [
+								{ key: '', name: 'Select an option' },
+								{ key: 'a', name: 'Option A (not allowed)' },
+								{ key: 'b', name: 'Option B' },
+							] }
+						/>
+					}
+					// TODO: Ref is not forwarded.
+					onReportCustomValidity={ ( value ) => {
+						if ( value === 'a' ) {
+							return 'Option A is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<ComboboxControl
+							__nextHasNoMarginBottom
+							__next40pxDefaultSize
+							// TODO: Rest props are not passed down.
+							required
+							label="Combobox"
+							help="Option A is not allowed."
+							options={ [
+								{ value: 'a', label: 'Option A (not allowed)' },
+								{ value: 'b', label: 'Option B' },
+							] }
+						/>
+					}
+					// TODO: onBlur is not passed down.
+					onReportCustomValidity={ ( value ) => {
+						if ( value === 'a' ) {
+							return 'Option A is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<RadioControl
+							label="Radio"
+							required
+							help="Option A is not allowed"
+							selected={ radioControlChecked }
+							onChange={ setRadioControlChecked }
+							options={ [
+								{ label: 'Option A', value: 'a' },
+								{ label: 'Option B (not allowed)', value: 'b' },
+							] }
+						/>
+					}
+					// TODO: Ref is not forwarded.
+					onReportCustomValidity={ ( value ) => {
+						if ( value === 'b' ) {
+							return 'Option B is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<RangeControl
+							// TODO: Use of `required` renders an invalid label in HTML.
+							required
+							label="Range"
+							help="Odd numbers are not allowed."
+							min={ 0 }
+							max={ 20 }
+						/>
+					}
+					onReportCustomValidity={ ( value ) => {
+						if ( value && parseInt( value, 10 ) % 2 !== 0 ) {
+							return 'Choose an even number.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<TextControl __next40pxDefaultSize __nextHasNoMarginBottom label="Text" required />
+					}
+					onReportCustomValidity={ ( value ) => {
+						if ( value.toLowerCase() === 'error' ) {
+							return 'The word "error" is not allowed.';
+						}
+					} }
+				/>
+				<ControlWithError
+					render={
+						<TextareaControl
+							__nextHasNoMarginBottom
+							label="Textarea"
+							required
+							help="The word 'error' will trigger an error."
+						/>
+					}
+					onReportCustomValidity={ ( value ) => {
+						if ( value.toLowerCase() === 'error' ) {
+							return 'The word "error" is not allowed.';
+						}
+					} }
+				/>
+			</>
 		);
 	},
 };
@@ -283,6 +269,7 @@ export const Password: StoryObj = {
 						label="Password"
 						help="Minimum 8 characters, include a number, capital letter, and symbol (!@£$%^&*#)."
 						minLength={ 8 }
+						required
 					/>
 				}
 				onReportCustomValidity={ ( value ) => {
@@ -299,4 +286,31 @@ export const Password: StoryObj = {
 			/>
 		);
 	},
+};
+
+export const ToggleGroup: StoryObj = {
+	render: () => (
+		<ControlWithError
+			render={
+				// TODO: Use of `required` renders an invalid label in HTML.
+				<ToggleGroupControl
+					__nextHasNoMarginBottom
+					label="Toggle Group"
+					isBlock
+					__next40pxDefaultSize
+					required
+					value="1"
+				>
+					<ToggleGroupControlOption value="1" label="Option 1" />
+					<ToggleGroupControlOption value="2" label="Option 2" />
+				</ToggleGroupControl>
+			}
+			// TODO: Needs custom handling
+			onReportCustomValidity={ ( value ) => {
+				if ( value === '1' ) {
+					return 'Option 1 is not allowed.';
+				}
+			} }
+		/>
+	),
 };
