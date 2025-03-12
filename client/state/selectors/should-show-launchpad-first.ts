9c9b22be3d579a52d743b2270e62e0761c897dec
Dale du Preez <dale@automattic.com>
2025-03-12 09:55:52 +0200

    Expand condition to include any falsy value

--- client/state/selectors/should-show-launchpad-first.ts
+++ client/state/selectors/should-show-launchpad-first.ts
@@ -11,7 +11,8 @@ export const shouldShowLaunchpadFirst = ( site: SiteDetails ): boolean => {
 	const wasSiteCreatedOnboardingFlow = site.options?.site_creation_flow === 'onboarding';
 	const isBigSkyIntent = site?.options?.site_intent === SiteIntent.AIAssembler;
 	// If we don't have a site intent, fall through to the next option.
-	const siteHasNoIntent = site && site.options && site.options.site_intent === '';
+	const siteHasNoIntent =
+		site && site.options && ( site.options.site_intent === '' || ! site.options.site_intent );
 
 	if ( isBigSkyIntent || ! wasSiteCreatedOnboardingFlow || siteHasNoIntent ) {
 		return false;
