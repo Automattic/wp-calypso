60514f0056eb239c8aa263c2a116c67292baa53d
escapemanuele <escapemanuele@gmail.com>
2025-03-12 16:37:19 +0100

    Check for chat status on checkout

--- packages/help-center/src/hooks/use-products-with-premium-support.ts
+++ packages/help-center/src/hooks/use-products-with-premium-support.ts
@@ -6,9 +6,10 @@ import {
 } from '@automattic/onboarding';
 import { ResponseCartProduct } from '@automattic/shopping-cart';
 import { __ } from '@wordpress/i18n';
+/* eslint-disable no-restricted-imports */
+import { useGeoLocationQuery } from 'calypso/data/geo/use-geolocation-query';
 import { FLOWS_ZENDESK_INITIAL_MESSAGES, FLOWS_ZENDESK_FLOWNAME } from '../constants';
 import { useSupportStatus } from '../data/use-support-status';
-
 const getUserFieldMessage = ( flowName: string, url?: string ) => {
 	return `${
 		FLOWS_ZENDESK_INITIAL_MESSAGES[ flowName as keyof typeof FLOWS_ZENDESK_INITIAL_MESSAGES ]
@@ -17,6 +18,7 @@ const getUserFieldMessage = ( flowName: string, url?: string ) => {
 
 export function useProductsWithPremiumSupport( products: ResponseCartProduct[], url?: string ) {
 	const { data: supportStatus } = useSupportStatus();
+	const { data: geoData } = useGeoLocationQuery();
 
 	for ( const product of products ) {
 		if ( isDIFMProduct( product ) ) {
@@ -24,7 +26,8 @@ export function useProductsWithPremiumSupport( products: ResponseCartProduct[], 
 				userFieldMessage: getUserFieldMessage( DIFM_FLOW, url ),
 				userFieldFlowName:
 					FLOWS_ZENDESK_FLOWNAME[ DIFM_FLOW as keyof typeof FLOWS_ZENDESK_FLOWNAME ],
-				hasPremiumSupport: supportStatus?.availability.is_difm_chat_open,
+				hasPremiumSupport:
+					supportStatus?.availability.is_difm_chat_open && geoData?.country_short === 'US',
 				helpCenterButtonCopy: __( 'Questions?', __i18n_text_domain__ ),
 				helpCenterButtonLink: __( 'Contact our site-building team', __i18n_text_domain__ ),
 			};
