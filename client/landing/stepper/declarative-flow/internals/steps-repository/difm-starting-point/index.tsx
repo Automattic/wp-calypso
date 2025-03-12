94ace0cd060c8a96d603191b77ecacff7eae10ff
Omar Alshaker <omar@omaralshaker.com>
2025-03-12 19:24:09 +0100

    Fix `DIFM` starting point

--- client/landing/stepper/declarative-flow/internals/steps-repository/difm-starting-point/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/difm-starting-point/index.tsx
@@ -12,9 +12,9 @@ import type { Step } from '../../types';
 import type { AppState } from 'calypso/types';
 
 const STEP_NAME = 'difmStartingPoint';
-const DIFMStartingPoint: Step< { submits: { newOrExistingSiteChoice: string } } > = function ( {
-	navigation,
-} ) {
+const DIFMStartingPoint: Step< {
+	submits: { newOrExistingSiteChoice: 'existing-site' | 'new-site' };
+} > = function ( { flow, navigation } ) {
 	const { goNext, goBack, submit } = navigation;
 	const translate = useTranslate();
 	const existingSiteCount = useSelector( ( state: AppState ) => getCurrentUserSiteCount( state ) );
@@ -27,7 +27,7 @@ const DIFMStartingPoint: Step< { submits: { newOrExistingSiteChoice: string } } 
 
 	const shouldRenderHelpCenterLink = useShouldRenderHelpCenterButton( { enabledGeos: [ 'US' ] } );
 
-	const onSubmit = ( value: string ) => {
+	const onSubmit = ( value: 'existing-site' | 'new-site' ) => {
 		submit?.( {
 			newOrExistingSiteChoice: value,
 		} );
