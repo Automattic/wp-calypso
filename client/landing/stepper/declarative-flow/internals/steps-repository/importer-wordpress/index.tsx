0541ffb98b6967d232ed6fa13034b049fd9a7b05
Dale du Preez <dale@automattic.com>
2025-03-12 15:44:07 +0200

    Remove migration upsell modal from importerWordpress step

--- client/landing/stepper/declarative-flow/internals/steps-repository/importer-wordpress/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer-wordpress/index.tsx
@@ -1,9 +1,4 @@
 import WordpressImporter from 'calypso/blocks/importer/wordpress';
-import { WPImportOption } from 'calypso/blocks/importer/wordpress/types';
-import { MigrationAssistanceModal } from 'calypso/landing/stepper/declarative-flow/internals/components/migration-assistance-modal';
-import { useStepNavigator } from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/importer/hooks/use-step-navigator';
-import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
-import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { withImporterWrapper } from '../importer';
 import type { StepProps } from '../../types';
 import type { FC, ReactElement } from 'react';
@@ -16,44 +11,14 @@ interface Props extends StepProps {
 }
 
 const ImporterWordpress: FC< Props > = function ( props ) {
-	const queryParams = useQuery();
-	const site = useSite();
-	const migrateFrom = queryParams.get( 'from' );
-	const showMigrationModal = queryParams.get( 'showModal' );
-	const siteSlug = queryParams.get( 'siteSlug' );
-	const stepNavigator = useStepNavigator(
-		props.flow,
-		props.navigation,
-		site?.ID,
-		siteSlug,
-		migrateFrom
-	);
-
 	let customizedActionButtons;
 
 	return (
-		<>
-			{ showMigrationModal && (
-				<MigrationAssistanceModal
-					onConfirm={ () => {
-						stepNavigator?.goToCheckoutPage?.( WPImportOption.EVERYTHING, {
-							redirect_to: `/setup/${ encodeURIComponent(
-								props.flow
-							) }/migrateMessage?from=${ encodeURIComponent(
-								migrateFrom || ''
-							) }&siteSlug=${ encodeURIComponent( siteSlug || '' ) }`,
-						} );
-					} }
-					migrateFrom={ migrateFrom }
-					navigateBack={ props.navigation.goBack }
-				/>
-			) }
-			<Importer
-				importer="wordpress"
-				{ ...props }
-				customizedActionButtons={ customizedActionButtons }
-			/>
-		</>
+		<Importer
+			importer="wordpress"
+			{ ...props }
+			customizedActionButtons={ customizedActionButtons }
+		/>
 	);
 };
 
