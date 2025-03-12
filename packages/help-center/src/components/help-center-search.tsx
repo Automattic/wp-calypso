a1deb6b741946beabf778f22fd05da8483494064
Renan <renansscarvalho@gmail.com>
2025-03-12 15:18:34 +0100

    Add comment

--- packages/help-center/src/components/help-center-search.tsx
+++ packages/help-center/src/components/help-center-search.tsx
@@ -56,8 +56,10 @@ export const HelpCenterSearch = ( { onSearchChange, currentRoute }: HelpCenterSe
 
 	// Search query can be a query param, if the user searches or clears the search field
 	// we need to keep the query param up-to-date with that
+	// Search term is used to set a search term before opening help center
 	useEffect( () => {
-		if ( query || searchTerm ) {
+		const hasSearchQuery = query || searchTerm;
+		if ( hasSearchQuery ) {
 			navigate( '/?query=' + searchQuery );
 		}
 	}, [ searchTerm, searchQuery, query, navigate ] );
