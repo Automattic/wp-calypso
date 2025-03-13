7549aaa9f5152e5a8b82b1560ffa3398699da694
Paulo Trentin <paulo@paulotrentin.com.br>
2025-03-13 13:58:58 -0300

    Test added for the new hook

--- client/state/selectors/test/should-show-launchpad-first.ts
+++ client/state/selectors/test/should-show-launchpad-first.ts
@@ -1,9 +1,16 @@
 /**
  * @jest-environment jsdom
  */
+import { useLaunchpad } from '@automattic/data-stores';
+import { useShouldShowLaunchpadFirst } from 'calypso/landing/stepper/hooks/use-should-show-launchpad-first';
 import { loadExperimentAssignment } from 'calypso/lib/explat';
 import { shouldShowLaunchpadFirst } from '../should-show-launchpad-first';
 import type { SiteDetails } from '@automattic/data-stores';
+
+jest.mock( '@automattic/data-stores', () => ( {
+	...jest.requireActual( '@automattic/data-stores' ),
+	useLaunchpad: jest.fn(),
+} ) );
 
 jest.mock( 'calypso/lib/explat', () => ( {
 	loadExperimentAssignment: jest.fn(),
@@ -78,5 +85,52 @@ describe( 'shouldShowLaunchpadFirst', () => {
 		} as SiteDetails;
 
 		expect( await shouldShowLaunchpadFirst( site ) ).toBe( false );
+	} );
+} );
+
+describe( 'useShouldShowLaunchpadFirst', () => {
+	const validSite = {
+		options: {
+			site_creation_flow: 'onboarding',
+			site_intent: 'sell',
+		},
+	} as SiteDetails;
+
+	const invalidSite = {
+		options: {
+			site_creation_flow: 'other',
+		},
+	} as SiteDetails;
+
+	it( 'should return true when shouldShowLaunchpadFirst returns true and launchpad is loading', () => {
+		( useLaunchpad as jest.Mock ).mockReturnValue( {
+			data: { checklist: null },
+		} );
+
+		expect( useShouldShowLaunchpadFirst( validSite ) ).toBe( true );
+	} );
+
+	it( 'should return true when shouldShowLaunchpadFirst returns true and launchpad has items', () => {
+		( useLaunchpad as jest.Mock ).mockReturnValue( {
+			data: { checklist: [ 'item1', 'item2' ] },
+		} );
+
+		expect( useShouldShowLaunchpadFirst( validSite ) ).toBe( true );
+	} );
+
+	it( 'should return false when shouldShowLaunchpadFirst returns true but launchpad is empty', () => {
+		( useLaunchpad as jest.Mock ).mockReturnValue( {
+			data: { checklist: [] },
+		} );
+
+		expect( useShouldShowLaunchpadFirst( validSite ) ).toBe( false );
+	} );
+
+	it( 'should return false when shouldShowLaunchpadFirst returns false regardless of launchpad state', () => {
+		( useLaunchpad as jest.Mock ).mockReturnValue( {
+			data: { checklist: [ 'item1' ] },
+		} );
+
+		expect( useShouldShowLaunchpadFirst( invalidSite ) ).toBe( false );
 	} );
 } );
