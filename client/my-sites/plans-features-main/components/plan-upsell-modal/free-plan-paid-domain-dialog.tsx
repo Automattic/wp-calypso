87dee351ab7af2c68b3e02ff418e6003a1200d37
Marco Ciampini <marco.ciampo@gmail.com>
2025-03-13 18:46:23 +0100

    More copy changes to other modals

--- client/my-sites/plans-features-main/components/plan-upsell-modal/free-plan-paid-domain-dialog.tsx
+++ client/my-sites/plans-features-main/components/plan-upsell-modal/free-plan-paid-domain-dialog.tsx
@@ -39,7 +39,7 @@ export function FreePlanPaidDomainDialog( {
 	return (
 		<DialogContainer>
 			<Heading id="plan-upsell-modal-title" shrinkMobileFont>
-				{ translate( 'A paid plan is required for a custom primary domain.' ) }
+				{ translate( 'Paid plan required' ) }
 			</Heading>
 			<SubHeading id="plan-upsell-modal-description">
 				{ translate(
