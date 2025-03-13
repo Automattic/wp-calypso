293f90087ceac311ca9b98f6ca1373de1f1170ca
Allison Levine <allison.levine@automattic.com>
2025-03-13 14:07:29 -0400

    Add more translation context and a success notice.

--- client/my-sites/subscribers/hooks/use-unsubscribe-modal.ts
+++ client/my-sites/subscribers/hooks/use-unsubscribe-modal.ts
@@ -1,8 +1,9 @@
-import { useTranslate } from 'i18n-calypso';
+import { useTranslate, numberFormat } from 'i18n-calypso';
 import { useEffect, useState } from 'react';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { navigate } from 'calypso/lib/navigate';
-import { useSelector } from 'calypso/state';
+import { useDispatch, useSelector } from 'calypso/state';
+import { successNotice } from 'calypso/state/notices/actions';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import { UnsubscribeActionType } from '../components/unsubscribe-modal';
 import { useSubscriberRemoveMutation } from '../mutations';
@@ -20,6 +21,7 @@ const useUnsubscribeModal = (
 	const recordRemoveModal = useRecordRemoveModal();
 	const { mutate } = useSubscriberRemoveMutation( siteId, subscriberQueryParams, detailsView );
 	const translate = useTranslate();
+	const dispatch = useDispatch();
 
 	const onSetUnsubscribers = ( subscribers: Subscriber[] ) => {
 		setCurrentSubscribers( subscribers );
@@ -43,6 +45,25 @@ const useUnsubscribeModal = (
 		) {
 			mutate( subscribers, {
 				onSuccess: () => {
+					// Show success notice.
+					dispatch(
+						successNotice(
+							translate(
+								'%s has been removed from your subscribers list.',
+								'%d subscribers have been removed from your list.',
+								{
+									count: subscribers.length,
+									args:
+										subscribers.length === 1
+											? [ subscribers[ 0 ].display_name ]
+											: [ numberFormat( subscribers.length ) ],
+									comment:
+										'First %s is subscriber name, second %d is the number of subscribers removed',
+								}
+							),
+							{ duration: 5000 }
+						)
+					);
 					resetSubscribers();
 					onSuccess?.();
 				},
