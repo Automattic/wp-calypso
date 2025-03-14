06c0000714e3344ff57ac2bae6478d3fecc84590
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-03-14 17:09:06 +0700

    Untangled Plans: show current plan info

--- client/my-sites/plans/main.jsx
+++ client/my-sites/plans/main.jsx
@@ -39,6 +39,7 @@ import PlansNavigation from 'calypso/my-sites/plans/navigation';
 import P2PlansMain from 'calypso/my-sites/plans/p2-plans-main';
 import PlansFeaturesMain from 'calypso/my-sites/plans-features-main';
 import { FeatureBreadcrumb } from 'calypso/sites/hooks/breadcrumbs/use-set-feature-breadcrumb';
+import CurrentPlan from 'calypso/sites/plan/components/current-plan';
 import { useSelector } from 'calypso/state';
 import { getByPurchaseId } from 'calypso/state/purchases/selectors';
 import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
@@ -234,7 +235,6 @@ class PlansComponent extends Component {
 	renderPlaceholder = () => {
 		return (
 			<div>
-				<DocumentHead title={ this.props.translate( 'Plans', { textOnly: true } ) } />
 				<Main wideLayout>
 					<div id="plans" className="plans plans__has-sidebar" />
 				</Main>
@@ -243,7 +243,7 @@ class PlansComponent extends Component {
 	};
 
 	renderPlansMain() {
-		const { selectedSite, isWPForTeamsSite, currentPlanIntervalType } = this.props;
+		const { selectedSite, isUntangled, isWPForTeamsSite, currentPlanIntervalType } = this.props;
 
 		if ( isEnabled( 'p2/p2-plus' ) && isWPForTeamsSite ) {
 			return (
@@ -281,6 +281,7 @@ class PlansComponent extends Component {
 				discountEndDate={ this.props.discountEndDate }
 				siteId={ selectedSite?.ID }
 				plansWithScroll={ false }
+				hideSpotlightPlan={ isUntangled }
 				hidePlansFeatureComparison={ this.props.isDomainAndPlanPackageFlow }
 				showLegacyStorageFeature={ this.props.siteHasLegacyStorage }
 				intent={ plansIntent }
@@ -362,6 +363,19 @@ class PlansComponent extends Component {
 	}
 
 	render() {
+		const { isUntangled, selectedSite, translate } = this.props;
+		return (
+			<>
+				<DocumentHead title={ translate( 'Plans', { textOnly: true } ) } />
+				{ isUntangled && (
+					<FeatureBreadcrumb siteId={ selectedSite.ID } title={ translate( 'Plan' ) } />
+				) }
+				{ this.renderContent() }
+			</>
+		);
+	}
+
+	renderContent() {
 		const {
 			selectedSite,
 			translate,
@@ -439,7 +453,6 @@ class PlansComponent extends Component {
 			<div>
 				{ ! isUntangled && ! isJetpackNotAtomic && <ModernizedLayout /> }
 				{ selectedSite.ID && <QuerySitePurchases siteId={ selectedSite.ID } /> }
-				<DocumentHead title={ translate( 'Plans', { textOnly: true } ) } />
 				<PageViewTracker path="/plans/:site" title="Plans" />
 				{ /** QueryProducts added to ensure currency-code state gets populated for usages of getCurrentUserCurrencyCode */ }
 				<QueryProducts />
@@ -455,9 +468,7 @@ class PlansComponent extends Component {
 				) }
 				{ canAccessPlans && (
 					<div>
-						{ isUntangled && (
-							<FeatureBreadcrumb siteId={ selectedSite.ID } title={ translate( 'Plan' ) } />
-						) }
+						{ isUntangled && <CurrentPlan /> }
 						{ ! isUntangled && ! isDomainAndPlanPackageFlow && (
 							<PlansHeader
 								domainFromHomeUpsellFlow={ domainFromHomeUpsellFlow }
