5ed5fe56ec1ac153663e1313477e104a271311d7
Jess Boctor <jessboctor@automattic.com>
2025-03-17 10:57:43 -0500

    Wrap PurchaseItem component to gather all props

--- client/me/purchases/purchases-list-in-dataviews/purchases-data-field.tsx
+++ client/me/purchases/purchases-list-in-dataviews/purchases-data-field.tsx
@@ -6,6 +6,32 @@ import { useSelector } from 'calypso/state';
 import { getSite } from 'calypso/state/sites/selectors';
 import PurchaseItem from '../purchase-item';
 
+function PurchaseItemRow( props: { purchase: Purchases.Purchase } ) {
+	const purchase = props.purchase;
+	const site = useSelector( ( state ) => getSite( state, purchase.siteId ?? 0 ) );
+
+	const paymentMethodsState = useStoredPaymentMethods( { type: 'card', expired: true } );
+	const cards = paymentMethodsState.paymentMethods;
+
+	const isBackupMethodAvailable = cards.some(
+		( card ) => card.stored_details_id !== purchase.payment.storedDetailsId && card.is_backup
+	);
+
+	return (
+		<PurchaseItem
+			key={ purchase.id }
+			slug={ purchase.siteName }
+			isDisconnectedSite={ ! site }
+			purchase={ purchase }
+			isJetpack={ isJetpackPlan( purchase ) || isJetpackProduct( purchase ) }
+			site={ site }
+			showSite /* Renders a button and few subscriptions */
+			name={ purchase.siteName }
+			isBackupMethodAvailable={ isBackupMethodAvailable }
+		/>
+	);
+}
+
 export const purchasesDataFields = [
 	{
 		id: 'site',
@@ -21,8 +47,8 @@ export const purchasesDataFields = [
 		getValue: ( { item }: { item: Purchase } ) => {
 			return item.siteId;
 		},
-		render: ( { item }: { item: Purchase } ) => {
-			return <div>{ item.siteId }</div>;
+		render: ( { item }: { item: Purchases.Purchase } ) => {
+			return <PurchaseItemRow purchase={ item } />;
 		},
 	},
 	// {
