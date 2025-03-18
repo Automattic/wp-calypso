4dbe4e05a3ad88e74e7fd9234267cd1dc375d0dd
Aneesh Devasthale <aneesh.devasthale@automattic.com>
2025-03-18 12:41:00 -0700

    update experiment slug

--- client/my-sites/plans-features-main/hooks/use-eligibility-for-term-savings-price-display.ts
+++ client/my-sites/plans-features-main/hooks/use-eligibility-for-term-savings-price-display.ts
@@ -104,7 +104,7 @@ const useEligibilityForTermSavingsPriceDisplay = ( {
 	} );
 
 	const [ isLoading, experimentAssignment ] = useExperiment(
-		'calypso_plans_page_emphasize_longer_term_savings_v2',
+		'wpcom_plans_page_emphasize_longer_term_savings',
 		{
 			isEligible: ! isAnyGridPlanDiscounted,
 		}
