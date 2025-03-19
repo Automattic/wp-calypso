ad9ad9e2f6519ac27975fdac20869e718f87ba0a
Dognose <dognose24@gmail.com>
2025-03-19 12:22:48 +0800

    Make the upsell modal view available on all Stats pages

--- client/my-sites/stats/components/stats-main/index.tsx
+++ client/my-sites/stats/components/stats-main/index.tsx
@@ -1,20 +1,26 @@
 import clsx from 'clsx';
 import Main, { MainProps } from 'calypso/components/main';
 import useWPAdminTheme from 'calypso/my-sites/stats/hooks/use-wp-admin-theme';
+import StatsUpsellModal from 'calypso/my-sites/stats/stats-upsell-modal';
 import { useSelector } from 'calypso/state';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
+import { getUpsellModalView } from 'calypso/state/stats/paid-stats-upsell/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 export default function StatsMain( { children, ...props }: MainProps ) {
-	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) ) as number;
 	const isSiteJetpack = useSelector( ( state ) =>
 		isJetpackSite( state, siteId, { treatAtomicAsJetpackSite: true } )
 	);
 	const customTheme = useWPAdminTheme( isSiteJetpack );
 
+	// Make the upsell modal view available on all Stats pages.
+	const upsellModalView = useSelector( ( state ) => getUpsellModalView( state, siteId ) );
+
 	return (
 		<Main { ...props } className={ clsx( 'stats-main', 'color-scheme', customTheme ) }>
 			{ children }
+			{ upsellModalView && <StatsUpsellModal siteId={ siteId } /> }
 		</Main>
 	);
 }
