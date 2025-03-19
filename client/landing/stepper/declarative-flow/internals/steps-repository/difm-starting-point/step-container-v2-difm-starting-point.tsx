4135516c0d35a7b34a37c18a37fc6d0d53be4e76
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-03-18 23:41:33 -0300

    Use StepContainerV2 in DIFM starting point

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/difm-starting-point/step-container-v2-difm-starting-point.tsx
@@ -0,0 +1,74 @@
+import { Step } from '@automattic/onboarding';
+import { useViewportMatch } from '@wordpress/compose';
+import { useTranslate } from 'i18n-calypso';
+import { ReactNode } from 'react';
+import AsyncLoad from 'calypso/components/async-load';
+import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
+import { DIFMFAQ } from 'calypso/my-sites/marketing/do-it-for-me/faq';
+import { DIFMServiceDescription } from 'calypso/my-sites/marketing/do-it-for-me/service-description';
+import { useDIFMHeading } from 'calypso/my-sites/marketing/do-it-for-me/use-difm-heading';
+
+import './style.scss';
+
+export const StepContainerV2DIFMStartingPoint = ( {
+	topBar,
+	siteId,
+	stickyBottomBar,
+	primaryButton,
+	secondaryButton,
+}: {
+	topBar: ReactNode;
+	stickyBottomBar: ReactNode;
+	primaryButton: ReactNode;
+	siteId?: number;
+	secondaryButton?: ReactNode;
+} ) => {
+	const translate = useTranslate();
+
+	const { headerText, subHeaderText } = useDIFMHeading( {
+		isStoreFlow: false,
+		siteId,
+	} );
+
+	const isMediumViewport = useViewportMatch( 'small', '>=' );
+	const isExtraLargeViewport = useViewportMatch( 'large', '>=' );
+
+	const ctas = isMediumViewport && (
+		<>
+			{ primaryButton }
+			{ secondaryButton && <span>{ translate( 'or' ) }</span> }
+			{ secondaryButton }
+		</>
+	);
+
+	return (
+		<Step.TwoColumnLayout
+			className="step-container-v2--difm-starting-point"
+			firstColumnWidth={ 1 }
+			secondColumnWidth={ 1 }
+			topBar={ topBar }
+			stickyBottomBar={ stickyBottomBar }
+			isMediumViewport={ isMediumViewport }
+			footer={ <DIFMFAQ isStoreFlow={ false } siteId={ siteId } ctaSection={ ctas } /> }
+		>
+			{ () => (
+				<>
+					<div className="step-container-v2--difm-starting-point__left-column">
+						<Step.Heading text={ headerText } subText={ subHeaderText } align="left" />
+						<DIFMServiceDescription isStoreFlow={ false } />
+						{ ctas && <div className="step-container-v2--difm-starting-point__ctas">{ ctas }</div> }
+					</div>
+					{ isExtraLargeViewport && (
+						<div style={ { height: '562px' } }>
+							<AsyncLoad
+								require="calypso/my-sites/marketing/do-it-for-me/site-build-showcase"
+								placeholder={ <LoadingEllipsis /> }
+								isStoreFlow={ false }
+							/>
+						</div>
+					) }
+				</>
+			) }
+		</Step.TwoColumnLayout>
+	);
+};
