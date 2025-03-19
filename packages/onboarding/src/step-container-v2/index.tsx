051ec60472f78035da4a85b152606b1dcaf9c849
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-03-19 12:47:16 -0300

    Add WideLayout wireframe

--- packages/onboarding/src/step-container-v2/index.tsx
+++ packages/onboarding/src/step-container-v2/index.tsx
@@ -10,3 +10,4 @@ export { StickyBottomBar } from './components/StickyBottomBar/StickyBottomBar';
 export { FullWidthLayout } from './wireframes/FullWidthLayout/FullWidthLayout';
 export { CenteredColumnLayout } from './wireframes/CenteredColumnLayout/CenteredColumnLayout';
 export { TwoColumnLayout } from './wireframes/TwoColumnLayout/TwoColumnLayout';
+export { WideLayout } from './wireframes/WideLayout/WideLayout';
