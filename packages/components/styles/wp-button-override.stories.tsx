1798488eb586f98c46467874ca80982a45e6f2a2
Lena Morita <lena@jaguchi.com>
2025-03-20 07:50:35 +0900

    Fix up

--- packages/components/styles/wp-button-override.stories.tsx
+++ packages/components/styles/wp-button-override.stories.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@wordpress/components';
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
-import './wp-button-override.scss';
+// Not using a relative path here to ensure that the build is working.
+import '@automattic/components/styles/wp-button-override.scss';
 
 // TODO: Investigate why displayName becomes `UnforwardedButton` when imported.
 Button.displayName = 'Button';
