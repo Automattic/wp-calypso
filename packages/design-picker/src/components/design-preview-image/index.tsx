62cc25c2814da1432e87d747d43edd0e62e92904
Griffith Chen <griffith.chen@automattic.com>
2025-03-21 16:36:37 +0800

    Revert changes

--- packages/design-picker/src/components/design-preview-image/index.tsx
+++ packages/design-picker/src/components/design-preview-image/index.tsx
@@ -2,7 +2,7 @@ import { MShotsImage } from '@automattic/onboarding';
 import { useViewportMatch } from '@wordpress/compose';
 import photon from 'photon';
 import { getDesignPreviewUrl, getMShotOptions } from '../../utils';
-import type { Design } from '../../types';
+import type { Design, StyleVariation } from '../../types';
 import type { FC } from 'react';
 
 const makeOptionId = ( { slug }: Design ): string => `design-picker__option-name__${ slug }`;
@@ -11,12 +11,14 @@ interface DesignPreviewImageProps {
 	design: Design;
 	imageOptimizationExperiment?: boolean;
 	locale?: string;
+	styleVariation?: StyleVariation;
 	oldHighResImageLoading?: boolean; // Temporary for A/B test.
 }
 
 const DesignPreviewImage: FC< DesignPreviewImageProps > = ( {
 	design,
 	locale,
+	styleVariation,
 	oldHighResImageLoading,
 } ) => {
 	const isMobile = useViewportMatch( 'small', '<' );
@@ -52,6 +54,7 @@ const DesignPreviewImage: FC< DesignPreviewImageProps > = ( {
 		<MShotsImage
 			url={ getDesignPreviewUrl( design, {
 				use_screenshot_overrides: true,
+				style_variation: styleVariation,
 				...( locale && { language: locale } ),
 			} ) }
 			aria-labelledby={ makeOptionId( design ) }
