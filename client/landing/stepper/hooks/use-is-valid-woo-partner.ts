65662d8f6b79618b7ff7dadf173973079ee83db8
Roberto Aranda <roberto.aranda@automattic.com>
2025-03-21 17:14:35 +0100

    PTO: Wait for WooCommerce to be active before redirecting

--- client/landing/stepper/hooks/use-is-valid-woo-partner.ts
+++ client/landing/stepper/hooks/use-is-valid-woo-partner.ts
@@ -1,12 +1,17 @@
+import { OnboardSelect } from '@automattic/data-stores';
+import { useSelect } from '@wordpress/data';
 import { useMemo } from 'react';
-import { useQuery } from './use-query';
+import { ONBOARD_STORE } from '../stores';
 
 const VALID_WOO_PARTNERS = [ 'square', 'paypal', 'stripe' ];
 
 export const useIsValidWooPartner = () => {
-	const query = useQuery();
-	const queryParams = Object.fromEntries( query );
-	const partnerBundle = queryParams.partnerBundle;
+	const { partnerBundle } = useSelect(
+		( select: ( arg: string ) => OnboardSelect ) => ( {
+			partnerBundle: select( ONBOARD_STORE ).getPartnerBundle(),
+		} ),
+		[]
+	);
 	return useMemo( () => {
 		if ( partnerBundle && VALID_WOO_PARTNERS.includes( partnerBundle ) ) {
 			return true;
