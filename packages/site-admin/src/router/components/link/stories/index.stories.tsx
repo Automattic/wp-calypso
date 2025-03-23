3ac337932f02a71edfe67072f7776ceff2248354
Damián Suárez <rdsuarez@gmail.com>
2025-03-23 10:41:37 +0000

    wrap the story with a RouterProvider instance It's required to make the navigation system works.

--- packages/site-admin/src/router/components/link/stories/index.stories.tsx
+++ packages/site-admin/src/router/components/link/stories/index.stories.tsx
@@ -1,10 +1,23 @@
+/**
+ * Internal dependencies
+ */
 import { Link } from '../';
+import { RouterProvider } from '../../../';
 import type { Meta, StoryObj } from '@storybook/react';
 import './style.stories.scss';
 
 const meta: Meta< typeof Link > = {
 	title: 'Components/Link',
 	component: Link,
+	decorators: [
+		function WithRouterProvider( Story ) {
+			return (
+				<RouterProvider routes={ [] }>
+					<Story />
+				</RouterProvider>
+			);
+		},
+	],
 };
 
 export default meta;
@@ -13,7 +26,7 @@ type Story = StoryObj< typeof Link >;
 
 export const Default: Story = {
 	args: {
-		to: '/',
-		children: 'Home page',
+		to: '/home',
+		children: 'Homepage',
 	},
 };
