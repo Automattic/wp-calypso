b7538107e5ed08883c2db2f5ccef6238b3e619bf
vykes-mac <knrymcleish@gmail.com>
2025-03-24 15:14:05 -0500

    create a new site object instead of mutating the exisitng object and pass that to the function

--- client/landing/stepper/declarative-flow/internals/hooks/use-launchpad-decider/index.tsx
+++ client/landing/stepper/declarative-flow/internals/hooks/use-launchpad-decider/index.tsx
@@ -27,12 +27,11 @@ export const useLaunchpadDecider = ( { exitFlow, navigate }: Props ) => {
 	const { getIntent } = useSelect( ( select ) => select( ONBOARD_STORE ) as OnboardSelect, [] );
 	const intent = getIntent();
 	const site = useSite();
-	// The site_intent option is not set until we exit site setup flow so we set the client value with data from the onboarding store
-	// as it is used in shouldShowLaunchpadFirst.
-	if ( site && site.options && site.options.site_intent === '' ) {
-		site.options.site_intent = intent;
-	}
-	const showCustomerHome = site && shouldShowLaunchpadFirst( site );
+
+	const showCustomerHome =
+		site &&
+		shouldShowLaunchpadFirst( { ...site, options: { ...site.options, site_intent: intent } } );
+
 	const sessionId = getSessionId();
 
 	return {
