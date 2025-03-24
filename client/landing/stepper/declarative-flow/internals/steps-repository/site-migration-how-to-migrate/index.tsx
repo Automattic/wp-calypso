2bcd3e0e863f5b0c58d251e57fb2013fca2af95d
Paulo Trentin <paulo@paulotrentin.com.br>
2025-03-24 11:58:23 -0300

    Add Migrate site to the new step container v2

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-how-to-migrate/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-how-to-migrate/index.tsx
@@ -1,5 +1,5 @@
 import { PLAN_BUSINESS, getPlan, isWpComBusinessPlan } from '@automattic/calypso-products';
-import { NextButton, StepContainer } from '@automattic/onboarding';
+import { NextButton, Step, StepContainer } from '@automattic/onboarding';
 import { Icon, copy, globe, lockOutline, scheduled } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import { useCallback, useMemo } from 'react';
@@ -10,11 +10,12 @@ import { HOW_TO_MIGRATE_OPTIONS } from 'calypso/landing/stepper/constants';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { usePresalesChat } from 'calypso/lib/presales-chat';
+import { shouldUseStepContainerV2 } from '../../../helpers/should-use-step-container-v2';
 import { DIYOption } from './diy-option';
-import type { Step } from '../../types';
+import type { Step as StepType } from '../../types';
 import './style.scss';
 
-const SiteMigrationHowToMigrate: Step< {
+const SiteMigrationHowToMigrate: StepType< {
 	accepts: {
 		headerText?: string;
 		subHeaderText?: string;
@@ -24,10 +25,11 @@ const SiteMigrationHowToMigrate: Step< {
 		destination: string;
 	};
 } > = ( props ) => {
-	const { navigation, headerText, stepName, subHeaderText } = props;
+	const { navigation, headerText, stepName, subHeaderText, flow } = props;
 	const translate = useTranslate();
 	const site = useSite();
 	const { mutate: cancelMigration } = useMigrationCancellation( site?.ID );
+	const isUsingStepContainerV2 = shouldUseStepContainerV2( flow );
 
 	usePresalesChat( 'wpcom' );
 
@@ -134,6 +136,32 @@ const SiteMigrationHowToMigrate: Step< {
 			</div>
 		);
 	};
+
+	if ( isUsingStepContainerV2 ) {
+		return (
+			<>
+				<DocumentHead title={ translate( 'Let us migrate your site' ) } />
+				<Step.CenteredColumnLayout
+					className="how-to-migrate-v2"
+					columnWidth={ 6 }
+					topBar={
+						<Step.TopBar
+							backButton={ <Step.BackButton onClick={ goBack } /> }
+							skipButton={ <DIYOption onClick={ handleClick } /> }
+						/>
+					}
+					heading={
+						<Step.Heading
+							text={ headerText ?? translate( 'Let us migrate your site' ) }
+							subText={ subHeaderText || renderSubHeaderText() }
+						/>
+					}
+				>
+					{ renderStepContent() }
+				</Step.CenteredColumnLayout>
+			</>
+		);
+	}
 
 	return (
 		<>
