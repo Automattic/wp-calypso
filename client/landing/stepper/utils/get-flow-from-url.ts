93a0cc5cb0a0a275019ef757e6aedf157fc05002
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-03-25 19:12:17 -0300

    Fix unit tests

--- client/landing/stepper/utils/get-flow-from-url.ts
+++ client/landing/stepper/utils/get-flow-from-url.ts
@@ -1,18 +1,24 @@
-import { matchPath } from 'react-router-dom';
-
 export const getFlowFromURL = (
 	url = window.location.pathname,
 	query = window.location.search
 ) => {
-	const fromPath = matchPath( { path: '/setup/:flow/*' }, url )?.params?.flow;
+	const fullUrl = new URL( url, 'http://wordpress.com' ); // Base URL needed for pathname-only strings
+	const [ stepper, flow ] = fullUrl.pathname.split( '/' ).filter( Boolean );
+	const fromPath = stepper === 'setup' ? flow : undefined;
+
 	// backward support the old Stepper URL structure (?flow=something)
 	const fromQuery = new URLSearchParams( query ).get( 'flow' );
 	// Need to update this to make sure we always get the flow from the URL and its not an empty string
 	return fromPath || fromQuery || '';
 };
 
-export const getStepFromURL = () => {
-	const fromPath = matchPath( { path: '/setup/:flow/:step' }, window.location.pathname )?.params
-		?.step;
-	return fromPath;
+export const getStepFromURL = ( url = window.location.pathname ) => {
+	const fullUrl = new URL( url, 'http://wordpress.com' ); // Base URL needed for pathname-only strings
+	const [ stepper, flow, step ] = fullUrl.pathname.split( '/' ).filter( Boolean );
+
+	if ( stepper !== 'setup' || ! flow || ! step ) {
+		return undefined;
+	}
+
+	return step;
 };
