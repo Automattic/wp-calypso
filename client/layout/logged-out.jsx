85cfd61c5f028b6f51819289502a1723256b5de3
Omar Alshaker <omar@omaralshaker.com>
2025-03-25 15:14:46 +0100

    Fix test

--- client/layout/logged-out.jsx
+++ client/layout/logged-out.jsx
@@ -130,7 +130,7 @@ const LayoutLoggedOut = ( {
 		isLoggedIn &&
 		// we want to show only the Help center in my home and the help section (but not the FAB)
 		( [ 'home', 'help' ].includes( sectionName ) ||
-			shouldLoadInlineHelp( sectionName, currentRoute ) ) &&
+			shouldLoadInlineHelp( sectionName, currentRoute || '' ) ) &&
 		userAllowedToHelpCenter;
 
 	const classes = {
