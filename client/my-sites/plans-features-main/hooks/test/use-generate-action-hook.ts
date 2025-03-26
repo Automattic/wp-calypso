8893bef6c2afdd5db638a14301ba1e31a5e3ecd2
Claudiu Filip <claudiu.filip@automattic.com>
2025-03-26 17:43:33 +0100

    Remove downgrade handle testcase

--- client/my-sites/plans-features-main/hooks/test/use-generate-action-hook.ts
+++ client/my-sites/plans-features-main/hooks/test/use-generate-action-hook.ts
@@ -453,15 +453,4 @@ describe( 'useGenerateActionHook', () => {
 
 		expect( action.primary.text ).toBe( 'Get %(plan)s' );
 	} );
-
-	it( 'should handle downgrade actions', () => {
-		const { result } = renderHook( () =>
-			useGenerateActionHook( { isInSignup: false, isLaunchPage: false } )
-		);
-
-		const action = result.current( { planSlug: PLAN_PERSONAL, availableForPurchase: false } );
-
-		expect( action.primary.text ).toBe( 'Downgrade' );
-		expect( action.primary.variant ).toBe( 'secondary' );
-	} );
 } );
