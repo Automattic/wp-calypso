44e292ba2dce150fc060cf2d3cbcbffa1a6d42ec
Paulo Trentin <paulo@paulotrentin.com.br>
2025-03-26 20:03:08 -0300

    Created initial structure replacing StepContainerV2 on import content

--- client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
@@ -1,18 +1,19 @@
-import { StepContainer } from '@automattic/onboarding';
+import { Step, StepContainer } from '@automattic/onboarding';
 import { useI18n } from '@wordpress/react-i18n';
 import React, { ReactElement, useEffect } from 'react';
 import CaptureStep from 'calypso/blocks/import/capture';
 import DocumentHead from 'calypso/components/data/document-head';
+import { shouldUseStepContainerV2 } from 'calypso/landing/stepper/declarative-flow/helpers/should-use-step-container-v2';
 import useMigrationConfirmation from 'calypso/landing/stepper/hooks/use-migration-confirmation';
 import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
 import { useSiteSlug } from 'calypso/landing/stepper/hooks/use-site-slug';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { generateStepPath } from './helper';
-import type { Step } from '../../types';
+import type { Step as StepType } from '../../types';
 import type { ImporterPlatform } from 'calypso/lib/importer/types';
 import './style.scss';
 
-export const ImportWrapper: Step< {
+export const ImportWrapper: StepType< {
 	submits: {
 		platform: ImporterPlatform;
 		url: string;
@@ -29,6 +30,34 @@ export const ImportWrapper: Step< {
 		// (see useMigrationConfirmation's implementation)
 		[ setMigrationConfirmed ]
 	);
+
+	const isUsingStepContainerV2 = shouldUseStepContainerV2( props.flow );
+
+	if ( isUsingStepContainerV2 ) {
+		return (
+			<>
+				<DocumentHead title={ __( 'Import your site content' ) } />
+				<Step.CenteredColumnLayout
+					className="step-container-v2--import__onboarding-page"
+					columnWidth={ 6 }
+					topBar={
+						<Step.TopBar
+							backButton={ <Step.BackButton onClick={ navigation.goBack } /> }
+							skipButton={
+								<Step.SkipButton
+									onClick={ navigation.goNext }
+									label={ __( 'Skip to dashboard' ) }
+								/>
+							}
+						/>
+					}
+					heading={ <Step.Heading text={ props.title } subText={ props.subTitle } /> }
+				>
+					{ children }
+				</Step.CenteredColumnLayout>
+			</>
+		);
+	}
 
 	return (
 		<>
@@ -51,7 +80,7 @@ export const ImportWrapper: Step< {
 	);
 };
 
-const ImportStep: Step< {
+const ImportStep: StepType< {
 	submits: {
 		platform: ImporterPlatform;
 		url: string;
