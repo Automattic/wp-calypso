18fab05c3f493dd8e49133e22899926b6259700d
Claudiu Filip <claudiu.filip@automattic.com>
2025-03-27 12:27:42 +0100

    Update refund copy

--- client/me/purchases/downgrade/index.tsx
+++ client/me/purchases/downgrade/index.tsx
@@ -227,7 +227,7 @@ export const Downgrade: React.FC< DowngradeProps > = ( props ) => {
 
 		return hasRefund
 			? translate(
-					'We will change the plan immediately and refund the remaining value of %(amount)s from %(currentPlan)s to %(targetPlan)s.',
+					"When you downgrade from %(currentPlan)s to %(targetPlan)s, we'll immediately process a refund of %(amount)s to your original payment method.",
 					{
 						args: {
 							...basePlanArgs,
