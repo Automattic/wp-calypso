dbd609ea85dda6b724826c36cc081fe10606f473
Claudiu Filip <claudiu.filip@automattic.com>
2025-03-27 16:42:17 +0100

    Properly fetch the atomatic transfer data from purchased site

--- client/me/purchases/downgrade/atomic-warning.tsx
+++ client/me/purchases/downgrade/atomic-warning.tsx
@@ -1,11 +1,12 @@
 import { WPCOM_FEATURES_BACKUPS, WPComPlan } from '@automattic/calypso-products';
 import { Button } from '@wordpress/components';
 import { TranslateResult, useTranslate } from 'i18n-calypso';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BlankCanvas } from 'calypso/components/blank-canvas';
 import { AtomicRevertStep } from 'calypso/components/marketing-survey/cancel-purchase-form/step-components/atomic-revert-step';
 import { Purchase } from 'calypso/lib/purchases/types';
-import { useSelector } from 'calypso/state';
+import { fetchAtomicTransfer } from 'calypso/state/atomic-transfer/actions';
 import getAtomicTransfer from 'calypso/state/selectors/get-atomic-transfer';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
 
@@ -36,7 +37,16 @@ export function AtomicWarning( {
 	const hasBackupsFeature = useSelector( ( state ) =>
 		siteHasFeature( state, site.ID, WPCOM_FEATURES_BACKUPS )
 	);
-	const atomicTransfer = useSelector( ( state ) => getAtomicTransfer( state, site.siteId ) );
+	const dispatch = useDispatch();
+
+	useEffect( () => {
+		if ( purchase?.siteId ) {
+			dispatch( fetchAtomicTransfer( purchase.siteId ) );
+		}
+	}, [ dispatch, purchase?.siteId ] );
+
+	const atomicTransfer = useSelector( ( state ) => getAtomicTransfer( state, purchase.siteId ) );
+
 	const translate = useTranslate();
 	return (
 		<div className="atomic-warning__wrapper">
