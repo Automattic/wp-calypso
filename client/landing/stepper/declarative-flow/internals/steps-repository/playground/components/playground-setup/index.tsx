64b3dcbef4abcf4c67754e9d393dffc093be8d98
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-03-27 09:48:50 +0100

    Move to useSiteData

--- client/landing/stepper/declarative-flow/internals/steps-repository/playground/components/playground-setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/playground/components/playground-setup/index.tsx
@@ -1,9 +1,9 @@
 import { useI18n } from '@wordpress/react-i18n';
-import { getQueryArg } from '@wordpress/url';
 import { PlaygroundClient } from '@wp-playground/client';
 import { useRef } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
 import Loading from 'calypso/components/loading';
+import { useSiteData } from 'calypso/landing/stepper/hooks/use-site-data';
 import StepWrapper from 'calypso/signup/step-wrapper';
 import { useIsPlaygroundEligible } from '../../../../../../hooks/use-is-playground-eligible';
 import { importPlaygroundSite } from '../../lib/import-playground';
@@ -25,10 +25,7 @@ export const PlaygroundSetupStep: Step< {
 
 	const { __ } = useI18n();
 	const playgroundClientRef = useRef< PlaygroundClient | null >( null );
-
-	// There's probably a better way to do this.
-	const siteSlug = getQueryArg( window.location.href, 'siteSlug' )?.toString();
-	const siteId = parseInt( getQueryArg( window.location.href, 'siteId' )?.toString() ?? '0' );
+	const { siteId, siteSlug } = useSiteData();
 
 	const startImport = async ( client: PlaygroundClient ) => {
 		if ( ! client ) {
