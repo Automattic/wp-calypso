b8ae3cb8951156e576a4f7318d849ab5ce922e4f
Damián Suárez <rdsuarez@gmail.com>
2025-03-31 14:51:19 +0200

    add support for custom onClick prop

--- packages/site-admin/src/router/components/link/index.tsx
+++ packages/site-admin/src/router/components/link/index.tsx
@@ -1,20 +1,32 @@
 /**
+ * External dependencies
+ */
+import { useCallback } from '@wordpress/element';
+/**
  * Internal dependencies
  */
 import { useLink } from '../../hooks';
 import { type NavigationOptions } from '../../types';
 
-type LinkProps = Omit< React.ComponentPropsWithoutRef< 'a' >, 'href' | 'onClick' > & {
+type LinkProps = Omit< React.ComponentPropsWithoutRef< 'a' >, 'href' > & {
 	to: string;
 	options?: NavigationOptions;
 	children: React.ReactNode;
 };
 
-export function Link( { to, options, children, ...props }: LinkProps ) {
+export function Link( { to, options, children, onClick: onClickProp, ...props }: LinkProps ) {
 	const { href, onClick } = useLink( to, options );
 
+	const handleClickEvent = useCallback(
+		( event: React.MouseEvent< HTMLAnchorElement > ) => {
+			onClickProp?.( event );
+			onClick?.( event );
+		},
+		[ onClick, onClickProp ]
+	);
+
 	return (
-		<a { ...props } href={ href } onClick={ onClick }>
+		<a { ...props } href={ href } onClick={ handleClickEvent }>
 			{ children }
 		</a>
 	);
