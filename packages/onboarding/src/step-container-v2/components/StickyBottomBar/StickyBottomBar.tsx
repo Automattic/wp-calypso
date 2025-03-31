8ce8aa798d29724b026f17f9bd0f0607a72440d3
Philip Jackson <p-jackson@live.com>
2025-03-31 15:26:27 +1300

    Removed children prop from StickyBottomBar
    No one uses this. Let's not provide a children slot if we're not sure
    why anyone would use it.

--- packages/onboarding/src/step-container-v2/components/StickyBottomBar/StickyBottomBar.tsx
+++ packages/onboarding/src/step-container-v2/components/StickyBottomBar/StickyBottomBar.tsx
@@ -6,36 +6,24 @@ interface StickyBottomBarProps {
 	leftElement?: ReactNode;
 	centerElement?: ReactNode;
 	rightElement?: ReactNode;
-	children?: ReactNode;
 }
 
 export const StickyBottomBar = ( {
 	leftElement,
 	centerElement,
 	rightElement,
-	children,
 }: StickyBottomBarProps ) => {
-	const getContent = () => {
-		if ( children ) {
-			return children;
-		}
-
-		return (
-			<>
-				{ leftElement && (
-					<div className="step-container-v2__sticky-bottom-bar-left-element">{ leftElement }</div>
-				) }
-				{ centerElement && (
-					<div className="step-container-v2__sticky-bottom-bar-center-element">
-						{ centerElement }
-					</div>
-				) }
-				{ rightElement && (
-					<div className="step-container-v2__sticky-bottom-bar-right-element">{ rightElement }</div>
-				) }
-			</>
-		);
-	};
-
-	return <div className="step-container-v2__sticky-bottom-bar">{ getContent() }</div>;
+	return (
+		<div className="step-container-v2__sticky-bottom-bar">
+			{ leftElement && (
+				<div className="step-container-v2__sticky-bottom-bar-left-element">{ leftElement }</div>
+			) }
+			{ centerElement && (
+				<div className="step-container-v2__sticky-bottom-bar-center-element">{ centerElement }</div>
+			) }
+			{ rightElement && (
+				<div className="step-container-v2__sticky-bottom-bar-right-element">{ rightElement }</div>
+			) }
+		</div>
+	);
 };
