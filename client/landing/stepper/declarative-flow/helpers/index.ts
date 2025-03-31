f1f461583f35f7811ad8580de78911870ab2e820
Gabriel Caires <gabriel.caires@automattic.com>
2025-03-31 16:22:46 +0100

    Rename param

--- client/landing/stepper/declarative-flow/helpers/index.ts
+++ client/landing/stepper/declarative-flow/helpers/index.ts
@@ -4,11 +4,11 @@ export const shouldRedirectToSiteMigration = (
 	step: string,
 	platform: string,
 	origin?: string | null,
-	ref?: string | null
+	entryPoint?: string | null
 ) => {
 	return (
 		step === STEPS.IMPORT_LIST.slug &&
 		platform === 'wordpress' &&
-		( origin === STEPS.SITE_MIGRATION_IDENTIFY.slug || ref === 'wp-admin-importers-list' )
+		( origin === STEPS.SITE_MIGRATION_IDENTIFY.slug || entryPoint === 'wp-admin-importers-list' )
 	);
 };
