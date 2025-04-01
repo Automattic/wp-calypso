4f0160a7ff57ed233f279d33d693ab16ac5d06b8
Renan <renansscarvalho@gmail.com>
2025-04-01 09:48:18 +0200

    Submit score

--- packages/odie-client/src/components/message/feedback-thumbs.tsx
+++ packages/odie-client/src/components/message/feedback-thumbs.tsx
@@ -1,20 +1,47 @@
 import { Button } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
+import Smooch from 'smooch';
+import { useOdieAssistantContext } from '../../context';
 import { ThumbsDownIcon, ThumbsUpIcon } from './thumbs-icons';
 
 export const FeedbackThumbs = () => {
+	const { chat } = useOdieAssistantContext();
 	const { __ } = useI18n();
+
+	const ratingConversation = async ( score: 'GOOD' | 'BAD' ) => {
+		const comment =
+			score === 'GOOD' ? 'User submitted positive feedback' : 'User submitted negative feedback';
+		try {
+			const payload = {
+				csat_rating: score,
+				comment,
+			};
+
+			await Smooch.sendMessage(
+				{
+					type: 'text',
+					text: score,
+					payload: JSON.stringify( payload ),
+				},
+				chat.conversationId
+			);
+		} catch ( error ) {
+			// console.error( 'Error sending rating:', error );
+		}
+	};
+
 	const text = __( 'Was this helpful?' );
+
 	return (
 		<div className="odie-conversation__feedback">
 			<div className="odie-conversation-feedback__text">
 				<p>{ text }</p>
 			</div>
 			<div className="odie-conversation-feedback__thumbs">
-				<Button rel="noreferrer" target="_blank">
+				<Button onClick={ () => ratingConversation( 'GOOD' ) } rel="noreferrer">
 					<ThumbsUpIcon />
 				</Button>
-				<Button rel="noreferrer" target="_blank">
+				<Button onClick={ () => ratingConversation( 'BAD' ) } rel="noreferrer">
 					<ThumbsDownIcon />
 				</Button>
 			</div>
