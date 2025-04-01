45805140165fa328d8c69bce1fa46af3cd8d13cb
Jess Boctor <jessboctor@automattic.com>
2025-04-01 08:35:10 -0500

    Handle multiple object types for selecting datafields set

--- client/me/purchases/use-field-definitions.ts
+++ client/me/purchases/use-field-definitions.ts
@@ -1,13 +1,46 @@
 import { useTranslate } from 'i18n-calypso';
 import { useMemo } from 'react';
 import { getTransactionFieldDefinitions } from './billing-history/field-definitions';
+import type { Purchases } from '@automattic/data-stores';
 import type { BillingTransaction } from 'calypso/state/billing-transactions/types';
 
-export function useFieldDefinitions( transactions: BillingTransaction[] | null ) {
+function getFieldDefinitionsByType( {
+	itemType,
+	items,
+	translate
+}: {
+	itemType: string | null;
+	items: BillingTransaction[] | Purchases.Purchase[] | null;
+	translate: typeof useTranslate;
+} ) {
+	if ( 'null' !== itemType || ! items ) {
+		return [];
+	}
+
+	if ( 'BillingTransaction' === itemType ) {
+		return getTransactionFieldDefinitions( items, translate );
+	}
+
+	// Eventually, the goal is to add more item types like this
+	/*
+	if ( 'Purchases.Purchase' === itemType ) {
+		return getPurchaseFieldDefinitions( items, translate );
+	}
+	*/
+}
+
+export function useFieldDefinitions( props: {
+	items: BillingTransaction[] | Purchases.Purchase[] | null;
+} ) {
 	const translate = useTranslate();
+	const items = props.items;
+	if ( ! items ) {
+		return [];
+	}
 
 	return useMemo( () => {
-		const fieldDefinitions = getTransactionFieldDefinitions( transactions, translate );
+		const firstItem = items[0] ?? null;
+		fieldDefinitions = getFieldDefinitionsByType( { typeof firstItem, items, translate } );
 		return Object.values( fieldDefinitions );
-	}, [ transactions, translate ] );
+	}, [ items, translate ] );
 }
