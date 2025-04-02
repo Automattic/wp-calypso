bf0e8de69a23b39062bd77c42698c8e695a1ca7f
Jess Boctor <jessboctor@automattic.com>
2025-04-02 06:08:20 -0500

    Render the status fields

--- client/me/purchases/purchases-list-in-dataviews/hooks/use-field-definitions.ts
+++ client/me/purchases/purchases-list-in-dataviews/hooks/use-field-definitions.ts
@@ -1,12 +1,14 @@
 import { useTranslate } from 'i18n-calypso';
 import { useMemo } from 'react';
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import { getPurchasesFieldDefinitions } from '../purchases-data-field';
 
 export function usePurchasesFieldDefinitions() {
 	const translate = useTranslate();
+	const moment = useLocalizedMoment();
 
 	return useMemo( () => {
-		const fieldDefinitions = getPurchasesFieldDefinitions( translate );
+		const fieldDefinitions = getPurchasesFieldDefinitions( { translate, moment } );
 		return fieldDefinitions;
-	}, [ translate ] );
+	}, [ translate, moment ] );
 }
