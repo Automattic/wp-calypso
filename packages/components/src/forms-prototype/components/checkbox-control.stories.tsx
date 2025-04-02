d0342aa03dac12883e848be389e1536f5b20258d
Lena Morita <lena@jaguchi.com>
2025-04-03 06:45:03 +0900

    Extract more stories

--- packages/components/src/forms-prototype/components/checkbox-control.stories.tsx
+++ packages/components/src/forms-prototype/components/checkbox-control.stories.tsx
@@ -1,37 +1,45 @@
+import { fn } from '@storybook/test';
 import { useState } from 'react';
 import { ValidatedCheckboxControl } from './checkbox-control';
 import { formDecorator } from './story-utils';
 import type { StoryObj, Meta } from '@storybook/react';
 
-const meta: Meta = {
+const meta: Meta< typeof ValidatedCheckboxControl > = {
 	title: 'Packages/Components/Validated Form Controls/ValidatedCheckboxControl',
 	component: ValidatedCheckboxControl,
 	decorators: formDecorator,
+	args: { onChange: fn() },
 	argTypes: {
+		checked: { control: false },
 		// TODO: Figure out why this deprecated prop is still showing up here and not in the WP Storybook.
 		heading: { table: { disable: true } },
 	},
 };
 export default meta;
 
-export const Default: StoryObj = {
-	render: function Template( args ) {
+export const Default: StoryObj< typeof ValidatedCheckboxControl > = {
+	render: function Template( { onChange, ...args } ) {
 		const [ checked, setChecked ] = useState( false );
 
 		return (
 			<ValidatedCheckboxControl
-				required
-				label="Checkbox"
-				help="This checkbox may neither be checked nor unchecked."
-				checked={ checked }
-				onChange={ setChecked }
-				onReportCustomValidity={ ( value ) => {
-					if ( value ) {
-						return 'This checkbox may not be checked.';
-					}
-				} }
 				{ ...args }
+				checked={ checked }
+				onChange={ ( value ) => {
+					setChecked( value );
+					onChange?.( value );
+				} }
 			/>
 		);
+	},
+};
+Default.args = {
+	required: true,
+	label: 'Checkbox',
+	help: 'This checkbox may neither be checked nor unchecked.',
+	onReportCustomValidity: ( value ) => {
+		if ( value ) {
+			return 'This checkbox may not be checked.';
+		}
 	},
 };
