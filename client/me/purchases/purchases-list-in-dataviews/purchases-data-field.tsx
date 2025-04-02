03891605447575a3935b5baf88faf5989b967dd5
Jess Boctor <jessboctor@automattic.com>
2025-04-02 06:08:20 -0500

    Render the status fields

--- client/me/purchases/purchases-list-in-dataviews/purchases-data-field.tsx
+++ client/me/purchases/purchases-list-in-dataviews/purchases-data-field.tsx
@@ -1,11 +1,13 @@
 import { isJetpackPlan, isJetpackProduct } from '@automattic/calypso-products';
 import { Purchases } from '@automattic/data-stores';
 import { Fields, Operator } from '@wordpress/dataviews';
+import { LocalizeProps } from 'i18n-calypso';
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import { useStoredPaymentMethods } from 'calypso/my-sites/checkout/src/hooks/use-stored-payment-methods';
 import { useSelector } from 'calypso/state';
 import { getSite } from 'calypso/state/sites/selectors';
 import { managePurchase } from '../paths';
-import PurchaseItem from '../purchase-item';
+import PurchaseItem, { PurchaseItemStatus } from '../purchase-item';
 
 function PurchaseItemRow( props: { purchase: Purchases.Purchase } ) {
 	const purchase = props.purchase;
@@ -34,22 +36,71 @@ function PurchaseItemRow( props: { purchase: Purchases.Purchase } ) {
 	);
 }
 
-export const purchasesDataFields = [
-	{
-		id: 'site',
-		label: 'Site',
-		type: 'text',
-		enableGlobalSearch: true,
-		enableSorting: true,
-		enableHiding: false,
-		filterBy: {
-			operators: [ 'is' as Operator ],
+function PurchaseItemRowStatus( props: {
+	purchase: Purchases.Purchase;
+	translate: LocalizeProps[ 'translate' ];
+	moment: ReturnType< typeof useLocalizedMoment >;
+	isJetpack?: boolean;
+	isDisconnectedSite?: boolean;
+} ) {
+	const { purchase, translate, moment, isJetpack, isDisconnectedSite } = props;
+
+	return (
+		<div className="purchase-item__status purchases-layout__status">
+			<PurchaseItemStatus
+				purchase={ purchase }
+				translate={ translate }
+				moment={ moment }
+				isJetpack={ isJetpack }
+				isDisconnectedSite={ isDisconnectedSite }
+			/>
+		</div>
+	);
+}
+
+export function getPurchasesFieldDefinitions( {
+	translate,
+	moment,
+}: {
+	translate: LocalizeProps[ 'translate' ];
+	moment: ReturnType< typeof useLocalizedMoment >;
+} ): Fields< Purchases.Purchase > {
+	return [
+		{
+			id: 'status',
+			label: 'status',
+			type: 'text',
+			enableGlobalSearch: true,
+			enableSorting: true,
+			enableHiding: false,
+			filterBy: {
+				operators: [ 'is' as Operator ],
+			},
+			getValue: ( { item }: { item: Purchases.Purchase } ) => {
+				return item.expiryStatus;
+			},
+			render: ( { item }: { item: Purchases.Purchase } ) => {
+				return (
+					<PurchaseItemRowStatus purchase={ item } translate={ translate } moment={ moment } />
+				);
+			},
 		},
-		getValue: ( { item }: { item: Purchases.Purchase } ) => {
-			return item.siteId;
+		{
+			id: 'site',
+			label: 'Site',
+			type: 'text',
+			enableGlobalSearch: true,
+			enableSorting: true,
+			enableHiding: false,
+			filterBy: {
+				operators: [ 'is' as Operator ],
+			},
+			getValue: ( { item }: { item: Purchases.Purchase } ) => {
+				return item.siteId;
+			},
+			render: ( { item }: { item: Purchases.Purchase } ) => {
+				return <PurchaseItemRow purchase={ item } />;
+			},
 		},
-		render: ( { item }: { item: Purchases.Purchase } ) => {
-			return <PurchaseItemRow purchase={ item } />;
-		},
-	},
-] as Fields< Purchases.Purchase >;
+	];
+}
