85070595f6a5e222d863ee7d9cc72aadbe5cf639
Jess Boctor <jessboctor@automattic.com>
2025-04-02 05:46:04 -0500

    Export Views as component

--- client/me/purchases/purchases-list-in-dataviews/purchases-data-view.tsx
+++ client/me/purchases/purchases-list-in-dataviews/purchases-data-view.tsx
@@ -1,4 +1,8 @@
-import { View } from '@wordpress/dataviews';
+import { Card } from '@automattic/components';
+import { Purchases } from '@automattic/data-stores';
+import { DataViews, View } from '@wordpress/dataviews';
+import { LocalizeProps } from 'i18n-calypso';
+import { usePurchasesFieldDefinitions } from './hooks/use-field-definitions';
 
 // This DataView configuration will continue to be updated
 // This is the basic version, as we pull more columns out from the PurchaseItem component it will be expanded
@@ -14,3 +18,32 @@ export const purchasesDataView = {
 	fields: [],
 	layout: {},
 } as View;
+
+export function PurchasesDataViews( props: {
+	purchases: Purchases.Purchase[];
+	translate: LocalizeProps[ 'translate' ];
+} ) {
+	const { purchases } = props;
+	const onChangeView = () => {
+		alert( 'You clicked something!!' );
+	};
+
+	const getItemId = ( item: Purchases.Purchase ) => {
+		return item.id.toString();
+	};
+	const purchasesDataFields = usePurchasesFieldDefinitions();
+	return (
+		<Card id="purchases-list" className="section-content" tagName="section">
+			<DataViews
+				data={ purchases }
+				fields={ purchasesDataFields }
+				view={ purchasesDataView }
+				onChangeView={ onChangeView }
+				defaultLayouts={ { table: {} } }
+				actions={ undefined }
+				getItemId={ getItemId }
+				paginationInfo={ { totalItems: 100, totalPages: 10 } }
+			/>
+		</Card>
+	);
+}
