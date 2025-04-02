e760d1dbb5bbb4802bb350fefb1890080c947c47
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2025-04-02 14:45:50 +0300

    Fix JSDoc for selector

--- client/state/selectors/get-locale-suggestions.js
+++ client/state/selectors/get-locale-suggestions.js
@@ -5,7 +5,7 @@ import 'calypso/state/i18n/init';
 /**
  * Returns an object of localized language names
  * @param  {Object}  state Global state tree
- * @returns {Object|null} an array of guessed locales for the user
+ * @returns {Object|null} an object with an array of guessed locales for the user and the availability text templates
  */
 export default function getLocaleSuggestions( state ) {
 	return get( state, 'i18n.localeSuggestions', null );
