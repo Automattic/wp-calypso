2f4a05552715f1f72a54fc4d5a3d3ddd7858c4f7
Damián Suárez <rdsuarez@gmail.com>
2025-04-03 15:58:30 +0200

    first implementation of Page and Header components

---
+++ packages/site-admin/src/components/page/index.tsx
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import clsx from 'clsx';
+import { NavigableRegion } from '../../interface';
+/**
+ * Internal dependencies
+ */
+import Header from './header';
+import './style.scss';
+
+type PageProps = {
+	title: string;
+	subTitle?: string;
+	actions?: React.ReactNode;
+	children: React.ReactNode;
+	className?: string;
+	hideTitleFromUI?: boolean;
+};
+
+export default function Page( {
+	title,
+	subTitle,
+	actions,
+	children,
+	className,
+	hideTitleFromUI = false,
+}: PageProps ) {
+	const classes = clsx( 'edit-site-page', className );
+
+	return (
+		<NavigableRegion className={ classes } ariaLabel={ title }>
+			<div className="edit-site-page-content">
+				{ ! hideTitleFromUI && title && (
+					<Header title={ title } subTitle={ subTitle } actions={ actions } />
+				) }
+				{ children }
+			</div>
+		</NavigableRegion>
+	);
+}
