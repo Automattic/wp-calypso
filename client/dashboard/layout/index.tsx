2cb758c543eccde9b19e2732a49e9e47b2abe2cd
Ella <ella@vandurpe.com>
2025-04-03 18:01:15 +0200

    use createBrowserRouter

--- client/dashboard/layout/index.tsx
+++ client/dashboard/layout/index.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, Navigate, Outlet } from 'react-router-dom';
 import Domains from '../domains';
 import Header from '../header';
 import Profile from '../profile';
@@ -11,24 +11,61 @@ import './style.scss';
 // Create a client
 const queryClient = new QueryClient();
 
+// Create the root layout component that will be used in the router
+function DashboardLayout() {
+	return (
+		<div className="dashboard__layout">
+			<Header />
+			<main className="dashboard__content">
+				<Outlet />
+			</main>
+		</div>
+	);
+}
+
+// Create the router configuration
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <DashboardLayout />,
+			children: [
+				{
+					path: 'sites',
+					element: <Sites />,
+				},
+				{
+					path: 'sites/:id',
+					element: <Site />,
+				},
+				{
+					path: 'sites/:id/backups',
+					element: <SiteBackups />,
+				},
+				{
+					path: 'domains',
+					element: <Domains />,
+				},
+				{
+					path: 'account/profile',
+					element: <Profile />,
+				},
+				{
+					path: '',
+					element: <Navigate to="/sites" replace />,
+				},
+			],
+		},
+	],
+	{
+		basename: '/v2',
+	}
+);
+
 function Layout() {
 	return (
 		<QueryClientProvider client={ queryClient }>
-			<BrowserRouter basename="/v2">
-				<div className="dashboard__layout">
-					<Header />
-					<main className="dashboard__content">
-						<Routes>
-							<Route path="/sites" element={ <Sites /> } />
-							<Route path="/sites/:id" element={ <Site /> } />
-							<Route path="/sites/:id/backups" element={ <SiteBackups /> } />
-							<Route path="/domains" element={ <Domains /> } />
-							<Route path="/account/profile" element={ <Profile /> } />
-							<Route path="/" element={ <Navigate to="/sites" replace /> } />
-						</Routes>
-					</main>
-				</div>
-			</BrowserRouter>
+			<RouterProvider router={ router } />
 		</QueryClientProvider>
 	);
 }
