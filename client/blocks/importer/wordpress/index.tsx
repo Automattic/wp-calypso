1a35892fc3caa5da523df5c9c8f74d339488431c
Marco Ciampini <marco.ciampo@gmail.com>
2025-04-04 16:34:55 +0200

    Remove unused "is migrate from WP" related code from session storage and onboard data store

--- client/blocks/importer/wordpress/index.tsx
+++ client/blocks/importer/wordpress/index.tsx
@@ -1,15 +1,11 @@
-import { useSelect, useDispatch } from '@wordpress/data';
 import React, { useEffect } from 'react';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
-import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { useSelector } from 'calypso/state';
 import { getUrlData } from 'calypso/state/imports/url-analyzer/selectors';
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import { getSite, isJetpackSite, hasAllSitesList } from 'calypso/state/sites/selectors';
 import { Importer, ImportJob, StepNavigator } from '../types';
 import ImportContentOnly from './import-content-only';
-import { storeMigrateSource, retrieveMigrateSource } from './utils';
-import type { OnboardSelect } from '@automattic/data-stores';
 
 interface Props {
 	job?: ImportJob;
@@ -28,29 +24,11 @@ export const WordpressImporter: React.FunctionComponent< Props > = ( props ) => 
 	const isSiteJetpack = useSelector( ( state ) => isJetpackSite( state, siteId ) );
 	const hasAllSitesFetched = useSelector( hasAllSitesList );
 	const fromSiteAnalyzedData = useSelector( getUrlData );
-	const { setIsMigrateFromWp } = useDispatch( ONBOARD_STORE );
-	const isMigrateFromWp = useSelect(
-		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getIsMigrateFromWp(),
-		[]
-	);
 
 	/**
 	 ↓ Effects
 	 */
 	useEffect( checkImporterAvailability, [ siteId ] );
-	useEffect( checkIfImportInitFromMigratePlugin, [] );
-
-	// /**
-	//  ↓ Methods
-	//  */
-	function checkIfImportInitFromMigratePlugin() {
-		if ( isMigrateFromWp ) {
-			storeMigrateSource();
-		}
-		if ( 'true' === retrieveMigrateSource() ) {
-			setIsMigrateFromWp( true );
-		}
-	}
 
 	function checkImporterAvailability() {
 		isNotAtomicJetpack() && redirectToWpAdminImportPage();
