707948ee31c0b5d8814a4228dd5863561aabe264
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-04-04 13:49:34 +0300

    Sort out dependencies

--- client/my-sites/stats/stats-purchase/tier-upgrade-slider/pricing-slider/test/index.tsx
+++ client/my-sites/stats/stats-purchase/tier-upgrade-slider/pricing-slider/test/index.tsx
@@ -1,8 +1,6 @@
 /**
  * @jest-environment jsdom
  */
-
-import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import PricingSlider from '../index';
