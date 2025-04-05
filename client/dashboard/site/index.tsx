6673a09e80ad1da484add81f949db50e7a0e767f
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-04-05 23:58:35 +0200

    Fix secondary menu

--- client/dashboard/site/index.tsx
+++ client/dashboard/site/index.tsx
@@ -1,11 +1,11 @@
 import { useLoaderData, Outlet } from '@tanstack/react-router';
 import { __ } from '@wordpress/i18n';
-import { type SiteObject } from '../data';
+import { type FetchSiteRouteResponse } from '../data';
 import HeaderBar from '../header-bar';
 import SiteMenu from '../site-menu';
 
 function Site() {
-	const siteData = useLoaderData( { from: '/sites/$siteId' } ) as SiteObject;
+	const siteData = useLoaderData( { from: '/sites/$siteId' } ) as FetchSiteRouteResponse;
 
 	if ( siteData === undefined ) {
 		return <p>{ __( 'No site found' ) }</p>;
@@ -14,7 +14,7 @@ function Site() {
 	return (
 		<>
 			<HeaderBar>
-				<SiteMenu siteId={ siteData.id } />
+				<SiteMenu siteId={ siteData.site.id } />
 			</HeaderBar>
 			<Outlet />
 		</>
