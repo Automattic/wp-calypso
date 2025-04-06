74925efc53e77da23819cad0644589d2198b070a
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-04-06 14:19:57 +0200

    Load A4A logo in a4a app

--- client/dashboard/header/index.tsx
+++ client/dashboard/header/index.tsx
@@ -1,14 +1,16 @@
+import { useAppContext } from '../app-context';
 import HeaderBar from '../header-bar';
-import Logo from '../logo';
+import LogoDotcom from '../logo';
+import LogoA4A from '../logo/a4a';
 import MainMenu from '../main-menu';
 import SecondaryMenu from '../secondary-menu';
 
 function Header() {
+	const { appType } = useAppContext();
 	return (
 		<HeaderBar as="header">
-			<div>
-				<Logo />
-			</div>
+			<div>{ appType === 'dotcom' && <LogoDotcom /> }</div>
+			<div>{ appType === 'a4a' && <LogoA4A /> }</div>
 			<div style={ { flexGrow: 1 } }>
 				<MainMenu />
 			</div>
