d57ac9b6c9bc5b12c4391628011563f90791212d
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2025-04-07 14:33:00 +0200

    Fix: add some changes to Playground tests

--- client/landing/stepper/declarative-flow/internals/steps-repository/playground/test/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/playground/test/index.tsx
@@ -2,9 +2,8 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React from 'react';
 import PlaygroundStep from '..';
 import { StepProps } from '../../../types';
 import { renderStep, mockStepProps, RenderStepOptions } from '../../test/helpers';
@@ -23,25 +22,22 @@ const renderPlaygroundStep = (
 	return renderStep( <PlaygroundStep { ...combinedProps } />, renderOptions );
 };
 
-const launchButton = () => screen.getByRole( 'button', { name: 'Launch on WordPress.com' } );
+const getLaunchButton = () => screen.getByRole( 'button', { name: 'Launch on WordPress.com' } );
 
 describe( 'Playground', () => {
 	describe( 'step', () => {
 		it( 'should render the iframe and the launch button', () => {
 			const { container } = renderPlaygroundStep();
 
-			expect( launchButton() ).toBeInTheDocument();
-			expect( container.querySelector( 'iframe' ) ).toBeInTheDocument();
+			expect( getLaunchButton() ).toBeVisible();
+			expect( container.querySelector( 'iframe' ) ).toBeVisible();
 		} );
 
 		it( 'should change page when the user clicks the launch button', async () => {
 			const submit = jest.fn();
 			renderPlaygroundStep( { navigation: { submit } } );
 
-			await waitFor( async () => {
-				await userEvent.click( launchButton() );
-			} );
-
+			await userEvent.click( getLaunchButton() );
 			expect( submit ).toHaveBeenCalled();
 		} );
 	} );
