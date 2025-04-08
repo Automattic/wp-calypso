d972c4ab4d44c113a625fb21654716a9834f0f94
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-04-08 19:58:37 -0300

    Introduce delay prop

--- client/components/loading/StepContainerV2Loading.tsx
+++ client/components/loading/StepContainerV2Loading.tsx
@@ -1,12 +1,24 @@
 import { Step } from '@automattic/onboarding';
-import { type ReactNode } from 'react';
+import { type ReactNode, useEffect, useState } from 'react';
 
 export const StepContainerV2Loading = ( {
 	title,
 	progress,
+	delay = 0,
 }: {
 	title?: ReactNode;
 	progress?: number;
+	delay?: number;
 } ) => {
-	return <Step.Loading title={ title } progress={ progress } />;
+	const [ shouldDisplayTitle, setShouldDisplayTitle ] = useState( delay === 0 );
+
+	useEffect( () => {
+		const timeout = setTimeout( () => {
+			setShouldDisplayTitle( true );
+		}, delay );
+
+		return () => clearTimeout( timeout );
+	}, [ delay ] );
+
+	return <Step.Loading title={ shouldDisplayTitle ? title : undefined } progress={ progress } />;
 };
