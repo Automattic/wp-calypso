b2a8306db97f29910d14c63a06164bf251244fbc
Marco Ciampini <marco.ciampo@gmail.com>
2025-04-09 02:41:37 +0200

    Setup Storybook

---
+++ packages/theme/.storybook/preview.tsx
@@ -0,0 +1,17 @@
+import './style.scss';
+
+const parameters = {
+	controls: {
+		expanded: true,
+		sort: 'requiredFirst',
+	},
+	docs: {
+		controls: {
+			sort: 'requiredFirst',
+		},
+	},
+};
+
+export default {
+	parameters,
+};
