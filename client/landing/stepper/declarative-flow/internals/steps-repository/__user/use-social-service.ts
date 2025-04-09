d5ecd0e9d9a6ef2112d01c6cb3dfe849f494359d
Dean Sas <dean@deansas.org>
2025-04-09 19:51:53 +0100

    Revert "resolve infinite update loop in GitHubLoginButton (#102511)"
    This reverts commit 5f2b93cc7358d2690bceed7a0962bae16cc44351.

--- client/landing/stepper/declarative-flow/internals/steps-repository/__user/use-social-service.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/__user/use-social-service.ts
@@ -1,24 +1,16 @@
-import { useEffect, useState } from 'react';
 import { getSocialServiceFromClientId } from 'calypso/lib/login';
 
 export function useSocialService() {
-	const [ state, setState ] = useState( { socialService: '', socialServiceResponse: {} } );
+	const hashEntries = new globalThis.URLSearchParams( window.location.hash.substring( 1 ) );
 
-	useEffect( () => {
-		const hashEntries = new globalThis.URLSearchParams( window.location.hash.substring( 1 ) );
-
-		if ( hashEntries.size > 0 ) {
-			const socialServiceResponse = Object.fromEntries( hashEntries.entries() );
-			const clientId = socialServiceResponse.client_id;
-			const socialService = getSocialServiceFromClientId( clientId ) ?? '';
-
-			if ( socialService ) {
-				setState( { socialService, socialServiceResponse } );
-				// Clear the hash to prevent reprocessing
-				window.location.hash = '';
-			}
+	if ( hashEntries.size > 0 ) {
+		const socialServiceResponse = Object.fromEntries( hashEntries.entries() );
+		const clientId = socialServiceResponse.client_id;
+		const socialService = getSocialServiceFromClientId( clientId ) ?? '';
+		if ( socialService ) {
+			return { socialService, socialServiceResponse };
 		}
-	}, [] );
+	}
 
-	return state;
+	return { socialService: '', socialServiceResponse: {} };
 }
