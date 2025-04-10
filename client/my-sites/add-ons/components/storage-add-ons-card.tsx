ad0aa598cfd4f991ba16055b55eea088aec092f7
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-04-10 15:50:15 +0700

    availableStorageAddOns[ 0 ]?.productSlug

--- client/my-sites/add-ons/components/storage-add-ons-card.tsx
+++ client/my-sites/add-ons/components/storage-add-ons-card.tsx
@@ -208,7 +208,10 @@ export default function StorageAddOnCard( { siteId, actionPrimary }: Props ) {
 	}
 
 	const onActionPrimary = () => {
-		actionPrimary?.( availableStorageAddOns[ 0 ].productSlug, selectedStorageAddOnStorageQuantity );
+		actionPrimary?.(
+			availableStorageAddOns[ 0 ]?.productSlug,
+			selectedStorageAddOnStorageQuantity
+		);
 	};
 
 	return (
