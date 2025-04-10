b713293d93e94ccefe89f944535d475c4da5905c
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2025-04-11 11:25:28 +1200

    Add magic login story

---
+++ client/components/social-buttons/stories/magic-login.stories.tsx
@@ -0,0 +1,38 @@
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import MagicLoginButton from '../magic-login';
+import { SocialButtonWrapper, WooWrapper, JetpackWrapper } from './shared';
+import type { Meta, StoryObj } from '@storybook/react';
+
+// Create minimal state with just what's needed
+const initialState = {
+	login: {
+		isFormDisabled: false,
+	},
+};
+
+const store = createStore( () => initialState );
+
+const meta: Meta< typeof MagicLoginButton > = {
+	title: 'client/components/Social Button/Magic Login',
+	component: MagicLoginButton,
+	args: {
+		loginUrl: 'https://example.com/magic-login',
+	},
+	decorators: [ ( Story ) => <Provider store={ store }>{ Story() }</Provider> ],
+};
+export default meta;
+
+type Story = StoryObj< typeof MagicLoginButton >;
+
+export const Default: Story = {
+	decorators: [ SocialButtonWrapper ],
+};
+
+export const Woo: Story = {
+	decorators: [ WooWrapper ],
+};
+
+export const Jetpack: Story = {
+	decorators: [ JetpackWrapper ],
+};
