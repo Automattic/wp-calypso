8e17ccf0db07f93c527e0919957011161935b23f
bogdanungureanu <bogdanungureanu21@gmail.com>
2025-04-14 17:43:21 +0300

    Add a guard.

--- client/sites/settings/controller.tsx
+++ client/sites/settings/controller.tsx
@@ -106,15 +106,16 @@ export function redirectToSiteSettingsIfAdvancedHostingFeaturesNotSupported(
 	const site = getSelectedSite( state );
 	const dispatch = context.store.dispatch;
 
-	dispatch( fetchSiteFeatures( site?.ID as number ) ).then( () => {
-		const isSupported = areAdvancedHostingFeaturesSupported( context.store.getState() );
+	site?.ID &&
+		dispatch( fetchSiteFeatures( site?.ID ) ).then( () => {
+			const isSupported = areAdvancedHostingFeaturesSupported( context.store.getState() );
 
-		if ( isSupported === false ) {
-			return page.redirect( `/sites/settings/site/${ site?.slug }` );
-		}
+			if ( isSupported === false ) {
+				return page.redirect( `/sites/settings/site/${ site?.slug }` );
+			}
 
-		next();
-	} );
+			next();
+		} );
 }
 
 export function siteSettings( context: PageJSContext, next: () => void ) {
