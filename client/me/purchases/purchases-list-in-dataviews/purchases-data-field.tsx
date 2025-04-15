9efbeaf1009e277fab672766a12485873cbc696f
Jess Boctor <contactme@jessboctor.com>
2025-04-15 08:20:21 -0500

    Include missing purchase title

--- client/me/purchases/purchases-list-in-dataviews/purchases-data-field.tsx
+++ client/me/purchases/purchases-list-in-dataviews/purchases-data-field.tsx
@@ -1,9 +1,11 @@
 import { Purchases } from '@automattic/data-stores';
 import { Fields, Operator } from '@wordpress/dataviews';
 import { LocalizeProps } from 'i18n-calypso';
+import { getDisplayName } from 'calypso/lib/purchases';
 import { useSelector } from 'calypso/state';
 import { getSite } from 'calypso/state/sites/selectors';
 import { PurchaseItemSiteIcon, PurchaseItemProduct } from '../purchase-item';
+import OwnerInfo from '../purchase-item/owner-info';
 
 function PurchaseItemRowProduct( props: {
 	purchase: Purchases.Purchase;
@@ -14,16 +16,25 @@ function PurchaseItemRowProduct( props: {
 	const slug = purchase.siteName ?? purchase.siteId;
 	// Need to figure out how to pass translate here
 	return (
-		<div className="purchase-item__purchase-type">
-			<PurchaseItemProduct
-				purchase={ purchase }
-				site={ site }
-				translate={ translate }
-				slug={ slug }
-				showSite
-				isDisconnectedSite={ ! site }
-			/>
-		</div>
+		<>
+			<div className="purchase-item__information purchases-layout__information">
+				<div className="purchase-item__title">
+					{ getDisplayName( purchase ) }
+					&nbsp;
+					<OwnerInfo purchase={ purchase } />
+				</div>
+				<div className="purchase-item__purchase-type">
+					<PurchaseItemProduct
+						purchase={ purchase }
+						site={ site }
+						translate={ translate }
+						slug={ slug }
+						showSite
+						isDisconnectedSite={ ! site }
+					/>
+				</div>
+			</div>
+		</>
 	);
 }
 
