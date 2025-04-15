8b6d949f8feaf777ca158c7a8d2780c52bccb7e9
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-04-15 18:42:44 +0200

    Update build:style command

--- packages/dataviews/build-style.js
+++ packages/dataviews/build-style.js
@@ -1,60 +1,138 @@
 /**
  * External dependencies
  */
+const { promisify } = require( 'util' );
 const fs = require( 'fs' );
 const path = require( 'path' );
 const sass = require( 'sass' );
+const postcss = require( 'postcss' );
 
-// Prepare the input
-const inputFile = path.join( __dirname, 'src', 'style.scss' );
-const contents = fs.readFileSync( inputFile, 'utf8' );
+/**
+ * Path to packages directory.
+ *
+ * @type {string}
+ */
+const PACKAGES_DIR = path
+	.resolve( __dirname, '../../packages' )
+	.replace( /\\/g, '/' );
 
-// Prepare the output
-const buildDir = path.join( __dirname, 'build-style' );
-if ( ! fs.existsSync( buildDir ) ) {
-	fs.mkdirSync( buildDir, { recursive: true } );
+/**
+ * Promisified fs.readFile.
+ *
+ * @type {Function}
+ */
+const readFile = promisify( fs.readFile );
+
+/**
+ * Promisified fs.writeFile.
+ *
+ * @type {Function}
+ */
+const writeFile = promisify( fs.writeFile );
+
+/**
+ * Promisified fs.writeFile.
+ *
+ * @type {Function}
+ */
+const makeDir = promisify( fs.mkdir );
+
+/**
+ * Promisified sass.render.
+ *
+ * @type {Function}
+ */
+const renderSass = promisify( sass.render );
+
+/**
+ * Get the package name for a specified file
+ *
+ * @param {string} file File name.
+ *
+ * @return {string} Package name.
+ */
+function getPackageName( file ) {
+	return path.relative( PACKAGES_DIR, file ).split( path.sep )[ 0 ];
 }
-const outputFile = path.join( buildDir, 'style.css' );
 
-// TODO: check what'd be the proper path
-const baseStylesPath = path.join(
-	__dirname,
-	'../../node_modules/@wordpress/base-styles'
-);
-const importList = [
-	'colors',
-	'breakpoints',
-	'variables',
-	'mixins',
-	'animations',
-	'z-index',
-	'default-custom-properties',
-]
-	.map( ( imported ) => `@import "${ imported }";` )
-	.join( ' ' );
+/**
+ * Get Build Path for a specified file.
+ *
+ * @param {string} file        File to build.
+ * @param {string} buildFolder Output folder.
+ *
+ * @return {string} Build path.
+ */
+function getBuildPath( file, buildFolder ) {
+	const pkgName = getPackageName( file );
+	const pkgSrcPath = path.resolve( PACKAGES_DIR, pkgName, 'src' );
+	const pkgBuildPath = path.resolve( PACKAGES_DIR, pkgName, buildFolder );
+	const relativeToSrcPath = path.relative( pkgSrcPath, file );
+	return path.resolve( pkgBuildPath, relativeToSrcPath );
+}
 
-try {
-	sass.render(
+async function buildCSS( file ) {
+	const outputFile = getBuildPath(
+		file.replace( '.scss', '.css' ),
+		'build-style'
+	);
+	const outputFileRTL = getBuildPath(
+		file.replace( '.scss', '-rtl.css' ),
+		'build-style'
+	);
+
+	const [ , contents ] = await Promise.all( [
+		makeDir( path.dirname( outputFile ), { recursive: true } ),
+		readFile( file, 'utf8' ),
+	] );
+
+	const importLists = [
+		'colors',
+		'breakpoints',
+		'variables',
+		'mixins',
+		'animations',
+		'z-index',
+	]
+		// Editor and component styles should be excluded from the default CSS vars output.
+		.concat(
+			file.includes( 'common.scss' ) ||
+				! (
+					file.includes( 'block-library' ) ||
+					file.includes( 'components' )
+				)
+				? [ 'default-custom-properties' ]
+				: []
+		)
+		.map( ( imported ) => `@import "${ imported }";` )
+		.join( ' ' );
+
+	const builtSass = await renderSass( {
+		file,
+		includePaths: [ path.join( PACKAGES_DIR, 'base-styles' ) ],
+		data: ''.concat( '@use "sass:math";', contents ),
+	} );
+
+	const result = await postcss( [
+		require( 'postcss-local-keyframes' ),
+		...require( '@wordpress/postcss-plugins-preset' ),
+	] ).process( builtSass.css, {
+		from: 'src/app.css',
+		to: 'dest/app.css',
+	} );
+
+	const resultRTL = await postcss( [ require( 'rtlcss' )() ] ).process(
+		result.css,
 		{
-			file: inputFile,
-			includePaths: [ baseStylesPath ],
-			data: ''.concat( '@use "sass:math";', importList, contents ),
-		},
-		function ( error, result ) {
-			if ( error ) {
-				console.error( 'SASS Error:', error.message );
-				console.error( 'Line:', error.line );
-				console.error( 'Column:', error.column );
-				console.error( 'File:', error.file );
-
-				process.exit( 1 );
-			}
-
-			fs.writeFileSync( outputFile, result.css );
+			from: 'src/app.css',
+			to: 'dest/app.css',
 		}
 	);
-} catch ( error ) {
-	console.error( 'Script Error:', error );
 
-	process.exit( 1 );
+	await Promise.all( [
+		writeFile( outputFile, result.css ),
+		writeFile( outputFileRTL, resultRTL.css ),
+	] );
 }
+
+buildCSS( 'src/style.scss' );
