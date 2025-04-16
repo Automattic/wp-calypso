b1186bb278f60413ed2d0d589a6851b42a45ab7e
Yashwin <yashwinpoojary@gmail.com>
2025-04-16 11:17:13 +0530

    A4A > Feedback: Implement General Feedback Mechanism

---
+++ client/a8c-for-agencies/components/a4a-feedback/hooks/use-provide-general-feedback.ts
@@ -0,0 +1,181 @@
+import { useTranslate } from 'i18n-calypso';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import useSubmitSupportFormMutation from 'calypso/a8c-for-agencies/data/support/use-submit-support-form-mutation';
+import { useDispatch, useSelector } from 'calypso/state';
+import { getActiveAgencyId } from 'calypso/state/a8c-for-agencies/agency/selectors';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { getCurrentUser } from 'calypso/state/current-user/selectors';
+import { successNotice, errorNotice } from 'calypso/state/notices/actions';
+import {
+	FeedbackType,
+	type FeedbackSurveyResponsesPayload,
+	type GeneralFeedbackParams,
+} from '../types';
+import useSaveFeedbackMutation from './use-save-feedback-mutation';
+import type { SubmitContactSupportParams } from 'calypso/a8c-for-agencies/data/support/types';
+
+export const useProvideGeneralFeedback = () => {
+	const translate = useTranslate();
+	const dispatch = useDispatch();
+
+	const agencyId = useSelector( getActiveAgencyId );
+	const user = useSelector( getCurrentUser );
+
+	const [ createTicket, setCreateTicket ] = useState( false );
+
+	const getMessage = useCallback( ( type: FeedbackType ) => {
+		switch ( type ) {
+			case FeedbackType.BugReport:
+				return 'A4A Feedback - Bug Report';
+			case FeedbackType.SuggestAFeature:
+				return 'A4A Feedback - Suggest a Feature';
+		}
+	}, [] );
+
+	const {
+		mutate: saveFeedback,
+		isPending: isSavingFeedback,
+		isSuccess: isSuccessFeedback,
+		isError: isErrorFeedback,
+	} = useSaveFeedbackMutation();
+
+	const {
+		mutateAsync: submitSupportForm,
+		isPending: isSubmittingSupportForm,
+		isSuccess: isSuccessSupportForm,
+		isError: isErrorSupportForm,
+	} = useSubmitSupportFormMutation();
+
+	const isSuccess = useMemo(
+		() => ( createTicket ? isSuccessSupportForm && isSuccessFeedback : isSuccessFeedback ),
+		[ createTicket, isSuccessSupportForm, isSuccessFeedback ]
+	);
+
+	const isError = useMemo(
+		() => ( createTicket ? isErrorSupportForm || isErrorFeedback : isErrorFeedback ),
+		[ createTicket, isErrorSupportForm, isErrorFeedback ]
+	);
+
+	const handleError = useCallback( () => {
+		dispatch(
+			errorNotice(
+				translate( 'An error occurred while submitting your feedback. Please try again.' ),
+				{
+					id: 'a4a-feedback-error',
+					duration: 10000,
+				}
+			)
+		);
+		setCreateTicket( false );
+	}, [ dispatch, translate ] );
+
+	const handleSuccess = useCallback( () => {
+		dispatch(
+			successNotice(
+				translate(
+					'Thanks! Our team will use your feedback to help prioritize improvements to Automattic for Agencies.'
+				),
+				{
+					id: 'a4a-feedback-success',
+					duration: 10000,
+				}
+			)
+		);
+		setCreateTicket( false );
+	}, [ dispatch, translate ] );
+
+	useEffect( () => {
+		if ( isError ) {
+			handleError();
+		}
+		if ( isSuccess ) {
+			handleSuccess();
+		}
+	}, [ isError, isSuccess, handleError, handleSuccess, createTicket ] );
+
+	const handleSaveFeedback = useCallback(
+		( feedback: GeneralFeedbackParams ) => {
+			if ( ! agencyId || ! feedback.responses ) {
+				return;
+			}
+			const { responses, type, ticketId } = feedback;
+			// Convert the responses object to a format that can be sent so that we can send the correct data to MC Marketing Survey tool
+			const surveyResponses = Object.entries( responses ).reduce(
+				( acc, [ key, value ] ) => ( {
+					...acc,
+					[ key ]: { text: value },
+				} ),
+				{}
+			);
+			const params: FeedbackSurveyResponsesPayload = {
+				site_id: agencyId,
+				survey_id: type,
+				survey_responses: { ...surveyResponses, ...( ticketId ? { ticket_id: ticketId } : {} ) },
+			};
+			saveFeedback( { params } );
+			dispatch(
+				recordTracksEvent( 'calypso_a4a_provide_feedback_feedback_submitted', {
+					feedback_type: type,
+				} )
+			);
+		},
+		[ agencyId, dispatch, saveFeedback ]
+	);
+
+	const submitGeneralFeedback = useCallback(
+		async ( feedback: GeneralFeedbackParams ) => {
+			setCreateTicket( false );
+			if ( ! agencyId || ! feedback.responses ) {
+				return;
+			}
+			const { responses, type, screenshot } = feedback;
+			// Create a Zendesk ticket for bug reports and feature suggestions.
+			if ( [ FeedbackType.BugReport, FeedbackType.SuggestAFeature ].includes( type ) && user ) {
+				setCreateTicket( true );
+				const formData = new FormData();
+				const fields = {
+					...responses,
+					product: 'a4a',
+					message: getMessage( type ),
+					name: user.display_name,
+					email: user.email,
+					agency_id: agencyId,
+				};
+				Object.entries( fields ).forEach( ( [ key, value ] ) => {
+					if ( value ) {
+						formData.append( key, value as string );
+					}
+				} );
+				if ( screenshot ) {
+					formData.append( 'screenshot', screenshot );
+				}
+				try {
+					const response = await submitSupportForm(
+						formData as unknown as SubmitContactSupportParams
+					);
+					dispatch(
+						recordTracksEvent( 'calypso_a4a_provide_feedback_zendesk_ticket_submitted', {
+							feedback_type: type,
+						} )
+					);
+					const updatedFeedback = {
+						...feedback,
+						ticketId: response?.ticket_id,
+					};
+					handleSaveFeedback( updatedFeedback );
+				} catch {
+					// We are already handling the error
+				}
+			} else {
+				handleSaveFeedback( feedback );
+			}
+		},
+		[ agencyId, dispatch, getMessage, handleSaveFeedback, submitSupportForm, user ]
+	);
+
+	return {
+		submitGeneralFeedback,
+		isSubmitting: isSubmittingSupportForm || isSavingFeedback,
+		isSuccess,
+	};
+};
