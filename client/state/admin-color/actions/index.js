5443a9ac8bdf84a91947030078b2a3fa623cf92e
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-04-16 10:10:52 +0700

    Revert "Admin color: block layout render when still fetching (#102591)"
    This reverts commit 26f7933b349da68d789c1ee96c08f5cf3eb152dd.

--- client/state/admin-color/actions/index.js
+++ client/state/admin-color/actions/index.js
@@ -1,23 +1,12 @@
 import 'calypso/state/admin-color/init';
 import 'calypso/state/data-layer/wpcom/sites/admin-color';
-import {
-	ADMIN_COLOR_RECEIVE,
-	ADMIN_COLOR_REQUEST,
-	ADMIN_COLOR_REQUEST_FAILURE,
-} from 'calypso/state/action-types';
+import { ADMIN_COLOR_RECEIVE, ADMIN_COLOR_REQUEST } from 'calypso/state/action-types';
 
 export function requestAdminColor( siteId, adminColor ) {
 	return {
 		type: ADMIN_COLOR_REQUEST,
 		siteId,
 		adminColor,
-	};
-}
-
-export function requestAdminColorFailure( siteId ) {
-	return {
-		type: ADMIN_COLOR_REQUEST_FAILURE,
-		siteId,
 	};
 }
 
--- client/state/admin-color/selectors/index.js
+++ client/state/admin-color/selectors/index.js
@@ -3,7 +3,3 @@ import 'calypso/state/admin-color/init';
 export function getAdminColor( state, siteId ) {
 	return state?.adminColor?.[ siteId ] || null;
 }
-
-export function isFetchingAdminColor( state, siteId ) {
-	return ! state?.adminColor?.[ siteId ];
-}
