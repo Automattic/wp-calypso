ae4847a601451a044d1639a0c3ded5c90a6159df
arthur <arthur.chu@automattic.com>
2025-04-17 12:07:34 +0800

    Don't use warning

--- client/sites/settings/administration/tools/leave-site/leave-site-modal-form.tsx
+++ client/sites/settings/administration/tools/leave-site/leave-site-modal-form.tsx
@@ -9,13 +9,14 @@ import {
 import { useTranslate } from 'i18n-calypso';
 import { useState } from 'react';
 import QuerySitePurchases from 'calypso/components/data/query-site-purchases';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { useRemoveDuplicateViewsExperimentEnabled } from 'calypso/lib/remove-duplicate-views-experiment';
 import { useDispatch, useSelector } from 'calypso/state';
 import { getCurrentUserId } from 'calypso/state/current-user/selectors';
 import { hasLoadedSitePurchasesFromServer } from 'calypso/state/purchases/selectors';
 import hasCancelableSitePurchases from 'calypso/state/selectors/has-cancelable-site-purchases';
 import { leaveSite } from 'calypso/state/sites/actions';
-import { getSite, getSiteDomain } from 'calypso/state/sites/selectors';
-import LeaveSiteModalWarning from './leave-site-modal-warning';
+import { getSite, getSiteDomain, getSiteSlug } from 'calypso/state/sites/selectors';
 import type { AppState } from 'calypso/types';
 import './leave-site-modal-form.scss';
 
@@ -30,6 +31,7 @@ const LeaveSiteModalForm = ( { siteId, onClose }: LeaveSiteModalFormProps ) => {
 	const [ isChecked, setChecked ] = useState( false );
 	const [ isSubmitting, setIsSubmitting ] = useState( false );
 	const userId = useSelector( ( state: AppState ) => getCurrentUserId( state ) );
+	const siteSlug = useSelector( ( state: AppState ) => getSiteSlug( state, siteId ) );
 	const siteDomain = useSelector( ( state: AppState ) => getSiteDomain( state, siteId ) || '' );
 	const siteOwnerId = useSelector( ( state: AppState ) => getSite( state, siteId )?.site_owner );
 	const sitePurchasesLoaded = useSelector( ( state: AppState ) =>
@@ -40,6 +42,8 @@ const LeaveSiteModalForm = ( { siteId, onClose }: LeaveSiteModalFormProps ) => {
 		hasCancelableSitePurchases( state, siteId )
 	);
 
+	const isUntangled = useRemoveDuplicateViewsExperimentEnabled();
+
 	const isSiteOwner = userId === siteOwnerId;
 
 	const handleLeaveSite = async () => {
@@ -47,8 +51,9 @@ const LeaveSiteModalForm = ( { siteId, onClose }: LeaveSiteModalFormProps ) => {
 			return;
 		}
 
+		recordTracksEvent( 'calypso_leave_site_modal_form_leave_site_click' );
+		setIsSubmitting( true );
 		try {
-			setIsSubmitting( true );
 			const result = await dispatch( leaveSite( siteId ) );
 			if ( result ) {
 				page.redirect( '/sites' );
@@ -57,6 +62,107 @@ const LeaveSiteModalForm = ( { siteId, onClose }: LeaveSiteModalFormProps ) => {
 		} finally {
 			setIsSubmitting( false );
 		}
+	};
+
+	const renderBody = () => {
+		if ( hasActiveCancelableSubscriptions ) {
+			return (
+				<p>
+					{ translate(
+						'You have active subscriptions associated with this site. These must be cancelled before you can leave the site.'
+					) }
+				</p>
+			);
+		}
+
+		if ( isSiteOwner ) {
+			return (
+				<p>
+					{ translate(
+						'You are the owner of this site. To leave you must first transfer ownership to another account.'
+					) }
+				</p>
+			);
+		}
+
+		return (
+			<>
+				<p>
+					{ translate( 'Are you sure to leave the site {{b}}%(siteDomain)s{{/b}}?', {
+						args: { siteDomain },
+						components: {
+							b: <b />,
+						},
+						comment: '%(siteDomain)s is the site domain',
+					} ) }
+				</p>
+				<p>
+					{ translate(
+						'Leaving will remove your access to the site, including all content, users, domains, upgrades, and anything else you have access to.'
+					) }
+				</p>
+				<p>
+					{ translate(
+						'To regain access, a current administrator must re-invite you. Please confirm this is your intent before proceeding.'
+					) }
+				</p>
+				<CheckboxControl
+					__nextHasNoMarginBottom
+					label={ translate( 'I understand the consequences of leaving' ) }
+					checked={ isChecked }
+					disabled={ isSubmitting }
+					onChange={ setChecked }
+				/>
+			</>
+		);
+	};
+
+	const renderPrimaryAction = () => {
+		if ( hasActiveCancelableSubscriptions ) {
+			return (
+				<Button
+					__next40pxDefaultSize
+					variant="primary"
+					href={ `/purchases/subscriptions/${ siteSlug }` }
+					onClick={ () =>
+						recordTracksEvent( 'calypso_leave_site_modal_form_manage_purchases_click' )
+					}
+				>
+					{ translate( 'Manage purchases' ) }
+				</Button>
+			);
+		}
+
+		if ( isSiteOwner ) {
+			return (
+				<Button
+					__next40pxDefaultSize
+					variant="primary"
+					href={
+						isUntangled
+							? `/sites/settings/site/${ siteSlug }/transfer-site`
+							: `/settings/start-site-transfer/${ siteSlug }?source=`
+					}
+					onClick={ () =>
+						recordTracksEvent( 'calypso_leave_site_modal_form_transfer_ownership_click' )
+					}
+				>
+					{ translate( 'Transfer ownership' ) }
+				</Button>
+			);
+		}
+
+		return (
+			<Button
+				__next40pxDefaultSize
+				variant="primary"
+				disabled={ ! isChecked || isSiteOwner || hasActiveCancelableSubscriptions }
+				isBusy={ isSubmitting }
+				onClick={ handleLeaveSite }
+			>
+				{ translate( 'Leave site' ) }
+			</Button>
+		);
 	};
 
 	return (
@@ -68,42 +174,8 @@ const LeaveSiteModalForm = ( { siteId, onClose }: LeaveSiteModalFormProps ) => {
 			} }
 		>
 			{ ! sitePurchasesLoaded && <QuerySitePurchases siteId={ siteId } /> }
-			<VStack spacing={ 6 }>
-				{ ( isSiteOwner || hasActiveCancelableSubscriptions ) && (
-					<LeaveSiteModalWarning
-						siteId={ siteId }
-						isSiteOwner={ isSiteOwner }
-						hasActiveCancelableSubscriptions={ hasActiveCancelableSubscriptions }
-					/>
-				) }
-				<VStack spacing={ 0 }>
-					<p>
-						{ translate( 'Are you sure to leave the site {{b}}%(siteDomain)s{{/b}}?', {
-							args: { siteDomain },
-							components: {
-								b: <b />,
-							},
-							comment: '%(siteDomain)s is the site domain',
-						} ) }
-					</p>
-					<p>
-						{ translate(
-							'Leaving will remove your access to the site, including all content, users, domains, upgrades, and anything else you have access to.'
-						) }
-					</p>
-					<p>
-						{ translate(
-							'To regain access, a current administrator must re-invite you. Please confirm this is your intent before proceeding.'
-						) }
-					</p>
-					<CheckboxControl
-						__nextHasNoMarginBottom
-						label={ translate( 'I understand the consequences of leaving' ) }
-						checked={ isChecked }
-						disabled={ isSubmitting }
-						onChange={ setChecked }
-					/>
-				</VStack>
+			<VStack spacing={ ! isSiteOwner && ! hasActiveCancelableSubscriptions ? 6 : 0 }>
+				<VStack spacing={ 0 }>{ renderBody() }</VStack>
 				<Flex justify="flex-end" expanded={ false }>
 					<FlexItem>
 						<Button
@@ -115,17 +187,7 @@ const LeaveSiteModalForm = ( { siteId, onClose }: LeaveSiteModalFormProps ) => {
 							{ translate( 'Cancel' ) }
 						</Button>
 					</FlexItem>
-					<FlexItem>
-						<Button
-							__next40pxDefaultSize
-							variant="primary"
-							disabled={ ! isChecked || isSiteOwner || hasActiveCancelableSubscriptions }
-							isBusy={ isSubmitting }
-							onClick={ handleLeaveSite }
-						>
-							{ translate( 'Leave site' ) }
-						</Button>
-					</FlexItem>
+					<FlexItem>{ renderPrimaryAction() }</FlexItem>
 				</Flex>
 			</VStack>
 		</form>
