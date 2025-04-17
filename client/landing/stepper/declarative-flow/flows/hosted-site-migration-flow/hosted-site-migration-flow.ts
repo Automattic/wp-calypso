164d398ac0d21bd96db9a9a7d7db5cd6baca534f
Gabriel Caires <gabriel.caires@automattic.com>
2025-04-17 10:58:47 +0100

    Deprecate the hosted-site-migration

--- client/landing/stepper/declarative-flow/flows/hosted-site-migration-flow/hosted-site-migration-flow.ts
+++ client/landing/stepper/declarative-flow/flows/hosted-site-migration-flow/hosted-site-migration-flow.ts
@@ -3,6 +3,9 @@ import { useSearchParams } from 'react-router-dom';
 import { type Flow } from '../../internals/types';
 import siteMigration from '../site-migration-flow/site-migration-flow';
 
+/**
+ * @deprecated Please use SITE_MIGRATION_FLOW instead
+ */
 const hostedSiteMigrationFlow: Flow = {
 	...siteMigration,
 	variantSlug: HOSTED_SITE_MIGRATION_FLOW,
