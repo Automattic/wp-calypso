a7768ee458de880b45e0a81d95c9aaf53964c979
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-04-18 23:39:11 +0500

    🐛 FIX: encoded text on tags header

--- client/blocks/reader-post-card/tag-link.jsx
+++ client/blocks/reader-post-card/tag-link.jsx
@@ -31,7 +31,7 @@ class TagLink extends Component {
 
 	render() {
 		const { tag } = this.props;
-		const path = addLocaleToPathLocaleInFront( `/tag/${ tag.slug }` );
+		const path = addLocaleToPathLocaleInFront( `/tag/${ encodeURIComponent( tag.slug ) }` );
 		const decodedSlug = decodeURIComponent( tag.slug ); // Other languages may contain non-ASCII characters so we need to decode it.
 
 		return (
