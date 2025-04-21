26e15f0b8d5191cadeb1478d9885d7f2074b7f62
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-04-21 16:45:03 -0300

    Ellipsis long domains in the middle

--- client/components/domains/domain-registration-suggestion/index.jsx
+++ client/components/domains/domain-registration-suggestion/index.jsx
@@ -265,6 +265,18 @@ class DomainRegistrationSuggestion extends Component {
 		};
 	}
 
+	getDomainName( name ) {
+		if ( name.length <= 24 ) {
+			return name;
+		}
+
+		return (
+			<abbr title={ name }>
+				{ name.slice( 0, 8 ) }…{ name.slice( -8 ) }
+			</abbr>
+		);
+	}
+
 	renderDomain( hasBadges = false ) {
 		const {
 			showHstsNotice,
@@ -288,7 +300,9 @@ class DomainRegistrationSuggestion extends Component {
 				<div className={ titleWrapperClassName }>
 					<h3 className="domain-registration-suggestion__title">
 						<div className="domain-registration-suggestion__domain-title">
-							<span className="domain-registration-suggestion__domain-title-name">{ name }</span>
+							<span className="domain-registration-suggestion__domain-title-name">
+								{ this.getDomainName( name ) }
+							</span>
 							<span className="domain-registration-suggestion__domain-title-tld">{ tld }</span>
 							{ ( showHstsNotice || showDotGayNotice ) && this.renderInfoBubble() }
 						</div>
