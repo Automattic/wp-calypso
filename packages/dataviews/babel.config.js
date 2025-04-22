371ab830176d1e71a3584d1d782a51aefdeb8301
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-04-22 13:56:22 +0200

    Delete unused babel.config

--- packages/dataviews/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = ( api ) => {
-	api.cache( true );
-
-	return {
-		presets: [ '@wordpress/babel-preset-default' ],
-		plugins: [ '@emotion/babel-plugin', 'babel-plugin-inline-json-import' ],
-	};
-};
