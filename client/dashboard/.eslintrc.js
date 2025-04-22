d7875297fe1447d0ace79484a94c9f9f51c3dc72
Ella <ella@vandurpe.com>
2025-04-22 14:28:03 +0200

    Add note

--- client/dashboard/.eslintrc.js
+++ client/dashboard/.eslintrc.js
@@ -11,11 +11,18 @@ module.exports = {
 							'calypso/lib/*',
 							'!calypso/lib/wp',
 							'calypso/lib/wp/*',
+							// Please do not add exceptions unless agreed on
+							// with the #architecture group.
 						],
 						message: 'Importing from calypso/ is not allowed in the dashboard folder.',
 					},
 					{
-						group: [ '@automattic/*', '!@automattic/calypso-config' ],
+						group: [
+							'@automattic/*',
+							'!@automattic/calypso-config',
+							// Please do not add exceptions unless agreed on
+							// with the #architecture group.
+						],
 						message: 'Importing from @automattic/ is not allowed in the dashboard folder.',
 					},
 				],
