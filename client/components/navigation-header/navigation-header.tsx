cb3916e02102273d02eac4cf1dab37d11ae55aad
Jasper Kang <jasper.kang@automattic.com>
2025-04-24 10:24:51 +1200

    add in padding

--- client/components/navigation-header/navigation-header.tsx
+++ client/components/navigation-header/navigation-header.tsx
@@ -27,42 +27,12 @@ interface HeaderProps extends React.HTMLAttributes< HTMLElement > {
 	hasScreenOptionsTab?: boolean;
 }
 
-// Arrow left icon component
-const ArrowLeftIcon: React.FC = () => (
-	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M15 18L9 12L15 6"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-		/>
-	</svg>
-);
-
 // Download icon component
 const DownloadIcon: React.FC = () => (
-	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path
-			d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-		/>
-		<path
-			d="M7 10L12 15L17 10"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-		/>
-		<path
-			d="M12 15V3"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
+			d="M14 8.3L13 7.2L9 11.2V0H7.5V11.3L3 7.2L2 8.3L8.2 14.1L14 8.3ZM14.5 12V15.5H1.5V12H0V17H16V12H14.5Z"
+			fill="#008710"
 		/>
 	</svg>
 );
@@ -145,7 +115,7 @@ const NavigationHeader: React.FC< HeaderProps > = ( {
 							}
 						} }
 					>
-						<ArrowLeftIcon /> { backLinkText }
+						← { backLinkText }
 					</a>
 				) }
 			</div>
