ed98639b7a6962360d518bdb513f70e9b6b3a002
Andrew Duthie <andrew.duthie@automattic.com>
2025-04-23 12:06:31 -0400

    Simplify SurveyManager to handle single migration flow

--- client/landing/stepper/declarative-flow/internals/components/survery-manager/index.tsx
+++ client/landing/stepper/declarative-flow/internals/components/survery-manager/index.tsx
@@ -1,28 +1,20 @@
 import { SITE_MIGRATION_FLOW } from '@automattic/onboarding';
-import { type ComponentType, Suspense } from 'react';
+import { Suspense } from 'react';
 import AsyncMigrationSurvey from '../../steps-repository/components/migration-survey/async';
 import { Flow } from '../../types';
 import { DeferredRender } from '../deferred-render';
 
-const FLOW_SURVEYS = new Map< string, ComponentType >( [
-	[ SITE_MIGRATION_FLOW, AsyncMigrationSurvey ],
-] );
+const MIGRATION_FLOWS = new Set< string >( [ SITE_MIGRATION_FLOW ] );
 
 const SurveyManager = ( { disabled = false, flow }: { disabled?: boolean; flow?: Flow } ) => {
-	if ( disabled || ! flow ) {
-		return null;
-	}
-
-	const SurveyComponent = FLOW_SURVEYS.get( flow.name );
-
-	if ( ! SurveyComponent ) {
+	if ( disabled || ! flow || ! MIGRATION_FLOWS.has( flow.name ) ) {
 		return null;
 	}
 
 	return (
 		<DeferredRender timeMs={ 2000 }>
 			<Suspense>
-				<SurveyComponent />
+				<AsyncMigrationSurvey />
 			</Suspense>
 		</DeferredRender>
 	);
