a10b46cd5140c804b7630054db74f7b6777f5e42
Ella <ella@vandurpe.com>
2025-04-23 13:01:22 +0200

    Rebase

--- client/dashboard/header/index.tsx
+++ client/dashboard/header/index.tsx
@@ -1,18 +1,14 @@
-import { useNavigate, useRouter } from '@tanstack/react-router';
-import { Button } from '@wordpress/components';
 import { useViewportMatch } from '@wordpress/compose';
 import { useAppContext } from '../app/context';
 import HeaderBar from '../header-bar';
 import PrimaryMenu from '../primary-menu';
 import PrimaryMenuMobile from '../primary-menu-mobile';
+import RouterLinkButton from '../router-link-button';
 import SecondaryMenu from '../secondary-menu';
 
 function Header() {
 	const { Logo } = useAppContext();
 	const isDesktop = useViewportMatch( 'medium' );
-	const navigate = useNavigate();
-	const router = useRouter();
-	const href = router.buildLocation( { to: '/' } ).href;
 
 	return (
 		<HeaderBar as="header">
@@ -24,14 +20,7 @@ function Header() {
 
 			{ Logo && (
 				<div style={ { display: 'flex', alignItems: 'center' } }>
-					<Button
-						icon={ <Logo /> }
-						href={ href }
-						onClick={ ( event: React.MouseEvent ) => {
-							event.preventDefault();
-							navigate( { to: '/' } );
-						} }
-					/>
+					<RouterLinkButton icon={ <Logo /> } to="/" />
 				</div>
 			) }
 
