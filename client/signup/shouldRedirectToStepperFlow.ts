3aaab505a917450063110975ca3ee66385fffa48
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-04-23 18:09:58 -0300

    Preserve locale

--- client/signup/shouldRedirectToStepperFlow.ts
+++ client/signup/shouldRedirectToStepperFlow.ts
@@ -2,8 +2,16 @@ import { getPlanByPathSlug } from '@automattic/calypso-products';
 import { Context } from '@automattic/calypso-router';
 import { addQueryArgs } from '@wordpress/url';
 
+const withLocale = ( url: string, locale?: string ) => {
+	if ( ! locale ) {
+		return url;
+	}
+
+	return `${ url }/${ locale }`;
+};
+
 export const shouldRedirectToStepperFlow = ( context: Context ) => {
-	const { flowName } = context.params;
+	const { flowName, lang } = context.params;
 
 	if ( ! flowName ) {
 		return null;
@@ -12,7 +20,9 @@ export const shouldRedirectToStepperFlow = ( context: Context ) => {
 	const plan = getPlanByPathSlug( flowName );
 
 	if ( plan || flowName === 'free' ) {
-		return addQueryArgs( '/setup/create-site', {
+		const stepperFlow = withLocale( '/setup/create-site', lang );
+
+		return addQueryArgs( stepperFlow, {
 			...context.query,
 			plan: flowName,
 		} );
