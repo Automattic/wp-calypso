458681aff542bf583deba97b2d5adaea59d4ec2f
Ahmed <ahmed.el.azzabi@automattic.com>
2025-04-23 11:31:16 +0100

    Export isWooCommercePaymentsOnboardingFlow function for onboarding flow checks

--- client/state/selectors/is-woo-jpc-flow.ts
+++ client/state/selectors/is-woo-jpc-flow.ts
@@ -11,7 +11,7 @@ const isLegacyJetpackWooOnboardingFlow = ( state: AppState ) => {
 	return 'woocommerce-onboarding' === get( getCurrentQueryArguments( state ), 'from' );
 };
 
-const isWooCommercePaymentsOnboardingFlow = ( state: AppState ) => {
+export const isWooCommercePaymentsOnboardingFlow = ( state: AppState ) => {
 	const from =
 		get( getInitialQueryArguments( state ), 'from' ) === 'woocommerce-payments' ||
 		get( getCurrentQueryArguments( state ), 'from' ) === 'woocommerce-onboarding';
