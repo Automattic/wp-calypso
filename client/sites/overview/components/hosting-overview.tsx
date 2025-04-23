d8414eb224fec66752f517308c315cf54d40c54e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-04-23 23:29:33 +0200

    Hosting Overview: Record tracks event on browsers back

--- client/sites/overview/components/hosting-overview.tsx
+++ client/sites/overview/components/hosting-overview.tsx
@@ -1,8 +1,11 @@
 import { useTranslate } from 'i18n-calypso';
-import { FC } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 import NavigationHeader from 'calypso/components/navigation-header';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { isNotAtomicJetpack, isMigrationInProgress } from 'calypso/sites-dashboard/utils';
 import { useSelector } from 'calypso/state';
+import getCurrentRoute from 'calypso/state/selectors/get-current-route';
+import getCurrentRoutePattern from 'calypso/state/selectors/get-current-route-pattern';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 import ActiveDomainsCard from './active-domains-card';
 import MigrationOverview from './migration-overview';
@@ -16,7 +19,29 @@ import './style.scss';
 
 const HostingOverview: FC = () => {
 	const site = useSelector( getSelectedSite );
+	const currentRoute = useSelector( getCurrentRoute );
+	const currentRoutePattern = useSelector( getCurrentRoutePattern );
 	const translate = useTranslate();
+
+	const handleBrowsersBack = useCallback(
+		( ev: PopStateEvent ) => {
+			recordTracksEvent( 'calypso_hosting_overview_back_button', {
+				from_route: currentRoute,
+				from_route_path: currentRoutePattern,
+				to_route: ev.state?.path,
+			} );
+		},
+		[ currentRoute, currentRoutePattern ]
+	);
+
+	useEffect( () => {
+		window.addEventListener( 'popstate', handleBrowsersBack );
+		return () => {
+			setTimeout( () => {
+				window.removeEventListener( 'popstate', handleBrowsersBack );
+			}, 0 );
+		};
+	}, [ handleBrowsersBack ] );
 
 	if ( site ) {
 		if ( isMigrationInProgress( site ) ) {
