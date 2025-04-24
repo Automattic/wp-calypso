8029e5254b11c944410792025dc7b11bc3cc7ca6
Riad Benguella <benguella@gmail.com>
2025-04-24 07:48:16 +0100

    More cleanup

--- client/my-sites/store/index.js
+++ client/my-sites/store/index.js
@@ -1,7 +1,4 @@
 import page from '@automattic/calypso-router';
-import { translate } from 'i18n-calypso';
-import { createElement } from 'react';
-import EmptyContent from 'calypso/components/empty-content';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import { navigation, siteSelection, sites } from 'calypso/my-sites/controller';
 import isSiteStore from 'calypso/state/selectors/is-site-store';
@@ -9,16 +6,6 @@ import { getSiteOption } from 'calypso/state/sites/selectors';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 import StatsController from './app/store-stats/controller';
 import './style.scss';
-
-function notFoundError( context, next ) {
-	context.primary = createElement( EmptyContent, {
-		className: 'content-404',
-		illustration: '/calypso/images/illustrations/illustration-404.svg',
-		title: translate( 'Uh oh. Page not found.' ),
-		line: translate( "Sorry, the page you were looking for doesn't exist or has been moved." ),
-	} );
-	next();
-}
 
 function redirectIfWooCommerceNotInstalled( context, next ) {
 	const state = context.store.getState();
@@ -55,6 +42,4 @@ export default async function () {
 		makeLayout,
 		clientRender
 	);
-
-	page( '/store/*', notFoundError, makeLayout, clientRender );
 }
