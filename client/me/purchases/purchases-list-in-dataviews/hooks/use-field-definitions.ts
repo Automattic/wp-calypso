1b0260811456a567c9c1fe0fbba8f51627ef1b79
Jess Boctor <contactme@jessboctor.com>
2025-04-25 11:42:00 -0500

    Use more stable payment methods state

--- client/me/purchases/purchases-list-in-dataviews/hooks/use-field-definitions.ts
+++ client/me/purchases/purchases-list-in-dataviews/hooks/use-field-definitions.ts
@@ -1,23 +1,24 @@
 import { useTranslate } from 'i18n-calypso';
 import { useMemo } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
-import { StoredPaymentMethod } from 'calypso/lib/checkout/payment-methods';
+import { useStoredPaymentMethods } from 'calypso/my-sites/checkout/src/hooks/use-stored-payment-methods';
 import { getPurchasesFieldDefinitions } from '../purchases-data-field';
 
-export function usePurchasesFieldDefinitions( paymentMethods: Array< StoredPaymentMethod > ) {
+export function usePurchasesFieldDefinitions() {
 	const translate = useTranslate();
 	const moment = useLocalizedMoment();
-
-	const backupPaymentMethods = paymentMethods.filter(
-		( paymentMethod ) => paymentMethod.is_backup === true
-	);
+	const paymentMethods = useStoredPaymentMethods().paymentMethods;
 
 	return useMemo( () => {
+		const backupPaymentMethods = paymentMethods.filter(
+			( paymentMethod ) => paymentMethod.is_backup === true
+		);
+
 		const fieldDefinitions = getPurchasesFieldDefinitions( {
 			translate,
 			moment,
 			backupPaymentMethods,
 		} );
 		return fieldDefinitions;
-	}, [ translate, moment, backupPaymentMethods ] );
+	}, [ translate, moment, paymentMethods ] );
 }
