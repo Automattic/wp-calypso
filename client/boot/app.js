26163865fb8d5f6e9a02158a8674db00fa6743f7
Andrew Duthie <andrew.duthie@automattic.com>
2025-04-28 09:37:15 -0400

    Restore Calypso polyfills to boot
    Still need and not related to removal of localStorage polyfill
    
    See: https://github.com/Automattic/wp-calypso/pull/102911/files#r2063481140

--- client/boot/app.js
+++ client/boot/app.js
@@ -1,3 +1,6 @@
+// Initialize polyfills before any dependencies are loaded
+import '@automattic/calypso-polyfills';
+
 import { bootApp } from './common';
 
 import 'calypso/assets/stylesheets/style.scss';
