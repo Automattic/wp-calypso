0c91213a861b4f5b79b3f04327b3f469c1887a53
Dean Sas <dean.sas@automattic.com>
2025-04-29 17:03:36 +0100

    JetpackLogo: Add variant prop to choose color
    The `variant` prop allows the Jetpack logo to be rendered in color,
    black or white. It supersedes the monochrome prop, though that is still
    supported for backwards compatibility.

--- client/components/jetpack-logo/docs/example.js
+++ client/components/jetpack-logo/docs/example.js
@@ -12,8 +12,16 @@ export default function JetpackLogoExample() {
 			<pre>{ '<JetpackLogo full size={ 64 } />' }</pre>
 			<JetpackLogo full size={ 64 } />
 			<hr />
-			<pre>{ '<JetpackLogo full monochrome />' }</pre>
-			<JetpackLogo full monochrome />
+			<pre>{ '<JetpackLogo full variant="color" />' }</pre>
+			<JetpackLogo full variant="color" />
+			<hr />
+			<pre>{ '<JetpackLogo full variant="black" />' }</pre>
+			<JetpackLogo full variant="black" />
+			<hr />
+			<pre>{ '<JetpackLogo full variant="white" style={{ background: "#222" }} />' }</pre>
+			<div style={ { background: '#222', display: 'inline-block', padding: 8 } }>
+				<JetpackLogo full variant="white" />
+			</div>
 		</div>
 	);
 }
