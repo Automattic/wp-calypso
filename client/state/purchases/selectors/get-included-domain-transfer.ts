018228ef707aa36ec6b5e65e5692e89f47714578
Fabien MILLERAND <fab@millerf.com>
2025-04-29 10:11:43 +0200

    Implemented feedback
    Move to TS for 2 files
    Remove lodash dependencies

--- client/state/purchases/selectors/get-included-domain-transfer.ts
+++ client/state/purchases/selectors/get-included-domain-transfer.ts
@@ -1,8 +1,8 @@
 import { isDomainTransfer } from '@automattic/calypso-products';
-import { find } from 'lodash';
 import { isSubscription } from 'calypso/lib/purchases';
 import { getSitePurchases } from './get-site-purchases';
-
+import type { Purchase } from 'calypso/lib/purchases/types';
+import type { AppState } from 'calypso/types';
 import 'calypso/state/purchases/init';
 
 /**
@@ -11,19 +11,21 @@ import 'calypso/state/purchases/init';
  * Even if a domain transfer was purchased with the subscription, it will
  * not be returned if the domain transfer product was paid for separately (eg: if it was
  * renewed on its own).
- * @param   {Object} state  global state
- * @param   {Object} subscriptionPurchase  subscription purchase object
- * @returns {Object} domain transfer purchase if there is one, null if none found or not a subscription object passed
+ * @param   {AppState} state  global state
+ * @param   {Purchase | null | undefined} subscriptionPurchase  subscription purchase object
+ * @returns {Purchase | null | undefined} domain transfer purchase if there is one, null if none found or not a subscription object passed
  */
-export const getIncludedDomainTransfer = ( state, subscriptionPurchase ) => {
+export const getIncludedDomainTransfer = (
+	state: AppState,
+	subscriptionPurchase: Purchase | null | undefined
+): Purchase | null | undefined => {
 	if ( ! subscriptionPurchase || ! isSubscription( subscriptionPurchase ) ) {
 		return null;
 	}
 
 	const { includedDomain } = subscriptionPurchase;
 	const sitePurchases = getSitePurchases( state, subscriptionPurchase.siteId );
-	return find(
-		sitePurchases,
+	return sitePurchases.find(
 		( purchase ) => isDomainTransfer( purchase ) && includedDomain === purchase.meta
 	);
 };
