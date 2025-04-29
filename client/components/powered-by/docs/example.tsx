1c42a8813e0470efd86dbaa20bc17368cd4c4cfd
Dean Sas <dean.sas@automattic.com>
2025-04-29 23:01:58 +0100

    PoweredBy: Improve docs
    Rework the example so that it fits in the half of the screen that
    /devdocs/design gives you, and also shows the code to render each
    example. This makes it consistent with the various *Logo docs.

--- client/components/powered-by/docs/example.tsx
+++ client/components/powered-by/docs/example.tsx
@@ -1,57 +1,61 @@
-import { Flex, FlexItem } from '@wordpress/components';
 import PoweredBy from 'calypso/components/powered-by';
 
 const PoweredByExample = () => {
 	return (
 		<div>
-			<h3>Color variant</h3>
-			<Flex gap={ 4 }>
-				<FlexItem>
-					<PoweredBy brand="jetpack" variant="color" />
-				</FlexItem>
-				<FlexItem>
-					<PoweredBy brand="woocommerce" variant="color" />
-				</FlexItem>
-				<FlexItem>
-					<PoweredBy brand="wpcloud" variant="color" />
-				</FlexItem>
-				<FlexItem>
-					<PoweredBy brand="wpcom" variant="color" />
-				</FlexItem>
-			</Flex>
+			<pre>{ '<PoweredBy brand="jetpack" />' }</pre>
+			<PoweredBy brand="jetpack" />
+			<hr />
 
-			<h3>Black variant</h3>
-			<Flex gap={ 4 }>
-				<FlexItem>
-					<PoweredBy brand="jetpack" variant="black" />
-				</FlexItem>
-				<FlexItem>
-					<PoweredBy brand="woocommerce" variant="black" />
-				</FlexItem>
-				<FlexItem>
-					<PoweredBy brand="wpcloud" variant="black" />
-				</FlexItem>
-				<FlexItem>
-					<PoweredBy brand="wpcom" variant="black" />
-				</FlexItem>
-			</Flex>
+			<pre>{ '<PoweredBy brand="jetpack" variant="black" />' }</pre>
+			<PoweredBy brand="jetpack" variant="black" />
+			<hr />
 
-			<h3>White variant</h3>
-			<div style={ { background: '#000', padding: '20px' } }>
-				<Flex gap={ 4 }>
-					<FlexItem>
-						<PoweredBy brand="jetpack" variant="white" />
-					</FlexItem>
-					<FlexItem>
-						<PoweredBy brand="woocommerce" variant="white" />
-					</FlexItem>
-					<FlexItem>
-						<PoweredBy brand="wpcloud" variant="white" />
-					</FlexItem>
-					<FlexItem>
-						<PoweredBy brand="wpcom" variant="white" />
-					</FlexItem>
-				</Flex>
+			<pre>{ '<PoweredBy brand="jetpack" variant="white" />' }</pre>
+			<div style={ { background: '#000', padding: '20px', display: 'inline-block' } }>
+				<PoweredBy brand="jetpack" variant="white" />
+			</div>
+			<hr />
+
+			<pre>{ '<PoweredBy brand="woocommerce" />' }</pre>
+			<PoweredBy brand="woocommerce" />
+			<hr />
+
+			<pre>{ '<PoweredBy brand="woocommerce" variant="black" />' }</pre>
+			<PoweredBy brand="woocommerce" variant="black" />
+			<hr />
+
+			<pre>{ '<PoweredBy brand="woocommerce" variant="white" />' }</pre>
+			<div style={ { background: '#000', padding: '20px', display: 'inline-block' } }>
+				<PoweredBy brand="woocommerce" variant="white" />
+			</div>
+			<hr />
+
+			<pre>{ '<PoweredBy brand="wpcloud" />' }</pre>
+			<PoweredBy brand="wpcloud" />
+			<hr />
+
+			<pre>{ '<PoweredBy brand="wpcloud" variant="black" />' }</pre>
+			<PoweredBy brand="wpcloud" variant="black" />
+			<hr />
+
+			<pre>{ '<PoweredBy brand="wpcloud" variant="white" />' }</pre>
+			<div style={ { background: '#000', padding: '20px', display: 'inline-block' } }>
+				<PoweredBy brand="wpcloud" variant="white" />
+			</div>
+			<hr />
+
+			<pre>{ '<PoweredBy brand="wpcom" />' }</pre>
+			<PoweredBy brand="wpcom" />
+			<hr />
+
+			<pre>{ '<PoweredBy brand="wpcom" variant="black" />' }</pre>
+			<PoweredBy brand="wpcom" variant="black" />
+			<hr />
+
+			<pre>{ '<PoweredBy brand="wpcom" variant="white" />' }</pre>
+			<div style={ { background: '#000', padding: '20px', display: 'inline-block' } }>
+				<PoweredBy brand="wpcom" variant="white" />
 			</div>
 		</div>
 	);
