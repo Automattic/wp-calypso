dacc39c9bfe767360c8d01b3f9d4480a1c75b09e
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-04-29 21:31:20 -0300

    Use Stepper's UserStepComponent

--- client/signup/signup.tsx
+++ client/signup/signup.tsx
@@ -1,3 +1,18 @@
+import UserStepComponent from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/__user';
+
 export const SignupPage = () => {
-	return <div>sign up page here</div>;
+	return (
+		<div className="step-route onboarding user">
+			<UserStepComponent
+				navigation={ {
+					submit() {
+						// eslint-disable-next-line no-console
+						console.log( 'Congratulations, you are now a user!' );
+					},
+				} }
+				stepName="user"
+				flow="onboarding"
+			/>
+		</div>
+	);
 };
