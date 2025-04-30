84fa4195a00b1def097eb4072c9dbe894b675427
Lena Morita <lena@jaguchi.com>
2025-04-30 19:06:35 +0900

    Improve types

--- packages/components/src/types.d.ts
+++ packages/components/src/types.d.ts
@@ -5,5 +5,12 @@ declare module '*.svg' {
 
 declare const __i18n_text_domain__: string;
 
-declare module '*.module.css';
-declare module '*.module.scss';
+declare module '*.module.css' {
+	const classes: { [ key: string ]: string };
+	export default classes;
+}
+
+declare module '*.module.scss' {
+	const classes: { [ key: string ]: string };
+	export default classes;
+}
