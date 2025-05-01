e02e78e9d850ccb1cfeb588761eea30e545ea5c9
StevenDufresne <dufresnesteven@gmail.com>
2025-05-01 17:29:12 +0900

    Add warning message for useUser.

--- client/performance-profiler/hooks/use-user.ts
+++ client/performance-profiler/hooks/use-user.ts
@@ -2,5 +2,11 @@ import { useContext } from 'react';
 import { UserContext, type UserContextType } from '../context';
 
 export function useUser(): UserContextType {
-	return useContext( UserContext );
+	const context = useContext( UserContext );
+
+	if ( context === undefined ) {
+		throw new Error( 'useUser must be used within a UserProvider' );
+	}
+
+	return context;
 }
