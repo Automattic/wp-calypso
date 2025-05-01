be8d8391d67ca79cb4548028621610e0fee51c26
Lena Morita <lena@jaguchi.com>
2025-05-02 01:02:19 +0900

    Initial Docusaurus scaffold (`classic` template with typescript)

---
+++ apps/design-system-docs/src/pages/index.tsx
@@ -0,0 +1,42 @@
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import styles from './index.module.css';
+import type { ReactNode } from 'react';
+
+function HomepageHeader() {
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={ clsx( 'hero hero--primary', styles.heroBanner ) }>
+			<div className="container">
+				<Heading as="h1" className="hero__title">
+					{ siteConfig.title }
+				</Heading>
+				<p className="hero__subtitle">{ siteConfig.tagline }</p>
+				<div className={ styles.buttons }>
+					<Link className="button button--secondary button--lg" to="/docs/intro">
+						Docusaurus Tutorial - 5min ⏱️
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default function Home(): ReactNode {
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<Layout
+			title={ `Hello from ${ siteConfig.title }` }
+			description="Description will go into a meta tag in <head />"
+		>
+			<HomepageHeader />
+			<main>
+				<HomepageFeatures />
+			</main>
+		</Layout>
+	);
+}
