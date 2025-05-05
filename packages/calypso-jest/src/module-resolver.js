8b73a2d5b056ab0f7a6e0136f914668a737eabe8
Jarda Snajdr <jsnajdr@gmail.com>
2025-05-05 10:37:25 +0200

    Jest resolver config: add exception for parsel-js

--- packages/calypso-jest/src/module-resolver.js
+++ packages/calypso-jest/src/module-resolver.js
@@ -1,4 +1,11 @@
 module.exports = function ( request, options ) {
-	const conditions = options.conditions && [ 'calypso:src', ...options.conditions ];
+	let conditions = options.conditions && [ 'calypso:src', ...options.conditions ];
+
+	// The `parsel-js` package has a `browser` condition in `exports` field that points to an ESM module,
+	// but the condition is matched also when requiring a CJS module.
+	if ( request === 'parsel-js' && conditions ) {
+		conditions = conditions.filter( ( c ) => c !== 'browser' );
+	}
+
 	return options.defaultResolver( request, { ...options, conditions } );
 };
