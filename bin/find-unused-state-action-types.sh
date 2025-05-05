18f84eb6a0cbe45ec96877bac1cf4e26d72cfd62
Andrew Duthie <andrew.duthie@automattic.com>
2025-05-05 14:34:16 -0400

    Simplify grep to avoid including file name in result

--- bin/find-unused-state-action-types.sh
+++ bin/find-unused-state-action-types.sh
@@ -4,8 +4,8 @@
 # they are expected to be used. It does so by grepping for all action types in the action-types.ts
 # file and then checking if any relevant files in the client/state directory use that action type.
 
-grep -r "export const [A-Z_]* =" client/state/action-types.ts | \
-    sed -E 's/.*export const ([A-Z_]+) =.*/\1/' | \
+grep "export const [A-Z_]* =" client/state/action-types.ts | \
+    sed -E 's/export const ([A-Z_]+) =.*/\1/' | \
     xargs -n 1 -P 8 -I{} sh -c \
     'grep -rq --include="*/actions/*" \
                 --include="reducer.js" \
