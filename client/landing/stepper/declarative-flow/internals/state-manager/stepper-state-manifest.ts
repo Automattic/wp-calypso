0f3b5bf0bf0389347514eb4eb4e1b5ef927e5651
Omar Alshaker <omar@omaralshaker.com>
2025-05-05 18:48:16 +0200

    Remove hardcoded step types

--- client/landing/stepper/declarative-flow/internals/state-manager/stepper-state-manifest.ts
+++ client/landing/stepper/declarative-flow/internals/state-manager/stepper-state-manifest.ts
@@ -1,5 +1,4 @@
-import type { DomainStepResult, PlansStepResult } from './types';
-import type { SiteDetails } from '@automattic/data-stores';
+import type { StepperStepsUnionedType } from '../types';
 import type { createSite } from 'calypso/landing/stepper/hooks/use-create-site-hook';
 
 type CreatedSite = Awaited< ReturnType< typeof createSite > >;
@@ -7,27 +6,11 @@ type CreatedSite = Awaited< ReturnType< typeof createSite > >;
 /**
  * The manifest of the state of all available state fields in Stepper. Feel free to type and add all the fields you need.
  */
-export type FlowStateManifest = Partial< {
-	domains: DomainStepResult;
+export type StepperMiscellaneousFields = Partial< {
 	flow: {
 		entryPoint: string;
 	};
-	plans: PlansStepResult;
-	newsletterSetup: {
-		siteTitle: string;
-		tagline: string;
-	};
-	newsletterGoals: {
-		goals: string[];
-	};
 	site: CreatedSite;
-	siteId: SiteDetails[ 'ID' ];
-	siteSlug: SiteDetails[ 'slug' ];
-	processing: {
-		siteId: number;
-		siteSlug: string;
-		domainItem?: DomainStepResult[ 'domainItem' ];
-		goToCheckout?: true;
-		goToHome?: true;
-	};
 } >;
+
+export type FlowStateManifest = StepperMiscellaneousFields & StepperStepsUnionedType;
