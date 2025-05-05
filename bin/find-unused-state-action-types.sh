39f8357113868dc65cc89af311928938a4a4988a
Andrew Duthie <andrew.duthie@automattic.com>
2025-05-05 14:41:32 -0400

    Consolidate extension matching for JS, TS
    Help readability
    
    Related: https://github.com/Automattic/wp-calypso/pull/102908/files#r2068252242

--- bin/find-unused-state-action-types.sh
+++ bin/find-unused-state-action-types.sh
@@ -13,13 +13,9 @@ grep "export const [A-Z_]* =" client/state/action-types.ts | \
     sed -E 's/export const ([A-Z_]+) =.*/\1/' | \
     xargs -n 1 -P 8 -I{} sh -c \
     'grep -rq --include="*/actions/*" \
-                --include="reducer.js" \
-                --include="reducer.ts" \
-                --include="actions.js" \
-                --include="actions.ts" \
-                --include="index.js" \
-                --include="index.ts" \
-                --include="middleware.js" \
-                --include="middleware.ts" \
+                --include="reducer.[jt]s" \
+                --include="actions.[jt]s" \
+                --include="index.[jt]s" \
+                --include="middleware.[jt]s" \
                 --include="reducer-utils.ts" \
                 --exclude-dir="test" "$1" client/state || (echo "Unexpected unused action type: $1"; exit 1)' _ {}
