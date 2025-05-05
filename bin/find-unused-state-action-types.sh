827e8f2fbbb7623bebbb265524083751e59e023b
Andrew Duthie <andrew.duthie@automattic.com>
2025-05-05 14:31:40 -0400

    Include comment explaining purpose of script
    See: https://github.com/Automattic/wp-calypso/pull/102908/files#r2068249168

--- bin/find-unused-state-action-types.sh
+++ bin/find-unused-state-action-types.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This script checks to ensure that all defined Redux action types are being used in files where
+# they are expected to be used. It does so by grepping for all action types in the action-types.ts
+# file and then checking if any relevant files in the client/state directory use that action type.
+
 grep -r "export const [A-Z_]* =" client/state/action-types.ts | \
     sed -E 's/.*export const ([A-Z_]+) =.*/\1/' | \
     xargs -n 1 -P 8 -I{} sh -c \
