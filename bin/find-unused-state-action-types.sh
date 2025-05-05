e95a8a265b6c19f9b139b243f92e06e1d52721da
Andrew Duthie <andrew.duthie@automattic.com>
2025-05-05 14:36:23 -0400

    Fail command if file not found
    See: https://github.com/Automattic/wp-calypso/pull/102908/files#r2068250538

--- bin/find-unused-state-action-types.sh
+++ bin/find-unused-state-action-types.sh
@@ -4,6 +4,11 @@
 # they are expected to be used. It does so by grepping for all action types in the action-types.ts
 # file and then checking if any relevant files in the client/state directory use that action type.
 
+if [ ! -f "client/state/action-types.ts" ]; then
+    echo "Action types file not found"
+    exit 1
+fi
+
 grep "export const [A-Z_]* =" client/state/action-types.ts | \
     sed -E 's/export const ([A-Z_]+) =.*/\1/' | \
     xargs -n 1 -P 8 -I{} sh -c \
