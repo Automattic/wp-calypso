e8a19860beda07433ec0e38584f1babb871b5df6
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-05-05 20:57:53 +0700

    Dashboard v2: code restructure

---
+++ client/dashboard/app/404/index.tsx
@@ -0,0 +1,19 @@
+import { __ } from '@wordpress/i18n';
+import PageLayout from '../../components/page-layout';
+import RouterLinkButton from '../../components/router-link-button';
+
+function NotFound() {
+	return (
+		<PageLayout
+			title={ __( '404 Not Found' ) }
+			description={ __( 'The page you are looking for does not exist.' ) }
+			actions={
+				<RouterLinkButton to="/sites" variant="primary" __next40pxDefaultSize>
+					{ __( 'Go to Sites' ) }
+				</RouterLinkButton>
+			}
+		/>
+	);
+}
+
+export default NotFound;
---
+++ client/dashboard/me/payment-methods/index.tsx
@@ -0,0 +1,10 @@
+import { __ } from '@wordpress/i18n';
+import PageLayout from '../../components/page-layout';
+
+export default function PaymentMethods() {
+	return (
+		<PageLayout title={ __( 'Payment Methods' ) }>
+			<div>Payment methods content will go here</div>
+		</PageLayout>
+	);
+}
