0a8b2197c44c0a0c7ff6db7a40b6acd4726adea4
Lena Morita <lena@jaguchi.com>
2025-05-06 13:44:16 +0900

    Include mdx files

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -2,7 +2,7 @@ const storybookDefaultConfig = require( '@automattic/calypso-storybook' );
 
 module.exports = {
 	...storybookDefaultConfig( {
-		stories: [ '../{src,styles}/**/*.stories.{js,jsx,ts,tsx}' ],
+		stories: [ '../{src,styles}/**/*.stories.{js,jsx,ts,tsx}', './**/*.mdx' ],
 	} ),
 	docs: { autodocs: true },
 };
