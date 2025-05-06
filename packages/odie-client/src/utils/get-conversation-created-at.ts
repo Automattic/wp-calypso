a2f0eb62cd120b3fb7b424e7e4689dd7f84f5ef3
Stéphane Thomas <stephanethomas@automattic.com>
2025-05-06 15:58:56 +0200

    Refine comments

--- packages/odie-client/src/utils/get-conversation-created-at.ts
+++ packages/odie-client/src/utils/get-conversation-created-at.ts
@@ -3,7 +3,7 @@ import type { ZendeskConversation, OdieConversation } from '../types';
 /**
  * Retrieves the creation date of the specified conversation.
  *
- * @returns The timestamp in milliseconds, or undefined if not available
+ * @returns The timestamp in milliseconds (e.g. 1745936539027), or undefined if not available
  */
 export function getConversationCreatedAt(
 	conversation: ZendeskConversation | OdieConversation
@@ -11,7 +11,7 @@ export function getConversationCreatedAt(
 	if ( 'metadata' in conversation && conversation.metadata?.createdAt ) {
 		return conversation.metadata.createdAt; // Format of this field is '1745936539027'
 	} else if ( 'createdAt' in conversation && conversation.createdAt ) {
-		return conversation.createdAt * 1000; // Format of this field is '1745936542.513'
+		return conversation.createdAt * 1000; // Format of this field is '1745936539.027'
 	}
 
 	return undefined;
--- packages/odie-client/src/utils/get-timestamp.ts
+++ packages/odie-client/src/utils/get-timestamp.ts
@@ -2,10 +2,10 @@
  * Converts the specified date string to a timestamp.
  *
  * @param date A date formatted as 'YYYY-MM-DD HH:MM:SS' (e.g. '2025-04-29 14:21:38')
- * @returns The timestamp in seconds (e.g. 1714395698)
+ * @returns The timestamp in seconds (e.g. 1745936539)
  */
 export function getTimestamp( date: string ): number {
-	// Replaces the space with 'T' and append 'Z' to indicate UTC time
+	// Ensures the date string is in ISO 8601 format for reliable parsing across browsers
 	const ts = Date.parse( date.replace( ' ', 'T' ) + 'Z' );
 
 	return Math.floor( ts / 1000 );
