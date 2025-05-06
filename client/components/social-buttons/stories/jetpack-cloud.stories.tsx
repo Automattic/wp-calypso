6c166d083f9be36e0249d70faaa78919651fe145
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2025-05-07 10:50:01 +1200

    Fix Jetpack login colors and add a social button story

--- client/components/social-buttons/stories/jetpack-cloud.stories.tsx
+++ client/components/social-buttons/stories/jetpack-cloud.stories.tsx
@@ -1,10 +1,16 @@
-import { appleStory, AuthFormSocial, gitHubStory, googleStory, JetpackWrapper } from './shared';
+import {
+	appleStory,
+	AuthFormSocial,
+	gitHubStory,
+	googleStory,
+	JetpackCloudWrapper,
+} from './shared';
 import type { Meta } from '@storybook/react';
 import '../../../layout/masterbar/oauth-client.scss';
 
 const meta: Meta = {
 	title: 'client/components/Social Button/Jetpack Cloud',
-	decorators: [ AuthFormSocial, JetpackWrapper ],
+	decorators: [ AuthFormSocial, JetpackCloudWrapper ],
 };
 
 export default meta;
