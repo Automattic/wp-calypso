00526020af1bc691ee21ed7662a9ea5ea7afc7ba
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-05-07 10:55:57 +0200

    Remove unnecessary routing handlers

--- client/gutenberg/editor/index.js
+++ client/gutenberg/editor/index.js
@@ -20,9 +20,7 @@ export default function () {
 		siteSelection,
 		redirect,
 		authenticate,
-		redirectPostEditor,
-		makeLayout,
-		clientRender
+		redirectPostEditor
 	);
 	page.exit( '/post/:site?/:post?', exitPost );
 	page( '/post/:site?', redirectLoggedOut, siteSelection, redirect, makeLayout, clientRender );
@@ -35,9 +33,7 @@ export default function () {
 		siteSelection,
 		redirect,
 		authenticate,
-		redirectPostEditor,
-		makeLayout,
-		clientRender
+		redirectPostEditor
 	);
 	page.exit( '/page/:site?/:post?', exitPost );
 	page( '/page/:site?', redirectLoggedOut, siteSelection, redirect, makeLayout, clientRender );
@@ -56,9 +52,7 @@ export default function () {
 		siteSelection,
 		redirect,
 		authenticate,
-		redirectPostEditor,
-		makeLayout,
-		clientRender
+		redirectPostEditor
 	);
 	page(
 		'/edit/:customPostType/:site?',
