07dfe45a9afee14887d98602f871674079885e8f
Noam Almosnino <noamalmosnino@gmail.com>
2025-05-08 11:21:42 +0100

    Quick layout fix

--- client/a8c-for-agencies/sections/reports/reports-component.tsx
+++ client/a8c-for-agencies/sections/reports/reports-component.tsx
@@ -1,11 +1,21 @@
 import { useTranslate } from 'i18n-calypso';
+import { LayoutWithGuidedTour as Layout } from 'calypso/a8c-for-agencies/components/layout/layout-with-guided-tour';
+import LayoutBody from 'calypso/layout/hosting-dashboard/body';
+
+import './style.scss';
 
 export default function ReportsComponent() {
 	const translate = useTranslate();
+	const title = translate( 'Reports' );
+
 	return (
-		<div style={ { padding: 32 } }>
-			<h1>{ translate( 'Reports' ) }</h1>
-			<p>{ translate( 'This is the Reports section. Content coming soon.' ) }</p>
-		</div>
+		<Layout title={ title } wide>
+			<LayoutBody className="a4a-reports-content">
+				<div style={ { padding: 32 } }>
+					<h1>{ title }</h1>
+					<p>{ translate( 'This is the Reports section. Content coming soon.' ) }</p>
+				</div>
+			</LayoutBody>
+		</Layout>
 	);
 }
