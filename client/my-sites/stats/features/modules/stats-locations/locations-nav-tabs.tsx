71fdf2a3e379fd69647e240441ac4df80148a142
Jasper Kang <jasper.kang@automattic.com>
2025-05-08 15:26:31 +1200

    add navtabs for locations

---
+++ client/my-sites/stats/features/modules/stats-locations/locations-nav-tabs.tsx
@@ -0,0 +1,77 @@
+import page from '@automattic/calypso-router';
+import { TabPanel } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+import { useMemo } from 'react';
+import {
+	STATS_FEATURE_LOCATION_REGION_VIEWS,
+	STATS_FEATURE_LOCATION_COUNTRY_VIEWS,
+	STATS_FEATURE_LOCATION_CITY_VIEWS,
+} from '../../../constants';
+
+const OPTION_KEYS = {
+	COUNTRIES: 'countries',
+	REGIONS: 'regions',
+	CITIES: 'cities',
+};
+
+function LocationsNavTabs( { period, query, givenSiteId } ) {
+	const translate = useTranslate();
+	const tabPanelTabs = useMemo( () => {
+		const optionLabels = {
+			[ OPTION_KEYS.COUNTRIES ]: {
+				selectLabel: translate( 'Countries' ),
+				headerLabel: translate( 'Top countries' ),
+				analyticsId: 'countries',
+				feature: STATS_FEATURE_LOCATION_COUNTRY_VIEWS,
+				countryFilterLabel: translate( 'All countries' ),
+			},
+			[ OPTION_KEYS.REGIONS ]: {
+				selectLabel: translate( 'Regions' ),
+				headerLabel: translate( 'Top regions' ),
+				analyticsId: 'regions',
+				feature: STATS_FEATURE_LOCATION_REGION_VIEWS,
+				countryFilterLabel: translate( 'All regions' ),
+			},
+			[ OPTION_KEYS.CITIES ]: {
+				selectLabel: translate( 'Cities' ),
+				headerLabel: translate( 'Top cities' ),
+				analyticsId: 'cities',
+				feature: STATS_FEATURE_LOCATION_CITY_VIEWS,
+				countryFilterLabel: translate( 'All cities' ),
+			},
+		};
+		return Object.entries( optionLabels ).map( ( [ key, item ] ) => {
+			const updateQuery = { ...query, geoMode: key };
+
+			return {
+				name: key,
+				title: item.selectLabel,
+				className: `stats-navigation__${ key }`,
+				path: `/stats/${ period.period || 'day' }/locations/${ givenSiteId }?${ new URLSearchParams(
+					updateQuery
+				).toString() }`,
+			};
+		} );
+	}, [] );
+
+	const selectedTab = query.geoMode || OPTION_KEYS.COUNTRIES;
+
+	return (
+		<TabPanel
+			className="stats-navigation__tabs"
+			tabs={ tabPanelTabs }
+			initialTabName={ selectedTab }
+			onSelect={ ( tabName ) => {
+				// TODO add analytics tracking here.
+				const tab = tabPanelTabs.find( ( tab ) => tab.name === tabName );
+				if ( tab?.path ) {
+					page( tab.path );
+				}
+			} }
+		>
+			{ () => null /* Placeholder div since content is rendered elsewhere */ }
+		</TabPanel>
+	);
+}
+
+export default LocationsNavTabs;
--- client/my-sites/stats/summary/index.jsx
+++ client/my-sites/stats/summary/index.jsx
@@ -21,6 +21,7 @@ import getEnvStatsFeatureSupportChecks from 'calypso/state/sites/selectors/get-e
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import PageHeader from '../components/headers/page-header';
 import StatsModuleLocations from '../features/modules/stats-locations';
+import LocationsTabNav from '../features/modules/stats-locations/locations-nav-tabs';
 import StatsModuleUTM from '../features/modules/stats-utm';
 import { StatsGlobalValuesContext } from '../pages/providers/global-provider';
 import DownloadCsv from '../stats-download-csv';
@@ -393,6 +394,15 @@ class StatsSummary extends Component {
 					{ ! isStatsNavigationImprovementEnabled && (
 						<NavigationHeader className="stats-summary-view" navigationItems={ navigationItems } />
 					) }
+
+					{ isStatsNavigationImprovementEnabled &&
+						this.props.context.params.module === 'locations' && (
+							<LocationsTabNav
+								period={ this.props.period }
+								query={ moduleQuery }
+								givenSiteId={ siteId }
+							/>
+						) }
 
 					<div id="my-stats-content" className="stats-summary-view stats-summary__positioned">
 						{ this.props.context.params.module === 'utm' ? (
