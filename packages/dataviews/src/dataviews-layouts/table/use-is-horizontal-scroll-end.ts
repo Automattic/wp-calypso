9ac58e721351b85d9e095e90e748debec55db150
arthur <arthur.chu@automattic.com>
2025-05-08 14:49:33 +0800

    Add notes to describe the limitation

--- packages/dataviews/src/dataviews-layouts/table/use-is-horizontal-scroll-end.ts
+++ packages/dataviews/src/dataviews-layouts/table/use-is-horizontal-scroll-end.ts
@@ -16,6 +16,11 @@ const isScrolledToEnd = ( element: Element ) => {
 
 /**
  * A hook to check if a given scroll container has reached the horizontal scroll end.
+ *
+ * The current way receives "refs" as arguments, but it lacks a mechanism to detect when a ref has changed.
+ * As a result, when the "ref" is updated and attached to a new div, the computation should trigger again.
+ * However, this isn't possible in the current setup because the hook is unaware that the ref has changed.
+ * See https://github.com/Automattic/wp-calypso/pull/103005#discussion_r2077567912.
  */
 export function useIsHorizontalScrollEnd( {
 	scrollContainerRef,
@@ -24,7 +29,8 @@ export function useIsHorizontalScrollEnd( {
 	scrollContainerRef: React.MutableRefObject< HTMLDivElement | null >;
 	enabled?: boolean;
 } ): boolean {
-	const [ isHorizontalScrollEnd, setIsHorizontalScrollEnd ] = useState( false );
+	const [ isHorizontalScrollEnd, setIsHorizontalScrollEnd ] =
+		useState( false );
 
 	const handleIsHorizontalScrollEnd = useDebounce(
 		useCallback( () => {
