a024186271ebe25f4c22c0225fd026a23b2eeba6
Copons <Copons@users.noreply.github.com>
2025-05-08 18:24:45 +0100

    Add an affordance for copying the password

--- client/me/application-passwords/new-password.jsx
+++ client/me/application-passwords/new-password.jsx
@@ -1,13 +1,54 @@
+import { Button } from '@automattic/components';
+import { Icon, check, copy } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
 import FormButton from 'calypso/components/forms/form-button';
 import FormButtonsBar from 'calypso/components/forms/form-buttons-bar';
+import { useDispatch } from 'calypso/state';
+import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 
 export default function NewAppPassword( { appName, newAppPassword, onClickDone } ) {
+	const dispatch = useDispatch();
 	const translate = useTranslate();
+
+	const [ copyStatus, setCopyStatus ] = useState( '' );
+
+	const handleCopyStatus = ( status ) => {
+		setCopyStatus( status );
+		setTimeout( () => {
+			setCopyStatus( '' );
+		}, 2000 );
+	};
+
+	const copyToClipboard = () => {
+		navigator.clipboard
+			.writeText( newAppPassword )
+			.then( () => {
+				handleCopyStatus( 'success' );
+				dispatch(
+					successNotice( translate( 'Application password copied to clipboard.' ), {
+						duration: 2000,
+					} )
+				);
+			} )
+			.catch( () => {
+				handleCopyStatus( 'error' );
+				dispatch(
+					errorNotice( translate( 'There was a problem copying the password.' ), {
+						duration: 2000,
+					} )
+				);
+			} );
+	};
 
 	return (
 		<div className="application-passwords__new-password">
-			<p className="application-passwords__new-password-display">{ newAppPassword }</p>
+			<p className="application-passwords__new-password-display">
+				<>{ newAppPassword }</>
+				<Button compact onClick={ copyToClipboard }>
+					<Icon icon={ 'success' === copyStatus ? check : copy } />
+				</Button>
+			</p>
 
 			<p className="application-passwords__new-password-help">
 				{ translate(
