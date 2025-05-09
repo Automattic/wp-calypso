75c23e86a25f9b6b0e5a65ce921098bcc59ba1a6
Miroslav Mitev <m1r0@users.noreply.github.com>
2025-05-09 16:52:22 +0300

    Remove the "Share your finished site" paragraph

--- client/my-sites/marketing/do-it-for-me/service-description.tsx
+++ client/my-sites/marketing/do-it-for-me/service-description.tsx
@@ -130,13 +130,6 @@ export const DIFMServiceDescription = ( { isStoreFlow }: { isStoreFlow: boolean 
 					}
 				/>
 			</VerticalStepProgress>
-			<p css={ { fontSize: '0.875rem', lineHeight: 1.5, color: 'var( --color-text )' } }>
-				{ translate( 'Share your finished site with the world in %(days)d business days or less!', {
-					args: {
-						days: 4,
-					},
-				} ) }
-			</p>
 		</ServiceDescription>
 	);
 };
