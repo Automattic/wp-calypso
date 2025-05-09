dd694f21db8a5028f5d26f6d7822c53a4714769f
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-05-09 10:45:48 +0200

    Remove unused `requestPostRevisions`

--- client/state/posts/revisions/actions.js
+++ client/state/posts/revisions/actions.js
@@ -10,27 +10,6 @@ import 'calypso/state/data-layer/wpcom/posts/revisions';
 import 'calypso/state/posts/init';
 
 /**
- * Action creator function: POST_REVISIONS_REQUEST
- * @param {string} siteId of the revisions
- * @param {string} postId of the revisions
- * @param {string} postType of the parent post
- * @param {string} [comparisons] list of revision objects to compare in format:
- * 					[
- * 						{ from: 6, to: 8 },
- * 						{ from: 4, to: 5 },
- * 					]
- * 					Optional. If not provided, the API will return a set of sequential diffs
- * @returns {Object} action object
- */
-export const requestPostRevisions = ( siteId, postId, postType = 'posts', comparisons = [] ) => ( {
-	type: POST_REVISIONS_REQUEST,
-	comparisons,
-	postId,
-	postType,
-	siteId,
-} );
-
-/**
  * Action creator function: POST_REVISIONS_RECEIVE
  */
 export const receivePostRevisions = ( { diffs, postId, revisions, revision_fields, siteId } ) => ( {
