ff08e52b7589f8662e701b867561b4291c8c43f4
Daniel Lind <daniel.lind@automattic.com>
2025-05-09 12:53:20 +0200

    Adding a temp string for testing string freeze

--- client/sites/marketing/tools/tools-translation-strings.js
+++ client/sites/marketing/tools/tools-translation-strings.js
@@ -3,3 +3,5 @@ import { translate } from 'i18n-calypso';
 translate(
 	"We've added premium plugins to boost your site's capabilities. From bookings and subscriptions to email marketing and SEO tools, we have you covered."
 );
+
+translate( 'Having trouble? {{button}}Let us migrate your site{{/button}}' );
