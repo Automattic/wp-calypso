c2fa01c435da3d29141df59d8bb94da1cf82c7c8
ashfame <mail@ashfame.com>
2025-05-12 19:23:03 +0400

    correct plan intent names

--- client/landing/stepper/declarative-flow/internals/steps-repository/playground/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/playground/index.tsx
@@ -49,11 +49,11 @@ export const PlaygroundStep: StepType = ( { navigation, flow } ) => {
 				require_once( '/wordpress/wp-load.php' );
 				$active_plugins = array_diff( get_option( 'active_plugins' ), array( 'hello.php', 'akismet/akismet.php' ) );
 				if ( in_array( 'woocommerce/woocommerce.php', $active_plugins ) ) {
-					echo 'woocommerce';
+					echo 'plans-playground';
 					return;
 				}
 				if ( count( $active_plugins ) > 0 ) {
-					echo 'business';
+					echo 'plans-playground';
 					return;
 				}
 				$has_user_global_styles = count( get_posts( array(
@@ -61,7 +61,7 @@ export const PlaygroundStep: StepType = ( { navigation, flow } ) => {
 					'post_status' => 'publish',
 					'posts_per_page' => 1,
 				) ) ) > 0;
-				echo $has_user_global_styles ? 'global-styles' : 'simple';`,
+				echo $has_user_global_styles ? 'plans-playground-premium' : 'plans-default-wpcom';`,
 			} )
 			.then( ( res: { text: string } ) => {
 				setPgIntent( res.text );
