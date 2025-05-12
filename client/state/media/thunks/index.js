1a9ef41bafae4f5915189af8ec5fbcba9c8c861e
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2025-05-12 11:31:03 +0300

    Calypsoify: Cleanup iframePort and isIframeLoaded remnants

--- client/state/media/thunks/index.js
+++ client/state/media/thunks/index.js
@@ -1,3 +1,2 @@
 export { fetchNextMediaPage } from './fetch-next-media-page';
 export { default as getMediaItem } from './get-media-item';
-export { gutenframeUpdateImageBlocks } from './gutenframe-update-image-blocks';
