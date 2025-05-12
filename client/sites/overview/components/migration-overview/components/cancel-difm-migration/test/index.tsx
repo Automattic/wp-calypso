98f2e4e6b99bad5ddfe6b148e964c2a2ea805e86
Mark Biek <mark.biek@automattic.com>
2025-05-12 17:26:42 -0400

    Make DIFM migration cancellation context aware
    We now display slightly different context depending on if the site has a
    migration in progress.
    
    If the site doesn't have a migration in progress (no
    migration-in-progress sticker), the user is allowed to cancel outright.
    
    Otherwise they are told they're sending a migration cancellation
    request.

--- client/sites/overview/components/migration-overview/components/cancel-difm-migration/test/index.tsx
+++ client/sites/overview/components/migration-overview/components/cancel-difm-migration/test/index.tsx
@@ -23,32 +23,77 @@ jest.mock( 'calypso/lib/wp', () => ( {
 	},
 } ) );
 
+jest.mock( 'calypso/blocks/blog-stickers/use-blog-stickers-query', () => ( {
+	useBlogStickersQuery: jest.fn(),
+} ) );
+
 describe( 'CancelDifmMigrationForm', () => {
 	const siteId = 123;
 	const isEnabled = jest.requireMock( '@automattic/calypso-config' ).isEnabled;
+	const useBlogStickersQuery = jest.requireMock(
+		'calypso/blocks/blog-stickers/use-blog-stickers-query'
+	).useBlogStickersQuery;
 
 	beforeEach( () => {
 		nock.cleanAll();
 		jest.clearAllMocks();
 		isEnabled.mockReturnValue( true );
+		useBlogStickersQuery.mockReturnValue( { data: [] } );
 	} );
 
 	it( 'renders nothing if feature flag is disabled', () => {
 		isEnabled.mockReturnValue( false );
 		renderWithProvider( <CancelDifmMigrationForm siteId={ siteId } /> );
-		expect( screen.queryByRole( 'button', { name: /request cancellation/i } ) ).toBeNull();
+		expect( screen.queryByRole( 'button', { name: /cancel migration/i } ) ).toBeNull();
 	} );
 
-	it( 'opens dialog on button click', async () => {
-		renderWithProvider( <CancelDifmMigrationForm siteId={ siteId } /> );
-		await userEvent.click( screen.getByRole( 'button', { name: /request cancellation/i } ) );
-		expect( screen.getByRole( 'dialog' ) ).toBeVisible();
-		expect( screen.getByText( /request migration cancellation/i ) ).toBeVisible();
+	describe( 'when migration is not in progress', () => {
+		beforeEach( () => {
+			useBlogStickersQuery.mockReturnValue( { data: [] } );
+		} );
+
+		it( 'shows cancel migration button', () => {
+			renderWithProvider( <CancelDifmMigrationForm siteId={ siteId } /> );
+			expect( screen.getByRole( 'button', { name: /cancel migration/i } ) ).toBeVisible();
+		} );
+
+		it( 'opens dialog with cancel migration content', async () => {
+			renderWithProvider( <CancelDifmMigrationForm siteId={ siteId } /> );
+			await userEvent.click( screen.getByRole( 'button', { name: /cancel migration/i } ) );
+			expect( screen.getByRole( 'dialog' ) ).toBeVisible();
+			expect( screen.getByRole( 'heading', { name: /cancel migration/i } ) ).toBeVisible();
+			expect( screen.getByText( /our Happiness Engineers will be notified/i ) ).toBeVisible();
+		} );
+	} );
+
+	describe( 'when migration is in progress', () => {
+		beforeEach( () => {
+			useBlogStickersQuery.mockReturnValue( { data: [ 'migration-in-progress' ] } );
+		} );
+
+		it( 'shows request cancellation button', () => {
+			renderWithProvider( <CancelDifmMigrationForm siteId={ siteId } /> );
+			expect(
+				screen.getByRole( 'button', { name: /request migration cancellation/i } )
+			).toBeVisible();
+		} );
+
+		it( 'opens dialog with request cancellation content', async () => {
+			renderWithProvider( <CancelDifmMigrationForm siteId={ siteId } /> );
+			await userEvent.click(
+				screen.getByRole( 'button', { name: /request migration cancellation/i } )
+			);
+			expect( screen.getByRole( 'dialog' ) ).toBeVisible();
+			expect(
+				screen.getByRole( 'heading', { name: /request migration cancellation/i } )
+			).toBeVisible();
+			expect( screen.getByText( /you'll lose all your progress/i ) ).toBeVisible();
+		} );
 	} );
 
 	it( 'closes dialog on cancel', async () => {
 		renderWithProvider( <CancelDifmMigrationForm siteId={ siteId } /> );
-		await userEvent.click( screen.getByRole( 'button', { name: /request cancellation/i } ) );
+		await userEvent.click( screen.getByRole( 'button', { name: /cancel migration/i } ) );
 		await userEvent.click( screen.getByRole( 'button', { name: /don't cancel migration/i } ) );
 		await waitFor( () => {
 			expect( screen.queryByRole( 'dialog' ) ).toBeNull();
