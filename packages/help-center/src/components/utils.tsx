7bf8a055118d0562b2b1a007f9d32ae1bc6128fe
Stéphane Thomas <stephanethomas@automattic.com>
2025-05-12 17:10:29 +0200

    Remove function no longer used

--- packages/help-center/src/components/utils.tsx
+++ packages/help-center/src/components/utils.tsx
@@ -55,68 +55,6 @@ export const getZendeskConversations = () => {
 	return conversations as unknown as ZendeskConversation[];
 };
 
-export const getSortedRecentAndArchivedConversations = ( {
-	conversations,
-}: {
-	conversations: ZendeskConversation[];
-} ) => {
-	const recentConversations: ZendeskConversation[] = [];
-	const archivedConversations: ZendeskConversation[] = [];
-
-	if ( Array.isArray( conversations ) ) {
-		conversations.forEach( ( conversation: ZendeskConversation ) => {
-			if ( ! conversation?.metadata?.createdAt ) {
-				recentConversations.push( conversation );
-				return;
-			}
-
-			const createdAt = conversation.metadata?.createdAt;
-			const createdAtDate = new Date( createdAt as string | number | Date );
-			const now = new Date();
-			const oneYearAgo = new Date( now.setFullYear( now.getFullYear() - 1 ) );
-
-			if ( createdAtDate < oneYearAgo ) {
-				archivedConversations.push( conversation );
-			} else {
-				recentConversations.push( conversation );
-			}
-		} );
-	}
-
-	if ( recentConversations.length > 0 ) {
-		recentConversations.sort( ( a, b ) => {
-			const aUnreadCount = a?.participants[ 0 ]?.unreadCount ?? 0;
-			const bUnreadCount = b?.participants[ 0 ]?.unreadCount ?? 0;
-			const aLastMessage = getLastMessage( { conversation: a } );
-			const bLastMessage = getLastMessage( { conversation: b } );
-
-			if ( aUnreadCount < bUnreadCount ) {
-				if ( aUnreadCount === 0 || bUnreadCount === 0 ) {
-					return 1;
-				}
-				if ( aLastMessage === null || bLastMessage === null ) {
-					return aLastMessage === null ? 1 : -1;
-				}
-				return aLastMessage < bLastMessage ? 1 : -1;
-			} else if ( aUnreadCount > bUnreadCount ) {
-				if ( aUnreadCount === 0 || bUnreadCount === 0 ) {
-					return -1;
-				}
-				if ( aLastMessage === null || bLastMessage === null ) {
-					return aLastMessage === null ? 1 : -1;
-				}
-				return aLastMessage < bLastMessage ? -1 : 1;
-			}
-			return 0;
-		} );
-	}
-
-	return {
-		recentConversations,
-		archivedConversations,
-	};
-};
-
 export const getClientId = ( conversations: ZendeskConversation[] ): string =>
 	conversations
 		.flatMap( ( conversation ) => conversation.messages )
