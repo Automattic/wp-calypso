0691d50d6810f9327b5d0cae76bfb19be5901b34
Nikhil <nikhil.chavan@automattic.com>
2025-05-13 17:30:50 +0530

    STATS-67: Don't reload page if current tab is already the selected tab

--- client/blocks/stats-navigation/index.js
+++ client/blocks/stats-navigation/index.js
@@ -141,12 +141,14 @@ const TabNav = ( { validNavItems, interval, slugPath, adminUrl, selectedItem, sh
 			className="stats-navigation__tabs"
 			tabs={ tabs }
 			onSelect={ ( tabName ) => {
-				const tab = tabs.find( ( { name } ) => name === tabName );
+				if ( tabName !== selectedItem ) {
+					const tab = tabs.find( ( { name } ) => name === tabName );
 
-				if ( tab.name === 'store' && config.isEnabled( 'is_running_in_jetpack_site' ) ) {
-					window.location.href = `${ adminUrl }admin.php?page=wc-admin&path=%2Fanalytics%2Foverview`;
-				} else if ( tab.path ) {
-					page( tab.path );
+					if ( tab.name === 'store' && config.isEnabled( 'is_running_in_jetpack_site' ) ) {
+						window.location.href = `${ adminUrl }admin.php?page=wc-admin&path=%2Fanalytics%2Foverview`;
+					} else if ( tab.path ) {
+						page( tab.path );
+					}
 				}
 			} }
 			initialTabName={ selectedItem }
