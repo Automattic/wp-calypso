4900207b4596239279a03f5b37f4a93b6c235908
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-05-13 19:41:13 +0200

    Add `RemovedFromCartItems`

---
+++ client/my-sites/checkout/src/components/removed-from-cart-items.tsx
@@ -0,0 +1,39 @@
+import { useShoppingCart } from '@automattic/shopping-cart';
+import { Button } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+import { useRestorableProducts } from 'calypso/my-sites/checkout/src/components/restorable-products-context';
+import useCartKey from 'calypso/my-sites/checkout/use-cart-key';
+
+export const RemovedFromCartItems = () => {
+	const cartKey = useCartKey();
+	const { restorableProducts, setRestorableProducts } = useRestorableProducts();
+	const { addProductsToCart } = useShoppingCart( cartKey );
+	const translate = useTranslate();
+
+	if ( ! restorableProducts || restorableProducts.length === 0 ) {
+		return null;
+	}
+
+	return (
+		<ul className="removed-from-cart-items">
+			{ restorableProducts.map( ( product ) => (
+				<li key={ product.uuid }>
+					{ translate( '%(product)s was removed from the shopping cart.', {
+						args: { product: product.product_name },
+					} ) }
+					<Button
+						className="restorable-product-button"
+						onClick={ () => {
+							setRestorableProducts(
+								restorableProducts.filter( ( p ) => p.uuid !== product.uuid )
+							);
+							addProductsToCart( [ product ] );
+						} }
+					>
+						{ translate( 'Restore' ) }
+					</Button>
+				</li>
+			) ) }
+		</ul>
+	);
+};
