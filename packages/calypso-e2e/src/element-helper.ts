9653aff58f61bb3df67cc72e72cbfd1afb5a4e17
Jasper Kang <jasper.kang@automattic.com>
2025-05-14 11:27:44 +1200

    Revert "Fix stats-navigation tab selectors for tests"
    This reverts commit 33a87c77898a05b05f2f93bf45e02a42e46a4796.

--- packages/calypso-e2e/src/element-helper.ts
+++ packages/calypso-e2e/src/element-helper.ts
@@ -1,16 +1,13 @@
 import { Locator, Page, Frame } from 'playwright';
 import envVariables from './env-variables';
 
-const navTabParent = 'div.stats-navigation__tabs';
-const legacyNavTabParent = 'div.section-nav';
+const navTabParent = 'div.section-nav';
 
 const selectors = {
 	// clickNavTab
 	navTabItem: ( { name = '', selected = false }: { name?: string; selected?: boolean } = {} ) =>
-		envVariables.VIEWPORT_NAME === 'mobile'
-			? `${ legacyNavTabParent } a[aria-current="${ selected }"]:has(span:has-text("${ name }"))`
-			: `${ navTabParent } button[aria-selected="${ selected }"]:has-text("${ name }")`,
-	navTabMobileToggleButton: `${ legacyNavTabParent } button.section-nav__mobile-header`,
+		`${ navTabParent } a[aria-current="${ selected }"]:has(span:has-text("${ name }"))`,
+	navTabMobileToggleButton: `${ navTabParent } button.section-nav__mobile-header`,
 };
 
 /**
@@ -106,7 +103,7 @@ export async function clickNavTabBase(
 		const navTabsButtonLocator = page.locator( tabSelectorMobile );
 		await navTabsButtonLocator.click( { noWaitAfter: true } );
 
-		const navTabIsOpenLocator = page.locator( `${ legacyNavTabParent }.is-open` );
+		const navTabIsOpenLocator = page.locator( `${ navTabParent }.is-open` );
 		await navTabIsOpenLocator.waitFor();
 	}
 
