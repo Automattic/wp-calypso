962abe00c5db918e94c2008a8082852cfa53ce11
Claudiu Filip <claudiu.filip@automattic.com>
2025-05-13 14:19:50 +0300

    Restrict assignment to onboarding flows only

--- client/my-sites/plans-features-main/hooks/use-streamlined-price-experiment.ts
+++ client/my-sites/plans-features-main/hooks/use-streamlined-price-experiment.ts
@@ -1,10 +1,20 @@
+import { getFlowFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url';
 import { useExperiment, loadExperimentAssignment } from 'calypso/lib/explat';
+import { getSignupCompleteFlowName } from 'calypso/signup/storageUtils';
 
 const STREAMLINED_PRICE_EXPERIMENT_NAME = 'calypso_streamlined_plans_checkout';
 
 export function useStreamlinedPriceExperiment(): [ boolean, string | null ] {
-	const [ isLoading, assignment ] = useExperiment( STREAMLINED_PRICE_EXPERIMENT_NAME );
-	return [ isLoading, assignment?.variationName ?? null ];
+	const flowFromStorage = getSignupCompleteFlowName(); // The flow for the Checkout page
+	const flowFromURL = getFlowFromURL(); // The flow for the Plans step
+	const flow = flowFromStorage || flowFromURL;
+
+	const [ isLoadingExperiment, assignment ] = useExperiment(
+		STREAMLINED_PRICE_EXPERIMENT_NAME,
+		{ isEligible: flow === 'onboarding' } // Only onboarding flow is eligible for streamlined pricing
+	);
+
+	return [ isLoadingExperiment, assignment?.variationName ?? null ];
 }
 
 export async function isStreamlinedPriceExperiment() {
