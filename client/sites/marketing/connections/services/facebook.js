212bd98fd91d535dfc4de26cd54d8b9f06d8f586
Andrew Duthie <andrew.duthie@automattic.com>
2025-05-14 13:36:35 -0400

    Restore link to support article

--- client/sites/marketing/connections/services/facebook.js
+++ client/sites/marketing/connections/services/facebook.js
@@ -1,3 +1,4 @@
+import { localizeUrl } from '@automattic/i18n-utils';
 import { SharingService, connectFor } from '../service';
 
 export class Facebook extends SharingService {
@@ -14,12 +15,29 @@ export class Facebook extends SharingService {
 	didKeyringConnectionSucceed( availableExternalAccounts ) {
 		if ( availableExternalAccounts.length === 0 ) {
 			this.props.warningNotice(
-				this.props.translate(
-					'The Facebook connection could not be made because this account does not have access to any Pages.',
-					{
-						context: 'Sharing: Jetpack Social connection error',
-					}
-				),
+				<>
+					{ this.props.translate(
+						'The Facebook connection could not be made because this account does not have access to any Pages.',
+						{
+							context: 'Sharing: Jetpack Social connection error',
+						}
+					) }{ ' ' }
+					{ this.props.translate(
+						'Facebook supports Jetpack Social connections to Facebook Pages, but not to Facebook Profiles. ' +
+							'{{a}}Learn More about Jetpack Social for Facebook{{/a}}',
+						{
+							components: {
+								a: (
+									<a
+										href={ localizeUrl( 'https://wordpress.com/support/post-to-facebook/' ) }
+										target="_blank"
+										rel="noopener noreferrer"
+									/>
+								),
+							},
+						}
+					) }
+				</>,
 				{ id: 'publicize' }
 			);
 			this.setState( { isConnecting: false } );
