14750f2db32e6e04b9cfd03e5c1a31e7f8b5de6b
Andrew Duthie <andrew.duthie@automattic.com>
2025-05-14 15:31:16 -0400

    Improve stability of user invites end-to-end test

--- test/e2e/specs/users/invite__new-user.ts
+++ test/e2e/specs/users/invite__new-user.ts
@@ -7,13 +7,11 @@ import {
 	EmailClient,
 	SidebarComponent,
 	AddPeoplePage,
-	InvitePeoplePage,
 	PeoplePage,
 	UserSignupPage,
 	RoleValue,
 	CloseAccountFlow,
 	TestAccount,
-	Roles,
 } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
 
@@ -26,7 +24,6 @@ describe( DataHelper.createSuiteTitle( 'Invite: New User' ), function () {
 		usernamePrefix: 'invited',
 	} );
 
-	let userManagementRevampFeature = false;
 	let acceptInviteLink: string;
 	let page: Page;
 	let invitePage: Page;
@@ -35,63 +32,46 @@ describe( DataHelper.createSuiteTitle( 'Invite: New User' ), function () {
 		page = await browser.newPage();
 	} );
 
-	describe( 'Invite user', function () {
-		let peoplePage: PeoplePage;
-		let sidebarComponent: SidebarComponent;
+	async function navigateToUsers() {
+		const sidebarComponent = new SidebarComponent( page );
+		await sidebarComponent.navigate( 'Users', 'All Users' );
+	}
 
+	async function createInvite() {
+		await navigateToUsers();
+
+		// Create new invite
+		const peoplePage = new PeoplePage( page );
+		await peoplePage.clickAddTeamMember();
+		const addPeoplePage = new AddPeoplePage( page );
+
+		await addPeoplePage.addTeamMember( {
+			email: testUser.email,
+			role: role.toLowerCase() as RoleValue,
+			message: `Test invite for role of ${ role }`,
+		} );
+	}
+
+	describe( 'Invite user', function () {
 		beforeAll( async () => {
 			const testAccount = new TestAccount( invitingUser );
 			await testAccount.authenticate( page );
-
-			userManagementRevampFeature = await page.evaluate(
-				"configData.features['user-management-revamp']"
-			);
-		} );
-
-		it( 'Navigate to Users > All Users', async function () {
-			sidebarComponent = new SidebarComponent( page );
-			await sidebarComponent.navigate( 'Users', 'All Users' );
 		} );
 
 		it( 'Create new invite', async function () {
-			peoplePage = new PeoplePage( page );
-			if ( userManagementRevampFeature ) {
-				await peoplePage.clickAddTeamMember();
-				const addPeoplePage = new AddPeoplePage( page );
+			await createInvite();
 
-				await addPeoplePage.addTeamMember( {
-					email: testUser.email,
-					role: role.toLowerCase() as RoleValue,
-					message: `Test invite for role of ${ role }`,
-				} );
-			} else {
-				await peoplePage.clickInviteUser();
-				const invitePeoplePage = new InvitePeoplePage( page );
-
-				await invitePeoplePage.invite( {
-					email: testUser.email,
-					role: role as Roles,
-					message: `Test invite for role of ${ role }`,
-				} );
-			}
-		} );
-
-		it( 'Confirm invite is pending', async function () {
-			await sidebarComponent.navigate( 'Users', 'All Users' );
-			! userManagementRevampFeature && ( await peoplePage.clickTab( 'Invites' ) );
+			// Confirm invite is pending
+			await navigateToUsers();
+			const peoplePage = new PeoplePage( page );
+			await peoplePage.clickTab( 'Invites' );
 			await peoplePage.selectInvitedUser( testUser.email );
 		} );
 	} );
 
 	describe( 'Accept invite', function () {
-		beforeAll( async () => {
-			// Create a fresh context in the same browser for invite acceptance
-			invitePage = await browser.newContext().then( ( context ) => context.newPage() );
-		} );
-
-		// Keep invitePage open as we'll need it for the close account flow
-
-		it( 'Invite email was received for test user', async function () {
+		async function getInviteLink() {
+			await createInvite();
 			const emailClient = new EmailClient();
 			const message = await emailClient.getLastMatchingMessage( {
 				inboxId: testUser.inboxId,
@@ -101,17 +81,28 @@ describe( DataHelper.createSuiteTitle( 'Invite: New User' ), function () {
 			acceptInviteLink = links.find( ( link: string ) =>
 				link.includes( 'accept-invite' )
 			) as string;
-			expect( acceptInviteLink ).toBeDefined();
+			return acceptInviteLink;
+		}
+
+		it( 'Invite email was received for test user', async function () {
+			const testAccount = new TestAccount( invitingUser );
+			await testAccount.authenticate( page );
+
+			await getInviteLink();
 		} );
 
 		it( 'Sign up as invited user from the invite link', async function () {
+			// Create a fresh context in the same browser for invite acceptance
+			const invitePage = await browser.newContext().then( ( context ) => context.newPage() );
+
+			const acceptInviteLink = await getInviteLink();
+			expect( acceptInviteLink ).toBeDefined();
 			await invitePage.goto( acceptInviteLink );
 
 			const userSignupPage = new UserSignupPage( invitePage );
 			await userSignupPage.signupThroughInvite( testUser.email );
-		} );
 
-		it( 'User sees welcome banner after signup', async function () {
+			// User sees welcome banner after signup
 			// Raw method call & selector used because `PostsPage` is not yet implemented.
 			// TODO: Once PostsPage is implemented, call a method from that
 			// POM instead.
