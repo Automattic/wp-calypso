b2e8492f76c6ba0906a9549932548124f68f85b7
Daniel Lind <daniel.lind@automattic.com>
2025-05-14 20:29:36 +0200

    Refactor the eligibility checks

--- client/my-sites/plans-features-main/hooks/use-streamlined-price-experiment.ts
+++ client/my-sites/plans-features-main/hooks/use-streamlined-price-experiment.ts
@@ -1,20 +1,25 @@
 import { getFlowFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url';
+import isAkismetCheckout from 'calypso/lib/akismet/is-akismet-checkout';
 import { useExperiment, loadExperimentAssignment } from 'calypso/lib/explat';
+import isJetpackCheckout from 'calypso/lib/jetpack/is-jetpack-checkout';
 import { getSignupCompleteFlowName } from 'calypso/signup/storageUtils';
 
 const STREAMLINED_PRICE_EXPERIMENT_NAME = 'calypso_streamlined_plans_checkout';
 
 export function useStreamlinedPriceExperiment(): [ boolean, string | null ] {
+	const [ isLoadingExperiment, assignment ] = useExperiment( STREAMLINED_PRICE_EXPERIMENT_NAME, {
+		isEligible: isEligibleForExperiment(),
+	} );
+
+	return [ isLoadingExperiment, assignment?.variationName ?? null ];
+}
+
+function isEligibleForExperiment(): boolean {
 	const flowFromStorage = getSignupCompleteFlowName(); // The flow for the Checkout page
 	const flowFromURL = getFlowFromURL(); // The flow for the Plans step
 	const flow = flowFromStorage || flowFromURL;
-
-	const [ isLoadingExperiment, assignment ] = useExperiment(
-		STREAMLINED_PRICE_EXPERIMENT_NAME,
-		{ isEligible: flow === 'onboarding' } // Only onboarding flow is eligible for streamlined pricing
-	);
-
-	return [ isLoadingExperiment, assignment?.variationName ?? null ];
+	// Only onboarding flow is eligible for streamlined pricing. Akismet/Jetpack checkouts are excluded as well.
+	return flow === 'onboarding' && ! isAkismetCheckout() && ! isJetpackCheckout();
 }
 
 export async function isStreamlinedPriceExperiment() {
@@ -34,4 +39,11 @@ export function isStreamlinedPriceRadioTreatment( variationName?: string | null 
 		return false;
 	}
 	return variationName.includes( 'checkout_radio' );
+}
+
+export function isStreamlinedPriceCheckoutTreatment( variationName?: string | null ) {
+	if ( ! variationName ) {
+		return false;
+	}
+	return ! variationName.includes( 'checkout_control' );
 }
