cdccc3da3af24605779b33642093869fe03a8435
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-05-14 16:31:12 +0200

    Update tests

--- client/my-sites/checkout/src/test/checkout-main.tsx
+++ client/my-sites/checkout/src/test/checkout-main.tsx
@@ -167,9 +167,6 @@ describe( 'CheckoutMain', () => {
 		const user = userEvent.setup();
 		expect( screen.getAllByLabelText( 'WordPress.com Personal' ) ).toHaveLength( 1 );
 		await user.click( removeProductButton );
-		const confirmModal = await screen.findByRole( 'dialog' );
-		const confirmButton = await within( confirmModal ).findByText( 'Continue' );
-		await user.click( confirmButton );
 		await waitFor( async () => {
 			expect( screen.queryByLabelText( 'WordPress.com Personal' ) ).not.toBeInTheDocument();
 		} );
@@ -190,9 +187,6 @@ describe( 'CheckoutMain', () => {
 		);
 		const user = userEvent.setup();
 		await user.click( removeProductButton );
-		const confirmModal = await screen.findByRole( 'dialog' );
-		const confirmButton = await within( confirmModal ).findByText( 'Continue' );
-		await user.click( confirmButton );
 		await waitFor( () => {
 			expect( navigate ).toHaveBeenCalledWith( '/plans/foo.com' );
 		} );
@@ -213,9 +207,6 @@ describe( 'CheckoutMain', () => {
 		);
 		const user = userEvent.setup();
 		await user.click( removeProductButton );
-		const confirmModal = await screen.findByRole( 'dialog' );
-		const confirmButton = await within( confirmModal ).findByText( 'Continue' );
-		await user.click( confirmButton );
 		await waitFor( async () => {
 			expect( navigate ).not.toHaveBeenCalledWith( '/plans/foo.com' );
 		} );
