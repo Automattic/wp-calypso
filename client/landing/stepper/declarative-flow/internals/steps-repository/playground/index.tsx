61e84b24b6cd8b432a775262a5f48677a0171133
ashfame <mail@ashfame.com>
2025-05-14 11:40:01 +0400

    move php script to calculate intent to its own file

--- client/landing/stepper/declarative-flow/internals/steps-repository/playground/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/playground/index.tsx
@@ -7,9 +7,9 @@ import DocumentHead from 'calypso/components/data/document-head';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { PlaygroundIframe } from './components/playground-iframe';
 import { getBlueprintLabelForTracking } from './lib/blueprint';
+import calculate_plan_intent_php_script from './lib/calculate-plan-intent-php-script';
 import type { Step as StepType } from '../../types';
 import './style.scss';
-
 const DEFAULT_PLAN_INTENT = 'plans-playground';
 
 export const PlaygroundStep: StepType = ( { navigation, flow } ) => {
@@ -39,25 +39,7 @@ export const PlaygroundStep: StepType = ( { navigation, flow } ) => {
 		setCalculatingIntent( true );
 		intentPromiseRef.current = pgc
 			.run( {
-				code: `<?php
-				require_once( '/wordpress/wp-load.php' );
-				die( 'plans-playground' ); // short-circuit the logic until we support free/premium plans
-
-				$active_plugins = array_diff( get_option( 'active_plugins' ), array( 'hello.php', 'akismet/akismet.php' ) );
-				if ( in_array( 'woocommerce/woocommerce.php', $active_plugins ) ) {
-					echo 'plans-playground';
-					return;
-				}
-				if ( count( $active_plugins ) > 0 ) {
-					echo 'plans-playground';
-					return;
-				}
-				$has_user_global_styles = count( get_posts( array(
-					'post_type' => 'wp_global_styles',
-					'post_status' => 'publish',
-					'posts_per_page' => 1,
-				) ) ) > 0;
-				echo $has_user_global_styles ? 'plans-playground-premium' : 'plans-default-wpcom';`,
+				code: calculate_plan_intent_php_script,
 			} )
 			.then( ( res: { text: string } ) => {
 				setPgIntent( res.text );
