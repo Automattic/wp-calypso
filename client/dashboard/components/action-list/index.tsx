fff675e575a2db446ff78c01616b23a5e92851ef
arthur <arthur.chu@automattic.com>
2025-05-15 22:34:48 +0900

    Add CardBody

--- client/dashboard/components/action-list/index.tsx
+++ client/dashboard/components/action-list/index.tsx
@@ -2,6 +2,7 @@ import {
 	__experimentalVStack as VStack,
 	__experimentalText as Text,
 	Card,
+	CardBody,
 } from '@wordpress/components';
 import { forwardRef } from 'react';
 import ActionItem from './action-item';
@@ -14,21 +15,25 @@ function UnforwardedActionList(
 ) {
 	return (
 		<Card className="action-list" ref={ ref }>
-			{ ( title || description ) && (
-				<VStack className="action-list__heading" spacing={ 2 }>
-					{ title && (
-						<Text size="15px" weight={ 500 } lineHeight="20px">
-							{ title }
-						</Text>
-					) }
-					{ description && (
-						<Text variant="muted" lineHeight="20px">
-							{ description }
-						</Text>
-					) }
+			<CardBody>
+				{ ( title || description ) && (
+					<VStack className="action-list__heading" spacing={ 2 }>
+						{ title && (
+							<Text size="15px" weight={ 500 } lineHeight="20px">
+								{ title }
+							</Text>
+						) }
+						{ description && (
+							<Text variant="muted" lineHeight="20px">
+								{ description }
+							</Text>
+						) }
+					</VStack>
+				) }
+				<VStack className="action-list__actions" spacing={ 0 }>
+					{ children }
 				</VStack>
-			) }
-			<VStack spacing={ 0 }>{ children }</VStack>
+			</CardBody>
 		</Card>
 	);
 }
