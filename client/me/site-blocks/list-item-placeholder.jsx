67a476a664db1ad4d6405cb762cabac60cf248ac
Mikael Korpela <mikael@ihminen.org>
2025-05-16 13:29:13 +0300

    Move placeholder outta component

--- client/me/site-blocks/list-item-placeholder.jsx
+++
@@ -1,9 +0,0 @@
-const SiteBlockListItemPlaceholder = () => {
-	return (
-		<div className="site-blocks__list-item is-placeholder" aria-hidden="true">
-			<span className="site-blocks__list-item-placeholder-text">Blocked site</span>
-		</div>
-	);
-};
-
-export default SiteBlockListItemPlaceholder;
