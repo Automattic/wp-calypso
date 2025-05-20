a2f2e84bac0cb0eceb41aac8b605bbecbf360606
arthur <arthur.chu@automattic.com>
2025-05-20 22:40:18 +0800

    Add ConfirmNewOwnerForm and update styles

--- client/dashboard/sites/settings-transfer-site/index.tsx
+++ client/dashboard/sites/settings-transfer-site/index.tsx
@@ -1,49 +1,20 @@
-import { DataForm } from '@automattic/dataviews';
 import { useQuery } from '@tanstack/react-query';
 import { notFound } from '@tanstack/react-router';
-import {
-	__experimentalHStack as HStack,
-	__experimentalVStack as VStack,
-	__experimentalText as Text,
-	Button,
-	Card,
-	CardBody,
-} from '@wordpress/components';
+import { Card, CardBody, ExternalLink } from '@wordpress/components';
 import { createInterpolateElement } from '@wordpress/element';
-import { sprintf, __ } from '@wordpress/i18n';
-import { useState } from 'react';
+import { __ } from '@wordpress/i18n';
 import { siteQuery } from '../../app/queries';
 import PageLayout from '../../components/page-layout';
 import { useCanTransferSite } from '../hooks/use-can-transfer-site';
 import SettingsPageHeader from '../settings-page-header';
-import type { Field, SimpleFormField } from '@automattic/dataviews';
-
-type FormData = {
-	email: string;
-};
-
-const fields: Field< FormData >[] = [
-	{
-		id: 'email',
-		label: __( 'Email' ),
-		type: 'text' as const,
-	},
-];
-
-const form = {
-	type: 'regular' as const,
-	fields: [ { id: 'email' } as SimpleFormField ],
-};
+import { ConfirmNewOwnerForm } from './confirm-new-owner-form';
 
 export default function SettingsTransferSite( { siteSlug }: { siteSlug: string } ) {
 	const { data: site } = useQuery( siteQuery( siteSlug ) );
 	const canTransferSite = useCanTransferSite( { site } );
-	const [ formData, setFormData ] = useState( {
-		email: '',
-	} );
 
 	// TODO: Integrate with the API.
-	const handleSubmit = ( event: React.FormEvent ) => {
+	const handleConfirmNewOwner = ( event: React.FormEvent ) => {
 		event.preventDefault();
 	};
 
@@ -61,50 +32,20 @@ export default function SettingsTransferSite( { siteSlug }: { siteSlug: string }
 			header={
 				<SettingsPageHeader
 					title={ __( 'Transfer site' ) }
-					description={ __( 'Transfer ownership of this site to another WordPress.com user.' ) }
+					description={ createInterpolateElement(
+						__(
+							'Transfer this site to a new or existing site member with just a few clicks. <a>Learn more.</a>'
+						),
+						{
+							a: <ExternalLink href="#learn-more" />,
+						}
+					) }
 				/>
 			}
 		>
 			<Card>
 				<CardBody>
-					<VStack>
-						<Text size="15px" weight={ 500 } lineHeight="20px">
-							{ __( 'Confirm new owner' ) }
-						</Text>
-						<Text variant="muted" lineHeight="20px">
-							{ createInterpolateElement(
-								sprintf(
-									/* translators: %(siteSlug)s - the current site slug */
-									__(
-										"Ready to transfer <strong>%(siteSlug)s</strong> and its associated purchases? Simply enter the new owner's email below, or choose an existing user to start the transfer process."
-									),
-									{
-										siteSlug,
-									}
-								),
-								{
-									strong: <strong />,
-								}
-							) }
-						</Text>
-						<form onSubmit={ handleSubmit }>
-							<VStack spacing={ 4 }>
-								<DataForm< FormData >
-									data={ formData }
-									fields={ fields }
-									form={ form }
-									onChange={ ( edits: FormData ) => {
-										setFormData( ( data ) => ( { ...data, ...edits } ) );
-									} }
-								/>
-								<HStack justify="flex-start">
-									<Button variant="primary" type="submit">
-										{ __( 'Continue' ) }
-									</Button>
-								</HStack>
-							</VStack>
-						</form>
-					</VStack>
+					<ConfirmNewOwnerForm siteSlug={ siteSlug } handleSubmit={ handleConfirmNewOwner } />
 				</CardBody>
 			</Card>
 		</PageLayout>
