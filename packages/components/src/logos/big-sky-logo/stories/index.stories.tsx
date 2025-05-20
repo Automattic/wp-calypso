6cc8d8b93ab2092d727e57d08f0ec6ee5f988aa1
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-05-20 14:14:58 +0200

    Create story for storybook

---
+++ packages/components/src/logos/big-sky-logo/stories/index.stories.tsx
@@ -0,0 +1,12 @@
+import { BigSkyLogo } from '..';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta< typeof BigSkyLogo > = {
+	title: 'Unaudited/Logos/BigSkyLogo',
+	component: BigSkyLogo,
+};
+export default meta;
+
+type Story = StoryObj< typeof BigSkyLogo >;
+
+export const Default: Story = {};
