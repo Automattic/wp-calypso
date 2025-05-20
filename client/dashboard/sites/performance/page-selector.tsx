003c26a85feb8e3b64eb2537bed37b1b8740e493
StevenDufresne <dufresnesteven@gmail.com>
2025-05-20 13:00:52 +0900

    Add styles to fix dropdown menu.

--- client/dashboard/sites/performance/page-selector.tsx
+++ client/dashboard/sites/performance/page-selector.tsx
@@ -68,47 +68,46 @@ export function PageSelector( { siteUrl, pages, currentPage, onChange }: PageSel
 	}, [ pages, siteUrl, currentPageOption ] );
 
 	return (
-		<ComboboxControl
-			className="performance-page-selector"
-			label={ __( 'Page' ) }
-			allowReset={ false }
-			options={ pageOptions }
-			value={ currentPageOption?.value }
-			hideLabelFromVision
-			onChange={ ( page_id ) => {
-				recordTracksEvent( 'calypso_performance_profiler_page_selector_change', {
-					is_home: page_id === '0',
-					version: profilerVersion(),
-				} );
+		<div className="performance-page-selector">
+			<ComboboxControl
+				label={ __( 'Page' ) }
+				allowReset={ false }
+				options={ pageOptions }
+				value={ currentPageOption?.value }
+				onChange={ ( page_id ) => {
+					recordTracksEvent( 'calypso_performance_profiler_page_selector_change', {
+						is_home: page_id === '0',
+						version: profilerVersion(),
+					} );
 
-				const url = new URL( window.location.href );
-				if ( page_id ) {
-					url.searchParams.set( 'page_id', page_id );
-				} else {
-					url.searchParams.delete( 'page_id' );
-				}
+					const url = new URL( window.location.href );
+					if ( page_id ) {
+						url.searchParams.set( 'page_id', page_id );
+					} else {
+						url.searchParams.delete( 'page_id' );
+					}
 
-				window.history.replaceState( {}, '', url.toString() );
-				onChange( page_id );
-			} }
-			css={ { maxWidth: 300, minWidth: 300 } }
-			__next40pxDefaultSize
-			__nextHasNoMarginBottom
-			__experimentalRenderItem={ ( { item } ) => {
-				if ( item.value === '-1' ) {
+					window.history.replaceState( {}, '', url.toString() );
+					onChange( page_id );
+				} }
+				__next40pxDefaultSize
+				__nextHasNoMarginBottom
+				__experimentalRenderItem={ ( { item } ) => {
+					if ( item.value === '-1' ) {
+						return (
+							<Text variant="muted">
+								{ __( 'Performance testing is available for the 20 most popular pages.' ) }
+							</Text>
+						);
+					}
 					return (
-						<Text variant="muted">
-							{ __( 'Performance testing is available for the 20 most popular pages.' ) }
-						</Text>
+						<VStack spacing="0">
+							<Text>{ item.label }</Text>
+							<Text variant="muted">{ item.path }</Text>
+						</VStack>
 					);
-				}
-				return (
-					<VStack spacing="0">
-						<Text>{ item.label }</Text>
-						<Text variant="muted">{ item.path }</Text>
-					</VStack>
-				);
-			} }
-		/>
+				} }
+			/>
+		</div>
 	);
 }
