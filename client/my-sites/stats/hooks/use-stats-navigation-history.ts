793f3e61a366cfca25abacc9d240f381316137a7
Nikhil <nikhil.chavan@automattic.com>
2025-05-21 17:24:49 +0530

    Update the modules from post summary

--- client/my-sites/stats/hooks/use-stats-navigation-history.ts
+++ client/my-sites/stats/hooks/use-stats-navigation-history.ts
@@ -17,6 +17,13 @@ const possibleBackLinks: { [ key: string ]: string | null } = {
 	subscribers: '/stats/subscribers/{period}/',
 	posts: '/stats/{period}/posts/',
 	authors: '/stats/{period}/authors/',
+	filedownloads: '/stats/{period}/filedownloads/',
+	referrers: '/stats/{period}/referrers/',
+	locations: '/stats/{period}/locations/',
+	utm: '/stats/{period}/utm/',
+	clicks: '/stats/{period}/clicks/',
+	searchterms: '/stats/{period}/searchterms/',
+	videoplays: '/stats/{period}/videoplays/',
 	annualstats: '/stats/day/annualstats/',
 	postDetails: null, // Last item in the history, the text is not displayed anywhere but this is used to track the item in history stack.
 };
@@ -54,8 +61,15 @@ export const useStatsNavigationHistory = (): { text: string; url: string | null 
 			subscribers: translate( 'Subscribers' ),
 			posts: translate( 'Posts & pages' ),
 			authors: translate( 'Authors' ),
+			filedownloads: translate( 'File Downloads' ),
+			referrers: translate( 'Referrers' ),
+			locations: translate( 'Locations' ),
+			utm: translate( 'UTM' ),
+			clicks: translate( 'Clicks' ),
+			searchterms: translate( 'Search Terms' ),
+			videoplays: translate( 'Videos' ),
 			annualstats: translate( 'Annual insights' ),
-			postDetails: null,
+			postDetails: null, // Last item in the history, the text is not displayed anywhere but this is used to track the item in history stack.
 		} ),
 		[]
 	);
