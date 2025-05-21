d4df4bae7be174e6997180f377cd8c022799cbf2
Marco Ciampini <marco.ciampo@gmail.com>
2025-05-22 00:01:18 +0200

    Emotion -> Sass modules

--- packages/components/src/utils/style-mixins.ts
+++
@@ -1,8 +0,0 @@
-// export { boxSizingReset } from './box-sizing';
-// export { rgba } from './colors';
-// export { rtl } from './rtl';
-// export { font } from './font';
-// export { breakpoint } from './breakpoint';
-export { default as CONFIG } from './config-values';
-export { COLORS } from './colors-values';
-// export { baseLabelTypography } from './base-label';
