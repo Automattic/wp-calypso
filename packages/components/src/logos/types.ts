e1fd63b816c8413e1531199ab481a40fc37aa3f2
Marco Ciampini <marco.ciampo@gmail.com>
2025-05-22 16:24:46 +0200

    Add more explicit docs around using `aria-hidden` to remove from assistive tech

--- packages/components/src/logos/types.ts
+++ packages/components/src/logos/types.ts
@@ -12,7 +12,14 @@ export interface CommonLogoProps {
 	 */
 	monochrome?: boolean;
 	/**
-	 * The accessible name of the logo
+	 * The accessible name of the logo.
+	 * To hide the logo from assistive technologies, use the `aria-hidden`
+	 * attribute.
 	 */
 	title?: string;
+	/**
+	 * Indicates whether the element is exposed to an accessibility API.
+	 * Use `aria-hidden="true"` to hide the logo from assistive technologies.
+	 */
+	'aria-hidden'?: React.SVGAttributes< SVGSVGElement >[ 'aria-hidden' ];
 }
