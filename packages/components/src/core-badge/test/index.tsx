5be7cffa808c85e9acbae268abe74f9b99346e4e
Lena Morita <lena@jaguchi.com>
2025-05-23 00:30:10 +0900

    Simplify tests

--- packages/components/src/core-badge/test/index.tsx
+++ packages/components/src/core-badge/test/index.tsx
@@ -2,30 +2,22 @@
  * @jest-environment jsdom
  */
 import { render, screen } from '@testing-library/react';
-import { CoreBadge as _Badge } from '..';
-
-const testid = 'my-badge';
-const Badge = ( props: React.ComponentProps< typeof _Badge > ) => (
-	<_Badge data-testid={ testid } { ...props } />
-);
+import { CoreBadge } from '..';
 
 describe( 'Badge', () => {
 	it( 'should render as a span', () => {
-		render( <Badge>Code is Poetry</Badge> );
-		const badge = screen.getByTestId( testid );
-		expect( badge ).toBeInTheDocument();
+		render( <CoreBadge>Code is Poetry</CoreBadge> );
+		const badge = screen.getByText( 'Code is Poetry' );
 		expect( badge.tagName ).toBe( 'SPAN' );
 	} );
 
-	it( 'should pass through a custom class name', () => {
-		render( <Badge className="custom-class">Code is Poetry</Badge> );
-		const badge = screen.getByTestId( testid );
-		expect( badge ).toHaveClass( 'custom-class' );
-	} );
-
 	it( 'should pass through additional props', () => {
-		render( <Badge data-testid="custom-badge">Code is Poetry</Badge> );
-		const badge = screen.getByTestId( 'custom-badge' );
-		expect( badge ).toHaveTextContent( 'Code is Poetry' );
+		render(
+			<CoreBadge data-testid="custom-data-attr" className="custom-class">
+				Code is Poetry
+			</CoreBadge>
+		);
+		const badge = screen.getByTestId( 'custom-data-attr' );
+		expect( badge ).toHaveClass( 'custom-class' );
 	} );
 } );
