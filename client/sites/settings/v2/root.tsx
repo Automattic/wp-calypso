b5b677b535fb04ee68f548501c497bd872635ad5
Griffith Chen <griffith.chen@automattic.com>
2025-05-22 15:39:56 +0800

    Remove WebpackBuildMonitor

--- client/sites/settings/v2/root.tsx
+++ client/sites/settings/v2/root.tsx
@@ -1,25 +1,12 @@
 import { Outlet } from '@tanstack/react-router';
-import { Suspense, lazy } from 'react';
 import Snackbars from 'calypso/dashboard/app/snackbars';
 import './style.scss';
-
-const WebpackBuildMonitor = lazy(
-	() =>
-		import(
-			/* webpackChunkName: "async-webpack-build-monitor" */ 'calypso/components/webpack-build-monitor'
-		)
-);
 
 export default function Root() {
 	return (
 		<>
 			<Outlet />
 			<Snackbars />
-			{ 'development' === process.env.NODE_ENV && (
-				<Suspense fallback={ null }>
-					<WebpackBuildMonitor />
-				</Suspense>
-			) }
 		</>
 	);
 }
