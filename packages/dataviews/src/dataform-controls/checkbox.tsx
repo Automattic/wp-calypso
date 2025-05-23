c186fbb0ecc76df2fb19dc855b24c9ad3c33eb28
arthur <arthur.chu@automattic.com>
2025-05-23 19:00:35 +0800

    Support help message

--- packages/dataviews/src/dataform-controls/checkbox.tsx
+++ packages/dataviews/src/dataform-controls/checkbox.tsx
@@ -14,12 +14,13 @@ export default function Checkbox< Item >( {
 	data,
 	hideLabelFromVision,
 }: DataFormControlProps< Item > ) {
-	const { id, getValue, label } = field;
+	const { id, getValue, label, description } = field;
 
 	return (
 		<CheckboxControl
 			__nextHasNoMarginBottom
 			label={ ! hideLabelFromVision ? label : '' }
+			help={ description }
 			checked={ getValue( { item: data } ) }
 			onChange={ () =>
 				onChange( { [ id ]: ! getValue( { item: data } ) } )
