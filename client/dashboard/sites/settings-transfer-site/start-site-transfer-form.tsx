4d4d330be27c8ff7e106bf32ae097fb1d3e163c5
arthur <arthur.chu@automattic.com>
2025-05-26 11:52:53 +0800

    Update styles

--- client/dashboard/sites/settings-transfer-site/start-site-transfer-form.tsx
+++ client/dashboard/sites/settings-transfer-site/start-site-transfer-form.tsx
@@ -5,6 +5,7 @@ import {
 	__experimentalText as Text,
 	Button,
 } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
 import { sprintf, __ } from '@wordpress/i18n';
 import React, { useState } from 'react';
 import type { Field } from '@automattic/dataviews';
@@ -20,16 +21,19 @@ const fields: Field< StartSiteTransferFormData >[] = [
 		id: 'accept_authorization',
 		label: __( 'I understand the changes that will be made once I authorize this transfer.' ),
 		type: 'boolean' as const,
+		Edit: 'checkbox',
 	},
 	{
 		id: 'accept_transfer',
 		label: __( 'I want to transfer the ownership of the site.' ),
 		type: 'boolean' as const,
+		Edit: 'checkbox',
 	},
 	{
 		id: 'accept_undone',
 		label: __( 'I understand that transferring a site cannot be undone.' ),
 		type: 'boolean' as const,
+		Edit: 'checkbox',
 	},
 ];
 
@@ -61,74 +65,106 @@ export function StartSiteTransferForm( {
 	const isSaveDisabled = Object.values( formData ).some( ( value ) => ! value );
 
 	return (
-		<VStack spacing={ 5 }>
-			<VStack style={ { padding: '8px 0' } }>
+		<VStack spacing={ 0 }>
+			<VStack style={ { padding: '8px 0 12px' } }>
 				<Text size="15px" weight={ 500 } lineHeight="32px">
 					{ __( 'Start site transfer' ) }
 				</Text>
 			</VStack>
-			{ /* TODO: Add notice when the component is ready */ }
-			<ul>
-				{ __( 'Content and ownership' ) }
-				<li>
-					{ sprintf(
-						/* translators: %(siteSlug)s - the current site slug, %(newOwnerEmail)s - the new owner's email */
-						__(
-							'You’ll be removed as owner of %(siteSlug)s and %(newOwnerEmail)s will the new owner from now on.'
-						),
-						{ siteSlug, newOwnerEmail }
-					) }
-				</li>
-				<li>
-					{ sprintf(
-						/* translators: %(newOwnerEmail)s - the new owner's email */
-						__( 'You will keep your admin access unless %(newOwnerEmail)s removes you.' ),
-						{ newOwnerEmail }
-					) }
-				</li>
-				<li>
-					{ sprintf(
-						/* translators: %(siteSlug)s - the current site slug */
-						__( 'Your posts on %(siteSlug)s will remain authored by your account.' ),
-						{ siteSlug }
-					) }
-				</li>
-			</ul>
-			<ul>
-				{ __( 'Domains' ) }
-				<li>
-					{ sprintf(
-						/* translators: %(siteSlug)s - the current site slug, %(newOwnerEmail)s - the new owner's email */
-						__(
-							'The domain name %(siteSlug)s will be transferred to %(newOwnerEmail)s and will remain working on the site.'
-						),
-						{ siteSlug, newOwnerEmail }
-					) }
-				</li>
-			</ul>
-			<form onSubmit={ handleSubmit }>
-				<VStack spacing={ 4 } style={ { padding: '8px 0' } }>
-					<span>
-						{ __( 'To transfer your site, review and accept the following statements:' ) }
-					</span>
-					<DataForm< StartSiteTransferFormData >
-						data={ formData }
-						fields={ fields }
-						form={ form }
-						onChange={ ( edits: Partial< StartSiteTransferFormData > ) => {
-							setFormData( ( data ) => ( { ...data, ...edits } ) );
-						} }
-					/>
-					<HStack justify="flex-start">
-						<Button variant="primary" type="submit" disabled={ isSaveDisabled }>
-							{ __( 'Start transfer' ) }
-						</Button>
-						<Button variant="tertiary" onClick={ handleBack }>
-							{ __( 'Back' ) }
-						</Button>
-					</HStack>
+			<VStack spacing={ 5 } style={ { padding: '8px 0' } }>
+				{ /* TODO: Add notice when the component is ready */ }
+				<VStack spacing={ 6 } style={ { padding: '8px 0' } }>
+					<VStack>
+						<Text weight={ 500 }>{ __( 'Content and ownership' ) }</Text>
+						<ul style={ { paddingInlineStart: '20px', margin: 0 } }>
+							<li>
+								{ createInterpolateElement(
+									sprintf(
+										/* translators: %(siteSlug)s - the current site slug, %(newOwnerEmail)s - the new owner's email */
+										__(
+											'You’ll be removed as owner of <strong>%(siteSlug)s</strong> and <strong>%(newOwnerEmail)s</strong> will the new owner from now on.'
+										),
+										{ siteSlug, newOwnerEmail }
+									),
+									{
+										strong: <strong />,
+									}
+								) }
+							</li>
+							<li>
+								{ createInterpolateElement(
+									sprintf(
+										/* translators: %(newOwnerEmail)s - the new owner's email */
+										__(
+											'You will keep your admin access unless <strong>%(newOwnerEmail)s</strong> removes you.'
+										),
+										{ newOwnerEmail }
+									),
+									{
+										strong: <strong />,
+									}
+								) }
+							</li>
+							<li>
+								{ createInterpolateElement(
+									sprintf(
+										/* translators: %(siteSlug)s - the current site slug */
+										__(
+											'Your posts on <strong>%(siteSlug)s</strong> will remain authored by your account.'
+										),
+										{ siteSlug }
+									),
+									{
+										strong: <strong />,
+									}
+								) }
+							</li>
+						</ul>
+					</VStack>
+					<VStack>
+						<Text weight={ 500 }>{ __( 'Domains' ) }</Text>
+						<ul style={ { paddingInlineStart: '20px', margin: 0 } }>
+							<li>
+								{ createInterpolateElement(
+									sprintf(
+										/* translators: %(siteSlug)s - the current site slug, %(newOwnerEmail)s - the new owner's email */
+										__(
+											'The domain name <strong>%(siteSlug)s</strong> will be transferred to <strong>%(newOwnerEmail)s</strong> and will remain working on the site.'
+										),
+										{ siteSlug, newOwnerEmail }
+									),
+									{
+										strong: <strong />,
+									}
+								) }
+							</li>
+						</ul>
+					</VStack>
 				</VStack>
-			</form>
+				<form onSubmit={ handleSubmit }>
+					<VStack>
+						<span style={ { textTransform: 'uppercase' } }>
+							{ __( 'To transfer your site, review and accept the following statements:' ) }
+						</span>
+						<DataForm< StartSiteTransferFormData >
+							data={ formData }
+							fields={ fields }
+							form={ form }
+							onChange={ ( edits: Partial< StartSiteTransferFormData > ) => {
+								setFormData( ( data ) => ( { ...data, ...edits } ) );
+							} }
+						/>
+						<HStack justify="flex-start" style={ { paddingTop: '8px' } }>
+							<Button variant="primary" type="submit" disabled={ isSaveDisabled }>
+								{ __( 'Start transfer' ) }
+							</Button>
+							<Button variant="tertiary" onClick={ handleBack }>
+								{ __( 'Back' ) }
+							</Button>
+						</HStack>
+					</VStack>
+				</form>
+			</VStack>
 		</VStack>
 	);
 }
