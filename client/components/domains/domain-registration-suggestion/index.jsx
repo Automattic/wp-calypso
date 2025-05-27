1481841da98266f1b14e4408fce0f8e634467fe8
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-05-27 11:07:00 +0200

    Simplify domain title markup by removing redundant h3 and div elements to fix accessibility

--- client/components/domains/domain-registration-suggestion/index.jsx
+++ client/components/domains/domain-registration-suggestion/index.jsx
@@ -299,17 +299,15 @@ class DomainRegistrationSuggestion extends Component {
 		return (
 			<div className={ wrapperClassName }>
 				<div className={ titleWrapperClassName }>
-					<h3 className="domain-registration-suggestion__title">
-						<div className="domain-registration-suggestion__domain-title">
-							<span aria-label={ domain }>
-								<span className="domain-registration-suggestion__domain-title-name">
-									{ this.getFormattedDomainName( name ) }
-								</span>
-								<span className="domain-registration-suggestion__domain-title-tld">{ tld }</span>
+					<div className="domain-registration-suggestion__title">
+						<span className="domain-registration-suggestion__domain-title">
+							<span className="domain-registration-suggestion__domain-title-name">
+								{ this.getFormattedDomainName( name ) }
 							</span>
-							{ ( showHstsNotice || showDotGayNotice ) && this.renderInfoBubble() }
-						</div>
-					</h3>
+							<span className="domain-registration-suggestion__domain-title-tld">{ tld }</span>
+						</span>
+						{ ( showHstsNotice || showDotGayNotice ) && this.renderInfoBubble() }
+					</div>
 				</div>
 			</div>
 		);
