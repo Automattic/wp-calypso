5a648c1aa16512268292dff34f27ac6c11d4091a
Griffith Chen <griffith.chen@automattic.com>
2025-05-27 17:26:15 +0800

    Update Blogging Quick Start course

--- client/data/courses/constants.ts
+++ client/data/courses/constants.ts
@@ -1,5 +1,5 @@
 export const COURSE_SLUGS = {
-	BLOGGING_QUICK_START: 'blogging-quick-start',
+	BLOGGING_QUICK_START: 'create-your-blog',
 	PAYMENTS_FEATURES: 'payments-features',
 	SITE_EDITOR_QUICK_START: 'site-editor-quick-start',
 } as const;
