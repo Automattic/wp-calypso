73218606a724b9c4b99fdd3459477d054e7f999b
Paulo Trentin <paulo@paulotrentin.com.br>
2025-05-28 09:34:21 -0300

    Disable the onboarding experiment by merging treatment

--- client/landing/stepper/hooks/use-mvp-onboarding-experiment.ts
+++ client/landing/stepper/hooks/use-mvp-onboarding-experiment.ts
@@ -1,16 +1,9 @@
-import { useExperiment, loadExperimentAssignment } from 'calypso/lib/explat';
 import { useQuery } from './use-query';
-
-const HOSTING_ONBOARDING_EXPERIMENT_NAME = 'calypso_signup_onboarding_simplified_hosting_flow_v3';
 
 export function useMvpOnboardingExperiment() {
 	const hasPlaygroundId = useQuery().has( 'playground' );
 
-	const [ isLoading, assignment ] = useExperiment( HOSTING_ONBOARDING_EXPERIMENT_NAME, {
-		isEligible: ! hasPlaygroundId,
-	} );
-
-	return [ isLoading, assignment?.variationName === 'treatment' ];
+	return [ false, ! hasPlaygroundId ];
 }
 
 export async function isMvpOnboardingExperiment() {
@@ -21,6 +14,5 @@ export async function isMvpOnboardingExperiment() {
 		return false;
 	}
 
-	const assignment = await loadExperimentAssignment( HOSTING_ONBOARDING_EXPERIMENT_NAME );
-	return assignment.variationName === 'treatment';
+	return true;
 }
