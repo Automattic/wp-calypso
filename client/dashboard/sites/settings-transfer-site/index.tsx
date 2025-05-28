690bc521c4e3ead3edec7dcb788d63d29eb0fec4
arthur <arthur.chu@automattic.com>
2025-05-28 14:52:17 +0800

    Don't redirect if there is confirmation hash

--- client/dashboard/sites/settings-transfer-site/index.tsx
+++ client/dashboard/sites/settings-transfer-site/index.tsx
@@ -69,7 +69,7 @@ export default function SettingsTransferSite( { siteSlug }: { siteSlug: string }
 		return null;
 	}
 
-	if ( ! canTransferSite ) {
+	if ( ! canTransferSite && ! confirmationHash ) {
 		throw notFound();
 	}
 
