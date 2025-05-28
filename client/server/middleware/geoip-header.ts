3f7b0e4d9d22878f5d832beb1c529ee74cec1ca8
Andrew Duthie <andrew.duthie@automattic.com>
2025-05-28 15:17:13 -0400

    Flatten to remove IIFE
    See: https://github.com/Automattic/wp-calypso/pull/103597#discussion_r2103895312

--- client/server/middleware/geoip-header.ts
+++ client/server/middleware/geoip-header.ts
@@ -6,34 +6,32 @@ import { type RequestHandler } from 'express';
  * @returns A middleware function that adds the `x-geoip-country-code` header to the request.
  */
 export default function (): RequestHandler {
-	// To avoid delays on handling individual requests, fetch the geolocation once at server
-	// startup. This will retrieve the geolocation of the server (not the client), but it's
-	// expected that the middleware would be enabled on a developer's own machine.
-	const getGeoCountryCode = ( () => {
-		let countryCode: string | undefined;
-		let countryCodePromise: Promise< string | undefined > | undefined;
+	// To avoid delays on handling individual requests, fetch the geolocation once during server
+	// startup. This will retrieve the geolocation of the server (not the client), but it's expected
+	// that the middleware would be enabled on a developer's own machine.
+	let countryCode: string | undefined;
+	let countryCodePromise: Promise< string | undefined > | undefined;
 
-		return async () => {
-			if ( ! countryCode ) {
-				if ( ! countryCodePromise ) {
-					countryCodePromise = fetch( 'https://public-api.wordpress.com/geo/' )
-						.then( async ( response ) => response.json() )
-						.then( ( { country_short } ) => country_short )
-						// Support offline development and set an expectation that the header is not
-						// always available by quietly skipping if the request fails.
-						.catch( () => {
-							// Reset the promise so we can try again on the next request
-							countryCodePromise = undefined;
-							return undefined;
-						} );
-				}
-
-				countryCode = await countryCodePromise;
+	async function getGeoCountryCode() {
+		if ( ! countryCode ) {
+			if ( ! countryCodePromise ) {
+				countryCodePromise = fetch( 'https://public-api.wordpress.com/geo/' )
+					.then( async ( response ) => response.json() )
+					.then( ( { country_short } ) => country_short )
+					// Support offline development and set an expectation that the header is not
+					// always available by quietly skipping if the request fails.
+					.catch( () => {
+						// Reset the promise so we can try again on the next request
+						countryCodePromise = undefined;
+						return undefined;
+					} );
 			}
 
-			return countryCode;
-		};
-	} )();
+			countryCode = await countryCodePromise;
+		}
+
+		return countryCode;
+	}
 
 	// Prime the cache when the middleware is initialized.
 	getGeoCountryCode();
