a7a00052103ddb5d823f107c36eed4b1e7057261
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2025-05-30 11:39:46 +0500

    💫 UPDATE: Paid Newsletter settings section to show dynamic button with values Add Plans / Manage Plans instead of a status Set Up button

--- client/my-sites/site-settings/settings-newsletter/PaidNewsletterSection.tsx
+++ client/my-sites/site-settings/settings-newsletter/PaidNewsletterSection.tsx
@@ -1,11 +1,17 @@
-import { Button, Card } from '@automattic/components';
+import { Card } from '@automattic/components';
+import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { useSelectedSiteSelector } from 'calypso/state/sites/hooks';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
 
-export const PaidNewsletterSection = () => {
+interface PaidNewsletterSectionProps {
+	newsletterHasActivePlan?: boolean;
+}
+
+export const PaidNewsletterSection = ( props: PaidNewsletterSectionProps ): JSX.Element => {
+	const { newsletterHasActivePlan } = props;
 	const translate = useTranslate();
 	const siteSlug = useSelectedSiteSelector( getSiteSlug );
 
@@ -15,8 +21,12 @@ export const PaidNewsletterSection = () => {
 
 	return (
 		<Card className="site-settings__card">
-			<Button href={ `/earn/payments/${ siteSlug }` } onClick={ onSetUpButtonClick }>
-				{ translate( 'Set up' ) }
+			<Button
+				variant="secondary"
+				href={ `/earn/payments/${ siteSlug }` }
+				onClick={ onSetUpButtonClick }
+			>
+				{ newsletterHasActivePlan ? translate( 'Manage Plans' ) : translate( 'Add Plans' ) }
 			</Button>
 			<FormSettingExplanation>
 				{ translate(
