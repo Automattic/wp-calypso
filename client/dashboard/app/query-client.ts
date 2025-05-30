bb6e50f4f497f66d4ce9877f0dee54a64c199274
StevenDufresne <dufresnesteven@gmail.com>
2025-05-30 14:36:25 +0900

    Remove 2fa exclusion.

--- client/dashboard/app/query-client.ts
+++ client/dashboard/app/query-client.ts
@@ -1,7 +1,6 @@
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import { QueryClient } from '@tanstack/react-query';
 import { persistQueryClient } from '@tanstack/react-query-persist-client';
-import { TWO_STEP_QUERY_KEY } from './auth';
 
 export const queryClient = new QueryClient( {
 	defaultOptions: {
@@ -22,11 +21,7 @@ const [ , persistPromise ] = persistQueryClient( {
 	buster: '2', // Bump when query data shape changes.
 	maxAge,
 	dehydrateOptions: {
-		shouldDehydrateQuery: ( { queryKey, meta } ) => {
-			if ( TWO_STEP_QUERY_KEY === queryKey ) {
-				return false;
-			}
-
+		shouldDehydrateQuery: ( { meta } ) => {
 			if ( meta?.persist === false ) {
 				return false;
 			}
