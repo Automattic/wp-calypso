6a89541526c58bd70a1e4bab23c4582047b1ab6d
MILLER/F <millerf@automattic.com>
2025-05-30 14:17:23 +0200

    Update index.tsx

--- client/my-sites/earn/paid-subscriptions/index.tsx
+++ client/my-sites/earn/paid-subscriptions/index.tsx
@@ -81,7 +81,7 @@ const PaidSubscriptionsSection = ( { query }: PaidSubscriptionsSectionProps ) =>
 				{ Object.values( paid_subscriptions ).length === 0 && (
 					<Card>
 						{ translate(
-							"You don't have any active subscriptions yet. {{learnMoreLink}}Learn more{{/learnMoreLink}} about payments.",
+							"You don't have any active subscription yet. {{learnMoreLink}}Learn more{{/learnMoreLink}} about payments.",
 							{
 								components: {
 									learnMoreLink: isJetpackCloud() ? (
