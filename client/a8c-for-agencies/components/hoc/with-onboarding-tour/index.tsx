33c93663d039241804f3282377028e5e4dc89567
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-06-03 23:59:52 +0800

    Move useOnboardingTour hook to separate file.

--- client/a8c-for-agencies/components/hoc/with-onboarding-tour/index.tsx
+++ client/a8c-for-agencies/components/hoc/with-onboarding-tour/index.tsx
@@ -1,45 +1,9 @@
-import { isEnabled } from '@automattic/calypso-config';
-import { ComponentType, useCallback, useEffect, useState } from 'react';
+import { ComponentType } from 'react';
 import OnboardingTourModal from '../../onboarding-tour-modal';
+import useOnboardingTour from './hooks/use-onboarding-tour';
 import useOnboardingTourSections from './hooks/use-onboarding-tour-sections';
 
 import './style.scss';
-
-export const ONBOARDING_TOUR_HASH = '#onboarding-tour';
-
-function useOnboardingTour() {
-	const [ isOpen, setIsOpen ] = useState( false );
-
-	useEffect( () => {
-		const handleHashChange = () => {
-			const hash = window.location.hash;
-			if ( isEnabled( 'a4a-unified-onboarding-tour' ) && hash === ONBOARDING_TOUR_HASH ) {
-				setIsOpen( true );
-			}
-		};
-
-		// Check hash on mount
-		handleHashChange();
-
-		// Listen for hash changes
-		window.addEventListener( 'hashchange', handleHashChange );
-
-		return () => {
-			window.removeEventListener( 'hashchange', handleHashChange );
-		};
-	}, [] );
-
-	const onClose = useCallback( () => {
-		setIsOpen( false );
-		// Remove the hash from the URL
-		window.history.replaceState( '', '', window.location.pathname );
-	}, [] );
-
-	return {
-		isOpen,
-		onClose,
-	};
-}
 
 export function withOnboardingTour< T extends JSX.IntrinsicAttributes >(
 	WrappedComponent: ComponentType< T >
