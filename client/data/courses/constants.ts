b186133da6ecb7ab8194e169ce41103f0c9626b6
Griffith Chen <griffith.chen@automattic.com>
2025-06-03 09:24:18 +0800

    Change back course slug

--- client/data/courses/constants.ts
+++ client/data/courses/constants.ts
@@ -1,5 +1,5 @@
 export const COURSE_SLUGS = {
-	BLOGGING_QUICK_START: 'create-your-blog',
+	BLOGGING_QUICK_START: 'blogging-quick-start',
 	PAYMENTS_FEATURES: 'payments-features',
 	SITE_EDITOR_QUICK_START: 'site-editor-quick-start',
 } as const;
