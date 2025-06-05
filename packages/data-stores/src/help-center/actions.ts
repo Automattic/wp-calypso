3914f507e973ed88480b08cf2db9f6d7e4fe17e7
escapemanuele <escapemanuele@gmail.com>
2025-06-05 14:21:45 +0200

    Check if the user can connect to zendesk

--- packages/data-stores/src/help-center/actions.ts
+++ packages/data-stores/src/help-center/actions.ts
@@ -210,6 +210,11 @@ export const setNewMessagingChat = function* ( {
 	yield setShowHelpCenter( true );
 };
 
+export const setNavigateToOdie = function* () {
+	yield setNavigateToRoute( '/odie' );
+	yield setShowHelpCenter( true );
+};
+
 export const setShowSupportDoc = function* ( link: string, postId?: number, blogId?: number ) {
 	const params = new URLSearchParams( {
 		link,
@@ -238,6 +243,7 @@ export type HelpCenterAction =
 			| typeof setAreSoundNotificationsEnabled
 			| typeof setZendeskClientId
 			| typeof setNavigateToRoute
+			| typeof setNavigateToOdie
 			| typeof setOdieInitialPromptText
 			| typeof setOdieBotNameSlug
 			| typeof setCurrentSupportInteraction
