da5d1b4e3ad7cc6b5713cd6de79b195cdda43cf9
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-06-05 20:22:21 +0800

    use withOnboardingTour on the sidebar.

--- client/a8c-for-agencies/sections/overview/sidebar/index.tsx
+++ client/a8c-for-agencies/sections/overview/sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { isEnabled } from '@automattic/calypso-config';
+import { withOnboardingTour } from 'calypso/a8c-for-agencies/components/hoc/with-onboarding-tour';
 import OverviewSidebarAgencyTier from './agency-tier';
 import OverviewSidebarContactSupport from './contact-support';
 import OverviewSidebarFeaturedWooPayments from './featured-woopayments';
@@ -19,4 +20,4 @@ const OverviewSidebar = () => {
 	);
 };
 
-export default OverviewSidebar;
+export default withOnboardingTour( OverviewSidebar );
