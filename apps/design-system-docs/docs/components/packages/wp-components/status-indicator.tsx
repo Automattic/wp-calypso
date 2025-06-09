998308b04e0a674f5357ae1e576cb78f87f71e5e
Lena Morita <lena@jaguchi.com>
2025-06-10 08:06:29 +0900

    Optimize value finding for status indicator

--- apps/design-system-docs/docs/components/packages/wp-components/status-indicator.tsx
+++ apps/design-system-docs/docs/components/packages/wp-components/status-indicator.tsx
@@ -3,13 +3,12 @@ import { ComponentStatus, statuses } from './data';
 import styles from './status-indicator.module.scss';
 
 export const StatusIndicator = ( { status }: { status: ComponentStatus } ) => {
+	const { label, icon } = statuses.find( ( s ) => s.value === status ) ?? {};
+
 	return (
-		<Tooltip text={ statuses.find( ( s ) => s.value === status )?.label }>
-			<div
-				className={ styles[ 'status-indicator' ] }
-				aria-label={ statuses.find( ( s ) => s.value === status )?.label }
-			>
-				{ statuses.find( ( s ) => s.value === status )?.icon }
+		<Tooltip text={ label }>
+			<div className={ styles[ 'status-indicator' ] } aria-label={ label }>
+				{ icon }
 			</div>
 		</Tooltip>
 	);
