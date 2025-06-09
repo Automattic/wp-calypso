cdb49091efad4a79b09725918cdb05b96514efbb
André Maneiro <583546+oandregal@users.noreply.github.com>
2025-06-09 13:35:04 +0200

    Add unit tests

--- packages/dataviews/src/test/normalize-fields.ts
+++ packages/dataviews/src/test/normalize-fields.ts
@@ -42,4 +42,116 @@ describe( 'normalizeFields: default getValue', () => {
 			expect( result ).toBe( 'value' );
 		} );
 	} );
+	describe( 'filterBy', () => {
+		it( 'returns the default field type definition if undefined for untyped field', () => {
+			const fields: Field< {} >[] = [
+				{
+					id: 'user',
+				},
+			];
+			const normalizedFields = normalizeFields( fields );
+			const result = normalizedFields[ 0 ].filterBy;
+			expect( result ).toBe( false );
+		} );
+
+		it( 'returns the field type definition for typed fields', () => {
+			const fields: Field< {} >[] = [
+				{
+					id: 'user',
+					type: 'integer',
+				},
+			];
+			const normalizedFields = normalizeFields( fields );
+			const result = normalizedFields[ 0 ].filterBy;
+			expect( result ).toStrictEqual( {
+				operators: [
+					'is',
+					'isNot',
+					'lessThan',
+					'greaterThan',
+					'lessThanOrEqual',
+					'greaterThanOrEqual',
+				],
+			} );
+		} );
+
+		it( 'returns false if is false', () => {
+			const fields: Field< {} >[] = [
+				{
+					id: 'user',
+					filterBy: false,
+				},
+			];
+			const normalizedFields = normalizeFields( fields );
+			const result = normalizedFields[ 0 ].filterBy;
+			expect( result ).toBe( false );
+		} );
+
+		it( 'returns the config if it provides one', () => {
+			const fields: Field< {} >[] = [
+				{
+					id: 'user',
+					filterBy: {
+						isPrimary: true,
+						operators: [ 'is', 'isNot' ],
+					},
+				},
+			];
+			const normalizedFields = normalizeFields( fields );
+			const result = normalizedFields[ 0 ].filterBy;
+			expect( result ).toStrictEqual( {
+				isPrimary: true,
+				operators: [ 'is', 'isNot' ],
+			} );
+		} );
+
+		it( 'returns false if the none of the operators are valid for the type', () => {
+			const fields: Field< {} >[] = [
+				{
+					id: 'user',
+					filterBy: {
+						// @ts-ignore
+						operators: [ 'invalid', 'operator' ],
+					},
+				},
+			];
+			const normalizedFields = normalizeFields( fields );
+			const result = normalizedFields[ 0 ].filterBy;
+			expect( result ).toBe( false );
+		} );
+
+		it( 'returns false if the list of operators is empty', () => {
+			const fields: Field< {} >[] = [
+				{
+					id: 'user',
+					filterBy: {
+						operators: [],
+					},
+				},
+			];
+			const normalizedFields = normalizeFields( fields );
+			const result = normalizedFields[ 0 ].filterBy;
+			expect( result ).toBe( false );
+		} );
+
+		it( 'removes invalid operators for the type', () => {
+			const fields: Field< {} >[] = [
+				{
+					id: 'user',
+					type: 'integer',
+					filterBy: {
+						isPrimary: true,
+						// @ts-ignore
+						operators: [ 'invalid', 'lessThan' ],
+					},
+				},
+			];
+			const normalizedFields = normalizeFields( fields );
+			const result = normalizedFields[ 0 ].filterBy;
+			expect( result ).toStrictEqual( {
+				isPrimary: true,
+				operators: [ 'lessThan' ],
+			} );
+		} );
+	} );
 } );
