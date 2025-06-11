6b366011bb3b7cfbfa710e32bd76818b2f5bb31d
Addison Stavlo <Addison.Stavlo@Automattic.com>
2025-06-11 10:02:40 -0400

    update calypso Gravatar component to optionally wrap with hovercards

--- client/components/gravatar/index.jsx
+++ client/components/gravatar/index.jsx
@@ -1,8 +1,22 @@
 import { Gravatar } from '@automattic/components';
+import { Hovercards } from '@gravatar-com/hovercards/react';
 import { get } from 'lodash';
 import { connect } from 'react-redux';
 import { getUserTempGravatar } from 'calypso/state/gravatar-status/selectors';
+import '@gravatar-com/hovercards/dist/style.css';
 
-export default connect( ( state, ownProps ) => ( {
+const ConnectedGravatar = connect( ( state, ownProps ) => ( {
 	tempImage: getUserTempGravatar( state, get( ownProps, 'user.ID', false ) ),
 } ) )( Gravatar );
+
+export default function GravatarWithHovercards( { showHovercards = false, ...props } ) {
+	if ( ! showHovercards ) {
+		return <ConnectedGravatar { ...props } />;
+	}
+
+	return (
+		<Hovercards>
+			<ConnectedGravatar { ...props } />
+		</Hovercards>
+	);
+}
