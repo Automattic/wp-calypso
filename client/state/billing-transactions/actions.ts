a6a9625d40e5f06d73a28d80a6c17ede78044f2a
MILLER/F <millerf@automattic.com>
2025-06-13 19:11:16 +0200

    Load all history

--- client/state/billing-transactions/actions.ts
+++ client/state/billing-transactions/actions.ts
@@ -13,61 +13,59 @@ import type { CalypsoDispatch } from '../types';
 import 'calypso/state/billing-transactions/init';
 
 export const requestBillingTransactions = ( transactionType?: BillingTransactionsType ) => {
-	return ( dispatch: CalypsoDispatch ) => {
+	return async ( dispatch: CalypsoDispatch ) => {
 		dispatch( {
 			type: BILLING_TRANSACTIONS_REQUEST,
 		} );
 
 		const limit = 200;
-		const url = '/me/billing-history' + ( transactionType ? `/${ transactionType }` : '' );
-		return wp.req
-			.get( url, {
-				apiVersion: '1.3',
-				limit,
-			} )
-			.then(
-				( {
-					billing_history,
-					upcoming_charges,
-				}: {
-					billing_history: BillingTransaction[];
-					upcoming_charges: UpcomingCharge[];
-				} ) => {
-					const fullBillingHistory = billing_history;
-					if ( billing_history.length === limit ) {
-						// If we have exactly the limit number of transactions, it means there are more transactions to fetch.
-						let nextData = [];
-						do {
-							// Fetch the next page of transactions.
-							return wp.req
-								.get( url, {
-									apiVersion: '1.3',
-									offset: billing_history.length,
-									limit,
-								} )
-								.then( ( { billing_history }: { billing_history: BillingTransaction[] } ) => {
-									fullBillingHistory.push( ...billing_history );
-									nextData = billing_history;
-								} );
-						} while ( nextData.length === limit );
-					}
+		let url = '/me/billing-history' + ( transactionType ? `/${ transactionType }` : '' );
+		if ( transactionType !== 'upcoming' ) {
+			url = url + '?limit=' + limit;
+		}
 
-					dispatch( {
-						type: BILLING_TRANSACTIONS_RECEIVE,
-						past: fullBillingHistory,
-						upcoming: upcoming_charges,
-					} );
-					dispatch( {
-						type: BILLING_TRANSACTIONS_REQUEST_SUCCESS,
-					} );
-				}
-			)
-			.catch( ( error: Error ) => {
-				dispatch( {
-					type: BILLING_TRANSACTIONS_REQUEST_FAILURE,
-					error,
-				} );
+		try {
+			const {
+				billing_history,
+				upcoming_charges,
+			}: {
+				billing_history: BillingTransaction[];
+				upcoming_charges: UpcomingCharge[];
+			} = await wp.req.get( url, {
+				apiVersion: '1.3',
 			} );
+
+			const fullBillingHistory = billing_history;
+			if ( billing_history.length === limit ) {
+				// If we have exactly the limit number of transactions, it means there are more transactions to fetch.
+				let nextData = [];
+				do {
+					// Fetch the next page of transactions.
+					const offset_url = url + '&offset=' + billing_history.length;
+
+					const res = await wp.req.get( offset_url, {
+						apiVersion: '1.3',
+					} );
+
+					fullBillingHistory.push( ...res.billing_history );
+					nextData = res.billing_history;
+				} while ( nextData.length === limit );
+			}
+
+			dispatch( {
+				type: BILLING_TRANSACTIONS_RECEIVE,
+				past: fullBillingHistory,
+				upcoming: upcoming_charges,
+			} );
+			dispatch( {
+				type: BILLING_TRANSACTIONS_REQUEST_SUCCESS,
+			} );
+		} catch ( error: any ) {
+			dispatch( {
+				type: BILLING_TRANSACTIONS_REQUEST_FAILURE,
+				error,
+			} );
+		}
 	};
 };
 
