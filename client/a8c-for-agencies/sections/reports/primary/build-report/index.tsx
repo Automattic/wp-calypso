84e7db1ed1ff5190fd00a2ff7330b298cb7105f9
Yashwin <yashwinpoojary@gmail.com>
2025-06-13 09:05:00 +0530

    fix type error

--- client/a8c-for-agencies/sections/reports/primary/build-report/index.tsx
+++ client/a8c-for-agencies/sections/reports/primary/build-report/index.tsx
@@ -317,7 +317,7 @@ const BuildReport = () => {
 									label={ translate( 'Teammate email(s)' ) }
 									value={ teammateEmails }
 									onChange={ setTeammateEmails }
-									type="email"
+									type="text"
 									help={
 										! hasFieldError( 'teammateEmails' )
 											? translate( 'Use commas to separate addresses.' )
