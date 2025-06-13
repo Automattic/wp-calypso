5c863597ce90008effa2e38a307e5bebe95461ff
Lena Morita <lena@jaguchi.com>
2025-06-14 07:54:18 +0900

    Remove from `@automattic/components`

--- packages/components/src/calendar/index.tsx
+++
@@ -1,3 +0,0 @@
-export { TZDate } from 'react-day-picker';
-export { DateCalendar } from './date-calendar';
-export { DateRangeCalendar } from './date-range-calendar';
--- packages/components/src/calendar/utils/misc.ts
+++
@@ -1,3 +0,0 @@
-export function clampNumberOfMonths( numberOfMonths: number ) {
-	return Math.min( 3, Math.max( 1, numberOfMonths ) );
-}
