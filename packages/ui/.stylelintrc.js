5bb72dbefa729f9335ad39d891793c3ab3e002c7
Lena Morita <lena@jaguchi.com>
2025-06-17 01:01:34 +0900

    Prevent more shorthand properties

--- packages/ui/.stylelintrc.js
+++ packages/ui/.stylelintrc.js
@@ -6,6 +6,11 @@ module.exports = {
 			// Prevents shorthand left/right values (unclear for RTL)
 			margin: 3,
 			padding: 3,
+			'border-width': 3,
+			'border-color': 3,
+			'border-style': 3,
+			'border-radius': 3,
+			inset: 3,
 		},
 		'plugin/use-logical-properties-and-values': [
 			true,
