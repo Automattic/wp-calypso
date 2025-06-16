fc538beac5b65e1c989fbe2e47d004c551819df3
arthur <arthur.chu@automattic.com>
2025-06-16 11:31:28 +0800

    Dashboard v2: Add icon to snackbar

--- client/dashboard/app/snackbars/index.tsx
+++ client/dashboard/app/snackbars/index.tsx
@@ -1,16 +1,29 @@
 import { SnackbarList } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
+import { Icon, published, error } from '@wordpress/icons';
 import { store as noticesStore } from '@wordpress/notices';
+import React from 'react';
 import './style.scss';
 
 // Last three notices. Slices from the tail end of the list.
 const MAX_VISIBLE_NOTICES = -3;
+
+const statusIcon: Record< string, React.JSX.Element > = {
+	success: published,
+	error,
+};
 
 export default function Snackbars() {
 	const notices = useSelect( ( select ) => select( noticesStore ).getNotices(), [] );
 	const { removeNotice } = useDispatch( noticesStore );
 	const snackbarNotices = notices
 		.filter( ( { type } ) => type === 'snackbar' )
+		.map( ( { status, ...notice } ) => ( {
+			...notice,
+			icon: statusIcon[ status ] && (
+				<Icon icon={ statusIcon[ status ] } style={ { fill: 'currentcolor' } } />
+			),
+		} ) )
 		.slice( MAX_VISIBLE_NOTICES );
 
 	return (
