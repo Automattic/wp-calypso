56bc2e75953cc7790e0f05f2e803998b8b53d0a9
Addison Stavlo <Addison.Stavlo@Automattic.com>
2025-06-17 16:10:57 -0400

    fix issue with some users not having a subscirbe button

--- client/components/gravatar-with-hovercards/hovercard-content.jsx
+++ client/components/gravatar-with-hovercards/hovercard-content.jsx
@@ -4,20 +4,38 @@ import ReaderFollowButton from 'calypso/reader/follow-button';
 import { useSelector, useDispatch } from 'calypso/state';
 import { requestSite } from 'calypso/state/reader/sites/actions';
 import { getSite } from 'calypso/state/reader/sites/selectors';
+import { requestUser } from 'calypso/state/reader/users/actions';
+import getReaderUser from 'calypso/state/selectors/get-reader-user';
 
 function HovercardContent( props ) {
 	const dispatch = useDispatch();
 	const { user } = props;
 
-	const primaryBlogId = user?.primary_blog || user?.site_ID;
+	// Prefer wpcom_id when it is given. Sometimes ID is specific to another site and wpcom_id is
+	// accurate. Use ID as a fallback as sometimes wpcom_id isn't provided (like self user data).
+	const userID = user.wpcom_id || user.ID;
+
+	// For some reason there are places where the user object passes in primary blog of -1. Lets
+	// find the read one with this selector.
+	const readerUserData = useSelector( ( state ) => getReaderUser( state, userID, true ) );
+
+	const primaryBlogId = readerUserData?.primary_blog || user?.primary_blog || user?.site_ID;
 	const site = useSelector( ( state ) => getSite( state, primaryBlogId ) );
 	const primaryBlogUrl = site?.URL;
 
 	useEffect( () => {
-		if ( ! site && user.ID ) {
+		if ( ! userID ) {
+			// This isnt a wpcom user, skip requesting data.
+			return;
+		}
+
+		if ( ! site ) {
 			dispatch( requestSite( primaryBlogId ) );
 		}
-	}, [ user, dispatch, site, primaryBlogId ] );
+		if ( ! readerUserData ) {
+			dispatch( requestUser( userID, true ) );
+		}
+	}, [ userID, dispatch, site, primaryBlogId, readerUserData ] );
 
 	if ( ! user.ID ) {
 		return null;
