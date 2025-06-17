ef2f5b85cbceb4778f079d94f108fb765c30be19
Lena Morita <lena@jaguchi.com>
2025-06-17 20:15:47 +0900

    UI: Use content hash for CSS modules

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,13 +1,5 @@
-/* eslint-disable import/no-nodejs-modules */
-import { execSync } from 'node:child_process';
-import { createHash } from 'node:crypto';
 import { sassPlugin, postcssModules } from 'esbuild-sass-plugin';
 import { defineConfig } from 'tsup';
-
-let gitHeadSha;
-try {
-	gitHeadSha = execSync( 'git rev-parse HEAD' ).toString();
-} catch {}
 
 export default defineConfig( {
 	entry: [ 'src/index.ts' ],
@@ -22,13 +14,7 @@ export default defineConfig( {
 			filter: /\.module\.(css|scss)$/,
 			embedded: true,
 			transform: postcssModules( {
-				generateScopedName: ( name, filename ) => {
-					const hash = createHash( 'md5' )
-						.update( name + filename + gitHeadSha )
-						.digest( 'hex' )
-						.slice( 0, 6 );
-					return `a8cui-${ hash }`;
-				},
+				generateScopedName: 'a8cui-[contenthash:base64:6]',
 			} ),
 		} ),
 	],
