52f3120482d33f984c9000555fbb2398d7c94e79
arthur <arthur.chu@automattic.com>
2025-06-18 11:47:25 +0800

    Set staleTime to 1 min

--- client/dashboard/app/query-client.ts
+++ client/dashboard/app/query-client.ts
@@ -5,7 +5,7 @@ import { persistQueryClient } from '@tanstack/react-query-persist-client';
 export const queryClient = new QueryClient( {
 	defaultOptions: {
 		queries: {
-			staleTime: 0,
+			staleTime: 1000 * 60 * 1, // 1 minutes
 			refetchOnWindowFocus: true,
 			refetchOnMount: true,
 		},
