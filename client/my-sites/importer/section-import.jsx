08fa8d3160fb1d9887baf71616a7f2d6ae5262cd
Gabriel Caires <gabriel.caires@automattic.com>
2025-06-19 18:03:31 +0100

    Revert "Use celebration modal"
    This reverts commit 291e3705d490ed55eb45321f2c2c38282c3bdcc5.

--- client/my-sites/importer/section-import.jsx
+++ client/my-sites/importer/section-import.jsx
@@ -39,7 +39,7 @@ import {
 	getSelectedSiteSlug,
 } from 'calypso/state/ui/selectors';
 import './section-import.scss';
-import CelebrateLaunchModal from './celebration-modal';
+import { SuccessPanel } from './success-panel';
 
 /**
  * Configuration mapping import engines to associated import components.
@@ -116,15 +116,11 @@ class SectionImport extends Component {
 	} );
 
 	state = {
-		showSuccessScreen: false,
+		showSuccessScreen: true,
 	};
 
 	showSuccessScreen = () => {
 		this.setState( { showSuccessScreen: true } );
-	};
-
-	closeSuccessScreen = () => {
-		this.setState( { showSuccessScreen: false } );
 	};
 
 	handleStateChanges = () => {
@@ -359,16 +355,13 @@ class SectionImport extends Component {
 			( importItem ) => importItem.importerState === appStates.IMPORT_SUCCESS
 		);
 
+		if ( this.state.showSuccessScreen ) {
+			return <SuccessPanel site={ site } importerId={ importSuccess?.importerId } />;
+		}
+
 		return (
 			<Main>
 				<DocumentHead title={ translate( 'Import Content' ) } />
-				{ this.state.showSuccessScreen && (
-					<CelebrateLaunchModal
-						setModalIsOpen={ this.closeSuccessScreen }
-						site={ site }
-						importerId={ importSuccess?.importerId }
-					/>
-				) }
 				<NavigationHeader
 					screenOptionsTab="import.php"
 					navigationItems={ [] }
