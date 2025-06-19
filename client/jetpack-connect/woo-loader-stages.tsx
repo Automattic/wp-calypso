8c32efac0e05225e25c66fbd45f8dac0f53a7bc5
Ahmed <ahmed.el.azzabi@automattic.com>
2025-06-19 08:40:55 +0100

    Remove OpeningTheDoorsStage given it's not used anymore

--- client/jetpack-connect/woo-loader-stages.tsx
+++ client/jetpack-connect/woo-loader-stages.tsx
@@ -1,6 +1,4 @@
 import { __ } from '@wordpress/i18n';
-import WooPurpleHeart from 'calypso/assets/images/onboarding/woo-purple-heart.png';
-import OpeningImage from './woo-loader-stage-images/OpeningImage';
 import SetupImage from './woo-loader-stage-images/SetupImage';
 
 export const ConnectingYourAccountStage = {
@@ -23,14 +21,4 @@ export const CreatingYourAccountStage = {
 	),
 	duration: 10000,
 	progress: 70,
-};
-
-export const OpeningTheDoorsStage = {
-	title: __( 'Opening the doors' ),
-	image: OpeningImage,
-	label: __( '#FunWooFact:' ),
-	text: __( 'Our favorite color is purple' ),
-	element: <img src={ WooPurpleHeart } alt="loader-hearticon" className="loader-hearticon" />,
-	duration: 30000,
-	progress: 100,
 };
