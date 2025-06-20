0580ff83c53a25ae11dfbcc1ab4a0d895ad9526c
Griffith Chen <griffith.chen@automattic.com>
2025-06-20 13:08:54 +0800

    Move logic from fetch to select

--- client/dashboard/data/site-jetpack-monitor-uptime.ts
+++ client/dashboard/data/site-jetpack-monitor-uptime.ts
@@ -6,31 +6,11 @@ export interface SiteUptime {
 }
 
 export async function fetchSiteUptime( siteId: number, period?: string ): Promise< number | null > {
-	const uptime: Record< string, SiteUptime > = await wpcom.req.get(
+	return wpcom.req.get(
 		{
 			path: `/sites/${ siteId }/jetpack-monitor-uptime`,
 			apiNamespace: 'wpcom/v2',
 		},
 		{ period: period ?? '30 days' }
 	);
-
-	// Post-process the data to calculate total number for up and down days during the time period.
-	const { upDays, downDays } = Object.values( uptime ).reduce(
-		( accumulator, { status } ) => {
-			if ( status === 'monitor_inactive' ) {
-				return accumulator;
-			}
-
-			accumulator[ status === 'up' ? 'upDays' : 'downDays' ] += 1;
-			return accumulator;
-		},
-		{ upDays: 0, downDays: 0 }
-	);
-
-	if ( ! upDays && ! downDays ) {
-		return null;
-	}
-
-	// Calculate the uptime percentage.
-	return Math.round( ( upDays / ( upDays + downDays ) ) * 1000 ) / 10;
 }
