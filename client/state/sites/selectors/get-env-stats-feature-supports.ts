be8682edc90f07f096cd820fa3bb512a68f19854
Dognose <dognose24@gmail.com>
2025-06-23 17:06:02 +0800

    Map the supported stats-admin version

--- client/state/sites/selectors/get-env-stats-feature-supports.ts
+++ client/state/sites/selectors/get-env-stats-feature-supports.ts
@@ -65,7 +65,7 @@ function getEnvStatsFeatureSupportChecks( state: object, siteId: number | null )
 		),
 		supportsArchiveStats: version_greater_than_or_equal(
 			statsAdminVersion,
-			'0.26.3',
+			'0.27.0',
 			isOdysseyStats
 		),
 		shouldUseStatsBuiltInPurchasesApi: version_greater_than_or_equal(
