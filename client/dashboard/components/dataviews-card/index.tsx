5a7534762e59e37dd8b1699952942d07017c71bc
arthur <arthur.chu@automattic.com>
2025-06-23 13:13:00 +0800

    Update paddings in dataviews component directly

--- client/dashboard/components/dataviews-card/index.tsx
+++ client/dashboard/components/dataviews-card/index.tsx
@@ -1,10 +1,9 @@
 import { Card, CardBody } from '@wordpress/components';
-import './styles.scss';
 
 function DataViewsCard( { children }: { children: React.ReactNode } ) {
 	return (
-		<Card className="dashboard-dataviews-card">
-			<CardBody>{ children }</CardBody>
+		<Card>
+			<CardBody style={ { padding: 0, overflow: 'hidden' } }>{ children }</CardBody>
 		</Card>
 	);
 }
