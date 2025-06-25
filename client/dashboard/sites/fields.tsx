c294521899e1aac9d3530f8587db05f48819e46f
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-06-25 14:59:29 +0700

    Refactor fields to separate file

---
+++ client/dashboard/sites/fields.tsx
@@ -0,0 +1,223 @@
+import { Link } from '@tanstack/react-router';
+import { __experimentalText as Text } from '@wordpress/components';
+import { useResizeObserver } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+import TimeSince from '../components/time-since';
+import { STATUS_LABELS, getSiteStatus } from '../utils/site-status';
+import { getFormattedWordPressVersion } from '../utils/wp-version';
+import { canManageSite } from './features';
+import {
+	EngagementStat,
+	LastBackup,
+	MediaStorage,
+	PHPVersion,
+	Plan,
+	Status,
+	Uptime,
+} from './site-fields';
+import SiteIcon from './site-icon';
+import SitePreview from './site-preview';
+import type { Site } from '../data/types';
+import type { Field, Operator } from '@automattic/dataviews';
+
+function getSiteManagementUrl( site: Site ) {
+	if ( canManageSite( site ) ) {
+		return `/sites/${ site.slug }`;
+	}
+	return site.options?.admin_url;
+}
+
+const DEFAULT_FIELDS: Field< Site >[] = [
+	{
+		id: 'name',
+		label: __( 'Site' ),
+		enableGlobalSearch: true,
+		getValue: ( { item } ) => item.name || new URL( item.URL ).hostname,
+		render: ( { field, item } ) => (
+			<Link to={ getSiteManagementUrl( item ) }>{ field.getValue( { item } ) }</Link>
+		),
+	},
+	{
+		id: 'URL',
+		label: __( 'URL' ),
+		enableGlobalSearch: true,
+		getValue: ( { item } ) => new URL( item.URL ).hostname,
+		render: ( { field, item } ) => (
+			<Text
+				as="span"
+				variant="muted"
+				style={ { overflowX: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' } }
+			>
+				{ field.getValue( { item } ) }
+			</Text>
+		),
+	},
+	{
+		id: 'icon.ico',
+		label: __( 'Site icon' ),
+		render: ( { item } ) => <SiteIcon site={ item } />,
+		enableSorting: false,
+	},
+	{
+		id: 'subscribers_count',
+		label: __( 'Subscribers' ),
+	},
+	{
+		id: 'backup',
+		label: __( 'Backup' ),
+		render: ( { item } ) => <LastBackup site={ item } />,
+		enableSorting: false,
+	},
+	{
+		id: 'plan',
+		label: __( 'Plan' ),
+		getValue: ( { item } ) => item.plan?.product_name_short ?? '',
+		render: ( { item } ) => <Plan site={ item } />,
+	},
+	{
+		id: 'status',
+		label: __( 'Status' ),
+		getValue: ( { item } ) => getSiteStatus( item ),
+		elements: Object.entries( STATUS_LABELS ).map( ( [ value, label ] ) => ( { value, label } ) ),
+		filterBy: {
+			operators: [ 'is' ],
+		},
+		render: ( { item } ) => <Status site={ item } />,
+	},
+	{
+		id: 'wp_version',
+		label: __( 'WP version' ),
+		getValue: ( { item } ) => getFormattedWordPressVersion( item ),
+	},
+	{
+		id: 'is_a8c',
+		type: 'boolean',
+		label: __( 'A8C owned' ),
+		elements: [
+			{ value: true, label: __( 'Yes' ) },
+			{ value: false, label: __( 'No' ) },
+		],
+		filterBy: {
+			operators: [ 'is' as Operator ],
+		},
+		render: ( { item } ) => ( item.is_a8c ? __( 'Yes' ) : __( 'No' ) ),
+	},
+	{
+		id: 'preview',
+		label: __( 'Preview' ),
+		render: function PreviewRender( { item } ) {
+			const [ resizeListener, { width } ] = useResizeObserver();
+			const { is_deleted, is_private, URL: url } = item;
+			// If the site is a private A8C site, X-Frame-Options is set to same
+			// origin.
+			const iframeDisabled = is_deleted || ( item.is_a8c && is_private );
+			return (
+				<Link
+					to={ getSiteManagementUrl( item ) }
+					style={ { display: 'block', height: '100%', width: '100%' } }
+				>
+					{ resizeListener }
+					{ iframeDisabled && (
+						<div
+							style={ {
+								fontSize: '24px',
+								display: 'flex',
+								alignItems: 'center',
+								justifyContent: 'center',
+								height: '100%',
+							} }
+						>
+							<SiteIcon site={ item } />
+						</div>
+					) }
+					{ width && ! iframeDisabled && (
+						<SitePreview url={ url } scale={ width / 1200 } height={ 1200 } />
+					) }
+				</Link>
+			);
+		},
+		enableSorting: false,
+	},
+	{
+		id: 'last_published',
+		label: __( 'Last published' ),
+		getValue: ( { item } ) => item.options?.updated_at ?? '',
+		render: ( { item } ) =>
+			item.options?.updated_at ? <TimeSince date={ item.options.updated_at } /> : '',
+	},
+	{
+		id: 'uptime',
+		label: __( 'Uptime' ),
+		render: ( { item } ) => <Uptime site={ item } />,
+		enableSorting: false,
+	},
+	{
+		id: 'visitors',
+		label: __( 'Visitors' ),
+		render: ( { item } ) => <EngagementStat site={ item } type="visitors" />,
+		enableSorting: false,
+	},
+	{
+		id: 'views',
+		label: __( 'Views' ),
+		render: ( { item } ) => <EngagementStat site={ item } type="views" />,
+		enableSorting: false,
+	},
+	{
+		id: 'likes',
+		label: __( 'Likes' ),
+		render: ( { item } ) => <EngagementStat site={ item } type="likes" />,
+		enableSorting: false,
+	},
+	{
+		id: 'php_version',
+		label: __( 'PHP version' ),
+		render: ( { item }: { item: Site } ) => <PHPVersion site={ item } />,
+	},
+	{
+		id: 'storage',
+		label: __( 'Storage' ),
+		render: ( { item } ) => <MediaStorage site={ item } />,
+		enableSorting: false,
+	},
+	{
+		id: 'host',
+		label: __( 'Host' ),
+		getValue: ( { item } ) => {
+			const provider = item.hosting_provider_guess;
+			if ( ! provider || provider === 'automattic' ) {
+				return 'WordPress.com';
+			}
+
+			switch ( provider ) {
+				case 'jurassic_ninja':
+					return 'Jurassic Ninja';
+				case 'pressable':
+					return 'Pressable';
+			}
+
+			return provider;
+		},
+		render: ( { field, item } ) => field.getValue( { item } ),
+	},
+];
+
+export function getFields( {
+	hasA8CSites,
+	viewType,
+}: {
+	hasA8CSites?: boolean;
+	viewType?: string;
+} ) {
+	return DEFAULT_FIELDS.filter( ( field ) => {
+		if ( field.id === 'is_a8c' && ! hasA8CSites ) {
+			return false;
+		}
+
+		if ( field.id === 'icon.ico' && viewType === 'grid' ) {
+			return false;
+		}
+
+		return true;
+	} );
+}
