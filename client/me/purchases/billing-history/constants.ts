26c8384c083c826f7691a7aa992f1a0660606d6b
Payton Swick <payton@foolord.com>
2025-06-25 18:28:47 -0400

    Collapse type and amount at desktop again and remove date at mobile

--- client/me/purchases/billing-history/constants.ts
+++ client/me/purchases/billing-history/constants.ts
@@ -6,8 +6,8 @@ export const DEFAULT_PER_PAGE = 10;
 export const defaultSortField: string = 'date';
 
 export const wideFields = [ 'date', 'service', 'type', 'amount' ];
-export const desktopFields = [ 'date', 'service', 'type', 'amount' ];
-export const mobileFields = [ 'date', 'service' ];
+export const desktopFields = [ 'date', 'service' ];
+export const mobileFields = [ 'service' ];
 
 export const defaultDataViewsState: View = {
 	type: 'table',
