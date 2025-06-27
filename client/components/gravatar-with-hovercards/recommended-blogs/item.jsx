46da3c81cda872075f1cdc2b0bcf49cc9626cfc3
Addison Stavlo <Addison.Stavlo@Automattic.com>
2025-06-27 11:40:47 -0400

    fix issue with items looking unfollowed

--- client/components/gravatar-with-hovercards/recommended-blogs/item.jsx
+++ client/components/gravatar-with-hovercards/recommended-blogs/item.jsx
@@ -1,24 +1,24 @@
 import ReaderAvatar from 'calypso/blocks/reader-avatar';
+import QueryReaderSite from 'calypso/components/data/query-reader-site';
 import ReaderFollowButton from 'calypso/reader/follow-button';
-import { useDispatch } from 'calypso/state';
-import { requestSite } from 'calypso/state/reader/sites/actions';
+import { useSelector } from 'calypso/state';
+import { getSite } from 'calypso/state/reader/sites/selectors';
 
 function RecommendedBlogItem( { blog } ) {
-	const dispatch = useDispatch();
-
 	const { image, name, feed_URL: feedUrl, blog_ID: siteId } = blog.meta?.data?.feed || {};
 
-	if ( siteId ) {
-		dispatch( requestSite( siteId ) );
-	}
-
-	// The default feed image for sites with no icon is
-	// awful for this case, treat it as no image to fallback
-	// to the globe icon.
-	const siteIcon = image?.includes( '/i/buttonw-com.png' ) ? null : image;
+	const site = useSelector( ( state ) => getSite( state, siteId ) );
+	const siteIcon = site?.icon?.img || site?.icon?.ico || image;
 
 	return (
 		<li className="gravatar-hovercard__recommended-blog-item">
+			{ /* Query the site not just for the icon, but to ensure it is properly loaded in follows state.
+				One example being mapped domains: initial follows state may list by wpcom subdomain, and
+				the url here might be of a mapped domain. The site request success also updates follows
+				state, and can bridge the gap to appropriately determine if a site from this list is
+				followed.
+			*/ }
+			<QueryReaderSite siteId={ siteId } />
 			<ReaderAvatar
 				isCompact
 				siteIcon={ siteIcon }
