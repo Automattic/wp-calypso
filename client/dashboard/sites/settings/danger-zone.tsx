8dfc214020f6e1c21b622c52e39862dfd31dd26c
Kateryna Kodonenko <kateryna@automattic.com>
2025-06-30 11:58:24 +0200

    Add back curly quote

--- client/dashboard/sites/settings/danger-zone.tsx
+++ client/dashboard/sites/settings/danger-zone.tsx
@@ -96,7 +96,7 @@ const SiteDeleteAction = ( { site }: { site: Site } ) => {
 			<ActionList.ActionItem
 				title={ __( 'Delete site' ) }
 				description={ __(
-					"Delete all your posts, pages, media, and data, and give up your site's address."
+					'Delete all your posts, pages, media, and data, and give up your site’s address.'
 				) }
 				actions={ deleteButton }
 			/>
