98e89d053468856b964d61af528d5b135eda8d82
Addison Stavlo <Addison.Stavlo@Automattic.com>
2025-06-30 13:33:17 -0400

    refactor out primary blog section

--- client/components/gravatar-with-hovercards/hovercard-content.jsx
+++ client/components/gravatar-with-hovercards/hovercard-content.jsx
@@ -1,23 +1,18 @@
 import { isEnabled } from '@automattic/calypso-config';
 import page from '@automattic/calypso-router';
-import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import ReaderAvatar from 'calypso/blocks/reader-avatar';
 import AutoDirection from 'calypso/components/auto-direction';
-import QueryReaderSite from 'calypso/components/data/query-reader-site';
-import ReaderFollowButton from 'calypso/reader/follow-button';
 import { useSelector, useDispatch } from 'calypso/state';
-import { getSite } from 'calypso/state/reader/sites/selectors';
 import { requestUser } from 'calypso/state/reader/users/actions';
 import getReaderUser from 'calypso/state/selectors/get-reader-user';
+import PrimaryBlog from './primary-blog-card';
 import RecommendedBlogs from './recommended-blogs';
 
 import './styles.scss';
 
 function HovercardContent( props ) {
 	const dispatch = useDispatch();
-	const translate = useTranslate();
 	const { user, gravatarData } = props;
 
 	// Prefer wpcom_id when it is given. Sometimes ID is specific to another site and wpcom_id is
@@ -31,8 +26,6 @@ function HovercardContent( props ) {
 	const profileUrl = userLogin ? `/reader/users/${ userLogin }` : gravatarData.profileUrl;
 
 	const primaryBlogId = readerUserData?.primary_blog || user?.primary_blog || user?.site_ID;
-	const site = useSelector( ( state ) => getSite( state, primaryBlogId ) );
-	const primaryBlogUrl = site?.URL;
 
 	useEffect( () => {
 		if ( ! readerUserData && userID ) {
@@ -87,44 +80,7 @@ function HovercardContent( props ) {
 				{ userID && (
 					<>
 						<div className="gravatar-hovercard__body">
-							<QueryReaderSite siteId={ primaryBlogId } />
-							{ primaryBlogUrl && (
-								<div className="gravatar-hovercard__primary-blog-card">
-									<div className="gravatar-hovercard__primary-blog-card-header">
-										<ReaderAvatar
-											isCompact
-											siteIcon={ site?.icon?.img || site?.icon?.ico }
-											className="gravatar-hovercard__primary-blog-card-site-icon"
-										/>
-										<div className="gravatar-hovercard__primary-blog-card-site-info">
-											<h5 className="gravatar-hovercard__primary-blog-card-site-title">
-												{ site.title }
-											</h5>
-
-											{ displayName && (
-												<p className="gravatar-hovercard__primary-blog-card-username">
-													{ translate( 'By %(displayName)s', {
-														args: {
-															displayName: displayName || '',
-														},
-													} ) }
-												</p>
-											) }
-										</div>
-									</div>
-
-									<p className="gravatar-hovercard__primary-blog-card-description">
-										{ site?.description }
-									</p>
-
-									<ReaderFollowButton
-										className="gravatar-hovercard__primary-blog-card-follow-button"
-										siteUrl={ primaryBlogUrl }
-										hasButtonStyle
-										followSource="gravatar-hovercard__primary-blog-card"
-									/>
-								</div>
-							) }
+							<PrimaryBlog primaryBlogId={ primaryBlogId } displayName={ displayName } />
 						</div>
 
 						<div className="gravatar-hovercard__footer">
