b529713598933b88ea3e5c07cba3a004551b3fef
Dognose <dognose24@gmail.com>
2025-07-01 22:35:12 +0800

    Determine defaultStatType by fetched data

--- client/my-sites/stats/features/modules/stats-top-posts/stats-top-posts.tsx
+++ client/my-sites/stats/features/modules/stats-top-posts/stats-top-posts.tsx
@@ -2,7 +2,7 @@ import config from '@automattic/calypso-config';
 import { SimplifiedSegmentedControl, StatsCard } from '@automattic/components';
 import { postList } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import QuerySiteStats from 'calypso/components/data/query-site-stats';
 import InlineSupportLink from 'calypso/components/inline-support-link';
@@ -34,6 +34,7 @@ type StatTypeOptionType = {
 	label: string;
 	mainItemLabel: string;
 	analyticsId: string;
+	disabled?: boolean;
 };
 
 const StatsTopPosts: React.FC< StatsModulePostsProps > = ( {
@@ -70,16 +71,6 @@ const StatsTopPosts: React.FC< StatsModulePostsProps > = ( {
 	const mainStatType = MAIN_STAT_TYPE;
 	const subStatType = SUB_STAT_TYPE;
 
-	const isRequestingTopPostsData = useSelector( ( state: StatsStateProps ) =>
-		isRequestingSiteStatsForQuery( state, siteId, mainStatType, query )
-	);
-	const isRequestingArchivesData = useSelector( ( state: StatsStateProps ) =>
-		isRequestingSiteStatsForQuery( state, siteId, subStatType, query )
-	);
-	const isRequestingData = isArchiveBreakdownEnabled
-		? isRequestingTopPostsData || isRequestingArchivesData
-		: isRequestingTopPostsData;
-
 	const [ localStatType, setLocalStatType ] = useState< StatType | null >( null );
 	const onStatTypeChange = ( option: StatTypeOptionType ) => {
 		trackStatsAnalyticsEvent( 'stats_posts_module_menu_clicked', {
@@ -89,17 +80,24 @@ const StatsTopPosts: React.FC< StatsModulePostsProps > = ( {
 		setLocalStatType( option.value );
 	};
 
-	const statType =
-		localStatType || getValidQueryViewType( query.viewType, supportsArchiveStats ) || mainStatType;
+	const isRequestingTopPostsData = useSelector( ( state: StatsStateProps ) =>
+		isRequestingSiteStatsForQuery( state, siteId, mainStatType, query )
+	);
+	const postsAndPagesData = useSelector( ( state ) =>
+		getSiteStatsNormalizedData( state, siteId, mainStatType, query )
+	) as [ id: number, label: string ];
 
-	const data = useSelector( ( state ) =>
-		getSiteStatsNormalizedData( state, siteId, statType, query )
-	) as [ id: number, label: string ]; // TODO: get post shape and share in an external type file.
-
+	const isRequestingArchivesData = useSelector( ( state: StatsStateProps ) =>
+		isRequestingSiteStatsForQuery( state, siteId, subStatType, query )
+	);
 	// Get the archives data to check if we should disable the archives option.
 	const archivesData = useSelector( ( state ) =>
 		getSiteStatsNormalizedData( state, siteId, subStatType, query )
 	) as [ id: number, label: string ];
+
+	const isRequestingData = isArchiveBreakdownEnabled
+		? isRequestingTopPostsData || isRequestingArchivesData
+		: isRequestingTopPostsData;
 
 	const optionLabels = useOptionLabels();
 	const options: StatTypeOptionType[] = Object.entries( optionLabels ).map( ( [ key, item ] ) => {
@@ -111,11 +109,21 @@ const StatsTopPosts: React.FC< StatsModulePostsProps > = ( {
 			// TODO: This is a temporary solution to disable the archives option when the archives data is not available.
 			disabled:
 				isArchiveBreakdownEnabled &&
-				key === subStatType &&
-				! isRequestingArchivesData &&
-				! archivesData.length,
+				( ( key === subStatType && ! isRequestingArchivesData && ! archivesData.length ) ||
+					( key === mainStatType && ! isRequestingTopPostsData && ! postsAndPagesData.length ) ),
 		};
 	} );
+
+	const availableStatTypes = useMemo( () => {
+		return options.filter( ( option ) => ! option.disabled );
+	}, [ options ] );
+	const defaultStatType =
+		availableStatTypes.length === 1 ? availableStatTypes[ 0 ].value : mainStatType;
+	const statType =
+		localStatType ||
+		getValidQueryViewType( query.viewType || defaultStatType, supportsArchiveStats );
+
+	const data = statType === subStatType ? archivesData : postsAndPagesData;
 
 	// Use StatsModule to display paywall upsell.
 	const shouldGateStatsModule = useShouldGateStats( mainStatType );
