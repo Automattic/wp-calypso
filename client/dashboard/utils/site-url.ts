36b657d03ba9bb1225a03a4878206501b48e55a5
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-07-01 09:46:24 +0700

    Add guard clause

--- client/dashboard/utils/site-url.ts
+++ client/dashboard/utils/site-url.ts
@@ -1,5 +1,8 @@
 import type { Site } from '../data/types';
 
 export function getSiteUrlWithoutProtocol( site: Site ) {
+	if ( site.URL === '' ) {
+		return site.URL;
+	}
 	return site.URL.replace( 'https://', '' ).replace( 'http://', '' );
 }
