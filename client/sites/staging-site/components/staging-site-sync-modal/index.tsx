eb14f1137cf9e018e9167762000460587a9d160f
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-07-02 13:31:59 +0200

    Move staging sync modal from v2 dashboard to sites directory

--- client/sites/staging-site/components/staging-site-sync-modal/index.tsx
+++ client/sites/staging-site/components/staging-site-sync-modal/index.tsx
@@ -14,9 +14,11 @@ import {
 import { createInterpolateElement } from '@wordpress/element';
 import { __, isRTL } from '@wordpress/i18n';
 import { chevronRight, chevronLeft } from '@wordpress/icons';
-import InlineSupportLink from '../../components/inline-support-link';
-import { SectionHeader } from '../../components/section-header';
-import SiteEnvironmentBadge, { EnvironmentType } from '../../components/site-environment-badge';
+import InlineSupportLink from 'calypso/dashboard/components/inline-support-link';
+import { SectionHeader } from 'calypso/dashboard/components/section-header';
+import SiteEnvironmentBadge, {
+	EnvironmentType,
+} from 'calypso/dashboard/components/site-environment-badge';
 
 const DirectionArrow = () => {
 	return (
@@ -57,6 +59,7 @@ const EnvironmentLabel = ( { label, environmentType }: EnvironmentLabelProps ) =
 
 interface SyncModalProps {
 	onClose: () => void;
+	onProceed?: () => void;
 	syncType: 'pull' | 'push';
 	environment: 'production' | 'staging';
 	siteSlug: string;
