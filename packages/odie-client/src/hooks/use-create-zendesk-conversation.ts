b4eac4d3ef7304433e8738c5c573c37a681ad5fd
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-07-02 12:47:39 +0200

    Rm `useCallback`

--- packages/odie-client/src/hooks/use-create-zendesk-conversation.ts
+++ packages/odie-client/src/hooks/use-create-zendesk-conversation.ts
@@ -2,7 +2,6 @@ import { HelpCenterSelect } from '@automattic/data-stores';
 import { HELP_CENTER_STORE } from '@automattic/help-center/src/stores';
 import { useUpdateZendeskUserFields } from '@automattic/zendesk-client';
 import { useSelect } from '@wordpress/data';
-import { useCallback } from 'react';
 import Smooch from 'smooch';
 import { logToLogstash } from 'calypso/lib/logstash'; // eslint-disable-line no-restricted-imports -- this import is safe, not introudcing any circular deps; also, it should be removed shortly after investigating the chats with missing zd ids issue
 import { ODIE_ON_ERROR_TRANSFER_MESSAGE, ODIE_TRANSFER_MESSAGE } from '../constants';
@@ -79,112 +78,94 @@ export const useCreateZendeskConversation = (): ( ( {
 	const { addEventToInteraction } = useManageSupportInteraction();
 	const chatId = chat.odieId;
 
-	const createConversation = useCallback(
-		async ( {
-			avoidTransfer = false,
-			interactionId = '',
-			section = '',
-			createdFrom = '',
-			isFromError = false,
-		}: {
-			avoidTransfer?: boolean;
-			interactionId?: string;
-			section?: string | null;
-			createdFrom?: string;
-			isFromError?: boolean;
-		} ) => {
-			const currentInteractionID = interactionId || currentSupportInteraction!.uuid;
-			if (
-				isSubmittingZendeskUserFields ||
-				chat.conversationId ||
-				chat.status === 'transfer' ||
-				chat.provider === 'zendesk'
-			) {
-				return;
-			}
+	const createConversation = async ( {
+		avoidTransfer = false,
+		interactionId = '',
+		section = '',
+		createdFrom = '',
+		isFromError = false,
+	}: {
+		avoidTransfer?: boolean;
+		interactionId?: string;
+		section?: string | null;
+		createdFrom?: string;
+		isFromError?: boolean;
+	} ) => {
+		const currentInteractionID = interactionId || currentSupportInteraction!.uuid;
+		if (
+			isSubmittingZendeskUserFields ||
+			chat.conversationId ||
+			chat.status === 'transfer' ||
+			chat.provider === 'zendesk'
+		) {
+			return;
+		}
 
-			setChat( ( prevChat ) => ( {
-				...prevChat,
-				messages: avoidTransfer
-					? prevChat.messages
-					: [
-							...prevChat.messages,
-							...( isFromError ? ODIE_ON_ERROR_TRANSFER_MESSAGE : ODIE_TRANSFER_MESSAGE ),
-					  ],
-				status: 'transfer',
-			} ) );
+		setChat( ( prevChat ) => ( {
+			...prevChat,
+			messages: avoidTransfer
+				? prevChat.messages
+				: [
+						...prevChat.messages,
+						...( isFromError ? ODIE_ON_ERROR_TRANSFER_MESSAGE : ODIE_TRANSFER_MESSAGE ),
+				  ],
+			status: 'transfer',
+		} ) );
 
-			// `direct_url` sends users directly to ZD, so there'll be no AI chat
-			if ( ! chatId && createdFrom !== 'direct_url' ) {
-				logMessageData( {
-					createdFrom,
-					selectedSiteId,
-					selectedSiteURL,
-					userFieldFlowName,
-					section,
-				} );
-			}
-
-			await submitUserFields( {
-				messaging_initial_message: userFieldMessage || undefined,
-				messaging_site_id: selectedSiteId || null,
-				messaging_ai_chat_id: chatId || undefined,
-				messaging_url: selectedSiteURL || null,
-				messaging_flow: userFieldFlowName || null,
-				messaging_source: section,
+		// `direct_url` sends users directly to ZD, so there'll be no AI chat
+		if ( ! chatId && createdFrom !== 'direct_url' ) {
+			logMessageData( {
+				createdFrom,
+				selectedSiteId,
+				selectedSiteURL,
+				userFieldFlowName,
+				section,
 			} );
-			const conversation = await Smooch.createConversation( {
+		}
+
+		await submitUserFields( {
+			messaging_initial_message: userFieldMessage || undefined,
+			messaging_site_id: selectedSiteId || null,
+			messaging_ai_chat_id: chatId || undefined,
+			messaging_url: selectedSiteURL || null,
+			messaging_flow: userFieldFlowName || null,
+			messaging_source: section,
+		} );
+		const conversation = await Smooch.createConversation( {
+			metadata: {
+				createdAt: Date.now(),
+				supportInteractionId: currentInteractionID,
+				...( chatId ? { odieChatId: chatId } : {} ),
+			},
+		} );
+
+		trackEvent( 'new_zendesk_conversation', {
+			support_interaction: currentInteractionID,
+			created_from: createdFrom,
+			messaging_site_id: selectedSiteId || null,
+			messaging_url: selectedSiteURL || null,
+		} );
+
+		const updatedInteraction = await addEventToInteraction.mutateAsync( {
+			interactionId: currentInteractionID,
+			eventData: { event_source: 'zendesk', event_external_id: conversation.id },
+		} );
+		const eventAddedToNewInteraction = updatedInteraction.uuid !== currentInteractionID;
+		if ( eventAddedToNewInteraction ) {
+			await Smooch.updateConversation( conversation.id, {
 				metadata: {
-					createdAt: Date.now(),
-					supportInteractionId: currentInteractionID,
-					...( chatId ? { odieChatId: chatId } : {} ),
+					supportInteractionId: updatedInteraction.uuid,
 				},
 			} );
+		}
 
-			trackEvent( 'new_zendesk_conversation', {
-				support_interaction: currentInteractionID,
-				created_from: createdFrom,
-				messaging_site_id: selectedSiteId || null,
-				messaging_url: selectedSiteURL || null,
-			} );
-
-			const updatedInteraction = await addEventToInteraction.mutateAsync( {
-				interactionId: currentInteractionID,
-				eventData: { event_source: 'zendesk', event_external_id: conversation.id },
-			} );
-			const eventAddedToNewInteraction = updatedInteraction.uuid !== currentInteractionID;
-			if ( eventAddedToNewInteraction ) {
-				await Smooch.updateConversation( conversation.id, {
-					metadata: {
-						supportInteractionId: updatedInteraction.uuid,
-					},
-				} );
-			}
-
-			setChat( ( prevChat ) => ( {
-				...prevChat,
-				conversationId: conversation.id,
-				provider: 'zendesk',
-				status: 'loaded',
-			} ) );
-		},
-		[
-			addEventToInteraction,
-			chat.conversationId,
-			chat.provider,
-			chat.status,
-			chatId,
-			currentSupportInteraction,
-			isSubmittingZendeskUserFields,
-			selectedSiteId,
-			selectedSiteURL,
-			setChat,
-			submitUserFields,
-			trackEvent,
-			userFieldFlowName,
-			userFieldMessage,
-		]
-	);
+		setChat( ( prevChat ) => ( {
+			...prevChat,
+			conversationId: conversation.id,
+			provider: 'zendesk',
+			status: 'loaded',
+		} ) );
+	};
 
 	return createConversation;
 };
