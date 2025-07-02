d75483ab5472ae249e79f3763c192a5578071fb9
Jeremy Herve <jeremy@jeremy.hu>
2025-07-02 16:26:33 +0200

    Use a tooltip instead of just title tag

--- client/blocks/post-edit-button/index.jsx
+++ client/blocks/post-edit-button/index.jsx
@@ -1,6 +1,7 @@
-import { Gridicon } from '@automattic/components';
+import { Gridicon, Tooltip } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import PropTypes from 'prop-types';
+import { useRef, useState } from 'react';
 import { getEditURL } from 'calypso/state/posts/utils';
 
 import './style.scss';
@@ -8,16 +9,32 @@ import './style.scss';
 const PostEditButton = ( { post, site, iconSize = 24, onClick } ) => {
 	const translate = useTranslate();
 	const editUrl = getEditURL( post, site );
+	const buttonRef = useRef( null );
+	const [ isTooltipVisible, setIsTooltipVisible ] = useState( false );
+
 	return (
-		<a
-			className="post-edit-button"
-			href={ editUrl }
-			onClick={ onClick }
-			title={ translate( 'Edit post' ) }
-		>
-			<Gridicon icon="pencil" size={ iconSize } className="post-edit-button__icon" />
-			<span className="post-edit-button__label">{ translate( 'Edit' ) }</span>
-		</a>
+		<>
+			<a
+				className="post-edit-button"
+				href={ editUrl }
+				onClick={ onClick }
+				ref={ buttonRef }
+				onMouseEnter={ () => setIsTooltipVisible( true ) }
+				onMouseLeave={ () => setIsTooltipVisible( false ) }
+				onMouseDown={ () => setIsTooltipVisible( false ) }
+			>
+				<Gridicon icon="pencil" size={ iconSize } className="post-edit-button__icon" />
+				<span className="post-edit-button__label">{ translate( 'Edit post' ) }</span>
+			</a>
+			<Tooltip
+				context={ buttonRef.current }
+				isVisible={ isTooltipVisible }
+				position="bottom"
+				showOnMobile
+			>
+				{ translate( 'Edit post' ) }
+			</Tooltip>
+		</>
 	);
 };
 
