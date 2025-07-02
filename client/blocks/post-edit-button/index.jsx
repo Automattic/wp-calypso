4ecb5167cf37bbea274531a3b374f82f6f58dfed
Jeremy Herve <jeremy@jeremy.hu>
2025-07-02 17:04:31 +0200

    Switch tooltip implementation to match comment icon

--- client/blocks/post-edit-button/index.jsx
+++ client/blocks/post-edit-button/index.jsx
@@ -1,7 +1,6 @@
-import { Gridicon, Tooltip } from '@automattic/components';
+import { Gridicon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import PropTypes from 'prop-types';
-import { useRef, useState } from 'react';
 import { getEditURL } from 'calypso/state/posts/utils';
 
 import './style.scss';
@@ -9,32 +8,17 @@ import './style.scss';
 const PostEditButton = ( { post, site, iconSize = 24, onClick } ) => {
 	const translate = useTranslate();
 	const editUrl = getEditURL( post, site );
-	const buttonRef = useRef( null );
-	const [ isTooltipVisible, setIsTooltipVisible ] = useState( false );
 
 	return (
-		<>
-			<a
-				className="post-edit-button"
-				href={ editUrl }
-				onClick={ onClick }
-				ref={ buttonRef }
-				onMouseEnter={ () => setIsTooltipVisible( true ) }
-				onMouseLeave={ () => setIsTooltipVisible( false ) }
-				onMouseDown={ () => setIsTooltipVisible( false ) }
-			>
-				<Gridicon icon="pencil" size={ iconSize } className="post-edit-button__icon" />
-				<span className="post-edit-button__label">{ translate( 'Edit post' ) }</span>
-			</a>
-			<Tooltip
-				context={ buttonRef.current }
-				isVisible={ isTooltipVisible }
-				position="bottom"
-				showOnMobile
-			>
-				{ translate( 'Edit post' ) }
-			</Tooltip>
-		</>
+		<a
+			className="post-edit-button tooltip"
+			href={ editUrl }
+			onClick={ onClick }
+			data-tooltip={ translate( 'Edit post' ) }
+		>
+			<Gridicon icon="pencil" size={ iconSize } className="post-edit-button__icon" />
+			<span className="post-edit-button__label">{ translate( 'Edit' ) }</span>
+		</a>
 	);
 };
 
