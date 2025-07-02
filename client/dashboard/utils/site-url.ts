412f2416034629cd24be252241159af55974a744
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-07-02 10:08:01 +0700

    Add docs

--- client/dashboard/utils/site-url.ts
+++ client/dashboard/utils/site-url.ts
@@ -1,8 +1,13 @@
 import type { Site } from '../data/types';
 
+/**
+ * Returns the site's URL without the protocol.
+ *
+ * This is useful for displaying the site's URL in a user-friendly way.
+ * Note that we cannot just return the URL's hostname, because the URL
+ * could contain the path to the site's subdirectory in case of multi-site
+ * installations.
+ */
 export function getSiteDisplayUrl( site: Site ) {
-	if ( site.URL === '' ) {
-		return site.URL;
-	}
 	return site.URL.replace( 'https://', '' ).replace( 'http://', '' );
 }
