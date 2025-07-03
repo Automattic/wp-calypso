731be8920c88035743b5c2f1156d6d96a836b70d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-07-03 12:48:25 +0200

    Rename main file to index.tsx

--- packages/domain-search/src/index.ts
+++ packages/domain-search/src/index.ts
@@ -1,5 +1,5 @@
 export { DomainSearch } from './components/DomainSearch/DomainSearch';
-export { DomainSearchControls } from './components/domain-search-controls/domain-search-controls';
+export { DomainSearchControls } from './components/domain-search-controls';
 export { DomainsMiniCart } from './components/DomainsMiniCart/DomainsMiniCart';
 export { DomainsFullCart } from './components/domains-full-cart';
 export * as DomainSuggestions from './components/DomainSuggestions';
