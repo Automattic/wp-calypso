22b97421161a9da83495fb84f77bdba703271b5e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-07-03 18:59:32 +0200

    Scafold the storybook stories

---
+++ packages/domain-search/src/components/domain-search-controls/index.stories.tsx
@@ -0,0 +1,30 @@
+import { DomainSearchControls } from '.';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta< typeof DomainSearchControls > = {
+	title: 'DomainSearchControls',
+	component: DomainSearchControls,
+	tags: [ 'autodocs' ],
+	parameters: {
+		layout: 'centered',
+	},
+};
+
+export default meta;
+type Story = StoryObj< typeof DomainSearchControls >;
+
+export const Default: Story = {
+	args: {},
+};
+
+export const WithoutFilters: Story = {
+	render: () => <DomainSearchControls.Input />,
+};
+
+export const FiltersOnly: Story = {
+	render: () => <DomainSearchControls.Filters />,
+};
+
+export const SubmitOnly: Story = {
+	render: () => <DomainSearchControls.Submit />,
+};
