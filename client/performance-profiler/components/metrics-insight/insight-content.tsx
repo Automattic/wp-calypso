dfe0c07f911c81b4ae643364aa40c633ef83015d
bcotrim <bernardo.cotrim@a8c.com>
2025-07-03 16:52:42 +0100

    change loading messages while generating recommendations

--- client/performance-profiler/components/metrics-insight/insight-content.tsx
+++ client/performance-profiler/components/metrics-insight/insight-content.tsx
@@ -60,6 +60,7 @@ export const InsightContent: React.FC< InsightContentProps > = ( props ) => {
 
 	/**
 	 * Updates the current message index and schedules the next message in the sequence.
+	 * It is used for rotating display message, while insights are fetched from the API.
 	 * Only schedules the next timer if loading is still active and there are more messages.
 	 * @param currentIndex - The index of the message to display
 	 */
