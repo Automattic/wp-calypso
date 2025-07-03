24f5313fe4c313634d04cb26258b3c56717b9519
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-07-03 18:39:26 +0200

    Adjust filters component

--- packages/domain-search/src/components/domain-search-controls/filters.tsx
+++ packages/domain-search/src/components/domain-search-controls/filters.tsx
@@ -1,6 +1,17 @@
 import { Button } from '@wordpress/components';
 import { funnel } from '@wordpress/icons';
+import { useI18n } from '@wordpress/react-i18n';
 
 export const DomainSearchFilters = () => {
-	return <Button icon={ funnel } variant="secondary" />;
+	const { __ } = useI18n();
+
+	return (
+		<Button
+			className="domain-search-controls__filters"
+			icon={ funnel }
+			variant="secondary"
+			label={ __( 'Filters' ) }
+			showTooltip
+		/>
+	);
 };
