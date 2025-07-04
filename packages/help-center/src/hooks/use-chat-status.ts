cb607a9fcbe181e7cdfef06d180261462208efcb
Omar Alshaker <omar@omaralshaker.com>
2025-07-04 14:25:07 +0200

    Block access to chat from the get go when ZD is blocked

--- packages/help-center/src/hooks/use-chat-status.ts
+++ packages/help-center/src/hooks/use-chat-status.ts
@@ -1,14 +1,17 @@
-import { isTestModeEnvironment } from '@automattic/zendesk-client';
+import { isTestModeEnvironment, useCanConnectToZendeskMessaging } from '@automattic/zendesk-client';
 import { useSupportActivity } from '../data/use-support-activity';
 import { useSupportStatus } from '../data/use-support-status';
 
 export default function useChatStatus() {
 	const { data: supportStatus } = useSupportStatus();
+	const { data: canConnectToZendesk } = useCanConnectToZendeskMessaging();
 	const availability = supportStatus?.availability;
 
 	// All paying customers are eligible for chat.
+	// They're only eligible if they can connect to Zendesk.
 	// See: pdDR7T-1vN-p2
-	const isEligibleForChat = Boolean( supportStatus?.eligibility?.is_user_eligible );
+	const isEligibleForChat =
+		Boolean( supportStatus?.eligibility?.is_user_eligible ) && canConnectToZendesk;
 
 	const { data: supportActivity, isInitialLoading: isLoadingSupportActivity } =
 		useSupportActivity( isEligibleForChat );
