6065d04ed7e9f708dc67fce1c687a98ffe5e6d43
Omar Alshaker <omar@omaralshaker.com>
2025-07-07 10:45:46 +0200

    Revert "Disable DV test to see if it's the culprit"
    This reverts commit 2711135cf5ebd2375dfd111e3c63124b8070768e.

--- packages/dataviews/src/test/filter-and-sort-data-view.js
+++ packages/dataviews/src/test/filter-and-sort-data-view.js
@@ -690,7 +690,7 @@ describe( 'filters', () => {
 		expect( result ).toStrictEqual( [ testData[ 1 ] ] );
 	} );
 
-	it.skip( 'should filter using IN_THE_PAST operator for datetime (years)', () => {
+	it( 'should filter using IN_THE_PAST operator for datetime (years)', () => {
 		const testData = [
 			{ title: 'Recent', date: subYears( new Date(), 1 ) },
 			{ title: 'Old', date: subYears( new Date(), 2 ) },
