3177a6ae9f42afab32bb17f75d026f80cba8762a
Dave Martin <davemart-in@users.noreply.github.com>
2025-07-07 09:55:12 -0400

    Implement survey in reader

--- client/reader/components/reader-main/index.jsx
+++ client/reader/components/reader-main/index.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
 import SyncReaderFollows from 'calypso/components/data/sync-reader-follows';
 import Main from 'calypso/components/main';
+import { getCurrentUserId } from 'calypso/state/current-user/selectors';
 import { ReaderPendingActionHandler } from './pending-action-handler';
 import './style.scss';
 
@@ -17,11 +19,44 @@ import './style.scss';
  * 3. dismount old ReaderMain from the first step (dismount, 1 ref)
  */
 let activeReaderMainRefCount = 0;
+let survicateScriptLoaded = false;
+
 const setIsReaderPage = ( add ) => {
 	if ( add ) {
 		document.querySelector( 'body' ).classList.add( 'is-reader-page' );
 	} else if ( activeReaderMainRefCount === 0 ) {
 		document.querySelector( 'body' ).classList.remove( 'is-reader-page' );
+	}
+};
+
+const loadSurvicateScript = ( userId ) => {
+	if ( survicateScriptLoaded || typeof window === 'undefined' ) {
+		return;
+	}
+
+	try {
+		// Define user IDs before the init of the tracking code
+		if ( userId ) {
+			( function ( opts ) {
+				opts.traits = {
+					user_id: userId.toString(),
+				};
+			} )( ( window._sva = window._sva || {} ) );
+		}
+
+		const script = document.createElement( 'script' );
+		script.src =
+			'https://survey.survicate.com/workspaces/e4794374cce15378101b63de24117572/web_surveys.js';
+		script.async = true;
+
+		const firstScript = document.getElementsByTagName( 'script' )[ 0 ];
+		if ( firstScript && firstScript.parentNode ) {
+			firstScript.parentNode.insertBefore( script, firstScript );
+			survicateScriptLoaded = true;
+		}
+	} catch ( error ) {
+		// eslint-disable-next-line no-console
+		console.warn( 'Failed to load Survicate script:', error );
 	}
 };
 
@@ -31,10 +66,11 @@ const setIsReaderPage = ( add ) => {
  * This class is used by pieces of the Reader to indicate they want "editorial" styles.
  * Notably, this overrides the background color of the document and is used as a hook by other parts to override styles.
  */
-export default class ReaderMain extends Component {
+class ReaderMain extends Component {
 	componentDidMount() {
 		activeReaderMainRefCount++;
 		setIsReaderPage( true );
+		loadSurvicateScript( this.props.userId );
 	}
 
 	componentWillUnmount() {
@@ -55,3 +91,7 @@ export default class ReaderMain extends Component {
 		);
 	}
 }
+
+export default connect( ( state ) => ( {
+	userId: getCurrentUserId( state ),
+} ) )( ReaderMain );
