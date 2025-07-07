e8be2782e9b5af36eff9a734e4df5b4b27fd1edd
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-07-07 16:00:31 +0200

    Add staging site management move info component with feature flag

---
+++ client/sites/staging-site/components/staging-site-management-move-info/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+const StagingSiteManagementMoveInfo: FunctionComponent = () => {
+	return <div>content will go here</div>;
+};
+
+export default StagingSiteManagementMoveInfo;
