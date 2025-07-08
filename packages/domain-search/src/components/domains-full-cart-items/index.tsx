1deec035228181a46fffc8ec10a993d91d8972d8
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-07-08 13:53:56 -0300

    Align remove link to the left

--- packages/domain-search/src/components/domains-full-cart-items/index.tsx
+++ packages/domain-search/src/components/domains-full-cart-items/index.tsx
@@ -21,7 +21,7 @@ export const DomainsFullCartItems = () => {
 				<Card key={ `${ domain.domain }.${ domain.tld }` }>
 					<CardBody size="small">
 						<HStack alignment="top" justify="space-between" spacing={ 6 }>
-							<VStack spacing={ 2 }>
+							<VStack spacing={ 2 } alignment="left">
 								<Text size="medium" aria-label={ `${ domain.domain }.${ domain.tld }` }>
 									{ domain.domain }
 									<Text size="inherit" weight={ 500 }>
