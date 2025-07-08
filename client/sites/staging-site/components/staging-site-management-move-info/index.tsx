90dfdb80335ff67727e82d0d6dc493ea0119cb3b
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2025-07-08 13:53:35 +0200

    Extract info card items into reusable component and data structure

--- client/sites/staging-site/components/staging-site-management-move-info/index.tsx
+++ client/sites/staging-site/components/staging-site-management-move-info/index.tsx
@@ -1,7 +1,6 @@
 import {
 	Card,
 	CardBody,
-	Icon,
 	__experimentalText as Text,
 	__experimentalVStack as VStack,
 	__experimentalGrid as Grid,
@@ -9,80 +8,76 @@ import {
 	__experimentalHStack as HStack,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { arrowUp, cog } from '@wordpress/icons';
+import { Icon, arrowUp, cog } from '@wordpress/icons';
 import { FunctionComponent } from 'react';
+
+interface InfoCardItem {
+	title: string;
+	description: string;
+	icon: Parameters< typeof Icon >[ 0 ][ 'icon' ];
+	location: string;
+}
+
+const infoCardItems: InfoCardItem[] = [
+	{
+		title: __( 'Create new staging site' ),
+		description: __(
+			'Create and manage staging environments using the button in the top navigation bar.'
+		),
+		icon: arrowUp,
+		location: __( 'Located in top navigation' ),
+	},
+	{
+		title: __( 'Switch environment' ),
+		description: __(
+			'Switch between production and staging environments using the environment switcher.'
+		),
+		icon: arrowUp,
+		location: __( 'Located in top navigation' ),
+	},
+	{
+		title: __( 'Delete staging site' ),
+		description: __( 'Remove staging sites and manage advanced configurations in the Settings.' ),
+		icon: cog,
+		location: __( 'Available in settings' ),
+	},
+	{
+		title: __( 'Try selective sync' ),
+		description: __( 'Sync specific files and folders, as well as database tables.' ),
+		icon: arrowUp,
+		location: __( 'Located in top navigation' ),
+	},
+];
+
+interface InfoCardProps {
+	item: InfoCardItem;
+}
+
+const InfoCard: FunctionComponent< InfoCardProps > = ( { item } ) => {
+	return (
+		<Card>
+			<CardBody>
+				<VStack>
+					<Text weight={ 500 }>{ item.title }</Text>
+					<Text>{ item.description }</Text>
+					<HStack alignment="left" spacing={ 1 }>
+						<Icon icon={ item.icon } size={ 16 } style={ { fill: '#757575' } } />
+						<Text variant="muted">{ item.location }</Text>
+					</HStack>
+				</VStack>
+			</CardBody>
+		</Card>
+	);
+};
 
 const StagingSiteManagementMoveInfo: FunctionComponent = () => {
 	return (
 		<Grid alignment="topLeft" columns={ 2 } gap={ 2 } style={ { maxWidth: '748px' } }>
-			<Item>
-				<Card>
-					<CardBody>
-						<VStack>
-							<Text weight={ 500 }>{ __( 'Create new staging site' ) }</Text>
-							<Text>
-								{ __(
-									'Create and manage staging environments using the button in the top navigation bar.'
-								) }
-							</Text>
-							<HStack alignment="left" spacing={ 1 }>
-								<Icon icon={ arrowUp } size={ 16 } style={ { fill: '#757575' } } />
-								<Text variant="muted">{ __( 'Located in top navigation' ) }</Text>
-							</HStack>
-						</VStack>
-					</CardBody>
-				</Card>
-			</Item>
-			<Item>
-				<Card>
-					<CardBody>
-						<VStack>
-							<Text weight={ 500 }>{ __( 'Switch environment' ) }</Text>
-							<Text>
-								{ __(
-									'Switch between production and staging environments using the environment switcher.'
-								) }
-							</Text>
-							<HStack alignment="left" spacing={ 1 }>
-								<Icon icon={ arrowUp } size={ 16 } style={ { fill: '#757575' } } />
-								<Text variant="muted">{ __( 'Located in top navigation' ) }</Text>
-							</HStack>
-						</VStack>
-					</CardBody>
-				</Card>
-			</Item>
-			<Item>
-				<Card>
-					<CardBody>
-						<VStack>
-							<Text weight={ 500 }>{ __( 'Delete staging site' ) }</Text>
-							<Text>
-								{ __( 'Remove staging sites and manage advanced configurations in the Settings.' ) }
-							</Text>
-							<HStack alignment="left" spacing={ 1 }>
-								<Icon icon={ cog } size={ 16 } style={ { fill: '#757575' } } />
-								<Text variant="muted">{ __( 'Available in settings' ) }</Text>
-							</HStack>
-						</VStack>
-					</CardBody>
-				</Card>
-			</Item>
-			<Item>
-				<Card>
-					<CardBody>
-						<VStack>
-							<Text weight={ 500 }>{ __( 'Try selective sync' ) }</Text>
-							<Text>
-								{ __( 'Sync specific files and folders, as well as well as database tables.' ) }
-							</Text>
-							<HStack alignment="left" spacing={ 1 }>
-								<Icon icon={ arrowUp } size={ 16 } style={ { fill: '#757575' } } />
-								<Text variant="muted">{ __( 'Located in top navigation' ) }</Text>
-							</HStack>
-						</VStack>
-					</CardBody>
-				</Card>
-			</Item>
+			{ infoCardItems.map( ( item, index ) => (
+				<Item key={ index }>
+					<InfoCard item={ item } />
+				</Item>
+			) ) }
 		</Grid>
 	);
 };
