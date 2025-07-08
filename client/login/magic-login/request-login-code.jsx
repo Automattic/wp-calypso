a63433ddedf868dded7eba33f607702665594c61
Christos <chriskmnds@gmail.com>
2025-07-08 20:19:35 +0300

    fix loopy context

--- client/login/magic-login/request-login-code.jsx
+++ client/login/magic-login/request-login-code.jsx
@@ -41,18 +41,21 @@ const RequestLoginCode = ( {
 	const [ usernameOrEmail, setUsernameOrEmail ] = useState( userEmail || '' );
 	const usernameOrEmailRef = useRef( null );
 	const { setHeaders } = useLoginContext();
+
 	useEffect( () => {
 		if ( onReady ) {
 			onReady();
 		}
+	}, [ onReady, setHeaders, translate ] );
 
+	useEffect( () => {
 		setHeaders( {
 			heading: translate( 'Email me a login code' ),
 			subHeading: translate(
 				"We'll send you an email with a code that will log you in right away."
 			),
 		} );
-	}, [ onReady, setHeaders, translate ] );
+	}, [] );
 
 	const onUsernameOrEmailFieldChange = ( event ) => {
 		setUsernameOrEmail( event.target.value );
