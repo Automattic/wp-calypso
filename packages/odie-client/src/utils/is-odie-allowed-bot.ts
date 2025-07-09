119c82f076d45801d79b145f918ada97b6b17aa3
Omar Alshaker <omar@omaralshaker.com>
2025-07-09 10:51:59 +0200

    Discard changes to packages/odie-client/src/utils/is-odie-allowed-bot.ts

--- packages/odie-client/src/utils/is-odie-allowed-bot.ts
+++ packages/odie-client/src/utils/is-odie-allowed-bot.ts
@@ -1,11 +1,8 @@
 import { ODIE_ALLOWED_BOTS } from '../constants';
 import type { OdieAllowedBots } from '../types';
 
-/**
- * Checks if a bot is allowed based on its name slug.
- * @param {string | undefined} botNameSlug - The slug of the bot to check.
- * @returns {boolean} - True if the bot is allowed, false otherwise.
- */
-export const isOdieAllowedBot = ( botNameSlug: string | undefined ): boolean => {
+export function isOdieAllowedBot(
+	botNameSlug: string | undefined
+): botNameSlug is OdieAllowedBots {
 	return !! ( botNameSlug && ODIE_ALLOWED_BOTS.includes( botNameSlug as OdieAllowedBots ) );
-};
+}
