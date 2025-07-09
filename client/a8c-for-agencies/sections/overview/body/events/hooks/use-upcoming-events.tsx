98d6ec882b7e6e840b892ccce9fc9d2691dced94
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-07-09 18:48:26 +0800

    Add Woo and A4A July 25th event tracker on Overview page.

--- client/a8c-for-agencies/sections/overview/body/events/hooks/use-upcoming-events.tsx
+++ client/a8c-for-agencies/sections/overview/body/events/hooks/use-upcoming-events.tsx
@@ -2,8 +2,8 @@ import { useTranslate } from 'i18n-calypso';
 import moment from 'moment';
 import { useMemo } from 'react';
 import { UpcomingEventProps } from 'calypso/a8c-for-agencies/components/upcoming-event/types';
-import klaviyoEventImage from 'calypso/assets/images/a8c-for-agencies/events/klaviyo-compliment-image.svg';
-import klaviyoLogo from 'calypso/assets/images/a8c-for-agencies/events/klaviyo-logo.svg';
+import a4aEventImage from 'calypso/assets/images/a8c-for-agencies/events/a4a-compliment-image.svg';
+import a4aLogo from 'calypso/assets/images/a8c-for-agencies/events/a4a-logo.svg';
 import wooEventImage from 'calypso/assets/images/a8c-for-agencies/events/woo-compliment-image.svg';
 import wooLogo from 'calypso/assets/images/a8c-for-agencies/events/woo-logo.svg';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
@@ -15,50 +15,66 @@ export const useUpcomingEvents = () => {
 	return useMemo( () => {
 		const eventsData: UpcomingEventProps[] = [
 			{
-				id: 'klaviyo-woocommerce-2025-06-12',
+				id: 'woo-2025-07-25',
 				date: {
-					from: moment( '2025-06-12' ),
-					to: moment( '2025-06-12' ),
+					from: moment( '2025-07-25' ),
+					to: moment( '2025-07-25' ),
 				},
-				title: translate( 'Grow your agency with Klaviyo and WooCommerce' ),
-				subtitle: translate( 'Automattic for Agencies / Klaviyo webinar' ),
+				title: translate( 'How are you preparing for the busiest time of year?' ),
+				subtitle: translate( 'Woo and Automattic for Agencies' ),
 				descriptions: [
 					translate(
-						"Learn how to leverage Klaviyo’s marketing tools with WooCommerce to grow your clients’ stores. Guided by industry experts, explore seamless integration and effective strategies designed to enhance your agency's ability to drive client success."
+						"As peak selling season approaches, Woo and Automattic for Agencies are conducting a survey to better understand how agencies are preparing for the busiest time of year. The insights gathered will inform actionable recommendations for Woo merchants and will be featured in upcoming thought leadership content on both Woo's website and the Automattic for Agencies blog."
+					),
+					translate(
+						'{{b}}Select agencies may also be highlighted as thought leaders in these publications, showcasing your expertise to a wider audience.{{/b}}',
+						{
+							components: {
+								b: <b />,
+							},
+						}
 					),
 				],
 				cta: {
-					label: translate( 'Register for free ↗' ),
-					url: 'https://us02web.zoom.us/webinar/register/4117473222555/WN_tp0MS84_TYyp4NP9XMmRfQ#/registration',
-				},
-				logoUrl: klaviyoLogo,
-				imageUrl: klaviyoEventImage,
-				trackEventName: 'calypso_a4a_overview_events_register_click_klaviyo_woocommerce_2025_06_12',
-				dateClassName: 'a4a-event__date--klaviyo',
-				imageClassName: 'a4a-event__image--klaviyo',
-			},
-			{
-				id: 'woo-marketing-2025-06-25',
-				date: {
-					from: moment( '2025-06-25' ),
-					to: moment( '2025-06-25' ),
-				},
-				title: translate( "You're invited to Step Inside: Woo Marketing" ),
-				subtitle: translate( 'Woo Marketing + Automattic for Agencies event' ),
-				descriptions: [
-					translate(
-						"Woo is extending an invitation to Automattic for Agencies partners to join a live conversation with CMO Tamara Niesen and VP Customer Marketing Mahrie Boyle about WooCommerce's future marketing direction. Learn about the recent brand refresh, upcoming 2025 advertising campaigns, demand generation, and how Woo plans to collaborate closely with the community to grow the ecosystem."
-					),
-				],
-				cta: {
-					label: translate( 'Register for free ↗' ),
-					url: 'https://developer.woocommerce.com/youre-invited-to-step-inside-woo-marketing/',
+					label: translate( 'Share your insights ↗' ),
+					url: 'https://2yp7yqymm1z.typeform.com/to/YC4Yw2VI',
 				},
 				logoUrl: wooLogo,
 				imageUrl: wooEventImage,
-				trackEventName: 'calypso_a4a_overview_events_register_click_woo_marketing_2025_06_25',
+				trackEventName: 'calypso_a4a_overview_events_register_click_woo_marketing_2025_07_25',
 				dateClassName: 'a4a-event__date--woo',
 				imageClassName: 'a4a-event__image--woo',
+			},
+			{
+				id: 'a4a-2025-07-25',
+				date: {
+					from: moment( '2025-07-25' ),
+					to: moment( '2025-07-25' ),
+				},
+				title: translate( 'Automattic for Agencies Partner Empowerment Survey' ),
+				subtitle: translate( 'Automattic for Agencies' ),
+				descriptions: [
+					translate(
+						"Participate in the Automattic for Agencies Partner Empowerment Survey and directly influence the training and resources we develop to better support your agency's success with WordPress and Automattic products. Your feedback is essential in helping us understand your needs and identify opportunities for improvement."
+					),
+					translate(
+						'{{b}}Share your insights by July 25th{{/b}} and play a key role in shaping our future offerings. Together, we can ensure that our programs and resources are tailored to help your agency thrive!',
+						{
+							components: {
+								b: <b />,
+							},
+						}
+					),
+				],
+				cta: {
+					label: translate( 'Share your insights ↗' ),
+					url: 'https://automattic.survey.fm/automattic-for-agencies-partner-empowerment-survey',
+				},
+				logoUrl: a4aLogo,
+				imageUrl: a4aEventImage,
+				trackEventName: 'calypso_a4a_overview_events_register_click_a4a_2025_07_25',
+				dateClassName: 'a4a-event__date--a4a',
+				imageClassName: 'a4a-event__image--a4a',
 			},
 		];
 
