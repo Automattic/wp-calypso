6724833c755580ef55175023e71a6a305c1f4569
Copons <Copons@users.noreply.github.com>
2025-07-09 16:09:33 +0100

    Add back the unsupported theme card

--- client/my-sites/theme/theme-support-tab/index.jsx
+++ client/my-sites/theme/theme-support-tab/index.jsx
@@ -4,9 +4,11 @@ import { localizeUrl } from '@automattic/i18n-utils';
 import { Button } from '@wordpress/components';
 import { useDispatch as useDataStoreDispatch } from '@wordpress/data';
 import { useTranslate } from 'i18n-calypso';
+import useSupportDocData from 'calypso/components/inline-support-link/use-support-doc-data';
 import { useDispatch, useSelector } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { useThemeTierForTheme } from 'calypso/state/themes/hooks/use-theme-tier-for-theme';
 
 import './style.scss';
 
@@ -17,6 +19,9 @@ export default function ThemeSupportTab( { themeId } ) {
 	const isLoggedIn = useSelector( isUserLoggedIn );
 	const { setNavigateToOdie, setShowHelpCenter, setNavigateToRoute } =
 		useDataStoreDispatch( HELP_CENTER_STORE );
+
+	const themeTier = useThemeTierForTheme( themeId );
+	const { openSupportDoc } = useSupportDocData( { supportContext: 'themes-unsupported' } );
 
 	return (
 		<>
@@ -77,32 +82,59 @@ export default function ThemeSupportTab( { themeId } ) {
 						</Button>
 					</Card>
 
-					<Card className="theme__sheet-card-support">
-						<Gridicon icon="comment" size={ 48 } />
-						<div className="theme__sheet-card-support-details">
-							{ translate( 'Contact support' ) }
-							<small>
-								{ translate(
-									'Get answers from our AI assistant, with access to 24/7 expert human support on paid plans.'
-								) }
-							</small>
-						</div>
-						<Button
-							__next40pxDefaultSize
-							onClick={ () => {
-								setNavigateToOdie();
-								dispatch(
-									recordTracksEvent( 'calypso_theme_sheet_button_click', {
-										theme_name: themeId,
-										button_context: 'help-center-ai',
-									} )
-								);
-							} }
-							variant="secondary"
-						>
-							{ translate( 'Get in touch' ) }
-						</Button>
-					</Card>
+					{ themeTier?.slug === 'community' || themeTier?.slug === 'partner' ? (
+						<Card className="theme__sheet-card-support">
+							<Gridicon icon="notice-outline" size={ 48 } />
+							<div className="theme__sheet-card-support-details">
+								{ translate( 'Help and support for this theme is not offered by WordPress.com.' ) }
+								<small>
+									{ translate( 'Contact the theme developer directly for help with this theme.' ) }
+								</small>
+							</div>
+							<Button
+								__next40pxDefaultSize
+								onClick={ () => {
+									openSupportDoc();
+									dispatch(
+										recordTracksEvent( 'calypso_theme_sheet_button_click', {
+											theme_name: themeId,
+											button_context: 'themes-unsupported',
+										} )
+									);
+								} }
+								variant="secondary"
+							>
+								{ translate( 'Learn more' ) }
+							</Button>
+						</Card>
+					) : (
+						<Card className="theme__sheet-card-support">
+							<Gridicon icon="comment" size={ 48 } />
+							<div className="theme__sheet-card-support-details">
+								{ translate( 'Contact support' ) }
+								<small>
+									{ translate(
+										'Get answers from our AI assistant, with access to 24/7 expert human support on paid plans.'
+									) }
+								</small>
+							</div>
+							<Button
+								__next40pxDefaultSize
+								onClick={ () => {
+									setNavigateToOdie();
+									dispatch(
+										recordTracksEvent( 'calypso_theme_sheet_button_click', {
+											theme_name: themeId,
+											button_context: 'help-center-ai',
+										} )
+									);
+								} }
+								variant="secondary"
+							>
+								{ translate( 'Get in touch' ) }
+							</Button>
+						</Card>
+					) }
 				</>
 			) }
 		</>
