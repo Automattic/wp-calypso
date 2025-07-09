26c56ef52cf7a9bd8c0b5bed2103253afb956d6b
escapemanuele <escapemanuele@gmail.com>
2025-07-09 15:35:22 +0200

    Search card tracking events

--- apps/happy-blocks/block-library/search-card/view.js
+++ apps/happy-blocks/block-library/search-card/view.js
@@ -1,8 +1,11 @@
 import './style.scss';
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+
 document.addEventListener( 'DOMContentLoaded', function () {
 	const links = document.querySelectorAll( 'a[data-search-query]' );
 	const input = document.getElementById( 'support-search-input' );
 	const submitButton = document.querySelector( '.search-submit-button' );
+	const form = document.getElementById( 'support-search-form' );
 
 	links.forEach( ( link ) => {
 		link.addEventListener( 'click', function ( e ) {
@@ -10,6 +13,11 @@ document.addEventListener( 'DOMContentLoaded', function () {
 			if ( ! input || ! query || ! submitButton ) {
 				return;
 			}
+
+			recordTracksEvent( 'calypso_happyblocks_support_suggested_search', {
+				query,
+				location: window.location.href,
+			} );
 
 			e.preventDefault();
 
@@ -19,6 +27,13 @@ document.addEventListener( 'DOMContentLoaded', function () {
 			setTimeout( () => {
 				input.value = '';
 			}, 100 );
+		} );
+	} );
+
+	form.addEventListener( 'submit', function () {
+		recordTracksEvent( 'calypso_happyblocks_support_custom_search', {
+			query: input.value,
+			location: window.location.href,
 		} );
 	} );
 } );
