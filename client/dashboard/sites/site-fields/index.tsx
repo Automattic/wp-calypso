7297b0fdf9002b62b99f9cc17aec2ff7fac8fd71
Griffith Chen <griffith.chen@automattic.com>
2025-07-10 09:12:21 +0800

    Update label from Coming Soon to Prepare for Launch when linking to Launchpad

--- client/dashboard/sites/site-fields/index.tsx
+++ client/dashboard/sites/site-fields/index.tsx
@@ -268,7 +268,7 @@ function SiteLaunchNag( { site }: { site: Site } ) {
 					recordTracksEvent( 'calypso_dashboard_sites_site_launch_nag_click' );
 				} }
 			>
-				{ getSiteStatusLabel( site ) }
+				{ __( 'Prepare for launch' ) }
 			</ExternalLink>
 		</>
 	);
