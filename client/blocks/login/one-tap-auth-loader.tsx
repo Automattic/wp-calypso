800e844a5c7ecbbec44be5e7fbe8bc32c4b30744
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-07-14 16:14:11 +0200

    Remove leftover from the previous try

--- client/blocks/login/one-tap-auth-loader.tsx
+++ client/blocks/login/one-tap-auth-loader.tsx
@@ -6,7 +6,7 @@ import './one-tap-auth-loader.scss';
 export default function OneTapAuthLoader() {
 	return (
 		<div className="one-tap-auth-loader">
-			<Step.TopBar className="one-tap-auth-loader__top-bar" compactLogo />
+			<Step.TopBar compactLogo />
 			<ProgressBar className="one-tap-auth-loader__progress-bar" />
 		</div>
 	);
