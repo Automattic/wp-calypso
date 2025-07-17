6257906bd2785c1050003cf67fbd4fe04781fbe4
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-07-17 13:02:06 +0200

    Translate subdomain skip

--- client/components/domain-search-v2/subdomain-skip/index.tsx
+++ client/components/domain-search-v2/subdomain-skip/index.tsx
@@ -5,23 +5,38 @@ import {
 	__experimentalText as Text,
 	__experimentalHeading as Heading,
 } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
 
 import './style.scss';
 
-const SubdomainSkip = () => {
+interface Props {
+	subdomain?: string;
+	onSkip: () => void;
+}
+
+const SubdomainSkip = ( { subdomain, onSkip }: Props ) => {
+	const translate = useTranslate();
+
 	return (
 		<Card className="subdomain-skip">
 			<CardBody>
 				<div className="subdomain-skip__content">
 					<Heading level="4" weight="normal">
-						WordPress.com subdomain
+						{ translate( 'WordPress.com subdomain' ) }
 					</Heading>
 					<Text>
-						thalassolasso.<strong>wordpress.com</strong> is included
+						{ translate( '%(subdomain)s.{{strong}}wordpress.com{{/strong}} is included', {
+							args: {
+								subdomain: subdomain ?? 'wordpress.com',
+							},
+							components: {
+								strong: <strong />,
+							},
+						} ) }
 					</Text>
 				</div>
-				<Button className="subdomain-skip__btn" variant="secondary">
-					Skip purchase
+				<Button className="subdomain-skip__btn" variant="secondary" onClick={ onSkip }>
+					{ translate( 'Skip purchase' ) }
 				</Button>
 			</CardBody>
 		</Card>
