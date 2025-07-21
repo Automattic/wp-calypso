aad43e34d3384b28761b76809ca139fbfc777ca6
arthur <arthur.chu@automattic.com>
2025-07-21 19:37:34 +0800

    Preload queries

--- client/sites/v2/site-overview/router.tsx
+++ client/sites/v2/site-overview/router.tsx
@@ -10,8 +10,14 @@ import {
 	redirect,
 } from '@tanstack/react-router';
 import { siteBySlugQuery } from 'calypso/dashboard/app/queries/site';
+import { siteLastBackupQuery } from 'calypso/dashboard/app/queries/site-backups';
+import { siteCurrentPlanQuery } from 'calypso/dashboard/app/queries/site-plans';
+import { sitePreviewLinksQuery } from 'calypso/dashboard/app/queries/site-preview-links';
+import { siteScanQuery } from 'calypso/dashboard/app/queries/site-scan';
 import { queryClient } from 'calypso/dashboard/app/query-client';
+import { DotcomFeatures } from 'calypso/dashboard/data/constants';
 import { canManageSite } from 'calypso/dashboard/sites/features';
+import { hasHostingFeature } from 'calypso/dashboard/utils/site-features';
 import Root from '../components/root';
 import { getRouterOptions, createBrowserHistoryAndMemoryRouterSync } from '../utils/router';
 
@@ -32,6 +38,19 @@ const siteRoute = createRoute( {
 const siteOverviewRoute = createRoute( {
 	getParentRoute: () => siteRoute,
 	path: '/',
+	loader: async ( { params: { siteSlug }, preload } ) => {
+		const site = await queryClient.ensureQueryData( siteBySlugQuery( siteSlug ) );
+		if ( preload ) {
+			Promise.all( [
+				queryClient.ensureQueryData( siteCurrentPlanQuery( site.ID ) ),
+				hasHostingFeature( site, DotcomFeatures.SCAN ) &&
+					queryClient.ensureQueryData( siteScanQuery( site.ID ) ),
+				hasHostingFeature( site, DotcomFeatures.BACKUPS ) &&
+					queryClient.ensureQueryData( siteLastBackupQuery( site.ID ) ),
+				site.is_a4a_dev_site && queryClient.ensureQueryData( sitePreviewLinksQuery( site.ID ) ),
+			] );
+		}
+	},
 } ).lazy( () =>
 	import( 'calypso/dashboard/sites/overview' ).then( ( d ) =>
 		createLazyRoute( 'overview' )( {
