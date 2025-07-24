57da404c5a39f4fb94f9e125b03e568a2e55419f
Yashwin <yashwinpoojary@gmail.com>
2025-07-24 13:06:36 +0530

    A4A > Referrals: Handle BD referral commissions

--- client/a8c-for-agencies/sections/referrals/lib/get-estimated-commission.ts
+++ client/a8c-for-agencies/sections/referrals/lib/get-estimated-commission.ts
@@ -19,60 +19,87 @@ export const getEstimatedCommission = (
 	products: APIProductFamilyProduct[],
 	activityWindow: { start: Date; finish: Date }
 ) => {
-	const totalCommissionInCents = referrals.reduce( ( acc, referral ) => {
-		if ( ! referral?.purchases?.length ) {
+	const { totalCommissionsFromSubscriptions, otherCommissionsInCents } = referrals.reduce(
+		( acc, referral ) => {
+			if ( ! referral?.purchases?.length ) {
+				return acc;
+			}
+			for ( const purchase of referral.purchases ) {
+				// We go over 'active' and 'cancelled' subscriptions
+				if ( ! purchase || purchase.status === 'pending' || purchase.status === 'error' ) {
+					continue;
+				}
+				// Find corresponding product for price
+				const product = products.find( ( product ) => purchase.product_id === product.product_id );
+				if ( ! product ) {
+					continue;
+				}
+
+				// Get commission percentage for the product (common for both subscription and license)
+				const commissionPercentage = getProductCommissionPercentage( product.family_slug );
+
+				// Day the license was issued
+				const issuedDate = new Date( purchase.license.issued_at );
+				// Set hours to 0 to compare from start of the day
+				issuedDate.setHours( 0, 0, 0, 0 );
+
+				if ( purchase.subscription ) {
+					if ( purchase.subscription.status !== 'active' ) {
+						continue;
+					}
+
+					// Skip if subscription started outside the activity window
+					if (
+						issuedDate.getTime() < activityWindow.start.getTime() ||
+						issuedDate.getTime() > activityWindow.finish.getTime()
+					) {
+						continue;
+					}
+
+					// FIXME: We will only consider USD price. This is just for testing
+					const totalPrice =
+						purchase.subscription.purchase_price_usd ?? purchase.subscription.purchase_price;
+
+					// Add commission to subscription total (in dollars)
+					acc.totalCommissionsFromSubscriptions += totalPrice * commissionPercentage;
+				} else {
+					// Day the license was revoked if present
+					const revokedAt = purchase.license.revoked_at
+						? new Date( purchase.license.revoked_at )
+						: null;
+
+					// Start date is the latest of the license issued date and activity window start
+					const start = Math.max( issuedDate.getTime(), activityWindow.start.getTime() );
+					// Finish date is the earliest of the license revoked date and activity window finish
+					const finish = Math.min(
+						revokedAt ? revokedAt.getTime() : Infinity,
+						activityWindow.finish.getTime()
+					);
+
+					// Total days is the difference between finish and start dates in days
+					// We add 1 to include end-to-end days
+					const totalDays = Math.floor( ( finish - start ) / ( 1000 * 60 * 60 * 24 ) ) + 1;
+
+					if ( totalDays < 1 ) {
+						continue;
+					}
+
+					const dailyPrice = getDailyPrice( product, purchase.quantity );
+
+					// Add commission to the total commission (in cents)
+					acc.otherCommissionsInCents += dailyPrice * totalDays * commissionPercentage;
+				}
+			}
 			return acc;
-		}
-		for ( const purchase of referral.purchases ) {
-			// We go over 'active' and 'cancelled' subscriptions
-			if ( ! purchase || purchase.status === 'pending' || purchase.status === 'error' ) {
-				continue;
-			}
-			// Find corresponding product for price
-			const product = products.find( ( product ) => purchase.product_id === product.product_id );
-			if ( ! product ) {
-				continue;
-			}
+		},
+		{ totalCommissionsFromSubscriptions: 0, otherCommissionsInCents: 0 }
+	);
 
-			// Day the license was issued
-			const issuedDate = new Date( purchase.license.issued_at );
-			// Set hours to 0 to compare from start of the day
-			issuedDate.setHours( 0, 0, 0, 0 );
-
-			// Day the license was revoked if present
-			const revokedAt = purchase.license.revoked_at
-				? new Date( purchase.license.revoked_at )
-				: null;
-
-			// Start date is the latest of the license issued date and activity window start
-			const start = Math.max( issuedDate.getTime(), activityWindow.start.getTime() );
-			// Finish date is the earliest of the license revoked date and activity window finish
-			const finish = Math.min(
-				revokedAt ? revokedAt.getTime() : Infinity,
-				activityWindow.finish.getTime()
-			);
-
-			// Total days is the difference between finish and start dates in days
-			// We add 1 to include end-to-end days
-			const totalDays = Math.floor( ( finish - start ) / ( 1000 * 60 * 60 * 24 ) ) + 1;
-
-			if ( totalDays < 1 ) {
-				continue;
-			}
-
-			const dailyPrice = getDailyPrice( product, purchase.quantity );
-
-			// Get commission percentage for the product
-			const commissionPercentage = getProductCommissionPercentage( product.family_slug );
-
-			// Calculate commission for the day
-			acc += dailyPrice * totalDays * commissionPercentage;
-		}
-		return acc;
-	}, 0 );
-
-	// Convert commission to dollars and round
-	const totalCommission = Number( ( totalCommissionInCents / 100 ).toFixed( 2 ) );
+	// Convert commission from cents to dollars,
+	// add subscriptions commission and round to 2 decimal places
+	const totalCommission = Number(
+		( totalCommissionsFromSubscriptions + otherCommissionsInCents / 100 ).toFixed( 2 )
+	);
 
 	return totalCommission;
 };
