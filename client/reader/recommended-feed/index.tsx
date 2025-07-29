f170a26e1c6a6d26b14d02ffe2570f1bef43a777
Gabriel Caires <gabriel.caires@automattic.com>
2025-07-29 12:42:58 +0100

    Fix follow button style

--- client/reader/recommended-feed/index.tsx
+++ client/reader/recommended-feed/index.tsx
@@ -84,6 +84,7 @@ export const RecommendedFeed = ( {
 
 			{ feedUrl && (
 				<ReaderFollowButton
+					className={ `${ classPrefix }__recommended-blog-subscribe-button` }
 					siteUrl={ feedUrl }
 					followSource={ `${ classPrefix }__recommended-blog-item` }
 					isButtonOnly={ compact }
