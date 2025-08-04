ae81a122b3b9394693381249050b10cfdea5d420
arthur <arthur.chu@automattic.com>
2025-08-04 18:11:17 +0800

    Don't use select as ensureQueryData doesn't support it

--- client/dashboard/app/queries/site-plans.ts
+++ client/dashboard/app/queries/site-plans.ts
@@ -4,9 +4,9 @@ import { fetchSitePlans } from '../../data/site-plans';
 
 export const siteCurrentPlanQuery = ( siteId: number ) =>
 	queryOptions( {
-		queryKey: [ 'site', siteId, 'plans' ],
-		queryFn: () => fetchSitePlans( siteId ),
-		select: ( plans ) => {
+		queryKey: [ 'site', siteId, 'plans', 'current' ],
+		queryFn: async () => {
+			const plans = await fetchSitePlans( siteId );
 			const plan = Object.values( plans ).find( ( plan ) => plan.current_plan );
 			if ( ! plan ) {
 				throw new Error( 'No current plan found' );
@@ -17,9 +17,9 @@ export const siteCurrentPlanQuery = ( siteId: number ) =>
 
 export const sitePlanBySlugQuery = ( siteId: number, productSlug: string ) =>
 	queryOptions( {
-		queryKey: [ 'site', siteId, 'plans' ],
-		queryFn: () => fetchSitePlans( siteId ),
-		select: ( plans ) => {
+		queryKey: [ 'site', siteId, 'plans', productSlug ],
+		queryFn: async () => {
+			const plans = await fetchSitePlans( siteId );
 			const plan = Object.values( plans ).find( ( plan ) => plan.product_slug === productSlug );
 			if ( ! plan ) {
 				throw new Error( `The plan ${ productSlug } cannot be found` );
