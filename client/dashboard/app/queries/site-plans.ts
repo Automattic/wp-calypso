76a8a5d69e38fbb85d1428ebcc57456dd07b50bc
arthur <arthur.chu@automattic.com>
2025-08-04 15:25:48 +0800

    Replace fetchCurrentSitePlan with fetchSitePlans

--- client/dashboard/app/queries/site-plans.ts
+++ client/dashboard/app/queries/site-plans.ts
@@ -1,17 +1,31 @@
 import { mutationOptions, queryOptions } from '@tanstack/react-query';
 import { restoreSitePlanSoftware } from '../../data/site-hosting';
-import { fetchCurrentSitePlan, fetchSitePlanBySlug } from '../../data/site-plans';
+import { fetchSitePlans } from '../../data/site-plans';
 
 export const siteCurrentPlanQuery = ( siteId: number ) =>
 	queryOptions( {
-		queryKey: [ 'site', siteId, 'plans', 'current' ],
-		queryFn: () => fetchCurrentSitePlan( siteId ),
+		queryKey: [ 'site', siteId, 'plans' ],
+		queryFn: () => fetchSitePlans( siteId ),
+		select: ( plans ) => {
+			const plan = Object.values( plans ).find( ( plan ) => plan.current_plan );
+			if ( ! plan ) {
+				throw new Error( 'No current plan found' );
+			}
+			return plan;
+		},
 	} );
 
 export const sitePlanBySlugQuery = ( siteId: number, productSlug: string ) =>
 	queryOptions( {
-		queryKey: [ 'site', siteId, 'plans', productSlug ],
-		queryFn: () => fetchSitePlanBySlug( siteId, productSlug ),
+		queryKey: [ 'site', siteId, 'plans' ],
+		queryFn: () => fetchSitePlans( siteId ),
+		select: ( plans ) => {
+			const plan = Object.values( plans ).find( ( plan ) => plan.product_slug === productSlug );
+			if ( ! plan ) {
+				throw new Error( `The plan ${ productSlug } cannot be found` );
+			}
+			return plan;
+		},
 	} );
 
 export const sitePlanSoftwareRestoreMutation = ( siteId: number ) =>
--- client/dashboard/data/site-plans.ts
+++ client/dashboard/data/site-plans.ts
@@ -14,26 +14,9 @@ export interface Plan {
 	user_facing_expiry?: string;
 }
 
-export async function fetchCurrentSitePlan( siteId: number ): Promise< Plan > {
-	const plans: Record< string, Plan > = await wpcom.req.get( {
+export function fetchSitePlans( siteId: number ): Promise< Record< string, Plan > > {
+	return wpcom.req.get( {
 		path: `/sites/${ siteId }/plans`,
 		apiVersion: '1.3',
 	} );
-	const plan = Object.values( plans ).find( ( plan ) => plan.current_plan );
-	if ( ! plan ) {
-		throw new Error( 'No current plan found' );
-	}
-	return plan;
-}
-
-export async function fetchSitePlanBySlug( siteId: number, productSlug: string ): Promise< Plan > {
-	const plans: Record< string, Plan > = await wpcom.req.get( {
-		path: `/sites/${ siteId }/plans`,
-		apiVersion: '1.3',
-	} );
-	const plan = Object.values( plans ).find( ( plan ) => plan.product_slug === productSlug );
-	if ( ! plan ) {
-		throw new Error( `The plan ${ productSlug } cannot be found` );
-	}
-	return plan;
 }
