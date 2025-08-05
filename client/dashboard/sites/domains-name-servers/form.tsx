e25e0d9b1fea697a02a1436c3166d0a1391dd297
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-05 19:01:43 +0200

    Create simple form component

---
+++ client/dashboard/sites/domains-name-servers/form.tsx
@@ -0,0 +1,94 @@
+import {
+	Button,
+	__experimentalView as View,
+	__experimentalVStack as VStack,
+	__experimentalHStack as HStack,
+	__experimentalInputControl as InputControl,
+	CheckboxControl,
+	__experimentalText as Text,
+} from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { useState } from 'react';
+
+const MIN_NAMESERVER_LENGTH = 2;
+const MAX_NAMESERVER_LENGTH = 7;
+
+export default function NameServersForm() {
+	const [ useCustomNameServers, setUseCustomNameServers ] = useState( true );
+	const [ nameServers, setNameServers ] = useState< string[] >(
+		new Array( MAX_NAMESERVER_LENGTH ).fill( '' )
+	);
+
+	const handleNameServerChange = ( index: number, value: string ) => {
+		const newNameServers = [ ...nameServers ];
+		newNameServers[ index ] = value;
+		setNameServers( newNameServers );
+	};
+
+	const shouldShowNextInput = ( index: number ) => {
+		if ( index >= MAX_NAMESERVER_LENGTH ) {
+			return false;
+		}
+		if ( index < MIN_NAMESERVER_LENGTH ) {
+			return true;
+		}
+		return nameServers[ index - 1 ] !== ''; // Show next if previous has value
+	};
+
+	return (
+		<VStack spacing={ 4 }>
+			<Text>
+				<CheckboxControl
+					label={ __( 'Use custom name servers' ) }
+					checked={ useCustomNameServers }
+					onChange={ () => setUseCustomNameServers( ! useCustomNameServers ) }
+				/>
+			</Text>
+			{ Array.from( { length: Math.ceil( MAX_NAMESERVER_LENGTH / 2 ) }, ( _, i ) => i * 2 ).map(
+				( rowIndex ) => {
+					// Check if either input in this row would be shown
+					const shouldShowRow =
+						shouldShowNextInput( rowIndex ) || shouldShowNextInput( rowIndex + 1 );
+
+					return (
+						shouldShowRow && (
+							<HStack key={ rowIndex } spacing={ 4 } justify="space-between" alignment="top">
+								{ [ 0, 1 ].map( ( colIndex ) => {
+									const index = rowIndex + colIndex;
+									return (
+										shouldShowNextInput( index ) && (
+											<div className="nameserver-input-container">
+												<InputControl
+													key={ index }
+													__next40pxDefaultSize
+													disabled={ ! useCustomNameServers }
+													// translators: sd is the name server number
+													label={ sprintf( __( 'Custom name server %s' ), index + 1 ) }
+													placeholder={
+														index < MIN_NAMESERVER_LENGTH
+															? // translators: s% is the name server number
+															  sprintf( __( 'ns%s.domain.com' ), index + 1 )
+															: // translators: s% is the name server number
+															  sprintf( __( 'ns%s.domain.com (optional)' ), index + 1 )
+													}
+													value={ nameServers[ index ] }
+													onChange={ ( value ) => handleNameServerChange( index, value as string ) }
+												/>
+												<Text className="is-error">This is an error message example</Text>
+											</div>
+										)
+									);
+								} ) }
+							</HStack>
+						)
+					);
+				}
+			) }
+			<View>
+				<Button variant="primary" disabled={ ! useCustomNameServers }>
+					{ __( 'Save' ) }
+				</Button>
+			</View>
+		</VStack>
+	);
+}
--- client/dashboard/sites/domains-name-servers/index.tsx
+++ client/dashboard/sites/domains-name-servers/index.tsx
@@ -1,17 +1,8 @@
-import {
-	Button,
-	Card,
-	CardBody,
-	__experimentalView as View,
-	__experimentalVStack as VStack,
-	__experimentalHStack as HStack,
-	__experimentalInputControl as InputControl,
-	CheckboxControl,
-	__experimentalText as Text,
-} from '@wordpress/components';
+import { Card, CardBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
+import NameServersForm from './form';
 import './styles.scss';
 
 export default function NameServers() {
@@ -19,32 +10,7 @@ export default function NameServers() {
 		<PageLayout size="small" header={ <PageHeader title={ __( 'Name Servers' ) } /> }>
 			<Card>
 				<CardBody className="domains-management__name-servers">
-					<VStack spacing={ 4 }>
-						<Text>
-							<CheckboxControl
-								label={ __( 'Use custom name servers' ) }
-								checked={ false }
-								onChange={ () => {} }
-							/>
-						</Text>
-						<HStack spacing={ 2 } justify="space-between">
-							<InputControl
-								__next40pxDefaultSize
-								label={ __( 'Custom name server 1' ) }
-								placeholder={ __( 'ns1.domain.com' ) }
-								onChange={ () => {} }
-							/>
-							<InputControl
-								__next40pxDefaultSize
-								label={ __( 'Custom name server 2' ) }
-								placeholder={ __( 'ns2.domain.com' ) }
-								onChange={ () => {} }
-							/>
-						</HStack>
-						<View>
-							<Button variant="primary">{ __( 'Save' ) }</Button>
-						</View>
-					</VStack>
+					<NameServersForm />
 				</CardBody>
 			</Card>
 		</PageLayout>
