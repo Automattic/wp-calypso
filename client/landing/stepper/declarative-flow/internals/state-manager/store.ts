a2121b6a86ce8ec48a4bcda970702933bfa5e2bf
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-08-05 10:15:48 -0300

    Don't useQuery when fetching flow state

--- client/landing/stepper/declarative-flow/internals/state-manager/store.ts
+++ client/landing/stepper/declarative-flow/internals/state-manager/store.ts
@@ -1,25 +1,10 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
 import { getFlowFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url';
 import { getSessionId } from 'calypso/landing/stepper/utils/use-session-id';
 import type { FlowStateManifest } from './stepper-state-manifest';
 
 const PREFIX = 'stepper-state-item';
 const VERSION = 'v1';
-
-/**
- * We don't to ever refetch this query. This is no backend to sync with.
- * The data only ever changes when we mutate it ourselves, in which case we invalidate the query manually.
- */
-const PERSISTENCE_CONFIG = {
-	staleTime: Infinity,
-	refetchOnMount: false,
-	refetchOnWindowFocus: false,
-	refetchOnReconnect: false,
-	/**
-	 * This query doesn't really depend on network, so ask useQuery to always fetch even if the network is offline.
-	 */
-	networkMode: 'always',
-} as const;
 
 /**
  * Returns a setter and a getter for the flow state. This persists the state for 7 days. The persistence is based on the flow and the session ID.
@@ -29,13 +14,8 @@ export function useFlowState() {
 	const flow = getFlowFromURL() || 'flow';
 	const session = getSessionId();
 
-	const { data: state } = useQuery< FlowStateManifest >( {
-		queryKey: [ PREFIX, flow, session, VERSION ],
-		queryFn: ( { client, queryKey } ) => {
-			return ( client.getQueryData( queryKey ) ?? {} ) as FlowStateManifest;
-		},
-		...PERSISTENCE_CONFIG,
-	} );
+	const key = [ PREFIX, flow, session, VERSION ] as const;
+	const state = ( queryClient.getQueryData( key ) ?? {} ) as FlowStateManifest;
 
 	function get< T extends keyof FlowStateManifest >( key: T ) {
 		return state?.[ key ];
