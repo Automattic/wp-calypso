4734faf1adf3d35a15bc3c9af2804a31f52f0746
Igor Giussani <gius80@gmail.com>
2025-08-05 16:03:25 +0200

    Add form

--- client/dashboard/domains/domains-dnssec/index.tsx
+++ client/dashboard/domains/domains-dnssec/index.tsx
@@ -1,10 +1,36 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { Card, CardBody } from '@wordpress/components';
+import {
+	Card,
+	CardBody,
+	__experimentalHStack as HStack,
+	__experimentalVStack as VStack,
+	Button,
+} from '@wordpress/components';
+import { DataForm, Field } from '@wordpress/dataviews';
+import { __ } from '@wordpress/i18n';
+import { useState } from 'react';
 import { domainsQuery } from '../../app/queries/domains';
 import { siteDomainsQuery } from '../../app/queries/site-domains';
 import { domainRoute } from '../../app/routes/domain-routes';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
+
+type DNSSECFormData = {
+	enabled: boolean;
+};
+
+const fields: Field< DNSSECFormData >[] = [
+	{
+		id: 'enabled',
+		label: __( 'Enable DNSSEC' ),
+		Edit: 'checkbox',
+	},
+];
+
+const form = {
+	type: 'regular' as const,
+	fields: [ 'enabled' ],
+};
 
 export default function DomainPlaceholder() {
 	const { data: allDomains } = useSuspenseQuery( domainsQuery() );
@@ -23,11 +49,43 @@ export default function DomainPlaceholder() {
 
 	const siteDomain = siteDomains.find( ( siteDomain ) => siteDomain.domain === domainName );
 
+	const [ formData, setFormData ] = useState< DNSSECFormData >( {
+		enabled: siteDomain?.is_dnssec_enabled ?? false,
+	} );
+
+	const isDirty = true;
+	const isPending = false;
+
 	return (
 		<PageLayout size="small" header={ <PageHeader title="DNSSEC" /> }>
 			<Card>
 				<CardBody>
-					{ siteDomain?.is_dnssec_enabled ? 'DNSSEC Enabled' : 'DNSSEC Disabled' }
+					<form
+						onSubmit={ ( e: React.FormEvent ) => {
+							e.preventDefault();
+						} }
+					>
+						<VStack spacing={ 4 }>
+							<DataForm< DNSSECFormData >
+								data={ formData }
+								fields={ fields }
+								form={ form }
+								onChange={ ( edits: Partial< DNSSECFormData > ) => {
+									setFormData( ( data ) => ( { ...data, ...edits } ) );
+								} }
+							/>
+							<HStack justify="flex-start">
+								<Button
+									variant="primary"
+									type="submit"
+									isBusy={ isPending }
+									disabled={ isPending || ! isDirty }
+								>
+									{ __( 'Save' ) }
+								</Button>
+							</HStack>
+						</VStack>
+					</form>
 				</CardBody>
 			</Card>
 		</PageLayout>
