4385602fc07e7f854bfb93b390d3611a09e9191a
Jason Johnston <jason.johnston@automattic.com>
2025-08-05 12:16:53 -0400

    Remove reset css from demo app (#50)

--- packages/agenttic-demo/src/index.tsx
+++ packages/agenttic-demo/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './reset.css';
 
 const container = document.getElementById( 'root' );
 if ( ! container ) {
