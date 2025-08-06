9dd45a63c0b240e960497e52f869ef3b190d69a8
Payton Swick <payton@foolord.com>
2025-08-06 12:03:33 -0400

    Clean up redundant undefined types in Purchase

--- client/dashboard/data/purchase.ts
+++ client/dashboard/data/purchase.ts
@@ -19,44 +19,48 @@ export interface RawPurchaseIntroductoryOffer {
 
 export interface PriceTierEntry {
 	minimum_units: number;
-	maximum_units?: undefined | null | number;
+	maximum_units?: null | number;
 	minimum_price: number;
 	minimum_price_display: string;
-	minimum_price_monthly_display?: string | null | undefined;
+	minimum_price_monthly_display?: string | null;
 	maximum_price: number;
-	maximum_price_display?: string | null | undefined;
-	maximum_price_monthly_display?: string | null | undefined;
+	maximum_price_display?: string | null;
+	maximum_price_monthly_display?: string | null;
+
 	/**
 	 * If set, is used to transform the usage/quantity of units used to derive the number of units
 	 * we want to bill the customer for, before multiplying by the per_unit_fee.
 	 *
 	 * To put simply, the purpose of this attribute is to bill the customer at a different granularity compared to their usage.
 	 */
-	transform_quantity_divide_by?: number | null | undefined;
+	transform_quantity_divide_by?: number | null;
+
 	/**
 	 * Used for rounding the number of units we want to bill the customer for (which is derived after dividing the
 	 * usage/quantity of units by the `transform_quantity_divide_by` number).
 	 *
 	 * Used only when `transform_quantity_divide_by` is set. Possible values are: `up`, `down`
 	 */
-	transform_quantity_round?: string | null | undefined;
+	transform_quantity_round?: string | null;
+
 	/**
 	 * The amount in the currency's smallest unit that this tier costs per unit.
 	 */
-	per_unit_fee?: number | null | undefined;
+	per_unit_fee?: number | null;
+
 	/**
 	 * The amount in the currency's smallest unit that this tier costs as a flat fee (for the entire tier).
 	 */
-	flat_fee?: number | null | undefined;
+	flat_fee?: number | null;
 }
 
 export interface PurchasePriceTier {
 	minimumUnits: number;
-	maximumUnits?: undefined | null | number;
+	maximumUnits?: null | number;
 	minimumPrice: number;
 	maximumPrice: number;
 	minimumPriceDisplay: string;
-	maximumPriceDisplay?: string | null | undefined;
+	maximumPriceDisplay?: string | null;
 }
 
 export interface RawPurchasePriceTierEntry extends PriceTierEntry {
