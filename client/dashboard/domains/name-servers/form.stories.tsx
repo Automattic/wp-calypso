dde234170629cdac682c2f74ec4a91057380516c
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-06 18:12:59 +0200

    Create storybook for the form

---
+++ client/dashboard/domains/name-servers/form.stories.tsx
@@ -0,0 +1,85 @@
+import { action } from '@storybook/addon-actions';
+import { Card, CardBody } from '@wordpress/components';
+import PageLayout from '../../components/page-layout';
+import NameServersForm from './form';
+import type { Meta, StoryObj } from '@storybook/react';
+import './styles.scss';
+
+const meta: Meta< typeof NameServersForm > = {
+	title: 'Domains/NameServersForm',
+	component: NameServersForm,
+	parameters: {
+		layout: 'none',
+	},
+	tags: [ 'autodocs' ],
+	decorators: [
+		( Story ) => (
+			<PageLayout size="small">
+				<Card>
+					<CardBody className="domains-management__name-servers">
+						<Story />
+					</CardBody>
+				</Card>
+			</PageLayout>
+		),
+	],
+};
+
+export default meta;
+type Story = StoryObj< typeof NameServersForm >;
+
+export const Default: Story = {
+	args: {
+		domainName: 'example.com',
+		serviceName: 'WordPress.com',
+		nameservers: [],
+		isBusy: false,
+		showUpsellNudge: false,
+		onSubmit: action( 'onSubmit' ),
+	},
+};
+
+export const WithNameservers: Story = {
+	args: {
+		domainName: 'example.com',
+		serviceName: 'WordPress.com',
+		nameservers: [ 'ns1.wordpress.com', 'ns2.wordpress.com', 'ns3.wordpress.com' ],
+		isBusy: false,
+		showUpsellNudge: false,
+		onSubmit: action( 'onSubmit' ),
+	},
+};
+
+export const WithUpsellNudge: Story = {
+	args: {
+		domainName: 'example.com',
+		serviceName: 'WordPress.com',
+		nameservers: [],
+		isBusy: false,
+		showUpsellNudge: true,
+		onSubmit: action( 'onSubmit' ),
+	},
+};
+
+export const Loading: Story = {
+	args: {
+		domainName: 'example.com',
+		serviceName: 'WordPress.com',
+		nameservers: [],
+		isBusy: true,
+		showUpsellNudge: false,
+		onSubmit: action( 'onSubmit' ),
+	},
+};
+
+export const WithError: Story = {
+	args: {
+		domainName: 'example.com',
+		serviceName: 'WordPress.com',
+		nameservers: [],
+		isBusy: false,
+		showUpsellNudge: false,
+		queryError: 'An error occurred while fetching nameservers.',
+		onSubmit: action( 'onSubmit' ),
+	},
+};
