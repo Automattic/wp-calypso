8b1649190f8a7c7ed44c761cd59eed192e076366
arthur <arthur.chu@automattic.com>
2025-08-07 10:21:43 +0800

    Remove the check of user in userHasFlag

--- client/dashboard/utils/user.ts
+++ client/dashboard/utils/user.ts
@@ -1,8 +1,5 @@
 import type { User, UserFlags } from '../data/types';
 
 export const userHasFlag = ( user: User, flag: UserFlags ) => {
-	if ( ! user ) {
-		return false;
-	}
 	return user.meta.data.flags.active_flags.includes( flag );
 };
