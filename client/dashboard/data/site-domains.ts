5f21029b775767a5e45ca22d8cf9f7cc7c22c061
arthur <arthur.chu@automattic.com>
2025-08-07 10:17:35 +0800

    Type the return value of setPrimaryDomain

--- client/dashboard/data/site-domains.ts
+++ client/dashboard/data/site-domains.ts
@@ -12,6 +12,6 @@ export async function fetchSiteDomains( siteId: number ): Promise< SiteDomain[] 
 	return domains;
 }
 
-export async function setPrimaryDomain( siteId: number, domain: string ) {
+export async function setPrimaryDomain( siteId: number, domain: string ): Promise< SiteDomain > {
 	return wpcom.req.post( `/sites/${ siteId }/domains/primary`, { domain } );
 }
