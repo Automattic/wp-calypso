d382e1aeec73907c20a982e348ec0e229c042fef
Omar Alshaker <omar@omaralshaker.com>
2025-08-07 10:47:51 +0200

    Fix persistence issue

--- client/landing/stepper/declarative-flow/internals/state-manager/store.ts
+++ client/landing/stepper/declarative-flow/internals/state-manager/store.ts
@@ -1,4 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query';
+import { useEffect, useMemo } from 'react';
 import { getFlowFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url';
 import { getSessionId } from 'calypso/landing/stepper/utils/use-session-id';
 import type { FlowStateManifest } from './stepper-state-manifest';
@@ -7,15 +8,33 @@ const PREFIX = 'stepper-state-item';
 const VERSION = 'v1';
 
 /**
+ * We don't to ever refetch this query. This is no backend to sync with.
+ * The data only ever changes when we mutate it ourselves, in which case we invalidate the query manually.
+ */
+const PERSISTENCE_CONFIG = {
+	staleTime: Infinity,
+	refetchOnMount: false,
+	refetchOnWindowFocus: false,
+	refetchOnReconnect: false,
+	/**
+	 * This query doesn't really depend on network, so ask useQuery to always fetch even if the network is offline.
+	 */
+	networkMode: 'always',
+} as const;
+
+/**
  * Returns a setter and a getter for the flow state. This persists the state for 7 days. The persistence is based on the flow and the session ID.
  */
 export function useFlowState() {
 	const queryClient = useQueryClient();
 	const flow = getFlowFromURL() || 'flow';
 	const session = getSessionId();
+	const queryKey = useMemo( () => [ PREFIX, flow, session, VERSION ] as const, [ flow, session ] );
+	const state = ( queryClient.getQueryData( queryKey ) ?? {} ) as FlowStateManifest;
 
-	const key = [ PREFIX, flow, session, VERSION ] as const;
-	const state = ( queryClient.getQueryData( key ) ?? {} ) as FlowStateManifest;
+	useEffect( () => {
+		queryClient.setQueryDefaults( queryKey, PERSISTENCE_CONFIG );
+	}, [ queryClient, queryKey ] );
 
 	function get< T extends keyof FlowStateManifest >( key: T ) {
 		return state?.[ key ];
