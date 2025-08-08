00c364f6c74f7ef1db34e2c62091614b8c452fcd
Igor Giussani <gius80@gmail.com>
2025-08-08 13:58:02 +0200

    Implement data view

--- client/dashboard/domains/overview-dns/index.tsx
+++ client/dashboard/domains/overview-dns/index.tsx
@@ -1,18 +1,107 @@
-import { Card, CardBody, __experimentalText as Text } from '@wordpress/components';
+import { useQuery } from '@tanstack/react-query';
+import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
+import { __ } from '@wordpress/i18n';
+import { useState, useMemo } from 'react';
+import { domainDnsQuery } from '../../app/queries/domain-dns';
 import { domainRoute } from '../../app/routes/domain-routes';
+import DataViewsCard from '../../components/dataviews-card';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
+import type { DnsRecord } from '../../data/domain-dns';
+import type { Field, ViewTable, ViewList, View } from '@wordpress/dataviews';
 
-export default function DomainDNSSEC() {
+function getDnsRecordId( record: DnsRecord ) {
+	return `${ record.id }-${ record.name }`;
+}
+
+type DnsView = ViewTable | ViewList;
+
+const DEFAULT_VIEW: DnsView = {
+	type: 'table',
+	search: '',
+	page: 1,
+	perPage: 20,
+	titleField: 'type',
+	sort: {
+		field: 'type',
+		direction: 'asc',
+	},
+	fields: [ 'name' ],
+	filters: [],
+};
+
+const DEFAULT_LAYOUTS = {
+	table: {},
+	list: {},
+};
+
+export default function DomainDns() {
 	const { domainName } = domainRoute.useParams();
+	const { data: dnsData, isLoading } = useQuery( domainDnsQuery( domainName ) );
+
+	const fields: Field< DnsRecord >[] = useMemo(
+		() => [
+			{
+				id: 'type',
+				label: __( 'Type' ),
+				enableHiding: false,
+				enableSorting: true,
+				getValue: ( { item } ) => item.type,
+			},
+			{
+				id: 'name',
+				label: __( 'Name' ),
+				enableHiding: false,
+				enableSorting: true,
+				getValue: ( { item } ) => {
+					const { name, service, protocol, type } = item;
+
+					if ( 'SRV' === type ) {
+						return `${ service }.${ protocol }.${
+							name.replace( /\.$/, '' ) === domainName ? name : name + '.' + domainName + '.'
+						}`;
+					}
+
+					if ( name.replace( /\.$/, '' ) === domainName ) {
+						return '@';
+					}
+
+					return name;
+				},
+			},
+		],
+		[ domainName ]
+	);
+
+	const [ view, setView ] = useState< DnsView >( DEFAULT_VIEW );
+
+	const { data: filteredData, paginationInfo } = filterSortAndPaginate(
+		dnsData?.records ?? [],
+		view,
+		fields
+	);
 
 	return (
-		<PageLayout size="small" header={ <PageHeader title="DNS Records" /> }>
-			<Card>
-				<CardBody>
-					<Text>DNS Placeholder { domainName }</Text>
-				</CardBody>
-			</Card>
+		<PageLayout size="small" header={ <PageHeader title={ __( 'DNS Records' ) } /> }>
+			<DataViewsCard>
+				{ dnsData?.records?.length === 0 && ! isLoading ? (
+					<div style={ { padding: '20px', textAlign: 'center' } }>
+						{ __( 'No DNS records found for this domain.' ) }
+					</div>
+				) : (
+					<DataViews< DnsRecord >
+						data={ filteredData || [] }
+						fields={ fields }
+						onChangeView={ ( view: View ) => setView( view as DnsView ) }
+						search={ false }
+						view={ view }
+						paginationInfo={ paginationInfo }
+						getItemId={ getDnsRecordId }
+						isLoading={ isLoading }
+						defaultLayouts={ DEFAULT_LAYOUTS }
+					/>
+				) }
+			</DataViewsCard>
 		</PageLayout>
 	);
 }
