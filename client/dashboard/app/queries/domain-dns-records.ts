7ec1e18a0acc6a323a24cc981c508fc6d874b358
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-08-08 19:28:38 -0300

    Remove query that does not belong to this PR

--- client/dashboard/app/queries/domain-dns-records.ts
+++
@@ -1,8 +0,0 @@
-import { queryOptions } from '@tanstack/react-query';
-import wpcom from 'calypso/lib/wp';
-
-export const domainDnsRecordsQuery = ( domain: string ) =>
-	queryOptions( {
-		queryKey: [ 'domain', domain, 'dns-records' ],
-		queryFn: () => wpcom.req.get( `/domains/${ domain }/dns` ),
-	} );
