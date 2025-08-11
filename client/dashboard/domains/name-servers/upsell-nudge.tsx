877cf5f129a15b7c35bb6768e7ce197f9dee4677
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-11 15:07:38 +0200

    Update translation

--- client/dashboard/domains/name-servers/upsell-nudge.tsx
+++ client/dashboard/domains/name-servers/upsell-nudge.tsx
@@ -1,7 +1,9 @@
+import { SETTING_PRIMARY_DOMAIN } from '@automattic/urls';
 import { __experimentalText as Text } from '@wordpress/components';
 import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 import { Callout } from '../../components/callout';
+import InlineSupportLink from '../../components/inline-support-link';
 import UpsellCTAButton from '../../components/upsell-cta-button';
 import illustrationUrl from './upsell-nudge-illustration.svg';
 
@@ -22,17 +24,13 @@ export default function UpsaleNudge( { domainName }: Props ) {
 			description={
 				<Text as="p">
 					{ createInterpolateElement(
-						sprintf(
-							// translators: %(domain)s is the domain name
-							__(
-								'Upgrade to a paid plan to make <strong>%s</strong> the primary address that your visitors see when they visit your site.<br /><a>Learn more</a>'
-							),
-							domainName
+						__(
+							'Upgrade to a paid plan to make <domain /> the primary address that your visitors see when they visit your site.<br /><learnMore />'
 						),
 						{
 							br: <br />,
-							strong: <strong>{ domainName }</strong>,
-							a: <a href="https://wordpress.com/" target="_blank" rel="noopener noreferrer" />,
+							domain: <strong>{ domainName }</strong>,
+							learnMore: <InlineSupportLink supportLink={ SETTING_PRIMARY_DOMAIN } />,
 						}
 					) }
 				</Text>
