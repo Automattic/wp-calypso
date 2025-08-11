b5e57d506bd6bfd1c243c38531959b2416dd5674
Veselin Nikolov <veselin@automattic.com>
2025-08-11 09:35:16 +0300

    Update my plan for summer special

--- client/blocks/product-purchase-features-list/index.jsx
+++ client/blocks/product-purchase-features-list/index.jsx
@@ -60,10 +60,12 @@ export class ProductPurchaseFeaturesList extends Component {
 	static propTypes = {
 		plan: PropTypes.oneOf( Object.keys( PLANS_LIST ) ).isRequired,
 		isPlaceholder: PropTypes.bool,
+		isSummerSpecial: PropTypes.bool,
 	};
 
 	static defaultProps = {
 		isPlaceholder: false,
+		isSummerSpecial: false,
 	};
 
 	// TODO: Define feature list.
@@ -161,6 +163,7 @@ export class ProductPurchaseFeaturesList extends Component {
 			plan,
 			planHasDomainCredit,
 			selectedSite,
+			isSummerSpecial,
 		} = this.props;
 
 		return (
@@ -175,6 +178,7 @@ export class ProductPurchaseFeaturesList extends Component {
 				<VideoAudioPosts selectedSite={ selectedSite } plan={ plan } />
 				{ canActivateWordadsInstant && <MonetizeSite selectedSite={ selectedSite } /> }
 				{ isEnabled( 'themes/premium' ) && <FindNewTheme selectedSite={ selectedSite } /> }
+				{ isSummerSpecial && <UploadPlugins selectedSite={ selectedSite } /> }
 				<SiteActivity />
 				<MobileApps onClick={ this.handleMobileAppsClick } />
 				<SellOnlinePaypal isJetpack={ false } />
@@ -183,7 +187,8 @@ export class ProductPurchaseFeaturesList extends Component {
 	}
 
 	getPersonalFeatures() {
-		const { isPlaceholder, isMonthlyPlan, selectedSite, planHasDomainCredit } = this.props;
+		const { isPlaceholder, isMonthlyPlan, selectedSite, planHasDomainCredit, isSummerSpecial } =
+			this.props;
 
 		return (
 			<Fragment>
@@ -192,6 +197,7 @@ export class ProductPurchaseFeaturesList extends Component {
 					<CustomDomain selectedSite={ selectedSite } hasDomainCredit={ planHasDomainCredit } />
 				) }
 				<AdvertisingRemoved isEligiblePlan selectedSite={ selectedSite } />
+				{ isSummerSpecial && <UploadPlugins selectedSite={ selectedSite } /> }
 				<SiteActivity />
 				<MobileApps onClick={ this.handleMobileAppsClick } />
 			</Fragment>
@@ -441,6 +447,7 @@ export default connect(
 			currentPlan: getCurrentPlan( state, selectedSiteId ),
 			scheduleId: getConciergeScheduleId( state ),
 			isMonthlyPlan: TERM_MONTHLY === getPlan( ownProps.plan )?.term,
+			isSummerSpecial: selectedSite?.options?.is_summer_special_2025 ?? false,
 		};
 	},
 	{
