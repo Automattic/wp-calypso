41eb2ac5e39ac599832287396f451654fb68f428
Igor Giussani <gius80@gmail.com>
2025-08-11 11:42:36 +0200

    Move fields in a separate file

--- client/dashboard/domains/overview-dns/index.tsx
+++ client/dashboard/domains/overview-dns/index.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { __experimentalVStack as VStack, Button } from '@wordpress/components';
 import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { useState } from 'react';
 import { domainDnsQuery } from '../../app/queries/domain-dns';
 import { domainRoute } from '../../app/routes/domain-routes';
@@ -10,16 +10,13 @@ import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
 import { useDnsActions } from './actions';
 import DnsActionsMenu from './dns-actions-menu';
+import { useDnsFields } from './fields';
 import type { DnsRecord } from '../../data/domain-dns';
-import type { Field, ViewTable, ViewList, View } from '@wordpress/dataviews';
+import type { ViewTable, ViewList, View } from '@wordpress/dataviews';
 
 function getDnsRecordId( record: DnsRecord ) {
 	return `${ record.id }-${ record.name }`;
 }
-
-const trimDot = ( str?: string ) => {
-	return str ? str.replace( /\.$/, '' ) : '';
-};
 
 type DnsView = ViewTable | ViewList;
 
@@ -51,82 +48,7 @@ export default function DomainDns() {
 
 	const actions = useDnsActions();
 
-	const fields: Field< DnsRecord >[] = [
-		{
-			id: 'type',
-			label: __( 'Type' ),
-			enableHiding: false,
-			enableSorting: true,
-			getValue: ( { item } ) => item.type,
-		},
-		{
-			id: 'name',
-			label: __( 'Name' ),
-			enableHiding: false,
-			enableSorting: true,
-			getValue: ( { item } ) => {
-				const { name, service, protocol, type } = item;
-
-				if ( 'SRV' === type ) {
-					return `${ service }.${ protocol }.${
-						name.replace( /\.$/, '' ) === domainName ? name : name + '.' + domainName + '.'
-					}`;
-				}
-
-				if ( name.replace( /\.$/, '' ) === domainName ) {
-					return '@';
-				}
-
-				return name;
-			},
-		},
-		{
-			id: 'value',
-			label: __( 'Value' ),
-			enableHiding: false,
-			enableSorting: true,
-			getValue: ( { item } ) => {
-				const { type, aux, port, weight } = item;
-				const data = trimDot( item.data );
-				const target = '.' !== item.target ? trimDot( item.target ) : '.';
-				if ( item.protected_field ) {
-					if ( 'MX' === type ) {
-						return __( 'Mail handled by WordPress.com email forwarding' );
-					}
-					return __( 'Handled by WordPress.com' );
-				}
-
-				switch ( type ) {
-					case 'MX':
-						return sprintf(
-							// translators: %(data)s is a hostname, %(aux)d is a priority
-							__( '%(data)s with priority %(aux)d' ),
-							{
-								data,
-								aux: aux as number,
-							}
-						);
-					case 'SRV':
-						return sprintf(
-							// translators: %(target)s is a hostname, %(port)d is a port, %(aux)d is a priority, %(weight)d is a weight
-							__( '%(target)s:%(port)d, with priority %(aux)d and weight %(weight)d' ),
-							{
-								target,
-								port: port as number,
-								aux: aux as number,
-								weight: weight as number,
-							}
-						);
-				}
-				return data;
-			},
-			render: ( { field, item } ) => (
-				<div style={ { whiteSpace: 'pre-wrap', wordBreak: 'break-word' } }>
-					{ field.getValue( { item } ) }
-				</div>
-			),
-		},
-	];
+	const fields = useDnsFields( domainName );
 
 	const [ view, setView ] = useState< DnsView >( DEFAULT_VIEW );
 
