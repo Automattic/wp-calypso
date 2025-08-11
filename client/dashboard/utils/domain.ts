cceda9e4c780147ce1571425c72ca6c2c8c6b8ae
Philip Jackson <p-jackson@live.com>
2025-08-11 19:34:28 +1200

    Clean email subscription checking logic

--- client/dashboard/utils/domain.ts
+++ client/dashboard/utils/domain.ts
@@ -72,11 +72,11 @@ export function canSetAsPrimary( {
 }
 
 export function hasGSuiteWithUs( domain: SiteDomain ) {
-	const status = domain.google_apps_subscription?.status ?? '';
-	return ! [ '', 'no_subscription', 'other_provider' ].includes( status );
+	const status = domain.google_apps_subscription?.status;
+	return status && ! [ 'no_subscription', 'other_provider' ].includes( status );
 }
 
 export function hasTitanMailWithUs( domain: SiteDomain ) {
-	const subscriptionStatus = domain.titan_mail_subscription?.status ?? '';
+	const subscriptionStatus = domain.titan_mail_subscription?.status;
 	return subscriptionStatus === 'active' || subscriptionStatus === 'suspended';
 }
