a8fcebbd0b7e83938894c4fa3b0bf730100a59ca
Copons <Copons@users.noreply.github.com>
2025-08-12 14:48:11 +0100

    Split SiteSettings and JetpackSettings types

--- client/dashboard/data/site-settings.ts
+++ client/dashboard/data/site-settings.ts
@@ -15,14 +15,6 @@ export interface SiteSettings {
 	wpcom_performance_report_url?: string;
 	wpcom_legacy_contact?: string;
 	wpcom_locked_mode?: boolean;
-	jetpack_waf_automatic_rules?: boolean;
-	jetpack_waf_automatic_rules_last_updated_timestamp?: number;
-	jetpack_waf_ip_allow_list_enabled?: boolean;
-	jetpack_waf_ip_allow_list?: string;
-	jetpack_waf_ip_block_list_enabled?: boolean;
-	jetpack_waf_ip_block_list?: string;
-	jetpack_sso_match_by_email?: boolean;
-	jetpack_sso_require_two_step?: boolean;
 }
 
 export async function fetchSiteSettings( siteId: number ): Promise< SiteSettings > {
