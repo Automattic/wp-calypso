8d52212becebbae788151447ffbba8600f634523
Igor Giussani <gius80@gmail.com>
2025-08-13 08:58:46 +0200

    Minor fixes after rebase

--- client/dashboard/app/queries/domain-dns-records.ts
+++ client/dashboard/app/queries/domain-dns-records.ts
@@ -1,19 +1,25 @@
 import { mutationOptions, queryOptions } from '@tanstack/react-query';
 import { fetchDomainDns, updateDomainDns, DnsRecord } from '../../data/domain-dns-records';
+import { queryClient } from '../query-client';
 
 export const domainDnsMutation = ( domainName: string ) =>
 	mutationOptions( {
-		mutationFn: ( params: {
+		mutationFn: ( data: {
 			recordsToAdd?: DnsRecord[];
 			recordsToRemove?: DnsRecord[];
 			restoreDefaultARecords?: boolean;
 		} ) =>
 			updateDomainDns(
 				domainName,
-				params.recordsToAdd,
-				params.recordsToRemove,
-				params.restoreDefaultARecords
+				data.recordsToAdd,
+				data.recordsToRemove,
+				data.restoreDefaultARecords
 			),
+		onSuccess: () => {
+			queryClient.invalidateQueries( {
+				queryKey: [ 'domains', domainName, 'domain-dns' ],
+			} );
+		},
 	} );
 
 export const domainDnsQuery = ( domainName: string ) =>
