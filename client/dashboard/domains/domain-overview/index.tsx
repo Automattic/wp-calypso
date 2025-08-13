92180984ca09aaf33cb42db56088d51735a367cf
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-08-12 21:41:40 -0300

    Wrap domain name if it doesnt fit in the container

--- client/dashboard/domains/domain-overview/index.tsx
+++ client/dashboard/domains/domain-overview/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
 import { Button } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
+import { useMemo } from 'react';
 import { useLocale } from '../../app/locale';
 import { domainQuery } from '../../app/queries/domain';
 import { sitePurchaseQuery } from '../../app/queries/site-purchases';
@@ -18,12 +19,24 @@ export default function DomainOverview() {
 		sitePurchaseQuery( domain.blog_id, parseInt( domain.subscription_id, 10 ) )
 	);
 
+	const wrappableDomainName = useMemo( () => {
+		const [ domainName, ...tlds ] = domain.domain.split( '.' );
+		const tld = tlds.join( '.' );
+
+		return (
+			<span style={ { wordBreak: 'break-word', hyphens: 'none' } }>
+				{ domainName }
+				<span style={ { whiteSpace: 'nowrap' } }>.{ tld }</span>
+			</span>
+		);
+	}, [ domain.domain ] );
+
 	return (
 		<PageLayout
 			size="small"
 			header={
 				<PageHeader
-					title={ domainName }
+					title={ wrappableDomainName }
 					// translators: date is the date the domain was registered.
 					description={ sprintf( __( 'Registered on %(date)s' ), {
 						date: formatDate( new Date( domain.registration_date ), locale, { dateStyle: 'long' } ),
