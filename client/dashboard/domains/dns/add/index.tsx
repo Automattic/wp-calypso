1eb309583c512e452f3ce98b58e167e5a700b8d3
Igor Giussani <gius80@gmail.com>
2025-08-13 15:24:09 +0200

    Fix type

--- client/dashboard/domains/dns/add/index.tsx
+++ client/dashboard/domains/dns/add/index.tsx
@@ -17,7 +17,6 @@ import { domainRoute } from '../../../app/routes/domain-routes';
 import { PageHeader } from '../../../components/page-header';
 import PageLayout from '../../../components/page-layout';
 import RequiredSelect from '../../../components/required-select';
-import { DnsRecordType } from '../../../data/domain-dns-records';
 import { DNS_RECORD_CONFIGS } from './dns-record-configs';
 import type { DnsRecordTypeFormData, DnsRecordFormData } from './dns-record-configs';
 
@@ -27,7 +26,7 @@ const typeForm = {
 };
 
 const defaultFormData = {
-	type: DnsRecordType.A,
+	type: 'A',
 	name: '',
 	data: '',
 	ttl: 3600,
