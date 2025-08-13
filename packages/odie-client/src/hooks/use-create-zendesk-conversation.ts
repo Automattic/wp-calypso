a2e314fb0b1cb054e986c937694646ba072cf4d1
Omar Alshaker <omar@omaralshaker.com>
2025-08-13 21:01:31 +0200

    Add more tracking information around creating conversations

--- packages/odie-client/src/hooks/use-create-zendesk-conversation.ts
+++ packages/odie-client/src/hooks/use-create-zendesk-conversation.ts
@@ -51,6 +51,18 @@ export const useCreateZendeskConversation = (): ( ( {
 		isFromError?: boolean;
 	} ) => {
 		const currentInteractionID = interactionId || currentSupportInteraction!.uuid;
+
+		trackEvent( 'create_zendesk_conversation', {
+			is_submitting_zendesk_user_fields: isSubmittingZendeskUserFields,
+			chat_conversation_id: chat.conversationId,
+			chat_status: chat.status,
+			chat_provider: chat.provider,
+			interaction_id: currentInteractionID,
+			created_from: createdFrom,
+			avoid_transfer: avoidTransfer,
+			is_from_error: isFromError,
+		} );
+
 		if (
 			isSubmittingZendeskUserFields ||
 			chat.conversationId ||
@@ -71,6 +83,15 @@ export const useCreateZendeskConversation = (): ( ( {
 			status: 'transfer',
 		} ) );
 
+		trackEvent( 'create_submitting_zendesk_user_fields', {
+			messaging_initial_message: userFieldMessage || undefined,
+			messaging_site_id: selectedSiteId || null,
+			messaging_ai_chat_id: chatId || undefined,
+			messaging_url: selectedSiteURL || window.location.href,
+			messaging_flow: userFieldFlowName || null,
+			messaging_source: sectionName,
+		} );
+
 		await submitUserFields( {
 			messaging_initial_message: userFieldMessage || undefined,
 			messaging_site_id: selectedSiteId || null,
@@ -79,6 +100,7 @@ export const useCreateZendeskConversation = (): ( ( {
 			messaging_flow: userFieldFlowName || null,
 			messaging_source: sectionName,
 		} );
+
 		const conversation = await Smooch.createConversation( {
 			metadata: {
 				createdAt: Date.now(),
