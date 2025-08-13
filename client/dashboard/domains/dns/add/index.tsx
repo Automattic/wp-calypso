ec33959d71058a5d97561b2248488ff38a1f7492
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-08-13 16:32:40 -0300

    Update `DomainAddDNS` component to also handle record updates

--- client/dashboard/domains/dns/add/index.tsx
+++ client/dashboard/domains/dns/add/index.tsx
@@ -1,5 +1,5 @@
-import { useMutation } from '@tanstack/react-query';
-import { useNavigate } from '@tanstack/react-router';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useNavigate, useRouter } from '@tanstack/react-router';
 import {
 	Card,
 	CardBody,
@@ -11,9 +11,9 @@ import { useDispatch } from '@wordpress/data';
 import { DataForm, Field } from '@wordpress/dataviews';
 import { __ } from '@wordpress/i18n';
 import { store as noticesStore } from '@wordpress/notices';
-import { useState } from 'react';
-import { domainDnsMutation } from '../../../app/queries/domain-dns-records';
-import { domainRoute } from '../../../app/router/domains';
+import { useEffect, useState } from 'react';
+import { domainDnsMutation, domainDnsQuery } from '../../../app/queries/domain-dns-records';
+import { domainRoute } from '../../../app/routes/domain-routes';
 import { PageHeader } from '../../../components/page-header';
 import PageLayout from '../../../components/page-layout';
 import RequiredSelect from '../../../components/required-select';
@@ -68,11 +68,19 @@ export default function DomainAddDNS() {
 		},
 	];
 
+	const router = useRouter();
 	const navigate = useNavigate();
 	const { createSuccessNotice, createErrorNotice } = useDispatch( noticesStore );
 	const { domainName } = domainRoute.useParams();
+	const { recordId } = domainRoute.useSearch();
 	const mutation = useMutation( domainDnsMutation( domainName ) );
 	const { isPending } = mutation;
+
+	const currentPath = router.state.location.pathname;
+	const isEditMode = currentPath.includes( '/dns/edit' );
+
+	const { data: dnsRecords } = useQuery( domainDnsQuery( domainName ) );
+	const recordToEdit = dnsRecords?.records.find( ( record ) => record.id === recordId );
 
 	const navigateToDNSOverviewPage = () => {
 		navigate( {
@@ -81,21 +89,64 @@ export default function DomainAddDNS() {
 		} );
 	};
 
+	// When editing a DNS record, check if the record exists and populate the form
+	useEffect( () => {
+		if ( ! isEditMode || ! recordToEdit ) {
+			return;
+		}
+
+		const formData = {
+			type: recordToEdit.type,
+			ttl: recordToEdit.ttl as number,
+			name: recordToEdit.name || '',
+			data: recordToEdit.data || '',
+			flags: recordToEdit.flags || 0,
+			tag: recordToEdit.tag || '',
+			aux: recordToEdit.aux || 0,
+			service: recordToEdit.service || '',
+			protocol: recordToEdit.protocol || '',
+			weight: recordToEdit.weight || 0,
+			target: recordToEdit.target || '',
+			port: recordToEdit.port || 0,
+		};
+		setTypeFormData( { type: recordToEdit.type } );
+		setFormData( formData );
+	}, [ isEditMode, recordToEdit ] );
+
+	if ( isEditMode && ! recordToEdit ) {
+		createErrorNotice( __( 'Invalid DNS record to edit.' ) );
+		navigateToDNSOverviewPage();
+		return;
+	}
+
 	const handleSubmit = ( e: React.FormEvent ) => {
 		e.preventDefault();
 
 		const formattedData = config.transformData( formData, domainName );
 
+		const recordsToAdd: DnsRecordFormData[] = [ formattedData ];
+		let recordsToRemove: DnsRecordFormData[] = [];
+		const successMessage = isEditMode
+			? __( 'DNS record updated successfully.' )
+			: __( 'DNS record added successfully.' );
+		const errorMessage = isEditMode
+			? __( 'Failed to update DNS record.' )
+			: __( 'Failed to add DNS record.' );
+
+		if ( isEditMode ) {
+			recordsToRemove = [ recordToEdit ];
+		}
+
 		mutation.mutate(
-			{ recordsToAdd: [ formattedData ] },
+			{ recordsToAdd, recordsToRemove },
 			{
 				onSuccess: () => {
-					createSuccessNotice( __( 'DNS record added successfully.' ), { type: 'snackbar' } );
+					createSuccessNotice( successMessage, { type: 'snackbar' } );
 					navigateToDNSOverviewPage();
 				},
 				onError: () => {
 					// TODO: Get DNS exception class and display correct error message
-					createErrorNotice( __( 'Failed to add DNS record.' ), {
+					createErrorNotice( errorMessage, {
 						type: 'snackbar',
 					} );
 				},
@@ -108,8 +159,11 @@ export default function DomainAddDNS() {
 		navigateToDNSOverviewPage();
 	};
 
+	const headerTitle = isEditMode ? __( 'Edit DNS record' ) : __( 'Add a new DNS record' );
+	const buttonText = isEditMode ? __( 'Update DNS record' ) : __( 'Add DNS record' );
+
 	return (
-		<PageLayout size="small" header={ <PageHeader title={ __( 'Add a new DNS record' ) } /> }>
+		<PageLayout size="small" header={ <PageHeader title={ headerTitle } /> }>
 			<Card>
 				<CardBody>
 					<form onSubmit={ handleSubmit }>
@@ -134,7 +188,7 @@ export default function DomainAddDNS() {
 							/>
 							<HStack justify="flex-start">
 								<Button variant="primary" type="submit" isBusy={ isPending } disabled={ isPending }>
-									{ __( 'Add DNS record' ) }
+									{ buttonText }
 								</Button>
 								<Button type="button" disabled={ isPending } onClick={ handleCancel }>
 									{ __( 'Cancel' ) }
