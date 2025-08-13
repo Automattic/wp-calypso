74d60c7a441859c609e7af2d42e6e48e7e78d633
Kamen Stanev <hambai@gmail.com>
2025-08-13 10:43:31 +0300

    Remove index.tsx as we do not need it

--- client/dashboard/domains/domain-forwarding/index.tsx
+++
@@ -1,4 +0,0 @@
-// Legacy export - use ./add.tsx and ./edit.tsx instead
-export { default as AddDomainForwarding } from './add';
-export { default as EditDomainForwarding } from './edit';
-export { default as DomainForwardingForm } from './form';
