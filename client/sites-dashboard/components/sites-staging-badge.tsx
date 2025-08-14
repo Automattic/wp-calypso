182b1e7130bafa56de76c420f6598b975e98fe2f
Kateryna Kodonenko <kateryna@automattic.com>
2025-08-14 14:51:21 +0200

    Fix unintended change

--- client/sites-dashboard/components/sites-staging-badge.tsx
+++ client/sites-dashboard/components/sites-staging-badge.tsx
@@ -17,7 +17,7 @@ const SitesStagingBadge = styled( SitesLaunchStatusBadge )( ( props: SitesStagin
 		color: COLOR,
 		backgroundColor: BACKGROUND_COLOR,
 	},
-	'.notouch .layout__secondary .site-selector.is-hover-enabled .site:hover &': {
+	'.notouch .layout__secondary .site-selector.is-hover-enabled .site:hover  &': {
 		color: '#1C1300',
 		backgroundColor: '#C08C00',
 	},
