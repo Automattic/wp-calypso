e290379ac90763ef677b2d9d19b44ce92814d625
arthur <arthur.chu@automattic.com>
2025-08-14 20:36:33 +0800

    Update the logic of the isRelativeUrl

--- client/dashboard/utils/url.ts
+++ client/dashboard/utils/url.ts
@@ -1,7 +1,5 @@
+import { getProtocol } from '@wordpress/url';
+
 export function isRelativeUrl( url: string ) {
-	try {
-		return new URL( url, window.location.href ).origin === window.location.origin;
-	} catch {
-		return false;
-	}
+	return ! url.startsWith( '//' ) && ! getProtocol( url );
 }
