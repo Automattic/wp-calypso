4394280c489605fd6550435ba17cfc125112b555
Paulo Trentin <paulo@paulotrentin.com.br>
2025-08-14 17:54:26 -0300

    Refactor domain-supported contries and states and removed lodash usage

--- client/dashboard/data/domain.ts
+++ client/dashboard/data/domain.ts
@@ -5,28 +5,9 @@ export interface Domain extends DomainSummary {
 	is_gravatar_domain: boolean;
 }
 
-export interface StatesListItem {
-	code: string;
-	name: string;
-}
-
 export function fetchDomain( domainName: string ): Promise< Domain > {
 	return wpcom.req.get( {
 		path: `/domain-details/${ domainName }`,
 		apiVersion: '1.2',
-	} );
-}
-
-export async function fetchCountryList(): Promise< CountryListItem[] > {
-	return await wpcom.req.get( {
-		path: '/domains/supported-countries',
-		apiVersion: '1.1',
-	} );
-}
-
-export async function fetchStatesList( countryCode: string ): Promise< StatesListItem[] > {
-	return await wpcom.req.get( {
-		path: `/domains/supported-states/${ countryCode }`,
-		apiVersion: '1.1',
 	} );
 }
