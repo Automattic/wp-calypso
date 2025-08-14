2cbbf5bcb04e418397c916c7cd6f3118fa6dca37
Rafael Agostini <rafael.agostini@automattic.com>
2025-08-13 21:43:50 -0500

    Removed unnecessary empty object from the post request

--- client/dashboard/data/site-backup.ts
+++ client/dashboard/data/site-backup.ts
@@ -26,13 +26,10 @@ export interface BackupEntry {
  * @returns A promise that resolves when the backup is enqueued.
  */
 export function enqueueSiteBackup( siteId: number ): Promise< void > {
-	return wpcom.req.post(
-		{
-			path: `/sites/${ siteId }/rewind/backups/enqueue`,
-			apiNamespace: 'wpcom/v2',
-		},
-		{}
-	);
+	return wpcom.req.post( {
+		path: `/sites/${ siteId }/rewind/backups/enqueue`,
+		apiNamespace: 'wpcom/v2',
+	} );
 }
 
 /**
