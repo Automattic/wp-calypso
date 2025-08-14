4e5f3969684917b6ba24a2748bbdd232c006ec13
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-14 10:56:50 +0200

    Optimize how the callback is being provide

--- client/dashboard/domains/name-servers/index.tsx
+++ client/dashboard/domains/name-servers/index.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { store as noticesStore } from '@wordpress/notices';
+import { useCallback } from 'react';
 import {
 	domainNameServersQuery,
 	domainNameServersMutation,
@@ -16,17 +17,23 @@ export default function NameServers() {
 	const { domainName } = domainRoute.useParams();
 	const { createSuccessNotice, createErrorNotice } = useDispatch( noticesStore );
 	const { data: nameServers, error: queryError } = useQuery( domainNameServersQuery( domainName ) );
-
 	const { mutate: updateNameServers, isPending: isUpdatingNameServers } = useMutation( {
 		...domainNameServersMutation( domainName ),
-		onError: ( e: Error ) => createErrorNotice( e.message, { type: 'snackbar' } ),
-		onSuccess: ( data, variables, context ) => {
-			// Call the original mutation's onSuccess handler
-			domainNameServersMutation( domainName )?.onSuccess?.( data, variables, context );
-			// Show success notice
-			createSuccessNotice( __( 'Name servers updated successfully.' ), { type: 'snackbar' } );
-		},
 	} );
+
+	const onSubmit = useCallback(
+		( ns: string[] ) => {
+			updateNameServers( ns, {
+				onSuccess: () => {
+					createSuccessNotice( __( 'Name servers updated successfully.' ), {
+						type: 'snackbar',
+					} );
+				},
+				onError: ( e: Error ) => createErrorNotice( e.message, { type: 'snackbar' } ),
+			} );
+		},
+		[ updateNameServers, createSuccessNotice, createErrorNotice ]
+	);
 
 	return (
 		<PageLayout size="small" header={ <PageHeader title={ __( 'Name Servers' ) } /> }>
@@ -36,8 +43,8 @@ export default function NameServers() {
 						domainName={ domainName }
 						queryError={ queryError?.message }
 						isBusy={ isUpdatingNameServers }
-						nameServers={ nameServers }
-						onSubmit={ updateNameServers }
+						nameServers={ nameServers ?? [] }
+						onSubmit={ onSubmit }
 					/>
 				</CardBody>
 			</Card>
