76ece59772f37a47029bf84e1fb2c391a05c491f
Rafael Agostini <rafael.agostini@automattic.com>
2025-08-14 21:44:10 -0500

    Add restore page scaffolding

---
+++ client/dashboard/sites/backups/restore/index.tsx
@@ -0,0 +1,65 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from '@tanstack/react-router';
+import {
+	Button,
+	Card,
+	CardBody,
+	__experimentalHStack as HStack,
+	__experimentalVStack as VStack,
+} from '@wordpress/components';
+import { __, isRTL } from '@wordpress/i18n';
+import { chevronLeft, chevronRight, rotateLeft } from '@wordpress/icons';
+import { siteBySlugQuery } from '../../../app/queries/site';
+import { siteBackupRestoreRoute, siteBackupsRoute } from '../../../app/router';
+import Notice from '../../../components/notice';
+import { PageHeader } from '../../../components/page-header';
+import PageLayout from '../../../components/page-layout';
+
+function SiteBackupRestore() {
+	const { siteSlug, rewindId } = siteBackupRestoreRoute.useParams();
+	const router = useRouter();
+	const { data: site } = useQuery( siteBySlugQuery( siteSlug ) );
+
+	if ( ! site ) {
+		return;
+	}
+
+	const backButton = (
+		<Button
+			className="dashboard-page-header__back-button"
+			icon={ isRTL() ? chevronRight : chevronLeft }
+			onClick={ () => {
+				router.navigate( { to: siteBackupsRoute.fullPath, params: { siteSlug } } );
+			} }
+		>
+			{ __( 'Backups' ) }
+		</Button>
+	);
+
+	return (
+		<PageLayout
+			size="small"
+			header={ <PageHeader prefix={ backButton } title={ __( 'Restore your site' ) } /> }
+		>
+			<Card>
+				<CardBody>
+					<VStack spacing={ 4 }>
+						<p>Rewind ID: { rewindId }</p>
+						<Notice variant="info" title={ __( 'Important' ) }>
+							{ __(
+								'This action will replace all settings, posts, pages and other site content with the information from the selected restore point.'
+							) }
+						</Notice>
+						<HStack justify="flex-start">
+							<Button variant="primary" type="submit" icon={ rotateLeft }>
+								{ __( 'Restore now' ) }
+							</Button>
+						</HStack>
+					</VStack>
+				</CardBody>
+			</Card>
+		</PageLayout>
+	);
+}
+
+export default SiteBackupRestore;
