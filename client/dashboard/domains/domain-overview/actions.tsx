2fb3f8da068c0128b679fbd67080024c9d7a6a5d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-15 18:18:29 +0200

    Add action items

--- client/dashboard/domains/domain-overview/actions.tsx
+++ client/dashboard/domains/domain-overview/actions.tsx
@@ -1,22 +1,38 @@
-import {
-	Card,
-	CardBody,
-	__experimentalText as Text,
-	__experimentalVStack as VStack,
-} from '@wordpress/components';
+import { Button, __experimentalVStack as VStack } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { ActionList } from '../../components/action-list';
+import { SectionHeader } from '../../components/section-header';
 
 export default function Actions() {
 	return (
 		<VStack spacing={ 4 }>
-			<Text as="h2" size="medium" weight={ 500 }>
-				{ __( 'Actions' ) }
-			</Text>
-			<Card>
-				<CardBody>
-					<Text>Lorem ipsum</Text>
-				</CardBody>
-			</Card>
+			<SectionHeader level={ 3 } title={ __( 'Actions' ) } />
+			<ActionList>
+				<ActionList.ActionItem
+					title={ __( 'Renew' ) }
+					description={ __( 'Renew domain registration.' ) }
+					actions={ <Button variant="secondary">{ __( 'Renew' ) }</Button> }
+				/>
+				<ActionList.ActionItem
+					title={ __( 'Transfer' ) }
+					description={ __( 'Transfer this domain to another site or WordPress.com user.' ) }
+					actions={ <Button variant="secondary">{ __( 'Transfer' ) }</Button> }
+				/>
+				<ActionList.ActionItem
+					title={ __( 'Detach' ) }
+					description={ __( 'Detach this domain from the site.' ) }
+					actions={ <Button variant="secondary">{ __( 'Detach' ) }</Button> }
+				/>
+				<ActionList.ActionItem
+					title={ __( 'Delete' ) }
+					description={ __( 'Remove this domain permanently.' ) }
+					actions={
+						<Button variant="secondary" isDestructive>
+							{ __( 'Delete' ) }
+						</Button>
+					}
+				/>
+			</ActionList>
 		</VStack>
 	);
 }
