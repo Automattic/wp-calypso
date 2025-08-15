2c2305cba71728a34db9726b6214669a26e767a5
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-08-15 14:42:47 -0300

    Add new step to Start

---
+++ client/signup/steps/domain-search/index.tsx
@@ -0,0 +1,21 @@
+import StepWrapper from 'calypso/signup/step-wrapper';
+
+export type StepProps = {
+	stepSectionName: string | null;
+	stepName: string;
+	flowName: string;
+	goToStep: () => void;
+	goToNextStep: () => void;
+	submitSignupStep: ( step: unknown, dependencies: unknown ) => void;
+};
+
+export default function DomainSearchStep( props: StepProps ) {
+	return (
+		<StepWrapper
+			headerText="Domain Search"
+			subHeaderText="Domain Search"
+			stepContent={ <div>DomainSearchStep</div> }
+			{ ...props }
+		/>
+	);
+}
