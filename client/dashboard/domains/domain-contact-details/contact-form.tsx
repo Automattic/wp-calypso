d5b68b04ea65d1fc166da47abd992042e7d2abc6
Paulo Trentin <paulo@paulotrentin.com.br>
2025-08-14 21:15:48 -0300

    Use contact-form-fields to store them so the main file doesn't get too big

--- client/dashboard/domains/domain-contact-details/contact-form.tsx
+++ client/dashboard/domains/domain-contact-details/contact-form.tsx
@@ -7,22 +7,18 @@ import {
 	Button,
 	Card,
 	CardBody,
-	CheckboxControl,
-	SelectControl,
-	// eslint-disable-next-line wpcalypso/no-unsafe-wp-apis
-	__experimentalInputControl as InputControl,
 } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
 import { DataForm, Field, isItemValid } from '@wordpress/dataviews';
-import { createInterpolateElement, useCallback } from '@wordpress/element';
+import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 import { store as noticesStore } from '@wordpress/notices';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useMemo } from 'react';
 import { countryListQuery, statesListQuery } from '../../app/queries/domain-supported-contries';
 import { domainWhoisMutation } from '../../app/queries/domain-whois';
-import InlineSupportLink from '../../components/inline-support-link';
 import Notice from '../../components/notice';
 import { fetchDomainWhoisValidate, type DomainContactDetails } from '../../data/domain-whois';
+import { getContactFormFields } from './contact-form-fields';
 
 import './contact-form.scss';
 interface ContactFormProps {
@@ -98,163 +94,10 @@ export default function ContactForm( {
 		}
 	}, [ formData.countryCode, setSelectedCountryCode ] );
 
-	// Memoize the custom Edit component for the state field to prevent focus loss
-	const StateFieldEdit = useCallback(
-		( { field, onChange, data, hideLabelFromVision }: any ) => {
-			const { id, getValue } = field;
-
-			if ( ! statesList || statesList?.length === 0 ) {
-				return (
-					<InputControl
-						__next40pxDefaultSize
-						label={ hideLabelFromVision ? '' : __( 'State' ) }
-						placeholder={ __( 'State' ) }
-						value={ getValue( { item: data } ) }
-						onChange={ ( value ) => onChange( { [ id ]: value } ) }
-					/>
-				);
-			}
-
-			// If the item data is not in the statesList, set the state to the first option
-			if ( ! statesList?.some( ( state ) => state.code === getValue( { item: data } ) ) ) {
-				onChange( { [ id ]: statesList[ 0 ]?.code } );
-			}
-
-			return (
-				<SelectControl
-					__next40pxDefaultSize
-					__nextHasNoMarginBottom
-					label={ hideLabelFromVision ? '' : __( 'State' ) }
-					value={ getValue( { item: data } ) }
-					options={
-						statesList.map( ( state ) => ( {
-							label: state.name,
-							value: state.code,
-						} ) ) ?? []
-					}
-					onChange={ ( value ) => onChange( { [ id ]: value } ) }
-				/>
-			);
-		},
-		[ statesList ]
+	const fields: Field< DomainContactDetails >[] = useMemo(
+		() => getContactFormFields( countryList ?? [], statesList ?? [] ),
+		[ countryList, statesList ]
 	);
-
-	const fields: Field< DomainContactDetails >[] = [
-		{
-			id: 'firstName',
-			label: __( 'First Name' ),
-			type: 'text',
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'lastName',
-			label: __( 'Last Name' ),
-			type: 'text',
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'organization',
-			label: __( 'Organization' ),
-			type: 'text',
-		},
-		{
-			id: 'email',
-			label: __( 'Email' ),
-			type: 'email',
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'phone',
-			label: __( 'Phone' ),
-			type: 'text',
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'countryCode',
-			label: __( 'Country' ),
-			type: 'select',
-			elements:
-				countryList?.map( ( country ) => ( {
-					label: country.name,
-					value: country.code,
-				} ) ) ?? [],
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'address1',
-			label: __( 'Address' ),
-			type: 'text',
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'address2',
-			label: __( 'Address Line 2' ),
-			type: 'text',
-		},
-		{
-			id: 'city',
-			label: __( 'City' ),
-			type: 'text',
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'state',
-			label: __( 'State' ),
-			type: 'text',
-			getValue: ( { item }: { item: DomainContactDetails } ) => item.state ?? '',
-			Edit: StateFieldEdit,
-		},
-		{
-			id: 'postalCode',
-			label: __( 'Post Code' ),
-			type: 'text',
-			isValid: {
-				required: true,
-			},
-		},
-		{
-			id: 'optOutTransferLock',
-			label: __( 'Opt-out of the 60-day transfer lock' ),
-			type: 'boolean',
-			Edit: ( { field, onChange, data, hideLabelFromVision } ) => {
-				const { id, getValue } = field;
-				return (
-					<CheckboxControl
-						label={
-							hideLabelFromVision
-								? ''
-								: createInterpolateElement(
-										sprintf(
-											/* translators: %s: "what is this?" link */
-											__( 'Opt-out of the 60-day transfer lock <link>%s</link>' ),
-											__( 'what is this?' )
-										),
-										{
-											link: <InlineSupportLink supportContext="60-day-transfer-lock" />,
-										}
-								  )
-						}
-						checked={ getValue( { item: data } ) }
-						onChange={ () => onChange( { [ id ]: ! getValue( { item: data } ) } ) }
-					/>
-				);
-			},
-		},
-	];
 
 	const form = {
 		type: 'regular' as const,
