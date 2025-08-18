e57bc55b9a152baa7b1143c82b5c6026937b049f
raicem <cem.unalan@automattic.com>
2025-08-19 00:25:22 +0300

    Login/Signup: add optional "No, thanks" button
    This button is used in the Woo Core profiler, to skip Jetpack connection
    step in the flow, in case user doesn't want to connect their store to
    Jetpack.

--- client/login/wp-login/index.jsx
+++ client/login/wp-login/index.jsx
@@ -39,13 +39,16 @@ import getInitialQueryArguments from 'calypso/state/selectors/get-initial-query-
 import getIsBlazePro from 'calypso/state/selectors/get-is-blaze-pro';
 import getIsWCCOM from 'calypso/state/selectors/get-is-wccom';
 import getIsWoo from 'calypso/state/selectors/get-is-woo';
-import isWooJPCFlow from 'calypso/state/selectors/is-woo-jpc-flow';
+import isWooJPCFlow, {
+	isWooCommercePaymentsOnboardingFlow,
+} from 'calypso/state/selectors/is-woo-jpc-flow';
 import { withEnhancers } from 'calypso/state/utils';
 import { LoginContext } from '../login-context';
 import OneLoginFooter from './components/one-login-footer';
 import OneLoginLayout from './components/one-login-layout';
 import GravPoweredLoginBlockFooter from './gravatar/grav-powered-login-block-footer';
 import getHeadingSubText from './hooks/get-heading-subtext';
+import getNoThanksRedirectUrl from './hooks/get-no-thanks-redirect';
 
 import './style.scss';
 
@@ -341,6 +344,15 @@ export class Login extends Component {
 		} );
 	}
 
+	getNoThanksRedirectUrl() {
+		const { currentRoute, currentQuery } = this.props;
+
+		return getNoThanksRedirectUrl( {
+			currentRoute,
+			currentQuery,
+		} );
+	}
+
 	render() {
 		const {
 			locale,
@@ -393,9 +405,9 @@ export class Login extends Component {
 				{ ! isGravPoweredClient && (
 					<OneLoginLayout
 						isJetpack={ isJetpack }
-						isFromAkismet={ isFromAkismet }
 						signupUrl={ this.props.signupUrl }
 						isLostPasswordView={ isLostPasswordView }
+						noThanksRedirectUrl={ this.getNoThanksRedirectUrl() }
 					>
 						{ mainContent }
 					</OneLoginLayout>
@@ -451,6 +463,7 @@ export default connect(
 					new URLSearchParams( getRedirectToOriginal( state )?.split( '?' )[ 1 ] ).get( 'from' ),
 			isManualRenewalImmediateLoginAttempt: wasManualRenewalImmediateLoginAttempted( state ),
 			isUserLoggedIn: isUserLoggedIn( state ),
+			isWooPaymentsFlow: isWooCommercePaymentsOnboardingFlow( state ),
 		};
 	},
 	{
