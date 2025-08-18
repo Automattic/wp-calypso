01277cdb05daa8226172cec74062da9caf87aade
Em Shreve <em.shreve@automattic.com>
2025-08-18 17:29:38 -0400

    Fix: Generate TypeScript declarations during build (#73)

--- packages/agenttic-ui/src/components/ui/button.stories.tsx
+++ packages/agenttic-ui/src/components/ui/button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './button';
 import { ArrowUpIcon } from '../icons/ArrowUpIcon';
 
-const meta = {
+const meta: Meta< typeof Button > = {
 	title: 'Primitives/Button',
 	component: Button,
 	parameters: {
