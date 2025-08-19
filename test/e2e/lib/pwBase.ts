fab6cb38c6aaa126b90cad79859da673e2b84024
Alister Scott <alister.scott@gmail.com>
2025-08-19 12:35:16 +1000

    Add more fixtures

--- test/e2e/lib/pwBase.ts
+++ test/e2e/lib/pwBase.ts
@@ -1,4 +1,8 @@
+/* eslint-disable no-empty-pattern */
 import {
+	envToFeatureKey,
+	envVariables,
+	getTestAccountByFeature,
 	PreviewComponent,
 	SidebarComponent,
 	SiteSelectComponent,
@@ -10,15 +14,21 @@ import { test as base } from '@playwright/test';
 
 export const test = base.extend< {
 	accountGutenbergSimple: TestAccount;
+	accountGivenByEnvironment: TestAccount;
 	componentSidebar: SidebarComponent;
 	componentSiteSelect: SiteSelectComponent;
 	pageThemes: ThemesPage;
 	pageThemeDetails: ThemesDetailPage;
 	componentPreview: PreviewComponent;
 } >( {
-	// eslint-disable-next-line no-empty-pattern
 	accountGutenbergSimple: async ( {}, use ) => {
-		const testAccount = new TestAccount( 'gutenbergSimpleSiteUser' );
+		const accountName = 'gutenbergSimpleSiteUser';
+		const testAccount = new TestAccount( accountName );
+		await use( testAccount );
+	},
+	accountGivenByEnvironment: async ( {}, use ) => {
+		const accountName = getTestAccountByFeature( envToFeatureKey( envVariables ) );
+		const testAccount = new TestAccount( accountName );
 		await use( testAccount );
 	},
 	componentSidebar: async ( { page }, use ) => {
--- test/e2e/specs/appearance/theme__details-preview.spec.ts
+++ test/e2e/specs/appearance/theme__details-preview.spec.ts
@@ -15,7 +15,56 @@ test.describe( 'Themes', () => {
 		// This test will use partial matching names to cycle between available themes.
 		const themeName = 'Twenty Twen';
 
-		await accountGutenbergSimple.authenticate( page );
+		await test.step( `Authenticate as '${ accountGutenbergSimple.accountName }'`, async function () {
+			await accountGutenbergSimple.authenticate( page );
+		} );
+
+		await test.step( 'Navigate to Appearance > Themes', async function () {
+			await componentSidebar.navigate( 'Appearance', 'Themes' );
+		} );
+
+		await test.step( `Choose test site ${ testAccountSiteDomain } if Site Selector is shown`, async function () {
+			if ( await componentSiteSelect.isSiteSelectorVisible() ) {
+				await componentSiteSelect.selectSite( testAccountSiteDomain );
+			}
+		} );
+
+		await test.step( `Search for theme with keyword ${ themeName }`, async function () {
+			await pageThemes.search( themeName );
+		} );
+
+		await test.step( `Select and view details of a theme starting with ${ themeName }`, async function () {
+			const selectedTheme = await pageThemes.select( themeName );
+			await pageThemes.hoverThenClick( selectedTheme );
+		} );
+
+		await test.step( 'Preview theme', async function () {
+			await pageThemeDetails.preview();
+			await componentPreview.previewReady();
+		} );
+
+		await test.step( 'Close theme preview', async function () {
+			await componentPreview.closePreview();
+		} );
+	} );
+
+	test( 'Preview (Environment Specified Account)', async ( {
+		page,
+		accountGivenByEnvironment,
+		componentSidebar,
+		componentSiteSelect,
+		pageThemes,
+		pageThemeDetails,
+		componentPreview,
+	} ) => {
+		const testAccountSiteDomain = accountGivenByEnvironment.getSiteURL( { protocol: false } );
+
+		// This test will use partial matching names to cycle between available themes.
+		const themeName = 'Twenty Twen';
+
+		await test.step( `Authenticate as '${ accountGivenByEnvironment.accountName }'`, async function () {
+			await accountGivenByEnvironment.authenticate( page );
+		} );
 
 		await test.step( 'Navigate to Appearance > Themes', async function () {
 			await componentSidebar.navigate( 'Appearance', 'Themes' );
