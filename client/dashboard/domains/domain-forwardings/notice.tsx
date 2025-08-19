12afc78396c08759e13abec53fb5955e3aebc209
Kamen Stanev <hambai@gmail.com>
2025-08-19 15:43:29 +0300

    Fix the links in the domain forwarding notices to work with the dashboard paths

--- client/dashboard/domains/domain-forwardings/notice.tsx
+++ client/dashboard/domains/domain-forwardings/notice.tsx
@@ -1,7 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { Link } from '@tanstack/react-router';
 import { Notice } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { domainQuery } from '../../app/queries/domain';
+import { domainNameServersRoute, siteDomainsRoute } from '../../app/router';
 
 interface DomainForwardingNoticeProps {
 	domainName: string;
@@ -13,20 +16,36 @@ export const DomainForwardingNotice = ( { domainName }: DomainForwardingNoticePr
 	const usesDefaultNameservers = domainData?.has_wpcom_nameservers;
 	const isPrimaryDomain = domainData?.primary_domain;
 	const isDomainOnly = domainData?.is_domain_only_site;
+	const siteSlug = domainData?.site_slug;
 
+	// TODO: Should the first notice link to the domain mapping setup page???
+	// That's how it works in client/my-sites/domains/domain-management/settings/index.tsx
+	// but I'm not sure how useful that is if at all???
+
+	// TODO: The second link is to set a primary domain for a site, but I'm not sure if that'll be the correct link - check it once it's clear what the correct link is
 	if ( ! usesDefaultNameservers ) {
 		return (
 			<Notice status="warning" isDismissible={ false }>
-				{ __(
-					"Your domain is using external name servers so the Domain Forwarding records you're editing won't be in effect until you switch to use WordPress.com name servers. Update your name servers now."
+				{ createInterpolateElement(
+					__(
+						"Your domain is using external name servers so the Domain Forwarding records you're editing won't be in effect until you switch to use WordPress.com name servers. <link>Update your name servers now</link>."
+					),
+					{
+						link: <Link to={ domainNameServersRoute.fullPath } params={ { domainName } } />,
+					}
 				) }
 			</Notice>
 		);
 	} else if ( isPrimaryDomain && ! isDomainOnly ) {
 		return (
 			<Notice status="info" isDismissible={ false }>
-				{ __(
-					"This domain is your site's main address. You can forward subdomains or set a new primary site address to forward the root domain."
+				{ createInterpolateElement(
+					__(
+						"This domain is your site's main address. You can forward subdomains or <link>set a new primary site address</link> to forward the root domain."
+					),
+					{
+						link: <Link to={ siteDomainsRoute.fullPath } params={ { siteSlug } } />,
+					}
 				) }
 			</Notice>
 		);
