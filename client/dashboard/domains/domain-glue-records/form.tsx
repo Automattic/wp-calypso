75d9deeb2e049891e2d9070838c004b311f85f1a
Miroslav Mitev <m1r0@users.noreply.github.com>
2025-08-19 16:33:09 +0300

    Replace `ValidatedInputControl` with `InputControl`
    The `ValidatedInputControl` component is still in beta, so we decided not to use it.
    This means that the input won't show error messages for now.

--- client/dashboard/domains/domain-glue-records/form.tsx
+++ client/dashboard/domains/domain-glue-records/form.tsx
@@ -1,16 +1,16 @@
-// eslint-disable-next-line no-restricted-imports
-import { ValidatedInputControl } from '@automattic/components/src/validated-form-controls/components/input-control';
 import {
 	Card,
 	CardBody,
 	__experimentalHStack as HStack,
 	__experimentalVStack as VStack,
+	__experimentalInputControl as InputControl,
 	__experimentalInputControlSuffixWrapper as InputControlSuffixWrapper,
 	Button,
 } from '@wordpress/components';
 import { DataForm, isItemValid } from '@wordpress/dataviews';
 import { useState, useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { Text } from '../../components/text';
 import { isValidIpAddress, isValidNameServerSubdomain } from '../../utils/domain';
 import type { DomainGlueRecord } from '../../data/domain-glue-records';
 import type { Field } from '@wordpress/dataviews';
@@ -63,22 +63,31 @@ export default function DomainGlueRecordsForm( {
 					const value = getValue( { item: data } ).replace( suffix, '' );
 
 					return (
-						<ValidatedInputControl
+						// TODO: Show the error via Data Form when the ValidatedInputControl component is ready.
+						<InputControl
+							__next40pxDefaultSize
 							label={ field.label }
 							placeholder={ field.placeholder }
 							value={ value }
 							onChange={ ( value ) => {
 								return onChange( { [ id ]: value + suffix } );
 							} }
-							customValidator={ ( value ) => {
-								if ( ! value || ! isValidNameServerSubdomain( value ) ) {
-									return __( 'Please enter a valid name server.' );
-								}
-							} }
-							required
-							suffix={ <InputControlSuffixWrapper>{ suffix }</InputControlSuffixWrapper> }
+							suffix={
+								<InputControlSuffixWrapper>
+									<Text variant="muted">{ suffix }</Text>
+								</InputControlSuffixWrapper>
+							}
 						/>
 					);
+				},
+				isValid: {
+					required: true,
+					custom: ( item ) => {
+						if ( ! isValidNameServerSubdomain( item.nameServer ) ) {
+							return __( 'Please enter a valid name server.' );
+						}
+						return null;
+					},
 				},
 				readOnly: isEdit,
 			},
