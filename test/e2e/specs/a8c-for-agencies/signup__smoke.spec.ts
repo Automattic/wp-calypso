3bf6ee7342530e9f929640cbc03d431635739c2f
Alister Scott <alister.scott@gmail.com>
2025-08-20 15:10:53 +1000

    Update A4A test to use Given/When/Then

--- test/e2e/specs/a8c-for-agencies/signup__smoke.spec.ts
+++ test/e2e/specs/a8c-for-agencies/signup__smoke.spec.ts
@@ -5,34 +5,39 @@ const A4A_URL = 'https://agencies.automattic.com';
 /**
  * Verify the A4A > Signup page loads
  */
-test.describe( 'A4A > Signup: Smoke Test @calypso-pr', () => {
-	test( 'Navigate to A4A > Signup (Acccount given by environment)', async ( {
+test.describe( 'Automattic For Agencies Sign Up Page', () => {
+	test( 'As an anauthenticated web agency owner I can enter my agency details and see these displayed', async ( {
 		page,
-		accountGivenByEnvironment,
 	} ) => {
-		await accountGivenByEnvironment.authenticate( page );
-
-		await page.goto( `${ A4A_URL }/signup` );
-
-		// Enter first name
 		const firstName = 'John';
-		await page.getByPlaceholder( 'Your first name' ).fill( firstName );
-
-		// Enter last name
 		const lastName = 'Doe';
-		await page.getByPlaceholder( 'Your last name' ).fill( lastName );
-
-		// Enter the agency name
 		const agencyName = 'Agency name';
-		await page.getByPlaceholder( 'Agency name' ).fill( agencyName );
-
-		// Enter the business URL
 		const businessURL = 'https://example.com';
-		await page.getByPlaceholder( 'Business URL' ).fill( businessURL );
 
-		// Verify the form values
-		expect( await page.getByPlaceholder( 'Your first name' ).inputValue() ).toBe( firstName );
-		expect( await page.getByPlaceholder( 'Your last name' ).inputValue() ).toBe( lastName );
-		expect( await page.getByPlaceholder( 'Business URL' ).inputValue() ).toBe( businessURL );
+		await test.step( 'Given I am on the Automattic For Agencies Sign Up Page', async () => {
+			await page.goto( `${ A4A_URL }/signup` );
+		} );
+
+		await test.step( 'When I enter my first name', async () => {
+			await page.getByPlaceholder( 'Your first name' ).fill( firstName );
+		} );
+
+		await test.step( 'And I enter my last name', async () => {
+			await page.getByPlaceholder( 'Your last name' ).fill( lastName );
+		} );
+
+		await test.step( 'And I enter my agency name', async () => {
+			await page.getByPlaceholder( 'Agency name' ).fill( agencyName );
+		} );
+
+		await test.step( 'And I enter my agency URL', async () => {
+			await page.getByPlaceholder( 'Business URL' ).fill( businessURL );
+		} );
+
+		await test.step( 'Then the form values display what I have enteredL', async () => {
+			expect( await page.getByPlaceholder( 'Your first name' ).inputValue() ).toBe( firstName );
+			expect( await page.getByPlaceholder( 'Your last name' ).inputValue() ).toBe( lastName );
+			expect( await page.getByPlaceholder( 'Business URL' ).inputValue() ).toBe( businessURL );
+		} );
 	} );
 } );
