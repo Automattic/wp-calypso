88f23771df0d5653971fdcc544742a1b7ecd7818
Richard Ortiz <rcrd.ortiz@gmail.com>
2025-08-21 10:30:35 +0200

    Updated from Link to button component to avoid opening the action in a new tab

--- packages/help-center/src/components/help-center-more-resources.tsx
+++ packages/help-center/src/components/help-center-more-resources.tsx
@@ -4,7 +4,7 @@ import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { localizeUrl } from '@automattic/i18n-utils';
 import { backup, chevronRight, external, Icon, rss, video } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useHelpCenterContext } from '../contexts/HelpCenterContext';
 
 import './help-center-more-resources.scss';
@@ -12,6 +12,7 @@ import './help-center-more-resources.scss';
 export const HelpCenterMoreResources = () => {
 	const { __ } = useI18n();
 	const { sectionName } = useHelpCenterContext();
+	const navigate = useNavigate();
 
 	const trackMoreResourcesButtonClick = ( resource: string ) => {
 		recordTracksEvent( 'calypso_help_moreresources_click', {
@@ -42,17 +43,18 @@ export const HelpCenterMoreResources = () => {
 			>
 				<li className="help-center-more-resources__resource-item help-center-link__item">
 					<div className="help-center-more-resources__resource-cell help-center-link__cell">
-						<Link
-							to="/chat-history"
+						<button
+							type="button"
 							onClick={ () => {
 								trackMoreResourcesButtonClick( 'support-history' );
+								navigate( '/chat-history' );
 							} }
 							className="help-center-more-resources__support-history"
 						>
 							<Icon icon={ backup } size={ 24 } />
 							<span>{ __( 'Support history', __i18n_text_domain__ ) }</span>
 							<Icon icon={ chevronRight } size={ 20 } />
-						</Link>
+						</button>
 					</div>
 				</li>
 				<li className="help-center-more-resources__resource-item help-center-link__item">
