ddf0731bdbe77e4a969df4533ca33ee12b96fb1a
Igor Giussani <gius80@gmail.com>
2025-08-21 16:08:28 +0200

    Simplify component

--- client/dashboard/domains/domain-dns/summary.tsx
+++ client/dashboard/domains/domain-dns/summary.tsx
@@ -2,7 +2,7 @@ import { __ } from '@wordpress/i18n';
 import RouterLinkSummaryButton from '../../components/router-link-summary-button';
 import type { Domain } from '../../data/types';
 
-export default function NameServersSettingsSummary( { domain }: { domain: Domain } ) {
+export default function DnsSettingsSummary( { domain }: { domain: Domain } ) {
 	let badges = [];
 	if ( domain.has_wpcom_nameservers ) {
 		badges = [ { text: __( 'Using WordPress.com name servers' ), intent: 'success' as const } ];
@@ -12,8 +12,8 @@ export default function NameServersSettingsSummary( { domain }: { domain: Domain
 
 	return (
 		<RouterLinkSummaryButton
-			to={ `/domains/${ domain.domain }/name-servers` }
-			title={ __( 'Name servers' ) }
+			to={ `/domains/${ domain.domain }/dns` }
+			title={ __( 'DNS records' ) }
 			badges={ badges }
 		/>
 	);
