fa80c37192d2c3a17a7ecae93f1fdeaf11ee9d62
Tiago Ilieve <tiago.ilieve@automattic.com>
2025-08-21 00:16:04 +0000

    Backups: add download page scaffolding

--- client/dashboard/app/router/sites.tsx
+++ client/dashboard/app/router/sites.tsx
@@ -251,6 +251,17 @@ export const siteBackupRestoreRoute = createRoute( {
 	)
 );
 
+export const siteBackupDownloadRoute = createRoute( {
+	getParentRoute: () => siteBackupsRoute,
+	path: '$rewindId/download',
+} ).lazy( () =>
+	import( '../../sites/backup-download' ).then( ( d ) =>
+		createLazyRoute( 'site-backup-download' )( {
+			component: d.default,
+		} )
+	)
+);
+
 export const siteDomainsRoute = createRoute( {
 	getParentRoute: () => siteRoute,
 	path: 'domains',
@@ -657,7 +668,11 @@ export const createSitesRoutes = ( config: AppConfig ) => {
 
 	if ( config.supports.sites.backups ) {
 		siteRoutes.push(
-			siteBackupsRoute.addChildren( [ siteBackupsIndexRoute, siteBackupRestoreRoute ] )
+			siteBackupsRoute.addChildren( [
+				siteBackupsIndexRoute,
+				siteBackupRestoreRoute,
+				siteBackupDownloadRoute,
+			] )
 		);
 	}
 
