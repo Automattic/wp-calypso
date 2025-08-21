8de261b2bb6a295e440518a56e6e273eca713862
Payton Swick <payton@foolord.com>
2025-08-21 10:14:31 -0400

    Move formatting of Jetpack key data to fetch function

--- client/dashboard/app/queries/site-jetpack-keys.ts
+++ client/dashboard/app/queries/site-jetpack-keys.ts
@@ -5,11 +5,4 @@ export const siteJetpackKeysQuery = ( siteId: number ) =>
 	queryOptions( {
 		queryKey: [ 'site', siteId, 'jetpack_keys' ],
 		queryFn: () => fetchJetpackKeys( siteId ),
-		select: ( data ): { slug: string; key: string }[] => {
-			const plugins: { slug: string; key: string }[] = [];
-			Object.entries( data.keys ).forEach( ( [ slug, key ] ) => {
-				plugins.push( { slug, key } );
-			} );
-			return plugins;
-		},
 	} );
--- client/dashboard/data/site-jetpack-keys.ts
+++ client/dashboard/data/site-jetpack-keys.ts
@@ -1,6 +1,6 @@
 import wpcom from 'calypso/lib/wp';
 
-export interface JetpackPluginKeys {
+interface RawJetpackPluginKeys {
 	success: true;
 	keys: {
 		vaultpress?: string;
@@ -8,6 +8,16 @@ export interface JetpackPluginKeys {
 	};
 }
 
-export async function fetchJetpackKeys( siteId: number ): Promise< JetpackPluginKeys > {
-	return wpcom.req.get( `/jetpack-blogs/${ siteId }/keys/` );
+export interface JetpackPluginKey {
+	slug: string;
+	key: string;
+}
+
+export async function fetchJetpackKeys( siteId: number ): Promise< JetpackPluginKey[] > {
+	const raw: RawJetpackPluginKeys = await wpcom.req.get( `/jetpack-blogs/${ siteId }/keys/` );
+	const plugins: JetpackPluginKey[] = [];
+	Object.entries( raw.keys ).forEach( ( [ slug, key ] ) => {
+		plugins.push( { slug, key } );
+	} );
+	return plugins;
 }
