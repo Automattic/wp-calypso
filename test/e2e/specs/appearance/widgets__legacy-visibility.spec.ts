f34ab620736c1fa49547cfe9186a9d08d7c7fd28
Alister Scott <alister.scott@gmail.com>
2025-08-22 15:48:52 +1000

    Create widgets__legacy-visibility.spec.ts

---
+++ test/e2e/specs/appearance/widgets__legacy-visibility.spec.ts
@@ -0,0 +1,48 @@
+import { BlockWidgetEditorComponent, envVariables, RestAPIClient } from '@automattic/calypso-e2e';
+import { test } from '../../lib/pw_base';
+
+test.describe( 'Theme Widgets (Legacy)', () => {
+	test( 'As a non-atomic site user, I can use widgets on my site', async ( {
+		page,
+		accountGivenByEnvironment,
+	} ) => {
+		test.skip(
+			envVariables.TEST_ON_ATOMIC,
+			'Skipping for Atomic sites as the themes do not support widgets'
+		);
+
+		await test.step( `Given I am authenticated as '${ accountGivenByEnvironment.accountName }'`, async function () {
+			await accountGivenByEnvironment.authenticate( page );
+		} );
+
+		await test.step( 'And I have cleared all widgets on my site', async function () {
+			const restAPIClient = new RestAPIClient( accountGivenByEnvironment.credentials );
+			await restAPIClient.deleteAllWidgets(
+				accountGivenByEnvironment.credentials.testSites?.primary.id as number
+			);
+		} );
+
+		await test.step( 'When I navigate to Appearance > Widgets', async function () {
+			await page.goto(
+				`https://${ accountGivenByEnvironment.credentials.testSites?.primary.url }/wp-admin/widgets.php`,
+				{ timeout: 25 * 1000 }
+			);
+		} );
+
+		await test.step( 'And I dismiss the Welcome modals', async function () {
+			const blockWidgetEditorComponent = new BlockWidgetEditorComponent( page );
+			await blockWidgetEditorComponent.dismissModals();
+		} );
+
+		await test.step( 'And I insert a Legacy Widget', async function () {
+			await page.getByRole( 'button', { name: 'Add block' } ).click();
+			await page.fill( 'input[placeholder="Search"]', 'Authors' );
+			await page.click( 'button.editor-block-list-item-legacy-widget\\/authors' );
+		} );
+
+		await test.step( 'Then visibility options are shown for the Legacy Widget', async function () {
+			await page.click( 'a.button:text("Visibility")' );
+			await page.waitForSelector( 'div.widget-conditional' );
+		} );
+	} );
+} );
