c59e543d5de8d4320707cfa57b6b6602f28656bf
Claudiu Filip <claudiu.filip@automattic.com>
2025-08-22 11:52:12 +0300

    Harcode winning variation to the streamlined pricing hook

--- client/my-sites/plans-features-main/hooks/use-streamlined-price-experiment.ts
+++ client/my-sites/plans-features-main/hooks/use-streamlined-price-experiment.ts
@@ -1,17 +1,12 @@
 import { getFlowFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url';
 import isAkismetCheckout from 'calypso/lib/akismet/is-akismet-checkout';
-import { useExperiment, loadExperimentAssignment } from 'calypso/lib/explat';
 import isJetpackCheckout from 'calypso/lib/jetpack/is-jetpack-checkout';
 import { getSignupCompleteFlowName } from 'calypso/signup/storageUtils';
 
-const STREAMLINED_PRICE_EXPERIMENT_NAME = 'calypso_streamlined_plans_checkout';
-
 export function useStreamlinedPriceExperiment(): [ boolean, string | null ] {
-	const [ isLoadingExperiment, assignment ] = useExperiment( STREAMLINED_PRICE_EXPERIMENT_NAME, {
-		isEligible: isEligibleForExperiment(),
-	} );
+	const variationName = isEligibleForExperiment() ? 'plans_1Y_checkout_radio' : null;
 
-	return [ isLoadingExperiment, assignment?.variationName ?? null ];
+	return [ false, variationName ];
 }
 
 function isEligibleForExperiment(): boolean {
@@ -20,11 +15,6 @@ function isEligibleForExperiment(): boolean {
 	const flow = flowFromStorage || flowFromURL;
 	// Only onboarding flow is eligible for streamlined pricing. Akismet/Jetpack checkouts are excluded as well.
 	return flow === 'onboarding' && ! isAkismetCheckout() && ! isJetpackCheckout();
-}
-
-export async function isStreamlinedPriceExperiment() {
-	const assignment = await loadExperimentAssignment( STREAMLINED_PRICE_EXPERIMENT_NAME );
-	return assignment?.variationName !== null;
 }
 
 export function isStreamlinedPricePlansTreatment( variationName?: string | null ) {
