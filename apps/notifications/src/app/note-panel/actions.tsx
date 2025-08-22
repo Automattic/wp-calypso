904d939e27f8fe64d5917f92b813e523537a42bb
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-08-22 22:32:21 +0700

    Notifications v2: add Keybord Shortcuts action menu

--- apps/notifications/src/app/note-panel/actions.tsx
+++ apps/notifications/src/app/note-panel/actions.tsx
@@ -1,26 +1,49 @@
 import { MenuGroup, MenuItem, DropdownMenu } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { moreVertical } from '@wordpress/icons';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import actions from '../../panel/state/actions';
+import getIsShortcutsPopoverOpen from '../../panel/state/selectors/get-is-shortcuts-popover-open';
+import NoteShortcuts from '../note-shortcuts';
 
 export default function NotePanelActions() {
 	const dispatch = useDispatch();
 
+	const isShortcutsPopoverOpen = useSelector( getIsShortcutsPopoverOpen );
+
 	return (
-		<DropdownMenu icon={ moreVertical } label={ __( 'Actions' ) }>
-			{ ( { onClose } ) => (
-				<MenuGroup>
-					<MenuItem
-						onClick={ () => {
-							onClose();
-							dispatch( actions.ui.viewSettings() );
-						} }
-					>
-						{ __( 'Settings' ) }
-					</MenuItem>
-				</MenuGroup>
-			) }
+		<DropdownMenu
+			icon={ moreVertical }
+			label={ __( 'Actions' ) }
+			onToggle={ ( willOpen ) => {
+				if ( ! willOpen ) {
+					dispatch( actions.ui.closeShortcutsPopover() );
+				}
+			} }
+		>
+			{ ( { onClose } ) =>
+				isShortcutsPopoverOpen ? (
+					<NoteShortcuts />
+				) : (
+					<MenuGroup>
+						<MenuItem
+							onClick={ () => {
+								dispatch( actions.ui.toggleShortcutsPopover() );
+							} }
+						>
+							{ __( 'Keyboard shortcuts' ) }
+						</MenuItem>
+						<MenuItem
+							onClick={ () => {
+								onClose();
+								dispatch( actions.ui.viewSettings() );
+							} }
+						>
+							{ __( 'Settings' ) }
+						</MenuItem>
+					</MenuGroup>
+				)
+			}
 		</DropdownMenu>
 	);
 }
