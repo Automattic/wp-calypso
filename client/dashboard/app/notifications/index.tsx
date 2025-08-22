98bd990f4c31db882e80d414a5a2886722040c83
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-08-22 11:39:12 +0700

    Notifications v2: refactor: dont async-load bell icon to avoid layout shift

--- client/dashboard/app/notifications/index.tsx
+++ client/dashboard/app/notifications/index.tsx
@@ -1,8 +1,12 @@
 import { useNavigate } from '@tanstack/react-router';
+import { Button, Dropdown } from '@wordpress/components';
+import '@wordpress/components/build-style/style.css';
+import { __ } from '@wordpress/i18n';
+import { bellUnread, bell } from '@wordpress/icons';
 import { Suspense, lazy } from 'react';
 import wpcom from 'calypso/lib/wp';
 
-const AsyncNoteDropdown = lazy( () => import( '@automattic/notifications/src/dropdown' ) );
+const AsyncNotificationApp = lazy( () => import( '@automattic/notifications/src/app' ) );
 
 export default function Notifications( { className }: { className: string } ) {
 	const navigate = useNavigate();
@@ -17,13 +21,32 @@ export default function Notifications( { className }: { className: string } ) {
 		CLOSE_PANEL: [ onClosePanel ],
 	} );
 
+	// TODO: fetch unread notifications count
+	const hasUnreadNotifications = false;
+
 	return (
-		<Suspense fallback={ null }>
-			<AsyncNoteDropdown
-				className={ className }
-				actionHandlers={ actionHandlers }
-				wpcom={ wpcom }
-			/>
-		</Suspense>
+		<Dropdown
+			popoverProps={ {
+				placement: 'bottom-end',
+				offset: 8,
+			} }
+			renderToggle={ ( { isOpen, onToggle } ) => (
+				<Button
+					className={ className }
+					onClick={ onToggle }
+					aria-expanded={ isOpen }
+					variant="tertiary"
+					label={ __( 'Notifications' ) }
+					icon={ hasUnreadNotifications ? bellUnread : bell }
+				/>
+			) }
+			renderContent={ ( { onClose } ) => (
+				<div style={ { width: '480px', margin: '-8px' } }>
+					<Suspense fallback={ null }>
+						<AsyncNotificationApp actionHandlers={ actionHandlers( onClose ) } wpcom={ wpcom } />
+					</Suspense>
+				</div>
+			) }
+		/>
 	);
 }
