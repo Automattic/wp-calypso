661bede93b1d5d153d56f7652abf40c20f115114
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-26 19:15:40 +0200

    Fix sprintf number replacement

--- client/dashboard/domains/domain-overview/featured-card-emails.tsx
+++ client/dashboard/domains/domain-overview/featured-card-emails.tsx
@@ -1,5 +1,5 @@
 import { Icon } from '@wordpress/components';
-import { __, _n } from '@wordpress/i18n';
+import { __, _n, sprintf } from '@wordpress/i18n';
 import { envelope } from '@wordpress/icons';
 import { Domain } from '../../data/domain';
 import OverviewCard from '../../sites/overview-card';
@@ -23,10 +23,16 @@ interface Props {
 }
 
 export default function FeaturedCardEmails( { domain, mailboxes }: Props ) {
+	const additionalMailboxes = mailboxes.length - 1;
+
 	const description =
-		mailboxes.length > 1
-			? // translators: %d is the number of additional mailboxes.
-			  _n( '+ one more mailbox', '+ %d more mailboxes', mailboxes.length - 1 )
+		additionalMailboxes > 0
+			? // eslint-disable-next-line @wordpress/valid-sprintf
+			  sprintf(
+					// translators: %d is the number of additional mailboxes.
+					_n( '+ one more mailbox', '+ %d more mailboxes', additionalMailboxes ),
+					additionalMailboxes
+			  )
 			: getAccountTypeLabel( mailboxes[ 0 ].account_type );
 
 	return (
