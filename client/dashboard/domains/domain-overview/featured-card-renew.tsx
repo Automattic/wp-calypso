fcd7d8fbe4de5c98628128f54334b0a46418b3d4
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-26 16:52:36 +0200

    Remove custom HTML and use OverviewCard

--- client/dashboard/domains/domain-overview/featured-card-renew.tsx
+++ client/dashboard/domains/domain-overview/featured-card-renew.tsx
@@ -1,15 +1,9 @@
-import {
-	Card,
-	CardBody,
-	Icon,
-	__experimentalText as Text,
-	__experimentalVStack as VStack,
-	__experimentalHStack as HStack,
-} from '@wordpress/components';
+import { Icon } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { calendar } from '@wordpress/icons';
 import { useLocale } from '../../app/locale';
 import { Domain } from '../../data/domain';
+import OverviewCard from '../../sites/overview-card';
 import { formatDate } from '../../utils/datetime';
 
 interface Props {
@@ -27,27 +21,13 @@ export default function FeaturedCardRenew( { domain }: Props ) {
 	} );
 
 	return (
-		<Card className="featured-card">
-			<CardBody>
-				<VStack spacing={ 4 }>
-					<HStack spacing={ 2 } justify="flex-start">
-						<Icon icon={ calendar } size={ 24 } />
-						<Text className="featured-card__title" upperCase size="footnote">
-							{ __( 'Renews' ) }
-						</Text>
-					</HStack>
-					<VStack>
-						<Text size="title" weight={ 500 }>
-							{ formattedDate }
-						</Text>
-						<Text className="featured-card__body" size="body">
-							{ domain.auto_renewing
-								? __( 'Auto-renew is enabled.' )
-								: __( 'Auto-renew is disabled.' ) }
-						</Text>
-					</VStack>
-				</VStack>
-			</CardBody>
-		</Card>
+		<OverviewCard
+			title={ __( 'Renews' ) }
+			heading={ formattedDate }
+			icon={ <Icon icon={ calendar } /> }
+			description={
+				domain.auto_renewing ? __( 'Auto-renew is enabled.' ) : __( 'Auto-renew is disabled.' )
+			}
+		/>
 	);
 }
