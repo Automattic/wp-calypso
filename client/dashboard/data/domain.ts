192fad1d6c8286bfaf2c2317885b3a8d5df79a0f
Igor Giussani <gius80@gmail.com>
2025-08-26 17:40:57 +0200

    Render transfer option - internal transfers

--- client/dashboard/data/domain.ts
+++ client/dashboard/data/domain.ts
@@ -3,6 +3,8 @@ import type { DomainSummary } from './domains';
 
 export interface Domain extends DomainSummary {
 	can_manage_name_servers: boolean;
+	can_transfer_to_any_user: boolean;
+	can_transfer_to_other_site: boolean;
 	cannot_manage_name_servers_reason: null | string;
 	dnssec_records?: {
 		dnskey: string[];
--- client/dashboard/domains/domain-transfer/index.tsx
+++ client/dashboard/domains/domain-transfer/index.tsx
@@ -2,20 +2,22 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { domainQuery } from '../../app/queries/domain';
-// import { sitePurchaseQuery } from '../../app/queries/site-purchases';
 import { domainRoute } from '../../app/router/domains';
+import { ActionList } from '../../components/action-list';
 import InlineSupportLink from '../../components/inline-support-link';
 import Notice from '../../components/notice';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
+import RouterLinkButton from '../../components/router-link-button';
+import { DomainSubtype } from '../../data/domains';
+
+export function getTopLevelOfTld( domainName: string ): string {
+	return domainName.substring( domainName.lastIndexOf( '.' ) + 1 );
+}
 
 export default function DomainOverview() {
-	// const locale = useLocale();
 	const { domainName } = domainRoute.useParams();
 	const { data: domain } = useSuspenseQuery( domainQuery( domainName ) );
-	// const { data: purchase } = useQuery(
-	// 	sitePurchaseQuery( domain.blog_id, parseInt( domain.subscription_id, 10 ) )
-	// );
 
 	const renderTransferInfo = () => {
 		return (
@@ -32,6 +34,77 @@ export default function DomainOverview() {
 		);
 	};
 
+	const renderInternalTransferOptions = () => {
+		const actions = [];
+		if ( ! domain.is_domain_only_site && domain.can_transfer_to_any_user ) {
+			const description =
+				domain.subtype.id === DomainSubtype.DOMAIN_CONNECTION
+					? __( 'Transfer this domain connection to any administrator on this site' )
+					: __( 'Transfer this domain to any administrator on this site' );
+			actions.push(
+				<ActionList.ActionItem
+					key="transfer-to-another-user"
+					title={ __( 'Transfer to another user' ) }
+					description={ description }
+					actions={
+						<RouterLinkButton
+							variant="secondary"
+							size="compact"
+							to={ `/domains/${ domain.domain }/transfer/other-user` }
+						>
+							{ __( 'Continue' ) }
+						</RouterLinkButton>
+					}
+				/>
+			);
+		} else if (
+			! [ 'uk', 'fr', 'ca', 'de', 'jp' ].includes( getTopLevelOfTld( domain.domain ) ) &&
+			domain.can_transfer_to_other_site
+		) {
+			actions.push(
+				<ActionList.ActionItem
+					key="transfer-to-any-use"
+					title={ __( 'To another WordPress.com user' ) }
+					description={ __( 'Transfer this domain to another WordPress.com user' ) }
+					actions={
+						<RouterLinkButton
+							variant="secondary"
+							size="compact"
+							to={ `/domains/${ domain.domain }/transfer/any-user` }
+						>
+							{ __( 'Continue' ) }
+						</RouterLinkButton>
+					}
+				/>
+			);
+		}
+		if ( domain.can_transfer_to_other_site ) {
+			const description =
+				domain.subtype.id === DomainSubtype.DOMAIN_CONNECTION
+					? __( 'Transfer this domain connection to any site you are an administrator on' )
+					: __( 'Transfer this domain to any site you are an administrator on' );
+			actions.push(
+				<ActionList.ActionItem
+					key="transfer-to-another-site"
+					title={ __( 'To another WordPress.com siter' ) }
+					description={ description }
+					actions={
+						<RouterLinkButton
+							variant="secondary"
+							size="compact"
+							to={ `/domains/${ domain.domain }/transfer/other-site` }
+						>
+							{ __( 'Continue' ) }
+						</RouterLinkButton>
+					}
+				/>
+			);
+		}
+		if ( actions.length > 0 ) {
+			return <ActionList>{ actions }</ActionList>;
+		}
+	};
+
 	const isDomainTransferable =
 		! domain.is_hundred_year_domain &&
 		! domain.is_redeemable &&
@@ -42,7 +115,7 @@ export default function DomainOverview() {
 	return (
 		<PageLayout size="small" header={ <PageHeader title={ __( 'Transfer' ) } /> }>
 			{ renderTransferInfo() }
-			{ isDomainTransferable && <>Domain is transferable</> }
+			{ isDomainTransferable && <>{ renderInternalTransferOptions() }</> }
 		</PageLayout>
 	);
 }
