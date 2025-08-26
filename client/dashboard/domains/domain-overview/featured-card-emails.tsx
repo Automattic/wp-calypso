18445fc2911eb7666631013bc6bedefa38a124fa
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-26 19:01:42 +0200

    Create email featured overview card

---
+++ client/dashboard/domains/domain-overview/featured-card-emails.tsx
@@ -0,0 +1,44 @@
+import { Icon } from '@wordpress/components';
+import { __, _n } from '@wordpress/i18n';
+import { envelope } from '@wordpress/icons';
+import { Domain } from '../../data/domain';
+import OverviewCard from '../../sites/overview-card';
+import type { Mailbox, EmailProvider } from '../../data/emails';
+
+const getAccountTypeLabel = ( accountType: EmailProvider ) => {
+	switch ( accountType ) {
+		case 'google_workspace':
+			return __( 'Google Workspace' );
+		case 'forwarding':
+			return __( 'Forwarding' );
+		case 'titan':
+		default:
+			return __( 'Professional Email' );
+	}
+};
+
+interface Props {
+	domain: Domain;
+	mailboxes: Mailbox[];
+}
+
+export default function FeaturedCardEmails( { domain, mailboxes }: Props ) {
+	const description =
+		mailboxes.length > 1
+			? // translators: %d is the number of additional mailboxes.
+			  _n( '+ one more mailbox', '+ %d more mailboxes', mailboxes.length - 1 )
+			: getAccountTypeLabel( mailboxes[ 0 ].account_type );
+
+	return (
+		<OverviewCard
+			title={ __( 'Emails' ) }
+			heading={
+				<span style={ { wordBreak: 'break-all' } }>
+					{ `${ mailboxes[ 0 ].mailbox }@${ domain.domain }` }
+				</span>
+			}
+			icon={ <Icon icon={ envelope } /> }
+			description={ description }
+		/>
+	);
+}
