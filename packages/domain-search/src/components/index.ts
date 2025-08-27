651da1e09fff0be41713dc0e69fdc9b0684883c1
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-08-27 16:32:03 -0300

    Update module exports

---
+++ packages/domain-search/src/components/index.ts
@@ -0,0 +1,1 @@
+export { Filter } from './search-bar/filter';
--- packages/domain-search/src/index.ts
+++ packages/domain-search/src/index.ts
@@ -1,5 +1,6 @@
 export * from './ui';
 export * from './helpers';
+export * from './components';
 
 export { useDomainSuggestionContainer } from './hooks/use-domain-suggestion-container';
 export { useTypedPlaceholder } from './hooks/use-typed-placeholder';
