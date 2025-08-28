123d76361f02fc8fa0b262b51154ff8d7fe08b2d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-28 12:28:21 +0200

    Move mailbox fetch to Email overview card

--- client/dashboard/domains/domain-overview/featured-card-emails.tsx
+++ client/dashboard/domains/domain-overview/featured-card-emails.tsx
@@ -1,10 +1,12 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { Icon } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
 import { envelope } from '@wordpress/icons';
+import { mailboxesQuery } from '../../app/queries/emails';
 import { emailsRoute } from '../../app/router/emails';
 import { Domain } from '../../data/domain';
 import OverviewCard from '../../sites/overview-card';
-import type { Mailbox, EmailProvider } from '../../data/emails';
+import type { EmailProvider, Mailbox } from '../../data/emails';
 
 const getAccountTypeLabel = ( accountType: EmailProvider ) => {
 	switch ( accountType ) {
@@ -28,30 +30,37 @@ const getAdditionlMailboxesLabel = ( count: number ) => {
 		  );
 };
 
-interface Props {
-	domain: Domain;
-	mailboxes: Mailbox[];
-}
-
-export default function FeaturedCardEmails( { domain, mailboxes }: Props ) {
+const getDescription = ( mailboxes: Mailbox[] ) => {
 	const additionalMailboxes = mailboxes.length - 1;
 
-	const description =
-		additionalMailboxes > 0
-			? getAdditionlMailboxesLabel( additionalMailboxes )
-			: getAccountTypeLabel( mailboxes[ 0 ].account_type );
+	if ( mailboxes.length === 0 ) {
+		return __( 'Add proffesional email' );
+	}
+
+	return additionalMailboxes > 0
+		? getAdditionlMailboxesLabel( additionalMailboxes )
+		: getAccountTypeLabel( mailboxes[ 0 ].account_type );
+};
+
+interface Props {
+	domain: Domain;
+}
+
+export default function FeaturedCardEmails( { domain }: Props ) {
+	const { data: mailboxes } = useSuspenseQuery( mailboxesQuery( domain.blog_id ) );
+
+	const email = mailboxes.length
+		? `${ mailboxes[ 0 ].mailbox }@${ domain.domain }`
+		: // translators: %s is the mailbox name: youremail@example.com
+		  sprintf( __( 'youremail@%s' ), domain.domain );
 
 	return (
 		<OverviewCard
 			title={ __( 'Emails' ) }
-			heading={
-				<span style={ { wordBreak: 'break-all' } }>
-					{ `${ mailboxes[ 0 ].mailbox }@${ domain.domain }` }
-				</span>
-			}
+			heading={ <span style={ { wordBreak: 'break-all' } }>{ email }</span> }
 			link={ emailsRoute.fullPath }
 			icon={ <Icon icon={ envelope } /> }
-			description={ description }
+			description={ getDescription( mailboxes ) }
 		/>
 	);
 }
