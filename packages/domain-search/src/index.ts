ae724b9441ab2f9fb21f6c2650e4c26733f8330d
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-08-28 13:53:05 -0300

    Do not export components

--- packages/domain-search/src/index.ts
+++ packages/domain-search/src/index.ts
@@ -1,6 +1,5 @@
 export * from './ui';
 export * from './helpers';
-export * from './components';
 
 export { useDomainSuggestionContainer } from './hooks/use-domain-suggestion-container';
 export { useTypedPlaceholder } from './hooks/use-typed-placeholder';
