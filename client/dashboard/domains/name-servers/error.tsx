e475b33a4666022600b4c73cce5162368e2376f5
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-08-28 13:06:24 +0200

    Create common domain specific error component

--- client/dashboard/domains/name-servers/error.tsx
+++ client/dashboard/domains/name-servers/error.tsx
@@ -1,17 +1,4 @@
-import { Card, CardBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import Notice from '../../components/notice';
-import { PageHeader } from '../../components/page-header';
-import PageLayout from '../../components/page-layout';
+import { createDomainErrorComponent } from '../domain-error';
 
-export default function NameServers( { error }: { error: Error } ) {
-	return (
-		<PageLayout size="small" header={ <PageHeader title={ __( 'Name Servers' ) } /> }>
-			<Card>
-				<CardBody>
-					<Notice variant="error">{ error.message }</Notice>
-				</CardBody>
-			</Card>
-		</PageLayout>
-	);
-}
+export default createDomainErrorComponent( __( 'Name Servers' ) );
