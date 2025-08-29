07acb313b3ef6037f41bc4a50d5fcce6d034a072
Veselin Nikolov <veselin@automattic.com>
2025-08-29 11:55:37 +0300

    Toggle simplification

--- client/my-sites/plans-features-main/components/intent-toggle/index.tsx
+++ client/my-sites/plans-features-main/components/intent-toggle/index.tsx
@@ -14,17 +14,13 @@ interface IntentToggleProps {
 
 export default function IntentToggle( { currentIntent, onIntentChange }: IntentToggleProps ) {
 	const translate = useTranslate();
-
-	const isWordPressHosting = currentIntent === 'plans-wordpress-hosting';
-	const currentValue = isWordPressHosting ? 'wordpress-hosting' : 'website-builder';
-
 	const handleToggle = ( value: string | number | undefined ) => {
 		if ( typeof value !== 'string' ) {
 			return;
 		}
 
 		const newIntent: PlansIntent =
-			value === 'wordpress-hosting' ? 'plans-wordpress-hosting' : 'plans-website-builder';
+			value === 'plans-wordpress-hosting' ? value : 'plans-website-builder';
 
 		recordTracksEvent( 'calypso_plans_intent_toggle_change', {
 			plans_intent: newIntent,
@@ -38,17 +34,17 @@ export default function IntentToggle( { currentIntent, onIntentChange }: IntentT
 			<ToggleGroupControl
 				label={ translate( 'Plan type selector' ) }
 				hideLabelFromVision
-				value={ currentValue }
+				value={ currentIntent || 'plans-website-builder' }
 				onChange={ handleToggle as ( value: string | number | undefined ) => void }
 				__next40pxDefaultSize
 				__nextHasNoMarginBottom
 			>
 				<ToggleGroupControlOption
-					value="website-builder"
+					value="plans-website-builder"
 					label={ translate( 'Website Builder' ) }
 				/>
 				<ToggleGroupControlOption
-					value="wordpress-hosting"
+					value="plans-wordpress-hosting"
 					label={ translate( 'WordPress Hosting' ) }
 				/>
 			</ToggleGroupControl>
