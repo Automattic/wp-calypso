20821fe586d90ce292bc0110c59da13708f1751e
Igor Giussani <gius80@gmail.com>
2025-08-29 10:53:43 +0200

    Improve message error

--- client/dashboard/data/domain-transfer.ts
+++ client/dashboard/data/domain-transfer.ts
@@ -1,3 +1,4 @@
+import { __ } from '@wordpress/i18n';
 import wpcom from 'calypso/lib/wp';
 
 export type IpsTag = {
@@ -41,13 +42,16 @@ export async function saveIpsTag( domain: string, ipsTag: string ): Promise< voi
 }
 
 export async function fetchIpsTagList(): Promise< IpsTag[] > {
+	/* translators: Please don't translate "IPS tag". */
+	const errorMessage = __( 'Failed to fetch IPS tag list. Please refresh the page and try again.' );
+	throw new Error( errorMessage );
 	try {
 		const response = await fetch( 'https://widgets.wp.com/domains/ips-tag-list.min.json' );
 		if ( ! response.ok ) {
-			throw new Error( `HTTP error! status: ${ response.status }` );
+			throw new Error( errorMessage );
 		}
 		return await response.json();
 	} catch ( error ) {
-		throw new Error( 'Failed to fetch IPS tag list' );
+		throw new Error( errorMessage );
 	}
 }
