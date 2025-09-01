d111e3ff7ca11512ac43dfb17fd49233c273d8f2
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-09-01 12:18:27 -0300

    Add API queries package

--- client/dashboard/.eslintrc.js
+++ client/dashboard/.eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
 							'!@automattic/components/src',
 							'@automattic/components/src/*',
 							'!@automattic/api-core',
+							'!@automattic/api-queries',
 							'!@automattic/components/src/circular-progress-bar',
 							'!@automattic/components/src/summary-button',
 							'!@automattic/components/src/breadcrumbs',
---
+++ packages/api-queries/README.md
@@ -0,0 +1,3 @@
+# API Queries
+
+Api queries
