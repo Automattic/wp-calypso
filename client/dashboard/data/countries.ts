c0cad7c0412ffb01ec6d76a7e70d0ce99000e11d
Chris McCluskey <chris.mccluskey@automattic.com>
2025-09-01 14:56:42 -0700

    Remove data directory leftover from conflict resolution

--- client/dashboard/data/countries.ts
+++
@@ -1,9 +0,0 @@
-import wpcom from 'calypso/lib/wp';
-import type { CountryListItem } from './types';
-
-export function fetchCountryList(): Promise< CountryListItem[] > {
-	return wpcom.req.get( {
-		path: '/me/transactions/supported-countries',
-		apiVersion: '1.1',
-	} );
-}
