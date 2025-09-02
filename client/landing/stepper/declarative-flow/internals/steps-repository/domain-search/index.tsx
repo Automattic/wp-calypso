6759cf679e4e1ab3738d302bafbec362db215d28
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-09-02 13:17:31 -0300

    StepContainerV2: Check for setup/domain manually to avoid Start flow clash

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-search/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-search/index.tsx
@@ -1,4 +1,4 @@
-import { Step, StepContainer } from '@automattic/onboarding';
+import { DOMAIN_FLOW, Step, StepContainer } from '@automattic/onboarding';
 import { WPCOMDomainSearch } from 'calypso/components/domains/wpcom-domain-search';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { shouldUseStepContainerV2 } from '../../../helpers/should-use-step-container-v2';
@@ -9,7 +9,7 @@ const DomainSearchStep: StepType = function DomainSearchStep( { flow } ) {
 		return <WPCOMDomainSearch />;
 	};
 
-	if ( shouldUseStepContainerV2( flow ) ) {
+	if ( shouldUseStepContainerV2( flow ) || flow === DOMAIN_FLOW ) {
 		return (
 			<Step.CenteredColumnLayout
 				topBar={ <Step.TopBar /> }
