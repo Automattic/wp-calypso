63c0856acadd933737be60c31018f24ceffaa8e2
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-09-02 17:11:03 +0300

    Add tags for Playwright Test specs

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -1008,7 +1008,7 @@ object PlaywrightTestPRMatrix : BuildType({
 
 				cd test/e2e
 				echo "Running Playwright tests for project: %playwrightProject%"
-				yarn test:pw:%playwrightProject%
+				yarn test:pw:%playwrightProject% --grep=@calypso-pr
 			"""
 			dockerImage = "%docker_image_e2e%"
 		}
