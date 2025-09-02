1e38d56c517198e11fc074a2d3cbc49979ded13c
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-09-02 19:48:13 +0300

    WebApp: Remove unused import for projectFeatures

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -12,7 +12,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.matrix
-import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.*
 
 object WebApp : Project({
 	id("WebApp")
