151bbab31c5215ad71bec1f2afc7e7b9895c5dea
Omar Alshaker <omar@omaralshaker.com>
2025-09-02 21:06:31 +0200

    Address feedback

--- packages/help-center/src/components/help-center-support-chat-message.tsx
+++ packages/help-center/src/components/help-center-support-chat-message.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-restricted-imports */
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { Gravatar, TimeSince } from '@automattic/components';
+import { Gravatar, TimeSince, WordPressLogo } from '@automattic/components';
 import SummaryButton from '@automattic/components/src/summary-button';
-import { HumanAvatar, WapuuAvatar } from '@automattic/odie-client/src/assets';
+import { WapuuAvatar } from '@automattic/odie-client/src/assets';
 import { useDispatch as useDataStoreDispatch } from '@wordpress/data';
 import { chevronRight, Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
@@ -72,7 +72,7 @@ export const HelpCenterSupportChatMessage = ( {
 		}
 
 		if ( role === 'business' ) {
-			return <HumanAvatar title={ __( 'User Avatar', __i18n_text_domain__ ) } />;
+			return <WordPressLogo size={ 20 } />;
 		}
 
 		return (
