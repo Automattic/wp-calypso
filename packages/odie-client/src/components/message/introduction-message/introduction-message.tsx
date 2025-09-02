58ebc86dfa691404cc9af2701c05ed24dc496890
Omar Alshaker <omar@omaralshaker.com>
2025-09-02 20:17:02 +0200

    Revert unnecessary changes

--- packages/odie-client/src/components/message/introduction-message/introduction-message.tsx
+++ packages/odie-client/src/components/message/introduction-message/introduction-message.tsx
@@ -3,16 +3,14 @@ import { MarkdownOrChildren } from '../mardown-or-children';
 import type { Message } from '../../../types';
 import './style.scss';
 
-export const IntroductionMessage = ( { content }: { content: Message[ 'content' ] } ) => {
-	return (
-		<div className="odie-introduction-message-content">
-			<div className="odie-introduction-big-sky-logo">
-				<BigSkyLogo.CentralLogo heartless size={ 50 } fill="#3858E9" />
-			</div>
-
-			<div className="odie-chatbox-introduction-message">
-				<MarkdownOrChildren messageContent={ content } />
-			</div>
+export const IntroductionMessage = ( { content }: { content: Message[ 'content' ] } ) => (
+	<div className="odie-introduction-message-content">
+		<div className="odie-introduction-big-sky-logo">
+			<BigSkyLogo.CentralLogo heartless size={ 50 } fill="#3858E9" />
 		</div>
-	);
-};
+
+		<div className="odie-chatbox-introduction-message">
+			<MarkdownOrChildren messageContent={ content } />
+		</div>
+	</div>
+);
