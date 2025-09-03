21c378ba375fed9533d4849175ec1ec8587d422c
Gergely Csécsey <gcsecsey@gmail.com>
2025-09-03 14:45:53 +0100

    fix hstack widths

--- client/dashboard/sites/deployments/dataviews/fields.tsx
+++ client/dashboard/sites/deployments/dataviews/fields.tsx
@@ -44,12 +44,12 @@ export function useDeploymentFields(): Field< CodeDeploymentData >[] {
 
 					const { commit_message, commit_sha, author } = run.metadata;
 					const [ installation, repo ] = item.repository_name.split( '/' );
-					const shortSha = commit_sha?.substring( commit_sha.length - 7 ) || '';
+					const shortSha = commit_sha?.substring( 0, 7 ) || '';
 
 					return (
 						<VStack spacing={ 1 }>
 							<Text title={ commit_message }>{ commit_message }</Text>
-							<HStack spacing={ 3 } alignment="left">
+							<HStack spacing={ 3 } alignment="left" style={ { width: 'auto' } }>
 								<a
 									href={ `https://github.com/${ installation }/${ repo }/commit/${ commit_sha }` }
 									target="_blank"
@@ -57,7 +57,7 @@ export function useDeploymentFields(): Field< CodeDeploymentData >[] {
 								>
 									<Text as="code">{ shortSha }</Text>
 								</a>
-								<HStack spacing={ 1 } alignment="left">
+								<HStack spacing={ 1 } alignment="left" style={ { width: 'auto' } }>
 									<svg width="12" height="12" viewBox="0 0 16 16" fill="#646970">
 										<path d="M11.75 2.5a.75.75 0 0 0-1.5 0V4H9A.75.75 0 0 0 9 5.5h1.25v1.25a.75.75 0 0 0 1.5 0V5.5H13a.75.75 0 0 0 0-1.5h-1.25V2.5z" />
 										<path d="M5.25 4a3.25 3.25 0 1 1 0 6.5 3.25 3.25 0 0 1 0-6.5zm0 1.5a1.75 1.75 0 1 0 0 3.5 1.75 1.75 0 0 0 0-3.5z" />
@@ -67,12 +67,12 @@ export function useDeploymentFields(): Field< CodeDeploymentData >[] {
 										{ item.branch_name }
 									</Text>
 								</HStack>
-								<HStack spacing={ 1.5 } alignment="left">
+								<HStack spacing={ 1.5 } alignment="left" style={ { width: 'auto' } }>
 									<img
 										src={ author.avatar_url }
 										alt={ author.name }
-										width={ 14 }
-										height={ 14 }
+										width={ 16 }
+										height={ 16 }
 										style={ { borderRadius: '50%' } }
 									/>
 									<Text variant="muted">{ author.name }</Text>
