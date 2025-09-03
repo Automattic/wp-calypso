933bc4d4145af9a86fbfda43f6dc8f3a542eef3d
Clemente <javacodia@gmail.com>
2025-09-03 23:20:38 +0100

    Update the Assgned To label to support the popover on cancellation

--- client/a8c-for-agencies/sections/referrals/referral-details/components/assigned-to.tsx
+++ client/a8c-for-agencies/sections/referrals/referral-details/components/assigned-to.tsx
@@ -1,7 +1,13 @@
-import { BadgeType, Button } from '@automattic/components';
+import { BadgeType, Button, Gridicon } from '@automattic/components';
+import { localizeUrl } from '@automattic/i18n-utils';
+import { useMobileBreakpoint } from '@automattic/viewport-react';
 import { useTranslate } from 'i18n-calypso';
+import { useState, useRef, useEffect } from 'react';
+import InfoModal from 'calypso/a8c-for-agencies/components/a4a-info-modal';
+import A4APopover from 'calypso/a8c-for-agencies/components/a4a-popover';
 import { A4A_SITES_LINK_NEEDS_SETUP } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import StatusBadge from 'calypso/a8c-for-agencies/components/step-section-item/status-badge';
+import { useSubscriptionDetails } from 'calypso/a8c-for-agencies/hooks/use-subscription-details';
 import { isWPCOMHostingProduct } from 'calypso/a8c-for-agencies/sections/marketplace/lib/hosting';
 import { addQueryArgs, urlToSlug } from 'calypso/lib/url';
 import { APIProductFamilyProduct } from 'calypso/state/partner-portal/types';
@@ -38,11 +44,11 @@ function getPurchaseStatus(
 
 const AssignedTo = ( { purchase, handleAssignToSite, data, isFetching }: Props ) => {
 	const translate = useTranslate();
+	const isMobile = useMobileBreakpoint();
 	const product = data?.find( ( product ) => product.product_id === purchase.product_id );
 	const isPressable = product?.slug.startsWith( 'pressable' );
 	const licenseKey = purchase.license?.license_key || '';
 	const isWPCOMLicense = isWPCOMHostingProduct( licenseKey );
-
 	const redirectUrl =
 		purchase.license &&
 		( isWPCOMLicense
@@ -51,16 +57,17 @@ const AssignedTo = ( { purchase, handleAssignToSite, data, isFetching }: Props )
 
 	const showAssignButton = purchase.status === 'active' && redirectUrl;
 	const [ statusType, statusText ] = getPurchaseStatus( purchase, translate );
+	const { expiryDate, isFetchingProductInfo } = useSubscriptionDetails( purchase );
+	const [ showPopover, setShowPopover ] = useState( false );
 
-	let tooltip = '';
+	const wrapperRef = useRef< HTMLDivElement >( null );
+	const popoverContentRef = useRef< HTMLDivElement >( null );
 
-	const isAwaitingPayment = purchase.status === 'pending';
-
-	if ( isAwaitingPayment ) {
-		tooltip = isWPCOMLicense
-			? translate( 'When your client pays, you can initiate this site.' )
-			: translate( 'When your client pays, you can assign this product to a site.' );
-	}
+	useEffect( () => {
+		if ( showPopover && popoverContentRef.current ) {
+			popoverContentRef.current.focus();
+		}
+	}, [ showPopover ] );
 
 	if ( purchase.site_assigned ) {
 		return isPressable ? (
@@ -81,8 +88,64 @@ const AssignedTo = ( { purchase, handleAssignToSite, data, isFetching }: Props )
 		);
 	}
 
+	let tooltip = '';
+
+	let cancellationInfo = null;
+
+	// If the purchase has a subscription we can get the information from the object.
+	if ( purchase.subscription && ! purchase.subscription.is_refundable ) {
+		cancellationInfo = (
+			<>
+				<p>
+					{ translate(
+						'Your client canceled this product, but it will remain active until {{b}}%(expiryDate)s{{/b}}. After that, it will not renew.',
+						{
+							args: {
+								expiryDate: isFetchingProductInfo ? '...' : expiryDate || 'N/A',
+							},
+							components: {
+								b: <b />,
+							},
+							comment:
+								'%(cancellationDate)s is the date when the client canceled the product. %(expiryDate)s is the date when the product will expire.',
+						}
+					) }
+				</p>
+				<p>
+					{ translate( '{{link}}Learn more about cancelations ↗{{/link}}', {
+						components: {
+							link: (
+								<a
+									href={ localizeUrl(
+										'https://wordpress.com/support/manage-purchases/cancel-a-purchase/'
+									) }
+									target="_blank"
+									rel="noreferrer noopener"
+								>
+									{  }
+								</a>
+							),
+						},
+					} ) }
+				</p>
+			</>
+		);
+	} else if ( purchase.subscription && purchase.subscription.is_refundable ) {
+		cancellationInfo = (
+			<p>{ translate( 'Your client canceled this product, and was refunded.' ) }</p>
+		);
+	}
+
+	const isAwaitingPayment = purchase.status === 'pending';
+
+	if ( isAwaitingPayment ) {
+		tooltip = isWPCOMLicense
+			? translate( 'When your client pays, you can initiate this site.' )
+			: translate( 'When your client pays, you can assign this product to a site.' );
+	}
+
 	return (
-		<>
+		<div className="badge-assigned-to">
 			<StatusBadge
 				statusProps={ {
 					children: statusText,
@@ -90,6 +153,41 @@ const AssignedTo = ( { purchase, handleAssignToSite, data, isFetching }: Props )
 					tooltip,
 				} }
 			/>
+			{ purchase.status === 'canceled' && cancellationInfo && (
+				<span
+					className="status-card__info-icon"
+					onClick={ () => setShowPopover( ! showPopover ) }
+					role="button"
+					tabIndex={ 0 }
+					onKeyDown={ ( event ) => {
+						if ( event.key === 'Enter' ) {
+							setShowPopover( ! showPopover );
+						}
+					} }
+					ref={ wrapperRef }
+				>
+					<Gridicon icon="info-outline" size={ 18 } />
+				</span>
+			) }
+			{ purchase.status === 'canceled' &&
+				cancellationInfo &&
+				showPopover &&
+				( isMobile ? (
+					<InfoModal title="" onClose={ () => setShowPopover( false ) }>
+						{ cancellationInfo }
+					</InfoModal>
+				) : (
+					<A4APopover
+						title=""
+						wrapperRef={ wrapperRef }
+						offset={ 8 }
+						onFocusOutside={ () => setShowPopover( false ) }
+					>
+						<div tabIndex={ -1 } ref={ popoverContentRef }>
+							{ cancellationInfo }
+						</div>
+					</A4APopover>
+				) ) }
 			{ showAssignButton && (
 				<Button
 					disabled={ isFetching }
@@ -100,7 +198,7 @@ const AssignedTo = ( { purchase, handleAssignToSite, data, isFetching }: Props )
 					{ isWPCOMLicense ? translate( 'Create site' ) : translate( 'Assign to site' ) }
 				</Button>
 			) }
-		</>
+		</div>
 	);
 };
 
