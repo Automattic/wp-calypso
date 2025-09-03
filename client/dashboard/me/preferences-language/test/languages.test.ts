2de228f97dbb560a547e21cdb27e63e1fcd27679
Andrea Grassi <andrea.grassi@automattic.com>
2025-09-03 17:21:51 +0200

    feat: import UserSettingsPreferences from api-core in languages test file

--- client/dashboard/me/preferences-language/test/languages.test.ts
+++ client/dashboard/me/preferences-language/test/languages.test.ts
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-
+import { UserSettingsPreferences } from '@automattic/api-core';
 import languages from '@automattic/languages';
 import {
 	languagesAsOptions,
