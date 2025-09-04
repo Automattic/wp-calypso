78a4ecc112ecff52ab0e7d9e75f86fd399861887
Christos <chriskmnds@gmail.com>
2025-09-04 15:16:05 +0300

    add loader prefetching for sites and plugins

--- client/dashboard/app/router/plugins.tsx
+++ client/dashboard/app/router/plugins.tsx
@@ -1,3 +1,4 @@
+import { sitesQuery, pluginsQuery, queryClient } from '@automattic/api-queries';
 import { createRoute, createLazyRoute, redirect } from '@tanstack/react-router';
 import { rootRoute } from './root';
 import type { AnyRoute } from '@tanstack/react-router';
@@ -57,6 +58,12 @@ export const pluginsScheduledUpdatesRoute = createRoute( {
 export const pluginsScheduledUpdatesNewRoute = createRoute( {
 	getParentRoute: () => pluginsRoute,
 	path: 'scheduled-updates/new',
+	loader: async () => {
+		await Promise.all( [
+			queryClient.ensureQueryData( sitesQuery() ),
+			queryClient.ensureQueryData( pluginsQuery() ),
+		] );
+	},
 } ).lazy( () =>
 	import( '../../plugins/scheduled-updates/new' ).then( ( d ) =>
 		createLazyRoute( 'plugins-scheduled-updates-new' )( {
