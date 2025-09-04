130b98cd59c5fbb37d288fd3a5a20014caae0f3e
Omar Alshaker <omar@omaralshaker.com>
2025-09-04 16:52:36 +0200

    Refactor: remove persisted support interaction in favor of already persisted MemoryRouter URLs (#105542)

--- packages/data-stores/src/help-center/reducer.ts
+++ packages/data-stores/src/help-center/reducer.ts
@@ -2,7 +2,6 @@ import { combineReducers } from '@wordpress/data';
 import { SiteDetails } from '../site';
 import type { HelpCenterAction } from './actions';
 import type { HelpCenterOptions } from './types';
-import type { SupportInteraction } from '@automattic/odie-client/src/types';
 import type { Location } from 'history';
 import type { Reducer } from 'redux';
 
@@ -51,16 +50,6 @@ const showMessagingWidget: Reducer< boolean | undefined, HelpCenterAction > = ( 
 	switch ( action.type ) {
 		case 'HELP_CENTER_SET_SHOW_MESSAGING_WIDGET':
 			return action.show;
-	}
-	return state;
-};
-
-const currentSupportInteraction: Reducer< SupportInteraction | undefined, HelpCenterAction > = (
-	state,
-	action
-) => {
-	if ( action.type === 'HELP_CENTER_SET_CURRENT_SUPPORT_INTERACTION' ) {
-		return action.supportInteraction;
 	}
 	return state;
 };
@@ -198,7 +187,6 @@ const helpCenterOptions: Reducer< HelpCenterOptions, HelpCenterAction > = (
 };
 
 const reducer = combineReducers( {
-	currentSupportInteraction,
 	showHelpCenter,
 	showMessagingLauncher,
 	showMessagingWidget,
