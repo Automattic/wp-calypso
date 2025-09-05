3c551f2606ad37a3bba0540b364879ce2c555ee4
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2025-09-05 16:20:11 +0800

    Add exports from api-queries

--- packages/api-queries/src/index.ts
+++ packages/api-queries/src/index.ts
@@ -19,6 +19,7 @@ export * from './me-dpa';
 export * from './me-payment-methods';
 export * from './me-preferences';
 export * from './me-profile';
+export * from './me-settings';
 export * from './me-purchases';
 export * from './p2';
 export * from './performance';
