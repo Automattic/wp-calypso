c98d02cb6b374f80cad4c3463bef610a7d7435ff
Tiago Noronha <hello@tiagonoronha.com>
2025-09-05 11:33:35 +0100

    Factor in previous quarter if payout dates not equal

--- client/a8c-for-agencies/sections/woopayments/lib/site-data.ts
+++ client/a8c-for-agencies/sections/woopayments/lib/site-data.ts
@@ -1,3 +1,4 @@
+import { areNextAndCurrentPayoutDatesEqual } from '../../referrals/lib/get-next-payout-date';
 import { WooPaymentsData } from '../types';
 
 interface WooPaymentsSiteData {
@@ -14,9 +15,19 @@ export const getSiteData = (
 	const sitePayout = siteData?.payout ?? 0;
 	const siteTransactions = siteData?.transactions ?? 0;
 
-	// Get estimated payout directly from current quarter site data
-	const estimatedPayout =
+	// Get estimated payout from current quarter
+	const currentQuarterEstimate =
 		woopaymentsData?.data?.estimated?.current_quarter?.sites?.[ siteId ]?.payout ?? 0;
+
+	// Get estimated payout from previous quarter
+	const previousQuarterEstimate =
+		woopaymentsData?.data?.estimated?.previous_quarter?.sites?.[ siteId ]?.payout ?? 0;
+
+	// If next and current payout dates are not equal, add previous quarter estimate
+	const now = new Date();
+	const estimatedPayout = areNextAndCurrentPayoutDatesEqual( now )
+		? currentQuarterEstimate
+		: currentQuarterEstimate + previousQuarterEstimate;
 
 	return {
 		transactions: siteTransactions,
