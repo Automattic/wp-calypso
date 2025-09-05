f77c6771aadb6b030db20d5e93a8ac962f87af4f
sergeymitr <sergeymitr@gmail.com>
2025-09-05 10:22:58 -0400

    Hosting Dashboard: Server performance data fetching.

---
+++ client/dashboard/sites/monitoring-performance-card/monitoring-performance-card.tsx
@@ -0,0 +1,103 @@
+import { useMemo } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { PeriodData, useSiteMetricsQuery } from '../../../sites/monitoring/hooks/use-metrics-query';
+import MonitoringCard from '../monitoring-card';
+import type { Site } from '@automattic/api-core';
+
+type TimeRange = {
+	start: number;
+	end: number;
+};
+
+function convertTimeRangeToUnix( timeRange: number ): TimeRange {
+	const start = Math.floor( new Date().getTime() / 1000 ) - timeRange * 3600;
+	const end = Math.floor( new Date().getTime() / 1000 );
+
+	return { start, end };
+}
+
+export function useSiteMetricsData( siteId: number, timeRange: number ) {
+	// Memoize timestamps to prevent graph reloading on every render. Only refresh the data on time range change.
+	const { start, end } = useMemo( () => convertTimeRangeToUnix( timeRange ), [ timeRange ] );
+
+	const { data: requestsData, isLoading } = useSiteMetricsQuery( siteId, {
+		start,
+		end,
+		metric: 'requests_persec',
+	} );
+	const { data: responseTimeData } = useSiteMetricsQuery( siteId, {
+		start,
+		end,
+		metric: 'response_time_average',
+	} );
+
+	// Function to get the dimension value for a specific key and period.
+	const getDimensionValue = ( period: PeriodData ) => {
+		if ( typeof period?.dimension === 'object' ) {
+			return Object.values( period.dimension ).length > 0
+				? Object.values( period.dimension )[ 0 ]
+				: 0;
+		}
+
+		return null;
+	};
+
+	// Process the data in the format accepted by uPlot
+	const formattedData =
+		requestsData?.data?.periods?.reduce(
+			( acc, period, index ) => {
+				// Check if the timestamp is already in the arrays, if not, push it
+				if ( acc[ 0 ][ acc[ 0 ].length - 1 ] !== period.timestamp ) {
+					// Temporarily convert timestamp to ISO string for better readability in the example.
+					// We will revert it back to epoch in the actual implementation:
+					// acc[ 0 ].push( period.timestamp );
+					acc[ 0 ].push( new Date( period.timestamp * 1000 ).toISOString() );
+
+					const requestsPerSecondValue = getDimensionValue( period );
+					if ( requestsPerSecondValue !== null ) {
+						const requestsPerMinuteValue = requestsPerSecondValue * 60; // Convert to requests per minute
+						acc[ 1 ].push( requestsPerMinuteValue ); // Push RPM value into the array
+					}
+					// Add response time data as a green line
+					if ( responseTimeData?.data?.periods && responseTimeData.data.periods[ index ] ) {
+						const responseTimeAverageValue = getDimensionValue(
+							responseTimeData.data.periods[ index ]
+						);
+						if ( responseTimeAverageValue !== null ) {
+							acc[ 2 ].push( responseTimeAverageValue * 1000 ); // Convert to response time average in milliseconds
+						}
+					}
+				}
+
+				return acc;
+			},
+			[ [], [], [] ] as Array< Array< number | null > > // Adjust the initial value with placeholders for both lines
+		) || ( [ [], [], [] ] as Array< Array< number | null > > ); // Return default value when data is not available yet
+
+	return {
+		formattedData,
+		isLoading,
+	};
+}
+
+export default function MonitoringPerformanceCard( {
+	site,
+	timeRange,
+}: {
+	site: Site;
+	timeRange: number;
+} ) {
+	const { formattedData, isLoading } = useSiteMetricsData( site.ID, timeRange );
+
+	return (
+		<MonitoringCard
+			title={ __( 'Server performance' ) }
+			description={ __( 'Requests per minute and average server response time.' ) }
+			onDownloadClick={ () => {} }
+			onAnchorClick={ () => {} }
+			isLoading={ isLoading }
+		>
+			<pre>{ JSON.stringify( formattedData, null, 2 ) }</pre>
+		</MonitoringCard>
+	);
+}
