33d948ab6a93bf97ac549c7fd4b7e3e5822cae85
Tiago Noronha <hello@tiagonoronha.com>
2025-09-05 15:09:05 +0100

    Remove unnecessary div wrappers; add currency utility function

--- client/a8c-for-agencies/sections/woopayments/primary/woopayments-overview/index.tsx
+++ client/a8c-for-agencies/sections/woopayments/primary/woopayments-overview/index.tsx
@@ -1,4 +1,5 @@
 import page from '@automattic/calypso-router';
+import { formatCurrency } from '@automattic/number-formatters';
 import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useMemo, useCallback } from 'react';
@@ -10,6 +11,7 @@ import PageSectionColumns from 'calypso/a8c-for-agencies/components/page-section
 import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
 import { A4A_WOOPAYMENTS_DASHBOARD_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import SimpleList from 'calypso/a8c-for-agencies/components/simple-list';
+import { formatCurrencyCompact } from 'calypso/a8c-for-agencies/lib/currency';
 import { extractStrings } from 'calypso/a8c-for-agencies/lib/translation';
 import cartImage from 'calypso/assets/images/a8c-for-agencies/woopayments/cart.png';
 import ccImage from 'calypso/assets/images/a8c-for-agencies/woopayments/cc-image.png';
@@ -293,22 +295,20 @@ const WooPaymentsOverview = () => {
 									) }
 								</div>
 							</div>
-							<div className="woopayments-overview__buttons-container">
-								<Button
-									__next40pxDefaultSize
-									href="https://automattic.com/for-agencies/program-incentives"
-									target="_blank"
-									rel="noopener noreferrer"
-									className="woopayments-overview__button"
-									onClick={ () => {
-										dispatch(
-											recordTracksEvent( 'calypso_a4a_woopayments_view_details_button_click' )
-										);
-									} }
-								>
-									{ translate( 'View details and start earning ↗' ) }
-								</Button>
-							</div>
+							<Button
+								__next40pxDefaultSize
+								href="https://automattic.com/for-agencies/program-incentives"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="woopayments-overview__button"
+								onClick={ () => {
+									dispatch(
+										recordTracksEvent( 'calypso_a4a_woopayments_view_details_button_click' )
+									);
+								} }
+							>
+								{ translate( 'View details and start earning ↗' ) }
+							</Button>
 						</div>
 					</PageSectionColumns.Column>
 					<PageSectionColumns.Column alignCenter>
@@ -321,108 +321,143 @@ const WooPaymentsOverview = () => {
 					heading={ translate( 'How revenue share works' ) }
 				>
 					<PageSectionColumns.Column>
-						<>
-							<div className="woopayments-overview__description woopayments-overview__revenue-share-container">
-								<div className="woopayments-overview__revenue-share-icon">
-									<img src={ iconStorePlus } alt="Revenue Share" />
-								</div>
-								<div className="woopayments-overview__revenue-share-subheading">
-									{ translate( 'For new WooPayments clients you help launch' ) }
-								</div>
-								<div>
-									{ preventWidows(
-										translate(
-											'Earn a generous 5 basis points (bps) revenue share on all Total Payments Volume (TPV) processed on client sites you onboard after joining Automattic for Agencies.'
-										)
-									) }
-								</div>
+						<div className="woopayments-overview__description woopayments-overview__revenue-share-container">
+							<div className="woopayments-overview__revenue-share-icon">
+								<img src={ iconStorePlus } alt="Revenue Share" />
 							</div>
-						</>
+							<div className="woopayments-overview__revenue-share-subheading">
+								{ translate( 'For new WooPayments clients you help launch' ) }
+							</div>
+							<div>
+								{ preventWidows(
+									translate(
+										'Earn a generous 5 basis points (bps) revenue share on all Total Payments Volume (TPV) processed on client sites you onboard after joining Automattic for Agencies.'
+									)
+								) }
+							</div>
+						</div>
 					</PageSectionColumns.Column>
 					<PageSectionColumns.Column>
-						<>
-							<div className="woopayments-overview__description woopayments-overview__revenue-share-container">
-								<div className="woopayments-overview__revenue-share-icon">
-									<img src={ iconStore } alt="Revenue Share" />
-								</div>
-								<div className="woopayments-overview__revenue-share-subheading">
-									{ translate( 'For existing WooPayments clients you already manage' ) }
-								</div>
-								<div>
-									{ translate(
-										'Bring your current WooPayments-enabled client stores into your Automattic for Agencies dashboard to unlock ongoing revenue share:'
-									) }
-								</div>
-								<div>
-									<SimpleList
-										className="woopayments-overview__list"
-										items={ [
-											translate( '$1M+ in annual TPV: 3 bps revenue share' ),
-											translate( '$500k-$1M in annual TPV: 2 bps revenue share' ),
-											translate(
-												'Connect eligible stores to A4A within 30 days of joining to participate in these tiers.'
-											),
-										] }
-									/>
-								</div>
+						<div className="woopayments-overview__description woopayments-overview__revenue-share-container">
+							<div className="woopayments-overview__revenue-share-icon">
+								<img src={ iconStore } alt="Revenue Share" />
 							</div>
-						</>
+							<div className="woopayments-overview__revenue-share-subheading">
+								{ translate( 'For existing WooPayments clients you already manage' ) }
+							</div>
+							<div>
+								{ translate(
+									'Bring your current WooPayments-enabled client stores into your Automattic for Agencies dashboard to unlock ongoing revenue share:'
+								) }
+							</div>
+							<div>
+								<SimpleList
+									className="woopayments-overview__list"
+									items={ [
+										translate( '%(amount)s+ in annual TPV: 3 bps revenue share', {
+											args: {
+												amount: formatCurrencyCompact( 1000000, 'USD' ),
+											},
+										} ),
+										translate( '%(amount)s-%(amount2)s in annual TPV: 2 bps revenue share', {
+											args: {
+												amount: formatCurrencyCompact( 500000, 'USD' ),
+												amount2: formatCurrencyCompact( 1000000, 'USD' ),
+											},
+										} ),
+										translate(
+											'Connect eligible stores to A4A within 30 days of joining to participate in these tiers.'
+										),
+									] }
+								/>
+							</div>
+						</div>
 					</PageSectionColumns.Column>
 					<PageSectionColumns.Column heading={ translate( 'Commissions at a glance' ) } fullWidth>
-						<>
-							<div className="woopayments-overview__description">
-								<table className="woopayments-overview__commissions-table">
-									<thead>
-										<tr>
-											<th>{ translate( 'Site Type' ) }</th>
-											<th>{ translate( 'TPV Processed' ) }</th>
-											<th>{ translate( 'Revenue Share (bps)' ) }</th>
-											<th>{ translate( 'Example Earnings*' ) }</th>
-											<th>{ translate( 'Requirements' ) }</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>{ translate( 'New Clients' ) }</td>
-											<td>{ translate( 'No minimum' ) }</td>
-											<td>{ translate( '5 bps' ) }</td>
-											<td>{ translate( '$500/year on $1M annual TPV' ) }</td>
-											<td>{ translate( 'Standard program terms' ) }</td>
-										</tr>
-										<tr>
-											<td>{ translate( 'Pre-existing Clients - Tier 1' ) }</td>
-											<td>{ translate( '$1M+/year' ) }</td>
-											<td>{ translate( '3 bps' ) }</td>
-											<td>{ translate( '$300/year on $1M annual TPV' ) }</td>
-											<td>{ translate( 'Register within 30 days of joining' ) }</td>
-										</tr>
-										<tr>
-											<td>{ translate( 'Pre-existing Clients - Tier 2' ) }</td>
-											<td>{ translate( '$500k-$999,999 TPV/year' ) }</td>
-											<td>{ translate( '2 bps' ) }</td>
-											<td>{ translate( '$100/year on $500k annual TPV' ) }</td>
-											<td>{ translate( 'Register within 30 days of joining' ) }</td>
-										</tr>
-									</tbody>
-								</table>
-								<div className="woopayments-overview__legend">
-									{ translate(
-										"*Example earnings are calculated annually based on the TPV and revenue share rate shown and are subject change based on your client's TPV."
-									) }
-								</div>
-								<div className="woopayments-overview__legend">
-									{ translate(
-										'{{b}}Legend:{{/b}} {{br/}}bps = basis points (1 bps = 0.01%) {{br/}}TPV = Total Payments Volume',
-										{
-											components: {
-												b: <b />,
-												br: <br />,
-											},
-										}
-									) }
-								</div>
+						<div className="woopayments-overview__description">
+							<table className="woopayments-overview__commissions-table">
+								<thead>
+									<tr>
+										<th>{ translate( 'Site Type' ) }</th>
+										<th>{ translate( 'TPV Processed' ) }</th>
+										<th>{ translate( 'Revenue Share (bps)' ) }</th>
+										<th>{ translate( 'Example Earnings*' ) }</th>
+										<th>{ translate( 'Requirements' ) }</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>{ translate( 'New Clients' ) }</td>
+										<td>{ translate( 'No minimum' ) }</td>
+										<td>{ translate( '5 bps' ) }</td>
+										<td>
+											{ translate( '%(amount)s/year on %(amount2)s annual TPV', {
+												args: {
+													amount: formatCurrency( 500, 'USD' ),
+													amount2: formatCurrencyCompact( 1000000, 'USD' ),
+												},
+											} ) }
+										</td>
+										<td>{ translate( 'Standard program terms' ) }</td>
+									</tr>
+									<tr>
+										<td>{ translate( 'Pre-existing Clients - Tier 1' ) }</td>
+										<td>
+											{ translate( '%(amount)s+/year', {
+												args: { amount: formatCurrencyCompact( 1000000, 'USD' ) },
+											} ) }
+										</td>
+										<td>{ translate( '3 bps' ) }</td>
+										<td>
+											{ translate( '%(amount)s/year on %(amount2)s annual TPV', {
+												args: {
+													amount: formatCurrency( 300, 'USD' ),
+													amount2: formatCurrencyCompact( 1000000, 'USD' ),
+												},
+											} ) }
+										</td>
+										<td>{ translate( 'Register within 30 days of joining' ) }</td>
+									</tr>
+									<tr>
+										<td>{ translate( 'Pre-existing Clients - Tier 2' ) }</td>
+										<td>
+											{ translate( '%(amount)s-%(amount2)s TPV/year', {
+												args: {
+													amount: formatCurrencyCompact( 500000, 'USD' ),
+													amount2: formatCurrencyCompact( 999999, 'USD' ),
+												},
+											} ) }
+										</td>
+										<td>{ translate( '2 bps' ) }</td>
+										<td>
+											{ translate( '%(amount)s/year on %(amount2)s annual TPV', {
+												args: {
+													amount: formatCurrency( 100, 'USD' ),
+													amount2: formatCurrencyCompact( 500000, 'USD' ),
+												},
+											} ) }
+										</td>
+										<td>{ translate( 'Register within 30 days of joining' ) }</td>
+									</tr>
+								</tbody>
+							</table>
+							<div className="woopayments-overview__legend">
+								{ translate(
+									"*Example earnings are calculated annually based on the TPV and revenue share rate shown and are subject change based on your client's TPV."
+								) }
 							</div>
-						</>
+							<div className="woopayments-overview__legend">
+								{ translate(
+									'{{b}}Legend:{{/b}} {{br/}}bps = basis points (1 bps = 0.01%) {{br/}}TPV = Total Payments Volume',
+									{
+										components: {
+											b: <b />,
+											br: <br />,
+										},
+									}
+								) }
+							</div>
+						</div>
 					</PageSectionColumns.Column>
 				</PageSectionColumns>
 
@@ -482,62 +517,60 @@ const WooPaymentsOverview = () => {
 					<PageSectionColumns.Column
 						heading={ translate( 'Still undecided if WooPayments is right for your clients?' ) }
 					>
-						<>
-							<div className="woopayments-overview__description">
-								{ translate(
-									"Explore all of WooPayments' benefits, browse the technical documentation, and {{a}}try the demo{{/a}} ↗ to see it in action.",
-									{
-										components: {
-											a: (
-												<a
-													className="woopayments-overview__demo-link"
-													href="https://woocommerce.com/products/woopayments"
-													target="_blank"
-													rel="noopener noreferrer"
-													onClick={ () => {
-														dispatch(
-															recordTracksEvent( 'calypso_a4a_woopayments_try_demo_button_click' )
-														);
-													} }
-												/>
-											),
-										},
-									}
-								) }
-							</div>
-							<div className="woopayments-overview__buttons-container">
-								<Button
-									__next40pxDefaultSize
-									variant="primary"
-									href={ CONTACT_URL_HASH_FRAGMENT_WITH_PRODUCT }
-									onClick={ () => {
-										dispatch(
-											recordTracksEvent(
-												'calypso_a4a_woopayments_contact_us_to_learn_more_button_click'
-											)
-										);
-									} }
-								>
-									{ translate( 'Contact us to learn more' ) }
-								</Button>
-								<Button
-									__next40pxDefaultSize
-									variant="secondary"
-									href="https://woocommerce.com/products/woopayments"
-									target="_blank"
-									rel="noopener noreferrer"
-									onClick={ () => {
-										dispatch(
-											recordTracksEvent(
-												'calypso_a4a_woopayments_explore_on_woocommerce_button_click'
-											)
-										);
-									} }
-								>
-									{ translate( 'Explore on WooCommerce.com ↗' ) }
-								</Button>
-							</div>
-						</>
+						<div className="woopayments-overview__description">
+							{ translate(
+								"Explore all of WooPayments' benefits, browse the technical documentation, and {{a}}try the demo{{/a}} ↗ to see it in action.",
+								{
+									components: {
+										a: (
+											<a
+												className="woopayments-overview__demo-link"
+												href="https://woocommerce.com/products/woopayments"
+												target="_blank"
+												rel="noopener noreferrer"
+												onClick={ () => {
+													dispatch(
+														recordTracksEvent( 'calypso_a4a_woopayments_try_demo_button_click' )
+													);
+												} }
+											/>
+										),
+									},
+								}
+							) }
+						</div>
+						<div className="woopayments-overview__buttons-container">
+							<Button
+								__next40pxDefaultSize
+								variant="primary"
+								href={ CONTACT_URL_HASH_FRAGMENT_WITH_PRODUCT }
+								onClick={ () => {
+									dispatch(
+										recordTracksEvent(
+											'calypso_a4a_woopayments_contact_us_to_learn_more_button_click'
+										)
+									);
+								} }
+							>
+								{ translate( 'Contact us to learn more' ) }
+							</Button>
+							<Button
+								__next40pxDefaultSize
+								variant="secondary"
+								href="https://woocommerce.com/products/woopayments"
+								target="_blank"
+								rel="noopener noreferrer"
+								onClick={ () => {
+									dispatch(
+										recordTracksEvent(
+											'calypso_a4a_woopayments_explore_on_woocommerce_button_click'
+										)
+									);
+								} }
+							>
+								{ translate( 'Explore on WooCommerce.com ↗' ) }
+							</Button>
+						</div>
 					</PageSectionColumns.Column>
 					<PageSectionColumns.Column alignCenter>
 						<img src={ demoImage } alt="WooPayments" />
