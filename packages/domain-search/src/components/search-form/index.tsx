3df179332bc6ff00f8079e97a561904dab094f65
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-09-08 10:26:15 -0300

    Domain Search Rewrite: Add search hint

--- packages/domain-search/src/components/search-form/index.tsx
+++ packages/domain-search/src/components/search-form/index.tsx
@@ -1,8 +1,17 @@
-import { __experimentalHStack as HStack } from '@wordpress/components';
+import {
+	Button,
+	__experimentalHStack as HStack,
+	__experimentalVStack as VStack,
+	__experimentalText as Text,
+} from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 import { useState } from 'react';
 import { useTypedPlaceholder } from '../../hooks/use-typed-placeholder';
 import { useDomainSearch } from '../../page/context';
 import { DomainSearchControls } from '../../ui';
+
+import './style.scss';
 
 const PLACEHOLDER_PHRASES = [
 	'dailywine.blog',
@@ -16,23 +25,55 @@ export const SearchForm = () => {
 	const { setQuery } = useDomainSearch();
 	const [ localQuery, setLocalQuery ] = useState( '' );
 	const { placeholder } = useTypedPlaceholder( PLACEHOLDER_PHRASES, false );
+	const [ showSearchHint, setShowSearchHint ] = useState( false );
 
 	const handleSubmit = ( event: React.FormEvent< HTMLFormElement > ) => {
 		event.preventDefault();
 		setQuery( localQuery );
+
+		if ( localQuery === '' ) {
+			setShowSearchHint( true );
+		}
 	};
 
 	return (
 		<form onSubmit={ handleSubmit }>
-			<HStack alignment="flex-start" spacing={ 4 }>
-				<DomainSearchControls.Input
-					value={ localQuery }
-					onChange={ ( value ) => setLocalQuery( value.trim() ) }
-					onReset={ () => setLocalQuery( '' ) }
-					placeholder={ placeholder }
-				/>
-				<DomainSearchControls.Submit />
-			</HStack>
+			<VStack spacing={ 2 }>
+				<HStack alignment="flex-start" spacing={ 4 }>
+					<DomainSearchControls.Input
+						value={ localQuery }
+						onChange={ ( value ) => setLocalQuery( value.trim() ) }
+						onReset={ () => setLocalQuery( '' ) }
+						placeholder={ placeholder }
+					/>
+					<DomainSearchControls.Submit />
+				</HStack>
+				{ showSearchHint && (
+					<Text variant="muted">
+						{ createInterpolateElement(
+							__(
+								'Try searching for a word like <studioLink>studio</studioLink> or <coffeeLink>coffee</coffeeLink> to get started.'
+							),
+							{
+								studioLink: (
+									<Button
+										variant="link"
+										onClick={ () => setQuery( 'studio' ) }
+										className="domain-search__search-form-hint"
+									/>
+								),
+								coffeeLink: (
+									<Button
+										variant="link"
+										onClick={ () => setQuery( 'coffee' ) }
+										className="domain-search__search-form-hint"
+									/>
+								),
+							}
+						) }
+					</Text>
+				) }
+			</VStack>
 		</form>
 	);
 };
