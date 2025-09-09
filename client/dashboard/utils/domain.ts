3fa95dcf7ec7b3834efc3f999a2d90890f176c6b
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-09-09 17:24:02 +0200

    Add missing subtype check

--- client/dashboard/utils/domain.ts
+++ client/dashboard/utils/domain.ts
@@ -27,7 +27,7 @@ export function getDomainRenewalUrl( domain: DomainSummary, purchase: Purchase )
 }
 
 export function isRegisteredDomain( domain: DomainSummary ) {
-	return ! domain.wpcom_domain && domain.has_registration;
+	return domain.subtype.id === DomainSubtype.DOMAIN_REGISTRATION;
 }
 
 export function isRecentlyRegistered( registrationDate: string, numberOfMinutes = 30 ) {
@@ -98,7 +98,9 @@ const shouldUpgradeToMakeDomainPrimary = ( {
 	user: User;
 } ) => {
 	return (
-		domain.subtype.id === DomainSubtype.DOMAIN_CONNECTION &&
+		[ DomainSubtype.DOMAIN_CONNECTION, DomainSubtype.DOMAIN_REGISTRATION ].includes(
+			domain.subtype.id
+		) &&
 		! domain.current_user_can_create_site_from_domain_only &&
 		! domain.primary_domain &&
 		! domain.wpcom_domain &&
