beb7664926979df5ea81f9799acfde722ccbefc8
Rafael Agostini <rafael.agostini@automattic.com>
2025-09-08 21:32:35 -0500

    Replace translate() calls with __() from @wordpress/i18n

--- client/my-sites/backup/backup-contents-page/file-browser/notices.ts
+++ client/my-sites/backup/backup-contents-page/file-browser/notices.ts
@@ -1,4 +1,4 @@
-import { translate } from 'i18n-calypso';
+import { __ } from '@wordpress/i18n';
 import { errorNotice } from 'calypso/state/notices/actions';
 
 const NOTICE_PERSISTENT = true;
@@ -6,7 +6,10 @@ const NOTICE_DURATION = 5000;
 
 export const onRetrievingFileInfoError = () => {
 	return errorNotice(
-		translate( 'There was an error retrieving your file information. Please, try again.' ),
+		__(
+			'There was an error retrieving your file information. Please, try again.',
+			'jetpack-backup'
+		),
 		{
 			duration: NOTICE_DURATION,
 			isPersistent: NOTICE_PERSISTENT,
@@ -16,7 +19,7 @@ export const onRetrievingFileInfoError = () => {
 
 export const onProcessingDownloadError = () => {
 	return errorNotice(
-		translate( 'There was an error processing your download. Please, try again.' ),
+		__( 'There was an error processing your download. Please, try again.', 'jetpack-backup' ),
 		{
 			duration: NOTICE_DURATION,
 			isPersistent: NOTICE_PERSISTENT,
@@ -26,7 +29,7 @@ export const onProcessingDownloadError = () => {
 
 export const onPreparingDownloadError = () => {
 	return errorNotice(
-		translate( 'There was an error preparing your download. Please, try again.' ),
+		__( 'There was an error preparing your download. Please, try again.', 'jetpack-backup' ),
 		{
 			duration: NOTICE_DURATION,
 			isPersistent: NOTICE_PERSISTENT,
