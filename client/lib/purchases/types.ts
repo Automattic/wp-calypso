cb9c9ac5280704b4ca94863c6b76ba9cc0b1c35c
MILLER/F <millerf@automattic.com>
2025-09-12 17:54:12 +0200

    Revert unecessary change

--- client/lib/purchases/types.ts
+++ client/lib/purchases/types.ts
@@ -16,6 +16,28 @@ export type PurchasePaymentWithPayPal = Purchases.PurchasePaymentWithPayPal;
 export type PurchasePaymentWithCreditCard = Purchases.PurchasePaymentWithCreditCard;
 export type PurchasePaymentCreditCard = Purchases.PurchasePaymentCreditCard;
 
+export interface MembershipSubscription {
+	ID: string;
+	currency: string;
+	end_date: string | null;
+	product_id: string;
+	renew_interval: string | null;
+	renewal_price: string;
+	site_id: string;
+	site_title: string;
+	site_url: string;
+	start_date: string;
+	status: string;
+	title: string;
+}
+
+export interface MembershipSubscriptionsSite {
+	id: string;
+	name: string;
+	domain: string;
+	subscriptions: MembershipSubscription[];
+}
+
 export interface Owner {
 	ID: number;
 	display_name?: string;
