158a30cea116d5d7abada0414dcf18e7ed3c3de3
Philip Jackson <p-jackson@live.com>
2025-09-12 19:03:01 +1200

    Adds centralised handling of mutation snackbar messages

--- client/dashboard/app/snackbars/index.tsx
+++ client/dashboard/app/snackbars/index.tsx
@@ -1,8 +1,12 @@
+import {
+	registerMutationSuccessCallback,
+	registerMutationErrorCallback,
+} from '@automattic/api-queries';
 import { SnackbarList } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { Icon, published, error } from '@wordpress/icons';
 import { store as noticesStore } from '@wordpress/notices';
-import React from 'react';
+import { useEffect } from 'react';
 import './style.scss';
 
 // Last three notices. Slices from the tail end of the list.
@@ -13,9 +17,44 @@ const statusIcon: Record< string, React.JSX.Element > = {
 	error,
 };
 
+declare module '@tanstack/react-query' {
+	interface Register {
+		mutationMeta: {
+			snackbar?: {
+				success?: string;
+				error?: string;
+			};
+		};
+	}
+}
+
 export default function Snackbars() {
 	const notices = useSelect( ( select ) => select( noticesStore ).getNotices(), [] );
-	const { removeNotice } = useDispatch( noticesStore );
+	const { removeNotice, createSuccessNotice, createErrorNotice } = useDispatch( noticesStore );
+
+	// Displays snackbars which have been requested through the `meta` option of
+	// the `useMutation` hook.
+	useEffect( () => {
+		const cleanupSuccess = registerMutationSuccessCallback(
+			( data, variables, context, mutation ) => {
+				const message = mutation.meta?.snackbar?.success;
+				if ( message ) {
+					createSuccessNotice( message, { type: 'snackbar' } );
+				}
+			}
+		);
+		const cleanupError = registerMutationErrorCallback( ( error, variables, context, mutation ) => {
+			const message = mutation.meta?.snackbar?.error;
+			if ( message ) {
+				createErrorNotice( message, { type: 'snackbar' } );
+			}
+		} );
+		return () => {
+			cleanupSuccess();
+			cleanupError();
+		};
+	}, [ createSuccessNotice, createErrorNotice ] );
+
 	const snackbarNotices = notices
 		.filter( ( { type } ) => type === 'snackbar' )
 		.map( ( { status, ...notice } ) => ( {
