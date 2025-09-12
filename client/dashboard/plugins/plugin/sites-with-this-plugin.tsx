c8e8db0d33d6fe850d56988b942b8d8fce7b2148
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-09-12 11:31:51 +0200

    Rename prop

--- client/dashboard/plugins/plugin/sites-with-this-plugin.tsx
+++ client/dashboard/plugins/plugin/sites-with-this-plugin.tsx
@@ -1,7 +1,16 @@
 import { Site } from '@automattic/api-core';
+import { sitePluginActivateMutation } from '@automattic/api-queries';
+import { useMutation } from '@tanstack/react-query';
+import {
+	__experimentalVStack as VStack,
+	__experimentalHStack as HStack,
+	__experimentalText as Text,
+	Button,
+} from '@wordpress/components';
 import { DataViews, filterSortAndPaginate, View } from '@wordpress/dataviews';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { useMemo, useState } from 'react';
+import { buildBulkSitesPluginAction } from '../manage/utils';
 import { usePlugin } from './use-plugin';
 
 const defaultView: View = {
@@ -11,9 +20,90 @@ const defaultView: View = {
 	titleField: 'domain',
 };
 
-export const SitesWithThisPlugin = ( { pluginId }: { pluginId: string } ) => {
+const ActionRenderModal = ( { actionId, item, closeModal, onExecute, pluginId } ) => {
+	const [ isBusy, setIsBusy ] = useState( false );
+
+	const handleConfirm = async () => {
+		setIsBusy( true );
+		try {
+			const { successCount, errorCount } = await onExecute( [
+				{ id: pluginId, siteIds: [ item.ID ] },
+			] );
+
+			console.debug( 'successCount', successCount );
+			console.debug( 'errorCount', errorCount );
+			// if ( successCount > 0 ) {
+			// 	const prefix = buildSuccessPrefix( actionId, items );
+			// 	createSuccessNotice(
+			// 		sprintf(
+			// 			// translators: %d is the number of sites.
+			// 			_n( '%1$s on %2$d site', '%1$s on %2$d sites', successCount, 'next-admin' ),
+			// 			prefix,
+			// 			successCount
+			// 		),
+			// 		{
+			// 			type: 'snackbar',
+			// 		}
+			// 	);
+			// }
+			// if ( errorCount > 0 ) {
+			// 	const errorPrefix = buildErrorPrefix( actionId, items );
+			// 	createErrorNotice(
+			// 		sprintf(
+			// 			// translators: %d is the number of sites.
+			// 			_n( '%1$s on %2$d site', '%1$s on %2$d sites', errorCount, 'next-admin' ),
+			// 			errorPrefix,
+			// 			errorCount
+			// 		),
+			// 		{
+			// 			type: 'snackbar',
+			// 		}
+			// 	);
+			// }
+		} finally {
+			setIsBusy( false );
+			closeModal?.();
+		}
+	};
+
+	return (
+		<VStack spacing={ 4 }>
+			<Text>
+				{ sprintf(
+					// translators: %1$s is the plugin name. %2$d is the number of sites.
+					__( 'You are about to activate the %1$s plugin installed on %2$d sites.' ),
+					'foo',
+					1
+				) }
+			</Text>
+			<HStack justify="right">
+				<Button
+					__next40pxDefaultSize
+					variant="tertiary"
+					onClick={ closeModal }
+					disabled={ isBusy }
+					accessibleWhenDisabled
+				>
+					{ __( 'Cancel' ) }
+				</Button>
+				<Button
+					__next40pxDefaultSize
+					variant="primary"
+					onClick={ handleConfirm }
+					isBusy={ isBusy }
+					disabled={ isBusy }
+					accessibleWhenDisabled
+				>
+					{ __( 'Activate' ) }
+				</Button>
+			</HStack>
+		</VStack>
+	);
+};
+
+export const SitesWithThisPlugin = ( { pluginSlug }: { pluginSlug: string } ) => {
 	const [ view, setView ] = useState< View >( defaultView );
-	const { isLoading, pluginBySiteId, sitesWithThisPlugin } = usePlugin( pluginId );
+	const { isLoading, pluginBySiteId, sitesWithThisPlugin } = usePlugin( pluginSlug );
 
 	const fields = useMemo(
 		() => [
@@ -65,6 +155,28 @@ export const SitesWithThisPlugin = ( { pluginId }: { pluginId: string } ) => {
 			onChangeView={ setView }
 			defaultLayouts={ { table: {} } }
 			actions={ [
+				{
+					id: 'activate',
+					label: __( 'Activate' ),
+					modalHeader: __( 'Activate plugin' ),
+					RenderModal: ( { items, closeModal } ) => {
+						const [ item ] = items;
+						console.debug( 'item', item );
+						const { mutateAsync } = useMutation( sitePluginActivateMutation() );
+						const action = buildBulkSitesPluginAction( mutateAsync );
+						return (
+							<ActionRenderModal
+								actionId="activate"
+								item={ item }
+								closeModal={ closeModal }
+								onExecute={ action }
+								pluginId={ pluginId }
+							/>
+						);
+					},
+					isEligible: ( item ) => ! item.isActive,
+					supportsBulk: true,
+				},
 				{
 					id: 'delete',
 					label: __( 'Delete' ),
--- client/dashboard/plugins/plugin/use-plugin.ts
+++ client/dashboard/plugins/plugin/use-plugin.ts
@@ -9,27 +9,27 @@ import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { useLocale } from '../../app/locale';
 
-export const usePlugin = ( pluginId: string ) => {
+export const usePlugin = ( pluginSlug: string ) => {
 	const locale = useLocale();
 	const { data: sitesPlugins, isLoading: isLoadingSitesPlugins } = useQuery( pluginsQuery() );
 	const { data: sites, isLoading: isLoadingSites } = useQuery( sitesQuery() );
 	const { data: marketplacePlugin, isLoading: isLoadingMarketplacePlugin } = useQuery(
-		marketplacePluginQuery( pluginId )
+		marketplacePluginQuery( pluginSlug )
 	);
 	const { data: wpOrgPlugin, isLoading: isLoadingWpOrgPlugin } = useQuery(
-		wpOrgPluginQuery( pluginId, locale )
+		wpOrgPluginQuery( pluginSlug, locale )
 	);
 
 	const pluginBySiteId = useMemo(
 		() =>
 			Object.entries( sitesPlugins?.sites || {} ).reduce( ( acc, [ siteId, plugins ] ) => {
-				const plugin = plugins.find( ( p ) => p.slug === pluginId );
+				const plugin = plugins.find( ( p ) => p.slug === pluginSlug );
 				if ( plugin ) {
 					acc.set( Number( siteId ), plugin );
 				}
 				return acc;
 			}, new Map< number, PluginItem >() ),
-		[ sitesPlugins, pluginId ]
+		[ sitesPlugins, pluginSlug ]
 	);
 
 	const siteIdsWithThisPlugin = Array.from( pluginBySiteId.keys() );
@@ -42,7 +42,13 @@ export const usePlugin = ( pluginId: string ) => {
 		? sites.reduce(
 				( acc, site ) => {
 					if ( siteIdsWithThisPlugin.includes( site.ID ) ) {
-						acc[ 0 ].push( site );
+						const isActive = pluginBySiteId.get( site.ID )?.active ?? false;
+
+						if ( ! isActive ) {
+							console.debug( 'not active - site', site );
+						}
+
+						acc[ 0 ].push( { ...site, isActive } );
 					} else {
 						acc[ 1 ].push( site );
 					}
