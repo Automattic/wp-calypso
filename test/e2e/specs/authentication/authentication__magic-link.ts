dec7946fe687a738201534f685aa5f9fe24e4712
Alister Scott <alister.scott@gmail.com>
2025-09-15 09:25:29 +1000

    Skip e2e magic link tests on CI as hitting WordPress.com magic link rate limit

--- test/e2e/specs/authentication/authentication__magic-link.ts
+++ test/e2e/specs/authentication/authentication__magic-link.ts
@@ -4,56 +4,60 @@
 
 import { DataHelper, EmailClient, LoginPage, SecretsManager } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
+import { skipDescribeIf } from '../../jest-helpers';
 import type { Message } from 'mailosaur/lib/models';
 
 declare const browser: Browser;
 
-describe( DataHelper.createSuiteTitle( 'Authentication: Magic Link' ), function () {
-	const credentials = SecretsManager.secrets.testAccounts.defaultUser;
-	let page: Page;
-	let loginPage: LoginPage;
-	let emailClient: EmailClient;
-	let magicLinkURL: URL;
-	let magicLinkEmail: Message;
+skipDescribeIf( !! process.env.CI )(
+	DataHelper.createSuiteTitle( 'Authentication: Magic Link' ),
+	function () {
+		const credentials = SecretsManager.secrets.testAccounts.defaultUser;
+		let page: Page;
+		let loginPage: LoginPage;
+		let emailClient: EmailClient;
+		let magicLinkURL: URL;
+		let magicLinkEmail: Message;
 
-	beforeAll( async () => {
-		page = await browser.newPage();
-	} );
-
-	it( 'Navigate to Magic Link screen from /login', async function () {
-		loginPage = new LoginPage( page );
-		await loginPage.visit();
-		await loginPage.clickSendMagicLink();
-	} );
-
-	it( 'Request magic link', async function () {
-		// It is safe to add type assertion here, since the email
-		// field is defined for `defaultUser`.
-		await loginPage.fillUsername( credentials.email as string );
-		await loginPage.clickSubmit();
-
-		emailClient = new EmailClient();
-		magicLinkEmail = await emailClient.getLastMatchingMessage( {
-			inboxId: SecretsManager.secrets.mailosaur.defaultUserInboxId,
-			sentTo: credentials.email as string,
-			subject: 'Log in to WordPress.com',
+		beforeAll( async () => {
+			page = await browser.newPage();
 		} );
-		magicLinkURL = emailClient.getMagicLink( magicLinkEmail );
 
-		expect( magicLinkURL.href ).toBeDefined();
-	} );
+		it( 'Navigate to Magic Link screen from /login', async function () {
+			loginPage = new LoginPage( page );
+			await loginPage.visit();
+			await loginPage.clickSendMagicLink();
+		} );
 
-	it( 'Open the magic link', async function () {
-		await page.goto( magicLinkURL.href );
-	} );
+		it( 'Request magic link', async function () {
+			// It is safe to add type assertion here, since the email
+			// field is defined for `defaultUser`.
+			await loginPage.fillUsername( credentials.email as string );
+			await loginPage.clickSubmit();
 
-	it( 'Redirected to Home dashboard', async function () {
-		await page.waitForURL( /home/, { timeout: 15 * 1000 } );
-	} );
+			emailClient = new EmailClient();
+			magicLinkEmail = await emailClient.getLastMatchingMessage( {
+				inboxId: SecretsManager.secrets.mailosaur.defaultUserInboxId,
+				sentTo: credentials.email as string,
+				subject: 'Log in to WordPress.com',
+			} );
+			magicLinkURL = emailClient.getMagicLink( magicLinkEmail );
 
-	afterAll( async () => {
-		if ( magicLinkEmail ) {
-			await emailClient.deleteMessage( magicLinkEmail );
-		}
-	} );
-} );
+			expect( magicLinkURL.href ).toBeDefined();
+		} );
+
+		it( 'Open the magic link', async function () {
+			await page.goto( magicLinkURL.href );
+		} );
+
+		it( 'Redirected to Home dashboard', async function () {
+			await page.waitForURL( /home/, { timeout: 15 * 1000 } );
+		} );
+
+		afterAll( async () => {
+			if ( magicLinkEmail ) {
+				await emailClient.deleteMessage( magicLinkEmail );
+			}
+		} );
+	}
+);
