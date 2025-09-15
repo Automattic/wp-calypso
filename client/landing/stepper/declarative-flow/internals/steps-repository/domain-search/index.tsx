7a620861361f674446c8f3feb13a19b5bf719b81
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-09-15 10:31:25 -0300

    Stretch domain search content in old StepContainer

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-search/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-search/index.tsx
@@ -8,6 +8,7 @@ import {
 } from '@automattic/onboarding';
 import { __ } from '@wordpress/i18n';
 import { WPCOMDomainSearch } from 'calypso/components/domains/wpcom-domain-search';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { getSuggestionsVendor } from 'calypso/lib/domains/suggestions';
 import { useQuery } from '../../../../hooks/use-query';
@@ -53,6 +54,9 @@ const DomainSearchStep: StepType< {
 		skippable: isNewsletterFlow( flow ),
 	};
 
+	const text = __( 'Claim your space on the web' );
+	const subText = __( 'Make it yours with a .com, .blog, or one of 350+ domain options.' );
+
 	if ( shouldUseStepContainerV2( flow ) ) {
 		return (
 			<Step.CenteredColumnLayout
@@ -65,12 +69,7 @@ const DomainSearchStep: StepType< {
 				}
 				columnWidth={ 10 }
 				className="step-container-v2--domain-search"
-				heading={
-					<Step.Heading
-						text={ __( 'Claim your space on the web' ) }
-						subText={ __( 'Make it yours with a .com, .blog, or one of 350+ domain options.' ) }
-					/>
-				}
+				heading={ <Step.Heading text={ text } subText={ subText } /> }
 			>
 				<WPCOMDomainSearch
 					className="step-container-v2-domain-search"
@@ -104,6 +103,7 @@ const DomainSearchStep: StepType< {
 			flowName={ flow }
 			goBack={ () => {} }
 			goNext={ () => {} }
+			formattedHeader={ <FormattedHeader headerText={ text } subHeaderText={ subText } /> }
 			stepContent={
 				<WPCOMDomainSearch flowName={ flow } config={ config } initialQuery={ initialQuery } />
 			}
