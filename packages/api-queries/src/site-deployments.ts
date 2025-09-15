82e0929d90ba86159cf2d4e33b281184f78cb1b3
Roberto Aranda <roberto.aranda@automattic.com>
2025-09-15 19:01:05 +0200

    Delete Deployment: Migrate delete mutation to new structure

--- packages/api-queries/src/site-deployments.ts
+++ packages/api-queries/src/site-deployments.ts
@@ -1,5 +1,9 @@
-import { fetchCodeDeployments, fetchCodeDeploymentRuns } from '@automattic/api-core';
-import { queryOptions } from '@tanstack/react-query';
+import {
+	fetchCodeDeployments,
+	fetchCodeDeploymentRuns,
+	deleteCodeDeployment,
+} from '@automattic/api-core';
+import { mutationOptions, queryOptions } from '@tanstack/react-query';
 
 export const codeDeploymentsQuery = ( siteId: number ) =>
 	queryOptions( {
@@ -11,4 +15,13 @@ export const codeDeploymentRunsQuery = ( siteId: number, deploymentId: number ) 
 	queryOptions( {
 		queryKey: [ 'site', siteId, 'code-deployments-runs', deploymentId ],
 		queryFn: () => fetchCodeDeploymentRuns( siteId, deploymentId ),
+	} );
+
+export const codeDeploymentDeleteMutation = (
+	siteId: number,
+	deploymentId: number,
+	removeFiles: boolean
+) =>
+	mutationOptions( {
+		mutationFn: () => deleteCodeDeployment( siteId, deploymentId, removeFiles ),
 	} );
