3d6c19ca8de47618bd9bacbc1dd114012cc76525
Omar Alshaker <omar@omaralshaker.com>
2025-09-15 17:15:21 +0200

    Use Agenttic's typing effect

--- packages/odie-client/src/components/message/typing-placeholder.tsx
+++ packages/odie-client/src/components/message/typing-placeholder.tsx
@@ -1,14 +1,10 @@
+import { ThinkingMessage } from '@automattic/agenttic-ui';
 import { __ } from '@wordpress/i18n';
-import TypingAnimation from '../../assets/zendesk-typing-animation.svg';
 
 export const TypingPlaceholder = () => {
 	return (
-		<div className="odie-chatbox-typing-placeholder">
-			<img
-				src={ TypingAnimation }
-				alt={ __( 'Happiness Engineer is typing', __i18n_text_domain__ ) }
-				className="odie-chatbox-thinking-icon"
-			/>
+		<div className="odie-chatbox-typing-placeholder agenttic">
+			<ThinkingMessage content={ __( 'Typing…', __i18n_text_domain__ ) } />
 		</div>
 	);
 };
