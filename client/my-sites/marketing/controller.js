13d699ebb9a70cb6d069dc1ba7fd42d7e7877b95
Konstantin Obenland <obenland@gmx.de>
2025-09-15 16:09:32 -0500

    Add ActivityPub site selector and redirect handler
    Adds a handler for /marketing/activitypub that displays a site selector
    and redirects to the appropriate ActivityPub settings page. For Jetpack/Atomic
    sites, checks if the ActivityPub plugin is installed and redirects to plugin
    installation if needed.

--- client/my-sites/marketing/controller.js
+++ client/my-sites/marketing/controller.js
@@ -7,6 +7,7 @@ import SharingButtons from 'calypso/sites/marketing/sharing/buttons';
 import MarketingTools from 'calypso/sites/marketing/tools';
 import Traffic from 'calypso/sites/marketing/traffic/traffic';
 import { errorNotice } from 'calypso/state/notices/actions';
+import { getPluginOnSite } from 'calypso/state/plugins/installed/selectors';
 import { fetchPreferences } from 'calypso/state/preferences/actions';
 import { getPreference, hasReceivedRemotePreferences } from 'calypso/state/preferences/selectors';
 import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
@@ -123,6 +124,38 @@ export const sharingButtons = ( context, next ) => {
 	context.contentComponent = createElement( SharingButtons );
 
 	next();
+};
+
+export const activitypub = ( { store } ) => {
+	const state = store.getState();
+	const siteId = getSelectedSiteId( state );
+
+	if ( ! siteId ) {
+		// Fallback to site selection if no site is selected.
+		page.redirect( '/marketing/activitypub' );
+	}
+
+	if ( ! canCurrentUser( state, siteId, 'manage_options' ) ) {
+		store.dispatch(
+			errorNotice(
+				translate( 'You are not authorized to manage ActivityPub settings for this site.' )
+			)
+		);
+	}
+
+	// For Jetpack/Atomic sites, check if ActivityPub plugin is installed
+	if ( isJetpackSite( state, siteId, { treatAtomicAsJetpackSite: true } ) ) {
+		const activitypubPlugin = getPluginOnSite( state, siteId, 'activitypub' );
+
+		if ( ! activitypubPlugin ) {
+			// Plugin not installed, redirect to plugin installation with search term
+			return navigate(
+				getSiteAdminUrl( state, siteId, 'plugin-install.php?s=activitypub&tab=search&type=term' )
+			);
+		}
+	}
+
+	return navigate( getSiteAdminUrl( state, siteId, 'options-general.php?page=activitypub' ) );
 };
 
 export const traffic = ( context, next ) => {
