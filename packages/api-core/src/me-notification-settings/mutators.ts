31d1e0f31df8837ff8e8d2d0730209c7841ed2c5
Addison Stavlo <Addison.Stavlo@Automattic.com>
2025-09-15 11:15:20 -0400

    remove allow list from mutator

--- packages/api-core/src/me-notification-settings/mutators.ts
+++ packages/api-core/src/me-notification-settings/mutators.ts
@@ -4,26 +4,5 @@ import type { WpcomNotificationSettings } from './types';
 export async function updateWpcomNotificationSettings(
 	data: Partial< WpcomNotificationSettings >
 ): Promise< Partial< WpcomNotificationSettings > > {
-	const saveableKeys: ( keyof WpcomNotificationSettings )[] = [
-		'marketing',
-		'research',
-		'community',
-		'promotion',
-		'news',
-		'digest',
-		'reports',
-		'news_developer',
-		'scheduled_updates',
-		'jetpack_marketing',
-		'jetpack_research',
-		'jetpack_promotion',
-		'jetpack_news',
-		'jetpack_reports',
-	];
-
-	const wpcomPayload = Object.fromEntries(
-		saveableKeys.filter( ( key ) => key in data ).map( ( key ) => [ key, data[ key ] ] )
-	);
-
-	return await wpcom.req.post( '/me/notifications/settings', { wpcom: wpcomPayload } );
+	return await wpcom.req.post( '/me/notifications/settings', { wpcom: data } );
 }
