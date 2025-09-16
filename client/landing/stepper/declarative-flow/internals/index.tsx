a516784fe81414dc151641a9c2c61ff7791e6208
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-09-16 10:51:59 -0300

    Remove setup from the redirect URL as its the base path

--- client/landing/stepper/declarative-flow/internals/index.tsx
+++ client/landing/stepper/declarative-flow/internals/index.tsx
@@ -196,12 +196,13 @@ export const FlowRenderer: React.FC< {
 
 		if ( step.slug === PRIVATE_STEPS.USER.slug ) {
 			const postAuthStepPath = createPath( {
-				pathname: generatePath( '/setup/:flow/', {
+				pathname: generatePath( '/:flow/', {
 					flow: flow.variantSlug ?? flow.name,
 				} ),
 				search: window.location.search,
 				hash: window.location.hash,
 			} );
+
 			// In this case, the user step is not able to determine the next step after auth. This happens when users somehow land in /flow/user directly.
 			// So we navigate to the landing page of the flow and let the flow decide what to do.
 			// If you intend to land in /flow/user, please point your URL to the step itself and Stepper will automatically redirect to the user step if needed.
