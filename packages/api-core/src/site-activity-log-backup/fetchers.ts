0a83efde8a46f29c8d37d8a770d7799d3c21547f
Andrea Grassi <andrea.grassi@automattic.com>
2025-09-16 11:53:52 +0200

    change return type to ActivityLogAPIResponse

--- packages/api-core/src/site-activity-log-backup/fetchers.ts
+++ packages/api-core/src/site-activity-log-backup/fetchers.ts
@@ -1,10 +1,10 @@
 import { wpcom } from '../wpcom-fetcher';
-import type { ActivityLog } from '../site-activity-log/types';
+import type { ActivityLogAPIResponse } from '../site-activity-log/types';
 
 export async function fetchSiteBackupActivityLog(
 	siteId: number,
 	{ number, aggregate = false }: { number: number; aggregate?: boolean }
-): Promise< ActivityLog > {
+): Promise< ActivityLogAPIResponse > {
 	return wpcom.req.get(
 		{
 			path: `/sites/${ siteId }/activity/rewindable`,
