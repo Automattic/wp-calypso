c2dd26b66c867934a3002b13b639e8cea2f5abd9
Tiago Noronha <hello@tiagonoronha.com>
2025-09-16 18:23:12 +0100

    Add copy checkout link action to pending referrals

--- client/a8c-for-agencies/sections/referrals/referral-details/referrals/index.tsx
+++ client/a8c-for-agencies/sections/referrals/referral-details/referrals/index.tsx
@@ -5,6 +5,7 @@ import { A4AConfirmationDialog } from 'calypso/a8c-for-agencies/components/a4a-c
 import useProductsQuery from 'calypso/a8c-for-agencies/data/marketplace/use-products-query';
 import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { successNotice, errorNotice } from 'calypso/state/notices/actions';
 import useHandleReferralArchive from '../../hooks/use-handle-referral-archive';
 import ClientReferrals from '../client-referrals';
 import ClientReferralsMobile from '../mobile/referrals-mobile';
@@ -28,6 +29,38 @@ const ReferralDetailsReferrals = ( { referrals }: { referrals: ReferralAPIRespon
 	);
 
 	const referralActions = [
+		{
+			id: 'copy-link',
+			label: translate( 'Copy link' ),
+			isPrimary: false,
+			callback( items: SetStateAction< ReferralAPIResponse | null >[] ) {
+				const referral = items[ 0 ] as ReferralAPIResponse;
+				if ( referral?.checkout_url ) {
+					dispatch( recordTracksEvent( 'calypso_a4a_referrals_copy_link_button_click' ) );
+					navigator.clipboard
+						.writeText( referral.checkout_url )
+						.then( () => {
+							dispatch(
+								successNotice( translate( 'Link has been copied to clipboard' ), {
+									id: 'copy-referral-link-success',
+									duration: 3000,
+								} )
+							);
+						} )
+						.catch( () => {
+							dispatch(
+								errorNotice( translate( "Couldn't copy link to clipboard" ), {
+									id: 'copy-referral-link-error',
+									duration: 5000,
+								} )
+							);
+						} );
+				}
+			},
+			isEligible( referral: ReferralAPIResponse ) {
+				return referral.status === 'pending';
+			},
+		},
 		{
 			id: 'archive-referral',
 			label: translate( 'Archive' ),
