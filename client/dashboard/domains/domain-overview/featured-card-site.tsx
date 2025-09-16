bf8299806627e11977e0a147735ce4fb9ac3ebe0
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2025-09-16 18:01:26 +0200

    Domains: Site overivew: Handle Add/Attach for domain only case

--- client/dashboard/domains/domain-overview/featured-card-site.tsx
+++ client/dashboard/domains/domain-overview/featured-card-site.tsx
@@ -1,6 +1,7 @@
-import { Domain } from '@automattic/api-core';
+import { Domain, DomainSubtype } from '@automattic/api-core';
 import { siteByIdQuery } from '@automattic/api-queries';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { Button, __experimentalHStack as HStack } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import OverviewCard from '../../sites/overview-card';
 import SiteIcon from '../../sites/site-icon';
@@ -16,13 +17,35 @@ export default function FeaturedCardSite( { domain }: Props ) {
 		return null;
 	}
 
+	const shouldShowAddAttachSite =
+		domain.is_domain_only_site &&
+		! domain.is_gravatar_restricted_domain &&
+		domain.subtype.id !== DomainSubtype.DOMAIN_TRANSFER;
+
 	return (
 		<OverviewCard
 			title={ __( 'Site' ) }
 			heading={ <span style={ { wordBreak: 'break-all' } }>{ site.name }</span> }
-			link={ `/sites/${ site.slug }` }
+			link={
+				shouldShowAddAttachSite
+					? `/domains/${ domain.domain }/transfer/other-site`
+					: `/sites/${ site.slug }`
+			}
 			icon={ <SiteIcon site={ site } /> }
-			description={ domain.domain }
+			description={ shouldShowAddAttachSite ? __( 'Attach to an existing site' ) : domain.domain }
+			bottom={
+				shouldShowAddAttachSite && (
+					<HStack>
+						<Button
+							size="compact"
+							variant="primary"
+							href={ `/start/site-selected/?siteSlug=${ site.slug }&siteId=${ site.ID }` }
+						>
+							{ __( 'Add a new site' ) }
+						</Button>
+					</HStack>
+				)
+			}
 		/>
 	);
 }
