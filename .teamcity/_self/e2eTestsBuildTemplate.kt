6e2ddd2ead9f6a6f63657b4c501cd277a0992ab0
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-09-16 14:10:48 +0300

    Add Calypso E2E Tests Build Template for Playwright integration

---
+++ .teamcity/_self/e2eTestsBuildTemplate.kt
@@ -0,0 +1,13 @@
+package TeamcitySandbox_QualityOpsSandbox.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.*
+
+object Calypso_e2e_Tests_BuildTemplate : Template({
+    name = "Calypso E2E Tests Build Template"
+    description = "Runs Calypso Playwright e2e tests using Playwright Test runner"
+
+    vcs {
+		root(Settings.WpCalypso)
+		cleanCheckout = true
+	}
+})
