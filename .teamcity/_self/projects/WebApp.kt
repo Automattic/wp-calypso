96673f01066610d5773f16ab091e346290518f2a
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-09-16 14:42:15 +0300

    Update PlaywrightTestPRMatrix to use Calypso E2E Tests Build Template

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -908,7 +908,7 @@ fun playwrightPrBuildType( targetDevice: String, buildUuid: String ): E2EBuildTy
 }
 
 object PlaywrightTestPRMatrix : BuildType({
-	templates(TeamcitySandbox_QualityOpsSandbox_BuildTemplateOne)
+	templates(Calypso_e2e_Tests_BuildTemplate)
 	id("calypso_WebApp_Calypso_E2E_Playwright_Test_Matrix")
 	uuid = "074d8ae0-0859-4b4d-bf66-709f24ae5406"
 	name = "E2E Tests (Playwright Test)"
