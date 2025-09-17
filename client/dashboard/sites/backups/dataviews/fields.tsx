f3a5d15d5cb601b23890274227dfa879b356c769
Rafael Agostini <rafael.agostini@automattic.com>
2025-09-17 00:03:36 -0500

    Update fields.tsx to disable sorting for the Icon field in the dataviews

--- client/dashboard/sites/backups/dataviews/fields.tsx
+++ client/dashboard/sites/backups/dataviews/fields.tsx
@@ -21,6 +21,7 @@ export function getFields(): Field< ActivityLogEntry >[] {
 		{
 			id: 'icon',
 			label: __( 'Icon' ),
+			enableSorting: false,
 			render: ( { item } ) => (
 				<Icon
 					icon={ gridiconToWordPressIcon( item.gridicon ) }
