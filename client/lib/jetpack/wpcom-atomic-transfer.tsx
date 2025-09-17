50e0a36bc63ccaba719853c58b1de1d27485babd
Claudiu Filip <claudiu.filip@automattic.com>
2025-09-17 16:04:14 +0200

    Show Backups upsell for uninitialized rewind atomic sites

--- client/lib/jetpack/wpcom-atomic-transfer.tsx
+++ client/lib/jetpack/wpcom-atomic-transfer.tsx
@@ -1,10 +1,31 @@
 import { isEnabled } from '@automattic/calypso-config';
 import BusinessATSwitch from 'calypso/components/jetpack/business-at-switch';
 import { AtomicContentSwitch } from 'calypso/components/jetpack/wpcom-business-at';
+import { useSelector } from 'calypso/state';
+import getRewindState from 'calypso/state/selectors/get-rewind-state';
+import isSiteWpcomAtomic from 'calypso/state/selectors/is-site-wpcom-atomic';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import type { Context } from './types';
-import type { ComponentType } from 'react';
+import type { ComponentType, ReactNode } from 'react';
+
+type GateProps = {
+	UpsellComponent: ComponentType;
+	content?: AtomicContentSwitch;
+	children?: ReactNode;
+};
+
+const AtomicATGate = ( { UpsellComponent, content, children }: GateProps ) => {
+	const siteId = useSelector( getSelectedSiteId ) as number | undefined;
+	const rewind = useSelector( ( state ) => getRewindState( state, siteId ) );
+	const isUnavailable = ( rewind as { state?: string } | undefined )?.state === 'unavailable';
+
+	if ( isUnavailable ) {
+		return <BusinessATSwitch UpsellComponent={ UpsellComponent } content={ content } />;
+	}
+
+	return children as ReactNode;
+};
 
 export default function wpcomAtomicTransfer(
 	UpsellComponent: ComponentType,
@@ -14,11 +35,21 @@ export default function wpcomAtomicTransfer(
 		const getState = context.store.getState;
 		const siteId = getSelectedSiteId( getState() );
 		const isJetpack = isJetpackSite( getState(), siteId );
+		const isAtomic = isSiteWpcomAtomic( getState(), siteId );
 
-		if ( ! isJetpack && ! isEnabled( 'jetpack-cloud' ) && context.primary ) {
-			context.primary = (
-				<BusinessATSwitch UpsellComponent={ UpsellComponent } content={ content } />
-			);
+		if ( ! isEnabled( 'jetpack-cloud' ) && context.primary ) {
+			const originalPrimary = context.primary;
+			if ( ! isJetpack ) {
+				context.primary = (
+					<BusinessATSwitch UpsellComponent={ UpsellComponent } content={ content } />
+				);
+			} else if ( isAtomic ) {
+				context.primary = (
+					<AtomicATGate UpsellComponent={ UpsellComponent } content={ content }>
+						{ originalPrimary }
+					</AtomicATGate>
+				);
+			}
 		}
 
 		next();
