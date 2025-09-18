39043382a16d9340ef6374d8f0dcfc0ff1052724
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-09-18 07:20:04 -0300

    Fix Start behavior

--- packages/domain-search/src/ui/domains-full-cart-skip-button/index.tsx
+++ packages/domain-search/src/ui/domains-full-cart-skip-button/index.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
 
+import './style.scss';
+
 interface Props {
 	onSkip: () => void;
 }
 
-export const ChooseDomainLater = ( { onSkip }: Props ) => {
+export const DomainsFullCartSkipButton = ( { onSkip }: Props ) => {
 	const { __ } = useI18n();
 
 	return (
-		<Button className="choose-domain-later__btn" variant="link" onClick={ onSkip }>
+		<Button className="domains-full-cart__skip-button" variant="link" onClick={ onSkip }>
 			{ __( 'Choose a domain later' ) }
 		</Button>
 	);
