a01375331bddab09d0f4c70b747da42e39d68836
Alister Scott <alister.scott@gmail.com>
2025-09-18 11:22:21 +1000

    Revert "Standardised env url access and added A8C_FOR_AGENCIES_URL to doco"
    This reverts commit 5e9d0fd34e5e2d223613819abcd0a787f2e467a4.

--- packages/calypso-e2e/src/env-variables.ts
+++ packages/calypso-e2e/src/env-variables.ts
@@ -184,29 +184,27 @@ class EnvVariables implements SupportedEnvVariables {
 		}
 		return value as JetpackTarget;
 	}
-	/**
-	 * Helper to get and validate a URL environment variable.
-	 */
-	private getValidatedUrlEnvVar( envVarName: keyof SupportedEnvVariables ): string {
-		const value = process.env[ envVarName as string ];
-		const defaultValue = this._defaultEnvVariables[ envVarName ];
-		const url = value ?? defaultValue;
-
-		try {
-			// eslint-disable-next-line no-new
-			new URL( url as string );
-		} catch ( error ) {
-			throw new Error( `Invalid ${ envVarName } value: ${ url }.\nYou must provide a valid URL.` );
-		}
-		return url as string;
-	}
 
 	/**
 	 * Returns the A8C for Agencies URL.
 	 * @example 'https://agencies.automattic.com'
 	 */
 	get A8C_FOR_AGENCIES_URL(): string {
-		return this.getValidatedUrlEnvVar( 'A8C_FOR_AGENCIES_URL' );
+		const value = process.env.A8C_FOR_AGENCIES_URL;
+		if ( ! value ) {
+			return this._defaultEnvVariables.A8C_FOR_AGENCIES_URL;
+		}
+
+		try {
+			// Disabling eslint because this constructor is really the simplest way to validate a URL.
+			// eslint-disable-next-line no-new
+			new URL( value );
+		} catch ( error ) {
+			throw new Error(
+				`Invalid A8C_FOR_AGENCIES_URL value: ${ value }.\nYou must provide a valid URL.`
+			);
+		}
+		return value;
 	}
 
 	/**
@@ -214,7 +212,21 @@ class EnvVariables implements SupportedEnvVariables {
 	 * @example 'http://localhost:3000'
 	 */
 	get CALYPSO_BASE_URL(): string {
-		return this.getValidatedUrlEnvVar( 'CALYPSO_BASE_URL' );
+		const value = process.env.CALYPSO_BASE_URL;
+		if ( ! value ) {
+			return this._defaultEnvVariables.CALYPSO_BASE_URL;
+		}
+
+		try {
+			// Disabling eslint because this constructor is really the simplest way to validate a URL.
+			// eslint-disable-next-line no-new
+			new URL( value );
+		} catch ( error ) {
+			throw new Error(
+				`Invalid CALYPSO_BASE_URL value: ${ value }.\nYou must provide a valid URL.`
+			);
+		}
+		return value;
 	}
 
 	/**
@@ -222,7 +234,19 @@ class EnvVariables implements SupportedEnvVariables {
 	 * @example 'https://wordpress.com'
 	 */
 	get WPCOM_BASE_URL(): string {
-		return this.getValidatedUrlEnvVar( 'WPCOM_BASE_URL' );
+		const value = process.env.WPCOM_BASE_URL;
+		if ( ! value ) {
+			return this._defaultEnvVariables.WPCOM_BASE_URL;
+		}
+
+		try {
+			// Disabling eslint because this constructor is really the simplest way to validate a URL.
+			// eslint-disable-next-line no-new
+			new URL( value );
+		} catch ( error ) {
+			throw new Error( `Invalid WPCOM_BASE_URL value: ${ value }.\nYou must provide a valid URL.` );
+		}
+		return value;
 	}
 
 	get BROWSER_NAME(): string {
