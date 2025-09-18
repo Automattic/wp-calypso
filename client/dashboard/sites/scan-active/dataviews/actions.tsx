536570b93d3b836dd6cbe79160238d1e93ed2a1b
Rafael Agostini <rafael.agostini@automattic.com>
2025-09-18 12:11:09 -0500

    Add ThreatsDetailCard to actions modals

--- client/dashboard/sites/scan-active/dataviews/actions.tsx
+++ client/dashboard/sites/scan-active/dataviews/actions.tsx
@@ -1,7 +1,14 @@
-import { Icon } from '@wordpress/components';
+import {
+	Button,
+	__experimentalVStack as VStack,
+	__experimentalText as Text,
+	Icon,
+} from '@wordpress/components';
 import { Action } from '@wordpress/dataviews';
 import { __ } from '@wordpress/i18n';
 import { tool } from '@wordpress/icons';
+import { ButtonStack } from '../../../components/button-stack';
+import { ThreatsDetailCard } from '../../scan/components/threats-detail-card';
 import type { Threat } from '@automattic/api-core';
 
 export function getActions(): Action< Threat >[] {
@@ -13,13 +20,31 @@ export function getActions(): Action< Threat >[] {
 			label: __( 'Fix threat' ),
 			modalHeader: __( 'Fix threat' ),
 			supportsBulk: true,
-			// @TODO: render the proper fix modal
-			RenderModal: ( { items } ) => {
-				if ( items.length > 1 ) {
-					return <p>Fix threats { items.map( ( threat ) => threat.id ).join( ', ' ) }</p>;
-				}
+			RenderModal: ( { items, closeModal } ) => {
+				const fixButtonLabel = items.length === 1 ? __( 'Fix threat' ) : __( 'Fix all threats' );
+				const description =
+					items.length === 1
+						? __( 'Jetpack will be fixing the following threat:' )
+						: __( 'Jetpack will be fixing the following threats:' );
 
-				return <p>Fix threat #{ items[ 0 ].id }</p>;
+				return (
+					<VStack spacing={ 4 }>
+						<Text variant="muted">{ description }</Text>
+						<ThreatsDetailCard threats={ items } />
+
+						{ /* @TODO: render the proper threat description here */ }
+
+						<ButtonStack justify="flex-end">
+							<Button variant="tertiary" onClick={ closeModal }>
+								{ __( 'Cancel' ) }
+							</Button>
+							{ /* @TODO: implement the auto-fix threat action and remove the disabled prop */ }
+							<Button variant="primary" disabled>
+								{ fixButtonLabel }
+							</Button>
+						</ButtonStack>
+					</VStack>
+				);
 			},
 			isEligible: ( threat: Threat ) => !! threat.fixable,
 		},
@@ -28,28 +53,24 @@ export function getActions(): Action< Threat >[] {
 			label: __( 'Ignore threat' ),
 			modalHeader: __( 'Ignore threat' ),
 			supportsBulk: false,
-			// @TODO: render the proper ignore modal
-			RenderModal: ( { items } ) => <p>Ignore threat { items[ 0 ].id }</p>,
-		},
-		{
-			id: 'view_details',
-			label: __( 'View details' ),
-			modalHeader: __( 'Active threat' ),
-			supportsBulk: false,
-			// @TODO: render the proper details modal
-			RenderModal: ( { items } ) => <p>Details of thread { items[ 0 ].id }</p>,
-		},
-		{
-			id: 'view_source',
-			label: __( 'View vulnerability source' ),
-			supportsBulk: false,
-			callback: ( items: Threat[] ) => {
-				const threat = items[ 0 ];
-				if ( threat.source ) {
-					window.open( threat.source, '_blank' );
-				}
-			},
-			isEligible: ( threat: Threat ) => !! threat.source,
+			RenderModal: ( { items, closeModal } ) => (
+				<VStack spacing={ 4 }>
+					<Text variant="muted">{ __( 'Jetpack will be ignoring the following threat:' ) }</Text>
+					<ThreatsDetailCard threats={ items } />
+
+					{ /* @TODO: render the proper threat description here */ }
+
+					<ButtonStack justify="flex-end">
+						<Button variant="tertiary" onClick={ closeModal }>
+							{ __( 'Cancel' ) }
+						</Button>
+						{ /* @TODO: implement the ignore threat action and remove the disabled prop */ }
+						<Button variant="primary" disabled>
+							{ __( 'Ignore threat' ) }
+						</Button>
+					</ButtonStack>
+				</VStack>
+			),
 		},
 	];
 }
--- client/dashboard/sites/scan-history/dataviews/actions.tsx
+++ client/dashboard/sites/scan-history/dataviews/actions.tsx
@@ -1,5 +1,12 @@
+import {
+	__experimentalVStack as VStack,
+	__experimentalText as Text,
+	Button,
+} from '@wordpress/components';
 import { Action } from '@wordpress/dataviews';
 import { __ } from '@wordpress/i18n';
+import { ButtonStack } from '../../../components/button-stack';
+import { ThreatsDetailCard } from '../../scan/components/threats-detail-card';
 import type { Threat } from '@automattic/api-core';
 
 export function getActions(): Action< Threat >[] {
@@ -10,17 +17,38 @@ export function getActions(): Action< Threat >[] {
 			label: __( 'Unignore threat' ),
 			modalHeader: __( 'Unignore threat' ),
 			supportsBulk: false,
-			// @TODO: render the proper unignore modal
-			RenderModal: ( { items } ) => <p>Unignore threat { items[ 0 ].id }</p>,
+			RenderModal: ( { items, closeModal } ) => (
+				<VStack spacing={ 4 }>
+					<Text variant="muted">{ __( 'Jetpack will be unignoring the following threat:' ) }</Text>
+					<ThreatsDetailCard threats={ items } />
+
+					{ /* @TODO: render the proper threat description here */ }
+
+					<ButtonStack justify="flex-end">
+						<Button variant="tertiary" onClick={ closeModal }>
+							{ __( 'Cancel' ) }
+						</Button>
+						{ /* @TODO: implement the unignore threat action and remove the disabled prop */ }
+						<Button variant="primary" disabled>
+							{ __( 'Unignore threat' ) }
+						</Button>
+					</ButtonStack>
+				</VStack>
+			),
 		},
 		{
 			id: 'view_details',
 			isEligible: ( threat: Threat ) => threat.status !== 'ignored',
 			label: __( 'View details' ),
-			modalHeader: __( 'Threat' ),
+			modalHeader: __( 'View threat details' ),
 			supportsBulk: false,
-			// @TODO: render the proper details modal
-			RenderModal: ( { items } ) => <p>Details of threat { items[ 0 ].id }</p>,
+			RenderModal: ( { items } ) => (
+				<VStack spacing={ 4 }>
+					<ThreatsDetailCard threats={ items } />
+
+					{ /* @TODO: render the proper threat description here */ }
+				</VStack>
+			),
 		},
 	];
 }
