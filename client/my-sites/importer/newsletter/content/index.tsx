1e3885ccc32c7a44e2593738cdeeef1b0640ff84
Tony Arcangelini <tony@arcangelini.com>
2025-09-19 22:24:28 +0200

    Fix error in logic

--- client/my-sites/importer/newsletter/content/index.tsx
+++ client/my-sites/importer/newsletter/content/index.tsx
@@ -38,12 +38,7 @@ const isImporting = ( importerState: ImporterState | undefined ) => {
 	return (
 		importerState &&
 		(
-			[
-				appStates.IMPORT_FAILURE,
-				appStates.IMPORT_SUCCESS,
-				appStates.IMPORTING,
-				appStates.MAP_AUTHORS,
-			] as ImporterState[]
+			[ appStates.IMPORT_FAILURE, appStates.IMPORTING, appStates.MAP_AUTHORS ] as ImporterState[]
 		 ).includes( importerState )
 	);
 };
@@ -53,6 +48,7 @@ const isUploading = ( importerState: ImporterState | undefined ) => {
 		importerState &&
 		(
 			[
+				appStates.IMPORT_SUCCESS,
 				appStates.UPLOAD_PROCESSING,
 				appStates.READY_FOR_UPLOAD,
 				appStates.UPLOAD_FAILURE,
