8c07f9fc21d6e7970ffab4c0e7a7d0b103daec77
Gabriel Caires <gabriel.caires@automattic.com>
2025-09-19 18:17:36 +0100

    Add missing types

--- packages/api-queries/src/me-notifications-settings.ts
+++ packages/api-queries/src/me-notifications-settings.ts
@@ -1,14 +1,38 @@
 import {
 	fetchUserNotificationSettings,
+	updateUserNotificationSettings,
 	updateWpcomNotificationSettings,
+	UserNotificationSettings,
 } from '@automattic/api-core';
 import { queryOptions, mutationOptions } from '@tanstack/react-query';
+import deepmerge from 'deepmerge';
 import { queryClient } from './query-client';
 
 export const userNotificationsSettingsQuery = () =>
 	queryOptions( {
 		queryKey: [ 'me', 'notifications', 'settings' ],
 		queryFn: fetchUserNotificationSettings,
+	} );
+
+export const userNotificationsSettingsMutation = () =>
+	mutationOptions( {
+		mutationFn: updateUserNotificationSettings,
+		mutationKey: [ 'me', 'notifications', 'settings' ],
+		onMutate: async ( variables ) => {
+			const oldData = queryClient.getQueryData( userNotificationsSettingsQuery().queryKey );
+
+			queryClient.setQueryData(
+				userNotificationsSettingsQuery().queryKey,
+				deepmerge( oldData || {}, variables.data ) as UserNotificationSettings
+			);
+		},
+		onSuccess: async ( newData ) => {
+			const oldData = queryClient.getQueryData( userNotificationsSettingsQuery().queryKey );
+			const updated = { ...deepmerge( oldData ?? {}, newData ) } as UserNotificationSettings;
+			queryClient.setQueryData( userNotificationsSettingsQuery().queryKey, updated );
+
+			return updated;
+		},
 	} );
 
 export const userNotificationsExtrasSettingsMutation = () =>
