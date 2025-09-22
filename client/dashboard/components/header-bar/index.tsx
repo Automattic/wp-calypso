50dc190be487fbec91f6d785d807a98e38439512
arthur <arthur.chu@automattic.com>
2025-09-22 21:23:23 +0800

    Fix spacing

--- client/dashboard/components/header-bar/index.tsx
+++ client/dashboard/components/header-bar/index.tsx
@@ -24,7 +24,11 @@ function Header( { as = 'div', children }: { as?: 'div' | 'header'; children?: R
 
 Header.Title = function HeaderBarTitle( { children }: { children: React.ReactNode } ) {
 	return (
-		<HStack style={ { width: 'auto', flexShrink: 0 } } className="dashboard-header-bar-title">
+		<HStack
+			style={ { width: 'auto', flexShrink: 0 } }
+			className="dashboard-header-bar-title"
+			spacing={ 3 }
+		>
 			{ children }
 		</HStack>
 	);
