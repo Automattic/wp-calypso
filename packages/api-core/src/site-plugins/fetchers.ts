335712bafb081731edcb53c882c3cfef8c85ac3d
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-09-22 13:34:48 +0200

    Add `fetchSitePlugin`

--- packages/api-core/src/site-plugins/fetchers.ts
+++ packages/api-core/src/site-plugins/fetchers.ts
@@ -1,5 +1,12 @@
 import { wpcom } from '../wpcom-fetcher';
-import type { SitePluginsResponse, CorePlugin } from './types';
+import type { SitePluginsResponse, CorePlugin, SitePlugin } from './types';
+
+export function fetchSitePlugin( site: number, pluginId: string ): Promise< SitePlugin > {
+	return wpcom.req.get( {
+		path: `/sites/${ site }/plugins/${ pluginId }`,
+		apiVersion: '1.2',
+	} );
+}
 
 export async function fetchSitePlugins( siteId: number ): Promise< SitePluginsResponse > {
 	return await wpcom.req.get( { path: `/sites/${ siteId }/plugins`, apiVersion: '1.2' } );
