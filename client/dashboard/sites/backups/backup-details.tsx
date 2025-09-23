bc0bf712eeb65ac143fbc2c75c1c6804bf93c70e
Rafael Agostini <rafael.agostini@automattic.com>
2025-09-23 08:57:10 -0500

    Use _n() for download and restore buttons

--- client/dashboard/sites/backups/backup-details.tsx
+++ client/dashboard/sites/backups/backup-details.tsx
@@ -11,7 +11,7 @@ import {
 	Icon,
 } from '@wordpress/components';
 import { useViewportMatch } from '@wordpress/compose';
-import { __, sprintf } from '@wordpress/i18n';
+import { __, _n, sprintf } from '@wordpress/i18n';
 import { rotateLeft, download } from '@wordpress/icons';
 import FileBrowser from '../../../my-sites/backup/backup-contents-page/file-browser';
 import { useFileBrowserContext } from '../../../my-sites/backup/backup-contents-page/file-browser/file-browser-context';
@@ -33,7 +33,7 @@ export function BackupDetails( { backup, site }: { backup: ActivityLogEntry; sit
 		timeStyle: 'short',
 	} );
 	const { fileBrowserState } = useFileBrowserContext();
-	const selectedFiles = fileBrowserState.getSelectedList();
+	const { totalItems: selectedFilesCount } = fileBrowserState.getCheckList();
 
 	const isSmallViewport = useViewportMatch( 'medium', '<' );
 	const direction = isSmallViewport ? 'column-reverse' : 'row';
@@ -52,7 +52,7 @@ export function BackupDetails( { backup, site }: { backup: ActivityLogEntry; sit
 		} );
 	};
 
-	const hasSelectedFiles = selectedFiles.length > 0;
+	const hasSelectedFiles = selectedFilesCount > 0;
 	const actions = backup.rewind_id ? (
 		<ButtonStack alignment="stretch" justify="center" direction={ direction }>
 			<Button
@@ -67,7 +67,9 @@ export function BackupDetails( { backup, site }: { backup: ActivityLogEntry; sit
 				label={ hasSelectedFiles ? 'Feature not available yet' : undefined }
 				showTooltip={ hasSelectedFiles }
 			>
-				{ hasSelectedFiles ? __( 'Download selected files' ) : __( 'Download backup' ) }
+				{ hasSelectedFiles
+					? _n( 'Download selected file', 'Download selected files', selectedFilesCount )
+					: __( 'Download backup' ) }
 			</Button>
 			<Button
 				variant="primary"
@@ -76,7 +78,9 @@ export function BackupDetails( { backup, site }: { backup: ActivityLogEntry; sit
 				onClick={ handleRestoreClick }
 				style={ { justifyContent: 'center' } }
 			>
-				{ hasSelectedFiles ? __( 'Restore selected files' ) : __( 'Restore to this point' ) }
+				{ hasSelectedFiles
+					? _n( 'Restore selected file', 'Restore selected files', selectedFilesCount )
+					: __( 'Restore to this point' ) }
 			</Button>
 		</ButtonStack>
 	) : null;
