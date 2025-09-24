2c61459798c8a173315b57d1aaef2e358b32c155
StevenDufresne <dufresnesteven@gmail.com>
2025-09-24 10:59:50 +0900

    Comment out rule to show only one when there are two.

--- client/dashboard/components/page-header/sub-navigation.tsx
+++ client/dashboard/components/page-header/sub-navigation.tsx
@@ -1,6 +1,4 @@
 import { Breadcrumbs } from '@automattic/components/src/breadcrumbs';
-import { isRTL } from '@wordpress/i18n';
-import { chevronLeft, chevronRight } from '@wordpress/icons';
 import RouterLinkButton from '../router-link-button';
 import type { SubNavigationProps } from './types';
 
@@ -11,17 +9,17 @@ export const SubNavigation = ( props: SubNavigationProps ) => {
 		return null;
 	}
 
-	if ( props.items.length === 2 ) {
-		return (
-			<RouterLinkButton
-				className="sub-navigation__button"
-				icon={ isRTL() ? chevronRight : chevronLeft }
-				to={ props.items[ 0 ].href }
-			>
-				{ props.items[ 0 ].label }
-			</RouterLinkButton>
-		);
-	}
+	// if ( props.items.length === 2 ) {
+	// 	return (
+	// 		<RouterLinkButton
+	// 			className="sub-navigation__button"
+	// 			icon={ isRTL() ? chevronRight : chevronLeft }
+	// 			to={ props.items[ 0 ].href }
+	// 		>
+	// 			{ props.items[ 0 ].label }
+	// 		</RouterLinkButton>
+	// 	);
+	// }
 
 	return (
 		<Breadcrumbs
