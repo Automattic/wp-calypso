c67c8eec7784113e1697eebe47dd4e0e157efffc
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-09-25 15:59:15 +0300

    Add expect timeout configuration for tests

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -29,6 +29,9 @@ export default defineConfig( {
 	workers: process.env.CI ? '50%' : '100%',
 	/* Global timeout for each test */
 	timeout: 60 * 1000,
+	expect: {
+		timeout: 10 * 1000,
+	},
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter,
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
