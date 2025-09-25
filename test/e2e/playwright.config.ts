e4692601732d6fcb8511e236650d7db3d1f69b0c
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-09-25 12:06:40 +0300

    Add actionTimeout configuration to Playwright settings

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -36,6 +36,8 @@ export default defineConfig( {
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		// baseURL: 'http://localhost:3000',
+		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+		actionTimeout: 10 * 1000,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
