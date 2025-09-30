bebe3c26da08b50de10938bec3c73c2f91c99210
Rafael Agostini <rafael.agostini@automattic.com>
2025-09-30 16:12:18 -0500

    Add track event when opening view details modal

--- client/dashboard/sites/scan/dataviews/actions.tsx
+++ client/dashboard/sites/scan/dataviews/actions.tsx
@@ -1,8 +1,6 @@
-import { __experimentalVStack as VStack } from '@wordpress/components';
 import { Action } from '@wordpress/dataviews';
 import { __ } from '@wordpress/i18n';
-import { ThreatDescription } from '../components/threat-description';
-import { ThreatsDetailCard } from '../components/threats-detail-card';
+import { ViewDetailsModal } from '../components/view-details-modal';
 import type { Threat, Site } from '@automattic/api-core';
 
 export function getViewDetailsAction( site: Site ): Action< Threat > {
@@ -11,11 +9,6 @@ export function getViewDetailsAction( site: Site ): Action< Threat > {
 		label: __( 'View details' ),
 		modalHeader: __( 'View threat details' ),
 		supportsBulk: false,
-		RenderModal: ( { items } ) => (
-			<VStack spacing={ 4 }>
-				<ThreatsDetailCard threats={ items } />
-				<ThreatDescription threat={ items[ 0 ] } site={ site } />
-			</VStack>
-		),
+		RenderModal: ( props ) => <ViewDetailsModal { ...props } site={ site } />,
 	};
 }
