c6c128c90aa56889d5d9642924000f5869dd2bb3
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-09-30 11:56:20 +0300

    Enhance failure conditions in Calypso E2E Tests Build Template to support test retries and enforce metrics for test pass rates

--- .teamcity/_self/CalypsoE2ETestsBuildTemplate.kt
+++ .teamcity/_self/CalypsoE2ETestsBuildTemplate.kt
@@ -153,8 +153,28 @@ object CalypsoE2ETestsBuildTemplate : Template({
 		}
   }
 
-  artifactRules = """
+  	artifactRules = """
 		test/e2e/output => %PROJECT%/output
 		test/e2e/blob-report => blob-report
 	""".trimIndent()
+  
+  	failureConditions {
+		executionTimeoutMin = 20
+		// Don't fail if the runner exists with a non zero code. This allows a build to pass if the failed tests have been muted previously.
+		nonZeroExitCode = false
+
+		// Support retries using the --onlyFailures flag in Jest.
+		supportTestRetry = true
+
+		// Fail if the number of passing tests is 50% or less than the last build. This will catch the case where the test runner crashes and no tests are run.
+		failOnMetricChange {
+			metric = BuildFailureOnMetric.MetricType.PASSED_TEST_COUNT
+			threshold = 50
+			units = BuildFailureOnMetric.MetricUnit.PERCENTS
+			comparison = BuildFailureOnMetric.MetricComparison.LESS
+			compareTo = build {
+				buildRule = lastSuccessful()
+			}
+		}
+	}
 })
