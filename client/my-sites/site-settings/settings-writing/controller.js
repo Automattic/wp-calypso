437ed1f07e635750e5ba1ee43115849aed750da0
Richard Ortiz <rcrd.ortiz@gmail.com>
2025-09-30 16:23:41 +0300

    Calypso untangling cleanup, removed writing settings

--- client/my-sites/site-settings/settings-writing/controller.js
+++
@@ -1,7 +0,0 @@
-import { createElement } from 'react';
-import WritingMain from 'calypso/my-sites/site-settings/settings-writing/main';
-
-export function writing( context, next ) {
-	context.primary = createElement( WritingMain );
-	next();
-}
--- client/my-sites/site-settings/settings-writing/index.js
+++ client/my-sites/site-settings/settings-writing/index.js
@@ -1,12 +1,10 @@
 import page from '@automattic/calypso-router';
 import {
 	makeLayout,
-	render as clientRender,
 	redirectIfDuplicatedView as _redirectIfDuplicatedView,
+	render as clientRender,
 } from 'calypso/controller';
-import { navigation, siteSelection, sites } from 'calypso/my-sites/controller';
-import { siteSettings } from 'calypso/my-sites/site-settings/settings-controller';
-import { writing } from './controller';
+import { sites, siteSelection } from 'calypso/my-sites/controller';
 
 const redirectIfDuplicatedView = ( context, next ) => {
 	_redirectIfDuplicatedView( `edit-tags.php?taxonomy=${ context.params.taxonomy }` )(
@@ -19,12 +17,7 @@ export default function () {
 	page(
 		'/settings/writing/:site_id',
 		siteSelection,
-		_redirectIfDuplicatedView( 'options-writing.php' ),
-		navigation,
-		siteSettings,
-		writing,
-		makeLayout,
-		clientRender
+		_redirectIfDuplicatedView( 'options-writing.php' )
 	);
 
 	page( '/settings/taxonomies/:taxonomy', siteSelection, sites, makeLayout, clientRender );
