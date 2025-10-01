f6ad2803cf67a2b5c68dd5f978daf49988168743
Bongnam <dufresnesteven@gmail.com>
2025-10-01 19:29:08 +0900

    Update to pass the fill on the svg itself.

--- client/dashboard/sites/performance/core-metrics-chart.tsx
+++ client/dashboard/sites/performance/core-metrics-chart.tsx
@@ -4,21 +4,24 @@ import { __, sprintf } from '@wordpress/i18n';
 import { Text } from '../../components/text';
 import { Valuation, getColorForStatus } from '../../utils/site-performance';
 
-const StatusIndicator = ( { speed }: { speed: Valuation } ) => {
+const StatusIndicator = ( { valuation }: { valuation: Valuation } ) => {
 	const innerSvg: Record< Valuation, React.ReactNode > = {
-		good: <rect x="1" y="1" width="10" height="10" rx="5" fill={ getColorForStatus( 'good' ) } />,
+		good: <rect x="1" y="1" width="10" height="10" rx="5" />,
 		needsImprovement: (
-			<path
-				d="M5.56292 0.786741C5.75342 0.443836 6.24658 0.443837 6.43708 0.786742L11.5873 10.0572C11.7725 10.3904 11.5315 10.8 11.1502 10.8H0.849757C0.468515 10.8 0.227531 10.3904 0.412679 10.0572L5.56292 0.786741Z"
-				fill={ getColorForStatus( 'needsImprovement' ) }
-			/>
+			<path d="M5.56292 0.786741C5.75342 0.443836 6.24658 0.443837 6.43708 0.786742L11.5873 10.0572C11.7725 10.3904 11.5315 10.8 11.1502 10.8H0.849757C0.468515 10.8 0.227531 10.3904 0.412679 10.0572L5.56292 0.786741Z" />
 		),
-		bad: <rect x="1" y="1" width="10" height="10" rx="2" fill={ getColorForStatus( 'bad' ) } />,
+		bad: <rect x="1" y="1" width="10" height="10" rx="2" />,
 	};
 
 	return (
-		<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-			{ innerSvg[ speed ] }
+		<svg
+			width="12"
+			height="12"
+			viewBox="0 0 12 12"
+			fill={ getColorForStatus( valuation ) }
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			{ innerSvg[ valuation ] }
 		</svg>
 	);
 };
@@ -34,7 +37,7 @@ const MetricLabel = ( {
 } ) => {
 	return (
 		<HStack justify="flex-start" alignment="center">
-			<StatusIndicator speed={ indicator } />
+			<StatusIndicator valuation={ indicator } />
 			<Text size="small">{ label }</Text>
 			<Text>{ value }</Text>
 		</HStack>
