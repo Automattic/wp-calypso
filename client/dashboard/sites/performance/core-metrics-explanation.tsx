1512b68e4c8cfe2193daea9a30e4c451ace8b26d
Bongnam <dufresnesteven@gmail.com>
2025-10-01 19:44:32 +0900

    Switch to external link.

--- client/dashboard/sites/performance/core-metrics-explanation.tsx
+++ client/dashboard/sites/performance/core-metrics-explanation.tsx
@@ -1,5 +1,6 @@
 import { Metrics } from '@automattic/api-core';
 import { localizeUrl } from '@automattic/i18n-utils';
+import { ExternalLink } from '@wordpress/components';
 import { createInterpolateElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { Text } from '../../components/text';
@@ -9,17 +10,17 @@ const metricsExplanations = {
 		explanation: createInterpolateElement(
 			/* translators: %(learnMore)s is the learn more link */
 			__(
-				'First Contentful Paint reflects the time it takes to display the first text or image to visitors. The best sites load in under 1.8 seconds. <learnMore>Learn more ↗</learnMore>'
+				'First Contentful Paint reflects the time it takes to display the first text or image to visitors. The best sites load in under 1.8 seconds. <learnMore>Learn more</learnMore>'
 			),
 			{
 				learnMore: (
-					<a
+					<ExternalLink
 						href={ localizeUrl(
 							'https://developer.wordpress.com/docs/site-performance/speed-test/#first-contentful-paint-fcp-'
 						) }
-						target="_blank"
-						rel="noreferrer"
-					/>
+					>
+						{ __( 'Learn more' ) }
+					</ExternalLink>
 				),
 			}
 		),
@@ -28,17 +29,17 @@ const metricsExplanations = {
 		explanation: createInterpolateElement(
 			/* translators: %(learnMore)s is the learn more link */
 			__(
-				'Largest Contentful Paint measures the time it takes for the largest visible element (like an image or text block) on a page to load. The best sites load in under 2.5 seconds. <learnMore>Learn more ↗</learnMore>'
+				'Largest Contentful Paint measures the time it takes for the largest visible element (like an image or text block) on a page to load. The best sites load in under 2.5 seconds. <learnMore>Learn more</learnMore>'
 			),
 			{
 				learnMore: (
-					<a
+					<ExternalLink
 						href={ localizeUrl(
 							'https://developer.wordpress.com/docs/site-performance/speed-test/#largest-contentful-paint-lcp-'
 						) }
-						target="_blank"
-						rel="noreferrer"
-					/>
+					>
+						{ __( 'Learn more' ) }
+					</ExternalLink>
 				),
 			}
 		),
@@ -47,17 +48,17 @@ const metricsExplanations = {
 		explanation: createInterpolateElement(
 			/* translators: %(learnMore)s is the learn more link */
 			__(
-				'Cumulative Layout Shift is assessed by measuring how often content moves unexpectedly during loading. The best sites have a score of 0.1 or lower. <learnMore>Learn more ↗</learnMore>'
+				'Cumulative Layout Shift is assessed by measuring how often content moves unexpectedly during loading. The best sites have a score of 0.1 or lower. <learnMore>Learn more</learnMore>'
 			),
 			{
 				learnMore: (
-					<a
+					<ExternalLink
 						href={ localizeUrl(
 							'https://developer.wordpress.com/docs/site-performance/speed-test/#cumulative-layout-shift-cls-'
 						) }
-						target="_blank"
-						rel="noreferrer"
-					/>
+					>
+						{ __( 'Learn more' ) }
+					</ExternalLink>
 				),
 			}
 		),
@@ -66,17 +67,17 @@ const metricsExplanations = {
 		explanation: createInterpolateElement(
 			/* translators: %(learnMore)s is the learn more link */
 			__(
-				'Interaction to Next Paint measures the overall responsiveness of a webpage by evaluating how quickly it reacts to user interactions. A good score is 200 milliseconds or less, indicating that the page responds swiftly to user inputs. <learnMore>Learn more ↗</learnMore>'
+				'Interaction to Next Paint measures the overall responsiveness of a webpage by evaluating how quickly it reacts to user interactions. A good score is 200 milliseconds or less, indicating that the page responds swiftly to user inputs. <learnMore>Learn more</learnMore>'
 			),
 			{
 				learnMore: (
-					<a
+					<ExternalLink
 						href={ localizeUrl(
 							'https://developer.wordpress.com/docs/site-performance/speed-test/#interaction-to-next-paint-inp-'
 						) }
-						target="_blank"
-						rel="noreferrer"
-					/>
+					>
+						{ __( 'Learn more' ) }
+					</ExternalLink>
 				),
 			}
 		),
@@ -85,17 +86,17 @@ const metricsExplanations = {
 		explanation: createInterpolateElement(
 			/* translators: %(learnMore)s is the learn more link */
 			__(
-				'Time to First Byte reflects the time taken for a user‘s browser to receive the first byte of data from the server after making a request. The best sites load around 800 milliseconds or less. <learnMore>Learn more ↗</learnMore>'
+				'Time to First Byte reflects the time taken for a user‘s browser to receive the first byte of data from the server after making a request. The best sites load around 800 milliseconds or less. <learnMore>Learn more</learnMore>'
 			),
 			{
 				learnMore: (
-					<a
+					<ExternalLink
 						href={ localizeUrl(
 							'https://developer.wordpress.com/docs/site-performance/speed-test/#time-to-first-byte-ttfb-'
 						) }
-						target="_blank"
-						rel="noreferrer"
-					/>
+					>
+						{ __( 'Learn more' ) }
+					</ExternalLink>
 				),
 			}
 		),
@@ -104,17 +105,17 @@ const metricsExplanations = {
 		explanation: createInterpolateElement(
 			/* translators: %(learnMore)s is the learn more link */
 			__(
-				'Total Blocking Time measures the total amount of time that a page is blocked from responding to user input, such as mouse clicks, screen taps, or keyboard presses. The best sites have a wait time of less than 200 milliseconds. <learnMore>Learn more ↗</learnMore>'
+				'Total Blocking Time measures the total amount of time that a page is blocked from responding to user input, such as mouse clicks, screen taps, or keyboard presses. The best sites have a wait time of less than 200 milliseconds. <learnMore>Learn more</learnMore>'
 			),
 			{
 				learnMore: (
-					<a
+					<ExternalLink
 						href={ localizeUrl(
 							'https://developer.wordpress.com/docs/site-performance/speed-test/#total-blocking-time-tbt-'
 						) }
-						target="_blank"
-						rel="noreferrer"
-					/>
+					>
+						{ __( 'Learn more' ) }
+					</ExternalLink>
 				),
 			}
 		),
@@ -123,17 +124,17 @@ const metricsExplanations = {
 		explanation: createInterpolateElement(
 			/* translators: %(learnMore)s is the learn more link */
 			__(
-				'The performance score is a combined representation of your site‘s individual speed metrics. <learnMore>Learn more ↗</learnMore>'
+				'The performance score is a combined representation of your site‘s individual speed metrics. <learnMore>Learn more</learnMore>'
 			),
 			{
 				learnMore: (
-					<a
+					<ExternalLink
 						href={ localizeUrl(
 							'https://developer.wordpress.com/docs/site-performance/speed-test/#performance-score'
 						) }
-						target="_blank"
-						rel="noreferrer"
-					/>
+					>
+						{ __( 'Learn more' ) }
+					</ExternalLink>
 				),
 			}
 		),
