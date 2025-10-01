84d0c4d8aa8ef58b6cf376cf337afb748b3e4c3d
Bongnam <dufresnesteven@gmail.com>
2025-10-01 20:52:01 +0900

    Change footer from div to p

--- client/dashboard/sites/performance/report-footer.tsx
+++ client/dashboard/sites/performance/report-footer.tsx
@@ -5,17 +5,17 @@ import { Text } from '../../components/text';
 export default function ReportFooter() {
 	return (
 		<VStack spacing={ 4 }>
-			<Text as="div">
+			<Text as="p">
 				{ __(
 					'The historical performance data and metrics presented in this site are sourced from the Google Chrome User Experience Report (CrUX) dataset, which reflects real-world user experiences and interactions with your site. Realtime data is provided by PageSpeed Insights. This data helps us provide actionable recommendations to improve your site‘s performance.'
 				) }
 			</Text>
-			<Text as="div">
+			<Text as="p">
 				{ __(
 					'While we strive to provide accurate and helpful insights, please note that performance improvements are dependent on various factors, including your current setup and specific use case. Our recommendations aim to guide you towards potential enhancements, but results may vary.'
 				) }
 			</Text>
-			<Text as="div">
+			<Text as="p">
 				<a
 					href="https://developer.wordpress.com/docs/site-performance/speed-test/#accessing-the-speed-test-tool"
 					target="_blank"
