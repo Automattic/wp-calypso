5f988f0cca6e412e9c28ac0648f9fd1ddad7d4bd
Kateryna Kodonenko <kateryna@automattic.com>
2025-10-01 11:20:38 +0200

    Add repositories to settings page

--- client/dashboard/sites/settings/index.tsx
+++ client/dashboard/sites/settings/index.tsx
@@ -14,6 +14,7 @@ import HundredYearPlanSettingsSummary from '../settings-hundred-year-plan/summar
 import McpSettingsSummary from '../settings-mcp/summary';
 import PHPSettingsSummary from '../settings-php/summary';
 import PrimaryDataCenterSettingsSummary from '../settings-primary-data-center/summary';
+import RepositoriesSettingsSummary from '../settings-repositories/summary';
 import SftpSshSettingsSummary from '../settings-sftp-ssh/summary';
 import SiteVisibilitySettingsSummary from '../settings-site-visibility/summary';
 import StaticFile404SettingsSummary from '../settings-static-file-404/summary';
@@ -45,6 +46,7 @@ export default function SiteSettings( { siteSlug }: { siteSlug: string } ) {
 					<WordPressSettingsSummary site={ site } />
 					<PHPSettingsSummary site={ site } />
 					<SftpSshSettingsSummary site={ site } />
+					<RepositoriesSettingsSummary site={ site } />
 					<DatabaseSettingsSummary site={ site } />
 					<PrimaryDataCenterSettingsSummary site={ site } />
 					<StaticFile404SettingsSummary site={ site } />
