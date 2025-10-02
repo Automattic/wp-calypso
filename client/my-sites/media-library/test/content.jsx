4b213fcae3025fe00fc2ec7e42c9c509a785ec94
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-10-02 11:28:43 +0200

    Fix tests

--- client/my-sites/media-library/test/content.jsx
+++ client/my-sites/media-library/test/content.jsx
@@ -11,8 +11,6 @@ import { renderWithProvider } from 'calypso/test-helpers/testing-library';
 const render = ( el, options ) =>
 	renderWithProvider( el, { ...options, reducers: { ui: uiReducer, media: mediaReducer } } );
 
-jest.mock( 'calypso/sites/marketing/connections/inline-connection', () => () => null );
-
 const googleConnection = {
 	service: 'google_photos',
 	status: 'ok',
