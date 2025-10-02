e234e8912626b573f985cd96ab36648c39900d9c
Omar Alshaker <omar@omaralshaker.com>
2025-10-02 11:04:34 +0300

    Remove unneeded fragment

--- packages/odie-client/src/components/message/messages-cluster/messages-cluster.tsx
+++ packages/odie-client/src/components/message/messages-cluster/messages-cluster.tsx
@@ -125,14 +125,12 @@ export function MessagesClusterizer( { messages }: { messages: Message[] } ) {
 					className={ cx( 'odie-chatbox-messages-cluster', `role-${ group.role }` ) }
 				>
 					{ group.messages.map( ( message, index ) => (
-						<>
-							<ChatMessage
-								key={ message.message_id || index }
-								message={ message }
-								currentUser={ currentUser }
-								header={ index === 0 ? messageHeader() : undefined }
-							/>
-						</>
+						<ChatMessage
+							key={ message.message_id || index }
+							message={ message }
+							currentUser={ currentUser }
+							header={ index === 0 ? messageHeader() : undefined }
+						/>
 					) ) }
 				</div>
 				{ startingHumanSupport && (
