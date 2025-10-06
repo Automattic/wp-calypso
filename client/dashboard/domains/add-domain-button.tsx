758084015174cdc61ec9876f3802005792600f63
Igor Giussani <gius80@gmail.com>
2025-10-06 15:44:10 +0200

    Create AddDomainButton component

---
+++ client/dashboard/domains/add-domain-button.tsx
@@ -0,0 +1,52 @@
+import { Button, Dropdown } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { search, globe, chevronUp, chevronDown } from '@wordpress/icons';
+import { addQueryArgs } from '@wordpress/url';
+import RouterLinkMenuItem from '../components/router-link-menu-item';
+
+export function AddDomainButton( { siteSlug }: { siteSlug?: string } ) {
+	return (
+		<Dropdown
+			renderToggle={ ( { isOpen, onToggle } ) => (
+				<Button
+					icon={ isOpen ? chevronUp : chevronDown }
+					iconPosition="right"
+					variant="primary"
+					__next40pxDefaultSize
+					onClick={ onToggle }
+					aria-expanded={ isOpen }
+				>
+					{ __( 'Add domain name' ) }
+				</Button>
+			) }
+			renderContent={ () => (
+				<>
+					<RouterLinkMenuItem
+						to={
+							siteSlug
+								? addQueryArgs( '/setup/domain', {
+										siteSlug,
+								  } )
+								: '/start/domain'
+						}
+						iconPosition="left"
+						icon={ search }
+					>
+						{ __( 'Search domain names' ) }
+					</RouterLinkMenuItem>
+					<RouterLinkMenuItem
+						to={
+							siteSlug
+								? addQueryArgs( '/setup/domain/use-my-domain', { siteSlug } )
+								: '/setup/domain-transfer'
+						}
+						iconPosition="left"
+						icon={ globe }
+					>
+						{ __( 'Use a domain I own' ) }
+					</RouterLinkMenuItem>
+				</>
+			) }
+		/>
+	);
+}
