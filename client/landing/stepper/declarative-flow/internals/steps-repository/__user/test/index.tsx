1ff16f85760b87c4cfe00cef6b05669432ee3e4d
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-10-06 16:59:14 -0300

    Fix tests

--- client/landing/stepper/declarative-flow/internals/steps-repository/__user/test/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/__user/test/index.tsx
@@ -1,39 +1,30 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
+import { screen } from '@testing-library/dom';
 import { MemoryRouter } from 'react-router-dom';
+import loginReducer from 'calypso/state/login/reducer';
+import routeReducer from 'calypso/state/route/reducer';
 import { renderWithProvider } from 'calypso/test-helpers/testing-library';
 import UserStep from '..';
-
-jest.mock( 'calypso/blocks/signup-form/signup-form-social-first', () => {
-	return {
-		__esModule: true,
-		default: ( props: any ) => (
-			<div data-testid="signup-form" data-user-email={ props.userEmail } />
-		),
-	};
-} );
 
 describe( 'User email signup step', () => {
 	const renderUserStep = ( url = '/onboarding/user?user_email=test@example.com' ) => {
 		return renderWithProvider(
 			<MemoryRouter initialEntries={ [ url ] }>
-				<UserStep flow="onboarding" stepName="user" navigation={ {} } />
+				<UserStep flow="onboarding" stepName="user" navigation={ { submit: jest.fn() } } />
 			</MemoryRouter>,
-			{ initialState: { currentUser: {} } }
+			{ reducers: { login: loginReducer, route: routeReducer } }
 		);
 	};
 
 	it( 'passes userEmail from user_email query param to SignupFormSocialFirst', () => {
-		const { getByTestId } = renderUserStep( '/onboarding/user?user_email=hello@wp.com' );
-		const node = getByTestId( 'signup-form' );
-		expect( node.getAttribute( 'data-user-email' ) ).toBe( 'hello@wp.com' );
+		renderUserStep( '/onboarding/user?user_email=hello@wp.com' );
+		expect( screen.getByLabelText( 'Enter your email' ) ).toHaveValue( 'hello@wp.com' );
 	} );
 
 	it( 'defaults userEmail to empty string when user_email is missing', () => {
-		const { getByTestId } = renderUserStep( '/onboarding/user' );
-		const node = getByTestId( 'signup-form' );
-		expect( node.getAttribute( 'data-user-email' ) ).toBe( '' );
+		renderUserStep( '/onboarding/user' );
+		expect( screen.getByLabelText( 'Enter your email' ) ).toHaveValue( '' );
 	} );
 } );
