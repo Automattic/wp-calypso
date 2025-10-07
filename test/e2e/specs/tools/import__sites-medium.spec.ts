762a0f8fdc90642065c876223e9a01282e5f376c
Alister Scott <alister.scott@gmail.com>
2025-10-07 11:02:52 +1000

    Use a new site for each importer test

--- test/e2e/specs/tools/import__sites-medium.spec.ts
+++ test/e2e/specs/tools/import__sites-medium.spec.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-import { Page } from 'playwright';
 import { expect, tags, test } from '../../lib/pw-base';
 
 const TEST_MEDIUM_EXPORT_FILE_PATH = path.join( __dirname, 'medium-export-example.zip' );
@@ -11,24 +10,18 @@ test.describe(
 		annotation: { type: 'flowchart', description: 'https://flowchart.fun/p/envious-tent-cost' },
 	},
 	() => {
-		test.describe.configure( { mode: 'serial' } ); // Since all tests use the same account you can only run a single import at a time
-		test( 'As a WordPress.com free plan user with a simple site, I can use the wp-admin Importers List Medium link to import my content from my medium.com account', async ( {
-			accountSimpleSiteFreePlan,
-			page,
+		test( 'As a New WordPress.com free plan user with a simple site, I can use the wp-admin Importers List Medium link to import my content from my medium.com account', async ( {
 			helperData,
+			page,
+			sitePublic,
 		}, workerInfo ) => {
 			test.skip( workerInfo.project.name !== 'chrome', 'The import tests only run in Chrome' );
 
-			await test.step( `Given I am authenticated as '${ accountSimpleSiteFreePlan.accountName }'`, async function () {
-				await accountSimpleSiteFreePlan.authenticate( page );
-			} );
-
 			await test.step( 'When I visit the Medium importer as coming from the wp-admin Tools > Import page', async function () {
-				await mockImportEndpoint( page );
 				await page.goto(
 					helperData.getCalypsoURL( 'setup/site-setup/importerMedium', {
 						ref: 'wp-admin-importers-list-direct-importer',
-						siteSlug: accountSimpleSiteFreePlan.getSiteURL( { protocol: false } ),
+						siteSlug: sitePublic.blog_details.site_slug,
 						isUploadInProgress: 'false',
 					} )
 				);
@@ -38,7 +31,6 @@ test.describe(
 				await expect(
 					page.getByRole( 'heading', { name: 'Import content from Medium' } )
 				).toBeVisible();
-				await unMockImportEndpoint( page );
 			} );
 
 			await test.step( 'When I upload a valid Medium export file', async function () {
@@ -57,24 +49,20 @@ test.describe(
 			} );
 		} );
 
-		test( 'As a WordPress.com free plan user with a simple site, I can use the wp-admin Importers List WordPress.com link to import my content from my medium.com account', async ( {
-			accountSimpleSiteFreePlan,
-			page,
+		test( 'As a New WordPress.com free plan user with a simple site, I can use the wp-admin Importers List WordPress.com link to import my content from my medium.com account', async ( {
 			helperData,
+			page,
+			sitePublic,
 		}, workerInfo ) => {
 			test.skip( workerInfo.project.name !== 'chrome', 'The import tests only run in Chrome' );
 
 			const mediumSiteURL = 'https://medium.com/@testacount';
 
-			await test.step( `Given I am authenticated as '${ accountSimpleSiteFreePlan.accountName }'`, async function () {
-				await accountSimpleSiteFreePlan.authenticate( page );
-			} );
-
 			await test.step( 'When I visit the "Let\'s find your site" page as coming from the wp-admin Tools > Import page', async function () {
 				await page.goto(
 					helperData.getCalypsoURL( 'setup/site-migration/site-migration-identify', {
 						hide_importer_link: 'true',
-						siteSlug: accountSimpleSiteFreePlan.getSiteURL( { protocol: false } ),
+						siteSlug: sitePublic.blog_details.site_slug,
 						isUploadInProgress: 'false',
 					} )
 				);
@@ -85,7 +73,6 @@ test.describe(
 			} );
 
 			await test.step( 'When I enter my Medium site URL and click Continue', async function () {
-				await mockImportEndpoint( page );
 				await page
 					.getByRole( 'textbox', { name: 'Enter your site address:' } )
 					.fill( mediumSiteURL );
@@ -96,7 +83,6 @@ test.describe(
 				await expect(
 					page.getByRole( 'heading', { name: 'Import content from Medium' } )
 				).toBeVisible();
-				await unMockImportEndpoint( page );
 			} );
 
 			await test.step( 'When I upload a valid Medium export file', async function () {
@@ -115,58 +101,7 @@ test.describe(
 			} );
 		} );
 
-		test( 'As a WordPress.com free plan user with a simple site, I can use the Calypso "Import Content" page to import my content from my medium.com account', async ( {
-			accountSimpleSiteFreePlan,
-			page,
-			helperData,
-		}, workerInfo ) => {
-			test.skip( workerInfo.project.name !== 'chrome', 'The import tests only run in Chrome' );
-
-			await test.step( `Given I am authenticated as '${ accountSimpleSiteFreePlan.accountName }'`, async function () {
-				await accountSimpleSiteFreePlan.authenticate( page );
-			} );
-
-			await test.step( 'When I visit the "Import Content" page for my site', async function () {
-				await mockImportEndpoint( page );
-				await page.goto(
-					helperData.getCalypsoURL(
-						`import/${ accountSimpleSiteFreePlan.getSiteURL( { protocol: false } ) }`
-					)
-				);
-			} );
-
-			await test.step( 'Then I see the "Import Content" Calypso page with the Medium import option', async function () {
-				await expect( page.getByRole( 'heading', { name: 'Import Content' } ) ).toBeVisible();
-				await expect( page.getByRole( 'button', { name: 'Medium' } ) ).toBeVisible();
-			} );
-
-			await test.step( 'When I choose the Medium importer', async function () {
-				await page.getByRole( 'button', { name: 'Medium' } ).click();
-			} );
-
-			await test.step( 'Then I see the Import content from Medium page', async function () {
-				await expect( page.getByRole( 'heading', { name: 'Import Content' } ) ).toBeVisible();
-				await expect( page.getByRole( 'heading', { name: 'Medium' } ) ).toBeVisible();
-				await unMockImportEndpoint( page );
-			} );
-
-			await test.step( 'When I upload a valid Medium export file', async function () {
-				await page
-					.locator( 'input[type="file"][name="exportFile"]' )
-					.setInputFiles( TEST_MEDIUM_EXPORT_FILE_PATH );
-			} );
-
-			await test.step( 'Then I see an Import confirmation page showing the authorship of the content to be imported', async function () {
-				await expect( page.getByRole( 'heading', { name: 'Import content' } ) ).toBeVisible();
-				await expect( page.getByText( 'Your file is ready to be imported' ) ).toBeVisible( {
-					timeout: 30000,
-				} );
-				await expect( page.getByRole( 'button', { name: 'Import' } ) ).toBeVisible();
-				await expect( page.getByRole( 'button', { name: 'Import' } ) ).toBeEnabled();
-			} );
-		} );
-
-		test( 'As a NEW WordPress.com free plan user with a simple site, I can use the Calypso "Import Content" page to import my content from my medium.com account', async ( {
+		test( 'As a New WordPress.com free plan user with a simple site, I can use the Calypso "Import Content" page to import my content from my medium.com account', async ( {
 			sitePublic,
 			page,
 			helperData,
@@ -210,16 +145,3 @@ test.describe(
 		} );
 	}
 );
-async function mockImportEndpoint( page: Page ) {
-	await page.route( /rest\/v1\.1\/sites\/\d+\/imports\//, async ( route ) => {
-		await route.fulfill( {
-			status: 200,
-			contentType: 'application/json',
-			body: '{}',
-		} );
-	} );
-}
-
-async function unMockImportEndpoint( page: Page ) {
-	await page.unroute( /rest\/v1\.1\/sites\/\d+\/imports\// );
-}
