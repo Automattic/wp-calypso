df57ee1e45fde9bac245f39a2f5bb288def5b122
Kamen Stanev <hambai@gmail.com>
2025-10-08 21:46:01 +0300

    Do not show the SSL status for transfers and also do not make unnecessary requests

--- client/dashboard/domains/dataviews/field-ssl.tsx
+++ client/dashboard/domains/dataviews/field-ssl.tsx
@@ -1,13 +1,26 @@
-import { DomainSummary } from '@automattic/api-core';
+import { DomainSubtype, DomainSummary } from '@automattic/api-core';
 import { sslDetailsQuery } from '@automattic/api-queries';
 import { Badge } from '@automattic/ui';
 import { useQuery } from '@tanstack/react-query';
 import { __ } from '@wordpress/i18n';
 
 export const DomainSslField = ( { domain }: { domain: DomainSummary } ) => {
-	const { data: sslDetails } = useQuery( sslDetailsQuery( domain.domain ) );
+	const { data: sslDetails } = useQuery( {
+		...sslDetailsQuery( domain.domain ),
+		enabled:
+			domain.subtype.id !== DomainSubtype.DOMAIN_TRANSFER &&
+			domain.subtype.id !== DomainSubtype.DEFAULT_ADDRESS,
+	} );
 
-	if ( sslDetails?.certificate_provisioned || domain.wpcom_domain ) {
+	if ( domain.subtype.id === DomainSubtype.DOMAIN_TRANSFER ) {
+		// TODO: only show this if there is no active domain connection for the domain transfer
+		return '-';
+	}
+
+	if (
+		domain.subtype.id === DomainSubtype.DEFAULT_ADDRESS ||
+		sslDetails?.certificate_provisioned
+	) {
 		return <Badge intent="success">{ __( 'SSL active' ) }</Badge>;
 	}
 
