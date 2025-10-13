2950bf7a86f8883a7254b4e3a5edce1ddb5afc3a
Philip Jackson <p-jackson@live.com>
2025-10-13 17:47:33 +1300

    Implement log out in v2 hosting dashboard

--- client/dashboard/app/auth/index.tsx
+++ client/dashboard/app/auth/index.tsx
@@ -1,12 +1,23 @@
 import { fetchUser } from '@automattic/api-core';
+import { clearQueryClient, disablePersistQueryClient } from '@automattic/api-queries';
+import config from '@automattic/calypso-config';
+import { magnificentNonEnLocales } from '@automattic/i18n-utils';
 import { useQuery } from '@tanstack/react-query';
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useMemo } from 'react';
 import type { User } from '@automattic/api-core';
 
 export const AUTH_QUERY_KEY = [ 'auth', 'user' ];
 
 interface AuthContextType {
 	user: User;
+
+	// Navigate here to log user out.
+	// Different from `user.logout_URL` which may not be appropriate for WP desktop
+	// app or have a redirect URL set.
+	logoutUrl: string;
+
+	// Perform cleanup of user data before navigating to `logoutUrl`.
+	handleLogout: () => void;
 }
 export const AuthContext = createContext< AuthContextType | undefined >( undefined );
 
@@ -32,6 +43,40 @@ export function AuthProvider( { children }: { children: React.ReactNode } ) {
 		},
 	} );
 
+	const value = useMemo( () => {
+		if ( ! user ) {
+			return undefined;
+		}
+
+		let logoutUrl = '';
+
+		// If logout_URL isn't set, then go ahead and return the logout URL
+		// without a proper nonce as a fallback.
+		// Note: we never want to use logout_URL in the desktop app
+		if ( ! user.logout_URL || config.isEnabled( 'always_use_logout_url' ) ) {
+			// Use localized version of the homepage in the redirect
+			let subdomain = '';
+			if ( magnificentNonEnLocales.includes( user.language ) ) {
+				subdomain = user.language + '.';
+			}
+
+			logoutUrl = ( config( 'logout_url' ) as string ).replace( '|subdomain|', subdomain );
+		} else {
+			logoutUrl = user.logout_URL;
+		}
+
+		return {
+			user,
+			logoutUrl,
+			handleLogout: () => {
+				// Both functions run synchronously so will complete
+				// before navigation occurs.
+				disablePersistQueryClient();
+				clearQueryClient();
+			},
+		};
+	}, [ user ] );
+
 	if ( userIsError ) {
 		if ( typeof window !== 'undefined' ) {
 			const currentPath = window.location.pathname;
@@ -45,7 +90,7 @@ export function AuthProvider( { children }: { children: React.ReactNode } ) {
 		return null;
 	}
 
-	return <AuthContext.Provider value={ { user } }>{ children }</AuthContext.Provider>;
+	return <AuthContext.Provider value={ value }>{ children }</AuthContext.Provider>;
 }
 
 /**
