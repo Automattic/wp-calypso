d74af8e5cd1d26399cd9d877c68b8685bdebf2b8
Claudiu Filip <claudiu.filip@automattic.com>
2025-10-13 15:47:34 +0300

    Show Flex site creation button for eligible users

--- client/components/sites-add-new-site/index.tsx
+++ client/components/sites-add-new-site/index.tsx
@@ -1,7 +1,13 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { Dropdown } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
+<<<<<<< HEAD
 import { AuthProvider } from '../../dashboard/app/auth';
+=======
+import { useSelector } from 'calypso/state';
+import { getCurrentUser } from 'calypso/state/current-user/selectors';
+import { AuthContext } from '../../dashboard/app/auth';
+>>>>>>> 185a5a577d7 (Show Flex site creation button for eligible users)
 import { AsyncContent } from './async';
 import AddNewSiteButton from './button';
 import type { AddNewSiteProps } from '../../dashboard/sites/add-new-site/types';
@@ -15,6 +21,7 @@ interface Props extends PropsWithChildren {
 
 export const SitesAddNewSitePopover = ( { showCompact, context }: Props ) => {
 	const translate = useTranslate();
+	const user = useSelector( getCurrentUser ) as any;
 
 	return (
 		<AuthProvider>
