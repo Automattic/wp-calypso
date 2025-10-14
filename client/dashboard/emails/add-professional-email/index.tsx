bf893c0beb1171642de54101fb3d1c74b7ff22b8
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-10-14 13:50:44 +0200

    Add card & buttons

--- client/dashboard/emails/add-professional-email/index.tsx
+++ client/dashboard/emails/add-professional-email/index.tsx
@@ -1,8 +1,28 @@
+import { Button, Card, CardBody } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { ButtonStack } from '../../components/button-stack';
+import { PageHeader } from '../../components/page-header';
+import PageLayout from '../../components/page-layout';
+
 const AddProfessionalEmail = () => {
 	return (
-		<div>
-			<h1>Add Professional Email</h1>
-		</div>
+		<PageLayout header={ <PageHeader /> } size="small">
+			<Card>
+				<CardBody>.</CardBody>
+			</Card>
+
+			<ButtonStack justify="flex-start">
+				<Button __next40pxDefaultSize variant="secondary" onClick={ () => {} }>
+					{ __( 'Add another mailbox' ) }
+				</Button>
+			</ButtonStack>
+
+			<ButtonStack justify="flex-start">
+				<Button __next40pxDefaultSize variant="primary" onClick={ () => {} }>
+					{ __( 'Continue' ) }
+				</Button>
+			</ButtonStack>
+		</PageLayout>
 	);
 };
 
