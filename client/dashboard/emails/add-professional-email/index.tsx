d250c3dc34b41b72da4debb8888f3a04b793eb1f
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-10-14 14:07:48 +0200

    Get the interval from the URL search

--- client/dashboard/emails/add-professional-email/index.tsx
+++ client/dashboard/emails/add-professional-email/index.tsx
@@ -1,13 +1,21 @@
+import { useRouter } from '@tanstack/react-router';
 import { Button, Card, CardBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useState } from 'react';
 import { ButtonStack } from '../../components/button-stack';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
+import { IntervalLength } from '../types';
 import { MailboxForm } from './components/mailbox-form';
 
 const AddProfessionalEmail = () => {
+	const router = useRouter();
 	const [ isSubmitting, setIsSubmitting ] = useState( false );
+
+	let interval: IntervalLength = router.state.location.search.interval;
+	if ( interval !== 'monthly' && interval !== 'annually' ) {
+		interval = 'annually';
+	}
 
 	const handleSubmit = async () => {
 		setIsSubmitting( true );
