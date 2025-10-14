e1e7afda9654dc2d88ed05ab20b1a854ea92e764
Igor Giussani <gius80@gmail.com>
2025-10-14 14:50:36 +0200

    Add site redirect mutation

--- packages/api-queries/src/site-redirect.ts
+++ packages/api-queries/src/site-redirect.ts
@@ -1,8 +1,19 @@
-import { fetchSiteRedirect } from '@automattic/api-core';
-import { queryOptions } from '@tanstack/react-query';
+import { fetchSiteRedirect, updateSiteRedirect } from '@automattic/api-core';
+import { mutationOptions, queryOptions } from '@tanstack/react-query';
+import { queryClient } from './query-client';
 
 export const siteRedirectQuery = ( siteId: number ) =>
 	queryOptions( {
 		queryKey: [ 'site', siteId, 'redirect' ],
 		queryFn: () => fetchSiteRedirect( siteId ),
+	} );
+
+export const updateSiteRedirectMutation = ( siteId: number, location: string ) =>
+	mutationOptions( {
+		mutationFn: () => updateSiteRedirect( siteId, location ),
+		onSuccess: () => {
+			queryClient.invalidateQueries( {
+				queryKey: [ 'site', siteId, 'redirect' ],
+			} );
+		},
 	} );
