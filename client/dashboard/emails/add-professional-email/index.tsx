85e5816f2f8eec10602ebce2ff4680cc3293cee6
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-10-14 14:06:06 +0200

    Disable on submit

--- client/dashboard/emails/add-professional-email/index.tsx
+++ client/dashboard/emails/add-professional-email/index.tsx
@@ -1,27 +1,44 @@
 import { Button, Card, CardBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { useState } from 'react';
 import { ButtonStack } from '../../components/button-stack';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
 import { MailboxForm } from './components/mailbox-form';
 
 const AddProfessionalEmail = () => {
+	const [ isSubmitting, setIsSubmitting ] = useState( false );
+
+	const handleSubmit = async () => {
+		setIsSubmitting( true );
+	};
+
 	return (
 		<PageLayout header={ <PageHeader /> } size="small">
 			<Card>
 				<CardBody>
-					<MailboxForm disabled={ false } />
+					<MailboxForm disabled={ isSubmitting } />
 				</CardBody>
 			</Card>
 
 			<ButtonStack justify="flex-start">
-				<Button __next40pxDefaultSize variant="secondary" onClick={ () => {} }>
+				<Button
+					__next40pxDefaultSize
+					variant="secondary"
+					disabled={ isSubmitting }
+					onClick={ () => {} }
+				>
 					{ __( 'Add another mailbox' ) }
 				</Button>
 			</ButtonStack>
 
 			<ButtonStack justify="flex-start">
-				<Button __next40pxDefaultSize variant="primary" onClick={ () => {} }>
+				<Button
+					__next40pxDefaultSize
+					variant="primary"
+					disabled={ isSubmitting }
+					onClick={ handleSubmit }
+				>
 					{ __( 'Continue' ) }
 				</Button>
 			</ButtonStack>
