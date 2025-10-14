837386d185c41cb0650ce77dbda87a3e11d5fc88
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-10-14 14:09:53 +0200

    Add `getProductSlugForProviderAndInterval`
    Taken from `client/my-sites/email/email-providers-comparison/stacked/provider-cards/selectors/get-product-by-provider-and-interval.ts`

---
+++ client/dashboard/emails/utils/get-product-slug-for-provider-and-interval.ts
@@ -0,0 +1,20 @@
+import { EmailProvider } from '@automattic/api-core';
+import { IntervalLength } from '../types';
+
+/**
+ * Returns the correct product slug for the specified provider and interval using a map.
+ */
+export const getProductSlugForProviderAndInterval = (
+	provider: EmailProvider,
+	intervalLength: IntervalLength
+) => {
+	switch ( provider ) {
+		case 'titan':
+			return intervalLength === 'monthly' ? 'wp_titan_mail_monthly' : 'wp_titan_mail_yearly';
+
+		case 'google_workspace':
+			return intervalLength === 'monthly'
+				? 'wp_google_workspace_business_starter_monthly'
+				: 'wp_google_workspace_business_starter_yearly';
+	}
+};
