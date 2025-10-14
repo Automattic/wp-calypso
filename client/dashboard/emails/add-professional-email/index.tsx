57f03a2ff0bdb34b22cb6b88328c3544593fba4a
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-10-14 21:39:25 +0200

    Add `createNewMailbox`

--- client/dashboard/emails/add-professional-email/index.tsx
+++ client/dashboard/emails/add-professional-email/index.tsx
@@ -1,20 +1,55 @@
-import { productsQuery } from '@automattic/api-queries';
+import { domainQuery, mailboxAccountsQuery, productsQuery } from '@automattic/api-queries';
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from '@tanstack/react-router';
 import { Button, Card, CardBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ButtonStack } from '../../components/button-stack';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
+import { MailboxForm as MailboxFormEntity } from '../entities/mailbox-form';
+import { SupportedEmailProvider } from '../entities/types';
 import { IntervalLength } from '../types';
 import { getProductSlugForProviderAndInterval } from '../utils/get-product-slug-for-provider-and-interval';
 import { MailboxForm } from './components/mailbox-form';
 
 const AddProfessionalEmail = () => {
-	const { data: products } = useQuery( productsQuery() );
 	const router = useRouter();
+	// Extract params from the current match for this route
+	const match = router.state.matches[ router.state.matches.length - 1 ];
+	const params = ( match?.params ?? {} ) as { domain?: string; type?: string };
+	const { domain: domainName = '' } = params;
+
+	const { data: domain } = useQuery( domainQuery( domainName ) );
+	const { data: products } = useQuery( productsQuery() );
+	const { data: existingMailboxes, isFetched } = useQuery( {
+		// @ts-expect-error the query is only enabled when domain has a value, so blog_id won't be undefined
+		...mailboxAccountsQuery( domain?.blog_id, domainName ),
+		enabled: !! domain,
+	} );
 	const [ isSubmitting, setIsSubmitting ] = useState( false );
+	const [ mailboxEntities, setMailboxEntities ] = useState<
+		MailboxFormEntity< SupportedEmailProvider >[]
+	>( [] );
+
+	const createNewMailbox = useCallback( () => {
+		const mailbox = new MailboxFormEntity< SupportedEmailProvider >(
+			'titan',
+			domainName,
+			( existingMailboxes ?? [] )
+				.flatMap( ( emailAccount ) => emailAccount.emails )
+				.map( ( emailBox ) => emailBox.mailbox )
+		);
+		// Set initial values
+		// Object.entries( initialFieldValues ).forEach( ( [ fieldName, value ] ) => {
+		// 	mailbox.setFieldValue( fieldName as FormFieldNames, value );
+		// } );
+		return mailbox;
+	}, [ domainName, existingMailboxes ] );
+
+	useEffect( () => {
+		isFetched && setMailboxEntities( [ createNewMailbox() ] );
+	}, [ createNewMailbox, isFetched ] );
 
 	let interval: IntervalLength = router.state.location.search.interval;
 	if ( interval !== 'monthly' && interval !== 'annually' ) {
@@ -33,7 +68,7 @@ const AddProfessionalEmail = () => {
 		<PageLayout header={ <PageHeader /> } size="small">
 			<Card>
 				<CardBody>
-					<MailboxForm disabled={ isSubmitting } />
+					<MailboxForm mailboxEntity={ mailboxEntities?.[ 0 ] } disabled={ isSubmitting } />
 				</CardBody>
 			</Card>
 
