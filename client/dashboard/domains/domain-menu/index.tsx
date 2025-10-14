78186ed472254585999b38aa9fa54a5047f7ed50
Kamen Stanev <hambai@gmail.com>
2025-10-14 11:16:55 +0300

    Add domainName as query param to the /emails route so you can see the emails filtered by domain name

--- client/dashboard/domains/domain-menu/index.tsx
+++ client/dashboard/domains/domain-menu/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from '@tanstack/react-router';
 import { __ } from '@wordpress/i18n';
 import { useAppContext } from '../../app/context';
 import { emailsRoute } from '../../app/router/emails';
@@ -5,6 +6,7 @@ import ResponsiveMenu from '../../components/responsive-menu';
 
 const DomainMenu = ( { domainName }: { domainName: string } ) => {
 	const { supports } = useAppContext();
+	const router = useRouter();
 
 	return (
 		<ResponsiveMenu label={ __( 'Domain Menu' ) }>
@@ -12,7 +14,11 @@ const DomainMenu = ( { domainName }: { domainName: string } ) => {
 				{ __( 'Overview' ) }
 			</ResponsiveMenu.Item>
 			{ supports.emails && (
-				<ResponsiveMenu.Item to={ emailsRoute.fullPath }>{ __( 'Emails' ) }</ResponsiveMenu.Item>
+				<ResponsiveMenu.Item
+					to={ router.buildLocation( { to: emailsRoute.fullPath, search: { domainName } } ).href }
+				>
+					{ __( 'Emails' ) }
+				</ResponsiveMenu.Item>
 			) }
 		</ResponsiveMenu>
 	);
