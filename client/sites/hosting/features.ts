01755fff964c749d7a4f7207b120b5034eadc402
Veselin Nikolov <veselin@automattic.com>
2025-10-15 16:45:13 +0300

    Overcome the eligibility check

--- client/sites/hosting/features.ts
+++ client/sites/hosting/features.ts
@@ -7,10 +7,11 @@ import { getSelectedSite } from 'calypso/state/ui/selectors';
 import { AppState } from 'calypso/types';
 
 export function areHostingFeaturesSupported( site?: SiteExcerptData | null ) {
-	const isAtomicSite = !! site?.is_wpcom_atomic || !! site?.is_wpcom_staging_site;
+	const isAtomicOrStagingSite = !! site?.is_wpcom_atomic || !! site?.is_wpcom_staging_site;
+	const isFlexSite = !! site?.is_wpcom_flex;
 	const isPlanExpired = site?.plan?.expired;
 
-	return isAtomicSite && ! isPlanExpired;
+	return ( isAtomicOrStagingSite || isFlexSite ) && ! isPlanExpired;
 }
 
 export function useAreHostingFeaturesSupported() {
