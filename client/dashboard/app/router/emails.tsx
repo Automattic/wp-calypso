0d56bfac7191935ab88de2b48431fb953524201a
mmtr <1233880+mmtr@users.noreply.github.com>
2025-10-15 19:21:09 +0200

    Polish layout and render price

--- client/dashboard/app/router/emails.tsx
+++ client/dashboard/app/router/emails.tsx
@@ -95,7 +95,7 @@ export const chooseEmailSolutionRoute = createRoute( {
 	head: () => ( {
 		meta: [
 			{
-				title: __( 'Choose email solution' ),
+				title: __( 'Choose an email solution' ),
 			},
 		],
 	} ),
