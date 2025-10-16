3c1a4eb50cc83a110f3c71e838d0210e027638d7
Veselin Nikolov <veselin@automattic.com>
2025-10-16 12:33:59 +0300

    Shorten the code based on feedback by @p-jackson

--- client/sites/hosting/features.ts
+++ client/sites/hosting/features.ts
@@ -7,11 +7,11 @@ import { getSelectedSite } from 'calypso/state/ui/selectors';
 import { AppState } from 'calypso/types';
 
 export function areHostingFeaturesSupported( site?: SiteExcerptData | null ) {
-	const isAtomicOrStagingSite = !! site?.is_wpcom_atomic || !! site?.is_wpcom_staging_site;
-	const isFlexSite = !! site?.is_wpcom_flex;
+	const isWoAOrStagingOrFlexSite =
+		!! site?.is_wpcom_atomic || !! site?.is_wpcom_staging_site || !! site?.is_wpcom_flex;
 	const isPlanExpired = site?.plan?.expired;
 
-	return ( isAtomicOrStagingSite || isFlexSite ) && ! isPlanExpired;
+	return isWoAOrStagingOrFlexSite && ! isPlanExpired;
 }
 
 export function useAreHostingFeaturesSupported() {
