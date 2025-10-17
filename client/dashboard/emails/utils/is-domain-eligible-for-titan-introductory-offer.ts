c91634d853b96249ed6b2b206a89a45d83fe2370
mmtr <1233880+mmtr@users.noreply.github.com>
2025-10-17 17:47:46 +0200

    Simplify isDomainEligibleForTitanIntroductoryOffer check

--- client/dashboard/emails/utils/is-domain-eligible-for-titan-introductory-offer.ts
+++ client/dashboard/emails/utils/is-domain-eligible-for-titan-introductory-offer.ts
@@ -4,14 +4,14 @@ export const isDomainEligibleForTitanIntroductoryOffer = ( {
 	domain,
 	product,
 }: {
-	domain: Domain | undefined;
+	domain: Domain;
 	product: Product;
 } ) => {
 	return !! (
-		domain?.titan_mail_subscription?.is_eligible_for_introductory_offer &&
-		product?.introductory_offer &&
-		product?.introductory_offer.cost_per_interval === 0 &&
-		product?.introductory_offer.interval_count > 0 &&
-		product?.introductory_offer?.interval_unit
+		domain.titan_mail_subscription?.is_eligible_for_introductory_offer &&
+		product.introductory_offer &&
+		product.introductory_offer.cost_per_interval === 0 &&
+		product.introductory_offer.interval_count > 0 &&
+		product.introductory_offer?.interval_unit
 	);
 };
