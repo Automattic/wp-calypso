148277642a68a70c5addaadd77323c4fefeaeeb3
Rafael Agostini <rafael.agostini@automattic.com>
2025-10-17 18:39:42 -0500

    Update useStorageStatusText to use core i18n components

--- client/components/backup-storage-space/usage-warning/use-storage-status-text.ts
+++ client/components/backup-storage-space/usage-warning/use-storage-status-text.ts
@@ -1,53 +1,54 @@
-import { useTranslate, TranslateResult } from 'i18n-calypso';
+import { _n, __, sprintf } from '@wordpress/i18n';
 import { useMemo } from 'react';
 import { StorageUsageLevelName, StorageUsageLevels } from 'calypso/state/rewind/storage/types';
 
-// @TODO: migrate translations to use `@wordpress/i18n` to comply with using core components
-// @see https://github.com/Automattic/wp-calypso/blob/trunk/client/dashboard/docs/i18n.md
 const useStorageStatusText = (
 	usageLevel: StorageUsageLevelName,
 	daysOfBackupsSaved: number,
 	minDaysOfBackupsAllowed: number
-): TranslateResult | null => {
-	const translate = useTranslate();
-
+): string | null => {
 	return useMemo( () => {
 		switch ( usageLevel ) {
 			case StorageUsageLevels.Warning:
-				return translate(
+				return __(
 					'You are close to reaching your storage limit. Once you do, we will delete your oldest backups to make space for new ones.'
 				);
 			case StorageUsageLevels.Critical:
-				return translate(
+				return __(
 					'You are very close to reaching your storage limit. Once you do, we will delete your oldest backups to make space for new ones.'
 				);
 			case StorageUsageLevels.Full:
-				return translate(
-					'You have reached your storage limit with %(daysOfBackupsSaved)d day(s) of backups saved. Backups have been stopped. Please upgrade your storage to resume backups.',
-					{
-						args: { daysOfBackupsSaved },
-					}
+				//@TODO: we can use `_n` for pluralization here
+				return sprintf(
+					// translators: %(daysOfBackupsSaved)d is a number of days.
+					__(
+						'You have reached your storage limit with %(daysOfBackupsSaved)d day(s) of backups saved. Backups have been stopped. Please upgrade your storage to resume backups.'
+					),
+					{ daysOfBackupsSaved }
 				);
 			case StorageUsageLevels.FullButForecastOk:
-				return translate(
-					'You have reached your storage limit with %d day of backups saved. If your site size stays the same, storage will come back within limits in the next few days as backups continue and older ones are replaced.',
-					'You have reached your storage limit with %d days of backups saved. If your site size stays the same, storage will come back within limits in the next few days as backups continue and older ones are replaced.',
-					{
-						count: daysOfBackupsSaved,
-						args: [ daysOfBackupsSaved ],
-					}
+				return sprintf(
+					// translators: %(daysOfBackupsSaved)d is a number of days.
+					_n(
+						'You have reached your storage limit with %(daysOfBackupsSaved)d day of backups saved. If your site size stays the same, storage will come back within limits in the next few days as backups continue and older ones are replaced.',
+						'You have reached your storage limit with %(daysOfBackupsSaved)d days of backups saved. If your site size stays the same, storage will come back within limits in the next few days as backups continue and older ones are replaced.',
+						daysOfBackupsSaved
+					),
+					{ daysOfBackupsSaved }
 				);
 			case StorageUsageLevels.BackupsDiscarded:
-				return translate(
-					'We removed your oldest backup(s) to make space for new ones. We will continue to remove old backups as needed, up to the last %(minDaysOfBackupsAllowed)d days.',
-					{
-						args: { minDaysOfBackupsAllowed },
-					}
+				//@TODO: we can use `_n` for pluralization here
+				return sprintf(
+					// translators: %(minDaysOfBackupsAllowed)d is a number of days.
+					__(
+						'We removed your oldest backup(s) to make space for new ones. We will continue to remove old backups as needed, up to the last %(minDaysOfBackupsAllowed)d days.'
+					),
+					{ minDaysOfBackupsAllowed }
 				);
 		}
 
 		return null;
-	}, [ translate, usageLevel, daysOfBackupsSaved, minDaysOfBackupsAllowed ] );
+	}, [ usageLevel, daysOfBackupsSaved, minDaysOfBackupsAllowed ] );
 };
 
 export default useStorageStatusText;
