fb7f2b4f10c95b553530b25d5e82bf590d03e1be
Philip Jackson <p-jackson@live.com>
2025-10-17 23:32:20 +1300

    Fix error in type_check_client
    The window object can not be extended because, in the editor, it
    believes that the `currentUser` property has already been set (by a type
    definition in `data-stores`). However if we don't extend the type, then
    the type_check_client script doesn't see the `currentUser` property
    definition and it errors.
    
    This should be fixed by aligning all the various user types together.

--- client/dashboard/app/auth/index.tsx
+++ client/dashboard/app/auth/index.tsx
@@ -22,13 +22,16 @@ async function initializeCurrentUser(): Promise< User > {
 	const useBootstrap = ! isSupportUserSession() && config.isEnabled( 'wpcom-user-bootstrap' );
 
 	if ( useBootstrap ) {
-		if ( window.currentUser ) {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		if ( ( window as any ).currentUser ) {
 			// TODO: align the various `currentUser` types. The different types have
 			// different opinions on which fields are required and optional.
 			// - packages/api-core/src/me/types.ts
 			// - packages/data-stores/src/user/types.ts
 			// - client/lib/user/user.d.ts
-			return window.currentUser as any;
+
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			return ( window as any ).currentUser;
 		}
 		throw new Error( 'Failed to bootstrap user object' );
 	}
