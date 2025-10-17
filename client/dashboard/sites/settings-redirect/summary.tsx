f8902d53626a10807e4f288734b8bd9538ebd8ee
Igor Giussani <gius80@gmail.com>
2025-10-17 13:58:15 +0200

    Add site rederect entry in site settings summary

---
+++ client/dashboard/sites/settings-redirect/summary.tsx
@@ -0,0 +1,31 @@
+import { Icon } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { globe } from '@wordpress/icons';
+import RouterLinkSummaryButton from '../../components/router-link-summary-button';
+import type { Site } from '@automattic/api-core';
+import type { Density } from '@automattic/components/src/summary-button/types';
+
+export default function SiteRedirectSettingsSummary( {
+	site,
+	density,
+}: {
+	site: Site;
+	density?: Density;
+} ) {
+	let badges = [];
+	if ( site.options?.is_redirect ) {
+		badges = [ { text: __( 'Enabled' ), intent: 'success' as const } ];
+	} else {
+		badges = [ { text: __( 'Disabled' ) } ];
+	}
+
+	return (
+		<RouterLinkSummaryButton
+			to={ `/sites/${ site.slug }/settings/site-redirect` }
+			title={ __( 'Site redirect' ) }
+			density={ density }
+			decoration={ <Icon icon={ globe } /> }
+			badges={ badges }
+		/>
+	);
+}
