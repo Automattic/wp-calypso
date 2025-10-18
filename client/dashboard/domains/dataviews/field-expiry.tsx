98e8e54598f8b7f4cac7d86953fddeaf32f84248
Kamen Stanev <hambai@gmail.com>
2025-10-18 11:51:12 +0300

    Fix the expiry field rendering, sorting and filtering

--- client/dashboard/domains/dataviews/field-expiry.tsx
+++ client/dashboard/domains/dataviews/field-expiry.tsx
@@ -1,70 +1,48 @@
-import { DomainSubtype } from '@automattic/api-core';
-import { Icon, __experimentalHStack as HStack } from '@wordpress/components';
-import { __, sprintf } from '@wordpress/i18n';
-import { caution, reusableBlock } from '@wordpress/icons';
+import { DomainSubtype, DomainStatus } from '@automattic/api-core';
+import { Link } from '@tanstack/react-router';
+import { __experimentalVStack as VStack } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { Text } from '../../components/text';
+import { getPurchaseUrlForId } from '../../me/billing-purchases/urls';
 import type { DomainSummary } from '@automattic/api-core';
 
 export const DomainExpiryField = ( {
+	inOverview,
 	domain,
 	value,
-	isCompact = false,
 }: {
+	inOverview: boolean;
 	domain: DomainSummary;
 	value: string;
-	isCompact?: boolean;
 } ) => {
-	if ( ! domain.expiry ) {
+	// Site Overview does not show the Status column, so we use this column for error messages.
+	if (
+		inOverview &&
+		domain.subtype.id === DomainSubtype.DOMAIN_CONNECTION &&
+		domain.domain_status.id === DomainStatus.CONNECTION_ERROR
+	) {
+		return <Text intent={ domain.domain_status.type }>{ domain.domain_status.label }</Text>;
+	}
+
+	if ( domain.expiry === null ) {
 		if ( domain.subtype.id === DomainSubtype.DEFAULT_ADDRESS ) {
 			return __( 'Free forever' );
 		}
 		return '-';
 	}
 
-	const isAutoRenewing = Boolean( domain.auto_renewing );
-	const isExpired = new Date( domain.expiry ) < new Date();
-	const isHundredYearDomain = Boolean( domain.tags.includes( 'hundred_year_domain' ) );
-	const renderExpiry = () => {
-		if ( isHundredYearDomain ) {
-			return sprintf(
-				/* translators: %s - The date until which a domain was paid for */
-				__( 'Paid until %s' ),
-				value
-			);
-		}
-
-		if ( isExpired ) {
-			return sprintf(
-				/* translators: %s - The date on which a domain has expired */
-				__( 'Expired on %s' ),
-				value
-			);
-		}
-
-		if ( ! isAutoRenewing ) {
-			return sprintf(
-				/* translators: %s - The date on which a domain expires */
-				__( 'Expires on %s' ),
-				value
-			);
-		}
-
-		return sprintf(
-			/* translators: %s - The future date on which a domain renews */
-			__( 'Renews on %s' ),
-			value
-		);
-	};
-
 	return (
-		<HStack justify="flex-start" alignment="center" spacing={ 1 }>
-			{ ! isCompact && (
-				<Icon
-					icon={ isExpired || isHundredYearDomain ? caution : reusableBlock }
-					size={ 16 }
-					style={ { fill: 'currentColor' } }
-				/>
-			) }
-			<span>{ renderExpiry() }</span>
-		</HStack>
+		<VStack justify="flex-start" alignment="left" spacing={ 1 }>
+			<Text intent={ domain.expired ? 'error' : undefined }>{ value }</Text>
+			<Text variant="muted">
+				{ domain.auto_renewing ? (
+					__( 'Auto-renew is on' )
+				) : (
+					<Link to={ getPurchaseUrlForId( domain.subscription_id ?? 0 ) }>
+						{ __( 'Turn on auto-renew' ) }
+					</Link>
+				) }
+			</Text>
+		</VStack>
 	);
 };
