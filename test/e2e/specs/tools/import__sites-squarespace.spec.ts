9ec6b5e4903600bfb276e1b0d4467b06ea9370d3
Alister Scott <alister.scott@gmail.com>
2025-10-20 18:49:15 +1000

    Update flowchart to MermaidJS (free open source works on GH too)

--- test/e2e/specs/tools/import__sites-squarespace.spec.ts
+++ test/e2e/specs/tools/import__sites-squarespace.spec.ts
@@ -11,7 +11,11 @@ test.describe(
 	'Site Import: Calypso: Squarespace',
 	{
 		tag: [ tags.CALYPSO_RELEASE, tags.IMPORTS, tags.DESKTOP_ONLY ],
-		annotation: { type: 'flowchart', description: 'https://flowchart.fun/p/dynamic-camp-misspell' },
+		annotation: {
+			type: 'flowchart',
+			description:
+				'https://www.mermaidchart.com/play?utm_source=mermaid_live_editor&utm_medium=toggle#pako:eNqNU92Ok0AUfpUTTLSbWJNhKaVcGFPaTUw2arTGC_FiFoZAnM7gzBBSN3u7D-Aj-iSedljArawDN2T4fjjf-bj1MpkzL_YKLtuspMrAbp0KwKut6dfUe7uvJR7O2npO830lwB4wpeG60uYi9b7BfA47pg28FwyfX4PwkfjpR0MV0zXNGPy-_wUfm4GLJOsh_BOhyvaDVSaFYcJAoeQeRio9CdHWJkDS55pLmsPPqoai4gNIBBaz-L8wzBIpikrtgTamlEpfDCoLqxL2Kv0rjKcffNdKi1si7otU-QeU1q8y9JkefWkp0Ztb4PSG8RhS75qZFxoHETkcZKNAVwZHgruOElnKCl22OIv6a4ojFmieH60HF0IQ_E4a0I1iIAvABNQBallhEiVTzC4QVYk_RJXYqEYyfge6PIKgpcg2EqrzZONHO4PnyIqQlZRSambTLxrOgWN_Rg6XQ5ylYl2TSHD2UTB7tPPRuki3dbJ4qhqk2yoJ_yE-3QUSdrTleRvE6qHID2C7K2xJKuyRNgfOTrXBr-HxsyTahsnqZSa5VPENp9n3MQ7_DAu7Ol1TsOO_YHHr9WZ7tZ6UC5zksO9OaqGb2tINFrmZrtzUCOlxyWbjT-N8N1sspptv5IgLHH0Xjnqho95T2zje3t0fS9Luog',
+		},
 	},
 	() => {
 		test( 'One: As a New WordPress.com free plan user with a simple site, I can use the "Squarespace Run Importer" link on the wp-admin Importers List page to import my content from my Squarespace account', async ( {
