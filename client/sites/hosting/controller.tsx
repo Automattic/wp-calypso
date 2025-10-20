e7d041e3b50a1e3ee2ff841c140db0e15d902b1d
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-10-20 09:39:04 +0700

    Update location so that it shows up on activation

--- client/sites/hosting/controller.tsx
+++ client/sites/hosting/controller.tsx
@@ -82,15 +82,19 @@ export function hostingFeaturesCallout(
 					<HostingActivationCallout siteId={ site.ID } />
 				) : (
 					<HostingFeatureCallout>
-						<PageViewTracker
-							title="Sites > Hosting Feature Callout"
-							path={ getRouteFromContext( context ) }
-						/>
 						<CalloutComponent siteSlug={ site.slug } titleAs="h3" />
 					</HostingFeatureCallout>
 				);
 
-			context.primary = <div className="hosting-features-callout">{ callout }</div>;
+			context.primary = (
+				<div className="hosting-features-callout">
+					<PageViewTracker
+						title="Sites > Hosting Feature Callout"
+						path={ getRouteFromContext( context ) }
+					/>
+					{ callout }
+				</div>
+			);
 		}
 
 		next();
