8a7e91487ba3b22d2ce2f39e547ec82eb655215d
Mikael Korpela <mikael@ihminen.org>
2025-10-21 13:25:42 +0300

    E2E: Fix regex for folder name matching for Forms test
    Updated regex pattern for radio button name matching, as a follow-up to https://github.com/Automattic/wp-calypso/pull/106601

--- packages/calypso-e2e/src/lib/pages/feedback-inbox-page.ts
+++ packages/calypso-e2e/src/lib/pages/feedback-inbox-page.ts
@@ -180,7 +180,7 @@ export class FeedbackInboxPage {
 			.getByRole( 'tab', { name: new RegExp( folderName, 'i' ) } )
 			.or(
 				this.page.getByRole( 'radio', {
-					name: new RegExp( `^${ folderName }\\s*\\(\\d+\\)$`, 'i' ),
+					name: new RegExp( `^${ folderName }\\s*\\(\\d+,\\)$`, 'i' ),
 				} )
 			)
 			.click();
