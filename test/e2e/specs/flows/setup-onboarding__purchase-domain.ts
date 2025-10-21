71f843b6c391c9666d981dde0e6d1b72374a7d42
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-10-21 20:11:02 -0300

    Add an e2e test for the happy path of the onboarding flow purchasing a domain

---
+++ test/e2e/specs/flows/setup-onboarding__purchase-domain.ts
@@ -0,0 +1,87 @@
+/**
+ * @group calypso-release
+ */
+
+import {
+	DataHelper,
+	BrowserManager,
+	DomainSearchComponent,
+	SignupPickPlanPage,
+	CartCheckoutPage,
+	UserSignupPage,
+	NewUserResponse,
+	RestAPIClient,
+} from '@automattic/calypso-e2e';
+import { Page, Browser } from 'playwright';
+import { apiCloseAccount } from '../shared/api-close-account';
+
+declare const browser: Browser;
+
+describe( DataHelper.createSuiteTitle( 'Onboarding flow: Purchase a domain' ), function () {
+	const testUser = DataHelper.getNewTestUser();
+	const planName = 'Personal';
+	let page: Page;
+	let selectedDomain: string;
+	let newUserDetails: NewUserResponse;
+
+	beforeAll( async () => {
+		page = await browser.newPage();
+		await BrowserManager.setStoreCookie( page, { currency: 'USD' } );
+	} );
+
+	it( 'Enter the flow', async function () {
+		const flowUrl = DataHelper.getCalypsoURL( '/setup/onboarding' );
+		await page.goto( flowUrl );
+	} );
+
+	it( 'Sign up as a new user', async function () {
+		const userSignupPage = new UserSignupPage( page );
+		newUserDetails = await userSignupPage.signupSocialFirstWithEmail( testUser.email );
+	} );
+
+	it( 'Search for a domain', async function () {
+		const domainSearchComponent = new DomainSearchComponent( page );
+		await domainSearchComponent.search( DataHelper.getBlogName() );
+	} );
+
+	it( 'Add the first suggestion to the cart', async function () {
+		const domainSearchComponent = new DomainSearchComponent( page );
+		selectedDomain = await domainSearchComponent.selectFirstSuggestion();
+	} );
+
+	it( 'Continue to next step', async function () {
+		const domainSearchComponent = new DomainSearchComponent( page );
+		await domainSearchComponent.continue();
+	} );
+
+	it( `Select ${ planName } plan`, async function () {
+		const signupPickPlanPage = new SignupPickPlanPage( page );
+		await signupPickPlanPage.selectPlan( planName );
+	} );
+
+	it( 'See plan and domain at checkout', async function () {
+		const cartCheckoutPage = new CartCheckoutPage( page );
+		await cartCheckoutPage.validateCartItem( `WordPress.com ${ planName }` );
+		await cartCheckoutPage.validateCartItem( selectedDomain );
+	} );
+
+	afterAll( async function () {
+		if ( ! newUserDetails ) {
+			return;
+		}
+
+		const restAPIClient = new RestAPIClient(
+			{
+				username: testUser.username,
+				password: testUser.password,
+			},
+			newUserDetails.body.bearer_token
+		);
+
+		await apiCloseAccount( restAPIClient, {
+			userID: newUserDetails.body.user_id,
+			username: newUserDetails.body.username,
+			email: testUser.email,
+		} );
+	} );
+} );
