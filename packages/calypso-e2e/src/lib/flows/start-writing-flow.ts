06bbb5b0403922e66b1d8fa4b044fefe78f63c1d
Alister Scott <alister.scott@gmail.com>
2025-10-22 16:38:10 +1000

    move getters into readonly properties

--- packages/calypso-e2e/src/lib/flows/start-writing-flow.ts
+++ packages/calypso-e2e/src/lib/flows/start-writing-flow.ts
@@ -7,9 +7,32 @@ import { UserSignupPage } from '../pages/signup/user-signup-page';
  */
 export class StartWritingFlow {
 	private page: Page;
-	readonly userSignupPage: UserSignupPage;
-	readonly editorPage: EditorPage;
+	readonly blogDescriptionInput: Locator;
+	readonly blogNameInput: Locator;
+	readonly blogsAlmostReadyHeading: Locator;
+	readonly completedChooseADomainItem: Locator;
+	readonly completedChooseAPlanItem: Locator;
+	readonly completedNameYourBlogItem: Locator;
+	readonly completedWriteFirstPostItem: Locator;
+	readonly connectAccountsButton: Locator;
+	readonly connectToSocialButton: Locator;
+	/** Domain search component within the start writing flow */
 	readonly domainSearchComponent: DomainSearchComponent;
+	/** The editor page within the start writing flow */
+	readonly editorPage: EditorPage;
+	readonly jetpackSocialPageHeading: Locator;
+	readonly keepUpMomentumText: Locator;
+	readonly launchYourBlogButton: Locator;
+	readonly nowItsTimeToConnectYourSocialAccountsText: Locator;
+	readonly progressBar: Locator;
+	readonly saveBlogNameAndContinueButton: Locator;
+	readonly selectToChooseDomainLink: Locator;
+	readonly selectToChoosePlanLink: Locator;
+	readonly selectToNameYourBlogLink: Locator;
+	readonly startWithFreePlanButton: Locator;
+	/** User signup page within the start writing flow */
+	readonly userSignupPage: UserSignupPage;
+	readonly yourBlogsReadyHeading: Locator;
 
 	/**
 	 * Constructs an instance of the flow.
@@ -18,9 +41,43 @@ export class StartWritingFlow {
 	 */
 	constructor( page: Page ) {
 		this.page = page;
-		this.userSignupPage = new UserSignupPage( page );
-		this.editorPage = new EditorPage( page );
+		this.blogDescriptionInput = this.page.getByPlaceholder(
+			"Let people know what your blog's about"
+		);
+		this.blogNameInput = this.page.getByPlaceholder( 'A catchy name to make your blog memorable' );
+		this.blogsAlmostReadyHeading = this.page.getByRole( 'heading', {
+			name: "Your blog's almost ready!",
+		} );
+		this.completedChooseADomainItem = this.getCompletedItemLocator( 'Choose a domain' );
+		this.completedChooseAPlanItem = this.getCompletedItemLocator( 'Choose a plan' );
+		this.completedNameYourBlogItem = this.getCompletedItemLocator( 'Name your blog' );
+		this.completedWriteFirstPostItem = this.getCompletedItemLocator( 'Write your first post' );
+		this.connectAccountsButton = this.page.getByRole( 'button', { name: 'Connect accounts' } );
+		this.connectToSocialButton = this.page.getByRole( 'button', { name: 'Connect to social' } );
 		this.domainSearchComponent = new DomainSearchComponent( page );
+		this.editorPage = new EditorPage( page );
+		this.jetpackSocialPageHeading = this.page.getByText( 'Write once, post everywhere' );
+		this.keepUpMomentumText = this.page.getByText( 'Keep up the momentum with these final steps.' );
+		this.launchYourBlogButton = this.page.getByRole( 'button', { name: 'Launch your blog' } );
+		this.nowItsTimeToConnectYourSocialAccountsText = this.page.getByText(
+			'Now it’s time to connect your social accounts.'
+		);
+		this.progressBar = this.page.getByRole( 'progressbar' );
+		this.saveBlogNameAndContinueButton = this.page.getByRole( 'button', {
+			name: 'Save and continue',
+		} );
+		this.selectToChooseDomainLink = this.page.getByRole( 'link', {
+			name: 'Select to choose a domain',
+		} );
+		this.selectToChoosePlanLink = this.page.getByRole( 'link', {
+			name: 'Select to choose a plan',
+		} );
+		this.selectToNameYourBlogLink = this.page.getByRole( 'link', {
+			name: 'Select to name your blog',
+		} );
+		this.startWithFreePlanButton = this.page.getByRole( 'button', { name: 'Start with Free' } );
+		this.userSignupPage = new UserSignupPage( page );
+		this.yourBlogsReadyHeading = this.page.getByRole( 'heading', { name: 'Your blog’s ready!' } );
 	}
 
 	/**
@@ -32,22 +89,6 @@ export class StartWritingFlow {
 	}
 
 	/**
-	 * Heading locator for the "Your blog's almost ready!" message.
-	 * @returns {Locator}
-	 */
-	get blogsAlmostReadyHeading(): Locator {
-		return this.page.getByRole( 'heading', { name: "Your blog's almost ready!" } );
-	}
-
-	/**
-	 * Locator for the "Keep up the momentum with these final steps." text.
-	 * @returns {Locator}
-	 */
-	get keepUpMomentumText(): Locator {
-		return this.page.getByText( 'Keep up the momentum with these final steps.' );
-	}
-
-	/**
 	 * Locator for a completed item by its name.
 	 * Used only by public getters to encapsulate the logic for completed items.
 	 * @param itemName
@@ -55,149 +96,5 @@ export class StartWritingFlow {
 	 */
 	private getCompletedItemLocator( itemName: string ): Locator {
 		return this.page.locator( 'li' ).filter( { hasText: `Completed: ${ itemName }` } );
-	}
-
-	/**
-	 * Locator for the "Completed: Write your first post" item.
-	 * @returns {Locator}
-	 */
-	get completedWriteFirstPostItem(): Locator {
-		return this.getCompletedItemLocator( 'Write your first post' );
-	}
-
-	/**
-	 * Locator for the "Completed: Name your blog" item.
-	 * @returns {Locator}
-	 */
-	get completedNameYourBlogItem(): Locator {
-		return this.getCompletedItemLocator( 'Name your blog' );
-	}
-
-	/**
-	 * Locator for the "Completed: Choose a domain" item.
-	 * @returns {Locator}
-	 */
-	get completedChooseADomainItem(): Locator {
-		return this.getCompletedItemLocator( 'Choose a domain' );
-	}
-
-	/**
-	 * Locator for the "Completed: Choose a plan" item.
-	 * @returns {Locator}
-	 */
-	get completedChooseAPlanItem(): Locator {
-		return this.getCompletedItemLocator( 'Choose a plan' );
-	}
-
-	/**
-	 * Locator for the progress bar.
-	 * @returns {Locator}
-	 */
-	get progressBar(): Locator {
-		return this.page.getByRole( 'progressbar' );
-	}
-
-	/**
-	 * Locator for the blog name input field.
-	 * @returns {Locator}
-	 */
-	get blogNameInput(): Locator {
-		return this.page.getByPlaceholder( 'A catchy name to make your blog memorable' );
-	}
-
-	/**
-	 * 	Locator for the blog description input field.
-	 * @returns {Locator}
-	 */
-	get blogDescriptionInput(): Locator {
-		return this.page.getByPlaceholder( "Let people know what your blog's about" );
-	}
-
-	/**
-	 * Locator for the "Select to name your blog" link.
-	 * @returns {Locator}
-	 */
-	get selectToNameYourBlogLink(): Locator {
-		return this.page.getByRole( 'link', { name: 'Select to name your blog' } );
-	}
-
-	/**
-	 * Locator for the "Select to choose a domain" link.
-	 * @returns {Locator}
-	 */
-	get selectToChooseDomainLink(): Locator {
-		return this.page.getByRole( 'link', { name: 'Select to choose a domain' } );
-	}
-
-	/**
-	 * Locator for the "Select to choose a plan" link.
-	 * @returns {Locator}
-	 */
-	get selectToChoosePlanLink(): Locator {
-		return this.page.getByRole( 'link', { name: 'Select to choose a plan' } );
-	}
-
-	/**
-	 * Locator for the "Start with Free" plan button.
-	 * @returns {Locator}
-	 */
-	get startWithFreePlanButton(): Locator {
-		return this.page.getByRole( 'button', { name: 'Start with Free' } );
-	}
-
-	/**
-	 * Locator for the "Save and continue" button.
-	 * @returns {Locator}
-	 */
-	get saveBlogNameAndContinueButton(): Locator {
-		return this.page.getByRole( 'button', { name: 'Save and continue' } );
-	}
-
-	/**
-	 * Locator for the "Launch your blog" button.
-	 * @returns {Locator}
-	 */
-	get launchYourBlogButton(): Locator {
-		return this.page.getByRole( 'button', { name: 'Launch your blog' } );
-	}
-
-	/**
-	 * Locator for the "Your blog's ready!" heading.
-	 * @returns {Locator}
-	 */
-	get yourBlogsReadyHeading(): Locator {
-		return this.page.getByRole( 'heading', { name: 'Your blog’s ready!' } );
-	}
-
-	/**
-	 * Locator for the "Now it’s time to connect your social accounts." text.
-	 * @returns {Locator}
-	 */
-	get nowItsTimeToConnectYourSocialAccountsText(): Locator {
-		return this.page.getByText( 'Now it’s time to connect your social accounts.' );
-	}
-
-	/**
-	 * Locator for the "Connect to social" button.
-	 * @returns {Locator}
-	 */
-	get connectToSocialButton(): Locator {
-		return this.page.getByRole( 'button', { name: 'Connect to social' } );
-	}
-
-	/**
-	 * Locator for the Jetpack Social page heading.
-	 * @returns {Locator}
-	 */
-	get jetpackSocialPageHeading(): Locator {
-		return this.page.getByText( 'Write once, post everywhere' );
-	}
-
-	/**
-	 * Locator for the Jetpack Social "Connect accounts" button.
-	 * @returns {Locator}
-	 */
-	get connectAccountsButton(): Locator {
-		return this.page.getByRole( 'button', { name: 'Connect accounts' } );
 	}
 }
