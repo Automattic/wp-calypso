babbb140914e229cbcdf9f9668c07ab371ec7eff
Miroslav Mitev <m1r0@users.noreply.github.com>
2025-10-22 15:58:30 +0300

    Remove the Woo social linking notice in favor of the wpcom one

--- client/blocks/login/login-form.jsx
+++ client/blocks/login/login-form.jsx
@@ -1,11 +1,9 @@
 import config from '@automattic/calypso-config';
 import page from '@automattic/calypso-router';
 import { Card, FormInputValidation, FormLabel, Gridicon } from '@automattic/components';
-import { alert } from '@automattic/components/src/icons';
 import { localizeUrl } from '@automattic/i18n-utils';
 import { suggestEmailCorrection } from '@automattic/onboarding';
 import { Button } from '@wordpress/components';
-import { Icon } from '@wordpress/icons';
 import clsx from 'clsx';
 import cookie from 'cookie';
 import emailValidator from 'email-validator';
@@ -20,9 +18,8 @@ import JetpackConnectSiteOnly from 'calypso/blocks/jetpack-connect-site-only';
 import LoginSubmitButton from 'calypso/blocks/login/login-submit-button';
 import FormPasswordInput from 'calypso/components/forms/form-password-input';
 import FormTextInput from 'calypso/components/forms/form-text-input';
-import Notice from 'calypso/components/notice';
 import { LastUsedSocialButton } from 'calypso/components/social-buttons';
-import DashboardNotice from 'calypso/dashboard/components/notice';
+import Notice from 'calypso/dashboard/components/notice';
 import {
 	getSignupUrl,
 	pathWithLeadingSlash,
@@ -690,6 +687,24 @@ export class LoginForm extends Component {
 			);
 		};
 
+		const renderSocialLinkingNotice = () => {
+			return (
+				<Notice variant="error">
+					{ this.props.translate(
+						'We found a WordPress.com account with the email address "%(email)s". ' +
+							'Log in to this account to connect it to your %(service)s profile, ' +
+							'or choose a different %(service)s profile.',
+						{
+							args: {
+								email: this.props.socialAccountLinkEmail,
+								service: capitalize( this.props.socialAccountLinkService ),
+							},
+						}
+					) }
+				</Notice>
+			);
+		};
+
 		return (
 			<Card className="login__form">
 				{ showLastUsedAuthenticationMethod ? (
@@ -710,21 +725,7 @@ export class LoginForm extends Component {
 						{ isWoo && <ErrorNotice /> }
 
 						<div className="login__form-userdata">
-							{ ! isWoo && linkingSocialUser && (
-								<DashboardNotice variant="error">
-									{ this.props.translate(
-										'We found a WordPress.com account with the email address "%(email)s". ' +
-											'Log in to this account to connect it to your %(service)s profile, ' +
-											'or choose a different %(service)s profile.',
-										{
-											args: {
-												email: this.props.socialAccountLinkEmail,
-												service: capitalize( this.props.socialAccountLinkService ),
-											},
-										}
-									) }
-								</DashboardNotice>
-							) }
+							{ linkingSocialUser && renderSocialLinkingNotice() }
 
 							<FormLabel htmlFor="usernameOrEmail" hasCoreStylesNoCaps>
 								{ this.renderUsernameorEmailLabel() }
@@ -803,24 +804,6 @@ export class LoginForm extends Component {
 														{ this.state.emailSuggestion }
 													</span>
 												),
-											},
-										}
-									) }
-								/>
-							) }
-
-							{ isWoo && linkingSocialUser && (
-								<Notice
-									className="login__form-user-exists-notice"
-									status="is-warning"
-									icon={ <Icon icon={ alert } size={ 20 } fill="#d67709" /> }
-									showDismiss
-									onDismissClick={ this.props.cancelSocialAccountConnectLinking }
-									text={ this.props.translate(
-										'You already have a WordPress.com account with this email address. Add your password to log in or {{signupLink}}create a new account{{/signupLink}}.',
-										{
-											components: {
-												signupLink: <a href={ signupUrl } />,
 											},
 										}
 									) }
