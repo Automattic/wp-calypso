0cc17a274b898814d6558de4e4c0c3442bfcc1ce
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-10-23 20:55:04 -0300

    Add one more test case for the `/start/launch-site` flow

---
+++ test/e2e/specs/flows/start-launch-site__purchase-plan.ts
@@ -0,0 +1,54 @@
+/**
+ * @group calypso-release
+ */
+
+import {
+	DataHelper,
+	DomainSearchComponent,
+	SignupPickPlanPage,
+	CartCheckoutPage,
+	TestAccount,
+} from '@automattic/calypso-e2e';
+import { Page, Browser } from 'playwright';
+import { getAccount } from '../../lib/get-account';
+
+declare const browser: Browser;
+
+describe( DataHelper.createSuiteTitle( 'Launch site flow: Purchase plan' ), function () {
+	const planName = 'Personal';
+	let page: Page;
+	let siteUrl: string;
+	let testAccount: TestAccount;
+
+	beforeAll( async () => {
+		page = await browser.newPage();
+		testAccount = await getAccount( page, 'simpleSiteFreePlanUser' );
+		await testAccount.authenticate( page );
+		siteUrl = testAccount.getSiteURL( { protocol: false } );
+	} );
+
+	it( 'Enter the flow', async function () {
+		const flowUrl = DataHelper.getCalypsoURL( `/start/launch-site?siteSlug=${ siteUrl }` );
+		await page.goto( flowUrl );
+	} );
+
+	it( 'Search for a domain', async function () {
+		const domainSearchComponent = new DomainSearchComponent( page );
+		await domainSearchComponent.search( DataHelper.getBlogName() );
+	} );
+
+	it( 'Skip domain selection', async function () {
+		const domainSearchComponent = new DomainSearchComponent( page );
+		await domainSearchComponent.skipPurchase();
+	} );
+
+	it( `Select ${ planName } plan`, async function () {
+		const signupPickPlanPage = new SignupPickPlanPage( page );
+		await signupPickPlanPage.selectPlanWithoutSiteCreation( planName );
+	} );
+
+	it( 'See plan and domain at checkout', async function () {
+		const cartCheckoutPage = new CartCheckoutPage( page );
+		await cartCheckoutPage.validateCartItem( `WordPress.com ${ planName }` );
+	} );
+} );
