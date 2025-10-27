b7728f2ccdb43d17ac4fc1c67926a888ce697a3e
Steven <steve.dufresne@automattic.com>
2025-10-27 16:22:20 +0900

    Use getRelativeTimeString instead of timeSince for better wordage.

--- client/dashboard/sites/performance/subtitle.tsx
+++ client/dashboard/sites/performance/subtitle.tsx
@@ -1,7 +1,7 @@
 import { __experimentalText as Text, Button } from '@wordpress/components';
 import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
-import { useTimeSince } from '../../components/time-since';
+import { getRelativeTimeString } from '../../utils/datetime';
 
 const REPORT_REFRESH_THRESHOLD_HOURS = 48;
 
@@ -23,12 +23,11 @@ export default function Subtitle( {
 	timestamp: string | undefined;
 	onClick: () => void;
 } ) {
-	const timeSince = useTimeSince( timestamp ?? '' );
-
 	if ( ! timestamp ) {
 		return <Text variant="muted">{ __( 'Testing your site may take around 30 seconds.' ) }</Text>;
 	}
 
+	const timeSince = getRelativeTimeString( new Date( timestamp ?? '' ) );
 	if ( isReportOlderThan( timestamp, REPORT_REFRESH_THRESHOLD_HOURS ) ) {
 		return createInterpolateElement(
 			sprintf(
