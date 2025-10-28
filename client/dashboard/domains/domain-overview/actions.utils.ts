28ce44f7d52e9202c5fc752edd55a2a26893fc15
Payton Swick <payton@foolord.com>
2025-10-28 12:06:13 -0400

    Remove unnecessary canAutoRenewBeTurnedOff in domains
    This logic has been moved to the server.

--- client/dashboard/domains/domain-overview/actions.utils.ts
+++ client/dashboard/domains/domain-overview/actions.utils.ts
@@ -8,18 +8,6 @@ export const transferableTypes: DomainSubtype[] = [
 ];
 export const disconnectableTypes: DomainSubtype[] = [ DomainSubtype.DOMAIN_REGISTRATION ];
 
-export const canAutoRenewBeTurnedOff = ( purchase: Purchase ) => {
-	if ( [ 'included', 'expired' ].includes( purchase.expiry_status ) ) {
-		return false;
-	}
-
-	if ( purchase.is_refundable && purchase.refund_amount > 0 ) {
-		return true;
-	}
-
-	return purchase.is_auto_renew_enabled;
-};
-
 export const shouldShowTransferAction = ( domain: Domain ) => {
 	if (
 		! domain.current_user_is_owner ||
@@ -68,7 +56,7 @@ export const shouldShowRemoveAction = ( domain: Domain, purchase?: Purchase ) =>
 		return false;
 	}
 
-	if ( canAutoRenewBeTurnedOff( purchase ) ) {
+	if ( purchase.can_disable_auto_renew ) {
 		return false;
 	}
 
@@ -94,7 +82,7 @@ export const shouldShowCancelAction = ( domain: Domain, purchase?: Purchase ) =>
 		return false;
 	}
 
-	if ( ! canAutoRenewBeTurnedOff( purchase ) ) {
+	if ( ! purchase.can_disable_auto_renew ) {
 		return false;
 	}
 
