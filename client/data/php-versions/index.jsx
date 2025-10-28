a41f5417acd57bc0e03a2e3674e491f8f03290b5
tbradsha <32492176+tbradsha@users.noreply.github.com>
2025-10-28 09:45:53 -0600

    Allow PHP 8.4 to be selected

--- client/data/php-versions/index.jsx
+++ client/data/php-versions/index.jsx
@@ -1,7 +1,7 @@
 import { __, sprintf } from '@wordpress/i18n';
 
 export const getPHPVersions = ( siteId ) => {
-	// PHP 8.3 is now the default recommended version
+	// PHP 8.3 is the default recommended version
 	const recommendedValue = '8.3';
 	// translators: PHP Version for a version switcher
 	const recommendedLabel = sprintf( __( '%s (Recommended)' ), recommendedValue );
@@ -28,17 +28,22 @@ export const getPHPVersions = ( siteId ) => {
 			// translators: PHP Version for a version switcher
 			label: sprintf( __( '%s (Deprecated)' ), '8.1' ),
 			value: '8.1',
-			disabled: ! PHP81BrokenSites.includes( siteId ?? 0 ), // EOL 31st December, 2025
+			disabled: ! PHP81BrokenSites.includes( siteId ?? 0 ), // EOL 31 December 2025
 		},
 		{
 			label: '8.2',
 			value: '8.2',
-			disabled: false, // EOL 31st December, 2026
+			disabled: false, // EOL 31 December 2026
 		},
 		{
 			label: recommendedLabel,
 			value: recommendedValue,
-			disabled: false, // EOL 31st December, 2027
+			disabled: false, // EOL 31 December 2027
+		},
+		{
+			label: '8.4',
+			value: '8.4',
+			disabled: false, // EOL 31 December 2028
 		},
 	];
 
