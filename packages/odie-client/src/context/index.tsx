0c5fc8c474f59bf41eba5d9718a4e3610e280d25
Omar Alshaker <omar@omaralshaker.com>
2025-10-29 12:31:31 +0100

    Default to `ODIE_DEFAULT_BOT_SLUG` in Odie

--- packages/odie-client/src/context/index.tsx
+++ packages/odie-client/src/context/index.tsx
@@ -3,6 +3,7 @@ import { HELP_CENTER_STORE } from '@automattic/help-center/src/stores';
 import { useSelect } from '@wordpress/data';
 import { createContext, useCallback, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ODIE_DEFAULT_BOT_SLUG } from '../constants';
 import { useOdieBroadcastWithCallbacks } from '../data';
 import { useGetCombinedChat } from '../hooks';
 import { isOdieAllowedBot, getIsRequestingHumanSupport } from '../utils';
@@ -32,7 +33,7 @@ export const emptyChat: Chat = {
 export const OdieAssistantContext = createContext< OdieAssistantContextInterface >( {
 	addMessage: noop,
 	botName: 'Wapuu',
-	botNameSlug: '',
+	botNameSlug: ODIE_DEFAULT_BOT_SLUG,
 	chat: emptyChat,
 	canConnectToZendesk: false,
 	isLoadingCanConnectToZendesk: false,
