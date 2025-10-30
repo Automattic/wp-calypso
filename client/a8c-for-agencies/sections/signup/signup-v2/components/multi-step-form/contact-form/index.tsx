e7ae7744a58519c9b79a5625ed31d4a621aeb901
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-10-30 18:03:30 +0800

    Use 'Continue' as CTA label.

--- client/a8c-for-agencies/sections/signup/signup-v2/components/multi-step-form/contact-form/index.tsx
+++ client/a8c-for-agencies/sections/signup/signup-v2/components/multi-step-form/contact-form/index.tsx
@@ -304,7 +304,7 @@ const SignupContactForm = ( { onContinue, initialFormData, withEmail = false }: 
 									setDuplicateAgencyFields( { agencyName: false, agencyUrl: false } );
 								} }
 							>
-								{ translate( 'Create new agency account' ) }
+								{ translate( 'Continue' ) }
 							</Button>
 							<Button
 								variant="secondary"
