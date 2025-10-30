8a78e58e0b7cac5dac31e5ea14d7bebc83054799
Miroslav Mitev <m1r0@users.noreply.github.com>
2025-10-30 15:44:34 +0200

    Update experiment name

--- packages/domain-search/src/hooks/use-escape-hatch.ts
+++ packages/domain-search/src/hooks/use-escape-hatch.ts
@@ -3,7 +3,8 @@ import { useMemo } from 'react';
 import { getFlowFromURL } from 'calypso/landing/stepper/utils/get-flow-from-url'; // eslint-disable-line no-restricted-imports
 import { useExperiment } from 'calypso/lib/explat'; // eslint-disable-line no-restricted-imports
 
-export const EXPERIMENT_NAME = 'miro_test_experiment';
+export const EXPERIMENT_NAME =
+	'calypso_signup_onboarding_domain_search_results_page_escape_hatch_202510';
 
 /**
  * Hook for the domain search escape hatch experiment.
