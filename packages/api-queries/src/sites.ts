968f03bad749d2ea43fb091f23fe6842e73d56ed
arthur <arthur.chu@automattic.com>
2025-10-30 10:44:53 +0800

    Use sitesQueryKey instead of sitesQueryFilter

--- packages/api-queries/src/sites.ts
+++ packages/api-queries/src/sites.ts
@@ -1,21 +1,14 @@
 import { fetchSites, SITE_FIELDS, SITE_OPTIONS } from '@automattic/api-core';
 import { queryOptions } from '@tanstack/react-query';
 import type { FetchSitesFilters, FetchSitesOptions } from '@automattic/api-core';
-import type { Query } from '@tanstack/react-query';
 
-const DEFAULT_QUERY_KEYS = [ 'sites', SITE_FIELDS, SITE_OPTIONS ];
+export const sitesQueryKey = [ 'sites', SITE_FIELDS, SITE_OPTIONS ];
 
 export const sitesQuery = (
 	siteFilters: FetchSitesFilters,
 	fetchSitesOptions: FetchSitesOptions = { site_visibility: 'visible', include_a8c_owned: false }
 ) =>
 	queryOptions( {
-		queryKey: [ ...DEFAULT_QUERY_KEYS, siteFilters, fetchSitesOptions ],
+		queryKey: [ ...sitesQueryKey, siteFilters, fetchSitesOptions ],
 		queryFn: () => fetchSites( siteFilters, fetchSitesOptions ),
 	} );
-
-export const sitesQueryFilter = () => ( {
-	predicate: ( { queryKey }: Query ) => {
-		return DEFAULT_QUERY_KEYS.every( ( value, i ) => value === queryKey[ i ] );
-	},
-} );
