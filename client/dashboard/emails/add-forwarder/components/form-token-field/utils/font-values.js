2485bb381efca063c70b5157f86e4c1bcf4446a6
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-10-30 16:34:56 +0100

    No need to import the component, it was already available 🤦🫠

--- client/dashboard/emails/add-forwarder/components/form-token-field/utils/font-values.js
+++
@@ -1,9 +0,0 @@
-export default {
-	'default.fontFamily':
-		"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif",
-	'default.fontSize': '13px',
-
-	'helpText.fontSize': '12px',
-
-	mobileTextMinFontSize: '16px',
-};
