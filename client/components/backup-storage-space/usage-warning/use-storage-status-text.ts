9112bea53eafa40860a2f11bd01d18ac4ed54efd
Rafael Agostini <rafael.agostini@automattic.com>
2025-10-30 11:43:52 -0500

    Pluralize storage status messages

--- client/components/backup-storage-space/usage-warning/use-storage-status-text.ts
+++ client/components/backup-storage-space/usage-warning/use-storage-status-text.ts
@@ -18,11 +18,12 @@ const useStorageStatusText = (
 					'You are very close to reaching your storage limit. Once you do, we will delete your oldest backups to make space for new ones.'
 				);
 			case StorageUsageLevels.Full:
-				//@TODO: we can use `_n` for pluralization here
 				return sprintf(
 					// translators: %(daysOfBackupsSaved)d is a number of days.
-					__(
-						'You have reached your storage limit with %(daysOfBackupsSaved)d day(s) of backups saved. Backups have been stopped. Please upgrade your storage to resume backups.'
+					_n(
+						'You have reached your storage limit with %(daysOfBackupsSaved)d day of backups saved. Backups have been stopped. Please upgrade your storage to resume backups.',
+						'You have reached your storage limit with %(daysOfBackupsSaved)d days of backups saved. Backups have been stopped. Please upgrade your storage to resume backups.',
+						daysOfBackupsSaved
 					),
 					{ daysOfBackupsSaved }
 				);
@@ -37,11 +38,12 @@ const useStorageStatusText = (
 					{ daysOfBackupsSaved }
 				);
 			case StorageUsageLevels.BackupsDiscarded:
-				//@TODO: we can use `_n` for pluralization here
 				return sprintf(
 					// translators: %(minDaysOfBackupsAllowed)d is a number of days.
-					__(
-						'We removed your oldest backup(s) to make space for new ones. We will continue to remove old backups as needed, up to the last %(minDaysOfBackupsAllowed)d days.'
+					_n(
+						'We removed your oldest backup to make space for new ones. We will continue to remove old backups as needed, keeping the last %(minDaysOfBackupsAllowed)d day.',
+						'We removed your oldest backups to make space for new ones. We will continue to remove old backups as needed, keeping the last %(minDaysOfBackupsAllowed)d days.',
+						minDaysOfBackupsAllowed
 					),
 					{ minDaysOfBackupsAllowed }
 				);
