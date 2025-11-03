0d9317500fb63e269e508edb98a0d41644a91f06
Igor Giussani <gius80@gmail.com>
2025-11-03 17:51:25 +0100

    Add tests

--- client/dashboard/domains/domain-connection-setup/nameservers-dataview.tsx
+++ client/dashboard/domains/domain-connection-setup/nameservers-dataview.tsx
@@ -90,7 +90,7 @@ export default function NameserversDataView( {
 				enableSorting: false,
 				header: '',
 				render: () => {
-					return <Icon icon={ arrowRight } fill="#757575" size={ 24 } />;
+					return <Icon icon={ arrowRight } fill="#CCCCCC" size={ 24 } />;
 				},
 			},
 			{
