fb22340665104fabf34852e740c8942bf5c73172
arthur <arthur.chu@automattic.com>
2025-11-03 12:17:43 +0800

    Keep decoded keys

--- packages/api-core/src/me/fetchers.ts
+++ packages/api-core/src/me/fetchers.ts
@@ -1,8 +1,30 @@
+import { decode } from 'he';
 import { wpcom } from '../wpcom-fetcher';
 import type { User, TwoStep } from './types';
 
+const decodedKeys = [ 'display_name', 'description', 'user_URL' ];
+
+function decodeEntities( text: string ) {
+	// Bypass decode if text doesn't include entities
+	if ( 'string' !== typeof text || -1 === text.indexOf( '&' ) ) {
+		return text;
+	}
+
+	return decode( text );
+}
+
 export async function fetchUser(): Promise< User > {
-	return wpcom.req.get( '/me', { meta: 'flags' } );
+	const user = await wpcom.req.get( '/me', { meta: 'flags' } );
+	for ( const key of user ) {
+		if ( ! decodedKeys.includes( key ) ) {
+			continue;
+		}
+
+		const value = user[ key ];
+		user[ key ] = value ? decodeEntities( value as string ) : value;
+	}
+
+	return user;
 }
 
 export async function fetchTwoStep(): Promise< TwoStep > {
