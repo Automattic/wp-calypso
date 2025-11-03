50c172c2bfbd00a30570da423b3283f46b306e09
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-11-03 19:58:14 -0300

    Refactor code and remove unneeded code

--- client/dashboard/domains/domain-connection-setup/components/dns-records-table.tsx
+++ client/dashboard/domains/domain-connection-setup/components/dns-records-table.tsx
@@ -1,15 +1,21 @@
 import { domainMappingStatusQuery, domainQuery } from '@automattic/api-queries';
 import { Badge } from '@automattic/ui';
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
+import { DataViews } from '@wordpress/dataviews';
 import { __ } from '@wordpress/i18n';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { DataViewsCard } from '../../../components/dataviews-card';
-import type { Field, ViewTable, View } from '@wordpress/dataviews';
+import type { Field, ViewTable } from '@wordpress/dataviews';
 
 import './dns-records-table-style.scss';
 
-interface FormData {
+const view: ViewTable = {
+	type: 'table',
+	page: 1,
+	fields: [ 'type', 'name', 'currentValue', 'expectedValue', 'status' ],
+};
+
+interface DnsRecordVerification {
 	type: string;
 	name: string;
 	currentValue: string;
@@ -17,17 +23,7 @@ interface FormData {
 	status: React.ReactNode;
 }
 
-const DEFAULT_VIEW: ViewTable = {
-	type: 'table',
-	page: 1,
-	sort: {
-		field: 'type',
-		direction: 'asc',
-	},
-	fields: [ 'type', 'name', 'currentValue', 'expectedValue', 'status' ],
-};
-
-const fields: Field< FormData >[] = [
+const fields: Field< DnsRecordVerification >[] = [
 	{
 		id: 'type',
 		label: __( 'Type' ),
@@ -68,68 +64,60 @@ const VerifyingBadge = () => {
 	return <Badge intent="warning">{ __( 'Verifying' ) }</Badge>;
 };
 
+const aRecordData = ( currentValue: string, expectedValue: string ) => {
+	return {
+		type: 'A',
+		name: '@',
+		currentValue: currentValue,
+		expectedValue: expectedValue,
+		status: currentValue === expectedValue ? <VerifiedBadge /> : <VerifyingBadge />,
+	};
+};
+
+const wwwCnameRecordData = ( currentValue: string, expectedValue: string ) => {
+	return {
+		type: 'CNAME',
+		name: 'www',
+		currentValue: currentValue,
+		expectedValue: expectedValue,
+		status: currentValue === expectedValue ? <VerifiedBadge /> : <VerifyingBadge />,
+	};
+};
+
 export default function DnsRecordsTable( { domainName }: { domainName: string } ) {
 	const { data: domain } = useSuspenseQuery( domainQuery( domainName ) );
 	const { data: domainMappingStatus } = useSuspenseQuery( domainMappingStatusQuery( domainName ) );
 
-	const formData = useMemo( (): FormData[] => {
-		const data: FormData[] = [];
+	const dnsRecords = useMemo( (): DnsRecordVerification[] => {
+		const data: DnsRecordVerification[] = [];
 
-		const hostIpAddresses = ( domainMappingStatus?.host_ip_addresses || [] ).sort();
+		const currentIpAddresses = ( domainMappingStatus?.host_ip_addresses || [] ).sort();
 		const expectedIpAddresses = ( domain?.a_records_required_for_mapping || [] ).sort();
-		const longestArray = Math.max( hostIpAddresses.length, expectedIpAddresses.length );
 
-		for ( let i = 0; i < longestArray; i++ ) {
-			const isVerified = hostIpAddresses[ i ] === expectedIpAddresses[ i ];
-
-			data.push( {
-				type: 'A',
-				name: '@',
-				currentValue: hostIpAddresses[ i ] || '-',
-				expectedValue: expectedIpAddresses[ i ] || '-',
-				status: isVerified ? <VerifiedBadge /> : <VerifyingBadge />,
-			} );
+		for ( let i = 0; i < Math.max( currentIpAddresses.length, expectedIpAddresses.length ); i++ ) {
+			data.push( aRecordData( currentIpAddresses[ i ], expectedIpAddresses[ i ] ) );
 		}
 
-		if ( domainMappingStatus.www_cname_record_target ) {
-			const isVerified = domainMappingStatus.www_cname_record_target === domainName;
-
-			data.push( {
-				type: 'CNAME',
-				name: 'www',
-				currentValue: domainMappingStatus.www_cname_record_target,
-				expectedValue: domainName,
-				status: isVerified ? <VerifiedBadge /> : <VerifyingBadge />,
-			} );
-		}
+		const wwwCnameRecordTarget = domainMappingStatus.www_cname_record_target || '-';
+		data.push( wwwCnameRecordData( wwwCnameRecordTarget, domainName ) );
 
 		return data;
 	}, [ domain, domainName, domainMappingStatus ] );
 
-	const [ view, setView ] = useState< ViewTable >( DEFAULT_VIEW );
-
-	const { data: filteredData, paginationInfo } = useMemo(
-		() => filterSortAndPaginate( formData, view, fields ),
-		[ formData, view ]
-	);
-
 	return (
-		<DataViewsCard className="update-dns-records-table">
-			<DataViews< FormData >
-				data={ filteredData }
+		<DataViewsCard className="dns-records-table">
+			<DataViews< DnsRecordVerification >
+				data={ dnsRecords }
 				fields={ fields }
 				view={ view }
 				defaultLayouts={ { table: {} } }
-				paginationInfo={ paginationInfo }
-				onChangeView={ ( view: View ) => setView( view as ViewTable ) }
-				getItemId={ ( item: FormData ) =>
-					`${ item.type }-${ item.name }-${ item.currentValue }-${ item.expectedValue }`
+				paginationInfo={ { totalItems: dnsRecords.length, totalPages: 1 } }
+				onChangeView={ () => {} }
+				getItemId={ ( item: DnsRecordVerification ) =>
+					`${ item.type }-${ item.name }-${ item.currentValue }`
 				}
 			>
-				<>
-					<DataViews.Layout />
-					<DataViews.Pagination />
-				</>
+				<DataViews.Layout />
 			</DataViews>
 		</DataViewsCard>
 	);
