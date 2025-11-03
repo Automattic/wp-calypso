7a6f0b07c8a83f2a271c00e8f6273343a622a887
Steven <steve.dufresne@automattic.com>
2025-11-03 16:42:30 +0900

    Use site details to guess our providers.

--- client/dashboard/utils/site-provider.ts
+++ client/dashboard/utils/site-provider.ts
@@ -1,16 +1,31 @@
 import type { Site } from '@automattic/api-core';
 
-export const DEFAULT_PROVIDER_NAME = 'WordPress.com';
+export const DEFAULT_PROVIDER_NAME = 'Unknown';
+
+const isWordPressComProvider = ( site: Site ) => {
+	return (
+		site.is_wpcom_atomic ||
+		site.is_a8c ||
+		site.is_garden ||
+		site.is_wpcom_staging_site ||
+		site.is_vip ||
+		site.is_wpcom_flex ||
+		site.slug.endsWith( 'wordpress.com' )
+	);
+};
 
 export function getSiteProviderName( site: Site ) {
 	const provider = site.hosting_provider_guess;
+
 	let providerName;
 	if ( provider === 'jurassic_ninja' ) {
 		providerName = 'Jurassic Ninja';
 	} else if ( provider === 'pressable' ) {
 		providerName = 'Pressable';
-	} else if ( provider === 'automattic' ) {
+	} else if ( isWordPressComProvider( site ) ) {
 		providerName = 'WordPress.com';
+	} else if ( site.jetpack ) {
+		providerName = 'Jetpack';
 	}
 
 	return providerName;
