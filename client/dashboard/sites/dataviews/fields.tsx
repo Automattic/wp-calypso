4a5ddaf5fd95e9dc7cb61fcbba70c93190b1a3d9
Daniel Lind <daniel.lind@automattic.com>
2025-11-04 01:15:31 +0100

    Refactor DEFAULT_FIELDS to get translations at runtime

--- client/dashboard/sites/dataviews/fields.tsx
+++ client/dashboard/sites/dataviews/fields.tsx
@@ -22,131 +22,133 @@ import {
 import type { Site } from '@automattic/api-core';
 import type { Field, Operator } from '@wordpress/dataviews';
 
-export const DEFAULT_FIELDS: Field< Site >[] = [
-	{
-		id: 'name',
-		label: __( 'Site' ),
-		enableHiding: false,
-		enableGlobalSearch: true,
-		getValue: ( { item } ) => getSiteDisplayName( item ),
-		render: ( { field, item } ) => <Name site={ item } value={ field.getValue( { item } ) } />,
-	},
-	{
-		id: 'URL',
-		label: __( 'URL' ),
-		enableGlobalSearch: true,
-		getValue: ( { item } ) => getSiteDisplayUrl( item ),
-		render: ( { field, item } ) => <URL site={ item } value={ field.getValue( { item } ) } />,
-	},
-	{
-		id: 'icon.ico',
-		label: __( 'Site icon' ),
-		render: ( { item } ) => <SiteIcon site={ item } />,
-		enableSorting: false,
-	},
-	{
-		id: 'subscribers_count',
-		label: __( 'Subscribers' ),
-	},
-	{
-		id: 'backup',
-		label: __( 'Backup' ),
-		render: ( { item } ) => <LastBackup site={ item } />,
-		enableSorting: false,
-	},
-	{
-		id: 'plan',
-		label: __( 'Plan' ),
-		getValue: ( { item } ) => getSitePlanDisplayName( item ) ?? '',
-		render: ( { item } ) => <Plan site={ item } />,
-	},
-	{
-		id: 'status',
-		label: __( 'Status' ),
-		getValue: ( { item } ) => getSiteStatus( item ),
-		elements: Object.entries( STATUS_LABELS ).map( ( [ value, label ] ) => ( { value, label } ) ),
-		filterBy: {
-			operators: [ 'isAny' as Operator ],
+export function getDefaultFields(): Field< Site >[] {
+	return [
+		{
+			id: 'name',
+			label: __( 'Site' ),
+			enableHiding: false,
+			enableGlobalSearch: true,
+			getValue: ( { item } ) => getSiteDisplayName( item ),
+			render: ( { field, item } ) => <Name site={ item } value={ field.getValue( { item } ) } />,
 		},
-		render: ( { item } ) => <Status site={ item } />,
-	},
-	{
-		id: 'wp_version',
-		label: __( 'WP version' ),
-		getValue: ( { item } ) => getFormattedWordPressVersion( item ),
-	},
-	{
-		id: 'is_a8c',
-		type: 'boolean',
-		label: __( 'A8C owned' ),
-		elements: [
-			{ value: true, label: __( 'Yes' ) },
-			{ value: false, label: __( 'No' ) },
-		],
-		filterBy: {
-			operators: [ 'is' as Operator ],
+		{
+			id: 'URL',
+			label: __( 'URL' ),
+			enableGlobalSearch: true,
+			getValue: ( { item } ) => getSiteDisplayUrl( item ),
+			render: ( { field, item } ) => <URL site={ item } value={ field.getValue( { item } ) } />,
 		},
-		render: ( { item } ) => ( item.is_a8c ? __( 'Yes' ) : __( 'No' ) ),
-	},
-	{
-		id: 'preview',
-		label: __( 'Preview' ),
-		render: ( { item } ) => <Preview site={ item } />,
-		enableHiding: false,
-		enableSorting: false,
-	},
-	{
-		id: 'last_published',
-		label: __( 'Last published' ),
-		getValue: ( { item } ) => item.options?.updated_at ?? '',
-		render: ( { item } ) =>
-			item.options?.updated_at ? <TimeSince timestamp={ item.options.updated_at } /> : '',
-	},
-	{
-		id: 'uptime',
-		label: __( '7-day uptime' ),
-		render: ( { item } ) => <Uptime site={ item } />,
-		enableSorting: false,
-	},
-	{
-		id: 'visitors',
-		label: __( '7-day visitors' ),
-		render: ( { item } ) => <EngagementStat site={ item } type="visitors" />,
-		enableSorting: false,
-	},
-	{
-		id: 'views',
-		label: __( '7-day views' ),
-		render: ( { item } ) => <EngagementStat site={ item } type="views" />,
-		enableSorting: false,
-	},
-	{
-		id: 'likes',
-		label: __( '7-day likes' ),
-		render: ( { item } ) => <EngagementStat site={ item } type="likes" />,
-		enableSorting: false,
-	},
-	{
-		id: 'php_version',
-		label: __( 'PHP version' ),
-		render: ( { item }: { item: Site } ) => <PHPVersion site={ item } />,
-		enableSorting: false,
-	},
-	{
-		id: 'storage',
-		label: __( 'Storage' ),
-		render: ( { item } ) => <MediaStorage site={ item } />,
-		enableSorting: false,
-	},
-	{
-		id: 'host',
-		label: __( 'Host' ),
-		getValue: ( { item } ) => {
-			return getSiteProviderName( item ) ?? DEFAULT_PROVIDER_NAME;
+		{
+			id: 'icon.ico',
+			label: __( 'Site icon' ),
+			render: ( { item } ) => <SiteIcon site={ item } />,
+			enableSorting: false,
 		},
-		render: ( { field, item } ) => field.getValue( { item } ),
-	},
-];
+		{
+			id: 'subscribers_count',
+			label: __( 'Subscribers' ),
+		},
+		{
+			id: 'backup',
+			label: __( 'Backup' ),
+			render: ( { item } ) => <LastBackup site={ item } />,
+			enableSorting: false,
+		},
+		{
+			id: 'plan',
+			label: __( 'Plan' ),
+			getValue: ( { item } ) => getSitePlanDisplayName( item ) ?? '',
+			render: ( { item } ) => <Plan site={ item } />,
+		},
+		{
+			id: 'status',
+			label: __( 'Status' ),
+			getValue: ( { item } ) => getSiteStatus( item ),
+			elements: Object.entries( STATUS_LABELS ).map( ( [ value, label ] ) => ( { value, label } ) ),
+			filterBy: {
+				operators: [ 'isAny' as Operator ],
+			},
+			render: ( { item } ) => <Status site={ item } />,
+		},
+		{
+			id: 'wp_version',
+			label: __( 'WP version' ),
+			getValue: ( { item } ) => getFormattedWordPressVersion( item ),
+		},
+		{
+			id: 'is_a8c',
+			type: 'boolean',
+			label: __( 'A8C owned' ),
+			elements: [
+				{ value: true, label: __( 'Yes' ) },
+				{ value: false, label: __( 'No' ) },
+			],
+			filterBy: {
+				operators: [ 'is' as Operator ],
+			},
+			render: ( { item } ) => ( item.is_a8c ? __( 'Yes' ) : __( 'No' ) ),
+		},
+		{
+			id: 'preview',
+			label: __( 'Preview' ),
+			render: ( { item } ) => <Preview site={ item } />,
+			enableHiding: false,
+			enableSorting: false,
+		},
+		{
+			id: 'last_published',
+			label: __( 'Last published' ),
+			getValue: ( { item } ) => item.options?.updated_at ?? '',
+			render: ( { item } ) =>
+				item.options?.updated_at ? <TimeSince timestamp={ item.options.updated_at } /> : '',
+		},
+		{
+			id: 'uptime',
+			label: __( '7-day uptime' ),
+			render: ( { item } ) => <Uptime site={ item } />,
+			enableSorting: false,
+		},
+		{
+			id: 'visitors',
+			label: __( '7-day visitors' ),
+			render: ( { item } ) => <EngagementStat site={ item } type="visitors" />,
+			enableSorting: false,
+		},
+		{
+			id: 'views',
+			label: __( '7-day views' ),
+			render: ( { item } ) => <EngagementStat site={ item } type="views" />,
+			enableSorting: false,
+		},
+		{
+			id: 'likes',
+			label: __( '7-day likes' ),
+			render: ( { item } ) => <EngagementStat site={ item } type="likes" />,
+			enableSorting: false,
+		},
+		{
+			id: 'php_version',
+			label: __( 'PHP version' ),
+			render: ( { item }: { item: Site } ) => <PHPVersion site={ item } />,
+			enableSorting: false,
+		},
+		{
+			id: 'storage',
+			label: __( 'Storage' ),
+			render: ( { item } ) => <MediaStorage site={ item } />,
+			enableSorting: false,
+		},
+		{
+			id: 'host',
+			label: __( 'Host' ),
+			getValue: ( { item } ) => {
+				return getSiteProviderName( item ) ?? DEFAULT_PROVIDER_NAME;
+			},
+			render: ( { field, item } ) => field.getValue( { item } ),
+		},
+	];
+}
 
 export function getFields( {
 	isAutomattician,
@@ -155,7 +157,8 @@ export function getFields( {
 	isAutomattician?: boolean;
 	viewType?: string;
 } ) {
-	return DEFAULT_FIELDS.filter( ( field ) => {
+	const defaultFields = getDefaultFields();
+	return defaultFields.filter( ( field ) => {
 		if ( field.id === 'is_a8c' && ! isAutomattician ) {
 			return false;
 		}
