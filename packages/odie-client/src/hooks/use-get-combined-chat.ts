fe03f15b4598c0608d3267d545aeac21dfd13104
Emanuele Buccelli <escapemanuele@gmail.com>
2025-11-06 15:44:04 +0100

    Add transfer message to direct escalations too

--- packages/odie-client/src/hooks/use-get-combined-chat.ts
+++ packages/odie-client/src/hooks/use-get-combined-chat.ts
@@ -141,11 +141,9 @@ export const useGetCombinedChat = (
 								conversationId: conversation.id,
 								messages: [
 									...( odieChat ? filteredOdieMessages : [] ),
-									...( odieChat
-										? getOdieTransferMessage(
-												currentSupportInteraction?.bot_slug as OdieAllBotSlugs
-										  )
-										: [] ),
+									...getOdieTransferMessage(
+										currentSupportInteraction?.bot_slug as OdieAllBotSlugs
+									),
 									...( deduplicateZDMessages( [
 										// During connection recovery, the user queued messages can be deleted. This ensure they remain. And `deduplicateZDMessages` takes of duplication.
 										...( isSameConversation
