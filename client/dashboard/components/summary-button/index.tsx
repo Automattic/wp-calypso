7a14b5930ac32ccd09fd9fda107601a6fb4451dc
Steven <steve.dufresne@automattic.com>
2025-11-11 10:19:01 +0900

    Create the wrapper for the summary button.

---
+++ client/dashboard/components/summary-button/index.tsx
@@ -0,0 +1,14 @@
+import SummaryButton from '@automattic/components/src/summary-button';
+import type { SummaryButtonProps } from '@automattic/components/src/summary-button/types';
+import './style.scss';
+
+/**
+ * This component is a wrapper of the `SummaryButton` component and adds a
+ * `dashboard-summary-button` class to the component to handle responsive padding temporarily.
+ * See https://github.com/Automattic/wp-dashboard/pull/106797 for more details.
+ * @param props - The props for the SummaryButton component.
+ * @returns The SummaryButton component.
+ */
+export default function DashboardSummaryButton( props: SummaryButtonProps ) {
+	return <SummaryButton className="dashboard-summary-button" { ...props } />;
+}
