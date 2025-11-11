889ef7afd9e820a9f848bcf52be221afa1486c1b
Steven <steve.dufresne@automattic.com>
2025-11-11 10:47:15 +0900

    Update pull request number.

--- client/dashboard/components/summary-button/index.tsx
+++ client/dashboard/components/summary-button/index.tsx
@@ -5,7 +5,7 @@ import './style.scss';
 /**
  * This component is a wrapper of the `SummaryButton` component and adds a
  * `dashboard-summary-button` class to the component to handle responsive padding temporarily.
- * See https://github.com/Automattic/wp-dashboard/pull/106797 for more details.
+ * See https://github.com/Automattic/wp-dashboard/pull/107025 for more details.
  * @param props - The props for the SummaryButton component.
  * @returns The SummaryButton component.
  */
