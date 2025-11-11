b7c775ef1cae355b3030933039357e070f834f1e
Igor Giussani <gius80@gmail.com>
2025-11-11 18:29:36 +0100

    Don\'t cache api call

--- packages/api-queries/src/domain-propagation-status.ts
+++ packages/api-queries/src/domain-propagation-status.ts
@@ -5,5 +5,4 @@ export const domainPropagationStatusQuery = ( domainName: string ) =>
 	queryOptions( {
 		queryKey: [ 'domain-propagation-status', domainName ],
 		queryFn: () => fetchDomainPropagationStatus( domainName ),
-		staleTime: 60 * 60 * 1000, // Consider data stale after 1 hour
 	} );
