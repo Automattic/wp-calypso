17c1cc724e333963b5ad0b86157888d0f7e4f857
Omar Alshaker <omar@omaralshaker.com>
2025-11-12 20:22:16 +0100

    Switch to BigSky

--- apps/help-center/help-center-embedded-chat.js
+++ apps/help-center/help-center-embedded-chat.js
@@ -1,10 +1,10 @@
 import './config';
 import {
-	HelpCenterChat as EmbeddedChat,
 	HelpCenterRequiredContextProvider,
+	HelpCenterContent,
+	PersistentRouter,
 } from '@automattic/help-center';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import './help-center.scss';
 
 const queryClient = new QueryClient();
@@ -19,28 +19,20 @@ export default function HelpCenterEmbeddedChat( {
 	isCommerceGarden,
 } ) {
 	return (
-		<MemoryRouter>
-			<Routes>
-				<Route path="/odie" element={ <div>Hello</div> } />
-				<Route
-					path="/"
-					element={
-						<QueryClientProvider client={ queryClient }>
-							<HelpCenterRequiredContextProvider
-								locale={ locale }
-								sectionName={ sectionName || 'wp-admin' }
-								currentUser={ currentUser }
-								site={ site }
-								hasPurchases={ hasPurchases }
-								onboardingUrl={ onboardingUrl }
-								isCommerceGarden={ isCommerceGarden }
-							>
-								<EmbeddedChat isLoadingStatus={ false } isUserEligibleForPaidSupport={ false } />
-							</HelpCenterRequiredContextProvider>
-						</QueryClientProvider>
-					}
-				/>
-			</Routes>
-		</MemoryRouter>
+		<PersistentRouter>
+			<QueryClientProvider client={ queryClient }>
+				<HelpCenterRequiredContextProvider
+					locale={ locale }
+					sectionName={ sectionName || 'wp-admin' }
+					currentUser={ currentUser }
+					site={ site }
+					hasPurchases={ hasPurchases }
+					onboardingUrl={ onboardingUrl }
+					isCommerceGarden={ isCommerceGarden }
+				>
+					<HelpCenterContent currentRoute="/bigsky" />
+				</HelpCenterRequiredContextProvider>
+			</QueryClientProvider>
+		</PersistentRouter>
 	);
 }
