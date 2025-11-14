6d453d539994960b6b336da669d4373cc47b854e
arthur <arthur.chu@automattic.com>
2025-11-14 18:02:05 +0800

    Use waitForComponentToBeInitializedWithSelectedTab

--- apps/notifications/src/app/note-panel/test/index.tsx
+++ apps/notifications/src/app/note-panel/test/index.tsx
@@ -1,16 +1,44 @@
-import { waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { renderWithProvider } from '../../../testing-library';
 import NodePanel, { NOTIFICATION_TABS } from '../index';
+
+// Copied from https://github.com/WordPress/gutenberg/blob/adf3ef6d41df4e70f283a35f552631668131dd95/packages/components/src/tabs/test/index.tsx#L181.
+async function waitForComponentToBeInitializedWithSelectedTab(
+	selectedTabName: string | undefined
+) {
+	if ( ! selectedTabName ) {
+		// Wait for the tablist to be tabbable as a mean to know
+		// that ariakit has finished initializing.
+		await waitFor( () =>
+			expect( screen.getByRole( 'tablist' ) ).toHaveAttribute(
+				'tabindex',
+				expect.stringMatching( /^(0|-1)$/ )
+			)
+		);
+		// No initially selected tabs or tabpanels.
+		await waitFor( () =>
+			expect( screen.queryByRole( 'tab', { selected: true } ) ).not.toBeInTheDocument()
+		);
+	} else {
+		// Waiting for a tab to be selected is a sign that the component
+		// has fully initialized.
+		expect(
+			await screen.findByRole( 'tab', {
+				selected: true,
+				name: selectedTabName,
+			} )
+		).toBeVisible();
+	}
+}
 
 describe( 'NotePanel', () => {
 	it( 'should render correctly', async () => {
 		const { getByText } = renderWithProvider( <NodePanel /> );
 
-		// Avoid the Tabs components that update state asynchronously to trigger warnings.
-		await waitFor( () => {
-			NOTIFICATION_TABS.forEach( ( { title }: { title: string } ) => {
-				expect( getByText( title ) ).toBeInTheDocument();
-			} );
+		await waitForComponentToBeInitializedWithSelectedTab( NOTIFICATION_TABS[ 0 ].title );
+
+		NOTIFICATION_TABS.forEach( ( { title }: { title: string } ) => {
+			expect( getByText( title ) ).toBeInTheDocument();
 		} );
 	} );
 } );
