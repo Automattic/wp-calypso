37fd7930e146d9338dc41b61c3ce878f73d14cbe
Steven <steve.dufresne@automattic.com>
2025-11-18 14:04:32 +0900

    Don't even use the max width. Just use 24px.

--- client/dashboard/components/page-layout/index.tsx
+++ client/dashboard/components/page-layout/index.tsx
@@ -1,5 +1,11 @@
 import { __experimentalVStack as VStack } from '@wordpress/components';
+import type { CSSProperties } from 'react';
 import './style.scss';
+
+const PAGE_LAYOUT_SIZES = {
+	large: { '--page-layout-max-width': '1344px' },
+	small: { '--page-layout-max-width': '660px' },
+};
 
 function PageLayout( {
 	children,
@@ -13,7 +19,11 @@ function PageLayout( {
 	size?: 'large' | 'small';
 } ) {
 	return (
-		<VStack spacing={ 8 } className={ `dashboard-page-layout is-${ size }` }>
+		<VStack
+			spacing={ 8 }
+			className={ `dashboard-page-layout is-${ size }` }
+			style={ PAGE_LAYOUT_SIZES[ size ] as CSSProperties }
+		>
 			{ header }
 			{ notices }
 			<VStack spacing={ 6 } className="dashboard-page-layout__content">
