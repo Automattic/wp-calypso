8a31653400cedb41712883c461832f0977da156c
Copons <Copons@users.noreply.github.com>
2025-11-19 16:54:42 +0000

    Don't remove pass-through props

--- client/my-sites/themes/single-site-wpcom.jsx
+++ client/my-sites/themes/single-site-wpcom.jsx
@@ -5,6 +5,8 @@ import Main from 'calypso/components/main';
 import { useRequestSiteChecklistTaskUpdate } from 'calypso/data/site-checklist';
 import { CHECKLIST_KNOWN_TASKS } from 'calypso/state/data-layer/wpcom/checklist/index.js';
 import isVipSite from 'calypso/state/selectors/is-vip-site';
+import { getCurrentPlan, isRequestingSitePlans } from 'calypso/state/sites/plans/selectors';
+import { getSiteSlug } from 'calypso/state/sites/selectors';
 import { getActiveTheme } from 'calypso/state/themes/selectors';
 import { connectOptions } from './theme-options';
 import ThemeShowcase from './theme-showcase';
@@ -27,5 +29,8 @@ const ConnectedSingleSiteWpcom = connectOptions( ( props ) => {
 
 export default connect( ( state, { siteId } ) => ( {
 	isVip: isVipSite( state, siteId ),
+	siteSlug: getSiteSlug( state, siteId ),
+	requestingSitePlans: isRequestingSitePlans( state, siteId ),
+	currentPlan: getCurrentPlan( state, siteId ),
 	currentThemeId: getActiveTheme( state, siteId ),
 } ) )( ConnectedSingleSiteWpcom );
