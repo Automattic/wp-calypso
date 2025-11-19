398c7183f42525d4255ecd53bb038d5351a684b3
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2025-11-19 19:46:29 -0300

    Fix issue when transferring domain to site with paid plan

--- client/landing/stepper/declarative-flow/flows/domain/domain.ts
+++ client/landing/stepper/declarative-flow/flows/domain/domain.ts
@@ -189,7 +189,7 @@ const domain: FlowV2< typeof initialize > = {
 						const mappingIsFree = hasPlanFeature( site, DotcomFeatures.DOMAIN_MAPPING );
 						const hasPaidPlan = siteHasPaidPlan( site );
 
-						if ( ( isDomainMapping && mappingIsFree ) || hasPaidPlan ) {
+						if ( isDomainMapping && ( mappingIsFree || hasPaidPlan ) ) {
 							const queryArgs = getQueryArgs( window.location.href );
 							const redirectTo = queryArgs.redirect_to as string | undefined;
 
