ea4a5e5d01d1e8bea4898ef95599e8a0c75bc3d5
Griffith Chen <griffith.chen@automattic.com>
2025-11-20 15:40:26 +0800

    Send unknown error to logstash

--- client/dashboard/app/500/index.tsx
+++ client/dashboard/app/500/index.tsx
@@ -1,10 +1,26 @@
+import config from '@automattic/calypso-config';
 import { Notice } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { useEffect } from 'react';
+import { logToLogstash } from 'calypso/lib/logstash';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
 import RouterLinkButton from '../../components/router-link-button';
 
 function UnknownError( { error }: { error: Error } ) {
+	useEffect( () => {
+		logToLogstash( {
+			feature: 'calypso_client',
+			message: 'Unknown error',
+			severity: config( 'env_id' ) === 'production' ? 'error' : 'debug',
+			tags: [ 'dashboard' ],
+			extra: {
+				env: config( 'env_id' ),
+				message: error.message,
+			},
+		} );
+	}, [ error ] );
+
 	return (
 		<PageLayout
 			header={
