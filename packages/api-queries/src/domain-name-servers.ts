8de958fa872975132c444952d48391754f331c16
Paulo Trentin <paulo@paulotrentin.com.br>
2025-11-20 13:30:15 -0300

    Invalidate domain-details data after a nameserver update

--- packages/api-queries/src/domain-name-servers.ts
+++ packages/api-queries/src/domain-name-servers.ts
@@ -3,6 +3,7 @@ import {
 	updateDomainNameServers,
 	DomainNameServersResponse,
 } from '@automattic/api-core';
+import { domainQuery } from '@automattic/api-queries';
 import { queryOptions, mutationOptions } from '@tanstack/react-query';
 import { queryClient } from './query-client';
 
@@ -25,5 +26,6 @@ export const domainNameServersMutation = ( domainName: string ) =>
 				nameServers: data,
 			} );
 			queryClient.invalidateQueries( domainNameServersQuery( domainName ) );
+			queryClient.invalidateQueries( domainQuery( domainName ) );
 		},
 	} );
