6f4daf905cc2d2c7178c8d1b42175352fc1217de
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-11-21 12:17:25 -0300

    Make it beautiful

--- test/e2e/specs/a8c-for-agencies/partner-directory__smoke.ts
+++ test/e2e/specs/a8c-for-agencies/partner-directory__smoke.ts
@@ -2,7 +2,7 @@
  * @group gutenberg
  */
 
-import { envVariables } from '@automattic/calypso-e2e';
+import { envVariables, PartnerDirectoryComponent } from '@automattic/calypso-e2e';
 import type { Browser, Page } from 'playwright';
 declare const browser: Browser;
 
@@ -11,33 +11,31 @@ declare const browser: Browser;
  */
 describe( 'Automattic For Agencies: Partner Directory', () => {
 	let page: Page;
+	let partnerDirectory: PartnerDirectoryComponent;
 
 	beforeAll( async () => {
 		page = await browser.newPage();
+		partnerDirectory = new PartnerDirectoryComponent( page );
 	} );
 
 	test( 'Navigate to the Partner Directory', async () => {
 		await page.goto( envVariables.PARTNER_DIRECTORY_BASE_URL );
 	} );
 
-	test( 'Click the Industries dropdown', async () => {
-		await page.getByRole( 'button', { name: 'Industries' } ).click();
-	} );
-
-	test( 'Select the Legal & Professional Services industry', async () => {
-		await page.getByRole( 'checkbox', { name: 'Legal & Professional Services' } ).click();
+	test( 'Apply the Legal & Professional Services industry filter', async () => {
+		await partnerDirectory.applyDropdownFilter( 'Industries', 'Legal & Professional Services' );
 	} );
 
 	test( 'Wait for the filter to be applied', async () => {
-		await page.waitForSelector( 'text=/\\d+ partners found for filters/', { timeout: 10000 } );
+		await partnerDirectory.waitForFilterToBeApplied();
 	} );
 
 	test( "Visit the first partner's details page", async () => {
 		await Promise.all( [
-			page.getByText( 'Accepting new clients' ).first().click(),
 			page.waitForURL( new RegExp( `^${ envVariables.PARTNER_DIRECTORY_BASE_URL }/[^/]+/[^/]+/` ), {
 				timeout: 10_000,
 			} ),
+			partnerDirectory.clickFirstPartner(),
 		] );
 	} );
 } );
