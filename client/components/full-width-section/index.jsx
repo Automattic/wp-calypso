ed7b1bdcbd55e5585b6d0a0920e3340b3d57166f
Claudiu Filip <claudiu.filip@automattic.com>
2025-11-21 11:35:19 +0200

    Move common style to full-width-section component

--- client/components/full-width-section/index.jsx
+++ client/components/full-width-section/index.jsx
@@ -2,6 +2,8 @@ import { isEnabled } from '@automattic/calypso-config';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
+import './style.scss';
+
 const FullWidthSection = ( { children, className } ) => {
 	const isMarketplaceRedesignEnabled = isEnabled( 'marketplace-redesign' );
 
