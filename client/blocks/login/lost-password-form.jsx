932073bdbe793ca5aa79d1cee77c9b70b4735d31
Christos <chriskmnds@gmail.com>
2025-11-21 13:07:19 +0200

    Use sentence case for lost-password help link

--- client/blocks/login/lost-password-form.jsx
+++ client/blocks/login/lost-password-form.jsx
@@ -202,7 +202,7 @@ const LostPasswordForm = ( {
 					) }
 					className="login__lostpassword-form-external-link"
 				>
-					{ translate( 'Need More Help?' ) }
+					{ translate( 'Need more help?' ) }
 				</ExternalLink>
 			</div>
 			<div className="login__form-action">
