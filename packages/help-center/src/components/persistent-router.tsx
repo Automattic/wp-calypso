ca11df96272ebd977627e262e3c4d676267ba8af
Joao Heringer <joao.heringer@automattic.com>
2025-11-25 12:32:29 -0300

    Adjust PersistentRouter to be reusable

--- packages/help-center/src/components/persistent-router.tsx
+++ packages/help-center/src/components/persistent-router.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import { Router } from 'react-router-dom';
 import { usePersistedHistory } from '../hooks/use-persisted-history';
+import type { Location } from 'history';
 
 type Props = {
 	children: React.ReactNode;
+	routerHistory?: { entries: Location[]; index: number } | undefined;
+	persistenceKey?: string | undefined;
 };
 
 /**
  * A router like MemoryRouter, but it persists the history to the server using user preferences.
  */
-export const PersistentRouter = ( { children }: Props ) => {
-	const { history, state } = usePersistedHistory();
+export const PersistentRouter = ( { children, routerHistory, persistenceKey }: Props ) => {
+	const { history, state } = usePersistedHistory( { routerHistory, persistenceKey } );
 
 	return (
 		<Router location={ state.location } navigator={ history } navigationType={ state.action }>
