eb2408a8db33bf21e7ebf6c51cab959039880fa5
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2025-11-25 20:26:40 -0300

    Add has-text-align-justify class name to the markup

--- apps/wpcom-block-editor/src/default/features/rich-text.js
+++ apps/wpcom-block-editor/src/default/features/rich-text.js
@@ -4,34 +4,63 @@ import { getBlockType } from '@wordpress/blocks';
 import { compose, ifCondition } from '@wordpress/compose';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { registerFormatType } from '@wordpress/rich-text';
+import clsx from 'clsx';
+import { objectHasValues } from '../../utils';
 
 const RichTextJustifyButton = ( {
 	blockId,
 	deprecatedIsBlockJustified,
 	styleAttributes,
 	updateBlockAttributes,
+	className,
 } ) => {
 	const isBlockJustified = 'justify' === styleAttributes.typography?.textAlign;
 
 	const onToggle = () => {
+		/**
+		 * The functionality introduced in Gutenberg 22.1 does not yet support text-align justify,
+		 * so we need to introduce the has-text-align-justify class name ourselves.
+		 */
+		const baseClassName = className.replace( 'has-text-align-justify', '' );
+
 		// TODO: Remove this once we know all Atomic sites are on Gutenberg 22.1 or higher
 		const isDeprecatedAlignAttribute =
 			getBlockType( 'core/paragraph' ).attributes.align !== undefined;
 
 		if ( isDeprecatedAlignAttribute ) {
+			const newAlignValue = deprecatedIsBlockJustified ? undefined : 'justify';
+
+			const newClassName = clsx(
+				baseClassName,
+				newAlignValue === 'justify' && 'has-text-align-justify'
+			);
+
 			return updateBlockAttributes( blockId, {
-				align: deprecatedIsBlockJustified ? null : 'justify',
+				align: newAlignValue,
+				className: newClassName || undefined,
 			} );
 		}
 
+		const newTextAlignValue = isBlockJustified ? undefined : 'justify';
+
+		const newClassName = clsx(
+			baseClassName,
+			newTextAlignValue === 'justify' && 'has-text-align-justify'
+		);
+
+		const typographyAttributes = {
+			...styleAttributes.typography,
+			textAlign: newTextAlignValue,
+		};
+
+		const newStyleAttributes = {
+			...styleAttributes,
+			typography: objectHasValues( typographyAttributes ) ? typographyAttributes : undefined,
+		};
+
 		updateBlockAttributes( blockId, {
-			style: {
-				...styleAttributes,
-				typography: {
-					...styleAttributes.typography,
-					textAlign: isBlockJustified ? null : 'justify',
-				},
-			},
+			style: objectHasValues( newStyleAttributes ) ? newStyleAttributes : undefined,
+			className: newClassName || undefined,
 		} );
 	};
 
@@ -58,6 +87,7 @@ const ConnectedRichTextJustifyButton = compose(
 			blockName: selectedBlock.name,
 			deprecatedIsBlockJustified: 'justify' === selectedBlock.attributes.align,
 			styleAttributes: selectedBlock.attributes.style || EMPTY_STYLES,
+			className: selectedBlock.attributes.className || '',
 		};
 	} ),
 	withDispatch( ( dispatch ) => ( {
