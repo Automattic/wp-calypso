6a0c0d9f11271a5732195988f339f15e4f65493a
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-11-26 20:21:36 +0700

    Simplify feature list construction

--- client/dashboard/sites/hosting-feature-list/index.tsx
+++ client/dashboard/sites/hosting-feature-list/index.tsx
@@ -5,49 +5,46 @@ import { hasPlanFeature } from '../../utils/site-features';
 
 export default function HostingFeatureList( { site }: { site: Site } ) {
 	const features = [
-		hasPlanFeature( site, HostingFeatures.DEPLOYMENT ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Git-based deployments' ) }
-			</Text>
-		),
-		hasPlanFeature( site, HostingFeatures.MONITOR ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Server monitoring' ) }
-			</Text>
-		),
-		hasPlanFeature( site, HostingFeatures.LOGS ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Access and error logs' ) }
-			</Text>
-		),
-		hasPlanFeature( site, HostingFeatures.SSH ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Secure access via SFTP/SSH' ) }
-			</Text>
-		),
-		hasPlanFeature( site, HostingFeatures.SFTP ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Advanced server settings' ) }
-			</Text>
-		),
-		hasPlanFeature( site, HostingFeatures.BACKUPS ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Backup and restore' ) }
-			</Text>
-		),
-		hasPlanFeature( site, HostingFeatures.SCAN ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Security scans' ) }
-			</Text>
-		),
-		hasPlanFeature( site, DotcomFeatures.ATOMIC ) && (
-			<Text as="li" variant="muted">
-				{ __( 'Install plugins and themes' ) }
-			</Text>
-		),
+		{
+			feature: HostingFeatures.DEPLOYMENT,
+			text: __( 'Git-based deployments' ),
+		},
+		{
+			feature: HostingFeatures.MONITOR,
+			text: __( 'Server monitoring' ),
+		},
+		{
+			feature: HostingFeatures.LOGS,
+			text: __( 'Access and error logs' ),
+		},
+		{
+			feature: HostingFeatures.SSH,
+			text: __( 'Secure access via SFTP/SSH' ),
+		},
+		{
+			feature: HostingFeatures.SFTP,
+			text: __( 'Advanced server settings' ),
+		},
+		{
+			feature: HostingFeatures.BACKUPS,
+			text: __( 'Backup and restore' ),
+		},
+		{
+			feature: HostingFeatures.SCAN,
+			text: __( 'Security scans' ),
+		},
+		{
+			feature: DotcomFeatures.ATOMIC,
+			text: __( 'Install plugins and themes' ),
+		},
 	]
-		.filter( Boolean )
-		.slice( 0, 5 );
+		.filter( ( { feature } ) => hasPlanFeature( site, feature ) )
+		.slice( 0, 5 )
+		.map( ( { text } ) => (
+			<Text as="li" key={ text } variant="muted">
+				{ text }
+			</Text>
+		) );
 
 	return <ul style={ { paddingInlineStart: '15px', margin: 0 } }>{ features }</ul>;
 }
