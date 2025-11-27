cea16e51f8bc14a244256cb2c99e6746d82c9b14
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2025-11-27 16:21:30 +0100

    Plugins Dashboard: Make `Delete` action available at all times

--- client/dashboard/plugins/manage/actions/delete.tsx
+++ client/dashboard/plugins/manage/actions/delete.tsx
@@ -1,4 +1,10 @@
-import { sitePluginRemoveMutation } from '@automattic/api-queries';
+import {
+	invalidatePlugins,
+	invalidateSitePlugins,
+	sitePluginAutoupdateDisableMutation,
+	sitePluginDeactivateMutation,
+	sitePluginRemoveMutation,
+} from '@automattic/api-queries';
 import { useMutation } from '@tanstack/react-query';
 import { __ } from '@wordpress/i18n';
 import ActionRenderModal, { getModalHeader } from '../components/action-render-modal';
@@ -12,8 +18,40 @@ export const deleteAction: Action< PluginListRow > = {
 	isPrimary: false,
 	modalHeader: getModalHeader( 'delete' ),
 	RenderModal: ( { items, closeModal, onActionPerformed } ) => {
-		const { mutateAsync } = useMutation( sitePluginRemoveMutation() );
-		const action = buildBulkSitesPluginAction( mutateAsync );
+		const { mutateAsync: deactivate } = useMutation( {
+			...sitePluginDeactivateMutation(),
+			onSuccess: () => {},
+		} );
+		const { mutateAsync: disableAutoupdate } = useMutation( {
+			...sitePluginAutoupdateDisableMutation(),
+			onSuccess: () => {},
+		} );
+		const { mutateAsync: remove } = useMutation( {
+			...sitePluginRemoveMutation(),
+			onSuccess: () => {},
+		} );
+
+		const action = async ( items: PluginListRow[] ) => {
+			const bulkDeactivate = buildBulkSitesPluginAction( deactivate );
+			const bulkDisableAutoupdate = buildBulkSitesPluginAction( disableAutoupdate );
+			const bulkRemove = buildBulkSitesPluginAction( remove );
+
+			// First deactivate all plugins
+			await bulkDeactivate( items );
+			// Then disable auto-updates
+			await bulkDisableAutoupdate( items );
+			// Finally remove the plugins
+			const { successCount, errorCount } = await bulkRemove( items );
+
+			items
+				.flatMap( ( item ) => item.siteIds )
+				.forEach( ( siteId ) => {
+					invalidateSitePlugins( siteId );
+				} );
+			invalidatePlugins();
+
+			return { successCount, errorCount };
+		};
 
 		return (
 			<ActionRenderModal
@@ -24,8 +62,5 @@ export const deleteAction: Action< PluginListRow > = {
 				onExecute={ action }
 			/>
 		);
-	},
-	isEligible: ( item: PluginListRow ) => {
-		return item.isActive === 'none';
 	},
 };
