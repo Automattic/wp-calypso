63e15880e0a90a24b670c23bf8fc71f8ba81abba
heavyweight <kpapazov@gmail.com>
2025-11-27 18:48:21 +0100

    optimize calculation

--- packages/odie-client/src/utils/chat-utils.ts
+++ packages/odie-client/src/utils/chat-utils.ts
@@ -4,16 +4,29 @@ import type { Chat } from '../types';
 const MAX_ESCALATION_ATTEMPT_TIME = 3 * 24 * 60 * 60 * 1000; // three days
 
 export const hasRecentEscalationAttempt = ( chat: Chat ) => {
-	return (
-		chat?.messages?.some( ( message ) => {
-			if ( ! message.context?.flags?.forward_to_human_support || ! message.created_at ) {
-				return false;
-			}
+	if ( ! chat?.messages?.length ) {
+		return false;
+	}
 
-			const messageTimestamp = getTimestamp( message.created_at ) * 1000;
-			const threeDaysAgo = Date.now() - MAX_ESCALATION_ATTEMPT_TIME;
+	const threeDaysAgo = Date.now() - MAX_ESCALATION_ATTEMPT_TIME;
 
-			return messageTimestamp >= threeDaysAgo;
-		} ) ?? false
-	);
+	for ( let i = chat.messages.length - 1; i >= 0; i-- ) {
+		const message = chat.messages[ i ];
+
+		if ( ! message.created_at ) {
+			continue;
+		}
+
+		const messageTimestamp = getTimestamp( message.created_at ) * 1000;
+
+		if ( messageTimestamp < threeDaysAgo ) {
+			break;
+		}
+
+		if ( message.context?.flags?.forward_to_human_support ) {
+			return true;
+		}
+	}
+
+	return false;
 };
