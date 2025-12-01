5df37821ecd5114295acc06a7ba31c6dc376dbd9
Rafael Agostini <rafael.agostini@automattic.com>
2025-12-01 17:02:06 -0500

    Add fillHeight prop to DataViewsCard for vertical centering

--- client/dashboard/components/dataviews/dataviews-card.tsx
+++ client/dashboard/components/dataviews/dataviews-card.tsx
@@ -2,12 +2,18 @@ import { forwardRef } from 'react';
 import { Card, CardBody } from '../card';
 
 function UnforwardedDataViewsCard(
-	{ className, children }: { className?: string; children: React.ReactNode },
+	{
+		className,
+		children,
+		fillHeight,
+	}: { className?: string; children: React.ReactNode; fillHeight?: boolean },
 	ref: React.ForwardedRef< HTMLDivElement >
 ) {
+	const bodyStyle = fillHeight ? { height: '100%' } : undefined;
+
 	return (
 		<Card ref={ ref } className={ className }>
-			<CardBody>{ children }</CardBody>
+			<CardBody style={ bodyStyle }>{ children }</CardBody>
 		</Card>
 	);
 }
