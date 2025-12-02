c58f5a715466ca65ca33dce3add78668242095eb
Claudiu Filip <claudiu.filip@automattic.com>
2025-12-02 10:42:37 +0200

    Restore all categories

--- client/my-sites/plugins/plugins-browser/index.jsx
+++ client/my-sites/plugins/plugins-browser/index.jsx
@@ -41,15 +41,6 @@ import SearchCategories from '../search-categories';
 import './style.scss';
 
 const MASTERBAR_HEIGHT = 32;
-const CATEGORIES = [
-	'discover',
-	'design',
-	'marketing',
-	'ecommerce',
-	'photo',
-	'security',
-	'analytics',
-];
 
 // If adding new, longer search terms, ensure that the search input field is wide enough to accommodate it.
 const searchTerms = [ 'woocommerce', 'seo', 'file manager', 'jetpack', 'ecommerce', 'form' ];
@@ -240,15 +231,7 @@ const PluginsBrowser = ( { trackPageViews = true, category, search } ) => {
 						</FullWidthSection>
 						<FullWidthSection className="plugins-browser__categories">
 							<div ref={ categoriesRef }>
-								<Categories
-									selected={ category }
-									noSelection={ !! search }
-									{ ...( isMarketplaceRedesignEnabled && {
-										categories: CATEGORIES,
-										forceSwipe: true,
-										swipeEnabled: false,
-									} ) }
-								/>
+								<Categories selected={ category } noSelection={ !! search } />
 							</div>
 						</FullWidthSection>
 					</>
