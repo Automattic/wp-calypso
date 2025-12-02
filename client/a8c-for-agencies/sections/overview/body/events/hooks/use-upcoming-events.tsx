48f6d75207792f2919c688a96a8ff8a22e13ccf1
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-12-02 21:04:49 +0800

    Add A4A Partner Survey upcoming event.

--- client/a8c-for-agencies/sections/overview/body/events/hooks/use-upcoming-events.tsx
+++ client/a8c-for-agencies/sections/overview/body/events/hooks/use-upcoming-events.tsx
@@ -1,6 +1,7 @@
 import { useTranslate } from 'i18n-calypso';
 import moment from 'moment';
 import { useMemo } from 'react';
+import A4ALogo from 'calypso/a8c-for-agencies/components/a4a-logo';
 import { UpcomingEventProps } from 'calypso/a8c-for-agencies/components/upcoming-event/types';
 import avalaraLogo from 'calypso/assets/images/a8c-for-agencies/events/avalara-logo.svg';
 import pressableLogo from 'calypso/assets/images/a8c-for-agencies/events/pressable-logo.svg';
@@ -12,6 +13,28 @@ export const useUpcomingEvents = () => {
 
 	return useMemo( () => {
 		const eventsData: UpcomingEventProps[] = [
+			{
+				id: 'a4a-partner-survey-2025-12-03',
+				date: {
+					from: moment( '2026-01-01' ),
+					to: moment( '2026-01-01' ),
+				},
+				displayDate: translate( 'Open until January 1, 2026' ),
+				title: translate( 'Automattic for Agencies Partner Survey' ),
+				subtitle: translate( 'Automattic for Agencies' ),
+				descriptions: [
+					translate(
+						"We invite you to share your input in our short Automattic for Agencies Partner Survey. Your feedback will help us better understand your experience with Automattic for Agencies and the products you use across our ecosystem. The survey is anonymous, and your insights will guide how we shape next year's incentives, tools, and product improvements to create more value for your agency and clients."
+					),
+				],
+				cta: {
+					label: translate( 'Take the survey now! ↗' ),
+					url: 'https://a4aprogramming.wordpress.com/2025/11/14/2025-automattic-for-agencies-satisfaction-survey/',
+				},
+				logoElement: <A4ALogo size={ 64 } />,
+				trackEventName: 'calypso_a4a_overview_events_a4a_partner_survey_2025_12_03_click',
+				dateClassName: 'a4a-event__date--neutral',
+			},
 			{
 				id: 'pressable-webinar-2025-11-06',
 				date: {
