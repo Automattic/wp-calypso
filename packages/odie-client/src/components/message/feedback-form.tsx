c69a9cae229c2ddf15459c90969bb17b882ddd88
heavyweight <kpapazov@gmail.com>
2025-12-03 13:10:39 +0100

    Apply design feedback

--- packages/odie-client/src/components/message/feedback-form.tsx
+++ packages/odie-client/src/components/message/feedback-form.tsx
@@ -45,7 +45,7 @@ export const FeedbackForm = ( { chatFeedbackOptions }: FeedbackFormProps ) => {
 				content:
 					score === 'good'
 						? __( 'Good 👍', __i18n_text_domain__ )
-						: __( 'Bad 👎', __i18n_text_domain__ ),
+						: __( 'Needs improvement 👎', __i18n_text_domain__ ),
 				payload: JSON.stringify( { csat_rating: score.toUpperCase() } ),
 				metadata: { rated: true },
 				role: 'user',
@@ -111,7 +111,7 @@ export const FeedbackForm = ( { chatFeedbackOptions }: FeedbackFormProps ) => {
 						<div>
 							{ score === 'good'
 								? __( 'Good 👍', __i18n_text_domain__ )
-								: __( 'Bad 👎', __i18n_text_domain__ ) }
+								: __( 'Needs improvement 👎', __i18n_text_domain__ ) }
 						</div>
 					</div>
 
@@ -123,10 +123,9 @@ export const FeedbackForm = ( { chatFeedbackOptions }: FeedbackFormProps ) => {
 
 					{ ! isFormHidden && (
 						<div ref={ feedbackRef } className="odie-conversation-feedback__message">
-							<h3>{ __( 'Thank you for your input', __i18n_text_domain__ ) }</h3>
 							<p>
 								{ __(
-									'Please share any details that can help us understand your rating',
+									'Thank you for your input. Please share any details that can help us understand your rating.',
 									__i18n_text_domain__
 								) }
 							</p>
