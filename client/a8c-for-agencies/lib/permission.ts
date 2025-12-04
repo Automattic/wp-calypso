f1a69ceb2f238ee2def150035ee8260ca05940c0
Tiago Noronha <hello@tiagonoronha.com>
2025-12-04 11:36:21 +0000

    Add Learn > Resource Center section

--- client/a8c-for-agencies/lib/permission.ts
+++ client/a8c-for-agencies/lib/permission.ts
@@ -54,6 +54,8 @@ import {
 	A4A_WOOPAYMENTS_SITE_SETUP_LINK,
 	A4A_WOOPAYMENTS_OVERVIEW_LINK,
 	A4A_PARTNER_OFFERS_LINK,
+	A4A_LEARN_LINK,
+	A4A_LEARN_RESOURCE_CENTER_LINK,
 } from '../components/sidebar-menu/lib/constants';
 import type { Agency } from 'calypso/state/a8c-for-agencies/types';
 
@@ -112,6 +114,8 @@ const MEMBER_ACCESSIBLE_PATHS: Record< string, string[] > = {
 	[ A4A_WOOPAYMENTS_OVERVIEW_LINK ]: [ 'a4a_read_referrals' ],
 	// FIXME: Add the correct capability for Partner Offers
 	[ A4A_PARTNER_OFFERS_LINK ]: [ 'a4a_read_marketplace' ],
+	[ A4A_LEARN_LINK ]: [ 'a4a_read_learn' ],
+	[ A4A_LEARN_RESOURCE_CENTER_LINK ]: [ 'a4a_read_learn' ],
 };
 
 const MEMBER_ACCESSIBLE_DYNAMIC_PATHS: Record< string, string[] > = {
