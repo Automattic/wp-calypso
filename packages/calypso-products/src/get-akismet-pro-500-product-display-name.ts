8f31df191a7ab412a6f76c6000800bbe269f793a
aperdomo <andy.perdomo@automattic.com>
2025-12-04 15:12:10 -0800

    Whitespace cleanup.

--- packages/calypso-products/src/get-akismet-pro-500-product-display-name.ts
+++ packages/calypso-products/src/get-akismet-pro-500-product-display-name.ts
@@ -1,6 +1,6 @@
 import { translate } from 'i18n-calypso';
 
-export function getAkismetPro500ProductDisplayName(productName: string, quantity: number | null ) {
+export function getAkismetPro500ProductDisplayName( productName: string, quantity: number | null ) {
 	if ( quantity && quantity > 1 ) {
 		/* translators: %s is the product name "Akismet Pro", %d is a number of requests/month */
 		return translate( '%(productName)s (%(requests)d requests/month)', {
