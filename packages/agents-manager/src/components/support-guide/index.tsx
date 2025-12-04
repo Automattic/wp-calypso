5e18907e580cbd649bc857f7ab6a64035e7e5196
Omar Alshaker <omar@omaralshaker.com>
2025-12-04 01:52:45 +0100

    CSS

--- packages/agents-manager/src/components/support-guide/index.tsx
+++ packages/agents-manager/src/components/support-guide/index.tsx
@@ -4,7 +4,11 @@ import './style.scss';
 export default function SupportGuide() {
 	return (
 		<div className="agenttic agent-manager-support-guide-wrapper">
-			<HelpCenterArticle />
+			<div className="agent-manager-support-guide-header">Header</div>
+			<div className="agent-manager-support-guide-content">
+				<HelpCenterArticle />
+			</div>
+			<div className="agent-manager-support-guide-header">Footer</div>
 		</div>
 	);
 }
