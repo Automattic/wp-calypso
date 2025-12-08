0aaa1c0953ee8fe7ce03e688b2fcf1212eb50d6b
Emanuele Buccelli <escapemanuele@gmail.com>
2025-12-08 16:46:39 +0100

    Pass query on new chat

--- packages/help-center/src/components/help-center-footer.tsx
+++ packages/help-center/src/components/help-center-footer.tsx
@@ -1,9 +1,12 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { HelpCenterSelect } from '@automattic/data-stores';
 import { Button, CardFooter } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useHelpCenterContext } from '../contexts/HelpCenterContext';
 import { useStillNeedHelpURL } from '../hooks';
+import { HELP_CENTER_STORE } from '../stores';
 
 import './help-center-footer.scss';
 
@@ -13,6 +16,10 @@ export const HelpCenterContactButton = () => {
 	const { sectionName } = useHelpCenterContext();
 	const redirectToWpcom = url === 'https://wordpress.com/help/contact';
 	const navigate = useNavigate();
+	const searchQuery = useSelect(
+		( select ) => ( select( HELP_CENTER_STORE ) as HelpCenterSelect ).getMessage(),
+		[]
+	);
 
 	const handleClick = () => {
 		recordTracksEvent( 'calypso_inlinehelp_morehelp_click', {
@@ -22,7 +29,13 @@ export const HelpCenterContactButton = () => {
 			button_type: 'Still need help?',
 		} );
 
-		const to = redirectToWpcom ? { pathname: url } : url;
+		let to: string | { pathname: string } = redirectToWpcom ? { pathname: url } : url;
+
+		// Pass search query to chat if navigating to /odie
+		if ( url === '/odie' && searchQuery ) {
+			to = `/odie?query=${ encodeURIComponent( searchQuery ) }`;
+		}
+
 		navigate( to );
 	};
 
