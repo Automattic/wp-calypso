dd3ee54ff708a164cc5c0a2e3c8850fd05d68a33
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-12-09 17:17:48 +0800

    Create reusable hook for toggling the Help center.

--- client/a8c-for-agencies/components/sidebar/header/help-center.tsx
+++ client/a8c-for-agencies/components/sidebar/header/help-center.tsx
@@ -1,43 +1,15 @@
-import { HelpCenter } from '@automattic/data-stores';
 import { HelpIcon } from '@automattic/help-center';
 import { Button } from '@wordpress/components';
-import {
-	useDispatch as useDataStoreDispatch,
-	useSelect as useDateStoreSelect,
-} from '@wordpress/data';
 import clsx from 'clsx';
-import type { HelpCenterSelect } from '@automattic/data-stores';
+import useHelpCenter from 'calypso/a8c-for-agencies/hooks/use-help-center';
 
-const HELP_CENTER_STORE = HelpCenter.register();
-
-type Props = {
-	onClick?: () => void;
-};
-
-const SidebarHelpCenter = ( { onClick }: Props ) => {
-	const { show, isMinimized } = useDateStoreSelect( ( select ) => {
-		const store = select( HELP_CENTER_STORE ) as HelpCenterSelect;
-		return {
-			show: store.isHelpCenterShown(),
-			isMinimized: store.getIsMinimized(),
-		};
-	}, [] );
-
-	const { setShowHelpCenter, setIsMinimized } = useDataStoreDispatch( HELP_CENTER_STORE );
-
-	const handleToggleHelpCenter = () => {
-		if ( isMinimized ) {
-			setIsMinimized( false );
-		} else {
-			setShowHelpCenter( ! show );
-		}
-		onClick?.();
-	};
+const SidebarHelpCenter = () => {
+	const { toggleHelpCenter, show } = useHelpCenter();
 
 	return (
 		<>
 			<Button
-				onClick={ handleToggleHelpCenter }
+				onClick={ toggleHelpCenter }
 				className={ clsx( 'sidebar__item-help', {
 					'is-active': show,
 				} ) }
---
+++ client/a8c-for-agencies/hooks/use-help-center.ts
@@ -0,0 +1,35 @@
+import { HelpCenter } from '@automattic/data-stores';
+import {
+	useDispatch as useDataStoreDispatch,
+	useSelect as useDateStoreSelect,
+} from '@wordpress/data';
+import type { HelpCenterSelect } from '@automattic/data-stores';
+
+const HELP_CENTER_STORE = HelpCenter.register();
+
+export default function useHelpCenter() {
+	const { show, isMinimized } = useDateStoreSelect( ( select ) => {
+		const store = select( HELP_CENTER_STORE ) as HelpCenterSelect;
+		return {
+			show: store.isHelpCenterShown(),
+			isMinimized: store.getIsMinimized(),
+		};
+	}, [] );
+
+	const { setShowHelpCenter, setIsMinimized } = useDataStoreDispatch( HELP_CENTER_STORE );
+
+	const handleToggleHelpCenter = () => {
+		if ( isMinimized ) {
+			setIsMinimized( false );
+		} else {
+			setShowHelpCenter( ! show );
+		}
+		onClick?.();
+	};
+
+	return {
+		toggleHelpCenter: handleToggleHelpCenter,
+		show,
+		isMinimized,
+	};
+}
