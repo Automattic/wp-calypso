8eca4567cfc982a9b8360dd244e35a541d8379d3
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-12-09 18:22:52 +0800

    Add tooltip and tracking on the new Help center button.

--- client/a8c-for-agencies/components/sidebar/header/help-center.tsx
+++ client/a8c-for-agencies/components/sidebar/header/help-center.tsx
@@ -1,21 +1,30 @@
 import { HelpIcon } from '@automattic/help-center';
-import { Button } from '@wordpress/components';
+import { Button, Tooltip } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import clsx from 'clsx';
 import useHelpCenter from 'calypso/a8c-for-agencies/hooks/use-help-center';
+import { useDispatch } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 
 const SidebarHelpCenter = () => {
 	const { toggleHelpCenter, show } = useHelpCenter();
+	const dispatch = useDispatch();
+
+	const handleOnClick = () => {
+		dispatch( recordTracksEvent( 'calypso_a4a_sidebar_help_center_click' ) );
+		toggleHelpCenter();
+	};
 
 	return (
-		<>
+		<Tooltip text={ __( 'Open Help Center' ) }>
 			<Button
-				onClick={ toggleHelpCenter }
+				onClick={ handleOnClick }
 				className={ clsx( 'sidebar__item-help', {
 					'is-active': show,
 				} ) }
 				icon={ <HelpIcon /> }
 			/>
-		</>
+		</Tooltip>
 	);
 };
 
