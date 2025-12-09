7e2afffb07e7f2c66c10caf91f1425d2f2ed3f43
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-12-09 18:01:29 +0800

    Activate Help center when Contact support hash is triggered.

--- client/a8c-for-agencies/hooks/use-help-center.ts
+++ client/a8c-for-agencies/hooks/use-help-center.ts
@@ -3,6 +3,12 @@ import {
 	useDispatch as useDataStoreDispatch,
 	useSelect as useDateStoreSelect,
 } from '@wordpress/data';
+import { useEffect } from 'react';
+import {
+	CONTACT_URL_FOR_MIGRATION_OFFER_HASH_FRAGMENT,
+	CONTACT_URL_HASH_FRAGMENT,
+	CONTACT_URL_HASH_FRAGMENT_WITH_PRODUCT,
+} from '../components/a4a-contact-support-widget';
 import type { HelpCenterSelect } from '@automattic/data-stores';
 
 const HELP_CENTER_STORE = HelpCenter.register();
@@ -24,13 +30,35 @@ export default function useHelpCenter() {
 			setIsMinimized( false );
 		} else {
 			setShowHelpCenter( ! show );
+			setNavigateToRoute( '/' );
 		}
 	};
 
+	const hasSupportFormHash =
+		window.location.hash === CONTACT_URL_HASH_FRAGMENT ||
+		window.location.hash === CONTACT_URL_HASH_FRAGMENT_WITH_PRODUCT ||
+		window.location.hash === CONTACT_URL_FOR_MIGRATION_OFFER_HASH_FRAGMENT;
+
+	useEffect( () => {
+		// We support URL hash fragments for the contact support form.
+		// When the hash changes, we need to check if it contains a support form hash fragment.
+		// If it does, we need to set the show help center to true and set the navigate to route to the contact form.
+		const handleHashChange = () => {
+			if ( hasSupportFormHash ) {
+				setShowHelpCenter( true );
+				setNavigateToRoute( '/a4a-contact-form' );
+				history.pushState( null, '', window.location.pathname + window.location.search );
+			}
+		};
+
+		window.addEventListener( 'hashchange', handleHashChange );
+		handleHashChange();
+
+		return () => window.removeEventListener( 'hashchange', handleHashChange );
+	}, [ hasSupportFormHash, setNavigateToRoute, setShowHelpCenter ] );
+
 	return {
 		toggleHelpCenter: handleToggleHelpCenter,
-		setNavigateToRoute,
 		show,
-		isMinimized,
 	};
 }
