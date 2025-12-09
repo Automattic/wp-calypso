899520e3f5899bda3e7657134783aea9d87017ec
Kateryna Kodonenko <kateryna@automattic.com>
2025-12-09 12:46:31 +0100

    Create two versions for the user profile

--- client/oauth2/components/user-card.tsx
+++ client/oauth2/components/user-card.tsx
@@ -1,30 +1,54 @@
+import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
 import Gravatar from 'calypso/components/gravatar';
 import type { AuthorizeMeta } from '../hooks/use-authorize-meta';
 
 interface UserCardProps {
 	user: NonNullable< AuthorizeMeta[ 'user' ] >;
+	variant?: 'horizontal' | 'centered';
 }
 
-const UserCard = ( { user }: UserCardProps ) => {
+const UserCard = ( { user, variant = 'horizontal' }: UserCardProps ) => {
 	const translate = useTranslate();
 
+	const isCentered = variant === 'centered';
+
+	const getUserDetails = () => {
+		if ( isCentered ) {
+			return user.email;
+		}
+
+		if ( user.username && user.site_count !== undefined ) {
+			return translate( '%(username)s - %(count)d site', '%(username)s - %(count)d sites', {
+				count: user.site_count,
+				args: {
+					username: user.username,
+					count: user.site_count,
+				},
+			} );
+		}
+
+		return user.email;
+	};
+
 	return (
-		<div className="oauth2-connect__user-card">
-			<Gravatar user={ user } size={ 72 } imgSize={ 144 } className="oauth2-connect__user-avatar" />
+		<div
+			className={ clsx( 'oauth2-connect__user-card', {
+				'oauth2-connect__user-card-centered': isCentered,
+				'oauth2-connect__user-card-horizontal': ! isCentered,
+			} ) }
+		>
+			<Gravatar
+				user={ user }
+				size={ isCentered ? 80 : 72 }
+				imgSize={ isCentered ? 160 : 144 }
+				className={ clsx( 'oauth2-connect__user-avatar', {
+					'oauth2-connect__user-avatar-centered': isCentered,
+				} ) }
+			/>
 			<div className="oauth2-connect__user-info">
 				<div className="oauth2-connect__user-name">{ user.display_name }</div>
-				<div className="oauth2-connect__user-details">
-					{ user.username && user.site_count !== undefined
-						? translate( '%(username)s - %(count)d site', '%(username)s - %(count)d sites', {
-								count: user.site_count,
-								args: {
-									username: user.username,
-									count: user.site_count,
-								},
-						  } )
-						: user.email }
-				</div>
+				<div className="oauth2-connect__user-details">{ getUserDetails() }</div>
 			</div>
 		</div>
 	);
