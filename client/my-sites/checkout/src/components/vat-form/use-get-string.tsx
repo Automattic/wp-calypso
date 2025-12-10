2efa55af87a6d2d976680ad4b4673d86b50bdc76
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2025-12-10 08:40:27 +0100

    Improve translator comments

--- client/my-sites/checkout/src/components/vat-form/use-get-string.tsx
+++ client/my-sites/checkout/src/components/vat-form/use-get-string.tsx
@@ -35,115 +35,115 @@ export function useGetVatFormString( countryCode: string | undefined ) {
 
 	const VAT_STRINGS: VatStringsValue = {
 		addVatCheckboxLabel: {
-			/* translators: This is a checkbox label. CT is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a checkbox label. CT is a country-specific tax abbreviation (Colombia Consumption Tax). Use the local name for this tax in your language. Most translators should NOT translate this, but if your locale uses a common translated term (e.g., "moms" in Swedish), use that instead. */
 			CT: translate( 'Add CT details', {
 				textOnly: true,
 			} ),
-			/* translators: This is a checkbox label. GST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a checkbox label. GST is a country-specific tax abbreviation (Goods and Services Tax, used in Canada, Australia, etc). Use the local name for this tax in your language. Examples: "TPS/TVH" (Quebec/Canada), "GST" (Australia). */
 			GST: translate( 'Add GST details', {
 				textOnly: true,
 			} ),
-			/* translators: This is a checkbox label. SST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a checkbox label. SST is a country-specific tax abbreviation (Sales and Services Tax, used in Malaysia). Use the local name for this tax in your language. */
 			SST: translate( 'Add SST details', {
 				textOnly: true,
 			} ),
-			/* translators: This is a checkbox label. VAT is Value-Added Tax. Translate to something that makes sense in the current locale */
+			/* translators: This is a checkbox label. VAT (Value-Added Tax) is used in most European countries and beyond. Use the local abbreviation or term. Examples: "TVA" (French), "IVA" (Spanish/Portuguese), "moms" (Swedish), "MwSt" (German). */
 			VAT: translate( 'Add VAT details', {
 				textOnly: true,
 			} ),
-			/* translators: This is a checkbox label */
+			/* translators: This is a checkbox label. Use this string only if the UI doesn't match a specific country's tax type. List the common tax names appropriate for your locale. */
 			fallback: translate( 'Add tax (VAT/GST/CT) details', {
 				textOnly: true,
 			} ),
 		},
 		organizationFieldLabel: {
-			/* translators: This is a form field label. CT is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. CT is a country-specific tax abbreviation (Colombia Consumption Tax). Use the local name for this tax in your language. Most translators should NOT translate this, but if your locale uses a common translated term (e.g., "moms" in Swedish), use that instead. */
 			CT: translate( 'Organization for CT', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. GST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. GST is a country-specific tax abbreviation (Goods and Services Tax, used in Canada, Australia, etc). Use the local name for this tax in your language. Examples: "TPS/TVH" (Quebec/Canada), "GST" (Australia). */
 			GST: translate( 'Organization for GST', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. SST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. SST is a country-specific tax abbreviation (Sales and Services Tax, used in Malaysia). Use the local name for this tax in your language. */
 			SST: translate( 'Organization for SST', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. VAT is Value-Added Tax. Translate to something that makes sense in the current locale */
+			/* translators: This is a form field label. VAT (Value-Added Tax) is used in most European countries and beyond. Use the local abbreviation or term. Examples: "TVA" (French), "IVA" (Spanish/Portuguese), "moms" (Swedish), "MwSt" (German). */
 			VAT: translate( 'Organization for VAT', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label */
+			/* translators: This is a form field label. Use this string only if the UI doesn't match a specific country's tax type. List the common tax names appropriate for your locale. */
 			fallback: translate( 'Organization for tax (VAT/GST/CT)', {
 				textOnly: true,
 			} ),
 		},
 		vatIdFieldLabel: {
-			/* translators: This is a form field label. CT is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. CT is a country-specific tax abbreviation (Colombia Consumption Tax). Use the local name for this tax in your language. Most translators should NOT translate this, but if your locale uses a common translated term (e.g., "moms" in Swedish), use that instead. */
 			CT: translate( 'CT ID', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. GST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. GST is a country-specific tax abbreviation (Goods and Services Tax, used in Canada, Australia, etc). Use the local name for this tax in your language. Examples: "TPS/TVH" (Quebec/Canada), "GST" (Australia). */
 			GST: translate( 'GST ID', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. SST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. SST is a country-specific tax abbreviation (Sales and Services Tax, used in Malaysia). Use the local name for this tax in your language. */
 			SST: translate( 'SST ID', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. VAT is Value-Added Tax. Translate to something that makes sense in the current locale */
+			/* translators: This is a form field label. VAT (Value-Added Tax) is used in most European countries and beyond. Use the local abbreviation or term. Examples: "TVA" (French), "IVA" (Spanish/Portuguese), "moms" (Swedish), "MwSt" (German). */
 			VAT: translate( 'VAT ID', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label */
+			/* translators: This is a form field label. Use this string only if the UI doesn't match a specific country's tax type. List the common tax names appropriate for your locale. */
 			fallback: translate( 'Tax ID (VAT/GST/CT)', {
 				textOnly: true,
 			} ),
 		},
 		vatAddressFieldLabel: {
-			/* translators: This is a form field label. CT is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. CT is a country-specific tax abbreviation (Colombia Consumption Tax). Use the local name for this tax in your language. Most translators should NOT translate this, but if your locale uses a common translated term (e.g., "moms" in Swedish), use that instead. */
 			CT: translate( 'Address for CT', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. GST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. GST is a country-specific tax abbreviation (Goods and Services Tax, used in Canada, Australia, etc). Use the local name for this tax in your language. Examples: "TPS/TVH" (Quebec/Canada), "GST" (Australia). */
 			GST: translate( 'Address for GST', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. SST is a localized name of Value-Added Tax (VAT). Preserve the name of the tax in the translation */
+			/* translators: This is a form field label. SST is a country-specific tax abbreviation (Sales and Services Tax, used in Malaysia). Use the local name for this tax in your language. */
 			SST: translate( 'Address for SST', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label. VAT is Value-Added Tax. Translate to something that makes sense in the current locale */
+			/* translators: This is a form field label. VAT (Value-Added Tax) is used in most European countries and beyond. Use the local abbreviation or term. Examples: "TVA" (French), "IVA" (Spanish/Portuguese), "moms" (Swedish), "MwSt" (German). */
 			VAT: translate( 'Address for VAT', {
 				textOnly: true,
 			} ),
-			/* translators: This is a form field label */
+			/* translators: This is a form field label. Use this string only if the UI doesn't match a specific country's tax type. List the common tax names appropriate for your locale. */
 			fallback: translate( 'Address for tax (VAT/GST/CT)', {
 				textOnly: true,
 			} ),
 		},
 		vatIdChangeExplanation: {
-			/* translators: This is a form field label. CT is a localized name of Value-Added Tax (VAT). Translate to something sensible for the current locale */
+			/* translators: This is an explanation message. CT is a country-specific tax abbreviation (Colombia Consumption Tax). Use the local name for this tax in your language. Most translators should NOT translate this, but if your locale uses a common translated term (e.g., "moms" in Swedish), use that instead. */
 			CT: translate(
 				'To change your CT ID, {{contactSupportLink}}please contact support{{/contactSupportLink}}.',
 				{ components: { contactSupportLink } }
 			),
-			/* translators: This is a form field label. GST is a localized name of Value-Added Tax (VAT). Translate to something sensible for the current locale */
+			/* translators: This is an explanation message. GST is a country-specific tax abbreviation (Goods and Services Tax, used in Canada, Australia, etc). Use the local name for this tax in your language. Examples: "TPS/TVH" (Quebec/Canada), "GST" (Australia). */
 			GST: translate(
 				'To change your GST ID, {{contactSupportLink}}please contact support{{/contactSupportLink}}.',
 				{ components: { contactSupportLink } }
 			),
-			/* translators: This is a form field label. SST is a localized name of Value-Added Tax (VAT). Translate to something sensible for the current locale */
+			/* translators: This is an explanation message. SST is a country-specific tax abbreviation (Sales and Services Tax, used in Malaysia). Use the local name for this tax in your language. */
 			SST: translate(
 				'To change your SST ID, {{contactSupportLink}}please contact support{{/contactSupportLink}}.',
 				{ components: { contactSupportLink } }
 			),
-			/* translators: This is a form field label. VAT is Value-Added Tax. Translate to something that makes sense in the current locale */
+			/* translators: This is an explanation message. VAT (Value-Added Tax) is used in most European countries and beyond. Use the local abbreviation or term. Examples: "TVA" (French), "IVA" (Spanish/Portuguese), "moms" (Swedish), "MwSt" (German). */
 			VAT: translate(
 				'To change your VAT ID, {{contactSupportLink}}please contact support{{/contactSupportLink}}.',
 				{ components: { contactSupportLink } }
 			),
-			/* translators: This is a form field label */
+			/* translators: This is an explanation message. Use this string only if the UI doesn't match a specific country's tax type. List the common tax names appropriate for your locale. */
 			fallback: translate(
 				'To change your tax ID, {{contactSupportLink}}please contact support{{/contactSupportLink}}.',
 				{ components: { contactSupportLink } }
