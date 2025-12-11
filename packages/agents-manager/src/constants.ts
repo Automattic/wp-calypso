4de8b2cf23a043e607ad175824a65ae10476ec95
Welly Shen <welly.shen@automattic.com>
2025-12-12 00:24:28 +0800

    Format code

--- packages/agents-manager/src/constants.ts
+++ packages/agents-manager/src/constants.ts
@@ -1,4 +1,5 @@
 export const API_BASE_URL = 'https://public-api.wordpress.com';
+
 export const ORCHESTRATOR_AGENT_URL = `${ API_BASE_URL }/wpcom/v2/ai/agent`;
 export const ORCHESTRATOR_AGENT_ID = 'wp-orchestrator';
 
