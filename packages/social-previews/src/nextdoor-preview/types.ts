98d586385907ea6d7760464cb56c2df081cb5c3b
Manzoor Wani <manzoorwani.jk@gmail.com>
2025-12-15 16:54:59 +0530

    Nuke social-previews

--- packages/social-previews/src/nextdoor-preview/types.ts
+++
@@ -1,9 +0,0 @@
-import { SocialPreviewBaseProps, SocialPreviewsBaseProps } from '../types';
-
-export type NextdoorPreviewProps = SocialPreviewBaseProps & {
-	neighborhood?: string;
-	name: string;
-	profileImage: string;
-};
-
-export type NextdoorPreviewsProps = NextdoorPreviewProps & SocialPreviewsBaseProps;
