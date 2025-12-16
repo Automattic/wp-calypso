b33815e3b05be096406900614bf3aee2f6eb8f94
Omar Alshaker <omar@omaralshaker.com>
2025-12-16 19:07:48 +0100

    help-center-ciab-admin

--- apps/help-center/webpack.config.js
+++ apps/help-center/webpack.config.js
@@ -81,6 +81,7 @@ function getWebpackConfig( env = { source: '' }, argv = {} ) {
 
 	return [
 		getIndividualConfig( { env, argv, name: 'help-center-gutenberg' } ),
+		getIndividualConfig( { env, argv, name: 'help-center-ciab-admin' } ),
 		getIndividualConfig( { env, argv, name: 'help-center-wp-admin' } ),
 		getIndividualConfig( { env, argv, name: 'help-center-customizer' } ),
 		getIndividualConfig( { env, argv, name: 'help-center-gutenberg-disconnected' } ),
