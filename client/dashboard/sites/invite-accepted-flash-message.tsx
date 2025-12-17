8681c8ad434a06407f89a6bf675c9ace46b3f9ef
Philip Jackson <p-jackson@live.com>
2025-12-18 10:42:59 +1300

    Reuse the FlashMessage component

--- client/dashboard/sites/invite-accepted-flash-message.tsx
+++ client/dashboard/sites/invite-accepted-flash-message.tsx
@@ -1,15 +1,15 @@
-import { useDispatch } from '@wordpress/data';
 import { __, sprintf } from '@wordpress/i18n';
-import { store as noticesStore } from '@wordpress/notices';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import FlashMessage from '../components/flash-message';
 
 export function InviteAcceptedFlashMessage() {
-	const { createSuccessNotice } = useDispatch( noticesStore );
+	const message = useRef( '' );
 
 	useEffect( () => {
 		if ( typeof window === 'undefined' ) {
 			return;
 		}
+
 		const params = new URLSearchParams( window.location.search );
 		if (
 			params.get( 'flash' ) === 'invite-accepted' &&
@@ -20,44 +20,40 @@ export function InviteAcceptedFlashMessage() {
 			const siteTitle = params.get( 'invite-site-title' )!;
 
 			// Construct role-based message (simplified for snackbar)
-			let message = '';
 			switch ( role ) {
 				case 'follower':
 					// Translators: %s is the site title.
-					message = sprintf( __( 'You’re now following %s.' ), siteTitle );
+					message.current = sprintf( __( 'You’re now following %s.' ), siteTitle );
 					break;
 				case 'viewer':
 					// Translators: %s is the site title.
-					message = sprintf( __( 'You’re now a viewer of %s.' ), siteTitle );
+					message.current = sprintf( __( 'You’re now a viewer of %s.' ), siteTitle );
 					break;
 				case 'administrator':
 					// Translators: %s is the site title.
-					message = sprintf( __( 'You’re now an Administrator of %s.' ), siteTitle );
+					message.current = sprintf( __( 'You’re now an Administrator of %s.' ), siteTitle );
 					break;
 				case 'editor':
 					// Translators: %s is the site title.
-					message = sprintf( __( 'You’re now an Editor of %s.' ), siteTitle );
+					message.current = sprintf( __( 'You’re now an Editor of %s.' ), siteTitle );
 					break;
 				case 'author':
 					// Translators: %s is the site title.
-					message = sprintf( __( 'You’re now an Author of %s.' ), siteTitle );
+					message.current = sprintf( __( 'You’re now an Author of %s.' ), siteTitle );
 					break;
 				case 'contributor':
 					// Translators: %s is the site title.
-					message = sprintf( __( 'You’re now a Contributor of %s.' ), siteTitle );
+					message.current = sprintf( __( 'You’re now a Contributor of %s.' ), siteTitle );
 					break;
 				case 'subscriber':
 					// Translators: %s is the site title.
-					message = sprintf( __( 'You’re now a Subscriber of %s.' ), siteTitle );
+					message.current = sprintf( __( 'You’re now a Subscriber of %s.' ), siteTitle );
 					break;
 				default:
 					// Translators: %s is the site title.
-					message = sprintf( __( '%s joined.' ), siteTitle );
+					message.current = sprintf( __( '%s joined.' ), siteTitle );
 			}
 
-			createSuccessNotice( message, { type: 'snackbar' } );
-
-			params.delete( 'flash' );
 			params.delete( 'invite-role' );
 			params.delete( 'invite-site-title' );
 			const newUrl =
@@ -65,10 +61,13 @@ export function InviteAcceptedFlashMessage() {
 			window.history.replaceState( {}, '', newUrl );
 		}
 
-		// This effect has side effects, like editing the URL. We only ever
-		// want to run it once on mount.
+		// This effect has side effects, like editing the URL. We only ever want to run it once on mount.
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
 
-	return null;
+	if ( ! message.current ) {
+		return null;
+	}
+
+	return <FlashMessage id="invite-accepted" message={ message.current } />;
 }
