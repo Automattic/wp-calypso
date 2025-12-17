4a9f12e399ea8d1e5c65dacef72c53c0771abeca
Paulo Trentin <paulo@paulotrentin.com.br>
2025-12-17 19:45:32 -0300

    Make sure we only show the feedback CTA when survicate is loaded

--- packages/help-center/src/components/help-center-more-resources.tsx
+++ packages/help-center/src/components/help-center-more-resources.tsx
@@ -11,7 +11,7 @@ import './help-center-more-resources.scss';
 
 export const HelpCenterMoreResources = () => {
 	const { __ } = useI18n();
-	const { sectionName, disableChatSupport, haveSurvicateEnabled, source } = useHelpCenterContext();
+	const { sectionName, disableChatSupport, haveSurvicateEnabled } = useHelpCenterContext();
 	const navigate = useNavigate();
 
 	const trackMoreResourcesButtonClick = ( resource: string ) => {
@@ -31,8 +31,6 @@ export const HelpCenterMoreResources = () => {
 		} );
 		trackMoreResourcesButtonClick( resourceType );
 	};
-
-	const isCalypsoContext = source === 'wpcom';
 
 	return (
 		<div className="help-center-more-resources">
@@ -58,24 +56,26 @@ export const HelpCenterMoreResources = () => {
 						</div>
 					</li>
 				) }
-				{ haveSurvicateEnabled && isCalypsoContext && (
-					<li className="help-center-more-resources__resource-item help-center-link__item">
-						<div className="help-center-more-resources__resource-cell help-center-link__cell">
-							<button
-								type="button"
-								onClick={ () => {
-									trackMoreResourcesButtonClick( 'survicate' );
-									window._sva?.invokeEvent( 'showFeedbackSurveyFromHelpCenter' );
-								} }
-								className="help-center-more-resources__support-history"
-							>
-								<Icon icon={ thumbsUp } size={ 24 } />
-								<span>{ __( 'Share feedback', __i18n_text_domain__ ) }</span>
-								<Icon icon={ chevronRight } size={ 20 } />
-							</button>
-						</div>
-					</li>
-				) }
+				{ haveSurvicateEnabled &&
+					typeof window._sva !== 'undefined' &&
+					window._sva?.invokeEvent && (
+						<li className="help-center-more-resources__resource-item help-center-link__item">
+							<div className="help-center-more-resources__resource-cell help-center-link__cell">
+								<button
+									type="button"
+									onClick={ () => {
+										trackMoreResourcesButtonClick( 'feedback-survey' );
+										window._sva?.invokeEvent( 'showFeedbackSurveyFromHelpCenter' );
+									} }
+									className="help-center-more-resources__survicate"
+								>
+									<Icon icon={ thumbsUp } size={ 24 } />
+									<span>{ __( 'Share feedback', __i18n_text_domain__ ) }</span>
+									<Icon icon={ chevronRight } size={ 20 } />
+								</button>
+							</div>
+						</li>
+					) }
 				<li className="help-center-more-resources__resource-item help-center-link__item">
 					<div className="help-center-more-resources__resource-cell help-center-link__cell">
 						<a
