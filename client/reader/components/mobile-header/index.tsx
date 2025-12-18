869255dfdea2e7c266731b1a82b1d05dd1d8ebee
Gabriel Caires <gabriel.caires@automattic.com>
2025-12-18 19:37:27 +0000

    Use js instead of css to hide the item

--- client/reader/components/mobile-header/index.tsx
+++ client/reader/components/mobile-header/index.tsx
@@ -1,8 +1,15 @@
 import { __experimentalHStack as HStack } from '@wordpress/components';
+import { useViewportMatch } from '@wordpress/compose';
 import AppTitle from '../app-title';
 import './index.scss';
 
 export const MobileHeader = () => {
+	const isDesktop = useViewportMatch( 'medium' );
+
+	if ( isDesktop ) {
+		return null;
+	}
+
 	return (
 		<HStack className="mobile-header" spacing={ 0 } justify="space-between">
 			<AppTitle />
