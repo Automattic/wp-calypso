00d38ac21e6bd8cc01037bf2969532cff7bd725c
Michael Cain <michael@michaeldcain.com>
2025-12-19 22:29:17 -0500

    Remove survey images

--- client/dashboard/me/billing-purchases/cancel-purchase/cancel-purchase-form/step-components/upsell-step.tsx
+++ client/dashboard/me/billing-purchases/cancel-purchase/cancel-purchase-form/step-components/upsell-step.tsx
@@ -8,12 +8,6 @@ import {
 import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 import * as React from 'react';
-import imgBuiltBy from 'calypso/assets/images/cancellation/built-by.png';
-import imgBusinessPlan from 'calypso/assets/images/cancellation/business-plan.png';
-import imgFreeMonth from 'calypso/assets/images/cancellation/free-month.png';
-import imgLiveChat from 'calypso/assets/images/cancellation/live-chat.png';
-import imgMonthlyPayments from 'calypso/assets/images/cancellation/monthly-payments.png';
-import imgSwitchPlan from 'calypso/assets/images/cancellation/switch-plan.png';
 import { useAnalytics } from '../../../../../app/analytics';
 import { useHelpCenter } from '../../../../../app/help-center';
 import { redirectToDashboardLink, wpcomLink } from '../../../../../utils/link';
@@ -21,7 +15,6 @@ import type { PlanProduct, Purchase } from '@automattic/api-core';
 
 type UpsellProps = {
 	children?: React.ReactNode;
-	image: string;
 	title: string;
 	acceptButtonText: string;
 	acceptButtonUrl?: string;
@@ -30,7 +23,7 @@ type UpsellProps = {
 	isBusy?: boolean;
 };
 
-function Upsell( { image, ...props }: UpsellProps ) {
+function Upsell( { ...props }: UpsellProps ) {
 	const declineButtonText = __( 'Cancel my current plan' );
 
 	return (
@@ -52,9 +45,6 @@ function Upsell( { image, ...props }: UpsellProps ) {
 						{ declineButtonText }
 					</Button>
 				</div>
-			</div>
-			<div>
-				<img className="cancel-purchase-form__upsell-image" src={ image } alt="" />
 			</div>
 		</VStack>
 	);
@@ -148,7 +138,6 @@ export default function UpsellStep( {
 					} }
 					onDecline={ props.onDeclineUpsell }
 					isBusy={ cancellationInProgress }
-					image={ imgLiveChat }
 				>
 					{ hasEnTranslation(
 						'If you’re feeling a bit stuck with your site, our expert <b>Happiness Engineers</b> are always ready to help. ' +
@@ -185,7 +174,6 @@ export default function UpsellStep( {
 					} }
 					onDecline={ props.onDeclineUpsell }
 					isBusy={ cancellationInProgress }
-					image={ imgBuiltBy }
 				>
 					{ __(
 						'Building a website from scratch can be a lot of work. ' +
@@ -218,7 +206,6 @@ export default function UpsellStep( {
 					} }
 					onDecline={ props.onDeclineUpsell }
 					isBusy={ cancellationInProgress }
-					image={ imgBusinessPlan }
 				>
 					{ createInterpolateElement(
 						sprintf(
@@ -247,7 +234,6 @@ export default function UpsellStep( {
 					onAccept={ () => props.onClickDowngrade?.( upsell ) }
 					onDecline={ props.onDeclineUpsell }
 					isBusy={ cancellationInProgress }
-					image={ imgMonthlyPayments }
 				>
 					<>
 						{ sprintf(
@@ -296,7 +282,6 @@ export default function UpsellStep( {
 					onAccept={ () => props.onClickDowngrade?.( upsell ) }
 					onDecline={ props.onDeclineUpsell }
 					isBusy={ cancellationInProgress }
-					image={ imgSwitchPlan }
 				>
 					<>
 						{ sprintf(
@@ -329,7 +314,6 @@ export default function UpsellStep( {
 					onAccept={ () => props.onClickFreeMonthOffer?.() }
 					onDecline={ props.onDeclineUpsell }
 					isBusy={ cancellationInProgress }
-					image={ imgFreeMonth }
 				>
 					{ sprintf(
 						/* translators: %(currentPlan)s is the name of the plan to which the customer is subscribed */
