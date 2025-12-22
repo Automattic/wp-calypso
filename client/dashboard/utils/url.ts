5c4e75e7efa958fda394872f1bf9376949f54cee
Ashar Fuadi <ashar.fuadi@automattic.com>
2025-12-22 19:03:48 +0700

    Use wpcomLink to enforce origin

--- client/dashboard/utils/url.ts
+++ client/dashboard/utils/url.ts
@@ -1,4 +1,5 @@
 import { getProtocol } from '@wordpress/url';
+import { wpcomLink } from './link';
 
 export function isRelativeUrl( url: string ) {
 	if ( ! url ) {
@@ -9,8 +10,7 @@ export function isRelativeUrl( url: string ) {
 }
 
 export function isOnboardingUrl( url: string ) {
-	const path = new URL( url, window.location.origin ).pathname;
-	return path.startsWith( '/setup' ) || path.startsWith( '/start' );
+	return [ '/setup', '/start' ].some( ( path ) => url.startsWith( wpcomLink( path ) ) );
 }
 
 export function urlToSlug( url: string ) {
