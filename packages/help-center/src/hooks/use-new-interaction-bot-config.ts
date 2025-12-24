4397cbab5297f941a0dfb2d2bf31b43dd2dca4b8
Emanuele Buccelli <escapemanuele@gmail.com>
2025-12-24 16:52:18 +0100

    Naming

--- packages/help-center/src/hooks/use-new-interaction-bot-config.ts
+++ packages/help-center/src/hooks/use-new-interaction-bot-config.ts
@@ -1,6 +1,6 @@
 import type { CurrentUser } from '@automattic/data-stores';
 
-export function getNewInteractionsBotConfig( currentUser?: CurrentUser ) {
+export function useNewInteractionsBotConfig( currentUser?: CurrentUser ) {
 	if ( ! currentUser?.ID ) {
 		return {
 			newInteractionsBotSlug: 'wpcom-chat-loggedout',
