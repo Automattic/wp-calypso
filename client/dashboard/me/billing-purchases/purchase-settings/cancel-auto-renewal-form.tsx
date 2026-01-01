331984aa15d8d404f4087f6a30cd15dfcdcba979
Chris McCluskey <chris.mccluskey@automattic.com>
2025-12-31 16:26:57 -0800

    Add marketing survey to cancel auto renewal form by making it into an FC

--- client/dashboard/me/billing-purchases/purchase-settings/cancel-auto-renewal-form.tsx
+++ client/dashboard/me/billing-purchases/purchase-settings/cancel-auto-renewal-form.tsx
@@ -1,35 +1,33 @@
+import { marketingSurveyMutation } from '@automattic/api-queries';
+import { useMutation } from '@tanstack/react-query';
 import { RadioControl, __experimentalConfirmDialog as ConfirmDialog } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
-import { Component } from 'react';
+import { useState } from 'react';
 import { SectionHeader } from '../../../components/section-header';
 import { shuffleArray } from '../../../utils/collection';
 import enrichedSurveyData from '../cancel-purchase/enriched-survey-data';
 import PrecancellationChatButton from './precancellation-chat-button';
-import type { MarketingSurveyResponses, Purchase } from '@automattic/api-core';
-// import { submitSurvey } from 'calypso/lib/purchases/actions'; //TODO: port submitSurvey to mutation
+import type { MarketingSurveyDetails, Purchase } from '@automattic/api-core';
+import type { FC } from 'react';
 
 interface CancelAutoRenewFormProps {
 	purchase: Purchase;
 	selectedSiteId: number;
 	isVisible: boolean;
 	onClose: () => void;
-	submitSurvey: (
-		survey: string,
-		selectedSiteId: number,
-		surveyData: Omit< MarketingSurveyResponses, 'purchaseId' | 'purchase' >
-	) => void;
 }
 
-class CancelAutoRenewalForm extends Component< CancelAutoRenewFormProps > {
-	state = {
+const CancelAutoRenewalForm: FC< CancelAutoRenewFormProps > = ( {
+	purchase,
+	selectedSiteId,
+	isVisible,
+	onClose,
+} ) => {
+	const [ state, setState ] = useState( {
 		response: '',
-	};
+	} );
 
-	radioButtons = {};
-
-	getProductTypeString = () => {
-		const { purchase } = this.props;
-
+	const getProductTypeString = () => {
 		if ( purchase.is_domain_registration ) {
 			/* translators: as in "domain name"*/
 			return __( 'domain' );
@@ -43,108 +41,114 @@ class CancelAutoRenewalForm extends Component< CancelAutoRenewFormProps > {
 		return __( 'subscription' );
 	};
 
-	constructor( props: CancelAutoRenewFormProps ) {
-		super( props );
+	const productType = getProductTypeString();
 
-		const productType = this.getProductTypeString();
+	const radioButtons = shuffleArray( [
+		{
+			value: 'let-it-expire',
+			/* translators: %(productType)s will be either "plan", "domain", or "subscription". */
+			label: sprintf( __( "I'm going to let this %(productType)s expire." ), {
+				productType,
+			} ),
+		},
+		{
+			value: 'manual-renew',
+			/* translators: %(productType)s will be either "plan", "domain", or "subscription". */
+			label: sprintf( __( "I'm going to renew the %(productType)s, but will do it manually." ), {
+				productType,
+			} ),
+		},
+		{
+			value: 'not-sure',
+			label: __( "I'm not sure." ),
+		},
+	] );
 
-		this.radioButtons = shuffleArray( [
-			{
-				value: 'let-it-expire',
-				/* translators: %(productType)s will be either "plan", "domain", or "subscription". */
-				label: sprintf( __( "I'm going to let this %(productType)s expire." ), {
-					productType,
-				} ),
+	const marketingSurveyMutate = useMutation( marketingSurveyMutation() );
+	const submitMarketingSurvey = ( surveyDetails: MarketingSurveyDetails ) =>
+		marketingSurveyMutate.mutate( surveyDetails, {
+			onSuccess: () => {
+				setState( ( state ) => ( {
+					...state,
+					isSubmitting: false,
+				} ) );
 			},
-
-			{
-				value: 'manual-renew',
-				/* translators: %(productType)s will be either "plan", "domain", or "subscription". */
-				label: sprintf( __( "I'm going to renew the %(productType)s, but will do it manually." ), {
-					productType,
-				} ),
+			onError: () => {
+				setState( ( state ) => ( {
+					...state,
+					isSubmitting: false,
+				} ) );
 			},
-			{
-				value: 'not-sure',
-				label: __( "I'm not sure." ),
-			},
-		] );
-	}
+		} );
 
-	onSubmit = () => {
-		const { purchase, selectedSiteId } = this.props;
-		const { response } = this.state;
+	const onSubmit = () => {
+		const { response } = state;
 
 		const surveyData = {
 			response,
 		};
 
-		this.props.submitSurvey(
-			'calypso-cancel-auto-renewal',
-			selectedSiteId,
-			enrichedSurveyData( surveyData, purchase )
-		);
+		submitMarketingSurvey( {
+			survey_id: 'calypso-cancel-auto-renewal',
+			site_id: selectedSiteId,
+			survey_responses: enrichedSurveyData( surveyData, purchase ),
+		} );
 
-		this.props.onClose();
+		onClose();
 	};
 
-	onRadioChange = ( value: string ) => {
-		this.setState( {
+	const onRadioChange = ( value: string ) => {
+		setState( {
 			response: value,
 		} );
 	};
 
-	render() {
-		const { isVisible, onClose, purchase } = this.props;
-
-		const productType = this.getProductTypeString();
-
-		if ( ! isVisible ) {
-			return null;
-		}
-
-		return (
-			<ConfirmDialog
-				onCancel={ onClose }
-				onConfirm={ this.onSubmit }
-				cancelButtonText={ __( 'Skip' ) }
-				confirmButtonText={ __( 'Submit' ) }
-			>
-				<SectionHeader
-					title={ __( 'Help us improve' ) }
-					description={
-						<fieldset role="group" className="cancel-auto-renewal-form__form-fieldset">
-							<p>{ __( "You've turned off auto-renewal." ) }</p>
-							<p>
-								{ sprintf(
-									/* translators: %(productType)s will be either "plan", "domain", or "subscription". */
-									__(
-										"Before you go, we'd love to know: " +
-											"are you letting this %(productType)s expire completely, or do you think you'll renew it manually?"
-									),
-									{ productType }
-								) }
-							</p>
-							<RadioControl
-								className="cancel-auto-renewal-form__radio-control"
-								hideLabelFromVision
-								options={ this.radioButtons }
-								selected={ this.state.response }
-								onChange={ this.onRadioChange }
-							/>
-						</fieldset>
-					}
-					actions={
-						<PrecancellationChatButton
-							purchase={ purchase }
-							onClick={ onClose }
-							className="cancel-auto-renewal-form__chat-button"
-						/>
-					}
-				/>
-			</ConfirmDialog>
-		);
+	// render
+	if ( ! isVisible ) {
+		return null;
 	}
-}
+
+	return (
+		<ConfirmDialog
+			onCancel={ onClose }
+			onConfirm={ onSubmit }
+			cancelButtonText={ __( 'Skip' ) }
+			confirmButtonText={ __( 'Submit' ) }
+		>
+			<SectionHeader
+				title={ __( 'Help us improve' ) }
+				description={
+					<fieldset role="group" className="cancel-auto-renewal-form__form-fieldset">
+						<p>{ __( "You've turned off auto-renewal." ) }</p>
+						<p>
+							{ sprintf(
+								/* translators: %(productType)s will be either "plan", "domain", or "subscription". */
+								__(
+									"Before you go, we'd love to know: " +
+										"are you letting this %(productType)s expire completely, or do you think you'll renew it manually?"
+								),
+								{ productType }
+							) }
+						</p>
+						<RadioControl
+							className="cancel-auto-renewal-form__radio-control"
+							hideLabelFromVision
+							options={ radioButtons }
+							selected={ state.response }
+							onChange={ onRadioChange }
+						/>
+					</fieldset>
+				}
+				actions={
+					<PrecancellationChatButton
+						purchase={ purchase }
+						onClick={ onClose }
+						className="cancel-auto-renewal-form__chat-button"
+					/>
+				}
+			/>
+		</ConfirmDialog>
+	);
+};
 
 export default CancelAutoRenewalForm;
