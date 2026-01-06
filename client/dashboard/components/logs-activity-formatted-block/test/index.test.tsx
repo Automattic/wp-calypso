e53f69d4ddc1979e26158bf3bca31230a1abbba3
Steven <steve.dufresne@automattic.com>
2026-01-06 10:55:36 +0900

    Update tests.

--- client/dashboard/components/logs-activity-formatted-block/test/index.test.tsx
+++ client/dashboard/components/logs-activity-formatted-block/test/index.test.tsx
@@ -7,16 +7,20 @@ import userEvent from '@testing-library/user-event';
 import { renderFormattedContent, createFormattedBlock } from '../';
 import isA8CForAgencies from '../../../../lib/a8c-for-agencies/is-a8c-for-agencies';
 import isJetpackCloud from '../../../../lib/jetpack/is-jetpack-cloud';
-import isDashboard from '../../../utils/is-dashboard';
+import { isDashboardBackport } from '../../../utils/is-dashboard-backport';
 import type { ActivityBlockContent, ActivityBlockMeta } from '../types';
 
 jest.mock( '../../../../lib/jetpack/is-jetpack-cloud', () => jest.fn() );
 jest.mock( '../../../../lib/a8c-for-agencies/is-a8c-for-agencies', () => jest.fn() );
-jest.mock( '../../../utils/is-dashboard', () => jest.fn() );
+jest.mock( '../../../utils/is-dashboard-backport', () => ( {
+	isDashboardBackport: jest.fn(),
+} ) );
 
 const mockedIsJetpackCloud = isJetpackCloud as jest.MockedFunction< typeof isJetpackCloud >;
 const mockedIsA8CForAgencies = isA8CForAgencies as jest.MockedFunction< typeof isA8CForAgencies >;
-const mockedIsDashboard = isDashboard as jest.MockedFunction< typeof isDashboard >;
+const mockedIsDashboardBackport = isDashboardBackport as jest.MockedFunction<
+	typeof isDashboardBackport
+>;
 
 const renderFormatted = (
 	items: ActivityBlockContent | ActivityBlockContent[],
@@ -276,27 +280,11 @@ describe( 'Plugin blocks', () => {
 	beforeEach( () => {
 		mockedIsJetpackCloud.mockReturnValue( false );
 		mockedIsA8CForAgencies.mockReturnValue( false );
-		mockedIsDashboard.mockReturnValue( false );
+		mockedIsDashboardBackport.mockReturnValue( false );
 		jest.clearAllMocks();
 	} );
 
-	test( 'renders a regular link when not in the dashboard', () => {
-		renderFormatted( {
-			type: 'plugin',
-			pluginSlug: 'plugin-slug',
-			siteSlug: 'site-slug',
-			children: [ 'A plugin' ],
-		} );
-
-		const link = screen.getByRole( 'link' );
-		expect( link.getAttribute( 'href' ) ).toMatch( /\/plugins\/plugin-slug\/site-slug$/ );
-		expect( link ).not.toHaveAttribute( 'target' );
-		expect( link ).not.toHaveAttribute( 'rel' );
-	} );
-
-	test( 'renders an external link when in the dashboard', () => {
-		mockedIsDashboard.mockReturnValue( true );
-
+	test( 'renders an external link when not in the backport', () => {
 		renderFormatted( {
 			type: 'plugin',
 			pluginSlug: 'plugin-slug',
@@ -308,6 +296,22 @@ describe( 'Plugin blocks', () => {
 		expect( link.getAttribute( 'href' ) ).toMatch( /\/plugins\/plugin-slug\/site-slug$/ );
 		expect( link ).toHaveAttribute( 'target', '_blank' );
 		expect( link ).toHaveAttribute( 'rel', 'external noreferrer noopener' );
+	} );
+
+	test( 'renders a regular link when in the backport', () => {
+		mockedIsDashboardBackport.mockReturnValue( true );
+
+		renderFormatted( {
+			type: 'plugin',
+			pluginSlug: 'plugin-slug',
+			siteSlug: 'site-slug',
+			children: [ 'A plugin' ],
+		} );
+
+		const link = screen.getByRole( 'link' );
+		expect( link.getAttribute( 'href' ) ).toMatch( /\/plugins\/plugin-slug\/site-slug$/ );
+		expect( link ).not.toHaveAttribute( 'target' );
+		expect( link ).not.toHaveAttribute( 'rel' );
 	} );
 
 	test( 'renders plain text when siteSlug is missing', () => {
