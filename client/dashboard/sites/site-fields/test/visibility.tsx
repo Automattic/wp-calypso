3b8f2ea734fe5001e38c4c8e87c7f4d137f50607
arthur <arthur.chu@automattic.com>
2026-01-08 16:56:16 +0800

    Update tests

--- client/dashboard/sites/site-fields/test/visibility.tsx
+++ client/dashboard/sites/site-fields/test/visibility.tsx
@@ -4,17 +4,17 @@
 import { render } from '../../../test-utils';
 import { wpcomLink } from '../../../utils/link';
 import { Visibility } from '../index';
-import type { Site } from '@automattic/api-core';
 
 describe( '<Visibility>', () => {
 	test( 'for unlaunched sites, it renders "Coming soon" with a "Finish setup" link', () => {
-		const site = {
-			slug: 'test.wordpress.com',
-			site_migration: {},
-			launch_status: 'unlaunched',
-			is_private: true,
-		} as Site;
-		const { container, getByRole } = render( <Visibility site={ site } /> );
+		const { container, getByRole } = render(
+			<Visibility
+				siteSlug="test.wordpress.com"
+				visibility="private"
+				status={ null }
+				isLaunched={ false }
+			/>
+		);
 		expect( container ).toHaveTextContent( 'Finish setup↗' );
 		expect( getByRole( 'link', { name: /Finish setup/ } ) ).toHaveAttribute(
 			'href',
@@ -23,29 +23,28 @@ describe( '<Visibility>', () => {
 	} );
 
 	test( 'for coming soon sites, it renders "Coming soon"', () => {
-		const site = {
-			site_migration: {},
-			is_coming_soon: true,
-		} as Site;
-		const { container } = render( <Visibility site={ site } /> );
+		const { container } = render(
+			<Visibility
+				siteSlug="test.wordpress.com"
+				visibility="coming_soon"
+				status={ null }
+				isLaunched
+			/>
+		);
 		expect( container.textContent ).toBe( 'Coming soon' );
 	} );
 
 	test( 'for private sites, it renders "Private"', () => {
-		const site = {
-			site_migration: {},
-			is_private: true,
-		} as Site;
-		const { container } = render( <Visibility site={ site } /> );
+		const { container } = render(
+			<Visibility siteSlug="test.wordpress.com" visibility="private" status={ null } isLaunched />
+		);
 		expect( container.textContent ).toBe( 'Private' );
 	} );
 
 	test( 'for public sites, it renders "Public"', () => {
-		const site = {
-			site_migration: {},
-			is_private: false,
-		} as Site;
-		const { container } = render( <Visibility site={ site } /> );
+		const { container } = render(
+			<Visibility siteSlug="test.wordpress.com" visibility="public" status={ null } isLaunched />
+		);
 		expect( container.textContent ).toBe( 'Public' );
 	} );
 } );
