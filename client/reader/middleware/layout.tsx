e64cfb4a93253d86f8392036bf0bc24960e60c8d
Gabriel Caires <gabriel.caires@automattic.com>
2026-01-09 19:13:53 +0000

    More tests

--- client/reader/middleware/layout.tsx
+++ client/reader/middleware/layout.tsx
@@ -2,17 +2,17 @@ import config from '@automattic/calypso-config';
 import { isWithinBreakpoint, subscribeIsWithinBreakpoint } from '@automattic/viewport';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { Component, useEffect } from 'react';
+import { Component } from 'react';
 import { connect } from 'react-redux';
 import AsyncLoad from 'calypso/components/async-load';
 import DocumentHead from 'calypso/components/data/document-head';
-import QuerySites from 'calypso/components/data/query-sites';
 import { withCurrentRoute } from 'calypso/components/route';
 import SympathyDevWarning from 'calypso/components/sympathy-dev-warning';
 import { retrieveMobileRedirect } from 'calypso/jetpack-connect/persistence-utils';
 import { isWcMobileApp } from 'calypso/lib/mobile-app';
 import { isCrowdsignalOAuth2Client } from 'calypso/lib/oauth2-clients';
 import UserVerificationChecker from 'calypso/lib/user/verification-checker';
+import Header from 'calypso/reader/components/header';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { getSidebarType, SidebarType } from 'calypso/state/global-sidebar/selectors';
 import { getCurrentOAuth2Client } from 'calypso/state/oauth2-clients/ui/selectors';
@@ -22,35 +22,6 @@ import { getCurrentLayoutFocus } from 'calypso/state/ui/layout-focus/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 import './style.scss';
-
-function SidebarOverflowDelay( { layoutFocus } ) {
-	const setSidebarOverflowClass = ( overflow ) => {
-		const classList = document.querySelector( 'body' ).classList;
-		if ( overflow ) {
-			classList.add( 'is-sidebar-overflow' );
-		} else {
-			classList.remove( 'is-sidebar-overflow' );
-		}
-	};
-
-	useEffect( () => {
-		if ( layoutFocus !== 'sites' ) {
-			// The sidebar menu uses a flyout design that requires the overflowing content
-			// to be visible. However, `overflow` isn't an animatable CSS property, so we
-			// need to set it after the sliding transition finishes. We wait for 150ms (the
-			// CSS transition time) + a grace period of 350ms (since the sidebar menu is
-			// rendered asynchronously).
-			// @see https://github.com/Automattic/wp-calypso/issues/47019
-			setTimeout( () => {
-				setSidebarOverflowClass( true );
-			}, 500 );
-		} else {
-			setSidebarOverflowClass( false );
-		}
-	}, [ layoutFocus ] );
-
-	return null;
-}
 
 class Layout extends Component {
 	static propTypes = {
@@ -93,25 +64,13 @@ class Layout extends Component {
 			'is-reader-msd-enabled': this.props.isMSDEnabledForReader,
 		} );
 
-		const shouldEnableCommandPalette =
-			// There is a custom command palette in the "Switch site" page, so we disable it.
-			config.isEnabled( 'yolo/command-palette' ) && this.props.currentRoute !== '/switch-site';
-
 		return (
 			<div className={ sectionClass }>
-				<SidebarOverflowDelay layoutFocus={ this.props.currentLayoutFocus } />
 				<DocumentHead />
-				{ this.props.shouldQueryAllSites ? (
-					<QuerySites allSites />
-				) : (
-					<QuerySites primaryAndRecent={ ! config.isEnabled( 'jetpack-cloud' ) } />
-				) }
 				<UserVerificationChecker />
-				{ config.isEnabled( 'layout/guided-tours' ) && (
-					<AsyncLoad require="calypso/layout/guided-tours" placeholder={ null } />
-				) }
+
 				<div className="layout__header-section">
-					<AsyncLoad require="calypso/reader/components/header" placeholder={ null } />;
+					<Header />
 				</div>
 				<div id="content" className="layout__content">
 					<AsyncLoad
@@ -128,7 +87,7 @@ class Layout extends Component {
 						{ this.props.primary }
 					</div>
 				</div>
-				<AsyncLoad require="calypso/layout/community-translator" placeholder={ null } />
+				{ /* <AsyncLoad require="calypso/layout/community-translator" placeholder={ null } /> */ }
 				{ 'development' === process.env.NODE_ENV && (
 					<>
 						<SympathyDevWarning />
@@ -146,10 +105,6 @@ class Layout extends Component {
 				) }
 				{ config.isEnabled( 'legal-updates-banner' ) && (
 					<AsyncLoad require="calypso/blocks/legal-updates-banner" placeholder={ null } />
-				) }
-
-				{ shouldEnableCommandPalette && (
-					<AsyncLoad require="calypso/layout/command-palette" placeholder={ null } />
 				) }
 			</div>
 		);
