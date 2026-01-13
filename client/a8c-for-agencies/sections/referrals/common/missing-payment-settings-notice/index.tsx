ecb1ac947fc9911bd450aec695b4933f415d1d78
James Kenneth Guidaven <jkguidaven@gmail.com>
2026-01-13 20:09:32 +0800

    Ensure we don't display the notice if fetch failed.

--- client/a8c-for-agencies/sections/referrals/common/missing-payment-settings-notice/index.tsx
+++ client/a8c-for-agencies/sections/referrals/common/missing-payment-settings-notice/index.tsx
@@ -9,14 +9,14 @@ import './style.scss';
 export const MissingPaymentSettingsNotice = ( { isFullWidth }: { isFullWidth?: boolean } ) => {
 	const translate = useTranslate();
 
-	const { data: tipaltiData, isFetching } = useGetTipaltiPayee();
+	const { data: tipaltiData, isFetching, isError } = useGetTipaltiPayee();
 	const isPayable = tipaltiData?.IsPayable;
 
 	const { data: referrals } = useFetchReferrals();
 
 	const hasReferrals = !! referrals?.length;
 
-	if ( isFetching || isPayable || ! hasReferrals ) {
+	if ( isFetching || isError || isPayable || ! hasReferrals ) {
 		return null;
 	}
 
