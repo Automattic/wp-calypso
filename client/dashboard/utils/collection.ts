471d1272cd8fee82567777d5ecd47f697d19a171
Chris McCluskey <chris.mccluskey@automattic.com>
2026-01-14 13:32:15 -0800

    Fix type error by reverting claude suggestion to use structuredClone

--- client/dashboard/utils/collection.ts
+++ client/dashboard/utils/collection.ts
@@ -3,7 +3,7 @@ const randomInt = ( lower: number, upper: number ): number =>
 
 export function shuffleArray< T >( collection: readonly T[] ): T[] {
 	let pointer = -1;
-	const newArray = structuredClone( collection );
+	const newArray = [ ...collection ];
 	collection.forEach( () => {
 		const randomPointer = randomInt( ++pointer, collection.length - 1 );
 		const valueAtRandomPointer = newArray[ randomPointer ];
