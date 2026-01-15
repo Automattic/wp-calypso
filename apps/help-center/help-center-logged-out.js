51307671ea6fcad15b62a94e34ad97489556a888
Omar Alshaker <omar@omaralshaker.com>
2026-01-15 17:31:07 +0100

    Add logged out Help Center variant for better performance

---
+++ apps/help-center/help-center-logged-out.js
@@ -0,0 +1,32 @@
+import './config';
+import './help-center.scss';
+import loadHelpCenter from './async-help-center';
+
+/**
+ * Checks if the help center should be automatically to keep session continuity.
+ * @returns {boolean} Whether the help center should be automatically loaded.
+ */
+function shouldAutoLoadHelpCenter() {
+	try {
+		const preferences = window.localStorage.getItem( 'logged_out_help_center_preferences' );
+		if ( preferences ) {
+			const preferencesObject = JSON.parse( preferences );
+			return preferencesObject.help_center_open;
+		}
+		return false;
+	} catch ( error ) {
+		return false;
+	}
+}
+
+window.addEventListener( 'DOMContentLoaded', () => {
+	shouldAutoLoadHelpCenter().then( ( shouldAutoLoad ) => {
+		if ( shouldAutoLoad ) {
+			loadHelpCenter();
+		}
+	} );
+} );
+
+document.dispatchEvent( new Event( 'help-center-ready-to-load' ) );
+
+export { loadHelpCenter };
