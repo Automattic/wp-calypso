791b8788a78e6d581945fdfd662ed7301fcc2c1f
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2026-01-19 17:09:41 +0100

    Get rid of `styled`

--- client/switch-site/components/site-switch.tsx
+++ client/switch-site/components/site-switch.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled';
 import {
 	__experimentalHStack as HStack,
 	Modal,
@@ -14,63 +13,6 @@ import { navigate } from 'calypso/lib/navigate';
 import type { SiteExcerptData } from '@automattic/sites';
 
 import './style.scss';
-
-const FillDefaultIconWhite = styled.div( {
-	flexShrink: 0,
-} );
-
-const SiteIcon = styled.img( {
-	width: '32px',
-	verticalAlign: 'middle',
-} );
-
-const EmptySiteIcon = styled.div( {
-	width: '32px',
-	height: '32px',
-	background: 'var(--color-neutral-10)',
-	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-} );
-
-const Empty = styled.div( {
-	fontSize: '13px',
-	textAlign: 'center',
-} );
-
-const BackButton = styled.button( {
-	cursor: 'pointer',
-} );
-
-const LabelWrapper = styled.div( {
-	display: 'flex',
-	flexDirection: 'column',
-	flex: 1,
-	maxWidth: 'calc(100% - 56px)',
-	justifyContent: 'center',
-} );
-
-const Label = styled.div( {
-	textOverflow: 'ellipsis',
-	whiteSpace: 'nowrap',
-	overflow: 'hidden',
-	fontSize: '1em',
-	'.switch-site__site': {
-		color: 'var(--studio-gray-100)',
-	},
-	'.switch-site__list [aria-selected=true] &': {
-		color: 'var(--studio-white)',
-	},
-	'.switch-site__site mark': {
-		fontWeight: 700,
-	},
-} );
-
-const SubLabel = styled( Label )( {
-	color: 'var(--studio-gray-60)',
-	opacity: 0.7,
-	fontSize: '0.9em',
-} );
 
 const Switcher = ( { redirectTo }: { redirectTo: string } ) => {
 	const { __ } = useI18n();
@@ -139,15 +81,16 @@ const Switcher = ( { redirectTo }: { redirectTo: string } ) => {
 			shouldCloseOnClickOutside={ false }
 		>
 			<div className="switch-site__header">
-				<BackButton
-					type="button"
+				<button
+					className="switch-site__back-button"
 					onClick={ () => {
 						window.history.back();
 					} }
 					aria-label={ __( 'Go back to the previous screen' ) }
 				>
 					<Icon icon={ backIcon } />
-				</BackButton>
+				</button>
+
 				<TextControl
 					ref={ input }
 					className="switch-site__search"
@@ -179,40 +122,39 @@ const Switcher = ( { redirectTo }: { redirectTo: string } ) => {
 								}
 							} }
 						>
-							<HStack alignment="left" className="commands-command-menu__item">
-								<FillDefaultIconWhite>
-									{ site.icon?.img ? (
-										<SiteIcon src={ site.icon.img } alt="" />
-									) : (
-										<EmptySiteIcon>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												width="24"
-												height="24"
-											>
-												<rect x="0" fill="none" width="24" height="24" />
-												<g>
-													<path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18l2-2 1-1v-2h-2v-1l-1-1H9v3l2 2v1.931C7.06 19.436 4 16.072 4 12l1 1h2v-2h2l3-3V6h-2L9 5v-.411a7.945 7.945 0 016 0V6l-1 1v2l1 1 3.13-3.13A7.983 7.983 0 0119.736 10H18l-2 2v2l1 1h2l.286.286C18.029 18.061 15.239 20 12 20z" />
-												</g>
-											</svg>
-										</EmptySiteIcon>
-									) }
-								</FillDefaultIconWhite>
-								<LabelWrapper>
-									<Label>
-										<TextHighlight text={ site.name || site.URL } highlight={ search } />
-									</Label>
+							<HStack alignment="left">
+								{ site.icon?.img ? (
+									<img className="switch-site__icon" src={ site.icon.img } alt="" />
+								) : (
+									<div className="switch-site__icon--empty">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+											width="24"
+											height="24"
+										>
+											<rect x="0" fill="none" width="24" height="24" />
+											<g>
+												<path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18l2-2 1-1v-2h-2v-1l-1-1H9v3l2 2v1.931C7.06 19.436 4 16.072 4 12l1 1h2v-2h2l3-3V6h-2L9 5v-.411a7.945 7.945 0 016 0V6l-1 1v2l1 1 3.13-3.13A7.983 7.983 0 0119.736 10H18l-2 2v2l1 1h2l.286.286C18.029 18.061 15.239 20 12 20z" />
+											</g>
+										</svg>
+									</div>
+								) }
 
-									<SubLabel>
+								<div className="switch-site__label-wrapper">
+									<div className="switch-site__label">
+										<TextHighlight text={ site.name || site.URL } highlight={ search } />
+									</div>
+
+									<div className="switch-site__sublabel">
 										<TextHighlight text={ site.URL } highlight={ search } />
-									</SubLabel>
-								</LabelWrapper>
+									</div>
+								</div>
 							</HStack>
 						</div>
 					) )
 				) : (
-					<Empty>{ __( 'No results found.' ) }</Empty>
+					<div className="switch-site__empty">{ __( 'No results found.' ) }</div>
 				) }
 			</div>
 		</Modal>
