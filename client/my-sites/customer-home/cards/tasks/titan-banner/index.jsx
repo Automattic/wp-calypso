68fb1385a93faf4be83ed76e0bc8f351cf82a988
Daniel Lind <daniel.lind@automattic.com>
2026-01-19 14:19:16 +0100

    Refactor TitanBanner to use plan duration instead of site feature

--- client/my-sites/customer-home/cards/tasks/titan-banner/index.jsx
+++ client/my-sites/customer-home/cards/tasks/titan-banner/index.jsx
@@ -1,11 +1,12 @@
-import { WPCOM_FEATURES_TITAN_MAIL_1YEAR_TRIAL } from '@automattic/calypso-products';
+import { TITAN_MAIL_YEARLY_SLUG } from '@automattic/calypso-products';
 import { useTranslate } from 'i18n-calypso';
 import { useSelector } from 'react-redux';
 import emailIllustration from 'calypso/assets/images/email-providers/email-illustration.svg';
+import QuerySiteProducts from 'calypso/components/data/query-site-products';
 import { TASK_UPSELL_TITAN } from 'calypso/my-sites/customer-home/cards/constants';
 import Task from 'calypso/my-sites/customer-home/cards/tasks/task';
 import { getEmailManagementPath } from 'calypso/my-sites/email/paths';
-import siteHasFeature from 'calypso/state/selectors/site-has-feature';
+import { getSiteAvailableProduct } from 'calypso/state/sites/products/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 const TitanBanner = () => {
@@ -13,35 +14,38 @@ const TitanBanner = () => {
 	const siteId = useSelector( getSelectedSiteId );
 	const siteSlug = useSelector( getSelectedSiteSlug );
 	const emailComparisonPath = getEmailManagementPath( siteSlug, siteSlug );
-	const hasOneYearTrial = useSelector( ( state ) =>
-		siteHasFeature( state, siteId, WPCOM_FEATURES_TITAN_MAIL_1YEAR_TRIAL )
+	const productSlug = TITAN_MAIL_YEARLY_SLUG;
+	const siteProduct = useSelector( ( state ) =>
+		siteId ? getSiteAvailableProduct( state, siteId, productSlug ) : null
 	);
-	const trialMonths = hasOneYearTrial ? 12 : 3;
-
+	const trialMonths = siteProduct?.introductory_offer?.interval_unit === 'year' ? 12 : 3;
 	return (
-		<Task
-			title={ translate( 'Get %(months)d months free Professional Email', {
-				args: {
-					months: trialMonths,
-				},
-				comment: '%(months)d is the number of free trial months',
-			} ) }
-			description={ translate(
-				'Build your brand with a custom @%(domain)s email address. Professional Email helps promote your site with every email you send.',
-				{
+		<>
+			{ siteId && <QuerySiteProducts siteId={ siteId } /> }
+			<Task
+				title={ translate( 'Get %(months)d months free Professional Email', {
 					args: {
-						domain: siteSlug,
+						months: trialMonths,
 					},
-				}
-			) }
-			actionText={ translate( 'Add email for free' ) }
-			actionUrl={ emailComparisonPath }
-			completeOnStart={ false }
-			enableSkipOptions
-			illustration={ emailIllustration }
-			taskId={ TASK_UPSELL_TITAN }
-			timing={ 3 }
-		/>
+					comment: '%(months)d is the number of free trial months',
+				} ) }
+				description={ translate(
+					'Build your brand with a custom @%(domain)s email address. Professional Email helps promote your site with every email you send.',
+					{
+						args: {
+							domain: siteSlug,
+						},
+					}
+				) }
+				actionText={ translate( 'Add email for free' ) }
+				actionUrl={ emailComparisonPath }
+				completeOnStart={ false }
+				enableSkipOptions
+				illustration={ emailIllustration }
+				taskId={ TASK_UPSELL_TITAN }
+				timing={ 3 }
+			/>
+		</>
 	);
 };
 
