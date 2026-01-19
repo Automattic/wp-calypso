b3bb5fc53a6e8b0491b0a7da2510efe2cf27a0d7
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2026-01-19 14:20:15 +0100

    Styling

--- client/switch-site/components/site-switch.tsx
+++ client/switch-site/components/site-switch.tsx
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled';
 import {
 	__experimentalHStack as HStack,
+	Modal,
+	TextControl,
 	TextHighlight,
-	__experimentalInputControl as InputControl,
 } from '@wordpress/components';
 import { chevronLeft as backIcon, Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
@@ -58,13 +59,13 @@ const Label = styled.div( {
 	whiteSpace: 'nowrap',
 	overflow: 'hidden',
 	fontSize: '1em',
-	'.commands-command-menu__container [cmdk-item] &': {
+	'.switch-site__site': {
 		color: 'var(--studio-gray-100)',
 	},
 	'.commands-command-menu__container [cmdk-item][aria-selected=true] &': {
 		color: 'var(--studio-white)',
 	},
-	'.commands-command-menu__container & mark': {
+	'.switch-site__site mark': {
 		fontWeight: 700,
 	},
 } );
@@ -104,8 +105,14 @@ const Switcher = ( { redirectTo }: { redirectTo: string } ) => {
 	} );
 
 	return (
-		<div>
-			<div className="commands-command-menu__header">
+		<Modal
+			className="switch-site__modal"
+			overlayClassName="switch-site__modal-overlay"
+			__experimentalHideHeader
+			onRequestClose={ () => {} }
+			shouldCloseOnClickOutside={ false }
+		>
+			<div className="switch-site__header">
 				<BackButton
 					type="button"
 					onClick={ () => {
@@ -115,8 +122,9 @@ const Switcher = ( { redirectTo }: { redirectTo: string } ) => {
 				>
 					<Icon icon={ backIcon } />
 				</BackButton>
-				<InputControl
+				<TextControl
 					ref={ input }
+					className="switch-site__search"
 					value={ search }
 					onChange={ ( value?: string ) => {
 						setSearch( value ?? '' );
@@ -127,65 +135,64 @@ const Switcher = ( { redirectTo }: { redirectTo: string } ) => {
 			</div>
 			<div>
 				{ filteredSites.length ? (
-					<div>
-						{ filteredSites.map( ( site ) => (
-							<div
-								key={ site.name }
-								role="option"
-								aria-selected="false"
-								tabIndex={ 0 }
-								onClick={ () => handleSiteSelect( site ) }
-								onKeyDown={ ( e ) => {
-									if ( e.key === 'Enter' || e.key === ' ' ) {
-										e.preventDefault();
-										handleSiteSelect( site );
-									}
-								} }
-								style={ { cursor: 'pointer' } }
+					filteredSites.map( ( site ) => (
+						<div
+							key={ site.name }
+							className="switch-site__site"
+							role="option"
+							aria-selected="false"
+							tabIndex={ 0 }
+							onClick={ () => handleSiteSelect( site ) }
+							onKeyDown={ ( e ) => {
+								if ( e.key === 'Enter' || e.key === ' ' ) {
+									e.preventDefault();
+									handleSiteSelect( site );
+								}
+							} }
+							style={ { cursor: 'pointer' } }
+						>
+							<HStack
+								alignment="left"
+								className={ clsx( 'commands-command-menu__item', {
+									'has-icon': site.icon,
+								} ) }
 							>
-								<HStack
-									alignment="left"
-									className={ clsx( 'commands-command-menu__item', {
-										'has-icon': site.icon,
-									} ) }
-								>
-									<FillDefaultIconWhite>
-										{ site.icon?.img ? (
-											<SiteIcon src={ site.icon.img } alt="" />
-										) : (
-											<EmptySiteIcon>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													viewBox="0 0 24 24"
-													width="24"
-													height="24"
-												>
-													<rect x="0" fill="none" width="24" height="24" />
-													<g>
-														<path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18l2-2 1-1v-2h-2v-1l-1-1H9v3l2 2v1.931C7.06 19.436 4 16.072 4 12l1 1h2v-2h2l3-3V6h-2L9 5v-.411a7.945 7.945 0 016 0V6l-1 1v2l1 1 3.13-3.13A7.983 7.983 0 0119.736 10H18l-2 2v2l1 1h2l.286.286C18.029 18.061 15.239 20 12 20z" />
-													</g>
-												</svg>
-											</EmptySiteIcon>
-										) }
-									</FillDefaultIconWhite>
-									<LabelWrapper>
-										<Label>
-											<TextHighlight text={ site.name || site.URL } highlight={ search } />
-										</Label>
+								<FillDefaultIconWhite>
+									{ site.icon?.img ? (
+										<SiteIcon src={ site.icon.img } alt="" />
+									) : (
+										<EmptySiteIcon>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 24 24"
+												width="24"
+												height="24"
+											>
+												<rect x="0" fill="none" width="24" height="24" />
+												<g>
+													<path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18l2-2 1-1v-2h-2v-1l-1-1H9v3l2 2v1.931C7.06 19.436 4 16.072 4 12l1 1h2v-2h2l3-3V6h-2L9 5v-.411a7.945 7.945 0 016 0V6l-1 1v2l1 1 3.13-3.13A7.983 7.983 0 0119.736 10H18l-2 2v2l1 1h2l.286.286C18.029 18.061 15.239 20 12 20z" />
+												</g>
+											</svg>
+										</EmptySiteIcon>
+									) }
+								</FillDefaultIconWhite>
+								<LabelWrapper>
+									<Label>
+										<TextHighlight text={ site.name || site.URL } highlight={ search } />
+									</Label>
 
-										<SubLabel>
-											<TextHighlight text={ site.URL } highlight={ search } />
-										</SubLabel>
-									</LabelWrapper>
-								</HStack>
-							</div>
-						) ) }
-					</div>
+									<SubLabel>
+										<TextHighlight text={ site.URL } highlight={ search } />
+									</SubLabel>
+								</LabelWrapper>
+							</HStack>
+						</div>
+					) )
 				) : (
 					<Empty>{ __( 'No results found.' ) }</Empty>
 				) }
 			</div>
-		</div>
+		</Modal>
 	);
 };
 
