62368f27a156f59d730b8d31c81946ac7e76de41
arthur <arthur.chu@automattic.com>
2026-01-21 14:29:49 +0800

    Add redirection for account notification settings

--- client/me/notification-settings/index.js
+++ client/me/notification-settings/index.js
@@ -1,11 +1,48 @@
 import page from '@automattic/calypso-router';
-import { makeLayout, render as clientRender } from 'calypso/controller';
+import {
+	makeLayout,
+	render as clientRender,
+	redirectIfMultiSiteDashboardForcedOptIn,
+} from 'calypso/controller';
+import { setupPreferences } from 'calypso/controller/preferences';
 import { sidebar } from 'calypso/me/controller';
 import { notifications, comments, updates, subscriptions } from './controller';
 
 export default function () {
-	page( '/me/notifications', sidebar, notifications, makeLayout, clientRender );
-	page( '/me/notifications/comments', sidebar, comments, makeLayout, clientRender );
-	page( '/me/notifications/updates', sidebar, updates, makeLayout, clientRender );
-	page( '/me/notifications/subscriptions', sidebar, subscriptions, makeLayout, clientRender );
+	page(
+		'/me/notifications',
+		setupPreferences,
+		redirectIfMultiSiteDashboardForcedOptIn( '/me/notifications' ),
+		sidebar,
+		notifications,
+		makeLayout,
+		clientRender
+	);
+	page(
+		'/me/notifications/comments',
+		setupPreferences,
+		redirectIfMultiSiteDashboardForcedOptIn( '/me/notifications/comments' ),
+		sidebar,
+		comments,
+		makeLayout,
+		clientRender
+	);
+	page(
+		'/me/notifications/updates',
+		setupPreferences,
+		redirectIfMultiSiteDashboardForcedOptIn( '/me/notifications/extras' ),
+		sidebar,
+		updates,
+		makeLayout,
+		clientRender
+	);
+	page(
+		'/me/notifications/subscriptions',
+		setupPreferences,
+		redirectIfMultiSiteDashboardForcedOptIn( '/me/notifications/emails' ),
+		sidebar,
+		subscriptions,
+		makeLayout,
+		clientRender
+	);
 }
