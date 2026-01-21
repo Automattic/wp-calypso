31eade882cf51cc750b9f556ce8189b21b321944
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2026-01-20 23:56:21 -0300

    Re-add noTopPadding and noBottomPadding properties

--- packages/onboarding/src/step-container-v2/components/ContentWrapper/ContentWrapper.tsx
+++ packages/onboarding/src/step-container-v2/components/ContentWrapper/ContentWrapper.tsx
@@ -7,6 +7,8 @@ type ContentWrapperProps = {
 	children: ReactNode;
 	centerAligned?: boolean;
 	axisDirection?: 'vertical' | 'horizontal';
+	noTopPadding?: boolean;
+	noBottomPadding?: boolean;
 	noPadding?: boolean;
 	isFullWidth?: boolean;
 };
@@ -15,6 +17,8 @@ export const ContentWrapper = ( {
 	children,
 	centerAligned,
 	axisDirection = 'vertical',
+	noTopPadding = false,
+	noBottomPadding = false,
 	noPadding = false,
 	isFullWidth = false,
 }: ContentWrapperProps ) => {
@@ -22,6 +26,8 @@ export const ContentWrapper = ( {
 		<div
 			className={ clsx( 'step-container-v2__content-wrapper', `axis-${ axisDirection }`, {
 				'center-aligned': centerAligned,
+				'no-top-padding': noTopPadding,
+				'no-bottom-padding': noBottomPadding,
 				'no-padding': noPadding,
 				'is-full-width': isFullWidth,
 			} ) }
