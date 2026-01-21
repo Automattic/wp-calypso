200dc1cd6a42a77da20b8d784b5ab700db00da69
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2026-01-20 22:16:36 -0300

    Simplify slider code

--- client/landing/stepper/declarative-flow/internals/steps-repository/__user/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/__user/index.tsx
@@ -149,35 +149,29 @@ const UserStepComponent: StepType = function UserStep( {
 	const carouselSlides = [
 		<div className="signup-slider-column" key="slide-1">
 			<div className="signup-slider-column-inner">
-				<div className="signup-slider-slide">
-					<p className="signup-slider-title">
-						{ translate( 'Join 472+ million websites worldwide' ) }
-					</p>
-				</div>
+				<p className="signup-slider-title">
+					{ translate( 'Join 472+ million websites worldwide' ) }
+				</p>
 			</div>
 		</div>,
 		<div className="signup-slider-column-2" key="slide-2">
 			<div className="signup-slider-column-inner">
-				<div className="signup-slider-slide">
-					<p className="signup-slider-testimonial">
-						{ translate(
-							'WordPress.com has made it easy to manage multiple news sites and blogs, letting me focus on the content rather than the technical aspects.'
-						) }
-					</p>
-					<p className="signup-slider-testimonial-author">{ translate( 'Brett S.' ) }</p>
-					<p className="signup-slider-testimonial-title">
-						{ translate( 'Founder/Blogger in Chief' ) }
-					</p>
-				</div>
+				<p className="signup-slider-testimonial">
+					{ translate(
+						'WordPress.com has made it easy to manage multiple news sites and blogs, letting me focus on the content rather than the technical aspects.'
+					) }
+				</p>
+				<p className="signup-slider-testimonial-author">{ translate( 'Brett S.' ) }</p>
+				<p className="signup-slider-testimonial-title">
+					{ translate( 'Founder/Blogger in Chief' ) }
+				</p>
 			</div>
 		</div>,
 		<div className="signup-slider-column-3" key="slide-3">
 			<div className="signup-slider-column-inner">
-				<div className="signup-slider-slide">
-					<p className="signup-slider-title">
-						{ translate( 'AI builds the site — you make it yours' ) }
-					</p>
-				</div>
+				<p className="signup-slider-title">
+					{ translate( 'AI builds the site — you make it yours' ) }
+				</p>
 			</div>
 		</div>,
 	];
