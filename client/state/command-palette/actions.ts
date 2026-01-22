bc7e0c0420768612bab1f3328ee499c650a702a2
Xavier Lozano Carreras <8511199+xavier-lc@users.noreply.github.com>
2026-01-22 10:25:55 +0100

    Remove the `command-palette` package and the `command-palette-wp-admin` app (#108214)

--- client/state/command-palette/actions.ts
+++
@@ -1,9 +0,0 @@
-import { COMMAND_PALETTE_OPEN, COMMAND_PALETTE_CLOSE } from 'calypso/state/action-types';
-
-export const openCommandPalette = () => ( {
-	type: COMMAND_PALETTE_OPEN,
-} );
-
-export const closeCommandPalette = () => ( {
-	type: COMMAND_PALETTE_CLOSE,
-} );
