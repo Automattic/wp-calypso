dcbf8c88cee7e8fd3543b39aa12738155dd53bc1
Philip Jackson <p-jackson@live.com>
2026-01-22 22:28:58 +1300

    Fix imports

--- client/dashboard/sites/site-fields/index.tsx
+++ client/dashboard/sites/site-fields/index.tsx
@@ -35,8 +35,8 @@ import { isSitePlanTrial } from '../plans';
 import SitePreview from '../site-preview';
 import { JetpackLogo } from './jetpack-logo';
 import type { SiteVisibility } from '../../types';
-import type { SiteBadge } from '../../utils/site-badge';
-import type { Site, SiteBlockingStatus } from '@automattic/api-core';
+import type { SiteBadge, SiteBlockingStatus } from '../../utils/site-badge';
+import type { Site } from '@automattic/api-core';
 import type { ComponentProps } from 'react';
 
 function IneligibleIndicator() {
--- client/dashboard/utils/site-status.ts
+++ client/dashboard/utils/site-status.ts
@@ -1,5 +1,6 @@
+import type { SiteBlockingStatus } from './site-badge';
 import type { SiteMigrationStatus } from '../types';
-import type { Site, SiteBlockingStatus } from '@automattic/api-core';
+import type { Site } from '@automattic/api-core';
 
 const MIGRATION_STATUSES: SiteMigrationStatus[ 'status' ][] = [ 'pending', 'started', 'completed' ];
 const MIGRATION_TYPES: SiteMigrationStatus[ 'type' ][] = [ 'difm', 'diy', 'ssh' ];
