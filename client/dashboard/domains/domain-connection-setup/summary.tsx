5d1012ec63fb7c139e284be8cd26c2f6eedaff27
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2026-01-23 18:54:13 -0300

    Remove redundant `as const`

--- client/dashboard/domains/domain-connection-setup/summary.tsx
+++ client/dashboard/domains/domain-connection-setup/summary.tsx
@@ -22,11 +22,11 @@ export default function DomainConnectionSetupSummary( {
 		isMappingVerificationSuccess( domainMappingStatus.mode ?? null, domainMappingStatus );
 
 	if ( isConnected ) {
-		badges.push( { text: __( 'Connected' ), intent: 'success' as const } );
+		badges.push( { text: __( 'Connected' ), intent: 'success' } );
 	} else if ( domainMappingStatus?.mode === DomainConnectionSetupMode.ADVANCED ) {
-		badges.push( { text: __( 'Verifying DNS' ), intent: 'warning' as const } );
+		badges.push( { text: __( 'Verifying DNS' ), intent: 'warning' } );
 	} else if ( domainMappingStatus?.mode === DomainConnectionSetupMode.SUGGESTED ) {
-		badges.push( { text: __( 'Verifying name servers' ), intent: 'warning' as const } );
+		badges.push( { text: __( 'Verifying name servers' ), intent: 'warning' } );
 	}
 
 	return (
