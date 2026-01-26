4883241852a18518835e084971899d10326e697a
Omar Alshaker <omar@omaralshaker.com>
2026-01-26 14:58:17 +0100

    Format happy blocks/support-footer (#108336)

--- apps/happy-blocks/block-library/support-content-footer/includes.php
+++ apps/happy-blocks/block-library/support-content-footer/includes.php
@@ -8,25 +8,24 @@
  * @package happy-blocks
  */
 
-if (! function_exists('happy_blocks_get_content_footer_asset')) {
+if ( ! function_exists( 'happy_blocks_get_content_footer_asset' ) ) {
 	/**
 	 * Find the URL of the asset file from happy-blocks.
 	 *
 	 * @param file $file The file name.
 	 */
-	function happy_blocks_get_content_footer_asset($file)
-	{
+	function happy_blocks_get_content_footer_asset( $file ) {
 		return array(
 			'path'    => "https://wordpress.com/wp-content/a8c-plugins/happy-blocks/block-library/support-content-footer/build/$file",
-			'version' => filemtime(__DIR__ . "/build/$file"),
+			'version' => filemtime( __DIR__ . "/build/$file" ),
 		);
 	}
 }
 
-$css = happy_blocks_get_content_footer_asset(is_rtl() ? 'view.rtl.css' : 'view.css');
-wp_enqueue_style('happy-blocks-support-footer-style', $css['path'], array(), $css['version']);
+$css = happy_blocks_get_content_footer_asset( is_rtl() ? 'view.rtl.css' : 'view.css' );
+wp_enqueue_style( 'happy-blocks-support-footer-style', $css['path'], array(), $css['version'] );
 
-if (! is_user_logged_in()) {
-	$js = happy_blocks_get_content_footer_asset('view.js');
-	wp_enqueue_script('happy-blocks-support-footer-view', $js['path'], array(), $js['version'], true);
+if ( ! is_user_logged_in() ) {
+	$js = happy_blocks_get_content_footer_asset( 'view.js' );
+	wp_enqueue_script( 'happy-blocks-support-footer-view', $js['path'], array(), $js['version'], true );
 }
