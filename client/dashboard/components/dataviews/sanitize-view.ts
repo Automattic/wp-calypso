ab144818072f7ea9c39679ee855f60689232333f
Ashar Fuadi <ashar.fuadi@automattic.com>
2026-01-26 10:33:41 +0700

    MSD: sanitize persisted view's filters (#108263)

--- client/dashboard/components/dataviews/sanitize-view.ts
+++ client/dashboard/components/dataviews/sanitize-view.ts
@@ -7,14 +7,7 @@ export function sanitizeView( view: View, fields: Field< any >[] ) {
 	// If no sanitization is needed then a reference to the same object should be returned.
 	let sanitized = view;
 
-	if ( sanitized.type === 'grid' && sanitized.layout?.previewSize ) {
-		// From PreviewSizePicker imageSizes in GB https://github.com/WordPress/gutenberg/blob/58a5abc7714bdff204d5f6bc350980f73686d54f/packages/dataviews/src/dataviews-layouts/grid/preview-size-picker.tsx#L14-L39
-		const smallestSize = 120;
-		if ( isNaN( sanitized.layout.previewSize ) || sanitized.layout.previewSize < smallestSize ) {
-			delete sanitized.layout.previewSize;
-		}
-	}
-
+	// TODO: remove once this upstream patch is released: https://github.com/WordPress/gutenberg/pull/73950
 	if ( sanitized.sort?.field ) {
 		const field = fields.find( ( field ) => field.id === sanitized.sort?.field );
 		if ( ! field || field.enableSorting === false ) {
@@ -24,7 +17,18 @@ export function sanitizeView( view: View, fields: Field< any >[] ) {
 	}
 
 	const fieldsSet = new Set( fields.map( ( field ) => field.id ) );
-	view.filters = view.filters?.filter( ( filter ) => fieldsSet.has( filter.field ) );
+	sanitized.filters = sanitized.filters?.filter( ( filter ) => {
+		if ( ! fieldsSet.has( filter.field ) ) {
+			return false;
+		}
+		if ( filter.operator === 'is' && Array.isArray( filter.value ) ) {
+			return false;
+		}
+		if ( filter.operator === 'isAny' && ! Array.isArray( filter.value ) ) {
+			return false;
+		}
+		return true;
+	} );
 
 	return sanitized;
 }
