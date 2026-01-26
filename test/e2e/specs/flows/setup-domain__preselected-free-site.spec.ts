4f8e713c4159a01605f234672a92a3d092701f72
Alister Scott <alister.scott@gmail.com>
2026-01-26 16:14:11 +1000

    Converted "test/e2e/specs/flows/setup-domain__preselected-free-site.spec.ts"

---
+++ test/e2e/specs/flows/setup-domain__preselected-free-site.spec.ts
@@ -0,0 +1,98 @@
+import {
+	BrowserManager,
+	NewTestUserDetails,
+	NewUserResponse,
+	NewSiteResponse,
+	RestAPIClient,
+} from '@automattic/calypso-e2e';
+import { tags, test } from '../../lib/pw-base';
+import { apiCloseAccount } from '../shared';
+
+test.describe(
+	'Domain flow: Purchase a domain for a pre-selected free site',
+	{
+		tag: [ tags.CALYPSO_RELEASE ],
+	},
+	() => {
+		const planName = 'Personal';
+		let selectedDomain: string;
+		let testUser: NewTestUserDetails;
+		let newUserDetails: NewUserResponse;
+		let newSiteDetails: NewSiteResponse;
+
+		test( 'As a new user, I can create a free site and then add a domain using pre-selected site flow', async ( {
+			page,
+			componentDomainSearch,
+			helperData,
+			pageCartCheckout,
+			pageSignupPickPlan,
+			pageUserSignUp,
+		} ) => {
+			testUser = helperData.getNewTestUser();
+
+			await test.step( 'When I enter the onboarding flow', async function () {
+				BrowserManager.setStoreCookie( page, { currency: 'USD' } );
+				await page.goto( helperData.getCalypsoURL( '/setup' ) );
+			} );
+
+			await test.step( 'And I sign up as a new user', async function () {
+				newUserDetails = await pageUserSignUp.signupSocialFirstWithEmail( testUser.email );
+			} );
+
+			await test.step( 'And I skip the domains step', async function () {
+				await componentDomainSearch.search( helperData.getBlogName() );
+				await componentDomainSearch.skipPurchase();
+			} );
+
+			await test.step( `And I select the ${ planName } plan`, async function () {
+				newSiteDetails = await pageSignupPickPlan.selectPlan( planName );
+			} );
+
+			await test.step( 'And I enter the domain flow with pre-selected site', async function () {
+				await page.goto(
+					helperData.getCalypsoURL(
+						`/setup/domain?siteSlug=${ newSiteDetails.blog_details.site_slug as string }`
+					)
+				);
+			} );
+
+			await test.step( 'And I add the first suggestion to the cart', async function () {
+				selectedDomain = await componentDomainSearch.selectFirstSuggestion();
+			} );
+
+			await test.step( 'And I continue to the next step', async function () {
+				await componentDomainSearch.continue();
+			} );
+
+			await test.step( `And I select the ${ planName } plan`, async function () {
+				await pageSignupPickPlan.selectPlanWithoutSiteCreation( planName );
+			} );
+
+			await test.step( 'Then I see the plan at checkout', async function () {
+				await pageCartCheckout.validateCartItem( `WordPress.com ${ planName }` );
+			} );
+
+			await test.step( 'And I see the domain at checkout', async function () {
+				await pageCartCheckout.validateCartItem( selectedDomain );
+			} );
+		} );
+
+		test.afterEach( 'Delete all user accounts generated', async function () {
+			if ( newUserDetails && testUser ) {
+				const restAPIClient = new RestAPIClient(
+					{
+						username: testUser.username,
+						password: testUser.password,
+					},
+					newUserDetails.body.bearer_token
+				);
+
+				await apiCloseAccount( restAPIClient, {
+					userID: newUserDetails.body.user_id,
+					username: newUserDetails.body.username,
+					email: testUser.email,
+				} );
+			}
+		} );
+	}
+);
