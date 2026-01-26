7fb9867dce47496f48c9628746fde8d13103d03b
Omar Alshaker <omar@omaralshaker.com>
2026-01-26 14:48:07 +0100

    Format happy blocks/support-footer

--- apps/happy-blocks/block-library/support-content-footer/includes.php
+++ apps/happy-blocks/block-library/support-content-footer/includes.php
@@ -16,8 +16,8 @@ if ( ! function_exists( 'happy_blocks_get_content_footer_asset' ) ) {
 	 */
 	function happy_blocks_get_content_footer_asset( $file ) {
 		return array(
-			'path'    => "https://wordpress.com/wp-content/a8c-plugins/happy-blocks/block-library/support-content-footer/$file",
-			'version' => filemtime( __DIR__ . "/$file" ),
+			'path'    => "https://wordpress.com/wp-content/a8c-plugins/happy-blocks/block-library/support-content-footer/build/$file",
+			'version' => filemtime( __DIR__ . "/build/$file" ),
 		);
 	}
 }
