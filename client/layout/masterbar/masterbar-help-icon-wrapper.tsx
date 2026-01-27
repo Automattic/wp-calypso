850caf4db068ee01d762932165a0eb70ad0692f3
Chris Blower <chris.blower@automattic.com>
2026-01-27 11:31:54 +0000

    not sure what this change is

---
+++ client/layout/masterbar/masterbar-help-icon-wrapper.tsx
@@ -0,0 +1,39 @@
+import { useShouldUseUnifiedAgent } from '@automattic/help-center/src/hooks';
+import AsyncLoad from 'calypso/components/async-load';
+
+interface MasterbarHelpIconWrapperProps {
+	siteId: number | null;
+	tooltip: string;
+}
+
+/**
+ * Wrapper component that decides which help icon to render based on the unified agent flag.
+ * Uses the API-based check (useShouldUseUnifiedAgent) instead of Redux preferences
+ * to ensure consistency with AgentsManagerLoader.
+ */
+export default function MasterbarHelpIconWrapper( {
+	siteId,
+	tooltip,
+}: MasterbarHelpIconWrapperProps ) {
+	const useUnifiedAgent = useShouldUseUnifiedAgent();
+
+	if ( useUnifiedAgent ) {
+		return (
+			<AsyncLoad
+				require="./masterbar-agents-manager"
+				siteId={ siteId }
+				tooltip={ tooltip }
+				placeholder={ null }
+			/>
+		);
+	}
+
+	return (
+		<AsyncLoad
+			require="./masterbar-help-center"
+			siteId={ siteId }
+			tooltip={ tooltip }
+			placeholder={ null }
+		/>
+	);
+}
