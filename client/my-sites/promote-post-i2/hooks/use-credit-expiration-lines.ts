2f5a5a9fa79a03e8429e813ff650e3fafdd654bb
Javier Olmo <43957544+jjolmo@users.noreply.github.com>
2026-01-30 14:07:08 +0100

    Replace moment library (#108440)

--- client/my-sites/promote-post-i2/hooks/use-credit-expiration-lines.ts
+++ client/my-sites/promote-post-i2/hooks/use-credit-expiration-lines.ts
@@ -1,7 +1,16 @@
+import { useLocale } from '@automattic/i18n-utils';
 import { formatCurrency } from '@automattic/number-formatters';
 import { useTranslate } from 'i18n-calypso';
-import moment from 'moment';
 import { ReactNode, useMemo } from 'react';
+
+function formatExpirationDate( expires: string, shortText: boolean, locale: string ): string {
+	const date = new Date( expires );
+	if ( isNaN( date.getTime() ) ) {
+		return expires;
+	}
+	const dateStyle = shortText ? 'short' : 'long';
+	return new Intl.DateTimeFormat( locale, { dateStyle } ).format( date );
+}
 
 /**
  * Custom hook to get formatted expiration lines for credits
@@ -11,6 +20,7 @@ export const useCreditExpirationLines = (
 	shortText: boolean
 ): ReactNode[] | null => {
 	const translate = useTranslate();
+	const locale = useLocale();
 
 	return useMemo( () => {
 		if ( ! sortedHistory || sortedHistory.length === 0 ) {
@@ -19,7 +29,7 @@ export const useCreditExpirationLines = (
 
 		if ( sortedHistory.length === 1 ) {
 			const firstItem = sortedHistory[ 0 ];
-			const firstDate = moment( firstItem.expires ).format( shortText ? 'L' : 'LL' );
+			const firstDate = formatExpirationDate( firstItem.expires, shortText, locale );
 			const firstAmount = formatCurrency( firstItem.amount, 'USD', { isSmallestUnit: true } );
 
 			return [
@@ -32,7 +42,7 @@ export const useCreditExpirationLines = (
 		const header = translate( 'You have several credit assignations:' );
 		const items = sortedHistory.map( ( item ) => {
 			const amount = formatCurrency( item.amount, 'USD', { isSmallestUnit: true } );
-			const date = moment( item.expires ).format( shortText ? 'L' : 'LL' );
+			const date = formatExpirationDate( item.expires, shortText, locale );
 			return shortText
 				? translate( '%(amount)s expiring on %(date)s', {
 						args: { amount, date },
@@ -43,5 +53,5 @@ export const useCreditExpirationLines = (
 		} );
 
 		return [ header, ...items ];
-	}, [ sortedHistory, shortText, translate ] );
+	}, [ sortedHistory, shortText, translate, locale ] );
 };
