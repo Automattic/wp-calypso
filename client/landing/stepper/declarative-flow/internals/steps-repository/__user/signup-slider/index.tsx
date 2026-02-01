da42fe13efce28777c7e55619a33b511678a0192
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2026-02-01 20:32:41 -0300

    Update slides according to design files

--- client/landing/stepper/declarative-flow/internals/steps-repository/__user/signup-slider/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/__user/signup-slider/index.tsx
@@ -1,5 +1,13 @@
 import { DotPager } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
+import slideImage1 from './images/img-01.png';
+import slideImage1x2 from './images/img-01@2x.png';
+import slideImage2 from './images/img-02.png';
+import slideImage2x2 from './images/img-02@2x.png';
+import slideImage3 from './images/img-03.png';
+import slideImage3x2 from './images/img-03@2x.png';
+import slideImage4 from './images/img-04.png';
+import slideImage4x2 from './images/img-04@2x.png';
 
 import './style.scss';
 
@@ -9,6 +17,15 @@ export const SignupSlider = ( { hideDescription }: { hideDescription: boolean } 
 	const carouselSlides = [
 		<div className="signup-slider__slide signup-slider__slide--1" key="slide-1">
 			<div className="signup-slider__slide-content">
+				<div className="signup-slider__slide-image-container">
+					<img
+						className="signup-slider__slide-image signup-slider__slide-image--1"
+						src={ slideImage1 }
+						srcSet={ `${ slideImage1 } 1x, ${ slideImage1x2 } 2x` }
+						alt=""
+						aria-hidden="true"
+					/>
+				</div>
 				<p className="signup-slider__headline">
 					{ translate( 'WordPress powers 43% of the web' ) }
 				</p>
@@ -23,6 +40,15 @@ export const SignupSlider = ( { hideDescription }: { hideDescription: boolean } 
 		</div>,
 		<div className="signup-slider__slide signup-slider__slide--2" key="slide-2">
 			<div className="signup-slider__slide-content signup-slider__slide-content--testimonial">
+				<div className="signup-slider__slide-image-container">
+					<img
+						className="signup-slider__slide-image signup-slider__slide-image--2"
+						src={ slideImage2 }
+						srcSet={ `${ slideImage2 } 1x, ${ slideImage2x2 } 2x` }
+						alt=""
+						aria-hidden="true"
+					/>
+				</div>
 				<p className="signup-slider__testimonial-text">
 					{ translate(
 						'“I wholeheartedly recommend WordPress.com to anyone seeking to build a professional, visually stunning website.”'
@@ -33,6 +59,15 @@ export const SignupSlider = ( { hideDescription }: { hideDescription: boolean } 
 		</div>,
 		<div className="signup-slider__slide signup-slider__slide--3" key="slide-3">
 			<div className="signup-slider__slide-content">
+				<div className="signup-slider__slide-image-container">
+					<img
+						className="signup-slider__slide-image signup-slider__slide-image--3"
+						src={ slideImage3 }
+						srcSet={ `${ slideImage3 } 1x, ${ slideImage3x2 } 2x` }
+						alt=""
+						aria-hidden="true"
+					/>
+				</div>
 				<p className="signup-slider__headline">{ translate( 'From idea to site in minutes' ) }</p>
 				{ ! hideDescription && (
 					<p className="signup-slider__description">
@@ -45,6 +80,15 @@ export const SignupSlider = ( { hideDescription }: { hideDescription: boolean } 
 		</div>,
 		<div className="signup-slider__slide signup-slider__slide--4" key="slide-4">
 			<div className="signup-slider__slide-content signup-slider__slide-content--testimonial">
+				<div className="signup-slider__slide-image-container">
+					<img
+						className="signup-slider__slide-image signup-slider__slide-image--4"
+						src={ slideImage4 }
+						srcSet={ `${ slideImage4 } 1x, ${ slideImage4x2 } 2x` }
+						alt=""
+						aria-hidden="true"
+					/>
+				</div>
 				<p className="signup-slider__testimonial-text">
 					{ translate(
 						'“I started with zero experience of running my own site, and WordPress.com made it easy to create something that really works for me and my site.”'
