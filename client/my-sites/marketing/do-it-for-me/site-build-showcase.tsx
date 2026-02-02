31173fd2f2708cf81456532341898861d9aa989f
arthur791004 <arthur791004@gmail.com>
2026-02-02 10:38:58 +0800

    Revert "Upgrade @wordpress/components to 32.1.0 (#108301)"
    This reverts commit 20ee411fbded1ac0a2b396d03cbb299dba9532f6.

--- client/my-sites/marketing/do-it-for-me/site-build-showcase.tsx
+++ client/my-sites/marketing/do-it-for-me/site-build-showcase.tsx
@@ -19,6 +19,7 @@ import wooSite3 from 'calypso/assets/images/difm/woo-site-builds-en/woo-3.jpg';
 import wooSite4 from 'calypso/assets/images/difm/woo-site-builds-en/woo-4.jpg';
 import wooSite5 from 'calypso/assets/images/difm/woo-site-builds-en/woo-5.jpg';
 import wooSite6 from 'calypso/assets/images/difm/woo-site-builds-en/woo-6.jpg';
+import type { CSSPropertiesWithMultiValues } from '@emotion/serialize';
 
 /**
  * Generate keyframes for the fade animation. There are two
@@ -62,15 +63,13 @@ function fadeAnimation( slideCount: number, interval: number, fadeDelay: number 
  * @param interval Time interval for displaying each slide, in seconds.
  */
 function nthOfTypeAnimationDelay( slideCount: number, interval: number ) {
-	const styles: string[] = [];
+	const styles: Record< string, CSSPropertiesWithMultiValues > = {};
 	for ( let i = 2; i <= slideCount; i++ ) {
-		styles.push( `
-			> div:nth-of-type(${ i }) {
-				animation-delay: ${ ( i - 1 ) * interval }s;
-			}
-		` );
+		styles[ `> div:nth-of-type( ${ i } )` ] = {
+			animationDelay: `${ ( i - 1 ) * interval }s`,
+		};
 	}
-	return styles.join( '' );
+	return styles;
 }
 
 type AnimatedSlideShowProps = {
