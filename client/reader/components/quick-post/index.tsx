7eea825d199afdb6791157aee0e8c3ce4718c3b4
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-02-03 14:46:36 +0500

    🐛 FIX: tests

--- client/reader/components/quick-post/index.tsx
+++ client/reader/components/quick-post/index.tsx
@@ -9,7 +9,7 @@ import { EmbedRequestParams } from '@automattic/verbum-block-editor/src/api';
 import { useMutation } from '@tanstack/react-query';
 // @ts-expect-error - No declaration file for heading block.
 import * as heading from '@wordpress/block-library/build-module/heading';
-import { createBlock, parse, serialize, unregisterBlockType } from '@wordpress/blocks';
+import { createBlock, parse, serialize } from '@wordpress/blocks';
 import { Button, __experimentalHStack as HStack } from '@wordpress/components';
 import { addQueryArgs } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
