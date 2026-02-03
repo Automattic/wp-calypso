33bd1ee2dbda4105816dde608baa00beb6e40087
Dave Martin <davemart-in@users.noreply.github.com>
2026-02-03 11:42:36 -0500

    Converted post-navigation.jsx to TypeScript

--- client/blocks/reader-full-post/post-navigation.tsx
+++ client/blocks/reader-full-post/post-navigation.tsx
@@ -1,10 +1,28 @@
 import { Gridicon } from '@automattic/components';
 import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
-import PropTypes from 'prop-types';
+
 import './post-navigation.scss';
 
-const NavigationButton = ( { direction, post, postKey, onNavigate, translate } ) => {
+interface PostKey {
+	feedId?: number;
+	blogId?: number;
+	postId: number;
+}
+
+interface Post {
+	title?: string;
+}
+
+interface NavigationButtonProps {
+	direction: 'previous' | 'next';
+	post: Post | null;
+	postKey: PostKey | null;
+	onNavigate: ( postKey: PostKey ) => void;
+}
+
+const NavigationButton = ( { direction, post, postKey, onNavigate }: NavigationButtonProps ) => {
+	const translate = useTranslate();
 	const isNext = direction === 'next';
 	const label = isNext ? translate( 'Next post' ) : translate( 'Previous post' );
 	const icon = isNext ? 'chevron-right' : 'chevron-left';
@@ -21,9 +39,11 @@ const NavigationButton = ( { direction, post, postKey, onNavigate, translate } )
 				'reader-full-post-navigation__link-button',
 				isNext && 'reader-full-post-navigation__link-button--next'
 			) }
-			aria-label={ translate( '%(label)s: %(title)s', {
-				args: { label, title: post?.title || label },
-			} ) }
+			aria-label={ String(
+				translate( '%(label)s: %(title)s', {
+					args: { label: String( label ), title: post?.title || String( label ) },
+				} )
+			) }
 		>
 			{ ! isNext && <Gridicon icon={ icon } size={ 18 } /> }
 			<div className="reader-full-post-navigation__link-content">
@@ -37,13 +57,13 @@ const NavigationButton = ( { direction, post, postKey, onNavigate, translate } )
 	);
 };
 
-NavigationButton.propTypes = {
-	direction: PropTypes.oneOf( [ 'previous', 'next' ] ).isRequired,
-	post: PropTypes.object,
-	postKey: PropTypes.object,
-	onNavigate: PropTypes.func.isRequired,
-	translate: PropTypes.func.isRequired,
-};
+interface ReaderFullPostNavigationProps {
+	previousPost: Post | null;
+	nextPost: Post | null;
+	previousPostKey: PostKey | null;
+	nextPostKey: PostKey | null;
+	onNavigate: ( postKey: PostKey ) => void;
+}
 
 const ReaderFullPostNavigation = ( {
 	previousPost,
@@ -51,9 +71,7 @@ const ReaderFullPostNavigation = ( {
 	previousPostKey,
 	nextPostKey,
 	onNavigate,
-} ) => {
-	const translate = useTranslate();
-
+}: ReaderFullPostNavigationProps ) => {
 	if ( ! previousPostKey && ! nextPostKey ) {
 		return null;
 	}
@@ -69,7 +87,6 @@ const ReaderFullPostNavigation = ( {
 						post={ previousPost }
 						postKey={ previousPostKey }
 						onNavigate={ onNavigate }
-						translate={ translate }
 					/>
 				</div>
 
@@ -79,20 +96,11 @@ const ReaderFullPostNavigation = ( {
 						post={ nextPost }
 						postKey={ nextPostKey }
 						onNavigate={ onNavigate }
-						translate={ translate }
 					/>
 				</div>
 			</div>
 		</div>
 	);
-};
-
-ReaderFullPostNavigation.propTypes = {
-	previousPost: PropTypes.object,
-	nextPost: PropTypes.object,
-	previousPostKey: PropTypes.object,
-	nextPostKey: PropTypes.object,
-	onNavigate: PropTypes.func.isRequired,
 };
 
 export default ReaderFullPostNavigation;
