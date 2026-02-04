3851cb0a2aa478b1171e4fe33d27a3418c24aed0
Welly Shen <welly.shen@automattic.com>
2026-02-05 00:15:37 +0800

    Make `AgentsManagerSite` doc more general

--- packages/data-stores/src/agents-manager/types.ts
+++ packages/data-stores/src/agents-manager/types.ts
@@ -4,11 +4,6 @@ import type { DispatchFromMap, SelectFromMap } from '../mapped-types';
 
 /**
  * Minimal site data needed by AgentsManager.
- *
- * Unlike HelpCenterSite which has many fields, AgentsManager only uses:
- * - ID: for JWT auth token generation
- * - URL: for constructing wp-admin links
- * - domain: for support article content filtering
  */
 export interface AgentsManagerSite {
 	ID: number | string;
