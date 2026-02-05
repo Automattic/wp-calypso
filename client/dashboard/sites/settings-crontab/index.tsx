4432b6eab0de871d147ca18e361f67019b06389e
Volodymyr Makukha <nei.css@gmail.com>
2026-02-05 10:25:34 +0000

    Print tooltip inline and muted color

--- client/dashboard/sites/settings-crontab/index.tsx
+++ client/dashboard/sites/settings-crontab/index.tsx
@@ -6,7 +6,7 @@ import {
 } from '@automattic/api-queries';
 import { useMutation, useQuery, useSuspenseQuery } from '@tanstack/react-query';
 import { useRouter } from '@tanstack/react-router';
-import { Icon, Button, __experimentalText as Text, Tooltip } from '@wordpress/components';
+import { Icon, Button, __experimentalText as Text } from '@wordpress/components';
 import { useDispatch } from '@wordpress/data';
 import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
 import { createInterpolateElement } from '@wordpress/element';
@@ -104,9 +104,12 @@ export default function CrontabSettings( { siteSlug }: { siteSlug: string } ) {
 				const cronDescription = cronstrue.toString( item.schedule, { verbose: true } );
 
 				return (
-					<Tooltip text={ cronDescription }>
-						<Text>{ getScheduleLabel( item.schedule ) }</Text>
-					</Tooltip>
+					<div>
+						<Text>{ getScheduleLabel( item.schedule ) }</Text>{ ' ' }
+						<Text variant="muted" size={ 12 }>
+							({ cronDescription })
+						</Text>
+					</div>
 				);
 			},
 			enableGlobalSearch: true,
