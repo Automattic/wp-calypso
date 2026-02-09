314eabc6bebdc9ccb5bee1844db766fe4eed8295
Addison Stavlo <Addison.Stavlo@Automattic.com>
2026-02-09 14:33:37 -0500

    update shouldCloseModal logic to consider loading state and actual data

--- client/blocks/reader-suggested-follows/dialog.jsx
+++ client/blocks/reader-suggested-follows/dialog.jsx
@@ -55,8 +55,8 @@ const ReaderSuggestedFollowsDialog = ( {
 		enabled: prefetch || isVisible,
 	} );
 
-	const hasData = Array.isArray( data ) && data.length > 0;
-	const shouldCloseModal = isFetched && isVisible && ! hasData;
+	const hasData = Array.isArray( data ) && data?.length > 0;
+	const shouldCloseModal = isFetched && isVisible && ! isLoading && ! hasData;
 
 	useEffect( () => {
 		if ( isVisible && resourceType ) {
