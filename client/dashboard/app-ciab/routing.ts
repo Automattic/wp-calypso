6b236308f59de7be54dc8c7b3b4cc5a00d9c1073
Ashar Fuadi <ashar.fuadi@automattic.com>
2026-02-10 22:19:16 +0700

    Fix tests

--- client/dashboard/app-ciab/routing.ts
+++ client/dashboard/app-ciab/routing.ts
@@ -1,7 +1,7 @@
 const CIAB_DASHBOARD_ALLOWED_HOSTNAMES = [ 'my.woo.localhost', 'my.woo.com' ];
 
-export function isAllowedCiabDashboardHostname( hostname: string ): boolean {
-	return CIAB_DASHBOARD_ALLOWED_HOSTNAMES.includes( hostname );
+export function isAllowedCiabDashboardHostname( hostname?: string ): boolean {
+	return CIAB_DASHBOARD_ALLOWED_HOSTNAMES.includes( hostname ?? '' );
 }
 
 export function getCiabDashboardBasePath( hostname: string ): string {
--- client/dashboard/app-dotcom/routing.ts
+++ client/dashboard/app-dotcom/routing.ts
@@ -1,10 +1,10 @@
 const DOTCOM_DASHBOARD_ALLOWED_HOSTNAMES = [ 'my.localhost', 'my.wordpress.com' ];
 
-export function isAllowedDotcomDashboardHostname( hostname: string ): boolean {
+export function isAllowedDotcomDashboardHostname( hostname?: string ): boolean {
 	// Calypso Live links
-	if ( hostname.endsWith( '.calypso.live' ) ) {
+	if ( hostname?.endsWith( '.calypso.live' ) ) {
 		return true;
 	}
 
-	return DOTCOM_DASHBOARD_ALLOWED_HOSTNAMES.includes( hostname );
+	return DOTCOM_DASHBOARD_ALLOWED_HOSTNAMES.includes( hostname ?? '' );
 }
