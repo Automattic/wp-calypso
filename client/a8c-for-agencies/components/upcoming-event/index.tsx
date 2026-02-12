afa04218612320524432101c3637de6cd4ed7990
James Kenneth Guidaven <jkguidaven@gmail.com>
2026-02-13 01:07:16 +0800

    Make logo decorative and no need to specify alt.

--- client/a8c-for-agencies/components/upcoming-event/index.tsx
+++ client/a8c-for-agencies/components/upcoming-event/index.tsx
@@ -62,9 +62,7 @@ const UpcomingEvent = ( {
 		<div className="a4a-event">
 			<div className="a4a-event__content">
 				<div className="a4a-event__header">
-					<div className="a4a-event__logo">
-						{ logoElement ?? <img src={ logoUrl } alt={ title as string } /> }
-					</div>
+					<div className="a4a-event__logo">{ logoElement ?? <img src={ logoUrl } alt="" /> }</div>
 					<div className="a4a-event__date-and-title">
 						<div className={ clsx( 'a4a-event__date', dateClassName ) }>
 							<time dateTime={ dateTimeString }>{ displayDate }</time>
