3fb801e6aa1e88a023064535a3229c76898932b4
Alister Scott <alister.scott@gmail.com>
2026-02-15 14:37:28 +1000

    Convert stats.ts to stats.spec.ts

---
+++ test/e2e/specs/stats/stats.spec.ts
@@ -0,0 +1,149 @@
+import {
+	DataHelper,
+	StatsPage,
+	envVariables,
+	envToFeatureKey,
+	getTestAccountByFeature,
+} from '@automattic/calypso-e2e';
+import { expect, tags, test } from '../../lib/pw-base';
+
+/**
+ * Shallowly tests the Stats feature, including Jetpack/Odyssey stats.
+ *
+ * Keywords: Stats, Jetpack, Odyssey Stats
+ */
+test.describe( 'Stats', { tag: [ tags.CALYPSO_PR, tags.JETPACK_WPCOM_INTEGRATION ] }, () => {
+	let statsPage: StatsPage;
+
+	test( 'View stats for Traffic, Insights, and Subscribers', async ( {
+		accountGivenByEnvironment,
+		componentSidebar,
+		page,
+	} ) => {
+		const accountName = getTestAccountByFeature( envToFeatureKey( envVariables ), [
+			{ gutenberg: 'stable', siteType: 'simple', accountName: 'defaultUser' },
+		] );
+
+		await test.step( `Given I am authenticated as '${ accountName }'`, async function () {
+			await accountGivenByEnvironment.authenticate( page );
+		} );
+
+		await test.step( 'When I navigate to Stats', async function () {
+			statsPage = new StatsPage( page );
+
+			if ( envVariables.ATOMIC_VARIATION === 'ecomm-plan' ) {
+				await statsPage.visit( DataHelper.getAccountSiteURL( accountName, { protocol: false } ) );
+			} else {
+				await componentSidebar.navigate( 'Stats' );
+			}
+		} );
+
+		await test.step( 'Then I see the Stats page', async function () {
+			await expect( page ).toHaveURL( /stats/ );
+		} );
+
+		await test.step( 'When I click on the Traffic tab', async function () {
+			await statsPage.clickTab( 'Traffic' );
+		} );
+
+		await test.step( 'Then I see the Traffic stats', async function () {
+			await expect( page ).toHaveURL( /stats\/day/ );
+		} );
+
+		await test.step( 'When I filter traffic activity to Likes', async function () {
+			await statsPage.showStatsOfType( { tab: 'Traffic', type: 'Likes' } );
+		} );
+
+		await test.step( 'Then I see the Likes stats', async function () {
+			// The stats page should still be visible and functional
+			await expect( page ).toHaveURL( /stats/ );
+		} );
+
+		await test.step( 'When I click on the Insights tab', async function () {
+			await statsPage.clickTab( 'Insights' );
+		} );
+
+		await test.step( 'Then I see the Insights stats', async function () {
+			await expect( page ).toHaveURL( /stats\/insights/ );
+		} );
+
+		await test.step( 'When I click to view all annual insights', async function () {
+			await statsPage.clickViewAllAnnualInsights();
+		} );
+
+		await test.step( 'Then I see the annual insights page', async function () {
+			// Right now, we can't actually verify stats data because if run right after a test site purge,
+			// there may be nothing in there. We just verify that we can get to the page.
+			await expect( page ).toHaveURL( /annualstats/ );
+		} );
+
+		await test.step( 'When I go back', async function () {
+			await page.goBack();
+		} );
+
+		await test.step( 'Then I return to the Insights tab', async function () {
+			await expect( page ).toHaveURL( /stats\/insights/ );
+		} );
+
+		await test.step( 'When I click on the Subscribers tab', async function () {
+			await statsPage.clickTab( 'Subscribers' );
+		} );
+
+		await test.step( 'Then I see the Subscribers stats', async function () {
+			await expect( page ).toHaveURL( /stats\/subscribers/ );
+		} );
+	} );
+
+	// The Store tab is not present unless Business or higher plan is on the site and the
+	// site has gone AT.
+	test( 'View Store stats', async ( { accountGivenByEnvironment, componentSidebar, page } ) => {
+		const accountName = getTestAccountByFeature( envToFeatureKey( envVariables ), [
+			{ gutenberg: 'stable', siteType: 'simple', accountName: 'defaultUser' },
+		] );
+
+		test.skip(
+			accountName !== 'jetpackAtomicEcommPlanUser',
+			'Store tab only available for jetpackAtomicEcommPlanUser'
+		);
+
+		await test.step( `Given I am authenticated as '${ accountName }'`, async function () {
+			await accountGivenByEnvironment.authenticate( page );
+		} );
+
+		await test.step( 'When I navigate to Stats', async function () {
+			statsPage = new StatsPage( page );
+
+			if ( envVariables.ATOMIC_VARIATION === 'ecomm-plan' ) {
+				await statsPage.visit( DataHelper.getAccountSiteURL( accountName, { protocol: false } ) );
+			} else {
+				await componentSidebar.navigate( 'Stats' );
+			}
+		} );
+
+		await test.step( 'When I click on the Store tab', async function () {
+			await statsPage.clickTab( 'Store' );
+		} );
+
+		await test.step( 'Then I see the Store stats', async function () {
+			await expect( page ).toHaveURL( /store\/stats\/orders/ );
+		} );
+
+		await test.step( 'When I select "Years" stats period', async function () {
+			await statsPage.selectStatsPeriod( 'Years' );
+		} );
+
+		await test.step( 'Then the Years period is selected', async function () {
+			// Verify the stats page is still functional
+			await expect( page ).toHaveURL( /store\/stats/ );
+		} );
+
+		await test.step( 'When I select "Gross Sales" stats type', async function () {
+			await statsPage.showStatsOfType( { tab: 'Store', type: 'Gross Sales' } );
+		} );
+
+		await test.step( 'Then I see the Gross Sales stats', async function () {
+			// Verify the stats page displays the selected type
+			await expect( page ).toHaveURL( /store\/stats/ );
+		} );
+	} );
+} );
