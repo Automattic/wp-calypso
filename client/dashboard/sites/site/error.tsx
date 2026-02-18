f01e4842e884fa124c30285f916127a44743f86f
Chris Jean <chris.jean@automattic.com>
2026-02-18 13:36:40 -0600

    Update: Change error messages on commerce garden sites
    When there is an issue pulling information from a commerce garden site,
    the error message displays Jetpack-specific errors. For these sites, we
    don't want to display Jetpack references. This update removes those
    references and the link to troubleshoot the site.

--- client/dashboard/sites/site/error.tsx
+++ client/dashboard/sites/site/error.tsx
@@ -1,12 +1,15 @@
 import { isInaccessibleJetpackError } from '@automattic/api-core';
+import { useQueryClient } from '@tanstack/react-query';
 import { ExternalLink } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import UnknownError from '../../app/500';
+import { getSiteFromCache } from '../../app/analytics/super-props';
 import { siteRoute } from '../../app/router/sites';
 import { Notice } from '../../components/notice';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
 import RouterLinkButton from '../../components/router-link-button';
+import { isCommerceGarden } from '../../utils/site-types';
 
 export default function Error( { error }: { error: Error } ) {
 	if ( isInaccessibleJetpackError( error ) ) {
@@ -17,6 +20,14 @@ export default function Error( { error }: { error: Error } ) {
 
 function InaccessibleJetpackError( { error }: { error: Error } ) {
 	const { siteSlug } = siteRoute.useParams();
+	const queryClient = useQueryClient();
+	const site = getSiteFromCache( queryClient, siteSlug );
+	const isCommerceGardenSite = site ? isCommerceGarden( site ) : false;
+
+	// For commerce garden sites, remove "Jetpack" from the API error message
+	const displayMessage = isCommerceGardenSite
+		? error.message.replace( /^The Jetpack site /, 'The site ' )
+		: error.message;
 
 	return (
 		<PageLayout
@@ -33,14 +44,20 @@ function InaccessibleJetpackError( { error }: { error: Error } ) {
 			notices={
 				<Notice
 					variant="error"
-					title={ __( 'Your Jetpack site can not be reached at this time.' ) }
+					title={
+						isCommerceGardenSite
+							? __( 'Your site can not be reached at this time.' )
+							: __( 'Your Jetpack site can not be reached at this time.' )
+					}
 					actions={
-						<ExternalLink href="https://jetpack.com/support/getting-started-with-jetpack/fixing-jetpack-connection-issues/">
-							{ __( 'Troubleshoot your Jetpack site' ) }
-						</ExternalLink>
+						! isCommerceGardenSite && (
+							<ExternalLink href="https://jetpack.com/support/getting-started-with-jetpack/fixing-jetpack-connection-issues/">
+								{ __( 'Troubleshoot your Jetpack site' ) }
+							</ExternalLink>
+						)
 					}
 				>
-					{ error.message }
+					{ displayMessage }
 				</Notice>
 			}
 		></PageLayout>
