1bd2d59da9a12e795a10bbffaa3f0e350bc093a1
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2026-02-18 11:07:44 -0300

    Fix empty benefits array

--- client/my-sites/checkout/checkout-thank-you/redesign-v2/pages/domain-only-new/index.tsx
+++ client/my-sites/checkout/checkout-thank-you/redesign-v2/pages/domain-only-new/index.tsx
@@ -65,7 +65,7 @@ export default function DomainOnlyNew( {
 									}
 								),
 						  ]
-						: []
+						: undefined
 				}
 				href={ createSiteFromDomainOnly( domainPurchase.meta, domainPurchase.blogId ) }
 			/>
