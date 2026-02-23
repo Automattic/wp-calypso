e192dad029cf07088e38102cc724893aa03f9182
upwardmomentum84 <93800099+upwardmomentum84@users.noreply.github.com>
2026-02-23 16:24:46 -0500

    Update test/index.jsx for testing the list on simple sites

--- client/my-sites/plugins/plugins-browser-item/test/index.jsx
+++ client/my-sites/plugins/plugins-browser-item/test/index.jsx
@@ -29,7 +29,7 @@ describe( 'PluginsBrowserItem Incompatible Plugins Message', () => {
 		isAtomicSite.mockImplementation( () => false );
 
 		const props = {
-			plugin: { name: 'wordfence', slug: 'wordfence' },
+			plugin: { name: 'really-simple-ssl', slug: 'really-simple-ssl' },
 		};
 
 		render( <PluginsBrowserListElement { ...props } /> );
