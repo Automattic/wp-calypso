6cc3fcc61341c34c42557a1f0521510f745c08fb
Payton Swick <payton@foolord.com>
2026-02-24 10:46:22 -0500

    Allow createRequestCartProduct with only purchaseId

--- packages/shopping-cart/src/create-request-cart-product.ts
+++ packages/shopping-cart/src/create-request-cart-product.ts
@@ -3,10 +3,15 @@ import type { RequestCartProduct, MinimalRequestCartProduct } from './types';
 export default function createRequestCartProduct(
 	properties: MinimalRequestCartProduct
 ): RequestCartProduct {
-	if ( ! properties.product_slug ) {
-		throw new Error( 'product_slug is required for request cart products' );
+	if ( ! properties.product_slug && ! properties.extra?.purchaseId ) {
+		throw new Error(
+			'product_slug or extra.purchaseId are required to create request cart products'
+		);
 	}
 	const { product_slug, product_id, meta, volume, quantity, extra } = properties;
+	if ( extra?.purchaseId && ! extra.purchaseType ) {
+		extra.purchaseType = 'renewal';
+	}
 	return {
 		product_slug,
 		product_id,
