603ea3cf3942a8b9d9936d3684046aca8d485e16
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-02-24 11:05:29 +0500

    💫 UPDATE: use Object.freeze in ADD_SUBSCRIPTION_CONFIGS

--- client/reader/new-subscription/components/add-subscription-form/consts.tsx
+++ client/reader/new-subscription/components/add-subscription-form/consts.tsx
@@ -22,110 +22,111 @@ interface AddSubscriptionFormConfig {
 }
 
 const BASE_URL: string = 'reader/new';
-export const ADD_SUBSCRIPTION_CONFIGS: Record< SubscriptionType, AddSubscriptionFormConfig > = {
-	[ 'add-new' ]: {
-		slug: 'add-new',
-		title: translate( 'Add new' ),
-		url: `/${ BASE_URL }`,
-		pathname: isDiscoverV3Enabled() ? '/reader/new' : '/discover/add-new',
-		source: isDiscoverV3Enabled() ? 'reader-add-new' : 'discover-add-new',
-		placeholder: undefined,
-	},
-	[ 'reddit' ]: {
-		slug: 'reddit',
-		title: translate( 'Reddit' ),
-		url: `/${ BASE_URL }/reddit`,
-		pathname: isDiscoverV3Enabled() ? '/reader/new/reddit' : '/discover/reddit',
-		source: isDiscoverV3Enabled() ? 'reader-add-reddit' : 'discover-reddit',
-		placeholder: translate( 'Search by Reddit URL' ),
-		instructions: {
-			icon: <ReaderRedditIcon iconSize={ 75 } />,
-			title: translate( 'Common Reddit URLs' ),
-			infoList: [
-				{ label: translate( 'Front page:' ), info: 'www.reddit.com/.rss' },
-				{
-					label: translate( 'A subreddit:' ),
-					info: 'www.reddit.com/r/{ SUBREDDIT }/.rss',
-				},
-				{
-					label: translate( 'A user:' ),
-					info: 'www.reddit.com/user/{ REDDITOR }/.rss',
-				},
-				{
-					label: translate( 'User comments:' ),
-					info: 'www.reddit.com/user/{ REDDITOR }/comments/.rss',
-				},
-				{
-					label: translate( 'User submissions:' ),
-					info: 'www.reddit.com/user/{ REDDITOR }/submitted/.rss',
-				},
-				{
-					label: translate( 'Search result:' ),
-					info: 'www.reddit.com/search.rss?q={ QUERY }',
-				},
-			],
+export const ADD_SUBSCRIPTION_CONFIGS: Record< SubscriptionType, AddSubscriptionFormConfig > =
+	Object.freeze( {
+		[ 'add-new' ]: {
+			slug: 'add-new',
+			title: translate( 'Add new' ),
+			url: `/${ BASE_URL }`,
+			pathname: isDiscoverV3Enabled() ? '/reader/new' : '/discover/add-new',
+			source: isDiscoverV3Enabled() ? 'reader-add-new' : 'discover-add-new',
+			placeholder: undefined,
 		},
-	},
-	[ 'youtube' ]: {
-		slug: 'youtube',
-		title: translate( 'YouTube' ),
-		url: `/${ BASE_URL }/youtube`,
-		pathname: '/reader/new/youtube',
-		source: 'reader-add-youtube',
-		placeholder: translate( 'Search by YouTube URL' ),
-		instructions: {
-			icon: <ReaderYouTubeIcon iconSize={ 75 } />,
-			title: translate( 'Common YouTube URLs' ),
-			infoList: [
-				{ label: translate( 'Channel:' ), info: 'www.youtube.com/@YT_HANDLE' },
-				{
-					label: translate( 'Channel using ID:' ),
-					info: 'https://www.youtube.com/channel/CHANNEL_ID',
-				},
-				{
-					label: translate( 'Playlist:' ),
-					info: 'www.youtube.com/feeds/videos.xml?playlist_id=PLAYLIST_ID',
-				},
-			],
+		[ 'reddit' ]: {
+			slug: 'reddit',
+			title: translate( 'Reddit' ),
+			url: `/${ BASE_URL }/reddit`,
+			pathname: isDiscoverV3Enabled() ? '/reader/new/reddit' : '/discover/reddit',
+			source: isDiscoverV3Enabled() ? 'reader-add-reddit' : 'discover-reddit',
+			placeholder: translate( 'Search by Reddit URL' ),
+			instructions: {
+				icon: <ReaderRedditIcon iconSize={ 75 } />,
+				title: translate( 'Common Reddit URLs' ),
+				infoList: [
+					{ label: translate( 'Front page:' ), info: 'www.reddit.com/.rss' },
+					{
+						label: translate( 'A subreddit:' ),
+						info: 'www.reddit.com/r/{ SUBREDDIT }/.rss',
+					},
+					{
+						label: translate( 'A user:' ),
+						info: 'www.reddit.com/user/{ REDDITOR }/.rss',
+					},
+					{
+						label: translate( 'User comments:' ),
+						info: 'www.reddit.com/user/{ REDDITOR }/comments/.rss',
+					},
+					{
+						label: translate( 'User submissions:' ),
+						info: 'www.reddit.com/user/{ REDDITOR }/submitted/.rss',
+					},
+					{
+						label: translate( 'Search result:' ),
+						info: 'www.reddit.com/search.rss?q={ QUERY }',
+					},
+				],
+			},
 		},
-	},
-	[ 'tumblr' ]: {
-		slug: 'tumblr',
-		title: translate( 'Tumblr' ),
-		url: `/${ BASE_URL }/tumblr`,
-		pathname: '/reader/new/tumblr',
-		source: 'new-tumblr-subscription',
-		placeholder: translate( 'Search by Tumblr URL' ),
-		instructions: {
-			icon: <ReaderTumblrIcon iconSize={ 75 } />,
-			title: translate( 'Common Tumblr URLs' ),
-			infoList: [
-				{ label: translate( 'Staff picks:' ), info: 'staff.tumblr.com/rss' },
-				{ label: translate( 'A blog:' ), info: '{ BLOG_NAME }.tumblr.com/rss' },
-				{
-					label: translate( 'Blog tag:' ),
-					info: '{ BLOG_NAME }.tumblr.com/tagged/{ TAG_NAME }/rss',
-				},
-			],
+		[ 'youtube' ]: {
+			slug: 'youtube',
+			title: translate( 'YouTube' ),
+			url: `/${ BASE_URL }/youtube`,
+			pathname: '/reader/new/youtube',
+			source: 'reader-add-youtube',
+			placeholder: translate( 'Search by YouTube URL' ),
+			instructions: {
+				icon: <ReaderYouTubeIcon iconSize={ 75 } />,
+				title: translate( 'Common YouTube URLs' ),
+				infoList: [
+					{ label: translate( 'Channel:' ), info: 'www.youtube.com/@YT_HANDLE' },
+					{
+						label: translate( 'Channel using ID:' ),
+						info: 'https://www.youtube.com/channel/CHANNEL_ID',
+					},
+					{
+						label: translate( 'Playlist:' ),
+						info: 'www.youtube.com/feeds/videos.xml?playlist_id=PLAYLIST_ID',
+					},
+				],
+			},
 		},
-	},
-	[ 'substack' ]: {
-		slug: 'substack',
-		title: translate( 'Substack' ),
-		url: `/${ BASE_URL }/substack`,
-		pathname: '/reader/new/substack',
-		source: 'reader-add-substack',
-		placeholder: translate( 'Search by Substack URL' ),
-		instructions: {
-			icon: <ReaderSubstackIcon iconSize={ 75 } />,
-			title: translate( 'Common Substack URLs' ),
-			infoList: [
-				{
-					label: translate( 'Publication:' ),
-					info: 'https://{ PUBLICATION }.substack.com',
-				},
-				{ label: translate( 'Custom domain:' ), info: 'https://{ CUSTOM_DOMAIN }/feed' },
-			],
+		[ 'tumblr' ]: {
+			slug: 'tumblr',
+			title: translate( 'Tumblr' ),
+			url: `/${ BASE_URL }/tumblr`,
+			pathname: '/reader/new/tumblr',
+			source: 'new-tumblr-subscription',
+			placeholder: translate( 'Search by Tumblr URL' ),
+			instructions: {
+				icon: <ReaderTumblrIcon iconSize={ 75 } />,
+				title: translate( 'Common Tumblr URLs' ),
+				infoList: [
+					{ label: translate( 'Staff picks:' ), info: 'staff.tumblr.com/rss' },
+					{ label: translate( 'A blog:' ), info: '{ BLOG_NAME }.tumblr.com/rss' },
+					{
+						label: translate( 'Blog tag:' ),
+						info: '{ BLOG_NAME }.tumblr.com/tagged/{ TAG_NAME }/rss',
+					},
+				],
+			},
 		},
-	},
-};
+		[ 'substack' ]: {
+			slug: 'substack',
+			title: translate( 'Substack' ),
+			url: `/${ BASE_URL }/substack`,
+			pathname: '/reader/new/substack',
+			source: 'reader-add-substack',
+			placeholder: translate( 'Search by Substack URL' ),
+			instructions: {
+				icon: <ReaderSubstackIcon iconSize={ 75 } />,
+				title: translate( 'Common Substack URLs' ),
+				infoList: [
+					{
+						label: translate( 'Publication:' ),
+						info: 'https://{ PUBLICATION }.substack.com',
+					},
+					{ label: translate( 'Custom domain:' ), info: 'https://{ CUSTOM_DOMAIN }/feed' },
+				],
+			},
+		},
+	} );
