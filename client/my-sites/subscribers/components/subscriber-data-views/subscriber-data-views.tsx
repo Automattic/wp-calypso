43928dbf1098dc3b24d6226abcb0fcc69114f4b8
Mikael Korpela <mikael@ihminen.org>
2026-02-25 11:55:08 +0200

    Remove JP logo temporarily

--- client/my-sites/subscribers/components/subscriber-data-views/subscriber-data-views.tsx
+++ client/my-sites/subscribers/components/subscriber-data-views/subscriber-data-views.tsx
@@ -10,7 +10,6 @@ import { DataViews, type View, type ViewTable, type Action, Operator } from '@wo
 import { useMemo, useState, useCallback, useEffect } from '@wordpress/element';
 import { plus, trash } from '@wordpress/icons';
 import { translate } from 'i18n-calypso';
-import JetpackLogo from 'calypso/components/jetpack-logo';
 import { useSubscribedNewsletterCategories } from 'calypso/data/newsletter-categories';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { useSelector, useDispatch } from 'calypso/state';
@@ -628,12 +627,7 @@ export default function SubscriberDataViews( {
 		>
 			<section className="subscriber-data-views__list">
 				<Page
-					title={
-						<span className="subscriber-data-views__title">
-							{ ! isJetpackCloud() && <JetpackLogo size={ 20 } /> }
-							{ translate( 'Subscribers' ) }
-						</span>
-					}
+					title={ translate( 'Subscribers' ) }
 					subTitle={
 						! selectedSubscriber &&
 						translate(
