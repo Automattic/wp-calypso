d17d55ef924c37b00cf58e76b51bd77e14763821
Noam Almosnino <noamalmosnino@gmail.com>
2026-02-25 12:13:29 +0100

    feat: build developer tools page

--- client/a8c-for-agencies/sections/dev-tools/controller.tsx
+++ client/a8c-for-agencies/sections/dev-tools/controller.tsx
@@ -1,12 +1,12 @@
 import { type Callback } from '@automattic/calypso-router';
 import PageViewTracker from 'calypso/a8c-for-agencies/components/a4a-page-view-tracker';
 import LearnSidebar from 'calypso/a8c-for-agencies/components/sidebar-menu/learn';
-import DevToolsOverview from './primary/dev-tools';
+import DevToolsOverview from './primary/dev-tools-overview';
 
 export const devToolsContext: Callback = ( context, next ) => {
 	context.primary = (
 		<>
-			<PageViewTracker title="Resource center > Developer tools" path={ context.path } />
+			<PageViewTracker title="Resources and tools > Developer tools" path={ context.path } />
 			<DevToolsOverview />
 		</>
 	);
