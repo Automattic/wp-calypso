9fb924db4c299c2b08475b40cb244d9b27159982
arthur <arthur.chu@automattic.com>
2026-02-26 15:55:47 +0800

    Display error if the site slug is not available

--- client/dashboard/sites/site/error.tsx
+++ client/dashboard/sites/site/error.tsx
@@ -1,5 +1,35 @@
+import { isInaccessibleJetpackError } from '@automattic/api-core';
+import { __ } from '@wordpress/i18n';
 import UnknownError from '../../app/500';
+import { siteRoute } from '../../app/router/sites';
+import { PageHeader } from '../../components/page-header';
+import PageLayout from '../../components/page-layout';
+import RouterLinkButton from '../../components/router-link-button';
+import { InaccessibleJetpackNotice } from './notices';
 
 export default function Error( { error }: { error: Error } ) {
+	if ( isInaccessibleJetpackError( error ) ) {
+		return <InaccessibleJetpackError error={ error } />;
+	}
 	return <UnknownError error={ error } />;
+}
+
+function InaccessibleJetpackError( { error }: { error: Error } ) {
+	const { siteSlug } = siteRoute.useParams();
+
+	return (
+		<PageLayout
+			header={
+				<PageHeader
+					title={ siteSlug }
+					actions={
+						<RouterLinkButton to="/sites" variant="primary" __next40pxDefaultSize>
+							{ __( 'Go to Sites' ) }
+						</RouterLinkButton>
+					}
+				/>
+			}
+			notices={ <InaccessibleJetpackNotice variant="error" error={ error } /> }
+		></PageLayout>
+	);
 }
--- client/dashboard/sites/site/notices.tsx
+++ client/dashboard/sites/site/notices.tsx
@@ -1,11 +1,18 @@
 import { ExternalLink } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { Notice } from '../../components/notice';
+import type { ComponentProps } from 'react';
 
-export function InaccessibleJetpackNotice( { error }: { error: Error } ) {
+export function InaccessibleJetpackNotice( {
+	variant = 'warning',
+	error,
+}: {
+	variant?: ComponentProps< typeof Notice >[ 'variant' ];
+	error: Error;
+} ) {
 	return (
 		<Notice
-			variant="warning"
+			variant={ variant }
 			title={ __( 'Your Jetpack site can not be reached at this time.' ) }
 			actions={
 				<ExternalLink href="https://jetpack.com/support/getting-started-with-jetpack/fixing-jetpack-connection-issues/">
