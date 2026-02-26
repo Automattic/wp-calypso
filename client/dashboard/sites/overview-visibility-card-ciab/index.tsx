1cac2ea45761529ed61fa34a0c3213cc8b7dee9c
Philip Jackson <p-jackson@live.com>
2026-02-26 16:07:51 +1300

    MSD: link CIAB visibility card to wp-admin settings

--- client/dashboard/sites/overview-visibility-card-ciab/index.tsx
+++ client/dashboard/sites/overview-visibility-card-ciab/index.tsx
@@ -10,10 +10,15 @@ const CARD_PROPS = {
 };
 
 export default function VisibilityCardCiab( { site }: { site: Site } ) {
+	const link = site.options?.admin_url
+		? `${ site.options.admin_url }admin.php?page=next-admin&p=%2Fwoocommerce%2Fsettings%2Fgeneral`
+		: undefined;
+
 	if ( site.is_coming_soon ) {
 		return (
 			<OverviewCard
 				{ ...CARD_PROPS }
+				link={ link }
 				icon={ launch }
 				heading={ __( 'Coming soon' ) }
 				description={ __( 'Visitors will see a coming soon page.' ) }
@@ -25,6 +30,7 @@ export default function VisibilityCardCiab( { site }: { site: Site } ) {
 		return (
 			<OverviewCard
 				{ ...CARD_PROPS }
+				link={ link }
 				icon={ lockOutline }
 				heading={ __( 'Private' ) }
 				description={ __( 'Only invited users can view your site.' ) }
@@ -35,6 +41,7 @@ export default function VisibilityCardCiab( { site }: { site: Site } ) {
 	return (
 		<OverviewCard
 			{ ...CARD_PROPS }
+			link={ link }
 			icon={ published }
 			heading={ __( 'Public' ) }
 			description={ __( 'Anyone can view your site.' ) }
