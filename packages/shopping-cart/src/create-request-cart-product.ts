738bd92d3170dc4aa6c1cf970d01f13b2fd76e6b
Payton Swick <payton@foolord.com>
2026-02-27 16:26:51 -0500

    Require explicitly setting purchaseType in createRequestCartProduct

--- packages/shopping-cart/src/create-request-cart-product.ts
+++ packages/shopping-cart/src/create-request-cart-product.ts
@@ -10,7 +10,10 @@ export default function createRequestCartProduct(
 	}
 	const { product_slug, product_id, meta, volume, quantity, extra } = properties;
 	if ( extra?.purchaseId && ! extra.purchaseType ) {
-		extra.purchaseType = 'renewal';
+		// eslint-disable-next-line no-console
+		console.warn(
+			'Creating request cart product with purchaseId but no purchaseType; that might be a mistake if this should be a renewal'
+		);
 	}
 	return {
 		product_slug,
