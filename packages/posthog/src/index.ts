9fb7776905cf7abff0d6bcecd4fb9f070de999da
Jorge A. Torres <jorge@0xbad.com>
2026-03-03 13:22:31 +0000

    Use `buckets.advertising` instead of `buckets.analytics`  for PostHog elegibility

--- packages/posthog/src/index.ts
+++ packages/posthog/src/index.ts
@@ -24,7 +24,7 @@ export function init( apiKey: string, user?: PostHogUser ) {
 	}
 
 	const { buckets } = getTrackingPrefs();
-	if ( ! buckets.analytics ) {
+	if ( ! buckets.advertising ) {
 		return;
 	}
 
