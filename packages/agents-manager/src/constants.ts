3d2ddfeb2f767408e300874d2df345188bdbceb4
Omar Alshaker <omar@omaralshaker.com>
2026-03-03 14:29:17 +0100

    Remove unneeded testing code

--- packages/agents-manager/src/constants.ts
+++ packages/agents-manager/src/constants.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = 'https://public-api.wordpress.com';
 
 export const ORCHESTRATOR_AGENT_URL = `${ API_BASE_URL }/wpcom/v2/ai/agent`;
-export const ORCHESTRATOR_AGENT_ID = 'wpcom-workflow-support_chat';
+export const ORCHESTRATOR_AGENT_ID = 'wp-orchestrator';
 
 export const LOCAL_TOOL_RUNNING_MESSAGE = 'local_tool_running';
