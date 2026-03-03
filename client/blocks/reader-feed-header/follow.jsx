f6ba7cf07ba3fb6b0c48e2299271b6ccade97b10
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-03-03 11:53:19 +0500

    🐛 FIX: code formatting

--- client/blocks/reader-feed-header/follow.jsx
+++ client/blocks/reader-feed-header/follow.jsx
@@ -122,7 +122,7 @@ export default function ReaderFeedHeaderFollow( props ) {
 										iconSize={ 24 }
 										showLabel={ false }
 										siteId={ siteId }
-										subscriptionId={ Number(subscriptionId) }
+										subscriptionId={ Number( subscriptionId ) }
 									/>
 								</div>
 							) }
