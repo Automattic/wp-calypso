e50efa6140c61a57e3c16332a9bcc1d2c0e2894e
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-03-06 19:29:13 +0500

    UPDATE: Use post title fallback on sidebar of full post view

--- client/reader/recent/recent-post-field.tsx
+++ client/reader/recent/recent-post-field.tsx
@@ -1,10 +1,16 @@
 import { forwardRef } from 'react';
 import ReaderFeaturedImage from 'calypso/blocks/reader-featured-image';
 import AutoDirection from 'calypso/components/auto-direction';
-import type { PostItem } from './types';
+import { getPostTitleFallback } from 'calypso/reader/utils';
 
 interface RecentPostFieldProps {
-	post: PostItem;
+	post: {
+		title: string;
+		excerpt: string;
+		content: string;
+		featured_image: string;
+		site_name: string;
+	};
 }
 
 const RecentPostField = forwardRef< HTMLDivElement, RecentPostFieldProps >( ( { post }, ref ) => {
@@ -16,7 +22,9 @@ const RecentPostField = forwardRef< HTMLDivElement, RecentPostFieldProps >( ( { 
 		<div className="recent-post-field" ref={ ref } role="button" tabIndex={ 0 }>
 			<AutoDirection>
 				<div className="recent-post-field__title">
-					<div className="recent-post-field__title-text">{ post?.title }</div>
+					<div className="recent-post-field__title-text">
+						{ post?.title || getPostTitleFallback( post ) }
+					</div>
 					<div className="recent-post-field__site-name">{ post?.site_name }</div>
 				</div>
 			</AutoDirection>
