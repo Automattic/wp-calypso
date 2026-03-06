dc469e8d69329c0b9a0c6686b60ac5905bf09c09
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-03-06 19:26:50 +0500

    UPDATE: Page title of full post view to use title fallback if not present

--- client/blocks/reader-full-post/index.jsx
+++ client/blocks/reader-full-post/index.jsx
@@ -42,7 +42,7 @@ import {
 	recordTrackForPost,
 	recordPermalinkClick,
 } from 'calypso/reader/stats';
-import { showSelectedPost } from 'calypso/reader/utils';
+import { getPostTitleFallback, showSelectedPost } from 'calypso/reader/utils';
 import { requestPostComments } from 'calypso/state/comments/actions';
 import { isCommentsApiDisabled } from 'calypso/state/comments/selectors/get-comments-api-disabled';
 import { like as likePost, unlike as unlikePost } from 'calypso/state/posts/likes/actions';
@@ -759,7 +759,9 @@ export class FullPostView extends Component {
 					{ ! post || post._state === 'pending' ? (
 						<DocumentHead title={ translate( 'Loading' ) } />
 					) : (
-						<DocumentHead title={ `${ post.title } ‹ ${ siteName } ‹ Reader` } />
+						<DocumentHead
+							title={ `${ post.title || getPostTitleFallback( post ) } ‹ ${ siteName } ‹ Reader` }
+						/>
 					) }
 					{ post && post.feed_ID && <QueryReaderFeed feedId={ +post.feed_ID } /> }
 					{ post && ! post.is_external && post.site_ID && (
