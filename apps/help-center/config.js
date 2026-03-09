231f175ecfb73aa431c5c4ab9eb8b6b8704e3138
Emanuele Buccelli <escapemanuele@gmail.com>
2026-03-09 19:07:33 +0100

    CIAB goes to prod Zendesk

--- apps/help-center/config.js
+++ apps/help-center/config.js
@@ -1,9 +1,11 @@
 /* global helpCenterData */
 const isProxied = typeof helpCenterData !== 'undefined' && helpCenterData?.isProxied;
+const isCIAB = typeof helpCenterData !== 'undefined' && helpCenterData?.isCommerceGarden;
+const envValue = isProxied && ! isCIAB ? 'staging' : 'production';
 
 window.configData = {
-	env_id: isProxied ? 'staging' : 'production',
-	env: isProxied ? 'staging' : 'production',
+	env_id: envValue,
+	env: envValue,
 	features: {
 		'help/gpt-response': true,
 	},
