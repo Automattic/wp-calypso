42dea0fd6aa5ea8ac9a2b0a986bbe8a925f2d645
Matthew Reishus <mreishus@users.noreply.github.com>
2026-03-10 14:30:14 +0000

    remove mtime

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -186,19 +186,6 @@ object BuildDockerImage : BuildType({
 		mergeTrunk( skipIfConflict = true )
 
 		script {
-			name = "Restore git mtime"
-			scriptContent = """
-				#!/usr/bin/env bash
-				sudo apt-get install -y git-restore-mtime
-				/usr/lib/git-core/git-restore-mtime --force --commit-time --skip-missing
-			"""
-			dockerImage = "%docker_image_e2e%"
-			dockerRunParameters = "-u %env.UID%"
-			dockerPull = true
-			dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-		}
-
-		script {
 			name = "Prepare Docker workspace manifests"
 			scriptContent = """
 				#!/usr/bin/env bash
