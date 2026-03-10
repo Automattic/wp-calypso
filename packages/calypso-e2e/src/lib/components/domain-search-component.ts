1d1a4f53bce7b4a60f382ec5d35c655305a7febf
Michael Cain <michael@michaeldcain.com>
2026-03-10 16:44:03 -0400

    Fix onboarding skip plans test

--- packages/calypso-e2e/src/lib/components/domain-search-component.ts
+++ packages/calypso-e2e/src/lib/components/domain-search-component.ts
@@ -176,7 +176,7 @@ export class DomainSearchComponent {
 		await addToCartButton.waitFor( { state: 'detached', timeout: 30000 } );
 
 		if ( waitForContinueButton ) {
-			const continueButton = row.getByRole( 'button', { name: 'Continue' } );
+			const continueButton = this.page.getByRole( 'button', { name: 'Continue' } );
 			await continueButton.waitFor();
 		}
 
