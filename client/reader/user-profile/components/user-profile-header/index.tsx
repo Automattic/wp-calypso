11a6f03f5a0746d920a9e5f429f236c06062624e
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-03-11 16:47:38 +0500

    💫 UPDATE: user profile header

--- client/reader/user-profile/components/user-profile-header/index.tsx
+++ client/reader/user-profile/components/user-profile-header/index.tsx
@@ -1,6 +1,6 @@
-import { external, Icon } from '@wordpress/icons';
+import './style.scss';
+import clsx from 'clsx';
 import { useTranslate } from 'i18n-calypso';
-import React, { useEffect, useRef, useState } from 'react';
 import ReaderAvatar from 'calypso/blocks/reader-avatar';
 import AutoDirection from 'calypso/components/auto-direction';
 import SectionNav from 'calypso/components/section-nav';
@@ -8,8 +8,6 @@ import NavItem from 'calypso/components/section-nav/item';
 import NavTabs from 'calypso/components/section-nav/tabs';
 import { UserProfileData } from 'calypso/lib/user/user';
 import { getUserProfileUrl } from 'calypso/reader/user-profile/user-profile.utils';
-
-import './style.scss';
 
 interface UserProfileHeaderProps {
 	user: UserProfileData;
@@ -39,69 +37,24 @@ const UserProfileHeader = ( { user, view }: UserProfileHeaderProps ): JSX.Elemen
 
 	const selectedTab = navigationItems.find( ( item ) => item.selected )?.label || '';
 
-	const avatarElement = (
-		<ReaderAvatar author={ { ...user, has_avatar: !! user.avatar_URL } } iconSize={ 116 } />
-	);
-
-	const bioRef = useRef< HTMLSpanElement >( null );
-	const [ isClamped, setIsClamped ] = useState( false );
-
-	useEffect( () => {
-		if ( bioRef.current ) {
-			const element = bioRef.current;
-			const originalHeight = element.offsetHeight;
-
-			// Temporarily remove the clamp
-			element.style.webkitLineClamp = 'unset';
-			const fullHeight = element.scrollHeight;
-
-			// Restore the clamp
-			element.style.webkitLineClamp = '3';
-
-			// Determine if the text is clamped
-			setIsClamped( fullHeight > originalHeight );
-		}
-	}, [ user.bio ] );
-
 	return (
-		<div className="user-profile-header">
-			<header className="user-profile-header__main">
-				<div
-					className="user-profile-header__avatar user-profile-header__avatar-desktop"
-					data-testid="desktop-avatar"
-				>
-					{ avatarElement }
-				</div>
-				<AutoDirection>
-					<div className="user-profile-header__details">
-						<div className="user-profile-header__display-name">
-							<div
-								className="user-profile-header__avatar user-profile-header__avatar-mobile"
-								data-testid="mobile-avatar"
-							>
-								{ avatarElement }
-							</div>
-
-							<h1>{ user.display_name }</h1>
-						</div>
-						{ user.bio && (
-							<div className="user-profile-header__bio">
-								<p className="user-profile-header__bio-desc">
-									<span ref={ bioRef } className="user-profile-header__bio-desc-text">
-										{ user.bio }
-									</span>
-
-									{ isClamped && user.profile_URL && (
-										<a className="user-profile-header__bio-desc-link" href={ user.profile_URL }>
-											{ translate( 'Read More' ) }{ ' ' }
-											<Icon width={ 18 } height={ 18 } icon={ external } />
-										</a>
-									) }
-								</p>
-							</div>
-						) }
+		<>
+			<header className="user-profile-header">
+				<div className="user-profile-header__user-info">
+					<ReaderAvatar author={ { ...user, has_avatar: !! user.avatar_URL } } iconSize={ 56 } />
+					<div className="user-profile-header__names">
+						<h1>{ user.display_name }</h1>
+						<p>@{ user.user_login }</p>
 					</div>
-				</AutoDirection>
+				</div>
+
+				{ user.bio && (
+					<AutoDirection>
+						<div className="user-profile-header__bio">
+							<p className={ clsx( 'user-profile-header__bio-desc' ) }>{ user.bio }</p>
+						</div>
+					</AutoDirection>
+				) }
 			</header>
 			<SectionNav enforceTabsView selectedText={ selectedTab } variation="minimal">
 				<NavTabs>
@@ -112,7 +65,7 @@ const UserProfileHeader = ( { user, view }: UserProfileHeaderProps ): JSX.Elemen
 					) ) }
 				</NavTabs>
 			</SectionNav>
-		</div>
+		</>
 	);
 };
 
