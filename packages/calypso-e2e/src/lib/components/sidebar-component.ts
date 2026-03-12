42e4ce2d58cf38dfa56779de77694617dda76182
Daniel Lind <daniel.lind@automattic.com>
2026-03-12 01:30:17 +0100

    Fix for simple sites

--- packages/calypso-e2e/src/lib/components/sidebar-component.ts
+++ packages/calypso-e2e/src/lib/components/sidebar-component.ts
@@ -89,10 +89,10 @@ export class SidebarComponent {
 			// all operations to avoid inconsistency if the DOM changes.
 			const subitemLocator = this.page
 				.locator(
-					`.is-toggle-open a:text-is("${ subitem }"), .wp-menu-open .wp-submenu a:text-is("${ subitem }")`
+					`.is-toggle-open a:has(:text-is("${ subitem }")), .wp-menu-open .wp-submenu a:text-is("${ subitem }")`
 				)
 				.first();
-			await subitemLocator.waitFor( { state: 'attached' } );
+			await subitemLocator.waitFor();
 			const hrefSubItemSelector = ( await subitemLocator.getAttribute( 'href' ) ) as string;
 
 			await subitemLocator.dispatchEvent( 'click' );
