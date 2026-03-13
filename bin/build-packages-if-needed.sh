1619d80bb5a90f0dceaccc2afc605ea24208f3cc
Matthew Reishus <mreishus@users.noreply.github.com>
2026-03-13 16:57:22 +0000

    build-packages: Pass --topological

--- bin/build-packages-if-needed.sh
+++ bin/build-packages-if-needed.sh
@@ -4,7 +4,7 @@
 # prerequisite packages were cleaned and need to be prepared again.
 if [ ! -d "packages/create-calypso-config/dist" ] ; then
 	echo "Building packages..."
-	yarn workspaces foreach --all --parallel --verbose run prepare
+	yarn workspaces foreach --all --parallel --topological --verbose run prepare
 else
 	echo "Packages are built."
 fi
