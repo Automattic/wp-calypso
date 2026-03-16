68e1e101d5272c2920b208b1cf88f325b8799624
Sérgio Gomes <mail@sgomes.com>
2026-03-16 15:39:52 +0000

    Lazily load 'he' dependency

--- packages/api-core/src/me/fetchers.ts
+++ packages/api-core/src/me/fetchers.ts
@@ -1,16 +1,20 @@
-import { decode } from 'he';
 import { wpcom } from '../wpcom-fetcher';
 import type { User, TwoStep } from './types';
 
-function decodeEntities( text: string ) {
+async function decode( text: string ) {
+	const { decode } = await import( 'he' );
+	return decode( text );
+}
+
+async function decodeEntities( text: string ) {
 	// Bypass decode if text doesn't include entities
 	if ( 'string' !== typeof text || -1 === text.indexOf( '&' ) ) {
 		return text;
 	}
-
 	return decode( text );
 }
-function decodeUserObject( user: User ): User {
+
+async function decodeUserObject( user: User ): Promise< User > {
 	const decodedKeys = [ 'display_name', 'description', 'user_URL' ];
 	const decodedUser = { ...user };
 
@@ -21,7 +25,7 @@ function decodeUserObject( user: User ): User {
 
 		const value = decodedUser[ key ];
 		if ( typeof value === 'string' ) {
-			decodedUser[ key ] = decodeEntities( value ) as never;
+			decodedUser[ key ] = ( await decodeEntities( value ) ) as never;
 		}
 	}
 
