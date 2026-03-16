6aafafe53d5133a578b66b1dfadcc4495c25d635
Copons <Copons@users.noreply.github.com>
2026-03-16 18:38:29 +0000

    Theme Showcase: Smooth scroll to filter bar when sticky
    When the modern filter bar is sticky and a search, filter, or tier
    change is triggered, smooth scroll to the filter bar sentinel instead
    of jumping to the top of the page. When not sticky, skip scrolling
    entirely since the user is already near the top.

--- client/my-sites/themes/filter-bar-modern/index.tsx
+++ client/my-sites/themes/filter-bar-modern/index.tsx
@@ -31,6 +31,7 @@ interface FilterBarModernProps {
 	showTierFilter?: boolean;
 	searchQuery?: string;
 	onSearch?: ( query: string ) => void;
+	sentinelRef?: React.RefObject< HTMLDivElement >;
 }
 
 const FilterBarModern = ( {
@@ -43,6 +44,7 @@ const FilterBarModern = ( {
 	showTierFilter = true,
 	searchQuery = '',
 	onSearch,
+	sentinelRef,
 }: FilterBarModernProps ) => {
 	const translate = useTranslate();
 	const [ isSticky, setIsSticky ] = useState( false );
@@ -115,7 +117,7 @@ const FilterBarModern = ( {
 				fallbackInView
 				onChange={ ( inView ) => setIsSticky( ! inView ) }
 			>
-				<div className="filter-bar-modern__sentinel" />
+				<div className="filter-bar-modern__sentinel" ref={ sentinelRef } />
 			</InView>
 			<div className={ clsx( 'filter-bar-modern', { 'is-sticky': isSticky } ) }>
 				<div className="filter-bar-modern__content">
