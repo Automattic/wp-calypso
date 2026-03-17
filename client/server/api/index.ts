e5440d7e186b3529f6e857d8ca22d51f631eff45
Omar Alshaker <omar@omaralshaker.com>
2026-03-17 10:22:36 +0100

    Remove unneeded code

--- client/server/api/index.ts
+++ client/server/api/index.ts
@@ -1,19 +1,12 @@
 import config from '@automattic/calypso-config';
-import bodyParser from 'body-parser';
 import express from 'express';
 import signInWithApple from './sign-in-with-apple';
-import runCommand from './verto/run-command';
 
 export default function api() {
 	const app = express();
 
 	if ( config.isEnabled( 'sign-in-with-apple/redirect' ) ) {
 		signInWithApple( app );
-	}
-
-	// Calypso Agentic Framework: run CLI commands (development only)
-	if ( config( 'env_id' ) === 'development' ) {
-		app.post( '/api/verto/run-command', bodyParser.json(), runCommand );
 	}
 
 	return app;
