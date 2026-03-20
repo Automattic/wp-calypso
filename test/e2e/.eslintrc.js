19b7db292e24a7c0aa19830897234117dbf814fe
Luca Tumedei <luca.tumedei@automattic.com>
2026-03-20 13:57:37 +0100

    Add eslint-plugin-playwright and replace Jest ESLint rules
    Replace Jest-specific ESLint configuration with Playwright rules
    now that all E2E tests have been migrated to Playwright Test.

--- test/e2e/.eslintrc.js
+++ test/e2e/.eslintrc.js
@@ -2,23 +2,16 @@ const nodeConfig = require( '@automattic/calypso-eslint-overrides/node' );
 
 module.exports = {
 	...nodeConfig,
+	plugins: [ 'playwright' ],
 	env: {
 		...nodeConfig.env,
 	},
+	extends: [ 'plugin:playwright/recommended' ],
 	overrides: [
 		{
 			files: [ 'specs/**/*' ],
 			rules: {
-				// We use jest-runner-groups to run spec suites, and these involve a custom doc header tag.
 				'jsdoc/check-tag-names': [ 'error', { definedTags: [ 'group', 'browser' ] } ],
-				'jest/no-standalone-expect': [ 'error', { additionalTestBlockFunctions: [ 'skipItIf' ] } ],
-			},
-		},
-		{
-			files: [ 'specs/**/shared/**/*', 'lib/shared-steps/**/*' ],
-			rules: {
-				// This directory is used to create shared specs that can be re-used in multiple places.
-				'jest/no-export': 'off',
 			},
 		},
 		{
@@ -31,16 +24,10 @@ module.exports = {
 	rules: {
 		...nodeConfig.rules,
 
-		// We have many tests that don't make an explicit `expect`, but instead puts the browser
-		// in certain state that will be used by the next test, or asserted by WebDriver
-		'jest/expect-expect': 'off',
-
-		// We compose the test titles dynamically
-		'jest/valid-title': 'off',
-
-		// The rule hasn't had the intended results (encouraging owners to re-enable and fix their tests).
-		// See GitHub issue #64870 for context (https://github.com/Automattic/wp-calypso/issues/64870).
-		'jest/no-disabled-tests': 'off',
+		// Many tests put the browser in a certain state without an explicit expect.
+		'playwright/expect-expect': 'off',
+		// We compose test titles dynamically.
+		'playwright/valid-title': 'off',
 
 		'jsdoc/tag-lines': [ 'off' ],
 	},
