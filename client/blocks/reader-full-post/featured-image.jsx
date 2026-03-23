78cd30a579a91f26822d8e04b21719ea77a33a51
Addison Stavlo <Addison.Stavlo@Automattic.com>
2026-03-23 15:39:10 -0400

    change featured image component away from the card version

--- client/blocks/reader-full-post/featured-image.jsx
+++ client/blocks/reader-full-post/featured-image.jsx
@@ -1,18 +1,36 @@
 import PropTypes from 'prop-types';
+import cssSafeUrl from 'calypso/lib/css-safe-url';
+import resizeImageUrl from 'calypso/lib/resize-image-url';
+import { getFeaturedImageAlt } from 'calypso/reader/get-helpers';
 
 const hideImageOnError = ( event ) => {
 	event.target.parentNode.style.display = 'none';
 };
 
-export default function FeaturedImage( { src, alt } ) {
+function featuredImageAltString( post ) {
+	const alt = getFeaturedImageAlt( post );
+	return typeof alt === 'string' ? alt : '';
+}
+
+export default function FeaturedImage( { post, maxWidth } ) {
+	if ( ! post?.featured_image ) {
+		return null;
+	}
+
+	const resizedUrl = resizeImageUrl( post.featured_image, maxWidth );
+	const safeSrc = cssSafeUrl( resizedUrl );
+	if ( ! safeSrc ) {
+		return null;
+	}
+
 	return (
 		<div className="reader-full-post__featured-image">
-			<img src={ src } alt={ alt } onError={ hideImageOnError } />
+			<img src={ safeSrc } alt={ featuredImageAltString( post ) } onError={ hideImageOnError } />
 		</div>
 	);
 }
 
 FeaturedImage.propTypes = {
-	src: PropTypes.string,
-	alt: PropTypes.string,
+	post: PropTypes.object.isRequired,
+	maxWidth: PropTypes.number.isRequired,
 };
--- client/blocks/reader-full-post/test/featured-image.jsx
+++ client/blocks/reader-full-post/test/featured-image.jsx
@@ -7,7 +7,9 @@ import FeaturedImage from '../featured-image';
 describe( 'FeaturedImage', () => {
 	test( 'sets the source to an empty string if the image fails to load', () => {
 		const nonExistentImage = 'http://sketchy-feed.com/missing-image-2.jpg';
-		const { container } = render( <FeaturedImage src={ nonExistentImage } /> );
+		const { container } = render(
+			<FeaturedImage post={ { featured_image: nonExistentImage } } maxWidth={ 600 } />
+		);
 		const div = container.getElementsByClassName( 'reader-full-post__featured-image' )[ 0 ];
 
 		fireEvent.error( div.getElementsByTagName( 'img' )[ 0 ] );
