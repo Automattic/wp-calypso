a78028a65c22c6f87f25e414a44fb2a2bf4e68db
Rob Pugh <rob.pugh@automattic.com>
2026-03-24 08:18:31 -0300

    NL-490 Test release with date naming and issue reference

--- client/reader/controller.js
+++ client/reader/controller.js
@@ -1,4 +1,4 @@
-// trigger workflow test v2
+// trigger workflow test v3
 import page from '@automattic/calypso-router';
 import { getAnyLanguageRouteParam, getLanguageRouteParam } from '@automattic/i18n-utils';
 import i18n from 'i18n-calypso';
