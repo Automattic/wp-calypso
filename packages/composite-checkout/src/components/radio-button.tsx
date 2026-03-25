6e4be314bce421d3ad00a43d66b927ef1773546c
Payton Swick <payton@foolord.com>
2026-03-25 16:24:02 -0400

    Remove unnecessary border-radius in selected billing cycle

--- packages/composite-checkout/src/components/radio-button.tsx
+++ packages/composite-checkout/src/components/radio-button.tsx
@@ -17,6 +17,7 @@ const RadioButtonWrapper = styled.div<
 >`
 	position: relative;
 	display: ${ ( props ) => ( props.hidden ? 'none' : 'block' ) };
+	border-radius: 3px;
 	box-sizing: border-box;
 	width: 100%;
 	outline: ${ getOutline };
