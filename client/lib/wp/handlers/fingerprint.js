a165ccbd41edac861bfcae0e112ea168bf8fbf19
Sérgio Gomes <mail@sgomes.com>
2026-03-27 19:05:19 +0000

    Fingerprint: lazy load and run (#109294)

--- client/lib/wp/handlers/fingerprint.js
+++ client/lib/wp/handlers/fingerprint.js
@@ -1,15 +1,24 @@
-import { load } from '@fingerprintjs/fingerprintjs';
+// Internal module cache.
+// Exported for use in testing.
+/** @type { result?: Promise<string|undefined> } */
+export const cache = {};
 
-let fingerprint;
-export async function loadFingerprint() {
-	const agent = await load( { monitoring: false } );
-	const result = await agent.get();
-	fingerprint = result.visitorId;
-}
-if ( document.readyState !== 'loading' ) {
-	loadFingerprint();
-} else {
-	document.addEventListener( 'DOMContentLoaded', loadFingerprint );
+/**
+ * Returns the fingerprint, loading the library and generating it
+ * if needed.
+ * @returns {Promise<string|undefined>} The fingerprint.
+ */
+async function getFingerprint() {
+	if ( ! cache.result ) {
+		cache.result = ( async () => {
+			const { load } = await import( '@fingerprintjs/fingerprintjs' );
+			const agent = await load( { monitoring: false } );
+			const result = await agent.get();
+			return result.visitorId;
+		} )();
+	}
+
+	return await cache.result;
 }
 
 /**
@@ -19,15 +28,18 @@ if ( document.readyState !== 'loading' ) {
 export function injectFingerprint( wpcom ) {
 	const request = wpcom.request.bind( wpcom );
 
-	wpcom.request = function ( params, callback ) {
-		if ( fingerprint && params?.path === '/me/transactions' ) {
-			params = {
-				...params,
-				headers: {
-					...( params.headers || {} ),
-					'X-Fingerprint': fingerprint,
-				},
-			};
+	wpcom.request = async function ( params, callback ) {
+		if ( params?.path === '/me/transactions' ) {
+			const fingerprint = await getFingerprint();
+			if ( fingerprint ) {
+				params = {
+					...params,
+					headers: {
+						...( params.headers || {} ),
+						'X-Fingerprint': fingerprint,
+					},
+				};
+			}
 		}
 		return request( params, callback );
 	};
