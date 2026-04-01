1f5153cf92e8c013d37397fb669cda10364018bf
Mikael Korpela <mikael.korpela@automattic.com>
2026-04-01 13:06:56 +0300

    Add simplified footer

---
+++ client/components/jetpack-footer/index.tsx
@@ -0,0 +1,40 @@
+import { JetpackLogo } from '@automattic/components';
+import { __ } from '@wordpress/i18n';
+import { Stack } from '@wordpress/ui';
+import AutomatticBylineLogo from '../jetpack/automattic-byline-logo';
+import './style.scss';
+import type { FC } from 'react';
+
+/**
+ * JetpackFooter component displays a tiny Jetpack logo on the left and the Automattic Airline "by line" on the right.
+ */
+const JetpackFooter: FC = () => {
+	return (
+		<Stack
+			render={ <footer /> }
+			className="jetpack-footer"
+			aria-label={ __( 'Jetpack', 'jetpack-components' ) }
+			role="contentinfo"
+			direction="row"
+			justify="flex-start"
+			align="center"
+			wrap="wrap"
+			gap="xl"
+		>
+			<Stack className="jetpack-footer__logo" direction="row" gap="sm" align="center">
+				<JetpackLogo size={ 16 } />
+				<span className="jetpack-footer__logo-text">Jetpack</span>
+			</Stack>
+			<a
+				className="jetpack-footer__a8c"
+				href="https://jetpack.com/redirect/?source=a8c-about"
+				rel="noopener noreferrer"
+				target="_blank"
+			>
+				<AutomatticBylineLogo height={ 8 } />
+			</a>
+		</Stack>
+	);
+};
+
+export default JetpackFooter;
