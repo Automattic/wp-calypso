6199b96909c5422a544d452e2f9370069d0aeb7c
arthur <arthur.chu@automattic.com>
2026-04-02 13:18:39 +0800

    useSuspenseQuery for WpVersionNotice

--- client/dashboard/sites/overview/wp-version-notice.tsx
+++ client/dashboard/sites/overview/wp-version-notice.tsx
@@ -4,7 +4,7 @@ import {
 	siteWordPressVersionQuery,
 	wpOrgCoreVersionQuery,
 } from '@automattic/api-queries';
-import { useSuspenseQuery, useQuery, useMutation } from '@tanstack/react-query';
+import { useSuspenseQuery, useMutation } from '@tanstack/react-query';
 import { __, sprintf } from '@wordpress/i18n';
 import Notice from '../../components/notice';
 import RouterLinkButton from '../../components/router-link-button';
@@ -20,8 +20,8 @@ export function WpVersionNotice( { site }: { site: Site } ) {
 		userPreferenceOptimisticMutation( `${ PREFERENCE_KEY }-${ site.ID }` )
 	);
 
-	const { data: currentVersionTag } = useQuery( siteWordPressVersionQuery( site.ID ) );
-	const { data: betaVersion } = useQuery( wpOrgCoreVersionQuery( 'beta' ) );
+	const { data: currentVersionTag } = useSuspenseQuery( siteWordPressVersionQuery( site.ID ) );
+	const { data: betaVersion } = useSuspenseQuery( wpOrgCoreVersionQuery( 'beta' ) );
 
 	// Don't show if already dismissed, already on beta, or no beta version available.
 	if ( isDismissed || currentVersionTag === 'beta' || ! betaVersion ) {
