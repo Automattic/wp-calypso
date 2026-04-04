2051091f19d13c38417b7869dd80b7af1fa81eb4
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-04-04 14:10:56 +0500

    Add PreloadedImage component

---
+++ client/components/preloaded-image/index.tsx
@@ -0,0 +1,30 @@
+import './style.scss';
+import clsx from 'clsx';
+import { useState } from 'react';
+
+interface PreloadedImageProps {
+	borderRadius?: string;
+	className?: string;
+	src: string;
+	alt?: string;
+	width: number;
+	height: number;
+}
+
+export default function PreloadedImage( props: PreloadedImageProps ) {
+	const { src, alt, className, width, height, borderRadius } = props;
+	const [ isLoaded, setIsLoaded ] = useState( false );
+
+	return (
+		<div className="preloaded-image-wrapper" style={ { borderRadius } }>
+			<img
+				className={ clsx( className, { 'is-loaded': isLoaded } ) }
+				src={ src }
+				alt={ isLoaded ? alt : '' }
+				width={ width }
+				height={ height }
+				onLoad={ () => setIsLoaded( true ) }
+			/>
+		</div>
+	);
+}
