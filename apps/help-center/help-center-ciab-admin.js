4fdb477e3036310a8881ff413d94f8b6fbc22517
Luigi Teschio <gigitux@gmail.com>
2026-04-10 11:49:27 +0200

    Remove CIAB disconnected entrypoints

--- apps/help-center/help-center-ciab-admin.js
+++ apps/help-center/help-center-ciab-admin.js
@@ -1,10 +1,9 @@
-/* global __i18n_text_domain__, helpCenterData */
+/* global helpCenterData */
 import './config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import apiFetch from '@wordpress/api-fetch';
-import { dispatch, select, subscribe } from '@wordpress/data';
+import { dispatch, select } from '@wordpress/data';
 import { useState, useEffect } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
 import { createRoot } from 'react-dom/client';
 import './help-center.scss';
 import wpcomRequest, { canAccessWpcomApis } from 'wpcom-proxy-request';
@@ -99,34 +98,9 @@ async function shouldAutoLoadHelpCenter() {
 	return preferences?.help_center_open;
 }
 
-const unsubscribe = subscribe( () => {
-	// Make sure the wp-logo menu item is registered before unregistering its default items.
-	// Use optional chaining since 'next-admin' store only exists in next-admin context
-	if ( select( 'next-admin' )?.getMetaMenuItems?.( 'wp-logo' )?.length > 1 ) {
-		unsubscribe();
-		// Check if the user has the HC already open from a previous session.
-		shouldAutoLoadHelpCenter().then( ( shouldAutoLoad ) => {
-			if ( shouldAutoLoad ) {
-				loadHelpCenter();
-			}
-		} );
-		// wait for the next tick to ensure the menu items are registered
-		queueMicrotask( () => {
-			select( 'next-admin' )
-				?.getMetaMenuItems?.( 'wp-logo' )
-				?.forEach( ( item ) => {
-					dispatch( 'next-admin' )?.unregisterSiteHubHelpMenuItem?.( item.id );
-				} );
-			dispatch( 'next-admin' )?.registerSiteHubHelpMenuItem?.( 'help-center', {
-				label: __( 'Help Center', __i18n_text_domain__ ),
-				parent: 'wp-logo',
-				callback: () => {
-					return loadHelpCenter().then( () => {
-						const state = select( 'automattic/help-center' ).isHelpCenterShown();
-						dispatch( 'automattic/help-center' ).setShowHelpCenter( ! state );
-					} );
-				},
-			} );
-		} );
+// Check if the user had the Help Center open in a previous session.
+shouldAutoLoadHelpCenter().then( ( shouldAutoLoad ) => {
+	if ( shouldAutoLoad ) {
+		loadHelpCenter();
 	}
 } );
