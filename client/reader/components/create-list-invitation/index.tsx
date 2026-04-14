7310f1eb1504658df469355ac5a1b2a66185b90b
Jeremy Herve <jeremy@jeremy.hu>
2026-04-14 01:42:00 +0100

    Reader Lists: wrap CreateListInvitation content in CardBody for proper padding
    @wordpress/components Card only applies padding through CardBody,
    CardHeader, and CardFooter subcomponents. Wrap the invitation content
    in CardBody and set 24px padding to match the blogging prompt card.

--- client/reader/components/create-list-invitation/index.tsx
+++ client/reader/components/create-list-invitation/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from '@wordpress/components';
+import { Button, Card, CardBody } from '@wordpress/components';
 import { Icon, formatListBullets } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 
@@ -12,22 +12,24 @@ export function CreateListInvitation( { onCreateClick }: CreateListInvitationPro
 	const translate = useTranslate();
 
 	return (
-		<Card className="create-list-invitation" size="large">
-			<div className="create-list-invitation__header">
-				<Icon className="create-list-invitation__icon" icon={ formatListBullets } size={ 20 } />
-				<span className="create-list-invitation__label">{ translate( 'Share a list' ) }</span>
-			</div>
-			<p className="create-list-invitation__prompt">{ translate( 'Create your own list' ) }</p>
-			<p className="create-list-invitation__description">
-				{ translate(
-					'Got a favorite corner of the web? Bundle the blogs you follow into a list of your own and help other readers find their next great read. It’s an easy way to support the creators you enjoy and grow community around shared interests.'
-				) }
-			</p>
-			<div className="create-list-invitation__footer">
-				<Button variant="primary" href="/reader/list/new" onClick={ onCreateClick }>
-					{ translate( 'Create a list' ) }
-				</Button>
-			</div>
+		<Card className="create-list-invitation">
+			<CardBody>
+				<div className="create-list-invitation__header">
+					<Icon className="create-list-invitation__icon" icon={ formatListBullets } size={ 20 } />
+					<span className="create-list-invitation__label">{ translate( 'Share a list' ) }</span>
+				</div>
+				<p className="create-list-invitation__prompt">{ translate( 'Create your own list' ) }</p>
+				<p className="create-list-invitation__description">
+					{ translate(
+						'Got a favorite corner of the web? Bundle the blogs you follow into a list of your own and help other readers find their next great read. It’s an easy way to support the creators you enjoy and grow community around shared interests.'
+					) }
+				</p>
+				<div className="create-list-invitation__footer">
+					<Button variant="primary" href="/reader/list/new" onClick={ onCreateClick }>
+						{ translate( 'Create a list' ) }
+					</Button>
+				</div>
+			</CardBody>
 		</Card>
 	);
 }
