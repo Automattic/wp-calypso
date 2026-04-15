ef93e8c36ac23c17e568ddd16ec05bad9052e02d
Mohammad Jangda <mo@automattic.com>
2026-04-15 12:20:17 -0400

    Update external link for repository creation documentation
    The existing link goes to a 404.

--- client/dashboard/sites/settings-repositories/connect-repository-form.tsx
+++ client/dashboard/sites/settings-repositories/connect-repository-form.tsx
@@ -517,7 +517,7 @@ export const ConnectRepositoryForm = ( {
 				),
 				createRepo: (
 					<ExternalLink
-						href="https://developer.wordpress.com/docs/developer-tools/github-deployments/create-repo-existing-source/"
+						href="https://developer.wordpress.com/docs/get-started/github/"
 						children={ null }
 					/>
 				),
