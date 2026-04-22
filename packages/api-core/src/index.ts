75567fbbc766f3cab068455cd61ce715419f83e2
Jeremy Herve <jeremy@jeremy.hu>
2026-04-22 14:55:51 +0200

    Reader ATmosphere: api-core barrel exports

--- packages/api-core/src/index.ts
+++ packages/api-core/src/index.ts
@@ -77,6 +77,7 @@ export * from './read-feeds';
 export * from './read-lists';
 export * from './read-sites';
 export * from './read-teams';
+export * from './reader-atmosphere';
 export * from './site';
 export * from './site-activity-log';
 export * from './site-activity-log-backup';
