3cfa12b2afe2f2acf7a72e00c22400ef2bb642c4
Addison Stavlo <Addison.Stavlo@Automattic.com>
2026-04-22 11:25:58 -0400

    also copy curated blogs listings

--- client/reader/onboarding-rsm/curated-blogs/index.tsx
+++ client/reader/onboarding-rsm/curated-blogs/index.tsx
@@ -1,2 +1,22 @@
-export { curatedBlogs } from 'calypso/reader/onboarding/curated-blogs';
-export type { CuratedBlog, CuratedBlogsList } from 'calypso/reader/onboarding/curated-blogs';
+import { creativeArtsBlogs } from './creative-arts';
+import { industryBlogs } from './industry';
+import { lifestyleBlogs } from './lifestyle';
+import { societyBlogs } from './society';
+import { technologyBlogs } from './technology';
+
+export const curatedBlogs = {
+	...lifestyleBlogs,
+	...technologyBlogs,
+	...creativeArtsBlogs,
+	...societyBlogs,
+	...industryBlogs,
+};
+
+export type CuratedBlog = {
+	feed_ID: number;
+	site_ID: number;
+	site_URL: string;
+	site_name: string;
+};
+
+export type CuratedBlogsList = Record< string, CuratedBlog[] >;
