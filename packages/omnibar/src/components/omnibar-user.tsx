5e3a4c8cb6ff06f712ccccac534c118818ea769e
Ashar Fuadi <ashar.fuadi@automattic.com>
2026-04-27 17:21:40 +0700

    Omnibar: construct user info node

--- packages/omnibar/src/components/omnibar-user.tsx
+++ packages/omnibar/src/components/omnibar-user.tsx
@@ -1,4 +1,7 @@
-import { __experimentalHStack as HStack } from '@wordpress/components';
+import {
+	__experimentalHStack as HStack,
+	__experimentalVStack as VStack,
+} from '@wordpress/components';
 import { OmnibarMenu } from './omnibar-menu';
 import type { OmnibarNode } from '../types';
 
@@ -15,6 +18,27 @@ export function OmnibarUserNode( { node }: { node: OmnibarNode } ) {
 						{ icon && <span className="omnibar__user-avatar">{ icon }</span> }
 					</HStack>
 				),
+				children: node.children?.map( ( child ) => ( {
+					...child,
+					children: child.children?.map( ( grandChild ) => {
+						if ( grandChild.id === 'user-info' ) {
+							return {
+								...grandChild,
+								render: ( { title, icon, meta } ) => (
+									<HStack spacing={ 3 } expanded={ false }>
+										{ icon && <span className="omnibar__user-avatar">{ icon }</span> }
+										<VStack>
+											<span>{ meta?.displayName }</span>
+											<span>@{ meta?.username }</span>
+											<span>{ title }</span>
+										</VStack>
+									</HStack>
+								),
+							};
+						}
+						return grandChild;
+					} ),
+				} ) ),
 			} }
 		/>
 	);
