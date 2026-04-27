cfc8d01fb8040483d51cc15154fd34c07f288bd7
Jeremy Herve <jeremy@jeremy.hu>
2026-04-27 18:42:28 +0200

    Reader ATmosphere: barrel-export SocialPostCard + SocialFeedList

--- client/reader/social/index.ts
+++ client/reader/social/index.ts
@@ -2,3 +2,6 @@ import './style.scss';
 
 export { SocialProfileCard } from './profile-card';
 export type { SocialProfileCardProps, SocialProfileStat } from './profile-card';
+
+export { SocialPostCard } from './components/post-card';
+export { SocialFeedList } from './components/feed-list';
