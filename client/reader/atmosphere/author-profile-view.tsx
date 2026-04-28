14ad3c515ed600953700af080232e74580893c5d
Jeremy Herve <jeremy@jeremy.hu>
2026-04-28 23:34:13 +0200

    Reader ATmosphere: AuthorProfileView mirrors slice-5 isError + title format

--- client/reader/atmosphere/author-profile-view.tsx
+++ client/reader/atmosphere/author-profile-view.tsx
@@ -1,5 +1,6 @@
 import { useConnectionsQuery } from '@automattic/api-queries';
 import page from '@automattic/calypso-router';
+import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
@@ -13,19 +14,33 @@ interface Props {
 
 export function AuthorProfileView( { connectionId, actor }: Props ) {
 	const translate = useTranslate();
-	const { data, isPending } = useConnectionsQuery();
+	const { data, isPending, isError, refetch } = useConnectionsQuery();
 
 	const connections = data?.connections ?? [];
 	const connection = connections.find( ( c ) => c.id === connectionId ) ?? null;
 
 	useEffect( () => {
-		if ( isPending ) {
+		if ( isPending || isError ) {
 			return;
 		}
 		if ( ! connection ) {
 			page.replace( '/reader/atmosphere' );
 		}
-	}, [ isPending, connection ] );
+	}, [ isPending, isError, connection, connectionId ] );
+
+	if ( isError ) {
+		return (
+			<ReaderMain className="atmosphere-view">
+				<DocumentHead title={ translate( 'Profile ‹ ATmosphere ‹ Reader' ) } />
+				<div role="alert" className="atmosphere-error">
+					<p>{ translate( "We couldn't load your Bluesky connections." ) }</p>
+					<Button variant="secondary" onClick={ () => refetch() }>
+						{ translate( 'Try again' ) }
+					</Button>
+				</div>
+			</ReaderMain>
+		);
+	}
 
 	if ( ! connection ) {
 		return (
@@ -38,10 +53,9 @@ export function AuthorProfileView( { connectionId, actor }: Props ) {
 		);
 	}
 
-	const docTitle = String( translate( '%(actor)s ‹ ATmosphere ‹ Reader', { args: { actor } } ) );
 	return (
 		<ReaderMain className="atmosphere-view">
-			<DocumentHead title={ docTitle } />
+			<DocumentHead title={ translate( '%s ‹ ATmosphere ‹ Reader', { args: actor } ) } />
 			<AuthorProfilePanel connection={ connection } actor={ actor } />
 		</ReaderMain>
 	);
