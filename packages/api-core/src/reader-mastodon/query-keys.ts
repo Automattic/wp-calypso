5fe7ce3ccb7adab503ae54e5522b009ed437b5b8
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-04-28 11:16:49 +0200

    Reader Mastodon: add timeline query key

--- packages/api-core/src/reader-mastodon/query-keys.ts
+++ packages/api-core/src/reader-mastodon/query-keys.ts
@@ -2,4 +2,6 @@ export const readerMastodonKeys = {
 	all: [ 'reader', 'mastodon' ] as const,
 	connections: () => [ ...readerMastodonKeys.all, 'connections' ] as const,
 	connection: ( id: number | null ) => [ ...readerMastodonKeys.all, 'connection', id ] as const,
+	timeline: ( connectionId: number ) =>
+		[ ...readerMastodonKeys.all, 'timeline', connectionId ] as const,
 };
