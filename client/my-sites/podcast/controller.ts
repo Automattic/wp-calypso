fa8caa1ee200882fc952cb7a3d458d16a9f89ef7
Tony Arcangelini <tony@arcangelini.com>
2026-04-29 23:36:47 +0300

    Podcast: rename route from /podcast to /podcasting

--- client/my-sites/podcast/controller.ts
+++ client/my-sites/podcast/controller.ts
@@ -3,7 +3,7 @@ import PodcastMain from './main';
 import type { Callback } from '@automattic/calypso-router';
 
 export const createPodcast: Callback = ( context, next ) => {
-	context.section.name = 'podcast';
+	context.section.name = 'podcasting';
 	context.primary = createElement( PodcastMain, {
 		section: context.params.section,
 	} );
