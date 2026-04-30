1f624000e79d88f6b33d7de2244ec94da65d7bde
Matthew Reishus <mreishus@users.noreply.github.com>
2026-04-30 01:22:18 +0000

    Harden mini-css runtime cache workaround

--- packages/calypso-build/webpack/mini-css-runtime-full-hash.js
+++ packages/calypso-build/webpack/mini-css-runtime-full-hash.js
@@ -1,14 +1,55 @@
+const { MODULE_TYPE: MINI_CSS_CONTENT_TYPE } = require( 'mini-css-extract-plugin/dist/utils' );
 const webpack = require( 'webpack' );
 
 const PLUGIN_NAME = 'MiniCSSRuntimeFullHash';
 const MINI_CSS_FILENAME_RUNTIME_MODULE_NAME = 'get mini-css chunk filename';
 const MINI_CSS_FILENAME_RUNTIME_GLOBAL = `${ webpack.RuntimeGlobals.require }.miniCssF`;
+const RUNTIME_MODULE_GENERATED_CODE_CACHE_FIELD = '_cachedGeneratedCode';
 
 function isMiniCssFilenameRuntimeModule( module ) {
 	return (
+		module &&
 		module.name === MINI_CSS_FILENAME_RUNTIME_MODULE_NAME &&
-		module.global === MINI_CSS_FILENAME_RUNTIME_GLOBAL
+		module.global === MINI_CSS_FILENAME_RUNTIME_GLOBAL &&
+		module.contentType === MINI_CSS_CONTENT_TYPE
 	);
+}
+
+function clearRuntimeModuleGeneratedCodeCache( module ) {
+	if (
+		! Object.prototype.hasOwnProperty.call( module, RUNTIME_MODULE_GENERATED_CODE_CACHE_FIELD )
+	) {
+		throw new Error(
+			`${ PLUGIN_NAME }: expected webpack RuntimeModule to expose ${ RUNTIME_MODULE_GENERATED_CODE_CACHE_FIELD }. ` +
+				'Webpack internals may have changed; refusing to silently emit possibly stale miniCssF runtime code.'
+		);
+	}
+
+	module[ RUNTIME_MODULE_GENERATED_CODE_CACHE_FIELD ] = undefined;
+}
+
+function iterableHasItems( iterable ) {
+	return !! iterable && ! iterable[ Symbol.iterator ]().next().done;
+}
+
+function hasAsyncMiniCssChunks( compilation ) {
+	const { chunkGraph } = compilation;
+
+	for ( const chunk of compilation.chunks ) {
+		if ( chunk.canBeInitial() ) {
+			continue;
+		}
+
+		if (
+			iterableHasItems(
+				chunkGraph.getChunkModulesIterableBySourceType( chunk, MINI_CSS_CONTENT_TYPE )
+			)
+		) {
+			return true;
+		}
+	}
+
+	return false;
 }
 
 module.exports = class MiniCSSRuntimeFullHashPlugin {
@@ -31,8 +72,21 @@ module.exports = class MiniCSSRuntimeFullHashPlugin {
 				for ( const module of miniCssFilenameRuntimeModules ) {
 					// RuntimeModule caches generated code internally. Clear any early code
 					// generated before the full-hash pass finalized referenced CSS hashes.
-					module._cachedGeneratedCode = undefined;
+					clearRuntimeModuleGeneratedCodeCache( module );
 				}
+			} );
+
+			compilation.hooks.afterSeal.tap( PLUGIN_NAME, () => {
+				if ( miniCssFilenameRuntimeModules.size > 0 || ! hasAsyncMiniCssChunks( compilation ) ) {
+					return;
+				}
+
+				throw new Error(
+					`${ PLUGIN_NAME }: expected to find webpack's mini-css filename runtime module, ` +
+						`but none matched ${ MINI_CSS_FILENAME_RUNTIME_MODULE_NAME } / ` +
+						`${ MINI_CSS_FILENAME_RUNTIME_GLOBAL } / ${ MINI_CSS_CONTENT_TYPE }. ` +
+						'Webpack or mini-css-extract-plugin internals may have changed.'
+				);
 			} );
 		} );
 	}
