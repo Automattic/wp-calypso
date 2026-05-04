f1f178a4f9b32c729682f34f32f52fd31f12c7a5
Jeremy Herve <jeremy@jeremy.hu>
2026-05-04 12:55:32 +0200

    ATmosphere composer: re-export ComposerEntryPoint from barrel

--- client/reader/atmosphere/composer/index.ts
+++ client/reader/atmosphere/composer/index.ts
@@ -1,3 +1,8 @@
 export { ComposerProvider, useComposer, useOptionalComposer } from './composer-provider';
 export { ComposerModal } from './composer-modal';
-export type { ComposerMode, ActiveMode, PreviewPost } from './composer-provider';
+export type {
+	ComposerMode,
+	ActiveMode,
+	PreviewPost,
+	ComposerEntryPoint,
+} from './composer-provider';
