cc4e05a9d109bffefeeff8a07e232039c73a4a7f
Philip Jackson <p-jackson@live.com>
2026-05-05 14:51:53 +1200

    Remove ineffective add new site dynamic import

--- client/components/sites-add-new-site/async.tsx
+++ client/components/sites-add-new-site/async.tsx
@@ -1,12 +1,6 @@
-import { Spinner } from '@wordpress/components';
-import AsyncLoad from 'calypso/components/async-load';
+import AddNewSite from '../../dashboard/sites/add-new-site';
 import type { AddNewSiteProps } from '../../dashboard/sites/add-new-site/types';
 
-const loadAddNewSite = () =>
-	import(
-		/* webpackChunkName: "async-load-calypso-dashboard-sites-add-new-site" */ 'calypso/dashboard/sites/add-new-site'
-	);
-
 export const AsyncContent = ( props: AddNewSiteProps ) => {
-	return <AsyncLoad require={ loadAddNewSite } placeholder={ <Spinner /> } { ...props } />;
+	return <AddNewSite { ...props } />;
 };
