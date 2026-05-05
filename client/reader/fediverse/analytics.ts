66b803bf5d16ba515a8d6604fc5d259cb5c8f1ba
Jeremy Herve <jeremy@jeremy.hu>
2026-05-05 17:52:40 +0200

    Reader Fediverse: prefix Tracks events with calypso_ to match Reader convention

--- client/reader/fediverse/analytics.ts
+++ client/reader/fediverse/analytics.ts
@@ -1,17 +1,17 @@
 import { recordReaderTracksEvent } from 'calypso/state/reader/analytics/actions';
 
 export const FEDIVERSE_EVENTS = {
-	CONNECT_STARTED: 'reader_fediverse_connect_started',
-	CAPABILITY_CHECK: 'reader_fediverse_capability_check',
-	FEATURE_ENABLED: 'reader_fediverse_feature_enabled',
-	C2S_ENABLED: 'reader_fediverse_c2s_enabled',
-	USER_ACTORS_ENABLED: 'reader_fediverse_user_actors_enabled',
-	AUTHORIZE_STARTED: 'reader_fediverse_authorize_started',
-	CONNECT_COMPLETED: 'reader_fediverse_connect_completed',
-	CONNECT_FAILED: 'reader_fediverse_connect_failed',
-	NOTE_POSTED: 'reader_fediverse_note_posted',
-	NOTE_FAILED: 'reader_fediverse_note_failed',
-	DISCONNECTED: 'reader_fediverse_disconnected',
+	CONNECT_STARTED: 'calypso_reader_fediverse_connect_started',
+	CAPABILITY_CHECK: 'calypso_reader_fediverse_capability_check',
+	FEATURE_ENABLED: 'calypso_reader_fediverse_feature_enabled',
+	C2S_ENABLED: 'calypso_reader_fediverse_c2s_enabled',
+	USER_ACTORS_ENABLED: 'calypso_reader_fediverse_user_actors_enabled',
+	AUTHORIZE_STARTED: 'calypso_reader_fediverse_authorize_started',
+	CONNECT_COMPLETED: 'calypso_reader_fediverse_connect_completed',
+	CONNECT_FAILED: 'calypso_reader_fediverse_connect_failed',
+	NOTE_POSTED: 'calypso_reader_fediverse_note_posted',
+	NOTE_FAILED: 'calypso_reader_fediverse_note_failed',
+	DISCONNECTED: 'calypso_reader_fediverse_disconnected',
 } as const;
 
 export function trackFediverseEvent(
