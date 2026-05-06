e3cc2db1e5a7b381374fb4da0c34c945407ae7a3
Jeremy Herve <jeremy@jeremy.hu>
2026-05-06 18:06:18 +0200

    Reader Mastodon: add SensitiveToggle component (CM-676)

---
+++ client/reader/mastodon/composer-media/sensitive-toggle.tsx
@@ -0,0 +1,20 @@
+import { ToggleControl } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+
+interface Props {
+	sensitive: boolean;
+	onChange: ( v: boolean ) => void;
+}
+
+export function SensitiveToggle( { sensitive, onChange }: Props ) {
+	const translate = useTranslate();
+	return (
+		<ToggleControl
+			label={ translate( 'Mark media as sensitive' ) as string }
+			help={ translate( 'Hides the images behind a click-through warning.' ) as string }
+			checked={ sensitive }
+			onChange={ onChange }
+			__nextHasNoMarginBottom
+		/>
+	);
+}
