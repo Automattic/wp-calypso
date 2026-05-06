49a4f297cfe252d1515d10bc61008cb5c1338ef5
Aaron Yan <aaron.yan@automattic.com>
2026-05-06 15:09:18 -0400

    feat: flip customer-home page title and document head with flag

--- client/my-sites/customer-home/main.tsx
+++ client/my-sites/customer-home/main.tsx
@@ -2,16 +2,20 @@ import { useTranslate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { useFeatureValue } from 'calypso/lib/explat';
 import HomeContent from './components/home-content';
 import type { SiteDetails } from '@automattic/data-stores';
 
 export default function CustomerHome( { site }: { site: SiteDetails } ) {
 	const translate = useTranslate();
+	const homeVariant = useFeatureValue( 'wpcom_explat_v2_demo_v1', 'control' );
+	const homeTitle =
+		homeVariant === 'treatment' ? translate( 'My Dashboard' ) : translate( 'My Home' );
 
 	return (
 		<Main wideLayout>
-			<PageViewTracker path="/home/:site" title={ translate( 'My Home' ) } />
-			<DocumentHead title={ translate( 'My Home' ) } />
+			<PageViewTracker path="/home/:site" title={ homeTitle } />
+			<DocumentHead title={ homeTitle } />
 			{ site.options && <HomeContent /> }
 		</Main>
 	);
