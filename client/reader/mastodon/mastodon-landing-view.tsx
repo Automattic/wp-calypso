85a19089c62da49fac2c7b866de0a02d1310fb45
Jeremy Herve <jeremy@jeremy.hu>
2026-05-07 16:04:19 +0200

    Revert "Reader Mastodon: tag connections that need reauth in the list"
    This reverts commit a017bb27a1ff4abb85965fd3f43f61be3905490d.

--- client/reader/mastodon/mastodon-landing-view.tsx
+++ client/reader/mastodon/mastodon-landing-view.tsx
@@ -1,16 +1,10 @@
-import { useMastodonAuthStatusQuery, useMastodonConnectionsQuery } from '@automattic/api-queries';
+import { useMastodonConnectionsQuery } from '@automattic/api-queries';
 import page from '@automattic/calypso-router';
 import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
 import ReaderMain from 'calypso/reader/components/reader-main';
-import { ConnectionReauthTag } from 'calypso/reader/social';
-
-function useMastodonAuthStatusForTag( connectionId: number ) {
-	const r = useMastodonAuthStatusQuery( connectionId );
-	return { needsReauth: r.data?.needs_reauth, isLoading: r.isPending };
-}
 
 export function MastodonLandingView() {
 	const translate = useTranslate();
@@ -28,44 +22,21 @@ export function MastodonLandingView() {
 		}
 	}, [ isPending, data, isError ] );
 
-	function renderBody() {
-		if ( isError ) {
-			return (
+	return (
+		<ReaderMain className="mastodon-view">
+			<DocumentHead title={ translate( 'Mastodon ‹ Reader' ) } />
+			{ isError ? (
 				<div role="alert" className="mastodon-error">
 					<p>{ translate( "We couldn't load your Mastodon connections." ) }</p>
 					<Button variant="secondary" onClick={ () => refetch() }>
 						{ translate( 'Try again' ) }
 					</Button>
 				</div>
-			);
-		}
-		if ( ! data ) {
-			return (
+			) : (
 				<div role="status" aria-live="polite">
 					{ translate( 'Loading…' ) }
 				</div>
-			);
-		}
-		return (
-			<ul className="mastodon-landing-connections">
-				{ data.connections.map( ( connection ) => (
-					<li key={ connection.id } className="mastodon-landing-connections__item">
-						<span className="mastodon-landing-connections__handle">{ connection.handle }</span>
-						<ConnectionReauthTag
-							connectionId={ connection.id }
-							useAuthStatus={ useMastodonAuthStatusForTag }
-							label={ translate( 'Needs reconnect' ) as string }
-						/>
-					</li>
-				) ) }
-			</ul>
-		);
-	}
-
-	return (
-		<ReaderMain className="mastodon-view">
-			<DocumentHead title={ translate( 'Mastodon ‹ Reader' ) } />
-			{ renderBody() }
+			) }
 		</ReaderMain>
 	);
 }
