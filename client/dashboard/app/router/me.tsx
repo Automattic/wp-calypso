3467d0d95c9320db5762cc439297f22d4faf2609
Michael Cain <michael@michaeldcain.com>
2026-05-08 13:27:11 -0400

    Add removedId to dashboard purchases-index search schema

--- client/dashboard/app/router/me.tsx
+++ client/dashboard/app/router/me.tsx
@@ -242,6 +242,7 @@ export const purchasesIndexRoute = createRoute( {
 		site?: number;
 		removed?: string;
 		removedDomain?: string;
+		removedId?: number;
 	} => {
 		return {
 			page: typeof search.page === 'number' ? search.page : undefined,
@@ -249,6 +250,7 @@ export const purchasesIndexRoute = createRoute( {
 			site: typeof search.site === 'number' ? search.site : undefined,
 			removed: typeof search.removed === 'string' ? search.removed : undefined,
 			removedDomain: typeof search.removedDomain === 'string' ? search.removedDomain : undefined,
+			removedId: typeof search.removedId === 'number' ? search.removedId : undefined,
 		};
 	},
 } ).lazy( () =>
