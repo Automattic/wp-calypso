28a5adaf0b3dae4047ba9252d3c0fbf58711dc7b
Hardik <hardiklohan7@gmail.com>
2026-05-20 01:13:57 +0530

    Add loading and error states to subscribers overview

--- client/my-sites/stats/stats-subscribers-overview/index.tsx
+++ client/my-sites/stats/stats-subscribers-overview/index.tsx
@@ -1,6 +1,8 @@
+import { Spinner } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import React from 'react';
 import CountCard from 'calypso/my-sites/stats/components/highlight-cards/count-card';
+import StatsError from 'calypso/my-sites/stats/stats-error';
 import useSubscribersOverview from 'calypso/my-sites/stats/hooks/use-subscribers-overview';
 
 interface SubscribersOverviewProps {
@@ -10,13 +12,14 @@ interface SubscribersOverviewProps {
 const SubscribersOverview: React.FC< SubscribersOverviewProps > = ( { siteId } ) => {
 	const { isLoading, isError, overviewData } = useSubscribersOverview( siteId );
 	const translate = useTranslate();
+	const showSpinner = isLoading && overviewData.length === 0;
 
 	return (
 		<div className="subscribers-overview highlight-cards">
 			<div className="highlight-cards-list">
+				{ showSpinner && <Spinner /> }
 				{ overviewData.map( ( { count, heading, note }, index ) => {
 					return (
-						// TODO: Communicate loading vs error state to the user.
 						<CountCard
 							key={ index }
 							heading={ heading }
@@ -28,6 +31,7 @@ const SubscribersOverview: React.FC< SubscribersOverviewProps > = ( { siteId } )
 					);
 				} ) }
 			</div>
+			{ isError && ! isLoading && <StatsError /> }
 		</div>
 	);
 };
