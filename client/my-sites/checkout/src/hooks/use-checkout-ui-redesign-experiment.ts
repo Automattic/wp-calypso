b0ecfccb409bd2b761ef48eaa095981e121ff3b7
MILLER/F <millerf@automattic.com>
2026-05-27 16:14:14 +0200

    Delete use-checkout-ui-redesign-experiment.ts

--- client/my-sites/checkout/src/hooks/use-checkout-ui-redesign-experiment.ts
+++
@@ -1,5 +0,0 @@
-// The checkout UI redesign experiment (wpcom_mobile_checkout_redesign_202603_v1) won,
-// so this hook now always returns the treatment (redesigned) UI.
-export function useCheckoutUiRedesignExperiment(): [ boolean, boolean ] {
-	return [ false, true ];
-}
