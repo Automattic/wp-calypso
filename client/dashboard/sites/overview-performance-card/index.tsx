59aa014dc79ce986d01633ed52748a79663dfe4e
Philip Jackson <p-jackson@live.com>
2026-06-09 14:54:48 +1200

    dashboard: fix incorrect translator comment on perf recommendations count

--- client/dashboard/sites/overview-performance-card/index.tsx
+++ client/dashboard/sites/overview-performance-card/index.tsx
@@ -61,7 +61,7 @@ function PerformanceCardContentWithFinishedTests( {
 	} else {
 		const recommendationCount = Object.keys( report.audits ).length;
 		description = sprintf(
-			// translators: %d is the number of days until the link expires.
+			// translators: %d is the number of performance recommendations available.
 			_n( '%d recommendation available.', '%d recommendations available.', recommendationCount ),
 			recommendationCount
 		);
