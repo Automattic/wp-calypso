7f5db797442cd654dd534b86b083887b4cae0ab3
Chris Rosser <chris.rosser@automattic.com>
2026-06-10 15:17:18 +1200

    Legacy Contact: use function component with useTranslate

--- client/me/legacy-contact/main.jsx
+++ client/me/legacy-contact/main.jsx
@@ -1,6 +1,5 @@
 import { Card } from '@automattic/components';
-import { localize } from 'i18n-calypso';
-import { Component } from 'react';
+import { useTranslate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
 import NavigationHeader from 'calypso/components/navigation-header';
@@ -8,26 +7,22 @@ import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 
 import './style.scss';
 
-class LegacyContact extends Component {
-	render() {
-		const { translate } = this.props;
+export default function LegacyContact() {
+	const translate = useTranslate();
 
-		return (
-			<Main wideLayout className="legacy-contact">
-				<PageViewTracker path="/me/legacy-contact" title="Me > Legacy Contact" />
-				<DocumentHead title={ translate( 'Legacy Contact' ) } />
-				<NavigationHeader navigationItems={ [] } title={ translate( 'Legacy Contact' ) } />
+	return (
+		<Main wideLayout className="legacy-contact">
+			<PageViewTracker path="/me/legacy-contact" title="Me > Legacy Contact" />
+			<DocumentHead title={ translate( 'Legacy Contact' ) } />
+			<NavigationHeader navigationItems={ [] } title={ translate( 'Legacy Contact' ) } />
 
-				<Card className="legacy-contact__intro">
-					<p>
-						{ translate(
-							'A legacy contact is someone you trust to have access to your account after your death.'
-						) }
-					</p>
-				</Card>
-			</Main>
-		);
-	}
+			<Card className="legacy-contact__intro">
+				<p>
+					{ translate(
+						'A legacy contact is someone you trust to have access to your account after your death.'
+					) }
+				</p>
+			</Card>
+		</Main>
+	);
 }
-
-export default localize( LegacyContact );
