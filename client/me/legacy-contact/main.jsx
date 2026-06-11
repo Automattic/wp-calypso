906ace25a5672cc5585e2ddd417535ece4b53fa1
Chris Rosser <chris.rosser@automattic.com>
2026-06-11 16:10:44 +1200

    Legacy Contact: read contact from wpcom/v2/me/legacy-contacts

--- client/me/legacy-contact/main.jsx
+++ client/me/legacy-contact/main.jsx
@@ -1,4 +1,6 @@
+import { legacyContactsQuery } from '@automattic/api-queries';
 import { Card } from '@automattic/components';
+import { useQuery } from '@tanstack/react-query';
 import { useTranslate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
@@ -9,6 +11,9 @@ import './style.scss';
 
 export default function LegacyContact() {
 	const translate = useTranslate();
+
+	const { data: contacts = [], isLoading } = useQuery( legacyContactsQuery() );
+	const contact = contacts[ 0 ];
 
 	return (
 		<Main wideLayout className="legacy-contact">
@@ -22,6 +27,15 @@ export default function LegacyContact() {
 						'A legacy contact is someone you trust to have access to your account after your death.'
 					) }
 				</p>
+
+				{ ! isLoading && contact && (
+					<p className="legacy-contact__current">
+						{ translate( 'Your legacy contact is {{strong}}%(email)s{{/strong}}.', {
+							args: { email: contact.email },
+							components: { strong: <strong /> },
+						} ) }
+					</p>
+				) }
 			</Card>
 		</Main>
 	);
