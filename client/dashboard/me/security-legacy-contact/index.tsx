73102f0f8c59787a4954b6628e5ddd5e53a214ee
Chris Rosser <chris.rosser@automattic.com>
2026-06-12 15:03:05 +1200

    Legacy contact: read contact from wpcom/v2/me/legacy-contacts in dashboard

--- client/dashboard/me/security-legacy-contact/index.tsx
+++ client/dashboard/me/security-legacy-contact/index.tsx
@@ -1,9 +1,18 @@
-import { __ } from '@wordpress/i18n';
+import { legacyContactsQuery } from '@automattic/api-queries';
+import { useQuery } from '@tanstack/react-query';
+import { __experimentalVStack as VStack, Button } from '@wordpress/components';
+import { createInterpolateElement } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
 import Breadcrumbs from '../../app/breadcrumbs';
+import { ButtonStack } from '../../components/button-stack';
+import { Card, CardBody } from '../../components/card';
 import { PageHeader } from '../../components/page-header';
 import PageLayout from '../../components/page-layout';
+import { Text } from '../../components/text';
 
 export default function SecurityLegacyContact() {
+	const { data: [ contact ] = [], isLoading } = useQuery( legacyContactsQuery() );
+
 	return (
 		<PageLayout
 			size="small"
@@ -17,7 +26,37 @@ export default function SecurityLegacyContact() {
 				/>
 			}
 		>
-			{ /* Content goes here */ }
+			{ ! isLoading && (
+				<Card>
+					<CardBody>
+						<VStack spacing={ 4 }>
+							{ contact ? (
+								<Text>
+									{ createInterpolateElement(
+										sprintf(
+											/* translators: %s is the email address of the legacy contact. */
+											__( 'Your legacy contact is <strong>%s</strong>.' ),
+											contact.email
+										),
+										{ strong: <strong /> }
+									) }
+								</Text>
+							) : (
+								<ButtonStack justify="flex-start">
+									<Button
+										variant="primary"
+										onClick={ () => {
+											// TODO: open the legacy contact setup flow.
+										} }
+									>
+										{ __( 'Set up legacy contact' ) }
+									</Button>
+								</ButtonStack>
+							) }
+						</VStack>
+					</CardBody>
+				</Card>
+			) }
 		</PageLayout>
 	);
 }
