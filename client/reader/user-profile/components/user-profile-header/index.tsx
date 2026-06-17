0d7ebe44fab7f38d3e5af6b9cfefab5ebe5ae9bb
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-06-17 16:24:28 +0500

    💫 UPDATE: remove years of service badge from Reader user profile

--- client/reader/user-profile/components/user-profile-header/index.tsx
+++ client/reader/user-profile/components/user-profile-header/index.tsx
@@ -11,7 +11,6 @@ import SectionNav from 'calypso/components/section-nav';
 import NavItem from 'calypso/components/section-nav/item';
 import NavTabs from 'calypso/components/section-nav/tabs';
 import { decodeEntities } from 'calypso/lib/formatting';
-import { AuthorAchievementBadges } from 'calypso/reader/components/achievements/author-achievement-badges';
 import useAchievementsVisibility from 'calypso/reader/components/achievements/use-achievements-visibility';
 import { useProfileTabVisibility } from 'calypso/reader/data/user-profile';
 import { getUserProfileUrl } from 'calypso/reader/user-profile/user-profile.utils';
@@ -118,7 +117,6 @@ const UserProfileHeader = ( { user, view }: UserProfileHeaderProps ): JSX.Elemen
 									/>
 								</a>
 							) }
-							<AuthorAchievementBadges authorLogin={ user.user_login } size="medium" />
 						</h1>
 						<p>
 							<span dir="ltr">@{ user.user_login }</span>
