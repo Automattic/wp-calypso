733ee1a9ddc3b5b2d9c015b18e507b913d20baa6
Tiago Noronha <hello@tiagonoronha.com>
2026-06-17 21:01:47 +0100

    feat(amplify): add Amplify item to the agency sidebar

--- client/dashboard/app/responsive-sidebar/agency.tsx
+++ client/dashboard/app/responsive-sidebar/agency.tsx
@@ -17,9 +17,18 @@ export default function AgencySidebar() {
 			<SidebarMenuItem icon={ home } to="/overview">
 				{ __( 'Home' ) }
 			</SidebarMenuItem>
-			{ supports.agency && supports.agency.tiers && (
-				<SidebarExpandableMenuItem label={ __( 'Agency' ) } icon={ globe } to="/agency/tiers">
-					<SidebarMenuItem to="/agency/tiers">{ __( 'Tiers' ) }</SidebarMenuItem>
+			{ supports.agency && ( supports.agency.tiers || supports.agency.amplify ) && (
+				<SidebarExpandableMenuItem
+					label={ __( 'Agency' ) }
+					icon={ globe }
+					to={ supports.agency.tiers ? '/agency/tiers' : '/agency/amplify' }
+				>
+					{ supports.agency.tiers && (
+						<SidebarMenuItem to="/agency/tiers">{ __( 'Tiers' ) }</SidebarMenuItem>
+					) }
+					{ supports.agency.amplify && (
+						<SidebarMenuItem to="/agency/amplify">{ __( 'Amplify' ) }</SidebarMenuItem>
+					) }
 				</SidebarExpandableMenuItem>
 			) }
 		</>
