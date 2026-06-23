8a6e4aecd587d70ffec26a966f24adf9a624fc2d
bluefuton <chris@bluefuton.com>
2026-06-23 20:04:43 +1200

    Legacy contact: fetch access key from single-contact endpoint

--- packages/api-core/src/me-legacy-contacts/fetchers.ts
+++ packages/api-core/src/me-legacy-contacts/fetchers.ts
@@ -1,9 +1,16 @@
 import { wpcom } from '../wpcom-fetcher';
-import type { LegacyContact } from './types';
+import type { LegacyContact, LegacyContactWithAccessKey } from './types';
 
 export async function fetchLegacyContacts(): Promise< LegacyContact[] > {
 	return wpcom.req.get( {
 		path: '/me/legacy-contacts',
+		apiNamespace: 'wpcom/v2',
+	} );
+}
+
+export async function fetchLegacyContact( id: number ): Promise< LegacyContactWithAccessKey > {
+	return wpcom.req.get( {
+		path: `/me/legacy-contacts/${ id }`,
 		apiNamespace: 'wpcom/v2',
 	} );
 }
