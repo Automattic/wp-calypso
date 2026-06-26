d9d8b4787c2f063279e5154d89fdcdf0b17a71fd
Addison Stavlo <Addison.Stavlo@Automattic.com>
2026-06-26 11:59:46 -0400

    fix dedupe alignment, consolidate test assertion format

--- client/reader/site-subscriptions-manager/test/not-found-site-subscriptions.test.tsx
+++ client/reader/site-subscriptions-manager/test/not-found-site-subscriptions.test.tsx
@@ -8,26 +8,58 @@ import { render, screen } from '@testing-library/react';
 import nock from 'nock';
 import NotFoundSiteSubscriptions from '../not-found-site-subscriptions';
 
+jest.mock( '@automattic/data-stores', () => {
+	const actual = jest.requireActual( '@automattic/data-stores' );
+	return {
+		...actual,
+		SubscriptionManager: {
+			...actual.SubscriptionManager,
+			useSiteSubscriptionsQuery: jest.fn(),
+		},
+	};
+} );
+
+const mockUseSiteSubscriptionsQuery = SubscriptionManager.useSiteSubscriptionsQuery as jest.Mock;
+
 const Wrapper =
-	( { searchTerm }: { searchTerm: string } ) =>
-	( { children }: { children: React.ReactNode } ) => (
-		<SubscriptionManager.SiteSubscriptionsQueryPropsProvider initialSearchTermState={ searchTerm }>
-			<QueryClientProvider
-				client={
-					new QueryClient( {
-						defaultOptions: {
-							queries: { retry: false },
-						},
-					} )
-				}
+	( {
+		searchTerm,
+		subscriptions = [],
+	}: {
+		searchTerm: string;
+		subscriptions?: Array< {
+			feed_ID: string;
+			URL: string;
+			isDeleted?: boolean;
+		} >;
+	} ) =>
+	( { children }: { children: React.ReactNode } ) => {
+		mockUseSiteSubscriptionsQuery.mockReturnValue( {
+			data: { subscriptions },
+		} );
+
+		return (
+			<SubscriptionManager.SiteSubscriptionsQueryPropsProvider
+				initialSearchTermState={ searchTerm }
 			>
-				{ children }
-			</QueryClientProvider>
-		</SubscriptionManager.SiteSubscriptionsQueryPropsProvider>
-	);
+				<QueryClientProvider
+					client={
+						new QueryClient( {
+							defaultOptions: {
+								queries: { retry: false },
+							},
+						} )
+					}
+				>
+					{ children }
+				</QueryClientProvider>
+			</SubscriptionManager.SiteSubscriptionsQueryPropsProvider>
+		);
+	};
 
 describe( 'NotFoundSiteSubscriptions', () => {
 	beforeEach( () => {
+		jest.clearAllMocks();
 		nock.disableNetConnect();
 	} );
 
@@ -39,7 +71,9 @@ describe( 'NotFoundSiteSubscriptions', () => {
 				feeds: [ { feed_ID: 1 }, { feed_ID: 2 } ],
 			} );
 
-		render( <NotFoundSiteSubscriptions />, { wrapper: Wrapper( { searchTerm: 'music' } ) } );
+		render( <NotFoundSiteSubscriptions />, {
+			wrapper: Wrapper( { searchTerm: 'music' } ),
+		} );
 
 		expect(
 			await screen.findByText(
@@ -56,13 +90,49 @@ describe( 'NotFoundSiteSubscriptions', () => {
 				feeds: [],
 			} );
 
-		render( <NotFoundSiteSubscriptions />, { wrapper: Wrapper( { searchTerm: 'music' } ) } );
+		render( <NotFoundSiteSubscriptions />, {
+			wrapper: Wrapper( { searchTerm: 'music' } ),
+		} );
 
 		expect(
 			await screen.findByText( "You're not subscribed to any matching sites." )
 		).toBeVisible();
 		expect(
 			screen.queryByText( 'Here are some other sites related to your search.' )
+		).not.toBeInTheDocument();
+	} );
+
+	it( 'does not append recommendation copy when all feed results are already subscribed', async () => {
+		nock( 'https://public-api.wordpress.com' )
+			.get( '/rest/v1.1/read/feed' )
+			.query( () => true )
+			.reply( 200, {
+				feeds: [
+					{
+						feed_ID: '123',
+						subscribe_URL: 'https://already-subscribed.example.com/feed',
+					},
+				],
+			} );
+
+		render( <NotFoundSiteSubscriptions />, {
+			wrapper: Wrapper( {
+				searchTerm: 'music',
+				subscriptions: [
+					{
+						feed_ID: '123',
+						URL: 'https://already-subscribed.example.com/feed',
+						isDeleted: false,
+					},
+				],
+			} ),
+		} );
+
+		expect(
+			await screen.findByText( "You're not subscribed to any matching sites." )
+		).toBeVisible();
+		expect(
+			screen.queryByText( 'Here is one result related to your search.' )
 		).not.toBeInTheDocument();
 	} );
 } );
