37964944586517d623a21267a11496a991522aba
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2026-06-30 14:17:28 +0500

    ➕ ADDS: Following tags on ReaderTags page

--- client/reader/tags/main.tsx
+++ client/reader/tags/main.tsx
@@ -1,6 +1,7 @@
 import { useTranslate } from 'i18n-calypso';
 import NavigationHeader from 'calypso/components/navigation-header';
 import ReaderMain from 'calypso/reader/components/reader-main';
+import { FollowedTags } from 'calypso/reader/tags/followed-tags';
 import { useSelector } from 'calypso/state';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import AlphabeticTags from './alphabetic-tags';
@@ -21,12 +22,13 @@ export default function TagsPage( { trendingTags, alphabeticTags }: Props ) {
 		<ReaderMain className="tags__main" wideLayout>
 			{ isLoggedIn && (
 				<NavigationHeader
-					title={ translate( 'Popular Tags' ) }
+					title={ translate( 'Tags' ) }
 					subtitle={ translate(
 						"For every one of your interests, there's a tag on WordPress.com."
 					) }
 				/>
 			) }
+			{ isLoggedIn && <FollowedTags /> }
 			<div className="tags__header">
 				<h4>
 					{
