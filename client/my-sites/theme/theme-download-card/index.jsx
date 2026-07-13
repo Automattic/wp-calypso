8eb9744414991649fa0ad28c208fa2011c0af7c9
Copons <Copons@users.noreply.github.com>
2026-07-13 17:17:18 +0100

    Theme: provide Emotion cache so the theme sheet action lists render on the server

--- client/my-sites/theme/theme-download-card/index.jsx
+++ client/my-sites/theme/theme-download-card/index.jsx
@@ -3,6 +3,7 @@ import { Button } from '@wordpress/ui';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
+import EmotionCacheProvider from 'calypso/components/emotion-cache-provider';
 import ActionList from 'calypso/dashboard/components/action-list';
 
 import './style.scss';
@@ -27,25 +28,27 @@ class ThemeDownloadCard extends PureComponent {
 			}
 		);
 		return (
-			<div className="theme-download-card">
-				<ActionList>
-					<ActionList.ActionItem
-						title={ translate( 'Download this theme' ) }
-						description={ downloadText }
-						actions={
-							<Button
-								nativeButton={ false }
-								render={ <a href={ href } /> }
-								size="compact"
-								variant="outline"
-							>
-								<Button.Icon icon={ cloudDownload } />
-								{ translate( 'Download' ) }
-							</Button>
-						}
-					/>
-				</ActionList>
-			</div>
+			<EmotionCacheProvider>
+				<div className="theme-download-card">
+					<ActionList>
+						<ActionList.ActionItem
+							title={ translate( 'Download this theme' ) }
+							description={ downloadText }
+							actions={
+								<Button
+									nativeButton={ false }
+									render={ <a href={ href } /> }
+									size="compact"
+									variant="outline"
+								>
+									<Button.Icon icon={ cloudDownload } />
+									{ translate( 'Download' ) }
+								</Button>
+							}
+						/>
+					</ActionList>
+				</div>
+			</EmotionCacheProvider>
 		);
 	}
 }
--- client/my-sites/theme/theme-support-tab/index.jsx
+++ client/my-sites/theme/theme-support-tab/index.jsx
@@ -3,6 +3,7 @@ import { localizeUrl } from '@automattic/i18n-utils';
 import { useDispatch as useDataStoreDispatch } from '@wordpress/data';
 import { Button } from '@wordpress/ui';
 import { useTranslate } from 'i18n-calypso';
+import EmotionCacheProvider from 'calypso/components/emotion-cache-provider';
 import useSupportDocData from 'calypso/components/inline-support-link/use-support-doc-data';
 import ActionList from 'calypso/dashboard/components/action-list';
 import { useDispatch, useSelector } from 'calypso/state';
@@ -22,115 +23,117 @@ export default function ThemeSupportTab( { themeId } ) {
 	const { openSupportDoc } = useSupportDocData( { supportContext: 'themes-unsupported' } );
 
 	return (
-		<ActionList>
-			<ActionList.ActionItem
-				title={ translate( 'Learn WordPress' ) }
-				description={ translate(
-					'Follow along with beginner-friendly courses and build your first website or blog.'
-				) }
-				actions={
-					<Button
-						nativeButton={ false }
-						render={
-							<a
-								href={ localizeUrl( 'https://wordpress.com/support/courses' ) }
-								rel="noreferrer"
-								target="_blank"
-							/>
-						}
-						onClick={ () =>
-							dispatch(
-								recordTracksEvent( 'calypso_theme_sheet_button_click', {
-									theme_name: themeId,
-									button_context: 'courses',
-								} )
-							)
-						}
-						size="compact"
-						variant="outline"
-					>
-						{ translate( 'Watch a course' ) }
-					</Button>
-				}
-			/>
-			{ isLoggedIn && (
+		<EmotionCacheProvider>
+			<ActionList>
 				<ActionList.ActionItem
-					title={ translate( 'Discover comprehensive guides' ) }
+					title={ translate( 'Learn WordPress' ) }
 					description={ translate(
-						'Explore deep-dive tutorials for every WordPress.com feature.'
+						'Follow along with beginner-friendly courses and build your first website or blog.'
 					) }
 					actions={
 						<Button
-							onClick={ () => {
-								setShowHelpCenter( true );
-								setNavigateToRoute( '/' );
+							nativeButton={ false }
+							render={
+								<a
+									href={ localizeUrl( 'https://wordpress.com/support/courses' ) }
+									rel="noreferrer"
+									target="_blank"
+								/>
+							}
+							onClick={ () =>
 								dispatch(
 									recordTracksEvent( 'calypso_theme_sheet_button_click', {
 										theme_name: themeId,
-										button_context: 'help-center',
+										button_context: 'courses',
 									} )
-								);
-							} }
+								)
+							}
 							size="compact"
 							variant="outline"
 						>
-							{ translate( 'Visit guides' ) }
+							{ translate( 'Watch a course' ) }
 						</Button>
 					}
 				/>
-			) }
-			{ isLoggedIn &&
-				( themeTier?.slug === 'community' ? (
+				{ isLoggedIn && (
 					<ActionList.ActionItem
-						title={ translate(
-							'Help and support for this theme is not offered by WordPress.com.'
-						) }
+						title={ translate( 'Discover comprehensive guides' ) }
 						description={ translate(
-							'Contact the theme developer directly for help with this theme.'
+							'Explore deep-dive tutorials for every WordPress.com feature.'
 						) }
 						actions={
 							<Button
 								onClick={ () => {
-									openSupportDoc();
+									setShowHelpCenter( true );
+									setNavigateToRoute( '/' );
 									dispatch(
 										recordTracksEvent( 'calypso_theme_sheet_button_click', {
 											theme_name: themeId,
-											button_context: 'themes-unsupported',
+											button_context: 'help-center',
 										} )
 									);
 								} }
 								size="compact"
 								variant="outline"
 							>
-								{ translate( 'Learn more' ) }
+								{ translate( 'Visit guides' ) }
 							</Button>
 						}
 					/>
-				) : (
-					<ActionList.ActionItem
-						title={ translate( 'Contact support' ) }
-						description={ translate(
-							'Get answers from our AI assistant, with access to 24/7 expert human support on paid plans.'
-						) }
-						actions={
-							<Button
-								onClick={ () => {
-									setNavigateToOdie();
-									dispatch(
-										recordTracksEvent( 'calypso_theme_sheet_button_click', {
-											theme_name: themeId,
-											button_context: 'help-center-ai',
-										} )
-									);
-								} }
-								size="compact"
-								variant="outline"
-							>
-								{ translate( 'Get in touch' ) }
-							</Button>
-						}
-					/>
-				) ) }
-		</ActionList>
+				) }
+				{ isLoggedIn &&
+					( themeTier?.slug === 'community' ? (
+						<ActionList.ActionItem
+							title={ translate(
+								'Help and support for this theme is not offered by WordPress.com.'
+							) }
+							description={ translate(
+								'Contact the theme developer directly for help with this theme.'
+							) }
+							actions={
+								<Button
+									onClick={ () => {
+										openSupportDoc();
+										dispatch(
+											recordTracksEvent( 'calypso_theme_sheet_button_click', {
+												theme_name: themeId,
+												button_context: 'themes-unsupported',
+											} )
+										);
+									} }
+									size="compact"
+									variant="outline"
+								>
+									{ translate( 'Learn more' ) }
+								</Button>
+							}
+						/>
+					) : (
+						<ActionList.ActionItem
+							title={ translate( 'Contact support' ) }
+							description={ translate(
+								'Get answers from our AI assistant, with access to 24/7 expert human support on paid plans.'
+							) }
+							actions={
+								<Button
+									onClick={ () => {
+										setNavigateToOdie();
+										dispatch(
+											recordTracksEvent( 'calypso_theme_sheet_button_click', {
+												theme_name: themeId,
+												button_context: 'help-center-ai',
+											} )
+										);
+									} }
+									size="compact"
+									variant="outline"
+								>
+									{ translate( 'Get in touch' ) }
+								</Button>
+							}
+						/>
+					) ) }
+			</ActionList>
+		</EmotionCacheProvider>
 	);
 }
