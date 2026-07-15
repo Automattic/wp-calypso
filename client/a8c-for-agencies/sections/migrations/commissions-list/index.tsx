fad995915198c5be53a08f1ac0caf7fb9aa10058
James Kenneth Guidaven <jkguidaven@gmail.com>
2026-07-15 21:35:33 +0800

    A4A Migrations commissions: thread analytics/notices through commissions list

--- client/a8c-for-agencies/sections/migrations/commissions-list/index.tsx
+++ client/a8c-for-agencies/sections/migrations/commissions-list/index.tsx
@@ -7,7 +7,7 @@ import RequestReviewModal from '../request-review-modal';
 import { MigratedOnColumn, ReviewStatusColumn, SiteColumn } from './commission-columns';
 import UntagSiteDialog from './untag-site-dialog';
 import useCommissionListActions from './use-commission-list-actions';
-import type { TaggedSite } from '../types';
+import type { RecordTracksEvent, TaggedSite } from '../types';
 import type { Field, View } from '@wordpress/dataviews';
 
 import '../commissions/components/dataviews/style.scss';
@@ -55,9 +55,15 @@ const INITIAL_VIEW: View = {
 export default function MigrationsCommissionsList( {
 	items,
 	migrationTags,
+	recordTracksEvent,
+	onSuccess,
+	onError,
 }: {
 	items: TaggedSite[];
 	migrationTags: string[];
+	recordTracksEvent: RecordTracksEvent;
+	onSuccess: ( message: ReactNode ) => void;
+	onError: ( message: ReactNode ) => void;
 } ) {
 	const isDesktop = useDesktopBreakpoint();
 
@@ -160,11 +166,18 @@ export default function MigrationsCommissionsList( {
 					site={ activeModal.site }
 					migrationTags={ migrationTags }
 					onClose={ closeModal }
+					onSuccess={ onSuccess }
 				/>
 			) }
 
 			{ activeModal?.kind === 'request-review' && (
-				<RequestReviewModal site={ activeModal.site } onClose={ closeModal } />
+				<RequestReviewModal
+					site={ activeModal.site }
+					onClose={ closeModal }
+					recordTracksEvent={ recordTracksEvent }
+					onSuccess={ onSuccess }
+					onError={ onError }
+				/>
 			) }
 		</>
 	);
