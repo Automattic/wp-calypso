c561696ed833d3ccad7c83b92ddc3eb64c9413dc
James Kenneth Guidaven <jkguidaven@gmail.com>
2026-07-15 21:41:09 +0800

    A4A Migrations commissions: render shared content from the A4A host

--- client/a8c-for-agencies/sections/migrations/primary/migrations-commissions/index.tsx
+++ client/a8c-for-agencies/sections/migrations/primary/migrations-commissions/index.tsx
@@ -1,31 +1,31 @@
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import clsx from 'clsx';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { LayoutWithGuidedTour as Layout } from 'calypso/a8c-for-agencies/components/layout/layout-with-guided-tour';
 import LayoutTop from 'calypso/a8c-for-agencies/components/layout/layout-with-payment-notification';
 import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
 import { A4A_MIGRATIONS_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import MissingPaymentSettingsNotice from 'calypso/a8c-for-agencies/sections/referrals/common/missing-payment-settings-notice';
-import { TextSkeleton } from 'calypso/dashboard/components/text-skeleton';
 import LayoutBody from 'calypso/layout/hosting-dashboard/body';
 import LayoutHeader, {
 	LayoutHeaderBreadcrumb as Breadcrumb,
 	LayoutHeaderActions as Actions,
 } from 'calypso/layout/hosting-dashboard/header';
-import { useDispatch } from 'calypso/state';
+import { useDispatch, useSelector } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import MigrationsCommissionsList from '../../commissions-list';
-import MigrationsConsolidatedCommissions from '../../consolidated-commissions';
+import { errorNotice, successNotice } from 'calypso/state/notices/actions';
+import getSites from 'calypso/state/selectors/get-sites';
+import MigrationsCommissionsContent from '../../commissions-content';
 import useCanTagSitesForCommission from '../../hooks/use-can-tag-sites-for-commission';
 import useFetchTaggedSitesForMigration from '../../hooks/use-fetch-tagged-sites-for-migration';
-import MigrationsTagSitesModal from '../../tag-sites-modal';
-import MigrationsCommissionsEmptyState from './empty-state';
+import type { ReactNode } from 'react';
 
 import './style.scss';
 
 export default function MigrationsCommissions() {
 	const dispatch = useDispatch();
+	const sites = useSelector( getSites );
 
 	const [ showAddSitesModal, setShowAddSitesModal ] = useState( false );
 	const {
@@ -36,38 +36,34 @@ export default function MigrationsCommissions() {
 
 	const title = __( 'Migrations: commissions' );
 
+	const recordTracks = useCallback(
+		( name: string, properties?: Record< string, unknown > ) =>
+			dispatch( recordTracksEvent( name, properties ) ),
+		[ dispatch ]
+	);
+
+	const onSuccess = useCallback(
+		( message: ReactNode ) => dispatch( successNotice( message ) ),
+		[ dispatch ]
+	);
+
+	const onError = useCallback(
+		( message: ReactNode ) => dispatch( errorNotice( message ) ),
+		[ dispatch ]
+	);
+
+	const getSiteCreatedAt = useCallback(
+		( blogId: number ) => sites.find( ( s ) => s?.ID === blogId )?.options?.created_at,
+		[ sites ]
+	);
+
 	const onTagSitesClick = useCallback( () => {
-		dispatch( recordTracksEvent( 'calypso_a8c_migrations_commissions_tag_sites_click' ) );
+		recordTracks( 'calypso_a8c_migrations_commissions_tag_sites_click' );
 		setShowAddSitesModal( true );
-	}, [ dispatch ] );
+	}, [ recordTracks ] );
 
-	const { data, isLoading } = useFetchTaggedSitesForMigration();
-	const taggedSites = data ?? [];
-
-	const showEmptyState = ! taggedSites.length;
-
-	const content = useMemo( () => {
-		if ( isLoading ) {
-			return (
-				<>
-					<TextSkeleton length={ 30 } />
-					<TextSkeleton length={ 30 } />
-				</>
-			);
-		}
-
-		return showEmptyState ? (
-			<MigrationsCommissionsEmptyState
-				setShowAddSitesModal={ setShowAddSitesModal }
-				canTagSitesForCommission={ canTagSitesForCommission }
-			/>
-		) : (
-			<div className="migrations-commissions__content">
-				{ canTagSitesForCommission && <MigrationsConsolidatedCommissions items={ taggedSites } /> }
-				<MigrationsCommissionsList items={ taggedSites } migrationTags={ migrationTags } />
-			</div>
-		);
-	}, [ isLoading, showEmptyState, canTagSitesForCommission, taggedSites, migrationTags ] );
+	const { data } = useFetchTaggedSitesForMigration();
+	const showEmptyState = ! ( data ?? [] ).length;
 
 	return (
 		<Layout
@@ -104,16 +100,17 @@ export default function MigrationsCommissions() {
 			</LayoutTop>
 
 			<LayoutBody>
-				<>
-					{ content }
-					{ showAddSitesModal && (
-						<MigrationsTagSitesModal
-							onClose={ () => setShowAddSitesModal( false ) }
-							taggedSites={ taggedSites }
-							migrationTags={ migrationTags }
-						/>
-					) }
-				</>
+				<MigrationsCommissionsContent
+					recordTracksEvent={ recordTracks }
+					onSuccess={ onSuccess }
+					onError={ onError }
+					getSiteCreatedAt={ getSiteCreatedAt }
+					canTagSitesForCommission={ canTagSitesForCommission }
+					migrationTags={ migrationTags }
+					isAddSitesModalOpen={ showAddSitesModal }
+					onCloseAddSitesModal={ () => setShowAddSitesModal( false ) }
+					onOpenAddSitesModal={ () => setShowAddSitesModal( true ) }
+				/>
 			</LayoutBody>
 		</Layout>
 	);
