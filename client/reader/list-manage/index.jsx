b2280d2e13aae827229251c1c26aa6eab6548ee6
Addison Stavlo <Addison.Stavlo@Automattic.com>
2026-07-20 10:16:26 -0400

    add punctuation

--- client/reader/list-manage/index.jsx
+++ client/reader/list-manage/index.jsx
@@ -139,7 +139,7 @@ function ReaderListCreate() {
 		<ReaderMain>
 			<NavigationHeader
 				title={ translate( 'Create List' ) }
-				subtitle={ translate( 'Lists let you organize multiple sites into custom feeds' ) }
+				subtitle={ translate( 'Lists let you organize multiple sites into custom feeds.' ) }
 			/>
 			<ListForm isCreateForm isSubmissionDisabled={ isCreatingList } onSubmit={ handleSubmit } />
 		</ReaderMain>
