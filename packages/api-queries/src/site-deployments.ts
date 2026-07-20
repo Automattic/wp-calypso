daf1ac42edbeb8804451093960fa90b8231b4da7
Philip Jackson <p-jackson@live.com>
2026-07-20 19:58:34 +1200

    api-queries: drop redundant code- prefix from deploy statIds

--- packages/api-queries/src/site-deployments.ts
+++ packages/api-queries/src/site-deployments.ts
@@ -33,7 +33,7 @@ export const codeDeploymentRunsQuery = ( siteId: number, deploymentId: number ) 
 
 export const createCodeDeploymentRunMutation = () =>
 	mutationOptions( {
-		meta: { statId: 'code-deploy-run-create' },
+		meta: { statId: 'deploy-run-create' },
 		mutationFn: async ( { siteId, deploymentId }: { siteId: number; deploymentId: number } ) =>
 			createCodeDeploymentRun( siteId, deploymentId ),
 		onSuccess: ( ...args ) => {
@@ -47,14 +47,14 @@ export const createCodeDeploymentRunMutation = () =>
 
 export const codeDeploymentDeleteMutation = ( siteId: number, deploymentId: number ) =>
 	mutationOptions( {
-		meta: { statId: 'code-deploy-delete' },
+		meta: { statId: 'deploy-delete' },
 		mutationFn: ( removeFiles: boolean ) =>
 			deleteCodeDeployment( siteId, deploymentId, removeFiles ),
 	} );
 
 export const createCodeDeploymentMutation = ( siteId: number ) =>
 	mutationOptions( {
-		meta: { statId: 'code-deploy-create' },
+		meta: { statId: 'deploy-create' },
 		mutationFn: ( variables: CreateAndUpdateCodeDeploymentVariables ) =>
 			createCodeDeployment( siteId, variables ),
 		onSuccess: () => {
@@ -64,7 +64,7 @@ export const createCodeDeploymentMutation = ( siteId: number ) =>
 
 export const updateCodeDeploymentMutation = ( siteId: number, deploymentId: number ) =>
 	mutationOptions( {
-		meta: { statId: 'code-deploy-update' },
+		meta: { statId: 'deploy-update' },
 		mutationFn: ( variables: CreateAndUpdateCodeDeploymentVariables ) =>
 			updateCodeDeployment( siteId, deploymentId, variables ),
 		onSuccess: () => {
