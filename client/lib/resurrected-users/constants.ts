791590f11603e997d5c6d0578eea7d55439bd39f
Sorin Nunca <sorin.nunca@automattic.com>
2026-07-21 20:42:06 +0300

    Returning user modal: Track returning user at 3 months.

--- client/lib/resurrected-users/constants.ts
+++ client/lib/resurrected-users/constants.ts
@@ -1,8 +1,10 @@
 export const RESURRECTION_DAY_LIMIT_DEFAULT = 373;
 export const RESURRECTION_DAY_LIMIT_EXPERIMENT = 180;
+export const RESURRECTION_DAY_LIMIT_EXPERIMENT2 = 90;
 
 export const RESURRECTED_EVENT = 'calypso_user_resurrected';
 export const RESURRECTED_EVENT_6M = 'calypso_user_resurrected_6m';
+export const RESURRECTED_EVENT_3M = 'calypso_user_resurrected_3m';
 
 /** Rolled-out welcome-back modal variation (manual onboarding + continue). */
 export const WELCOME_BACK_VARIATION_MANUAL = 'treatment_manual_dual' as const;
