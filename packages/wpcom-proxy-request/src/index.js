8b3227fd7808865c8412838f24571651fbaf456c
Omar Alshaker <omar@omaralshaker.com>
2026-07-23 14:42:50 +0200

    Allow all jurassic tube

--- packages/wpcom-proxy-request/src/index.js
+++ packages/wpcom-proxy-request/src/index.js
@@ -553,7 +553,6 @@ const wpcomAllowedOrigins = [
 	'https://agencies.automattic.com',
 	'https://my.wordpress.com',
 	'https://woocommerce.com',
-	'https://woocommercealshakero.jurassic.tube',
 	'http://wpcalypso.wordpress.com', // for running docker on dev instances
 	'http://widgets.wp.com',
 	'https://widgets.wp.com',
@@ -595,6 +594,7 @@ function isAllowedOrigin( urlOrigin ) {
 		wpcomAllowedOrigins.includes( urlOrigin ) ||
 		isLocalDevOrigin( urlOrigin ) ||
 		/^https:\/\/[a-z0-9-]+\.calypso\.live$/.test( urlOrigin ) ||
+		/^https:\/\/[a-z0-9-]+\.jurassic\.tube$/.test( urlOrigin ) ||
 		/^https:\/\/([a-z0-9-]+\.)+wordpress\.com$/.test( urlOrigin )
 	);
 }
