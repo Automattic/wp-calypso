35fed6c102610753add0cb78906f777016ef558a
Philip Jackson <p-jackson@users.noreply.github.com>
2026-07-31 18:39:03 +1200

    E2E: skip broken new-hosted-site purchase+cancel test (DOTMSD-1480) (#113167)

--- test/e2e/specs/onboarding/onboarding__new-hosted-site.spec.ts
+++ test/e2e/specs/onboarding/onboarding__new-hosted-site.spec.ts
@@ -52,7 +52,8 @@ test.describe(
 			} );
 		} );
 
-		test( 'As a new user, I can purchase a hosted site and cancel it', async ( { page } ) => {
+		// Test currently broken on opening the dashboard sidebar on mobile DOTMSD-1480
+		test.skip( 'As a new user, I can purchase a hosted site and cancel it', async ( { page } ) => {
 			// ~360s of dominant waits (90s purchase + 180s Atomic transfer + 30s
 			// checkout + two 30s refund notices) plus signup, billing and two
 			// cancellation navigations; 420s covers the worst case.
