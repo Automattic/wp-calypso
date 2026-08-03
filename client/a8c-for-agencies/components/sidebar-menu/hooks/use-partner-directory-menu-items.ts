ffe46b96a7c0945dc5bac411fa412ff7be707861
Clemente <cleacos@users.noreply.github.com>
2026-08-03 16:06:39 +0100

    A4A-3007: Open Lead Matching to all agencies

--- client/a8c-for-agencies/components/sidebar-menu/hooks/use-partner-directory-menu-items.ts
+++ client/a8c-for-agencies/components/sidebar-menu/hooks/use-partner-directory-menu-items.ts
@@ -9,7 +9,6 @@ import {
 	PARTNER_DIRECTORY_DASHBOARD_SLUG,
 	PARTNER_DIRECTORY_LEAD_MATCHING_SLUG,
 } from 'calypso/a8c-for-agencies/sections/partner-directory/constants';
-import { isLeadMatchingSectionVisible } from 'calypso/a8c-for-agencies/sections/partner-directory/lib/lead-matching-visibility';
 import { useSelector } from 'calypso/state';
 import { getActiveAgency } from 'calypso/state/a8c-for-agencies/agency/selectors';
 
@@ -24,8 +23,6 @@ const usePartnerDirectoryMenuItems = ( path: string ) => {
 	const hasDirectoryApproval = agency?.profile?.partner_directory_application?.directories.some(
 		( { status } ) => status === 'approved'
 	);
-	const canAccessLeadMatching = isLeadMatchingSectionVisible();
-
 	const menuItems = useMemo( () => {
 		return [
 			createItem(
@@ -64,27 +61,23 @@ const usePartnerDirectoryMenuItems = ( path: string ) => {
 						),
 				  ]
 				: [] ),
-			...( canAccessLeadMatching
-				? [
-						createItem(
-							{
-								icon: people,
-								path: A4A_PARTNER_DIRECTORY_LINK,
-								link: `${ A4A_PARTNER_DIRECTORY_LINK }/${ PARTNER_DIRECTORY_LEAD_MATCHING_SLUG }`,
-								title: translate( 'Lead matching' ),
-								trackEventProps: {
-									menu_item: 'Automattic for Agencies / Partner Directory / Lead matching',
-								},
-								isSelected: isSelected( path, [
-									`${ A4A_PARTNER_DIRECTORY_LINK }/${ PARTNER_DIRECTORY_LEAD_MATCHING_SLUG }`,
-								] ),
-							},
-							path
-						),
-				  ]
-				: [] ),
+			createItem(
+				{
+					icon: people,
+					path: A4A_PARTNER_DIRECTORY_LINK,
+					link: `${ A4A_PARTNER_DIRECTORY_LINK }/${ PARTNER_DIRECTORY_LEAD_MATCHING_SLUG }`,
+					title: translate( 'Lead matching' ),
+					trackEventProps: {
+						menu_item: 'Automattic for Agencies / Partner Directory / Lead matching',
+					},
+					isSelected: isSelected( path, [
+						`${ A4A_PARTNER_DIRECTORY_LINK }/${ PARTNER_DIRECTORY_LEAD_MATCHING_SLUG }`,
+					] ),
+				},
+				path
+			),
 		];
-	}, [ canAccessLeadMatching, hasDirectoryApproval, path, translate ] );
+	}, [ hasDirectoryApproval, path, translate ] );
 	return menuItems;
 };
 
