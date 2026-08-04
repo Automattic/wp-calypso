10cb577413de3372b099195fa740d87e88337728
Clemente <cleacos@users.noreply.github.com>
2026-08-04 11:16:49 +0100

    Remove Marketplace section background artwork

--- client/a8c-for-agencies/components/page-section/backgrounds/index.tsx
+++ client/a8c-for-agencies/components/page-section/backgrounds/index.tsx
@@ -1,6 +1,4 @@
 import Background1Image from './background-1.svg';
-import Background10Image from './background-10.svg';
-import Background11Image from './background-11.svg';
 import Background2Image from './background-2.svg';
 import Background3Image from './background-3.svg';
 import Background4Image from './background-4.svg';
@@ -8,10 +6,9 @@ import Background5Image from './background-5.svg';
 import Background6Image from './background-6.svg';
 import Background7Image from './background-7.svg';
 import Background8Image from './background-8.svg';
-import Background9Image from './background-9.svg';
 
 export type SectionBackground = {
-	image: string;
+	image?: string;
 	color: string;
 	size?: string;
 };
@@ -62,18 +59,13 @@ export const BackgroundType8: SectionBackground = {
 };
 
 export const BackgroundType9: SectionBackground = {
-	image: `url(${ Background9Image })`,
 	color: '#EBF4FA',
-	size: 'auto 100%',
 };
 
 export const BackgroundType10: SectionBackground = {
-	image: `url(${ Background10Image })`,
 	color: '#EBF4FA',
-	size: 'auto 100%',
 };
 
 export const BackgroundType11: SectionBackground = {
-	image: `url(${ Background11Image })`,
 	color: '#F5F2F1',
 };
