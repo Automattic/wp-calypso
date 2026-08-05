643dba863ae5a011ac6bcdc92c5b4b8cd4ac2a54
Luca Tumedei <luca.tumedei@automattic.com>
2026-08-05 13:52:42 +0200

    E2E: prime only the account the i18n and p2 builds use
    Both builds run a single tagged group against one account, so the default
    list made them wait on eight logins they never touch.

--- .teamcity/_self/projects/WPComTests.kt
+++ .teamcity/_self/projects/WPComTests.kt
@@ -391,6 +391,7 @@ private object I18NTests : BuildType({
 		param("CALYPSO_BASE_URL", "https://wordpress.com")
 		param("DASHBOARD_BASE_URL", "https://my.wordpress.com")
 		param("env.E2E_CTRF_APP_NAME", "i18n (calypso)")
+		param("env.AUTHENTICATE_ACCOUNTS", "i18nUser")
 	}
 
 	features {
@@ -435,6 +436,7 @@ private object P2E2ETests : BuildType({
 		param("CALYPSO_BASE_URL", "https://wpcalypso.wordpress.com")
 		param("DASHBOARD_BASE_URL", "https://my.wordpress.com")
 		param("env.E2E_CTRF_APP_NAME", "p2 (calypso)")
+		param("env.AUTHENTICATE_ACCOUNTS", "p2User")
 	}
 
 	features {
