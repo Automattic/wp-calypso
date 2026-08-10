78f83252a45a75c525a48e6020c4aabaf4fd57ab
Joao Heringer <joao.heringer@automattic.com>
2026-08-10 20:52:42 -0300

    Add per-package lint config for agenttic packages

--- packages/agenttic-client/.eslintrc.cjs
+++ packages/agenttic-client/.eslintrc.cjs
@@ -1,17 +1,4 @@
 module.exports = {
-	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: [ './tsconfig.json' ],
-	},
-	rules: {
-		'@wordpress/i18n-text-domain': [
-			'error',
-			{
-				allowedTextDomain: 'a8c-agenttic',
-			},
-		],
-	},
 	overrides: [
 		{
 			files: [
