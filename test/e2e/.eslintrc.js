3aa8a199f4b307eb67a5aaa5726e11bf4fc0fb37
Luca Tumedei <luca.tumedei@automattic.com>
2026-08-11 11:11:00 +0200

    E2E: drop the jest leftovers from the test workspace
    The suite runs on Playwright. Nothing under test/e2e imports jest, babel or
    webpack, so the eslint overrides silenced rules that no longer fire and the
    deps resolved for no one.

--- test/e2e/.eslintrc.js
+++ test/e2e/.eslintrc.js
@@ -27,13 +27,6 @@ module.exports = {
 			},
 		},
 		{
-			files: [ 'specs/**/shared/**/*', 'lib/shared-steps/**/*' ],
-			rules: {
-				// This directory is used to create shared specs that can be re-used in multiple places.
-				'jest/no-export': 'off',
-			},
-		},
-		{
 			files: [ 'docs/tests_local.md' ],
 			rules: {
 				'lint-no-multiple-toplevel-headings': 'off',
@@ -42,17 +35,6 @@ module.exports = {
 	],
 	rules: {
 		...nodeConfig.rules,
-
-		// We have many tests that don't make an explicit `expect`, but instead puts the browser
-		// in certain state that will be used by the next test, or asserted by WebDriver
-		'jest/expect-expect': 'off',
-
-		// We compose the test titles dynamically
-		'jest/valid-title': 'off',
-
-		// The rule hasn't had the intended results (encouraging owners to re-enable and fix their tests).
-		// See GitHub issue #64870 for context (https://github.com/Automattic/wp-calypso/issues/64870).
-		'jest/no-disabled-tests': 'off',
 
 		'jsdoc/tag-lines': [ 'off' ],
 	},
