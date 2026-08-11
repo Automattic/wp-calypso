a5ad0aac7ebded04a606db3210f808a89fce1c8f
Luca Tumedei <luca.tumedei@automattic.com>
2026-08-11 12:58:05 +0200

    E2E: drop two comments describing what is no longer there
    The globalThis note outlived the var declarations it explained, and
    tablet has not been a viewport since getViewports() listed two.

--- packages/calypso-e2e/src/global.d.ts
+++ packages/calypso-e2e/src/global.d.ts
@@ -1,10 +1,6 @@
 import type { TracksEvent } from './types/editor-tracks.types';
 
 declare global {
-	// Node 16 types removed the NodeJS.Global interface in favor of just using globalThis.
-	// See https://github.com/DefinitelyTyped/DefinitelyTyped/pull/53669
-	// This is the new syntax to modify globalThis. Note, 'let' and 'const' do not work!
-	// See https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#type-checking-for-globalthis
 	interface Window {
 		_e2eEventsStack: TracksEvent[];
 	}
