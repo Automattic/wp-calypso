31cc5c297fcc28ee088e9314f761e512987e43c2
Chris McCluskey <chris.mccluskey@automattic.com>
2026-08-21 15:50:50 -0700

    Copy InfoPopover to MSD, use in purchase settings

---
+++ client/dashboard/components/info-popover/index.jsx
@@ -0,0 +1,158 @@
+import { Popover } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { Icon } from '@wordpress/icons';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { createRef, Component, Fragment } from 'react';
+import { gridiconToWordPressIcon } from '../../utils/gridicons';
+
+import './style.scss';
+
+export default class InfoPopover extends Component {
+	static propTypes = {
+		autoRtl: PropTypes.bool,
+		className: PropTypes.string,
+		gaEventCategory: PropTypes.string,
+		icon: PropTypes.oneOfType( [ PropTypes.string, PropTypes.element ] ),
+		iconSize: PropTypes.number,
+		id: PropTypes.string,
+		ignoreContext: PropTypes.shape( {
+			getDOMNode: PropTypes.func,
+		} ),
+		onOpen: PropTypes.func,
+		onClose: PropTypes.func,
+		popoverName: PropTypes.string,
+		position: PropTypes.oneOf( [
+			'top',
+			'top right',
+			'right',
+			'bottom right',
+			'bottom',
+			'bottom left',
+			'left',
+			'top left',
+		] ),
+		showOnHover: PropTypes.bool,
+		children: PropTypes.node,
+		'aria-label': PropTypes.string,
+	};
+
+	static defaultProps = {
+		autoRtl: true,
+		icon: 'info-outline',
+		iconSize: 18,
+		position: 'bottom',
+		showOnHover: false,
+	};
+
+	iconRef = createRef();
+
+	state = { showPopover: false };
+
+	handleClick = ( e ) => {
+		const { onOpen, showOnHover } = this.props;
+		const { showPopover } = this.state;
+
+		e.preventDefault();
+		e.stopPropagation();
+
+		if ( showOnHover ) {
+			return;
+		}
+
+		// There's no "handleOpen" method for us to hook into,
+		// so we check here to see if the intent is to open the popover
+		// and fire onOpen accordingly
+		if ( ! showPopover ) {
+			onOpen?.();
+		}
+
+		this.setState( { showPopover: ! showPopover } );
+	};
+
+	handleClose = () => {
+		this.props.onClose?.();
+		this.setState( { showPopover: false } );
+	};
+
+	handleOnMouseEnterButton = () => {
+		const { onOpen, showOnHover } = this.props;
+
+		if ( ! showOnHover ) {
+			return;
+		}
+
+		onOpen?.();
+		this.setState( { showPopover: true } );
+	};
+
+	handleOnMouseLeave = () => {
+		setTimeout( () => {
+			const { showOnHover } = this.props;
+
+			if ( ! showOnHover ) {
+				return;
+			}
+
+			if ( this.inPopover ) {
+				return;
+			}
+
+			this.setState( { showPopover: false } );
+		}, 250 );
+	};
+
+	handleOnMouseEnterPopover = () => {
+		this.inPopover = true;
+	};
+
+	handleOnMouseLeavePopover = () => {
+		this.inPopover = false;
+		this.handleOnMouseLeave();
+	};
+
+	render() {
+		return (
+			<Fragment>
+				<button
+					type="button"
+					aria-haspopup
+					aria-expanded={ this.state.showPopover }
+					aria-label={ this.props[ 'aria-label' ] || __( 'More information' ) }
+					onClick={ this.handleClick }
+					onMouseEnter={ this.handleOnMouseEnterButton }
+					onMouseLeave={ this.handleOnMouseLeave }
+					ref={ this.iconRef }
+					className={ clsx( 'info-popover', this.props.className, {
+						'is-active': this.state.showPopover,
+					} ) }
+				>
+					{ typeof this.props.icon === 'string' ? (
+						<Icon
+							icon={ gridiconToWordPressIcon( this.props.icon ) }
+							size={ this.props.iconSize }
+						/>
+					) : (
+						this.props.icon
+					) }
+				</button>
+				{ this.state.showPopover && (
+					<Popover
+						autoRtl={ this.props.autoRtl }
+						id={ this.props.id }
+						isVisible
+						context={ this.iconRef.current }
+						ignoreContext={ this.props.ignoreContext }
+						position={ this.props.position }
+						onClose={ this.handleClose }
+						className={ clsx( 'info-popover__tooltip', this.props.className ) }
+						onMouseEnter={ this.handleOnMouseEnterPopover }
+						onMouseLeave={ this.handleOnMouseLeavePopover }
+					>
+						{ this.props.children }
+					</Popover>
+				) }
+			</Fragment>
+		);
+	}
+}
