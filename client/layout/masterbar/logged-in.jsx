937882939f9fb0a02a583dc2e77ab130028ec6ed
Steven <steve.dufresne@automattic.com>
2026-08-27 09:26:46 +0900

    Masterbar: drop the opted-out variant of the management links
    The hosting dashboard rollout is at 100%, so the not-opted-in branch was
    only reachable by support forced-opt-out accounts.

--- client/layout/masterbar/logged-in.jsx
+++ client/layout/masterbar/logged-in.jsx
@@ -306,45 +306,23 @@ class MasterbarLoggedIn extends Component {
 							url: dashboardOptIn ? dashboardLink( '/domains' ) : '/domains/manage',
 							onClick: () => this.props.recordTracksEvent( 'calypso_masterbar_domains_clicked' ),
 						},
-						...( dashboardOptIn
-							? [
-									{
-										label: translate( 'Emails' ),
-										url: dashboardLink( '/emails' ),
-										onClick: () =>
-											this.props.recordTracksEvent( 'calypso_masterbar_emails_clicked' ),
-									},
-									{
-										label: translate( 'Plugins' ),
-										url: dashboardLink( '/plugins/manage' ),
-										onClick: () =>
-											this.props.recordTracksEvent( 'calypso_masterbar_plugins_clicked' ),
-									},
-									{
-										label: translate( 'Themes' ),
-										url: wpcomLink( '/themes' ),
-										openInNewTab: true,
-										className: 'masterbar__item-subitems-item-external',
-										onClick: () =>
-											this.props.recordTracksEvent( 'calypso_masterbar_themes_clicked' ),
-									},
-							  ]
-							: [
-									{
-										label: translate( 'Plugins' ),
-										url: '/plugins/manage/sites',
-										onClick: () =>
-											this.props.recordTracksEvent( 'calypso_masterbar_plugins_clicked' ),
-									},
-									{
-										label: translate( 'Themes' ),
-										url: wpcomLink( '/themes' ),
-										openInNewTab: true,
-										className: 'masterbar__item-subitems-item-external',
-										onClick: () =>
-											this.props.recordTracksEvent( 'calypso_masterbar_themes_clicked' ),
-									},
-							  ] ),
+						{
+							label: translate( 'Emails' ),
+							url: dashboardLink( '/emails' ),
+							onClick: () => this.props.recordTracksEvent( 'calypso_masterbar_emails_clicked' ),
+						},
+						{
+							label: translate( 'Plugins' ),
+							url: dashboardLink( '/plugins/manage' ),
+							onClick: () => this.props.recordTracksEvent( 'calypso_masterbar_plugins_clicked' ),
+						},
+						{
+							label: translate( 'Themes' ),
+							url: wpcomLink( '/themes' ),
+							openInNewTab: true,
+							className: 'masterbar__item-subitems-item-external',
+							onClick: () => this.props.recordTracksEvent( 'calypso_masterbar_themes_clicked' ),
+						},
 					],
 					...( this.props.isSimpleSite
 						? []
