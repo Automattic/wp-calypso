07374b794654c17b642cc2d01a8b3f7bc7da29e9
Ashar Fuadi <ashar.fuadi@automattic.com>
2026-09-07 11:16:50 +0700

    Dashboard: don't pass locale variants straight to Intl constructors

--- client/dashboard/app/hooks/use-intl-collator.ts
+++ client/dashboard/app/hooks/use-intl-collator.ts
@@ -1,20 +1,12 @@
-import { useLocale } from '../locale';
+import { useIntlLocale } from '../locale';
 
 const useIntlCollator = () => {
-	const userLocale = useLocale();
+	const userLocale = useIntlLocale();
 
 	// Backup locale in case the user's locale isn't supported
 	const backupLocale = 'en';
 
-	const sortLocales = userLocale
-		? /*
-		   * Locale slugs could contain underscores, but Intl.Collator expects them to use dashes
-		   * @see https://developer.mozilla.org/en-US/docs/Glossary/BCP_47_language_tag
-		   */
-		  [ userLocale.replace( /_/g, '-' ), backupLocale ]
-		: [ backupLocale ];
-
-	return new Intl.Collator( sortLocales );
+	return new Intl.Collator( [ userLocale, backupLocale ] );
 };
 
 export default useIntlCollator;
--- client/dashboard/app/locale/index.ts
+++ client/dashboard/app/locale/index.ts
@@ -1,3 +1,4 @@
+import { getIntlLocale } from '../../utils/locale';
 import { useAuth } from '../auth';
 import { getUserLanguage } from '../shared-locale-loader';
 import { useSessionLocale } from './session-locale';
@@ -24,4 +25,9 @@ export function useLocale() {
 
 	const u = user as typeof user & ComputedAttributes;
 	return u.localeVariant || u.localeSlug || user.locale_variant || user.language || 'en';
+}
+
+// The locale as a BCP 47 language tag, safe to pass to `Intl` constructors.
+export function useIntlLocale() {
+	return getIntlLocale( useLocale() );
 }
