64e719faf559738f65a416d9d2fc55e05a29a26b
Griffith Chen <griffith.chen@automattic.com>
2026-09-11 14:58:01 +0800

    Login: restyle the existing-account notice (#114240)

--- client/blocks/login/test/password-reset-success-notice.jsx
+++ client/blocks/login/test/password-reset-success-notice.jsx
@@ -1,7 +1,6 @@
 /**
  * @jest-environment jsdom
  */
-import { screen } from '@testing-library/react';
 import PasswordResetSuccessNotice from 'calypso/blocks/login/password-reset-success-notice';
 import { renderWithProvider } from 'calypso/test-helpers/testing-library';
 
@@ -10,9 +9,9 @@ const renderAtPath = ( initialPath ) =>
 
 describe( 'PasswordResetSuccessNotice', () => {
 	test( 'shows the confirmation when password_reset is success', () => {
-		renderAtPath( '/log-in?password_reset=success' );
+		const { container } = renderAtPath( '/log-in?password_reset=success' );
 
-		expect( screen.getByText( 'Your password has been reset successfully.' ) ).toBeInTheDocument();
+		expect( container ).toHaveTextContent( 'Your password has been reset successfully.' );
 	} );
 
 	test( 'shows nothing when password_reset is absent', () => {
