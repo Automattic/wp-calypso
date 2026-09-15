5f2b7162b617a1835937cec4931240a16524b9a3
Griffith Chen <griffith.chen@automattic.com>
2026-09-15 14:27:15 +0800

    Launchpad: groundwork for dropping the data-stores dependency (#114325)

--- packages/launchpad/src/action-components/share-site-modal/index.tsx
+++ packages/launchpad/src/action-components/share-site-modal/index.tsx
@@ -13,13 +13,13 @@ import { __, sprintf } from '@wordpress/i18n';
 import { copy, share, check } from '@wordpress/icons';
 import { useState, useMemo, useCallback } from 'react';
 import { SocialLogo } from 'social-logos';
+import type { LaunchpadSiteDetails } from '../../site-type';
 import type { Task } from '../../types';
-import type { SiteDetails } from '@automattic/data-stores';
 import './style.scss';
 
 interface ShareSiteModalProps {
 	setModalIsOpen: ( isOpen: boolean ) => void;
-	site: SiteDetails | null;
+	site: LaunchpadSiteDetails | null;
 	task: Task | null;
 }
 
@@ -106,7 +106,7 @@ const getShareLinks = ( siteUrl: string, text: string ): ShareLink[] => {
 	];
 };
 
-const getSiteSlug = ( site: SiteDetails | null ) => {
+const getSiteSlug = ( site: LaunchpadSiteDetails | null ) => {
 	if ( ! site ) {
 		return '';
 	}
@@ -121,7 +121,7 @@ const getSiteSlug = ( site: SiteDetails | null ) => {
 	return '';
 };
 
-const getShareData = ( site: SiteDetails | null ) => {
+const getShareData = ( site: LaunchpadSiteDetails | null ) => {
 	const siteSlug = getSiteSlug( site );
 	return {
 		title: siteSlug,
