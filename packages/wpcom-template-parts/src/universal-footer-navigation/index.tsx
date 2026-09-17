ad2c05e83060b810bb4ddeee4022d1060f520ab8
Claudiu Filip <claudiu.filip@automattic.com>
2026-09-17 11:34:49 +0300

    Global Footer 2026: ship the app download QR as image files
    The QR is now two small SVG files (light and dark) shown with a regular
    image tag, instead of code pasted into the component, so it is easy to
    review and swap.

--- packages/wpcom-template-parts/src/universal-footer-navigation/index.tsx
+++ packages/wpcom-template-parts/src/universal-footer-navigation/index.tsx
@@ -10,8 +10,9 @@ import { __ } from '@wordpress/i18n';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect, useLayoutEffect, useState } from 'react';
 import { AutomatticBrand, getAutomatticBrandingNoun } from '../utils';
+import appDownloadQrDark from './assets/app-download-qr-dark.svg';
+import appDownloadQrLight from './assets/app-download-qr-light.svg';
 import {
-	AppDownloadQrCodeSvg,
 	AppStoreIconSvg,
 	ChevronSvg,
 	FacebookIconSvg,
@@ -134,6 +135,7 @@ const FooterStack = ( {
 );
 
 const Footer2026Bottom = ( {
+	colorway,
 	isLoggedIn,
 	locale,
 	currentRoute,
@@ -194,7 +196,13 @@ const Footer2026Bottom = ( {
 		</div>
 		<div className="lp-footer-bottom__downloads">
 			<a className="lp-footer-app-download" href={ APP_DOWNLOAD_URL }>
-				<AppDownloadQrCodeSvg />
+				<img
+					src={ colorway === 'dark' ? appDownloadQrDark : appDownloadQrLight }
+					width="34"
+					height="34"
+					alt=""
+					loading="lazy"
+				/>
 				<span>{ __( 'Download our app', __i18n_text_domain__ ) }</span>
 			</a>
 			<ul className="lp-footer-apps" aria-label={ __( 'Mobile Apps', __i18n_text_domain__ ) }>
@@ -396,6 +404,7 @@ export const PureUniversalNavbarFooter = ( {
 						</div>
 						{ colorway ? (
 							<Footer2026Bottom
+								colorway={ colorway }
 								isLoggedIn={ isLoggedIn }
 								locale={ locale }
 								currentRoute={ currentRoute }
