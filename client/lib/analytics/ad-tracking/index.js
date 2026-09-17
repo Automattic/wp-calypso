c82e8b8a017e7c9b9c0566635e7a58d5826565cf
Griffith Chen <griffith.chen@automattic.com>
2026-09-17 15:10:14 +0800

    Lib: remove unused helpers and exports from client/lib (#114409)

--- client/lib/analytics/ad-tracking/index.js
+++ client/lib/analytics/ad-tracking/index.js
@@ -4,10 +4,8 @@ export { adTrackSignupStart } from './ad-track-signup-start';
 export { recordAddToCart } from './record-add-to-cart';
 export { recordAliasInFloodlight } from './record-alias-in-floodlight';
 export { recordOrder } from './record-order';
-export { recordViewCheckout } from './record-view-checkout';
 export { retarget } from './retarget';
 export { retargetViewPlans } from './retarget-view-plans';
-export { tracksAnonymousUserId } from './track-anonymous-user-id';
 export * as GA4 from './google-analytics-4';
 export {
 	trackCustomFacebookConversionEvent,
